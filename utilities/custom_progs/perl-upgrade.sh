30b99650ad211972fbf03754034b9b34b7fc3ce9
Voncloft <voncloft@gmail.com>
2021-07-12 08:35:15 -0400

    edit perl-upgrade.sh script

--- utilities/custom_progs/perl-upgrade.sh
+++ utilities/custom_progs/perl-upgrade.sh
@@ -21,6 +21,7 @@ fi
 
 ###to get rid of <> tags in html
 grep -Po "(?<=>)[^<>]*(?=<)" $file | grep -v : | tr '[:upper:]' '[:lower:]' | grep tar.gz > test.txt
+grep -Po "(?<=>)[^<>]*(?=<)" $file | grep -v : | tr '[:upper:]' '[:lower:]' | grep .tgz > test.txt
 
 ###return only numbers
 #grep -E -o '\<[0-9]{1,2}\.[0-9]{2,5}\>' $file >> test.txt
@@ -29,35 +30,8 @@ grep -Po "(?<=>)[^<>]*(?=<)" $file | grep -v : | tr '[:upper:]' '[:lower:]' | gr
 #egrep -o "([0-9]{1,}\.)+[0-9]{1,}" $file > test.txt
 
 ###CUSTOM COMMANDS FOR WEBSITE STRIPPING###
-#sed -i -e "s/$name-//g" test.txt
 sed -i -e 's/.tar.gz//g' test.txt
-#sed -i -e 's/.tar.bz2//g' test.txt
-#sed -i -e '/stable/d' test.txt
-#sed -i -e "/$name/d" test.txt
-#sed -i -e "s/v//g" test.txt
-#sed -i -e 's/v//g' test.txt
 sed -i -e 's/^/perl-/' test.txt
-#sed -i -e 's/-0/\n0/g' test.txt
-#sed -i -e 's/-1/\n1/g' test.txt
-#sed -i -e 's/-2/\n2/g' test.txt
-#sed -i -e 's/-3/\n3/g' test.txt
-#sed -i -e 's/-4/\n4/g' test.txt
-#sed -i -e 's/-5/\n5/g' test.txt
-#sed -i -e 's/-6/\n6/g' test.txt
-#sed -i -e 's/-7/\n7/g' test.txt
-#sed -i -e 's/-8/\n8/g' test.txt
-#sed -i -e 's/-9/\n9/g' test.txt
-
-#sed -i -e 's/-v0/\n0/g' test.txt
-#sed -i -e 's/-v1/\n1/g' test.txt
-#sed -i -e 's/-v2/\n2/g' test.txt
-##sed -i -e 's/-v3/\n3/g' test.txt
-#sed -i -e 's/-v4/\n4/g' test.txt
-#sed -i -e 's/-v5/\n5/g' test.txt
-#sed -i -e 's/-v6/\n6/g' test.txt
-#sed -i -e 's/-v7/\n7/g' test.txt
-#sed -i -e 's/-v8/\n8/g' test.txt
-#sed -i -e 's/-v9/\n9/g' test.txt
 
 ###beta###
 command="cat test.txt"
