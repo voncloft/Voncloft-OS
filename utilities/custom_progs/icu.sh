b28818c96e33e42c33c67fbdd677d9c753441cbc
Voncloft <voncloft@gmail.com>
2021-08-21 19:40:07 -0400

    Minor Updates

--- utilities/custom_progs/icu.sh
+++ utilities/custom_progs/icu.sh
@@ -1,6 +1,6 @@
-url=https://rubygems.org/gems/$1/versions
-name=
-file=versions
+url=https://github.com/unicode-org/icu/tags
+name=icu
+file=tags
 echo $name
 cd /var/log/old
 
@@ -13,10 +13,10 @@ then
 fi
 
 ###show url in tags
-grep -i "href" $file | grep -Po '(?<=href=")[^"]*' | egrep -o "([0-9]{1,}\.)+[0-9]{1,}" > test.txt
+grep -i "href" $file | grep .tar.gz | grep -v cldr | grep -Po '(?<=href=")[^"]*' > test.txt
 
 ###to get rid of <> tags in html
-#grep -Po "(?<=>)[^<>]*(?=<)" $file > test.txt
+#grep -Po "(?<=>)[^<>]*(?=<)" $file | grep "tar.gz"  > test.txt
 
 ###return only numbers
 #grep -E -o '\<[0-9]{1,2}\.[0-9]{2,5}\>' $file >> test.txt
@@ -25,8 +25,9 @@ grep -i "href" $file | grep -Po '(?<=href=")[^"]*' | egrep -o "([0-9]{1,}\.)+[0-
 #egrep -o "([0-9]{1,}\.)+[0-9]{1,}" $file > test.txt
 
 ###CUSTOM COMMANDS FOR WEBSITE STRIPPING###
-#sed -i -e "s/$name-//g" test.txt
-#sed -i -e 's/.tar.gz//g' test.txt
+sed -i -e "s/release-//g" test.txt
+sed -i -e 's/.tar.gz//g' test.txt
+
 #sed -i -e 's/.tar.bz2//g' test.txt
 #sed -i -e '/stable/d' test.txt
 #sed -i -e "/$name/d" test.txt
@@ -34,19 +35,20 @@ grep -i "href" $file | grep -Po '(?<=href=")[^"]*' | egrep -o "([0-9]{1,}\.)+[0-
 #sed -i -e 's/v//g' test.txt
 
 ###beta###
-command="cat test.txt | sort -V -r | head -n 1"
-eval $command
+command="cat test.txt"
+final=$(eval $command | head -n 1)
+echo ${final##*/}
 size=$(eval $command)
 
 check=${#size}
 
-#if [ $check -ge 1 ];
-#then
+if [ $check -ge 1 ];
+then
 	##Production###
 	#if [ -f $file ];
 	#then
-	#	echo $name >> stripped_info.txt
-	#	eval $command >> stripped_info.txt
-	#	rm -v $file
+		echo $name >> stripped_info.txt
+		echo ${final##*/} >> stripped_info.txt
+		rm -v $file
 	#fi
-#fi
+fi
