d9650afc10eb83e066b2b5bdb2d871d5f85a68f6
Voncloft <voncloft@gmail.com>
2021-09-14 13:14:52 -0400

    created a beta script to be universal in the works

---
+++ utilities/beta/update.v1.1.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+get_url()
+{
+	source $1
+	url="${source[0]}"
+	url_finalized=$(echo $url | cut -d ' ' -f1 | sed 's|\(.*\)/.*|\1|')/
+	
+}
+fetch() {
+	echo $PWD
+        wget $url_finalized
+        echo "wget $url_finalized"
+}
+main()
+{
+	get_url $1
+
+
+	echo "URL:       $url_finalized"
+	echo "Filename:  $name"
+	echo "Version:   $version"
+	fetch
+}
+main $@
