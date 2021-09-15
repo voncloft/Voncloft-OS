5d570ecce74714d72cc29aaf4ce1c86b71a9fcb7
Voncloft <voncloft@gmail.com>
2021-09-15 10:27:57 -0400

    Minor Updates

--- utilities/beta/update.v1.1.sh
+++ utilities/beta/update.v1.1.sh
@@ -15,14 +15,14 @@ vercomp() {
 get_url()
 {
 	source $1
+	ppath=$1
 	url="${source[0]}"
 	#url_finalized=$(echo $url | cut -d ' ' -f1 | sed 's|\(.*\)/.*|\1|')/
 	url=$(echo $url | cut -d ' ' -f1 | sed 's|\(.*\)/.*|\1|')/
 }
 fetch() {
 	#echo $PWD
-        wget $url
-        echo "wget $url"
+        wget -q $url
 }
 alter_per_url() {
         case $url in
@@ -44,55 +44,125 @@ alter_per_url() {
                         url=https://ftp.gnome.org/pub/gnome/sources/$filename/cache.json;;
                 *archive.xfce.org*)
                         url=http://archive.xfce.org/src/$(echo $url | cut -d / -f5)/$name/;;
+		*/plasma/*)
+			url="$(echo $url | cut -d / -f1-5)/"
+			;;
+                */frameworks/*)
+                        url="$(echo $url | cut -d / -f1-5)/"
+                        ;;
+		*.kde.*/*/release-service/*)
+			url="$(echo $url | cut -d / -f1-5)/"
+			;;
                 *)
-                	url="wee";;
+                	
 
         esac
 }
-scan_packages
+cmd_torun()
 {
         case $url in
 		#under each function maybe have a .sh ran and eval it
                 *github.com*)
+                	cmd="github"
+                	fetch
+                	;;
                 *downloads.sourceforge.net*)
+                	cmd="sourceforge"
+                	fetch
+                	;;
                 *sourceforge.net*)
+                	cmd="sourcefoge"
+                	fetch
+                	;;
                 *gitlab.com*)
+                	cmd="gitlab"
+			fetch
+                	;;
                 *python.org*|*pypi.org*|*pythonhosted.org*|*pypi.io*|*pypi.org*)
+                	cmd="python"
+                	fetch;;
                 *rubygems.org*)
+                	cmd="ruby"
+                	fetch
+                	;;
                 *launchpad.net*)
+                	cmd="launchpad"
+			fetch
+                	;;
                 *ftp.gnome.org*)
+                	cmd="gnome"
+                	fetch
+                	;;
                 *archive.xfce.org*)
+                	cmd="xfce"
+                	fetch
+                	;;
+		*/plasma/*)
+			cmd="plasma"
+			rm test.txt
+			fetch 
+			egrep -o "([0-9]{1,}\.)+[0-9]{1,}" index.html >> test.txt
+                        uversion=$(cat test.txt | sort -V -r | head -n 1)
+			;;
+                */frameworks/*)
+                	cmd="frameworks"
+                	fetch
+                	rm test.txt
+                	egrep -o "([0-9]{1,}\.)+[0-9]{1,}" index.html >> test.txt
+                	uversion=$(cat test.txt | sort -V -r | head -n 1)
+                	uversion=${uversion}.0
+                	;;
+                *.kde.*/*/release-service/*)
+                	cmd="kde-apps"
+                	fetch
+                	rm test.txt
+                	egrep -o "([0-9]{1,}\.)+[0-9]{1,}" index.html >> test.txt
+                	uversion=$(cat test.txt | sort -V -r | head -n 1)
+                	;;
                 *)
+                	cmd="generic"
+			fetch
+                	;;
 
         esac
 }
 main()
 {
 	rm index.html
-	uversion="7.0"
+	#options="-q"
+	#uversion="7.0"
 	echo "Ignoring: $ignoring"
 	echo ""
 	get_url $1
+	echo "PPath:     $ppath"
 	echo "URL:       $url"
 	echo "Filename:  $name"
 	echo "Version:   $version"
-	echo "Upgraded:  $uversion"
+
 	alter_per_url
-	echo "New URL: $url"
-	#fetch
+	echo "New URL:   $url"
+	cmd_torun
+	echo "Command:   $cmd"
+	echo "Upgraded:  $uversion"
         #find upgraded version from fetch code compare and then upgrade package using uversion as variable name
-        #if [ "$uversion" != "$version" ];
-        #then
-        #	vercomp $uversion $version
-        #	if [ $? = 2 ]; then
-        #		#do things
-        #		echo "NEW"
-        #	elif [ $? = 1 ];then
-        #		echo "OLD"
-        #	else
-        #		echo "Same"
-        #	fi
-        #fi
+	check=${#uversion}
+	if [ $check -ge 1 ];
+	then
+        	if [ "$uversion" != "$version" ];
+        	then
+        	vercomp $uversion $version
+        		if [ $? = 2 ]; then
+        		#do things
+        			echo "NEW"
+        			sed -i -e "s/version=$version/version=$uversion/g" $ppath
+        		
+        		elif [ $? = 1 ];then
+        			echo "OLD"
+        		else
+        			echo "Same"
+        		fi
+        	fi
+	fi
 }
 ignoring="kf5 plasma kde-apps python perl"
 main $@
