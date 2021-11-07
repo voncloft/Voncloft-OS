f621dd3501d16542f3a9acdd2d965eb87cf1c1d0
Voncloft <voncloft@gmail.com>
2021-11-07 09:27:36 -0500

    upgraded beta qt6 and added override scripts for it

--- test_updates.sh
+++
@@ -1,4 +0,0 @@
-sed -i -e "s/version=5.15.1/version=5.15.2/g" qt/qt5-x11extras/spkgbuild
-changelog "qt/qt5-x11extras/spkgbuild" "Upgraded from version 5.15.1 to version 5.15.2"
-sed -i -e "s/version=5.15.1/version=5.15.2/g" qt/qtwebengine/spkgbuild
-changelog "qt/qtwebengine/spkgbuild" "Upgraded from version 5.15.1 to version 5.15.2"
