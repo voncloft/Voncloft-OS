e7608f3d3cbd34e1a05ea9b045b2c7b8e1d3944f
Voncloft <voncloft@gmail.com>
2021-11-13 16:04:15 -0500

    Minor Updates

--- test_updates.sh
+++
@@ -1,3 +0,0 @@
-sed -i -e "s,,,g" beta/plasma-meta/spkgbuild
-sed -i -e "s/version=5.21.1/version=5.23/g" beta/plasma-meta/spkgbuild
-changelog "beta/plasma-meta/spkgbuild" "Upgraded from version 5.21.1 to version 5.23"
