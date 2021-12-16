fce6ac205f8a755baed1454e67f65ee94a582ba2
Voncloft <voncloft@gmail.com>
2021-12-16 07:32:10 -0500

    Added dependencies to plasma packages

---
+++ old-test-updates/test_updates-12-16-21.sh
@@ -0,0 +1,24 @@
+sed -i -e "s/version=3.8.0/version=3.10.0/g" core/fortune-mod/spkgbuild
+changelog "core/fortune-mod/spkgbuild" "Upgraded from version 3.8.0 to version 3.10.0"
+sed -i -e "s/version=3.2.4/version=3.2.5/g" extra/flexget/spkgbuild
+changelog "extra/flexget/spkgbuild" "Upgraded from version 3.2.4 to version 3.2.5"
+sed -i -e "s/version=96.0b4/version=96.0b5/g" networking/firefox/spkgbuild
+changelog "networking/firefox/spkgbuild" "Upgraded from version 96.0b4 to version 96.0b5"
+sed -i -e "s/version=2.5.4/version=2.5.5/g" networking/openvpn/spkgbuild
+changelog "networking/openvpn/spkgbuild" "Upgraded from version 2.5.4 to version 2.5.5"
+sed -i -e "s/version=3.13.0/version=3.13.1/g" python/python-django-rest-framework/spkgbuild
+changelog "python/python-django-rest-framework/spkgbuild" "Upgraded from version 3.13.0 to version 3.13.1"
+sed -i -e "s/version=4.28.3/version=4.28.4/g" python/python-fonttools/spkgbuild
+changelog "python/python-fonttools/spkgbuild" "Upgraded from version 4.28.3 to version 4.28.4"
+sed -i -e "s/version=3.4.1/version=3.4.3/g" python/python-hunter/spkgbuild
+changelog "python/python-hunter/spkgbuild" "Upgraded from version 3.4.1 to version 3.4.3"
+sed -i -e "s/version=6.31.5/version=6.31.6/g" python/python-hypothesis/spkgbuild
+changelog "python/python-hypothesis/spkgbuild" "Upgraded from version 6.31.5 to version 6.31.6"
+sed -i -e "s/version=4.2.1/version=4.3.0/g" python/python-jsonschema/spkgbuild
+changelog "python/python-jsonschema/spkgbuild" "Upgraded from version 4.2.1 to version 4.3.0"
+sed -i -e "s/version=1.6.0/version=1.7.1/g" python/python-lazy-object-proxy/spkgbuild
+changelog "python/python-lazy-object-proxy/spkgbuild" "Upgraded from version 1.6.0 to version 1.7.1"
+sed -i -e "s/version=10.16.0/version=10.16.1/g" python/python-rich/spkgbuild
+changelog "python/python-rich/spkgbuild" "Upgraded from version 10.16.0 to version 10.16.1"
+sed -i -e "s/version=21.1.1/version=21.1.2/g" xorg/xorg-server/spkgbuild
+changelog "xorg/xorg-server/spkgbuild" "Upgraded from version 21.1.1 to version 21.1.2"
