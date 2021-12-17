67c5ea38b0639f476db9cad4fb31216683b84f2d
Voncloft <voncloft@gmail.com>
2021-12-17 16:14:31 -0500

    casual updates

---
+++ old-test-updates/test_updates-12-17-21.sh
@@ -0,0 +1,53 @@
+sed -i -e "s/version=5.2.5/version=5.2.6/g" cinnamon/cinnamon/spkgbuild
+changelog "cinnamon/cinnamon/spkgbuild" "Upgraded from version 5.2.5 to version 5.2.6"
+sed -i -e "s/version=5.2.0/version=5.2.1/g" cinnamon/cinnamon-control-center/spkgbuild
+changelog "cinnamon/cinnamon-control-center/spkgbuild" "Upgraded from version 5.2.0 to version 5.2.1"
+sed -i -e "s/version=5.2.1/version=5.2.2/g" cinnamon/nemo/spkgbuild
+changelog "cinnamon/nemo/spkgbuild" "Upgraded from version 5.2.1 to version 5.2.2"
+sed -i -e "s/version=1.13.18/version=1.13.20/g" core/dbus/spkgbuild
+changelog "core/dbus/spkgbuild" "Upgraded from version 1.13.18 to version 1.13.20"
+sed -i -e "s/version=3.0.2/version=3.0.3/g" core/dkms/spkgbuild
+changelog "core/dkms/spkgbuild" "Upgraded from version 3.0.2 to version 3.0.3"
+sed -i -e "s/version=0.90.1/version=0.91.0/g" core/hugo/spkgbuild
+changelog "core/hugo/spkgbuild" "Upgraded from version 0.90.1 to version 0.91.0"
+sed -i -e "s/version=3.73/version=3.73.1/g" core/nss/spkgbuild
+changelog "core/nss/spkgbuild" "Upgraded from version 3.73 to version 3.73.1"
+sed -i -e "s/version=0.3.41/version=0.3.42/g" core/pipewire/spkgbuild
+changelog "core/pipewire/spkgbuild" "Upgraded from version 0.3.41 to version 0.3.42"
+sed -i -e "s/version=3.2.5/version=3.2.6/g" extra/flexget/spkgbuild
+changelog "extra/flexget/spkgbuild" "Upgraded from version 3.2.5 to version 3.2.6"
+sed -i -e "s/version=4.32/version=4.33/g" extra/nspr/spkgbuild
+changelog "extra/nspr/spkgbuild" "Upgraded from version 4.32 to version 4.33"
+sed -i -e "s/version=1.3.4/version=1.4.0/g" multilib/libglvnd-32/spkgbuild
+changelog "multilib/libglvnd-32/spkgbuild" "Upgraded from version 1.3.4 to version 1.4.0"
+sed -i -e "s/version=1.3.4/version=1.4.0/g" libs/libglvnd/spkgbuild
+changelog "libs/libglvnd/spkgbuild" "Upgraded from version 1.3.4 to version 1.4.0"
+sed -i -e "s/version=4.4.26/version=4.4.27/g" libs/libxcrypt/spkgbuild
+changelog "libs/libxcrypt/spkgbuild" "Upgraded from version 4.4.26 to version 4.4.27"
+sed -i -e "s/version=96.0b5/version=96.0b6/g" networking/firefox/spkgbuild
+changelog "networking/firefox/spkgbuild" "Upgraded from version 96.0b5 to version 96.0b6"
+sed -i -e "s/version=2021.06.06/version=2021.12.17/g" networking/youtube-dl/spkgbuild
+changelog "networking/youtube-dl/spkgbuild" "Upgraded from version 2021.06.06 to version 2021.12.17"
+sed -i -e "s/version=1.11.0/version=1.12.0/g" python/python-flake8-typing-imports/spkgbuild
+changelog "python/python-flake8-typing-imports/spkgbuild" "Upgraded from version 1.11.0 to version 1.12.0"
+sed -i -e "s,https://files.pythonhosted.org/packages/source/g/google-api-core/google-api-core-2.3.0.tar.gz,https://files.pythonhosted.org/packages/source/g/google-api-core/google-api-core-2.3.2.tar.gz,g" python/python-google-api-core/spkgbuild
+sed -i -e "s/version=2.3.0/version=2.3.2/g" python/python-google-api-core/spkgbuild
+changelog "python/python-google-api-core/spkgbuild" "Upgraded from version 2.3.0 to version 2.3.2"
+sed -i -e "s/version=4.8.2/version=4.9.0/g" python/python-importlib-metadata/spkgbuild
+changelog "python/python-importlib-metadata/spkgbuild" "Upgraded from version 4.8.2 to version 4.9.0"
+sed -i -e "s/version=0.7.0/version=1.0.0/g" python/python-janus/spkgbuild
+changelog "python/python-janus/spkgbuild" "Upgraded from version 0.7.0 to version 1.0.0"
+sed -i -e "s/version=4.3.0/version=4.3.1/g" python/python-jsonschema/spkgbuild
+changelog "python/python-jsonschema/spkgbuild" "Upgraded from version 4.3.0 to version 4.3.1"
+sed -i -e "s,https://files.pythonhosted.org/packages/source/m/moto/moto-2.2.17.tar.gz,https://files.pythonhosted.org/packages/source/m/moto/moto-2.2.19.tar.gz,g" python/python-moto/spkgbuild
+sed -i -e "s/version=2.2.17/version=2.2.19/g" python/python-moto/spkgbuild
+changelog "python/python-moto/spkgbuild" "Upgraded from version 2.2.17 to version 2.2.19"
+sed -i -e "s/version=0.16.0/version=0.16.1/g" python/python-myst-parser/spkgbuild
+changelog "python/python-myst-parser/spkgbuild" "Upgraded from version 0.16.0 to version 0.16.1"
+sed -i -e "s/version=21.4.0/version=21.5.0/g" python/python-structlog/spkgbuild
+changelog "python/python-structlog/spkgbuild" "Upgraded from version 21.4.0 to version 21.5.0"
+sed -i -e "s/version=4.3.0/version=5.0.0/g" python/python-ujson/spkgbuild
+changelog "python/python-ujson/spkgbuild" "Upgraded from version 4.3.0 to version 5.0.0"
+sed -i -e "s,https://pypi.org/packages/source/p/python-utils/python-utils-2.5.6.tar.gz,https://pypi.org/packages/source/p/python-utils/python-utils-2.6.0.tar.gz,g" python/python-utils/spkgbuild
+sed -i -e "s/version=2.5.6/version=2.6.0/g" python/python-utils/spkgbuild
+changelog "python/python-utils/spkgbuild" "Upgraded from version 2.5.6 to version 2.6.0"
---
+++ test_updates-12-17-21.sh
@@ -0,0 +1,27 @@
+sed -i -e "s/version=5.2.5/version=5.2.6/g" cinnamon/cinnamon/spkgbuild
+changelog "cinnamon/cinnamon/spkgbuild" "Upgraded from version 5.2.5 to version 5.2.6"
+sed -i -e "s/version=5.2.0/version=5.2.1/g" cinnamon/cinnamon-control-center/spkgbuild
+changelog "cinnamon/cinnamon-control-center/spkgbuild" "Upgraded from version 5.2.0 to version 5.2.1"
+sed -i -e "s/version=5.2.1/version=5.2.2/g" cinnamon/nemo/spkgbuild
+changelog "cinnamon/nemo/spkgbuild" "Upgraded from version 5.2.1 to version 5.2.2"
+sed -i -e "s/version=3.73/version=3.73.1/g" core/nss/spkgbuild
+changelog "core/nss/spkgbuild" "Upgraded from version 3.73 to version 3.73.1"
+sed -i -e "s/version=0.3.41/version=0.3.42/g" core/pipewire/spkgbuild
+changelog "core/pipewire/spkgbuild" "Upgraded from version 0.3.41 to version 0.3.42"
+sed -i -e "s/version=3.2.5/version=3.2.6/g" extra/flexget/spkgbuild
+changelog "extra/flexget/spkgbuild" "Upgraded from version 3.2.5 to version 3.2.6"
+sed -i -e "s/version=2021.06.06/version=2021.12.17/g" networking/youtube-dl/spkgbuild
+changelog "networking/youtube-dl/spkgbuild" "Upgraded from version 2021.06.06 to version 2021.12.17"
+sed -i -e "s/version=1.11.0/version=1.12.0/g" python/python-flake8-typing-imports/spkgbuild
+changelog "python/python-flake8-typing-imports/spkgbuild" "Upgraded from version 1.11.0 to version 1.12.0"
+sed -i -e "s/version=4.8.2/version=4.8.3/g" python/python-importlib-metadata/spkgbuild
+changelog "python/python-importlib-metadata/spkgbuild" "Upgraded from version 4.8.2 to version 4.8.3"
+sed -i -e "s/version=0.16.0/version=0.16.1/g" python/python-myst-parser/spkgbuild
+changelog "python/python-myst-parser/spkgbuild" "Upgraded from version 0.16.0 to version 0.16.1"
+sed -i -e "s/version=21.4.0/version=21.5.0/g" python/python-structlog/spkgbuild
+changelog "python/python-structlog/spkgbuild" "Upgraded from version 21.4.0 to version 21.5.0"
+sed -i -e "s/version=4.3.0/version=5.0.0/g" python/python-ujson/spkgbuild
+changelog "python/python-ujson/spkgbuild" "Upgraded from version 4.3.0 to version 5.0.0"
+sed -i -e "s,https://pypi.org/packages/source/p/python-utils/python-utils-2.5.6.tar.gz,https://pypi.org/packages/source/p/python-utils/python-utils-2.6.0.tar.gz,g" python/python-utils/spkgbuild
+sed -i -e "s/version=2.5.6/version=2.6.0/g" python/python-utils/spkgbuild
+changelog "python/python-utils/spkgbuild" "Upgraded from version 2.5.6 to version 2.6.0"
