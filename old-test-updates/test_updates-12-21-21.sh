3647e7f12895a3e479e9c484618daefcbe64cecd
Voncloft <voncloft@gmail.com>
2021-12-21 07:54:40 -0500

    casual updates

---
+++ old-test-updates/test_updates-12-21-21.sh
@@ -0,0 +1,58 @@
+sed -i -e "s/version=7.2.0/version=7.4.0/g" core/mlt/spkgbuild
+changelog "core/mlt/spkgbuild" "Upgraded from version 7.2.0 to version 7.4.0"
+sed -i -e "s/version=4.9/version=4.10/g" core/shadow/spkgbuild
+changelog "core/shadow/spkgbuild" "Upgraded from version 4.9 to version 4.10"
+sed -i -e "s/version=2.10.28/version=2.10.30/g" extra/gimp/spkgbuild
+changelog "extra/gimp/spkgbuild" "Upgraded from version 2.10.28 to version 2.10.30"
+sed -i -e "s/version=4.8/version=4.9/g" extra/gnucash/spkgbuild
+changelog "extra/gnucash/spkgbuild" "Upgraded from version 4.8 to version 4.9"
+sed -i -e "s/version=1.1.0rc3/version=1.1.1rc1/g" extra/solaar/spkgbuild
+changelog "extra/solaar/spkgbuild" "Upgraded from version 1.1.0rc3 to version 1.1.1rc1"
+sed -i -e "s/version=2.4.1/version=2.4.2/g" libs/expat/spkgbuild
+changelog "libs/expat/spkgbuild" "Upgraded from version 2.4.1 to version 2.4.2"
+sed -i -e "s/version=96.0b6/version=96.0b7/g" networking/firefox/spkgbuild
+changelog "networking/firefox/spkgbuild" "Upgraded from version 96.0b6 to version 96.0b7"
+sed -i -e "s/version=5.0.8/version=5.0.9/g" python/python-amqp/spkgbuild
+changelog "python/python-amqp/spkgbuild" "Upgraded from version 5.0.8 to version 5.0.9"
+sed -i -e "s/version=4.0.1/version=4.0.2/g" python/python-async-timeout/spkgbuild
+changelog "python/python-async-timeout/spkgbuild" "Upgraded from version 4.0.1 to version 4.0.2"
+sed -i -e "s/version=1.20.24/version=1.20.25/g" python/python-boto3/spkgbuild
+changelog "python/python-boto3/spkgbuild" "Upgraded from version 1.20.24 to version 1.20.25"
+sed -i -e "s/version=1.23.24/version=1.23.25/g" python/python-botocore/spkgbuild
+changelog "python/python-botocore/spkgbuild" "Upgraded from version 1.23.24 to version 1.23.25"
+sed -i -e "s,https://pypi.io/packages/source/c/cogapp/cogapp-3.2.0.tar.gz,https://pypi.io/packages/source/c/cogapp/cogapp-3.3.0.tar.gz,g" python/python-cogapp/spkgbuild
+sed -i -e "s/version=3.2.0/version=3.3.0/g" python/python-cogapp/spkgbuild
+changelog "python/python-cogapp/spkgbuild" "Upgraded from version 3.2.0 to version 3.3.0"
+sed -i -e "s,https://pypi.io/packages/source/c/cryptography/cryptography-36.0.0.tar.gz,https://pypi.io/packages/source/c/cryptography/cryptography-36.0.1.tar.gz,g" python/python-cryptography/spkgbuild
+sed -i -e "s/version=36.0.0/version=36.0.1/g" python/python-cryptography/spkgbuild
+changelog "python/python-cryptography/spkgbuild" "Upgraded from version 36.0.0 to version 36.0.1"
+sed -i -e "s,https://pypi.io/packages/source/c/cryptography-vectors/cryptography_vectors-36.0.0.tar.gz,https://pypi.io/packages/source/c/cryptography-vectors/cryptography_vectors-36.0.1.tar.gz,g" python/python-cryptography-vectors/spkgbuild
+sed -i -e "s/version=36.0.0/version=36.0.1/g" python/python-cryptography-vectors/spkgbuild
+changelog "python/python-cryptography-vectors/spkgbuild" "Upgraded from version 36.0.0 to version 36.0.1"
+sed -i -e "s/version=1.0.8/version=1.0.9/g" python/python-filetype/spkgbuild
+changelog "python/python-filetype/spkgbuild" "Upgraded from version 1.0.8 to version 1.0.9"
+sed -i -e "s/version=1.16/version=1.17/g" python/python-funcy/spkgbuild
+changelog "python/python-funcy/spkgbuild" "Upgraded from version 1.16 to version 1.17"
+sed -i -e "s/version=4.9.0/version=4.10.0/g" python/python-importlib-metadata/spkgbuild
+changelog "python/python-importlib-metadata/spkgbuild" "Upgraded from version 4.9.0 to version 4.10.0"
+sed -i -e "s,https://pypi.io/packages/source/j/josepy/josepy-1.10.0.tar.gz,https://pypi.io/packages/source/j/josepy/josepy-1.11.0.tar.gz,g" python/python-josepy/spkgbuild
+sed -i -e "s/version=1.10.0/version=1.11.0/g" python/python-josepy/spkgbuild
+changelog "python/python-josepy/spkgbuild" "Upgraded from version 1.10.0 to version 1.11.0"
+sed -i -e "s/version=4.3.1/version=4.3.2/g" python/python-jsonschema/spkgbuild
+changelog "python/python-jsonschema/spkgbuild" "Upgraded from version 4.3.1 to version 4.3.2"
+sed -i -e "s,https://pypi.io/packages/source/l/logilab-common/logilab-common-1.8.1.tar.gz,https://pypi.io/packages/source/l/logilab-common/logilab-common-1.8.2.tar.gz,g" python/python-logilab-common/spkgbuild
+sed -i -e "s/version=1.8.1/version=1.8.2/g" python/python-logilab-common/spkgbuild
+changelog "python/python-logilab-common/spkgbuild" "Upgraded from version 1.8.1 to version 1.8.2"
+sed -i -e "s/version=0.15.0/version=0.17.1/g" python/python-pytest-httpx/spkgbuild
+changelog "python/python-pytest-httpx/spkgbuild" "Upgraded from version 0.15.0 to version 0.17.1"
+sed -i -e "s/version=6.0.2/version=6.1.1/g" python/python-rdflib/spkgbuild
+changelog "python/python-rdflib/spkgbuild" "Upgraded from version 6.0.2 to version 6.1.1"
+sed -i -e "s/version=59.7.0/version=60.0.1/g" python/python-setuptools/spkgbuild
+changelog "python/python-setuptools/spkgbuild" "Upgraded from version 59.7.0 to version 60.0.1"
+sed -i -e "s,https://pypi.io/packages/source/S/SQLAlchemy/SQLAlchemy-1.4.25.tar.gz,https://pypi.io/packages/source/S/SQLAlchemy/SQLAlchemy-1.4.26.tar.gz,g" python/python-sqlalchemy/spkgbuild
+sed -i -e "s/version=1.4.25/version=1.4.26/g" python/python-sqlalchemy/spkgbuild
+changelog "python/python-sqlalchemy/spkgbuild" "Upgraded from version 1.4.25 to version 1.4.26"
+sed -i -e "s/version=5.0.0/version=5.1.0/g" python/python-ujson/spkgbuild
+changelog "python/python-ujson/spkgbuild" "Upgraded from version 5.0.0 to version 5.1.0"
+sed -i -e "s/version=2.4.51/version=2.4.52/g" server/httpd/spkgbuild
+changelog "server/httpd/spkgbuild" "Upgraded from version 2.4.51 to version 2.4.52"
