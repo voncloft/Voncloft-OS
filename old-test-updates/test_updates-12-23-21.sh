4f6e5a7e0132d7461e97036267ea11c7ab980914
Voncloft <voncloft@gmail.com>
2021-12-23 16:43:33 -0500

    casual updates

---
+++ old-test-updates/test_updates-12-23-21.sh
@@ -0,0 +1,44 @@
+sed -i -e "s/version=0.60.2/version=0.60.3/g" compilers/meson/spkgbuild
+changelog "compilers/meson/spkgbuild" "Upgraded from version 0.60.2 to version 0.60.3"
+sed -i -e "s/version=3.1.6/version=3.1.7/g" core/double-conversion/spkgbuild
+changelog "core/double-conversion/spkgbuild" "Upgraded from version 3.1.6 to version 3.1.7"
+sed -i -e "s/version=0.91.0/version=0.91.2/g" core/hugo/spkgbuild
+changelog "core/hugo/spkgbuild" "Upgraded from version 0.91.0 to version 0.91.2"
+sed -i -e "s/version=3.2.6/version=3.2.7/g" extra/flexget/spkgbuild
+changelog "extra/flexget/spkgbuild" "Upgraded from version 3.2.6 to version 3.2.7"
+sed -i -e "s/version=1.5.0/version=1.5.1/g" media/chromaprint/spkgbuild
+changelog "media/chromaprint/spkgbuild" "Upgraded from version 1.5.0 to version 1.5.1"
+sed -i -e "s/version=96.0b7/version=96.0b8/g" networking/firefox/spkgbuild
+changelog "networking/firefox/spkgbuild" "Upgraded from version 96.0b7 to version 96.0b8"
+sed -i -e "s,https://search.cpan.org/CPAN/authors/id/O/OA/OALDERS/HTTP-Message-6.33.tar.gz,https://search.cpan.org/CPAN/authors/id/O/OA/OALDERS/HTTP-Message-6.34.tar.gz,g" perl/perl-http-message/spkgbuild
+sed -i -e "s/version=6.33/version=6.34/g" perl/perl-http-message/spkgbuild
+changelog "perl/perl-http-message/spkgbuild" "Upgraded from version 6.33 to version 6.34"
+sed -i -e "s/version=6.31.6/version=6.32.1/g" python/python-hypothesis/spkgbuild
+changelog "python/python-hypothesis/spkgbuild" "Upgraded from version 6.31.6 to version 6.32.1"
+sed -i -e "s,https://pypi.io/packages/source/j/jupyterlab_server/jupyterlab_server-2.9.0.tar.gz,https://pypi.io/packages/source/j/jupyterlab_server/jupyterlab_server-2.10.1.tar.gz,g" python/python-jupyterlab_server/spkgbuild
+sed -i -e "s/version=2.9.0/version=2.10.1/g" python/python-jupyterlab_server/spkgbuild
+changelog "python/python-jupyterlab_server/spkgbuild" "Upgraded from version 2.9.0 to version 2.10.1"
+sed -i -e "s/version=0.17.1/version=0.17.2/g" python/python-pytest-httpx/spkgbuild
+changelog "python/python-pytest-httpx/spkgbuild" "Upgraded from version 0.17.1 to version 0.17.2"
+sed -i -e "s/version=1.11.3/version=2.0.0/g" python/python-qtpy/spkgbuild
+changelog "python/python-qtpy/spkgbuild" "Upgraded from version 1.11.3 to version 2.0.0"
+sed -i -e "s/version=60.0.3/version=60.0.5/g" python/python-setuptools/spkgbuild
+changelog "python/python-setuptools/spkgbuild" "Upgraded from version 60.0.3 to version 60.0.5"
+sed -i -e "s,https://files.pythonhosted.org/packages/source/s/sphinx-jinja/sphinx-jinja-1.1.1.tar.gz,git+https://github.com/tardyp/sphinx-jinja.git?signed#tag=,g" python/python-sphinx-jinja/spkgbuild
+sed -i -e "s/version=1.1.1/version=1.2.1/g" python/python-sphinx-jinja/spkgbuild
+changelog "python/python-sphinx-jinja/spkgbuild" "Upgraded from version 1.1.1 to version 1.2.1"
+sed -i -e "s/version=2.1/version=2.2/g" python/python-sphobjinv/spkgbuild
+changelog "python/python-sphobjinv/spkgbuild" "Upgraded from version 2.1 to version 2.2"
+sed -i -e "s,https://pypi.io/packages/source/S/SQLAlchemy/SQLAlchemy-1.4.26.tar.gz,https://pypi.io/packages/source/S/SQLAlchemy/SQLAlchemy-1.4.29.tar.gz,g" python/python-sqlalchemy/spkgbuild
+sed -i -e "s/version=1.4.26/version=1.4.29/g" python/python-sqlalchemy/spkgbuild
+changelog "python/python-sqlalchemy/spkgbuild" "Upgraded from version 1.4.26 to version 1.4.29"
+sed -i -e "s/version=0.37.0/version=0.37.1/g" python/python-wheel/spkgbuild
+changelog "python/python-wheel/spkgbuild" "Upgraded from version 0.37.0 to version 0.37.1"
+sed -i -e "s/version=3.0.0/version=3.0.1/g" python/python-wtforms/spkgbuild
+changelog "python/python-wtforms/spkgbuild" "Upgraded from version 3.0.0 to version 3.0.1"
+sed -i -e "s/version=1.9.1/version=1.9.2/g" python/python-xmlschema/spkgbuild
+changelog "python/python-xmlschema/spkgbuild" "Upgraded from version 1.9.1 to version 1.9.2"
+sed -i -e "s/version=2.3.0/version=2.3.1/g" ruby-gems/ruby-bundler/spkgbuild
+changelog "ruby-gems/ruby-bundler/spkgbuild" "Upgraded from version 2.3.0 to version 2.3.1"
+sed -i -e "s/version=1.4.4/version=1.5.0/g" ruby-gems/ruby-diff-lcs/spkgbuild
+changelog "ruby-gems/ruby-diff-lcs/spkgbuild" "Upgraded from version 1.4.4 to version 1.5.0"
