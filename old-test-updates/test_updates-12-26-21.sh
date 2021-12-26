0573ad45dc48c94653dd20422672f99882ec64a0
Voncloft <voncloft@gmail.com>
2021-12-26 09:13:08 -0500

    casual updates

---
+++ old-test-updates/test_updates-12-26-21.sh
@@ -0,0 +1,32 @@
+sed -i -e "s/version=2021.12.01/version=2021.12.25/g" networking/yt-dlp/spkgbuild
+changelog "networking/yt-dlp/spkgbuild" "Upgraded from version 2021.12.01 to version 2021.12.25"
+sed -i -e "s,https://files.pythonhosted.org/packages/source/a/apipkg/apipkg-1.5.tar.gz,https://files.pythonhosted.org/packages/source/a/apipkg/apipkg-2.1.0.tar.gz,g" python/python-apipkg/spkgbuild
+sed -i -e "s/version=1.5/version=2.1.0/g" python/python-apipkg/spkgbuild
+changelog "python/python-apipkg/spkgbuild" "Upgraded from version 1.5 to version 2.1.0"
+sed -i -e "s/version=1.2.2/version=1.3.0/g" python/python-basemap/spkgbuild
+changelog "python/python-basemap/spkgbuild" "Upgraded from version 1.2.2 to version 1.3.0"
+sed -i -e "s,https://pypi.io/packages/source/c/celery/celery-5.1.2.tar.gz,https://pypi.io/packages/source/c/celery/celery-5.2.1.tar.gz,g" python/python-celery/spkgbuild
+sed -i -e "s/version=5.2.0/version=5.2.1/g" python/python-celery/spkgbuild
+changelog "python/python-celery/spkgbuild" "Upgraded from version 5.2.0 to version 5.2.1"
+sed -i -e "s,https://pypi.io/packages/source/c/cmd2/cmd2-2.1.2.tar.gz,https://pypi.io/packages/source/c/cmd2/cmd2-2.3.3.tar.gz,g" python/python-cmd2/spkgbuild
+sed -i -e "s/version=2.2.0/version=2.3.3/g" python/python-cmd2/spkgbuild
+changelog "python/python-cmd2/spkgbuild" "Upgraded from version 2.2.0 to version 2.3.3"
+sed -i -e "s,https://pypi.io/packages/source/e/emoji/emoji-1.2.0.tar.gz,https://pypi.io/packages/source/e/emoji/emoji-1.6.1.tar.gz,g" python/python-emoji/spkgbuild
+sed -i -e "s/version=1.4.2/version=1.6.1/g" python/python-emoji/spkgbuild
+changelog "python/python-emoji/spkgbuild" "Upgraded from version 1.4.2 to version 1.6.1"
+sed -i -e "s,https://files.pythonhosted.org/packages/source/M/Markups/Markups-3.1.2.tar.gz,https://files.pythonhosted.org/packages/source/M/Markups/Markups-3.1.3.tar.gz,g" python/python-markups/spkgbuild
+sed -i -e "s/version=3.1.2/version=3.1.3/g" python/python-markups/spkgbuild
+changelog "python/python-markups/spkgbuild" "Upgraded from version 3.1.2 to version 3.1.3"
+sed -i -e "s,https://files.pythonhosted.org/packages/source/m/moto/moto-2.2.19.tar.gz,https://files.pythonhosted.org/packages/source/m/moto/moto-2.3.0.tar.gz,g" python/python-moto/spkgbuild
+sed -i -e "s/version=2.2.19/version=2.3.0/g" python/python-moto/spkgbuild
+changelog "python/python-moto/spkgbuild" "Upgraded from version 2.2.19 to version 2.3.0"
+sed -i -e "s/version=8.12.39/version=8.12.40/g" python/python-phonenumbers/spkgbuild
+changelog "python/python-phonenumbers/spkgbuild" "Upgraded from version 8.12.39 to version 8.12.40"
+sed -i -e "s,https://pypi.io/packages/source/p/pygame/pygame-2.1.0.tar.gz,https://pypi.io/packages/source/p/pygame/pygame-2.1.1.tar.gz,g" python/python-pygame/spkgbuild
+sed -i -e "s/version=2.1.0/version=2.1.1/g" python/python-pygame/spkgbuild
+changelog "python/python-pygame/spkgbuild" "Upgraded from version 2.1.0 to version 2.1.1"
+sed -i -e "s/version=1.0.1/version=1.0.2/g" python/python-scikit-learn/spkgbuild
+changelog "python/python-scikit-learn/spkgbuild" "Upgraded from version 1.0.1 to version 1.0.2"
+sed -i -e "s,https://pypi.org/packages/source/S/Sphinx/Sphinx-4.3.1.tar.gz,https://pypi.org/packages/source/S/Sphinx/Sphinx-4.3.2.tar.gz,g" python/python-sphinx/spkgbuild
+sed -i -e "s/version=4.3.1/version=4.3.2/g" python/python-sphinx/spkgbuild
+changelog "python/python-sphinx/spkgbuild" "Upgraded from version 4.3.1 to version 4.3.2"
