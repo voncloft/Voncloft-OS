c0f9804d4ebc017122489872a0c21148589b96be
Voncloft <voncloft@gmail.com>
2021-12-29 05:12:42 -0500

    fixed vlc, added lua52 to repo to do so and changed its dependency accordingly

---
+++ REPOS/test_updates.sh
@@ -0,0 +1,14 @@
+sed -i -e "s/version=0.30.1/version=0.31.0/g" python/python-binary-memcached/spkgbuild
+changelog "python/python-binary-memcached/spkgbuild" "Upgraded from version 0.30.1 to version 0.31.0"
+sed -i -e "s/version=11.0.0/version=11.1.0/g" python/python-faker/spkgbuild
+changelog "python/python-faker/spkgbuild" "Upgraded from version 11.0.0 to version 11.1.0"
+sed -i -e "s/version=8.0.1/version=8.1.0/g" python/python-pdoc/spkgbuild
+changelog "python/python-pdoc/spkgbuild" "Upgraded from version 8.0.1 to version 8.1.0"
+sed -i -e "s/version=1.0.0/version=1.0.1/g" python/python-pytest-httpbin/spkgbuild
+changelog "python/python-pytest-httpbin/spkgbuild" "Upgraded from version 1.0.0 to version 1.0.1"
+sed -i -e "s/version=0.17.2/version=0.17.3/g" python/python-pytest-httpx/spkgbuild
+changelog "python/python-pytest-httpx/spkgbuild" "Upgraded from version 0.17.2 to version 0.17.3"
+sed -i -e "s/version=60.1.0/version=60.1.1/g" python/python-setuptools/spkgbuild
+changelog "python/python-setuptools/spkgbuild" "Upgraded from version 60.1.0 to version 60.1.1"
+sed -i -e "s/version=20.10.0/version=20.11.0/g" python/python-virtualenv/spkgbuild
+changelog "python/python-virtualenv/spkgbuild" "Upgraded from version 20.10.0 to version 20.11.0"
