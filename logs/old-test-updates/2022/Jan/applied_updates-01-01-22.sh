fb7fbe53323ceb1c2cfeec8ecd1b1038f39247db
Voncloft <voncloft@gmail.com>
2022-01-01 16:50:11 -0500

    casual updates

---
+++ logs/old-test-updates/2022/Jan/applied_updates-01-01-22.sh
@@ -0,0 +1,10 @@
+sed -i -e "s/version=1.0.0/version=1.0.1/g" lxqt/lxqt-session/spkgbuild
+changelog "lxqt/lxqt-session/spkgbuild" "Upgraded from version 1.0.0 to version 1.0.1"
+sed -i -e "s/version=4.3.2/version=4.3.3/g" python/python-jsonschema/spkgbuild
+changelog "python/python-jsonschema/spkgbuild" "Upgraded from version 4.3.2 to version 4.3.3"
+sed -i -e "s/version=1.2.0/version=2.0.0/g" python/python-sphinx-issues/spkgbuild
+changelog "python/python-sphinx-issues/spkgbuild" "Upgraded from version 1.2.0 to version 2.0.0"
+sed -i -e "s/version=20.12.0/version=20.12.1/g" python/python-virtualenv/spkgbuild
+changelog "python/python-virtualenv/spkgbuild" "Upgraded from version 20.12.0 to version 20.12.1"
+sed -i -e "s/version=4.16.0/version=4.17.0/g" xfce/tumbler/spkgbuild
+changelog "xfce/tumbler/spkgbuild" "Upgraded from version 4.16.0 to version 4.17.0"
