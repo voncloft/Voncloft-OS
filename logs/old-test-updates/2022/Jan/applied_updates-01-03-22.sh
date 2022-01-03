0d9adb0c5eadacbc7304f48cf5501f210542f8eb
Voncloft <voncloft@gmail.com>
2022-01-03 04:21:15 -0500

    casual updates

---
+++ logs/old-test-updates/2022/Jan/applied_updates-01-03-22.sh
@@ -0,0 +1,10 @@
+sed -i -e "s/version=8.0.1/version=8.1.0/g" libs/fmt/spkgbuild
+changelog "libs/fmt/spkgbuild" "Upgraded from version 8.0.1 to version 8.1.0"
+sed -i -e "s/version=10.16.1/version=10.16.2/g" python/python-rich/spkgbuild
+changelog "python/python-rich/spkgbuild" "Upgraded from version 10.16.1 to version 10.16.2"
+sed -i -e "s/version=2021.11.23/version=2022.01.02/g" python/python-sphinx-furo/spkgbuild
+changelog "python/python-sphinx-furo/spkgbuild" "Upgraded from version 2021.11.23 to version 2022.01.02"
+sed -i -e "s/version=2021.8.17/version=2022.01.02/g" python/python-sphinx-inline-tabs/spkgbuild
+changelog "python/python-sphinx-inline-tabs/spkgbuild" "Upgraded from version 2021.8.17 to version 2022.01.02"
+sed -i -e "s/version=20.12.1/version=20.13.0/g" python/python-virtualenv/spkgbuild
+changelog "python/python-virtualenv/spkgbuild" "Upgraded from version 20.12.1 to version 20.13.0"
