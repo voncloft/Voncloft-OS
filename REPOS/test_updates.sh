dc02200509b00de9c501da18409732a74233a646
Voncloft <voncloft@gmail.com>
2022-01-04 03:45:54 -0500

    casual updates

---
+++ REPOS/test_updates.sh
@@ -0,0 +1,30 @@
+sed -i -e "s/version=0.60.3/version=0.61.0/g" compilers/meson/spkgbuild
+changelog "compilers/meson/spkgbuild" "Upgraded from version 0.60.3 to version 0.61.0"
+sed -i -e "s/version=0.34.0/version=0.34.1/g" core/mpv/spkgbuild
+changelog "core/mpv/spkgbuild" "Upgraded from version 0.34.0 to version 0.34.1"
+sed -i -e "s/version=4.10/version=4.11.1/g" core/shadow/spkgbuild
+changelog "core/shadow/spkgbuild" "Upgraded from version 4.10 to version 4.11.1"
+sed -i -e "s/version=0.11.0/version=0.11.2/g" libs/hidapi/spkgbuild
+changelog "libs/hidapi/spkgbuild" "Upgraded from version 0.11.0 to version 0.11.2"
+sed -i -e "s/version=5.5.10/version=5.5.11/g" networking/whois/spkgbuild
+changelog "networking/whois/spkgbuild" "Upgraded from version 5.5.10 to version 5.5.11"
+sed -i -e "s/version=5.0.0/version=5.1.0/g" python/python-auditwheel/spkgbuild
+changelog "python/python-auditwheel/spkgbuild" "Upgraded from version 5.0.0 to version 5.1.0"
+sed -i -e "s/version=1.20.26/version=1.20.27/g" python/python-boto3/spkgbuild
+changelog "python/python-boto3/spkgbuild" "Upgraded from version 1.20.26 to version 1.20.27"
+sed -i -e "s/version=1.23.26/version=1.23.27/g" python/python-botocore/spkgbuild
+changelog "python/python-botocore/spkgbuild" "Upgraded from version 1.23.26 to version 1.23.27"
+sed -i -e "s/version=2.3.0/version=2.3.1/g" python/python-gphoto2/spkgbuild
+changelog "python/python-gphoto2/spkgbuild" "Upgraded from version 2.3.0 to version 2.3.1"
+sed -i -e "s/version=1.0.0/version=1.0.1/g" python/python-kikit/spkgbuild
+changelog "python/python-kikit/spkgbuild" "Upgraded from version 1.0.0 to version 1.0.1"
+sed -i -e "s/version=2.26.0/version=2.27.0/g" python/python-requests/spkgbuild
+changelog "python/python-requests/spkgbuild" "Upgraded from version 2.26.0 to version 2.27.0"
+sed -i -e "s/version=60.2.0/version=60.3.0/g" python/python-setuptools/spkgbuild
+changelog "python/python-setuptools/spkgbuild" "Upgraded from version 60.2.0 to version 60.3.0"
+sed -i -e "s/version=1.1.1/version=1.1.2/g" python/python-solidpython/spkgbuild
+changelog "python/python-solidpython/spkgbuild" "Upgraded from version 1.1.1 to version 1.1.2"
+sed -i -e "s/version=13.9/version=13.10/g" python/python-telegram-bot/spkgbuild
+changelog "python/python-telegram-bot/spkgbuild" "Upgraded from version 13.9 to version 13.10"
+sed -i -e "s/version=3.9.36/version=3.9.39/g" python/python-trimesh/spkgbuild
+changelog "python/python-trimesh/spkgbuild" "Upgraded from version 3.9.36 to version 3.9.39"
