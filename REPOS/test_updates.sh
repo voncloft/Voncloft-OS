cae05304dacb8099d44315cfe82fb337eac2843f
Voncloft <voncloft@gmail.com>
2022-01-06 05:29:54 -0500

    added some scripts to linux kernel

---
+++ REPOS/test_updates.sh
@@ -0,0 +1,6 @@
+sed -i -e "s/version=5.14.4/version=5.15.13/g" core/linux-desktop/spkgbuild
+changelog "core/linux-desktop/spkgbuild" "Upgraded from version 5.14.4 to version 5.15.13"
+sed -i -e "s/version=5.14.4/version=5.15.13/g" core/linux-router/spkgbuild
+changelog "core/linux-router/spkgbuild" "Upgraded from version 5.14.4 to version 5.15.13"
+sed -i -e "s/version=7.2.4/version=7.2.5/g" nonfree/libreoffice/spkgbuild
+changelog "nonfree/libreoffice/spkgbuild" "Upgraded from version 7.2.4 to version 7.2.5"
