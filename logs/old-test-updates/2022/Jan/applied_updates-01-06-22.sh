09074f5157f303a8d87a6c432a0be06869c2165e
Voncloft <voncloft@gmail.com>
2022-01-06 03:23:45 -0500

    casual updates

---
+++ logs/old-test-updates/2022/Jan/applied_updates-01-06-22.sh
@@ -0,0 +1,64 @@
+sed -i -e "s/version=5.1.12/version=5.1.16/g" core/bash/spkgbuild
+changelog "core/bash/spkgbuild" "Upgraded from version 5.1.12 to version 5.1.16"
+sed -i -e "s/version=6.0/version=6.0.1/g" core/mercurial/spkgbuild
+changelog "core/mercurial/spkgbuild" "Upgraded from version 6.0 to version 6.0.1"
+sed -i -e "s/version=0.3.42/version=0.3.43/g" core/pipewire/spkgbuild
+changelog "core/pipewire/spkgbuild" "Upgraded from version 0.3.42 to version 0.3.43"
+sed -i -e "s/version=8.1/version=8.1.2/g" core/readline/spkgbuild
+changelog "core/readline/spkgbuild" "Upgraded from version 8.1 to version 8.1.2"
+sed -i -e "s/version=0.9.10/version=1.0/g" libs/libunistring/spkgbuild
+changelog "libs/libunistring/spkgbuild" "Upgraded from version 0.9.10 to version 1.0"
+sed -i -e "s/version=21.12.0/version=22.01.0/g" libs/poppler/spkgbuild
+changelog "libs/poppler/spkgbuild" "Upgraded from version 21.12.0 to version 22.01.0"
+sed -i -e "s/version=96.0b3/version=96.0b4/g" networking/thunderbird/spkgbuild
+changelog "networking/thunderbird/spkgbuild" "Upgraded from version 96.0b3 to version 96.0b4"
+sed -i -e "s/version=1.20.28/version=1.20.29/g" python/python-boto3/spkgbuild
+changelog "python/python-boto3/spkgbuild" "Upgraded from version 1.20.28 to version 1.20.29"
+sed -i -e "s/version=1.23.28/version=1.23.29/g" python/python-botocore/spkgbuild
+changelog "python/python-botocore/spkgbuild" "Upgraded from version 1.23.28 to version 1.23.29"
+sed -i -e "s/version=2.1.0/version=2.2.0/g" python/python-covdefaults/spkgbuild
+changelog "python/python-covdefaults/spkgbuild" "Upgraded from version 2.1.0 to version 2.2.0"
+sed -i -e "s/version=11.1.0/version=11.3.0/g" python/python-faker/spkgbuild
+changelog "python/python-faker/spkgbuild" "Upgraded from version 11.1.0 to version 11.3.0"
+sed -i -e "s/version=2.10.1/version=3.0.0/g" python/python-gitlab/spkgbuild
+changelog "python/python-gitlab/spkgbuild" "Upgraded from version 2.10.1 to version 3.0.0"
+sed -i -e "s/version=0.14.3/version=0.14.4/g" python/python-httpcore/spkgbuild
+changelog "python/python-httpcore/spkgbuild" "Upgraded from version 0.14.3 to version 0.14.4"
+sed -i -e "s/version=6.34.1/version=6.34.2/g" python/python-hypothesis/spkgbuild
+changelog "python/python-hypothesis/spkgbuild" "Upgraded from version 6.34.1 to version 6.34.2"
+sed -i -e "s/version=2021.08/version=2021.12/g" python/python-litedram/spkgbuild
+changelog "python/python-litedram/spkgbuild" "Upgraded from version 2021.08 to version 2021.12"
+sed -i -e "s/version=2021.08/version=2021.12/g" python/python-liteeth/spkgbuild
+changelog "python/python-liteeth/spkgbuild" "Upgraded from version 2021.08 to version 2021.12"
+sed -i -e "s/version=2021.08/version=2021.12/g" python/python-liteiclink/spkgbuild
+changelog "python/python-liteiclink/spkgbuild" "Upgraded from version 2021.08 to version 2021.12"
+sed -i -e "s/version=2021.08/version=2021.12/g" python/python-litejesd204b/spkgbuild
+changelog "python/python-litejesd204b/spkgbuild" "Upgraded from version 2021.08 to version 2021.12"
+sed -i -e "s/version=2021.08/version=2021.12/g" python/python-litepcie/spkgbuild
+changelog "python/python-litepcie/spkgbuild" "Upgraded from version 2021.08 to version 2021.12"
+sed -i -e "s/version=2021.08/version=2021.12/g" python/python-litesata/spkgbuild
+changelog "python/python-litesata/spkgbuild" "Upgraded from version 2021.08 to version 2021.12"
+sed -i -e "s/version=2021.08/version=2021.12/g" python/python-litescope/spkgbuild
+changelog "python/python-litescope/spkgbuild" "Upgraded from version 2021.08 to version 2021.12"
+sed -i -e "s/version=2021.08/version=2021.12/g" python/python-litesdcard/spkgbuild
+changelog "python/python-litesdcard/spkgbuild" "Upgraded from version 2021.08 to version 2021.12"
+sed -i -e "s/version=2021.08/version=2021.12/g" python/python-liteusb/spkgbuild
+changelog "python/python-liteusb/spkgbuild" "Upgraded from version 2021.08 to version 2021.12"
+sed -i -e "s/version=2021.08/version=2021.12/g" python/python-litex/spkgbuild
+changelog "python/python-litex/spkgbuild" "Upgraded from version 2021.08 to version 2021.12"
+sed -i -e "s/version=2021.08/version=2021.12/g" python/python-litex-boards/spkgbuild
+changelog "python/python-litex-boards/spkgbuild" "Upgraded from version 2021.08 to version 2021.12"
+sed -i -e "s/version=1.4.0/version=1.5.0/g" python/python-pandas/spkgbuild
+changelog "python/python-pandas/spkgbuild" "Upgraded from version 1.4.0 to version 1.5.0"
+sed -i -e "s/version=8.1.0/version=8.2.0/g" python/python-pdoc/spkgbuild
+changelog "python/python-pdoc/spkgbuild" "Upgraded from version 8.1.0 to version 8.2.0"
+sed -i -e "s/version=7.3.2112051810/version=7.3.2201042251/g" python/python-pycontracts/spkgbuild
+changelog "python/python-pycontracts/spkgbuild" "Upgraded from version 7.3.2112051810 to version 7.3.2201042251"
+sed -i -e "s/version=5.1.0/version=5.2.0/g" python/python-pynamodb/spkgbuild
+changelog "python/python-pynamodb/spkgbuild" "Upgraded from version 5.1.0 to version 5.2.0"
+sed -i -e "s/version=2.27.0/version=2.27.1/g" python/python-requests/spkgbuild
+changelog "python/python-requests/spkgbuild" "Upgraded from version 2.27.0 to version 2.27.1"
+sed -i -e "s/version=3.9.39/version=3.9.40/g" python/python-trimesh/spkgbuild
+changelog "python/python-trimesh/spkgbuild" "Upgraded from version 3.9.39 to version 3.9.40"
+sed -i -e "s/version=1.20.2/version=1.21.5/g" server/nginx/spkgbuild
+changelog "server/nginx/spkgbuild" "Upgraded from version 1.20.2 to version 1.21.5"
