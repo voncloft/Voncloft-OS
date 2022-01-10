fb30a11883659974df84483e51fbeb072d8d4ca1
Voncloft <voncloft@gmail.com>
2022-01-10 06:36:20 -0500

    casual updates

---
+++ logs/old-test-updates/2022/Jan/applied_updates-01-10-22.sh
@@ -0,0 +1,8 @@
+sed -i -e "s/version=1.7.4.2/version=1.7.4.3/g" core/socat/spkgbuild
+changelog "core/socat/spkgbuild" "Upgraded from version 1.7.4.2 to version 1.7.4.3"
+sed -i -e "s/version=2.9.2/version=2.9.3/g" python/python-astroid/spkgbuild
+changelog "python/python-astroid/spkgbuild" "Upgraded from version 2.9.2 to version 2.9.3"
+sed -i -e "s/version=3.10.3/version=3.10.4/g" python/python-mocket/spkgbuild
+changelog "python/python-mocket/spkgbuild" "Upgraded from version 3.10.3 to version 3.10.4"
+sed -i -e "s/version=4.1.2/version=5.0.0/g" python/python-tempora/spkgbuild
+changelog "python/python-tempora/spkgbuild" "Upgraded from version 4.1.2 to version 5.0.0"
