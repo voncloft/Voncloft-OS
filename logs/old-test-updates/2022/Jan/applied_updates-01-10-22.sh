41f695616cf1223db809b30cfe5d709362c03161
Voncloft <voncloft@gmail.com>
2022-01-10 16:30:59 -0500

    Minor Updates

--- logs/old-test-updates/2022/Jan/applied_updates-01-10-22.sh
+++ logs/old-test-updates/2022/Jan/applied_updates-01-10-22.sh
@@ -6,3 +6,31 @@ sed -i -e "s/version=3.10.3/version=3.10.4/g" python/python-mocket/spkgbuild
 changelog "python/python-mocket/spkgbuild" "Upgraded from version 3.10.3 to version 3.10.4"
 sed -i -e "s/version=4.1.2/version=5.0.0/g" python/python-tempora/spkgbuild
 changelog "python/python-tempora/spkgbuild" "Upgraded from version 4.1.2 to version 5.0.0"
+sed -i -e "s/version=3.1.7/version=3.2.0/g" core/double-conversion/spkgbuild
+changelog "core/double-conversion/spkgbuild" "Upgraded from version 3.1.7 to version 3.2.0"
+sed -i -e "s/version=9.62/version=9.63/g" core/hdparm/spkgbuild
+changelog "core/hdparm/spkgbuild" "Upgraded from version 9.62 to version 9.63"
+sed -i -e "s/version=5.15.13/version=5.16/g" core/linux-desktop/spkgbuild
+changelog "core/linux-desktop/spkgbuild" "Upgraded from version 5.15.13 to version 5.16"
+sed -i -e "s/version=5.15.13/version=5.16/g" core/linux-router/spkgbuild
+changelog "core/linux-router/spkgbuild" "Upgraded from version 5.15.13 to version 5.16"
+sed -i -e "s/version=20211214/version=20220110/g" core/scratchpkg/spkgbuild
+changelog "core/scratchpkg/spkgbuild" "Upgraded from version 20211214 to version 20220110"
+sed -i -e "s/version=0.3.9/version=0.3.10/g" libs/libgusb/spkgbuild
+changelog "libs/libgusb/spkgbuild" "Upgraded from version 0.3.9 to version 0.3.10"
+sed -i -e "s/version=96.0b10/version=97.0b1/g" networking/firefox/spkgbuild
+changelog "networking/firefox/spkgbuild" "Upgraded from version 96.0b10 to version 97.0b1"
+sed -i -e "s/version=1.20.31/version=1.20.32/g" python/python-boto3/spkgbuild
+changelog "python/python-boto3/spkgbuild" "Upgraded from version 1.20.31 to version 1.20.32"
+sed -i -e "s/version=1.23.31/version=1.23.32/g" python/python-botocore/spkgbuild
+changelog "python/python-botocore/spkgbuild" "Upgraded from version 1.23.31 to version 1.23.32"
+sed -i -e "s/version=0.56.3/version=0.56.4/g" python/python-cfn-lint/spkgbuild
+changelog "python/python-cfn-lint/spkgbuild" "Upgraded from version 0.56.3 to version 0.56.4"
+sed -i -e "s/version=0.7.14/version=0.7.15/g" python/python-pony/spkgbuild
+changelog "python/python-pony/spkgbuild" "Upgraded from version 0.7.14 to version 0.7.15"
+sed -i -e "s/version=3.10.3/version=3.11.0/g" python/python-pytest-randomly/spkgbuild
+changelog "python/python-pytest-randomly/spkgbuild" "Upgraded from version 3.10.3 to version 3.11.0"
+sed -i -e "s/version=0.16.0/version=0.17.0/g" python/python-responses/spkgbuild
+changelog "python/python-responses/spkgbuild" "Upgraded from version 0.16.0 to version 0.17.0"
+sed -i -e "s/version=1.15.4/version=1.15.5/g" ruby-gems/ruby-ffi/spkgbuild
+changelog "ruby-gems/ruby-ffi/spkgbuild" "Upgraded from version 1.15.4 to version 1.15.5"
