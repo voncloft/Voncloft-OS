8e09312b20f25bde478761d91e7205f89c753d6c
Voncloft <voncloft@gmail.com>
2022-01-12 06:16:25 -0500

    Minor Updates

---
+++ logs/old-test-updates/2022/Jan/applied_updates-01-12-22.sh
@@ -0,0 +1,34 @@
+sed -i -e "s/version=5.2.0/version=5.2.1/g" cinnamon/cinnamon-desktop/spkgbuild
+changelog "cinnamon/cinnamon-desktop/spkgbuild" "Upgraded from version 5.2.0 to version 5.2.1"
+sed -i -e "s/version=5.2.3/version=5.2.4/g" cinnamon/nemo/spkgbuild
+changelog "cinnamon/nemo/spkgbuild" "Upgraded from version 5.2.3 to version 5.2.4"
+sed -i -e "s/version=3.19.2/version=3.19.3/g" extra/protobuf/spkgbuild
+changelog "extra/protobuf/spkgbuild" "Upgraded from version 3.19.2 to version 3.19.3"
+sed -i -e "s/version=2.0.18/version=2.0.20/g" libs/sdl2/spkgbuild
+changelog "libs/sdl2/spkgbuild" "Upgraded from version 2.0.18 to version 2.0.20"
+sed -i -e "s/version=0.7.2/version=0.7.3/g" python/python-aioresponses/spkgbuild
+changelog "python/python-aioresponses/spkgbuild" "Upgraded from version 0.7.2 to version 0.7.3"
+sed -i -e "s/version=3.4.0/version=3.5.0/g" python/python-anyio/spkgbuild
+changelog "python/python-anyio/spkgbuild" "Upgraded from version 3.4.0 to version 3.5.0"
+sed -i -e "s/version=1.20.32/version=1.20.33/g" python/python-boto3/spkgbuild
+changelog "python/python-boto3/spkgbuild" "Upgraded from version 1.20.32 to version 1.20.33"
+sed -i -e "s/version=1.23.32/version=1.23.33/g" python/python-botocore/spkgbuild
+changelog "python/python-botocore/spkgbuild" "Upgraded from version 1.23.32 to version 1.23.33"
+sed -i -e "s/version=4.3.0/version=4.3.1/g" python/python-engineio/spkgbuild
+changelog "python/python-engineio/spkgbuild" "Upgraded from version 4.3.0 to version 4.3.1"
+sed -i -e "s/version=4.12.1/version=4.13.0/g" python/python-libcharon/spkgbuild
+changelog "python/python-libcharon/spkgbuild" "Upgraded from version 4.12.1 to version 4.13.0"
+sed -i -e "s/version=0.10.2/version=0.10.3/g" python/python-libtmux/spkgbuild
+changelog "python/python-libtmux/spkgbuild" "Upgraded from version 0.10.2 to version 0.10.3"
+sed -i -e "s/version=8.12.40/version=8.12.41/g" python/python-phonenumbers/spkgbuild
+changelog "python/python-phonenumbers/spkgbuild" "Upgraded from version 8.12.40 to version 8.12.41"
+sed -i -e "s/version=1.0.0/version=1.0.2/g" python/python-pyppeteer/spkgbuild
+changelog "python/python-pyppeteer/spkgbuild" "Upgraded from version 1.0.0 to version 1.0.2"
+sed -i -e "s/version=60.5.0/version=60.5.1/g" python/python-setuptools/spkgbuild
+changelog "python/python-setuptools/spkgbuild" "Upgraded from version 60.5.0 to version 60.5.1"
+sed -i -e "s/version=5.5.0/version=5.5.1/g" python/python-socketio/spkgbuild
+changelog "python/python-socketio/spkgbuild" "Upgraded from version 5.5.0 to version 5.5.1"
+sed -i -e "s/version=2.0.0/version=3.0.1/g" python/python-sphinx-issues/spkgbuild
+changelog "python/python-sphinx-issues/spkgbuild" "Upgraded from version 2.0.0 to version 3.0.1"
+sed -i -e "s/version=3.0.0/version=3.1.1/g" ruby-gems/ruby-rainbow/spkgbuild
+changelog "ruby-gems/ruby-rainbow/spkgbuild" "Upgraded from version 3.0.0 to version 3.1.1"
