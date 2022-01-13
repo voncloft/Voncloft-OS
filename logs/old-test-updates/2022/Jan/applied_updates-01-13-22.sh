bdc2fa3264eda2705cd6a8a5028af29b65e5c7bb
Voncloft <voncloft@gmail.com>
2022-01-13 06:58:56 -0500

    Minor Updates

---
+++ logs/old-test-updates/2022/Jan/applied_updates-01-13-22.sh
@@ -0,0 +1,22 @@
+sed -i -e "s/version=0.91.2/version=0.92.0/g" core/hugo/spkgbuild
+changelog "core/hugo/spkgbuild" "Upgraded from version 0.91.2 to version 0.92.0"
+sed -i -e "s/version=96.0b4/version=97.0b1/g" networking/thunderbird/spkgbuild
+changelog "networking/thunderbird/spkgbuild" "Upgraded from version 96.0b4 to version 97.0b1"
+sed -i -e "s/version=1.5/version=2.1/g" python/python-argparse-manpage/spkgbuild
+changelog "python/python-argparse-manpage/spkgbuild" "Upgraded from version 1.5 to version 2.1"
+sed -i -e "s/version=1.20.33/version=1.20.34/g" python/python-boto3/spkgbuild
+changelog "python/python-boto3/spkgbuild" "Upgraded from version 1.20.33 to version 1.20.34"
+sed -i -e "s/version=1.23.33/version=1.23.34/g" python/python-botocore/spkgbuild
+changelog "python/python-botocore/spkgbuild" "Upgraded from version 1.23.33 to version 1.23.34"
+sed -i -e "s/version=2.0.0/version=2.1.0/g" python/python-jsonpickle/spkgbuild
+changelog "python/python-jsonpickle/spkgbuild" "Upgraded from version 2.0.0 to version 2.1.0"
+sed -i -e "s/version=4.3.3/version=4.4.0/g" python/python-jsonschema/spkgbuild
+changelog "python/python-jsonschema/spkgbuild" "Upgraded from version 4.3.3 to version 4.4.0"
+sed -i -e "s/version=0.17.0/version=0.18.0/g" python/python-meilisearch/spkgbuild
+changelog "python/python-meilisearch/spkgbuild" "Upgraded from version 0.17.0 to version 0.18.0"
+sed -i -e "s/version=0.13.3/version=0.13.4/g" python/python-pybuilder/spkgbuild
+changelog "python/python-pybuilder/spkgbuild" "Upgraded from version 0.13.3 to version 0.13.4"
+sed -i -e "s/version=60.5.1/version=60.5.2/g" python/python-setuptools/spkgbuild
+changelog "python/python-setuptools/spkgbuild" "Upgraded from version 60.5.1 to version 60.5.2"
+sed -i -e "s/version=8.0.1/version=8.1.0/g" python/python-webargs/spkgbuild
+changelog "python/python-webargs/spkgbuild" "Upgraded from version 8.0.1 to version 8.1.0"
