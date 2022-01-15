ca9f9151e423886d3f7b456d7b36803e8d2b565c
Voncloft <voncloft@gmail.com>
2022-01-15 09:41:58 -0500

    Minor Updates

--- REPOS/server/monitor_process/wireguard-check.sh
+++ REPOS/server/monitor_process/wireguard-check.sh
@@ -7,9 +7,9 @@ do
 	fi
 done
 if [[ $flag -eq 1 ]];then
-	echo -e ${GREEN}"Wireguard is running normally."${NC}
+	echo -e ${GREEN}"wireguard is running normally."${NC}
 else
-	mailme $email "Wireguard is not running - restarting"
-	echo -e ${RED}"Wireguard is not running - restarting"${NC}
+	mailme $email "wireguard is not running - restarting"
+	echo -e ${RED}"wireguard is not running - restarting"${NC}
 	/etc/init.d/wireguard start
 fi
