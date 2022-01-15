9029bd62218bec850a8d1805fb3b3425a798bc5a
Voncloft <voncloft@gmail.com>
2022-01-15 01:18:51 -0500

    Minor Updates

---
+++ logs/old-test-updates/2022/Jan/applied_updates-01-15-22.sh
@@ -0,0 +1,20 @@
+sed -i -e "s/version=4.4.1/version=5.0/g" media/ffmpeg/spkgbuild
+changelog "media/ffmpeg/spkgbuild" "Upgraded from version 4.4.1 to version 5.0"
+sed -i -e "s/version=97.0b2/version=97.0b3/g" networking/firefox/spkgbuild
+changelog "networking/firefox/spkgbuild" "Upgraded from version 97.0b2 to version 97.0b3"
+sed -i -e "s/version=2.17/version=2.17.0.1/g" nonfree/pandoc-bin/spkgbuild
+changelog "nonfree/pandoc-bin/spkgbuild" "Upgraded from version 2.17 to version 2.17.0.1"
+sed -i -e "s/version=0.5.3/version=0.5.4/g" python/python-databases/spkgbuild
+changelog "python/python-databases/spkgbuild" "Upgraded from version 0.5.3 to version 0.5.4"
+sed -i -e "s/version=3.0.0/version=3.1.0/g" python/python-gitlab/spkgbuild
+changelog "python/python-gitlab/spkgbuild" "Upgraded from version 3.0.0 to version 3.1.0"
+sed -i -e "s/version=2.0.1/version=2.0.2/g" python/python-mistune/spkgbuild
+changelog "python/python-mistune/spkgbuild" "Upgraded from version 2.0.1 to version 2.0.2"
+sed -i -e "s/version=8.2.0/version=8.3.0/g" python/python-pdoc/spkgbuild
+changelog "python/python-pdoc/spkgbuild" "Upgraded from version 8.2.0 to version 8.3.0"
+sed -i -e "s/version=60.5.2/version=60.5.3/g" python/python-setuptools/spkgbuild
+changelog "python/python-setuptools/spkgbuild" "Upgraded from version 60.5.2 to version 60.5.3"
+sed -i -e "s/version=3.10.1/version=3.10.2/g" ruby-gems/ruby-rspec-expectations/spkgbuild
+changelog "ruby-gems/ruby-rspec-expectations/spkgbuild" "Upgraded from version 3.10.1 to version 3.10.2"
+sed -i -e "s/version=0.12.1/version=0.12.2/g" xfce/ristretto/spkgbuild
+changelog "xfce/ristretto/spkgbuild" "Upgraded from version 0.12.1 to version 0.12.2"
