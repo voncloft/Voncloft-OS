389df148b75a97bf1841e2630f249703178b5b75
Voncloft <voncloft@gmail.com>
2022-01-17 04:57:19 -0500

    Minor Updates

---
+++ logs/old-test-updates/2022/Jan/applied_updates-01-17-22.sh
@@ -0,0 +1,16 @@
+sed -i -e "s/version=11.1/version=11.2/g" compilers/gdb/spkgbuild
+changelog "compilers/gdb/spkgbuild" "Upgraded from version 11.1 to version 11.2"
+sed -i -e "s/version=3.10.1/version=3.10.2/g" core/geos/spkgbuild
+changelog "core/geos/spkgbuild" "Upgraded from version 3.10.1 to version 3.10.2"
+sed -i -e "s/version=5.16/version=5.16.1/g" core/linux-desktop/spkgbuild
+changelog "core/linux-desktop/spkgbuild" "Upgraded from version 5.16 to version 5.16.1"
+sed -i -e "s/version=5.16/version=5.16.1/g" core/linux-router/spkgbuild
+changelog "core/linux-router/spkgbuild" "Upgraded from version 5.16 to version 5.16.1"
+sed -i -e "s/version=3.2.8/version=3.2.9/g" extra/flexget/spkgbuild
+changelog "extra/flexget/spkgbuild" "Upgraded from version 3.2.8 to version 3.2.9"
+sed -i -e "s/version=2.4.2/version=2.4.3/g" libs/expat/spkgbuild
+changelog "libs/expat/spkgbuild" "Upgraded from version 2.4.2 to version 2.4.3"
+sed -i -e "s/version=0.9.25/version=0.9.26/g" python/python-asteval/spkgbuild
+changelog "python/python-asteval/spkgbuild" "Upgraded from version 0.9.25 to version 0.9.26"
+sed -i -e "s/version=0.71.0/version=0.72.0/g" python/python-fastapi/spkgbuild
+changelog "python/python-fastapi/spkgbuild" "Upgraded from version 0.71.0 to version 0.72.0"
