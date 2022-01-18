fe8abba171e56ede56c4f86c01e3eadc5341bc0a
Voncloft <voncloft@gmail.com>
2022-01-18 07:47:03 -0500

    Minor Updates

---
+++ logs/old-test-updates/2022/Jan/applied_updates-01-18-22.sh
@@ -0,0 +1,36 @@
+sed -i -e "s/version=0.61.0/version=0.61.1/g" compilers/meson/spkgbuild
+changelog "compilers/meson/spkgbuild" "Upgraded from version 0.61.0 to version 0.61.1"
+sed -i -e "s/version=0.24.0/version=0.24.1/g" core/p11-kit/spkgbuild
+changelog "core/p11-kit/spkgbuild" "Upgraded from version 0.24.0 to version 0.24.1"
+sed -i -e "s/version=0.24.0/version=0.24.1/g" multilib/p11-kit-32/spkgbuild
+changelog "multilib/p11-kit-32/spkgbuild" "Upgraded from version 0.24.0 to version 0.24.1"
+sed -i -e "s/version=3.0.12/version=3.0.13/g" libs/libical/spkgbuild
+changelog "libs/libical/spkgbuild" "Upgraded from version 3.0.12 to version 3.0.13"
+sed -i -e "s/version=1.99.1/version=2.0.0/g" libs/libwacom/spkgbuild
+changelog "libs/libwacom/spkgbuild" "Upgraded from version 1.99.1 to version 2.0.0"
+sed -i -e "s/version=97.0b3/version=97.0b4/g" networking/firefox/spkgbuild
+changelog "networking/firefox/spkgbuild" "Upgraded from version 97.0b3 to version 97.0b4"
+sed -i -e "s/version=2.9/version=2.10/g" networking/hostapd/spkgbuild
+changelog "networking/hostapd/spkgbuild" "Upgraded from version 2.9 to version 2.10"
+sed -i -e "s/version=2.9/version=2.10/g" networking/wpa_supplicant/spkgbuild
+changelog "networking/wpa_supplicant/spkgbuild" "Upgraded from version 2.9 to version 2.10"
+sed -i -e "s/version=2.3.0/version=2.4.0/g" python/python-debtcollector/spkgbuild
+changelog "python/python-debtcollector/spkgbuild" "Upgraded from version 2.3.0 to version 2.4.0"
+sed -i -e "s/version=0.16.3/version=0.17.2/g" python/python-fasteners/spkgbuild
+changelog "python/python-fasteners/spkgbuild" "Upgraded from version 0.16.3 to version 0.17.2"
+sed -i -e "s/version=6.35.0/version=6.35.1/g" python/python-hypothesis/spkgbuild
+changelog "python/python-hypothesis/spkgbuild" "Upgraded from version 6.35.0 to version 6.35.1"
+sed -i -e "s/version=4.10.0/version=4.10.1/g" python/python-importlib-metadata/spkgbuild
+changelog "python/python-importlib-metadata/spkgbuild" "Upgraded from version 4.10.0 to version 4.10.1"
+sed -i -e "s/version=0.3.1/version=0.3.2/g" python/python-openapi-spec-validator/spkgbuild
+changelog "python/python-openapi-spec-validator/spkgbuild" "Upgraded from version 0.3.1 to version 0.3.2"
+sed -i -e "s/version=2021.2.2/version=2022.1/g" python/python-pudb/spkgbuild
+changelog "python/python-pudb/spkgbuild" "Upgraded from version 2021.2.2 to version 2022.1"
+sed -i -e "s/version=0.17.0/version=0.17.2/g" python/python-pytest-asyncio/spkgbuild
+changelog "python/python-pytest-asyncio/spkgbuild" "Upgraded from version 0.17.0 to version 0.17.2"
+sed -i -e "s/version=0.17.3/version=0.18.0/g" python/python-pytest-httpx/spkgbuild
+changelog "python/python-pytest-httpx/spkgbuild" "Upgraded from version 0.17.3 to version 0.18.0"
+sed -i -e "s/version=60.5.3/version=60.5.4/g" python/python-setuptools/spkgbuild
+changelog "python/python-setuptools/spkgbuild" "Upgraded from version 60.5.3 to version 60.5.4"
+sed -i -e "s/version=5.15.6/version=5.15.8/g" qt/qtwebengine/spkgbuild
+changelog "qt/qtwebengine/spkgbuild" "Upgraded from version 5.15.6 to version 5.15.8"
