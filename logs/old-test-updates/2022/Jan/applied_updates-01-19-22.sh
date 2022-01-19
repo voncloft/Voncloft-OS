d47c4fe017058b39a88b86066d4a610d59d5ebc2
Voncloft <voncloft@gmail.com>
2022-01-19 10:05:15 -0500

    Minor Updates

---
+++ logs/old-test-updates/2022/Jan/applied_updates-01-19-22.sh
@@ -0,0 +1,26 @@
+sed -i -e "s/version=1.26/version=1.27/g" core/fakeroot/spkgbuild
+changelog "core/fakeroot/spkgbuild" "Upgraded from version 1.26 to version 1.27"
+sed -i -e "s/version=3.7.2/version=3.7.3/g" core/gnutls/spkgbuild
+changelog "core/gnutls/spkgbuild" "Upgraded from version 3.7.2 to version 3.7.3"
+sed -i -e "s/version=97.0b1/version=97.0b2/g" networking/thunderbird/spkgbuild
+changelog "networking/thunderbird/spkgbuild" "Upgraded from version 97.0b1 to version 97.0b2"
+sed -i -e "s/version=6.1.30/version=6.1.32/g" nonfree/virtualbox/spkgbuild
+changelog "nonfree/virtualbox/spkgbuild" "Upgraded from version 6.1.30 to version 6.1.32"
+sed -i -e "s/version=6.1.30/version=6.1.32/g" nonfree/virtualbox-guest-modules/spkgbuild
+changelog "nonfree/virtualbox-guest-modules/spkgbuild" "Upgraded from version 6.1.30 to version 6.1.32"
+sed -i -e "s/version=6.1.30/version=6.1.32/g" nonfree/virtualbox-guest-utils/spkgbuild
+changelog "nonfree/virtualbox-guest-utils/spkgbuild" "Upgraded from version 6.1.30 to version 6.1.32"
+sed -i -e "s/version=6.1.30/version=6.1.32/g" nonfree/virtualbox-modules/spkgbuild
+changelog "nonfree/virtualbox-modules/spkgbuild" "Upgraded from version 6.1.30 to version 6.1.32"
+sed -i -e "s/version=1.20.37/version=1.20.38/g" python/python-boto3/spkgbuild
+changelog "python/python-boto3/spkgbuild" "Upgraded from version 1.20.37 to version 1.20.38"
+sed -i -e "s/version=1.23.37/version=1.23.38/g" python/python-botocore/spkgbuild
+changelog "python/python-botocore/spkgbuild" "Upgraded from version 1.23.37 to version 1.23.38"
+sed -i -e "s/version=0.14.4/version=0.14.5/g" python/python-httpcore/spkgbuild
+changelog "python/python-httpcore/spkgbuild" "Upgraded from version 0.14.4 to version 0.14.5"
+sed -i -e "s/version=8.0.27/version=8.0.28/g" python/python-mysql-connector/spkgbuild
+changelog "python/python-mysql-connector/spkgbuild" "Upgraded from version 8.0.27 to version 8.0.28"
+sed -i -e "s/version=2.16.0/version=2.17.0/g" python/python-pre-commit/spkgbuild
+changelog "python/python-pre-commit/spkgbuild" "Upgraded from version 2.16.0 to version 2.17.0"
+sed -i -e "s/version=1.0.3/version=1.0.4/g" python/python-precis_i18n/spkgbuild
+changelog "python/python-precis_i18n/spkgbuild" "Upgraded from version 1.0.3 to version 1.0.4"
