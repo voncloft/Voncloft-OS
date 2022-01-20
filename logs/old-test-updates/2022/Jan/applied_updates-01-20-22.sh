d90556d1a2df350c9a152525ba0ac5afcc7bf465
Voncloft <voncloft@gmail.com>
2022-01-20 05:21:16 -0500

    Minor Updates

---
+++ logs/old-test-updates/2022/Jan/applied_updates-01-20-22.sh
@@ -0,0 +1,28 @@
+sed -i -e "s/version=3.2.9/version=3.2.10/g" extra/flexget/spkgbuild
+changelog "extra/flexget/spkgbuild" "Upgraded from version 3.2.9 to version 3.2.10"
+sed -i -e "s/version=1.8.32/version=1.8.34/g" libs/libnma/spkgbuild
+changelog "libs/libnma/spkgbuild" "Upgraded from version 1.8.32 to version 1.8.34"
+sed -i -e "s/version=97.0b4/version=97.0b5/g" networking/firefox/spkgbuild
+changelog "networking/firefox/spkgbuild" "Upgraded from version 97.0b4 to version 97.0b5"
+sed -i -e "s/version=1.20.38/version=1.20.39/g" python/python-boto3/spkgbuild
+changelog "python/python-boto3/spkgbuild" "Upgraded from version 1.20.38 to version 1.20.39"
+sed -i -e "s/version=1.23.38/version=1.23.39/g" python/python-botocore/spkgbuild
+changelog "python/python-botocore/spkgbuild" "Upgraded from version 1.23.38 to version 1.23.39"
+sed -i -e "s/version=0.56.4/version=0.57.0/g" python/python-cfn-lint/spkgbuild
+changelog "python/python-cfn-lint/spkgbuild" "Upgraded from version 0.56.4 to version 0.57.0"
+sed -i -e "s/version=6.35.1/version=6.36.0/g" python/python-hypothesis/spkgbuild
+changelog "python/python-hypothesis/spkgbuild" "Upgraded from version 6.35.1 to version 6.36.0"
+sed -i -e "s/version=0.5.6/version=0.6.0/g" python/python-parse-type/spkgbuild
+changelog "python/python-parse-type/spkgbuild" "Upgraded from version 0.5.6 to version 0.6.0"
+sed -i -e "s/version=2.3.0/version=2.3.1/g" python/python-pytest-regressions/spkgbuild
+changelog "python/python-pytest-regressions/spkgbuild" "Upgraded from version 2.3.0 to version 2.3.1"
+sed -i -e "s/version=2.0.2/version=2.1.0/g" python/python-pytest-timeout/spkgbuild
+changelog "python/python-pytest-timeout/spkgbuild" "Upgraded from version 2.0.2 to version 2.1.0"
+sed -i -e "s/version=1.3.11/version=1.3.12/g" python/python-sqlalchemy-continuum/spkgbuild
+changelog "python/python-sqlalchemy-continuum/spkgbuild" "Upgraded from version 1.3.11 to version 1.3.12"
+sed -i -e "s/version=3.0.4/version=3.1.0/g" python/python-unittest-xml-reporting/spkgbuild
+changelog "python/python-unittest-xml-reporting/spkgbuild" "Upgraded from version 3.0.4 to version 3.1.0"
+sed -i -e "s/version=0.16.0/version=0.17.0/g" python/python-zstandard/spkgbuild
+changelog "python/python-zstandard/spkgbuild" "Upgraded from version 0.16.0 to version 0.17.0"
+sed -i -e "s/version=4.15.3/version=4.15.4/g" server/samba/spkgbuild
+changelog "server/samba/spkgbuild" "Upgraded from version 4.15.3 to version 4.15.4"
