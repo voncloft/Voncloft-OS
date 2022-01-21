d9547bca5e4de067c9f25f5983f57cf97159ca01
Voncloft <voncloft@gmail.com>
2022-01-20 21:45:11 -0500

    Minor Updates

--- logs/old-test-updates/2022/Jan/applied_updates-01-20-22.sh
+++ logs/old-test-updates/2022/Jan/applied_updates-01-20-22.sh
@@ -26,3 +26,31 @@ sed -i -e "s/version=0.16.0/version=0.17.0/g" python/python-zstandard/spkgbuild
 changelog "python/python-zstandard/spkgbuild" "Upgraded from version 0.16.0 to version 0.17.0"
 sed -i -e "s/version=4.15.3/version=4.15.4/g" server/samba/spkgbuild
 changelog "server/samba/spkgbuild" "Upgraded from version 4.15.3 to version 4.15.4"
+sed -i -e "s/version=2.6/version=2.7/g" core/fop/spkgbuild
+changelog "core/fop/spkgbuild" "Upgraded from version 2.6 to version 2.7"
+sed -i -e "s/version=2.9.4/version=2.9.5/g" core/icewm/spkgbuild
+changelog "core/icewm/spkgbuild" "Upgraded from version 2.9.4 to version 2.9.5"
+sed -i -e "s/version=5.16.1/version=5.16.2/g" core/linux-desktop/spkgbuild
+changelog "core/linux-desktop/spkgbuild" "Upgraded from version 5.16.1 to version 5.16.2"
+sed -i -e "s/version=5.16.1/version=5.16.2/g" core/linux-router/spkgbuild
+changelog "core/linux-router/spkgbuild" "Upgraded from version 5.16.1 to version 5.16.2"
+sed -i -e "s/version=3.2.10/version=3.2.11/g" extra/flexget/spkgbuild
+changelog "extra/flexget/spkgbuild" "Upgraded from version 3.2.10 to version 3.2.11"
+sed -i -e "s/version=1.2.1/version=1.2.2/g" libs/libwebp/spkgbuild
+changelog "libs/libwebp/spkgbuild" "Upgraded from version 1.2.1 to version 1.2.2"
+sed -i -e "s/version=1.20.39/version=1.20.40/g" python/python-boto3/spkgbuild
+changelog "python/python-boto3/spkgbuild" "Upgraded from version 1.20.39 to version 1.20.40"
+sed -i -e "s/version=1.23.39/version=1.23.40/g" python/python-botocore/spkgbuild
+changelog "python/python-botocore/spkgbuild" "Upgraded from version 1.23.39 to version 1.23.40"
+sed -i -e "s/version=1.5.1/version=1.5.1.1/g" python/python-cftime/spkgbuild
+changelog "python/python-cftime/spkgbuild" "Upgraded from version 1.5.1 to version 1.5.1.1"
+sed -i -e "s/version=0.3.0/version=0.3.2/g" python/python-edalize/spkgbuild
+changelog "python/python-edalize/spkgbuild" "Upgraded from version 0.3.0 to version 0.3.2"
+sed -i -e "s/version=4.12.0/version=4.12.1/g" python/python-oslo-utils/spkgbuild
+changelog "python/python-oslo-utils/spkgbuild" "Upgraded from version 4.12.0 to version 4.12.1"
+sed -i -e "s/version=7.3.2201062214/version=7.3.2201202112/g" python/python-pycontracts/spkgbuild
+changelog "python/python-pycontracts/spkgbuild" "Upgraded from version 7.3.2201062214 to version 7.3.2201202112"
+sed -i -e "s/version=0.3.0/version=1.0.1/g" python/python-pytest-aiohttp/spkgbuild
+changelog "python/python-pytest-aiohttp/spkgbuild" "Upgraded from version 0.3.0 to version 1.0.1"
+sed -i -e "s/version=3.1.0/version=3.2.0/g" python/python-unittest-xml-reporting/spkgbuild
+changelog "python/python-unittest-xml-reporting/spkgbuild" "Upgraded from version 3.1.0 to version 3.2.0"
