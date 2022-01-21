2a1cba92c0012ffcbd71a31ab82faddbcf0cad96
Voncloft <voncloft@gmail.com>
2022-01-21 16:37:07 -0500

    Minor Updates

---
+++ logs/old-test-updates/2022/Jan/applied_updates-01-21-22.sh
@@ -0,0 +1,30 @@
+sed -i -e "s/version=1.58.0/version=1.58.1/g" compilers/rust/spkgbuild
+changelog "compilers/rust/spkgbuild" "Upgraded from version 1.58.0 to version 1.58.1"
+sed -i -e "s/version=3.7.9/version=3.7.10/g" core/recode/spkgbuild
+changelog "core/recode/spkgbuild" "Upgraded from version 3.7.9 to version 3.7.10"
+sed -i -e "s/version=1.5.1/version=1.5.2/g" core/zstd/spkgbuild
+changelog "core/zstd/spkgbuild" "Upgraded from version 1.5.1 to version 1.5.2"
+sed -i -e "s/version=3.2.11/version=3.2.12/g" extra/flexget/spkgbuild
+changelog "extra/flexget/spkgbuild" "Upgraded from version 3.2.11 to version 3.2.12"
+sed -i -e "s/version=97.0b5/version=97.0b6/g" networking/firefox/spkgbuild
+changelog "networking/firefox/spkgbuild" "Upgraded from version 97.0b5 to version 97.0b6"
+sed -i -e "s/version=2021.12.27/version=2022.01.21/g" networking/yt-dlp/spkgbuild
+changelog "networking/yt-dlp/spkgbuild" "Upgraded from version 2021.12.27 to version 2022.01.21"
+sed -i -e "s/version=1.20.40/version=1.20.41/g" python/python-boto3/spkgbuild
+changelog "python/python-boto3/spkgbuild" "Upgraded from version 1.20.40 to version 1.20.41"
+sed -i -e "s/version=1.23.40/version=1.23.41/g" python/python-botocore/spkgbuild
+changelog "python/python-botocore/spkgbuild" "Upgraded from version 1.23.40 to version 1.23.41"
+sed -i -e "s/version=0.5.4/version=0.5.5/g" python/python-databases/spkgbuild
+changelog "python/python-databases/spkgbuild" "Upgraded from version 0.5.4 to version 0.5.5"
+sed -i -e "s/version=0.3.2/version=0.3.3/g" python/python-edalize/spkgbuild
+changelog "python/python-edalize/spkgbuild" "Upgraded from version 0.3.2 to version 0.3.3"
+sed -i -e "s/version=2.3.1/version=2.3.2/g" python/python-gphoto2/spkgbuild
+changelog "python/python-gphoto2/spkgbuild" "Upgraded from version 2.3.1 to version 2.3.2"
+sed -i -e "s/version=2.2.0/version=2.3.0/g" python/python-internetarchive/spkgbuild
+changelog "python/python-internetarchive/spkgbuild" "Upgraded from version 2.2.0 to version 2.3.0"
+sed -i -e "s/version=7.3.2201202112/version=7.3.2201211939/g" python/python-pycontracts/spkgbuild
+changelog "python/python-pycontracts/spkgbuild" "Upgraded from version 7.3.2201202112 to version 7.3.2201211939"
+sed -i -e "s/version=3.0.6/version=3.0.7/g" python/python-pyparsing/spkgbuild
+changelog "python/python-pyparsing/spkgbuild" "Upgraded from version 3.0.6 to version 3.0.7"
+sed -i -e "s/version=1.0.1/version=1.0.3/g" python/python-pytest-aiohttp/spkgbuild
+changelog "python/python-pytest-aiohttp/spkgbuild" "Upgraded from version 1.0.1 to version 1.0.3"
