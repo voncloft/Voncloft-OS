3e1ba67b304f2484f3ae3a4800b081159aa81b6c
Voncloft <voncloft@gmail.com>
2022-01-22 20:46:23 -0500

    Minor Updates

---
+++ logs/old-test-updates/2022/Jan/applied_updates-01-22-22.sh
@@ -0,0 +1,10 @@
+sed -i -e "s/version=3.2.12/version=3.2.13/g" extra/flexget/spkgbuild
+changelog "extra/flexget/spkgbuild" "Upgraded from version 3.2.12 to version 3.2.13"
+sed -i -e "s/version=1.3.0/version=1.3.1/g" python/python-basemap/spkgbuild
+changelog "python/python-basemap/spkgbuild" "Upgraded from version 1.3.0 to version 1.3.1"
+sed -i -e "s/version=1.5.1.1/version=1.5.2/g" python/python-cftime/spkgbuild
+changelog "python/python-cftime/spkgbuild" "Upgraded from version 1.5.1.1 to version 1.5.2"
+sed -i -e "s/version=3.2/version=4.0/g" python/python-libarchive-c/spkgbuild
+changelog "python/python-libarchive-c/spkgbuild" "Upgraded from version 3.2 to version 4.0"
+sed -i -e "s/version=3.6.5/version=3.6.6/g" python/python-orjson/spkgbuild
+changelog "python/python-orjson/spkgbuild" "Upgraded from version 3.6.5 to version 3.6.6"
