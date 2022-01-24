70ce75cb57fe28e19d2637c369f4fda0b4d63e51
Voncloft <voncloft@gmail.com>
2022-01-24 16:55:43 -0500

    Minor Updates

--- logs/old-test-updates/2022/Jan/applied_updates-01-24-22.sh
+++ logs/old-test-updates/2022/Jan/applied_updates-01-24-22.sh
@@ -14,3 +14,31 @@ sed -i -e "s/version=0.22/version=0.23.0/g" python/python-pyudev/spkgbuild
 changelog "python/python-pyudev/spkgbuild" "Upgraded from version 0.22 to version 0.23.0"
 sed -i -e "s/version=0.17.1/version=0.18.0/g" python/python-starlette/spkgbuild
 changelog "python/python-starlette/spkgbuild" "Upgraded from version 0.17.1 to version 0.18.0"
+sed -i -e "s/version=2.37.2/version=2.37.3/g" core/util-linux/spkgbuild
+changelog "core/util-linux/spkgbuild" "Upgraded from version 2.37.2 to version 2.37.3"
+sed -i -e "s/version=3.2.14/version=3.2.15/g" extra/flexget/spkgbuild
+changelog "extra/flexget/spkgbuild" "Upgraded from version 3.2.14 to version 3.2.15"
+sed -i -e "s/version=97.0b6/version=97.0b7/g" networking/firefox/spkgbuild
+changelog "networking/firefox/spkgbuild" "Upgraded from version 97.0b6 to version 97.0b7"
+sed -i -e "s/version=1.20.41/version=1.20.42/g" python/python-boto3/spkgbuild
+changelog "python/python-boto3/spkgbuild" "Upgraded from version 1.20.41 to version 1.20.42"
+sed -i -e "s/version=1.23.41/version=1.23.42/g" python/python-botocore/spkgbuild
+changelog "python/python-botocore/spkgbuild" "Upgraded from version 1.23.41 to version 1.23.42"
+sed -i -e "s/version=6.9/version=6.10/g" python/python-cx-freeze/spkgbuild
+changelog "python/python-cx-freeze/spkgbuild" "Upgraded from version 6.9 to version 6.10"
+sed -i -e "s/version=0.72.0/version=0.73.0/g" python/python-fastapi/spkgbuild
+changelog "python/python-fastapi/spkgbuild" "Upgraded from version 0.72.0 to version 0.73.0"
+sed -i -e "s/version=4.28.5/version=4.29.0/g" python/python-fonttools/spkgbuild
+changelog "python/python-fonttools/spkgbuild" "Upgraded from version 4.28.5 to version 4.29.0"
+sed -i -e "s/version=2.0.0/version=2.0.1/g" python/python-markdown-it-py/spkgbuild
+changelog "python/python-markdown-it-py/spkgbuild" "Upgraded from version 2.0.0 to version 2.0.1"
+sed -i -e "s/version=0.6.1/version=0.7.0/g" python/python-mccabe/spkgbuild
+changelog "python/python-mccabe/spkgbuild" "Upgraded from version 0.6.1 to version 0.7.0"
+sed -i -e "s/version=6.0.1/version=6.0.2/g" python/python-multidict/spkgbuild
+changelog "python/python-multidict/spkgbuild" "Upgraded from version 6.0.1 to version 6.0.2"
+sed -i -e "s/version=3.1.0/version=3.2.0/g" python/python-mypy-protobuf/spkgbuild
+changelog "python/python-mypy-protobuf/spkgbuild" "Upgraded from version 3.1.0 to version 3.2.0"
+sed -i -e "s/version=8.3.0/version=9.0.0/g" python/python-pdoc/spkgbuild
+changelog "python/python-pdoc/spkgbuild" "Upgraded from version 8.3.0 to version 9.0.0"
+sed -i -e "s/version=1.5.1/version=1.5.2/g" python/python-typed-ast/spkgbuild
+changelog "python/python-typed-ast/spkgbuild" "Upgraded from version 1.5.1 to version 1.5.2"
