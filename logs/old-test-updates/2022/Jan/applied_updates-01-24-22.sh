81de5bc926aec4ab7a0b3645a68a3e79319fd215
Voncloft <voncloft@gmail.com>
2022-01-24 06:40:12 -0500

    Minor Updates

---
+++ logs/old-test-updates/2022/Jan/applied_updates-01-24-22.sh
@@ -0,0 +1,16 @@
+sed -i -e "s/version=0.15.0/version=0.16.0/g" core/alacritty/spkgbuild
+changelog "core/alacritty/spkgbuild" "Upgraded from version 0.15.0 to version 0.16.0"
+sed -i -e "s/version=3.0.3/version=3.1.0/g" core/streamlink/spkgbuild
+changelog "core/streamlink/spkgbuild" "Upgraded from version 3.0.3 to version 3.1.0"
+sed -i -e "s/version=3.2.13/version=3.2.14/g" extra/flexget/spkgbuild
+changelog "extra/flexget/spkgbuild" "Upgraded from version 3.2.13 to version 3.2.14"
+sed -i -e "s/version=5.2.0/version=6.0.1/g" python/python-multidict/spkgbuild
+changelog "python/python-multidict/spkgbuild" "Upgraded from version 5.2.0 to version 6.0.1"
+sed -i -e "s/version=3.12.0/version=3.13.0/g" python/python-pycryptodome/spkgbuild
+changelog "python/python-pycryptodome/spkgbuild" "Upgraded from version 3.12.0 to version 3.13.0"
+sed -i -e "s/version=0.5.0/version=0.5.1/g" python/python-pytest-socket/spkgbuild
+changelog "python/python-pytest-socket/spkgbuild" "Upgraded from version 0.5.0 to version 0.5.1"
+sed -i -e "s/version=0.22/version=0.23.0/g" python/python-pyudev/spkgbuild
+changelog "python/python-pyudev/spkgbuild" "Upgraded from version 0.22 to version 0.23.0"
+sed -i -e "s/version=0.17.1/version=0.18.0/g" python/python-starlette/spkgbuild
+changelog "python/python-starlette/spkgbuild" "Upgraded from version 0.17.1 to version 0.18.0"
