51c4cbe0eedc20028351328c36e45a11ea7ea2fe
Voncloft <voncloft@gmail.com>
2022-01-26 05:18:22 -0500

    Minor Updates

---
+++ logs/old-test-updates/2022/Jan/applied_updates-01-26-22.sh
@@ -0,0 +1,24 @@
+sed -i -e "s/version=3.22.1/version=3.22.2/g" compilers/cmake/spkgbuild
+changelog "compilers/cmake/spkgbuild" "Upgraded from version 3.22.1 to version 3.22.2"
+sed -i -e "s/version=3.7.10/version=3.7.11/g" core/recode/spkgbuild
+changelog "core/recode/spkgbuild" "Upgraded from version 3.7.10 to version 3.7.11"
+sed -i -e "s/version=3.1.0/version=3.1.1/g" core/streamlink/spkgbuild
+changelog "core/streamlink/spkgbuild" "Upgraded from version 3.1.0 to version 3.1.1"
+sed -i -e "s/version=97.0b2/version=97.0b3/g" networking/thunderbird/spkgbuild
+changelog "networking/thunderbird/spkgbuild" "Upgraded from version 97.0b2 to version 97.0b3"
+sed -i -e "s/version=1.20.42/version=1.20.43/g" python/python-boto3/spkgbuild
+changelog "python/python-boto3/spkgbuild" "Upgraded from version 1.20.42 to version 1.20.43"
+sed -i -e "s/version=1.23.42/version=1.23.43/g" python/python-botocore/spkgbuild
+changelog "python/python-botocore/spkgbuild" "Upgraded from version 1.23.42 to version 1.23.43"
+sed -i -e "s/version=6.2/version=6.3/g" python/python-coverage/spkgbuild
+changelog "python/python-coverage/spkgbuild" "Upgraded from version 6.2 to version 6.3"
+sed -i -e "s/version=0.17.2/version=0.17.3/g" python/python-fasteners/spkgbuild
+changelog "python/python-fasteners/spkgbuild" "Upgraded from version 0.17.2 to version 0.17.3"
+sed -i -e "s/version=1.1.0/version=1.2.0/g" python/python-numpydoc/spkgbuild
+changelog "python/python-numpydoc/spkgbuild" "Upgraded from version 1.1.0 to version 1.2.0"
+sed -i -e "s/version=0.12.0/version=0.13.0/g" python/python-prometheus_client/spkgbuild
+changelog "python/python-prometheus_client/spkgbuild" "Upgraded from version 0.12.0 to version 0.13.0"
+sed -i -e "s/version=4.4.0/version=4.5.0/g" python/python-tweepy/spkgbuild
+changelog "python/python-tweepy/spkgbuild" "Upgraded from version 4.4.0 to version 4.5.0"
+sed -i -e "s/version=1.21.5/version=1.21.6/g" server/nginx/spkgbuild
+changelog "server/nginx/spkgbuild" "Upgraded from version 1.21.5 to version 1.21.6"
