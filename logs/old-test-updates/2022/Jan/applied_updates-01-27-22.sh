0b9c21320360597a58e7776a5de5084b86aa66b0
Voncloft <voncloft@gmail.com>
2022-01-27 18:37:29 -0500

    Minor Updates

---
+++ logs/old-test-updates/2022/Jan/applied_updates-01-27-22.sh
@@ -0,0 +1,26 @@
+sed -i -e "s/version=0.92.0/version=0.92.1/g" core/hugo/spkgbuild
+changelog "core/hugo/spkgbuild" "Upgraded from version 0.92.0 to version 0.92.1"
+sed -i -e "s/version=5.16.2/version=5.16.3/g" core/linux-desktop/spkgbuild
+changelog "core/linux-desktop/spkgbuild" "Upgraded from version 5.16.2 to version 5.16.3"
+sed -i -e "s/version=5.16.2/version=5.16.3/g" core/linux-router/spkgbuild
+changelog "core/linux-router/spkgbuild" "Upgraded from version 5.16.2 to version 5.16.3"
+sed -i -e "s/version=0.3.43/version=0.3.44/g" core/pipewire/spkgbuild
+changelog "core/pipewire/spkgbuild" "Upgraded from version 0.3.43 to version 0.3.44"
+sed -i -e "s/version=3.2.15/version=3.2.16/g" extra/flexget/spkgbuild
+changelog "extra/flexget/spkgbuild" "Upgraded from version 3.2.15 to version 3.2.16"
+sed -i -e "s/version=1.20/version=1.21/g" extra/iotop/spkgbuild
+changelog "extra/iotop/spkgbuild" "Upgraded from version 1.20 to version 1.21"
+sed -i -e "s/version=1.43/version=1.44/g" libs/libgpg-error/spkgbuild
+changelog "libs/libgpg-error/spkgbuild" "Upgraded from version 1.43 to version 1.44"
+sed -i -e "s/version=0.3.3/version=0.4.0/g" python/python-aioconsole/spkgbuild
+changelog "python/python-aioconsole/spkgbuild" "Upgraded from version 0.3.3 to version 0.4.0"
+sed -i -e "s/version=1.20.44/version=1.20.45/g" python/python-boto3/spkgbuild
+changelog "python/python-boto3/spkgbuild" "Upgraded from version 1.20.44 to version 1.20.45"
+sed -i -e "s/version=1.23.44/version=1.23.45/g" python/python-botocore/spkgbuild
+changelog "python/python-botocore/spkgbuild" "Upgraded from version 1.23.44 to version 1.23.45"
+sed -i -e "s/version=0.3.2/version=0.3.3/g" python/python-openapi-spec-validator/spkgbuild
+changelog "python/python-openapi-spec-validator/spkgbuild" "Upgraded from version 0.3.2 to version 0.3.3"
+sed -i -e "s/version=0.23.0/version=0.23.1/g" python/python-pyudev/spkgbuild
+changelog "python/python-pyudev/spkgbuild" "Upgraded from version 0.23.0 to version 0.23.1"
+sed -i -e "s/version=21.7.0/version=22.1.0/g" python/python-twisted/spkgbuild
+changelog "python/python-twisted/spkgbuild" "Upgraded from version 21.7.0 to version 22.1.0"
