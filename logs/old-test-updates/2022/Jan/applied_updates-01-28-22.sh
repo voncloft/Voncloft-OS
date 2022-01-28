9f2b4ff2bcb4026fb699d492ca301be62b93a15c
Voncloft <voncloft@gmail.com>
2022-01-28 15:46:43 -0500

    Minor Updates

---
+++ logs/old-test-updates/2022/Jan/applied_updates-01-28-22.sh
@@ -0,0 +1,36 @@
+sed -i -e "s/version=3.19.3/version=3.19.4/g" extra/protobuf/spkgbuild
+changelog "extra/protobuf/spkgbuild" "Upgraded from version 3.19.3 to version 3.19.4"
+sed -i -e "s/version=1.24/version=1.25/g" extra/wayland-protocols/spkgbuild
+changelog "extra/wayland-protocols/spkgbuild" "Upgraded from version 1.24 to version 1.25"
+sed -i -e "s/version=1.19.3/version=1.19.90/g" media/gst-libav/spkgbuild
+changelog "media/gst-libav/spkgbuild" "Upgraded from version 1.19.3 to version 1.19.90"
+sed -i -e "s/version=1.19.3/version=1.19.90/g" media/gst-plugins-bad/spkgbuild
+changelog "media/gst-plugins-bad/spkgbuild" "Upgraded from version 1.19.3 to version 1.19.90"
+sed -i -e "s/version=1.19.3/version=1.19.90/g" media/gst-plugins-good/spkgbuild
+changelog "media/gst-plugins-good/spkgbuild" "Upgraded from version 1.19.3 to version 1.19.90"
+sed -i -e "s/version=1.19.3/version=1.19.90/g" media/gst-plugins-ugly/spkgbuild
+changelog "media/gst-plugins-ugly/spkgbuild" "Upgraded from version 1.19.3 to version 1.19.90"
+sed -i -e "s/version=4.0.0/version=5.0.0/g" libs/libnfs/spkgbuild
+changelog "libs/libnfs/spkgbuild" "Upgraded from version 4.0.0 to version 5.0.0"
+sed -i -e "s/version=97.0b8/version=97.0b9/g" networking/firefox/spkgbuild
+changelog "networking/firefox/spkgbuild" "Upgraded from version 97.0b8 to version 97.0b9"
+sed -i -e "s/version=1.20.45/version=1.20.46/g" python/python-boto3/spkgbuild
+changelog "python/python-boto3/spkgbuild" "Upgraded from version 1.20.45 to version 1.20.46"
+sed -i -e "s/version=1.23.45/version=1.23.46/g" python/python-botocore/spkgbuild
+changelog "python/python-botocore/spkgbuild" "Upgraded from version 1.23.45 to version 1.23.46"
+sed -i -e "s/version=0.17.3/version=0.17.4/g" python/python-django-modeltranslation/spkgbuild
+changelog "python/python-django-modeltranslation/spkgbuild" "Upgraded from version 0.17.3 to version 0.17.4"
+sed -i -e "s/version=3.1.0/version=3.1.1/g" python/python-gitlab/spkgbuild
+changelog "python/python-gitlab/spkgbuild" "Upgraded from version 3.1.0 to version 3.1.1"
+sed -i -e "s/version=0.4.0/version=0.4.1/g" python/python-libcst/spkgbuild
+changelog "python/python-libcst/spkgbuild" "Upgraded from version 0.4.0 to version 0.4.1"
+sed -i -e "s/version=0.3.3/version=0.5.0/g" python/python-openapi-spec-validator/spkgbuild
+changelog "python/python-openapi-spec-validator/spkgbuild" "Upgraded from version 0.3.3 to version 0.5.0"
+sed -i -e "s/version=0.7.15/version=0.7.16/g" python/python-pony/spkgbuild
+changelog "python/python-pony/spkgbuild" "Upgraded from version 0.7.15 to version 0.7.16"
+sed -i -e "s/version=0.13.0/version=0.13.1/g" python/python-prometheus_client/spkgbuild
+changelog "python/python-prometheus_client/spkgbuild" "Upgraded from version 0.13.0 to version 0.13.1"
+sed -i -e "s/version=3.6.1/version=3.7.0/g" python/python-pytest-mock/spkgbuild
+changelog "python/python-pytest-mock/spkgbuild" "Upgraded from version 3.6.1 to version 3.7.0"
+sed -i -e "s/version=3.10.1/version=3.10.2/g" ruby-gems/ruby-rspec-core/spkgbuild
+changelog "ruby-gems/ruby-rspec-core/spkgbuild" "Upgraded from version 3.10.1 to version 3.10.2"
