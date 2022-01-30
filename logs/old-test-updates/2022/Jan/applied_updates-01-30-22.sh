54b06be133cccfbb37fd3bc93dbded78e84a2f9c
Voncloft <voncloft@gmail.com>
2022-01-30 11:11:42 -0500

    Minor Updates

---
+++ logs/old-test-updates/2022/Jan/applied_updates-01-30-22.sh
@@ -0,0 +1,18 @@
+sed -i -e "s/version=2.12/version=2.13/g" core/lcms2/spkgbuild
+changelog "core/lcms2/spkgbuild" "Upgraded from version 2.12 to version 2.13"
+sed -i -e "s/version=5.16.3/version=5.16.4/g" core/linux-desktop/spkgbuild
+changelog "core/linux-desktop/spkgbuild" "Upgraded from version 5.16.3 to version 5.16.4"
+sed -i -e "s/version=5.16.3/version=5.16.4/g" core/linux-router/spkgbuild
+changelog "core/linux-router/spkgbuild" "Upgraded from version 5.16.3 to version 5.16.4"
+sed -i -e "s/version=2022.01.0/version=2022.01.1/g" python/python-distributed/spkgbuild
+changelog "python/python-distributed/spkgbuild" "Upgraded from version 2022.01.0 to version 2022.01.1"
+sed -i -e "s/version=3.23.0/version=4.0.0/g" python/python-mongomock/spkgbuild
+changelog "python/python-mongomock/spkgbuild" "Upgraded from version 3.23.0 to version 4.0.0"
+sed -i -e "s/version=8.12.41/version=8.12.42/g" python/python-phonenumbers/spkgbuild
+changelog "python/python-phonenumbers/spkgbuild" "Upgraded from version 8.12.41 to version 8.12.42"
+sed -i -e "s/version=1.3.0/version=1.3.1/g" python/python-requests-oauthlib/spkgbuild
+changelog "python/python-requests-oauthlib/spkgbuild" "Upgraded from version 1.3.0 to version 1.3.1"
+sed -i -e "s/version=3.10.2/version=3.10.3/g" ruby-gems/ruby-rspec-mocks/spkgbuild
+changelog "ruby-gems/ruby-rspec-mocks/spkgbuild" "Upgraded from version 3.10.2 to version 3.10.3"
+sed -i -e "s/version=1.2.5/version=1.2.6/g" xfce/xfce4-cpufreq-plugin/spkgbuild
+changelog "xfce/xfce4-cpufreq-plugin/spkgbuild" "Upgraded from version 1.2.5 to version 1.2.6"
