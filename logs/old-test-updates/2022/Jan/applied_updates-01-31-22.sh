b181af917fd86c7d10d4e47a468d48620a1c77c0
Voncloft <voncloft@gmail.com>
2022-01-31 06:34:10 -0500

    Minor Updates

---
+++ logs/old-test-updates/2022/Jan/applied_updates-01-31-22.sh
@@ -0,0 +1,20 @@
+sed -i -e "s/version=0.2.8/version=0.2.9/g" core/waylandpp/spkgbuild
+changelog "core/waylandpp/spkgbuild" "Upgraded from version 0.2.8 to version 0.2.9"
+sed -i -e "s/version=2.4.3/version=2.4.4/g" libs/expat/spkgbuild
+changelog "libs/expat/spkgbuild" "Upgraded from version 2.4.3 to version 2.4.4"
+sed -i -e "s/version=4.31.0/version=4.32.0/g" python/python-breathe/spkgbuild
+changelog "python/python-breathe/spkgbuild" "Upgraded from version 4.31.0 to version 4.32.0"
+sed -i -e "s/version=0.17.4/version=0.17.5/g" python/python-django-modeltranslation/spkgbuild
+changelog "python/python-django-modeltranslation/spkgbuild" "Upgraded from version 0.17.4 to version 0.17.5"
+sed -i -e "s/version=0.2.3/version=0.2.24/g" python/python-flake8-black/spkgbuild
+changelog "python/python-flake8-black/spkgbuild" "Upgraded from version 0.2.3 to version 0.2.24"
+sed -i -e "s/version=21.3.1/version=22.0.1/g" python/python-pip/spkgbuild
+changelog "python/python-pip/spkgbuild" "Upgraded from version 21.3.1 to version 22.0.1"
+sed -i -e "s/version=3.13.0/version=3.14.0/g" python/python-pycryptodome/spkgbuild
+changelog "python/python-pycryptodome/spkgbuild" "Upgraded from version 3.13.0 to version 3.14.0"
+sed -i -e "s/version=0.8.1/version=0.9.0/g" python/python-pytest-mypy/spkgbuild
+changelog "python/python-pytest-mypy/spkgbuild" "Upgraded from version 0.8.1 to version 0.9.0"
+sed -i -e "s/version=21.5.0/version=22.1.0/g" python/python-treq/spkgbuild
+changelog "python/python-treq/spkgbuild" "Upgraded from version 21.5.0 to version 22.1.0"
+sed -i -e "s/version=1.7.0/version=1.8.0/g" python/python-userpath/spkgbuild
+changelog "python/python-userpath/spkgbuild" "Upgraded from version 1.7.0 to version 1.8.0"
