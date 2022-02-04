d2abf82a245f26b095ea9823fd9e337ed7106d49
Voncloft <voncloft@gmail.com>
2022-02-04 17:19:25 -0500

    fixed pkgconf to include 32 and 64 bit locations

---
+++ REPOS/test_updates.sh
@@ -0,0 +1,28 @@
+sed -i -e "s/version=1.22/version=1.23/g" core/gdbm/spkgbuild
+changelog "core/gdbm/spkgbuild" "Upgraded from version 1.22 to version 1.23"
+sed -i -e "s/version=3.2.16/version=3.2.17/g" extra/flexget/spkgbuild
+changelog "extra/flexget/spkgbuild" "Upgraded from version 3.2.16 to version 3.2.17"
+sed -i -e "s/version=2.13.95/version=2.13.96/g" fonts/fontconfig/spkgbuild
+changelog "fonts/fontconfig/spkgbuild" "Upgraded from version 2.13.95 to version 2.13.96"
+sed -i -e "s/version=1.19.90/version=1.20.0/g" media/gst-libav/spkgbuild
+changelog "media/gst-libav/spkgbuild" "Upgraded from version 1.19.90 to version 1.20.0"
+sed -i -e "s/version=1.19.90/version=1.20.0/g" media/gst-plugins-bad/spkgbuild
+changelog "media/gst-plugins-bad/spkgbuild" "Upgraded from version 1.19.90 to version 1.20.0"
+sed -i -e "s/version=1.19.90/version=1.20.0/g" media/gst-plugins-good/spkgbuild
+changelog "media/gst-plugins-good/spkgbuild" "Upgraded from version 1.19.90 to version 1.20.0"
+sed -i -e "s/version=1.19.90/version=1.20.0/g" media/gst-plugins-ugly/spkgbuild
+changelog "media/gst-plugins-ugly/spkgbuild" "Upgraded from version 1.19.90 to version 1.20.0"
+sed -i -e "s/version=1.3.1/version=1.4.0/g" libs/libxkbcommon/spkgbuild
+changelog "libs/libxkbcommon/spkgbuild" "Upgraded from version 1.3.1 to version 1.4.0"
+sed -i -e "s/version=2022.02.03/version=2022.02.04/g" networking/yt-dlp/spkgbuild
+changelog "networking/yt-dlp/spkgbuild" "Upgraded from version 2022.02.03 to version 2022.02.04"
+sed -i -e "s/version=12.0.0/version=12.1.0/g" python/python-faker/spkgbuild
+changelog "python/python-faker/spkgbuild" "Upgraded from version 12.0.0 to version 12.1.0"
+sed -i -e "s/version=0.14.6/version=0.14.7/g" python/python-httpcore/spkgbuild
+changelog "python/python-httpcore/spkgbuild" "Upgraded from version 0.14.6 to version 0.14.7"
+sed -i -e "s/version=3.14.0/version=3.14.1/g" python/python-pycryptodome/spkgbuild
+changelog "python/python-pycryptodome/spkgbuild" "Upgraded from version 3.14.0 to version 3.14.1"
+sed -i -e "s/version=1.7.2/version=1.8.0/g" python/python-pygit2/spkgbuild
+changelog "python/python-pygit2/spkgbuild" "Upgraded from version 1.7.2 to version 1.8.0"
+sed -i -e "s/version=0.23.1/version=0.23.2/g" python/python-pyudev/spkgbuild
+changelog "python/python-pyudev/spkgbuild" "Upgraded from version 0.23.1 to version 0.23.2"
