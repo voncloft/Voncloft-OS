94b324a68613df1666a1d96f8694541d39a1a5f2
Voncloft <voncloft@gmail.com>
2022-02-05 15:42:49 -0500

    Minor Updates

---
+++ logs/old-test-updates/2022/Feb/applied_updates-02-05-22.sh
@@ -0,0 +1,18 @@
+sed -i -e "s/version=5.16.5/version=5.16.7/g" core/linux-desktop/spkgbuild
+changelog "core/linux-desktop/spkgbuild" "Upgraded from version 5.16.5 to version 5.16.7"
+sed -i -e "s/version=5.16.5/version=5.16.7/g" core/linux-router/spkgbuild
+changelog "core/linux-router/spkgbuild" "Upgraded from version 5.16.5 to version 5.16.7"
+sed -i -e "s/version=3.2.17/version=3.2.18/g" extra/flexget/spkgbuild
+changelog "extra/flexget/spkgbuild" "Upgraded from version 3.2.17 to version 3.2.18"
+sed -i -e "s/version=3.0.13/version=3.0.14/g" libs/libical/spkgbuild
+changelog "libs/libical/spkgbuild" "Upgraded from version 3.0.13 to version 3.0.14"
+sed -i -e "s/version=1.20.48/version=1.20.49/g" python/python-boto3/spkgbuild
+changelog "python/python-boto3/spkgbuild" "Upgraded from version 1.20.48 to version 1.20.49"
+sed -i -e "s/version=1.23.48/version=1.23.49/g" python/python-botocore/spkgbuild
+changelog "python/python-botocore/spkgbuild" "Upgraded from version 1.23.48 to version 1.23.49"
+sed -i -e "s/version=0.19.0/version=0.20.0/g" python/python-pytest-httpx/spkgbuild
+changelog "python/python-pytest-httpx/spkgbuild" "Upgraded from version 0.19.0 to version 0.20.0"
+sed -i -e "s/version=5.9.8/version=5.9.9/g" python/python-softlayer/spkgbuild
+changelog "python/python-softlayer/spkgbuild" "Upgraded from version 5.9.8 to version 5.9.9"
+sed -i -e "s/version=20.13.0/version=20.13.1/g" python/python-virtualenv/spkgbuild
+changelog "python/python-virtualenv/spkgbuild" "Upgraded from version 20.13.0 to version 20.13.1"
