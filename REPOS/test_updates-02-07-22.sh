0226c0f1275a72e11132779d346a7949f6f425b0
Voncloft <voncloft@gmail.com>
2022-02-07 13:01:39 -0500

    Minor Updates

---
+++ REPOS/test_updates-02-07-22.sh
@@ -0,0 +1,12 @@
+sed -i -e "s/version=3.2.1/version=3.2.2/g" core/freeglut/spkgbuild
+changelog "core/freeglut/spkgbuild" "Upgraded from version 3.2.1 to version 3.2.2"
+sed -i -e "s/version=2.0.1/version=3.0.0/g" python/python-libusb1/spkgbuild
+changelog "python/python-libusb1/spkgbuild" "Upgraded from version 2.0.1 to version 3.0.0"
+sed -i -e "s/version=60.7.1/version=60.8.1/g" python/python-setuptools/spkgbuild
+changelog "python/python-setuptools/spkgbuild" "Upgraded from version 60.7.1 to version 60.8.1"
+sed -i -e "s/version=2.2/version=2.2.1/g" python/python-sphobjinv/spkgbuild
+changelog "python/python-sphobjinv/spkgbuild" "Upgraded from version 2.2 to version 2.2.1"
+sed -i -e "s/version=0.11.0/version=0.11.1/g" ruby-gems/ruby-rb-fsevent/spkgbuild
+changelog "ruby-gems/ruby-rb-fsevent/spkgbuild" "Upgraded from version 0.11.0 to version 0.11.1"
+sed -i -e "s/version=3.6.4/version=3.7.0/g" server/postfix/spkgbuild
+changelog "server/postfix/spkgbuild" "Upgraded from version 3.6.4 to version 3.7.0"
