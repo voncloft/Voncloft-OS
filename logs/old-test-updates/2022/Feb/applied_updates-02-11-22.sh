8a258ef9847f675b4a0a13515712ac3cce2f023e
Voncloft <voncloft@gmail.com>
2022-02-11 15:47:42 -0500

    Minor Updates

--- logs/old-test-updates/2022/Feb/applied_updates-02-11-22.sh
+++ logs/old-test-updates/2022/Feb/applied_updates-02-11-22.sh
@@ -14,3 +14,39 @@ sed -i -e "s/version=5.2.0/version=5.2.1/g" python/python-pynamodb/spkgbuild
 changelog "python/python-pynamodb/spkgbuild" "Upgraded from version 5.2.0 to version 5.2.1"
 sed -i -e "s/version=0.18.0/version=0.18.1/g" python/python-pytest-asyncio/spkgbuild
 changelog "python/python-pytest-asyncio/spkgbuild" "Upgraded from version 0.18.0 to version 0.18.1"
+sed -i -e "s/version=5.0/version=5.1/g" core/dmenu/spkgbuild
+changelog "core/dmenu/spkgbuild" "Upgraded from version 5.0 to version 5.1"
+sed -i -e "s/version=0.92.1/version=0.92.2/g" core/hugo/spkgbuild
+changelog "core/hugo/spkgbuild" "Upgraded from version 0.92.1 to version 0.92.2"
+sed -i -e "s/version=5.16.8/version=5.16.9/g" core/linux-desktop/spkgbuild
+changelog "core/linux-desktop/spkgbuild" "Upgraded from version 5.16.8 to version 5.16.9"
+sed -i -e "s/version=5.16.8/version=5.16.9/g" core/linux-router/spkgbuild
+changelog "core/linux-router/spkgbuild" "Upgraded from version 5.16.8 to version 5.16.9"
+sed -i -e "s/version=3.0.7/version=3.0.8/g" libs/guile/spkgbuild
+changelog "libs/guile/spkgbuild" "Upgraded from version 3.0.7 to version 3.0.8"
+sed -i -e "s/version=1.30.2/version=1.30.4/g" libs/libqmi/spkgbuild
+changelog "libs/libqmi/spkgbuild" "Upgraded from version 1.30.2 to version 1.30.4"
+sed -i -e "s/version=2.0.0/version=2.1.0/g" libs/libwacom/spkgbuild
+changelog "libs/libwacom/spkgbuild" "Upgraded from version 2.0.0 to version 2.1.0"
+sed -i -e "s/version=98.0b2/version=98.0b3/g" networking/firefox/spkgbuild
+changelog "networking/firefox/spkgbuild" "Upgraded from version 98.0b2 to version 98.0b3"
+sed -i -e "s/version=97.0b3/version=98.0b1/g" networking/thunderbird/spkgbuild
+changelog "networking/thunderbird/spkgbuild" "Upgraded from version 97.0b3 to version 98.0b1"
+sed -i -e "s/version=2.1.0/version=2.1.1/g" python/python-aiobotocore/spkgbuild
+changelog "python/python-aiobotocore/spkgbuild" "Upgraded from version 2.1.0 to version 2.1.1"
+sed -i -e "s/version=1.20.53/version=1.20.54/g" python/python-boto3/spkgbuild
+changelog "python/python-boto3/spkgbuild" "Upgraded from version 1.20.53 to version 1.20.54"
+sed -i -e "s/version=1.23.53/version=1.23.54/g" python/python-botocore/spkgbuild
+changelog "python/python-botocore/spkgbuild" "Upgraded from version 1.23.53 to version 1.23.54"
+sed -i -e "s/version=2022.01.1/version=2022.02.0/g" python/python-distributed/spkgbuild
+changelog "python/python-distributed/spkgbuild" "Upgraded from version 2022.01.1 to version 2022.02.0"
+sed -i -e "s/version=4.10.1/version=4.11.0/g" python/python-importlib-metadata/spkgbuild
+changelog "python/python-importlib-metadata/spkgbuild" "Upgraded from version 4.10.1 to version 4.11.0"
+sed -i -e "s/version=0.16.1/version=0.17.0/g" python/python-myst-parser/spkgbuild
+changelog "python/python-myst-parser/spkgbuild" "Upgraded from version 0.16.1 to version 0.17.0"
+sed -i -e "s/version=2.7.0/version=2.8.0/g" python/python-tensorflow-serving-api/spkgbuild
+changelog "python/python-tensorflow-serving-api/spkgbuild" "Upgraded from version 2.7.0 to version 2.8.0"
+sed -i -e "s/version=4.4/version=4.5/g" python/python-zope-exceptions/spkgbuild
+changelog "python/python-zope-exceptions/spkgbuild" "Upgraded from version 4.4 to version 4.5"
+sed -i -e "s/version=0.1.2/version=0.1.3/g" xorg/xdg-dbus-proxy/spkgbuild
+changelog "xorg/xdg-dbus-proxy/spkgbuild" "Upgraded from version 0.1.2 to version 0.1.3"
