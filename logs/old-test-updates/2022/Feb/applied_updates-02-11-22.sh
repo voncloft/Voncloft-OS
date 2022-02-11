66260cb5283c956e9fec430bceef5667cb1ba03f
Voncloft <voncloft@gmail.com>
2022-02-11 09:37:53 -0500

    Minor Updates

---
+++ logs/old-test-updates/2022/Feb/applied_updates-02-11-22.sh
@@ -0,0 +1,16 @@
+sed -i -e "s/version=5.2.0/version=5.2.1/g" cinnamon/cinnamon-screensaver/spkgbuild
+changelog "cinnamon/cinnamon-screensaver/spkgbuild" "Upgraded from version 5.2.0 to version 5.2.1"
+sed -i -e "s/version=1.3.1/version=1.3.2/g" python/python-basemap/spkgbuild
+changelog "python/python-basemap/spkgbuild" "Upgraded from version 1.3.1 to version 1.3.2"
+sed -i -e "s/version=1.20.52/version=1.20.53/g" python/python-boto3/spkgbuild
+changelog "python/python-boto3/spkgbuild" "Upgraded from version 1.20.52 to version 1.20.53"
+sed -i -e "s/version=1.23.52/version=1.23.53/g" python/python-botocore/spkgbuild
+changelog "python/python-botocore/spkgbuild" "Upgraded from version 1.23.52 to version 1.23.53"
+sed -i -e "s/version=12.1.0/version=12.3.0/g" python/python-faker/spkgbuild
+changelog "python/python-faker/spkgbuild" "Upgraded from version 12.1.0 to version 12.3.0"
+sed -i -e "s/version=8.12.42/version=8.12.43/g" python/python-phonenumbers/spkgbuild
+changelog "python/python-phonenumbers/spkgbuild" "Upgraded from version 8.12.42 to version 8.12.43"
+sed -i -e "s/version=5.2.0/version=5.2.1/g" python/python-pynamodb/spkgbuild
+changelog "python/python-pynamodb/spkgbuild" "Upgraded from version 5.2.0 to version 5.2.1"
+sed -i -e "s/version=0.18.0/version=0.18.1/g" python/python-pytest-asyncio/spkgbuild
+changelog "python/python-pytest-asyncio/spkgbuild" "Upgraded from version 0.18.0 to version 0.18.1"
