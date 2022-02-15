ef1c897a153c829228709446fb51bd2df9d17713
Voncloft <voncloft@gmail.com>
2022-02-15 08:48:25 -0500

    Minor Updates

---
+++ logs/old-test-updates/2022/Feb/applied_updates-02-15-22.sh
@@ -0,0 +1,34 @@
+sed -i -e "s/version=0.61.1/version=0.61.2/g" compilers/meson/spkgbuild
+changelog "compilers/meson/spkgbuild" "Upgraded from version 0.61.1 to version 0.61.2"
+sed -i -e "s/version=3.22.0/version=4.0.0/g" core/qalculate/spkgbuild
+changelog "core/qalculate/spkgbuild" "Upgraded from version 3.22.0 to version 4.0.0"
+sed -i -e "s/version=2.37.3/version=2.37.4/g" core/util-linux/spkgbuild
+changelog "core/util-linux/spkgbuild" "Upgraded from version 2.37.3 to version 2.37.4"
+sed -i -e "s/version=3.2.18/version=3.3.0/g" extra/flexget/spkgbuild
+changelog "extra/flexget/spkgbuild" "Upgraded from version 3.2.18 to version 3.3.0"
+sed -i -e "s/version=3.22.0/version=4.0.0/g" libs/libqalculate/spkgbuild
+changelog "libs/libqalculate/spkgbuild" "Upgraded from version 3.22.0 to version 4.0.0"
+sed -i -e "s/version=98.0b3/version=98.0b4/g" networking/firefox/spkgbuild
+changelog "networking/firefox/spkgbuild" "Upgraded from version 98.0b3 to version 98.0b4"
+sed -i -e "s/version=1.0.8/version=1.0.9/g" networking/libnetfilter_conntrack/spkgbuild
+changelog "networking/libnetfilter_conntrack/spkgbuild" "Upgraded from version 1.0.8 to version 1.0.9"
+sed -i -e "s/version=1.20.54/version=1.21.0/g" python/python-boto3/spkgbuild
+changelog "python/python-boto3/spkgbuild" "Upgraded from version 1.20.54 to version 1.21.0"
+sed -i -e "s/version=1.23.54/version=1.24.0/g" python/python-botocore/spkgbuild
+changelog "python/python-botocore/spkgbuild" "Upgraded from version 1.23.54 to version 1.24.0"
+sed -i -e "s/version=4.32.0/version=4.33.1/g" python/python-breathe/spkgbuild
+changelog "python/python-breathe/spkgbuild" "Upgraded from version 4.32.0 to version 4.33.1"
+sed -i -e "s/version=12.3.0/version=12.3.3/g" python/python-faker/spkgbuild
+changelog "python/python-faker/spkgbuild" "Upgraded from version 12.3.0 to version 12.3.3"
+sed -i -e "s/version=2.0.2/version=2.0.3/g" python/python-flask/spkgbuild
+changelog "python/python-flask/spkgbuild" "Upgraded from version 2.0.2 to version 2.0.3"
+sed -i -e "s/version=7.7.0/version=7.8.0/g" python/python-neutronclient/spkgbuild
+changelog "python/python-neutronclient/spkgbuild" "Upgraded from version 7.7.0 to version 7.8.0"
+sed -i -e "s/version=3.6.6/version=3.6.7/g" python/python-orjson/spkgbuild
+changelog "python/python-orjson/spkgbuild" "Upgraded from version 3.6.6 to version 3.6.7"
+sed -i -e "s/version=9.0.1/version=10.0.1/g" python/python-pdoc/spkgbuild
+changelog "python/python-pdoc/spkgbuild" "Upgraded from version 9.0.1 to version 10.0.1"
+sed -i -e "s/version=2022.01.02/version=2022.02.14.1/g" python/python-sphinx-furo/spkgbuild
+changelog "python/python-sphinx-furo/spkgbuild" "Upgraded from version 2022.01.02 to version 2022.02.14.1"
+sed -i -e "s/version=1.7.2/version=1.8.0/g" python/python-yarl/spkgbuild
+changelog "python/python-yarl/spkgbuild" "Upgraded from version 1.7.2 to version 1.8.0"
