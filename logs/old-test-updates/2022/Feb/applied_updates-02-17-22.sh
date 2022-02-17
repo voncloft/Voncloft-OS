da8d369a8537113257666d3e191d85376012b747
Voncloft <voncloft@gmail.com>
2022-02-17 05:19:30 -0500

    Minor Updates

---
+++ logs/old-test-updates/2022/Feb/applied_updates-02-17-22.sh
@@ -0,0 +1,34 @@
+sed -i -e "s/version=1.4.5/version=1.4.6/g" core/colord/spkgbuild
+changelog "core/colord/spkgbuild" "Upgraded from version 1.4.5 to version 1.4.6"
+sed -i -e "s/version=5.16.9/version=5.16.10/g" core/linux-desktop/spkgbuild
+changelog "core/linux-desktop/spkgbuild" "Upgraded from version 5.16.9 to version 5.16.10"
+sed -i -e "s/version=5.16.9/version=5.16.10/g" core/linux-router/spkgbuild
+changelog "core/linux-router/spkgbuild" "Upgraded from version 5.16.9 to version 5.16.10"
+sed -i -e "s/version=1.12/version=1.12.1/g" core/pugixml/spkgbuild
+changelog "core/pugixml/spkgbuild" "Upgraded from version 1.12 to version 1.12.1"
+sed -i -e "s/version=2.4.109/version=2.4.110/g" libs/libdrm/spkgbuild
+changelog "libs/libdrm/spkgbuild" "Upgraded from version 2.4.109 to version 2.4.110"
+sed -i -e "s/version=98.0b4/version=98.0b5/g" networking/firefox/spkgbuild
+changelog "networking/firefox/spkgbuild" "Upgraded from version 98.0b4 to version 98.0b5"
+sed -i -e "s/version=1.21.0/version=1.21.1/g" python/python-boto3/spkgbuild
+changelog "python/python-boto3/spkgbuild" "Upgraded from version 1.21.0 to version 1.21.1"
+sed -i -e "s/version=1.24.0/version=1.24.1/g" python/python-botocore/spkgbuild
+changelog "python/python-botocore/spkgbuild" "Upgraded from version 1.24.0 to version 1.24.1"
+sed -i -e "s/version=1.7.2/version=1.7.3/g" python/python-gssapi/spkgbuild
+changelog "python/python-gssapi/spkgbuild" "Upgraded from version 1.7.2 to version 1.7.3"
+sed -i -e "s/version=4.9.1/version=4.9.2/g" python/python-jupyter_core/spkgbuild
+changelog "python/python-jupyter_core/spkgbuild" "Upgraded from version 4.9.1 to version 4.9.2"
+sed -i -e "s/version=4.0.0/version=4.1.0/g" python/python-oslo-context/spkgbuild
+changelog "python/python-oslo-context/spkgbuild" "Upgraded from version 4.0.0 to version 4.1.0"
+sed -i -e "s/version=4.2.0/version=4.3.0/g" python/python-oslo-serialization/spkgbuild
+changelog "python/python-oslo-serialization/spkgbuild" "Upgraded from version 4.2.0 to version 4.3.0"
+sed -i -e "s/version=4.12.1/version=4.12.2/g" python/python-oslo-utils/spkgbuild
+changelog "python/python-oslo-utils/spkgbuild" "Upgraded from version 4.12.1 to version 4.12.2"
+sed -i -e "s/version=1.3.0/version=1.3.1/g" python/python-requests-credssp/spkgbuild
+changelog "python/python-requests-credssp/spkgbuild" "Upgraded from version 1.3.0 to version 1.3.1"
+sed -i -e "s/version=60.9.1/version=60.9.2/g" python/python-setuptools/spkgbuild
+changelog "python/python-setuptools/spkgbuild" "Upgraded from version 60.9.1 to version 60.9.2"
+sed -i -e "s/version=0.15.0/version=0.16.0/g" python/python-thrift/spkgbuild
+changelog "python/python-thrift/spkgbuild" "Upgraded from version 0.15.0 to version 0.16.0"
+sed -i -e "s/version=4.4.0/version=4.4.1/g" qt/qbittorrent/spkgbuild
+changelog "qt/qbittorrent/spkgbuild" "Upgraded from version 4.4.0 to version 4.4.1"
