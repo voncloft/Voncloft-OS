d05c8ba0a3abc8f461858fdd50ee0764e3fb792c
Voncloft <voncloft@gmail.com>
2022-02-18 07:53:40 -0500

    Minor Updates

---
+++ logs/old-test-updates/2022/Feb/applied_updates-02-18-22.sh
@@ -0,0 +1,18 @@
+sed -i -e "s/version=0.5.5/version=0.5.6/g" core/bamf/spkgbuild
+changelog "core/bamf/spkgbuild" "Upgraded from version 0.5.5 to version 0.5.6"
+sed -i -e "s/version=0.3.45/version=0.3.46/g" core/pipewire/spkgbuild
+changelog "core/pipewire/spkgbuild" "Upgraded from version 0.3.45 to version 0.3.46"
+sed -i -e "s/version=3.7.11/version=3.7.12/g" core/recode/spkgbuild
+changelog "core/recode/spkgbuild" "Upgraded from version 3.7.11 to version 3.7.12"
+sed -i -e "s/version=2.13.0/version=2.14.0/g" libs/libva/spkgbuild
+changelog "libs/libva/spkgbuild" "Upgraded from version 2.13.0 to version 2.14.0"
+sed -i -e "s/version=1.21.1/version=1.21.2/g" python/python-boto3/spkgbuild
+changelog "python/python-boto3/spkgbuild" "Upgraded from version 1.21.1 to version 1.21.2"
+sed -i -e "s/version=1.24.1/version=1.24.2/g" python/python-botocore/spkgbuild
+changelog "python/python-botocore/spkgbuild" "Upgraded from version 1.24.1 to version 1.24.2"
+sed -i -e "s/version=0.58.0/version=0.58.1/g" python/python-cfn-lint/spkgbuild
+changelog "python/python-cfn-lint/spkgbuild" "Upgraded from version 0.58.0 to version 0.58.1"
+sed -i -e "s/version=0.73.0/version=0.74.0/g" python/python-fastapi/spkgbuild
+changelog "python/python-fastapi/spkgbuild" "Upgraded from version 0.73.0 to version 0.74.0"
+sed -i -e "s/version=4.7.1/version=4.8.0/g" python/python-lxml/spkgbuild
+changelog "python/python-lxml/spkgbuild" "Upgraded from version 4.7.1 to version 4.8.0"
