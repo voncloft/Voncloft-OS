fb6bc79126b544b24f7772a99593be98113c8c98
Voncloft <voncloft@gmail.com>
2022-02-18 22:18:08 -0500

    Minor Updates

--- logs/old-test-updates/2022/Feb/applied_updates-02-18-22.sh
+++ logs/old-test-updates/2022/Feb/applied_updates-02-18-22.sh
@@ -16,3 +16,27 @@ sed -i -e "s/version=0.73.0/version=0.74.0/g" python/python-fastapi/spkgbuild
 changelog "python/python-fastapi/spkgbuild" "Upgraded from version 0.73.0 to version 0.74.0"
 sed -i -e "s/version=4.7.1/version=4.8.0/g" python/python-lxml/spkgbuild
 changelog "python/python-lxml/spkgbuild" "Upgraded from version 4.7.1 to version 4.8.0"
+sed -i -e "s/version=6.0.2/version=6.0.3/g" core/mercurial/spkgbuild
+changelog "core/mercurial/spkgbuild" "Upgraded from version 6.0.2 to version 6.0.3"
+sed -i -e "s/version=0.3.46/version=0.3.47/g" core/pipewire/spkgbuild
+changelog "core/pipewire/spkgbuild" "Upgraded from version 0.3.46 to version 0.3.47"
+sed -i -e "s/version=98.0b5/version=98.0b6/g" networking/firefox/spkgbuild
+changelog "networking/firefox/spkgbuild" "Upgraded from version 98.0b5 to version 98.0b6"
+sed -i -e "s/version=1.21.2/version=1.21.3/g" python/python-boto3/spkgbuild
+changelog "python/python-boto3/spkgbuild" "Upgraded from version 1.21.2 to version 1.21.3"
+sed -i -e "s/version=1.24.2/version=1.24.3/g" python/python-botocore/spkgbuild
+changelog "python/python-botocore/spkgbuild" "Upgraded from version 1.24.2 to version 1.24.3"
+sed -i -e "s/version=4.13.1/version=4.14.0/g" python/python-braintree/spkgbuild
+changelog "python/python-braintree/spkgbuild" "Upgraded from version 4.13.1 to version 4.14.0"
+sed -i -e "s/version=3.10.0/version=3.10.1/g" python/python-cliff/spkgbuild
+changelog "python/python-cliff/spkgbuild" "Upgraded from version 3.10.0 to version 3.10.1"
+sed -i -e "s/version=6.36.2/version=6.37.0/g" python/python-hypothesis/spkgbuild
+changelog "python/python-hypothesis/spkgbuild" "Upgraded from version 6.36.2 to version 6.37.0"
+sed -i -e "s/version=2.4.2/version=2.5.0/g" python/python-osc-lib/spkgbuild
+changelog "python/python-osc-lib/spkgbuild" "Upgraded from version 2.4.2 to version 2.5.0"
+sed -i -e "s/version=60.9.2/version=60.9.3/g" python/python-setuptools/spkgbuild
+changelog "python/python-setuptools/spkgbuild" "Upgraded from version 60.9.2 to version 60.9.3"
+sed -i -e "s/version=0.8.1/version=0.9.0/g" python/python-symengine/spkgbuild
+changelog "python/python-symengine/spkgbuild" "Upgraded from version 0.8.1 to version 0.9.0"
+sed -i -e "s/version=1.9/version=1.10/g" python/python-sympy/spkgbuild
+changelog "python/python-sympy/spkgbuild" "Upgraded from version 1.9 to version 1.10"
