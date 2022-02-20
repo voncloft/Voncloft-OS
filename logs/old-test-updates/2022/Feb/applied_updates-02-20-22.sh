1fcafa5daf31efe8050586c6a067e07a92978701
Voncloft <voncloft@gmail.com>
2022-02-20 10:21:16 -0500

    Minor Updates

---
+++ logs/old-test-updates/2022/Feb/applied_updates-02-20-22.sh
@@ -0,0 +1,8 @@
+sed -i -e "s/version=2.4.4/version=2.4.5/g" libs/expat/spkgbuild
+changelog "libs/expat/spkgbuild" "Upgraded from version 2.4.4 to version 2.4.5"
+sed -i -e "s/version=8.0.3/version=8.0.4/g" python/python-click/spkgbuild
+changelog "python/python-click/spkgbuild" "Upgraded from version 8.0.3 to version 8.0.4"
+sed -i -e "s/version=1.5.5/version=1.6.0/g" python/python-cpplint/spkgbuild
+changelog "python/python-cpplint/spkgbuild" "Upgraded from version 1.5.5 to version 1.6.0"
+sed -i -e "s/version=0.2.0/version=0.2.1/g" python/python-protego/spkgbuild
+changelog "python/python-protego/spkgbuild" "Upgraded from version 0.2.0 to version 0.2.1"
