aa2cc408adadcbbb8695aa0e66864b616ed26c6b
Voncloft <voncloft@gmail.com>
2022-02-21 11:07:32 -0500

    Minor Updates

---
+++ logs/old-test-updates/2022/Feb/applied_updates-02-21-22.sh
@@ -0,0 +1,10 @@
+sed -i -e "s/version=1.5.15/version=1.5.16/g" cinnamon/system-config-printer/spkgbuild
+changelog "cinnamon/system-config-printer/spkgbuild" "Upgraded from version 1.5.15 to version 1.5.16"
+sed -i -e "s/version=2.4.5/version=2.4.6/g" libs/expat/spkgbuild
+changelog "libs/expat/spkgbuild" "Upgraded from version 2.4.5 to version 2.4.6"
+sed -i -e "s/version=1.25.7/version=1.25.9/g" nonfree/veracrypt/spkgbuild
+changelog "nonfree/veracrypt/spkgbuild" "Upgraded from version 1.25.7 to version 1.25.9"
+sed -i -e "s/version=3.0.1/version=3.1.0/g" python/python-fints/spkgbuild
+changelog "python/python-fints/spkgbuild" "Upgraded from version 3.0.1 to version 3.1.0"
+sed -i -e "s/version=2.6.3/version=2.7/g" python/python-networkx/spkgbuild
+changelog "python/python-networkx/spkgbuild" "Upgraded from version 2.6.3 to version 2.7"
