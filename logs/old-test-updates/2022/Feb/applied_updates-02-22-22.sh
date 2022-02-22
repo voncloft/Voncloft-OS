79de705cd0b4bf84913f6b4f58743161e4bb03eb
Voncloft <voncloft@gmail.com>
2022-02-22 09:22:12 -0500

    Minor Updates

---
+++ logs/old-test-updates/2022/Feb/applied_updates-02-22-22.sh
@@ -0,0 +1,28 @@
+sed -i -e "s/version=15.0/version=15.99.1/g" media/pulseaudio/spkgbuild
+changelog "media/pulseaudio/spkgbuild" "Upgraded from version 15.0 to version 15.99.1"
+sed -i -e "s/version=98.0b6/version=98.0b7/g" networking/firefox/spkgbuild
+changelog "networking/firefox/spkgbuild" "Upgraded from version 98.0b6 to version 98.0b7"
+sed -i -e "s/version=1.0.1/version=1.0.2/g" networking/nftables/spkgbuild
+changelog "networking/nftables/spkgbuild" "Upgraded from version 1.0.1 to version 1.0.2"
+sed -i -e "s/version=1.21.3/version=1.21.4/g" python/python-boto3/spkgbuild
+changelog "python/python-boto3/spkgbuild" "Upgraded from version 1.21.3 to version 1.21.4"
+sed -i -e "s/version=1.24.3/version=1.24.4/g" python/python-botocore/spkgbuild
+changelog "python/python-botocore/spkgbuild" "Upgraded from version 1.24.3 to version 1.24.4"
+sed -i -e "s/version=6.3.1/version=6.3.2/g" python/python-coverage/spkgbuild
+changelog "python/python-coverage/spkgbuild" "Upgraded from version 6.3.1 to version 6.3.2"
+sed -i -e "s/version=0.74.0/version=0.74.1/g" python/python-fastapi/spkgbuild
+changelog "python/python-fastapi/spkgbuild" "Upgraded from version 0.74.0 to version 0.74.1"
+sed -i -e "s/version=6.37.0/version=6.37.1/g" python/python-hypothesis/spkgbuild
+changelog "python/python-hypothesis/spkgbuild" "Upgraded from version 6.37.0 to version 6.37.1"
+sed -i -e "s/version=0.23.1/version=0.24.0/g" python/python-mongoengine/spkgbuild
+changelog "python/python-mongoengine/spkgbuild" "Upgraded from version 0.23.1 to version 0.24.0"
+sed -i -e "s/version=1.3.1/version=2.0.0/g" python/python-requests-credssp/spkgbuild
+changelog "python/python-requests-credssp/spkgbuild" "Upgraded from version 1.3.1 to version 2.0.0"
+sed -i -e "s/version=0.8.19/version=0.8.20/g" python/python-scrypt/spkgbuild
+changelog "python/python-scrypt/spkgbuild" "Upgraded from version 0.8.19 to version 0.8.20"
+sed -i -e "s/version=0.9.0/version=0.9.1/g" python/python-symengine/spkgbuild
+changelog "python/python-symengine/spkgbuild" "Upgraded from version 0.9.0 to version 0.9.1"
+sed -i -e "s/version=3.10.0/version=3.10.1/g" python/python-trimesh/spkgbuild
+changelog "python/python-trimesh/spkgbuild" "Upgraded from version 3.10.0 to version 3.10.1"
+sed -i -e "s/version=10.1/version=10.2/g" python/python-websockets/spkgbuild
+changelog "python/python-websockets/spkgbuild" "Upgraded from version 10.1 to version 10.2"
