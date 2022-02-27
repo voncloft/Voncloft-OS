7144bd357016b91b9b63eb585aef47dcc4dcfc10
Voncloft <voncloft@gmail.com>
2022-02-27 13:17:00 -0500

    Minor Updates

---
+++ logs/old-test-updates/2022/Feb/applied_updates-02-27-22.sh
@@ -0,0 +1,12 @@
+sed -i -e "s/version=1.21.2/version=1.21.3/g" core/wget/spkgbuild
+changelog "core/wget/spkgbuild" "Upgraded from version 1.21.2 to version 1.21.3"
+sed -i -e "s/version=2.1.2/version=2.1.3/g" libs/libjpeg-turbo/spkgbuild
+changelog "libs/libjpeg-turbo/spkgbuild" "Upgraded from version 2.1.2 to version 2.1.3"
+sed -i -e "s/version=0.58.1/version=0.58.2/g" python/python-cfn-lint/spkgbuild
+changelog "python/python-cfn-lint/spkgbuild" "Upgraded from version 0.58.1 to version 0.58.2"
+sed -i -e "s/version=6.37.2/version=6.38.0/g" python/python-hypothesis/spkgbuild
+changelog "python/python-hypothesis/spkgbuild" "Upgraded from version 6.37.2 to version 6.38.0"
+sed -i -e "s/version=0.10.3/version=0.11.0/g" python/python-libtmux/spkgbuild
+changelog "python/python-libtmux/spkgbuild" "Upgraded from version 0.10.3 to version 0.11.0"
+sed -i -e "s/version=16.3.0/version=16.4.0/g" python/python-path/spkgbuild
+changelog "python/python-path/spkgbuild" "Upgraded from version 16.3.0 to version 16.4.0"
