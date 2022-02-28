4c957a3dfce86c8c25e13fe9636893a9f7386102
Voncloft <voncloft@gmail.com>
2022-02-28 13:01:49 -0500

    Minor Updates

---
+++ logs/old-test-updates/2022/Feb/applied_updates-02-28-22.sh
@@ -0,0 +1,8 @@
+sed -i -e "s/version=1.4/version=1.5/g" media/mplayer/spkgbuild
+changelog "media/mplayer/spkgbuild" "Upgraded from version 1.4 to version 1.5"
+sed -i -e "s/version=4.11.1/version=4.11.2/g" python/python-importlib-metadata/spkgbuild
+changelog "python/python-importlib-metadata/spkgbuild" "Upgraded from version 4.11.1 to version 4.11.2"
+sed -i -e "s/version=0.22.0/version=0.23.0/g" python/python-rope/spkgbuild
+changelog "python/python-rope/spkgbuild" "Upgraded from version 0.22.0 to version 0.23.0"
+sed -i -e "s/version=3.0.2/version=3.0.3/g" python/python-xlsxwriter/spkgbuild
+changelog "python/python-xlsxwriter/spkgbuild" "Upgraded from version 3.0.2 to version 3.0.3"
