04438256277b8d5398365306c88bd7a81479a300
Voncloft <voncloft@gmail.com>
2022-03-02 08:21:24 -0500

    Minor Updates

---
+++ logs/old-test-updates/2022/Mar/applied_updates-03-02-22.sh
@@ -0,0 +1,46 @@
+sed -i -e "s/version=4.5.1/version=4.6/g" compilers/ccache/spkgbuild
+changelog "compilers/ccache/spkgbuild" "Upgraded from version 4.5.1 to version 4.6"
+sed -i -e "s/version=1.13.22/version=1.14.0/g" core/dbus/spkgbuild
+changelog "core/dbus/spkgbuild" "Upgraded from version 1.13.22 to version 1.14.0"
+sed -i -e "s/version=0.92.2/version=0.93.0/g" core/hugo/spkgbuild
+changelog "core/hugo/spkgbuild" "Upgraded from version 0.92.2 to version 0.93.0"
+sed -i -e "s/version=6.0.3/version=6.1/g" core/mercurial/spkgbuild
+changelog "core/mercurial/spkgbuild" "Upgraded from version 6.0.3 to version 6.1"
+sed -i -e "s/version=7.2/version=7.3/g" core/smartmontools/spkgbuild
+changelog "core/smartmontools/spkgbuild" "Upgraded from version 7.2 to version 7.3"
+sed -i -e "s/version=21.10.0/version=22.2.0/g" core/smplayer/spkgbuild
+changelog "core/smplayer/spkgbuild" "Upgraded from version 21.10.0 to version 22.2.0"
+sed -i -e "s/version=3.3.1/version=3.3.2/g" extra/flexget/spkgbuild
+changelog "extra/flexget/spkgbuild" "Upgraded from version 3.3.1 to version 3.3.2"
+sed -i -e "s/version=98.0b2/version=98.0b3/g" networking/thunderbird/spkgbuild
+changelog "networking/thunderbird/spkgbuild" "Upgraded from version 98.0b2 to version 98.0b3"
+sed -i -e "s/version=0.5.1/version=0.5.2/g" python/python-adblock/spkgbuild
+changelog "python/python-adblock/spkgbuild" "Upgraded from version 0.5.1 to version 0.5.2"
+sed -i -e "s/version=2.9.3/version=2.10.0/g" python/python-astroid/spkgbuild
+changelog "python/python-astroid/spkgbuild" "Upgraded from version 2.9.3 to version 2.10.0"
+sed -i -e "s/version=1.21.8/version=1.21.10/g" python/python-boto3/spkgbuild
+changelog "python/python-boto3/spkgbuild" "Upgraded from version 1.21.8 to version 1.21.10"
+sed -i -e "s/version=1.24.8/version=1.24.10/g" python/python-botocore/spkgbuild
+changelog "python/python-botocore/spkgbuild" "Upgraded from version 1.24.8 to version 1.24.10"
+sed -i -e "s/version=8.0.0/version=8.0.1/g" python/python-elasticsearch/spkgbuild
+changelog "python/python-elasticsearch/spkgbuild" "Upgraded from version 8.0.0 to version 8.0.1"
+sed -i -e "s/version=13.2.0/version=13.3.0/g" python/python-faker/spkgbuild
+changelog "python/python-faker/spkgbuild" "Upgraded from version 13.2.0 to version 13.3.0"
+sed -i -e "s/version=3.1.1/version=3.2.0/g" python/python-gitlab/spkgbuild
+changelog "python/python-gitlab/spkgbuild" "Upgraded from version 3.1.1 to version 3.2.0"
+sed -i -e "s/version=6.38.0/version=6.39.0/g" python/python-hypothesis/spkgbuild
+changelog "python/python-hypothesis/spkgbuild" "Upgraded from version 6.38.0 to version 6.39.0"
+sed -i -e "s/version=2.2.0/version=2.3.0/g" python/python-jaraco.envs/spkgbuild
+changelog "python/python-jaraco.envs/spkgbuild" "Upgraded from version 2.2.0 to version 2.3.0"
+sed -i -e "s/version=1.0.2/version=1.0.3/g" python/python-kikit/spkgbuild
+changelog "python/python-kikit/spkgbuild" "Upgraded from version 1.0.2 to version 1.0.3"
+sed -i -e "s/version=1.1.1/version=1.1.2/g" python/python-lark-parser/spkgbuild
+changelog "python/python-lark-parser/spkgbuild" "Upgraded from version 1.1.1 to version 1.1.2"
+sed -i -e "s/version=10.0.1/version=10.0.2/g" python/python-pdoc/spkgbuild
+changelog "python/python-pdoc/spkgbuild" "Upgraded from version 10.0.1 to version 10.0.2"
+sed -i -e "s/version=6.18.4/version=6.18.5/g" python/python-testfixtures/spkgbuild
+changelog "python/python-testfixtures/spkgbuild" "Upgraded from version 6.18.4 to version 6.18.5"
+sed -i -e "s/version=4.62.3/version=4.63.0/g" python/python-tqdm/spkgbuild
+changelog "python/python-tqdm/spkgbuild" "Upgraded from version 4.62.3 to version 4.63.0"
+sed -i -e "s/version=0.38.3/version=0.38.4/g" python/python-zeroconf/spkgbuild
+changelog "python/python-zeroconf/spkgbuild" "Upgraded from version 0.38.3 to version 0.38.4"
