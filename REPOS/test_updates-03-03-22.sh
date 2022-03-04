1f716483f1eb6c80cfe6130c8a1fe9349cdfe678
Voncloft <voncloft@gmail.com>
2022-03-04 14:36:50 -0500

    Minor Updates

---
+++ REPOS/test_updates-03-03-22.sh
@@ -0,0 +1,16 @@
+sed -i -e "s/version=1.8.0/version=1.8.1/g" core/dunst/spkgbuild
+changelog "core/dunst/spkgbuild" "Upgraded from version 1.8.0 to version 1.8.1"
+sed -i -e "s/version=0.93.0/version=0.93.1/g" core/hugo/spkgbuild
+changelog "core/hugo/spkgbuild" "Upgraded from version 0.93.0 to version 0.93.1"
+sed -i -e "s/version=5.16.11/version=5.16.12/g" core/linux-desktop/spkgbuild
+changelog "core/linux-desktop/spkgbuild" "Upgraded from version 5.16.11 to version 5.16.12"
+sed -i -e "s/version=5.16.11/version=5.16.12/g" core/linux-router/spkgbuild
+changelog "core/linux-router/spkgbuild" "Upgraded from version 5.16.11 to version 5.16.12"
+sed -i -e "s/version=10.1/version=11.0/g" core/yad/spkgbuild
+changelog "core/yad/spkgbuild" "Upgraded from version 10.1 to version 11.0"
+sed -i -e "s/version=22.02.0/version=22.03.0/g" libs/poppler/spkgbuild
+changelog "libs/poppler/spkgbuild" "Upgraded from version 22.02.0 to version 22.03.0"
+sed -i -e "s/version=1.21.10/version=1.21.11/g" python/python-boto3/spkgbuild
+changelog "python/python-boto3/spkgbuild" "Upgraded from version 1.21.10 to version 1.21.11"
+sed -i -e "s/version=1.24.10/version=1.24.11/g" python/python-botocore/spkgbuild
+changelog "python/python-botocore/spkgbuild" "Upgraded from version 1.24.10 to version 1.24.11"
