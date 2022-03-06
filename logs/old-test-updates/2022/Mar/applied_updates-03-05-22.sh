596e356e942a0a63d29d1617001bb549a0a5391b
Voncloft <voncloft@gmail.com>
2022-03-05 20:23:45 -0500

    Minor Updates

---
+++ logs/old-test-updates/2022/Mar/applied_updates-03-05-22.sh
@@ -0,0 +1,48 @@
+sed -i -e "s/version=7.81.0/version=7.82.0/g" core/curl/spkgbuild
+changelog "core/curl/spkgbuild" "Upgraded from version 7.81.0 to version 7.82.0"
+sed -i -e "s/version=1.27/version=1.28/g" core/fakeroot/spkgbuild
+changelog "core/fakeroot/spkgbuild" "Upgraded from version 1.27 to version 1.28"
+sed -i -e "s/version=0.93.1/version=0.93.2/g" core/hugo/spkgbuild
+changelog "core/hugo/spkgbuild" "Upgraded from version 0.93.1 to version 0.93.2"
+sed -i -e "s/version=4.0.37/version=4.0.38/g" core/mtools/spkgbuild
+changelog "core/mtools/spkgbuild" "Upgraded from version 4.0.37 to version 4.0.38"
+sed -i -e "s/version=3.1.1/version=3.2.0/g" core/streamlink/spkgbuild
+changelog "core/streamlink/spkgbuild" "Upgraded from version 3.1.1 to version 3.2.0"
+sed -i -e "s/version=2.4.6/version=2.4.7/g" libs/expat/spkgbuild
+changelog "libs/expat/spkgbuild" "Upgraded from version 2.4.6 to version 2.4.7"
+sed -i -e "s/version=1.28/version=1.28.0/g" libs/pkcs11-helper/spkgbuild
+changelog "libs/pkcs11-helper/spkgbuild" "Upgraded from version 1.28 to version 1.28.0"
+sed -i -e "s/version=2.1.1/version=2.1.2/g" python/python-aiobotocore/spkgbuild
+changelog "python/python-aiobotocore/spkgbuild" "Upgraded from version 2.1.1 to version 2.1.2"
+sed -i -e "s/version=1.21.12/version=1.21.13/g" python/python-boto3/spkgbuild
+changelog "python/python-boto3/spkgbuild" "Upgraded from version 1.21.12 to version 1.21.13"
+sed -i -e "s/version=1.24.12/version=1.24.13/g" python/python-botocore/spkgbuild
+changelog "python/python-botocore/spkgbuild" "Upgraded from version 1.24.12 to version 1.24.13"
+sed -i -e "s/version=1.5.0/version=1.6.0/g" python/python-canonicaljson/spkgbuild
+changelog "python/python-canonicaljson/spkgbuild" "Upgraded from version 1.5.0 to version 1.6.0"
+sed -i -e "s/version=0.7.0/version=2022.3.4/g" python/python-cmarkgfm/spkgbuild
+changelog "python/python-cmarkgfm/spkgbuild" "Upgraded from version 0.7.0 to version 2022.3.4"
+sed -i -e "s/version=2.4.0/version=2.5.0/g" python/python-elementpath/spkgbuild
+changelog "python/python-elementpath/spkgbuild" "Upgraded from version 2.4.0 to version 2.5.0"
+sed -i -e "s/version=0.74.1/version=0.75.0/g" python/python-fastapi/spkgbuild
+changelog "python/python-fastapi/spkgbuild" "Upgraded from version 0.74.1 to version 0.75.0"
+sed -i -e "s/version=2.3.2/version=2.3.3/g" python/python-gphoto2/spkgbuild
+changelog "python/python-gphoto2/spkgbuild" "Upgraded from version 2.3.2 to version 2.3.3"
+sed -i -e "s/version=2.7/version=2.7.1/g" python/python-networkx/spkgbuild
+changelog "python/python-networkx/spkgbuild" "Upgraded from version 2.7 to version 2.7.1"
+sed -i -e "s/version=0.12.3/version=0.12.4/g" python/python-pyqtgraph/spkgbuild
+changelog "python/python-pyqtgraph/spkgbuild" "Upgraded from version 0.12.3 to version 0.12.4"
+sed -i -e "s/version=1.0.7/version=1.1.0/g" python/python-pytest-flake8/spkgbuild
+changelog "python/python-pytest-flake8/spkgbuild" "Upgraded from version 1.0.7 to version 1.1.0"
+sed -i -e "s/version=32.0/version=33.0/g" python/python-readme-renderer/spkgbuild
+changelog "python/python-readme-renderer/spkgbuild" "Upgraded from version 32.0 to version 33.0"
+sed -i -e "s/version=2022.02.23/version=2022.03.04/g" python/python-sphinx-furo/spkgbuild
+changelog "python/python-sphinx-furo/spkgbuild" "Upgraded from version 2022.02.23 to version 2022.03.04"
+sed -i -e "s/version=1.13.3/version=1.14.0/g" python/python-wrapt/spkgbuild
+changelog "python/python-wrapt/spkgbuild" "Upgraded from version 1.13.3 to version 1.14.0"
+sed -i -e "s/version=5.15.2/version=5.15.3/g" qt/qt5-multimedia/spkgbuild
+changelog "qt/qt5-multimedia/spkgbuild" "Upgraded from version 5.15.2 to version 5.15.3"
+sed -i -e "s/version=5.15.2/version=5.15.3/g" qt/qt5-speech/spkgbuild
+changelog "qt/qt5-speech/spkgbuild" "Upgraded from version 5.15.2 to version 5.15.3"
+sed -i -e "s/version=0.9.1/version=0.9.2/g" xfce/xfce4-terminal/spkgbuild
+changelog "xfce/xfce4-terminal/spkgbuild" "Upgraded from version 0.9.1 to version 0.9.2"
