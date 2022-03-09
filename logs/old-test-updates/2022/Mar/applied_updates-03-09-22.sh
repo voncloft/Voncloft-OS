beb9d817939479817f20ecc48f170eb8b8fa3450
Voncloft <voncloft@gmail.com>
2022-03-09 07:24:21 -0500

    Minor Updates

---
+++ logs/old-test-updates/2022/Mar/applied_updates-03-09-22.sh
@@ -0,0 +1,32 @@
+sed -i -e "s/version=5.2.0/version=5.2.1/g" cinnamon/muffin/spkgbuild
+changelog "cinnamon/muffin/spkgbuild" "Upgraded from version 5.2.0 to version 5.2.1"
+sed -i -e "s/version=1.20.1/version=1.21.0/g" python/python-cairo/spkgbuild
+changelog "python/python-cairo/spkgbuild" "Upgraded from version 1.20.1 to version 1.21.0"
+sed -i -e "s/version=13.3.0/version=13.3.1/g" python/python-faker/spkgbuild
+changelog "python/python-faker/spkgbuild" "Upgraded from version 13.3.0 to version 13.3.1"
+sed -i -e "s/version=6.39.1/version=6.39.3/g" python/python-hypothesis/spkgbuild
+changelog "python/python-hypothesis/spkgbuild" "Upgraded from version 6.39.1 to version 6.39.3"
+sed -i -e "s/version=4.4.0/version=4.5.0/g" python/python-keystoneauth1/spkgbuild
+changelog "python/python-keystoneauth1/spkgbuild" "Upgraded from version 4.4.0 to version 4.5.0"
+sed -i -e "s/version=3.14.9/version=3.14.10/g" python/python-peewee/spkgbuild
+changelog "python/python-peewee/spkgbuild" "Upgraded from version 3.14.9 to version 3.14.10"
+sed -i -e "s/version=4.7.0/version=4.8.0/g" python/python-persistent/spkgbuild
+changelog "python/python-persistent/spkgbuild" "Upgraded from version 4.7.0 to version 4.8.0"
+sed -i -e "s/version=22.0.3/version=22.0.4/g" python/python-pip/spkgbuild
+changelog "python/python-pip/spkgbuild" "Upgraded from version 22.0.3 to version 22.0.4"
+sed -i -e "s/version=0.18.0/version=0.19.0/g" python/python-responses/spkgbuild
+changelog "python/python-responses/spkgbuild" "Upgraded from version 0.18.0 to version 0.19.0"
+sed -i -e "s/version=20.13.2/version=20.13.3/g" python/python-virtualenv/spkgbuild
+changelog "python/python-virtualenv/spkgbuild" "Upgraded from version 20.13.2 to version 20.13.3"
+sed -i -e "s/version=1.9.2/version=1.10.0/g" python/python-xmlschema/spkgbuild
+changelog "python/python-xmlschema/spkgbuild" "Upgraded from version 1.9.2 to version 1.10.0"
+sed -i -e "s/version=4.9/version=4.10/g" python/python-zope-testing/spkgbuild
+changelog "python/python-zope-testing/spkgbuild" "Upgraded from version 4.9 to version 4.10"
+sed -i -e "s/version=4.9/version=4.10/g" python/python-zope.testing/spkgbuild
+changelog "python/python-zope.testing/spkgbuild" "Upgraded from version 4.9 to version 4.10"
+sed -i -e "s/version=5.15.2/version=5.15.3/g" qt/qt5-multimedia/spkgbuild
+changelog "qt/qt5-multimedia/spkgbuild" "Upgraded from version 5.15.2 to version 5.15.3"
+sed -i -e "s/version=5.15.2/version=5.15.3/g" qt/qt5-speech/spkgbuild
+changelog "qt/qt5-speech/spkgbuild" "Upgraded from version 5.15.2 to version 5.15.3"
+sed -i -e "s/version=1.9.9/version=1.9.10/g" xfce/xfce4-screenshooter/spkgbuild
+changelog "xfce/xfce4-screenshooter/spkgbuild" "Upgraded from version 1.9.9 to version 1.9.10"
