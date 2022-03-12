0915bd54dd919e474d52f368d132b6813c0afef4
Voncloft <voncloft@gmail.com>
2022-03-12 17:14:25 -0500

    Minor Updates

---
+++ logs/old-test-updates/2022/Mar/applied_updates-03-12-22.sh
@@ -0,0 +1,44 @@
+sed -i -e "s/version=0.61.2/version=0.62.0/g" compilers/meson/spkgbuild
+changelog "compilers/meson/spkgbuild" "Upgraded from version 0.61.2 to version 0.62.0"
+sed -i -e "s/version=0.93.3/version=0.94.2/g" core/hugo/spkgbuild
+changelog "core/hugo/spkgbuild" "Upgraded from version 0.93.3 to version 0.94.2"
+sed -i -e "s/version=5.16.13/version=5.16.14/g" core/linux-desktop/spkgbuild
+changelog "core/linux-desktop/spkgbuild" "Upgraded from version 5.16.13 to version 5.16.14"
+sed -i -e "s/version=5.16.13/version=5.16.14/g" core/linux-router/spkgbuild
+changelog "core/linux-router/spkgbuild" "Upgraded from version 5.16.13 to version 5.16.14"
+sed -i -e "s/version=3.3.2/version=3.3.3/g" extra/flexget/spkgbuild
+changelog "extra/flexget/spkgbuild" "Upgraded from version 3.3.2 to version 3.3.3"
+sed -i -e "s/version=99.0b1/version=99.0b2/g" networking/firefox/spkgbuild
+changelog "networking/firefox/spkgbuild" "Upgraded from version 99.0b1 to version 99.0b2"
+sed -i -e "s/version=1.18.4/version=1.18.5/g" networking/krb5/spkgbuild
+changelog "networking/krb5/spkgbuild" "Upgraded from version 1.18.4 to version 1.18.5"
+sed -i -e "s/version=98.0b3/version=99.0b1/g" networking/thunderbird/spkgbuild
+changelog "networking/thunderbird/spkgbuild" "Upgraded from version 98.0b3 to version 99.0b1"
+sed -i -e "s/version=2.10.0/version=2.11.0/g" python/python-astroid/spkgbuild
+changelog "python/python-astroid/spkgbuild" "Upgraded from version 2.10.0 to version 2.11.0"
+sed -i -e "s/version=1.21.16/version=1.21.18/g" python/python-boto3/spkgbuild
+changelog "python/python-boto3/spkgbuild" "Upgraded from version 1.21.16 to version 1.21.18"
+sed -i -e "s/version=1.24.16/version=1.24.18/g" python/python-botocore/spkgbuild
+changelog "python/python-botocore/spkgbuild" "Upgraded from version 1.24.16 to version 1.24.18"
+sed -i -e "s/version=1.1.0/version=1.2.0/g" python/python-cppy/spkgbuild
+changelog "python/python-cppy/spkgbuild" "Upgraded from version 1.1.0 to version 1.2.0"
+sed -i -e "s/version=4.29.1/version=4.30.0/g" python/python-fonttools/spkgbuild
+changelog "python/python-fonttools/spkgbuild" "Upgraded from version 4.29.1 to version 4.30.0"
+sed -i -e "s/version=2.9.2/version=2.10.1/g" python/python-paramiko/spkgbuild
+changelog "python/python-paramiko/spkgbuild" "Upgraded from version 2.9.2 to version 2.10.1"
+sed -i -e "s/version=4.8.0/version=4.9.0/g" python/python-persistent/spkgbuild
+changelog "python/python-persistent/spkgbuild" "Upgraded from version 4.8.0 to version 4.9.0"
+sed -i -e "s/version=8.12.44/version=8.12.45/g" python/python-phonenumbers/spkgbuild
+changelog "python/python-phonenumbers/spkgbuild" "Upgraded from version 8.12.44 to version 8.12.45"
+sed -i -e "s/version=2022.1/version=2022.1.1/g" python/python-pudb/spkgbuild
+changelog "python/python-pudb/spkgbuild" "Upgraded from version 2022.1 to version 2022.1.1"
+sed -i -e "s/version=7.3.2201211939/version=7.3.2203121745/g" python/python-pycontracts/spkgbuild
+changelog "python/python-pycontracts/spkgbuild" "Upgraded from version 7.3.2201211939 to version 7.3.2203121745"
+sed -i -e "s/version=5.9.9/version=6.0.1/g" python/python-softlayer/spkgbuild
+changelog "python/python-softlayer/spkgbuild" "Upgraded from version 5.9.9 to version 6.0.1"
+sed -i -e "s/version=5.1.1/version=5.2/g" python/python-zope-security/spkgbuild
+changelog "python/python-zope-security/spkgbuild" "Upgraded from version 5.1.1 to version 5.2"
+sed -i -e "s/version=5.15.2/version=5.15.3/g" qt/qt5-multimedia/spkgbuild
+changelog "qt/qt5-multimedia/spkgbuild" "Upgraded from version 5.15.2 to version 5.15.3"
+sed -i -e "s/version=5.15.2/version=5.15.3/g" qt/qt5-speech/spkgbuild
+changelog "qt/qt5-speech/spkgbuild" "Upgraded from version 5.15.2 to version 5.15.3"
