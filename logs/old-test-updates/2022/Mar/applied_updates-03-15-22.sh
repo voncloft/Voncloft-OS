f9a9c3a09f5bac1acc3db93c310c5d49946ef889
Voncloft <voncloft@gmail.com>
2022-03-15 05:21:01 -0400

    Minor Updates

---
+++ logs/old-test-updates/2022/Mar/applied_updates-03-15-22.sh
@@ -0,0 +1,42 @@
+sed -i -e "s/version=1.5.25/version=1.5.26/g" core/ibus/spkgbuild
+changelog "core/ibus/spkgbuild" "Upgraded from version 1.5.25 to version 1.5.26"
+sed -i -e "s/version=4.0.0/version=4.1.0/g" core/qalculate/spkgbuild
+changelog "core/qalculate/spkgbuild" "Upgraded from version 4.0.0 to version 4.1.0"
+sed -i -e "s/version=1.20.0/version=1.20.1/g" media/gst-libav/spkgbuild
+changelog "media/gst-libav/spkgbuild" "Upgraded from version 1.20.0 to version 1.20.1"
+sed -i -e "s/version=1.20.0/version=1.20.1/g" media/gst-plugins-bad/spkgbuild
+changelog "media/gst-plugins-bad/spkgbuild" "Upgraded from version 1.20.0 to version 1.20.1"
+sed -i -e "s/version=1.20.0/version=1.20.1/g" media/gst-plugins-good/spkgbuild
+changelog "media/gst-plugins-good/spkgbuild" "Upgraded from version 1.20.0 to version 1.20.1"
+sed -i -e "s/version=1.20.0/version=1.20.1/g" media/gst-plugins-ugly/spkgbuild
+changelog "media/gst-plugins-ugly/spkgbuild" "Upgraded from version 1.20.0 to version 1.20.1"
+sed -i -e "s/version=4.0.0/version=4.1.0/g" libs/libqalculate/spkgbuild
+changelog "libs/libqalculate/spkgbuild" "Upgraded from version 4.0.0 to version 4.1.0"
+sed -i -e "s/version=99.0b2/version=99.0b3/g" networking/firefox/spkgbuild
+changelog "networking/firefox/spkgbuild" "Upgraded from version 99.0b2 to version 99.0b3"
+sed -i -e "s/version=1.21.18/version=1.21.19/g" python/python-boto3/spkgbuild
+changelog "python/python-boto3/spkgbuild" "Upgraded from version 1.21.18 to version 1.21.19"
+sed -i -e "s/version=1.24.18/version=1.24.19/g" python/python-botocore/spkgbuild
+changelog "python/python-botocore/spkgbuild" "Upgraded from version 1.24.18 to version 1.24.19"
+sed -i -e "s/version=2.4.0/version=2.5.0/g" python/python-debtcollector/spkgbuild
+changelog "python/python-debtcollector/spkgbuild" "Upgraded from version 2.4.0 to version 2.5.0"
+sed -i -e "s/version=13.3.1/version=13.3.2/g" python/python-faker/spkgbuild
+changelog "python/python-faker/spkgbuild" "Upgraded from version 13.3.1 to version 13.3.2"
+sed -i -e "s/version=6.9.1/version=6.9.2/g" python/python-ipykernel/spkgbuild
+changelog "python/python-ipykernel/spkgbuild" "Upgraded from version 6.9.1 to version 6.9.2"
+sed -i -e "s/version=0.18.0/version=0.18.1/g" python/python-meilisearch/spkgbuild
+changelog "python/python-meilisearch/spkgbuild" "Upgraded from version 0.18.0 to version 0.18.1"
+sed -i -e "s/version=4.6.1/version=4.7.0/g" python/python-oslo-log/spkgbuild
+changelog "python/python-oslo-log/spkgbuild" "Upgraded from version 4.6.1 to version 4.7.0"
+sed -i -e "s/version=33.0/version=34.0/g" python/python-readme-renderer/spkgbuild
+changelog "python/python-readme-renderer/spkgbuild" "Upgraded from version 33.0 to version 34.0"
+sed -i -e "s/version=1.8.5/version=2.0.0/g" python/python-sparqlwrapper/spkgbuild
+changelog "python/python-sparqlwrapper/spkgbuild" "Upgraded from version 1.8.5 to version 2.0.0"
+sed -i -e "s/version=7.3.0/version=7.3.1/g" python/python-traitsui/spkgbuild
+changelog "python/python-traitsui/spkgbuild" "Upgraded from version 7.3.0 to version 7.3.1"
+sed -i -e "s/version=5.15.2/version=5.15.3/g" qt/qt5-multimedia/spkgbuild
+changelog "qt/qt5-multimedia/spkgbuild" "Upgraded from version 5.15.2 to version 5.15.3"
+sed -i -e "s/version=5.15.2/version=5.15.3/g" qt/qt5-speech/spkgbuild
+changelog "qt/qt5-speech/spkgbuild" "Upgraded from version 5.15.2 to version 5.15.3"
+sed -i -e "s/version=2.4.52/version=2.4.53/g" server/httpd/spkgbuild
+changelog "server/httpd/spkgbuild" "Upgraded from version 2.4.52 to version 2.4.53"
