7137bd667733b76e821adfd4a62de8fbaf1bdafe
Voncloft <voncloft@gmail.com>
2022-03-16 08:49:48 -0400

    Minor Updates

---
+++ logs/old-test-updates/2022/Mar/applied_updates-03-16-22.sh
@@ -0,0 +1,18 @@
+sed -i -e "s/version=3.5.0/version=3.5.1/g" libs/libressl/spkgbuild
+changelog "libs/libressl/spkgbuild" "Upgraded from version 3.5.0 to version 3.5.1"
+sed -i -e "s/version=1.5.0/version=1.5.1/g" python/python-asn1crypto/spkgbuild
+changelog "python/python-asn1crypto/spkgbuild" "Upgraded from version 1.5.0 to version 1.5.1"
+sed -i -e "s/version=1.2.58/version=1.2.60/g" python/python-cloudscraper/spkgbuild
+changelog "python/python-cloudscraper/spkgbuild" "Upgraded from version 1.2.58 to version 1.2.60"
+sed -i -e "s/version=0.10.0/version=1.0.0/g" python/python-jmespath/spkgbuild
+changelog "python/python-jmespath/spkgbuild" "Upgraded from version 0.10.0 to version 1.0.0"
+sed -i -e "s/version=1.3.2/version=1.4.0/g" python/python-kiwisolver/spkgbuild
+changelog "python/python-kiwisolver/spkgbuild" "Upgraded from version 1.3.2 to version 1.4.0"
+sed -i -e "s/version=2.10.1/version=2.10.2/g" python/python-paramiko/spkgbuild
+changelog "python/python-paramiko/spkgbuild" "Upgraded from version 2.10.1 to version 2.10.2"
+sed -i -e "s/version=5.15.2/version=5.15.3/g" qt/qt5-multimedia/spkgbuild
+changelog "qt/qt5-multimedia/spkgbuild" "Upgraded from version 5.15.2 to version 5.15.3"
+sed -i -e "s/version=5.15.2/version=5.15.3/g" qt/qt5-speech/spkgbuild
+changelog "qt/qt5-speech/spkgbuild" "Upgraded from version 5.15.2 to version 5.15.3"
+sed -i -e "s/version=4.15.5/version=4.15.6/g" server/samba/spkgbuild
+changelog "server/samba/spkgbuild" "Upgraded from version 4.15.5 to version 4.15.6"
