f5aa4a65ad39cf0dd1b63603d401c7d2c3d8f704
Voncloft <voncloft@gmail.com>
2022-03-17 13:27:48 -0400

    fix qt packages

---
+++ logs/old-test-updates/2022/Mar/applied_updates-03-17-22.sh
@@ -0,0 +1,34 @@
+sed -i -e "s/version=0.9.3/version=0.9.4/g" core/herbstluftwm/spkgbuild
+changelog "core/herbstluftwm/spkgbuild" "Upgraded from version 0.9.3 to version 0.9.4"
+sed -i -e "s/version=0.94.2/version=0.95.0/g" core/hugo/spkgbuild
+changelog "core/hugo/spkgbuild" "Upgraded from version 0.94.2 to version 0.95.0"
+sed -i -e "s/version=5.16.14/version=5.16.15/g" core/linux-desktop/spkgbuild
+changelog "core/linux-desktop/spkgbuild" "Upgraded from version 5.16.14 to version 5.16.15"
+sed -i -e "s/version=5.16.14/version=5.16.15/g" core/linux-router/spkgbuild
+changelog "core/linux-router/spkgbuild" "Upgraded from version 5.16.14 to version 5.16.15"
+sed -i -e "s/version=6.1.0/version=6.2.0/g" core/qemu/spkgbuild
+changelog "core/qemu/spkgbuild" "Upgraded from version 6.1.0 to version 6.2.0"
+sed -i -e "s/version=6.0.0/version=6.1.0/g" fonts/awesome-font/spkgbuild
+changelog "fonts/awesome-font/spkgbuild" "Upgraded from version 6.0.0 to version 6.1.0"
+sed -i -e "s/version=4.17.4/version=4.17.5/g" libs/libxfce4ui/spkgbuild
+changelog "libs/libxfce4ui/spkgbuild" "Upgraded from version 4.17.4 to version 4.17.5"
+sed -i -e "s/version=99.0b3/version=99.0b4/g" networking/firefox/spkgbuild
+changelog "networking/firefox/spkgbuild" "Upgraded from version 99.0b3 to version 99.0b4"
+sed -i -e "s/version=2.5.5/version=2.5.6/g" networking/openvpn/spkgbuild
+changelog "networking/openvpn/spkgbuild" "Upgraded from version 2.5.5 to version 2.5.6"
+sed -i -e "s/version=1.21.19/version=1.21.20/g" python/python-boto3/spkgbuild
+changelog "python/python-boto3/spkgbuild" "Upgraded from version 1.21.19 to version 1.21.20"
+sed -i -e "s/version=1.24.19/version=1.24.20/g" python/python-botocore/spkgbuild
+changelog "python/python-botocore/spkgbuild" "Upgraded from version 1.24.19 to version 1.24.20"
+sed -i -e "s/version=1.0.2/version=2.0.0/g" python/python-pytest-httpbin/spkgbuild
+changelog "python/python-pytest-httpbin/spkgbuild" "Upgraded from version 1.0.2 to version 2.0.0"
+sed -i -e "s/version=60.9.3/version=60.10.0/g" python/python-setuptools/spkgbuild
+changelog "python/python-setuptools/spkgbuild" "Upgraded from version 60.9.3 to version 60.10.0"
+sed -i -e "s/version=3.10.2/version=3.10.3/g" python/python-trimesh/spkgbuild
+changelog "python/python-trimesh/spkgbuild" "Upgraded from version 3.10.2 to version 3.10.3"
+sed -i -e "s/version=1.26.8/version=1.26.9/g" python/python-urllib3/spkgbuild
+changelog "python/python-urllib3/spkgbuild" "Upgraded from version 1.26.8 to version 1.26.9"
+sed -i -e "s/version=5.15.2/version=5.15.3/g" qt/qt5-multimedia/spkgbuild
+changelog "qt/qt5-multimedia/spkgbuild" "Upgraded from version 5.15.2 to version 5.15.3"
+sed -i -e "s/version=5.15.2/version=5.15.3/g" qt/qt5-speech/spkgbuild
+changelog "qt/qt5-speech/spkgbuild" "Upgraded from version 5.15.2 to version 5.15.3"
