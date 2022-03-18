97b1fa1ae64de361b2a155b1de11dc9d39252e79
Voncloft <voncloft@gmail.com>
2022-03-18 05:26:42 -0400

    Minor Updates

---
+++ logs/old-test-updates/2022/Mar/applied_updates-03-18-22.sh
@@ -0,0 +1,20 @@
+sed -i -e "s/version=3.7.3/version=3.7.4/g" core/gnutls/spkgbuild
+changelog "core/gnutls/spkgbuild" "Upgraded from version 3.7.3 to version 3.7.4"
+sed -i -e "s/version=2.4.6/version=2.4.7/g" libs/libtool/spkgbuild
+changelog "libs/libtool/spkgbuild" "Upgraded from version 2.4.6 to version 2.4.7"
+sed -i -e "s/version=2.1.2/version=2.2.0/g" python/python-aiobotocore/spkgbuild
+changelog "python/python-aiobotocore/spkgbuild" "Upgraded from version 2.1.2 to version 2.2.0"
+sed -i -e "s/version=1.21.20/version=1.21.21/g" python/python-boto3/spkgbuild
+changelog "python/python-boto3/spkgbuild" "Upgraded from version 1.21.20 to version 1.21.21"
+sed -i -e "s/version=1.24.20/version=1.24.21/g" python/python-botocore/spkgbuild
+changelog "python/python-botocore/spkgbuild" "Upgraded from version 1.24.20 to version 1.24.21"
+sed -i -e "s/version=6.39.3/version=6.39.4/g" python/python-hypothesis/spkgbuild
+changelog "python/python-hypothesis/spkgbuild" "Upgraded from version 6.39.3 to version 6.39.4"
+sed -i -e "s/version=3.10.3/version=3.10.5/g" python/python-trimesh/spkgbuild
+changelog "python/python-trimesh/spkgbuild" "Upgraded from version 3.10.3 to version 3.10.5"
+sed -i -e "s/version=2.1.0/version=2.1.1/g" python/python-waitress/spkgbuild
+changelog "python/python-waitress/spkgbuild" "Upgraded from version 2.1.0 to version 2.1.1"
+sed -i -e "s/version=5.15.2/version=5.15.3/g" qt/qt5-speech/spkgbuild
+changelog "qt/qt5-speech/spkgbuild" "Upgraded from version 5.15.2 to version 5.15.3"
+sed -i -e "s/version=4.12.1/version=4.16.0/g" xfce/orage/spkgbuild
+changelog "xfce/orage/spkgbuild" "Upgraded from version 4.12.1 to version 4.16.0"
