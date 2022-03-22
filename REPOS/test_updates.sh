dcadec9da1240cf75432638f5bac2a75b59ad3e0
Voncloft <voncloft@gmail.com>
2022-03-22 10:27:43 -0400

    fix vidtomp3 clean up avademux metadata crap

---
+++ REPOS/test_updates.sh
@@ -0,0 +1,16 @@
+sed -i -e "s/version=5.16.16/version=5.17/g" core/linux-desktop/spkgbuild
+changelog "core/linux-desktop/spkgbuild" "Upgraded from version 5.16.16 to version 5.17"
+sed -i -e "s/version=5.16.16/version=5.17/g" core/linux-router/spkgbuild
+changelog "core/linux-router/spkgbuild" "Upgraded from version 5.16.16 to version 5.17"
+sed -i -e "s/version=2.35.90/version=2.36.0/g" core/webkitgtk/spkgbuild
+changelog "core/webkitgtk/spkgbuild" "Upgraded from version 2.35.90 to version 2.36.0"
+sed -i -e "s/version=99.0b5/version=99.0b6/g" networking/firefox/spkgbuild
+changelog "networking/firefox/spkgbuild" "Upgraded from version 99.0b5 to version 99.0b6"
+sed -i -e "s/version=1.21.22/version=1.21.23/g" python/python-boto3/spkgbuild
+changelog "python/python-boto3/spkgbuild" "Upgraded from version 1.21.22 to version 1.21.23"
+sed -i -e "s/version=1.24.22/version=1.24.23/g" python/python-botocore/spkgbuild
+changelog "python/python-botocore/spkgbuild" "Upgraded from version 1.24.22 to version 1.24.23"
+sed -i -e "s/version=5.0.0/version=5.0.1/g" python/python-svg.path/spkgbuild
+changelog "python/python-svg.path/spkgbuild" "Upgraded from version 5.0.0 to version 5.0.1"
+sed -i -e "s/version=4.15.6/version=4.16.0/g" server/samba/spkgbuild
+changelog "server/samba/spkgbuild" "Upgraded from version 4.15.6 to version 4.16.0"
