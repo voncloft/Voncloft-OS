eab866ca144f0b22d1f556b63e1f26366c4c738a
Voncloft <voncloft@gmail.com>
2022-03-22 23:04:41 -0400

    Minor Updates

--- logs/old-test-updates/2022/Mar/applied_updates-03-22-22.sh
+++ logs/old-test-updates/2022/Mar/applied_updates-03-22-22.sh
@@ -14,3 +14,23 @@ sed -i -e "s/version=5.0.0/version=5.0.1/g" python/python-svg.path/spkgbuild
 changelog "python/python-svg.path/spkgbuild" "Upgraded from version 5.0.0 to version 5.0.1"
 sed -i -e "s/version=4.15.6/version=4.16.0/g" server/samba/spkgbuild
 changelog "server/samba/spkgbuild" "Upgraded from version 4.15.6 to version 4.16.0"
+sed -i -e "s/version=3.3.17/version=4.0.0/g" core/procps-ng/spkgbuild
+changelog "core/procps-ng/spkgbuild" "Upgraded from version 3.3.17 to version 4.0.0"
+sed -i -e "s/version=6.1.0/version=6.1.1/g" fonts/awesome-font/spkgbuild
+changelog "fonts/awesome-font/spkgbuild" "Upgraded from version 6.1.0 to version 6.1.1"
+sed -i -e "s/version=0.1.90/version=0.1.92/g" libs/babl/spkgbuild
+changelog "libs/babl/spkgbuild" "Upgraded from version 0.1.90 to version 0.1.92"
+sed -i -e "s/version=99.0b1/version=99.0b2/g" networking/thunderbird/spkgbuild
+changelog "networking/thunderbird/spkgbuild" "Upgraded from version 99.0b1 to version 99.0b2"
+sed -i -e "s/version=0.58.3/version=0.58.4/g" python/python-cfn-lint/spkgbuild
+changelog "python/python-cfn-lint/spkgbuild" "Upgraded from version 0.58.3 to version 0.58.4"
+sed -i -e "s/version=8.1.0/version=8.1.1/g" python/python-elasticsearch/spkgbuild
+changelog "python/python-elasticsearch/spkgbuild" "Upgraded from version 8.1.0 to version 8.1.1"
+sed -i -e "s/version=4.31.1/version=4.31.2/g" python/python-fonttools/spkgbuild
+changelog "python/python-fonttools/spkgbuild" "Upgraded from version 4.31.1 to version 4.31.2"
+sed -i -e "s/version=0.24.0/version=0.24.1/g" python/python-mongoengine/spkgbuild
+changelog "python/python-mongoengine/spkgbuild" "Upgraded from version 0.24.0 to version 0.24.1"
+sed -i -e "s/version=3.16.0/version=3.17.0/g" python/python-openmdao/spkgbuild
+changelog "python/python-openmdao/spkgbuild" "Upgraded from version 3.16.0 to version 3.17.0"
+sed -i -e "s/version=1.9.0/version=1.9.1/g" python/python-pygit2/spkgbuild
+changelog "python/python-pygit2/spkgbuild" "Upgraded from version 1.9.0 to version 1.9.1"
