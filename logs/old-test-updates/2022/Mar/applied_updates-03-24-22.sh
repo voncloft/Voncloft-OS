a0279239436439e781d44d2c181db639d66cfcb8
Voncloft <voncloft@gmail.com>
2022-03-24 05:28:32 -0400

    Minor Updates

---
+++ logs/old-test-updates/2022/Mar/applied_updates-03-24-22.sh
@@ -0,0 +1,26 @@
+sed -i -e "s/version=13.0.1/version=14.0.0/g" compilers/clang/spkgbuild
+changelog "compilers/clang/spkgbuild" "Upgraded from version 13.0.1 to version 14.0.0"
+sed -i -e "s/version=13.0.1/version=14.0.0/g" compilers/llvm/spkgbuild
+changelog "compilers/llvm/spkgbuild" "Upgraded from version 13.0.1 to version 14.0.0"
+sed -i -e "s/version=13.0.1/version=14.0.0/g" multilib/llvm-32/spkgbuild
+changelog "multilib/llvm-32/spkgbuild" "Upgraded from version 13.0.1 to version 14.0.0"
+sed -i -e "s/version=99.0b6/version=99.0b7/g" networking/firefox/spkgbuild
+changelog "networking/firefox/spkgbuild" "Upgraded from version 99.0b6 to version 99.0b7"
+sed -i -e "s/version=2.11.0/version=2.11.1/g" python/python-astroid/spkgbuild
+changelog "python/python-astroid/spkgbuild" "Upgraded from version 2.11.0 to version 2.11.1"
+sed -i -e "s/version=1.42.0/version=1.43.0/g" python/python-aws-sam-translator/spkgbuild
+changelog "python/python-aws-sam-translator/spkgbuild" "Upgraded from version 1.42.0 to version 1.43.0"
+sed -i -e "s/version=1.21.23/version=1.21.25/g" python/python-boto3/spkgbuild
+changelog "python/python-boto3/spkgbuild" "Upgraded from version 1.21.23 to version 1.21.25"
+sed -i -e "s/version=1.24.23/version=1.24.25/g" python/python-botocore/spkgbuild
+changelog "python/python-botocore/spkgbuild" "Upgraded from version 1.24.23 to version 1.24.25"
+sed -i -e "s/version=13.3.2/version=13.3.3/g" python/python-faker/spkgbuild
+changelog "python/python-faker/spkgbuild" "Upgraded from version 13.3.2 to version 13.3.3"
+sed -i -e "s/version=10.0.3/version=10.0.4/g" python/python-pdoc/spkgbuild
+changelog "python/python-pdoc/spkgbuild" "Upgraded from version 10.0.3 to version 10.0.4"
+sed -i -e "s/version=2022.1.8/version=2022.3.23/g" python/python-pipenv/spkgbuild
+changelog "python/python-pipenv/spkgbuild" "Upgraded from version 2022.1.8 to version 2022.3.23"
+sed -i -e "s/version=2.2.1/version=2.2.2/g" python/python-sphobjinv/spkgbuild
+changelog "python/python-sphobjinv/spkgbuild" "Upgraded from version 2.2.1 to version 2.2.2"
+sed -i -e "s/version=5.0.1/version=5.1/g" python/python-svg.path/spkgbuild
+changelog "python/python-svg.path/spkgbuild" "Upgraded from version 5.0.1 to version 5.1"
