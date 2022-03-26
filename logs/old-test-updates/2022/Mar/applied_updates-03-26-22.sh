a3a5a1d3b7627eb81b01924b9dc1cf43b892583e
Voncloft <voncloft@gmail.com>
2022-03-26 18:32:40 -0400

    Minor Updates

---
+++ logs/old-test-updates/2022/Mar/applied_updates-03-26-22.sh
@@ -0,0 +1,6 @@
+sed -i -e "s/version=0.95.0/version=0.96.0/g" core/hugo/spkgbuild
+changelog "core/hugo/spkgbuild" "Upgraded from version 0.95.0 to version 0.96.0"
+sed -i -e "s/version=7.3.2203121745/version=7.3.2203252029/g" python/python-pycontracts/spkgbuild
+changelog "python/python-pycontracts/spkgbuild" "Upgraded from version 7.3.2203121745 to version 7.3.2203252029"
+sed -i -e "s/version=61.1.0/version=61.1.1/g" python/python-setuptools/spkgbuild
+changelog "python/python-setuptools/spkgbuild" "Upgraded from version 61.1.0 to version 61.1.1"
