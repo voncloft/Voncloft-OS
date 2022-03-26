6f03b9398aeec273cd3e363bf03bea586ed7e344
Voncloft <voncloft@gmail.com>
2022-03-25 20:50:17 -0400

    Minor Updates

---
+++ logs/old-test-updates/2022/Mar/applied_updates-03-25-22.sh
@@ -0,0 +1,44 @@
+sed -i -e "s/version=0.20.0/version=0.21.0/g" core/cbindgen/spkgbuild
+changelog "core/cbindgen/spkgbuild" "Upgraded from version 0.20.0 to version 0.21.0"
+sed -i -e "s/version=3.3.3/version=3.3.4/g" extra/flexget/spkgbuild
+changelog "extra/flexget/spkgbuild" "Upgraded from version 3.3.3 to version 3.3.4"
+sed -i -e "s/version=3.19.4/version=3.20.0/g" extra/protobuf/spkgbuild
+changelog "extra/protobuf/spkgbuild" "Upgraded from version 3.19.4 to version 3.20.0"
+sed -i -e "s/version=1.12.0/version=1.12.1/g" libs/libevdev/spkgbuild
+changelog "libs/libevdev/spkgbuild" "Upgraded from version 1.12.0 to version 1.12.1"
+sed -i -e "s/version=1.8.34/version=1.8.36/g" libs/libnma/spkgbuild
+changelog "libs/libnma/spkgbuild" "Upgraded from version 1.8.34 to version 1.8.36"
+sed -i -e "s/version=2.1.0/version=2.2.0/g" libs/libwacom/spkgbuild
+changelog "libs/libwacom/spkgbuild" "Upgraded from version 2.1.0 to version 2.2.0"
+sed -i -e "s/version=99.0b7/version=99.0b8/g" networking/firefox/spkgbuild
+changelog "networking/firefox/spkgbuild" "Upgraded from version 99.0b7 to version 99.0b8"
+sed -i -e "s/version=1.21.25/version=1.21.27/g" python/python-boto3/spkgbuild
+changelog "python/python-boto3/spkgbuild" "Upgraded from version 1.21.25 to version 1.21.27"
+sed -i -e "s/version=1.24.25/version=1.24.27/g" python/python-botocore/spkgbuild
+changelog "python/python-botocore/spkgbuild" "Upgraded from version 1.24.25 to version 1.24.27"
+sed -i -e "s/version=0.49.0/version=0.50.0/g" python/python-django-allauth/spkgbuild
+changelog "python/python-django-allauth/spkgbuild" "Upgraded from version 0.49.0 to version 0.50.0"
+sed -i -e "s/version=0.19.2/version=0.20.0/g" python/python-dotenv/spkgbuild
+changelog "python/python-dotenv/spkgbuild" "Upgraded from version 0.19.2 to version 0.20.0"
+sed -i -e "s/version=2022.3.23/version=2022.3.24/g" python/python-pipenv/spkgbuild
+changelog "python/python-pipenv/spkgbuild" "Upgraded from version 2022.3.23 to version 2022.3.24"
+sed -i -e "s/version=0.18.2/version=0.18.3/g" python/python-pytest-asyncio/spkgbuild
+changelog "python/python-pytest-asyncio/spkgbuild" "Upgraded from version 0.18.2 to version 0.18.3"
+sed -i -e "s/version=60.10.0/version=61.1.0/g" python/python-setuptools/spkgbuild
+changelog "python/python-setuptools/spkgbuild" "Upgraded from version 60.10.0 to version 61.1.0"
+sed -i -e "s/version=1.4.1/version=1.4.2/g" python/python-svgwrite/spkgbuild
+changelog "python/python-svgwrite/spkgbuild" "Upgraded from version 1.4.1 to version 1.4.2"
+sed -i -e "s/version=4.63.0/version=4.63.1/g" python/python-tqdm/spkgbuild
+changelog "python/python-tqdm/spkgbuild" "Upgraded from version 4.63.0 to version 4.63.1"
+sed -i -e "s/version=3.10.5/version=3.10.7/g" python/python-trimesh/spkgbuild
+changelog "python/python-trimesh/spkgbuild" "Upgraded from version 3.10.5 to version 3.10.7"
+sed -i -e "s/version=4.7.0/version=4.8.0/g" python/python-tweepy/spkgbuild
+changelog "python/python-tweepy/spkgbuild" "Upgraded from version 4.7.0 to version 4.8.0"
+sed -i -e "s/version=20.13.4/version=20.14.0/g" python/python-virtualenv/spkgbuild
+changelog "python/python-virtualenv/spkgbuild" "Upgraded from version 20.13.4 to version 20.14.0"
+sed -i -e "s/version=2.1.6/version=2.1.7/g" python/python-watchdog/spkgbuild
+changelog "python/python-watchdog/spkgbuild" "Upgraded from version 2.1.6 to version 2.1.7"
+sed -i -e "s/version=4.4.1/version=4.4.2/g" qt/qbittorrent/spkgbuild
+changelog "qt/qbittorrent/spkgbuild" "Upgraded from version 4.4.1 to version 4.4.2"
+sed -i -e "s/version=2.3.9/version=2.3.10/g" ruby-gems/ruby-bundler/spkgbuild
+changelog "ruby-gems/ruby-bundler/spkgbuild" "Upgraded from version 2.3.9 to version 2.3.10"
