02a231f8570b5c632c241bbc9cb3a3f301dc1c90
Voncloft <voncloft@gmail.com>
2022-03-31 18:26:06 -0400

    Minor Updates

---
+++ logs/old-test-updates/2022/Mar/applied_updates-03-31-22.sh
@@ -0,0 +1,44 @@
+sed -i -e "s/version=3.76.1/version=3.77/g" core/nss/spkgbuild
+changelog "core/nss/spkgbuild" "Upgraded from version 3.76.1 to version 3.77"
+sed -i -e "s/version=3.3.6/version=3.3.7/g" extra/flexget/spkgbuild
+changelog "extra/flexget/spkgbuild" "Upgraded from version 3.3.6 to version 3.3.7"
+sed -i -e "s/version=2.13.96/version=2.14.0/g" fonts/fontconfig/spkgbuild
+changelog "fonts/fontconfig/spkgbuild" "Upgraded from version 2.13.96 to version 2.14.0"
+sed -i -e "s/version=7.3.1/version=7.3.2/g" nonfree/libreoffice/spkgbuild
+changelog "nonfree/libreoffice/spkgbuild" "Upgraded from version 7.3.1 to version 7.3.2"
+sed -i -e "s/version=0.7.0/version=0.8.0/g" python/python-atom/spkgbuild
+changelog "python/python-atom/spkgbuild" "Upgraded from version 0.7.0 to version 0.8.0"
+sed -i -e "s/version=1.21.29/version=1.21.31/g" python/python-boto3/spkgbuild
+changelog "python/python-boto3/spkgbuild" "Upgraded from version 1.21.29 to version 1.21.31"
+sed -i -e "s/version=1.24.29/version=1.24.31/g" python/python-botocore/spkgbuild
+changelog "python/python-botocore/spkgbuild" "Upgraded from version 1.24.29 to version 1.24.31"
+sed -i -e "s/version=8.1.0/version=8.1.1/g" python/python-click/spkgbuild
+changelog "python/python-click/spkgbuild" "Upgraded from version 8.1.0 to version 8.1.1"
+sed -i -e "s/version=2.8.15/version=2.9.8/g" python/python-cloudflare/spkgbuild
+changelog "python/python-cloudflare/spkgbuild" "Upgraded from version 2.8.15 to version 2.9.8"
+sed -i -e "s/version=1.2.0/version=1.2.1/g" python/python-cppy/spkgbuild
+changelog "python/python-cppy/spkgbuild" "Upgraded from version 1.2.0 to version 1.2.1"
+sed -i -e "s/version=11.28.0/version=11.29.0/g" python/python-dropbox/spkgbuild
+changelog "python/python-dropbox/spkgbuild" "Upgraded from version 11.28.0 to version 11.29.0"
+sed -i -e "s/version=8.1.1/version=8.1.2/g" python/python-elasticsearch/spkgbuild
+changelog "python/python-elasticsearch/spkgbuild" "Upgraded from version 8.1.1 to version 8.1.2"
+sed -i -e "s/version=2.1.0/version=2.1.1/g" python/python-flask/spkgbuild
+changelog "python/python-flask/spkgbuild" "Upgraded from version 2.1.0 to version 2.1.1"
+sed -i -e "s/version=0.5.0/version=0.6.0/g" python/python-flask-login/spkgbuild
+changelog "python/python-flask-login/spkgbuild" "Upgraded from version 0.5.0 to version 0.6.0"
+sed -i -e "s/version=6.10.0/version=6.11.0/g" python/python-ipykernel/spkgbuild
+changelog "python/python-ipykernel/spkgbuild" "Upgraded from version 6.10.0 to version 6.11.0"
+sed -i -e "s/version=0.18.1/version=0.18.2/g" python/python-meilisearch/spkgbuild
+changelog "python/python-meilisearch/spkgbuild" "Upgraded from version 0.18.1 to version 0.18.2"
+sed -i -e "s/version=1.2.0/version=1.2.1/g" python/python-numpydoc/spkgbuild
+changelog "python/python-numpydoc/spkgbuild" "Upgraded from version 1.2.0 to version 1.2.1"
+sed -i -e "s/version=5.9.10/version=5.9.11/g" python/python-pypugjs/spkgbuild
+changelog "python/python-pypugjs/spkgbuild" "Upgraded from version 5.9.10 to version 5.9.11"
+sed -i -e "s/version=61.2.0/version=61.3.0/g" python/python-setuptools/spkgbuild
+changelog "python/python-setuptools/spkgbuild" "Upgraded from version 61.2.0 to version 61.3.0"
+sed -i -e "s/version=6.0.1/version=6.0.2/g" python/python-softlayer/spkgbuild
+changelog "python/python-softlayer/spkgbuild" "Upgraded from version 6.0.1 to version 6.0.2"
+sed -i -e "s/version=2.2.1/version=2.3.0/g" python/python-spyder-kernels/spkgbuild
+changelog "python/python-spyder-kernels/spkgbuild" "Upgraded from version 2.2.1 to version 2.3.0"
+sed -i -e "s/version=0.4.0/version=0.4.1/g" python/python-typer/spkgbuild
+changelog "python/python-typer/spkgbuild" "Upgraded from version 0.4.0 to version 0.4.1"
