a381f9629ab5d6e540e054c92ea8cd157c98d2a5
Voncloft <voncloft@gmail.com>
2022-04-02 12:50:45 -0400

    Minor Updates

--- REPOS/test_updates-03-31-22.sh
+++
@@ -1,8 +0,0 @@
-sed -i -e "s/version=0.5.0/version=0.6.0/g" python/python-flask-login/spkgbuild
-changelog "python/python-flask-login/spkgbuild" "Upgraded from version 0.5.0 to version 0.6.0"
-sed -i -e "s/version=1.2.0/version=1.2.1/g" python/python-numpydoc/spkgbuild
-changelog "python/python-numpydoc/spkgbuild" "Upgraded from version 1.2.0 to version 1.2.1"
-sed -i -e "s/version=2.2.1/version=2.3.0/g" python/python-spyder-kernels/spkgbuild
-changelog "python/python-spyder-kernels/spkgbuild" "Upgraded from version 2.2.1 to version 2.3.0"
-sed -i -e "s/version=0.4.0/version=0.4.1/g" python/python-typer/spkgbuild
-changelog "python/python-typer/spkgbuild" "Upgraded from version 0.4.0 to version 0.4.1"
---
+++ logs/old-test-updates/2022/Apr/applied_updates-04-02-22.sh
@@ -0,0 +1,30 @@
+sed -i -e "s/version=2.4.0/version=2.5.0/g" core/qutebrowser/spkgbuild
+changelog "core/qutebrowser/spkgbuild" "Upgraded from version 2.4.0 to version 2.5.0"
+sed -i -e "s/version=0.36.0/version=0.37.0/g" libs/libfilezilla/spkgbuild
+changelog "libs/libfilezilla/spkgbuild" "Upgraded from version 0.36.0 to version 0.37.0"
+sed -i -e "s/version=4.17.5/version=4.17.6/g" libs/libxfce4ui/spkgbuild
+changelog "libs/libxfce4ui/spkgbuild" "Upgraded from version 4.17.5 to version 4.17.6"
+sed -i -e "s/version=22.03.0/version=22.04.0/g" libs/poppler/spkgbuild
+changelog "libs/poppler/spkgbuild" "Upgraded from version 22.03.0 to version 22.04.0"
+sed -i -e "s/version=1.21.31/version=1.21.32/g" python/python-boto3/spkgbuild
+changelog "python/python-boto3/spkgbuild" "Upgraded from version 1.21.31 to version 1.21.32"
+sed -i -e "s/version=1.24.31/version=1.24.32/g" python/python-botocore/spkgbuild
+changelog "python/python-botocore/spkgbuild" "Upgraded from version 1.24.31 to version 1.24.32"
+sed -i -e "s/version=4.14.0/version=4.15.2/g" python/python-braintree/spkgbuild
+changelog "python/python-braintree/spkgbuild" "Upgraded from version 4.14.0 to version 4.15.2"
+sed -i -e "s/version=8.1.1/version=8.1.2/g" python/python-click/spkgbuild
+changelog "python/python-click/spkgbuild" "Upgraded from version 8.1.1 to version 8.1.2"
+sed -i -e "s/version=2022.03.0/version=2022.04.0/g" python/python-distributed/spkgbuild
+changelog "python/python-distributed/spkgbuild" "Upgraded from version 2022.03.0 to version 2022.04.0"
+sed -i -e "s/version=6.40.0/version=6.40.2/g" python/python-hypothesis/spkgbuild
+changelog "python/python-hypothesis/spkgbuild" "Upgraded from version 6.40.0 to version 6.40.2"
+sed -i -e "s/version=8.12.45/version=8.12.46/g" python/python-phonenumbers/spkgbuild
+changelog "python/python-phonenumbers/spkgbuild" "Upgraded from version 8.12.45 to version 8.12.46"
+sed -i -e "s/version=2.1.0/version=2.1.1/g" python/python-werkzeug/spkgbuild
+changelog "python/python-werkzeug/spkgbuild" "Upgraded from version 2.1.0 to version 2.1.1"
+sed -i -e "s/version=3.11.0/version=3.11.1/g" ruby-gems/ruby-rspec-mocks/spkgbuild
+changelog "ruby-gems/ruby-rspec-mocks/spkgbuild" "Upgraded from version 3.11.0 to version 3.11.1"
+sed -i -e "s/version=0.5.8/version=0.5.9/g" xfce/mousepad/spkgbuild
+changelog "xfce/mousepad/spkgbuild" "Upgraded from version 0.5.8 to version 0.5.9"
+sed -i -e "s/version=0.9.2/version=1.0.0/g" xfce/xfce4-terminal/spkgbuild
+changelog "xfce/xfce4-terminal/spkgbuild" "Upgraded from version 0.9.2 to version 1.0.0"
