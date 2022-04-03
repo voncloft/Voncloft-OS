8ddeecd78016d7bdc8df001e1da96e51ee18c327
Voncloft <voncloft@gmail.com>
2022-04-03 08:26:17 -0400

    Minor Updates

---
+++ logs/old-test-updates/2022/Apr/applied_updates-04-03-22.sh
@@ -0,0 +1,14 @@
+sed -i -e "s/version=1.0.11/version=1.0.12/g" core/twm/spkgbuild
+changelog "core/twm/spkgbuild" "Upgraded from version 1.0.11 to version 1.0.12"
+sed -i -e "s/version=2.9.8/version=2.9.9/g" python/python-cloudflare/spkgbuild
+changelog "python/python-cloudflare/spkgbuild" "Upgraded from version 2.9.8 to version 2.9.9"
+sed -i -e "s/version=0.75.0/version=0.75.1/g" python/python-fastapi/spkgbuild
+changelog "python/python-fastapi/spkgbuild" "Upgraded from version 0.75.0 to version 0.75.1"
+sed -i -e "s/version=0.2.0/version=0.3.0/g" python/python-flask-paranoid/spkgbuild
+changelog "python/python-flask-paranoid/spkgbuild" "Upgraded from version 0.2.0 to version 0.3.0"
+sed -i -e "s/version=6.40.2/version=6.41.0/g" python/python-hypothesis/spkgbuild
+changelog "python/python-hypothesis/spkgbuild" "Upgraded from version 6.40.2 to version 6.41.0"
+sed -i -e "s/version=61.3.0/version=61.3.1/g" python/python-setuptools/spkgbuild
+changelog "python/python-setuptools/spkgbuild" "Upgraded from version 61.3.0 to version 61.3.1"
+sed -i -e "s/version=5.1/version=6.0/g" python/python-svg.path/spkgbuild
+changelog "python/python-svg.path/spkgbuild" "Upgraded from version 5.1 to version 6.0"
