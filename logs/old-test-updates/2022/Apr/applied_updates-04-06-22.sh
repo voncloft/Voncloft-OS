aefc85482f06385612c0a0c3da072f909e7ea5da
Voncloft <voncloft@gmail.com>
2022-04-06 05:18:17 -0400

    Minor Updates

--- logs/old-test-updates/2022/Apr/applied_updates-04-06-22.sh
+++ logs/old-test-updates/2022/Apr/applied_updates-04-06-22.sh
@@ -1,5 +1,7 @@
 sed -i -e "s/version=27.2/version=28.1/g" core/emacs/spkgbuild
 changelog "core/emacs/spkgbuild" "Upgraded from version 27.2 to version 28.1"
+sed -i -e "s/version=6.1/version=6.1.1/g" core/mercurial/spkgbuild
+changelog "core/mercurial/spkgbuild" "Upgraded from version 6.1 to version 6.1.1"
 sed -i -e "s/version=4.1.0/version=4.1.1/g" core/qalculate/spkgbuild
 changelog "core/qalculate/spkgbuild" "Upgraded from version 4.1.0 to version 4.1.1"
 sed -i -e "s/version=1.3.2/version=1.3.3/g" core/setxkbmap/spkgbuild
@@ -10,20 +12,34 @@ sed -i -e "s/version=1.2.1/version=1.2.2/g" fonts/mkfontscale/spkgbuild
 changelog "fonts/mkfontscale/spkgbuild" "Upgraded from version 1.2.1 to version 1.2.2"
 sed -i -e "s/version=1.0.6/version=1.0.7/g" fonts/xlsfonts/spkgbuild
 changelog "fonts/xlsfonts/spkgbuild" "Upgraded from version 1.0.6 to version 1.0.7"
+sed -i -e "s/version=5.0/version=5.0.1/g" media/ffmpeg/spkgbuild
+changelog "media/ffmpeg/spkgbuild" "Upgraded from version 5.0 to version 5.0.1"
+sed -i -e "s/version=1.0.4/version=1.0.5/g" libs/libmnl/spkgbuild
+changelog "libs/libmnl/spkgbuild" "Upgraded from version 1.0.4 to version 1.0.5"
 sed -i -e "s/version=4.1.0/version=4.1.1/g" libs/libqalculate/spkgbuild
 changelog "libs/libqalculate/spkgbuild" "Upgraded from version 4.1.0 to version 4.1.1"
 sed -i -e "s/version=1.9.2/version=1.10.0/g" libs/spdlog/spkgbuild
 changelog "libs/spdlog/spkgbuild" "Upgraded from version 1.9.2 to version 1.10.0"
+sed -i -e "s/version=99.0b8/version=100.0b1/g" networking/firefox/spkgbuild
+changelog "networking/firefox/spkgbuild" "Upgraded from version 99.0b8 to version 100.0b1"
+sed -i -e "s/version=1.0.1/version=1.0.2/g" networking/libnfnetlink/spkgbuild
+changelog "networking/libnfnetlink/spkgbuild" "Upgraded from version 1.0.1 to version 1.0.2"
 sed -i -e "s/version=0.8.6/version=0.8.7/g" networking/nethogs/spkgbuild
 changelog "networking/nethogs/spkgbuild" "Upgraded from version 0.8.6 to version 0.8.7"
 sed -i -e "s/version=2.17.1.1/version=2.18/g" nonfree/pandoc-bin/spkgbuild
 changelog "nonfree/pandoc-bin/spkgbuild" "Upgraded from version 2.17.1.1 to version 2.18"
-sed -i -e "s/version=1.21.32/version=1.21.33/g" python/python-boto3/spkgbuild
-changelog "python/python-boto3/spkgbuild" "Upgraded from version 1.21.32 to version 1.21.33"
-sed -i -e "s/version=1.24.32/version=1.24.33/g" python/python-botocore/spkgbuild
-changelog "python/python-botocore/spkgbuild" "Upgraded from version 1.24.32 to version 1.24.33"
+sed -i -e "s/version=1.21.32/version=1.21.34/g" python/python-boto3/spkgbuild
+changelog "python/python-boto3/spkgbuild" "Upgraded from version 1.21.32 to version 1.21.34"
+sed -i -e "s/version=1.24.32/version=1.24.34/g" python/python-botocore/spkgbuild
+changelog "python/python-botocore/spkgbuild" "Upgraded from version 1.24.32 to version 1.24.34"
+sed -i -e "s/version=5.1.0/version=6.0.0/g" python/python-flask-dance/spkgbuild
+changelog "python/python-flask-dance/spkgbuild" "Upgraded from version 5.1.0 to version 6.0.0"
 sed -i -e "s/version=6.11.0/version=6.12.1/g" python/python-ipykernel/spkgbuild
 changelog "python/python-ipykernel/spkgbuild" "Upgraded from version 6.11.0 to version 6.12.1"
+sed -i -e "s/version=2.7.1/version=2.8/g" python/python-networkx/spkgbuild
+changelog "python/python-networkx/spkgbuild" "Upgraded from version 2.7.1 to version 2.8"
+sed -i -e "s/version=0.9.7/version=1.0.0/g" python/python-rtree/spkgbuild
+changelog "python/python-rtree/spkgbuild" "Upgraded from version 0.9.7 to version 1.0.0"
 sed -i -e "s/version=0.13.1/version=0.14.0/g" python/python-scikit-build/spkgbuild
 changelog "python/python-scikit-build/spkgbuild" "Upgraded from version 0.13.1 to version 0.14.0"
 sed -i -e "s/version=61.3.1/version=62.0.0/g" python/python-setuptools/spkgbuild
@@ -32,5 +48,7 @@ sed -i -e "s/version=4.63.1/version=4.64.0/g" python/python-tqdm/spkgbuild
 changelog "python/python-tqdm/spkgbuild" "Upgraded from version 4.63.1 to version 4.64.0"
 sed -i -e "s/version=22.2.0/version=22.4.0/g" python/python-twisted/spkgbuild
 changelog "python/python-twisted/spkgbuild" "Upgraded from version 22.2.0 to version 22.4.0"
+sed -i -e "s/version=4.5.1/version=4.6.0/g" xfce/xfce4-eyes-plugin/spkgbuild
+changelog "xfce/xfce4-eyes-plugin/spkgbuild" "Upgraded from version 4.5.1 to version 4.6.0"
 sed -i -e "s/version=1.0.9/version=1.1.0/g" xorg/xclock/spkgbuild
 changelog "xorg/xclock/spkgbuild" "Upgraded from version 1.0.9 to version 1.1.0"
