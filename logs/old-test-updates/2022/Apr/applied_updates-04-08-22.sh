60263ce4dc3a6446b2e990ca68f75602517d7870
Voncloft <voncloft@gmail.com>
2022-04-08 15:47:18 -0400

    Minor Updates

--- logs/old-test-updates/2022/Apr/applied_updates-04-08-22.sh
+++ logs/old-test-updates/2022/Apr/applied_updates-04-08-22.sh
@@ -36,3 +36,37 @@ sed -i -e "s/version=4.4.0/version=4.4.1/g" python/python-zope-configuration/spk
 changelog "python/python-zope-configuration/spkgbuild" "Upgraded from version 4.4.0 to version 4.4.1"
 sed -i -e "s/version=2.3.10/version=2.3.11/g" ruby-gems/ruby-bundler/spkgbuild
 changelog "ruby-gems/ruby-bundler/spkgbuild" "Upgraded from version 2.3.10 to version 2.3.11"
+sed -i -e "s/version=5.17.1/version=5.17.2/g" core/linux-desktop/spkgbuild
+changelog "core/linux-desktop/spkgbuild" "Upgraded from version 5.17.1 to version 5.17.2"
+sed -i -e "s/version=5.17.1/version=5.17.2/g" core/linux-router/spkgbuild
+changelog "core/linux-router/spkgbuild" "Upgraded from version 5.17.1 to version 5.17.2"
+sed -i -e "s/version=1.3.2/version=1.3.3/g" core/xdpyinfo/spkgbuild
+changelog "core/xdpyinfo/spkgbuild" "Upgraded from version 1.3.2 to version 1.3.3"
+sed -i -e "s/version=3.6.0/version=3.6.1/g" libs/libarchive/spkgbuild
+changelog "libs/libarchive/spkgbuild" "Upgraded from version 3.6.0 to version 3.6.1"
+sed -i -e "s/version=0.37.0/version=0.37.1/g" libs/libfilezilla/spkgbuild
+changelog "libs/libfilezilla/spkgbuild" "Upgraded from version 0.37.0 to version 0.37.1"
+sed -i -e "s/version=100.0b2/version=100.0b3/g" networking/firefox/spkgbuild
+changelog "networking/firefox/spkgbuild" "Upgraded from version 100.0b2 to version 100.0b3"
+sed -i -e "s/version=8.9/version=9.0/g" networking/openssh/spkgbuild
+changelog "networking/openssh/spkgbuild" "Upgraded from version 8.9 to version 9.0"
+sed -i -e "s/version=5.5.12/version=5.5.13/g" networking/whois/spkgbuild
+changelog "networking/whois/spkgbuild" "Upgraded from version 5.5.12 to version 5.5.13"
+sed -i -e "s/version=2022.03.08.1/version=2022.04.08/g" networking/yt-dlp/spkgbuild
+changelog "networking/yt-dlp/spkgbuild" "Upgraded from version 2022.03.08.1 to version 2022.04.08"
+sed -i -e "s/version=1.21.36/version=1.21.37/g" python/python-boto3/spkgbuild
+changelog "python/python-boto3/spkgbuild" "Upgraded from version 1.21.36 to version 1.21.37"
+sed -i -e "s/version=1.24.36/version=1.24.37/g" python/python-botocore/spkgbuild
+changelog "python/python-botocore/spkgbuild" "Upgraded from version 1.24.36 to version 1.24.37"
+sed -i -e "s/version=2.9.9/version=2.9.10/g" python/python-cloudflare/spkgbuild
+changelog "python/python-cloudflare/spkgbuild" "Upgraded from version 2.9.9 to version 2.9.10"
+sed -i -e "s/version=4.31.2/version=4.32.0/g" python/python-fonttools/spkgbuild
+changelog "python/python-fonttools/spkgbuild" "Upgraded from version 4.31.2 to version 4.32.0"
+sed -i -e "s/version=1.0.4/version=1.0.5/g" python/python-kikit/spkgbuild
+changelog "python/python-kikit/spkgbuild" "Upgraded from version 1.0.4 to version 1.0.5"
+sed -i -e "s/version=0.14.0/version=0.14.1/g" python/python-prometheus_client/spkgbuild
+changelog "python/python-prometheus_client/spkgbuild" "Upgraded from version 0.14.0 to version 0.14.1"
+sed -i -e "s/version=0.14.0/version=0.14.1/g" python/python-scikit-build/spkgbuild
+changelog "python/python-scikit-build/spkgbuild" "Upgraded from version 0.14.0 to version 0.14.1"
+sed -i -e "s/version=1.1.0/version=1.1.1/g" xorg/xclock/spkgbuild
+changelog "xorg/xclock/spkgbuild" "Upgraded from version 1.1.0 to version 1.1.1"
