64969b2d00d67996806a48e19bfd2eb6ccd06d5e
Voncloft <voncloft@gmail.com>
2022-04-12 07:09:43 -0400

    Minor Updates

--- REPOS/test_updates-04-10-22.sh
+++
@@ -1,8 +0,0 @@
-sed -i -e "s/version=0.19.1/version=0.19.2/g" python/python-graphviz/spkgbuild
-changelog "python/python-graphviz/spkgbuild" "Upgraded from version 0.19.1 to version 0.19.2"
-sed -i -e "s/version=6.41.0/version=6.42.0/g" python/python-hypothesis/spkgbuild
-changelog "python/python-hypothesis/spkgbuild" "Upgraded from version 6.41.0 to version 6.42.0"
-sed -i -e "s/version=1.14.2/version=1.14.3/g" python/python-jsbeautifier/spkgbuild
-changelog "python/python-jsbeautifier/spkgbuild" "Upgraded from version 1.14.2 to version 1.14.3"
-sed -i -e "s/version=2022.3.28/version=2022.4.8/g" python/python-pipenv/spkgbuild
-changelog "python/python-pipenv/spkgbuild" "Upgraded from version 2022.3.28 to version 2022.4.8"
---
+++ logs/old-test-updates/2022/Apr/applied_updates-04-12-22.sh
@@ -0,0 +1,30 @@
+sed -i -e "s/version=1.49.1/version=1.49.2/g" core/help2man/spkgbuild
+changelog "core/help2man/spkgbuild" "Upgraded from version 1.49.1 to version 1.49.2"
+sed -i -e "s/version=2.25/version=2.26/g" core/links/spkgbuild
+changelog "core/links/spkgbuild" "Upgraded from version 2.25 to version 2.26"
+sed -i -e "s/version=3.18.1/version=3.19.0/g" core/valgrind/spkgbuild
+changelog "core/valgrind/spkgbuild" "Upgraded from version 3.18.1 to version 3.19.0"
+sed -i -e "s/version=1.1.2rc2/version=1.1.3rc1/g" extra/solaar/spkgbuild
+changelog "extra/solaar/spkgbuild" "Upgraded from version 1.1.2rc2 to version 1.1.3rc1"
+sed -i -e "s/version=100.0b3/version=100.0b4/g" networking/firefox/spkgbuild
+changelog "networking/firefox/spkgbuild" "Upgraded from version 100.0b3 to version 100.0b4"
+sed -i -e "s/version=1.14.1/version=1.14.2/g" networking/subversion/spkgbuild
+changelog "networking/subversion/spkgbuild" "Upgraded from version 1.14.1 to version 1.14.2"
+sed -i -e "s/version=0.0.22/version=0.1.0/g" python/python-aiomysql/spkgbuild
+changelog "python/python-aiomysql/spkgbuild" "Upgraded from version 0.0.22 to version 0.1.0"
+sed -i -e "s/version=4.9.3/version=4.10.0/g" python/python-antlr4/spkgbuild
+changelog "python/python-antlr4/spkgbuild" "Upgraded from version 4.9.3 to version 4.10.0"
+sed -i -e "s/version=1.21.37/version=1.21.38/g" python/python-boto3/spkgbuild
+changelog "python/python-boto3/spkgbuild" "Upgraded from version 1.21.37 to version 1.21.38"
+sed -i -e "s/version=1.24.37/version=1.24.38/g" python/python-botocore/spkgbuild
+changelog "python/python-botocore/spkgbuild" "Upgraded from version 1.24.37 to version 1.24.38"
+sed -i -e "s/version=6.42.2/version=6.42.3/g" python/python-hypothesis/spkgbuild
+changelog "python/python-hypothesis/spkgbuild" "Upgraded from version 6.42.2 to version 6.42.3"
+sed -i -e "s/version=6.12.1/version=6.13.0/g" python/python-ipykernel/spkgbuild
+changelog "python/python-ipykernel/spkgbuild" "Upgraded from version 6.12.1 to version 6.13.0"
+sed -i -e "s/version=62.0.0/version=62.1.0/g" python/python-setuptools/spkgbuild
+changelog "python/python-setuptools/spkgbuild" "Upgraded from version 62.0.0 to version 62.1.0"
+sed -i -e "s/version=20.14.0/version=20.14.1/g" python/python-virtualenv/spkgbuild
+changelog "python/python-virtualenv/spkgbuild" "Upgraded from version 20.14.0 to version 20.14.1"
+sed -i -e "s/version=5.15.8/version=5.15.9/g" qt/qtwebengine/spkgbuild
+changelog "qt/qtwebengine/spkgbuild" "Upgraded from version 5.15.8 to version 5.15.9"
