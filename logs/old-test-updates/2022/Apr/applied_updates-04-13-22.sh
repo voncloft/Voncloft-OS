856577503cb9d7ab9593b9be28068f35c583d05b
Voncloft <voncloft@gmail.com>
2022-04-13 05:27:16 -0400

    Minor Updates

---
+++ logs/old-test-updates/2022/Apr/applied_updates-04-13-22.sh
@@ -0,0 +1,10 @@
+sed -i -e "s/version=3.23.0/version=3.23.1/g" compilers/cmake/spkgbuild
+changelog "compilers/cmake/spkgbuild" "Upgraded from version 3.23.0 to version 3.23.1"
+sed -i -e "s/version=3.0.3/version=3.0.4/g" compilers/ruby/spkgbuild
+changelog "compilers/ruby/spkgbuild" "Upgraded from version 3.0.3 to version 3.0.4"
+sed -i -e "s/version=100.0b1/version=100.0b2/g" networking/thunderbird/spkgbuild
+changelog "networking/thunderbird/spkgbuild" "Upgraded from version 100.0b1 to version 100.0b2"
+sed -i -e "s/version=0.58.4/version=0.59.0/g" python/python-cfn-lint/spkgbuild
+changelog "python/python-cfn-lint/spkgbuild" "Upgraded from version 0.58.4 to version 0.59.0"
+sed -i -e "s/version=6.42.3/version=6.43.0/g" python/python-hypothesis/spkgbuild
+changelog "python/python-hypothesis/spkgbuild" "Upgraded from version 6.42.3 to version 6.43.0"
