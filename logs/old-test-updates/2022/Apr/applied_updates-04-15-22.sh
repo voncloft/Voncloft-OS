ae8f6c0ecf69a61036a82a315ad023464a16c80b
Voncloft <voncloft@gmail.com>
2022-04-15 22:40:34 -0400

    Minor Updates

---
+++ logs/old-test-updates/2022/Apr/applied_updates-04-15-22.sh
@@ -0,0 +1,28 @@
+sed -i -e "s/version=13.0.1/version=14.0.1/g" compilers/lld/spkgbuild
+changelog "compilers/lld/spkgbuild" "Upgraded from version 13.0.1 to version 14.0.1"
+sed -i -e "s/version=1.0.8/version=1.0.9/g" core/gptfdisk/spkgbuild
+changelog "core/gptfdisk/spkgbuild" "Upgraded from version 1.0.8 to version 1.0.9"
+sed -i -e "s/version=0.96.0/version=0.97.0/g" core/hugo/spkgbuild
+changelog "core/hugo/spkgbuild" "Upgraded from version 0.96.0 to version 0.97.0"
+sed -i -e "s/version=1.1.3rc1/version=1.1.3rc2/g" extra/solaar/spkgbuild
+changelog "extra/solaar/spkgbuild" "Upgraded from version 1.1.3rc1 to version 1.1.3rc2"
+sed -i -e "s/version=100.0b5/version=100.0b6/g" networking/firefox/spkgbuild
+changelog "networking/firefox/spkgbuild" "Upgraded from version 100.0b5 to version 100.0b6"
+sed -i -e "s/version=2.35.2/version=2.35.3/g" networking/git/spkgbuild
+changelog "networking/git/spkgbuild" "Upgraded from version 2.35.2 to version 2.35.3"
+sed -i -e "s/version=1.21.40/version=1.21.42/g" python/python-boto3/spkgbuild
+changelog "python/python-boto3/spkgbuild" "Upgraded from version 1.21.40 to version 1.21.42"
+sed -i -e "s/version=1.24.40/version=1.24.42/g" python/python-botocore/spkgbuild
+changelog "python/python-botocore/spkgbuild" "Upgraded from version 1.24.40 to version 1.24.42"
+sed -i -e "s/version=4.1.1/version=4.1.3/g" python/python-flask-security-too/spkgbuild
+changelog "python/python-flask-security-too/spkgbuild" "Upgraded from version 4.1.1 to version 4.1.3"
+sed -i -e "s/version=3.5.1/version=3.7.1/g" python/python-flit/spkgbuild
+changelog "python/python-flit/spkgbuild" "Upgraded from version 3.5.1 to version 3.7.1"
+sed -i -e "s/version=2021.10.1/version=2022.3.0/g" python/python-fsspec/spkgbuild
+changelog "python/python-fsspec/spkgbuild" "Upgraded from version 2021.10.1 to version 2022.3.0"
+sed -i -e "s/version=2.6.4/version=2.6.5/g" python/python-google-auth/spkgbuild
+changelog "python/python-google-auth/spkgbuild" "Upgraded from version 2.6.4 to version 2.6.5"
+sed -i -e "s/version=0.17.0/version=0.17.1/g" python/python-myst-parser/spkgbuild
+changelog "python/python-myst-parser/spkgbuild" "Upgraded from version 0.17.0 to version 0.17.1"
+sed -i -e "s/version=8.1.4/version=8.1.5/g" server/php/spkgbuild
+changelog "server/php/spkgbuild" "Upgraded from version 8.1.4 to version 8.1.5"
