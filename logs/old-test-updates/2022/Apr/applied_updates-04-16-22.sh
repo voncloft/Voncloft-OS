1498444277258feb3c45673eea62b4d0de46250c
Voncloft <voncloft@gmail.com>
2022-04-16 16:15:05 -0400

    Minor Updates

---
+++ logs/old-test-updates/2022/Apr/applied_updates-04-16-22.sh
@@ -0,0 +1,78 @@
+sed -i -e "s/version=9.0/version=9.1/g" core/coreutils/spkgbuild
+changelog "core/coreutils/spkgbuild" "Upgraded from version 9.0 to version 9.1"
+sed -i -e "s/version=2.3.2/version=2.3.3/g" core/enchant/spkgbuild
+changelog "core/enchant/spkgbuild" "Upgraded from version 2.3.2 to version 2.3.3"
+sed -i -e "s/version=0.97.0/version=0.97.1/g" core/hugo/spkgbuild
+changelog "core/hugo/spkgbuild" "Upgraded from version 0.97.0 to version 0.97.1"
+sed -i -e "s/version=1.9.20/version=1.9.21/g" core/jack/spkgbuild
+changelog "core/jack/spkgbuild" "Upgraded from version 1.9.20 to version 1.9.21"
+sed -i -e "s/version=3.2.3/version=3.2.4/g" core/rsync/spkgbuild
+changelog "core/rsync/spkgbuild" "Upgraded from version 3.2.3 to version 3.2.4"
+sed -i -e "s/version=1.0.0/version=1.1.0/g" lxde/lximage-qt/spkgbuild
+changelog "lxde/lximage-qt/spkgbuild" "Upgraded from version 1.0.0 to version 1.1.0"
+sed -i -e "s/version=1.0.0/version=1.1.0/g" lxqt/lxqt-about/spkgbuild
+changelog "lxqt/lxqt-about/spkgbuild" "Upgraded from version 1.0.0 to version 1.1.0"
+sed -i -e "s/version=1.0.0/version=1.1.0/g" lxqt/lxqt-admin/spkgbuild
+changelog "lxqt/lxqt-admin/spkgbuild" "Upgraded from version 1.0.0 to version 1.1.0"
+sed -i -e "s/version=0.5.0/version=0.6.0/g" lxqt/lxqt-archiver/spkgbuild
+changelog "lxqt/lxqt-archiver/spkgbuild" "Upgraded from version 0.5.0 to version 0.6.0"
+sed -i -e "s/version=0.10.0/version=0.11.0/g" lxqt/lxqt-build-tools/spkgbuild
+changelog "lxqt/lxqt-build-tools/spkgbuild" "Upgraded from version 0.10.0 to version 0.11.0"
+sed -i -e "s/version=1.0.0/version=1.1.0/g" lxqt/lxqt-config/spkgbuild
+changelog "lxqt/lxqt-config/spkgbuild" "Upgraded from version 1.0.0 to version 1.1.0"
+sed -i -e "s/version=1.0.1/version=1.1.0/g" lxqt/lxqt-globalkeys/spkgbuild
+changelog "lxqt/lxqt-globalkeys/spkgbuild" "Upgraded from version 1.0.1 to version 1.1.0"
+sed -i -e "s/version=1.0.0/version=1.1.0/g" lxqt/lxqt-notificationd/spkgbuild
+changelog "lxqt/lxqt-notificationd/spkgbuild" "Upgraded from version 1.0.0 to version 1.1.0"
+sed -i -e "s/version=1.0.0/version=1.1.0/g" lxqt/lxqt-openssh-askpass/spkgbuild
+changelog "lxqt/lxqt-openssh-askpass/spkgbuild" "Upgraded from version 1.0.0 to version 1.1.0"
+sed -i -e "s/version=1.0.0/version=1.1.0/g" lxqt/lxqt-panel/spkgbuild
+changelog "lxqt/lxqt-panel/spkgbuild" "Upgraded from version 1.0.0 to version 1.1.0"
+sed -i -e "s/version=1.0.0/version=1.1.0/g" lxqt/lxqt-policykit/spkgbuild
+changelog "lxqt/lxqt-policykit/spkgbuild" "Upgraded from version 1.0.0 to version 1.1.0"
+sed -i -e "s/version=1.0.0/version=1.1.0/g" lxqt/lxqt-powermanagement/spkgbuild
+changelog "lxqt/lxqt-powermanagement/spkgbuild" "Upgraded from version 1.0.0 to version 1.1.0"
+sed -i -e "s/version=1.0.0/version=1.1.0/g" lxqt/lxqt-qtplugin/spkgbuild
+changelog "lxqt/lxqt-qtplugin/spkgbuild" "Upgraded from version 1.0.0 to version 1.1.0"
+sed -i -e "s/version=1.0.0/version=1.1.0/g" lxqt/lxqt-runner/spkgbuild
+changelog "lxqt/lxqt-runner/spkgbuild" "Upgraded from version 1.0.0 to version 1.1.0"
+sed -i -e "s/version=1.0.1/version=1.1.0/g" lxqt/lxqt-session/spkgbuild
+changelog "lxqt/lxqt-session/spkgbuild" "Upgraded from version 1.0.1 to version 1.1.0"
+sed -i -e "s/version=1.0.0/version=1.1.0/g" lxqt/lxqt-sudo/spkgbuild
+changelog "lxqt/lxqt-sudo/spkgbuild" "Upgraded from version 1.0.0 to version 1.1.0"
+sed -i -e "s/version=1.0.0/version=1.1.0/g" lxqt/lxqt-themes/spkgbuild
+changelog "lxqt/lxqt-themes/spkgbuild" "Upgraded from version 1.0.0 to version 1.1.0"
+sed -i -e "s/version=1.0.0/version=1.1.0/g" libs/libfm-qt/spkgbuild
+changelog "libs/libfm-qt/spkgbuild" "Upgraded from version 1.0.0 to version 1.1.0"
+sed -i -e "s/version=1.0.0/version=1.1.0/g" libs/liblxqt/spkgbuild
+changelog "libs/liblxqt/spkgbuild" "Upgraded from version 1.0.0 to version 1.1.0"
+sed -i -e "s/version=3.8.0/version=3.9.0/g" libs/libqtxdg/spkgbuild
+changelog "libs/libqtxdg/spkgbuild" "Upgraded from version 3.8.0 to version 3.9.0"
+sed -i -e "s/version=2.0.5/version=2.0.6/g" libs/libtorrent/spkgbuild
+changelog "libs/libtorrent/spkgbuild" "Upgraded from version 2.0.5 to version 2.0.6"
+sed -i -e "s/version=2.0.5/version=2.0.6/g" libs/libtorrent-rasterbar/spkgbuild
+changelog "libs/libtorrent-rasterbar/spkgbuild" "Upgraded from version 2.0.5 to version 2.0.6"
+sed -i -e "s/version=4.10.0/version=4.10.1/g" python/python-antlr4/spkgbuild
+changelog "python/python-antlr4/spkgbuild" "Upgraded from version 4.10.0 to version 4.10.1"
+sed -i -e "s/version=2022.04.0/version=2022.04.1/g" python/python-distributed/spkgbuild
+changelog "python/python-distributed/spkgbuild" "Upgraded from version 2022.04.0 to version 2022.04.1"
+sed -i -e "s/version=0.19.2/version=0.20/g" python/python-graphviz/spkgbuild
+changelog "python/python-graphviz/spkgbuild" "Upgraded from version 0.19.2 to version 0.20"
+sed -i -e "s/version=2.0.1/version=2.1.0/g" python/python-markdown-it-py/spkgbuild
+changelog "python/python-markdown-it-py/spkgbuild" "Upgraded from version 2.0.1 to version 2.1.0"
+sed -i -e "s/version=3.6.7/version=3.6.8/g" python/python-orjson/spkgbuild
+changelog "python/python-orjson/spkgbuild" "Upgraded from version 3.6.7 to version 3.6.8"
+sed -i -e "s/version=7.3.2203252029/version=7.3.2204161625/g" python/python-pycontracts/spkgbuild
+changelog "python/python-pycontracts/spkgbuild" "Upgraded from version 7.3.2203252029 to version 7.3.2204161625"
+sed -i -e "s/version=1.5.2/version=1.5.3/g" python/python-typed-ast/spkgbuild
+changelog "python/python-typed-ast/spkgbuild" "Upgraded from version 1.5.2 to version 1.5.3"
+sed -i -e "s/version=1.0.0/version=1.1.0/g" qt/pavucontrol-qt/spkgbuild
+changelog "qt/pavucontrol-qt/spkgbuild" "Upgraded from version 1.0.0 to version 1.1.0"
+sed -i -e "s/version=1.0.0/version=1.1.0/g" qt/pcmanfm-qt/spkgbuild
+changelog "qt/pcmanfm-qt/spkgbuild" "Upgraded from version 1.0.0 to version 1.1.0"
+sed -i -e "s/version=1.0.0/version=1.1.0/g" qt/qterminal/spkgbuild
+changelog "qt/qterminal/spkgbuild" "Upgraded from version 1.0.0 to version 1.1.0"
+sed -i -e "s/version=1.0.0/version=1.1.0/g" qt/qtermwidget/spkgbuild
+changelog "qt/qtermwidget/spkgbuild" "Upgraded from version 1.0.0 to version 1.1.0"
+sed -i -e "s/version=4.16.3/version=4.17.0/g" xfce/xfce4-panel/spkgbuild
+changelog "xfce/xfce4-panel/spkgbuild" "Upgraded from version 4.16.3 to version 4.17.0"
