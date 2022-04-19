12755998a30a40df164ca095f02ad257ad094ccd
Voncloft <voncloft@gmail.com>
2022-04-19 07:49:05 -0400

    Minor Updates

---
+++ logs/old-test-updates/2022/Apr/applied_updates-04-19-22.sh
@@ -0,0 +1,26 @@
+sed -i -e "s/version=0.97.1/version=0.97.3/g" core/hugo/spkgbuild
+changelog "core/hugo/spkgbuild" "Upgraded from version 0.97.1 to version 0.97.3"
+sed -i -e "s/version=3.7.0/version=3.8.0/g" core/pciutils/spkgbuild
+changelog "core/pciutils/spkgbuild" "Upgraded from version 3.7.0 to version 3.8.0"
+sed -i -e "s/version=0.10.3/version=0.10.4/g" libs/libofx/spkgbuild
+changelog "libs/libofx/spkgbuild" "Upgraded from version 0.10.3 to version 0.10.4"
+sed -i -e "s/version=100.0b6/version=100.0b7/g" networking/firefox/spkgbuild
+changelog "networking/firefox/spkgbuild" "Upgraded from version 100.0b6 to version 100.0b7"
+sed -i -e "s/version=2.35.3/version=2.36.0/g" networking/git/spkgbuild
+changelog "networking/git/spkgbuild" "Upgraded from version 2.35.3 to version 2.36.0"
+sed -i -e "s/version=5.1.0/version=5.1.1/g" python/python-amqp/spkgbuild
+changelog "python/python-amqp/spkgbuild" "Upgraded from version 5.1.0 to version 5.1.1"
+sed -i -e "s/version=0.75.1/version=0.75.2/g" python/python-fastapi/spkgbuild
+changelog "python/python-fastapi/spkgbuild" "Upgraded from version 0.75.1 to version 0.75.2"
+sed -i -e "s/version=6.43.1/version=6.43.3/g" python/python-hypothesis/spkgbuild
+changelog "python/python-hypothesis/spkgbuild" "Upgraded from version 6.43.1 to version 6.43.3"
+sed -i -e "s/version=4.9.2/version=4.10.0/g" python/python-jupyter_core/spkgbuild
+changelog "python/python-jupyter_core/spkgbuild" "Upgraded from version 4.9.2 to version 4.10.0"
+sed -i -e "s/version=0.17.1/version=0.17.2/g" python/python-myst-parser/spkgbuild
+changelog "python/python-myst-parser/spkgbuild" "Upgraded from version 0.17.1 to version 0.17.2"
+sed -i -e "s/version=1.6.1/version=1.6.3/g" python/python-requirementslib/spkgbuild
+changelog "python/python-requirementslib/spkgbuild" "Upgraded from version 1.6.1 to version 1.6.3"
+sed -i -e "s/version=10.2/version=10.3/g" python/python-websockets/spkgbuild
+changelog "python/python-websockets/spkgbuild" "Upgraded from version 10.2 to version 10.3"
+sed -i -e "s/version=3.7.0/version=3.7.1/g" server/postfix/spkgbuild
+changelog "server/postfix/spkgbuild" "Upgraded from version 3.7.0 to version 3.7.1"
