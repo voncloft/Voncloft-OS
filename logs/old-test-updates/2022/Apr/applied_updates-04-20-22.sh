9a80bde47cc2b27a5cb9cef10aa36d15e5b08d97
Voncloft <voncloft@gmail.com>
2022-04-20 08:43:36 -0400

    manual update listed from distrowatch

---
+++ logs/old-test-updates/2022/Apr/applied_updates-04-20-22.sh
@@ -0,0 +1,12 @@
+sed -i -e "s/version=5.17.3/version=5.17.4/g" core/linux-router/spkgbuild
+changelog "core/linux-router/spkgbuild" "Upgraded from version 5.17.3 to version 5.17.4"
+sed -i -e "s/version=5.17.3/version=5.17.4/g" core/linux-desktop/spkgbuild
+changelog "core/linux-desktop/spkgbuild" "Upgraded from version 5.17.3 to version 5.17.4"
+sed -i -e "s/version=6.1.32/version=6.1.34/g" nonfree/virtualbox/spkgbuild
+changelog "nonfree/virtualbox/spkgbuild" "Upgraded from version 6.1.32 to version 6.1.34"
+sed -i -e "s/version=6.1.32/version=6.1.34/g" nonfree/virtualbox-modules/spkgbuild
+changelog "nonfree/virtualbox-modules/spkgbuild" "Upgraded from version 6.1.32 to version 6.1.34"
+sed -i -e "s/version=6.1.32/version=6.1.34/g" nonfree/virtualbox-guest-utils/spkgbuild
+changelog "nonfree/virtualbox-guest-utils/spkgbuild" "Upgraded from version 6.1.32 to version 6.1.34"
+sed -i -e "s/version=6.1.32/version=6.1.34/g" nonfree/virtualbox-guest-modules/spkgbuild
+changelog "nonfree/virtualbox-guest-modules/spkgbuild" "Upgraded from version 6.1.32 to version 6.1.34"
