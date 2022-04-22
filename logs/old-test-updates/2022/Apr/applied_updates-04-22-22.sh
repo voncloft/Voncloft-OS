d50cd0e5af99e00e8302f5595dc1724881c5714c
Voncloft <voncloft@gmail.com>
2022-04-22 05:54:14 -0400

    Minor Updates

---
+++ logs/old-test-updates/2022/Apr/applied_updates-04-22-22.sh
@@ -0,0 +1,44 @@
+sed -i -e "s/version=0.21.0/version=0.22.0/g" core/cbindgen/spkgbuild
+changelog "core/cbindgen/spkgbuild" "Upgraded from version 0.21.0 to version 0.22.0"
+sed -i -e "s/version=1.0.11/version=1.0.12/g" core/fribidi/spkgbuild
+changelog "core/fribidi/spkgbuild" "Upgraded from version 1.0.11 to version 1.0.12"
+sed -i -e "s/version=3.4/version=3.5/g" core/parted/spkgbuild
+changelog "core/parted/spkgbuild" "Upgraded from version 3.4 to version 3.5"
+sed -i -e "s/version=1.1.1/version=1.1.2/g" core/xauth/spkgbuild
+changelog "core/xauth/spkgbuild" "Upgraded from version 1.1.1 to version 1.1.2"
+sed -i -e "s/version=0.3.112/version=0.3.113/g" libs/libaio/spkgbuild
+changelog "libs/libaio/spkgbuild" "Upgraded from version 0.3.112 to version 0.3.113"
+sed -i -e "s/version=1.19.3/version=1.19.4/g" libs/libinput/spkgbuild
+changelog "libs/libinput/spkgbuild" "Upgraded from version 1.19.3 to version 1.19.4"
+sed -i -e "s/version=100.0b7/version=100.0b8/g" networking/firefox/spkgbuild
+changelog "networking/firefox/spkgbuild" "Upgraded from version 100.0b7 to version 100.0b8"
+sed -i -e "s/version=100.0b2/version=100.0b3/g" networking/thunderbird/spkgbuild
+changelog "networking/thunderbird/spkgbuild" "Upgraded from version 100.0b2 to version 100.0b3"
+sed -i -e "s/version=2.11.2/version=2.11.3/g" python/python-astroid/spkgbuild
+changelog "python/python-astroid/spkgbuild" "Upgraded from version 2.11.2 to version 2.11.3"
+sed -i -e "s/version=2.9.1/version=2.10.1/g" python/python-babel/spkgbuild
+changelog "python/python-babel/spkgbuild" "Upgraded from version 2.9.1 to version 2.10.1"
+sed -i -e "s/version=1.21.42/version=1.21.44/g" python/python-boto3/spkgbuild
+changelog "python/python-boto3/spkgbuild" "Upgraded from version 1.21.42 to version 1.21.44"
+sed -i -e "s/version=1.24.42/version=1.24.44/g" python/python-botocore/spkgbuild
+changelog "python/python-botocore/spkgbuild" "Upgraded from version 1.24.42 to version 1.24.44"
+sed -i -e "s/version=0.12.10/version=0.12.11/g" python/python-cachecontrol/spkgbuild
+changelog "python/python-cachecontrol/spkgbuild" "Upgraded from version 0.12.10 to version 0.12.11"
+sed -i -e "s/version=8.1.2/version=8.1.3/g" python/python-elasticsearch/spkgbuild
+changelog "python/python-elasticsearch/spkgbuild" "Upgraded from version 8.1.2 to version 8.1.3"
+sed -i -e "s/version=13.3.4/version=13.3.5/g" python/python-faker/spkgbuild
+changelog "python/python-faker/spkgbuild" "Upgraded from version 13.3.4 to version 13.3.5"
+sed -i -e "s/version=1.0.10/version=1.0.12/g" python/python-filetype/spkgbuild
+changelog "python/python-filetype/spkgbuild" "Upgraded from version 1.0.10 to version 1.0.12"
+sed -i -e "s/version=4.1.3/version=4.1.4/g" python/python-flask-security-too/spkgbuild
+changelog "python/python-flask-security-too/spkgbuild" "Upgraded from version 4.1.3 to version 4.1.4"
+sed -i -e "s/version=2.44.0/version=2.45.0/g" python/python-google-api-python-client/spkgbuild
+changelog "python/python-google-api-python-client/spkgbuild" "Upgraded from version 2.44.0 to version 2.45.0"
+sed -i -e "s/version=7.3.2204161625/version=7.3.2204191504/g" python/python-pycontracts/spkgbuild
+changelog "python/python-pycontracts/spkgbuild" "Upgraded from version 7.3.2204161625 to version 7.3.2204191504"
+sed -i -e "s/version=34.0/version=35.0/g" python/python-readme-renderer/spkgbuild
+changelog "python/python-readme-renderer/spkgbuild" "Upgraded from version 34.0 to version 35.0"
+sed -i -e "s/version=2.3.11/version=2.3.12/g" ruby-gems/ruby-bundler/spkgbuild
+changelog "ruby-gems/ruby-bundler/spkgbuild" "Upgraded from version 2.3.11 to version 2.3.12"
+sed -i -e "s/version=1.0.1/version=1.0.2/g" xfce/xfce4-terminal/spkgbuild
+changelog "xfce/xfce4-terminal/spkgbuild" "Upgraded from version 1.0.1 to version 1.0.2"
