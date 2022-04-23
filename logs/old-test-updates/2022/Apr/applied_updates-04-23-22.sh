843f74a8464723518590488bf2c90ef717f16e46
Voncloft <voncloft@gmail.com>
2022-04-23 17:44:52 -0400

    Minor Updates

---
+++ logs/old-test-updates/2022/Apr/applied_updates-04-23-22.sh
@@ -0,0 +1,6 @@
+sed -i -e "s/version=0.62.0/version=0.62.1/g" compilers/meson/spkgbuild
+changelog "compilers/meson/spkgbuild" "Upgraded from version 0.62.0 to version 0.62.1"
+sed -i -e "s/version=0.4.1555/version=0.4.1602/g" networking/you-get/spkgbuild
+changelog "networking/you-get/spkgbuild" "Upgraded from version 0.4.1555 to version 0.4.1602"
+sed -i -e "s/version=6.44.0/version=6.45.0/g" python/python-hypothesis/spkgbuild
+changelog "python/python-hypothesis/spkgbuild" "Upgraded from version 6.44.0 to version 6.45.0"
