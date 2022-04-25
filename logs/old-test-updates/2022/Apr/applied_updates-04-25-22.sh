af44450b714f063b56091ce8c855b3c4031889e9
Voncloft <voncloft@gmail.com>
2022-04-25 19:42:18 -0400

    Minor Updates

--- logs/old-test-updates/2022/Apr/applied_updates-04-25-22.sh
+++ logs/old-test-updates/2022/Apr/applied_updates-04-25-22.sh
@@ -16,3 +16,25 @@ sed -i -e "s/version=7.3.2204211403/version=7.3.2204232321/g" python/python-pyco
 changelog "python/python-pycontracts/spkgbuild" "Upgraded from version 7.3.2204211403 to version 7.3.2204232321"
 sed -i -e "s/version=5.5.2/version=5.6.0/g" python/python-socketio/spkgbuild
 changelog "python/python-socketio/spkgbuild" "Upgraded from version 5.5.2 to version 5.6.0"
+sed -i -e "s/version=1.5.5/version=1.6.1/g" core/cronie/spkgbuild
+changelog "core/cronie/spkgbuild" "Upgraded from version 1.5.5 to version 1.6.1"
+sed -i -e "s/version=2.3.5/version=2.3.6/g" core/gnupg/spkgbuild
+changelog "core/gnupg/spkgbuild" "Upgraded from version 2.3.5 to version 2.3.6"
+sed -i -e "s/version=0.2.10/version=1.0.0/g" core/waylandpp/spkgbuild
+changelog "core/waylandpp/spkgbuild" "Upgraded from version 0.2.10 to version 1.0.0"
+sed -i -e "s/version=2.0.20/version=2.0.22/g" libs/sdl2/spkgbuild
+changelog "libs/sdl2/spkgbuild" "Upgraded from version 2.0.20 to version 2.0.22"
+sed -i -e "s/version=1.21.46/version=1.22.0/g" python/python-boto3/spkgbuild
+changelog "python/python-boto3/spkgbuild" "Upgraded from version 1.21.46 to version 1.22.0"
+sed -i -e "s/version=1.24.46/version=1.25.0/g" python/python-botocore/spkgbuild
+changelog "python/python-botocore/spkgbuild" "Upgraded from version 1.24.46 to version 1.25.0"
+sed -i -e "s/version=1.0.4/version=1.0.5/g" python/python-fuse/spkgbuild
+changelog "python/python-fuse/spkgbuild" "Upgraded from version 1.0.4 to version 1.0.5"
+sed -i -e "s/version=3.10.4/version=3.10.5/g" python/python-mocket/spkgbuild
+changelog "python/python-mocket/spkgbuild" "Upgraded from version 3.10.4 to version 3.10.5"
+sed -i -e "s/version=2.10.3/version=2.10.4/g" python/python-paramiko/spkgbuild
+changelog "python/python-paramiko/spkgbuild" "Upgraded from version 2.10.3 to version 2.10.4"
+sed -i -e "s/version=7.3.2204232321/version=7.3.2204242212/g" python/python-pycontracts/spkgbuild
+changelog "python/python-pycontracts/spkgbuild" "Upgraded from version 7.3.2204232321 to version 7.3.2204242212"
+sed -i -e "s/version=1.6.3/version=1.6.4/g" python/python-requirementslib/spkgbuild
+changelog "python/python-requirementslib/spkgbuild" "Upgraded from version 1.6.3 to version 1.6.4"
