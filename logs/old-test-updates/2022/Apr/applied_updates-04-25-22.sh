be75a600afad588f8de311f3815ba17bf8618df0
Voncloft <voncloft@gmail.com>
2022-04-25 07:56:42 -0400

    Minor Updates

---
+++ logs/old-test-updates/2022/Apr/applied_updates-04-25-22.sh
@@ -0,0 +1,18 @@
+sed -i -e "s/version=1.21.1/version=1.22.0/g" media/openal/spkgbuild
+changelog "media/openal/spkgbuild" "Upgraded from version 1.21.1 to version 1.22.0"
+sed -i -e "s/version=1.5.5/version=1.5.6/g" libs/libpipeline/spkgbuild
+changelog "libs/libpipeline/spkgbuild" "Upgraded from version 1.5.5 to version 1.5.6"
+sed -i -e "s/version=3.5.1/version=3.5.2/g" libs/libressl/spkgbuild
+changelog "libs/libressl/spkgbuild" "Upgraded from version 3.5.1 to version 3.5.2"
+sed -i -e "s/version=1.1.3/version=1.2.0/g" python/python-email-validator/spkgbuild
+changelog "python/python-email-validator/spkgbuild" "Upgraded from version 1.1.3 to version 1.2.0"
+sed -i -e "s/version=4.3.1/version=4.3.2/g" python/python-engineio/spkgbuild
+changelog "python/python-engineio/spkgbuild" "Upgraded from version 4.3.1 to version 4.3.2"
+sed -i -e "s/version=5.1.1/version=5.1.2/g" python/python-flask-socketio/spkgbuild
+changelog "python/python-flask-socketio/spkgbuild" "Upgraded from version 5.1.1 to version 5.1.2"
+sed -i -e "s/version=11.0.0/version=11.1.0/g" python/python-pdoc/spkgbuild
+changelog "python/python-pdoc/spkgbuild" "Upgraded from version 11.0.0 to version 11.1.0"
+sed -i -e "s/version=7.3.2204211403/version=7.3.2204232321/g" python/python-pycontracts/spkgbuild
+changelog "python/python-pycontracts/spkgbuild" "Upgraded from version 7.3.2204211403 to version 7.3.2204232321"
+sed -i -e "s/version=5.5.2/version=5.6.0/g" python/python-socketio/spkgbuild
+changelog "python/python-socketio/spkgbuild" "Upgraded from version 5.5.2 to version 5.6.0"
