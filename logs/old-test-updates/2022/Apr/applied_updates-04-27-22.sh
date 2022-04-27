a8d8388e468430382bafaf6627bab1acf312ae27
Voncloft <voncloft@gmail.com>
2022-04-27 10:19:35 -0400

    Minor Updates

---
+++ logs/old-test-updates/2022/Apr/applied_updates-04-27-22.sh
@@ -0,0 +1,22 @@
+sed -i -e "s/version=5.16.2/version=5.17/g" core/btrfs-progs/spkgbuild
+changelog "core/btrfs-progs/spkgbuild" "Upgraded from version 5.16.2 to version 5.17"
+sed -i -e "s/version=4.4/version=4.5/g" core/nnn/spkgbuild
+changelog "core/nnn/spkgbuild" "Upgraded from version 4.4 to version 4.5"
+sed -i -e "s/version=3.03/version=3.04/g" core/sysvinit/spkgbuild
+changelog "core/sysvinit/spkgbuild" "Upgraded from version 3.03 to version 3.04"
+sed -i -e "s/version=100.0b3/version=100.0b4/g" networking/thunderbird/spkgbuild
+changelog "networking/thunderbird/spkgbuild" "Upgraded from version 100.0b3 to version 100.0b4"
+sed -i -e "s/version=1.22.0/version=1.22.1/g" python/python-boto3/spkgbuild
+changelog "python/python-boto3/spkgbuild" "Upgraded from version 1.22.0 to version 1.22.1"
+sed -i -e "s/version=1.25.0/version=1.25.1/g" python/python-botocore/spkgbuild
+changelog "python/python-botocore/spkgbuild" "Upgraded from version 1.25.0 to version 1.25.1"
+sed -i -e "s/version=4.33.2/version=4.33.3/g" python/python-fonttools/spkgbuild
+changelog "python/python-fonttools/spkgbuild" "Upgraded from version 4.33.2 to version 4.33.3"
+sed -i -e "s/version=2.45.0/version=2.46.0/g" python/python-google-api-python-client/spkgbuild
+changelog "python/python-google-api-python-client/spkgbuild" "Upgraded from version 2.45.0 to version 2.46.0"
+sed -i -e "s/version=8.0.28/version=8.0.29/g" python/python-mysql-connector/spkgbuild
+changelog "python/python-mysql-connector/spkgbuild" "Upgraded from version 8.0.28 to version 8.0.29"
+sed -i -e "s/version=1.2.1/version=1.3/g" python/python-numpydoc/spkgbuild
+changelog "python/python-numpydoc/spkgbuild" "Upgraded from version 1.2.1 to version 1.3"
+sed -i -e "s/version=12.2.0/version=12.3.0/g" python/python-rich/spkgbuild
+changelog "python/python-rich/spkgbuild" "Upgraded from version 12.2.0 to version 12.3.0"
