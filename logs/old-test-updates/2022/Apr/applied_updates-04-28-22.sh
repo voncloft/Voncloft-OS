fa7527121867fed81748ba83db429c43b17ad228
Voncloft <voncloft@gmail.com>
2022-04-28 10:25:01 -0400

    Minor Updates

---
+++ logs/old-test-updates/2022/Apr/applied_updates-04-28-22.sh
@@ -0,0 +1,40 @@
+sed -i -e "s/version=14.0.1/version=14.0.2/g" compilers/clang/spkgbuild
+changelog "compilers/clang/spkgbuild" "Upgraded from version 14.0.1 to version 14.0.2"
+sed -i -e "s/version=14.0.1/version=14.0.2/g" compilers/lld/spkgbuild
+changelog "compilers/lld/spkgbuild" "Upgraded from version 14.0.1 to version 14.0.2"
+sed -i -e "s/version=14.0.1/version=14.0.2/g" compilers/llvm/spkgbuild
+changelog "compilers/llvm/spkgbuild" "Upgraded from version 14.0.1 to version 14.0.2"
+sed -i -e "s/version=7.82.0/version=7.83.0/g" core/curl/spkgbuild
+changelog "core/curl/spkgbuild" "Upgraded from version 7.82.0 to version 7.83.0"
+sed -i -e "s/version=5.17.4/version=5.17.5/g" core/linux-desktop/spkgbuild
+changelog "core/linux-desktop/spkgbuild" "Upgraded from version 5.17.4 to version 5.17.5"
+sed -i -e "s/version=5.17.4/version=5.17.5/g" core/linux-router/spkgbuild
+changelog "core/linux-router/spkgbuild" "Upgraded from version 5.17.4 to version 5.17.5"
+sed -i -e "s/version=6.2.6/version=7.0.0/g" core/redis/spkgbuild
+changelog "core/redis/spkgbuild" "Upgraded from version 6.2.6 to version 7.0.0"
+sed -i -e "s/version=14.0.1/version=14.0.2/g" multilib/llvm-32/spkgbuild
+changelog "multilib/llvm-32/spkgbuild" "Upgraded from version 14.0.1 to version 14.0.2"
+sed -i -e "s/version=0.4.6.10/version=0.4.7.7/g" networking/tor/spkgbuild
+changelog "networking/tor/spkgbuild" "Upgraded from version 0.4.6.10 to version 0.4.7.7"
+sed -i -e "s/version=3.5.0/version=3.6.3/g" networking/wireshark/spkgbuild
+changelog "networking/wireshark/spkgbuild" "Upgraded from version 3.5.0 to version 3.6.3"
+sed -i -e "s/version=1.22.1/version=1.22.2/g" python/python-boto3/spkgbuild
+changelog "python/python-boto3/spkgbuild" "Upgraded from version 1.22.1 to version 1.22.2"
+sed -i -e "s/version=1.25.1/version=1.25.2/g" python/python-botocore/spkgbuild
+changelog "python/python-botocore/spkgbuild" "Upgraded from version 1.25.1 to version 1.25.2"
+sed -i -e "s/version=13.4.0/version=13.6.0/g" python/python-faker/spkgbuild
+changelog "python/python-faker/spkgbuild" "Upgraded from version 13.4.0 to version 13.6.0"
+sed -i -e "s/version=6.45.0/version=6.45.1/g" python/python-hypothesis/spkgbuild
+changelog "python/python-hypothesis/spkgbuild" "Upgraded from version 6.45.0 to version 6.45.1"
+sed -i -e "s/version=2.4.12/version=2.5.0/g" python/python-identify/spkgbuild
+changelog "python/python-identify/spkgbuild" "Upgraded from version 2.4.12 to version 2.5.0"
+sed -i -e "s/version=11.2.0/version=11.3.0/g" python/python-oslo-db/spkgbuild
+changelog "python/python-oslo-db/spkgbuild" "Upgraded from version 11.2.0 to version 11.3.0"
+sed -i -e "s/version=4.12.2/version=4.13.0/g" python/python-oslo-utils/spkgbuild
+changelog "python/python-oslo-utils/spkgbuild" "Upgraded from version 4.12.2 to version 4.13.0"
+sed -i -e "s/version=1.0.2/version=1.1.0/g" python/python-scikit-learn/spkgbuild
+changelog "python/python-scikit-learn/spkgbuild" "Upgraded from version 1.0.2 to version 1.1.0"
+sed -i -e "s/version=3.10.8/version=3.11.2/g" python/python-trimesh/spkgbuild
+changelog "python/python-trimesh/spkgbuild" "Upgraded from version 3.10.8 to version 3.11.2"
+sed -i -e "s/version=5.2/version=5.3/g" python/python-zope-security/spkgbuild
+changelog "python/python-zope-security/spkgbuild" "Upgraded from version 5.2 to version 5.3"
