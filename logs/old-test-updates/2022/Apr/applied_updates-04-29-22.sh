425b23668d5df3e92d5fc82fce7d0c3baff028e0
Voncloft <voncloft@gmail.com>
2022-04-29 07:05:21 -0400

    Minor Updates

---
+++ logs/old-test-updates/2022/Apr/applied_updates-04-29-22.sh
@@ -0,0 +1,32 @@
+sed -i -e "s/version=0.97.3/version=0.98.0/g" core/hugo/spkgbuild
+changelog "core/hugo/spkgbuild" "Upgraded from version 0.97.3 to version 0.98.0"
+sed -i -e "s/version=3.77/version=3.78/g" core/nss/spkgbuild
+changelog "core/nss/spkgbuild" "Upgraded from version 3.77 to version 3.78"
+sed -i -e "s/version=0.3.50/version=0.3.51/g" core/pipewire/spkgbuild
+changelog "core/pipewire/spkgbuild" "Upgraded from version 0.3.50 to version 0.3.51"
+sed -i -e "s/version=2.1.1/version=2.1.2/g" extra/ncdu/spkgbuild
+changelog "extra/ncdu/spkgbuild" "Upgraded from version 2.1.1 to version 2.1.2"
+sed -i -e "s/version=1.26.2/version=1.26.4/g" libs/libmbim/spkgbuild
+changelog "libs/libmbim/spkgbuild" "Upgraded from version 1.26.2 to version 1.26.4"
+sed -i -e "s/version=1.30.4/version=1.30.6/g" libs/libqmi/spkgbuild
+changelog "libs/libqmi/spkgbuild" "Upgraded from version 1.30.4 to version 1.30.6"
+sed -i -e "s/version=1.22.2/version=1.22.3/g" python/python-boto3/spkgbuild
+changelog "python/python-boto3/spkgbuild" "Upgraded from version 1.22.2 to version 1.22.3"
+sed -i -e "s/version=1.25.2/version=1.25.3/g" python/python-botocore/spkgbuild
+changelog "python/python-botocore/spkgbuild" "Upgraded from version 1.25.2 to version 1.25.3"
+sed -i -e "s/version=8.1.2/version=8.1.3/g" python/python-click/spkgbuild
+changelog "python/python-click/spkgbuild" "Upgraded from version 8.1.2 to version 8.1.3"
+sed -i -e "s/version=2.13.0/version=2.13.1/g" python/python-connexion/spkgbuild
+changelog "python/python-connexion/spkgbuild" "Upgraded from version 2.13.0 to version 2.13.1"
+sed -i -e "s/version=2.5.0/version=2.5.1/g" python/python-elementpath/spkgbuild
+changelog "python/python-elementpath/spkgbuild" "Upgraded from version 2.5.0 to version 2.5.1"
+sed -i -e "s/version=2.1.1/version=2.1.2/g" python/python-flask/spkgbuild
+changelog "python/python-flask/spkgbuild" "Upgraded from version 2.1.1 to version 2.1.2"
+sed -i -e "s/version=3.3.0/version=3.4.0/g" python/python-gitlab/spkgbuild
+changelog "python/python-gitlab/spkgbuild" "Upgraded from version 3.3.0 to version 3.4.0"
+sed -i -e "s/version=2.4.2/version=2.4.3/g" python/python-markdown2/spkgbuild
+changelog "python/python-markdown2/spkgbuild" "Upgraded from version 2.4.2 to version 2.4.3"
+sed -i -e "s/version=2.1.1/version=2.1.2/g" python/python-werkzeug/spkgbuild
+changelog "python/python-werkzeug/spkgbuild" "Upgraded from version 2.1.1 to version 2.1.2"
+sed -i -e "s/version=3.7.1/version=3.7.2/g" server/postfix/spkgbuild
+changelog "server/postfix/spkgbuild" "Upgraded from version 3.7.1 to version 3.7.2"
