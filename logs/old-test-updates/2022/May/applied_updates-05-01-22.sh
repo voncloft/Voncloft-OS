294186df4b1733cf09a9dd3232ea80dd4f79d49f
Voncloft <voncloft@gmail.com>
2022-05-01 11:30:37 -0400

    Minor Updates

---
+++ logs/old-test-updates/2022/May/applied_updates-05-01-22.sh
@@ -0,0 +1,36 @@
+sed -i -e "s/version=14.0.2/version=14.0.3/g" compilers/clang/spkgbuild
+changelog "compilers/clang/spkgbuild" "Upgraded from version 14.0.2 to version 14.0.3"
+sed -i -e "s/version=14.0.2/version=14.0.3/g" compilers/lld/spkgbuild
+changelog "compilers/lld/spkgbuild" "Upgraded from version 14.0.2 to version 14.0.3"
+sed -i -e "s/version=14.0.2/version=14.0.3/g" compilers/llvm/spkgbuild
+changelog "compilers/llvm/spkgbuild" "Upgraded from version 14.0.2 to version 14.0.3"
+sed -i -e "s/version=14.0.2/version=14.0.3/g" multilib/llvm-32/spkgbuild
+changelog "multilib/llvm-32/spkgbuild" "Upgraded from version 14.0.2 to version 14.0.3"
+sed -i -e "s/version=0.37.1/version=0.37.2/g" libs/libfilezilla/spkgbuild
+changelog "libs/libfilezilla/spkgbuild" "Upgraded from version 0.37.1 to version 0.37.2"
+sed -i -e "s/version=6.9.7.1/version=6.9.8/g" libs/oniguruma/spkgbuild
+changelog "libs/oniguruma/spkgbuild" "Upgraded from version 6.9.7.1 to version 6.9.8"
+sed -i -e "s/version=2.14.8/version=2.14.9/g" networking/pidgin/spkgbuild
+changelog "networking/pidgin/spkgbuild" "Upgraded from version 2.14.8 to version 2.14.9"
+sed -i -e "s/version=5.1.1/version=5.2.0/g" python/python-apispec/spkgbuild
+changelog "python/python-apispec/spkgbuild" "Upgraded from version 5.1.1 to version 5.2.0"
+sed -i -e "s/version=1.22.3/version=1.22.4/g" python/python-boto3/spkgbuild
+changelog "python/python-boto3/spkgbuild" "Upgraded from version 1.22.3 to version 1.22.4"
+sed -i -e "s/version=1.25.3/version=1.25.4/g" python/python-botocore/spkgbuild
+changelog "python/python-botocore/spkgbuild" "Upgraded from version 1.25.3 to version 1.25.4"
+sed -i -e "s/version=2022.04.1/version=2022.04.2/g" python/python-distributed/spkgbuild
+changelog "python/python-distributed/spkgbuild" "Upgraded from version 2022.04.1 to version 2022.04.2"
+sed -i -e "s/version=0.17.5/version=0.17.6/g" python/python-django-modeltranslation/spkgbuild
+changelog "python/python-django-modeltranslation/spkgbuild" "Upgraded from version 0.17.5 to version 0.17.6"
+sed -i -e "s/version=6.45.1/version=6.45.3/g" python/python-hypothesis/spkgbuild
+changelog "python/python-hypothesis/spkgbuild" "Upgraded from version 6.45.1 to version 6.45.3"
+sed -i -e "s/version=2.3.0/version=2.4.0/g" python/python-jaraco.envs/spkgbuild
+changelog "python/python-jaraco.envs/spkgbuild" "Upgraded from version 2.3.0 to version 2.4.0"
+sed -i -e "s/version=1.3/version=1.3.1/g" python/python-numpydoc/spkgbuild
+changelog "python/python-numpydoc/spkgbuild" "Upgraded from version 1.3 to version 1.3.1"
+sed -i -e "s/version=22.0.4/version=22.1/g" python/python-pip/spkgbuild
+changelog "python/python-pip/spkgbuild" "Upgraded from version 22.0.4 to version 22.1"
+sed -i -e "s/version=7.6.0/version=7.7.0/g" python/python-qiniu/spkgbuild
+changelog "python/python-qiniu/spkgbuild" "Upgraded from version 7.6.0 to version 7.7.0"
+sed -i -e "s/version=0.4.0/version=0.5.0/g" xfce/thunar-archive-plugin/spkgbuild
+changelog "xfce/thunar-archive-plugin/spkgbuild" "Upgraded from version 0.4.0 to version 0.5.0"
