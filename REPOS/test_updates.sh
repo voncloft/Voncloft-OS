8925efde2b04e1fa91fa886605d9e52faccb471d
Voncloft <voncloft@gmail.com>
2022-05-03 07:50:40 -0400

    moved tigervnc from beta to networking

---
+++ REPOS/test_updates.sh
@@ -0,0 +1,34 @@
+sed -i -e "s/version=11.2/version=12.1/g" compilers/gdb/spkgbuild
+changelog "compilers/gdb/spkgbuild" "Upgraded from version 11.2 to version 12.1"
+sed -i -e "s/version=3.10.5/version=3.11.0/g" core/fuse2/spkgbuild
+changelog "core/fuse2/spkgbuild" "Upgraded from version 3.10.5 to version 3.11.0"
+sed -i -e "s/version=3.10.5/version=3.11.0/g" core/fuse3/spkgbuild
+changelog "core/fuse3/spkgbuild" "Upgraded from version 3.10.5 to version 3.11.0"
+sed -i -e "s/version=2.9.6/version=2.9.7/g" core/icewm/spkgbuild
+changelog "core/icewm/spkgbuild" "Upgraded from version 2.9.6 to version 2.9.7"
+sed -i -e "s/version=5.2.0/version=5.2.1/g" python/python-apispec/spkgbuild
+changelog "python/python-apispec/spkgbuild" "Upgraded from version 5.2.0 to version 5.2.1"
+sed -i -e "s/version=2.11.3/version=2.11.4/g" python/python-astroid/spkgbuild
+changelog "python/python-astroid/spkgbuild" "Upgraded from version 2.11.3 to version 2.11.4"
+sed -i -e "s/version=1.22.4/version=1.22.5/g" python/python-boto3/spkgbuild
+changelog "python/python-boto3/spkgbuild" "Upgraded from version 1.22.4 to version 1.22.5"
+sed -i -e "s/version=1.25.4/version=1.25.5/g" python/python-botocore/spkgbuild
+changelog "python/python-botocore/spkgbuild" "Upgraded from version 1.25.4 to version 1.25.5"
+sed -i -e "s/version=0.59.0/version=0.59.1/g" python/python-cfn-lint/spkgbuild
+changelog "python/python-cfn-lint/spkgbuild" "Upgraded from version 0.59.0 to version 0.59.1"
+sed -i -e "s/version=2022.04.2/version=2022.05.0/g" python/python-distributed/spkgbuild
+changelog "python/python-distributed/spkgbuild" "Upgraded from version 2022.04.2 to version 2022.05.0"
+sed -i -e "s/version=1.2.0/version=1.2.1/g" python/python-email-validator/spkgbuild
+changelog "python/python-email-validator/spkgbuild" "Upgraded from version 1.2.0 to version 1.2.1"
+sed -i -e "s/version=13.6.0/version=13.7.0/g" python/python-faker/spkgbuild
+changelog "python/python-faker/spkgbuild" "Upgraded from version 13.6.0 to version 13.7.0"
+sed -i -e "s/version=0.6.0/version=0.6.1/g" python/python-flask-login/spkgbuild
+changelog "python/python-flask-login/spkgbuild" "Upgraded from version 0.6.0 to version 0.6.1"
+sed -i -e "s/version=2.4.15/version=2.4.16/g" python/python-fs/spkgbuild
+changelog "python/python-fs/spkgbuild" "Upgraded from version 2.4.15 to version 2.4.16"
+sed -i -e "s/version=2.0.1/version=2.1.0/g" python/python-qtpy/spkgbuild
+changelog "python/python-qtpy/spkgbuild" "Upgraded from version 2.0.1 to version 2.1.0"
+sed -i -e "s/version=1.14.0/version=1.14.1/g" python/python-wrapt/spkgbuild
+changelog "python/python-wrapt/spkgbuild" "Upgraded from version 1.14.0 to version 1.14.1"
+sed -i -e "s/version=4.16.0/version=4.16.1/g" server/samba/spkgbuild
+changelog "server/samba/spkgbuild" "Upgraded from version 4.16.0 to version 4.16.1"
