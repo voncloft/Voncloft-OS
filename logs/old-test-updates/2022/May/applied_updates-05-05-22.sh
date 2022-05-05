3c3cb2dc31924879d4c6224421f7b3ab0d1b6f6d
Voncloft <voncloft@gmail.com>
2022-05-05 07:43:26 -0400

    Minor Updates

---
+++ logs/old-test-updates/2022/May/applied_updates-05-05-22.sh
@@ -0,0 +1,30 @@
+sed -i -e "s/version=6.1.1/version=6.1.2/g" core/mercurial/spkgbuild
+changelog "core/mercurial/spkgbuild" "Upgraded from version 6.1.1 to version 6.1.2"
+sed -i -e "s/version=1.14/version=1.15/g" libs/libxcb/spkgbuild
+changelog "libs/libxcb/spkgbuild" "Upgraded from version 1.14 to version 1.15"
+sed -i -e "s/version=22.04.0/version=22.05.0/g" libs/poppler/spkgbuild
+changelog "libs/poppler/spkgbuild" "Upgraded from version 22.04.0 to version 22.05.0"
+sed -i -e "s/version=101.0b1/version=101.0b2/g" networking/firefox/spkgbuild
+changelog "networking/firefox/spkgbuild" "Upgraded from version 101.0b1 to version 101.0b2"
+sed -i -e "s/version=1.22.5/version=1.22.7/g" python/python-boto3/spkgbuild
+changelog "python/python-boto3/spkgbuild" "Upgraded from version 1.22.5 to version 1.22.7"
+sed -i -e "s/version=1.25.5/version=1.25.7/g" python/python-botocore/spkgbuild
+changelog "python/python-botocore/spkgbuild" "Upgraded from version 1.25.5 to version 1.25.7"
+sed -i -e "s/version=0.17.6/version=0.17.7/g" python/python-django-modeltranslation/spkgbuild
+changelog "python/python-django-modeltranslation/spkgbuild" "Upgraded from version 0.17.6 to version 0.17.7"
+sed -i -e "s/version=0.4.1/version=0.4.2/g" python/python-libcst/spkgbuild
+changelog "python/python-libcst/spkgbuild" "Upgraded from version 0.4.1 to version 0.4.2"
+sed -i -e "s/version=2021.12/version=2022.04/g" python/python-litejesd204b/spkgbuild
+changelog "python/python-litejesd204b/spkgbuild" "Upgraded from version 2021.12 to version 2022.04"
+sed -i -e "s/version=2021.12/version=2022.04/g" python/python-liteusb/spkgbuild
+changelog "python/python-liteusb/spkgbuild" "Upgraded from version 2021.12 to version 2022.04"
+sed -i -e "s/version=11.1.0/version=11.2.0/g" python/python-pdoc/spkgbuild
+changelog "python/python-pdoc/spkgbuild" "Upgraded from version 11.1.0 to version 11.2.0"
+sed -i -e "s/version=0.18.2/version=0.19.0/g" python/python-validators/spkgbuild
+changelog "python/python-validators/spkgbuild" "Upgraded from version 0.18.2 to version 0.19.0"
+sed -i -e "s/version=2.3.12/version=2.3.13/g" ruby-gems/ruby-bundler/spkgbuild
+changelog "ruby-gems/ruby-bundler/spkgbuild" "Upgraded from version 2.3.12 to version 2.3.13"
+sed -i -e "s/version=5.15.0/version=5.16.0/g" xfce/xfsprogs/spkgbuild
+changelog "xfce/xfsprogs/spkgbuild" "Upgraded from version 5.15.0 to version 5.16.0"
+sed -i -e "s/version=1.14/version=1.15/g" xorg/xcb-proto/spkgbuild
+changelog "xorg/xcb-proto/spkgbuild" "Upgraded from version 1.14 to version 1.15"
