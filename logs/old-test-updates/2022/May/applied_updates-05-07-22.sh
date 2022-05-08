7a22179f196b72668ffd903502631ea708508f8c
Voncloft <voncloft@gmail.com>
2022-05-07 21:15:20 -0400

    Minor Updates

---
+++ logs/old-test-updates/2022/May/applied_updates-05-07-22.sh
@@ -0,0 +1,56 @@
+sed -i -e "s/version=5.2.1/version=5.3.0/g" core/jemalloc/spkgbuild
+changelog "core/jemalloc/spkgbuild" "Upgraded from version 5.2.1 to version 5.3.0"
+sed -i -e "s/version=3.3.8/version=3.3.9/g" extra/flexget/spkgbuild
+changelog "extra/flexget/spkgbuild" "Upgraded from version 3.3.8 to version 3.3.9"
+sed -i -e "s/version=101.0b2/version=101.0b3/g" networking/firefox/spkgbuild
+changelog "networking/firefox/spkgbuild" "Upgraded from version 101.0b2 to version 101.0b3"
+sed -i -e "s/version=2.36.0/version=2.36.1/g" networking/git/spkgbuild
+changelog "networking/git/spkgbuild" "Upgraded from version 2.36.0 to version 2.36.1"
+sed -i -e "s/version=3.6.3/version=3.6.5/g" networking/wireshark/spkgbuild
+changelog "networking/wireshark/spkgbuild" "Upgraded from version 3.6.3 to version 3.6.5"
+sed -i -e "s/version=7.3.2/version=7.3.3/g" nonfree/libreoffice/spkgbuild
+changelog "nonfree/libreoffice/spkgbuild" "Upgraded from version 7.3.2 to version 7.3.3"
+sed -i -e "s/version=1.6.0/version=1.7.0/g" plasma/plasma-wayland-protocols/spkgbuild
+changelog "plasma/plasma-wayland-protocols/spkgbuild" "Upgraded from version 1.6.0 to version 1.7.0"
+sed -i -e "s/version=2.2.0/version=2.3.1/g" python/python-aiobotocore/spkgbuild
+changelog "python/python-aiobotocore/spkgbuild" "Upgraded from version 2.2.0 to version 2.3.1"
+sed -i -e "s/version=1.22.7/version=1.22.9/g" python/python-boto3/spkgbuild
+changelog "python/python-boto3/spkgbuild" "Upgraded from version 1.22.7 to version 1.22.9"
+sed -i -e "s/version=1.25.7/version=1.25.9/g" python/python-botocore/spkgbuild
+changelog "python/python-botocore/spkgbuild" "Upgraded from version 1.25.7 to version 1.25.9"
+sed -i -e "s/version=0.75.2/version=0.76.0/g" python/python-fastapi/spkgbuild
+changelog "python/python-fastapi/spkgbuild" "Upgraded from version 0.75.2 to version 0.76.0"
+sed -i -e "s/version=0.9.6/version=0.9.7/g" python/python-json5/spkgbuild
+changelog "python/python-json5/spkgbuild" "Upgraded from version 0.9.6 to version 0.9.7"
+sed -i -e "s/version=4.4.0/version=4.5.1/g" python/python-jsonschema/spkgbuild
+changelog "python/python-jsonschema/spkgbuild" "Upgraded from version 4.4.0 to version 4.5.1"
+sed -i -e "s/version=23.3.0/version=23.6.0/g" python/python-kubernetes/spkgbuild
+changelog "python/python-kubernetes/spkgbuild" "Upgraded from version 23.3.0 to version 23.6.0"
+sed -i -e "s/version=1.0.3/version=2.0.0/g" python/python-linkify-it-py/spkgbuild
+changelog "python/python-linkify-it-py/spkgbuild" "Upgraded from version 1.0.3 to version 2.0.0"
+sed -i -e "s/version=3.17.0/version=3.18.0/g" python/python-openmdao/spkgbuild
+changelog "python/python-openmdao/spkgbuild" "Upgraded from version 3.17.0 to version 3.18.0"
+sed -i -e "s/version=4.5.0/version=4.5.1/g" python/python-oslo-concurrency/spkgbuild
+changelog "python/python-oslo-concurrency/spkgbuild" "Upgraded from version 4.5.0 to version 4.5.1"
+sed -i -e "s/version=4.7.0/version=4.8.0/g" python/python-oslo-log/spkgbuild
+changelog "python/python-oslo-log/spkgbuild" "Upgraded from version 4.7.0 to version 4.8.0"
+sed -i -e "s/version=3.4.2/version=3.4.3/g" python/python-osprofiler/spkgbuild
+changelog "python/python-osprofiler/spkgbuild" "Upgraded from version 3.4.2 to version 3.4.3"
+sed -i -e "s/version=5.8.1/version=5.9.0/g" python/python-pbr/spkgbuild
+changelog "python/python-pbr/spkgbuild" "Upgraded from version 5.8.1 to version 5.9.0"
+sed -i -e "s/version=8.12.47/version=8.12.48/g" python/python-phonenumbers/spkgbuild
+changelog "python/python-phonenumbers/spkgbuild" "Upgraded from version 8.12.47 to version 8.12.48"
+sed -i -e "s/version=2.18.1/version=2.19.0/g" python/python-pre-commit/spkgbuild
+changelog "python/python-pre-commit/spkgbuild" "Upgraded from version 2.18.1 to version 2.19.0"
+sed -i -e "s/version=12.3.0/version=12.4.0/g" python/python-rich/spkgbuild
+changelog "python/python-rich/spkgbuild" "Upgraded from version 12.3.0 to version 12.4.0"
+sed -i -e "s/version=13.11/version=20.0/g" python/python-telegram-bot/spkgbuild
+changelog "python/python-telegram-bot/spkgbuild" "Upgraded from version 13.11 to version 20.0"
+sed -i -e "s/version=3.11.2/version=3.12.0/g" python/python-trimesh/spkgbuild
+changelog "python/python-trimesh/spkgbuild" "Upgraded from version 3.11.2 to version 3.12.0"
+sed -i -e "s/version=4.8.0/version=4.9.0/g" python/python-tweepy/spkgbuild
+changelog "python/python-tweepy/spkgbuild" "Upgraded from version 4.8.0 to version 4.9.0"
+sed -i -e "s/version=0.38.5/version=0.38.6/g" python/python-zeroconf/spkgbuild
+changelog "python/python-zeroconf/spkgbuild" "Upgraded from version 0.38.5 to version 0.38.6"
+sed -i -e "s/version=1.31.0/version=1.31.1/g" ruby-gems/ruby-tins/spkgbuild
+changelog "ruby-gems/ruby-tins/spkgbuild" "Upgraded from version 1.31.0 to version 1.31.1"
