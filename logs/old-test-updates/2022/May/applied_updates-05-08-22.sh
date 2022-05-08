8e304199ec9fbe2ad83441b2e42e49a6f435d558
Voncloft <voncloft@gmail.com>
2022-05-08 19:46:29 -0400

    Minor Updates

---
+++ logs/old-test-updates/2022/May/applied_updates-05-08-22.sh
@@ -0,0 +1,6 @@
+sed -i -e "s/version=1.1.0/version=1.1.1/g" lxqt/lxqt-session/spkgbuild
+changelog "lxqt/lxqt-session/spkgbuild" "Upgraded from version 1.1.0 to version 1.1.1"
+sed -i -e "s/version=3.9.0/version=3.9.1/g" libs/libqtxdg/spkgbuild
+changelog "libs/libqtxdg/spkgbuild" "Upgraded from version 3.9.0 to version 3.9.1"
+sed -i -e "s/version=12.4.0/version=12.4.1/g" python/python-rich/spkgbuild
+changelog "python/python-rich/spkgbuild" "Upgraded from version 12.4.0 to version 12.4.1"
