d45d3e5053e7d5afeab53f13aab23d49ce6eff37
Voncloft <voncloft@gmail.com>
2022-05-10 10:05:54 -0400

    Minor Updates

---
+++ logs/old-test-updates/2022/May/applied_updates-05-10-22.sh
@@ -0,0 +1,24 @@
+sed -i -e "s/version=5.17.5/version=5.17.6/g" core/linux-desktop/spkgbuild
+changelog "core/linux-desktop/spkgbuild" "Upgraded from version 5.17.5 to version 5.17.6"
+sed -i -e "s/version=5.17.5/version=5.17.6/g" core/linux-router/spkgbuild
+changelog "core/linux-router/spkgbuild" "Upgraded from version 5.17.5 to version 5.17.6"
+sed -i -e "s/version=23.4/version=23.5/g" core/psmisc/spkgbuild
+changelog "core/psmisc/spkgbuild" "Upgraded from version 23.4 to version 23.5"
+sed -i -e "s/version=101.0b3/version=101.0b4/g" networking/firefox/spkgbuild
+changelog "networking/firefox/spkgbuild" "Upgraded from version 101.0b3 to version 101.0b4"
+sed -i -e "s/version=2.3.1/version=2.3.2/g" python/python-aiobotocore/spkgbuild
+changelog "python/python-aiobotocore/spkgbuild" "Upgraded from version 2.3.1 to version 2.3.2"
+sed -i -e "s/version=0.1.0/version=0.1.1/g" python/python-aiomysql/spkgbuild
+changelog "python/python-aiomysql/spkgbuild" "Upgraded from version 0.1.0 to version 0.1.1"
+sed -i -e "s/version=2.11.4/version=2.11.5/g" python/python-astroid/spkgbuild
+changelog "python/python-astroid/spkgbuild" "Upgraded from version 2.11.4 to version 2.11.5"
+sed -i -e "s/version=1.22.9/version=1.22.10/g" python/python-boto3/spkgbuild
+changelog "python/python-boto3/spkgbuild" "Upgraded from version 1.22.9 to version 1.22.10"
+sed -i -e "s/version=1.25.9/version=1.25.10/g" python/python-botocore/spkgbuild
+changelog "python/python-botocore/spkgbuild" "Upgraded from version 1.25.9 to version 1.25.10"
+sed -i -e "s/version=13.7.0/version=13.11.0/g" python/python-faker/spkgbuild
+changelog "python/python-faker/spkgbuild" "Upgraded from version 13.7.0 to version 13.11.0"
+sed -i -e "s/version=0.9.7/version=0.9.8/g" python/python-json5/spkgbuild
+changelog "python/python-json5/spkgbuild" "Upgraded from version 0.9.7 to version 0.9.8"
+sed -i -e "s/version=0.18.2/version=0.18.3/g" python/python-meilisearch/spkgbuild
+changelog "python/python-meilisearch/spkgbuild" "Upgraded from version 0.18.2 to version 0.18.3"
