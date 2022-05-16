6614ad2334d48a8a5b2ff084afe5dff11ec43b95
Voncloft <voncloft@gmail.com>
2022-05-16 06:42:57 -0400

    Minor Updates

---
+++ logs/old-test-updates/2022/May/applied_updates-05-16-22.sh
@@ -0,0 +1,22 @@
+sed -i -e "s/version=4.6/version=4.6.1/g" compilers/ccache/spkgbuild
+changelog "compilers/ccache/spkgbuild" "Upgraded from version 4.6 to version 4.6.1"
+sed -i -e "s/version=1.10.2/version=1.11.0/g" compilers/ninja/spkgbuild
+changelog "compilers/ninja/spkgbuild" "Upgraded from version 1.10.2 to version 1.11.0"
+sed -i -e "s/version=5.17.7/version=5.17.8/g" core/linux-desktop/spkgbuild
+changelog "core/linux-desktop/spkgbuild" "Upgraded from version 5.17.7 to version 5.17.8"
+sed -i -e "s/version=5.17.7/version=5.17.8/g" core/linux-router/spkgbuild
+changelog "core/linux-router/spkgbuild" "Upgraded from version 5.17.7 to version 5.17.8"
+sed -i -e "s/version=5.8.1/version=5.9/g" core/zsh/spkgbuild
+changelog "core/zsh/spkgbuild" "Upgraded from version 5.8.1 to version 5.9"
+sed -i -e "s/version=0.18.0/version=0.18.2/g" python/python-django-modeltranslation/spkgbuild
+changelog "python/python-django-modeltranslation/spkgbuild" "Upgraded from version 0.18.0 to version 0.18.2"
+sed -i -e "s/version=0.77.1/version=0.78.0/g" python/python-fastapi/spkgbuild
+changelog "python/python-fastapi/spkgbuild" "Upgraded from version 0.77.1 to version 0.78.0"
+sed -i -e "s/version=6.46.3/version=6.46.5/g" python/python-hypothesis/spkgbuild
+changelog "python/python-hypothesis/spkgbuild" "Upgraded from version 6.46.3 to version 6.46.5"
+sed -i -e "s/version=11.2.0/version=12.0.0/g" python/python-pdoc/spkgbuild
+changelog "python/python-pdoc/spkgbuild" "Upgraded from version 11.2.0 to version 12.0.0"
+sed -i -e "s/version=2.1.7/version=2.1.8/g" python/python-watchdog/spkgbuild
+changelog "python/python-watchdog/spkgbuild" "Upgraded from version 2.1.7 to version 2.1.8"
+sed -i -e "s/version=4.16.1/version=4.17.0/g" xfce/garcon/spkgbuild
+changelog "xfce/garcon/spkgbuild" "Upgraded from version 4.16.1 to version 4.17.0"
