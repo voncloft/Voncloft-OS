ed9167db4413eea51c4108c67f1bf58821ace7cb
Voncloft <voncloft@gmail.com>
2022-05-17 07:57:49 -0400

    Minor Updates

---
+++ REPOS/test_updates.sh
@@ -0,0 +1,18 @@
+sed -i -e "s/version=0.98.0/version=0.99.0/g" core/hugo/spkgbuild
+changelog "core/hugo/spkgbuild" "Upgraded from version 0.98.0 to version 0.99.0"
+sed -i -e "s/version=1.13.1/version=1.13.2/g" libs/libwpe/spkgbuild
+changelog "libs/libwpe/spkgbuild" "Upgraded from version 1.13.1 to version 1.13.2"
+sed -i -e "s/version=101.0b6/version=101.0b7/g" networking/firefox/spkgbuild
+changelog "networking/firefox/spkgbuild" "Upgraded from version 101.0b6 to version 101.0b7"
+sed -i -e "s/version=1.23.0/version=1.23.1/g" python/python-boto3/spkgbuild
+changelog "python/python-boto3/spkgbuild" "Upgraded from version 1.23.0 to version 1.23.1"
+sed -i -e "s/version=1.26.0/version=1.26.1/g" python/python-botocore/spkgbuild
+changelog "python/python-botocore/spkgbuild" "Upgraded from version 1.26.0 to version 1.26.1"
+sed -i -e "s/version=5.0.0/version=5.1.0/g" python/python-cachetools/spkgbuild
+changelog "python/python-cachetools/spkgbuild" "Upgraded from version 5.0.0 to version 5.1.0"
+sed -i -e "s/version=0.3.2/version=0.3.3/g" python/python-flake8-black/spkgbuild
+changelog "python/python-flake8-black/spkgbuild" "Upgraded from version 0.3.2 to version 0.3.3"
+sed -i -e "s/version=1.10.1/version=1.11.2/g" python/python-manuel/spkgbuild
+changelog "python/python-manuel/spkgbuild" "Upgraded from version 1.10.1 to version 1.11.2"
+sed -i -e "s/version=0.11.2/version=0.12.0/g" python/python-seaborn/spkgbuild
+changelog "python/python-seaborn/spkgbuild" "Upgraded from version 0.11.2 to version 0.12.0"
