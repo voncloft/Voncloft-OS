0a1eeeed5dbcec478e928b9d54ee44d2b1a16d03
Voncloft <voncloft@gmail.com>
2022-05-22 13:38:34 -0400

    Minor Updates

---
+++ logs/old-test-updates/2022/May/applied_updates-05-22-22.sh
@@ -0,0 +1,24 @@
+sed -i -e "s/version=3.3.10/version=3.3.11/g" extra/flexget/spkgbuild
+changelog "extra/flexget/spkgbuild" "Upgraded from version 3.3.10 to version 3.3.11"
+sed -i -e "s/version=6.0.9/version=6.0.10/g" python/python-feedparser/spkgbuild
+changelog "python/python-feedparser/spkgbuild" "Upgraded from version 6.0.9 to version 6.0.10"
+sed -i -e "s/version=3.6.0/version=4.0.0/g" python/python-glanceclient/spkgbuild
+changelog "python/python-glanceclient/spkgbuild" "Upgraded from version 3.6.0 to version 4.0.0"
+sed -i -e "s/version=4.11.3/version=4.11.4/g" python/python-importlib-metadata/spkgbuild
+changelog "python/python-importlib-metadata/spkgbuild" "Upgraded from version 4.11.3 to version 4.11.4"
+sed -i -e "s/version=2.8.1/version=2.8.2/g" python/python-networkx/spkgbuild
+changelog "python/python-networkx/spkgbuild" "Upgraded from version 2.8.1 to version 2.8.2"
+sed -i -e "s/version=17.7.0/version=18.0.0/g" python/python-novaclient/spkgbuild
+changelog "python/python-novaclient/spkgbuild" "Upgraded from version 17.7.0 to version 18.0.0"
+sed -i -e "s/version=2.5.0/version=2.6.0/g" python/python-osc-lib/spkgbuild
+changelog "python/python-osc-lib/spkgbuild" "Upgraded from version 2.5.0 to version 2.6.0"
+sed -i -e "s/version=22.1/version=22.1.1/g" python/python-pip/spkgbuild
+changelog "python/python-pip/spkgbuild" "Upgraded from version 22.1 to version 22.1.1"
+sed -i -e "s/version=5.9.0/version=5.9.1/g" python/python-psutil/spkgbuild
+changelog "python/python-psutil/spkgbuild" "Upgraded from version 5.9.0 to version 5.9.1"
+sed -i -e "s/version=2.3.0/version=2.3.1/g" python/python-spyder-kernels/spkgbuild
+changelog "python/python-spyder-kernels/spkgbuild" "Upgraded from version 2.3.0 to version 2.3.1"
+sed -i -e "s/version=3.13.1/version=4.0.0/g" python/python-swiftclient/spkgbuild
+changelog "python/python-swiftclient/spkgbuild" "Upgraded from version 3.13.1 to version 4.0.0"
+sed -i -e "s/version=2022.4.6/version=2022.5.5/g" server/homeassistant/spkgbuild
+changelog "server/homeassistant/spkgbuild" "Upgraded from version 2022.4.6 to version 2022.5.5"
