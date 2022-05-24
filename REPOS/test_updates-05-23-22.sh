3f5f8b63fe61d0ce9e919e01e2f469b70ec8263f
Voncloft <voncloft@gmail.com>
2022-05-23 20:09:41 -0400

    fixed vlc dont know why but it craps out about a compiler issue the way it was

---
+++ REPOS/test_updates-05-23-22.sh
@@ -0,0 +1,12 @@
+sed -i -e "s/version=3.3.11/version=3.3.12/g" extra/flexget/spkgbuild
+changelog "extra/flexget/spkgbuild" "Upgraded from version 3.3.11 to version 3.3.12"
+sed -i -e "s/version=1.4.0/version=1.4.1/g" libs/libxkbcommon/spkgbuild
+changelog "libs/libxkbcommon/spkgbuild" "Upgraded from version 1.4.0 to version 1.4.1"
+sed -i -e "s/version=5.1.2/version=5.2.0/g" python/python-flask-socketio/spkgbuild
+changelog "python/python-flask-socketio/spkgbuild" "Upgraded from version 5.1.2 to version 5.2.0"
+sed -i -e "s/version=1.5.3/version=1.5.4/g" python/python-typed-ast/spkgbuild
+changelog "python/python-typed-ast/spkgbuild" "Upgraded from version 1.5.3 to version 1.5.4"
+sed -i -e "s/version=1.11.0/version=1.11.1/g" python/python-xmlschema/spkgbuild
+changelog "python/python-xmlschema/spkgbuild" "Upgraded from version 1.11.0 to version 1.11.1"
+sed -i -e "s/version=1.0.3/version=1.0.4/g" xfce/xfce4-terminal/spkgbuild
+changelog "xfce/xfce4-terminal/spkgbuild" "Upgraded from version 1.0.3 to version 1.0.4"
--- REPOS/test_updates.sh
+++ REPOS/test_updates.sh
@@ -1,12 +1,34 @@
+sed -i -e "s/version=1.28/version=1.29/g" core/fakeroot/spkgbuild
+changelog "core/fakeroot/spkgbuild" "Upgraded from version 1.28 to version 1.29"
+sed -i -e "s/version=5.17.9/version=5.18/g" core/linux-desktop/spkgbuild
+changelog "core/linux-desktop/spkgbuild" "Upgraded from version 5.17.9 to version 5.18"
+sed -i -e "s/version=5.17.9/version=5.18/g" core/linux-router/spkgbuild
+changelog "core/linux-router/spkgbuild" "Upgraded from version 5.17.9 to version 5.18"
 sed -i -e "s/version=3.3.11/version=3.3.12/g" extra/flexget/spkgbuild
 changelog "extra/flexget/spkgbuild" "Upgraded from version 3.3.11 to version 3.3.12"
 sed -i -e "s/version=1.4.0/version=1.4.1/g" libs/libxkbcommon/spkgbuild
 changelog "libs/libxkbcommon/spkgbuild" "Upgraded from version 1.4.0 to version 1.4.1"
+sed -i -e "s/version=1.23.5/version=1.23.6/g" python/python-boto3/spkgbuild
+changelog "python/python-boto3/spkgbuild" "Upgraded from version 1.23.5 to version 1.23.6"
+sed -i -e "s/version=1.26.5/version=1.26.6/g" python/python-botocore/spkgbuild
+changelog "python/python-botocore/spkgbuild" "Upgraded from version 1.26.5 to version 1.26.6"
+sed -i -e "s/version=6.3.3/version=6.4/g" python/python-coverage/spkgbuild
+changelog "python/python-coverage/spkgbuild" "Upgraded from version 6.3.3 to version 6.4"
 sed -i -e "s/version=5.1.2/version=5.2.0/g" python/python-flask-socketio/spkgbuild
 changelog "python/python-flask-socketio/spkgbuild" "Upgraded from version 5.1.2 to version 5.2.0"
+sed -i -e "s/version=1.23.0/version=1.24.0/g" python/python-numpy/spkgbuild
+changelog "python/python-numpy/spkgbuild" "Upgraded from version 1.23.0 to version 1.24.0"
+sed -i -e "s/version=1.8/version=1.8.1/g" python/python-pypandoc/spkgbuild
+changelog "python/python-pypandoc/spkgbuild" "Upgraded from version 1.8 to version 1.8.1"
+sed -i -e "s/version=12.4.1/version=12.4.3/g" python/python-rich/spkgbuild
+changelog "python/python-rich/spkgbuild" "Upgraded from version 12.4.1 to version 12.4.3"
 sed -i -e "s/version=1.5.3/version=1.5.4/g" python/python-typed-ast/spkgbuild
 changelog "python/python-typed-ast/spkgbuild" "Upgraded from version 1.5.3 to version 1.5.4"
 sed -i -e "s/version=1.11.0/version=1.11.1/g" python/python-xmlschema/spkgbuild
 changelog "python/python-xmlschema/spkgbuild" "Upgraded from version 1.11.0 to version 1.11.1"
+sed -i -e "s/version=4.4.2/version=4.4.3/g" qt/qbittorrent/spkgbuild
+changelog "qt/qbittorrent/spkgbuild" "Upgraded from version 4.4.2 to version 4.4.3"
+sed -i -e "s/version=4.17.0/version=4.17.1/g" xfce/xfce4-panel/spkgbuild
+changelog "xfce/xfce4-panel/spkgbuild" "Upgraded from version 4.17.0 to version 4.17.1"
 sed -i -e "s/version=1.0.3/version=1.0.4/g" xfce/xfce4-terminal/spkgbuild
 changelog "xfce/xfce4-terminal/spkgbuild" "Upgraded from version 1.0.3 to version 1.0.4"
