dc262e3393bdb142aa45ae8388c009fb4256883a
Voncloft <voncloft@gmail.com>
2022-05-25 05:31:25 -0400

    Minor Updates

---
+++ logs/old-test-updates/2022/May/applied_updates-05-25-22.sh
@@ -0,0 +1,26 @@
+sed -i -e "s/version=3.3.12/version=3.3.13/g" extra/flexget/spkgbuild
+changelog "extra/flexget/spkgbuild" "Upgraded from version 3.3.12 to version 3.3.13"
+sed -i -e "s/version=0.9.26/version=0.9.27/g" python/python-asteval/spkgbuild
+changelog "python/python-asteval/spkgbuild" "Upgraded from version 0.9.26 to version 0.9.27"
+sed -i -e "s/version=1.23.6/version=1.23.7/g" python/python-boto3/spkgbuild
+changelog "python/python-boto3/spkgbuild" "Upgraded from version 1.23.6 to version 1.23.7"
+sed -i -e "s/version=1.26.6/version=1.26.7/g" python/python-botocore/spkgbuild
+changelog "python/python-botocore/spkgbuild" "Upgraded from version 1.26.6 to version 1.26.7"
+sed -i -e "s/version=1.4.4/version=1.5.0/g" python/python-ddt/spkgbuild
+changelog "python/python-ddt/spkgbuild" "Upgraded from version 1.4.4 to version 1.5.0"
+sed -i -e "s/version=2022.05.0/version=2022.05.1/g" python/python-distributed/spkgbuild
+changelog "python/python-distributed/spkgbuild" "Upgraded from version 2022.05.0 to version 2022.05.1"
+sed -i -e "s/version=8.2.0/version=8.2.1/g" python/python-elasticsearch/spkgbuild
+changelog "python/python-elasticsearch/spkgbuild" "Upgraded from version 8.2.0 to version 8.2.1"
+sed -i -e "s/version=1.1.0/version=1.1.1/g" python/python-pycapnp/spkgbuild
+changelog "python/python-pycapnp/spkgbuild" "Upgraded from version 1.1.0 to version 1.1.1"
+sed -i -e "s/version=1.9.1/version=1.9.2/g" python/python-pygit2/spkgbuild
+changelog "python/python-pygit2/spkgbuild" "Upgraded from version 1.9.1 to version 1.9.2"
+sed -i -e "s/version=4.0.1/version=4.0.2/g" python/python-pykeepass/spkgbuild
+changelog "python/python-pykeepass/spkgbuild" "Upgraded from version 4.0.1 to version 4.0.2"
+sed -i -e "s/version=0.20.0/version=0.21.0/g" python/python-pytest-httpx/spkgbuild
+changelog "python/python-pytest-httpx/spkgbuild" "Upgraded from version 0.20.0 to version 0.21.0"
+sed -i -e "s/version=12.4.3/version=12.4.4/g" python/python-rich/spkgbuild
+changelog "python/python-rich/spkgbuild" "Upgraded from version 12.4.3 to version 12.4.4"
+sed -i -e "s/version=1.21.6/version=1.22.0/g" server/nginx/spkgbuild
+changelog "server/nginx/spkgbuild" "Upgraded from version 1.21.6 to version 1.22.0"
