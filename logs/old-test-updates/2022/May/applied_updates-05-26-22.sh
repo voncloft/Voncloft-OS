eeb4c9c77a12cc5ad4a822dd39bb25368290e28c
Voncloft <voncloft@gmail.com>
2022-05-26 05:32:27 -0400

    Minor Updates

---
+++ logs/old-test-updates/2022/May/applied_updates-05-26-22.sh
@@ -0,0 +1,42 @@
+sed -i -e "s/version=14.0.3/version=14.0.4/g" compilers/clang/spkgbuild
+changelog "compilers/clang/spkgbuild" "Upgraded from version 14.0.3 to version 14.0.4"
+sed -i -e "s/version=3.23.1/version=3.23.2/g" compilers/cmake/spkgbuild
+changelog "compilers/cmake/spkgbuild" "Upgraded from version 3.23.1 to version 3.23.2"
+sed -i -e "s/version=14.0.3/version=14.0.4/g" compilers/lld/spkgbuild
+changelog "compilers/lld/spkgbuild" "Upgraded from version 14.0.3 to version 14.0.4"
+sed -i -e "s/version=14.0.3/version=14.0.4/g" compilers/llvm/spkgbuild
+changelog "compilers/llvm/spkgbuild" "Upgraded from version 14.0.3 to version 14.0.4"
+sed -i -e "s/version=5.17/version=5.18/g" core/btrfs-progs/spkgbuild
+changelog "core/btrfs-progs/spkgbuild" "Upgraded from version 5.17 to version 5.18"
+sed -i -e "s/version=3.19.0/version=3.20.1/g" core/logrotate/spkgbuild
+changelog "core/logrotate/spkgbuild" "Upgraded from version 3.19.0 to version 3.20.1"
+sed -i -e "s/version=4.1.1/version=4.2.0/g" core/qalculate/spkgbuild
+changelog "core/qalculate/spkgbuild" "Upgraded from version 4.1.1 to version 4.2.0"
+sed -i -e "s/version=3.3.13/version=3.3.14/g" extra/flexget/spkgbuild
+changelog "extra/flexget/spkgbuild" "Upgraded from version 3.3.13 to version 3.3.14"
+sed -i -e "s/version=14.0.3/version=14.0.4/g" multilib/llvm-32/spkgbuild
+changelog "multilib/llvm-32/spkgbuild" "Upgraded from version 14.0.3 to version 14.0.4"
+sed -i -e "s/version=0.11.2/version=0.12.0/g" libs/hidapi/spkgbuild
+changelog "libs/hidapi/spkgbuild" "Upgraded from version 0.11.2 to version 0.12.0"
+sed -i -e "s/version=2.6/version=2.7/g" libs/libbytesize/spkgbuild
+changelog "libs/libbytesize/spkgbuild" "Upgraded from version 2.6 to version 2.7"
+sed -i -e "s/version=4.1.1/version=4.2.0/g" libs/libqalculate/spkgbuild
+changelog "libs/libqalculate/spkgbuild" "Upgraded from version 4.1.1 to version 4.2.0"
+sed -i -e "s/version=1.23.7/version=1.23.8/g" python/python-boto3/spkgbuild
+changelog "python/python-boto3/spkgbuild" "Upgraded from version 1.23.7 to version 1.23.8"
+sed -i -e "s/version=1.26.7/version=1.26.8/g" python/python-botocore/spkgbuild
+changelog "python/python-botocore/spkgbuild" "Upgraded from version 1.26.7 to version 1.26.8"
+sed -i -e "s/version=13.11.1/version=13.12.0/g" python/python-faker/spkgbuild
+changelog "python/python-faker/spkgbuild" "Upgraded from version 13.11.1 to version 13.12.0"
+sed -i -e "s/version=6.46.7/version=6.46.9/g" python/python-hypothesis/spkgbuild
+changelog "python/python-hypothesis/spkgbuild" "Upgraded from version 6.46.7 to version 6.46.9"
+sed -i -e "s/version=1.0.3/version=1.0.4/g" python/python-msgpack/spkgbuild
+changelog "python/python-msgpack/spkgbuild" "Upgraded from version 1.0.3 to version 1.0.4"
+sed -i -e "s/version=0.20.0/version=0.21.0/g" python/python-responses/spkgbuild
+changelog "python/python-responses/spkgbuild" "Upgraded from version 0.20.0 to version 0.21.0"
+sed -i -e "s/version=1.0.0/version=1.1.1/g" python/python-rope/spkgbuild
+changelog "python/python-rope/spkgbuild" "Upgraded from version 1.0.0 to version 1.1.1"
+sed -i -e "s/version=4.4.3/version=4.4.3.1/g" qt/qbittorrent/spkgbuild
+changelog "qt/qbittorrent/spkgbuild" "Upgraded from version 4.4.3 to version 4.4.3.1"
+sed -i -e "s/version=2022.5.5/version=2022.6.0/g" server/homeassistant/spkgbuild
+changelog "server/homeassistant/spkgbuild" "Upgraded from version 2022.5.5 to version 2022.6.0"
