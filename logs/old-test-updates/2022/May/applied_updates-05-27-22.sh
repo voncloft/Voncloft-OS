8ab7d108888142949ca73d5ddfdd9df76b8a9dbb
Voncloft <voncloft@gmail.com>
2022-05-27 17:57:46 -0400

    Minor Updates

---
+++ logs/old-test-updates/2022/May/applied_updates-05-27-22.sh
@@ -0,0 +1,38 @@
+sed -i -e "s/version=2.3.3/version=2.3.6/g" core/cups/spkgbuild
+changelog "core/cups/spkgbuild" "Upgraded from version 2.3.3 to version 2.3.6"
+sed -i -e "s/version=3.7.5/version=3.7.6/g" core/gnutls/spkgbuild
+changelog "core/gnutls/spkgbuild" "Upgraded from version 3.7.5 to version 3.7.6"
+sed -i -e "s/version=5.17.0/version=5.18.0/g" core/iproute2/spkgbuild
+changelog "core/iproute2/spkgbuild" "Upgraded from version 5.17.0 to version 5.18.0"
+sed -i -e "s/version=5.16/version=5.19/g" core/iw/spkgbuild
+changelog "core/iw/spkgbuild" "Upgraded from version 5.16 to version 5.19"
+sed -i -e "s/version=1.79/version=1.80/g" core/os-prober/spkgbuild
+changelog "core/os-prober/spkgbuild" "Upgraded from version 1.79 to version 1.80"
+sed -i -e "s/version=2.5.0/version=2.5.1/g" core/qutebrowser/spkgbuild
+changelog "core/qutebrowser/spkgbuild" "Upgraded from version 2.5.0 to version 2.5.1"
+sed -i -e "s/version=4.33/version=4.34/g" extra/nspr/spkgbuild
+changelog "extra/nspr/spkgbuild" "Upgraded from version 4.33 to version 4.34"
+sed -i -e "s/version=3.20.1/version=21.1/g" extra/protobuf/spkgbuild
+changelog "extra/protobuf/spkgbuild" "Upgraded from version 3.20.1 to version 21.1"
+sed -i -e "s/version=3.7.2/version=3.7.3/g" networking/sshfs/spkgbuild
+changelog "networking/sshfs/spkgbuild" "Upgraded from version 3.7.2 to version 3.7.3"
+sed -i -e "s/version=1.23.8/version=1.23.10/g" python/python-boto3/spkgbuild
+changelog "python/python-boto3/spkgbuild" "Upgraded from version 1.23.8 to version 1.23.10"
+sed -i -e "s/version=1.26.8/version=1.26.10/g" python/python-botocore/spkgbuild
+changelog "python/python-botocore/spkgbuild" "Upgraded from version 1.26.8 to version 1.26.10"
+sed -i -e "s/version=0.12.19/version=0.12.21/g" python/python-bottle/spkgbuild
+changelog "python/python-bottle/spkgbuild" "Upgraded from version 0.12.19 to version 0.12.21"
+sed -i -e "s/version=2022.05.1/version=2022.05.2/g" python/python-distributed/spkgbuild
+changelog "python/python-distributed/spkgbuild" "Upgraded from version 2022.05.1 to version 2022.05.2"
+sed -i -e "s/version=8.2.1/version=8.2.2/g" python/python-elasticsearch/spkgbuild
+changelog "python/python-elasticsearch/spkgbuild" "Upgraded from version 8.2.1 to version 8.2.2"
+sed -i -e "s/version=2.48.0/version=2.49.0/g" python/python-google-api-python-client/spkgbuild
+changelog "python/python-google-api-python-client/spkgbuild" "Upgraded from version 2.48.0 to version 2.49.0"
+sed -i -e "s/version=0.7.0/version=0.8.0/g" python/python-pytest-subtests/spkgbuild
+changelog "python/python-pytest-subtests/spkgbuild" "Upgraded from version 0.7.0 to version 0.8.0"
+sed -i -e "s/version=1.3.6/version=1.3.7/g" python/python-qt.py/spkgbuild
+changelog "python/python-qt.py/spkgbuild" "Upgraded from version 1.3.6 to version 1.3.7"
+sed -i -e "s/version=2.8.5/version=2.10.0/g" python/python-semantic-version/spkgbuild
+changelog "python/python-semantic-version/spkgbuild" "Upgraded from version 2.8.5 to version 2.10.0"
+sed -i -e "s/version=3.12.3/version=3.12.5/g" python/python-trimesh/spkgbuild
+changelog "python/python-trimesh/spkgbuild" "Upgraded from version 3.12.3 to version 3.12.5"
