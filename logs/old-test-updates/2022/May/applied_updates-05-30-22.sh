23e9be93f058d6700da6f8bf3090e67cb3b31e34
Voncloft <voncloft@gmail.com>
2022-05-30 10:43:19 -0400

    Minor Updates

---
+++ logs/old-test-updates/2022/May/applied_updates-05-30-22.sh
@@ -0,0 +1,26 @@
+sed -i -e "s/version=2.13/version=2.14/g" core/i3lock/spkgbuild
+changelog "core/i3lock/spkgbuild" "Upgraded from version 2.13 to version 2.14"
+sed -i -e "s/version=2.26/version=2.27/g" core/links/spkgbuild
+changelog "core/links/spkgbuild" "Upgraded from version 2.26 to version 2.27"
+sed -i -e "s/version=2.3.0/version=2.4.0/g" core/sysklogd/spkgbuild
+changelog "core/sysklogd/spkgbuild" "Upgraded from version 2.3.0 to version 2.4.0"
+sed -i -e "s/version=21.09.1/version=22.06.1/g" core/timeshift/spkgbuild
+changelog "core/timeshift/spkgbuild" "Upgraded from version 21.09.1 to version 22.06.1"
+sed -i -e "s/version=2.36.2/version=2.36.3/g" core/webkitgtk/spkgbuild
+changelog "core/webkitgtk/spkgbuild" "Upgraded from version 2.36.2 to version 2.36.3"
+sed -i -e "s/version=15.99.1/version=16.0/g" media/pulseaudio/spkgbuild
+changelog "media/pulseaudio/spkgbuild" "Upgraded from version 15.99.1 to version 16.0"
+sed -i -e "s/version=1.10.1/version=1.11.1/g" python/python-flask-caching/spkgbuild
+changelog "python/python-flask-caching/spkgbuild" "Upgraded from version 1.10.1 to version 1.11.1"
+sed -i -e "s/version=4.6.0/version=4.7.0/g" python/python-flask-httpauth/spkgbuild
+changelog "python/python-flask-httpauth/spkgbuild" "Upgraded from version 4.6.0 to version 4.7.0"
+sed -i -e "s/version=3.4.0/version=3.5.0/g" python/python-gitlab/spkgbuild
+changelog "python/python-gitlab/spkgbuild" "Upgraded from version 3.4.0 to version 3.5.0"
+sed -i -e "s/version=8.12.48/version=8.12.49/g" python/python-phonenumbers/spkgbuild
+changelog "python/python-phonenumbers/spkgbuild" "Upgraded from version 8.12.48 to version 8.12.49"
+sed -i -e "s/version=1.0.0/version=1.1.0/g" python/python-pipx/spkgbuild
+changelog "python/python-pipx/spkgbuild" "Upgraded from version 1.0.0 to version 1.1.0"
+sed -i -e "s/version=0.13.5/version=0.13.6/g" python/python-pybuilder/spkgbuild
+changelog "python/python-pybuilder/spkgbuild" "Upgraded from version 0.13.5 to version 0.13.6"
+sed -i -e "s/version=0.20.0/version=0.20.1/g" python/python-starlette/spkgbuild
+changelog "python/python-starlette/spkgbuild" "Upgraded from version 0.20.0 to version 0.20.1"
