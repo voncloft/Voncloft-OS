5ecb41176039df780899978563b24d449e503982
Voncloft <voncloft@gmail.com>
2022-05-31 06:43:32 -0400

    Minor Updates

---
+++ logs/old-test-updates/2022/May/applied_updates-05-31-22.sh
@@ -0,0 +1,18 @@
+sed -i -e "s/version=5.18/version=5.18.1/g" core/linux-desktop/spkgbuild
+changelog "core/linux-desktop/spkgbuild" "Upgraded from version 5.18 to version 5.18.1"
+sed -i -e "s/version=5.18/version=5.18.1/g" core/linux-router/spkgbuild
+changelog "core/linux-router/spkgbuild" "Upgraded from version 5.18 to version 5.18.1"
+sed -i -e "s/version=4.0.1/version=4.1.0/g" core/streamlink/spkgbuild
+changelog "core/streamlink/spkgbuild" "Upgraded from version 4.0.1 to version 4.1.0"
+sed -i -e "s/version=0.4.1602/version=0.4.1612/g" networking/you-get/spkgbuild
+changelog "networking/you-get/spkgbuild" "Upgraded from version 0.4.1602 to version 0.4.1612"
+sed -i -e "s/version=5.1.0/version=5.2.0/g" python/python-cachetools/spkgbuild
+changelog "python/python-cachetools/spkgbuild" "Upgraded from version 5.1.0 to version 5.2.0"
+sed -i -e "s/version=0.5.5/version=0.6.0/g" python/python-databases/spkgbuild
+changelog "python/python-databases/spkgbuild" "Upgraded from version 0.5.5 to version 0.6.0"
+sed -i -e "s/version=2.5.2/version=2.5.3/g" python/python-elementpath/spkgbuild
+changelog "python/python-elementpath/spkgbuild" "Upgraded from version 2.5.2 to version 2.5.3"
+sed -i -e "s/version=4.8.0/version=4.9.0/g" python/python-lxml/spkgbuild
+changelog "python/python-lxml/spkgbuild" "Upgraded from version 4.8.0 to version 4.9.0"
+sed -i -e "s/version=3.0.2/version=3.1/g" python/python-qdarkstyle/spkgbuild
+changelog "python/python-qdarkstyle/spkgbuild" "Upgraded from version 3.0.2 to version 3.1"
