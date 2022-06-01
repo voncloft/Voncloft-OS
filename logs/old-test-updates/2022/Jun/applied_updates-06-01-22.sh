5e2f7848fcfe0ec362d0e33ea7b296025a2379ac
Voncloft <voncloft@gmail.com>
2022-06-01 17:41:52 -0400

    Minor Updates

--- logs/old-test-updates/2022/Jun/applied_updates-06-01-22.sh
+++ logs/old-test-updates/2022/Jun/applied_updates-06-01-22.sh
@@ -14,3 +14,37 @@ sed -i -e "s/version=2.5.6/version=2.5.7/g" networking/openvpn/spkgbuild
 changelog "networking/openvpn/spkgbuild" "Upgraded from version 2.5.6 to version 2.5.7"
 sed -i -e "s/version=1.2.6/version=1.2.7/g" core/alsa-utils/spkgbuild
 changelog "core/alsa-utils/spkgbuild" "Upgraded from version 1.2.6 to version 1.2.7"
+sed -i -e "s/version=0.100.0/version=0.100.1/g" core/hugo/spkgbuild
+changelog "core/hugo/spkgbuild" "Upgraded from version 0.100.0 to version 0.100.1"
+sed -i -e "s/version=3.2a/version=3.3/g" core/tmux/spkgbuild
+changelog "core/tmux/spkgbuild" "Upgraded from version 3.2a to version 3.3"
+sed -i -e "s/version=22.05.0/version=22.06.0/g" libs/poppler/spkgbuild
+changelog "libs/poppler/spkgbuild" "Upgraded from version 22.05.0 to version 22.06.0"
+sed -i -e "s/version=102.0b1/version=102.0b2/g" networking/firefox/spkgbuild
+changelog "networking/firefox/spkgbuild" "Upgraded from version 102.0b1 to version 102.0b2"
+sed -i -e "s/version=22.5.1/version=22.5.2/g" python/python-awesomeversion/spkgbuild
+changelog "python/python-awesomeversion/spkgbuild" "Upgraded from version 22.5.1 to version 22.5.2"
+sed -i -e "s/version=1.23.10/version=1.24.1/g" python/python-boto3/spkgbuild
+changelog "python/python-boto3/spkgbuild" "Upgraded from version 1.23.10 to version 1.24.1"
+sed -i -e "s/version=1.26.10/version=1.27.1/g" python/python-botocore/spkgbuild
+changelog "python/python-botocore/spkgbuild" "Upgraded from version 1.26.10 to version 1.27.1"
+sed -i -e "s/version=0.60.1/version=0.61.0/g" python/python-cfn-lint/spkgbuild
+changelog "python/python-cfn-lint/spkgbuild" "Upgraded from version 0.60.1 to version 0.61.0"
+sed -i -e "s/version=3.7.0/version=3.7.1/g" python/python-filelock/spkgbuild
+changelog "python/python-filelock/spkgbuild" "Upgraded from version 3.7.0 to version 3.7.1"
+sed -i -e "s/version=6.46.9/version=6.46.10/g" python/python-hypothesis/spkgbuild
+changelog "python/python-hypothesis/spkgbuild" "Upgraded from version 6.46.9 to version 6.46.10"
+sed -i -e "s/version=4.5.1/version=4.6.0/g" python/python-jsonschema/spkgbuild
+changelog "python/python-jsonschema/spkgbuild" "Upgraded from version 4.5.1 to version 4.6.0"
+sed -i -e "s/version=3.6.8/version=3.6.9/g" python/python-orjson/spkgbuild
+changelog "python/python-orjson/spkgbuild" "Upgraded from version 3.6.8 to version 3.6.9"
+sed -i -e "s/version=22.1.1/version=22.1.2/g" python/python-pip/spkgbuild
+changelog "python/python-pip/spkgbuild" "Upgraded from version 22.1.1 to version 22.1.2"
+sed -i -e "s/version=0.5.2/version=0.6.0/g" python/python-s3transfer/spkgbuild
+changelog "python/python-s3transfer/spkgbuild" "Upgraded from version 0.5.2 to version 0.6.0"
+sed -i -e "s/version=2.1.1/version=2.1.2/g" python/python-waitress/spkgbuild
+changelog "python/python-waitress/spkgbuild" "Upgraded from version 2.1.1 to version 2.1.2"
+sed -i -e "s/version=2.3.14/version=2.3.15/g" ruby-gems/ruby-bundler/spkgbuild
+changelog "ruby-gems/ruby-bundler/spkgbuild" "Upgraded from version 2.3.14 to version 2.3.15"
+sed -i -e "s/version=0.9.27/version=0.9.28/g" ruby-gems/ruby-yard/spkgbuild
+changelog "ruby-gems/ruby-yard/spkgbuild" "Upgraded from version 0.9.27 to version 0.9.28"
