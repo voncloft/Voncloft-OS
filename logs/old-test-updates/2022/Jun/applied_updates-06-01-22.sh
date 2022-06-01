196929df7508ead68e92e17d935bc42756b370a5
Voncloft <voncloft@gmail.com>
2022-06-01 08:20:33 -0400

    Minor Updates

--- logs/old-test-updates/2022/Jun/applied_updates-06-01-22.sh
+++ logs/old-test-updates/2022/Jun/applied_updates-06-01-22.sh
@@ -12,3 +12,5 @@ sed -i -e "s/version=1.0.2/version=1.0.3/g" networking/nftables/spkgbuild
 changelog "networking/nftables/spkgbuild" "Upgraded from version 1.0.2 to version 1.0.3"
 sed -i -e "s/version=2.5.6/version=2.5.7/g" networking/openvpn/spkgbuild
 changelog "networking/openvpn/spkgbuild" "Upgraded from version 2.5.6 to version 2.5.7"
+sed -i -e "s/version=1.2.6/version=1.2.7/g" core/alsa-utils/spkgbuild
+changelog "core/alsa-utils/spkgbuild" "Upgraded from version 1.2.6 to version 1.2.7"
