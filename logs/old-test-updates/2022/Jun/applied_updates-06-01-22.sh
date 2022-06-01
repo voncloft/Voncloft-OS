2f6b94332f3da7b584b0299d013f33307dc657ff
Voncloft <voncloft@gmail.com>
2022-06-01 04:57:03 -0400

    Minor Updates

---
+++ logs/old-test-updates/2022/Jun/applied_updates-06-01-22.sh
@@ -0,0 +1,14 @@
+sed -i -e "s/version=1.2.6/version=1.2.7/g" core/alsa-utils/spkgbuild
+changelog "core/alsa-utils/spkgbuild" "Upgraded from version 1.2.6 to version 1.2.7"
+sed -i -e "s/version=0.99.1/version=0.100.0/g" core/hugo/spkgbuild
+changelog "core/hugo/spkgbuild" "Upgraded from version 0.99.1 to version 0.100.0"
+sed -i -e "s/version=3.78/version=3.79/g" core/nss/spkgbuild
+changelog "core/nss/spkgbuild" "Upgraded from version 3.78 to version 3.79"
+sed -i -e "s/version=1.2.6.1/version=1.2.7/g" libs/alsa-lib/spkgbuild
+changelog "libs/alsa-lib/spkgbuild" "Upgraded from version 1.2.6.1 to version 1.2.7"
+sed -i -e "s/version=101.0b9/version=102.0b1/g" networking/firefox/spkgbuild
+changelog "networking/firefox/spkgbuild" "Upgraded from version 101.0b9 to version 102.0b1"
+sed -i -e "s/version=1.0.2/version=1.0.3/g" networking/nftables/spkgbuild
+changelog "networking/nftables/spkgbuild" "Upgraded from version 1.0.2 to version 1.0.3"
+sed -i -e "s/version=2.5.6/version=2.5.7/g" networking/openvpn/spkgbuild
+changelog "networking/openvpn/spkgbuild" "Upgraded from version 2.5.6 to version 2.5.7"
