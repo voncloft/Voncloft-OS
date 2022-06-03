46cc749376e89a1ba3331933f45e3db307ac6a8c
Voncloft <voncloft@gmail.com>
2022-06-03 08:43:54 -0400

    Minor Updates

---
+++ logs/old-test-updates/2022/Jun/applied_updates-06-03-22.sh
@@ -0,0 +1,26 @@
+sed -i -e "s/version=0.62.1/version=0.62.2/g" compilers/meson/spkgbuild
+changelog "compilers/meson/spkgbuild" "Upgraded from version 0.62.1 to version 0.62.2"
+sed -i -e "s/version=6.1.2/version=6.1.3/g" core/mercurial/spkgbuild
+changelog "core/mercurial/spkgbuild" "Upgraded from version 6.1.2 to version 6.1.3"
+sed -i -e "s/version=3.3.14/version=3.3.15/g" extra/flexget/spkgbuild
+changelog "extra/flexget/spkgbuild" "Upgraded from version 3.3.14 to version 3.3.15"
+sed -i -e "s/version=3.7.3/version=3.8/g" libs/nettle/spkgbuild
+changelog "libs/nettle/spkgbuild" "Upgraded from version 3.7.3 to version 3.8"
+sed -i -e "s/version=101.0b4/version=102.0b1/g" networking/thunderbird/spkgbuild
+changelog "networking/thunderbird/spkgbuild" "Upgraded from version 101.0b4 to version 102.0b1"
+sed -i -e "s/version=1.24.1/version=1.24.2/g" python/python-boto3/spkgbuild
+changelog "python/python-boto3/spkgbuild" "Upgraded from version 1.24.1 to version 1.24.2"
+sed -i -e "s/version=1.27.1/version=1.27.2/g" python/python-botocore/spkgbuild
+changelog "python/python-botocore/spkgbuild" "Upgraded from version 1.27.1 to version 1.27.2"
+sed -i -e "s/version=6.4/version=6.4.1/g" python/python-coverage/spkgbuild
+changelog "python/python-coverage/spkgbuild" "Upgraded from version 6.4 to version 6.4.1"
+sed -i -e "s/version=6.46.10/version=6.46.11/g" python/python-hypothesis/spkgbuild
+changelog "python/python-hypothesis/spkgbuild" "Upgraded from version 6.46.10 to version 6.46.11"
+sed -i -e "s/version=3.0.0/version=3.0.1/g" python/python-internetarchive/spkgbuild
+changelog "python/python-internetarchive/spkgbuild" "Upgraded from version 3.0.0 to version 3.0.1"
+sed -i -e "s/version=1.12.1/version=1.12.2/g" python/python-param/spkgbuild
+changelog "python/python-param/spkgbuild" "Upgraded from version 1.12.1 to version 1.12.2"
+sed -i -e "s/version=0.2.0/version=0.2.1/g" python/python-tubes/spkgbuild
+changelog "python/python-tubes/spkgbuild" "Upgraded from version 0.2.0 to version 0.2.1"
+sed -i -e "s/version=2.35.1/version=2.36/g" xorg/xkeyboard-config/spkgbuild
+changelog "xorg/xkeyboard-config/spkgbuild" "Upgraded from version 2.35.1 to version 2.36"
