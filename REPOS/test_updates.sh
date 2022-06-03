0e1062c395c15e09553cd7474573682b8588f4ad
Voncloft <voncloft@gmail.com>
2022-06-03 16:02:09 -0400

    fixed solaar crap

---
+++ REPOS/test_updates.sh
@@ -0,0 +1,18 @@
+sed -i -e "s/version=3.10.2/version=3.10.3/g" core/geos/spkgbuild
+changelog "core/geos/spkgbuild" "Upgraded from version 3.10.2 to version 3.10.3"
+sed -i -e "s/version=2.4.110/version=2.4.111/g" libs/libdrm/spkgbuild
+changelog "libs/libdrm/spkgbuild" "Upgraded from version 2.4.110 to version 2.4.111"
+sed -i -e "s/version=102.0b2/version=102.0b3/g" networking/firefox/spkgbuild
+changelog "networking/firefox/spkgbuild" "Upgraded from version 102.0b2 to version 102.0b3"
+sed -i -e "s/version=2.14.9/version=2.14.10/g" networking/pidgin/spkgbuild
+changelog "networking/pidgin/spkgbuild" "Upgraded from version 2.14.9 to version 2.14.10"
+sed -i -e "s/version=102.0b1/version=102.0b2/g" networking/thunderbird/spkgbuild
+changelog "networking/thunderbird/spkgbuild" "Upgraded from version 102.0b1 to version 102.0b2"
+sed -i -e "s/version=3.6.9/version=3.7.0/g" python/python-orjson/spkgbuild
+changelog "python/python-orjson/spkgbuild" "Upgraded from version 3.6.9 to version 3.7.0"
+sed -i -e "s/version=12.0.0/version=12.0.1/g" python/python-pdoc/spkgbuild
+changelog "python/python-pdoc/spkgbuild" "Upgraded from version 12.0.0 to version 12.0.1"
+sed -i -e "s/version=2022.6.0/version=2022.6.1/g" server/homeassistant/spkgbuild
+changelog "server/homeassistant/spkgbuild" "Upgraded from version 2022.6.0 to version 2022.6.1"
+sed -i -e "s/version=5.16.0/version=5.18.0/g" xfce/xfsprogs/spkgbuild
+changelog "xfce/xfsprogs/spkgbuild" "Upgraded from version 5.16.0 to version 5.18.0"
