82ca314f46f6337cb16f1e75e09f3984dc58e2c9
Voncloft <voncloft@gmail.com>
2022-06-06 05:34:42 -0400

    Minor Updates

---
+++ logs/old-test-updates/2022/Jun/applied_updates-06-06-22.sh
@@ -0,0 +1,8 @@
+sed -i -e "s/version=4.0.39/version=4.0.40/g" core/mtools/spkgbuild
+changelog "core/mtools/spkgbuild" "Upgraded from version 4.0.39 to version 4.0.40"
+sed -i -e "s/version=5.3.0/version=5.3.1/g" python/python-qtconsole/spkgbuild
+changelog "python/python-qtconsole/spkgbuild" "Upgraded from version 5.3.0 to version 5.3.1"
+sed -i -e "s/version=0.19.0/version=0.20.0/g" python/python-validators/spkgbuild
+changelog "python/python-validators/spkgbuild" "Upgraded from version 0.19.0 to version 0.20.0"
+sed -i -e "s/version=2022.6.1/version=2022.6.2/g" server/homeassistant/spkgbuild
+changelog "server/homeassistant/spkgbuild" "Upgraded from version 2022.6.1 to version 2022.6.2"
