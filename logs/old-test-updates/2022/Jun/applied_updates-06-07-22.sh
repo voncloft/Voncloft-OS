b4c86cc92207a84eb424d35974c50d197ef6afc9
Voncloft <voncloft@gmail.com>
2022-06-07 07:18:25 -0400

    Minor Updates

---
+++ logs/old-test-updates/2022/Jun/applied_updates-06-07-22.sh
@@ -0,0 +1,16 @@
+sed -i -e "s/version=5.18/version=5.18.1/g" core/btrfs-progs/spkgbuild
+changelog "core/btrfs-progs/spkgbuild" "Upgraded from version 5.18 to version 5.18.1"
+sed -i -e "s/version=5.18.1/version=5.18.2/g" core/linux-desktop/spkgbuild
+changelog "core/linux-desktop/spkgbuild" "Upgraded from version 5.18.1 to version 5.18.2"
+sed -i -e "s/version=5.18.1/version=5.18.2/g" core/linux-router/spkgbuild
+changelog "core/linux-router/spkgbuild" "Upgraded from version 5.18.1 to version 5.18.2"
+sed -i -e "s/version=3.1.6/version=3.1.7/g" core/wxgtk3/spkgbuild
+changelog "core/wxgtk3/spkgbuild" "Upgraded from version 3.1.6 to version 3.1.7"
+sed -i -e "s/version=102.0b3/version=102.0b4/g" networking/firefox/spkgbuild
+changelog "networking/firefox/spkgbuild" "Upgraded from version 102.0b3 to version 102.0b4"
+sed -i -e "s/version=13.12.0/version=13.12.1/g" python/python-faker/spkgbuild
+changelog "python/python-faker/spkgbuild" "Upgraded from version 13.12.0 to version 13.12.1"
+sed -i -e "s/version=6.13.0/version=6.13.1/g" python/python-ipykernel/spkgbuild
+changelog "python/python-ipykernel/spkgbuild" "Upgraded from version 6.13.0 to version 6.13.1"
+sed -i -e "s/version=0.25.0/version=0.26.0/g" python/python-owslib/spkgbuild
+changelog "python/python-owslib/spkgbuild" "Upgraded from version 0.25.0 to version 0.26.0"
