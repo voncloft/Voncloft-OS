6442c2cdb56dc810ba5e6288f2a6b2a947a73d48
Voncloft <voncloft@gmail.com>
2022-06-08 19:54:12 -0400

    Minor Updates

---
+++ REPOS/test_updates-06-08-22.sh
@@ -0,0 +1,34 @@
+sed -i -e "s/version=0.23.0/version=0.24.1/g" core/cbindgen/spkgbuild
+changelog "core/cbindgen/spkgbuild" "Upgraded from version 0.23.0 to version 0.24.1"
+sed -i -e "s/version=3.22.1.0/version=3.22.6.0/g" extra/inotify-tools/spkgbuild
+changelog "extra/inotify-tools/spkgbuild" "Upgraded from version 3.22.1.0 to version 3.22.6.0"
+sed -i -e "s/version=1.2.1/version=1.2.2/g" networking/libnftnl/spkgbuild
+changelog "networking/libnftnl/spkgbuild" "Upgraded from version 1.2.1 to version 1.2.2"
+sed -i -e "s/version=1.0.3/version=1.0.4/g" networking/nftables/spkgbuild
+changelog "networking/nftables/spkgbuild" "Upgraded from version 1.0.3 to version 1.0.4"
+sed -i -e "s/version=102.0b2/version=102.0b3/g" networking/thunderbird/spkgbuild
+changelog "networking/thunderbird/spkgbuild" "Upgraded from version 102.0b2 to version 102.0b3"
+sed -i -e "s/version=1.45.0/version=1.46.0/g" python/python-aws-sam-translator/spkgbuild
+changelog "python/python-aws-sam-translator/spkgbuild" "Upgraded from version 1.45.0 to version 1.46.0"
+sed -i -e "s/version=1.24.2/version=1.24.4/g" python/python-boto3/spkgbuild
+changelog "python/python-boto3/spkgbuild" "Upgraded from version 1.24.2 to version 1.24.4"
+sed -i -e "s/version=1.27.2/version=1.27.4/g" python/python-botocore/spkgbuild
+changelog "python/python-botocore/spkgbuild" "Upgraded from version 1.27.2 to version 1.27.4"
+sed -i -e "s/version=0.50.0/version=0.51.0/g" python/python-django-allauth/spkgbuild
+changelog "python/python-django-allauth/spkgbuild" "Upgraded from version 0.50.0 to version 0.51.0"
+sed -i -e "s/version=2.49.0/version=2.50.0/g" python/python-google-api-python-client/spkgbuild
+changelog "python/python-google-api-python-client/spkgbuild" "Upgraded from version 2.49.0 to version 2.50.0"
+sed -i -e "s/version=6.46.11/version=6.47.0/g" python/python-hypothesis/spkgbuild
+changelog "python/python-hypothesis/spkgbuild" "Upgraded from version 6.46.11 to version 6.47.0"
+sed -i -e "s/version=23.6.0/version=24.1.0/g" python/python-kubernetes/spkgbuild
+changelog "python/python-kubernetes/spkgbuild" "Upgraded from version 23.6.0 to version 24.1.0"
+sed -i -e "s/version=0.17.2/version=0.18.0/g" python/python-myst-parser/spkgbuild
+changelog "python/python-myst-parser/spkgbuild" "Upgraded from version 0.17.2 to version 0.18.0"
+sed -i -e "s/version=3.7.1/version=3.7.2/g" python/python-orjson/spkgbuild
+changelog "python/python-orjson/spkgbuild" "Upgraded from version 3.7.1 to version 3.7.2"
+sed -i -e "s/version=2022.5.2/version=2022.6.7/g" python/python-pipenv/spkgbuild
+changelog "python/python-pipenv/spkgbuild" "Upgraded from version 2022.5.2 to version 2022.6.7"
+sed -i -e "s/version=62.3.2/version=62.3.3/g" python/python-setuptools/spkgbuild
+changelog "python/python-setuptools/spkgbuild" "Upgraded from version 62.3.2 to version 62.3.3"
+sed -i -e "s/version=2022.6.2/version=2022.6.3/g" server/homeassistant/spkgbuild
+changelog "server/homeassistant/spkgbuild" "Upgraded from version 2022.6.2 to version 2022.6.3"
---
+++ logs/old-test-updates/2022/Jun/applied_updates-06-08-22.sh
@@ -0,0 +1,58 @@
+sed -i -e "s/version=0.23.0/version=0.24.2/g" core/cbindgen/spkgbuild
+changelog "core/cbindgen/spkgbuild" "Upgraded from version 0.23.0 to version 0.24.2"
+sed -i -e "s/version=0.100.1/version=0.100.2/g" core/hugo/spkgbuild
+changelog "core/hugo/spkgbuild" "Upgraded from version 0.100.1 to version 0.100.2"
+sed -i -e "s/version=7.0.0/version=7.0.1/g" core/redis/spkgbuild
+changelog "core/redis/spkgbuild" "Upgraded from version 7.0.0 to version 7.0.1"
+sed -i -e "s/version=3.22.1.0/version=3.22.6.0/g" extra/inotify-tools/spkgbuild
+changelog "extra/inotify-tools/spkgbuild" "Upgraded from version 3.22.1.0 to version 3.22.6.0"
+sed -i -e "s/version=102.0b4/version=102.0b5/g" networking/firefox/spkgbuild
+changelog "networking/firefox/spkgbuild" "Upgraded from version 102.0b4 to version 102.0b5"
+sed -i -e "s/version=1.2.1/version=1.2.2/g" networking/libnftnl/spkgbuild
+changelog "networking/libnftnl/spkgbuild" "Upgraded from version 1.2.1 to version 1.2.2"
+sed -i -e "s/version=1.0.3/version=1.0.4/g" networking/nftables/spkgbuild
+changelog "networking/nftables/spkgbuild" "Upgraded from version 1.0.3 to version 1.0.4"
+sed -i -e "s/version=102.0b2/version=102.0b3/g" networking/thunderbird/spkgbuild
+changelog "networking/thunderbird/spkgbuild" "Upgraded from version 102.0b2 to version 102.0b3"
+sed -i -e "s/version=2.3.2/version=2.3.3/g" python/python-aiobotocore/spkgbuild
+changelog "python/python-aiobotocore/spkgbuild" "Upgraded from version 2.3.2 to version 2.3.3"
+sed -i -e "s/version=1.45.0/version=1.46.0/g" python/python-aws-sam-translator/spkgbuild
+changelog "python/python-aws-sam-translator/spkgbuild" "Upgraded from version 1.45.0 to version 1.46.0"
+sed -i -e "s/version=1.24.2/version=1.24.4/g" python/python-boto3/spkgbuild
+changelog "python/python-boto3/spkgbuild" "Upgraded from version 1.24.2 to version 1.24.4"
+sed -i -e "s/version=1.27.2/version=1.27.4/g" python/python-botocore/spkgbuild
+changelog "python/python-botocore/spkgbuild" "Upgraded from version 1.27.2 to version 1.27.4"
+sed -i -e "s/version=1.6.1/version=1.6.2/g" python/python-canonicaljson/spkgbuild
+changelog "python/python-canonicaljson/spkgbuild" "Upgraded from version 1.6.1 to version 1.6.2"
+sed -i -e "s/version=0.50.0/version=0.51.0/g" python/python-django-allauth/spkgbuild
+changelog "python/python-django-allauth/spkgbuild" "Upgraded from version 0.50.0 to version 0.51.0"
+sed -i -e "s/version=2.49.0/version=2.50.0/g" python/python-google-api-python-client/spkgbuild
+changelog "python/python-google-api-python-client/spkgbuild" "Upgraded from version 2.49.0 to version 2.50.0"
+sed -i -e "s/version=2.6.6/version=2.7.0/g" python/python-google-auth/spkgbuild
+changelog "python/python-google-auth/spkgbuild" "Upgraded from version 2.6.6 to version 2.7.0"
+sed -i -e "s/version=6.46.11/version=6.47.0/g" python/python-hypothesis/spkgbuild
+changelog "python/python-hypothesis/spkgbuild" "Upgraded from version 6.46.11 to version 6.47.0"
+sed -i -e "s/version=23.6.0/version=24.1.0/g" python/python-kubernetes/spkgbuild
+changelog "python/python-kubernetes/spkgbuild" "Upgraded from version 23.6.0 to version 24.1.0"
+sed -i -e "s/version=0.17.2/version=0.18.0/g" python/python-myst-parser/spkgbuild
+changelog "python/python-myst-parser/spkgbuild" "Upgraded from version 0.17.2 to version 0.18.0"
+sed -i -e "s/version=1.3.1/version=1.4.0/g" python/python-numpydoc/spkgbuild
+changelog "python/python-numpydoc/spkgbuild" "Upgraded from version 1.3.1 to version 1.4.0"
+sed -i -e "s/version=3.7.1/version=3.7.2/g" python/python-orjson/spkgbuild
+changelog "python/python-orjson/spkgbuild" "Upgraded from version 3.7.1 to version 3.7.2"
+sed -i -e "s/version=12.0.1/version=12.0.2/g" python/python-pdoc/spkgbuild
+changelog "python/python-pdoc/spkgbuild" "Upgraded from version 12.0.1 to version 12.0.2"
+sed -i -e "s/version=2022.5.2/version=2022.6.7/g" python/python-pipenv/spkgbuild
+changelog "python/python-pipenv/spkgbuild" "Upgraded from version 2022.5.2 to version 2022.6.7"
+sed -i -e "s/version=7.3.2204242212/version=7.3.2206081204/g" python/python-pycontracts/spkgbuild
+changelog "python/python-pycontracts/spkgbuild" "Upgraded from version 7.3.2204242212 to version 7.3.2206081204"
+sed -i -e "s/version=62.3.2/version=62.3.3/g" python/python-setuptools/spkgbuild
+changelog "python/python-setuptools/spkgbuild" "Upgraded from version 62.3.2 to version 62.3.3"
+sed -i -e "s/version=0.20.1/version=0.20.2/g" python/python-starlette/spkgbuild
+changelog "python/python-starlette/spkgbuild" "Upgraded from version 0.20.1 to version 0.20.2"
+sed -i -e "s/version=2.8.0/version=2.8.2/g" python/python-tensorflow-serving-api/spkgbuild
+changelog "python/python-tensorflow-serving-api/spkgbuild" "Upgraded from version 2.8.0 to version 2.8.2"
+sed -i -e "s/version=2022.6.2/version=2022.6.4/g" server/homeassistant/spkgbuild
+changelog "server/homeassistant/spkgbuild" "Upgraded from version 2022.6.2 to version 2022.6.4"
+sed -i -e "s/version=2.4.53/version=2.4.54/g" server/httpd/spkgbuild
+changelog "server/httpd/spkgbuild" "Upgraded from version 2.4.53 to version 2.4.54"
