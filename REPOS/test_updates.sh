8f11cbf3660cd2a58797c8d0c25e818e2bb01571
Voncloft <voncloft@gmail.com>
2022-06-10 15:22:28 -0400

    added patches to thunderbird and firefox for cbindgen-0.24.2 not sure if its a temporary problem or permanent

---
+++ REPOS/test_updates.sh
@@ -0,0 +1,32 @@
+sed -i -e "s/version=5.2.7/version=5.4.0/g" cinnamon/cinnamon/spkgbuild
+changelog "cinnamon/cinnamon/spkgbuild" "Upgraded from version 5.2.7 to version 5.4.0"
+sed -i -e "s/version=5.2.1/version=5.4.0/g" cinnamon/cinnamon-control-center/spkgbuild
+changelog "cinnamon/cinnamon-control-center/spkgbuild" "Upgraded from version 5.2.1 to version 5.4.0"
+sed -i -e "s/version=5.2.1/version=5.4.0/g" cinnamon/cinnamon-desktop/spkgbuild
+changelog "cinnamon/cinnamon-desktop/spkgbuild" "Upgraded from version 5.2.1 to version 5.4.0"
+sed -i -e "s/version=5.2.0/version=5.4.0/g" cinnamon/cinnamon-menus/spkgbuild
+changelog "cinnamon/cinnamon-menus/spkgbuild" "Upgraded from version 5.2.0 to version 5.4.0"
+sed -i -e "s/version=5.2.1/version=5.4.0/g" cinnamon/cinnamon-screensaver/spkgbuild
+changelog "cinnamon/cinnamon-screensaver/spkgbuild" "Upgraded from version 5.2.1 to version 5.4.0"
+sed -i -e "s/version=5.2.0/version=5.4.0/g" cinnamon/cinnamon-session/spkgbuild
+changelog "cinnamon/cinnamon-session/spkgbuild" "Upgraded from version 5.2.0 to version 5.4.0"
+sed -i -e "s/version=5.2.0/version=5.4.0/g" cinnamon/cinnamon-settings-daemon/spkgbuild
+changelog "cinnamon/cinnamon-settings-daemon/spkgbuild" "Upgraded from version 5.2.0 to version 5.4.0"
+sed -i -e "s/version=5.2.2/version=5.4.0/g" cinnamon/cinnamon-translations/spkgbuild
+changelog "cinnamon/cinnamon-translations/spkgbuild" "Upgraded from version 5.2.2 to version 5.4.0"
+sed -i -e "s/version=5.2.0/version=5.4.0/g" cinnamon/cjs/spkgbuild
+changelog "cinnamon/cjs/spkgbuild" "Upgraded from version 5.2.0 to version 5.4.0"
+sed -i -e "s/version=5.2.1/version=5.4.0/g" cinnamon/muffin/spkgbuild
+changelog "cinnamon/muffin/spkgbuild" "Upgraded from version 5.2.1 to version 5.4.0"
+sed -i -e "s/version=5.2.4/version=5.4.0/g" cinnamon/nemo/spkgbuild
+changelog "cinnamon/nemo/spkgbuild" "Upgraded from version 5.2.4 to version 5.4.0"
+sed -i -e "s/version=2.2.4/version=2.2.5/g" core/blueman/spkgbuild
+changelog "core/blueman/spkgbuild" "Upgraded from version 2.2.4 to version 2.2.5"
+sed -i -e "s/version=0.24.2/version=0.24.3/g" core/cbindgen/spkgbuild
+changelog "core/cbindgen/spkgbuild" "Upgraded from version 0.24.2 to version 0.24.3"
+sed -i -e "s/version=5.41/version=5.42/g" core/file/spkgbuild
+changelog "core/file/spkgbuild" "Upgraded from version 5.41 to version 5.42"
+sed -i -e "s/version=2.2.9/version=2.2.10/g" libs/xapp/spkgbuild
+changelog "libs/xapp/spkgbuild" "Upgraded from version 2.2.9 to version 2.2.10"
+sed -i -e "s/version=102.0b5/version=102.0b6/g" networking/firefox/spkgbuild
+changelog "networking/firefox/spkgbuild" "Upgraded from version 102.0b5 to version 102.0b6"
