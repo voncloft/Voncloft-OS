446cd6c13be0764e9548a2834dfe5f5d12873f31
Voncloft <voncloft@gmail.com>
2022-06-13 19:09:39 -0400

    Minor Updates

---
+++ logs/old-test-updates/2022/Jun/applied_updates-06-13-22.sh
@@ -0,0 +1,22 @@
+sed -i -e "s/version=1.3.4/version=1.3.5/g" core/vsv/spkgbuild
+changelog "core/vsv/spkgbuild" "Upgraded from version 1.3.4 to version 1.3.5"
+sed -i -e "s/version=3.3.16/version=3.3.17/g" extra/flexget/spkgbuild
+changelog "extra/flexget/spkgbuild" "Upgraded from version 3.3.16 to version 3.3.17"
+sed -i -e "s/version=2.10.30/version=2.10.32/g" extra/gimp/spkgbuild
+changelog "extra/gimp/spkgbuild" "Upgraded from version 2.10.30 to version 2.10.32"
+sed -i -e "s/version=102.0b6/version=102.0b7/g" networking/firefox/spkgbuild
+changelog "networking/firefox/spkgbuild" "Upgraded from version 102.0b6 to version 102.0b7"
+sed -i -e "s/version=2.11.5/version=2.11.6/g" python/python-astroid/spkgbuild
+changelog "python/python-astroid/spkgbuild" "Upgraded from version 2.11.5 to version 2.11.6"
+sed -i -e "s/version=0.8.0/version=0.8.1/g" python/python-atom/spkgbuild
+changelog "python/python-atom/spkgbuild" "Upgraded from version 0.8.0 to version 0.8.1"
+sed -i -e "s/version=6.13.1/version=6.14.0/g" python/python-ipykernel/spkgbuild
+changelog "python/python-ipykernel/spkgbuild" "Upgraded from version 6.13.1 to version 6.14.0"
+sed -i -e "s/version=1.4.2/version=1.4.3/g" python/python-kiwisolver/spkgbuild
+changelog "python/python-kiwisolver/spkgbuild" "Upgraded from version 1.4.2 to version 1.4.3"
+sed -i -e "s/version=0.4.3/version=0.4.4/g" python/python-libcst/spkgbuild
+changelog "python/python-libcst/spkgbuild" "Upgraded from version 0.4.3 to version 0.4.4"
+sed -i -e "s/version=62.3.4/version=62.4.0/g" python/python-setuptools/spkgbuild
+changelog "python/python-setuptools/spkgbuild" "Upgraded from version 62.3.4 to version 62.4.0"
+sed -i -e "s/version=4.16.1/version=4.16.2/g" server/samba/spkgbuild
+changelog "server/samba/spkgbuild" "Upgraded from version 4.16.1 to version 4.16.2"
