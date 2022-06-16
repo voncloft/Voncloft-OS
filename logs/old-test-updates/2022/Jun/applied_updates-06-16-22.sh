0ed6dd1601cf3526b4699c4a74e53dc5eeb65f3d
Voncloft <voncloft@gmail.com>
2022-06-16 16:01:14 -0400

    Minor Updates

---
+++ logs/old-test-updates/2022/Jun/applied_updates-06-16-22.sh
@@ -0,0 +1,62 @@
+sed -i -e "s/version=0.100.2/version=0.101.0/g" core/hugo/spkgbuild
+changelog "core/hugo/spkgbuild" "Upgraded from version 0.100.2 to version 0.101.0"
+sed -i -e "s/version=5.18.4/version=5.18.5/g" core/linux-desktop/spkgbuild
+changelog "core/linux-desktop/spkgbuild" "Upgraded from version 5.18.4 to version 5.18.5"
+sed -i -e "s/version=5.18.4/version=5.18.5/g" core/linux-router/spkgbuild
+changelog "core/linux-router/spkgbuild" "Upgraded from version 5.18.4 to version 5.18.5"
+sed -i -e "s/version=6.1.3/version=6.1.4/g" core/mercurial/spkgbuild
+changelog "core/mercurial/spkgbuild" "Upgraded from version 6.1.3 to version 6.1.4"
+sed -i -e "s/version=1.20.2/version=1.20.3/g" media/gst-libav/spkgbuild
+changelog "media/gst-libav/spkgbuild" "Upgraded from version 1.20.2 to version 1.20.3"
+sed -i -e "s/version=1.20.2/version=1.20.3/g" media/gst-plugins-bad/spkgbuild
+changelog "media/gst-plugins-bad/spkgbuild" "Upgraded from version 1.20.2 to version 1.20.3"
+sed -i -e "s/version=1.20.2/version=1.20.3/g" media/gst-plugins-base/spkgbuild
+changelog "media/gst-plugins-base/spkgbuild" "Upgraded from version 1.20.2 to version 1.20.3"
+sed -i -e "s/version=1.20.2/version=1.20.3/g" media/gst-plugins-good/spkgbuild
+changelog "media/gst-plugins-good/spkgbuild" "Upgraded from version 1.20.2 to version 1.20.3"
+sed -i -e "s/version=1.20.2/version=1.20.3/g" media/gst-plugins-ugly/spkgbuild
+changelog "media/gst-plugins-ugly/spkgbuild" "Upgraded from version 1.20.2 to version 1.20.3"
+sed -i -e "s/version=1.20.2/version=1.20.3/g" media/gstreamer/spkgbuild
+changelog "media/gstreamer/spkgbuild" "Upgraded from version 1.20.2 to version 1.20.3"
+sed -i -e "s/version=1.8.38/version=1.8.40/g" libs/libnma/spkgbuild
+changelog "libs/libnma/spkgbuild" "Upgraded from version 1.8.38 to version 1.8.40"
+sed -i -e "s/version=2.2.10/version=2.2.11/g" libs/xapp/spkgbuild
+changelog "libs/xapp/spkgbuild" "Upgraded from version 2.2.10 to version 2.2.11"
+sed -i -e "s/version=9.19.1/version=9.19.2/g" networking/bind/spkgbuild
+changelog "networking/bind/spkgbuild" "Upgraded from version 9.19.1 to version 9.19.2"
+sed -i -e "s/version=102.0b7/version=102.0b8/g" networking/firefox/spkgbuild
+changelog "networking/firefox/spkgbuild" "Upgraded from version 102.0b7 to version 102.0b8"
+sed -i -e "s/version=102.0b5/version=102.0b6/g" networking/thunderbird/spkgbuild
+changelog "networking/thunderbird/spkgbuild" "Upgraded from version 102.0b5 to version 102.0b6"
+sed -i -e "s/version=5.24.0/version=5.25.0/g" plasma/kwayland-server/spkgbuild
+changelog "plasma/kwayland-server/spkgbuild" "Upgraded from version 5.24.0 to version 5.25.0"
+sed -i -e "s/version=2.10.2/version=2.10.3/g" python/python-babel/spkgbuild
+changelog "python/python-babel/spkgbuild" "Upgraded from version 2.10.2 to version 2.10.3"
+sed -i -e "s/version=1.24.8/version=1.24.10/g" python/python-boto3/spkgbuild
+changelog "python/python-boto3/spkgbuild" "Upgraded from version 1.24.8 to version 1.24.10"
+sed -i -e "s/version=1.27.8/version=1.27.10/g" python/python-botocore/spkgbuild
+changelog "python/python-botocore/spkgbuild" "Upgraded from version 1.27.8 to version 1.27.10"
+sed -i -e "s/version=0.4.4/version=0.4.5/g" python/python-colorama/spkgbuild
+changelog "python/python-colorama/spkgbuild" "Upgraded from version 0.4.4 to version 0.4.5"
+sed -i -e "s/version=11.31.0/version=11.32.0/g" python/python-dropbox/spkgbuild
+changelog "python/python-dropbox/spkgbuild" "Upgraded from version 11.31.0 to version 11.32.0"
+sed -i -e "s/version=2.7.0/version=2.8.0/g" python/python-google-auth/spkgbuild
+changelog "python/python-google-auth/spkgbuild" "Upgraded from version 2.7.0 to version 2.8.0"
+sed -i -e "s/version=6.47.2/version=6.47.3/g" python/python-hypothesis/spkgbuild
+changelog "python/python-hypothesis/spkgbuild" "Upgraded from version 6.47.2 to version 6.47.3"
+sed -i -e "s/version=0.1.7/version=0.2.0/g" python/python-ifaddr/spkgbuild
+changelog "python/python-ifaddr/spkgbuild" "Upgraded from version 0.1.7 to version 0.2.0"
+sed -i -e "s/version=3.0.1/version=3.0.2/g" python/python-internetarchive/spkgbuild
+changelog "python/python-internetarchive/spkgbuild" "Upgraded from version 3.0.1 to version 3.0.2"
+sed -i -e "s/version=6.14.0/version=6.15.0/g" python/python-ipykernel/spkgbuild
+changelog "python/python-ipykernel/spkgbuild" "Upgraded from version 6.14.0 to version 6.15.0"
+sed -i -e "s/version=0.3.1/version=0.3.2/g" python/python-ipyxact/spkgbuild
+changelog "python/python-ipyxact/spkgbuild" "Upgraded from version 0.3.1 to version 0.3.2"
+sed -i -e "s/version=1.3.3/version=2.0.0/g" python/python-warlock/spkgbuild
+changelog "python/python-warlock/spkgbuild" "Upgraded from version 1.3.3 to version 2.0.0"
+sed -i -e "s/version=2.3.15/version=2.3.16/g" ruby-gems/ruby-bundler/spkgbuild
+changelog "ruby-gems/ruby-bundler/spkgbuild" "Upgraded from version 2.3.15 to version 2.3.16"
+sed -i -e "s/version=2022.6.5/version=2022.6.6/g" server/homeassistant/spkgbuild
+changelog "server/homeassistant/spkgbuild" "Upgraded from version 2022.6.5 to version 2022.6.6"
+sed -i -e "s/version=1.15/version=1.15.1/g" xorg/xcb-proto/spkgbuild
+changelog "xorg/xcb-proto/spkgbuild" "Upgraded from version 1.15 to version 1.15.1"
