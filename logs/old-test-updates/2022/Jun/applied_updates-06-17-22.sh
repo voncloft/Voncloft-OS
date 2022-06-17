927af899aed5e36fc4d7ea32511231ba38d3078e
Voncloft <voncloft@gmail.com>
2022-06-17 19:30:45 -0400

    Minor Updates

---
+++ logs/old-test-updates/2022/Jun/applied_updates-06-17-22.sh
@@ -0,0 +1,38 @@
+sed -i -e "s/version=1.2.6/version=1.2.7.1/g" core/alsa-plugins/spkgbuild
+changelog "core/alsa-plugins/spkgbuild" "Upgraded from version 1.2.6 to version 1.2.7.1"
+sed -i -e "s/version=1.2.7/version=1.2.7.1/g" libs/alsa-lib/spkgbuild
+changelog "libs/alsa-lib/spkgbuild" "Upgraded from version 1.2.7 to version 1.2.7.1"
+sed -i -e "s/version=102.0b8/version=102.0b9/g" networking/firefox/spkgbuild
+changelog "networking/firefox/spkgbuild" "Upgraded from version 102.0b8 to version 102.0b9"
+sed -i -e "s/version=0.4.7.7/version=0.4.7.8/g" networking/tor/spkgbuild
+changelog "networking/tor/spkgbuild" "Upgraded from version 0.4.7.7 to version 0.4.7.8"
+sed -i -e "s/version=1.24.10/version=1.24.11/g" python/python-boto3/spkgbuild
+changelog "python/python-boto3/spkgbuild" "Upgraded from version 1.24.10 to version 1.24.11"
+sed -i -e "s/version=1.27.10/version=1.27.11/g" python/python-botocore/spkgbuild
+changelog "python/python-botocore/spkgbuild" "Upgraded from version 1.27.10 to version 1.27.11"
+sed -i -e "s/version=4.5.0/version=4.5.1/g" python/python-designateclient/spkgbuild
+changelog "python/python-designateclient/spkgbuild" "Upgraded from version 4.5.0 to version 4.5.1"
+sed -i -e "s/version=1.0.0/version=1.0.1/g" python/python-jmespath/spkgbuild
+changelog "python/python-jmespath/spkgbuild" "Upgraded from version 1.0.0 to version 1.0.1"
+sed -i -e "s/version=0.4.4/version=0.4.5/g" python/python-libcst/spkgbuild
+changelog "python/python-libcst/spkgbuild" "Upgraded from version 0.4.4 to version 0.4.5"
+sed -i -e "s/version=4.13.0/version=5.0.0/g" python/python-oslo-utils/spkgbuild
+changelog "python/python-oslo-utils/spkgbuild" "Upgraded from version 4.13.0 to version 5.0.0"
+sed -i -e "s/version=3.14.10/version=3.15.0/g" python/python-peewee/spkgbuild
+changelog "python/python-peewee/spkgbuild" "Upgraded from version 3.14.10 to version 3.15.0"
+sed -i -e "s/version=6.1/version=6.2/g" python/python-svg.path/spkgbuild
+changelog "python/python-svg.path/spkgbuild" "Upgraded from version 6.1 to version 6.2"
+sed -i -e "s/version=2.0.0/version=2.0.1/g" python/python-warlock/spkgbuild
+changelog "python/python-warlock/spkgbuild" "Upgraded from version 2.0.0 to version 2.0.1"
+sed -i -e "s/version=5.15.4/version=5.15.5/g" qt/qt5/spkgbuild
+changelog "qt/qt5/spkgbuild" "Upgraded from version 5.15.4 to version 5.15.5"
+sed -i -e "s/version=5.15.4/version=5.15.5/g" qt/qt5-multimedia/spkgbuild
+changelog "qt/qt5-multimedia/spkgbuild" "Upgraded from version 5.15.4 to version 5.15.5"
+sed -i -e "s/version=5.15.4/version=5.15.5/g" qt/qt5-speech/spkgbuild
+changelog "qt/qt5-speech/spkgbuild" "Upgraded from version 5.15.4 to version 5.15.5"
+sed -i -e "s/version=5.15.4/version=5.15.5/g" qt/qt5-x11extras/spkgbuild
+changelog "qt/qt5-x11extras/spkgbuild" "Upgraded from version 5.15.4 to version 5.15.5"
+sed -i -e "s/version=5.15.4/version=5.15.5/g" qt/qtsql/spkgbuild
+changelog "qt/qtsql/spkgbuild" "Upgraded from version 5.15.4 to version 5.15.5"
+sed -i -e "s/version=1.15.1/version=1.15.2/g" xorg/xcb-proto/spkgbuild
+changelog "xorg/xcb-proto/spkgbuild" "Upgraded from version 1.15.1 to version 1.15.2"
