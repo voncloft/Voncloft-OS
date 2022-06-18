27c0344d12e1d907a92c9ad26732a5bc428047e9
Voncloft <voncloft@gmail.com>
2022-06-18 12:51:56 -0400

    Minor Updates

---
+++ logs/old-test-updates/2022/Jun/applied_updates-06-18-22.sh
@@ -0,0 +1,8 @@
+sed -i -e "s/version=102.0b6/version=102.0b7/g" networking/thunderbird/spkgbuild
+changelog "networking/thunderbird/spkgbuild" "Upgraded from version 102.0b6 to version 102.0b7"
+sed -i -e "s/version=1.24.11/version=1.24.12/g" python/python-boto3/spkgbuild
+changelog "python/python-boto3/spkgbuild" "Upgraded from version 1.24.11 to version 1.24.12"
+sed -i -e "s/version=1.27.11/version=1.27.12/g" python/python-botocore/spkgbuild
+changelog "python/python-botocore/spkgbuild" "Upgraded from version 1.27.11 to version 1.27.12"
+sed -i -e "s/version=62.4.0/version=62.5.0/g" python/python-setuptools/spkgbuild
+changelog "python/python-setuptools/spkgbuild" "Upgraded from version 62.4.0 to version 62.5.0"
