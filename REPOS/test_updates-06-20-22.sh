a52ee37ea8c6b649af13aa9ec5a3d7987d6323a0
Voncloft <voncloft@gmail.com>
2022-06-21 21:14:39 -0400

    Minor Updates

---
+++ REPOS/test_updates-06-20-22.sh
@@ -0,0 +1,24 @@
+sed -i -e "s/version=5.4.0/version=5.4.1/g" cinnamon/cinnamon/spkgbuild
+changelog "cinnamon/cinnamon/spkgbuild" "Upgraded from version 5.4.0 to version 5.4.1"
+sed -i -e "s/version=5.4.0/version=5.4.1/g" cinnamon/cinnamon-control-center/spkgbuild
+changelog "cinnamon/cinnamon-control-center/spkgbuild" "Upgraded from version 5.4.0 to version 5.4.1"
+sed -i -e "s/version=5.4.0/version=5.4.1/g" cinnamon/muffin/spkgbuild
+changelog "cinnamon/muffin/spkgbuild" "Upgraded from version 5.4.0 to version 5.4.1"
+sed -i -e "s/version=0.62.2/version=0.63.0/g" compilers/meson/spkgbuild
+changelog "compilers/meson/spkgbuild" "Upgraded from version 0.62.2 to version 0.63.0"
+sed -i -e "s/version=2.2.0/version=2.3.0/g" libs/libwacom/spkgbuild
+changelog "libs/libwacom/spkgbuild" "Upgraded from version 2.2.0 to version 2.3.0"
+sed -i -e "s/version=1.5/version=2.0/g" libs/sbc/spkgbuild
+changelog "libs/sbc/spkgbuild" "Upgraded from version 1.5 to version 2.0"
+sed -i -e "s/version=1.24.12/version=1.24.13/g" python/python-boto3/spkgbuild
+changelog "python/python-boto3/spkgbuild" "Upgraded from version 1.24.12 to version 1.24.13"
+sed -i -e "s/version=1.27.12/version=1.27.13/g" python/python-botocore/spkgbuild
+changelog "python/python-botocore/spkgbuild" "Upgraded from version 1.27.12 to version 1.27.13"
+sed -i -e "s/version=4.33.1/version=4.34.0/g" python/python-breathe/spkgbuild
+changelog "python/python-breathe/spkgbuild" "Upgraded from version 4.33.1 to version 4.34.0"
+sed -i -e "s/version=62.5.0/version=62.6.0/g" python/python-setuptools/spkgbuild
+changelog "python/python-setuptools/spkgbuild" "Upgraded from version 62.5.0 to version 62.6.0"
+sed -i -e "s/version=3.1.6/version=3.1.7/g" python/python-uncertainties/spkgbuild
+changelog "python/python-uncertainties/spkgbuild" "Upgraded from version 3.1.6 to version 3.1.7"
+sed -i -e "s/version=4.17.1/version=4.17.2/g" xfce/tumbler/spkgbuild
+changelog "xfce/tumbler/spkgbuild" "Upgraded from version 4.17.1 to version 4.17.2"
