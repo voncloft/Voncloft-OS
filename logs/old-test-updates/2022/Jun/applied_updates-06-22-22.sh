70c0e579d60dde32fc8d409960093cb5371005fe
Voncloft <voncloft@gmail.com>
2022-06-22 18:40:12 -0400

    Minor Updates

---
+++ logs/old-test-updates/2022/Jun/applied_updates-06-22-22.sh
@@ -0,0 +1,36 @@
+sed -i -e "s/version=5.18.5/version=5.18.6/g" core/linux-desktop/spkgbuild
+changelog "core/linux-desktop/spkgbuild" "Upgraded from version 5.18.5 to version 5.18.6"
+sed -i -e "s/version=5.18.5/version=5.18.6/g" core/linux-router/spkgbuild
+changelog "core/linux-router/spkgbuild" "Upgraded from version 5.18.5 to version 5.18.6"
+sed -i -e "s/version=7.6.0/version=7.8.0/g" core/mlt/spkgbuild
+changelog "core/mlt/spkgbuild" "Upgraded from version 7.6.0 to version 7.8.0"
+sed -i -e "s/version=2.5.1/version=2.5.2/g" core/qutebrowser/spkgbuild
+changelog "core/qutebrowser/spkgbuild" "Upgraded from version 2.5.1 to version 2.5.2"
+sed -i -e "s/version=1.1.3rc3/version=1.1.4rc1/g" extra/solaar/spkgbuild
+changelog "extra/solaar/spkgbuild" "Upgraded from version 1.1.3rc3 to version 1.1.4rc1"
+sed -i -e "s/version=16.0/version=16.1/g" media/pulseaudio/spkgbuild
+changelog "media/pulseaudio/spkgbuild" "Upgraded from version 16.0 to version 16.1"
+sed -i -e "s/version=102.0b7/version=102.0b8/g" networking/thunderbird/spkgbuild
+changelog "networking/thunderbird/spkgbuild" "Upgraded from version 102.0b7 to version 102.0b8"
+sed -i -e "s/version=2022.05.18/version=2022.06.22.1/g" networking/yt-dlp/spkgbuild
+changelog "networking/yt-dlp/spkgbuild" "Upgraded from version 2022.05.18 to version 2022.06.22.1"
+sed -i -e "s/version=1.24.14/version=1.24.15/g" python/python-boto3/spkgbuild
+changelog "python/python-boto3/spkgbuild" "Upgraded from version 1.24.14 to version 1.24.15"
+sed -i -e "s/version=1.27.14/version=1.27.15/g" python/python-botocore/spkgbuild
+changelog "python/python-botocore/spkgbuild" "Upgraded from version 1.27.14 to version 1.27.15"
+sed -i -e "s/version=0.61.0/version=0.61.1/g" python/python-cfn-lint/spkgbuild
+changelog "python/python-cfn-lint/spkgbuild" "Upgraded from version 0.61.0 to version 0.61.1"
+sed -i -e "s/version=2.9.10/version=2.9.11/g" python/python-cloudflare/spkgbuild
+changelog "python/python-cloudflare/spkgbuild" "Upgraded from version 2.9.10 to version 2.9.11"
+sed -i -e "s/version=6.11.0/version=6.11.1/g" python/python-cx-freeze/spkgbuild
+changelog "python/python-cx-freeze/spkgbuild" "Upgraded from version 6.11.0 to version 6.11.1"
+sed -i -e "s/version=13.13.0/version=13.14.0/g" python/python-faker/spkgbuild
+changelog "python/python-faker/spkgbuild" "Upgraded from version 13.13.0 to version 13.14.0"
+sed -i -e "s/version=24.1.0/version=24.2.0/g" python/python-kubernetes/spkgbuild
+changelog "python/python-kubernetes/spkgbuild" "Upgraded from version 24.1.0 to version 24.2.0"
+sed -i -e "s/version=3.14.1/version=3.15.0/g" python/python-pycryptodome/spkgbuild
+changelog "python/python-pycryptodome/spkgbuild" "Upgraded from version 3.14.1 to version 3.15.0"
+sed -i -e "s/version=2022.06.04.1/version=2022.06.21/g" python/python-sphinx-furo/spkgbuild
+changelog "python/python-sphinx-furo/spkgbuild" "Upgraded from version 2022.06.04.1 to version 2022.06.21"
+sed -i -e "s/version=2022.6.6/version=2022.6.7/g" server/homeassistant/spkgbuild
+changelog "server/homeassistant/spkgbuild" "Upgraded from version 2022.6.6 to version 2022.6.7"
