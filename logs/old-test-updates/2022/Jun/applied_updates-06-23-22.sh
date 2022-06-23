9f3d0c56a75086d1c784016274d4897bf812bc01
Voncloft <voncloft@gmail.com>
2022-06-23 16:37:18 -0400

    Minor Updates

---
+++ logs/old-test-updates/2022/Jun/applied_updates-06-23-22.sh
@@ -0,0 +1,18 @@
+sed -i -e "s/version=1.10.0/version=1.12.0/g" core/gtest/spkgbuild
+changelog "core/gtest/spkgbuild" "Upgraded from version 1.10.0 to version 1.12.0"
+sed -i -e "s/version=3.79/version=3.80/g" core/nss/spkgbuild
+changelog "core/nss/spkgbuild" "Upgraded from version 3.79 to version 3.80"
+sed -i -e "s/version=2.3.3/version=2.3.4/g" python/python-aiobotocore/spkgbuild
+changelog "python/python-aiobotocore/spkgbuild" "Upgraded from version 2.3.3 to version 2.3.4"
+sed -i -e "s/version=6.47.3/version=6.47.4/g" python/python-hypothesis/spkgbuild
+changelog "python/python-hypothesis/spkgbuild" "Upgraded from version 6.47.3 to version 6.47.4"
+sed -i -e "s/version=1.3.1/version=1.3.3/g" python/python-mockito/spkgbuild
+changelog "python/python-mockito/spkgbuild" "Upgraded from version 1.3.1 to version 1.3.3"
+sed -i -e "s/version=3.7.2/version=3.7.3/g" python/python-orjson/spkgbuild
+changelog "python/python-orjson/spkgbuild" "Upgraded from version 3.7.2 to version 3.7.3"
+sed -i -e "s/version=5.0.0/version=6.0.0/g" python/python-oslo-utils/spkgbuild
+changelog "python/python-oslo-utils/spkgbuild" "Upgraded from version 5.0.0 to version 6.0.0"
+sed -i -e "s/version=1.2.1/version=1.3.0/g" python/python-pytest-enabler/spkgbuild
+changelog "python/python-pytest-enabler/spkgbuild" "Upgraded from version 1.2.1 to version 1.3.0"
+sed -i -e "s/version=5.15.9/version=5.15.10/g" qt/qtwebengine/spkgbuild
+changelog "qt/qtwebengine/spkgbuild" "Upgraded from version 5.15.9 to version 5.15.10"
