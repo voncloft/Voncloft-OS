0132532c9aef67ab45238f568e9a5f3f93e23883
Voncloft <voncloft@gmail.com>
2022-06-24 23:17:16 -0400

    Minor Updates

---
+++ logs/old-test-updates/2022/Jun/applied_updates-06-24-22.sh
@@ -0,0 +1,24 @@
+sed -i -e "s/version=1.2.0/version=1.2.1/g" core/speex/spkgbuild
+changelog "core/speex/spkgbuild" "Upgraded from version 1.2.0 to version 1.2.1"
+sed -i -e "s/version=3.3.17/version=3.3.18/g" extra/flexget/spkgbuild
+changelog "extra/flexget/spkgbuild" "Upgraded from version 3.3.17 to version 3.3.18"
+sed -i -e "s/version=21.1/version=21.2/g" extra/protobuf/spkgbuild
+changelog "extra/protobuf/spkgbuild" "Upgraded from version 21.1 to version 21.2"
+sed -i -e "s/version=1.30.6/version=1.30.8/g" libs/libqmi/spkgbuild
+changelog "libs/libqmi/spkgbuild" "Upgraded from version 1.30.6 to version 1.30.8"
+sed -i -e "s/version=1.2.0/version=1.2.1/g" libs/speexdsp/spkgbuild
+changelog "libs/speexdsp/spkgbuild" "Upgraded from version 1.2.0 to version 1.2.1"
+sed -i -e "s/version=1.24.15/version=1.24.17/g" python/python-boto3/spkgbuild
+changelog "python/python-boto3/spkgbuild" "Upgraded from version 1.24.15 to version 1.24.17"
+sed -i -e "s/version=1.27.15/version=1.27.17/g" python/python-botocore/spkgbuild
+changelog "python/python-botocore/spkgbuild" "Upgraded from version 1.27.15 to version 1.27.17"
+sed -i -e "s/version=2022.6.0/version=2022.6.1/g" python/python-distributed/spkgbuild
+changelog "python/python-distributed/spkgbuild" "Upgraded from version 2022.6.0 to version 2022.6.1"
+sed -i -e "s/version=1.11.2/version=1.12.4/g" python/python-manuel/spkgbuild
+changelog "python/python-manuel/spkgbuild" "Upgraded from version 1.11.2 to version 1.12.4"
+sed -i -e "s/version=3.7.0/version=3.8.1/g" python/python-pytest-mock/spkgbuild
+changelog "python/python-pytest-mock/spkgbuild" "Upgraded from version 3.7.0 to version 3.8.1"
+sed -i -e "s/version=1.11.2/version=1.11.3/g" python/python-xmlschema/spkgbuild
+changelog "python/python-xmlschema/spkgbuild" "Upgraded from version 1.11.2 to version 1.11.3"
+sed -i -e "s/version=5.4.0/version=5.5/g" python/python-zope-testrunner/spkgbuild
+changelog "python/python-zope-testrunner/spkgbuild" "Upgraded from version 5.4.0 to version 5.5"
