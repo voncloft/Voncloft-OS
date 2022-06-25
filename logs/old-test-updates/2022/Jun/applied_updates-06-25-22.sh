77b3407f0f0bfa2278884f64e0d7a738f1355650
Voncloft <voncloft@gmail.com>
2022-06-25 01:24:03 -0400

    added subsonic upgrade script

---
+++ logs/old-test-updates/2022/Jun/applied_updates-06-25-22.sh
@@ -0,0 +1,2 @@
+sed -i -e "s/version=1.27.0.5897/version=1.27.1.5916/g" server/plex-media-server/spkgbuild
+changelog "server/plex-media-server/spkgbuild" "Upgraded from version 1.27.0.5897 to version 1.27.1.5916"
