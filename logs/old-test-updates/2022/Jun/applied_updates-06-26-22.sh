95688be4a26a395c46b6eaeec0776e494188b228
Voncloft <voncloft@gmail.com>
2022-06-26 19:26:36 -0400

    Minor Updates

---
+++ logs/old-test-updates/2022/Jun/applied_updates-06-26-22.sh
@@ -0,0 +1,26 @@
+sed -i -e "s/version=14.0.5/version=14.0.6/g" compilers/clang/spkgbuild
+changelog "compilers/clang/spkgbuild" "Upgraded from version 14.0.5 to version 14.0.6"
+sed -i -e "s/version=14.0.5/version=14.0.6/g" compilers/lld/spkgbuild
+changelog "compilers/lld/spkgbuild" "Upgraded from version 14.0.5 to version 14.0.6"
+sed -i -e "s/version=14.0.5/version=14.0.6/g" compilers/llvm/spkgbuild
+changelog "compilers/llvm/spkgbuild" "Upgraded from version 14.0.5 to version 14.0.6"
+sed -i -e "s/version=5.18.6/version=5.18.7/g" core/linux-desktop/spkgbuild
+changelog "core/linux-desktop/spkgbuild" "Upgraded from version 5.18.6 to version 5.18.7"
+sed -i -e "s/version=5.18.6/version=5.18.7/g" core/linux-router/spkgbuild
+changelog "core/linux-router/spkgbuild" "Upgraded from version 5.18.6 to version 5.18.7"
+sed -i -e "s/version=1.29.3/version=1.30.0/g" media/mpg123/spkgbuild
+changelog "media/mpg123/spkgbuild" "Upgraded from version 1.29.3 to version 1.30.0"
+sed -i -e "s/version=14.0.5/version=14.0.6/g" multilib/llvm-32/spkgbuild
+changelog "multilib/llvm-32/spkgbuild" "Upgraded from version 14.0.5 to version 14.0.6"
+sed -i -e "s/version=1.40/version=1.41/g" libs/libidn/spkgbuild
+changelog "libs/libidn/spkgbuild" "Upgraded from version 1.40 to version 1.41"
+sed -i -e "s/version=6.47.4/version=6.47.5/g" python/python-hypothesis/spkgbuild
+changelog "python/python-hypothesis/spkgbuild" "Upgraded from version 6.47.4 to version 6.47.5"
+sed -i -e "s/version=4.11.4/version=4.12.0/g" python/python-importlib-metadata/spkgbuild
+changelog "python/python-importlib-metadata/spkgbuild" "Upgraded from version 4.11.4 to version 4.12.0"
+sed -i -e "s/version=1.6.0/version=1.7.0/g" python/python-nodeenv/spkgbuild
+changelog "python/python-nodeenv/spkgbuild" "Upgraded from version 1.6.0 to version 1.7.0"
+sed -i -e "s/version=7.3.2206081204/version=7.3.2206251328/g" python/python-pycontracts/spkgbuild
+changelog "python/python-pycontracts/spkgbuild" "Upgraded from version 7.3.2206081204 to version 7.3.2206251328"
+sed -i -e "s/version=20.14.2/version=20.15.0/g" python/python-virtualenv/spkgbuild
+changelog "python/python-virtualenv/spkgbuild" "Upgraded from version 20.14.2 to version 20.15.0"
