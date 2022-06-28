8424c05821560414ca34c4bf0a5a133da566dca7
Voncloft <voncloft@gmail.com>
2022-06-27 20:58:49 -0400

    Minor Updates

---
+++ logs/old-test-updates/2022/Jun/applied_updates-06-27-22.sh
@@ -0,0 +1,26 @@
+sed -i -e "s/version=5.4.0/version=5.4.1/g" cinnamon/cinnamon-translations/spkgbuild
+changelog "cinnamon/cinnamon-translations/spkgbuild" "Upgraded from version 5.4.0 to version 5.4.1"
+sed -i -e "s/version=7.83.1/version=7.84.0/g" core/curl/spkgbuild
+changelog "core/curl/spkgbuild" "Upgraded from version 7.83.1 to version 7.84.0"
+sed -i -e "s/version=1.8.1/version=1.9.0/g" core/dunst/spkgbuild
+changelog "core/dunst/spkgbuild" "Upgraded from version 1.8.1 to version 1.9.0"
+sed -i -e "s/version=4.10/version=4.11/g" extra/gnucash/spkgbuild
+changelog "extra/gnucash/spkgbuild" "Upgraded from version 4.10 to version 4.11"
+sed -i -e "s/version=1.22.1/version=1.22.2/g" media/openal/spkgbuild
+changelog "media/openal/spkgbuild" "Upgraded from version 1.22.1 to version 1.22.2"
+sed -i -e "s/version=2.2.11/version=2.2.12/g" libs/xapp/spkgbuild
+changelog "libs/xapp/spkgbuild" "Upgraded from version 2.2.11 to version 2.2.12"
+sed -i -e "s/version=102.0b9/version=102.0esr/g" networking/firefox/spkgbuild
+changelog "networking/firefox/spkgbuild" "Upgraded from version 102.0b9 to version 102.0esr"
+sed -i -e "s/version=2.36.1/version=2.37.0/g" networking/git/spkgbuild
+changelog "networking/git/spkgbuild" "Upgraded from version 2.36.1 to version 2.37.0"
+sed -i -e "s/version=22.5.2/version=22.6.0/g" python/python-awesomeversion/spkgbuild
+changelog "python/python-awesomeversion/spkgbuild" "Upgraded from version 22.5.2 to version 22.6.0"
+sed -i -e "s/version=1.24.17/version=1.24.18/g" python/python-boto3/spkgbuild
+changelog "python/python-boto3/spkgbuild" "Upgraded from version 1.24.17 to version 1.24.18"
+sed -i -e "s/version=1.27.17/version=1.27.18/g" python/python-botocore/spkgbuild
+changelog "python/python-botocore/spkgbuild" "Upgraded from version 1.27.17 to version 1.27.18"
+sed -i -e "s/version=1.11.1/version=2.0.0/g" python/python-flask-caching/spkgbuild
+changelog "python/python-flask-caching/spkgbuild" "Upgraded from version 1.11.1 to version 2.0.0"
+sed -i -e "s/version=6.47.5/version=6.48.0/g" python/python-hypothesis/spkgbuild
+changelog "python/python-hypothesis/spkgbuild" "Upgraded from version 6.47.5 to version 6.48.0"
