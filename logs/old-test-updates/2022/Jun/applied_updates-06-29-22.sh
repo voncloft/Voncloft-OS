1c304d68bf086629415193f712ce7a240f365b6b
Voncloft <voncloft@gmail.com>
2022-06-29 05:22:17 -0400

    Minor Updates

---
+++ logs/old-test-updates/2022/Jun/applied_updates-06-29-22.sh
@@ -0,0 +1,132 @@
+sed -i -e "s/version=2.2.5/version=2.3/g" core/blueman/spkgbuild
+changelog "core/blueman/spkgbuild" "Upgraded from version 2.2.5 to version 2.3"
+sed -i -e "s/version=2.14.0/version=2.15.0/g" libs/libva/spkgbuild
+changelog "libs/libva/spkgbuild" "Upgraded from version 2.14.0 to version 2.15.0"
+sed -i -e "s/version=102.0esr/version=103.0b1/g" networking/firefox/spkgbuild
+changelog "networking/firefox/spkgbuild" "Upgraded from version 102.0esr to version 103.0b1"
+sed -i -e "s/version=5.25.1/version=5.25.2/g" plasma/bluedevil/spkgbuild
+changelog "plasma/bluedevil/spkgbuild" "Upgraded from version 5.25.1 to version 5.25.2"
+sed -i -e "s/version=5.25.1/version=5.25.2/g" plasma/breeze/spkgbuild
+changelog "plasma/breeze/spkgbuild" "Upgraded from version 5.25.1 to version 5.25.2"
+sed -i -e "s/version=5.25.1/version=5.25.2/g" plasma/breeze-grub/spkgbuild
+changelog "plasma/breeze-grub/spkgbuild" "Upgraded from version 5.25.1 to version 5.25.2"
+sed -i -e "s/version=5.25.1/version=5.25.2/g" plasma/breeze-gtk/spkgbuild
+changelog "plasma/breeze-gtk/spkgbuild" "Upgraded from version 5.25.1 to version 5.25.2"
+sed -i -e "s/version=5.25.1/version=5.25.2/g" plasma/breeze-plymouth/spkgbuild
+changelog "plasma/breeze-plymouth/spkgbuild" "Upgraded from version 5.25.1 to version 5.25.2"
+sed -i -e "s/version=5.25.1/version=5.25.2/g" plasma/discover/spkgbuild
+changelog "plasma/discover/spkgbuild" "Upgraded from version 5.25.1 to version 5.25.2"
+sed -i -e "s/version=5.25.1/version=5.25.2/g" plasma/drkonqi/spkgbuild
+changelog "plasma/drkonqi/spkgbuild" "Upgraded from version 5.25.1 to version 5.25.2"
+sed -i -e "s/version=5.25.1/version=5.25.2/g" plasma/kactivitymanagerd/spkgbuild
+changelog "plasma/kactivitymanagerd/spkgbuild" "Upgraded from version 5.25.1 to version 5.25.2"
+sed -i -e "s/version=5.25.1/version=5.25.2/g" plasma/kde-cli-tools/spkgbuild
+changelog "plasma/kde-cli-tools/spkgbuild" "Upgraded from version 5.25.1 to version 5.25.2"
+sed -i -e "s/version=5.25.1/version=5.25.2/g" plasma/kde-gtk-config/spkgbuild
+changelog "plasma/kde-gtk-config/spkgbuild" "Upgraded from version 5.25.1 to version 5.25.2"
+sed -i -e "s/version=5.25.1/version=5.25.2/g" plasma/kdecoration/spkgbuild
+changelog "plasma/kdecoration/spkgbuild" "Upgraded from version 5.25.1 to version 5.25.2"
+sed -i -e "s/version=5.25.1/version=5.25.2/g" plasma/kdeplasma-addons/spkgbuild
+changelog "plasma/kdeplasma-addons/spkgbuild" "Upgraded from version 5.25.1 to version 5.25.2"
+sed -i -e "s/version=5.25.1/version=5.25.2/g" plasma/kgamma5/spkgbuild
+changelog "plasma/kgamma5/spkgbuild" "Upgraded from version 5.25.1 to version 5.25.2"
+sed -i -e "s/version=5.25.1/version=5.25.2/g" plasma/khotkeys/spkgbuild
+changelog "plasma/khotkeys/spkgbuild" "Upgraded from version 5.25.1 to version 5.25.2"
+sed -i -e "s/version=5.25.1/version=5.25.2/g" plasma/kinfocenter/spkgbuild
+changelog "plasma/kinfocenter/spkgbuild" "Upgraded from version 5.25.1 to version 5.25.2"
+sed -i -e "s/version=5.25.1/version=5.25.2/g" plasma/kmenuedit/spkgbuild
+changelog "plasma/kmenuedit/spkgbuild" "Upgraded from version 5.25.1 to version 5.25.2"
+sed -i -e "s/version=5.25.1/version=5.25.2/g" plasma/kscreen/spkgbuild
+changelog "plasma/kscreen/spkgbuild" "Upgraded from version 5.25.1 to version 5.25.2"
+sed -i -e "s/version=5.25.1/version=5.25.2/g" plasma/kscreenlocker/spkgbuild
+changelog "plasma/kscreenlocker/spkgbuild" "Upgraded from version 5.25.1 to version 5.25.2"
+sed -i -e "s/version=5.25.1/version=5.25.2/g" plasma/ksshaskpass/spkgbuild
+changelog "plasma/ksshaskpass/spkgbuild" "Upgraded from version 5.25.1 to version 5.25.2"
+sed -i -e "s/version=5.25.1/version=5.25.2/g" plasma/kwallet-pam/spkgbuild
+changelog "plasma/kwallet-pam/spkgbuild" "Upgraded from version 5.25.1 to version 5.25.2"
+sed -i -e "s/version=5.25.1/version=5.25.2/g" plasma/kwayland-integration/spkgbuild
+changelog "plasma/kwayland-integration/spkgbuild" "Upgraded from version 5.25.1 to version 5.25.2"
+sed -i -e "s/version=5.25.1/version=5.25.2/g" plasma/kwin/spkgbuild
+changelog "plasma/kwin/spkgbuild" "Upgraded from version 5.25.1 to version 5.25.2"
+sed -i -e "s/version=5.25.1/version=5.25.2/g" plasma/kwrited/spkgbuild
+changelog "plasma/kwrited/spkgbuild" "Upgraded from version 5.25.1 to version 5.25.2"
+sed -i -e "s/version=5.25.1/version=5.25.2/g" plasma/libkscreen/spkgbuild
+changelog "plasma/libkscreen/spkgbuild" "Upgraded from version 5.25.1 to version 5.25.2"
+sed -i -e "s/version=5.25.1/version=5.25.2/g" plasma/libksysguard/spkgbuild
+changelog "plasma/libksysguard/spkgbuild" "Upgraded from version 5.25.1 to version 5.25.2"
+sed -i -e "s/version=5.25.1/version=5.25.2/g" plasma/milou/spkgbuild
+changelog "plasma/milou/spkgbuild" "Upgraded from version 5.25.1 to version 5.25.2"
+sed -i -e "s/version=5.25.1/version=5.25.2/g" plasma/oxygen/spkgbuild
+changelog "plasma/oxygen/spkgbuild" "Upgraded from version 5.25.1 to version 5.25.2"
+sed -i -e "s/version=5.25.1/version=5.25.2/g" plasma/plasma-browser-integration/spkgbuild
+changelog "plasma/plasma-browser-integration/spkgbuild" "Upgraded from version 5.25.1 to version 5.25.2"
+sed -i -e "s/version=5.25.1/version=5.25.2/g" plasma/plasma-depends/spkgbuild
+changelog "plasma/plasma-depends/spkgbuild" "Upgraded from version 5.25.1 to version 5.25.2"
+sed -i -e "s/version=5.25.1/version=5.25.2/g" plasma/plasma-desktop/spkgbuild
+changelog "plasma/plasma-desktop/spkgbuild" "Upgraded from version 5.25.1 to version 5.25.2"
+sed -i -e "s/version=5.25.1/version=5.25.2/g" plasma/plasma-disks/spkgbuild
+changelog "plasma/plasma-disks/spkgbuild" "Upgraded from version 5.25.1 to version 5.25.2"
+sed -i -e "s/version=5.25.1/version=5.25.2/g" plasma/plasma-firewall/spkgbuild
+changelog "plasma/plasma-firewall/spkgbuild" "Upgraded from version 5.25.1 to version 5.25.2"
+sed -i -e "s/version=5.25.1/version=5.25.2/g" plasma/plasma-integration/spkgbuild
+changelog "plasma/plasma-integration/spkgbuild" "Upgraded from version 5.25.1 to version 5.25.2"
+sed -i -e "s/version=5.25.1/version=5.25.2/g" plasma/plasma-nano/spkgbuild
+changelog "plasma/plasma-nano/spkgbuild" "Upgraded from version 5.25.1 to version 5.25.2"
+sed -i -e "s/version=5.25.1/version=5.25.2/g" plasma/plasma-nm/spkgbuild
+changelog "plasma/plasma-nm/spkgbuild" "Upgraded from version 5.25.1 to version 5.25.2"
+sed -i -e "s/version=5.25.1/version=5.25.2/g" plasma/plasma-pa/spkgbuild
+changelog "plasma/plasma-pa/spkgbuild" "Upgraded from version 5.25.1 to version 5.25.2"
+sed -i -e "s/version=5.25.1/version=5.25.2/g" plasma/plasma-phone-components/spkgbuild
+changelog "plasma/plasma-phone-components/spkgbuild" "Upgraded from version 5.25.1 to version 5.25.2"
+sed -i -e "s/version=5.25.1/version=5.25.2/g" plasma/plasma-programs/spkgbuild
+changelog "plasma/plasma-programs/spkgbuild" "Upgraded from version 5.25.1 to version 5.25.2"
+sed -i -e "s/version=5.25.1/version=5.25.2/g" plasma/plasma-sdk/spkgbuild
+changelog "plasma/plasma-sdk/spkgbuild" "Upgraded from version 5.25.1 to version 5.25.2"
+sed -i -e "s/version=5.25.1/version=5.25.2/g" plasma/plasma-systemmonitor/spkgbuild
+changelog "plasma/plasma-systemmonitor/spkgbuild" "Upgraded from version 5.25.1 to version 5.25.2"
+sed -i -e "s/version=5.25.1/version=5.25.2/g" plasma/plasma-tests/spkgbuild
+changelog "plasma/plasma-tests/spkgbuild" "Upgraded from version 5.25.1 to version 5.25.2"
+sed -i -e "s/version=5.25.1/version=5.25.2/g" plasma/plasma-thunderbolt/spkgbuild
+changelog "plasma/plasma-thunderbolt/spkgbuild" "Upgraded from version 5.25.1 to version 5.25.2"
+sed -i -e "s/version=5.25.1/version=5.25.2/g" plasma/plasma-vault/spkgbuild
+changelog "plasma/plasma-vault/spkgbuild" "Upgraded from version 5.25.1 to version 5.25.2"
+sed -i -e "s/version=5.25.1/version=5.25.2/g" plasma/plasma-workspace/spkgbuild
+changelog "plasma/plasma-workspace/spkgbuild" "Upgraded from version 5.25.1 to version 5.25.2"
+sed -i -e "s/version=5.25.1/version=5.25.2/g" plasma/plasma-workspace-wallpapers/spkgbuild
+changelog "plasma/plasma-workspace-wallpapers/spkgbuild" "Upgraded from version 5.25.1 to version 5.25.2"
+sed -i -e "s/version=5.25.1/version=5.25.2/g" plasma/plymouth-kcm/spkgbuild
+changelog "plasma/plymouth-kcm/spkgbuild" "Upgraded from version 5.25.1 to version 5.25.2"
+sed -i -e "s/version=5.25.1/version=5.25.2/g" plasma/polkit-kde-agent-1/spkgbuild
+changelog "plasma/polkit-kde-agent-1/spkgbuild" "Upgraded from version 5.25.1 to version 5.25.2"
+sed -i -e "s/version=5.25.1/version=5.25.2/g" plasma/powerdevil/spkgbuild
+changelog "plasma/powerdevil/spkgbuild" "Upgraded from version 5.25.1 to version 5.25.2"
+sed -i -e "s/version=5.25.1/version=5.25.2/g" plasma/qqc2-breeze-style/spkgbuild
+changelog "plasma/qqc2-breeze-style/spkgbuild" "Upgraded from version 5.25.1 to version 5.25.2"
+sed -i -e "s/version=5.25.1/version=5.25.2/g" plasma/sddm-kcm/spkgbuild
+changelog "plasma/sddm-kcm/spkgbuild" "Upgraded from version 5.25.1 to version 5.25.2"
+sed -i -e "s/version=5.25.1/version=5.25.2/g" plasma/systemsettings/spkgbuild
+changelog "plasma/systemsettings/spkgbuild" "Upgraded from version 5.25.1 to version 5.25.2"
+sed -i -e "s/version=5.25.1/version=5.25.2/g" plasma/xdg-desktop-portal-kde/spkgbuild
+changelog "plasma/xdg-desktop-portal-kde/spkgbuild" "Upgraded from version 5.25.1 to version 5.25.2"
+sed -i -e "s/version=1.24.18/version=1.24.19/g" python/python-boto3/spkgbuild
+changelog "python/python-boto3/spkgbuild" "Upgraded from version 1.24.18 to version 1.24.19"
+sed -i -e "s/version=1.27.18/version=1.27.19/g" python/python-botocore/spkgbuild
+changelog "python/python-botocore/spkgbuild" "Upgraded from version 1.27.18 to version 1.27.19"
+sed -i -e "s/version=8.2.3/version=8.3.0/g" python/python-elasticsearch/spkgbuild
+changelog "python/python-elasticsearch/spkgbuild" "Upgraded from version 8.2.3 to version 8.3.0"
+sed -i -e "s/version=3.5.0/version=3.6.0/g" python/python-gitlab/spkgbuild
+changelog "python/python-gitlab/spkgbuild" "Upgraded from version 3.5.0 to version 3.6.0"
+sed -i -e "s/version=2.51.0/version=2.52.0/g" python/python-google-api-python-client/spkgbuild
+changelog "python/python-google-api-python-client/spkgbuild" "Upgraded from version 2.51.0 to version 2.52.0"
+sed -i -e "s/version=6.48.0/version=6.48.1/g" python/python-hypothesis/spkgbuild
+changelog "python/python-hypothesis/spkgbuild" "Upgraded from version 6.48.0 to version 6.48.1"
+sed -i -e "s/version=4.6.0/version=4.6.1/g" python/python-jsonschema/spkgbuild
+changelog "python/python-jsonschema/spkgbuild" "Upgraded from version 4.6.0 to version 4.6.1"
+sed -i -e "s/version=3.7.3/version=3.7.5/g" python/python-orjson/spkgbuild
+changelog "python/python-orjson/spkgbuild" "Upgraded from version 3.7.3 to version 3.7.5"
+sed -i -e "s/version=1.6.4/version=1.6.5/g" python/python-requirementslib/spkgbuild
+changelog "python/python-requirementslib/spkgbuild" "Upgraded from version 1.6.4 to version 1.6.5"
+sed -i -e "s/version=0.20.3/version=0.20.4/g" python/python-starlette/spkgbuild
+changelog "python/python-starlette/spkgbuild" "Upgraded from version 0.20.3 to version 0.20.4"
+sed -i -e "s/version=5.25.1/version=5.25.2/g" qt/layer-shell-qt/spkgbuild
+changelog "qt/layer-shell-qt/spkgbuild" "Upgraded from version 5.25.1 to version 5.25.2"
