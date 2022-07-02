79f7614bba73c25e5720185c382d28787e9de0e0
Voncloft <voncloft@gmail.com>
2022-07-02 17:49:23 -0400

    Minor Updates

---
+++ logs/old-test-updates/2022/Jul/applied_updates-07-02-22.sh
@@ -0,0 +1,20 @@
+sed -i -e "s/version=5.18.8/version=5.18.9/g" core/linux-desktop/spkgbuild
+changelog "core/linux-desktop/spkgbuild" "Upgraded from version 5.18.8 to version 5.18.9"
+sed -i -e "s/version=5.18.8/version=5.18.9/g" core/linux-router/spkgbuild
+changelog "core/linux-router/spkgbuild" "Upgraded from version 5.18.8 to version 5.18.9"
+sed -i -e "s/version=2.34.1/version=2.35/g" extra/discordchatexporter/spkgbuild
+changelog "extra/discordchatexporter/spkgbuild" "Upgraded from version 2.34.1 to version 2.35"
+sed -i -e "s/version=3.3.18/version=3.3.19/g" extra/flexget/spkgbuild
+changelog "extra/flexget/spkgbuild" "Upgraded from version 3.3.18 to version 3.3.19"
+sed -i -e "s/version=1.1.19/version=1.1.20/g" libs/libmtp/spkgbuild
+changelog "libs/libmtp/spkgbuild" "Upgraded from version 1.1.19 to version 1.1.20"
+sed -i -e "s/version=103.0b1/version=103.0b2/g" networking/thunderbird/spkgbuild
+changelog "networking/thunderbird/spkgbuild" "Upgraded from version 103.0b1 to version 103.0b2"
+sed -i -e "s/version=0.4.1612/version=0.4.1620/g" networking/you-get/spkgbuild
+changelog "networking/you-get/spkgbuild" "Upgraded from version 0.4.1612 to version 0.4.1620"
+sed -i -e "s/version=3.12.6/version=3.12.7/g" python/python-trimesh/spkgbuild
+changelog "python/python-trimesh/spkgbuild" "Upgraded from version 3.12.6 to version 3.12.7"
+sed -i -e "s/version=0.4.1/version=0.4.2/g" python/python-typer/spkgbuild
+changelog "python/python-typer/spkgbuild" "Upgraded from version 0.4.1 to version 0.4.2"
+sed -i -e "s/version=5.3.0/version=5.4.0/g" python/python-ujson/spkgbuild
+changelog "python/python-ujson/spkgbuild" "Upgraded from version 5.3.0 to version 5.4.0"
