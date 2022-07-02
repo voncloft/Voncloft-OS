5b7a199535548652b496fe5ba3379e211eafa28a
Voncloft <voncloft@gmail.com>
2022-07-01 20:08:57 -0400

    Minor Updates

---
+++ logs/old-test-updates/2022/Jul/applied_updates-07-01-22.sh
@@ -0,0 +1,40 @@
+sed -i -e "s/version=1.61.0/version=1.62.0/g" compilers/rust/spkgbuild
+changelog "compilers/rust/spkgbuild" "Upgraded from version 1.61.0 to version 1.62.0"
+sed -i -e "s/version=3.10.3/version=3.11.0/g" core/geos/spkgbuild
+changelog "core/geos/spkgbuild" "Upgraded from version 3.10.3 to version 3.11.0"
+sed -i -e "s/version=0.3.52/version=0.3.53/g" core/pipewire/spkgbuild
+changelog "core/pipewire/spkgbuild" "Upgraded from version 0.3.52 to version 0.3.53"
+sed -i -e "s/version=103.0b2/version=103.0b3/g" networking/firefox/spkgbuild
+changelog "networking/firefox/spkgbuild" "Upgraded from version 103.0b2 to version 103.0b3"
+sed -i -e "s/version=102.0b8/version=103.0b1/g" networking/thunderbird/spkgbuild
+changelog "networking/thunderbird/spkgbuild" "Upgraded from version 102.0b8 to version 103.0b1"
+sed -i -e "s/version=2.1.5/version=2.1.6/g" python/python-aresponses/spkgbuild
+changelog "python/python-aresponses/spkgbuild" "Upgraded from version 2.1.5 to version 2.1.6"
+sed -i -e "s/version=2.9.0/version=2.10.0/g" python/python-aws-xray-sdk/spkgbuild
+changelog "python/python-aws-xray-sdk/spkgbuild" "Upgraded from version 2.9.0 to version 2.10.0"
+sed -i -e "s/version=1.24.20/version=1.24.22/g" python/python-boto3/spkgbuild
+changelog "python/python-boto3/spkgbuild" "Upgraded from version 1.24.20 to version 1.24.22"
+sed -i -e "s/version=1.27.20/version=1.27.22/g" python/python-botocore/spkgbuild
+changelog "python/python-botocore/spkgbuild" "Upgraded from version 1.27.20 to version 1.27.22"
+sed -i -e "s/version=1.6.0/version=1.6.1/g" python/python-cftime/spkgbuild
+changelog "python/python-cftime/spkgbuild" "Upgraded from version 1.6.0 to version 1.6.1"
+sed -i -e "s/version=8.3.0/version=8.3.1/g" python/python-elasticsearch/spkgbuild
+changelog "python/python-elasticsearch/spkgbuild" "Upgraded from version 8.3.0 to version 8.3.1"
+sed -i -e "s/version=6.48.1/version=6.48.2/g" python/python-hypothesis/spkgbuild
+changelog "python/python-hypothesis/spkgbuild" "Upgraded from version 6.48.1 to version 6.48.2"
+sed -i -e "s/version=3.0.0/version=3.1.0/g" python/python-jsonlines/spkgbuild
+changelog "python/python-jsonlines/spkgbuild" "Upgraded from version 3.0.0 to version 3.1.0"
+sed -i -e "s/version=4.9.0/version=4.9.1/g" python/python-lxml/spkgbuild
+changelog "python/python-lxml/spkgbuild" "Upgraded from version 4.9.0 to version 4.9.1"
+sed -i -e "s/version=2.0.2/version=2.0.3/g" python/python-mistune/spkgbuild
+changelog "python/python-mistune/spkgbuild" "Upgraded from version 2.0.2 to version 2.0.3"
+sed -i -e "s/version=8.12.50/version=8.12.51/g" python/python-phonenumbers/spkgbuild
+changelog "python/python-phonenumbers/spkgbuild" "Upgraded from version 8.12.50 to version 8.12.51"
+sed -i -e "s/version=0.7.1/version=0.7.2/g" python/python-pip-shims/spkgbuild
+changelog "python/python-pip-shims/spkgbuild" "Upgraded from version 0.7.1 to version 0.7.2"
+sed -i -e "s/version=1.6.5/version=1.6.6/g" python/python-requirementslib/spkgbuild
+changelog "python/python-requirementslib/spkgbuild" "Upgraded from version 1.6.5 to version 1.6.6"
+sed -i -e "s/version=6.0.2/version=6.1.0/g" python/python-softlayer/spkgbuild
+changelog "python/python-softlayer/spkgbuild" "Upgraded from version 6.0.2 to version 6.1.0"
+sed -i -e "s/version=4.1.1/version=4.2.0/g" python/python-vcrpy/spkgbuild
+changelog "python/python-vcrpy/spkgbuild" "Upgraded from version 4.1.1 to version 4.2.0"
