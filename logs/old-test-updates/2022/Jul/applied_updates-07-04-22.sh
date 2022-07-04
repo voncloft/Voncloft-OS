8faeac66599e42a4498f1d44d0b60f1b081cda7a
Voncloft <voncloft@gmail.com>
2022-07-04 08:07:53 -0400

    Minor Updates

---
+++ logs/old-test-updates/2022/Jul/applied_updates-07-04-22.sh
@@ -0,0 +1,8 @@
+sed -i -e "s/version=0.7.5/version=0.7.7/g" libs/libdmtx/spkgbuild
+changelog "libs/libdmtx/spkgbuild" "Upgraded from version 0.7.5 to version 0.7.7"
+sed -i -e "s/version=6.48.2/version=6.48.3/g" python/python-hypothesis/spkgbuild
+changelog "python/python-hypothesis/spkgbuild" "Upgraded from version 6.48.2 to version 6.48.3"
+sed -i -e "s/version=3.7.5/version=3.7.6/g" python/python-orjson/spkgbuild
+changelog "python/python-orjson/spkgbuild" "Upgraded from version 3.7.5 to version 3.7.6"
+sed -i -e "s/version=1.4.1/version=1.4.2/g" python/python-pecan/spkgbuild
+changelog "python/python-pecan/spkgbuild" "Upgraded from version 1.4.1 to version 1.4.2"
