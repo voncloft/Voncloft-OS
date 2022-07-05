230495dc1e1c068fe0132cc5a71391c23c2beb5e
Voncloft <voncloft@gmail.com>
2022-07-04 21:01:12 -0400

    Minor Updates

--- logs/old-test-updates/2022/Jul/applied_updates-07-04-22.sh
+++ logs/old-test-updates/2022/Jul/applied_updates-07-04-22.sh
@@ -6,3 +6,19 @@ sed -i -e "s/version=3.7.5/version=3.7.6/g" python/python-orjson/spkgbuild
 changelog "python/python-orjson/spkgbuild" "Upgraded from version 3.7.5 to version 3.7.6"
 sed -i -e "s/version=1.4.1/version=1.4.2/g" python/python-pecan/spkgbuild
 changelog "python/python-pecan/spkgbuild" "Upgraded from version 1.4.1 to version 1.4.2"
+sed -i -e "s/version=5.4.2/version=5.4.3/g" cinnamon/cinnamon-control-center/spkgbuild
+changelog "cinnamon/cinnamon-control-center/spkgbuild" "Upgraded from version 5.4.2 to version 5.4.3"
+sed -i -e "s/version=22.06.0/version=22.07.0/g" libs/poppler/spkgbuild
+changelog "libs/poppler/spkgbuild" "Upgraded from version 22.06.0 to version 22.07.0"
+sed -i -e "s/version=103.0b3/version=103.0b4/g" networking/firefox/spkgbuild
+changelog "networking/firefox/spkgbuild" "Upgraded from version 103.0b3 to version 103.0b4"
+sed -i -e "s/version=4.3.2/version=4.3.3/g" python/python-engineio/spkgbuild
+changelog "python/python-engineio/spkgbuild" "Upgraded from version 4.3.2 to version 4.3.3"
+sed -i -e "s/version=0.4.5/version=0.4.6/g" python/python-libcst/spkgbuild
+changelog "python/python-libcst/spkgbuild" "Upgraded from version 0.4.5 to version 0.4.6"
+sed -i -e "s/version=0.9.4/version=1.0.0/g" python/python-pydle/spkgbuild
+changelog "python/python-pydle/spkgbuild" "Upgraded from version 0.9.4 to version 1.0.0"
+sed -i -e "s/version=63.0.0/version=64.0.0/g" python/python-setuptools/spkgbuild
+changelog "python/python-setuptools/spkgbuild" "Upgraded from version 63.0.0 to version 64.0.0"
+sed -i -e "s/version=5.6.0/version=5.7.0/g" python/python-socketio/spkgbuild
+changelog "python/python-socketio/spkgbuild" "Upgraded from version 5.6.0 to version 5.7.0"
