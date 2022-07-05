31c47f8fe8b91e1fc4cad9c5e0764f871c32384b
Voncloft <voncloft@gmail.com>
2022-07-05 18:35:47 -0400

    Minor Updates

---
+++ logs/old-test-updates/2022/Jul/applied_updates-07-05-22.sh
@@ -0,0 +1,22 @@
+sed -i -e "s/version=2.36.3/version=2.36.4/g" core/webkitgtk/spkgbuild
+changelog "core/webkitgtk/spkgbuild" "Upgraded from version 2.36.3 to version 2.36.4"
+sed -i -e "s/version=8.1.1/version=9.0.0/g" libs/fmt/spkgbuild
+changelog "libs/fmt/spkgbuild" "Upgraded from version 8.1.1 to version 9.0.0"
+sed -i -e "s/version=3.0.4/version=3.0.5/g" networking/openssl/spkgbuild
+changelog "networking/openssl/spkgbuild" "Upgraded from version 3.0.4 to version 3.0.5"
+sed -i -e "s/version=1.24.22/version=1.24.23/g" python/python-boto3/spkgbuild
+changelog "python/python-boto3/spkgbuild" "Upgraded from version 1.24.22 to version 1.24.23"
+sed -i -e "s/version=1.27.22/version=1.27.23/g" python/python-botocore/spkgbuild
+changelog "python/python-botocore/spkgbuild" "Upgraded from version 1.27.22 to version 1.27.23"
+sed -i -e "s/version=6.48.3/version=6.49.1/g" python/python-hypothesis/spkgbuild
+changelog "python/python-hypothesis/spkgbuild" "Upgraded from version 6.48.3 to version 6.49.1"
+sed -i -e "s/version=3.4.0/version=3.4.1/g" python/python-ldap/spkgbuild
+changelog "python/python-ldap/spkgbuild" "Upgraded from version 3.4.0 to version 3.4.1"
+sed -i -e "s/version=2022.6.7/version=2022.7.4/g" python/python-pipenv/spkgbuild
+changelog "python/python-pipenv/spkgbuild" "Upgraded from version 2022.6.7 to version 2022.7.4"
+sed -i -e "s/version=3.8.1/version=3.8.2/g" python/python-pytest-mock/spkgbuild
+changelog "python/python-pytest-mock/spkgbuild" "Upgraded from version 3.8.1 to version 3.8.2"
+sed -i -e "s/version=2.2.1/version=2.3.0/g" ruby-gems/ruby-paint/spkgbuild
+changelog "ruby-gems/ruby-paint/spkgbuild" "Upgraded from version 2.2.1 to version 2.3.0"
+sed -i -e "s/version=4.16.2/version=4.16.3/g" xfce/xfce4-settings/spkgbuild
+changelog "xfce/xfce4-settings/spkgbuild" "Upgraded from version 4.16.2 to version 4.16.3"
