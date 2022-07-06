d71168c54a1b75f01513c69b3410746fba15e339
Voncloft <voncloft@gmail.com>
2022-07-06 16:57:53 -0400

    Minor Updates

---
+++ logs/old-test-updates/2022/Jul/applied_updates-07-06-22.sh
@@ -0,0 +1,32 @@
+sed -i -e "s/version=2.9.1/version=2.10.0/g" core/cmus/spkgbuild
+changelog "core/cmus/spkgbuild" "Upgraded from version 2.9.1 to version 2.10.0"
+sed -i -e "s/version=3.1.7/version=3.2.0/g" core/wxgtk3/spkgbuild
+changelog "core/wxgtk3/spkgbuild" "Upgraded from version 3.1.7 to version 3.2.0"
+sed -i -e "s/version=2.4.111/version=2.4.112/g" libs/libdrm/spkgbuild
+changelog "libs/libdrm/spkgbuild" "Upgraded from version 2.4.111 to version 2.4.112"
+sed -i -e "s/version=103.0b4/version=103.0b5/g" networking/firefox/spkgbuild
+changelog "networking/firefox/spkgbuild" "Upgraded from version 103.0b4 to version 103.0b5"
+sed -i -e "s/version=103.0b2/version=103.0b3/g" networking/thunderbird/spkgbuild
+changelog "networking/thunderbird/spkgbuild" "Upgraded from version 103.0b2 to version 103.0b3"
+sed -i -e "s/version=0.4.1/version=0.5.0/g" python/python-aioconsole/spkgbuild
+changelog "python/python-aioconsole/spkgbuild" "Upgraded from version 0.4.1 to version 0.5.0"
+sed -i -e "s/version=1.24.23/version=1.24.24/g" python/python-boto3/spkgbuild
+changelog "python/python-boto3/spkgbuild" "Upgraded from version 1.24.23 to version 1.24.24"
+sed -i -e "s/version=1.27.23/version=1.27.24/g" python/python-botocore/spkgbuild
+changelog "python/python-botocore/spkgbuild" "Upgraded from version 1.27.23 to version 1.27.24"
+sed -i -e "s/version=13.14.0/version=13.15.0/g" python/python-faker/spkgbuild
+changelog "python/python-faker/spkgbuild" "Upgraded from version 13.14.0 to version 13.15.0"
+sed -i -e "s/version=4.33.3/version=4.34.2/g" python/python-fonttools/spkgbuild
+changelog "python/python-fonttools/spkgbuild" "Upgraded from version 4.33.3 to version 4.34.2"
+sed -i -e "s/version=2.3.3/version=2.3.4/g" python/python-gphoto2/spkgbuild
+changelog "python/python-gphoto2/spkgbuild" "Upgraded from version 2.3.3 to version 2.3.4"
+sed -i -e "s/version=3.4.1/version=3.4.2/g" python/python-ldap/spkgbuild
+changelog "python/python-ldap/spkgbuild" "Upgraded from version 3.4.1 to version 3.4.2"
+sed -i -e "s/version=2.3.1/version=2.3.2/g" python/python-spyder-kernels/spkgbuild
+changelog "python/python-spyder-kernels/spkgbuild" "Upgraded from version 2.3.1 to version 2.3.2"
+sed -i -e "s/version=6.18.5/version=7.0.0/g" python/python-testfixtures/spkgbuild
+changelog "python/python-testfixtures/spkgbuild" "Upgraded from version 6.18.5 to version 7.0.0"
+sed -i -e "s/version=0.4.2/version=0.5.0/g" python/python-typer/spkgbuild
+changelog "python/python-typer/spkgbuild" "Upgraded from version 0.4.2 to version 0.5.0"
+sed -i -e "s/version=1.27.1.5916/version=1.27.2.5929/g" server/plex-media-server/spkgbuild
+changelog "server/plex-media-server/spkgbuild" "Upgraded from version 1.27.1.5916 to version 1.27.2.5929"
