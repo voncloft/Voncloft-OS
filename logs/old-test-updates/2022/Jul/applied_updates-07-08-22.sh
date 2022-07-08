5bda30b67d28f543cdab2062ea1e582a7bdeb16a
Voncloft <voncloft@gmail.com>
2022-07-08 16:06:05 -0400

    Minor Updates

---
+++ logs/old-test-updates/2022/Jul/applied_updates-07-08-22.sh
@@ -0,0 +1,26 @@
+sed -i -e "s/version=2.2/version=2.3/g" core/inetutils/spkgbuild
+changelog "core/inetutils/spkgbuild" "Upgraded from version 2.2 to version 2.3"
+sed -i -e "s/version=1.2.7.1/version=1.2.7.2/g" libs/alsa-lib/spkgbuild
+changelog "libs/alsa-lib/spkgbuild" "Upgraded from version 1.2.7.1 to version 1.2.7.2"
+sed -i -e "s/version=0.37.2/version=0.38.0/g" libs/libfilezilla/spkgbuild
+changelog "libs/libfilezilla/spkgbuild" "Upgraded from version 0.37.2 to version 0.38.0"
+sed -i -e "s/version=103.0b5/version=103.0b6/g" networking/firefox/spkgbuild
+changelog "networking/firefox/spkgbuild" "Upgraded from version 103.0b5 to version 103.0b6"
+sed -i -e "s/version=103.0b3/version=103.0b4/g" networking/thunderbird/spkgbuild
+changelog "networking/thunderbird/spkgbuild" "Upgraded from version 103.0b3 to version 103.0b4"
+sed -i -e "s/version=1.24.24/version=1.24.25/g" python/python-boto3/spkgbuild
+changelog "python/python-boto3/spkgbuild" "Upgraded from version 1.24.24 to version 1.24.25"
+sed -i -e "s/version=1.27.24/version=1.27.25/g" python/python-botocore/spkgbuild
+changelog "python/python-botocore/spkgbuild" "Upgraded from version 1.27.24 to version 1.27.25"
+sed -i -e "s/version=0.61.1/version=0.61.2/g" python/python-cfn-lint/spkgbuild
+changelog "python/python-cfn-lint/spkgbuild" "Upgraded from version 0.61.1 to version 0.61.2"
+sed -i -e "s/version=4.34.3/version=4.34.4/g" python/python-fonttools/spkgbuild
+changelog "python/python-fonttools/spkgbuild" "Upgraded from version 4.34.3 to version 4.34.4"
+sed -i -e "s/version=6.15.0/version=6.15.1/g" python/python-ipykernel/spkgbuild
+changelog "python/python-ipykernel/spkgbuild" "Upgraded from version 6.15.0 to version 6.15.1"
+sed -i -e "s/version=4.11.0/version=4.11.1/g" python/python-jupyter_core/spkgbuild
+changelog "python/python-jupyter_core/spkgbuild" "Upgraded from version 4.11.0 to version 4.11.1"
+sed -i -e "s/version=2.1.2/version=2.2.0/g" python/python-werkzeug/spkgbuild
+changelog "python/python-werkzeug/spkgbuild" "Upgraded from version 2.1.2 to version 2.2.0"
+sed -i -e "s/version=2022.7.0/version=2022.7.1/g" server/homeassistant/spkgbuild
+changelog "server/homeassistant/spkgbuild" "Upgraded from version 2022.7.0 to version 2022.7.1"
