9dca3abc0abcb30f177a222a5a5aaaf1984a5690
Voncloft <voncloft@gmail.com>
2022-07-11 06:55:31 -0400

    Minor Updates

---
+++ logs/old-test-updates/2022/Jul/applied_updates-07-11-22.sh
@@ -0,0 +1,44 @@
+sed -i -e "s/version=4.1/version=4.2/g" core/audacious/spkgbuild
+changelog "core/audacious/spkgbuild" "Upgraded from version 4.1 to version 4.2"
+sed -i -e "s/version=4.1/version=4.2/g" core/audacious-plugins/spkgbuild
+changelog "core/audacious-plugins/spkgbuild" "Upgraded from version 4.1 to version 4.2"
+sed -i -e "s/version=6.1.4/version=6.2/g" core/mercurial/spkgbuild
+changelog "core/mercurial/spkgbuild" "Upgraded from version 6.1.4 to version 6.2"
+sed -i -e "s/version=3.5/version=3.6/g" core/weechat/spkgbuild
+changelog "core/weechat/spkgbuild" "Upgraded from version 3.5 to version 3.6"
+sed -i -e "s/version=1.0.5/version=1.0.6/g" core/xcmsdb/spkgbuild
+changelog "core/xcmsdb/spkgbuild" "Upgraded from version 1.0.5 to version 1.0.6"
+sed -i -e "s/version=1.0.10/version=1.0.11/g" core/xmodmap/spkgbuild
+changelog "core/xmodmap/spkgbuild" "Upgraded from version 1.0.10 to version 1.0.11"
+sed -i -e "s/version=1.0.5/version=1.1.0/g" core/xpr/spkgbuild
+changelog "core/xpr/spkgbuild" "Upgraded from version 1.0.5 to version 1.1.0"
+sed -i -e "s/version=1.0.5/version=1.0.6/g" core/xwud/spkgbuild
+changelog "core/xwud/spkgbuild" "Upgraded from version 1.0.5 to version 1.0.6"
+sed -i -e "s/version=3.3.19/version=3.3.20/g" extra/flexget/spkgbuild
+changelog "extra/flexget/spkgbuild" "Upgraded from version 3.3.19 to version 3.3.20"
+sed -i -e "s/version=1.21/version=1.22/g" extra/iotop/spkgbuild
+changelog "extra/iotop/spkgbuild" "Upgraded from version 1.21 to version 1.22"
+sed -i -e "s/version=103.0b6/version=103.0b7/g" networking/firefox/spkgbuild
+changelog "networking/firefox/spkgbuild" "Upgraded from version 103.0b6 to version 103.0b7"
+sed -i -e "s/version=2.12.0/version=2.12.1/g" python/python-astroid/spkgbuild
+changelog "python/python-astroid/spkgbuild" "Upgraded from version 2.12.0 to version 2.12.1"
+sed -i -e "s/version=4.6.2/version=4.7.0/g" python/python-jsonschema/spkgbuild
+changelog "python/python-jsonschema/spkgbuild" "Upgraded from version 4.6.2 to version 4.7.0"
+sed -i -e "s/version=2.19.0/version=2.20.0/g" python/python-pre-commit/spkgbuild
+changelog "python/python-pre-commit/spkgbuild" "Upgraded from version 2.19.0 to version 2.20.0"
+sed -i -e "s/version=0.38.2/version=0.38.3/g" python/python-sqlalchemy-utils/spkgbuild
+changelog "python/python-sqlalchemy-utils/spkgbuild" "Upgraded from version 0.38.2 to version 0.38.3"
+sed -i -e "s/version=2.1.1/version=2.1.2/g" server/deluge/spkgbuild
+changelog "server/deluge/spkgbuild" "Upgraded from version 2.1.1 to version 2.1.2"
+sed -i -e "s/version=2022.7.2/version=2022.7.3/g" server/homeassistant/spkgbuild
+changelog "server/homeassistant/spkgbuild" "Upgraded from version 2022.7.2 to version 2022.7.3"
+sed -i -e "s/version=4.17.0/version=4.17.1/g" xfce/garcon/spkgbuild
+changelog "xfce/garcon/spkgbuild" "Upgraded from version 4.17.0 to version 4.17.1"
+sed -i -e "s/version=0.12.2/version=0.12.3/g" xfce/ristretto/spkgbuild
+changelog "xfce/ristretto/spkgbuild" "Upgraded from version 0.12.2 to version 0.12.3"
+sed -i -e "s/version=4.17.8/version=4.17.9/g" xfce/thunar/spkgbuild
+changelog "xfce/thunar/spkgbuild" "Upgraded from version 4.17.8 to version 4.17.9"
+sed -i -e "s/version=4.17.1/version=4.17.2/g" xfce/xfce4-panel/spkgbuild
+changelog "xfce/xfce4-panel/spkgbuild" "Upgraded from version 4.17.1 to version 4.17.2"
+sed -i -e "s/version=1.0.6/version=1.1.0/g" xfce/xfontsel/spkgbuild
+changelog "xfce/xfontsel/spkgbuild" "Upgraded from version 1.0.6 to version 1.1.0"
