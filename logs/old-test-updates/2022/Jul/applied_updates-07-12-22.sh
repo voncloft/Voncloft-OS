a6974700a529a8d239711f423d35234b9596f08b
Voncloft <voncloft@gmail.com>
2022-07-12 05:34:13 -0400

    Minor Updates

---
+++ logs/old-test-updates/2022/Jul/applied_updates-07-12-22.sh
@@ -0,0 +1,20 @@
+sed -i -e "s/version=2.3.6/version=2.3.7/g" core/gnupg/spkgbuild
+changelog "core/gnupg/spkgbuild" "Upgraded from version 2.3.6 to version 2.3.7"
+sed -i -e "s/version=7.0.2/version=7.0.3/g" core/redis/spkgbuild
+changelog "core/redis/spkgbuild" "Upgraded from version 7.0.2 to version 7.0.3"
+sed -i -e "s/version=1.24.26/version=1.24.27/g" python/python-boto3/spkgbuild
+changelog "python/python-boto3/spkgbuild" "Upgraded from version 1.24.26 to version 1.24.27"
+sed -i -e "s/version=1.27.26/version=1.27.27/g" python/python-botocore/spkgbuild
+changelog "python/python-botocore/spkgbuild" "Upgraded from version 1.27.26 to version 1.27.27"
+sed -i -e "s/version=2.0.3/version=2.0.4/g" python/python-json-logger/spkgbuild
+changelog "python/python-json-logger/spkgbuild" "Upgraded from version 2.0.3 to version 2.0.4"
+sed -i -e "s/version=4.7.0/version=4.7.1/g" python/python-jsonschema/spkgbuild
+changelog "python/python-jsonschema/spkgbuild" "Upgraded from version 4.7.0 to version 4.7.1"
+sed -i -e "s/version=0.18.3/version=0.19.0/g" python/python-meilisearch/spkgbuild
+changelog "python/python-meilisearch/spkgbuild" "Upgraded from version 0.18.3 to version 0.19.0"
+sed -i -e "s/version=12.4.4/version=12.5.1/g" python/python-rich/spkgbuild
+changelog "python/python-rich/spkgbuild" "Upgraded from version 12.4.4 to version 12.5.1"
+sed -i -e "s/version=2.9.0/version=2.9.1/g" python/python-tensorflow-serving-api/spkgbuild
+changelog "python/python-tensorflow-serving-api/spkgbuild" "Upgraded from version 2.9.0 to version 2.9.1"
+sed -i -e "s/version=8.1.0/version=8.2.0/g" python/python-webargs/spkgbuild
+changelog "python/python-webargs/spkgbuild" "Upgraded from version 8.1.0 to version 8.2.0"
