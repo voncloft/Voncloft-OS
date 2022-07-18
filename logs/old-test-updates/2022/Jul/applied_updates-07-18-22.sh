e0d333c1f2ad27481402e9ad21f3ef25b9d3be60
Voncloft <voncloft@gmail.com>
2022-07-18 05:29:59 -0400

    Minor Updates

---
+++ logs/old-test-updates/2022/Jul/applied_updates-07-18-22.sh
@@ -0,0 +1,12 @@
+sed -i -e "s/version=2.3/version=2.3.1/g" core/blueman/spkgbuild
+changelog "core/blueman/spkgbuild" "Upgraded from version 2.3 to version 2.3.1"
+sed -i -e "s/version=2.0.6/version=2.0.7/g" libs/libtorrent/spkgbuild
+changelog "libs/libtorrent/spkgbuild" "Upgraded from version 2.0.6 to version 2.0.7"
+sed -i -e "s/version=2.0.6/version=2.0.7/g" libs/libtorrent-rasterbar/spkgbuild
+changelog "libs/libtorrent-rasterbar/spkgbuild" "Upgraded from version 2.0.6 to version 2.0.7"
+sed -i -e "s/version=2.5.3/version=3.0.0/g" python/python-elementpath/spkgbuild
+changelog "python/python-elementpath/spkgbuild" "Upgraded from version 2.5.3 to version 3.0.0"
+sed -i -e "s/version=0.6.7/version=0.6.8/g" python/python-wand/spkgbuild
+changelog "python/python-wand/spkgbuild" "Upgraded from version 0.6.7 to version 0.6.8"
+sed -i -e "s/version=3.10.15/version=3.10.17/g" server/blacklist-hosts/spkgbuild
+changelog "server/blacklist-hosts/spkgbuild" "Upgraded from version 3.10.15 to version 3.10.17"
