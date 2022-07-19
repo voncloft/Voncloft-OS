b2edaedfde27fe0fc01c11a50508daf1f67175c3
Voncloft <voncloft@gmail.com>
2022-07-19 05:26:10 -0400

    Minor Updates

---
+++ logs/old-test-updates/2022/Jul/applied_updates-07-19-22.sh
@@ -0,0 +1,32 @@
+sed -i -e "s/version=1.80/version=1.81/g" core/os-prober/spkgbuild
+changelog "core/os-prober/spkgbuild" "Upgraded from version 1.80 to version 1.81"
+sed -i -e "s/version=7.0.3/version=7.0.4/g" core/redis/spkgbuild
+changelog "core/redis/spkgbuild" "Upgraded from version 7.0.3 to version 7.0.4"
+sed -i -e "s/version=1.30.0/version=1.32.0/g" displaym/lightdm/spkgbuild
+changelog "displaym/lightdm/spkgbuild" "Upgraded from version 1.30.0 to version 1.32.0"
+sed -i -e "s/version=2.64/version=2.65/g" libs/libcap/spkgbuild
+changelog "libs/libcap/spkgbuild" "Upgraded from version 2.64 to version 2.65"
+sed -i -e "s/version=2022.06.29/version=2022.07.18/g" networking/yt-dlp/spkgbuild
+changelog "networking/yt-dlp/spkgbuild" "Upgraded from version 2022.06.29 to version 2022.07.18"
+sed -i -e "s/version=0.5.2/version=0.6.0/g" python/python-adblock/spkgbuild
+changelog "python/python-adblock/spkgbuild" "Upgraded from version 0.5.2 to version 0.6.0"
+sed -i -e "s/version=1.24.31/version=1.24.32/g" python/python-boto3/spkgbuild
+changelog "python/python-boto3/spkgbuild" "Upgraded from version 1.24.31 to version 1.24.32"
+sed -i -e "s/version=1.27.31/version=1.27.32/g" python/python-botocore/spkgbuild
+changelog "python/python-botocore/spkgbuild" "Upgraded from version 1.27.31 to version 1.27.32"
+sed -i -e "s/version=0.61.2/version=0.61.3/g" python/python-cfn-lint/spkgbuild
+changelog "python/python-cfn-lint/spkgbuild" "Upgraded from version 0.61.2 to version 0.61.3"
+sed -i -e "s/version=1.5.0/version=1.6.0/g" python/python-evdev/spkgbuild
+changelog "python/python-evdev/spkgbuild" "Upgraded from version 1.5.0 to version 1.6.0"
+sed -i -e "s/version=6.50.1/version=6.52.1/g" python/python-hypothesis/spkgbuild
+changelog "python/python-hypothesis/spkgbuild" "Upgraded from version 6.50.1 to version 6.52.1"
+sed -i -e "s/version=0.24.1/version=0.24.2/g" python/python-mongoengine/spkgbuild
+changelog "python/python-mongoengine/spkgbuild" "Upgraded from version 0.24.1 to version 0.24.2"
+sed -i -e "s/version=7.4.1/version=7.4.2/g" python/python-pyface/spkgbuild
+changelog "python/python-pyface/spkgbuild" "Upgraded from version 7.4.1 to version 7.4.2"
+sed -i -e "s/version=3.12.8/version=3.12.9/g" python/python-trimesh/spkgbuild
+changelog "python/python-trimesh/spkgbuild" "Upgraded from version 3.12.8 to version 3.12.9"
+sed -i -e "s/version=1.11.3/version=2.0.0/g" python/python-xmlschema/spkgbuild
+changelog "python/python-xmlschema/spkgbuild" "Upgraded from version 1.11.3 to version 2.0.0"
+sed -i -e "s/version=4.16.2/version=4.16.3/g" server/samba/spkgbuild
+changelog "server/samba/spkgbuild" "Upgraded from version 4.16.2 to version 4.16.3"
