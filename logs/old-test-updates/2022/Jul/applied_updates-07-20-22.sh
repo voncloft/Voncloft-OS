3ba9e1d796f9ff02e714e5f41d1161ccbd0e92a1
Voncloft <voncloft@gmail.com>
2022-07-20 05:17:22 -0400

    Minor Updates

---
+++ logs/old-test-updates/2022/Jul/applied_updates-07-20-22.sh
@@ -0,0 +1,36 @@
+sed -i -e "s/version=1.62.0/version=1.62.1/g" compilers/rust/spkgbuild
+changelog "compilers/rust/spkgbuild" "Upgraded from version 1.62.0 to version 1.62.1"
+sed -i -e "s/version=2.9.7/version=2.9.8/g" core/icewm/spkgbuild
+changelog "core/icewm/spkgbuild" "Upgraded from version 2.9.7 to version 2.9.8"
+sed -i -e "s/version=0.3.55/version=0.3.56/g" core/pipewire/spkgbuild
+changelog "core/pipewire/spkgbuild" "Upgraded from version 0.3.55 to version 0.3.56"
+sed -i -e "s/version=4.0.0/version=4.0.1/g" libs/libimagequant/spkgbuild
+changelog "libs/libimagequant/spkgbuild" "Upgraded from version 4.0.0 to version 4.0.1"
+sed -i -e "s/version=0.1.1/version=0.1.2/g" libs/libxcvt/spkgbuild
+changelog "libs/libxcvt/spkgbuild" "Upgraded from version 0.1.1 to version 0.1.2"
+sed -i -e "s/version=103.0b5/version=103.0b6/g" networking/thunderbird/spkgbuild
+changelog "networking/thunderbird/spkgbuild" "Upgraded from version 103.0b5 to version 103.0b6"
+sed -i -e "s/version=1.24.32/version=1.24.33/g" python/python-boto3/spkgbuild
+changelog "python/python-boto3/spkgbuild" "Upgraded from version 1.24.32 to version 1.24.33"
+sed -i -e "s/version=1.27.32/version=1.27.33/g" python/python-botocore/spkgbuild
+changelog "python/python-botocore/spkgbuild" "Upgraded from version 1.27.32 to version 1.27.33"
+sed -i -e "s/version=0.18.2/version=0.18.3/g" python/python-django-modeltranslation/spkgbuild
+changelog "python/python-django-modeltranslation/spkgbuild" "Upgraded from version 0.18.2 to version 0.18.3"
+sed -i -e "s/version=2.53.0/version=2.54.0/g" python/python-google-api-python-client/spkgbuild
+changelog "python/python-google-api-python-client/spkgbuild" "Upgraded from version 2.53.0 to version 2.54.0"
+sed -i -e "s/version=6.52.1/version=6.52.3/g" python/python-hypothesis/spkgbuild
+changelog "python/python-hypothesis/spkgbuild" "Upgraded from version 6.52.1 to version 6.52.3"
+sed -i -e "s/version=2.8.4/version=2.8.5/g" python/python-networkx/spkgbuild
+changelog "python/python-networkx/spkgbuild" "Upgraded from version 2.8.4 to version 2.8.5"
+sed -i -e "s/version=3.7.7/version=3.7.8/g" python/python-orjson/spkgbuild
+changelog "python/python-orjson/spkgbuild" "Upgraded from version 3.7.7 to version 3.7.8"
+sed -i -e "s/version=8.12.51/version=8.12.52/g" python/python-phonenumbers/spkgbuild
+changelog "python/python-phonenumbers/spkgbuild" "Upgraded from version 8.12.51 to version 8.12.52"
+sed -i -e "s/version=0.0.29/version=0.0.30/g" python/python-pip-api/spkgbuild
+changelog "python/python-pip-api/spkgbuild" "Upgraded from version 0.0.29 to version 0.0.30"
+sed -i -e "s/version=2022.1.1/version=2022.1.2/g" python/python-pudb/spkgbuild
+changelog "python/python-pudb/spkgbuild" "Upgraded from version 2022.1.1 to version 2022.1.2"
+sed -i -e "s/version=3.10.17/version=3.10.18/g" server/blacklist-hosts/spkgbuild
+changelog "server/blacklist-hosts/spkgbuild" "Upgraded from version 3.10.17 to version 3.10.18"
+sed -i -e "s/version=1.23.0/version=1.23.1/g" server/nginx/spkgbuild
+changelog "server/nginx/spkgbuild" "Upgraded from version 1.23.0 to version 1.23.1"
