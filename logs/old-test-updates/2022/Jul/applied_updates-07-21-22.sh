0b159e84d3d6cca9805e3a51aa0dc6d8135bc7ee
Voncloft <voncloft@gmail.com>
2022-07-21 10:26:03 -0400

    Minor Updates

---
+++ logs/old-test-updates/2022/Jul/applied_updates-07-21-22.sh
@@ -0,0 +1,32 @@
+sed -i -e "s/version=5.4.4/version=5.4.5/g" cinnamon/cinnamon/spkgbuild
+changelog "cinnamon/cinnamon/spkgbuild" "Upgraded from version 5.4.4 to version 5.4.5"
+sed -i -e "s/version=5.4.1/version=5.4.2/g" cinnamon/cinnamon-desktop/spkgbuild
+changelog "cinnamon/cinnamon-desktop/spkgbuild" "Upgraded from version 5.4.1 to version 5.4.2"
+sed -i -e "s/version=5.4.2/version=5.4.3/g" cinnamon/muffin/spkgbuild
+changelog "cinnamon/muffin/spkgbuild" "Upgraded from version 5.4.2 to version 5.4.3"
+sed -i -e "s/version=0.38.0/version=0.38.1/g" libs/libfilezilla/spkgbuild
+changelog "libs/libfilezilla/spkgbuild" "Upgraded from version 0.38.0 to version 0.38.1"
+sed -i -e "s/version=9.19.2/version=9.19.3/g" networking/bind/spkgbuild
+changelog "networking/bind/spkgbuild" "Upgraded from version 9.19.2 to version 9.19.3"
+sed -i -e "s/version=6.1.34/version=6.1.36/g" nonfree/virtualbox/spkgbuild
+changelog "nonfree/virtualbox/spkgbuild" "Upgraded from version 6.1.34 to version 6.1.36"
+sed -i -e "s/version=6.1.34/version=6.1.36/g" nonfree/virtualbox-guest-modules/spkgbuild
+changelog "nonfree/virtualbox-guest-modules/spkgbuild" "Upgraded from version 6.1.34 to version 6.1.36"
+sed -i -e "s/version=6.1.34/version=6.1.36/g" nonfree/virtualbox-guest-utils/spkgbuild
+changelog "nonfree/virtualbox-guest-utils/spkgbuild" "Upgraded from version 6.1.34 to version 6.1.36"
+sed -i -e "s/version=6.1.34/version=6.1.36/g" nonfree/virtualbox-modules/spkgbuild
+changelog "nonfree/virtualbox-modules/spkgbuild" "Upgraded from version 6.1.34 to version 6.1.36"
+sed -i -e "s/version=1.24.33/version=1.24.34/g" python/python-boto3/spkgbuild
+changelog "python/python-boto3/spkgbuild" "Upgraded from version 1.24.33 to version 1.24.34"
+sed -i -e "s/version=1.27.33/version=1.27.34/g" python/python-botocore/spkgbuild
+changelog "python/python-botocore/spkgbuild" "Upgraded from version 1.27.33 to version 1.27.34"
+sed -i -e "s/version=2.5.1/version=2.5.2/g" python/python-identify/spkgbuild
+changelog "python/python-identify/spkgbuild" "Upgraded from version 2.5.1 to version 2.5.2"
+sed -i -e "s/version=2.3.1/version=2.4.0/g" python/python-perf/spkgbuild
+changelog "python/python-perf/spkgbuild" "Upgraded from version 2.3.1 to version 2.4.0"
+sed -i -e "s/version=0.0.12/version=0.0.13/g" python/python-pyadi-iio/spkgbuild
+changelog "python/python-pyadi-iio/spkgbuild" "Upgraded from version 0.0.12 to version 0.0.13"
+sed -i -e "s/version=7.8.0/version=7.9.0/g" python/python-qiniu/spkgbuild
+changelog "python/python-qiniu/spkgbuild" "Upgraded from version 7.8.0 to version 7.9.0"
+sed -i -e "s/version=21.5.0/version=22.1.0/g" python/python-structlog/spkgbuild
+changelog "python/python-structlog/spkgbuild" "Upgraded from version 21.5.0 to version 22.1.0"
