df6e94562ba94ac82880d0b210cb397c5a044c9e
Voncloft <voncloft@gmail.com>
2022-07-21 17:25:47 -0400

    Minor Updates

--- logs/old-test-updates/2022/Jul/applied_updates-07-21-22.sh
+++ logs/old-test-updates/2022/Jul/applied_updates-07-21-22.sh
@@ -30,3 +30,23 @@ sed -i -e "s/version=7.8.0/version=7.9.0/g" python/python-qiniu/spkgbuild
 changelog "python/python-qiniu/spkgbuild" "Upgraded from version 7.8.0 to version 7.9.0"
 sed -i -e "s/version=21.5.0/version=22.1.0/g" python/python-structlog/spkgbuild
 changelog "python/python-structlog/spkgbuild" "Upgraded from version 21.5.0 to version 22.1.0"
+sed -i -e "s/version=5.4.1/version=5.4.2/g" cinnamon/nemo/spkgbuild
+changelog "cinnamon/nemo/spkgbuild" "Upgraded from version 5.4.1 to version 5.4.2"
+sed -i -e "s/version=3.80/version=3.81/g" core/nss/spkgbuild
+changelog "core/nss/spkgbuild" "Upgraded from version 3.80 to version 3.81"
+sed -i -e "s/version=21.2/version=21.3/g" extra/protobuf/spkgbuild
+changelog "extra/protobuf/spkgbuild" "Upgraded from version 21.2 to version 21.3"
+sed -i -e "s/version=2.2.13/version=2.2.14/g" libs/xapp/spkgbuild
+changelog "libs/xapp/spkgbuild" "Upgraded from version 2.2.13 to version 2.2.14"
+sed -i -e "s/version=7.3.4/version=7.3.5/g" nonfree/libreoffice/spkgbuild
+changelog "nonfree/libreoffice/spkgbuild" "Upgraded from version 7.3.4 to version 7.3.5"
+sed -i -e "s/version=1.24.34/version=1.24.35/g" python/python-boto3/spkgbuild
+changelog "python/python-boto3/spkgbuild" "Upgraded from version 1.24.34 to version 1.24.35"
+sed -i -e "s/version=1.27.34/version=1.27.35/g" python/python-botocore/spkgbuild
+changelog "python/python-botocore/spkgbuild" "Upgraded from version 1.27.34 to version 1.27.35"
+sed -i -e "s/version=22.1.2/version=22.2/g" python/python-pip/spkgbuild
+changelog "python/python-pip/spkgbuild" "Upgraded from version 22.1.2 to version 22.2"
+sed -i -e "s/version=2.0.0/version=2.0.1/g" python/python-xmlschema/spkgbuild
+changelog "python/python-xmlschema/spkgbuild" "Upgraded from version 2.0.0 to version 2.0.1"
+sed -i -e "s/version=2022.7.5/version=2022.7.6/g" server/homeassistant/spkgbuild
+changelog "server/homeassistant/spkgbuild" "Upgraded from version 2022.7.5 to version 2022.7.6"
