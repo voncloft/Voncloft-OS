90afb5121419d9dc53ce9b366b164b04e2ce8235
Voncloft <voncloft@gmail.com>
2022-07-24 10:56:32 -0400

    Minor Updates

---
+++ logs/old-test-updates/2022/Jul/applied_updates-07-24-22.sh
@@ -0,0 +1,30 @@
+sed -i -e "s/version=5.4.5/version=5.4.6/g" cinnamon/cinnamon/spkgbuild
+changelog "cinnamon/cinnamon/spkgbuild" "Upgraded from version 5.4.5 to version 5.4.6"
+sed -i -e "s/version=5.18.12/version=5.18.14/g" core/linux-desktop/spkgbuild
+changelog "core/linux-desktop/spkgbuild" "Upgraded from version 5.18.12 to version 5.18.14"
+sed -i -e "s/version=5.18.12/version=5.18.14/g" core/linux-router/spkgbuild
+changelog "core/linux-router/spkgbuild" "Upgraded from version 5.18.12 to version 5.18.14"
+sed -i -e "s/version=5.0.1/version=5.1/g" media/ffmpeg/spkgbuild
+changelog "media/ffmpeg/spkgbuild" "Upgraded from version 5.0.1 to version 5.1"
+sed -i -e "s/version=1.24.35/version=1.24.36/g" python/python-boto3/spkgbuild
+changelog "python/python-boto3/spkgbuild" "Upgraded from version 1.24.35 to version 1.24.36"
+sed -i -e "s/version=1.27.35/version=1.27.36/g" python/python-botocore/spkgbuild
+changelog "python/python-botocore/spkgbuild" "Upgraded from version 1.27.35 to version 1.27.36"
+sed -i -e "s/version=2022.7.0/version=2022.7.1/g" python/python-distributed/spkgbuild
+changelog "python/python-distributed/spkgbuild" "Upgraded from version 2022.7.0 to version 2022.7.1"
+sed -i -e "s/version=0.18.3/version=0.18.4/g" python/python-django-modeltranslation/spkgbuild
+changelog "python/python-django-modeltranslation/spkgbuild" "Upgraded from version 0.18.3 to version 0.18.4"
+sed -i -e "s/version=3.0.0/version=3.0.1/g" python/python-elementpath/spkgbuild
+changelog "python/python-elementpath/spkgbuild" "Upgraded from version 3.0.0 to version 3.0.1"
+sed -i -e "s/version=13.15.0/version=13.15.1/g" python/python-faker/spkgbuild
+changelog "python/python-faker/spkgbuild" "Upgraded from version 13.15.0 to version 13.15.1"
+sed -i -e "s/version=0.20/version=0.20.1/g" python/python-graphviz/spkgbuild
+changelog "python/python-graphviz/spkgbuild" "Upgraded from version 0.20 to version 0.20.1"
+sed -i -e "s/version=6.52.3/version=6.52.4/g" python/python-hypothesis/spkgbuild
+changelog "python/python-hypothesis/spkgbuild" "Upgraded from version 6.52.3 to version 6.52.4"
+sed -i -e "s/version=7.3.2206251328/version=7.3.2207231127/g" python/python-pycontracts/spkgbuild
+changelog "python/python-pycontracts/spkgbuild" "Upgraded from version 7.3.2206251328 to version 7.3.2207231127"
+sed -i -e "s/version=1.6.7/version=1.6.8/g" python/python-requirementslib/spkgbuild
+changelog "python/python-requirementslib/spkgbuild" "Upgraded from version 1.6.7 to version 1.6.8"
+sed -i -e "s/version=3.10.18/version=3.11.1/g" server/blacklist-hosts/spkgbuild
+changelog "server/blacklist-hosts/spkgbuild" "Upgraded from version 3.10.18 to version 3.11.1"
