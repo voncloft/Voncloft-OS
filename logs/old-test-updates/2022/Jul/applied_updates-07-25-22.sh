13b76c8fe8c75c96e266998ebfe87adc7524d092
Voncloft <voncloft@gmail.com>
2022-07-25 05:35:09 -0400

    Minor Updates

---
+++ logs/old-test-updates/2022/Jul/applied_updates-07-25-22.sh
@@ -0,0 +1,12 @@
+sed -i -e "s/version=1.4.7/version=1.4.8/g" cinnamon/blueberry/spkgbuild
+changelog "cinnamon/blueberry/spkgbuild" "Upgraded from version 1.4.7 to version 1.4.8"
+sed -i -e "s/version=5.4.6/version=5.4.7/g" cinnamon/cinnamon/spkgbuild
+changelog "cinnamon/cinnamon/spkgbuild" "Upgraded from version 5.4.6 to version 5.4.7"
+sed -i -e "s/version=5.4.2/version=5.4.3/g" cinnamon/cinnamon-settings-daemon/spkgbuild
+changelog "cinnamon/cinnamon-settings-daemon/spkgbuild" "Upgraded from version 5.4.2 to version 5.4.3"
+sed -i -e "s/version=5.4.1/version=5.4.2/g" cinnamon/cinnamon-translations/spkgbuild
+changelog "cinnamon/cinnamon-translations/spkgbuild" "Upgraded from version 5.4.1 to version 5.4.2"
+sed -i -e "s/version=2022.7.4/version=2022.7.24/g" python/python-pipenv/spkgbuild
+changelog "python/python-pipenv/spkgbuild" "Upgraded from version 2022.7.4 to version 2022.7.24"
+sed -i -e "s/version=1.9.2/version=1.10.0/g" python/python-pygit2/spkgbuild
+changelog "python/python-pygit2/spkgbuild" "Upgraded from version 1.9.2 to version 1.10.0"
