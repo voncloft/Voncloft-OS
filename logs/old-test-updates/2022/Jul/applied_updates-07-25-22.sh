e319f1888ba1054a80bc664736a46c7a56822431
Voncloft <voncloft@gmail.com>
2022-07-25 18:25:02 -0400

    Minor Updates

--- logs/old-test-updates/2022/Jul/applied_updates-07-25-22.sh
+++ logs/old-test-updates/2022/Jul/applied_updates-07-25-22.sh
@@ -10,3 +10,27 @@ sed -i -e "s/version=2022.7.4/version=2022.7.24/g" python/python-pipenv/spkgbuil
 changelog "python/python-pipenv/spkgbuild" "Upgraded from version 2022.7.4 to version 2022.7.24"
 sed -i -e "s/version=1.9.2/version=1.10.0/g" python/python-pygit2/spkgbuild
 changelog "python/python-pygit2/spkgbuild" "Upgraded from version 1.9.2 to version 1.10.0"
+sed -i -e "s/version=5.4.7/version=5.4.8/g" cinnamon/cinnamon/spkgbuild
+changelog "cinnamon/cinnamon/spkgbuild" "Upgraded from version 5.4.7 to version 5.4.8"
+sed -i -e "s/version=5.4.3/version=5.4.4/g" cinnamon/muffin/spkgbuild
+changelog "cinnamon/muffin/spkgbuild" "Upgraded from version 5.4.3 to version 5.4.4"
+sed -i -e "s/version=5.64/version=5.65/g" core/bluez/spkgbuild
+changelog "core/bluez/spkgbuild" "Upgraded from version 5.64 to version 5.65"
+sed -i -e "s/version=3.3.21/version=3.3.22/g" extra/flexget/spkgbuild
+changelog "extra/flexget/spkgbuild" "Upgraded from version 3.3.21 to version 3.3.22"
+sed -i -e "s/version=6.1.1/version=6.1.2/g" fonts/awesome-font/spkgbuild
+changelog "fonts/awesome-font/spkgbuild" "Upgraded from version 6.1.1 to version 6.1.2"
+sed -i -e "s/version=4.15.2/version=4.16.0/g" python/python-braintree/spkgbuild
+changelog "python/python-braintree/spkgbuild" "Upgraded from version 4.15.2 to version 4.16.0"
+sed -i -e "s/version=2.6.0/version=2.7.0/g" python/python-dominate/spkgbuild
+changelog "python/python-dominate/spkgbuild" "Upgraded from version 2.6.0 to version 2.7.0"
+sed -i -e "s/version=0.6.1/version=0.6.2/g" python/python-flask-login/spkgbuild
+changelog "python/python-flask-login/spkgbuild" "Upgraded from version 0.6.1 to version 0.6.2"
+sed -i -e "s/version=6.52.4/version=6.53.0/g" python/python-hypothesis/spkgbuild
+changelog "python/python-hypothesis/spkgbuild" "Upgraded from version 6.52.4 to version 6.53.0"
+sed -i -e "s/version=1.26.10/version=1.26.11/g" python/python-urllib3/spkgbuild
+changelog "python/python-urllib3/spkgbuild" "Upgraded from version 1.26.10 to version 1.26.11"
+sed -i -e "s/version=20.15.1/version=20.16.0/g" python/python-virtualenv/spkgbuild
+changelog "python/python-virtualenv/spkgbuild" "Upgraded from version 20.15.1 to version 20.16.0"
+sed -i -e "s/version=3.11.1/version=3.11.2/g" server/blacklist-hosts/spkgbuild
+changelog "server/blacklist-hosts/spkgbuild" "Upgraded from version 3.11.1 to version 3.11.2"
