85f60f2772038ecba11cf0e7b0f5c641acb93eee
Voncloft <voncloft@gmail.com>
2022-07-27 05:19:32 -0400

    Minor Updates

---
+++ logs/old-test-updates/2022/Jul/applied_updates-07-27-22.sh
@@ -0,0 +1,20 @@
+sed -i -e "s/version=4.5/version=4.6/g" core/nnn/spkgbuild
+changelog "core/nnn/spkgbuild" "Upgraded from version 4.5 to version 4.6"
+sed -i -e "s/version=21.3/version=21.4/g" extra/protobuf/spkgbuild
+changelog "extra/protobuf/spkgbuild" "Upgraded from version 21.3 to version 21.4"
+sed -i -e "s/version=103.0b9/version=104.0b1/g" networking/firefox/spkgbuild
+changelog "networking/firefox/spkgbuild" "Upgraded from version 103.0b9 to version 104.0b1"
+sed -i -e "s/version=1.47.0/version=1.48.0/g" python/python-aws-sam-translator/spkgbuild
+changelog "python/python-aws-sam-translator/spkgbuild" "Upgraded from version 1.47.0 to version 1.48.0"
+sed -i -e "s/version=1.24.36/version=1.24.38/g" python/python-boto3/spkgbuild
+changelog "python/python-boto3/spkgbuild" "Upgraded from version 1.24.36 to version 1.24.38"
+sed -i -e "s/version=1.27.36/version=1.27.38/g" python/python-botocore/spkgbuild
+changelog "python/python-botocore/spkgbuild" "Upgraded from version 1.27.36 to version 1.27.38"
+sed -i -e "s/version=3.0.1/version=4.0.0/g" python/python-django-sekizai/spkgbuild
+changelog "python/python-django-sekizai/spkgbuild" "Upgraded from version 3.0.1 to version 4.0.0"
+sed -i -e "s/version=2.54.0/version=2.55.0/g" python/python-google-api-python-client/spkgbuild
+changelog "python/python-google-api-python-client/spkgbuild" "Upgraded from version 2.54.0 to version 2.55.0"
+sed -i -e "s/version=8.0.29/version=8.0.30/g" python/python-mysql-connector/spkgbuild
+changelog "python/python-mysql-connector/spkgbuild" "Upgraded from version 8.0.29 to version 8.0.30"
+sed -i -e "s/version=2022.7.6/version=2022.7.7/g" server/homeassistant/spkgbuild
+changelog "server/homeassistant/spkgbuild" "Upgraded from version 2022.7.6 to version 2022.7.7"
