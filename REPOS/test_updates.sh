b82af8c9f2a45457f27705869b731e04e3ca528d
Voncloft <voncloft@gmail.com>
2022-07-29 16:33:43 -0400

    Minor Updates

---
+++ REPOS/test_updates.sh
@@ -0,0 +1,50 @@
+sed -i -e "s/version=3.7.6/version=3.7.7/g" core/gnutls/spkgbuild
+changelog "core/gnutls/spkgbuild" "Upgraded from version 3.7.6 to version 3.7.7"
+sed -i -e "s/version=5.18.14/version=5.18.15/g" core/linux-desktop/spkgbuild
+changelog "core/linux-desktop/spkgbuild" "Upgraded from version 5.18.14 to version 5.18.15"
+sed -i -e "s/version=5.18.14/version=5.18.15/g" core/linux-router/spkgbuild
+changelog "core/linux-router/spkgbuild" "Upgraded from version 5.18.14 to version 5.18.15"
+sed -i -e "s/version=6.2/version=6.2.1/g" core/mercurial/spkgbuild
+changelog "core/mercurial/spkgbuild" "Upgraded from version 6.2 to version 6.2.1"
+sed -i -e "s/version=2.3.0/version=2.4.0/g" libs/libwacom/spkgbuild
+changelog "libs/libwacom/spkgbuild" "Upgraded from version 2.3.0 to version 2.4.0"
+sed -i -e "s/version=3.8/version=3.8.1/g" libs/nettle/spkgbuild
+changelog "libs/nettle/spkgbuild" "Upgraded from version 3.8 to version 3.8.1"
+sed -i -e "s/version=104.0b2/version=104.0b3/g" networking/firefox/spkgbuild
+changelog "networking/firefox/spkgbuild" "Upgraded from version 104.0b2 to version 104.0b3"
+sed -i -e "s/version=103.0b6/version=104.0b1/g" networking/thunderbird/spkgbuild
+changelog "networking/thunderbird/spkgbuild" "Upgraded from version 103.0b6 to version 104.0b1"
+sed -i -e "s/version=3.7.1/version=3.7.2/g" networking/wireshark/spkgbuild
+changelog "networking/wireshark/spkgbuild" "Upgraded from version 3.7.1 to version 3.7.2"
+sed -i -e "s/version=1.24.38/version=1.24.41/g" python/python-boto3/spkgbuild
+changelog "python/python-boto3/spkgbuild" "Upgraded from version 1.24.38 to version 1.24.41"
+sed -i -e "s/version=1.27.38/version=1.27.41/g" python/python-botocore/spkgbuild
+changelog "python/python-botocore/spkgbuild" "Upgraded from version 1.27.38 to version 1.27.41"
+sed -i -e "s/version=0.61.3/version=0.61.4/g" python/python-cfn-lint/spkgbuild
+changelog "python/python-cfn-lint/spkgbuild" "Upgraded from version 0.61.3 to version 0.61.4"
+sed -i -e "s/version=8.3.2/version=8.3.3/g" python/python-elasticsearch/spkgbuild
+changelog "python/python-elasticsearch/spkgbuild" "Upgraded from version 8.3.2 to version 8.3.3"
+sed -i -e "s/version=4.1.4/version=4.1.5/g" python/python-flask-security-too/spkgbuild
+changelog "python/python-flask-security-too/spkgbuild" "Upgraded from version 4.1.4 to version 4.1.5"
+sed -i -e "s/version=2022.5.0/version=2022.7.1/g" python/python-fsspec/spkgbuild
+changelog "python/python-fsspec/spkgbuild" "Upgraded from version 2022.5.0 to version 2022.7.1"
+sed -i -e "s/version=3.6.0/version=3.7.0/g" python/python-gitlab/spkgbuild
+changelog "python/python-gitlab/spkgbuild" "Upgraded from version 3.6.0 to version 3.7.0"
+sed -i -e "s/version=4.7.2/version=4.8.0/g" python/python-jsonschema/spkgbuild
+changelog "python/python-jsonschema/spkgbuild" "Upgraded from version 4.7.2 to version 4.8.0"
+sed -i -e "s/version=0.19.0/version=0.19.1/g" python/python-meilisearch/spkgbuild
+changelog "python/python-meilisearch/spkgbuild" "Upgraded from version 0.19.0 to version 0.19.1"
+sed -i -e "s/version=5.9.11/version=5.9.12/g" python/python-pypugjs/spkgbuild
+changelog "python/python-pypugjs/spkgbuild" "Upgraded from version 5.9.11 to version 5.9.12"
+sed -i -e "s/version=1.6.8/version=1.6.9/g" python/python-requirementslib/spkgbuild
+changelog "python/python-requirementslib/spkgbuild" "Upgraded from version 1.6.8 to version 1.6.9"
+sed -i -e "s/version=1.1.1/version=1.3.0/g" python/python-rope/spkgbuild
+changelog "python/python-rope/spkgbuild" "Upgraded from version 1.1.1 to version 1.3.0"
+sed -i -e "s/version=20.16.1/version=20.16.2/g" python/python-virtualenv/spkgbuild
+changelog "python/python-virtualenv/spkgbuild" "Upgraded from version 20.16.1 to version 20.16.2"
+sed -i -e "s/version=0.5.2/version=0.5.6/g" python/python-vistir/spkgbuild
+changelog "python/python-vistir/spkgbuild" "Upgraded from version 0.5.2 to version 0.5.6"
+sed -i -e "s/version=2.2.0/version=2.2.1/g" python/python-werkzeug/spkgbuild
+changelog "python/python-werkzeug/spkgbuild" "Upgraded from version 2.2.0 to version 2.2.1"
+sed -i -e "s/version=3.11.2/version=3.11.3/g" server/blacklist-hosts/spkgbuild
+changelog "server/blacklist-hosts/spkgbuild" "Upgraded from version 3.11.2 to version 3.11.3"
