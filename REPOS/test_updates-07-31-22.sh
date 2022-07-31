a44d21158ed573836b80e990354f2c9316c7cc9d
Voncloft <voncloft@gmail.com>
2022-07-31 11:43:58 -0400

    Minor Updates

---
+++ REPOS/test_updates-07-31-22.sh
@@ -0,0 +1,2 @@
+sed -i -e "s/version=3.7.2/version=3.7.2 3.6.7 3.4.15/g" networking/wireshark/spkgbuild
+changelog "networking/wireshark/spkgbuild" "Upgraded from version 3.7.2 to version 3.7.2 3.6.7 3.4.15"
