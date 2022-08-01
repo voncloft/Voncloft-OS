1d38c2cf16fcd342ccde9a298424419d6f53b67f
Voncloft <voncloft@gmail.com>
2022-08-01 05:28:27 -0400

    Minor Updates

--- REPOS/test_updates-07-31-22.sh
+++
@@ -1,2 +0,0 @@
-sed -i -e "s/version=3.7.2/version=3.7.2 3.6.7 3.4.15/g" networking/wireshark/spkgbuild
-changelog "networking/wireshark/spkgbuild" "Upgraded from version 3.7.2 to version 3.7.2 3.6.7 3.4.15"
---
+++ logs/old-test-updates/2022/Aug/applied_updates-08-01-22.sh
@@ -0,0 +1,18 @@
+sed -i -e "s/version=4.3.0/version=4.4.0/g" core/scons/spkgbuild
+changelog "core/scons/spkgbuild" "Upgraded from version 4.3.0 to version 4.4.0"
+sed -i -e "s/version=1.17.6/version=1.17.7/g" libs/cairo/spkgbuild
+changelog "libs/cairo/spkgbuild" "Upgraded from version 1.17.6 to version 1.17.7"
+sed -i -e "s/version=3.36.0/version=3.39.2.0/g" python/python-apsw/spkgbuild
+changelog "python/python-apsw/spkgbuild" "Upgraded from version 3.36.0 to version 3.39.2.0"
+sed -i -e "s/version=1.24.41/version=1.24.42/g" python/python-boto3/spkgbuild
+changelog "python/python-boto3/spkgbuild" "Upgraded from version 1.24.41 to version 1.24.42"
+sed -i -e "s/version=1.27.41/version=1.27.42/g" python/python-botocore/spkgbuild
+changelog "python/python-botocore/spkgbuild" "Upgraded from version 1.27.41 to version 1.27.42"
+sed -i -e "s/version=4.8.0/version=4.9.0/g" python/python-jsonschema/spkgbuild
+changelog "python/python-jsonschema/spkgbuild" "Upgraded from version 4.8.0 to version 4.9.0"
+sed -i -e "s/version=3.7.10/version=3.7.11/g" python/python-orjson/spkgbuild
+changelog "python/python-orjson/spkgbuild" "Upgraded from version 3.7.10 to version 3.7.11"
+sed -i -e "s/version=2.8.0/version=2.9.0/g" python/python-pycodestyle/spkgbuild
+changelog "python/python-pycodestyle/spkgbuild" "Upgraded from version 2.8.0 to version 2.9.0"
+sed -i -e "s/version=7.3.2207231127/version=7.3.2207311741/g" python/python-pycontracts/spkgbuild
+changelog "python/python-pycontracts/spkgbuild" "Upgraded from version 7.3.2207231127 to version 7.3.2207311741"
