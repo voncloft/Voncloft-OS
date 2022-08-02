bcfc2f6ee11335a82bb4ab43482f991dc512f3ff
Voncloft <voncloft@gmail.com>
2022-08-02 07:56:52 -0400

    adding dkms modules for modules

--- REPOS/core/linux-desktop/post-upgrade.sh
+++ REPOS/core/linux-desktop/post-upgrade.sh
@@ -2,4 +2,4 @@
 kernel_version=$(ls /usr/lib/modules)
 grub-mkconfig -o /boot/grub/grub.cfg
 #sudo scratch install -rc nvidia-modules nvidia nvidia-32 virtualbox-modules
-dkms autoinstall -k $kernel_version
+sudo dkms autoinstall -k $kernel_version
