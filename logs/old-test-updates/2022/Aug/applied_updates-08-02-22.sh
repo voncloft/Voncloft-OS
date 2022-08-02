8d64a0674d354dc2fffe26521489832709a85814
Voncloft <voncloft@gmail.com>
2022-08-02 09:57:11 -0400

    Minor Updates

--- logs/old-test-updates/2022/Aug/applied_updates-08-02-22.sh
+++ logs/old-test-updates/2022/Aug/applied_updates-08-02-22.sh
@@ -1,2 +1,4 @@
 sed -i -e "s/version=6.1.22.144080/version=6.1.36/g" nonfree/virtualbox/spkgbuild
 changelog "nonfree/virtualbox/spkgbuild" "Upgraded from version 6.1.22.144080 to version 6.1.36"
+sed -i -e "s/version=6.0/version=6.4/g" core/nano/spkgbuild
+changelog "core/nano/spkgbuild" "Upgraded from version 6.0 to version 6.4"
