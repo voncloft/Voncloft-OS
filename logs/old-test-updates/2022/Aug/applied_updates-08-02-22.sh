174c4cfb5a6bf9fe9454c17408046f743754e229
Voncloft <voncloft@gmail.com>
2022-08-02 11:22:46 -0400

    Minor Updates

--- logs/old-test-updates/2022/Aug/applied_updates-08-02-22.sh
+++ logs/old-test-updates/2022/Aug/applied_updates-08-02-22.sh
@@ -2,3 +2,9 @@ sed -i -e "s/version=6.1.22.144080/version=6.1.36/g" nonfree/virtualbox/spkgbuil
 changelog "nonfree/virtualbox/spkgbuild" "Upgraded from version 6.1.22.144080 to version 6.1.36"
 sed -i -e "s/version=6.0/version=6.4/g" core/nano/spkgbuild
 changelog "core/nano/spkgbuild" "Upgraded from version 6.0 to version 6.4"
+sed -i -e "s/version=515.57/version=515.65.01/g" nonfree/nvidia-modules/spkgbuild
+changelog "nonfree/nvidia-modules/spkgbuild" "Upgraded from version 515.57 to version 515.65.01"
+sed -i -e "s/version=515.57/version=515.65.01/g" nonfree/nvidia/spkgbuild
+changelog "nonfree/nvidia/spkgbuild" "Upgraded from version 515.57 to version 515.65.01"
+sed -i -e "s/version=515.57/version=515.65.01/g" nonfree/nvidia-32/spkgbuild
+changelog "nonfree/nvidia-32/spkgbuild" "Upgraded from version 515.57 to version 515.65.01"
