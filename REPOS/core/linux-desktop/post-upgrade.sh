914855b4a118d10db7f2432ffd8b3d7e07cd5154
Voncloft <voncloft@gmail.com>
2022-08-02 07:45:55 -0400

    Minor Updates

--- REPOS/core/linux-desktop/post-upgrade.sh
+++ REPOS/core/linux-desktop/post-upgrade.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
+kernel_version=$(ls /usr/lib/modules)
 grub-mkconfig -o /boot/grub/grub.cfg
 #sudo scratch install -rc nvidia-modules nvidia nvidia-32 virtualbox-modules
+dkms autoinstall -k $kernel_version
