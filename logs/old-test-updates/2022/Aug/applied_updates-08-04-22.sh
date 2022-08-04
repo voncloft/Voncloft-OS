8f886dc4ddc80b8163672877f42a893b1c03a288
Voncloft <voncloft@gmail.com>
2022-08-04 07:59:36 -0400

    fixed util-linux override

--- logs/old-test-updates/2022/Aug/applied_updates-08-04-22.sh
+++ logs/old-test-updates/2022/Aug/applied_updates-08-04-22.sh
@@ -1,2 +1,4 @@
 sed -i -e "s/version=2.37.4/version=2.38/g" core/util-linux/spkgbuild
 changelog "core/util-linux/spkgbuild" "Upgraded from version 2.37.4 to version 2.38"
+sed -i -e "s/version=2.38/version=2.38.1/g" core/util-linux/spkgbuild
+changelog "core/util-linux/spkgbuild" "Upgraded from version 2.38 to version 2.38.1"
