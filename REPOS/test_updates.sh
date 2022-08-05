6db9e9eb16334e991df1496e5a10d087e0d686be
Voncloft <voncloft@gmail.com>
2022-08-04 21:15:18 -0400

    updated cups

---
+++ REPOS/test_updates.sh
@@ -0,0 +1,26 @@
+sed -i -e "s/version=3.2.0/version=3.2.1/g" core/double-conversion/spkgbuild
+changelog "core/double-conversion/spkgbuild" "Upgraded from version 3.2.0 to version 3.2.1"
+sed -i -e "s/version=2.18/version=2.19/g" nonfree/pandoc-bin/spkgbuild
+changelog "nonfree/pandoc-bin/spkgbuild" "Upgraded from version 2.18 to version 2.19"
+sed -i -e "s/version=1.24.45/version=1.24.46/g" python/python-boto3/spkgbuild
+changelog "python/python-boto3/spkgbuild" "Upgraded from version 1.24.45 to version 1.24.46"
+sed -i -e "s/version=1.27.45/version=1.27.46/g" python/python-botocore/spkgbuild
+changelog "python/python-botocore/spkgbuild" "Upgraded from version 1.27.45 to version 1.27.46"
+sed -i -e "s/version=2.2.0/version=2.2.1/g" python/python-flask/spkgbuild
+changelog "python/python-flask/spkgbuild" "Upgraded from version 2.2.0 to version 2.2.1"
+sed -i -e "s/version=3.7.0/version=3.8.0/g" python/python-gitlab/spkgbuild
+changelog "python/python-gitlab/spkgbuild" "Upgraded from version 3.7.0 to version 3.8.0"
+sed -i -e "s/version=2.5.2/version=2.5.3/g" python/python-identify/spkgbuild
+changelog "python/python-identify/spkgbuild" "Upgraded from version 2.5.2 to version 2.5.3"
+sed -i -e "s/version=4.5.1/version=5.0.0/g" python/python-oslo-concurrency/spkgbuild
+changelog "python/python-oslo-concurrency/spkgbuild" "Upgraded from version 4.5.1 to version 5.0.0"
+sed -i -e "s/version=8.12.52/version=8.12.53/g" python/python-phonenumbers/spkgbuild
+changelog "python/python-phonenumbers/spkgbuild" "Upgraded from version 8.12.52 to version 8.12.53"
+sed -i -e "s/version=2.9.0/version=2.9.1/g" python/python-pycodestyle/spkgbuild
+changelog "python/python-pycodestyle/spkgbuild" "Upgraded from version 2.9.0 to version 2.9.1"
+sed -i -e "s/version=1.4.0/version=1.5.0/g" python/python-shellingham/spkgbuild
+changelog "python/python-shellingham/spkgbuild" "Upgraded from version 1.4.0 to version 1.5.0"
+sed -i -e "s/version=20.16.2/version=20.16.3/g" python/python-virtualenv/spkgbuild
+changelog "python/python-virtualenv/spkgbuild" "Upgraded from version 20.16.2 to version 20.16.3"
+sed -i -e "s/version=0.6.8/version=0.6.9/g" python/python-wand/spkgbuild
+changelog "python/python-wand/spkgbuild" "Upgraded from version 0.6.8 to version 0.6.9"
