b5310c25d172f6f034286836c3c18a0f57195695
Voncloft <voncloft@gmail.com>
2022-08-08 08:31:23 -0400

    Minor Updates

---
+++ logs/old-test-updates/2022/Aug/applied_updates-08-08-22.sh
@@ -0,0 +1,12 @@
+sed -i -e "s/version=1.8.0/version=1.9.0/g" compilers/pkgconf/spkgbuild
+changelog "compilers/pkgconf/spkgbuild" "Upgraded from version 1.8.0 to version 1.9.0"
+sed -i -e "s/version=1.3.2/version=1.3.3/g" libs/libtirpc/spkgbuild
+changelog "libs/libtirpc/spkgbuild" "Upgraded from version 1.3.2 to version 1.3.3"
+sed -i -e "s/version=6.4.2/version=6.4.3/g" python/python-coverage/spkgbuild
+changelog "python/python-coverage/spkgbuild" "Upgraded from version 6.4.2 to version 6.4.3"
+sed -i -e "s/version=1.14.4/version=1.14.5/g" python/python-jsbeautifier/spkgbuild
+changelog "python/python-jsbeautifier/spkgbuild" "Upgraded from version 1.14.4 to version 1.14.5"
+sed -i -e "s/version=2.0.3/version=2.0.4/g" python/python-pyhamcrest/spkgbuild
+changelog "python/python-pyhamcrest/spkgbuild" "Upgraded from version 2.0.3 to version 2.0.4"
+sed -i -e "s/version=2022.8.1/version=2022.8.2/g" server/homeassistant/spkgbuild
+changelog "server/homeassistant/spkgbuild" "Upgraded from version 2022.8.1 to version 2022.8.2"
