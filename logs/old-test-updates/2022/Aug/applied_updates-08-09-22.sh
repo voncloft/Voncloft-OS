5c4a546ed71d4554cd0f3922cb79e470f5c49dc5
Voncloft <voncloft@gmail.com>
2022-08-09 05:30:27 -0400

    Minor Updates

---
+++ logs/old-test-updates/2022/Aug/applied_updates-08-09-22.sh
@@ -0,0 +1,18 @@
+sed -i -e "s/version=1.9.0/version=1.9.2/g" compilers/pkgconf/spkgbuild
+changelog "compilers/pkgconf/spkgbuild" "Upgraded from version 1.9.0 to version 1.9.2"
+sed -i -e "s/version=3.3.22/version=3.3.23/g" extra/flexget/spkgbuild
+changelog "extra/flexget/spkgbuild" "Upgraded from version 3.3.22 to version 3.3.23"
+sed -i -e "s/version=3.0.14/version=3.0.15/g" libs/gnu-efi/spkgbuild
+changelog "libs/gnu-efi/spkgbuild" "Upgraded from version 3.0.14 to version 3.0.15"
+sed -i -e "s/version=104.0b6/version=104.0b7/g" networking/firefox/spkgbuild
+changelog "networking/firefox/spkgbuild" "Upgraded from version 104.0b6 to version 104.0b7"
+sed -i -e "s/version=1.48.0/version=1.49.0/g" python/python-aws-sam-translator/spkgbuild
+changelog "python/python-aws-sam-translator/spkgbuild" "Upgraded from version 1.48.0 to version 1.49.0"
+sed -i -e "s/version=1.24.46/version=1.24.47/g" python/python-boto3/spkgbuild
+changelog "python/python-boto3/spkgbuild" "Upgraded from version 1.24.46 to version 1.24.47"
+sed -i -e "s/version=1.27.46/version=1.27.47/g" python/python-botocore/spkgbuild
+changelog "python/python-botocore/spkgbuild" "Upgraded from version 1.27.46 to version 1.27.47"
+sed -i -e "s/version=2022.1.7/version=2022.8.7/g" python/python-nox/spkgbuild
+changelog "python/python-nox/spkgbuild" "Upgraded from version 2022.1.7 to version 2022.8.7"
+sed -i -e "s/version=1.9.10/version=1.9.11/g" xfce/xfce4-screenshooter/spkgbuild
+changelog "xfce/xfce4-screenshooter/spkgbuild" "Upgraded from version 1.9.10 to version 1.9.11"
