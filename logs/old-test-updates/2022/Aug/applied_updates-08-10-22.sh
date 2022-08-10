e7ded4a45328ca1dcfdfa51bddf2fd88e7be69c7
Voncloft <voncloft@gmail.com>
2022-08-10 07:49:18 -0400

    Minor Updates

---
+++ logs/old-test-updates/2022/Aug/applied_updates-08-10-22.sh
@@ -0,0 +1,22 @@
+sed -i -e "s/version=5.4.4/version=5.4.5/g" cinnamon/cinnamon-settings-daemon/spkgbuild
+changelog "cinnamon/cinnamon-settings-daemon/spkgbuild" "Upgraded from version 5.4.4 to version 5.4.5"
+sed -i -e "s/version=1.12.0/version=1.13.1/g" core/wpebackend-fdo/spkgbuild
+changelog "core/wpebackend-fdo/spkgbuild" "Upgraded from version 1.12.0 to version 1.13.1"
+sed -i -e "s/version=21.4/version=21.5/g" extra/protobuf/spkgbuild
+changelog "extra/protobuf/spkgbuild" "Upgraded from version 21.4 to version 21.5"
+sed -i -e "s/version=1.2.2/version=1.2.3/g" networking/libnftnl/spkgbuild
+changelog "networking/libnftnl/spkgbuild" "Upgraded from version 1.2.2 to version 1.2.3"
+sed -i -e "s/version=2022.07.18/version=2022.08.08/g" networking/yt-dlp/spkgbuild
+changelog "networking/yt-dlp/spkgbuild" "Upgraded from version 2022.07.18 to version 2022.08.08"
+sed -i -e "s/version=1.24.47/version=1.24.48/g" python/python-boto3/spkgbuild
+changelog "python/python-boto3/spkgbuild" "Upgraded from version 1.24.47 to version 1.24.48"
+sed -i -e "s/version=1.27.47/version=1.27.48/g" python/python-botocore/spkgbuild
+changelog "python/python-botocore/spkgbuild" "Upgraded from version 1.27.47 to version 1.27.48"
+sed -i -e "s/version=0.6.0/version=0.6.1/g" python/python-databases/spkgbuild
+changelog "python/python-databases/spkgbuild" "Upgraded from version 0.6.0 to version 0.6.1"
+sed -i -e "s/version=2.2.1/version=2.2.2/g" python/python-flask/spkgbuild
+changelog "python/python-flask/spkgbuild" "Upgraded from version 2.2.1 to version 2.2.2"
+sed -i -e "s/version=2.55.0/version=2.56.0/g" python/python-google-api-python-client/spkgbuild
+changelog "python/python-google-api-python-client/spkgbuild" "Upgraded from version 2.55.0 to version 2.56.0"
+sed -i -e "s/version=2.2.1/version=2.2.2/g" python/python-werkzeug/spkgbuild
+changelog "python/python-werkzeug/spkgbuild" "Upgraded from version 2.2.1 to version 2.2.2"
