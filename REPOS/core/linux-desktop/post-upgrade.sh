4cb669418e50ce7bd6e653e19ea06f014988875f
Voncloft <voncloft@gmail.com>
2022-08-12 16:35:33 -0400

    fixed kernel stuff

--- REPOS/core/linux-desktop/post-upgrade.sh
+++ REPOS/core/linux-desktop/post-upgrade.sh
@@ -3,3 +3,6 @@
 grub-mkconfig -o /boot/grub/grub.cfg
 #sudo scratch install -rc nvidia-modules nvidia nvidia-32 virtualbox-modules
 #sudo dkms autoinstall -k $kernel_version
+cd /usr/src/vbox*
+make
+make install
