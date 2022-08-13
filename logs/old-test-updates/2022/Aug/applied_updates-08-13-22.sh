107aef53c4003e3c57aa3b2692ff4f72d0b16b8c
Voncloft <voncloft@gmail.com>
2022-08-13 18:06:27 -0400

    Minor Updates

---
+++ logs/old-test-updates/2022/Aug/applied_updates-08-13-22.sh
@@ -0,0 +1,12 @@
+sed -i -e "s/version=5.4.9/version=5.4.10/g" cinnamon/cinnamon/spkgbuild
+changelog "cinnamon/cinnamon/spkgbuild" "Upgraded from version 5.4.9 to version 5.4.10"
+sed -i -e "s/version=5.4.2/version=5.4.3/g" cinnamon/nemo/spkgbuild
+changelog "cinnamon/nemo/spkgbuild" "Upgraded from version 5.4.2 to version 5.4.3"
+sed -i -e "s/version=0.63.0/version=0.63.1/g" compilers/meson/spkgbuild
+changelog "compilers/meson/spkgbuild" "Upgraded from version 0.63.0 to version 0.63.1"
+sed -i -e "s/version=2.4.3/version=2.4.4/g" core/sysklogd/spkgbuild
+changelog "core/sysklogd/spkgbuild" "Upgraded from version 2.4.3 to version 2.4.4"
+sed -i -e "s/version=2.9.11/version=2.9.12/g" python/python-cloudflare/spkgbuild
+changelog "python/python-cloudflare/spkgbuild" "Upgraded from version 2.9.11 to version 2.9.12"
+sed -i -e "s/version=2022.8.5/version=2022.8.13/g" python/python-pipenv/spkgbuild
+changelog "python/python-pipenv/spkgbuild" "Upgraded from version 2022.8.5 to version 2022.8.13"
