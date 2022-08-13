aa19fc955caacd70c534fa43e1eec834716835d5
Voncloft <voncloft@gmail.com>
2022-08-12 21:22:26 -0400

    w2i8fk7j

--- logs/old-test-updates/2022/Aug/applied_updates-08-12-22.sh
+++ logs/old-test-updates/2022/Aug/applied_updates-08-12-22.sh
@@ -72,3 +72,41 @@ sed -i -e "s/version=2022.8.2/version=2022.8.3/g" server/homeassistant/spkgbuild
 changelog "server/homeassistant/spkgbuild" "Upgraded from version 2022.8.2 to version 2022.8.3"
 sed -i -e "s/version=2022.1/version=2022.2/g" xorg/xorgproto/spkgbuild
 changelog "xorg/xorgproto/spkgbuild" "Upgraded from version 2022.1 to version 2022.2"
+sed -i -e "s/version=2.4.2/version=2.4.3/g" core/sysklogd/spkgbuild
+changelog "core/sysklogd/spkgbuild" "Upgraded from version 2.4.2 to version 2.4.3"
+sed -i -e "s/version=5.2.5/version=5.2.6/g" core/xz/spkgbuild
+changelog "core/xz/spkgbuild" "Upgraded from version 5.2.5 to version 5.2.6"
+sed -i -e "s/version=2.1.3/version=2.1.4/g" libs/libjpeg-turbo/spkgbuild
+changelog "libs/libjpeg-turbo/spkgbuild" "Upgraded from version 2.1.3 to version 2.1.4"
+sed -i -e "s/version=104.0b8/version=104.0b9/g" networking/firefox/spkgbuild
+changelog "networking/firefox/spkgbuild" "Upgraded from version 104.0b8 to version 104.0b9"
+sed -i -e "s/version=0.4.7.9/version=0.4.7.10/g" networking/tor/spkgbuild
+changelog "networking/tor/spkgbuild" "Upgraded from version 0.4.7.9 to version 0.4.7.10"
+sed -i -e "s/version=1.24.50/version=1.24.51/g" python/python-boto3/spkgbuild
+changelog "python/python-boto3/spkgbuild" "Upgraded from version 1.24.50 to version 1.24.51"
+sed -i -e "s/version=1.27.50/version=1.27.51/g" python/python-botocore/spkgbuild
+changelog "python/python-botocore/spkgbuild" "Upgraded from version 1.27.50 to version 1.27.51"
+sed -i -e "s/version=3.0.1/version=3.0.2/g" python/python-elementpath/spkgbuild
+changelog "python/python-elementpath/spkgbuild" "Upgraded from version 3.0.1 to version 3.0.2"
+sed -i -e "s/version=13.15.1/version=14.0.0/g" python/python-faker/spkgbuild
+changelog "python/python-faker/spkgbuild" "Upgraded from version 13.15.1 to version 14.0.0"
+sed -i -e "s/version=1.7.3/version=1.8.0/g" python/python-gssapi/spkgbuild
+changelog "python/python-gssapi/spkgbuild" "Upgraded from version 1.7.3 to version 1.8.0"
+sed -i -e "s/version=6.54.2/version=6.54.3/g" python/python-hypothesis/spkgbuild
+changelog "python/python-hypothesis/spkgbuild" "Upgraded from version 6.54.2 to version 6.54.3"
+sed -i -e "s/version=12.0.2/version=12.1.0/g" python/python-pdoc/spkgbuild
+changelog "python/python-pdoc/spkgbuild" "Upgraded from version 12.0.2 to version 12.1.0"
+sed -i -e "s/version=7.3.2208031847/version=7.3.2208121110/g" python/python-pycontracts/spkgbuild
+changelog "python/python-pycontracts/spkgbuild" "Upgraded from version 7.3.2208031847 to version 7.3.2208121110"
+sed -i -e "s/version=64.0.0/version=64.0.3/g" python/python-setuptools/spkgbuild
+changelog "python/python-setuptools/spkgbuild" "Upgraded from version 64.0.0 to version 64.0.3"
+sed -i -e "s/version=6.4.0/version=6.4.1/g" python/python-traits/spkgbuild
+changelog "python/python-traits/spkgbuild" "Upgraded from version 6.4.0 to version 6.4.1"
+sed -i -e "s/version=2.0.1/version=2.0.2/g" python/python-xmlschema/spkgbuild
+changelog "python/python-xmlschema/spkgbuild" "Upgraded from version 2.0.1 to version 2.0.2"
+sed -i -e "s/version=3.11.5/version=3.11.6/g" server/blacklist-hosts/spkgbuild
+changelog "server/blacklist-hosts/spkgbuild" "Upgraded from version 3.11.5 to version 3.11.6"
+sed -i -e "s/version=2022.8.3/version=2022.8.4/g" server/homeassistant/spkgbuild
+changelog "server/homeassistant/spkgbuild" "Upgraded from version 2022.8.3 to version 2022.8.4"
+sed -i -e "s/version=5.18.0/version=5.19.0/g" xfce/xfsprogs/spkgbuild
+changelog "xfce/xfsprogs/spkgbuild" "Upgraded from version 5.18.0 to version 5.19.0"
