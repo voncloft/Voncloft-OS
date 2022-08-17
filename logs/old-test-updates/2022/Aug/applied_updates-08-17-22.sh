314264d90f165fa910f85b041e4b24c81759a603
Voncloft <voncloft@gmail.com>
2022-08-17 10:18:47 -0400

    Minor Updates

---
+++ logs/old-test-updates/2022/Aug/applied_updates-08-17-22.sh
@@ -0,0 +1,26 @@
+sed -i -e "s/version=5.18.1/version=5.19/g" core/btrfs-progs/spkgbuild
+changelog "core/btrfs-progs/spkgbuild" "Upgraded from version 5.18.1 to version 5.19"
+sed -i -e "s/version=1.9.3/version=1.9.4/g" core/lz4/spkgbuild
+changelog "core/lz4/spkgbuild" "Upgraded from version 1.9.3 to version 1.9.4"
+sed -i -e "s/version=104.0b3/version=104.0b4/g" networking/thunderbird/spkgbuild
+changelog "networking/thunderbird/spkgbuild" "Upgraded from version 104.0b3 to version 104.0b4"
+sed -i -e "s/version=5.1.0/version=5.2.0/g" python/python-apptools/spkgbuild
+changelog "python/python-apptools/spkgbuild" "Upgraded from version 5.1.0 to version 5.2.0"
+sed -i -e "s/version=1.24.52/version=1.24.53/g" python/python-boto3/spkgbuild
+changelog "python/python-boto3/spkgbuild" "Upgraded from version 1.24.52 to version 1.24.53"
+sed -i -e "s/version=1.27.52/version=1.27.53/g" python/python-botocore/spkgbuild
+changelog "python/python-botocore/spkgbuild" "Upgraded from version 1.27.52 to version 1.27.53"
+sed -i -e "s/version=6.4.3/version=6.4.4/g" python/python-coverage/spkgbuild
+changelog "python/python-coverage/spkgbuild" "Upgraded from version 6.4.3 to version 6.4.4"
+sed -i -e "s/version=2.56.0/version=2.57.0/g" python/python-google-api-python-client/spkgbuild
+changelog "python/python-google-api-python-client/spkgbuild" "Upgraded from version 2.56.0 to version 2.57.0"
+sed -i -e "s/version=1.8.0/version=1.8.1/g" python/python-gssapi/spkgbuild
+changelog "python/python-gssapi/spkgbuild" "Upgraded from version 1.8.0 to version 1.8.1"
+sed -i -e "s/version=4.9.1/version=4.10.0/g" python/python-jsonschema/spkgbuild
+changelog "python/python-jsonschema/spkgbuild" "Upgraded from version 4.9.1 to version 4.10.0"
+sed -i -e "s/version=3.10.6/version=3.10.7/g" python/python-mocket/spkgbuild
+changelog "python/python-mocket/spkgbuild" "Upgraded from version 3.10.6 to version 3.10.7"
+sed -i -e "s/version=65.0.0/version=65.0.1/g" python/python-setuptools/spkgbuild
+changelog "python/python-setuptools/spkgbuild" "Upgraded from version 65.0.0 to version 65.0.1"
+sed -i -e "s/version=2022.8.4/version=2022.8.5/g" server/homeassistant/spkgbuild
+changelog "server/homeassistant/spkgbuild" "Upgraded from version 2022.8.4 to version 2022.8.5"
