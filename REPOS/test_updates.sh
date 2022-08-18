9e45176ee4cd910e552c381c5c0513ffc6900165
Voncloft <voncloft@gmail.com>
2022-08-18 06:50:50 -0400

    added lsof

---
+++ REPOS/test_updates.sh
@@ -0,0 +1,38 @@
+sed -i -e "s/version=0.4.5.1/version=0.4.5.2/g" cinnamon/timezonemap/spkgbuild
+changelog "cinnamon/timezonemap/spkgbuild" "Upgraded from version 0.4.5.1 to version 0.4.5.2"
+sed -i -e "s/version=1.9.2/version=1.9.3/g" compilers/pkgconf/spkgbuild
+changelog "compilers/pkgconf/spkgbuild" "Upgraded from version 1.9.2 to version 1.9.3"
+sed -i -e "s/version=5.19.1/version=5.19.2/g" core/linux-desktop/spkgbuild
+changelog "core/linux-desktop/spkgbuild" "Upgraded from version 5.19.1 to version 5.19.2"
+sed -i -e "s/version=5.19.1/version=5.19.2/g" core/linux-router/spkgbuild
+changelog "core/linux-router/spkgbuild" "Upgraded from version 5.19.1 to version 5.19.2"
+sed -i -e "s/version=9.19.3/version=9.19.4/g" networking/bind/spkgbuild
+changelog "networking/bind/spkgbuild" "Upgraded from version 9.19.3 to version 9.19.4"
+sed -i -e "s/version=3.7.2/version=4.0.0rc1/g" networking/wireshark/spkgbuild
+changelog "networking/wireshark/spkgbuild" "Upgraded from version 3.7.2 to version 4.0.0rc1"
+sed -i -e "s/version=1.49.0/version=1.50.0/g" python/python-aws-sam-translator/spkgbuild
+changelog "python/python-aws-sam-translator/spkgbuild" "Upgraded from version 1.49.0 to version 1.50.0"
+sed -i -e "s/version=1.24.53/version=1.24.54/g" python/python-boto3/spkgbuild
+changelog "python/python-boto3/spkgbuild" "Upgraded from version 1.24.53 to version 1.24.54"
+sed -i -e "s/version=1.27.53/version=1.27.54/g" python/python-botocore/spkgbuild
+changelog "python/python-botocore/spkgbuild" "Upgraded from version 1.27.53 to version 1.27.54"
+sed -i -e "s/version=14.0.0/version=14.1.0/g" python/python-faker/spkgbuild
+changelog "python/python-faker/spkgbuild" "Upgraded from version 14.0.0 to version 14.1.0"
+sed -i -e "s/version=4.35.0/version=4.36.0/g" python/python-fonttools/spkgbuild
+changelog "python/python-fonttools/spkgbuild" "Upgraded from version 4.35.0 to version 4.36.0"
+sed -i -e "s/version=1.17.0/version=1.18.0/g" python/python-hcloud/spkgbuild
+changelog "python/python-hcloud/spkgbuild" "Upgraded from version 1.17.0 to version 1.18.0"
+sed -i -e "s/version=4.10.0/version=4.10.2/g" python/python-jsonschema/spkgbuild
+changelog "python/python-jsonschema/spkgbuild" "Upgraded from version 4.10.0 to version 4.10.2"
+sed -i -e "s/version=0.14.0/version=0.14.2/g" python/python-libtmux/spkgbuild
+changelog "python/python-libtmux/spkgbuild" "Upgraded from version 0.14.0 to version 0.14.2"
+sed -i -e "s/version=2022.8.15/version=2022.8.17/g" python/python-pipenv/spkgbuild
+changelog "python/python-pipenv/spkgbuild" "Upgraded from version 2022.8.15 to version 2022.8.17"
+sed -i -e "s/version=65.0.1/version=65.0.2/g" python/python-setuptools/spkgbuild
+changelog "python/python-setuptools/spkgbuild" "Upgraded from version 65.0.1 to version 65.0.2"
+sed -i -e "s/version=0.7.1/version=0.8.0/g" python/python-typing_inspect/spkgbuild
+changelog "python/python-typing_inspect/spkgbuild" "Upgraded from version 0.7.1 to version 0.8.0"
+sed -i -e "s/version=3.11.6/version=3.11.7/g" server/blacklist-hosts/spkgbuild
+changelog "server/blacklist-hosts/spkgbuild" "Upgraded from version 3.11.6 to version 3.11.7"
+sed -i -e "s/version=1.28.0.5999/version=1.28.1.6092/g" server/plex-media-server/spkgbuild
+changelog "server/plex-media-server/spkgbuild" "Upgraded from version 1.28.0.5999 to version 1.28.1.6092"
