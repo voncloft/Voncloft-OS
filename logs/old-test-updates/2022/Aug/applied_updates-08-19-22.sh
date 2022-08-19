8e6beaaf640292617b32e413f38bcd60a7b1605e
Voncloft <voncloft@gmail.com>
2022-08-19 16:32:19 -0400

    Minor Updates

---
+++ logs/old-test-updates/2022/Aug/applied_updates-08-19-22.sh
@@ -0,0 +1,22 @@
+sed -i -e "s/version=2.37.1/version=2.37.90/g" core/webkitgtk/spkgbuild
+changelog "core/webkitgtk/spkgbuild" "Upgraded from version 2.37.1 to version 2.37.90"
+sed -i -e "s/version=2.0.22/version=2.24.0/g" libs/sdl2/spkgbuild
+changelog "libs/sdl2/spkgbuild" "Upgraded from version 2.0.22 to version 2.24.0"
+sed -i -e "s/version=2022.08.14/version=2022.08.19/g" networking/yt-dlp/spkgbuild
+changelog "networking/yt-dlp/spkgbuild" "Upgraded from version 2022.08.14 to version 2022.08.19"
+sed -i -e "s/version=2.19/version=2.19.1/g" nonfree/pandoc-bin/spkgbuild
+changelog "nonfree/pandoc-bin/spkgbuild" "Upgraded from version 2.19 to version 2.19.1"
+sed -i -e "s/version=0.61.5/version=0.62.0/g" python/python-cfn-lint/spkgbuild
+changelog "python/python-cfn-lint/spkgbuild" "Upgraded from version 0.61.5 to version 0.62.0"
+sed -i -e "s/version=0.79.0/version=0.79.1/g" python/python-fastapi/spkgbuild
+changelog "python/python-fastapi/spkgbuild" "Upgraded from version 0.79.0 to version 0.79.1"
+sed -i -e "s/version=8.0.1/version=8.0.2/g" python/python-ipywidgets/spkgbuild
+changelog "python/python-ipywidgets/spkgbuild" "Upgraded from version 8.0.1 to version 8.0.2"
+sed -i -e "s/version=4.12.1/version=4.13.0/g" python/python-jsonschema/spkgbuild
+changelog "python/python-jsonschema/spkgbuild" "Upgraded from version 4.12.1 to version 4.13.0"
+sed -i -e "s/version=8.12.53/version=8.12.54/g" python/python-phonenumbers/spkgbuild
+changelog "python/python-phonenumbers/spkgbuild" "Upgraded from version 8.12.53 to version 8.12.54"
+sed -i -e "s/version=2022.8.17/version=2022.8.19/g" python/python-pipenv/spkgbuild
+changelog "python/python-pipenv/spkgbuild" "Upgraded from version 2022.8.17 to version 2022.8.19"
+sed -i -e "s/version=6.1.0/version=6.1.1/g" python/python-softlayer/spkgbuild
+changelog "python/python-softlayer/spkgbuild" "Upgraded from version 6.1.0 to version 6.1.1"
