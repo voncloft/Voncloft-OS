6d9474998c952b73b693f35acf290fb197d30c94
Voncloft <voncloft@gmail.com>
2022-08-22 07:12:21 -0400

    Minor Updates

---
+++ REPOS/test_updates-08-21-22.sh
@@ -0,0 +1,18 @@
+sed -i -e "s/version=1.24.55/version=1.24.56/g" python/python-boto3/spkgbuild
+changelog "python/python-boto3/spkgbuild" "Upgraded from version 1.24.55 to version 1.24.56"
+sed -i -e "s/version=1.27.55/version=1.27.56/g" python/python-botocore/spkgbuild
+changelog "python/python-botocore/spkgbuild" "Upgraded from version 1.27.55 to version 1.27.56"
+sed -i -e "s/version=1.6.0/version=1.6.1/g" python/python-cpplint/spkgbuild
+changelog "python/python-cpplint/spkgbuild" "Upgraded from version 1.6.0 to version 1.6.1"
+sed -i -e "s/version=2022.8.0/version=2022.8.1/g" python/python-distributed/spkgbuild
+changelog "python/python-distributed/spkgbuild" "Upgraded from version 2022.8.0 to version 2022.8.1"
+sed -i -e "s/version=2.10.0/version=2.11.0/g" python/python-google-auth/spkgbuild
+changelog "python/python-google-auth/spkgbuild" "Upgraded from version 2.10.0 to version 2.11.0"
+sed -i -e "s/version=6.54.3/version=6.54.4/g" python/python-hypothesis/spkgbuild
+changelog "python/python-hypothesis/spkgbuild" "Upgraded from version 6.54.3 to version 6.54.4"
+sed -i -e "s/version=3.11.7/version=3.11.8/g" server/blacklist-hosts/spkgbuild
+changelog "server/blacklist-hosts/spkgbuild" "Upgraded from version 3.11.7 to version 3.11.8"
+sed -i -e "s/version=4.17.2/version=4.17.3/g" xfce/xfce4-panel/spkgbuild
+changelog "xfce/xfce4-panel/spkgbuild" "Upgraded from version 4.17.2 to version 4.17.3"
+sed -i -e "s/version=4.16.0/version=4.17.0/g" xfce/xfdesktop/spkgbuild
+changelog "xfce/xfdesktop/spkgbuild" "Upgraded from version 4.16.0 to version 4.17.0"
