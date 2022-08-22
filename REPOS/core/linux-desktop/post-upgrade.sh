87332d44268bf707e1897c776303756b47d1247e
Voncloft <voncloft@gmail.com>
2022-08-22 10:07:06 -0400

    fix kernel upgrade to install modules in post-upgrade

--- REPOS/core/linux-desktop/post-upgrade.sh
+++ REPOS/core/linux-desktop/post-upgrade.sh
@@ -1,6 +1,14 @@
 #!/bin/sh
 #kernel_version=$(ls /usr/lib/modules)
 grub-mkconfig -o /boot/grub/grub.cfg
+version=$(cat /etc/LINUX_VERSION)
+#virtualbox modules
+cd /usr/src/virtual*
+KER_VER=$version make
+KER_VER=$version make install
+
+#virtualbox
+sudo dkms install -m nvidia -v $version
 #sudo scratch install -rc nvidia-modules nvidia nvidia-32 virtualbox-modules
 #sudo dkms autoinstall -k $kernel_version
 #cd /usr/src/vbox*
