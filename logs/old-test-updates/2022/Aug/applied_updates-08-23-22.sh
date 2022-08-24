6e29db36cdfbcc465eb6e11eea9a841ef3d3edd7
Voncloft <voncloft@gmail.com>
2022-08-23 22:00:24 -0400

    Minor Updates

---
+++ logs/old-test-updates/2022/Aug/applied_updates-08-23-22.sh
@@ -0,0 +1,32 @@
+sed -i -e "s/version=4.6.1/version=4.6.2/g" compilers/ccache/spkgbuild
+changelog "compilers/ccache/spkgbuild" "Upgraded from version 4.6.1 to version 4.6.2"
+sed -i -e "s/version=0.1.94/version=0.1.96/g" libs/babl/spkgbuild
+changelog "libs/babl/spkgbuild" "Upgraded from version 0.1.94 to version 0.1.96"
+sed -i -e "s/version=4.18.0/version=4.19.0/g" libs/libtasn1/spkgbuild
+changelog "libs/libtasn1/spkgbuild" "Upgraded from version 4.18.0 to version 4.19.0"
+sed -i -e "s/version=1.13.3/version=1.13.90/g" libs/libwpe/spkgbuild
+changelog "libs/libwpe/spkgbuild" "Upgraded from version 1.13.3 to version 1.13.90"
+sed -i -e "s/version=104.0b9/version=105.0b1/g" networking/firefox/spkgbuild
+changelog "networking/firefox/spkgbuild" "Upgraded from version 104.0b9 to version 105.0b1"
+sed -i -e "s/version=2.12.2/version=2.12.3/g" python/python-astroid/spkgbuild
+changelog "python/python-astroid/spkgbuild" "Upgraded from version 2.12.2 to version 2.12.3"
+sed -i -e "s/version=2.14.0/version=2.14.1/g" python/python-connexion/spkgbuild
+changelog "python/python-connexion/spkgbuild" "Upgraded from version 2.14.0 to version 2.14.1"
+sed -i -e "s/version=0.79.1/version=0.80.0/g" python/python-fastapi/spkgbuild
+changelog "python/python-fastapi/spkgbuild" "Upgraded from version 0.79.1 to version 0.80.0"
+sed -i -e "s/version=4.36.0/version=4.37.0/g" python/python-fonttools/spkgbuild
+changelog "python/python-fonttools/spkgbuild" "Upgraded from version 4.36.0 to version 4.37.0"
+sed -i -e "s/version=2.57.0/version=2.58.0/g" python/python-google-api-python-client/spkgbuild
+changelog "python/python-google-api-python-client/spkgbuild" "Upgraded from version 2.57.0 to version 2.58.0"
+sed -i -e "s/version=3.10.7/version=3.10.8/g" python/python-mocket/spkgbuild
+changelog "python/python-mocket/spkgbuild" "Upgraded from version 3.10.7 to version 3.10.8"
+sed -i -e "s/version=1.5.0/version=1.6.0/g" python/python-pandas/spkgbuild
+changelog "python/python-pandas/spkgbuild" "Upgraded from version 1.5.0 to version 1.6.0"
+sed -i -e "s/version=1.10/version=1.10.0/g" python/python-pydantic/spkgbuild
+changelog "python/python-pydantic/spkgbuild" "Upgraded from version 1.10 to version 1.10.0"
+sed -i -e "s/version=3.13.5/version=3.14.0/g" python/python-trimesh/spkgbuild
+changelog "python/python-trimesh/spkgbuild" "Upgraded from version 3.13.5 to version 3.14.0"
+sed -i -e "s/version=4.10.0/version=4.10.1/g" python/python-tweepy/spkgbuild
+changelog "python/python-tweepy/spkgbuild" "Upgraded from version 4.10.0 to version 4.10.1"
+sed -i -e "s/version=4.4.3.1/version=4.4.4/g" qt/qbittorrent/spkgbuild
+changelog "qt/qbittorrent/spkgbuild" "Upgraded from version 4.4.3.1 to version 4.4.4"
