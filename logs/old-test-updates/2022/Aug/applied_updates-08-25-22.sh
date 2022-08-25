9059d61ad89b1a2006a315f4c8fa242c075d3aee
Voncloft <voncloft@gmail.com>
2022-08-25 11:02:47 -0400

    Minor Updates

---
+++ logs/old-test-updates/2022/Aug/applied_updates-08-25-22.sh
@@ -0,0 +1,32 @@
+sed -i -e "s/version=9.56.1/version=10.0.0/g" core/ghostscript/spkgbuild
+changelog "core/ghostscript/spkgbuild" "Upgraded from version 9.56.1 to version 10.0.0"
+sed -i -e "s/version=1.2.0/version=1.2.1/g" core/pinentry/spkgbuild
+changelog "core/pinentry/spkgbuild" "Upgraded from version 1.2.0 to version 1.2.1"
+sed -i -e "s/version=105.0b1/version=105.0b2/g" networking/firefox/spkgbuild
+changelog "networking/firefox/spkgbuild" "Upgraded from version 105.0b1 to version 105.0b2"
+sed -i -e "s/version=3.2.2/version=4.0.0/g" python/python-bcrypt/spkgbuild
+changelog "python/python-bcrypt/spkgbuild" "Upgraded from version 3.2.2 to version 4.0.0"
+sed -i -e "s/version=1.24.57/version=1.24.58/g" python/python-boto3/spkgbuild
+changelog "python/python-boto3/spkgbuild" "Upgraded from version 1.24.57 to version 1.24.58"
+sed -i -e "s/version=1.27.57/version=1.27.58/g" python/python-botocore/spkgbuild
+changelog "python/python-botocore/spkgbuild" "Upgraded from version 1.27.57 to version 1.27.58"
+sed -i -e "s/version=8.3.3/version=8.4.0/g" python/python-elasticsearch/spkgbuild
+changelog "python/python-elasticsearch/spkgbuild" "Upgraded from version 8.3.3 to version 8.4.0"
+sed -i -e "s/version=5.2.0/version=5.3.0/g" python/python-flask-socketio/spkgbuild
+changelog "python/python-flask-socketio/spkgbuild" "Upgraded from version 5.2.0 to version 5.3.0"
+sed -i -e "s/version=4.37.0/version=4.37.1/g" python/python-fonttools/spkgbuild
+changelog "python/python-fonttools/spkgbuild" "Upgraded from version 4.37.0 to version 4.37.1"
+sed -i -e "s/version=0.100.0/version=0.101.0/g" python/python-openstacksdk/spkgbuild
+changelog "python/python-openstacksdk/spkgbuild" "Upgraded from version 0.100.0 to version 0.101.0"
+sed -i -e "s/version=2.6.1/version=2.6.2/g" python/python-osc-lib/spkgbuild
+changelog "python/python-osc-lib/spkgbuild" "Upgraded from version 2.6.1 to version 2.6.2"
+sed -i -e "s/version=6.0.0/version=6.0.1/g" python/python-oslo-utils/spkgbuild
+changelog "python/python-oslo-utils/spkgbuild" "Upgraded from version 6.0.0 to version 6.0.1"
+sed -i -e "s/version=1.6.9/version=2.0.0/g" python/python-requirementslib/spkgbuild
+changelog "python/python-requirementslib/spkgbuild" "Upgraded from version 1.6.9 to version 2.0.0"
+sed -i -e "s/version=65.2.0/version=65.3.0/g" python/python-setuptools/spkgbuild
+changelog "python/python-setuptools/spkgbuild" "Upgraded from version 65.2.0 to version 65.3.0"
+sed -i -e "s/version=2.3.20/version=2.3.21/g" ruby-gems/ruby-bundler/spkgbuild
+changelog "ruby-gems/ruby-bundler/spkgbuild" "Upgraded from version 2.3.20 to version 2.3.21"
+sed -i -e "s/version=3.11.8/version=3.11.9/g" server/blacklist-hosts/spkgbuild
+changelog "server/blacklist-hosts/spkgbuild" "Upgraded from version 3.11.8 to version 3.11.9"
