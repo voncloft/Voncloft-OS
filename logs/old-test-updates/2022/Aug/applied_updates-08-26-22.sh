f75c57ee7d4c6df57f54befbdc60fb61caa120e5
Voncloft <voncloft@gmail.com>
2022-08-26 08:15:11 -0400

    Minor Updates

---
+++ logs/old-test-updates/2022/Aug/applied_updates-08-26-22.sh
@@ -0,0 +1,34 @@
+sed -i -e "s/version=1.5.16/version=1.5.17/g" cinnamon/system-config-printer/spkgbuild
+changelog "cinnamon/system-config-printer/spkgbuild" "Upgraded from version 1.5.16 to version 1.5.17"
+sed -i -e "s/version=5.19.3/version=5.19.4/g" core/linux-desktop/spkgbuild
+changelog "core/linux-desktop/spkgbuild" "Upgraded from version 5.19.3 to version 5.19.4"
+sed -i -e "s/version=5.19.3/version=5.19.4/g" core/linux-router/spkgbuild
+changelog "core/linux-router/spkgbuild" "Upgraded from version 5.19.3 to version 5.19.4"
+sed -i -e "s/version=7.6.12/version=7.6.14/g" libs/libatomic_ops/spkgbuild
+changelog "libs/libatomic_ops/spkgbuild" "Upgraded from version 7.6.12 to version 7.6.14"
+sed -i -e "s/version=4.0.2/version=4.0.4/g" libs/libimagequant/spkgbuild
+changelog "libs/libimagequant/spkgbuild" "Upgraded from version 4.0.2 to version 4.0.4"
+sed -i -e "s/version=2.1.2/version=2.3.1/g" libs/soundtouch/spkgbuild
+changelog "libs/soundtouch/spkgbuild" "Upgraded from version 2.1.2 to version 2.3.1"
+sed -i -e "s/version=104.0b4/version=105.0b1/g" networking/thunderbird/spkgbuild
+changelog "networking/thunderbird/spkgbuild" "Upgraded from version 104.0b4 to version 105.0b1"
+sed -i -e "s/version=2.3.4/version=2.4.0/g" python/python-aiobotocore/spkgbuild
+changelog "python/python-aiobotocore/spkgbuild" "Upgraded from version 2.3.4 to version 2.4.0"
+sed -i -e "s/version=2.12.3/version=2.12.4/g" python/python-astroid/spkgbuild
+changelog "python/python-astroid/spkgbuild" "Upgraded from version 2.12.3 to version 2.12.4"
+sed -i -e "s/version=1.24.58/version=1.24.59/g" python/python-boto3/spkgbuild
+changelog "python/python-boto3/spkgbuild" "Upgraded from version 1.24.58 to version 1.24.59"
+sed -i -e "s/version=1.27.58/version=1.27.59/g" python/python-botocore/spkgbuild
+changelog "python/python-botocore/spkgbuild" "Upgraded from version 1.27.58 to version 1.27.59"
+sed -i -e "s/version=0.62.0/version=0.63.0/g" python/python-cfn-lint/spkgbuild
+changelog "python/python-cfn-lint/spkgbuild" "Upgraded from version 0.62.0 to version 0.63.0"
+sed -i -e "s/version=1.0.4/version=1.0.5/g" python/python-itemloaders/spkgbuild
+changelog "python/python-itemloaders/spkgbuild" "Upgraded from version 1.0.4 to version 1.0.5"
+sed -i -e "s/version=3.2.0/version=3.3.0/g" python/python-mypy-protobuf/spkgbuild
+changelog "python/python-mypy-protobuf/spkgbuild" "Upgraded from version 3.2.0 to version 3.3.0"
+sed -i -e "s/version=2022.8.19/version=2022.8.24/g" python/python-pipenv/spkgbuild
+changelog "python/python-pipenv/spkgbuild" "Upgraded from version 2022.8.19 to version 2022.8.24"
+sed -i -e "s/version=0.23.2/version=0.24.0/g" python/python-pyudev/spkgbuild
+changelog "python/python-pyudev/spkgbuild" "Upgraded from version 0.23.2 to version 0.24.0"
+sed -i -e "s/version=2.0.2/version=2.0.3/g" python/python-xmlschema/spkgbuild
+changelog "python/python-xmlschema/spkgbuild" "Upgraded from version 2.0.2 to version 2.0.3"
