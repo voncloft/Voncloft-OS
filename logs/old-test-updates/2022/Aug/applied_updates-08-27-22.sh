6d439f12229f156963394077f993859803219bb6
Voncloft <voncloft@gmail.com>
2022-08-27 21:19:08 -0400

    Minor Updates

---
+++ logs/old-test-updates/2022/Aug/applied_updates-08-27-22.sh
@@ -0,0 +1,36 @@
+sed -i -e "s/version=4.6.2/version=4.6.3/g" compilers/ccache/spkgbuild
+changelog "compilers/ccache/spkgbuild" "Upgraded from version 4.6.2 to version 4.6.3"
+sed -i -e "s/version=8.2.0/version=8.2.2/g" core/gc/spkgbuild
+changelog "core/gc/spkgbuild" "Upgraded from version 8.2.0 to version 8.2.2"
+sed -i -e "s/version=2.35/version=2.35.1/g" extra/discordchatexporter/spkgbuild
+changelog "extra/discordchatexporter/spkgbuild" "Upgraded from version 2.35 to version 2.35.1"
+sed -i -e "s/version=9.0.0/version=9.1.0/g" libs/fmt/spkgbuild
+changelog "libs/fmt/spkgbuild" "Upgraded from version 9.0.0 to version 9.1.0"
+sed -i -e "s/version=1.1.4/version=1.1.5/g" libs/libfontenc/spkgbuild
+changelog "libs/libfontenc/spkgbuild" "Upgraded from version 1.1.4 to version 1.1.5"
+sed -i -e "s/version=105.0b2/version=105.0b3/g" networking/firefox/spkgbuild
+changelog "networking/firefox/spkgbuild" "Upgraded from version 105.0b2 to version 105.0b3"
+sed -i -e "s/version=6.1.36/version=7.0.0/g" nonfree/virtualbox-guest-modules/spkgbuild
+changelog "nonfree/virtualbox-guest-modules/spkgbuild" "Upgraded from version 6.1.36 to version 7.0.0"
+sed -i -e "s/version=6.1.36/version=7.0.0/g" nonfree/virtualbox-guest-utils/spkgbuild
+changelog "nonfree/virtualbox-guest-utils/spkgbuild" "Upgraded from version 6.1.36 to version 7.0.0"
+sed -i -e "s/version=0.11.0/version=0.11.1/g" python/python-bitcoinlib/spkgbuild
+changelog "python/python-bitcoinlib/spkgbuild" "Upgraded from version 0.11.0 to version 0.11.1"
+sed -i -e "s/version=1.24.59/version=1.24.61/g" python/python-boto3/spkgbuild
+changelog "python/python-boto3/spkgbuild" "Upgraded from version 1.24.59 to version 1.24.61"
+sed -i -e "s/version=1.27.59/version=1.27.61/g" python/python-botocore/spkgbuild
+changelog "python/python-botocore/spkgbuild" "Upgraded from version 1.27.59 to version 1.27.61"
+sed -i -e "s/version=1.2.60/version=1.2.62/g" python/python-cloudscraper/spkgbuild
+changelog "python/python-cloudscraper/spkgbuild" "Upgraded from version 1.2.60 to version 1.2.62"
+sed -i -e "s/version=0.80.0/version=0.81.0/g" python/python-fastapi/spkgbuild
+changelog "python/python-fastapi/spkgbuild" "Upgraded from version 0.80.0 to version 0.81.0"
+sed -i -e "s/version=1.3.5/version=1.4.0/g" python/python-mockito/spkgbuild
+changelog "python/python-mockito/spkgbuild" "Upgraded from version 1.3.5 to version 1.4.0"
+sed -i -e "s/version=3.7.12/version=3.8.0/g" python/python-orjson/spkgbuild
+changelog "python/python-orjson/spkgbuild" "Upgraded from version 3.7.12 to version 3.8.0"
+sed -i -e "s/version=3.15.1/version=3.15.2/g" python/python-peewee/spkgbuild
+changelog "python/python-peewee/spkgbuild" "Upgraded from version 3.15.1 to version 3.15.2"
+sed -i -e "s/version=3.11.9/version=3.11.10/g" server/blacklist-hosts/spkgbuild
+changelog "server/blacklist-hosts/spkgbuild" "Upgraded from version 3.11.9 to version 3.11.10"
+sed -i -e "s/version=2022.8.6/version=2022.8.7/g" server/homeassistant/spkgbuild
+changelog "server/homeassistant/spkgbuild" "Upgraded from version 2022.8.6 to version 2022.8.7"
