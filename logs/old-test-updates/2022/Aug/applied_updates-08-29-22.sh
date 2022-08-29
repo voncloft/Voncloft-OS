38fb4971886906c9054cb40bb48d2845ba38710a
Voncloft <voncloft@gmail.com>
2022-08-29 07:23:34 -0400

    Minor Updates

---
+++ logs/old-test-updates/2022/Aug/applied_updates-08-29-22.sh
@@ -0,0 +1,4 @@
+sed -i -e "s/version=5.19.4/version=5.19.5/g" core/linux-desktop/spkgbuild
+changelog "core/linux-desktop/spkgbuild" "Upgraded from version 5.19.4 to version 5.19.5"
+sed -i -e "s/version=5.19.4/version=5.19.5/g" core/linux-router/spkgbuild
+changelog "core/linux-router/spkgbuild" "Upgraded from version 5.19.4 to version 5.19.5"
