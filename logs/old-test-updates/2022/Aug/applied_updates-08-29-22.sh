c819342d649b496355becf03fb4c4f77b1112cda
Voncloft <voncloft@gmail.com>
2022-08-29 15:53:35 -0400

    Minor Updates

--- logs/old-test-updates/2022/Aug/applied_updates-08-29-22.sh
+++ logs/old-test-updates/2022/Aug/applied_updates-08-29-22.sh
@@ -2,3 +2,19 @@ sed -i -e "s/version=5.19.4/version=5.19.5/g" core/linux-desktop/spkgbuild
 changelog "core/linux-desktop/spkgbuild" "Upgraded from version 5.19.4 to version 5.19.5"
 sed -i -e "s/version=5.19.4/version=5.19.5/g" core/linux-router/spkgbuild
 changelog "core/linux-router/spkgbuild" "Upgraded from version 5.19.4 to version 5.19.5"
+sed -i -e "s/version=0.102.0/version=0.102.1/g" core/hugo/spkgbuild
+changelog "core/hugo/spkgbuild" "Upgraded from version 0.102.0 to version 0.102.1"
+sed -i -e "s/version=105.0b3/version=105.0b4/g" networking/firefox/spkgbuild
+changelog "networking/firefox/spkgbuild" "Upgraded from version 105.0b3 to version 105.0b4"
+sed -i -e "s/version=2.12.4/version=2.12.5/g" python/python-astroid/spkgbuild
+changelog "python/python-astroid/spkgbuild" "Upgraded from version 2.12.4 to version 2.12.5"
+sed -i -e "s/version=6.6.0/version=6.7.0/g" python/python-colorlog/spkgbuild
+changelog "python/python-colorlog/spkgbuild" "Upgraded from version 6.6.0 to version 6.7.0"
+sed -i -e "s/version=6.15.1/version=6.15.2/g" python/python-ipykernel/spkgbuild
+changelog "python/python-ipykernel/spkgbuild" "Upgraded from version 6.15.1 to version 6.15.2"
+sed -i -e "s/version=1.0.5/version=1.0.6/g" python/python-itemloaders/spkgbuild
+changelog "python/python-itemloaders/spkgbuild" "Upgraded from version 1.0.5 to version 1.0.6"
+sed -i -e "s/version=1.1.1/version=1.2.0/g" python/python-pycapnp/spkgbuild
+changelog "python/python-pycapnp/spkgbuild" "Upgraded from version 1.1.1 to version 1.2.0"
+sed -i -e "s/version=0.11.1/version=0.11.2/g" ruby-gems/ruby-rb-fsevent/spkgbuild
+changelog "ruby-gems/ruby-rb-fsevent/spkgbuild" "Upgraded from version 0.11.1 to version 0.11.2"
