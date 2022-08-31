8c531617d684eb221ee394633ea71c86c1cd8405
Voncloft <voncloft@gmail.com>
2022-08-30 20:55:10 -0400

    Minor Updates

---
+++ logs/old-test-updates/2022/Aug/applied_updates-08-30-22.sh
@@ -0,0 +1,38 @@
+sed -i -e "s/version=1.5.17/version=1.5.18/g" cinnamon/system-config-printer/spkgbuild
+changelog "cinnamon/system-config-printer/spkgbuild" "Upgraded from version 1.5.17 to version 1.5.18"
+sed -i -e "s/version=7.0.0/version=7.1.0/g" extra/qemu/spkgbuild
+changelog "extra/qemu/spkgbuild" "Upgraded from version 7.0.0 to version 7.1.0"
+sed -i -e "s/version=2.37.2/version=2.37.3/g" networking/git/spkgbuild
+changelog "networking/git/spkgbuild" "Upgraded from version 2.37.2 to version 2.37.3"
+sed -i -e "s/version=0.5.0/version=0.5.1/g" python/python-aioconsole/spkgbuild
+changelog "python/python-aioconsole/spkgbuild" "Upgraded from version 0.5.0 to version 0.5.1"
+sed -i -e "s/version=1.24.61/version=1.24.62/g" python/python-boto3/spkgbuild
+changelog "python/python-boto3/spkgbuild" "Upgraded from version 1.24.61 to version 1.24.62"
+sed -i -e "s/version=1.27.61/version=1.27.62/g" python/python-botocore/spkgbuild
+changelog "python/python-botocore/spkgbuild" "Upgraded from version 1.27.61 to version 1.27.62"
+sed -i -e "s/version=0.63.0/version=0.63.1/g" python/python-cfn-lint/spkgbuild
+changelog "python/python-cfn-lint/spkgbuild" "Upgraded from version 0.63.0 to version 0.63.1"
+sed -i -e "s/version=8.4.0/version=8.4.1/g" python/python-elasticsearch/spkgbuild
+changelog "python/python-elasticsearch/spkgbuild" "Upgraded from version 8.4.0 to version 8.4.1"
+sed -i -e "s/version=14.1.0/version=14.1.1/g" python/python-faker/spkgbuild
+changelog "python/python-faker/spkgbuild" "Upgraded from version 14.1.0 to version 14.1.1"
+sed -i -e "s/version=2022.7.1/version=2022.8.0/g" python/python-fsspec/spkgbuild
+changelog "python/python-fsspec/spkgbuild" "Upgraded from version 2022.7.1 to version 2022.8.0"
+sed -i -e "s/version=2.58.0/version=2.59.0/g" python/python-google-api-python-client/spkgbuild
+changelog "python/python-google-api-python-client/spkgbuild" "Upgraded from version 2.58.0 to version 2.59.0"
+sed -i -e "s/version=12.0.0/version=12.1.0/g" python/python-oslo-db/spkgbuild
+changelog "python/python-oslo-db/spkgbuild" "Upgraded from version 12.0.0 to version 12.1.0"
+sed -i -e "s/version=1.9.3/version=1.10.0/g" python/python-requests-mock/spkgbuild
+changelog "python/python-requests-mock/spkgbuild" "Upgraded from version 1.9.3 to version 1.10.0"
+sed -i -e "s/version=2.0.0/version=2.0.1/g" python/python-requirementslib/spkgbuild
+changelog "python/python-requirementslib/spkgbuild" "Upgraded from version 2.0.0 to version 2.0.1"
+sed -i -e "s/version=1.11/version=1.11.1/g" python/python-sympy/spkgbuild
+changelog "python/python-sympy/spkgbuild" "Upgraded from version 1.11 to version 1.11.1"
+sed -i -e "s/version=20.16.3/version=20.16.4/g" python/python-virtualenv/spkgbuild
+changelog "python/python-virtualenv/spkgbuild" "Upgraded from version 20.16.3 to version 20.16.4"
+sed -i -e "s/version=0.5.6/version=0.6.1/g" python/python-vistir/spkgbuild
+changelog "python/python-vistir/spkgbuild" "Upgraded from version 0.5.6 to version 0.6.1"
+sed -i -e "s/version=4.4.4/version=4.4.5/g" qt/qbittorrent/spkgbuild
+changelog "qt/qbittorrent/spkgbuild" "Upgraded from version 4.4.4 to version 4.4.5"
+sed -i -e "s/version=1.28.1.6104/version=1.28.2.6151/g" server/plex-media-server/spkgbuild
+changelog "server/plex-media-server/spkgbuild" "Upgraded from version 1.28.1.6104 to version 1.28.2.6151"
