1621b601b983fbcbbb01c0f4a014eef7ba51744e
Voncloft <voncloft@gmail.com>
2022-09-01 06:28:16 -0400

    Minor Updates

---
+++ logs/old-test-updates/2022/Sep/applied_updates-09-01-22.sh
@@ -0,0 +1,42 @@
+sed -i -e "s/version=1.11.0/version=1.11.1/g" compilers/ninja/spkgbuild
+changelog "compilers/ninja/spkgbuild" "Upgraded from version 1.11.0 to version 1.11.1"
+sed -i -e "s/version=7.84.0/version=7.85.0/g" core/curl/spkgbuild
+changelog "core/curl/spkgbuild" "Upgraded from version 7.84.0 to version 7.85.0"
+sed -i -e "s/version=0.102.1/version=0.102.2/g" core/hugo/spkgbuild
+changelog "core/hugo/spkgbuild" "Upgraded from version 0.102.1 to version 0.102.2"
+sed -i -e "s/version=5.19.5/version=5.19.6/g" core/linux-desktop/spkgbuild
+changelog "core/linux-desktop/spkgbuild" "Upgraded from version 5.19.5 to version 5.19.6"
+sed -i -e "s/version=5.19.5/version=5.19.6/g" core/linux-router/spkgbuild
+changelog "core/linux-router/spkgbuild" "Upgraded from version 5.19.5 to version 5.19.6"
+sed -i -e "s/version=2.4.112/version=2.4.113/g" libs/libdrm/spkgbuild
+changelog "libs/libdrm/spkgbuild" "Upgraded from version 2.4.112 to version 2.4.113"
+sed -i -e "s/version=1.1.5/version=1.1.6/g" libs/libfontenc/spkgbuild
+changelog "libs/libfontenc/spkgbuild" "Upgraded from version 1.1.5 to version 1.1.6"
+sed -i -e "s/version=105.0b4/version=105.0b5/g" networking/firefox/spkgbuild
+changelog "networking/firefox/spkgbuild" "Upgraded from version 105.0b4 to version 105.0b5"
+sed -i -e "s/version=1.24.62/version=1.24.64/g" python/python-boto3/spkgbuild
+changelog "python/python-boto3/spkgbuild" "Upgraded from version 1.24.62 to version 1.24.64"
+sed -i -e "s/version=1.27.62/version=1.27.64/g" python/python-botocore/spkgbuild
+changelog "python/python-botocore/spkgbuild" "Upgraded from version 1.27.62 to version 1.27.64"
+sed -i -e "s/version=0.63.1/version=0.63.2/g" python/python-cfn-lint/spkgbuild
+changelog "python/python-cfn-lint/spkgbuild" "Upgraded from version 0.63.1 to version 0.63.2"
+sed -i -e "s/version=9.0.0/version=9.1.0/g" python/python-cinderclient/spkgbuild
+changelog "python/python-cinderclient/spkgbuild" "Upgraded from version 9.0.0 to version 9.1.0"
+sed -i -e "s/version=14.1.1/version=14.2.0/g" python/python-faker/spkgbuild
+changelog "python/python-faker/spkgbuild" "Upgraded from version 14.1.1 to version 14.2.0"
+sed -i -e "s/version=2022.8.0/version=2022.8.1/g" python/python-fsspec/spkgbuild
+changelog "python/python-fsspec/spkgbuild" "Upgraded from version 2022.8.0 to version 2022.8.1"
+sed -i -e "s/version=4.14.0/version=4.15.0/g" python/python-jsonschema/spkgbuild
+changelog "python/python-jsonschema/spkgbuild" "Upgraded from version 4.14.0 to version 4.15.0"
+sed -i -e "s/version=8.0.0/version=8.1.0/g" python/python-neutronclient/spkgbuild
+changelog "python/python-neutronclient/spkgbuild" "Upgraded from version 8.0.0 to version 8.1.0"
+sed -i -e "s/version=2022.8.24/version=2022.8.30/g" python/python-pipenv/spkgbuild
+changelog "python/python-pipenv/spkgbuild" "Upgraded from version 2022.8.24 to version 2022.8.30"
+sed -i -e "s/version=1.10.0/version=1.10.1/g" python/python-pydantic/spkgbuild
+changelog "python/python-pydantic/spkgbuild" "Upgraded from version 1.10.0 to version 1.10.1"
+sed -i -e "s/version=4.2.0/version=4.2.1/g" python/python-vcrpy/spkgbuild
+changelog "python/python-vcrpy/spkgbuild" "Upgraded from version 4.2.0 to version 4.2.1"
+sed -i -e "s/version=3.11.10/version=3.11.11/g" server/blacklist-hosts/spkgbuild
+changelog "server/blacklist-hosts/spkgbuild" "Upgraded from version 3.11.10 to version 3.11.11"
+sed -i -e "s/version=2022.8.7/version=2022.9.0/g" server/homeassistant/spkgbuild
+changelog "server/homeassistant/spkgbuild" "Upgraded from version 2022.8.7 to version 2022.9.0"
