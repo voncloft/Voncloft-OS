3ee4fe6ad90216649a9ab5b5fe1b2c1305a67fd5
Voncloft <voncloft@gmail.com>
2022-09-01 23:53:50 -0400

    Minor Updates

--- logs/old-test-updates/2022/Sep/applied_updates-09-01-22.sh
+++ logs/old-test-updates/2022/Sep/applied_updates-09-01-22.sh
@@ -40,3 +40,35 @@ sed -i -e "s/version=3.11.10/version=3.11.11/g" server/blacklist-hosts/spkgbuild
 changelog "server/blacklist-hosts/spkgbuild" "Upgraded from version 3.11.10 to version 3.11.11"
 sed -i -e "s/version=2022.8.7/version=2022.9.0/g" server/homeassistant/spkgbuild
 changelog "server/homeassistant/spkgbuild" "Upgraded from version 2022.8.7 to version 2022.9.0"
+sed -i -e "s/version=0.102.2/version=0.102.3/g" core/hugo/spkgbuild
+changelog "core/hugo/spkgbuild" "Upgraded from version 0.102.2 to version 0.102.3"
+sed -i -e "s/version=6.2.1/version=6.2.2/g" core/mercurial/spkgbuild
+changelog "core/mercurial/spkgbuild" "Upgraded from version 6.2.1 to version 6.2.2"
+sed -i -e "s/version=3.3.24/version=3.3.25/g" extra/flexget/spkgbuild
+changelog "extra/flexget/spkgbuild" "Upgraded from version 3.3.24 to version 3.3.25"
+sed -i -e "s/version=6.1.2/version=6.2.0/g" fonts/awesome-font/spkgbuild
+changelog "fonts/awesome-font/spkgbuild" "Upgraded from version 6.1.2 to version 6.2.0"
+sed -i -e "s/version=5.1/version=5.1.1/g" media/ffmpeg/spkgbuild
+changelog "media/ffmpeg/spkgbuild" "Upgraded from version 5.1 to version 5.1.1"
+sed -i -e "s/version=1.17.7/version=1.17.8/g" libs/cairo/spkgbuild
+changelog "libs/cairo/spkgbuild" "Upgraded from version 1.17.7 to version 1.17.8"
+sed -i -e "s/version=22.08.0/version=22.09.0/g" libs/poppler/spkgbuild
+changelog "libs/poppler/spkgbuild" "Upgraded from version 22.08.0 to version 22.09.0"
+sed -i -e "s/version=105.0b1/version=105.0b2/g" networking/thunderbird/spkgbuild
+changelog "networking/thunderbird/spkgbuild" "Upgraded from version 105.0b1 to version 105.0b2"
+sed -i -e "s/version=2022.08.19/version=2022.09.01/g" networking/yt-dlp/spkgbuild
+changelog "networking/yt-dlp/spkgbuild" "Upgraded from version 2022.08.19 to version 2022.09.01"
+sed -i -e "s/version=1.24.64/version=1.24.65/g" python/python-boto3/spkgbuild
+changelog "python/python-boto3/spkgbuild" "Upgraded from version 1.24.64 to version 1.24.65"
+sed -i -e "s/version=1.27.64/version=1.27.65/g" python/python-botocore/spkgbuild
+changelog "python/python-botocore/spkgbuild" "Upgraded from version 1.27.64 to version 1.27.65"
+sed -i -e "s/version=2022.8.1/version=2022.8.2/g" python/python-fsspec/spkgbuild
+changelog "python/python-fsspec/spkgbuild" "Upgraded from version 2022.8.1 to version 2022.8.2"
+sed -i -e "s/version=4.0.1/version=4.1.0/g" python/python-glanceclient/spkgbuild
+changelog "python/python-glanceclient/spkgbuild" "Upgraded from version 4.0.1 to version 4.1.0"
+sed -i -e "s/version=18.0.0/version=18.1.0/g" python/python-novaclient/spkgbuild
+changelog "python/python-novaclient/spkgbuild" "Upgraded from version 18.0.0 to version 18.1.0"
+sed -i -e "s/version=2022.8.30/version=2022.8.31/g" python/python-pipenv/spkgbuild
+changelog "python/python-pipenv/spkgbuild" "Upgraded from version 2022.8.30 to version 2022.8.31"
+sed -i -e "s/version=8.1.9/version=8.1.10/g" server/php/spkgbuild
+changelog "server/php/spkgbuild" "Upgraded from version 8.1.9 to version 8.1.10"
