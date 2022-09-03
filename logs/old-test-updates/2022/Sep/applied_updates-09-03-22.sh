f82534bf8a6d4ca1eef2529d39a63508ca5c83c4
Voncloft <voncloft@gmail.com>
2022-09-03 11:14:52 -0400

    Minor Updates

---
+++ logs/old-test-updates/2022/Sep/applied_updates-09-03-22.sh
@@ -0,0 +1,32 @@
+sed -i -e "s/version=2.9.7/version=2.9.8/g" core/cracklib/spkgbuild
+changelog "core/cracklib/spkgbuild" "Upgraded from version 2.9.7 to version 2.9.8"
+sed -i -e "s/version=0.3.56/version=0.3.57/g" core/pipewire/spkgbuild
+changelog "core/pipewire/spkgbuild" "Upgraded from version 0.3.56 to version 0.3.57"
+sed -i -e "s/version=2.37.90/version=2.37.91/g" core/webkitgtk/spkgbuild
+changelog "core/webkitgtk/spkgbuild" "Upgraded from version 2.37.90 to version 2.37.91"
+sed -i -e "s/version=2.9.7/version=2.9.8/g" libs/cracklib-dict/spkgbuild
+changelog "libs/cracklib-dict/spkgbuild" "Upgraded from version 2.9.7 to version 2.9.8"
+sed -i -e "s/version=105.0b5/version=105.0b6/g" networking/firefox/spkgbuild
+changelog "networking/firefox/spkgbuild" "Upgraded from version 105.0b5 to version 105.0b6"
+sed -i -e "s/version=7.92/version=7.93/g" networking/nmap/spkgbuild
+changelog "networking/nmap/spkgbuild" "Upgraded from version 7.92 to version 7.93"
+sed -i -e "s/version=1.24.65/version=1.24.66/g" python/python-boto3/spkgbuild
+changelog "python/python-boto3/spkgbuild" "Upgraded from version 1.24.65 to version 1.24.66"
+sed -i -e "s/version=1.27.65/version=1.27.66/g" python/python-botocore/spkgbuild
+changelog "python/python-botocore/spkgbuild" "Upgraded from version 1.27.65 to version 1.27.66"
+sed -i -e "s/version=3.0.0/version=3.1.0/g" python/python-heatclient/spkgbuild
+changelog "python/python-heatclient/spkgbuild" "Upgraded from version 3.0.0 to version 3.1.0"
+sed -i -e "s/version=8.0.2/version=8.0.3/g" python/python-ipywidgets/spkgbuild
+changelog "python/python-ipywidgets/spkgbuild" "Upgraded from version 8.0.2 to version 8.0.3"
+sed -i -e "s/version=0.9.9/version=0.9.10/g" python/python-json5/spkgbuild
+changelog "python/python-json5/spkgbuild" "Upgraded from version 0.9.9 to version 0.9.10"
+sed -i -e "s/version=5.0.0/version=5.0.1/g" python/python-keystoneclient/spkgbuild
+changelog "python/python-keystoneclient/spkgbuild" "Upgraded from version 5.0.0 to version 5.0.1"
+sed -i -e "s/version=2022.8.31/version=2022.9.2/g" python/python-pipenv/spkgbuild
+changelog "python/python-pipenv/spkgbuild" "Upgraded from version 2022.8.31 to version 2022.9.2"
+sed -i -e "s/version=4.0.1/version=4.1.0/g" python/python-swiftclient/spkgbuild
+changelog "python/python-swiftclient/spkgbuild" "Upgraded from version 4.0.1 to version 4.1.0"
+sed -i -e "s/version=3.14.0/version=3.14.1/g" python/python-trimesh/spkgbuild
+changelog "python/python-trimesh/spkgbuild" "Upgraded from version 3.14.0 to version 3.14.1"
+sed -i -e "s/version=0.6.3/version=0.6.4/g" xfce/xfce4-notifyd/spkgbuild
+changelog "xfce/xfce4-notifyd/spkgbuild" "Upgraded from version 0.6.3 to version 0.6.4"
