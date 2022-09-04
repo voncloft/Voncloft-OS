dab07f3c2422e69d10965b3bcb73de862b6952f8
Voncloft <voncloft@gmail.com>
2022-09-04 15:56:00 -0400

    Minor Updates

---
+++ logs/old-test-updates/2022/Sep/applied_updates-09-04-22.sh
@@ -0,0 +1,28 @@
+sed -i -e "s/version=2022.02.11/version=2022.09.03/g" compilers/autoconf-archive/spkgbuild
+changelog "compilers/autoconf-archive/spkgbuild" "Upgraded from version 2022.02.11 to version 2022.09.03"
+sed -i -e "s/version=0.63.1/version=0.63.2/g" compilers/meson/spkgbuild
+changelog "compilers/meson/spkgbuild" "Upgraded from version 0.63.1 to version 0.63.2"
+sed -i -e "s/version=5.1.1/version=5.2.0/g" core/gawk/spkgbuild
+changelog "core/gawk/spkgbuild" "Upgraded from version 5.1.1 to version 5.2.0"
+sed -i -e "s/version=3.7/version=3.8/g" core/grep/spkgbuild
+changelog "core/grep/spkgbuild" "Upgraded from version 3.7 to version 3.8"
+sed -i -e "s/version=1.25/version=1.26/g" libs/libaccounts-glib/spkgbuild
+changelog "libs/libaccounts-glib/spkgbuild" "Upgraded from version 1.25 to version 1.26"
+sed -i -e "s/version=0.8.0/version=22.1.0/g" python/python-aiofiles/spkgbuild
+changelog "python/python-aiofiles/spkgbuild" "Upgraded from version 0.8.0 to version 22.1.0"
+sed -i -e "s/version=4.10.1/version=4.11.0/g" python/python-antlr4/spkgbuild
+changelog "python/python-antlr4/spkgbuild" "Upgraded from version 4.10.1 to version 4.11.0"
+sed -i -e "s/version=2022.8.1/version=2022.9.0/g" python/python-distributed/spkgbuild
+changelog "python/python-distributed/spkgbuild" "Upgraded from version 2022.8.1 to version 2022.9.0"
+sed -i -e "s/version=0.20.0/version=0.21.0/g" python/python-dotenv/spkgbuild
+changelog "python/python-dotenv/spkgbuild" "Upgraded from version 0.20.0 to version 0.21.0"
+sed -i -e "s/version=2022.9.2/version=2022.9.4/g" python/python-pipenv/spkgbuild
+changelog "python/python-pipenv/spkgbuild" "Upgraded from version 2022.9.2 to version 2022.9.4"
+sed -i -e "s/version=37.0/version=37.1/g" python/python-readme-renderer/spkgbuild
+changelog "python/python-readme-renderer/spkgbuild" "Upgraded from version 37.0 to version 37.1"
+sed -i -e "s/version=2.9.0/version=2.10/g" python/python-tensorflow-estimator/spkgbuild
+changelog "python/python-tensorflow-estimator/spkgbuild" "Upgraded from version 2.9.0 to version 2.10"
+sed -i -e "s/version=4.64.0/version=4.64.1/g" python/python-tqdm/spkgbuild
+changelog "python/python-tqdm/spkgbuild" "Upgraded from version 4.64.0 to version 4.64.1"
+sed -i -e "s/version=3.11.11/version=3.11.12/g" server/blacklist-hosts/spkgbuild
+changelog "server/blacklist-hosts/spkgbuild" "Upgraded from version 3.11.11 to version 3.11.12"
