771e00470182d6594875a0f26193004fc9d6fa4c
Voncloft <voncloft@gmail.com>
2022-09-07 22:33:04 -0400

    Minor Updates

---
+++ logs/old-test-updates/2022/Sep/applied_updates-09-07-22.sh
@@ -0,0 +1,32 @@
+sed -i -e "s/version=9.64/version=9.65/g" core/hdparm/spkgbuild
+changelog "core/hdparm/spkgbuild" "Upgraded from version 9.64 to version 9.65"
+sed -i -e "s/version=1.8.40/version=1.10.0/g" libs/libnma/spkgbuild
+changelog "libs/libnma/spkgbuild" "Upgraded from version 1.8.40 to version 1.10.0"
+sed -i -e "s/version=105.0b7/version=105.0b8/g" networking/firefox/spkgbuild
+changelog "networking/firefox/spkgbuild" "Upgraded from version 105.0b7 to version 105.0b8"
+sed -i -e "s/version=105.0b2/version=105.0b3/g" networking/thunderbird/spkgbuild
+changelog "networking/thunderbird/spkgbuild" "Upgraded from version 105.0b2 to version 105.0b3"
+sed -i -e "s/version=2.12.8/version=2.12.9/g" python/python-astroid/spkgbuild
+changelog "python/python-astroid/spkgbuild" "Upgraded from version 2.12.8 to version 2.12.9"
+sed -i -e "s/version=0.64.0/version=0.64.1/g" python/python-cfn-lint/spkgbuild
+changelog "python/python-cfn-lint/spkgbuild" "Upgraded from version 0.64.0 to version 0.64.1"
+sed -i -e "s/version=3.3.1/version=3.4.0/g" python/python-pyproj/spkgbuild
+changelog "python/python-pyproj/spkgbuild" "Upgraded from version 3.3.1 to version 3.4.0"
+sed -i -e "s/version=1.3.12/version=1.3.13/g" python/python-sqlalchemy-continuum/spkgbuild
+changelog "python/python-sqlalchemy-continuum/spkgbuild" "Upgraded from version 1.3.12 to version 1.3.13"
+sed -i -e "s/version=5.5/version=5.5.1/g" python/python-zope-testrunner/spkgbuild
+changelog "python/python-zope-testrunner/spkgbuild" "Upgraded from version 5.5 to version 5.5.1"
+sed -i -e "s/version=5.15.5/version=5.15.6/g" qt/qt5/spkgbuild
+changelog "qt/qt5/spkgbuild" "Upgraded from version 5.15.5 to version 5.15.6"
+sed -i -e "s/version=5.15.5/version=5.15.6/g" qt/qt5-multimedia/spkgbuild
+changelog "qt/qt5-multimedia/spkgbuild" "Upgraded from version 5.15.5 to version 5.15.6"
+sed -i -e "s/version=5.15.5/version=5.15.6/g" qt/qt5-speech/spkgbuild
+changelog "qt/qt5-speech/spkgbuild" "Upgraded from version 5.15.5 to version 5.15.6"
+sed -i -e "s/version=5.15.5/version=5.15.6/g" qt/qt5-x11extras/spkgbuild
+changelog "qt/qt5-x11extras/spkgbuild" "Upgraded from version 5.15.5 to version 5.15.6"
+sed -i -e "s/version=5.15.5/version=5.15.6/g" qt/qtsql/spkgbuild
+changelog "qt/qtsql/spkgbuild" "Upgraded from version 5.15.5 to version 5.15.6"
+sed -i -e "s/version=2.3.21/version=2.3.22/g" ruby-gems/ruby-bundler/spkgbuild
+changelog "ruby-gems/ruby-bundler/spkgbuild" "Upgraded from version 2.3.21 to version 2.3.22"
+sed -i -e "s/version=4.16.4/version=4.16.5/g" server/samba/spkgbuild
+changelog "server/samba/spkgbuild" "Upgraded from version 4.16.4 to version 4.16.5"
