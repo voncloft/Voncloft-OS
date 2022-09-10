1ca3313cd6313ac3d6afa19f95e9fba2a36eefdf
Voncloft <voncloft@gmail.com>
2022-09-10 17:08:08 -0400

    Minor Updates

---
+++ logs/old-test-updates/2022/Sep/applied_updates-09-10-22.sh
@@ -0,0 +1,4 @@
+sed -i -e "s/version=3.2.5/version=3.2.6/g" core/rsync/spkgbuild
+changelog "core/rsync/spkgbuild" "Upgraded from version 3.2.5 to version 3.2.6"
+sed -i -e "s/version=1.1.4rc2/version=1.1.5rc1/g" extra/solaar/spkgbuild
+changelog "extra/solaar/spkgbuild" "Upgraded from version 1.1.4rc2 to version 1.1.5rc1"
