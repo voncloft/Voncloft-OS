718bdbf6a780019b92788a6ce3849f5aa7869907
Voncloft <voncloft@gmail.com>
2022-09-12 18:59:03 -0400

    Minor Updates

---
+++ logs/old-test-updates/2022/Sep/applied_updates-09-12-22.sh
@@ -0,0 +1,60 @@
+sed -i -e "s/version=5.19/version=5.19.1/g" core/btrfs-progs/spkgbuild
+changelog "core/btrfs-progs/spkgbuild" "Upgraded from version 5.19 to version 5.19.1"
+sed -i -e "s/version=28.1/version=28.2/g" core/emacs/spkgbuild
+changelog "core/emacs/spkgbuild" "Upgraded from version 28.1 to version 28.2"
+sed -i -e "s/version=4.34/version=4.35/g" extra/nspr/spkgbuild
+changelog "extra/nspr/spkgbuild" "Upgraded from version 4.34 to version 4.35"
+sed -i -e "s/version=0.3.10/version=0.4.0/g" libs/libgusb/spkgbuild
+changelog "libs/libgusb/spkgbuild" "Upgraded from version 0.3.10 to version 0.4.0"
+sed -i -e "s/version=1.4.11/version=1.4.13/g" python/python-aiodiscover/spkgbuild
+changelog "python/python-aiodiscover/spkgbuild" "Upgraded from version 1.4.11 to version 1.4.13"
+sed -i -e "s/version=3.39.2.1/version=3.39.3.0/g" python/python-apsw/spkgbuild
+changelog "python/python-apsw/spkgbuild" "Upgraded from version 3.39.2.1 to version 3.39.3.0"
+sed -i -e "s/version=1.24.70/version=1.24.71/g" python/python-boto3/spkgbuild
+changelog "python/python-boto3/spkgbuild" "Upgraded from version 1.24.70 to version 1.24.71"
+sed -i -e "s/version=1.27.70/version=1.27.71/g" python/python-botocore/spkgbuild
+changelog "python/python-botocore/spkgbuild" "Upgraded from version 1.27.70 to version 1.27.71"
+sed -i -e "s/version=4.10.0/version=4.10.1/g" python/python-btrees/spkgbuild
+changelog "python/python-btrees/spkgbuild" "Upgraded from version 4.10.0 to version 4.10.1"
+sed -i -e "s/version=0.12.11/version=0.12.12/g" python/python-cachecontrol/spkgbuild
+changelog "python/python-cachecontrol/spkgbuild" "Upgraded from version 0.12.11 to version 0.12.12"
+sed -i -e "s/version=2022.04/version=2022.08/g" python/python-litedram/spkgbuild
+changelog "python/python-litedram/spkgbuild" "Upgraded from version 2022.04 to version 2022.08"
+sed -i -e "s/version=2022.04/version=2022.08/g" python/python-liteeth/spkgbuild
+changelog "python/python-liteeth/spkgbuild" "Upgraded from version 2022.04 to version 2022.08"
+sed -i -e "s/version=2022.04/version=2022.08/g" python/python-liteiclink/spkgbuild
+changelog "python/python-liteiclink/spkgbuild" "Upgraded from version 2022.04 to version 2022.08"
+sed -i -e "s/version=2022.04/version=2022.08/g" python/python-litejesd204b/spkgbuild
+changelog "python/python-litejesd204b/spkgbuild" "Upgraded from version 2022.04 to version 2022.08"
+sed -i -e "s/version=2022.04/version=2022.08/g" python/python-litepcie/spkgbuild
+changelog "python/python-litepcie/spkgbuild" "Upgraded from version 2022.04 to version 2022.08"
+sed -i -e "s/version=2022.04/version=2022.08/g" python/python-litesata/spkgbuild
+changelog "python/python-litesata/spkgbuild" "Upgraded from version 2022.04 to version 2022.08"
+sed -i -e "s/version=2022.04/version=2022.08/g" python/python-litescope/spkgbuild
+changelog "python/python-litescope/spkgbuild" "Upgraded from version 2022.04 to version 2022.08"
+sed -i -e "s/version=2022.04/version=2022.08/g" python/python-litesdcard/spkgbuild
+changelog "python/python-litesdcard/spkgbuild" "Upgraded from version 2022.04 to version 2022.08"
+sed -i -e "s/version=2022.04/version=2022.08/g" python/python-liteusb/spkgbuild
+changelog "python/python-liteusb/spkgbuild" "Upgraded from version 2022.04 to version 2022.08"
+sed -i -e "s/version=2022.04/version=2022.08/g" python/python-litex/spkgbuild
+changelog "python/python-litex/spkgbuild" "Upgraded from version 2022.04 to version 2022.08"
+sed -i -e "s/version=2022.04/version=2022.08/g" python/python-litex-boards/spkgbuild
+changelog "python/python-litex-boards/spkgbuild" "Upgraded from version 2022.04 to version 2022.08"
+sed -i -e "s/version=1.2.0/version=1.2.1/g" python/python-pycapnp/spkgbuild
+changelog "python/python-pycapnp/spkgbuild" "Upgraded from version 1.2.0 to version 1.2.1"
+sed -i -e "s/version=2020.08/version=2022.08/g" python/python-pythondata-cpu-lm32/spkgbuild
+changelog "python/python-pythondata-cpu-lm32/spkgbuild" "Upgraded from version 2020.08 to version 2022.08"
+sed -i -e "s/version=2020.08/version=2022.08/g" python/python-pythondata-cpu-mor1kx/spkgbuild
+changelog "python/python-pythondata-cpu-mor1kx/spkgbuild" "Upgraded from version 2020.08 to version 2022.08"
+sed -i -e "s/version=2020.08/version=2022.08/g" python/python-pythondata-cpu-picorv32/spkgbuild
+changelog "python/python-pythondata-cpu-picorv32/spkgbuild" "Upgraded from version 2020.08 to version 2022.08"
+sed -i -e "s/version=2020.08/version=2022.08/g" python/python-pythondata-cpu-vexriscv/spkgbuild
+changelog "python/python-pythondata-cpu-vexriscv/spkgbuild" "Upgraded from version 2020.08 to version 2022.08"
+sed -i -e "s/version=2020.08/version=2022.08/g" python/python-pythondata-software-compiler_rt/spkgbuild
+changelog "python/python-pythondata-software-compiler_rt/spkgbuild" "Upgraded from version 2020.08 to version 2022.08"
+sed -i -e "s/version=2.9.1/version=2.10.0/g" python/python-tensorflow-serving-api/spkgbuild
+changelog "python/python-tensorflow-serving-api/spkgbuild" "Upgraded from version 2.9.1 to version 2.10.0"
+sed -i -e "s/version=3.11.0/version=3.11.1/g" ruby-gems/ruby-rspec-expectations/spkgbuild
+changelog "ruby-gems/ruby-rspec-expectations/spkgbuild" "Upgraded from version 3.11.0 to version 3.11.1"
+sed -i -e "s/version=3.11.0/version=3.11.1/g" ruby-gems/ruby-rspec-support/spkgbuild
+changelog "ruby-gems/ruby-rspec-support/spkgbuild" "Upgraded from version 3.11.0 to version 3.11.1"
