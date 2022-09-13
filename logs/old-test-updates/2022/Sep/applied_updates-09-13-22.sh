6fcfd78a8a01b22b37d6550ae863b184a469e28a
Voncloft <voncloft@gmail.com>
2022-09-13 19:04:13 -0400

    Minor Updates

---
+++ logs/old-test-updates/2022/Sep/applied_updates-09-13-22.sh
@@ -0,0 +1,22 @@
+sed -i -e "s/version=3.24.1/version=3.24.2/g" compilers/cmake/spkgbuild
+changelog "compilers/cmake/spkgbuild" "Upgraded from version 3.24.1 to version 3.24.2"
+sed -i -e "s/version=5.42/version=5.43/g" core/file/spkgbuild
+changelog "core/file/spkgbuild" "Upgraded from version 5.42 to version 5.43"
+sed -i -e "s/version=0.39.0/version=0.39.1/g" libs/libfilezilla/spkgbuild
+changelog "libs/libfilezilla/spkgbuild" "Upgraded from version 0.39.0 to version 0.39.1"
+sed -i -e "s/version=105.0b3/version=105.0b4/g" networking/thunderbird/spkgbuild
+changelog "networking/thunderbird/spkgbuild" "Upgraded from version 105.0b3 to version 105.0b4"
+sed -i -e "s/version=1.50.0/version=1.51.0/g" python/python-aws-sam-translator/spkgbuild
+changelog "python/python-aws-sam-translator/spkgbuild" "Upgraded from version 1.50.0 to version 1.51.0"
+sed -i -e "s/version=1.24.71/version=1.24.72/g" python/python-boto3/spkgbuild
+changelog "python/python-boto3/spkgbuild" "Upgraded from version 1.24.71 to version 1.24.72"
+sed -i -e "s/version=1.27.71/version=1.27.72/g" python/python-botocore/spkgbuild
+changelog "python/python-botocore/spkgbuild" "Upgraded from version 1.27.71 to version 1.27.72"
+sed -i -e "s/version=2.60.0/version=2.61.0/g" python/python-google-api-python-client/spkgbuild
+changelog "python/python-google-api-python-client/spkgbuild" "Upgraded from version 2.60.0 to version 2.61.0"
+sed -i -e "s/version=6.15.2/version=6.15.3/g" python/python-ipykernel/spkgbuild
+changelog "python/python-ipykernel/spkgbuild" "Upgraded from version 6.15.2 to version 6.15.3"
+sed -i -e "s/version=5.1.0/version=5.2/g" python/python-zope-hookable/spkgbuild
+changelog "python/python-zope-hookable/spkgbuild" "Upgraded from version 5.1.0 to version 5.2"
+sed -i -e "s/version=4.16.5/version=4.17.0/g" server/samba/spkgbuild
+changelog "server/samba/spkgbuild" "Upgraded from version 4.16.5 to version 4.17.0"
