03e397c640cf467d616fbfee90afe051d9a9bcd8
Voncloft <voncloft@gmail.com>
2022-09-14 19:06:31 -0400

    Minor Updates

---
+++ logs/old-test-updates/2022/Sep/applied_updates-09-14-22.sh
@@ -0,0 +1,24 @@
+sed -i -e "s/version=21.5/version=21.6/g" extra/protobuf/spkgbuild
+changelog "extra/protobuf/spkgbuild" "Upgraded from version 21.5 to version 21.6"
+sed -i -e "s/version=1.24.72/version=1.24.73/g" python/python-boto3/spkgbuild
+changelog "python/python-boto3/spkgbuild" "Upgraded from version 1.24.72 to version 1.24.73"
+sed -i -e "s/version=1.27.72/version=1.27.73/g" python/python-botocore/spkgbuild
+changelog "python/python-botocore/spkgbuild" "Upgraded from version 1.27.72 to version 1.27.73"
+sed -i -e "s/version=0.64.1/version=0.65.0/g" python/python-cfn-lint/spkgbuild
+changelog "python/python-cfn-lint/spkgbuild" "Upgraded from version 0.64.1 to version 0.65.0"
+sed -i -e "s/version=0.83.0/version=0.84.0/g" python/python-fastapi/spkgbuild
+changelog "python/python-fastapi/spkgbuild" "Upgraded from version 0.83.0 to version 0.84.0"
+sed -i -e "s/version=0.17.3/version=0.18/g" python/python-fasteners/spkgbuild
+changelog "python/python-fasteners/spkgbuild" "Upgraded from version 0.17.3 to version 0.18"
+sed -i -e "s/version=2.0.1/version=2.0.2/g" python/python-requirementslib/spkgbuild
+changelog "python/python-requirementslib/spkgbuild" "Upgraded from version 2.0.1 to version 2.0.2"
+sed -i -e "s/version=0.16.0/version=0.17.0/g" python/python-uvloop/spkgbuild
+changelog "python/python-uvloop/spkgbuild" "Upgraded from version 0.16.0 to version 0.17.0"
+sed -i -e "s/version=6.2.0/version=6.2.1/g" python/python-zope-schema/spkgbuild
+changelog "python/python-zope-schema/spkgbuild" "Upgraded from version 6.2.0 to version 6.2.1"
+sed -i -e "s/version=3.11.15/version=3.11.16/g" server/blacklist-hosts/spkgbuild
+changelog "server/blacklist-hosts/spkgbuild" "Upgraded from version 3.11.15 to version 3.11.16"
+sed -i -e "s/version=2022.9.2/version=2022.9.4/g" server/homeassistant/spkgbuild
+changelog "server/homeassistant/spkgbuild" "Upgraded from version 2022.9.2 to version 2022.9.4"
+sed -i -e "s/version=4.16.0/version=4.17.0/g" xfce/xfce4-session/spkgbuild
+changelog "xfce/xfce4-session/spkgbuild" "Upgraded from version 4.16.0 to version 4.17.0"
