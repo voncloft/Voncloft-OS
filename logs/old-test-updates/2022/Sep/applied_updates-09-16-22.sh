194df9a8aa0132577b0f8591eaaa3327000652a6
Voncloft <voncloft@gmail.com>
2022-09-16 17:48:16 -0400

    Minor Updates

---
+++ logs/old-test-updates/2022/Sep/applied_updates-09-16-22.sh
@@ -0,0 +1,32 @@
+sed -i -e "s/version=2.0.33/version=2.0.34/g" core/acpid/spkgbuild
+changelog "core/acpid/spkgbuild" "Upgraded from version 2.0.33 to version 2.0.34"
+sed -i -e "s/version=4.3.0/version=5.0.0/g" core/streamlink/spkgbuild
+changelog "core/streamlink/spkgbuild" "Upgraded from version 4.3.0 to version 5.0.0"
+sed -i -e "s/version=2.37.91/version=2.38.0/g" core/webkitgtk/spkgbuild
+changelog "core/webkitgtk/spkgbuild" "Upgraded from version 2.37.91 to version 2.38.0"
+sed -i -e "s/version=2.35.2/version=2.36/g" extra/discordchatexporter/spkgbuild
+changelog "extra/discordchatexporter/spkgbuild" "Upgraded from version 2.35.2 to version 2.36"
+sed -i -e "s/version=1.6.0/version=1.6.1/g" libs/libksba/spkgbuild
+changelog "libs/libksba/spkgbuild" "Upgraded from version 1.6.0 to version 1.6.1"
+sed -i -e "s/version=1.24.74/version=1.24.75/g" python/python-boto3/spkgbuild
+changelog "python/python-boto3/spkgbuild" "Upgraded from version 1.24.74 to version 1.24.75"
+sed -i -e "s/version=1.27.74/version=1.27.75/g" python/python-botocore/spkgbuild
+changelog "python/python-botocore/spkgbuild" "Upgraded from version 1.27.74 to version 1.27.75"
+sed -i -e "s/version=2022.9.0/version=2022.9.1/g" python/python-distributed/spkgbuild
+changelog "python/python-distributed/spkgbuild" "Upgraded from version 2022.9.0 to version 2022.9.1"
+sed -i -e "s/version=1.1.0/version=1.2.0/g" python/python-joblib/spkgbuild
+changelog "python/python-joblib/spkgbuild" "Upgraded from version 1.1.0 to version 1.2.0"
+sed -i -e "s/version=3.4.2/version=3.4.3/g" python/python-ldap/spkgbuild
+changelog "python/python-ldap/spkgbuild" "Upgraded from version 3.4.2 to version 3.4.3"
+sed -i -e "s/version=3.7.0/version=4.0.0/g" python/python-magnumclient/spkgbuild
+changelog "python/python-magnumclient/spkgbuild" "Upgraded from version 3.7.0 to version 4.0.0"
+sed -i -e "s/version=4.9.0/version=4.9.1/g" python/python-persistent/spkgbuild
+changelog "python/python-persistent/spkgbuild" "Upgraded from version 4.9.0 to version 4.9.1"
+sed -i -e "s/version=1.2.0/version=1.2.1/g" python/python-poetry/spkgbuild
+changelog "python/python-poetry/spkgbuild" "Upgraded from version 1.2.0 to version 1.2.1"
+sed -i -e "s/version=1.1.0/version=1.2.0/g" python/python-poetry-core/spkgbuild
+changelog "python/python-poetry-core/spkgbuild" "Upgraded from version 1.1.0 to version 1.2.0"
+sed -i -e "s/version=2.3.1/version=2.4.0/g" python/python-pytest-regressions/spkgbuild
+changelog "python/python-pytest-regressions/spkgbuild" "Upgraded from version 2.3.1 to version 2.4.0"
+sed -i -e "s/version=3.14.1/version=3.15.1/g" python/python-trimesh/spkgbuild
+changelog "python/python-trimesh/spkgbuild" "Upgraded from version 3.14.1 to version 3.15.1"
