4d188d37476c90e11051cdda7b8739093e04e84a
Voncloft <voncloft@gmail.com>
2022-09-15 21:16:50 -0400

    Minor Updates

---
+++ logs/old-test-updates/2022/Sep/applied_updates-09-15-22.sh
@@ -0,0 +1,38 @@
+sed -i -e "s/version=0.102.3/version=0.103.0/g" core/hugo/spkgbuild
+changelog "core/hugo/spkgbuild" "Upgraded from version 0.102.3 to version 0.103.0"
+sed -i -e "s/version=5.19.8/version=5.19.9/g" core/linux-desktop/spkgbuild
+changelog "core/linux-desktop/spkgbuild" "Upgraded from version 5.19.8 to version 5.19.9"
+sed -i -e "s/version=5.19.8/version=5.19.9/g" core/linux-router/spkgbuild
+changelog "core/linux-router/spkgbuild" "Upgraded from version 5.19.8 to version 5.19.9"
+sed -i -e "s/version=3.82/version=3.83/g" core/nss/spkgbuild
+changelog "core/nss/spkgbuild" "Upgraded from version 3.82 to version 3.83"
+sed -i -e "s/version=0.3.57/version=0.3.58/g" core/pipewire/spkgbuild
+changelog "core/pipewire/spkgbuild" "Upgraded from version 0.3.57 to version 0.3.58"
+sed -i -e "s/version=1.6.37/version=1.6.38/g" libs/libpng/spkgbuild
+changelog "libs/libpng/spkgbuild" "Upgraded from version 1.6.37 to version 1.6.38"
+sed -i -e "s/version=4.0.0rc1/version=4.0.0rc2/g" networking/wireshark/spkgbuild
+changelog "networking/wireshark/spkgbuild" "Upgraded from version 4.0.0rc1 to version 4.0.0rc2"
+sed -i -e "s/version=0.9.19/version=0.9.20/g" networking/xrdp/spkgbuild
+changelog "networking/xrdp/spkgbuild" "Upgraded from version 0.9.19 to version 0.9.20"
+sed -i -e "s/version=0.0.19/version=0.0.20/g" nonfree/discord/spkgbuild
+changelog "nonfree/discord/spkgbuild" "Upgraded from version 0.0.19 to version 0.0.20"
+sed -i -e "s/version=7.4.0/version=7.4.1/g" nonfree/libreoffice/spkgbuild
+changelog "nonfree/libreoffice/spkgbuild" "Upgraded from version 7.4.0 to version 7.4.1"
+sed -i -e "s/version=22.8.0/version=22.9.0/g" python/python-awesomeversion/spkgbuild
+changelog "python/python-awesomeversion/spkgbuild" "Upgraded from version 22.8.0 to version 22.9.0"
+sed -i -e "s/version=1.24.73/version=1.24.74/g" python/python-boto3/spkgbuild
+changelog "python/python-boto3/spkgbuild" "Upgraded from version 1.24.73 to version 1.24.74"
+sed -i -e "s/version=1.27.73/version=1.27.74/g" python/python-botocore/spkgbuild
+changelog "python/python-botocore/spkgbuild" "Upgraded from version 1.27.73 to version 1.27.74"
+sed -i -e "s/version=0.84.0/version=0.85.0/g" python/python-fastapi/spkgbuild
+changelog "python/python-fastapi/spkgbuild" "Upgraded from version 0.84.0 to version 0.85.0"
+sed -i -e "s/version=4.37.1/version=4.37.2/g" python/python-fonttools/spkgbuild
+changelog "python/python-fonttools/spkgbuild" "Upgraded from version 4.37.1 to version 4.37.2"
+sed -i -e "s/version=2022.06.21/version=2022.09.15/g" python/python-sphinx-furo/spkgbuild
+changelog "python/python-sphinx-furo/spkgbuild" "Upgraded from version 2022.06.21 to version 2022.09.15"
+sed -i -e "s/version=5.4.0/version=5.5.0/g" python/python-ujson/spkgbuild
+changelog "python/python-ujson/spkgbuild" "Upgraded from version 5.4.0 to version 5.5.0"
+sed -i -e "s/version=4.5.0/version=4.5.1/g" python/python-zope-proxy/spkgbuild
+changelog "python/python-zope-proxy/spkgbuild" "Upgraded from version 4.5.0 to version 4.5.1"
+sed -i -e "s/version=5.3/version=5.4/g" python/python-zope-security/spkgbuild
+changelog "python/python-zope-security/spkgbuild" "Upgraded from version 5.3 to version 5.4"
