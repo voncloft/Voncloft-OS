0925c7ba08743c5087a38453904ffd53008ee133
Voncloft <voncloft@gmail.com>
2022-09-17 20:55:01 -0400

    Minor Updates

---
+++ logs/old-test-updates/2022/Sep/applied_updates-09-17-22.sh
@@ -0,0 +1,12 @@
+sed -i -e "s/version=4.95.0/version=4.96.3/g" core/lsof/spkgbuild
+changelog "core/lsof/spkgbuild" "Upgraded from version 4.95.0 to version 4.96.3"
+sed -i -e "s/version=2.12.9/version=2.12.10/g" python/python-astroid/spkgbuild
+changelog "python/python-astroid/spkgbuild" "Upgraded from version 2.12.9 to version 2.12.10"
+sed -i -e "s/version=0.15.1/version=0.15.2/g" python/python-libtmux/spkgbuild
+changelog "python/python-libtmux/spkgbuild" "Upgraded from version 0.15.1 to version 0.15.2"
+sed -i -e "s/version=2.4.0/version=2.5.0/g" python/python-pyjwt/spkgbuild
+changelog "python/python-pyjwt/spkgbuild" "Upgraded from version 2.4.0 to version 2.5.0"
+sed -i -e "s/version=2.4.0/version=2.4.1/g" python/python-pytest-regressions/spkgbuild
+changelog "python/python-pytest-regressions/spkgbuild" "Upgraded from version 2.4.0 to version 2.4.1"
+sed -i -e "s/version=3.11.16/version=3.11.17/g" server/blacklist-hosts/spkgbuild
+changelog "server/blacklist-hosts/spkgbuild" "Upgraded from version 3.11.16 to version 3.11.17"
