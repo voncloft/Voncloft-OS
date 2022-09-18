2744938e7491ad5526ac53264d6ae0a0b5a341f6
Voncloft <voncloft@gmail.com>
2022-09-18 17:08:22 -0400

    Minor Updates

---
+++ logs/old-test-updates/2022/Sep/applied_updates-09-18-22.sh
@@ -0,0 +1,16 @@
+sed -i -e "s/version=0.103.0/version=0.103.1/g" core/hugo/spkgbuild
+changelog "core/hugo/spkgbuild" "Upgraded from version 0.103.0 to version 0.103.1"
+sed -i -e "s/version=4.0.40/version=4.0.41/g" core/mtools/spkgbuild
+changelog "core/mtools/spkgbuild" "Upgraded from version 4.0.40 to version 4.0.41"
+sed -i -e "s/version=1.6.1/version=1.6.2/g" python/python-cftime/spkgbuild
+changelog "python/python-cftime/spkgbuild" "Upgraded from version 1.6.1 to version 1.6.2"
+sed -i -e "s/version=6.54.5/version=6.54.6/g" python/python-hypothesis/spkgbuild
+changelog "python/python-hypothesis/spkgbuild" "Upgraded from version 6.54.5 to version 6.54.6"
+sed -i -e "s/version=1.4/version=1.5/g" python/python-lazy/spkgbuild
+changelog "python/python-lazy/spkgbuild" "Upgraded from version 1.4 to version 1.5"
+sed -i -e "s/version=7.3.2208121110/version=7.3.2209181503/g" python/python-pycontracts/spkgbuild
+changelog "python/python-pycontracts/spkgbuild" "Upgraded from version 7.3.2208121110 to version 7.3.2209181503"
+sed -i -e "s/version=0.16.0/version=0.17.0/g" python/python-thrift/spkgbuild
+changelog "python/python-thrift/spkgbuild" "Upgraded from version 0.16.0 to version 0.17.0"
+sed -i -e "s/version=2022.9.4/version=2022.9.5/g" server/homeassistant/spkgbuild
+changelog "server/homeassistant/spkgbuild" "Upgraded from version 2022.9.4 to version 2022.9.5"
