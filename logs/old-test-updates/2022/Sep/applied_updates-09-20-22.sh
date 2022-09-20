036c9f79042471d2c656d46e5e76879e22d5759e
Voncloft <voncloft@gmail.com>
2022-09-20 17:54:50 -0400

    Minor Updates

--- logs/old-test-updates/2022/Sep/applied_updates-09-20-22.sh
+++ logs/old-test-updates/2022/Sep/applied_updates-09-20-22.sh
@@ -18,3 +18,43 @@ sed -i -e "s/version=0.19.1/version=0.19.2/g" python/python-meilisearch/spkgbuil
 changelog "python/python-meilisearch/spkgbuild" "Upgraded from version 0.19.1 to version 0.19.2"
 sed -i -e "s/version=5.15.10/version=5.15.11/g" qt/qtwebengine/spkgbuild
 changelog "qt/qtwebengine/spkgbuild" "Upgraded from version 5.15.10 to version 5.15.11"
+sed -i -e "s/version=15.0.0/version=15.0.1/g" compilers/clang/spkgbuild
+changelog "compilers/clang/spkgbuild" "Upgraded from version 15.0.0 to version 15.0.1"
+sed -i -e "s/version=15.0.0/version=15.0.1/g" compilers/lld/spkgbuild
+changelog "compilers/lld/spkgbuild" "Upgraded from version 15.0.0 to version 15.0.1"
+sed -i -e "s/version=15.0.0/version=15.0.1/g" compilers/llvm/spkgbuild
+changelog "compilers/llvm/spkgbuild" "Upgraded from version 15.0.0 to version 15.0.1"
+sed -i -e "s/version=5.19.9/version=5.19.10/g" core/linux-desktop/spkgbuild
+changelog "core/linux-desktop/spkgbuild" "Upgraded from version 5.19.9 to version 5.19.10"
+sed -i -e "s/version=5.19.9/version=5.19.10/g" core/linux-router/spkgbuild
+changelog "core/linux-router/spkgbuild" "Upgraded from version 5.19.9 to version 5.19.10"
+sed -i -e "s/version=1.13.1/version=1.14.0/g" core/wpebackend-fdo/spkgbuild
+changelog "core/wpebackend-fdo/spkgbuild" "Upgraded from version 1.13.1 to version 1.14.0"
+sed -i -e "s/version=15.0.0/version=15.0.1/g" multilib/llvm-32/spkgbuild
+changelog "multilib/llvm-32/spkgbuild" "Upgraded from version 15.0.0 to version 15.0.1"
+sed -i -e "s/version=2.4.8/version=2.4.9/g" libs/expat/spkgbuild
+changelog "libs/expat/spkgbuild" "Upgraded from version 2.4.8 to version 2.4.9"
+sed -i -e "s/version=1.13.90/version=1.14.0/g" libs/libwpe/spkgbuild
+changelog "libs/libwpe/spkgbuild" "Upgraded from version 1.13.90 to version 1.14.0"
+sed -i -e "s/version=0.65.0/version=0.65.1/g" python/python-cfn-lint/spkgbuild
+changelog "python/python-cfn-lint/spkgbuild" "Upgraded from version 0.65.0 to version 0.65.1"
+sed -i -e "s/version=8.4.1/version=8.4.2/g" python/python-elasticsearch/spkgbuild
+changelog "python/python-elasticsearch/spkgbuild" "Upgraded from version 8.4.1 to version 8.4.2"
+sed -i -e "s/version=4.37.2/version=4.37.3/g" python/python-fonttools/spkgbuild
+changelog "python/python-fonttools/spkgbuild" "Upgraded from version 4.37.2 to version 4.37.3"
+sed -i -e "s/version=2.61.0/version=2.62.0/g" python/python-google-api-python-client/spkgbuild
+changelog "python/python-google-api-python-client/spkgbuild" "Upgraded from version 2.61.0 to version 2.62.0"
+sed -i -e "s/version=2.11.0/version=2.11.1/g" python/python-google-auth/spkgbuild
+changelog "python/python-google-auth/spkgbuild" "Upgraded from version 2.11.0 to version 2.11.1"
+sed -i -e "s/version=0.15.2/version=0.15.3/g" python/python-libtmux/spkgbuild
+changelog "python/python-libtmux/spkgbuild" "Upgraded from version 0.15.2 to version 0.15.3"
+sed -i -e "s/version=12.1.0/version=12.2.0/g" python/python-pdoc/spkgbuild
+changelog "python/python-pdoc/spkgbuild" "Upgraded from version 12.1.0 to version 12.2.0"
+sed -i -e "s/version=2022.9.8/version=2022.9.20/g" python/python-pipenv/spkgbuild
+changelog "python/python-pipenv/spkgbuild" "Upgraded from version 2022.9.8 to version 2022.9.20"
+sed -i -e "s/version=3.9.1/version=3.10.0/g" python/python-sqlobject/spkgbuild
+changelog "python/python-sqlobject/spkgbuild" "Upgraded from version 3.9.1 to version 3.10.0"
+sed -i -e "s/version=0.8.1/version=0.8.2/g" xfce/xfce4-datetime-plugin/spkgbuild
+changelog "xfce/xfce4-datetime-plugin/spkgbuild" "Upgraded from version 0.8.1 to version 0.8.2"
+sed -i -e "s/version=1.8.1/version=1.8.2/g" xfce/xfce4-places-plugin/spkgbuild
+changelog "xfce/xfce4-places-plugin/spkgbuild" "Upgraded from version 1.8.1 to version 1.8.2"
