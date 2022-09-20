cb356a8042a0d1fbe6275142f43e2591be94b16e
Voncloft <voncloft@gmail.com>
2022-09-20 08:55:25 -0400

    Minor Updates

---
+++ logs/old-test-updates/2022/Sep/applied_updates-09-20-22.sh
@@ -0,0 +1,20 @@
+sed -i -e "s/version=1.12.2/version=1.13.1/g" core/conky/spkgbuild
+changelog "core/conky/spkgbuild" "Upgraded from version 1.12.2 to version 1.13.1"
+sed -i -e "s/version=3.3.26/version=3.3.27/g" extra/flexget/spkgbuild
+changelog "extra/flexget/spkgbuild" "Upgraded from version 3.3.26 to version 3.3.27"
+sed -i -e "s/version=3.3/version=3.4.3/g" libs/libffi/spkgbuild
+changelog "libs/libffi/spkgbuild" "Upgraded from version 3.3 to version 3.4.3"
+sed -i -e "s/version=105.0b9/version=106.0b1/g" networking/firefox/spkgbuild
+changelog "networking/firefox/spkgbuild" "Upgraded from version 105.0b9 to version 106.0b1"
+sed -i -e "s/version=1.24.75/version=1.24.76/g" python/python-boto3/spkgbuild
+changelog "python/python-boto3/spkgbuild" "Upgraded from version 1.24.75 to version 1.24.76"
+sed -i -e "s/version=1.27.75/version=1.27.76/g" python/python-botocore/spkgbuild
+changelog "python/python-botocore/spkgbuild" "Upgraded from version 1.27.75 to version 1.27.76"
+sed -i -e "s/version=1.2.1/version=1.3.0/g" python/python-email-validator/spkgbuild
+changelog "python/python-email-validator/spkgbuild" "Upgraded from version 1.2.1 to version 1.3.0"
+sed -i -e "s/version=2.5.1/version=3.0.0/g" python/python-flask-sqlalchemy/spkgbuild
+changelog "python/python-flask-sqlalchemy/spkgbuild" "Upgraded from version 2.5.1 to version 3.0.0"
+sed -i -e "s/version=0.19.1/version=0.19.2/g" python/python-meilisearch/spkgbuild
+changelog "python/python-meilisearch/spkgbuild" "Upgraded from version 0.19.1 to version 0.19.2"
+sed -i -e "s/version=5.15.10/version=5.15.11/g" qt/qtwebengine/spkgbuild
+changelog "qt/qtwebengine/spkgbuild" "Upgraded from version 5.15.10 to version 5.15.11"
