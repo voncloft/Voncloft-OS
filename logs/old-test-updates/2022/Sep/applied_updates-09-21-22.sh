843c6e4927b8f64934622012e6fcfe75d698eb6e
Voncloft <voncloft@gmail.com>
2022-09-21 16:42:27 -0400

    Minor Updates

--- logs/old-test-updates/2022/Sep/applied_updates-09-21-22.sh
+++ logs/old-test-updates/2022/Sep/applied_updates-09-21-22.sh
@@ -4,3 +4,31 @@ sed -i -e "s/version=515.65.01/version=515.76/g" nonfree/nvidia-32/spkgbuild
 changelog "nonfree/nvidia-32/spkgbuild" "Upgraded from version 515.65.01 to version 515.76"
 sed -i -e "s/version=515.65.01/version=515.76/g" nonfree/nvidia-modules/spkgbuild
 changelog "nonfree/nvidia-modules/spkgbuild" "Upgraded from version 515.65.01 to version 515.76"
+sed -i -e "s/version=1.16.1/version=1.16.2/g" core/cairomm/spkgbuild
+changelog "core/cairomm/spkgbuild" "Upgraded from version 1.16.1 to version 1.16.2"
+sed -i -e "s/version=1.9.1/version=1.9.2/g" core/deadbeef/spkgbuild
+changelog "core/deadbeef/spkgbuild" "Upgraded from version 1.9.1 to version 1.9.2"
+sed -i -e "s/version=4.20.1/version=4.21/g" core/i3/spkgbuild
+changelog "core/i3/spkgbuild" "Upgraded from version 4.20.1 to version 4.21"
+sed -i -e "s/version=4.20.1/version=4.21/g" core/i3-gaps/spkgbuild
+changelog "core/i3-gaps/spkgbuild" "Upgraded from version 4.20.1 to version 4.21"
+sed -i -e "s/version=3.3.27/version=3.3.28/g" extra/flexget/spkgbuild
+changelog "extra/flexget/spkgbuild" "Upgraded from version 3.3.27 to version 3.3.28"
+sed -i -e "s/version=9.19.4/version=9.19.5/g" networking/bind/spkgbuild
+changelog "networking/bind/spkgbuild" "Upgraded from version 9.19.4 to version 9.19.5"
+sed -i -e "s/version=106.0b1/version=106.0b2/g" networking/firefox/spkgbuild
+changelog "networking/firefox/spkgbuild" "Upgraded from version 106.0b1 to version 106.0b2"
+sed -i -e "s/version=105.0b4/version=106.0b1/g" networking/thunderbird/spkgbuild
+changelog "networking/thunderbird/spkgbuild" "Upgraded from version 105.0b4 to version 106.0b1"
+sed -i -e "s/version=1.24.76/version=1.24.77/g" python/python-boto3/spkgbuild
+changelog "python/python-boto3/spkgbuild" "Upgraded from version 1.24.76 to version 1.24.77"
+sed -i -e "s/version=1.27.76/version=1.27.77/g" python/python-botocore/spkgbuild
+changelog "python/python-botocore/spkgbuild" "Upgraded from version 1.27.76 to version 1.27.77"
+sed -i -e "s/version=0.19.2/version=0.20.0/g" python/python-meilisearch/spkgbuild
+changelog "python/python-meilisearch/spkgbuild" "Upgraded from version 0.19.2 to version 0.20.0"
+sed -i -e "s/version=2022.9.20/version=2022.9.21/g" python/python-pipenv/spkgbuild
+changelog "python/python-pipenv/spkgbuild" "Upgraded from version 2022.9.20 to version 2022.9.21"
+sed -i -e "s/version=0.3.0/version=0.3.1/g" python/python-plette/spkgbuild
+changelog "python/python-plette/spkgbuild" "Upgraded from version 0.3.0 to version 0.3.1"
+sed -i -e "s/version=8.0.1/version=8.1.0/g" python/python-tenacity/spkgbuild
+changelog "python/python-tenacity/spkgbuild" "Upgraded from version 8.0.1 to version 8.1.0"
