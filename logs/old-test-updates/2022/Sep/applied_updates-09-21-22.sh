e14d8559ccd6d2f6979b699d59bf88401d753760
Voncloft <voncloft@gmail.com>
2022-09-21 08:09:29 -0400

    Minor Updates

--- logs/old-test-updates/2022/Sep/applied_updates-09-21-22.sh
+++ logs/old-test-updates/2022/Sep/applied_updates-09-21-22.sh
@@ -2,3 +2,5 @@ sed -i -e "s/version=515.65.01/version=515.76/g" nonfree/nvidia/spkgbuild
 changelog "nonfree/nvidia/spkgbuild" "Upgraded from version 515.65.01 to version 515.76"
 sed -i -e "s/version=515.65.01/version=515.76/g" nonfree/nvidia-32/spkgbuild
 changelog "nonfree/nvidia-32/spkgbuild" "Upgraded from version 515.65.01 to version 515.76"
+sed -i -e "s/version=515.65.01/version=515.76/g" nonfree/nvidia-modules/spkgbuild
+changelog "nonfree/nvidia-modules/spkgbuild" "Upgraded from version 515.65.01 to version 515.76"
