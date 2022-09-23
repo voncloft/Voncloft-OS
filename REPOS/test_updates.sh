9eabad58bb3c394b9a9813d739015c24e6c49126
Voncloft <voncloft@gmail.com>
2022-09-22 22:48:07 -0400

    Minor Updates

---
+++ REPOS/test_updates.sh
@@ -0,0 +1,22 @@
+sed -i -e "s/version=1.63.0/version=1.64.0/g" compilers/rust/spkgbuild
+changelog "compilers/rust/spkgbuild" "Upgraded from version 1.63.0 to version 1.64.0"
+sed -i -e "s/version=1.14.0/version=1.15.0/g" core/dbus/spkgbuild
+changelog "core/dbus/spkgbuild" "Upgraded from version 1.14.0 to version 1.15.0"
+sed -i -e "s/version=1.4.0/version=1.4.1/g" core/flac/spkgbuild
+changelog "core/flac/spkgbuild" "Upgraded from version 1.4.0 to version 1.4.1"
+sed -i -e "s/version=5.0.0/version=5.0.1/g" core/streamlink/spkgbuild
+changelog "core/streamlink/spkgbuild" "Upgraded from version 5.0.0 to version 5.0.1"
+sed -i -e "s/version=3.3.28/version=3.3.29/g" extra/flexget/spkgbuild
+changelog "extra/flexget/spkgbuild" "Upgraded from version 3.3.28 to version 3.3.29"
+sed -i -e "s/version=1.24.77/version=1.24.79/g" python/python-boto3/spkgbuild
+changelog "python/python-boto3/spkgbuild" "Upgraded from version 1.24.77 to version 1.24.79"
+sed -i -e "s/version=1.27.77/version=1.27.79/g" python/python-botocore/spkgbuild
+changelog "python/python-botocore/spkgbuild" "Upgraded from version 1.27.77 to version 1.27.79"
+sed -i -e "s/version=14.2.0/version=14.2.1/g" python/python-faker/spkgbuild
+changelog "python/python-faker/spkgbuild" "Upgraded from version 14.2.0 to version 14.2.1"
+sed -i -e "s/version=0.15.3/version=0.15.4/g" python/python-libtmux/spkgbuild
+changelog "python/python-libtmux/spkgbuild" "Upgraded from version 0.15.3 to version 0.15.4"
+sed -i -e "s/version=3.15.2/version=3.15.3/g" python/python-peewee/spkgbuild
+changelog "python/python-peewee/spkgbuild" "Upgraded from version 3.15.2 to version 3.15.3"
+sed -i -e "s/version=2022.9.5/version=2022.9.6/g" server/homeassistant/spkgbuild
+changelog "server/homeassistant/spkgbuild" "Upgraded from version 2022.9.5 to version 2022.9.6"
