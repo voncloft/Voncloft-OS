826d4c44484a3be7722e44a50e44321d449d36f2
Voncloft <voncloft@gmail.com>
2022-09-24 14:21:07 -0400

    Minor Updates

---
+++ logs/old-test-updates/2022/Sep/applied_updates-09-24-22.sh
@@ -0,0 +1,40 @@
+sed -i -e "s/version=31.0.3/version=33.0.3/g" core/android-tools/spkgbuild
+changelog "core/android-tools/spkgbuild" "Upgraded from version 31.0.3 to version 33.0.3"
+sed -i -e "s/version=0.103.1/version=0.104.0/g" core/hugo/spkgbuild
+changelog "core/hugo/spkgbuild" "Upgraded from version 0.103.1 to version 0.104.0"
+sed -i -e "s/version=5.19.10/version=5.19.11/g" core/linux-desktop/spkgbuild
+changelog "core/linux-desktop/spkgbuild" "Upgraded from version 5.19.10 to version 5.19.11"
+sed -i -e "s/version=5.19.10/version=5.19.11/g" core/linux-router/spkgbuild
+changelog "core/linux-router/spkgbuild" "Upgraded from version 5.19.10 to version 5.19.11"
+sed -i -e "s/version=7.0.4/version=7.0.5/g" core/redis/spkgbuild
+changelog "core/redis/spkgbuild" "Upgraded from version 7.0.4 to version 7.0.5"
+sed -i -e "s/version=3.3.29/version=3.3.30/g" extra/flexget/spkgbuild
+changelog "extra/flexget/spkgbuild" "Upgraded from version 3.3.29 to version 3.3.30"
+sed -i -e "s/version=106.0b2/version=106.0b3/g" networking/firefox/spkgbuild
+changelog "networking/firefox/spkgbuild" "Upgraded from version 106.0b2 to version 106.0b3"
+sed -i -e "s/version=1.24.79/version=1.24.80/g" python/python-boto3/spkgbuild
+changelog "python/python-boto3/spkgbuild" "Upgraded from version 1.24.79 to version 1.24.80"
+sed -i -e "s/version=1.27.79/version=1.27.80/g" python/python-botocore/spkgbuild
+changelog "python/python-botocore/spkgbuild" "Upgraded from version 1.27.79 to version 1.27.80"
+sed -i -e "s/version=1.3.0/version=1.4.0/g" python/python-cairocffi/spkgbuild
+changelog "python/python-cairocffi/spkgbuild" "Upgraded from version 1.3.0 to version 1.4.0"
+sed -i -e "s/version=1.6.2/version=1.6.3/g" python/python-canonicaljson/spkgbuild
+changelog "python/python-canonicaljson/spkgbuild" "Upgraded from version 1.6.2 to version 1.6.3"
+sed -i -e "s/version=3.13.1/version=3.14.0/g" python/python-django-rest-framework/spkgbuild
+changelog "python/python-django-rest-framework/spkgbuild" "Upgraded from version 3.13.1 to version 3.14.0"
+sed -i -e "s/version=5.0.1/version=5.0.2/g" python/python-flask-security-too/spkgbuild
+changelog "python/python-flask-security-too/spkgbuild" "Upgraded from version 5.0.1 to version 5.0.2"
+sed -i -e "s/version=1.0.5/version=1.1.0/g" python/python-kikit/spkgbuild
+changelog "python/python-kikit/spkgbuild" "Upgraded from version 1.0.5 to version 1.1.0"
+sed -i -e "s/version=1.8.1/version=1.9/g" python/python-pypandoc/spkgbuild
+changelog "python/python-pypandoc/spkgbuild" "Upgraded from version 1.8.1 to version 1.9"
+sed -i -e "s/version=6.1.1/version=6.1.2/g" python/python-softlayer/spkgbuild
+changelog "python/python-softlayer/spkgbuild" "Upgraded from version 6.1.1 to version 6.1.2"
+sed -i -e "s/version=0.4.2/version=0.4.3/g" python/python-sqlparse/spkgbuild
+changelog "python/python-sqlparse/spkgbuild" "Upgraded from version 0.4.2 to version 0.4.3"
+sed -i -e "s/version=3.15.1/version=3.15.2/g" python/python-trimesh/spkgbuild
+changelog "python/python-trimesh/spkgbuild" "Upgraded from version 3.15.1 to version 3.15.2"
+sed -i -e "s/version=0.11.1/version=0.12.0/g" python/python-xcffib/spkgbuild
+changelog "python/python-xcffib/spkgbuild" "Upgraded from version 0.11.1 to version 0.12.0"
+sed -i -e "s/version=3.11.17/version=3.11.18/g" server/blacklist-hosts/spkgbuild
+changelog "server/blacklist-hosts/spkgbuild" "Upgraded from version 3.11.17 to version 3.11.18"
