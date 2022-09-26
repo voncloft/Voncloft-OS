3c6d38919c430a7a018eeed463f8fa397bf51b19
Voncloft <voncloft@gmail.com>
2022-09-26 09:11:49 -0400

    Minor Updates

---
+++ logs/old-test-updates/2022/Sep/applied_updates-09-26-22.sh
@@ -0,0 +1,16 @@
+sed -i -e "s/version=5.1.1/version=5.1.2/g" media/ffmpeg/spkgbuild
+changelog "media/ffmpeg/spkgbuild" "Upgraded from version 5.1.1 to version 5.1.2"
+sed -i -e "s/version=2.65/version=2.66/g" libs/libcap/spkgbuild
+changelog "libs/libcap/spkgbuild" "Upgraded from version 2.65 to version 2.66"
+sed -i -e "s/version=1.3.4/version=1.3.5/g" python/python-aiopg/spkgbuild
+changelog "python/python-aiopg/spkgbuild" "Upgraded from version 1.3.4 to version 1.3.5"
+sed -i -e "s/version=1.1.0/version=1.1.1/g" python/python-kikit/spkgbuild
+changelog "python/python-kikit/spkgbuild" "Upgraded from version 1.1.0 to version 1.1.1"
+sed -i -e "s/version=16.4.0/version=16.5.0/g" python/python-path/spkgbuild
+changelog "python/python-path/spkgbuild" "Upgraded from version 16.4.0 to version 16.5.0"
+sed -i -e "s/version=2022.9.21/version=2022.9.24/g" python/python-pipenv/spkgbuild
+changelog "python/python-pipenv/spkgbuild" "Upgraded from version 2022.9.21 to version 2022.9.24"
+sed -i -e "s/version=0.12.0/version=0.12.1/g" python/python-xcffib/spkgbuild
+changelog "python/python-xcffib/spkgbuild" "Upgraded from version 0.12.0 to version 0.12.1"
+sed -i -e "s/version=2.0.4/version=2.1.0/g" python/python-xmlschema/spkgbuild
+changelog "python/python-xmlschema/spkgbuild" "Upgraded from version 2.0.4 to version 2.1.0"
