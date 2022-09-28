ffd736ebf03144af98e6253ce67b93218844cebc
Voncloft <voncloft@gmail.com>
2022-09-28 17:31:29 -0400

    Minor Updates

---
+++ logs/old-test-updates/2022/Sep/applied_updates-09-28-22.sh
@@ -0,0 +1,70 @@
+sed -i -e "s/version=5.1.16/version=5.2/g" core/bash/spkgbuild
+changelog "core/bash/spkgbuild" "Upgraded from version 5.1.16 to version 5.2"
+sed -i -e "s/version=3.0.6/version=3.0.7/g" core/dkms/spkgbuild
+changelog "core/dkms/spkgbuild" "Upgraded from version 3.0.6 to version 3.0.7"
+sed -i -e "s/version=3.7.7/version=3.7.8/g" core/gnutls/spkgbuild
+changelog "core/gnutls/spkgbuild" "Upgraded from version 3.7.7 to version 3.7.8"
+sed -i -e "s/version=0.104.0/version=0.104.1/g" core/hugo/spkgbuild
+changelog "core/hugo/spkgbuild" "Upgraded from version 0.104.0 to version 0.104.1"
+sed -i -e "s/version=5.19.11/version=5.19.12/g" core/linux-desktop/spkgbuild
+changelog "core/linux-desktop/spkgbuild" "Upgraded from version 5.19.11 to version 5.19.12"
+sed -i -e "s/version=5.19.11/version=5.19.12/g" core/linux-router/spkgbuild
+changelog "core/linux-router/spkgbuild" "Upgraded from version 5.19.11 to version 5.19.12"
+sed -i -e "s/version=8.1.2/version=8.2/g" core/readline/spkgbuild
+changelog "core/readline/spkgbuild" "Upgraded from version 8.1.2 to version 8.2"
+sed -i -e "s/version=4.11/version=4.12/g" extra/gnucash/spkgbuild
+changelog "extra/gnucash/spkgbuild" "Upgraded from version 4.11 to version 4.12"
+sed -i -e "s/version=0.10.7/version=0.10.8/g" libs/libofx/spkgbuild
+changelog "libs/libofx/spkgbuild" "Upgraded from version 0.10.7 to version 0.10.8"
+sed -i -e "s/version=2.15.0/version=2.16.0/g" libs/libva/spkgbuild
+changelog "libs/libva/spkgbuild" "Upgraded from version 2.15.0 to version 2.16.0"
+sed -i -e "s/version=106.0b3/version=106.0b5/g" networking/firefox/spkgbuild
+changelog "networking/firefox/spkgbuild" "Upgraded from version 106.0b3 to version 106.0b5"
+sed -i -e "s/version=106.0b1/version=106.0b2/g" networking/thunderbird/spkgbuild
+changelog "networking/thunderbird/spkgbuild" "Upgraded from version 106.0b1 to version 106.0b2"
+sed -i -e "s/version=1.8.0/version=1.9.0/g" plasma/plasma-wayland-protocols/spkgbuild
+changelog "plasma/plasma-wayland-protocols/spkgbuild" "Upgraded from version 1.8.0 to version 1.9.0"
+sed -i -e "s/version=1.24.80/version=1.24.82/g" python/python-boto3/spkgbuild
+changelog "python/python-boto3/spkgbuild" "Upgraded from version 1.24.80 to version 1.24.82"
+sed -i -e "s/version=1.27.80/version=1.27.82/g" python/python-botocore/spkgbuild
+changelog "python/python-botocore/spkgbuild" "Upgraded from version 1.27.80 to version 1.27.82"
+sed -i -e "s/version=14.2.1/version=15.0.0/g" python/python-faker/spkgbuild
+changelog "python/python-faker/spkgbuild" "Upgraded from version 14.2.1 to version 15.0.0"
+sed -i -e "s/version=3.9.0/version=3.10.0/g" python/python-gitlab/spkgbuild
+changelog "python/python-gitlab/spkgbuild" "Upgraded from version 3.9.0 to version 3.10.0"
+sed -i -e "s/version=2.62.0/version=2.63.0/g" python/python-google-api-python-client/spkgbuild
+changelog "python/python-google-api-python-client/spkgbuild" "Upgraded from version 2.62.0 to version 2.63.0"
+sed -i -e "s/version=2.11.1/version=2.12.0/g" python/python-google-auth/spkgbuild
+changelog "python/python-google-auth/spkgbuild" "Upgraded from version 2.11.1 to version 2.12.0"
+sed -i -e "s/version=3.5.0/version=3.5.1/g" python/python-hunter/spkgbuild
+changelog "python/python-hunter/spkgbuild" "Upgraded from version 3.5.0 to version 3.5.1"
+sed -i -e "s/version=6.15.3/version=6.16.0/g" python/python-ipykernel/spkgbuild
+changelog "python/python-ipykernel/spkgbuild" "Upgraded from version 6.15.3 to version 6.16.0"
+sed -i -e "s/version=4.11.1/version=5.0.0/g" python/python-jupyter_core/spkgbuild
+changelog "python/python-jupyter_core/spkgbuild" "Upgraded from version 4.11.1 to version 5.0.0"
+sed -i -e "s/version=2.4.3/version=2.4.5/g" python/python-markdown2/spkgbuild
+changelog "python/python-markdown2/spkgbuild" "Upgraded from version 2.4.3 to version 2.4.5"
+sed -i -e "s/version=0.3.0/version=0.3.1/g" python/python-mdit_py_plugins/spkgbuild
+changelog "python/python-mdit_py_plugins/spkgbuild" "Upgraded from version 0.3.0 to version 0.3.1"
+sed -i -e "s/version=0.20.0/version=0.21.0/g" python/python-meilisearch/spkgbuild
+changelog "python/python-meilisearch/spkgbuild" "Upgraded from version 0.20.0 to version 0.21.0"
+sed -i -e "s/version=0.18.0/version=0.18.1/g" python/python-myst-parser/spkgbuild
+changelog "python/python-myst-parser/spkgbuild" "Upgraded from version 0.18.0 to version 0.18.1"
+sed -i -e "s/version=0.12.4/version=0.13.0/g" python/python-pyqtgraph/spkgbuild
+changelog "python/python-pyqtgraph/spkgbuild" "Upgraded from version 0.12.4 to version 0.13.0"
+sed -i -e "s/version=2.7.1/version=2.8.1/g" python/python-pytest-checkdocs/spkgbuild
+changelog "python/python-pytest-checkdocs/spkgbuild" "Upgraded from version 2.7.1 to version 2.8.1"
+sed -i -e "s/version=3.0.0/version=4.0.0/g" python/python-pytest-cov/spkgbuild
+changelog "python/python-pytest-cov/spkgbuild" "Upgraded from version 3.0.0 to version 4.0.0"
+sed -i -e "s/version=3.8.2/version=3.9.0/g" python/python-pytest-mock/spkgbuild
+changelog "python/python-pytest-mock/spkgbuild" "Upgraded from version 3.8.2 to version 3.9.0"
+sed -i -e "s/version=0.9.1/version=0.10.0/g" python/python-pytest-mypy/spkgbuild
+changelog "python/python-pytest-mypy/spkgbuild" "Upgraded from version 0.9.1 to version 0.10.0"
+sed -i -e "s/version=0.20.4/version=0.21.0/g" python/python-starlette/spkgbuild
+changelog "python/python-starlette/spkgbuild" "Upgraded from version 0.20.4 to version 0.21.0"
+sed -i -e "s/version=2.86/version=2.87/g" server/dnsmasq/spkgbuild
+changelog "server/dnsmasq/spkgbuild" "Upgraded from version 2.86 to version 2.87"
+sed -i -e "s/version=2022.9.6/version=2022.10.0/g" server/homeassistant/spkgbuild
+changelog "server/homeassistant/spkgbuild" "Upgraded from version 2022.9.6 to version 2022.10.0"
+sed -i -e "s/version=1.28.2.6151/version=1.29.0.6244/g" server/plex-media-server/spkgbuild
+changelog "server/plex-media-server/spkgbuild" "Upgraded from version 1.28.2.6151 to version 1.29.0.6244"
