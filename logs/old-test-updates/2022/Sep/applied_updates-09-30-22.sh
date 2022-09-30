e7baf55bebd885de3535055be0eac35c7ca9a8f5
Voncloft <voncloft@gmail.com>
2022-09-30 17:56:53 -0400

    Minor Updates

---
+++ logs/old-test-updates/2022/Sep/applied_updates-09-30-22.sh
@@ -0,0 +1,36 @@
+sed -i -e "s/version=0.104.1/version=0.104.2/g" core/hugo/spkgbuild
+changelog "core/hugo/spkgbuild" "Upgraded from version 0.104.1 to version 0.104.2"
+sed -i -e "s/version=0.3.58/version=0.3.59/g" core/pipewire/spkgbuild
+changelog "core/pipewire/spkgbuild" "Upgraded from version 0.3.58 to version 0.3.59"
+sed -i -e "s/version=5.2.6/version=5.2.7/g" core/xz/spkgbuild
+changelog "core/xz/spkgbuild" "Upgraded from version 5.2.6 to version 5.2.7"
+sed -i -e "s/version=21.6/version=21.7/g" extra/protobuf/spkgbuild
+changelog "extra/protobuf/spkgbuild" "Upgraded from version 21.6 to version 21.7"
+sed -i -e "s/version=1.6.0/version=1.7.0/g" libs/libmaxminddb/spkgbuild
+changelog "libs/libmaxminddb/spkgbuild" "Upgraded from version 1.6.0 to version 1.7.0"
+sed -i -e "s/version=106.0b5/version=106.0b6/g" networking/firefox/spkgbuild
+changelog "networking/firefox/spkgbuild" "Upgraded from version 106.0b5 to version 106.0b6"
+sed -i -e "s/version=106.0b2/version=106.0b3/g" networking/thunderbird/spkgbuild
+changelog "networking/thunderbird/spkgbuild" "Upgraded from version 106.0b2 to version 106.0b3"
+sed -i -e "s/version=1.24.82/version=1.24.84/g" python/python-boto3/spkgbuild
+changelog "python/python-boto3/spkgbuild" "Upgraded from version 1.24.82 to version 1.24.84"
+sed -i -e "s/version=1.27.82/version=1.27.84/g" python/python-botocore/spkgbuild
+changelog "python/python-botocore/spkgbuild" "Upgraded from version 1.27.82 to version 1.27.84"
+sed -i -e "s/version=0.65.1/version=0.66.0/g" python/python-cfn-lint/spkgbuild
+changelog "python/python-cfn-lint/spkgbuild" "Upgraded from version 0.65.1 to version 0.66.0"
+sed -i -e "s/version=6.4.4/version=6.5.0/g" python/python-coverage/spkgbuild
+changelog "python/python-coverage/spkgbuild" "Upgraded from version 6.4.4 to version 6.5.0"
+sed -i -e "s/version=2022.9.1/version=2022.9.2/g" python/python-distributed/spkgbuild
+changelog "python/python-distributed/spkgbuild" "Upgraded from version 2022.9.1 to version 2022.9.2"
+sed -i -e "s/version=4.37.3/version=4.37.4/g" python/python-fonttools/spkgbuild
+changelog "python/python-fonttools/spkgbuild" "Upgraded from version 4.37.3 to version 4.37.4"
+sed -i -e "s/version=6.54.6/version=6.55.0/g" python/python-hypothesis/spkgbuild
+changelog "python/python-hypothesis/spkgbuild" "Upgraded from version 6.54.6 to version 6.55.0"
+sed -i -e "s/version=3.20.0/version=3.21.0/g" python/python-openmdao/spkgbuild
+changelog "python/python-openmdao/spkgbuild" "Upgraded from version 3.20.0 to version 3.21.0"
+sed -i -e "s/version=0.13.0/version=0.13.1/g" python/python-pyqtgraph/spkgbuild
+changelog "python/python-pyqtgraph/spkgbuild" "Upgraded from version 0.13.0 to version 0.13.1"
+sed -i -e "s/version=65.4.0/version=65.4.1/g" python/python-setuptools/spkgbuild
+changelog "python/python-setuptools/spkgbuild" "Upgraded from version 65.4.0 to version 65.4.1"
+sed -i -e "s/version=2022.09.15/version=2022.09.29/g" python/python-sphinx-furo/spkgbuild
+changelog "python/python-sphinx-furo/spkgbuild" "Upgraded from version 2022.09.15 to version 2022.09.29"
