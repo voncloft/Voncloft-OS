16dbb239fc1d9092c9cdfe124dfceee7972627b8
Voncloft <voncloft@gmail.com>
2022-10-04 13:02:27 -0400

    fix part of kernel dkms upgrade need to work on nvidia

--- REPOS/core/linux-desktop/post-upgrade.sh
+++ REPOS/core/linux-desktop/post-upgrade.sh
@@ -1,14 +1,14 @@
 #!/bin/sh
 #kernel_version=$(ls /usr/lib/modules)
 grub-mkconfig -o /boot/grub/grub.cfg
-version=$(cat /etc/LINUX_VERSION)
+version=$(cat /lib/modules/KERNELVERSION)
 #virtualbox modules
-cd /usr/src/virtual*
-KER_VER=$version make
-KER_VER=$version make install
+cd /usr/src/vbox*
+KERN_DIR=/lib/modules/$version/build KER_VER=$version make
+KERN_DIR=/lib/modules/$version/build KER_VER=$version make install
 
 #virtualbox
-sudo dkms install -m nvidia -v $version
+sudo dkms install -m nvidia -k $version
 #sudo scratch install -rc nvidia-modules nvidia nvidia-32 virtualbox-modules
 #sudo dkms autoinstall -k $kernel_version
 #cd /usr/src/vbox*
