8f1fcfc23c24aaf0ec20e6677a4b7ad22af5f2bd
Voncloft <voncloft@gmail.com>
2022-10-05 08:31:54 -0400

    Minor Updates

---
+++ logs/old-test-updates/2022/Oct/applied_updates-10-05-22.sh
@@ -0,0 +1,4 @@
+sed -i -e "s/version=5.19.13/version=5.19.14/g" core/linux-desktop/spkgbuild
+changelog "core/linux-desktop/spkgbuild" "Upgraded from version 5.19.13 to version 5.19.14"
+sed -i -e "s/version=5.19.12/version=5.19.14/g" core/linux-router/spkgbuild
+changelog "core/linux-router/spkgbuild" "Upgraded from version 5.19.12 to version 5.19.14"
