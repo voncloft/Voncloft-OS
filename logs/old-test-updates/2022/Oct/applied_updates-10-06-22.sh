20ef12d92bfc593d51e4f3d3701e0d56d9e1feb0
Voncloft <voncloft@gmail.com>
2022-10-06 06:18:51 -0400

    Minor Updates

---
+++ logs/old-test-updates/2022/Oct/applied_updates-10-06-22.sh
@@ -0,0 +1,68 @@
+sed -i -e "s/version=15.0.1/version=15.0.2/g" compilers/clang/spkgbuild
+changelog "compilers/clang/spkgbuild" "Upgraded from version 15.0.1 to version 15.0.2"
+sed -i -e "s/version=15.0.1/version=15.0.2/g" compilers/lld/spkgbuild
+changelog "compilers/lld/spkgbuild" "Upgraded from version 15.0.1 to version 15.0.2"
+sed -i -e "s/version=15.0.1/version=15.0.2/g" compilers/llvm/spkgbuild
+changelog "compilers/llvm/spkgbuild" "Upgraded from version 15.0.1 to version 15.0.2"
+sed -i -e "s/version=1.15.0/version=1.15.2/g" core/dbus/spkgbuild
+changelog "core/dbus/spkgbuild" "Upgraded from version 1.15.0 to version 1.15.2"
+sed -i -e "s/version=5.1/version=5.2/g" core/dmenu/spkgbuild
+changelog "core/dmenu/spkgbuild" "Upgraded from version 5.1 to version 5.2"
+sed -i -e "s/version=0.104.2/version=0.104.3/g" core/hugo/spkgbuild
+changelog "core/hugo/spkgbuild" "Upgraded from version 0.104.2 to version 0.104.3"
+sed -i -e "s/version=5.19.0/version=6.0.0/g" core/iproute2/spkgbuild
+changelog "core/iproute2/spkgbuild" "Upgraded from version 5.19.0 to version 6.0.0"
+sed -i -e "s/version=6.2.2/version=6.2.3/g" core/mercurial/spkgbuild
+changelog "core/mercurial/spkgbuild" "Upgraded from version 6.2.2 to version 6.2.3"
+sed -i -e "s/version=2.0.15/version=2.0.16/g" core/numactl/spkgbuild
+changelog "core/numactl/spkgbuild" "Upgraded from version 2.0.15 to version 2.0.16"
+sed -i -e "s/version=1.20.3/version=1.21.1/g" media/gst-libav/spkgbuild
+changelog "media/gst-libav/spkgbuild" "Upgraded from version 1.20.3 to version 1.21.1"
+sed -i -e "s/version=1.20.3/version=1.21.1/g" media/gst-plugins-bad/spkgbuild
+changelog "media/gst-plugins-bad/spkgbuild" "Upgraded from version 1.20.3 to version 1.21.1"
+sed -i -e "s/version=1.20.3/version=1.21.1/g" media/gst-plugins-base/spkgbuild
+changelog "media/gst-plugins-base/spkgbuild" "Upgraded from version 1.20.3 to version 1.21.1"
+sed -i -e "s/version=1.20.3/version=1.21.1/g" media/gst-plugins-good/spkgbuild
+changelog "media/gst-plugins-good/spkgbuild" "Upgraded from version 1.20.3 to version 1.21.1"
+sed -i -e "s/version=1.20.3/version=1.21.1/g" media/gst-plugins-ugly/spkgbuild
+changelog "media/gst-plugins-ugly/spkgbuild" "Upgraded from version 1.20.3 to version 1.21.1"
+sed -i -e "s/version=1.20.3/version=1.21.1/g" media/gstreamer/spkgbuild
+changelog "media/gstreamer/spkgbuild" "Upgraded from version 1.20.3 to version 1.21.1"
+sed -i -e "s/version=15.0.1/version=15.0.2/g" multilib/llvm-32/spkgbuild
+changelog "multilib/llvm-32/spkgbuild" "Upgraded from version 15.0.1 to version 15.0.2"
+sed -i -e "s/version=0.10.1/version=0.10.2/g" libs/lirc/spkgbuild
+changelog "libs/lirc/spkgbuild" "Upgraded from version 0.10.1 to version 0.10.2"
+sed -i -e "s/version=2.24.0/version=2.24.1/g" libs/sdl2/spkgbuild
+changelog "libs/sdl2/spkgbuild" "Upgraded from version 2.24.0 to version 2.24.1"
+sed -i -e "s/version=106.0b7/version=106.0b8/g" networking/firefox/spkgbuild
+changelog "networking/firefox/spkgbuild" "Upgraded from version 106.0b7 to version 106.0b8"
+sed -i -e "s/version=9.0/version=9.1/g" networking/openssh/spkgbuild
+changelog "networking/openssh/spkgbuild" "Upgraded from version 9.0 to version 9.1"
+sed -i -e "s/version=106.0b3/version=106.0b4/g" networking/thunderbird/spkgbuild
+changelog "networking/thunderbird/spkgbuild" "Upgraded from version 106.0b3 to version 106.0b4"
+sed -i -e "s/version=2022.09.01/version=2022.10.04/g" networking/yt-dlp/spkgbuild
+changelog "networking/yt-dlp/spkgbuild" "Upgraded from version 2022.09.01 to version 2022.10.04"
+sed -i -e "s/version=5.2.2/version=6.0.0/g" python/python-apispec/spkgbuild
+changelog "python/python-apispec/spkgbuild" "Upgraded from version 5.2.2 to version 6.0.0"
+sed -i -e "s/version=1.24.85/version=1.24.87/g" python/python-boto3/spkgbuild
+changelog "python/python-boto3/spkgbuild" "Upgraded from version 1.24.85 to version 1.24.87"
+sed -i -e "s/version=1.27.85/version=1.27.87/g" python/python-botocore/spkgbuild
+changelog "python/python-botocore/spkgbuild" "Upgraded from version 1.27.85 to version 1.27.87"
+sed -i -e "s/version=4.16.0/version=4.17.0/g" python/python-braintree/spkgbuild
+changelog "python/python-braintree/spkgbuild" "Upgraded from version 4.16.0 to version 4.17.0"
+sed -i -e "s/version=8.4.2/version=8.4.3/g" python/python-elasticsearch/spkgbuild
+changelog "python/python-elasticsearch/spkgbuild" "Upgraded from version 8.4.2 to version 8.4.3"
+sed -i -e "s/version=2.63.0/version=2.64.0/g" python/python-google-api-python-client/spkgbuild
+changelog "python/python-google-api-python-client/spkgbuild" "Upgraded from version 2.63.0 to version 2.64.0"
+sed -i -e "s/version=6.56.0/version=6.56.1/g" python/python-hypothesis/spkgbuild
+changelog "python/python-hypothesis/spkgbuild" "Upgraded from version 6.56.0 to version 6.56.1"
+sed -i -e "s/version=1.4.0/version=1.5.0/g" python/python-numpydoc/spkgbuild
+changelog "python/python-numpydoc/spkgbuild" "Upgraded from version 1.4.0 to version 1.5.0"
+sed -i -e "s/version=2022.9.24/version=2022.10.4/g" python/python-pipenv/spkgbuild
+changelog "python/python-pipenv/spkgbuild" "Upgraded from version 2022.9.24 to version 2022.10.4"
+sed -i -e "s/version=3.9.0/version=3.10.0/g" python/python-pytest-mock/spkgbuild
+changelog "python/python-pytest-mock/spkgbuild" "Upgraded from version 3.9.0 to version 3.10.0"
+sed -i -e "s/version=2.3.22/version=2.3.23/g" ruby-gems/ruby-bundler/spkgbuild
+changelog "ruby-gems/ruby-bundler/spkgbuild" "Upgraded from version 2.3.22 to version 2.3.23"
+sed -i -e "s/version=2.36/version=2.37/g" xorg/xkeyboard-config/spkgbuild
+changelog "xorg/xkeyboard-config/spkgbuild" "Upgraded from version 2.36 to version 2.37"
