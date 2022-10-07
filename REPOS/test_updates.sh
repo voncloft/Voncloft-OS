1858d52e6cdc7c442596e327bef6c7eaf102ca3b
Voncloft <voncloft@gmail.com>
2022-10-07 17:41:27 -0400

    Minor Updates

--- REPOS/test_updates.sh
+++
@@ -1,2 +0,0 @@
-sed -i -e "s/version=0.63.2/version=0.63.3/g" compilers/meson/spkgbuild
-changelog "compilers/meson/spkgbuild" "Upgraded from version 0.63.2 to version 0.63.3"
---
+++ logs/old-test-updates/2022/Oct/applied_updates-10-07-22.sh
@@ -0,0 +1,34 @@
+sed -i -e "s/version=0.63.2/version=0.63.3/g" compilers/meson/spkgbuild
+changelog "compilers/meson/spkgbuild" "Upgraded from version 0.63.2 to version 0.63.3"
+sed -i -e "s/version=3.2.2/version=3.4.0/g" core/freeglut/spkgbuild
+changelog "core/freeglut/spkgbuild" "Upgraded from version 3.2.2 to version 3.4.0"
+sed -i -e "s/version=3.0.0/version=3.0.1/g" core/icewm/spkgbuild
+changelog "core/icewm/spkgbuild" "Upgraded from version 3.0.0 to version 3.0.1"
+sed -i -e "s/version=3.3.30/version=3.3.31/g" extra/flexget/spkgbuild
+changelog "extra/flexget/spkgbuild" "Upgraded from version 3.3.30 to version 3.3.31"
+sed -i -e "s/version=1.45/version=1.46/g" libs/libgpg-error/spkgbuild
+changelog "libs/libgpg-error/spkgbuild" "Upgraded from version 1.45 to version 1.46"
+sed -i -e "s/version=3.0.14/version=3.0.15/g" libs/libical/spkgbuild
+changelog "libs/libical/spkgbuild" "Upgraded from version 3.0.14 to version 3.0.15"
+sed -i -e "s/version=1.6.1/version=1.6.2/g" libs/libksba/spkgbuild
+changelog "libs/libksba/spkgbuild" "Upgraded from version 1.6.1 to version 1.6.2"
+sed -i -e "s/version=3.5.3/version=3.6.0/g" libs/libressl/spkgbuild
+changelog "libs/libressl/spkgbuild" "Upgraded from version 3.5.3 to version 3.6.0"
+sed -i -e "s/version=106.0b8/version=106.0b9/g" networking/firefox/spkgbuild
+changelog "networking/firefox/spkgbuild" "Upgraded from version 106.0b8 to version 106.0b9"
+sed -i -e "s/version=1.51.0/version=1.52.0/g" python/python-aws-sam-translator/spkgbuild
+changelog "python/python-aws-sam-translator/spkgbuild" "Upgraded from version 1.51.0 to version 1.52.0"
+sed -i -e "s/version=1.24.87/version=1.24.89/g" python/python-boto3/spkgbuild
+changelog "python/python-boto3/spkgbuild" "Upgraded from version 1.24.87 to version 1.24.89"
+sed -i -e "s/version=1.27.87/version=1.27.89/g" python/python-botocore/spkgbuild
+changelog "python/python-botocore/spkgbuild" "Upgraded from version 1.27.87 to version 1.27.89"
+sed -i -e "s/version=4.1.0/version=5.0.0/g" python/python-hacking/spkgbuild
+changelog "python/python-hacking/spkgbuild" "Upgraded from version 4.1.0 to version 5.0.0"
+sed -i -e "s/version=1.2.0/version=1.3.2/g" python/python-poetry-core/spkgbuild
+changelog "python/python-poetry-core/spkgbuild" "Upgraded from version 1.2.0 to version 1.3.2"
+sed -i -e "s/version=0.9.1/version=0.10.0/g" python/python-requests-toolbelt/spkgbuild
+changelog "python/python-requests-toolbelt/spkgbuild" "Upgraded from version 0.9.1 to version 0.10.0"
+sed -i -e "s/version=3.11.19/version=3.11.20/g" server/blacklist-hosts/spkgbuild
+changelog "server/blacklist-hosts/spkgbuild" "Upgraded from version 3.11.19 to version 3.11.20"
+sed -i -e "s/version=2022.10.0/version=2022.10.1/g" server/homeassistant/spkgbuild
+changelog "server/homeassistant/spkgbuild" "Upgraded from version 2022.10.0 to version 2022.10.1"
