6658ead12e7a0b9033b9b5628308f594c70b7185
Voncloft <voncloft@gmail.com>
2022-10-08 18:23:07 -0400

    Minor Updates

---
+++ logs/old-test-updates/2022/Oct/applied_updates-10-08-22.sh
@@ -0,0 +1,12 @@
+sed -i -e "s/version=2.36.1/version=2.36.2/g" extra/discordchatexporter/spkgbuild
+changelog "extra/discordchatexporter/spkgbuild" "Upgraded from version 2.36.1 to version 2.36.2"
+sed -i -e "s/version=3.3.31/version=3.3.32/g" extra/flexget/spkgbuild
+changelog "extra/flexget/spkgbuild" "Upgraded from version 3.3.31 to version 3.3.32"
+sed -i -e "s/version=21.12.0/version=22.08.0/g" python/python-gevent/spkgbuild
+changelog "python/python-gevent/spkgbuild" "Upgraded from version 21.12.0 to version 22.08.0"
+sed -i -e "s/version=0.3.1/version=0.4.0/g" python/python-plette/spkgbuild
+changelog "python/python-plette/spkgbuild" "Upgraded from version 0.3.1 to version 0.4.0"
+sed -i -e "s/version=3.8.1/version=3.9.0/g" python/python-zipp/spkgbuild
+changelog "python/python-zipp/spkgbuild" "Upgraded from version 3.8.1 to version 3.9.0"
+sed -i -e "s/version=3.7.2/version=3.7.3/g" server/postfix/spkgbuild
+changelog "server/postfix/spkgbuild" "Upgraded from version 3.7.2 to version 3.7.3"
