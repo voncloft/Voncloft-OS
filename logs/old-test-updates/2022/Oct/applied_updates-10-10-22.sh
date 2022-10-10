92939a55f18bd5ee63f607a5a595267c2f7b19e2
Voncloft <voncloft@gmail.com>
2022-10-10 08:05:33 -0400

    Minor Updates

---
+++ logs/old-test-updates/2022/Oct/applied_updates-10-10-22.sh
@@ -0,0 +1,16 @@
+sed -i -e "s/version=3.6/version=3.7/g" core/weechat/spkgbuild
+changelog "core/weechat/spkgbuild" "Upgraded from version 3.6 to version 3.7"
+sed -i -e "s/version=3.3.32/version=3.3.33/g" extra/flexget/spkgbuild
+changelog "extra/flexget/spkgbuild" "Upgraded from version 3.3.32 to version 3.3.33"
+sed -i -e "s/version=4.0.0/version=4.0.1/g" python/python-bcrypt/spkgbuild
+changelog "python/python-bcrypt/spkgbuild" "Upgraded from version 4.0.0 to version 4.0.1"
+sed -i -e "s/version=2022.10.4/version=2022.10.9/g" python/python-pipenv/spkgbuild
+changelog "python/python-pipenv/spkgbuild" "Upgraded from version 2022.10.4 to version 2022.10.9"
+sed -i -e "s/version=2.8.1/version=2.9.0/g" python/python-pytest-checkdocs/spkgbuild
+changelog "python/python-pytest-checkdocs/spkgbuild" "Upgraded from version 2.8.1 to version 2.9.0"
+sed -i -e "s/version=2.0.3/version=2.1.0/g" python/python-requirementslib/spkgbuild
+changelog "python/python-requirementslib/spkgbuild" "Upgraded from version 2.0.3 to version 2.1.0"
+sed -i -e "s/version=3.9.0/version=3.9.1/g" python/python-zipp/spkgbuild
+changelog "python/python-zipp/spkgbuild" "Upgraded from version 3.9.0 to version 3.9.1"
+sed -i -e "s/version=3.11.20/version=3.11.21/g" server/blacklist-hosts/spkgbuild
+changelog "server/blacklist-hosts/spkgbuild" "Upgraded from version 3.11.20 to version 3.11.21"
