2943e837e68b5fbea6dfa57d1ae0813a980ae6c2
Voncloft <voncloft@gmail.com>
2022-10-14 05:49:04 -0400

    Minor Updates

---
+++ REPOS/test_updates-10-13-22.sh
@@ -0,0 +1,28 @@
+sed -i -e "s/version=2.3.2/version=2.3.3/g" core/blueman/spkgbuild
+changelog "core/blueman/spkgbuild" "Upgraded from version 2.3.2 to version 2.3.3"
+sed -i -e "s/version=1.13.1/version=1.14.0/g" core/conky/spkgbuild
+changelog "core/conky/spkgbuild" "Upgraded from version 1.13.1 to version 1.14.0"
+sed -i -e "s/version=5.19.14/version=5.19.15/g" core/linux-desktop/spkgbuild
+changelog "core/linux-desktop/spkgbuild" "Upgraded from version 5.19.14 to version 5.19.15"
+sed -i -e "s/version=5.19.14/version=5.19.15/g" core/linux-router/spkgbuild
+changelog "core/linux-router/spkgbuild" "Upgraded from version 5.19.14 to version 5.19.15"
+sed -i -e "s/version=515.76/version=520.56.06/g" nonfree/nvidia/spkgbuild
+changelog "nonfree/nvidia/spkgbuild" "Upgraded from version 515.76 to version 520.56.06"
+sed -i -e "s/version=515.76/version=520.56.06/g" nonfree/nvidia-32/spkgbuild
+changelog "nonfree/nvidia-32/spkgbuild" "Upgraded from version 515.76 to version 520.56.06"
+sed -i -e "s/version=515.76/version=520.56.06/g" nonfree/nvidia-modules/spkgbuild
+changelog "nonfree/nvidia-modules/spkgbuild" "Upgraded from version 515.76 to version 520.56.06"
+sed -i -e "s/version=0.18.4/version=0.18.5/g" python/python-django-modeltranslation/spkgbuild
+changelog "python/python-django-modeltranslation/spkgbuild" "Upgraded from version 0.18.4 to version 0.18.5"
+sed -i -e "s/version=11.34.0/version=11.35.0/g" python/python-dropbox/spkgbuild
+changelog "python/python-dropbox/spkgbuild" "Upgraded from version 11.34.0 to version 11.35.0"
+sed -i -e "s/version=15.0.0/version=15.1.0/g" python/python-faker/spkgbuild
+changelog "python/python-faker/spkgbuild" "Upgraded from version 15.0.0 to version 15.1.0"
+sed -i -e "s/version=6.1.1/version=6.2.0/g" python/python-flask-dance/spkgbuild
+changelog "python/python-flask-dance/spkgbuild" "Upgraded from version 6.1.1 to version 6.2.0"
+sed -i -e "s/version=2.8.7/version=3.0/g" python/python-networkx/spkgbuild
+changelog "python/python-networkx/spkgbuild" "Upgraded from version 2.8.7 to version 3.0"
+sed -i -e "s/version=0.4.1/version=0.4.2/g" python/python-plette/spkgbuild
+changelog "python/python-plette/spkgbuild" "Upgraded from version 0.4.1 to version 0.4.2"
+sed -i -e "s/version=3.15.3/version=3.15.4/g" python/python-trimesh/spkgbuild
+changelog "python/python-trimesh/spkgbuild" "Upgraded from version 3.15.3 to version 3.15.4"
