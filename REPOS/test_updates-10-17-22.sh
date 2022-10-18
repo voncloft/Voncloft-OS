c275f9f06de6df0353836fbd5cfedc6062cd531e
Voncloft <voncloft@gmail.com>
2022-10-18 08:45:53 -0400

    Minor Updates

---
+++ REPOS/test_updates-10-17-22.sh
@@ -0,0 +1,56 @@
+sed -i -e "s/version=2.3.3/version=2.3.4/g" core/blueman/spkgbuild
+changelog "core/blueman/spkgbuild" "Upgraded from version 2.3.3 to version 2.3.4"
+sed -i -e "s/version=1.14.0/version=1.15.0/g" core/conky/spkgbuild
+changelog "core/conky/spkgbuild" "Upgraded from version 1.14.0 to version 1.15.0"
+sed -i -e "s/version=5.19.15/version=5.19.16/g" core/linux-desktop/spkgbuild
+changelog "core/linux-desktop/spkgbuild" "Upgraded from version 5.19.15 to version 5.19.16"
+sed -i -e "s/version=5.19.15/version=5.19.16/g" core/linux-router/spkgbuild
+changelog "core/linux-router/spkgbuild" "Upgraded from version 5.19.15 to version 5.19.16"
+sed -i -e "s/version=2.10.2/version=2.11.0/g" core/man-db/spkgbuild
+changelog "core/man-db/spkgbuild" "Upgraded from version 2.10.2 to version 2.11.0"
+sed -i -e "s/version=5.13/version=6.00/g" core/man-pages/spkgbuild
+changelog "core/man-pages/spkgbuild" "Upgraded from version 5.13 to version 6.00"
+sed -i -e "s/version=1.2.12/version=1.2.13/g" core/zlib/spkgbuild
+changelog "core/zlib/spkgbuild" "Upgraded from version 1.2.12 to version 1.2.13"
+sed -i -e "s/version=3.3.34/version=3.3.36/g" extra/flexget/spkgbuild
+changelog "extra/flexget/spkgbuild" "Upgraded from version 3.3.34 to version 3.3.36"
+sed -i -e "s/version=1.1.5rc1/version=1.1.6rc4/g" extra/solaar/spkgbuild
+changelog "extra/solaar/spkgbuild" "Upgraded from version 1.1.5rc1 to version 1.1.6rc4"
+sed -i -e "s/version=2.4.0/version=2.5.0/g" libs/libwacom/spkgbuild
+changelog "libs/libwacom/spkgbuild" "Upgraded from version 2.4.0 to version 2.5.0"
+sed -i -e "s/version=1.24.90/version=1.24.91/g" python/python-boto3/spkgbuild
+changelog "python/python-boto3/spkgbuild" "Upgraded from version 1.24.90 to version 1.24.91"
+sed -i -e "s/version=1.27.90/version=1.27.91/g" python/python-botocore/spkgbuild
+changelog "python/python-botocore/spkgbuild" "Upgraded from version 1.27.90 to version 1.27.91"
+sed -i -e "s/version=4.17.0/version=4.17.1/g" python/python-braintree/spkgbuild
+changelog "python/python-braintree/spkgbuild" "Upgraded from version 4.17.0 to version 4.17.1"
+sed -i -e "s/version=2.0.6/version=2.0.7/g" python/python-cymem/spkgbuild
+changelog "python/python-cymem/spkgbuild" "Upgraded from version 2.0.6 to version 2.0.7"
+sed -i -e "s/version=2022.9.2/version=2022.10.0/g" python/python-distributed/spkgbuild
+changelog "python/python-distributed/spkgbuild" "Upgraded from version 2022.9.2 to version 2022.10.0"
+sed -i -e "s/version=0.85.0/version=0.85.1/g" python/python-fastapi/spkgbuild
+changelog "python/python-fastapi/spkgbuild" "Upgraded from version 0.85.0 to version 0.85.1"
+sed -i -e "s/version=3.0.1/version=3.0.2/g" python/python-flask-sqlalchemy/spkgbuild
+changelog "python/python-flask-sqlalchemy/spkgbuild" "Upgraded from version 3.0.1 to version 3.0.2"
+sed -i -e "s/version=22.08.0/version=22.10.1/g" python/python-gevent/spkgbuild
+changelog "python/python-gevent/spkgbuild" "Upgraded from version 22.08.0 to version 22.10.1"
+sed -i -e "s/version=21.3.0/version=22.10.0/g" python/python-incremental/spkgbuild
+changelog "python/python-incremental/spkgbuild" "Upgraded from version 21.3.0 to version 22.10.0"
+sed -i -e "s/version=24.2.0/version=25.2.0/g" python/python-kubernetes/spkgbuild
+changelog "python/python-kubernetes/spkgbuild" "Upgraded from version 24.2.0 to version 25.2.0"
+sed -i -e "s/version=8.12.56/version=8.12.57/g" python/python-phonenumbers/spkgbuild
+changelog "python/python-phonenumbers/spkgbuild" "Upgraded from version 8.12.56 to version 8.12.57"
+sed -i -e "s/version=22.2.2/version=22.3/g" python/python-pip/spkgbuild
+changelog "python/python-pip/spkgbuild" "Upgraded from version 22.2.2 to version 22.3"
+sed -i -e "s/version=1.13.4/version=1.14.0/g" python/python-pytest-twisted/spkgbuild
+changelog "python/python-pytest-twisted/spkgbuild" "Upgraded from version 1.13.4 to version 1.14.0"
+sed -i -e "s/version=65.4.1/version=65.5.0/g" python/python-setuptools/spkgbuild
+changelog "python/python-setuptools/spkgbuild" "Upgraded from version 65.4.1 to version 65.5.0"
+sed -i -e "s/version=3.11.21/version=3.11.23/g" server/blacklist-hosts/spkgbuild
+changelog "server/blacklist-hosts/spkgbuild" "Upgraded from version 3.11.21 to version 3.11.23"
+sed -i -e "s/version=2022.10.3/version=2022.10.4/g" server/homeassistant/spkgbuild
+changelog "server/homeassistant/spkgbuild" "Upgraded from version 2022.10.3 to version 2022.10.4"
+sed -i -e "s/version=4.17.9/version=4.17.10/g" xfce/thunar/spkgbuild
+changelog "xfce/thunar/spkgbuild" "Upgraded from version 4.17.9 to version 4.17.10"
+sed -i -e "s/version=1.0.6/version=1.0.7/g" xorg/smproxy/spkgbuild
+changelog "xorg/smproxy/spkgbuild" "Upgraded from version 1.0.6 to version 1.0.7"
