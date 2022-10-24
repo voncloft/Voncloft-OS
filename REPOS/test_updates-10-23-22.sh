f90d043aa95b83ca2963eaaa62e1e59cccd08d74
Voncloft <voncloft@gmail.com>
2022-10-24 05:24:28 -0400

    Minor Updates

---
+++ REPOS/test_updates-10-23-22.sh
@@ -0,0 +1,12 @@
+sed -i -e "s/version=4.7/version=4.7.1/g" compilers/ccache/spkgbuild
+changelog "compilers/ccache/spkgbuild" "Upgraded from version 4.7 to version 4.7.1"
+sed -i -e "s/version=1.4.1/version=1.4.2/g" core/flac/spkgbuild
+changelog "core/flac/spkgbuild" "Upgraded from version 1.4.1 to version 1.4.2"
+sed -i -e "s/version=4.0.41/version=4.0.42/g" core/mtools/spkgbuild
+changelog "core/mtools/spkgbuild" "Upgraded from version 4.0.41 to version 4.0.42"
+sed -i -e "s/version=5.1.2/version=5.2.0/g" python/python-auditwheel/spkgbuild
+changelog "python/python-auditwheel/spkgbuild" "Upgraded from version 5.1.2 to version 5.2.0"
+sed -i -e "s/version=1.24.95/version=1.24.96/g" python/python-boto3/spkgbuild
+changelog "python/python-boto3/spkgbuild" "Upgraded from version 1.24.95 to version 1.24.96"
+sed -i -e "s/version=1.27.95/version=1.27.96/g" python/python-botocore/spkgbuild
+changelog "python/python-botocore/spkgbuild" "Upgraded from version 1.27.95 to version 1.27.96"
---
+++ logs/old-test-updates/2022/Oct/applied_updates-10-24-22.sh
@@ -0,0 +1,20 @@
+sed -i -e "s/version=4.7/version=4.7.1/g" compilers/ccache/spkgbuild
+changelog "compilers/ccache/spkgbuild" "Upgraded from version 4.7 to version 4.7.1"
+sed -i -e "s/version=1.4.1/version=1.4.2/g" core/flac/spkgbuild
+changelog "core/flac/spkgbuild" "Upgraded from version 1.4.1 to version 1.4.2"
+sed -i -e "s/version=4.0.41/version=4.0.42/g" core/mtools/spkgbuild
+changelog "core/mtools/spkgbuild" "Upgraded from version 4.0.41 to version 4.0.42"
+sed -i -e "s/version=3.3.39/version=3.3.40/g" extra/flexget/spkgbuild
+changelog "extra/flexget/spkgbuild" "Upgraded from version 3.3.39 to version 3.3.40"
+sed -i -e "s/version=2.3.3/version=2.3.4/g" libs/libidn2/spkgbuild
+changelog "libs/libidn2/spkgbuild" "Upgraded from version 2.3.3 to version 2.3.4"
+sed -i -e "s/version=2.0.7/version=2.0.8/g" libs/libtorrent/spkgbuild
+changelog "libs/libtorrent/spkgbuild" "Upgraded from version 2.0.7 to version 2.0.8"
+sed -i -e "s/version=2.0.7/version=2.0.8/g" libs/libtorrent-rasterbar/spkgbuild
+changelog "libs/libtorrent-rasterbar/spkgbuild" "Upgraded from version 2.0.7 to version 2.0.8"
+sed -i -e "s/version=5.1.2/version=5.2.0/g" python/python-auditwheel/spkgbuild
+changelog "python/python-auditwheel/spkgbuild" "Upgraded from version 5.1.2 to version 5.2.0"
+sed -i -e "s/version=1.24.95/version=1.24.96/g" python/python-boto3/spkgbuild
+changelog "python/python-boto3/spkgbuild" "Upgraded from version 1.24.95 to version 1.24.96"
+sed -i -e "s/version=1.27.95/version=1.27.96/g" python/python-botocore/spkgbuild
+changelog "python/python-botocore/spkgbuild" "Upgraded from version 1.27.95 to version 1.27.96"
