2496a908dcf226375e14a7c5201d2d0c4da2663b
Voncloft <voncloft@gmail.com>
2022-11-07 08:43:01 -0500

    Minor Updates

---
+++ logs/old-test-updates/2022/Nov/applied_updates-11-07-22.sh
@@ -0,0 +1,18 @@
+sed -i -e "s/version=4.7.2/version=4.7.3/g" compilers/ccache/spkgbuild
+changelog "compilers/ccache/spkgbuild" "Upgraded from version 4.7.2 to version 4.7.3"
+sed -i -e "s/version=3.5.3/version=3.5.4/g" extra/flexget/spkgbuild
+changelog "extra/flexget/spkgbuild" "Upgraded from version 3.5.3 to version 3.5.4"
+sed -i -e "s/version=6.0.0/version=6.0.1/g" python/python-apispec/spkgbuild
+changelog "python/python-apispec/spkgbuild" "Upgraded from version 6.0.0 to version 6.0.1"
+sed -i -e "s/version=0.15.10/version=0.16.0/g" python/python-libtmux/spkgbuild
+changelog "python/python-libtmux/spkgbuild" "Upgraded from version 0.15.10 to version 0.16.0"
+sed -i -e "s/version=8.12.57/version=8.13.0/g" python/python-phonenumbers/spkgbuild
+changelog "python/python-phonenumbers/spkgbuild" "Upgraded from version 8.12.57 to version 8.13.0"
+sed -i -e "s/version=1.10.1/version=1.11.0/g" python/python-pygit2/spkgbuild
+changelog "python/python-pygit2/spkgbuild" "Upgraded from version 1.10.1 to version 1.11.0"
+sed -i -e "s/version=4.12.0/version=4.12.1/g" python/python-tweepy/spkgbuild
+changelog "python/python-tweepy/spkgbuild" "Upgraded from version 4.12.0 to version 4.12.1"
+sed -i -e "s/version=0.6.1/version=0.7.0/g" python/python-typer/spkgbuild
+changelog "python/python-typer/spkgbuild" "Upgraded from version 0.6.1 to version 0.7.0"
+sed -i -e "s/version=5.0.1/version=5.1.0/g" python/python-zope-i18nmessageid/spkgbuild
+changelog "python/python-zope-i18nmessageid/spkgbuild" "Upgraded from version 5.0.1 to version 5.1.0"
