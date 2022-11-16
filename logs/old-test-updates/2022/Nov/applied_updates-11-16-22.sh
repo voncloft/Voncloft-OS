07a02f4b453394c7964b859053cb6b6b8b2e566e
Voncloft <voncloft@gmail.com>
2022-11-16 17:56:51 -0500

    Minor Updates

---
+++ logs/old-test-updates/2022/Nov/applied_updates-11-16-22.sh
@@ -0,0 +1,66 @@
+sed -i -e "s/version=5.4.7/version=5.6.0/g" cinnamon/cinnamon-control-center/spkgbuild
+changelog "cinnamon/cinnamon-control-center/spkgbuild" "Upgraded from version 5.4.7 to version 5.6.0"
+sed -i -e "s/version=5.4.4/version=5.6.0/g" cinnamon/cinnamon-screensaver/spkgbuild
+changelog "cinnamon/cinnamon-screensaver/spkgbuild" "Upgraded from version 5.4.4 to version 5.6.0"
+sed -i -e "s/version=5.4.0/version=5.6.0/g" cinnamon/cinnamon-session/spkgbuild
+changelog "cinnamon/cinnamon-session/spkgbuild" "Upgraded from version 5.4.0 to version 5.6.0"
+sed -i -e "s/version=5.4.5/version=5.6.0/g" cinnamon/cinnamon-settings-daemon/spkgbuild
+changelog "cinnamon/cinnamon-settings-daemon/spkgbuild" "Upgraded from version 5.4.5 to version 5.6.0"
+sed -i -e "s/version=5.4.2/version=5.6.0/g" cinnamon/cinnamon-translations/spkgbuild
+changelog "cinnamon/cinnamon-translations/spkgbuild" "Upgraded from version 5.4.2 to version 5.6.0"
+sed -i -e "s/version=15.0.4/version=15.0.5/g" compilers/clang/spkgbuild
+changelog "compilers/clang/spkgbuild" "Upgraded from version 15.0.4 to version 15.0.5"
+sed -i -e "s/version=15.0.4/version=15.0.5/g" compilers/lld/spkgbuild
+changelog "compilers/lld/spkgbuild" "Upgraded from version 15.0.4 to version 15.0.5"
+sed -i -e "s/version=15.0.4/version=15.0.5/g" compilers/llvm/spkgbuild
+changelog "compilers/llvm/spkgbuild" "Upgraded from version 15.0.4 to version 15.0.5"
+sed -i -e "s/version=15.0.4/version=15.0.5/g" multilib/llvm-32/spkgbuild
+changelog "multilib/llvm-32/spkgbuild" "Upgraded from version 15.0.4 to version 15.0.5"
+sed -i -e "s/version=3.11/version=3.11.0/g" libs/blas/spkgbuild
+changelog "libs/blas/spkgbuild" "Upgraded from version 3.11 to version 3.11.0"
+sed -i -e "s/version=3.11/version=3.11.0/g" libs/cblas/spkgbuild
+changelog "libs/cblas/spkgbuild" "Upgraded from version 3.11 to version 3.11.0"
+sed -i -e "s/version=3.11/version=3.11.0/g" libs/lapack/spkgbuild
+changelog "libs/lapack/spkgbuild" "Upgraded from version 3.11 to version 3.11.0"
+sed -i -e "s/version=2.4.0/version=2.4.1/g" libs/xapp/spkgbuild
+changelog "libs/xapp/spkgbuild" "Upgraded from version 2.4.0 to version 2.4.1"
+sed -i -e "s/version=9.19.6/version=9.19.7/g" networking/bind/spkgbuild
+changelog "networking/bind/spkgbuild" "Upgraded from version 9.19.6 to version 9.19.7"
+sed -i -e "s/version=108.0b1/version=108.0b2/g" networking/firefox/spkgbuild
+changelog "networking/firefox/spkgbuild" "Upgraded from version 108.0b1 to version 108.0b2"
+sed -i -e "s/version=107.0b4/version=108.0b1/g" networking/thunderbird/spkgbuild
+changelog "networking/thunderbird/spkgbuild" "Upgraded from version 107.0b4 to version 108.0b1"
+sed -i -e "s/version=3.1.9/version=4.0.1/g" python/python-bitstring/spkgbuild
+changelog "python/python-bitstring/spkgbuild" "Upgraded from version 3.1.9 to version 4.0.1"
+sed -i -e "s/version=1.26.10/version=1.26.11/g" python/python-boto3/spkgbuild
+changelog "python/python-boto3/spkgbuild" "Upgraded from version 1.26.10 to version 1.26.11"
+sed -i -e "s/version=1.29.10/version=1.29.11/g" python/python-botocore/spkgbuild
+changelog "python/python-botocore/spkgbuild" "Upgraded from version 1.29.10 to version 1.29.11"
+sed -i -e "s/version=4.11.1/version=4.11.2/g" python/python-btrees/spkgbuild
+changelog "python/python-btrees/spkgbuild" "Upgraded from version 4.11.1 to version 4.11.2"
+sed -i -e "s/version=5.0.0/version=5.1.0/g" python/python-designateclient/spkgbuild
+changelog "python/python-designateclient/spkgbuild" "Upgraded from version 5.0.0 to version 5.1.0"
+sed -i -e "s/version=4.1.0/version=4.2.0/g" python/python-glanceclient/spkgbuild
+changelog "python/python-glanceclient/spkgbuild" "Upgraded from version 4.1.0 to version 4.2.0"
+sed -i -e "s/version=2.65.0/version=2.67.0/g" python/python-google-api-python-client/spkgbuild
+changelog "python/python-google-api-python-client/spkgbuild" "Upgraded from version 2.65.0 to version 2.67.0"
+sed -i -e "s/version=5.0.1/version=5.0.2/g" python/python-oslo-log/spkgbuild
+changelog "python/python-oslo-log/spkgbuild" "Upgraded from version 5.0.1 to version 5.0.2"
+sed -i -e "s/version=4.9.2/version=4.9.3/g" python/python-persistent/spkgbuild
+changelog "python/python-persistent/spkgbuild" "Upgraded from version 4.9.2 to version 4.9.3"
+sed -i -e "s/version=4.0.34/version=4.0.35/g" python/python-pyodbc/spkgbuild
+changelog "python/python-pyodbc/spkgbuild" "Upgraded from version 4.0.34 to version 4.0.35"
+sed -i -e "s/version=0.8.1/version=0.9.0/g" python/python-resolvelib/spkgbuild
+changelog "python/python-resolvelib/spkgbuild" "Upgraded from version 0.8.1 to version 0.9.0"
+sed -i -e "s/version=3.16.2/version=3.16.3/g" python/python-trimesh/spkgbuild
+changelog "python/python-trimesh/spkgbuild" "Upgraded from version 3.16.2 to version 3.16.3"
+sed -i -e "s/version=4.6.0/version=4.6.1/g" python/python-zope-proxy/spkgbuild
+changelog "python/python-zope-proxy/spkgbuild" "Upgraded from version 4.6.0 to version 4.6.1"
+sed -i -e "s/version=5.5/version=5.6/g" python/python-zope-security/spkgbuild
+changelog "python/python-zope-security/spkgbuild" "Upgraded from version 5.5 to version 5.6"
+sed -i -e "s/version=2022.11.2/version=2022.11.3/g" server/homeassistant/spkgbuild
+changelog "server/homeassistant/spkgbuild" "Upgraded from version 2022.11.2 to version 2022.11.3"
+sed -i -e "s/version=1.8.2/version=1.8.3/g" xfce/xfce4-places-plugin/spkgbuild
+changelog "xfce/xfce4-places-plugin/spkgbuild" "Upgraded from version 1.8.2 to version 1.8.3"
+sed -i -e "s/version=1.9.11/version=1.10.0/g" xfce/xfce4-screenshooter/spkgbuild
+changelog "xfce/xfce4-screenshooter/spkgbuild" "Upgraded from version 1.9.11 to version 1.10.0"
