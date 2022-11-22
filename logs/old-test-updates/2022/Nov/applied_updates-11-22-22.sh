34f06e50640f1ffe62041bd5571b652834611143
Voncloft <voncloft@gmail.com>
2022-11-22 07:02:36 -0500

    Minor Updates

---
+++ logs/old-test-updates/2022/Nov/applied_updates-11-22-22.sh
@@ -0,0 +1,38 @@
+sed -i -e "s/version=5.6.0/version=5.6.1/g" cinnamon/cinnamon-screensaver/spkgbuild
+changelog "cinnamon/cinnamon-screensaver/spkgbuild" "Upgraded from version 5.6.0 to version 5.6.1"
+sed -i -e "s/version=5.6.0/version=5.6.1/g" cinnamon/cjs/spkgbuild
+changelog "cinnamon/cjs/spkgbuild" "Upgraded from version 5.6.0 to version 5.6.1"
+sed -i -e "s/version=5.4.3/version=5.6.0/g" cinnamon/nemo/spkgbuild
+changelog "cinnamon/nemo/spkgbuild" "Upgraded from version 5.4.3 to version 5.6.0"
+sed -i -e "s/version=4.7.3/version=4.7.4/g" compilers/ccache/spkgbuild
+changelog "compilers/ccache/spkgbuild" "Upgraded from version 4.7.3 to version 4.7.4"
+sed -i -e "s/version=1.29/version=1.30/g" extra/wayland-protocols/spkgbuild
+changelog "extra/wayland-protocols/spkgbuild" "Upgraded from version 1.29 to version 1.30"
+sed -i -e "s/version=1.6.38/version=1.6.39/g" libs/libpng/spkgbuild
+changelog "libs/libpng/spkgbuild" "Upgraded from version 1.6.38 to version 1.6.39"
+sed -i -e "s/version=0.52.21/version=0.52.22/g" libs/newt/spkgbuild
+changelog "libs/newt/spkgbuild" "Upgraded from version 0.52.21 to version 0.52.22"
+sed -i -e "s/version=108.0b3/version=108.0b4/g" networking/firefox/spkgbuild
+changelog "networking/firefox/spkgbuild" "Upgraded from version 108.0b3 to version 108.0b4"
+sed -i -e "s/version=0.3.4/version=0.3.5/g" python/python-flake8-black/spkgbuild
+changelog "python/python-flake8-black/spkgbuild" "Upgraded from version 0.3.4 to version 0.3.5"
+sed -i -e "s/version=5.3.1/version=5.3.2/g" python/python-flask-socketio/spkgbuild
+changelog "python/python-flask-socketio/spkgbuild" "Upgraded from version 5.3.1 to version 5.3.2"
+sed -i -e "s/version=6.57.1/version=6.58.0/g" python/python-hypothesis/spkgbuild
+changelog "python/python-hypothesis/spkgbuild" "Upgraded from version 6.57.1 to version 6.58.0"
+sed -i -e "s/version=6.17.1/version=6.18.0/g" python/python-ipykernel/spkgbuild
+changelog "python/python-ipykernel/spkgbuild" "Upgraded from version 6.17.1 to version 6.18.0"
+sed -i -e "s/version=2022.8.7/version=2022.11.21/g" python/python-nox/spkgbuild
+changelog "python/python-nox/spkgbuild" "Upgraded from version 2022.8.7 to version 2022.11.21"
+sed -i -e "s/version=3.8.1/version=3.8.2/g" python/python-orjson/spkgbuild
+changelog "python/python-orjson/spkgbuild" "Upgraded from version 3.8.1 to version 3.8.2"
+sed -i -e "s/version=1.6.4/version=1.6.5/g" python/python-texttable/spkgbuild
+changelog "python/python-texttable/spkgbuild" "Upgraded from version 1.6.4 to version 1.6.5"
+sed -i -e "s/version=5.6/version=5.7/g" python/python-zope-security/spkgbuild
+changelog "python/python-zope-security/spkgbuild" "Upgraded from version 5.6 to version 5.7"
+sed -i -e "s/version=1.32.0/version=1.32.1/g" ruby-gems/ruby-tins/spkgbuild
+changelog "ruby-gems/ruby-tins/spkgbuild" "Upgraded from version 1.32.0 to version 1.32.1"
+sed -i -e "s/version=3.11.30/version=3.11.32/g" server/blacklist-hosts/spkgbuild
+changelog "server/blacklist-hosts/spkgbuild" "Upgraded from version 3.11.30 to version 3.11.32"
+sed -i -e "s/version=2022.11.3/version=2022.11.4/g" server/homeassistant/spkgbuild
+changelog "server/homeassistant/spkgbuild" "Upgraded from version 2022.11.3 to version 2022.11.4"
