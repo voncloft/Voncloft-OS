b4a9e48bc7f3feb2347a39dc19ffffca5cd4c053
Voncloft <voncloft@gmail.com>
2022-11-23 04:33:39 -0500

    Minor Updates

---
+++ logs/old-test-updates/2022/Nov/applied_updates-11-23-22.sh
@@ -0,0 +1,34 @@
+sed -i -e "s/version=2.3.4/version=2.3.5/g" core/blueman/spkgbuild
+changelog "core/blueman/spkgbuild" "Upgraded from version 2.3.4 to version 2.3.5"
+sed -i -e "s/version=5.2.0/version=5.2.1/g" core/gawk/spkgbuild
+changelog "core/gawk/spkgbuild" "Upgraded from version 5.2.0 to version 5.2.1"
+sed -i -e "s/version=9.1/version=10.1/g" core/picom/spkgbuild
+changelog "core/picom/spkgbuild" "Upgraded from version 9.1 to version 10.1"
+sed -i -e "s/version=12.0/version=12.1/g" core/yad/spkgbuild
+changelog "core/yad/spkgbuild" "Upgraded from version 12.0 to version 12.1"
+sed -i -e "s/version=1.5.0/version=1.6.0/g" multilib/libglvnd-32/spkgbuild
+changelog "multilib/libglvnd-32/spkgbuild" "Upgraded from version 1.5.0 to version 1.6.0"
+sed -i -e "s/version=1.5.0/version=1.6.0/g" libs/libglvnd/spkgbuild
+changelog "libs/libglvnd/spkgbuild" "Upgraded from version 1.5.0 to version 1.6.0"
+sed -i -e "s/version=2.24.2/version=2.26.0/g" libs/sdl2/spkgbuild
+changelog "libs/sdl2/spkgbuild" "Upgraded from version 2.24.2 to version 2.26.0"
+sed -i -e "s/version=1.26.13/version=1.26.15/g" python/python-boto3/spkgbuild
+changelog "python/python-boto3/spkgbuild" "Upgraded from version 1.26.13 to version 1.26.15"
+sed -i -e "s/version=1.29.13/version=1.29.15/g" python/python-botocore/spkgbuild
+changelog "python/python-botocore/spkgbuild" "Upgraded from version 1.29.13 to version 1.29.15"
+sed -i -e "s/version=0.71.1/version=0.72.0/g" python/python-cfn-lint/spkgbuild
+changelog "python/python-cfn-lint/spkgbuild" "Upgraded from version 0.71.1 to version 0.72.0"
+sed -i -e "s/version=1.0.0/version=2.0.0/g" python/python-cleo/spkgbuild
+changelog "python/python-cleo/spkgbuild" "Upgraded from version 1.0.0 to version 2.0.0"
+sed -i -e "s/version=8.5.1/version=8.5.2/g" python/python-elasticsearch/spkgbuild
+changelog "python/python-elasticsearch/spkgbuild" "Upgraded from version 8.5.1 to version 8.5.2"
+sed -i -e "s/version=15.3.2/version=15.3.3/g" python/python-faker/spkgbuild
+changelog "python/python-faker/spkgbuild" "Upgraded from version 15.3.2 to version 15.3.3"
+sed -i -e "s/version=1.24.0/version=1.25.0/g" python/python-numpy/spkgbuild
+changelog "python/python-numpy/spkgbuild" "Upgraded from version 1.24.0 to version 1.25.0"
+sed -i -e "s/version=1.3.2/version=1.4.0/g" python/python-poetry-core/spkgbuild
+changelog "python/python-poetry-core/spkgbuild" "Upgraded from version 1.3.2 to version 1.4.0"
+sed -i -e "s/version=1.4.0/version=1.5.0/g" python/python-rope/spkgbuild
+changelog "python/python-rope/spkgbuild" "Upgraded from version 1.4.0 to version 1.5.0"
+sed -i -e "s/version=1.6.5/version=1.6.6/g" python/python-texttable/spkgbuild
+changelog "python/python-texttable/spkgbuild" "Upgraded from version 1.6.5 to version 1.6.6"
