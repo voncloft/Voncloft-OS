34c7dda60a8ce1c3d45b4620b0cf2da32a23f32a
Voncloft <voncloft@gmail.com>
2022-11-25 05:04:27 -0500

    Minor Updates

---
+++ logs/old-test-updates/2022/Nov/applied_updates-11-25-22.sh
@@ -0,0 +1,64 @@
+sed -i -e "s/version=0.64.0/version=0.64.1/g" compilers/meson/spkgbuild
+changelog "compilers/meson/spkgbuild" "Upgraded from version 0.64.0 to version 0.64.1"
+sed -i -e "s/version=3.1.2/version=3.1.3/g" compilers/ruby/spkgbuild
+changelog "compilers/ruby/spkgbuild" "Upgraded from version 3.1.2 to version 3.1.3"
+sed -i -e "s/version=6.0.1/version=6.0.2/g" core/btrfs-progs/spkgbuild
+changelog "core/btrfs-progs/spkgbuild" "Upgraded from version 6.0.1 to version 6.0.2"
+sed -i -e "s/version=0.106.0/version=0.107.0/g" core/hugo/spkgbuild
+changelog "core/hugo/spkgbuild" "Upgraded from version 0.106.0 to version 0.107.0"
+sed -i -e "s/version=4.6/version=4.7/g" core/nnn/spkgbuild
+changelog "core/nnn/spkgbuild" "Upgraded from version 4.6 to version 4.7"
+sed -i -e "s/version=0.3.60/version=0.3.61/g" core/pipewire/spkgbuild
+changelog "core/pipewire/spkgbuild" "Upgraded from version 0.3.60 to version 0.3.61"
+sed -i -e "s/version=5.1.0/version=5.1.1/g" core/streamlink/spkgbuild
+changelog "core/streamlink/spkgbuild" "Upgraded from version 5.1.0 to version 5.1.1"
+sed -i -e "s/version=6.24.01/version=6.24.02/g" core/tcsh/spkgbuild
+changelog "core/tcsh/spkgbuild" "Upgraded from version 6.24.01 to version 6.24.02"
+sed -i -e "s/version=3.0.17/version=3.0.18/g" media/vlc/spkgbuild
+changelog "media/vlc/spkgbuild" "Upgraded from version 3.0.17 to version 3.0.18"
+sed -i -e "s/version=108.0b4/version=108.0b5/g" networking/firefox/spkgbuild
+changelog "networking/firefox/spkgbuild" "Upgraded from version 108.0b4 to version 108.0b5"
+sed -i -e "s/version=7.4.2/version=7.4.3/g" nonfree/libreoffice/spkgbuild
+changelog "nonfree/libreoffice/spkgbuild" "Upgraded from version 7.4.2 to version 7.4.3"
+sed -i -e "s/version=1.26.15/version=1.26.16/g" python/python-boto3/spkgbuild
+changelog "python/python-boto3/spkgbuild" "Upgraded from version 1.26.15 to version 1.26.16"
+sed -i -e "s/version=1.29.15/version=1.29.16/g" python/python-botocore/spkgbuild
+changelog "python/python-botocore/spkgbuild" "Upgraded from version 1.29.15 to version 1.29.16"
+sed -i -e "s/version=2.0.0/version=2.0.1/g" python/python-cleo/spkgbuild
+changelog "python/python-cleo/spkgbuild" "Upgraded from version 2.0.0 to version 2.0.1"
+sed -i -e "s/version=2.10.5/version=2.11.1/g" python/python-cloudflare/spkgbuild
+changelog "python/python-cloudflare/spkgbuild" "Upgraded from version 2.10.5 to version 2.11.1"
+sed -i -e "s/version=5.0.0/version=5.1.0/g" python/python-importlib-metadata/spkgbuild
+changelog "python/python-importlib-metadata/spkgbuild" "Upgraded from version 5.0.0 to version 5.1.0"
+sed -i -e "s/version=4.17.0/version=4.17.1/g" python/python-jsonschema/spkgbuild
+changelog "python/python-jsonschema/spkgbuild" "Upgraded from version 4.17.0 to version 4.17.1"
+sed -i -e "s/version=0.9.1/version=0.9.2/g" python/python-kajiki/spkgbuild
+changelog "python/python-kajiki/spkgbuild" "Upgraded from version 0.9.1 to version 0.9.2"
+sed -i -e "s/version=5.0.0/version=5.1.0/g" python/python-keystoneauth1/spkgbuild
+changelog "python/python-keystoneauth1/spkgbuild" "Upgraded from version 5.0.0 to version 5.1.0"
+sed -i -e "s/version=3.1.0/version=3.2.0/g" python/python-octaviaclient/spkgbuild
+changelog "python/python-octaviaclient/spkgbuild" "Upgraded from version 3.1.0 to version 3.2.0"
+sed -i -e "s/version=0.102.0/version=0.103.0/g" python/python-openstacksdk/spkgbuild
+changelog "python/python-openstacksdk/spkgbuild" "Upgraded from version 0.102.0 to version 0.103.0"
+sed -i -e "s/version=2.1.2/version=3.0.0/g" python/python-pendulum/spkgbuild
+changelog "python/python-pendulum/spkgbuild" "Upgraded from version 2.1.2 to version 3.0.0"
+sed -i -e "s/version=2022.11.11/version=2022.11.23/g" python/python-pipenv/spkgbuild
+changelog "python/python-pipenv/spkgbuild" "Upgraded from version 2022.11.11 to version 2022.11.23"
+sed -i -e "s/version=2.9.1/version=2.10.0/g" python/python-pycodestyle/spkgbuild
+changelog "python/python-pycodestyle/spkgbuild" "Upgraded from version 2.9.1 to version 2.10.0"
+sed -i -e "s/version=1.3.0/version=2.0.0/g" python/python-pytest-enabler/spkgbuild
+changelog "python/python-pytest-enabler/spkgbuild" "Upgraded from version 1.3.0 to version 2.0.0"
+sed -i -e "s/version=65.6.0/version=65.6.3/g" python/python-setuptools/spkgbuild
+changelog "python/python-setuptools/spkgbuild" "Upgraded from version 65.6.0 to version 65.6.3"
+sed -i -e "s/version=2.3/version=2.3.1/g" python/python-sphobjinv/spkgbuild
+changelog "python/python-sphobjinv/spkgbuild" "Upgraded from version 2.3 to version 2.3.1"
+sed -i -e "s/version=22.2.0/version=22.3.0/g" python/python-structlog/spkgbuild
+changelog "python/python-structlog/spkgbuild" "Upgraded from version 22.2.0 to version 22.3.0"
+sed -i -e "s/version=2.10/version=2.11.0/g" python/python-tensorflow-estimator/spkgbuild
+changelog "python/python-tensorflow-estimator/spkgbuild" "Upgraded from version 2.10 to version 2.11.0"
+sed -i -e "s/version=2.10.0/version=2.11.0/g" python/python-tensorflow-serving-api/spkgbuild
+changelog "python/python-tensorflow-serving-api/spkgbuild" "Upgraded from version 2.10.0 to version 2.11.0"
+sed -i -e "s/version=1.6.6/version=1.6.7/g" python/python-texttable/spkgbuild
+changelog "python/python-texttable/spkgbuild" "Upgraded from version 1.6.6 to version 1.6.7"
+sed -i -e "s/version=8.1.12/version=8.1.13/g" server/php/spkgbuild
+changelog "server/php/spkgbuild" "Upgraded from version 8.1.12 to version 8.1.13"
