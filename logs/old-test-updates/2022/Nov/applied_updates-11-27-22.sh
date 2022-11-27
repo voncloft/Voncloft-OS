6a4fe0ba513d4ffcd384c2621ee1f78f376336e0
Voncloft <voncloft@gmail.com>
2022-11-27 08:54:51 -0500

    Minor Updates

---
+++ logs/old-test-updates/2022/Nov/applied_updates-11-27-22.sh
@@ -0,0 +1,22 @@
+sed -i -e "s/version=5.6.1/version=5.6.3/g" cinnamon/cinnamon/spkgbuild
+changelog "cinnamon/cinnamon/spkgbuild" "Upgraded from version 5.6.1 to version 5.6.3"
+sed -i -e "s/version=22.11.80/version=22.11.90/g" kde-apps/kalendar/spkgbuild
+changelog "kde-apps/kalendar/spkgbuild" "Upgraded from version 22.11.80 to version 22.11.90"
+sed -i -e "s/version=108.0b5/version=108.0b6/g" networking/firefox/spkgbuild
+changelog "networking/firefox/spkgbuild" "Upgraded from version 108.0b5 to version 108.0b6"
+sed -i -e "s/version=0.16.1/version=0.16.2/g" python/python-httpcore/spkgbuild
+changelog "python/python-httpcore/spkgbuild" "Upgraded from version 0.16.1 to version 0.16.2"
+sed -i -e "s/version=6.58.0/version=6.58.1/g" python/python-hypothesis/spkgbuild
+changelog "python/python-hypothesis/spkgbuild" "Upgraded from version 6.58.0 to version 6.58.1"
+sed -i -e "s/version=2022.11.23/version=2022.11.25/g" python/python-pipenv/spkgbuild
+changelog "python/python-pipenv/spkgbuild" "Upgraded from version 2022.11.23 to version 2022.11.25"
+sed -i -e "s/version=3.15.0/version=3.16.0/g" python/python-pycryptodome/spkgbuild
+changelog "python/python-pycryptodome/spkgbuild" "Upgraded from version 3.15.0 to version 3.16.0"
+sed -i -e "s/version=0.10.1/version=0.10.2/g" python/python-pytest-mypy/spkgbuild
+changelog "python/python-pytest-mypy/spkgbuild" "Upgraded from version 0.10.1 to version 0.10.2"
+sed -i -e "s/version=1.5.0/version=1.5.1/g" python/python-rope/spkgbuild
+changelog "python/python-rope/spkgbuild" "Upgraded from version 1.5.0 to version 1.5.1"
+sed -i -e "s/version=3.10.0/version=3.11.0/g" python/python-zipp/spkgbuild
+changelog "python/python-zipp/spkgbuild" "Upgraded from version 3.10.0 to version 3.11.0"
+sed -i -e "s/version=3.11.32/version=3.11.33/g" server/blacklist-hosts/spkgbuild
+changelog "server/blacklist-hosts/spkgbuild" "Upgraded from version 3.11.32 to version 3.11.33"
