502a56b29454be14bb77fc4f6d0ce883801ad6e8
Voncloft <voncloft@gmail.com>
2022-11-28 07:59:35 -0500

    Minor Updates

---
+++ logs/old-test-updates/2022/Nov/applied_updates-11-28-22.sh
@@ -0,0 +1,8 @@
+sed -i -e "s/version=0.72.0/version=0.72.1/g" python/python-cfn-lint/spkgbuild
+changelog "python/python-cfn-lint/spkgbuild" "Upgraded from version 0.72.0 to version 0.72.1"
+sed -i -e "s/version=0.87.0/version=0.88.0/g" python/python-fastapi/spkgbuild
+changelog "python/python-fastapi/spkgbuild" "Upgraded from version 0.87.0 to version 0.88.0"
+sed -i -e "s/version=5.3.2/version=5.3.3/g" python/python-pynamodb/spkgbuild
+changelog "python/python-pynamodb/spkgbuild" "Upgraded from version 5.3.2 to version 5.3.3"
+sed -i -e "s/version=4.4.5/version=4.5.0/g" qt/qbittorrent/spkgbuild
+changelog "qt/qbittorrent/spkgbuild" "Upgraded from version 4.4.5 to version 4.5.0"
