f3c8d4e6e903b4448f304af6b77c88f5226163cc
Voncloft <voncloft@gmail.com>
2022-11-29 07:43:36 -0500

    Minor Updates

---
+++ logs/old-test-updates/2022/Nov/applied_updates-11-29-22.sh
@@ -0,0 +1,42 @@
+sed -i -e "s/version=2.39.1/version=2.39.2/g" core/webkitgtk/spkgbuild
+changelog "core/webkitgtk/spkgbuild" "Upgraded from version 2.39.1 to version 2.39.2"
+sed -i -e "s/version=3.5.5/version=3.5.6/g" extra/flexget/spkgbuild
+changelog "extra/flexget/spkgbuild" "Upgraded from version 3.5.5 to version 3.5.6"
+sed -i -e "s/version=108.0b6/version=108.0b7/g" networking/firefox/spkgbuild
+changelog "networking/firefox/spkgbuild" "Upgraded from version 108.0b6 to version 108.0b7"
+sed -i -e "s/version=525.53/version=525.60.11/g" nonfree/nvidia/spkgbuild
+changelog "nonfree/nvidia/spkgbuild" "Upgraded from version 525.53 to version 525.60.11"
+sed -i -e "s/version=525.53/version=525.60.11/g" nonfree/nvidia-32/spkgbuild
+changelog "nonfree/nvidia-32/spkgbuild" "Upgraded from version 525.53 to version 525.60.11"
+sed -i -e "s/version=525.53/version=525.60.11/g" nonfree/nvidia-modules/spkgbuild
+changelog "nonfree/nvidia-modules/spkgbuild" "Upgraded from version 525.53 to version 525.60.11"
+sed -i -e "s/version=3.39.4.0/version=3.40.0.0/g" python/python-apsw/spkgbuild
+changelog "python/python-apsw/spkgbuild" "Upgraded from version 3.39.4.0 to version 3.40.0.0"
+sed -i -e "s/version=1.26.16/version=1.26.17/g" python/python-boto3/spkgbuild
+changelog "python/python-boto3/spkgbuild" "Upgraded from version 1.26.16 to version 1.26.17"
+sed -i -e "s/version=1.29.16/version=1.29.17/g" python/python-botocore/spkgbuild
+changelog "python/python-botocore/spkgbuild" "Upgraded from version 1.29.16 to version 1.29.17"
+sed -i -e "s/version=1.22.0/version=1.23.0/g" python/python-cairo/spkgbuild
+changelog "python/python-cairo/spkgbuild" "Upgraded from version 1.22.0 to version 1.23.0"
+sed -i -e "s/version=3.11.0/version=3.12.0/g" python/python-gitlab/spkgbuild
+changelog "python/python-gitlab/spkgbuild" "Upgraded from version 3.11.0 to version 3.12.0"
+sed -i -e "s/version=6.18.0/version=6.18.1/g" python/python-ipykernel/spkgbuild
+changelog "python/python-ipykernel/spkgbuild" "Upgraded from version 6.18.0 to version 6.18.1"
+sed -i -e "s/version=4.17.1/version=4.17.2/g" python/python-jsonschema/spkgbuild
+changelog "python/python-jsonschema/spkgbuild" "Upgraded from version 4.17.1 to version 4.17.2"
+sed -i -e "s/version=5.0.0/version=5.1.0/g" python/python-jupyter_core/spkgbuild
+changelog "python/python-jupyter_core/spkgbuild" "Upgraded from version 5.0.0 to version 5.1.0"
+sed -i -e "s/version=0.22.2/version=0.23.0/g" python/python-meilisearch/spkgbuild
+changelog "python/python-meilisearch/spkgbuild" "Upgraded from version 0.22.2 to version 0.23.0"
+sed -i -e "s/version=4.26.0/version=4.27.0/g" python/python-mt-940/spkgbuild
+changelog "python/python-mt-940/spkgbuild" "Upgraded from version 4.26.0 to version 4.27.0"
+sed -i -e "s/version=8.13.0/version=8.13.1/g" python/python-phonenumbers/spkgbuild
+changelog "python/python-phonenumbers/spkgbuild" "Upgraded from version 8.13.0 to version 8.13.1"
+sed -i -e "s/version=1.1.3/version=1.2.0/g" python/python-scikit-learn/spkgbuild
+changelog "python/python-scikit-learn/spkgbuild" "Upgraded from version 1.1.3 to version 1.2.0"
+sed -i -e "s/version=20.16.7/version=20.17.0/g" python/python-virtualenv/spkgbuild
+changelog "python/python-virtualenv/spkgbuild" "Upgraded from version 20.16.7 to version 20.17.0"
+sed -i -e "s/version=2.0.1/version=2.1.0/g" python/python-w3lib/spkgbuild
+changelog "python/python-w3lib/spkgbuild" "Upgraded from version 2.0.1 to version 2.1.0"
+sed -i -e "s/version=3.11.33/version=3.11.34/g" server/blacklist-hosts/spkgbuild
+changelog "server/blacklist-hosts/spkgbuild" "Upgraded from version 3.11.33 to version 3.11.34"
