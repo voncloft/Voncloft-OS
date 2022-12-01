a7e175291fd31552958afe966e642cf2b72e878e
Voncloft <voncloft@gmail.com>
2022-12-01 03:27:03 -0500

    Minor Updates

---
+++ logs/old-test-updates/2022/Dec/applied_updates-12-01-22.sh
@@ -0,0 +1,42 @@
+sed -i -e "s/version=3.8.0/version=3.9.0/g" core/pciutils/spkgbuild
+changelog "core/pciutils/spkgbuild" "Upgraded from version 3.8.0 to version 3.9.0"
+sed -i -e "s/version=7.0/version=7.0.1/g" core/texinfo/spkgbuild
+changelog "core/texinfo/spkgbuild" "Upgraded from version 7.0 to version 7.0.1"
+sed -i -e "s/version=5.2.8/version=5.2.9/g" core/xz/spkgbuild
+changelog "core/xz/spkgbuild" "Upgraded from version 5.2.8 to version 5.2.9"
+sed -i -e "s/version=21.9/version=21.10/g" extra/protobuf/spkgbuild
+changelog "extra/protobuf/spkgbuild" "Upgraded from version 21.9 to version 21.10"
+sed -i -e "s/version=0.16.0/version=0.17.0/g" libs/libass/spkgbuild
+changelog "libs/libass/spkgbuild" "Upgraded from version 0.16.0 to version 0.17.0"
+sed -i -e "s/version=4.17.8/version=4.17.9/g" libs/libxfce4ui/spkgbuild
+changelog "libs/libxfce4ui/spkgbuild" "Upgraded from version 4.17.8 to version 4.17.9"
+sed -i -e "s/version=4.17.3/version=4.17.4/g" libs/libxfce4util/spkgbuild
+changelog "libs/libxfce4util/spkgbuild" "Upgraded from version 4.17.3 to version 4.17.4"
+sed -i -e "s/version=108.0b7/version=108.0b8/g" networking/firefox/spkgbuild
+changelog "networking/firefox/spkgbuild" "Upgraded from version 108.0b7 to version 108.0b8"
+sed -i -e "s/version=1.26.18/version=1.26.20/g" python/python-boto3/spkgbuild
+changelog "python/python-boto3/spkgbuild" "Upgraded from version 1.26.18 to version 1.26.20"
+sed -i -e "s/version=1.29.18/version=1.29.20/g" python/python-botocore/spkgbuild
+changelog "python/python-botocore/spkgbuild" "Upgraded from version 1.29.18 to version 1.29.20"
+sed -i -e "s/version=15.3.3/version=15.3.4/g" python/python-faker/spkgbuild
+changelog "python/python-faker/spkgbuild" "Upgraded from version 15.3.3 to version 15.3.4"
+sed -i -e "s/version=6.58.1/version=6.58.2/g" python/python-hypothesis/spkgbuild
+changelog "python/python-hypothesis/spkgbuild" "Upgraded from version 6.58.1 to version 6.58.2"
+sed -i -e "s/version=6.18.2/version=6.18.3/g" python/python-ipykernel/spkgbuild
+changelog "python/python-ipykernel/spkgbuild" "Upgraded from version 6.18.2 to version 6.18.3"
+sed -i -e "s/version=2022.11.25/version=2022.11.30/g" python/python-pipenv/spkgbuild
+changelog "python/python-pipenv/spkgbuild" "Upgraded from version 2022.11.25 to version 2022.11.30"
+sed -i -e "s/version=2.6.0/version=2.6.1/g" python/python-sepaxml/spkgbuild
+changelog "python/python-sepaxml/spkgbuild" "Upgraded from version 2.6.0 to version 2.6.1"
+sed -i -e "s/version=5.7/version=5.8/g" python/python-zope-security/spkgbuild
+changelog "python/python-zope-security/spkgbuild" "Upgraded from version 5.7 to version 5.8"
+sed -i -e "s/version=3.11.34/version=3.11.35/g" server/blacklist-hosts/spkgbuild
+changelog "server/blacklist-hosts/spkgbuild" "Upgraded from version 3.11.34 to version 3.11.35"
+sed -i -e "s/version=2022.11.4/version=2022.11.5/g" server/homeassistant/spkgbuild
+changelog "server/homeassistant/spkgbuild" "Upgraded from version 2022.11.4 to version 2022.11.5"
+sed -i -e "s/version=4.17.2/version=4.17.3/g" xfce/garcon/spkgbuild
+changelog "xfce/garcon/spkgbuild" "Upgraded from version 4.17.2 to version 4.17.3"
+sed -i -e "s/version=4.17.4/version=4.17.5/g" xfce/xfce4-panel/spkgbuild
+changelog "xfce/xfce4-panel/spkgbuild" "Upgraded from version 4.17.4 to version 4.17.5"
+sed -i -e "s/version=4.17.1/version=4.17.2/g" xfce/xfconf/spkgbuild
+changelog "xfce/xfconf/spkgbuild" "Upgraded from version 4.17.1 to version 4.17.2"
