2057e37610c9b34b46deb79c1f9830c5bea122ba
Voncloft <voncloft@gmail.com>
2022-12-02 06:11:53 -0500

    Minor Updates

---
+++ logs/old-test-updates/2022/Dec/applied_updates-12-02-22.sh
@@ -0,0 +1,36 @@
+sed -i -e "s/version=3.25.0-rc4/version=3.25.1/g" compilers/cmake/spkgbuild
+changelog "compilers/cmake/spkgbuild" "Upgraded from version 3.25.0-rc4 to version 3.25.1"
+sed -i -e "s/version=4.1.0/version=4.1.1/g" core/swig/spkgbuild
+changelog "core/swig/spkgbuild" "Upgraded from version 4.1.0 to version 4.1.1"
+sed -i -e "s/version=1.26.20/version=1.26.21/g" python/python-boto3/spkgbuild
+changelog "python/python-boto3/spkgbuild" "Upgraded from version 1.26.20 to version 1.26.21"
+sed -i -e "s/version=1.29.20/version=1.29.21/g" python/python-botocore/spkgbuild
+changelog "python/python-botocore/spkgbuild" "Upgraded from version 1.29.20 to version 1.29.21"
+sed -i -e "s/version=3.0.1/version=4.0.0/g" python/python-django-classy-tags/spkgbuild
+changelog "python/python-django-classy-tags/spkgbuild" "Upgraded from version 3.0.1 to version 4.0.0"
+sed -i -e "s/version=2.67.0/version=2.68.0/g" python/python-google-api-python-client/spkgbuild
+changelog "python/python-google-api-python-client/spkgbuild" "Upgraded from version 2.67.0 to version 2.68.0"
+sed -i -e "s/version=16.5.0/version=16.6.0/g" python/python-path/spkgbuild
+changelog "python/python-path/spkgbuild" "Upgraded from version 16.5.0 to version 16.6.0"
+sed -i -e "s/version=0.16.2/version=0.16.3/g" python/python-scikit-build/spkgbuild
+changelog "python/python-scikit-build/spkgbuild" "Upgraded from version 0.16.2 to version 0.16.3"
+sed -i -e "s/version=5.5.0/version=5.6.0/g" python/python-ujson/spkgbuild
+changelog "python/python-ujson/spkgbuild" "Upgraded from version 5.5.0 to version 5.6.0"
+sed -i -e "s/version=2022.11.5/version=2022.12.0/g" server/homeassistant/spkgbuild
+changelog "server/homeassistant/spkgbuild" "Upgraded from version 2022.11.5 to version 2022.12.0"
+sed -i -e "s/version=4.17.3/version=4.17.4/g" xfce/exo/spkgbuild
+changelog "xfce/exo/spkgbuild" "Upgraded from version 4.17.3 to version 4.17.4"
+sed -i -e "s/version=4.17.11/version=4.17.12/g" xfce/thunar/spkgbuild
+changelog "xfce/thunar/spkgbuild" "Upgraded from version 4.17.11 to version 4.17.12"
+sed -i -e "s/version=4.17.0/version=4.17.1/g" xfce/thunar-volman/spkgbuild
+changelog "xfce/thunar-volman/spkgbuild" "Upgraded from version 4.17.0 to version 4.17.1"
+sed -i -e "s/version=4.17.0/version=4.17.1/g" xfce/xfce4-power-manager/spkgbuild
+changelog "xfce/xfce4-power-manager/spkgbuild" "Upgraded from version 4.17.0 to version 4.17.1"
+sed -i -e "s/version=4.17.1/version=4.17.2/g" xfce/xfce4-session/spkgbuild
+changelog "xfce/xfce4-session/spkgbuild" "Upgraded from version 4.17.1 to version 4.17.2"
+sed -i -e "s/version=4.17.2/version=4.17.3/g" xfce/xfce4-settings/spkgbuild
+changelog "xfce/xfce4-settings/spkgbuild" "Upgraded from version 4.17.2 to version 4.17.3"
+sed -i -e "s/version=4.17.1/version=4.17.2/g" xfce/xfdesktop/spkgbuild
+changelog "xfce/xfdesktop/spkgbuild" "Upgraded from version 4.17.1 to version 4.17.2"
+sed -i -e "s/version=4.17.1/version=4.17.2/g" xfce/xfwm4/spkgbuild
+changelog "xfce/xfwm4/spkgbuild" "Upgraded from version 4.17.1 to version 4.17.2"
