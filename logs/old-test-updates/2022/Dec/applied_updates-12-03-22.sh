a1d7cec27f80f23fa8aa5e4e85519b7067c123dd
Voncloft <voncloft@gmail.com>
2022-12-03 11:42:35 -0500

    Minor Updates

---
+++ logs/old-test-updates/2022/Dec/applied_updates-12-03-22.sh
@@ -0,0 +1,30 @@
+sed -i -e "s/version=6.0.10/version=6.0.11/g" core/linux-api-headers/spkgbuild
+changelog "core/linux-api-headers/spkgbuild" "Upgraded from version 6.0.10 to version 6.0.11"
+sed -i -e "s/version=6.0.10/version=6.0.11/g" core/linux-desktop/spkgbuild
+changelog "core/linux-desktop/spkgbuild" "Upgraded from version 6.0.10 to version 6.0.11"
+sed -i -e "s/version=6.0.10/version=6.0.11/g" core/linux-router/spkgbuild
+changelog "core/linux-router/spkgbuild" "Upgraded from version 6.0.10 to version 6.0.11"
+sed -i -e "s/version=22.11.0/version=22.12.0/g" libs/poppler/spkgbuild
+changelog "libs/poppler/spkgbuild" "Upgraded from version 22.11.0 to version 22.12.0"
+sed -i -e "s/version=2.26.0/version=2.26.1/g" libs/sdl2/spkgbuild
+changelog "libs/sdl2/spkgbuild" "Upgraded from version 2.26.0 to version 2.26.1"
+sed -i -e "s/version=2.4.1/version=2.4.2/g" libs/xapp/spkgbuild
+changelog "libs/xapp/spkgbuild" "Upgraded from version 2.4.1 to version 2.4.2"
+sed -i -e "s/version=108.0b8/version=108.0b9/g" networking/firefox/spkgbuild
+changelog "networking/firefox/spkgbuild" "Upgraded from version 108.0b8 to version 108.0b9"
+sed -i -e "s/version=2022.11.1/version=2022.12.0/g" python/python-distributed/spkgbuild
+changelog "python/python-distributed/spkgbuild" "Upgraded from version 2022.11.1 to version 2022.12.0"
+sed -i -e "s/version=2.14.1/version=2.15.0/g" python/python-google-auth/spkgbuild
+changelog "python/python-google-auth/spkgbuild" "Upgraded from version 2.14.1 to version 2.15.0"
+sed -i -e "s/version=6.58.2/version=6.59.0/g" python/python-hypothesis/spkgbuild
+changelog "python/python-hypothesis/spkgbuild" "Upgraded from version 6.58.2 to version 6.59.0"
+sed -i -e "s/version=2.2.0/version=3.0.0/g" python/python-jsonpickle/spkgbuild
+changelog "python/python-jsonpickle/spkgbuild" "Upgraded from version 2.2.0 to version 3.0.0"
+sed -i -e "s/version=3.8.2/version=3.8.3/g" python/python-orjson/spkgbuild
+changelog "python/python-orjson/spkgbuild" "Upgraded from version 3.8.2 to version 3.8.3"
+sed -i -e "s/version=1.2.1/version=1.2.2/g" python/python-pycapnp/spkgbuild
+changelog "python/python-pycapnp/spkgbuild" "Upgraded from version 1.2.1 to version 1.2.2"
+sed -i -e "s/version=6.1.2/version=6.1.3/g" python/python-softlayer/spkgbuild
+changelog "python/python-softlayer/spkgbuild" "Upgraded from version 6.1.2 to version 6.1.3"
+sed -i -e "s/version=3.16.4/version=3.17.1/g" python/python-trimesh/spkgbuild
+changelog "python/python-trimesh/spkgbuild" "Upgraded from version 3.16.4 to version 3.17.1"
