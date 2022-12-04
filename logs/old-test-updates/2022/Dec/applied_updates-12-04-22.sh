3c3aaec694ecb94b8e4395cc53149165757a0d27
Voncloft <voncloft@gmail.com>
2022-12-04 10:35:41 -0500

    Minor Updates

---
+++ logs/old-test-updates/2022/Dec/applied_updates-12-04-22.sh
@@ -0,0 +1,24 @@
+sed -i -e "s/version=5.6.1/version=5.6.2/g" cinnamon/cinnamon-screensaver/spkgbuild
+changelog "cinnamon/cinnamon-screensaver/spkgbuild" "Upgraded from version 5.6.1 to version 5.6.2"
+sed -i -e "s/version=5.1.1/version=5.1.2/g" core/streamlink/spkgbuild
+changelog "core/streamlink/spkgbuild" "Upgraded from version 5.1.1 to version 5.1.2"
+sed -i -e "s/version=6.24.02/version=6.24.03/g" core/tcsh/spkgbuild
+changelog "core/tcsh/spkgbuild" "Upgraded from version 6.24.02 to version 6.24.03"
+sed -i -e "s/version=1.26.21/version=1.26.22/g" python/python-boto3/spkgbuild
+changelog "python/python-boto3/spkgbuild" "Upgraded from version 1.26.21 to version 1.26.22"
+sed -i -e "s/version=1.29.21/version=1.29.22/g" python/python-botocore/spkgbuild
+changelog "python/python-botocore/spkgbuild" "Upgraded from version 1.29.21 to version 1.29.22"
+sed -i -e "s/version=2.2.0/version=2.2.2/g" python/python-covdefaults/spkgbuild
+changelog "python/python-covdefaults/spkgbuild" "Upgraded from version 2.2.0 to version 2.2.2"
+sed -i -e "s/version=6.6.0/version=7.0.0/g" python/python-coverage/spkgbuild
+changelog "python/python-coverage/spkgbuild" "Upgraded from version 6.6.0 to version 7.0.0"
+sed -i -e "s/version=6.0.2/version=6.0.3/g" python/python-multidict/spkgbuild
+changelog "python/python-multidict/spkgbuild" "Upgraded from version 6.0.2 to version 6.0.3"
+sed -i -e "s/version=3.0.2/version=3.1.0/g" python/python-pytest-xdist/spkgbuild
+changelog "python/python-pytest-xdist/spkgbuild" "Upgraded from version 3.0.2 to version 3.1.0"
+sed -i -e "s/version=1.7.0/version=2.1.0/g" python/python-sqlitedict/spkgbuild
+changelog "python/python-sqlitedict/spkgbuild" "Upgraded from version 1.7.0 to version 2.1.0"
+sed -i -e "s/version=1.8.1/version=1.8.2/g" python/python-yarl/spkgbuild
+changelog "python/python-yarl/spkgbuild" "Upgraded from version 1.8.1 to version 1.8.2"
+sed -i -e "s/version=1.0.7/version=1.0.8/g" xorg/xcursorgen/spkgbuild
+changelog "xorg/xcursorgen/spkgbuild" "Upgraded from version 1.0.7 to version 1.0.8"
