f1b48ba485acfe9c556df060866dc6594b0f1d5d
Voncloft <voncloft@gmail.com>
2022-12-05 05:37:26 -0500

    Minor Updates

---
+++ logs/old-test-updates/2022/Dec/applied_updates-12-05-22.sh
@@ -0,0 +1,26 @@
+sed -i -e "s/version=6.24.03/version=6.24.05/g" core/tcsh/spkgbuild
+changelog "core/tcsh/spkgbuild" "Upgraded from version 6.24.03 to version 6.24.05"
+sed -i -e "s/version=6.59.0/version=6.60.0/g" python/python-hypothesis/spkgbuild
+changelog "python/python-hypothesis/spkgbuild" "Upgraded from version 6.59.0 to version 6.60.0"
+sed -i -e "s/version=3.10.8/version=3.10.9/g" python/python-mocket/spkgbuild
+changelog "python/python-mocket/spkgbuild" "Upgraded from version 3.10.8 to version 3.10.9"
+sed -i -e "s/version=0.7.4/version=0.7.5/g" python/python-vistir/spkgbuild
+changelog "python/python-vistir/spkgbuild" "Upgraded from version 0.7.4 to version 0.7.5"
+sed -i -e "s/version=0.10.0/version=0.10.1/g" python/python-xattr/spkgbuild
+changelog "python/python-xattr/spkgbuild" "Upgraded from version 0.10.0 to version 0.10.1"
+sed -i -e "s/version=3.11.35/version=3.11.37/g" server/blacklist-hosts/spkgbuild
+changelog "server/blacklist-hosts/spkgbuild" "Upgraded from version 3.11.35 to version 3.11.37"
+sed -i -e "s/version=1.1.3/version=1.1.4/g" xfce/xfd/spkgbuild
+changelog "xfce/xfd/spkgbuild" "Upgraded from version 1.1.3 to version 1.1.4"
+sed -i -e "s/version=1.0.6/version=1.0.7/g" xorg/xgamma/spkgbuild
+changelog "xorg/xgamma/spkgbuild" "Upgraded from version 1.0.6 to version 1.0.7"
+sed -i -e "s/version=1.4.1/version=1.4.2/g" xorg/xinit/spkgbuild
+changelog "xorg/xinit/spkgbuild" "Upgraded from version 1.4.1 to version 1.4.2"
+sed -i -e "s/version=1.2.5/version=1.2.6/g" xorg/xprop/spkgbuild
+changelog "xorg/xprop/spkgbuild" "Upgraded from version 1.2.5 to version 1.2.6"
+sed -i -e "s/version=1.5.1/version=1.5.2/g" xorg/xrandr/spkgbuild
+changelog "xorg/xrandr/spkgbuild" "Upgraded from version 1.5.1 to version 1.5.2"
+sed -i -e "s/version=1.2.4/version=1.2.5/g" xorg/xset/spkgbuild
+changelog "xorg/xset/spkgbuild" "Upgraded from version 1.2.4 to version 1.2.5"
+sed -i -e "s/version=1.1.4/version=1.1.5/g" xorg/xvinfo/spkgbuild
+changelog "xorg/xvinfo/spkgbuild" "Upgraded from version 1.1.4 to version 1.1.5"
