7a02c95d2642171852b5bb1d31f94749b72aabf4
Voncloft <voncloft@gmail.com>
2022-12-06 08:13:10 -0500

    Minor Updates

---
+++ logs/old-test-updates/2022/Dec/applied_updates-12-06-22.sh
@@ -0,0 +1,32 @@
+sed -i -e "s/version=3.0.8/version=3.0.9/g" core/dkms/spkgbuild
+changelog "core/dkms/spkgbuild" "Upgraded from version 3.0.8 to version 3.0.9"
+sed -i -e "s/version=3.2.2/version=3.2.3/g" core/icewm/spkgbuild
+changelog "core/icewm/spkgbuild" "Upgraded from version 3.2.2 to version 3.2.3"
+sed -i -e "s/version=4.0.1/version=4.0.2/g" core/procps-ng/spkgbuild
+changelog "core/procps-ng/spkgbuild" "Upgraded from version 4.0.1 to version 4.0.2"
+sed -i -e "s/version=1.21.2/version=1.21.3/g" media/gst-libav/spkgbuild
+changelog "media/gst-libav/spkgbuild" "Upgraded from version 1.21.2 to version 1.21.3"
+sed -i -e "s/version=1.21.2/version=1.21.3/g" media/gst-plugins-bad/spkgbuild
+changelog "media/gst-plugins-bad/spkgbuild" "Upgraded from version 1.21.2 to version 1.21.3"
+sed -i -e "s/version=1.21.2/version=1.21.3/g" media/gst-plugins-base/spkgbuild
+changelog "media/gst-plugins-base/spkgbuild" "Upgraded from version 1.21.2 to version 1.21.3"
+sed -i -e "s/version=1.21.2/version=1.21.3/g" media/gst-plugins-good/spkgbuild
+changelog "media/gst-plugins-good/spkgbuild" "Upgraded from version 1.21.2 to version 1.21.3"
+sed -i -e "s/version=1.21.2/version=1.21.3/g" media/gst-plugins-ugly/spkgbuild
+changelog "media/gst-plugins-ugly/spkgbuild" "Upgraded from version 1.21.2 to version 1.21.3"
+sed -i -e "s/version=1.21.2/version=1.21.3/g" media/gstreamer/spkgbuild
+changelog "media/gstreamer/spkgbuild" "Upgraded from version 1.21.2 to version 1.21.3"
+sed -i -e "s/version=3.8.0/version=3.8.2/g" python/python-filelock/spkgbuild
+changelog "python/python-filelock/spkgbuild" "Upgraded from version 3.8.0 to version 3.8.2"
+sed -i -e "s/version=0.3.1/version=0.3.2/g" python/python-mdit_py_plugins/spkgbuild
+changelog "python/python-mdit_py_plugins/spkgbuild" "Upgraded from version 0.3.1 to version 0.3.2"
+sed -i -e "s/version=0.22.0/version=0.23.0/g" python/python-starlette/spkgbuild
+changelog "python/python-starlette/spkgbuild" "Upgraded from version 0.22.0 to version 0.23.0"
+sed -i -e "s/version=7.0.3/version=7.0.4/g" python/python-testfixtures/spkgbuild
+changelog "python/python-testfixtures/spkgbuild" "Upgraded from version 7.0.3 to version 7.0.4"
+sed -i -e "s/version=20.17.0/version=20.17.1/g" python/python-virtualenv/spkgbuild
+changelog "python/python-virtualenv/spkgbuild" "Upgraded from version 20.17.0 to version 20.17.1"
+sed -i -e "s/version=2.1.9/version=2.2.0/g" python/python-watchdog/spkgbuild
+changelog "python/python-watchdog/spkgbuild" "Upgraded from version 2.1.9 to version 2.2.0"
+sed -i -e "s/version=2.87/version=2.88/g" server/dnsmasq/spkgbuild
+changelog "server/dnsmasq/spkgbuild" "Upgraded from version 2.87 to version 2.88"
