1d1573d2ec7409cf1446efbd2725e14378d34685
Voncloft <voncloft@gmail.com>
2022-12-07 05:21:45 -0500

    Minor Updates

---
+++ logs/old-test-updates/2022/Dec/applied_updates-12-07-22.sh
@@ -0,0 +1,26 @@
+sed -i -e "s/version=3.1.3/version=3.2.0.rc1/g" compilers/ruby/spkgbuild
+changelog "compilers/ruby/spkgbuild" "Upgraded from version 3.1.3 to version 3.2.0.rc1"
+sed -i -e "s/version=0.107.0/version=0.108.0/g" core/hugo/spkgbuild
+changelog "core/hugo/spkgbuild" "Upgraded from version 0.107.0 to version 0.108.0"
+sed -i -e "s/version=3.5.6/version=3.5.7/g" extra/flexget/spkgbuild
+changelog "extra/flexget/spkgbuild" "Upgraded from version 3.5.6 to version 3.5.7"
+sed -i -e "s/version=108.0b2/version=108.0b3/g" networking/thunderbird/spkgbuild
+changelog "networking/thunderbird/spkgbuild" "Upgraded from version 108.0b2 to version 108.0b3"
+sed -i -e "s/version=0.4.7.11/version=0.4.7.12/g" networking/tor/spkgbuild
+changelog "networking/tor/spkgbuild" "Upgraded from version 0.4.7.11 to version 0.4.7.12"
+sed -i -e "s/version=1.26.22/version=1.26.24/g" python/python-boto3/spkgbuild
+changelog "python/python-boto3/spkgbuild" "Upgraded from version 1.26.22 to version 1.26.24"
+sed -i -e "s/version=1.29.22/version=1.29.24/g" python/python-botocore/spkgbuild
+changelog "python/python-botocore/spkgbuild" "Upgraded from version 1.29.22 to version 1.29.24"
+sed -i -e "s/version=0.72.1/version=0.72.2/g" python/python-cfn-lint/spkgbuild
+changelog "python/python-cfn-lint/spkgbuild" "Upgraded from version 0.72.1 to version 0.72.2"
+sed -i -e "s/version=1.1.4/version=1.1.5/g" python/python-lark-parser/spkgbuild
+changelog "python/python-lark-parser/spkgbuild" "Upgraded from version 1.1.4 to version 1.1.5"
+sed -i -e "s/version=0.3.2/version=0.3.3/g" python/python-mdit_py_plugins/spkgbuild
+changelog "python/python-mdit_py_plugins/spkgbuild" "Upgraded from version 0.3.2 to version 0.3.3"
+sed -i -e "s/version=2.2.0/version=2.2.1/g" python/python-requirementslib/spkgbuild
+changelog "python/python-requirementslib/spkgbuild" "Upgraded from version 2.2.0 to version 2.2.1"
+sed -i -e "s/version=3.6.0/version=3.6.1/g" python/python-zconfig/spkgbuild
+changelog "python/python-zconfig/spkgbuild" "Upgraded from version 3.6.0 to version 3.6.1"
+sed -i -e "s/version=3.11.37/version=3.11.38/g" server/blacklist-hosts/spkgbuild
+changelog "server/blacklist-hosts/spkgbuild" "Upgraded from version 3.11.37 to version 3.11.38"
