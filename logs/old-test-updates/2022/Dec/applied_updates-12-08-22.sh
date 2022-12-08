9eeb32b3c62607c406d9a3c080a0d2130ca6d152
Voncloft <voncloft@gmail.com>
2022-12-08 07:58:36 -0500

    Minor Updates

---
+++ logs/old-test-updates/2022/Dec/applied_updates-12-08-22.sh
@@ -0,0 +1,14 @@
+sed -i -e "s/version=1.21.1/version=1.21.2/g" libs/blosc/spkgbuild
+changelog "libs/blosc/spkgbuild" "Upgraded from version 1.21.1 to version 1.21.2"
+sed -i -e "s/version=1.10.6/version=1.11.0/g" python/python-blosc/spkgbuild
+changelog "python/python-blosc/spkgbuild" "Upgraded from version 1.10.6 to version 1.11.0"
+sed -i -e "s/version=2.1.2/version=2.1.3/g" python/python-gmpy2/spkgbuild
+changelog "python/python-gmpy2/spkgbuild" "Upgraded from version 2.1.2 to version 2.1.3"
+sed -i -e "s/version=8.0.3/version=8.0.4/g" python/python-ipywidgets/spkgbuild
+changelog "python/python-ipywidgets/spkgbuild" "Upgraded from version 8.0.3 to version 8.0.4"
+sed -i -e "s/version=1.3.0/version=1.4.0/g" python/python-lmdb/spkgbuild
+changelog "python/python-lmdb/spkgbuild" "Upgraded from version 1.3.0 to version 1.4.0"
+sed -i -e "s/version=2022.09.29/version=2022.12.07/g" python/python-sphinx-furo/spkgbuild
+changelog "python/python-sphinx-furo/spkgbuild" "Upgraded from version 2022.09.29 to version 2022.12.07"
+sed -i -e "s/version=3.11.38/version=3.11.39/g" server/blacklist-hosts/spkgbuild
+changelog "server/blacklist-hosts/spkgbuild" "Upgraded from version 3.11.38 to version 3.11.39"
