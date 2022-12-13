9c6b065d60ddd87c2fd5568b95c55f5758a79127
Voncloft <voncloft@gmail.com>
2022-12-13 03:49:49 -0500

    Minor Updates

---
+++ logs/old-test-updates/2022/Dec/applied_updates-12-13-22.sh
@@ -0,0 +1,42 @@
+sed -i -e "s/version=0.5.11.5/version=0.5.12/g" core/dash/spkgbuild
+changelog "core/dash/spkgbuild" "Upgraded from version 0.5.11.5 to version 0.5.12"
+sed -i -e "s/version=6.0.12/version=6.1/g" core/linux-api-headers/spkgbuild
+changelog "core/linux-api-headers/spkgbuild" "Upgraded from version 6.0.12 to version 6.1"
+sed -i -e "s/version=6.0.12/version=6.1/g" core/linux-desktop/spkgbuild
+changelog "core/linux-desktop/spkgbuild" "Upgraded from version 6.0.12 to version 6.1"
+sed -i -e "s/version=6.0.12/version=6.1/g" core/linux-router/spkgbuild
+changelog "core/linux-router/spkgbuild" "Upgraded from version 6.0.12 to version 6.1"
+sed -i -e "s/version=7.0.5/version=7.0.6/g" core/redis/spkgbuild
+changelog "core/redis/spkgbuild" "Upgraded from version 7.0.5 to version 7.0.6"
+sed -i -e "s/version=6.24.05/version=6.24.06/g" core/tcsh/spkgbuild
+changelog "core/tcsh/spkgbuild" "Upgraded from version 6.24.05 to version 6.24.06"
+sed -i -e "s/version=3.5.9/version=3.5.10/g" extra/flexget/spkgbuild
+changelog "extra/flexget/spkgbuild" "Upgraded from version 3.5.9 to version 3.5.10"
+sed -i -e "s/version=1.1.8rc2/version=1.1.8rc3/g" extra/solaar/spkgbuild
+changelog "extra/solaar/spkgbuild" "Upgraded from version 1.1.8rc2 to version 1.1.8rc3"
+sed -i -e "s/version=0.39.2/version=0.40.0/g" libs/libfilezilla/spkgbuild
+changelog "libs/libfilezilla/spkgbuild" "Upgraded from version 0.39.2 to version 0.40.0"
+sed -i -e "s/version=3.6.1/version=3.7.0/g" libs/libressl/spkgbuild
+changelog "libs/libressl/spkgbuild" "Upgraded from version 3.6.1 to version 3.7.0"
+sed -i -e "s/version=2.38.1/version=2.39.0/g" networking/git/spkgbuild
+changelog "networking/git/spkgbuild" "Upgraded from version 2.38.1 to version 2.39.0"
+sed -i -e "s/version=0.4.1620/version=0.4.1650/g" networking/you-get/spkgbuild
+changelog "networking/you-get/spkgbuild" "Upgraded from version 0.4.1620 to version 0.4.1650"
+sed -i -e "s/version=1.26.27/version=1.26.28/g" python/python-boto3/spkgbuild
+changelog "python/python-boto3/spkgbuild" "Upgraded from version 1.26.27 to version 1.26.28"
+sed -i -e "s/version=1.29.27/version=1.29.28/g" python/python-botocore/spkgbuild
+changelog "python/python-botocore/spkgbuild" "Upgraded from version 1.29.27 to version 1.29.28"
+sed -i -e "s/version=6.60.0/version=6.61.0/g" python/python-hypothesis/spkgbuild
+changelog "python/python-hypothesis/spkgbuild" "Upgraded from version 6.60.0 to version 6.61.0"
+sed -i -e "s/version=5.10.1/version=5.11.0/g" python/python-isort/spkgbuild
+changelog "python/python-isort/spkgbuild" "Upgraded from version 5.10.1 to version 5.11.0"
+sed -i -e "s/version=0.16.0/version=0.16.1/g" python/python-libtmux/spkgbuild
+changelog "python/python-libtmux/spkgbuild" "Upgraded from version 0.16.0 to version 0.16.1"
+sed -i -e "s/version=3.7/version=3.8/g" python/python-nltk/spkgbuild
+changelog "python/python-nltk/spkgbuild" "Upgraded from version 3.7 to version 3.8"
+sed -i -e "s/version=1.3.0/version=1.3.1/g" python/python-poetry/spkgbuild
+changelog "python/python-poetry/spkgbuild" "Upgraded from version 1.3.0 to version 1.3.1"
+sed -i -e "s/version=3.11.39/version=3.11.40/g" server/blacklist-hosts/spkgbuild
+changelog "server/blacklist-hosts/spkgbuild" "Upgraded from version 3.11.39 to version 3.11.40"
+sed -i -e "s/version=2022.12.1/version=2022.12.3/g" server/homeassistant/spkgbuild
+changelog "server/homeassistant/spkgbuild" "Upgraded from version 2022.12.1 to version 2022.12.3"
