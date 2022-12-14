e0318dfa560b41e67691279ce8dc693560d242a3
Voncloft <voncloft@gmail.com>
2022-12-14 08:09:43 -0500

    Minor Updates

---
+++ logs/old-test-updates/2022/Dec/applied_updates-12-14-22.sh
@@ -0,0 +1,36 @@
+sed -i -e "s/version=5.6.1/version=5.6.2/g" cinnamon/muffin/spkgbuild
+changelog "cinnamon/muffin/spkgbuild" "Upgraded from version 5.6.1 to version 5.6.2"
+sed -i -e "s/version=5.6.0/version=5.6.1/g" cinnamon/nemo/spkgbuild
+changelog "cinnamon/nemo/spkgbuild" "Upgraded from version 5.6.0 to version 5.6.1"
+sed -i -e "s/version=5.2.9/version=5.2.15/g" core/bash/spkgbuild
+changelog "core/bash/spkgbuild" "Upgraded from version 5.2.9 to version 5.2.15"
+sed -i -e "s/version=6.0.0/version=6.1.0/g" core/iproute2/spkgbuild
+changelog "core/iproute2/spkgbuild" "Upgraded from version 6.0.0 to version 6.1.0"
+sed -i -e "s/version=3.20.1/version=3.21.0/g" core/logrotate/spkgbuild
+changelog "core/logrotate/spkgbuild" "Upgraded from version 3.20.1 to version 3.21.0"
+sed -i -e "s/version=23.5/version=23.6/g" core/psmisc/spkgbuild
+changelog "core/psmisc/spkgbuild" "Upgraded from version 23.5 to version 23.6"
+sed -i -e "s/version=5.2.9/version=5.4.0/g" core/xz/spkgbuild
+changelog "core/xz/spkgbuild" "Upgraded from version 5.2.9 to version 5.4.0"
+sed -i -e "s/version=108.0b9/version=109.0b1/g" networking/firefox/spkgbuild
+changelog "networking/firefox/spkgbuild" "Upgraded from version 108.0b9 to version 109.0b1"
+sed -i -e "s/version=0.9.21/version=0.9.21.1/g" networking/xrdp/spkgbuild
+changelog "networking/xrdp/spkgbuild" "Upgraded from version 0.9.21 to version 0.9.21.1"
+sed -i -e "s/version=0.3.5/version=0.3.6/g" python/python-flake8-black/spkgbuild
+changelog "python/python-flake8-black/spkgbuild" "Upgraded from version 0.3.5 to version 0.3.6"
+sed -i -e "s/version=2.69.0/version=2.70.0/g" python/python-google-api-python-client/spkgbuild
+changelog "python/python-google-api-python-client/spkgbuild" "Upgraded from version 2.69.0 to version 2.70.0"
+sed -i -e "s/version=0.13.9/version=0.13.11/g" python/python-ipdb/spkgbuild
+changelog "python/python-ipdb/spkgbuild" "Upgraded from version 0.13.9 to version 0.13.11"
+sed -i -e "s/version=5.11.0/version=5.11.2/g" python/python-isort/spkgbuild
+changelog "python/python-isort/spkgbuild" "Upgraded from version 5.11.0 to version 5.11.2"
+sed -i -e "s/version=3.11.40/version=3.11.41/g" server/blacklist-hosts/spkgbuild
+changelog "server/blacklist-hosts/spkgbuild" "Upgraded from version 3.11.40 to version 3.11.41"
+sed -i -e "s/version=2022.12.3/version=2022.12.5/g" server/homeassistant/spkgbuild
+changelog "server/homeassistant/spkgbuild" "Upgraded from version 2022.12.3 to version 2022.12.5"
+sed -i -e "s/version=1.23.2/version=1.23.3/g" server/nginx/spkgbuild
+changelog "server/nginx/spkgbuild" "Upgraded from version 1.23.2 to version 1.23.3"
+sed -i -e "s/version=1.29.2.6364/version=1.30.0.6486/g" server/plex-media-server/spkgbuild
+changelog "server/plex-media-server/spkgbuild" "Upgraded from version 1.29.2.6364 to version 1.30.0.6486"
+sed -i -e "s/version=1.0.8/version=1.0.9/g" xorg/xhost/spkgbuild
+changelog "xorg/xhost/spkgbuild" "Upgraded from version 1.0.8 to version 1.0.9"
