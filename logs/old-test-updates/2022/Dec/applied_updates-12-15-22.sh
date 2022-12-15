011795228ca50dfe5fc0eb63f3e3149a33fd18c3
Voncloft <voncloft@gmail.com>
2022-12-15 07:30:07 -0500

    Minor Updates

---
+++ logs/old-test-updates/2022/Dec/applied_updates-12-15-22.sh
@@ -0,0 +1,34 @@
+sed -i -e "s/version=7.0/version=7.1/g" core/nano/spkgbuild
+changelog "core/nano/spkgbuild" "Upgraded from version 7.0 to version 7.1"
+sed -i -e "s/version=2.39.2/version=2.39.3/g" core/webkitgtk/spkgbuild
+changelog "core/webkitgtk/spkgbuild" "Upgraded from version 2.39.2 to version 2.39.3"
+sed -i -e "s/version=3.5.10/version=3.5.11/g" extra/flexget/spkgbuild
+changelog "extra/flexget/spkgbuild" "Upgraded from version 3.5.10 to version 3.5.11"
+sed -i -e "s/version=21.11/version=21.12/g" extra/protobuf/spkgbuild
+changelog "extra/protobuf/spkgbuild" "Upgraded from version 21.11 to version 21.12"
+sed -i -e "s/version=109.0b1/version=109.0b2/g" networking/firefox/spkgbuild
+changelog "networking/firefox/spkgbuild" "Upgraded from version 109.0b1 to version 109.0b2"
+sed -i -e "s/version=0.7.3/version=0.7.4/g" python/python-aioresponses/spkgbuild
+changelog "python/python-aioresponses/spkgbuild" "Upgraded from version 0.7.3 to version 0.7.4"
+sed -i -e "s/version=0.31.1/version=0.31.2/g" python/python-binary-memcached/spkgbuild
+changelog "python/python-binary-memcached/spkgbuild" "Upgraded from version 0.31.1 to version 0.31.2"
+sed -i -e "s/version=1.26.28/version=1.26.29/g" python/python-boto3/spkgbuild
+changelog "python/python-boto3/spkgbuild" "Upgraded from version 1.26.28 to version 1.26.29"
+sed -i -e "s/version=1.29.28/version=1.29.29/g" python/python-botocore/spkgbuild
+changelog "python/python-botocore/spkgbuild" "Upgraded from version 1.29.28 to version 1.29.29"
+sed -i -e "s/version=0.72.2/version=0.72.3/g" python/python-cfn-lint/spkgbuild
+changelog "python/python-cfn-lint/spkgbuild" "Upgraded from version 0.72.2 to version 0.72.3"
+sed -i -e "s/version=4.9.1/version=4.9.2/g" python/python-lxml/spkgbuild
+changelog "python/python-lxml/spkgbuild" "Upgraded from version 4.9.1 to version 4.9.2"
+sed -i -e "s/version=3.21.0/version=3.22.0/g" python/python-openmdao/spkgbuild
+changelog "python/python-openmdao/spkgbuild" "Upgraded from version 3.21.0 to version 3.22.0"
+sed -i -e "s/version=0.4.2/version=0.4.3/g" python/python-plette/spkgbuild
+changelog "python/python-plette/spkgbuild" "Upgraded from version 0.4.2 to version 0.4.3"
+sed -i -e "s/version=0.6.8/version=0.6.9/g" python/python-podcastparser/spkgbuild
+changelog "python/python-podcastparser/spkgbuild" "Upgraded from version 0.6.8 to version 0.6.9"
+sed -i -e "s/version=0.0.8/version=0.0.10/g" python/python-tox-current-env/spkgbuild
+changelog "python/python-tox-current-env/spkgbuild" "Upgraded from version 0.0.8 to version 0.0.10"
+sed -i -e "s/version=2022.12.5/version=2022.12.6/g" server/homeassistant/spkgbuild
+changelog "server/homeassistant/spkgbuild" "Upgraded from version 2022.12.5 to version 2022.12.6"
+sed -i -e "s/version=21.1.4/version=21.1.5/g" xorg/xorg-server/spkgbuild
+changelog "xorg/xorg-server/spkgbuild" "Upgraded from version 21.1.4 to version 21.1.5"
