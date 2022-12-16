d22d8ffb9a74a3dfd203acce176834ae8980ab84
Voncloft <voncloft@gmail.com>
2022-12-16 06:36:09 -0500

    Minor Updates

---
+++ logs/old-test-updates/2022/Dec/applied_updates-12-16-22.sh
@@ -0,0 +1,64 @@
+sed -i -e "s/version=5.6.4/version=5.6.5/g" cinnamon/cinnamon/spkgbuild
+changelog "cinnamon/cinnamon/spkgbuild" "Upgraded from version 5.6.4 to version 5.6.5"
+sed -i -e "s/version=1.65.0/version=1.66.0/g" compilers/rust/spkgbuild
+changelog "compilers/rust/spkgbuild" "Upgraded from version 1.65.0 to version 1.66.0"
+sed -i -e "s/version=1.49.2/version=1.49.3/g" core/help2man/spkgbuild
+changelog "core/help2man/spkgbuild" "Upgraded from version 1.49.2 to version 1.49.3"
+sed -i -e "s/version=0.3.62/version=0.3.63/g" core/pipewire/spkgbuild
+changelog "core/pipewire/spkgbuild" "Upgraded from version 0.3.62 to version 0.3.63"
+sed -i -e "s/version=12.1/version=12.2/g" core/yad/spkgbuild
+changelog "core/yad/spkgbuild" "Upgraded from version 12.1 to version 12.2"
+sed -i -e "s/version=7.1.0/version=7.2.0/g" extra/qemu/spkgbuild
+changelog "extra/qemu/spkgbuild" "Upgraded from version 7.1.0 to version 7.2.0"
+sed -i -e "s/version=1.21.2/version=1.21.3/g" libs/blosc/spkgbuild
+changelog "libs/blosc/spkgbuild" "Upgraded from version 1.21.2 to version 1.21.3"
+sed -i -e "s/version=4.17.9/version=4.18.0/g" libs/libxfce4ui/spkgbuild
+changelog "libs/libxfce4ui/spkgbuild" "Upgraded from version 4.17.9 to version 4.18.0"
+sed -i -e "s/version=4.17.4/version=4.18.0/g" libs/libxfce4util/spkgbuild
+changelog "libs/libxfce4util/spkgbuild" "Upgraded from version 4.17.4 to version 4.18.0"
+sed -i -e "s/version=108.0b3/version=109.0b1/g" networking/thunderbird/spkgbuild
+changelog "networking/thunderbird/spkgbuild" "Upgraded from version 108.0b3 to version 109.0b1"
+sed -i -e "s/version=1.12.0/version=1.12.90/g" networking/tigervnc/spkgbuild
+changelog "networking/tigervnc/spkgbuild" "Upgraded from version 1.12.0 to version 1.12.90"
+sed -i -e "s/version=1.11.0/version=1.11.1/g" python/python-blosc/spkgbuild
+changelog "python/python-blosc/spkgbuild" "Upgraded from version 1.11.0 to version 1.11.1"
+sed -i -e "s/version=1.26.29/version=1.26.31/g" python/python-boto3/spkgbuild
+changelog "python/python-boto3/spkgbuild" "Upgraded from version 1.26.29 to version 1.26.31"
+sed -i -e "s/version=1.29.29/version=1.29.31/g" python/python-botocore/spkgbuild
+changelog "python/python-botocore/spkgbuild" "Upgraded from version 1.29.29 to version 1.29.31"
+sed -i -e "s/version=2.5.9/version=2.5.10/g" python/python-identify/spkgbuild
+changelog "python/python-identify/spkgbuild" "Upgraded from version 2.5.9 to version 2.5.10"
+sed -i -e "s/version=0.4.3/version=0.4.4/g" python/python-plette/spkgbuild
+changelog "python/python-plette/spkgbuild" "Upgraded from version 0.4.3 to version 0.4.4"
+sed -i -e "s/version=1.5.1/version=1.6.0/g" python/python-rope/spkgbuild
+changelog "python/python-rope/spkgbuild" "Upgraded from version 1.5.1 to version 1.6.0"
+sed -i -e "s/version=4.5.0/version=4.6/g" python/python-zope-event/spkgbuild
+changelog "python/python-zope-event/spkgbuild" "Upgraded from version 4.5.0 to version 4.6"
+sed -i -e "s/version=4.17.3/version=4.17.4/g" server/samba/spkgbuild
+changelog "server/samba/spkgbuild" "Upgraded from version 4.17.3 to version 4.17.4"
+sed -i -e "s/version=4.17.4/version=4.18.0/g" xfce/exo/spkgbuild
+changelog "xfce/exo/spkgbuild" "Upgraded from version 4.17.4 to version 4.18.0"
+sed -i -e "s/version=4.17.3/version=4.18.0/g" xfce/garcon/spkgbuild
+changelog "xfce/garcon/spkgbuild" "Upgraded from version 4.17.3 to version 4.18.0"
+sed -i -e "s/version=4.17.12/version=4.18.0/g" xfce/thunar/spkgbuild
+changelog "xfce/thunar/spkgbuild" "Upgraded from version 4.17.12 to version 4.18.0"
+sed -i -e "s/version=4.17.1/version=4.18.0/g" xfce/thunar-volman/spkgbuild
+changelog "xfce/thunar-volman/spkgbuild" "Upgraded from version 4.17.1 to version 4.18.0"
+sed -i -e "s/version=4.17.3/version=4.18.0/g" xfce/tumbler/spkgbuild
+changelog "xfce/tumbler/spkgbuild" "Upgraded from version 4.17.3 to version 4.18.0"
+sed -i -e "s/version=4.17.2/version=4.18.0/g" xfce/xfce4-appfinder/spkgbuild
+changelog "xfce/xfce4-appfinder/spkgbuild" "Upgraded from version 4.17.2 to version 4.18.0"
+sed -i -e "s/version=4.17.5/version=4.18.0/g" xfce/xfce4-panel/spkgbuild
+changelog "xfce/xfce4-panel/spkgbuild" "Upgraded from version 4.17.5 to version 4.18.0"
+sed -i -e "s/version=4.17.1/version=4.18.0/g" xfce/xfce4-power-manager/spkgbuild
+changelog "xfce/xfce4-power-manager/spkgbuild" "Upgraded from version 4.17.1 to version 4.18.0"
+sed -i -e "s/version=4.17.2/version=4.18.0/g" xfce/xfce4-session/spkgbuild
+changelog "xfce/xfce4-session/spkgbuild" "Upgraded from version 4.17.2 to version 4.18.0"
+sed -i -e "s/version=4.17.3/version=4.18.0/g" xfce/xfce4-settings/spkgbuild
+changelog "xfce/xfce4-settings/spkgbuild" "Upgraded from version 4.17.3 to version 4.18.0"
+sed -i -e "s/version=4.17.2/version=4.18.0/g" xfce/xfconf/spkgbuild
+changelog "xfce/xfconf/spkgbuild" "Upgraded from version 4.17.2 to version 4.18.0"
+sed -i -e "s/version=4.17.2/version=4.18.0/g" xfce/xfdesktop/spkgbuild
+changelog "xfce/xfdesktop/spkgbuild" "Upgraded from version 4.17.2 to version 4.18.0"
+sed -i -e "s/version=4.17.2/version=4.18.0/g" xfce/xfwm4/spkgbuild
+changelog "xfce/xfwm4/spkgbuild" "Upgraded from version 4.17.2 to version 4.18.0"
