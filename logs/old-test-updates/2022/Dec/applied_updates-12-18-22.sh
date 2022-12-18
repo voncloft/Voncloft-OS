87d8bc04388352c3549ba77f73aefef70240c964
Voncloft <voncloft@gmail.com>
2022-12-18 06:52:09 -0500

    Minor Updates

---
+++ logs/old-test-updates/2022/Dec/applied_updates-12-18-22.sh
@@ -0,0 +1,46 @@
+sed -i -e "s/version=5.6.0/version=5.6.1/g" cinnamon/cinnamon-translations/spkgbuild
+changelog "cinnamon/cinnamon-translations/spkgbuild" "Upgraded from version 5.6.0 to version 5.6.1"
+sed -i -e "s/version=1.9.3/version=1.9.4/g" core/deadbeef/spkgbuild
+changelog "core/deadbeef/spkgbuild" "Upgraded from version 1.9.3 to version 1.9.4"
+sed -i -e "s/version=10.1/version=10.2/g" core/picom/spkgbuild
+changelog "core/picom/spkgbuild" "Upgraded from version 10.1 to version 10.2"
+sed -i -e "s/version=7.0.6/version=7.0.7/g" core/redis/spkgbuild
+changelog "core/redis/spkgbuild" "Upgraded from version 7.0.6 to version 7.0.7"
+sed -i -e "s/version=12.2/version=12.3/g" core/yad/spkgbuild
+changelog "core/yad/spkgbuild" "Upgraded from version 12.2 to version 12.3"
+sed -i -e "s/version=3.5.11/version=3.5.12/g" extra/flexget/spkgbuild
+changelog "extra/flexget/spkgbuild" "Upgraded from version 3.5.11 to version 3.5.12"
+sed -i -e "s/version=109.0b2/version=109.0b3/g" networking/firefox/spkgbuild
+changelog "networking/firefox/spkgbuild" "Upgraded from version 109.0b2 to version 109.0b3"
+sed -i -e "s/version=1.26.31/version=1.26.32/g" python/python-boto3/spkgbuild
+changelog "python/python-boto3/spkgbuild" "Upgraded from version 1.26.31 to version 1.26.32"
+sed -i -e "s/version=1.29.31/version=1.29.32/g" python/python-botocore/spkgbuild
+changelog "python/python-botocore/spkgbuild" "Upgraded from version 1.29.31 to version 1.29.32"
+sed -i -e "s/version=0.72.3/version=0.72.5/g" python/python-cfn-lint/spkgbuild
+changelog "python/python-cfn-lint/spkgbuild" "Upgraded from version 0.72.3 to version 0.72.5"
+sed -i -e "s/version=3.10.1/version=3.10.2/g" python/python-chameleon/spkgbuild
+changelog "python/python-chameleon/spkgbuild" "Upgraded from version 3.10.1 to version 3.10.2"
+sed -i -e "s/version=9.1.0/version=9.2.0/g" python/python-cinderclient/spkgbuild
+changelog "python/python-cinderclient/spkgbuild" "Upgraded from version 9.1.0 to version 9.2.0"
+sed -i -e "s/version=2022.12.0/version=2022.12.1/g" python/python-distributed/spkgbuild
+changelog "python/python-distributed/spkgbuild" "Upgraded from version 2022.12.0 to version 2022.12.1"
+sed -i -e "s/version=0.6.1/version=0.6.2/g" python/python-editdistance/spkgbuild
+changelog "python/python-editdistance/spkgbuild" "Upgraded from version 0.6.1 to version 0.6.2"
+sed -i -e "s/version=5.11.2/version=5.11.3/g" python/python-isort/spkgbuild
+changelog "python/python-isort/spkgbuild" "Upgraded from version 5.11.2 to version 5.11.3"
+sed -i -e "s/version=3.0.0/version=3.0.1/g" python/python-jsonpickle/spkgbuild
+changelog "python/python-jsonpickle/spkgbuild" "Upgraded from version 3.0.0 to version 3.0.1"
+sed -i -e "s/version=2022.11.30/version=2022.12.17/g" python/python-pipenv/spkgbuild
+changelog "python/python-pipenv/spkgbuild" "Upgraded from version 2022.11.30 to version 2022.12.17"
+sed -i -e "s/version=0.16.3/version=0.16.4/g" python/python-scikit-build/spkgbuild
+changelog "python/python-scikit-build/spkgbuild" "Upgraded from version 0.16.3 to version 0.16.4"
+sed -i -e "s/version=1.1.1/version=1.2.0/g" python/python-sphinx_rtd_theme/spkgbuild
+changelog "python/python-sphinx_rtd_theme/spkgbuild" "Upgraded from version 1.1.1 to version 1.2.0"
+sed -i -e "s/version=3.12.0/version=3.12.1/g" ruby-gems/ruby-rspec-expectations/spkgbuild
+changelog "ruby-gems/ruby-rspec-expectations/spkgbuild" "Upgraded from version 3.12.0 to version 3.12.1"
+sed -i -e "s/version=3.11.41/version=3.11.42/g" server/blacklist-hosts/spkgbuild
+changelog "server/blacklist-hosts/spkgbuild" "Upgraded from version 3.11.41 to version 3.11.42"
+sed -i -e "s/version=0.12.3/version=0.12.4/g" xfce/ristretto/spkgbuild
+changelog "xfce/ristretto/spkgbuild" "Upgraded from version 0.12.3 to version 0.12.4"
+sed -i -e "s/version=0.6.4/version=0.6.5/g" xfce/xfce4-notifyd/spkgbuild
+changelog "xfce/xfce4-notifyd/spkgbuild" "Upgraded from version 0.6.4 to version 0.6.5"
