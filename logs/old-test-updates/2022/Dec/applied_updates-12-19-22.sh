e75705581c300ad30b2f9b82691c682afd67ecfa
Voncloft <voncloft@gmail.com>
2022-12-19 04:53:57 -0500

    Minor Updates

---
+++ logs/old-test-updates/2022/Dec/applied_updates-12-19-22.sh
@@ -0,0 +1,8 @@
+sed -i -e "s/version=0.6.2/version=0.7.0/g" python/python-databases/spkgbuild
+changelog "python/python-databases/spkgbuild" "Upgraded from version 0.6.2 to version 0.7.0"
+sed -i -e "s/version=0.10.2/version=0.10.3/g" python/python-pytest-mypy/spkgbuild
+changelog "python/python-pytest-mypy/spkgbuild" "Upgraded from version 0.10.2 to version 0.10.3"
+sed -i -e "s/version=0.39.4/version=0.44.0/g" python/python-zeroconf/spkgbuild
+changelog "python/python-zeroconf/spkgbuild" "Upgraded from version 0.39.4 to version 0.44.0"
+sed -i -e "s/version=2022.12.6/version=2022.12.7/g" server/homeassistant/spkgbuild
+changelog "server/homeassistant/spkgbuild" "Upgraded from version 2022.12.6 to version 2022.12.7"
