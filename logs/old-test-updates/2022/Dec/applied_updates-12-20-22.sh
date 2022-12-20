8132c8fc0aa29b073e41ad57df6e1acfb74b151e
Voncloft <voncloft@gmail.com>
2022-12-20 07:36:52 -0500

    Minor Updates

---
+++ logs/old-test-updates/2022/Dec/applied_updates-12-20-22.sh
@@ -0,0 +1,28 @@
+sed -i -e "s/version=4.4.0/version=4.5.0/g" core/qalculate/spkgbuild
+changelog "core/qalculate/spkgbuild" "Upgraded from version 4.4.0 to version 4.5.0"
+sed -i -e "s/version=4.12/version=4.13/g" extra/gnucash/spkgbuild
+changelog "extra/gnucash/spkgbuild" "Upgraded from version 4.12 to version 4.13"
+sed -i -e "s/version=4.4.0/version=4.5.0/g" libs/libqalculate/spkgbuild
+changelog "libs/libqalculate/spkgbuild" "Upgraded from version 4.4.0 to version 4.5.0"
+sed -i -e "s/version=109.0b3/version=109.0b4/g" networking/firefox/spkgbuild
+changelog "networking/firefox/spkgbuild" "Upgraded from version 109.0b3 to version 109.0b4"
+sed -i -e "s/version=0.17.0/version=0.18.0/g" python/python-aiosqlite/spkgbuild
+changelog "python/python-aiosqlite/spkgbuild" "Upgraded from version 0.17.0 to version 0.18.0"
+sed -i -e "s/version=1.26.32/version=1.26.33/g" python/python-boto3/spkgbuild
+changelog "python/python-boto3/spkgbuild" "Upgraded from version 1.26.32 to version 1.26.33"
+sed -i -e "s/version=1.29.32/version=1.29.33/g" python/python-botocore/spkgbuild
+changelog "python/python-botocore/spkgbuild" "Upgraded from version 1.29.32 to version 1.29.33"
+sed -i -e "s/version=2.1.3/version=2.1.5/g" python/python-gmpy2/spkgbuild
+changelog "python/python-gmpy2/spkgbuild" "Upgraded from version 2.1.3 to version 2.1.5"
+sed -i -e "s/version=5.1.0/version=5.2.0/g" python/python-importlib-metadata/spkgbuild
+changelog "python/python-importlib-metadata/spkgbuild" "Upgraded from version 5.1.0 to version 5.2.0"
+sed -i -e "s/version=6.19.2/version=6.19.3/g" python/python-ipykernel/spkgbuild
+changelog "python/python-ipykernel/spkgbuild" "Upgraded from version 6.19.2 to version 6.19.3"
+sed -i -e "s/version=2022.12.17/version=2022.12.19/g" python/python-pipenv/spkgbuild
+changelog "python/python-pipenv/spkgbuild" "Upgraded from version 2022.12.17 to version 2022.12.19"
+sed -i -e "s/version=2.2.1/version=2.2.2/g" python/python-requirementslib/spkgbuild
+changelog "python/python-requirementslib/spkgbuild" "Upgraded from version 2.2.1 to version 2.2.2"
+sed -i -e "s/version=3.11.42/version=3.11.43/g" server/blacklist-hosts/spkgbuild
+changelog "server/blacklist-hosts/spkgbuild" "Upgraded from version 3.11.42 to version 3.11.43"
+sed -i -e "s/version=21.1.5/version=21.1.6/g" xorg/xorg-server/spkgbuild
+changelog "xorg/xorg-server/spkgbuild" "Upgraded from version 21.1.5 to version 21.1.6"
