f049bf88a4c2f295d22095002de478081a4b9319
Voncloft <voncloft@gmail.com>
2022-12-21 03:41:44 -0500

    Minor Updates

---
+++ logs/old-test-updates/2022/Dec/applied_updates-12-21-22.sh
@@ -0,0 +1,30 @@
+sed -i -e "s/version=6.0/version=6.1/g" core/ethtool/spkgbuild
+changelog "core/ethtool/spkgbuild" "Upgraded from version 6.0 to version 6.1"
+sed -i -e "s/version=2.3.8/version=2.4.0/g" core/gnupg/spkgbuild
+changelog "core/gnupg/spkgbuild" "Upgraded from version 2.3.8 to version 2.4.0"
+sed -i -e "s/version=3.2.3/version=3.3.0/g" core/icewm/spkgbuild
+changelog "core/icewm/spkgbuild" "Upgraded from version 3.2.3 to version 3.3.0"
+sed -i -e "s/version=1.6.2/version=1.6.3/g" libs/libksba/spkgbuild
+changelog "libs/libksba/spkgbuild" "Upgraded from version 1.6.2 to version 1.6.3"
+sed -i -e "s/version=109.0b1/version=109.0b2/g" networking/thunderbird/spkgbuild
+changelog "networking/thunderbird/spkgbuild" "Upgraded from version 109.0b1 to version 109.0b2"
+sed -i -e "s/version=1.26.33/version=1.26.34/g" python/python-boto3/spkgbuild
+changelog "python/python-boto3/spkgbuild" "Upgraded from version 1.26.33 to version 1.26.34"
+sed -i -e "s/version=1.29.33/version=1.29.34/g" python/python-botocore/spkgbuild
+changelog "python/python-botocore/spkgbuild" "Upgraded from version 1.29.33 to version 1.29.34"
+sed -i -e "s/version=0.16.2/version=0.16.3/g" python/python-httpcore/spkgbuild
+changelog "python/python-httpcore/spkgbuild" "Upgraded from version 0.16.2 to version 0.16.3"
+sed -i -e "s/version=2.5.10/version=2.5.11/g" python/python-identify/spkgbuild
+changelog "python/python-identify/spkgbuild" "Upgraded from version 2.5.10 to version 2.5.11"
+sed -i -e "s/version=6.19.3/version=6.19.4/g" python/python-ipykernel/spkgbuild
+changelog "python/python-ipykernel/spkgbuild" "Upgraded from version 6.19.3 to version 6.19.4"
+sed -i -e "s/version=4.10/version=5.0.1/g" python/python-zope-testing/spkgbuild
+changelog "python/python-zope-testing/spkgbuild" "Upgraded from version 4.10 to version 5.0.1"
+sed -i -e "s/version=4.10/version=5.0.1/g" python/python-zope.testing/spkgbuild
+changelog "python/python-zope.testing/spkgbuild" "Upgraded from version 4.10 to version 5.0.1"
+sed -i -e "s/version=3.11.43/version=3.11.44/g" server/blacklist-hosts/spkgbuild
+changelog "server/blacklist-hosts/spkgbuild" "Upgraded from version 3.11.43 to version 3.11.44"
+sed -i -e "s/version=0.4.0/version=0.4.1/g" xorg/xcb-util/spkgbuild
+changelog "xorg/xcb-util/spkgbuild" "Upgraded from version 0.4.0 to version 0.4.1"
+sed -i -e "s/version=1.0.6/version=1.0.7/g" xorg/xdriinfo/spkgbuild
+changelog "xorg/xdriinfo/spkgbuild" "Upgraded from version 1.0.6 to version 1.0.7"
