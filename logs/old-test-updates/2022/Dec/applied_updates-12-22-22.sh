c24926fb7049ee812a973e94bd4b4820c04e50f1
Voncloft <voncloft@gmail.com>
2022-12-22 04:30:26 -0500

    Minor Updates

---
+++ logs/old-test-updates/2022/Dec/applied_updates-12-22-22.sh
@@ -0,0 +1,24 @@
+sed -i -e "s/version=7.86.0/version=7.87.0/g" core/curl/spkgbuild
+changelog "core/curl/spkgbuild" "Upgraded from version 7.86.0 to version 7.87.0"
+sed -i -e "s/version=6.1/version=6.1.1/g" core/linux-api-headers/spkgbuild
+changelog "core/linux-api-headers/spkgbuild" "Upgraded from version 6.1 to version 6.1.1"
+sed -i -e "s/version=6.1/version=6.1.1/g" core/linux-desktop/spkgbuild
+changelog "core/linux-desktop/spkgbuild" "Upgraded from version 6.1 to version 6.1.1"
+sed -i -e "s/version=6.1/version=6.1.1/g" core/linux-router/spkgbuild
+changelog "core/linux-router/spkgbuild" "Upgraded from version 6.1 to version 6.1.1"
+sed -i -e "s/version=3.5.12/version=3.5.13/g" extra/flexget/spkgbuild
+changelog "extra/flexget/spkgbuild" "Upgraded from version 3.5.12 to version 3.5.13"
+sed -i -e "s/version=9.19.7/version=9.19.8/g" networking/bind/spkgbuild
+changelog "networking/bind/spkgbuild" "Upgraded from version 9.19.7 to version 9.19.8"
+sed -i -e "s/version=109.0b4/version=109.0b5/g" networking/firefox/spkgbuild
+changelog "networking/firefox/spkgbuild" "Upgraded from version 109.0b4 to version 109.0b5"
+sed -i -e "s/version=1.0.5/version=1.0.6/g" networking/nftables/spkgbuild
+changelog "networking/nftables/spkgbuild" "Upgraded from version 1.0.5 to version 1.0.6"
+sed -i -e "s/version=7.4.3/version=7.4.4/g" python/python-pyface/spkgbuild
+changelog "python/python-pyface/spkgbuild" "Upgraded from version 7.4.3 to version 7.4.4"
+sed -i -e "s/version=0.44.0/version=0.45.0/g" python/python-zeroconf/spkgbuild
+changelog "python/python-zeroconf/spkgbuild" "Upgraded from version 0.44.0 to version 0.45.0"
+sed -i -e "s/version=2022.12.7/version=2022.12.8/g" server/homeassistant/spkgbuild
+changelog "server/homeassistant/spkgbuild" "Upgraded from version 2022.12.7 to version 2022.12.8"
+sed -i -e "s/version=1.10.0/version=1.10.1/g" xfce/xfce4-screenshooter/spkgbuild
+changelog "xfce/xfce4-screenshooter/spkgbuild" "Upgraded from version 1.10.0 to version 1.10.1"
