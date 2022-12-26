593f9ab79f02bc745ff0447262b11a486198490f
Voncloft <voncloft@gmail.com>
2022-12-26 09:10:56 -0500

    Minor Updates

---
+++ logs/old-test-updates/2022/Dec/applied_updates-12-26-22.sh
@@ -0,0 +1,50 @@
+sed -i -e "s/version=6.0.2/version=6.1/g" core/btrfs-progs/spkgbuild
+changelog "core/btrfs-progs/spkgbuild" "Upgraded from version 6.0.2 to version 6.1"
+sed -i -e "s/version=1.15.0/version=1.16.0/g" core/conky/spkgbuild
+changelog "core/conky/spkgbuild" "Upgraded from version 1.15.0 to version 1.16.0"
+sed -i -e "s/version=0.108.0/version=0.109.0/g" core/hugo/spkgbuild
+changelog "core/hugo/spkgbuild" "Upgraded from version 0.108.0 to version 0.109.0"
+sed -i -e "s/version=6.01/version=6.02/g" core/man-pages/spkgbuild
+changelog "core/man-pages/spkgbuild" "Upgraded from version 6.01 to version 6.02"
+sed -i -e "s/version=6.24.06/version=6.24.07/g" core/tcsh/spkgbuild
+changelog "core/tcsh/spkgbuild" "Upgraded from version 6.24.06 to version 6.24.07"
+sed -i -e "s/version=109.0b5/version=109.0b6/g" networking/firefox/spkgbuild
+changelog "networking/firefox/spkgbuild" "Upgraded from version 109.0b5 to version 109.0b6"
+sed -i -e "s/version=2.4.1/version=2.4.2/g" python/python-aiobotocore/spkgbuild
+changelog "python/python-aiobotocore/spkgbuild" "Upgraded from version 2.4.1 to version 2.4.2"
+sed -i -e "s/version=1.26.34/version=1.26.37/g" python/python-boto3/spkgbuild
+changelog "python/python-boto3/spkgbuild" "Upgraded from version 1.26.34 to version 1.26.37"
+sed -i -e "s/version=1.29.34/version=1.29.37/g" python/python-botocore/spkgbuild
+changelog "python/python-botocore/spkgbuild" "Upgraded from version 1.29.34 to version 1.29.37"
+sed -i -e "s/version=2.2.0/version=2.3.0/g" python/python-ciso8601/spkgbuild
+changelog "python/python-ciso8601/spkgbuild" "Upgraded from version 2.2.0 to version 2.3.0"
+sed -i -e "s/version=7.0.0/version=7.0.1/g" python/python-coverage/spkgbuild
+changelog "python/python-coverage/spkgbuild" "Upgraded from version 7.0.0 to version 7.0.1"
+sed -i -e "s/version=5.1.0/version=5.1.1/g" python/python-jupyter_core/spkgbuild
+changelog "python/python-jupyter_core/spkgbuild" "Upgraded from version 5.1.0 to version 5.1.1"
+sed -i -e "s/version=6.0.3/version=6.0.4/g" python/python-multidict/spkgbuild
+changelog "python/python-multidict/spkgbuild" "Upgraded from version 6.0.3 to version 6.0.4"
+sed -i -e "s/version=2.0.2/version=2.0.3/g" python/python-pallets-sphinx-themes/spkgbuild
+changelog "python/python-pallets-sphinx-themes/spkgbuild" "Upgraded from version 2.0.2 to version 2.0.3"
+sed -i -e "s/version=8.13.2/version=8.13.3/g" python/python-phonenumbers/spkgbuild
+changelog "python/python-phonenumbers/spkgbuild" "Upgraded from version 8.13.2 to version 8.13.3"
+sed -i -e "s/version=3.7.0/version=3.8.0/g" python/python-pytables/spkgbuild
+changelog "python/python-pytables/spkgbuild" "Upgraded from version 3.7.0 to version 3.8.0"
+sed -i -e "s/version=0.38.3/version=0.39.0/g" python/python-sqlalchemy-utils/spkgbuild
+changelog "python/python-sqlalchemy-utils/spkgbuild" "Upgraded from version 0.38.3 to version 0.39.0"
+sed -i -e "s/version=3.10.0/version=3.10.1/g" python/python-sqlobject/spkgbuild
+changelog "python/python-sqlobject/spkgbuild" "Upgraded from version 3.10.0 to version 3.10.1"
+sed -i -e "s/version=3.0.1/version=4.0.0/g" python/python-sybil/spkgbuild
+changelog "python/python-sybil/spkgbuild" "Upgraded from version 3.0.1 to version 4.0.0"
+sed -i -e "s/version=0.12.1/version=1.1.2/g" python/python-xcffib/spkgbuild
+changelog "python/python-xcffib/spkgbuild" "Upgraded from version 0.12.1 to version 1.1.2"
+sed -i -e "s/version=0.32/version=0.33/g" python/python-xlib/spkgbuild
+changelog "python/python-xlib/spkgbuild" "Upgraded from version 0.32 to version 0.33"
+sed -i -e "s/version=0.45.0/version=0.47.1/g" python/python-zeroconf/spkgbuild
+changelog "python/python-zeroconf/spkgbuild" "Upgraded from version 0.45.0 to version 0.47.1"
+sed -i -e "s/version=2.3.26/version=2.4.1/g" ruby-gems/ruby-bundler/spkgbuild
+changelog "ruby-gems/ruby-bundler/spkgbuild" "Upgraded from version 2.3.26 to version 2.4.1"
+sed -i -e "s/version=4.18.0/version=4.18.1/g" xfce/thunar/spkgbuild
+changelog "xfce/thunar/spkgbuild" "Upgraded from version 4.18.0 to version 4.18.1"
+sed -i -e "s/version=4.18.0/version=4.18.1/g" xfce/xfce4-settings/spkgbuild
+changelog "xfce/xfce4-settings/spkgbuild" "Upgraded from version 4.18.0 to version 4.18.1"
