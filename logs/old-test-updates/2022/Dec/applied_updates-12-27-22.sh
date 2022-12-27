a2cdc0665d71f329262c9946bb771f17cde9d043
Voncloft <voncloft@gmail.com>
2022-12-27 08:14:23 -0500

    Minor Updates

---
+++ logs/old-test-updates/2022/Dec/applied_updates-12-27-22.sh
@@ -0,0 +1,22 @@
+sed -i -e "s/version=1.16.0/version=1.16.1/g" core/conky/spkgbuild
+changelog "core/conky/spkgbuild" "Upgraded from version 1.16.0 to version 1.16.1"
+sed -i -e "s/version=5.43/version=5.44/g" core/file/spkgbuild
+changelog "core/file/spkgbuild" "Upgraded from version 5.43 to version 5.44"
+sed -i -e "s/version=4.96.4/version=4.96.5/g" core/lsof/spkgbuild
+changelog "core/lsof/spkgbuild" "Upgraded from version 4.96.4 to version 4.96.5"
+sed -i -e "s/version=3.5.13/version=3.5.14/g" extra/flexget/spkgbuild
+changelog "extra/flexget/spkgbuild" "Upgraded from version 3.5.13 to version 3.5.14"
+sed -i -e "s/version=0.21.1/version=0.21.2/g" libs/libpsl/spkgbuild
+changelog "libs/libpsl/spkgbuild" "Upgraded from version 0.21.1 to version 0.21.2"
+sed -i -e "s/version=2.16.0/version=2.17.0/g" libs/libva/spkgbuild
+changelog "libs/libva/spkgbuild" "Upgraded from version 2.16.0 to version 2.17.0"
+sed -i -e "s/version=0.4.0/version=0.4.1/g" python/python-edalize/spkgbuild
+changelog "python/python-edalize/spkgbuild" "Upgraded from version 0.4.0 to version 0.4.1"
+sed -i -e "s/version=6.19.4/version=6.20.0/g" python/python-ipykernel/spkgbuild
+changelog "python/python-ipykernel/spkgbuild" "Upgraded from version 6.19.4 to version 6.20.0"
+sed -i -e "s/version=2.20.0/version=2.21.0/g" python/python-pre-commit/spkgbuild
+changelog "python/python-pre-commit/spkgbuild" "Upgraded from version 2.20.0 to version 2.21.0"
+sed -i -e "s/version=2.2.2/version=2.2.3/g" python/python-requirementslib/spkgbuild
+changelog "python/python-requirementslib/spkgbuild" "Upgraded from version 2.2.2 to version 2.2.3"
+sed -i -e "s/version=3.11.44/version=3.11.45/g" server/blacklist-hosts/spkgbuild
+changelog "server/blacklist-hosts/spkgbuild" "Upgraded from version 3.11.44 to version 3.11.45"
