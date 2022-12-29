3c7571e4c8406f40aa85d8b1d7838fbd68f52de9
Voncloft <voncloft@gmail.com>
2022-12-29 10:10:37 -0500

    Minor Updates

---
+++ logs/old-test-updates/2022/Dec/applied_updates-12-29-22.sh
@@ -0,0 +1,30 @@
+sed -i -e "s/version=0.5.1/version=0.6.1/g" core/zita-alsa-pcmi/spkgbuild
+changelog "core/zita-alsa-pcmi/spkgbuild" "Upgraded from version 0.5.1 to version 0.6.1"
+sed -i -e "s/version=3.5.14/version=3.5.15/g" extra/flexget/spkgbuild
+changelog "extra/flexget/spkgbuild" "Upgraded from version 3.5.14 to version 3.5.15"
+sed -i -e "s/version=5.5.14/version=5.5.15/g" networking/whois/spkgbuild
+changelog "networking/whois/spkgbuild" "Upgraded from version 5.5.14 to version 5.5.15"
+sed -i -e "s/version=1.26.37/version=1.26.39/g" python/python-boto3/spkgbuild
+changelog "python/python-boto3/spkgbuild" "Upgraded from version 1.26.37 to version 1.26.39"
+sed -i -e "s/version=1.29.37/version=1.29.39/g" python/python-botocore/spkgbuild
+changelog "python/python-botocore/spkgbuild" "Upgraded from version 1.29.37 to version 1.29.39"
+sed -i -e "s/version=3.8.2/version=3.9.0/g" python/python-filelock/spkgbuild
+changelog "python/python-filelock/spkgbuild" "Upgraded from version 3.8.2 to version 3.9.0"
+sed -i -e "s/version=1.18.1/version=1.18.2/g" python/python-hcloud/spkgbuild
+changelog "python/python-hcloud/spkgbuild" "Upgraded from version 1.18.1 to version 1.18.2"
+sed -i -e "s/version=0.16.1/version=0.18.1/g" python/python-libtmux/spkgbuild
+changelog "python/python-libtmux/spkgbuild" "Upgraded from version 0.16.1 to version 0.18.1"
+sed -i -e "s/version=4.0.3/version=5.0.0/g" python/python-mock/spkgbuild
+changelog "python/python-mock/spkgbuild" "Upgraded from version 4.0.3 to version 5.0.0"
+sed -i -e "s/version=0.24.2/version=0.25.0/g" python/python-mongoengine/spkgbuild
+changelog "python/python-mongoengine/spkgbuild" "Upgraded from version 0.24.2 to version 0.25.0"
+sed -i -e "s/version=1.4.3/version=2.0.0/g" python/python-pyquery/spkgbuild
+changelog "python/python-pyquery/spkgbuild" "Upgraded from version 1.4.3 to version 2.0.0"
+sed -i -e "s/version=1.2.1/version=1.2.2/g" python/python-qtawesome/spkgbuild
+changelog "python/python-qtawesome/spkgbuild" "Upgraded from version 1.2.1 to version 1.2.2"
+sed -i -e "s/version=3.0.3/version=3.0.4/g" python/python-xlsxwriter/spkgbuild
+changelog "python/python-xlsxwriter/spkgbuild" "Upgraded from version 3.0.3 to version 3.0.4"
+sed -i -e "s/version=3.1.0/version=3.2.0/g" python/python-xxhash/spkgbuild
+changelog "python/python-xxhash/spkgbuild" "Upgraded from version 3.1.0 to version 3.2.0"
+sed -i -e "s/version=2022.12.8/version=2023.1.0/g" server/homeassistant/spkgbuild
+changelog "server/homeassistant/spkgbuild" "Upgraded from version 2022.12.8 to version 2023.1.0"
