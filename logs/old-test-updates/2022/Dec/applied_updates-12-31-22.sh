1d53bb9985658b3d1d2e73b41df1bb32834fb14c
Voncloft <voncloft@gmail.com>
2022-12-31 07:20:03 -0500

    Minor Updates

---
+++ logs/old-test-updates/2022/Dec/applied_updates-12-31-22.sh
@@ -0,0 +1,26 @@
+sed -i -e "s/version=20210715.151551.e7ad03a/version=20221229.172126/g" core/atomicparsley/spkgbuild
+changelog "core/atomicparsley/spkgbuild" "Upgraded from version 20210715.151551.e7ad03a to version 20221229.172126"
+sed -i -e "s/version=1.7.1/version=1.7.2/g" core/hunspell/spkgbuild
+changelog "core/hunspell/spkgbuild" "Upgraded from version 1.7.1 to version 1.7.2"
+sed -i -e "s/version=109.0b6/version=109.0b7/g" networking/firefox/spkgbuild
+changelog "networking/firefox/spkgbuild" "Upgraded from version 109.0b6 to version 109.0b7"
+sed -i -e "s/version=1.26.39/version=1.26.41/g" python/python-boto3/spkgbuild
+changelog "python/python-boto3/spkgbuild" "Upgraded from version 1.26.39 to version 1.26.41"
+sed -i -e "s/version=1.29.39/version=1.29.41/g" python/python-botocore/spkgbuild
+changelog "python/python-botocore/spkgbuild" "Upgraded from version 1.29.39 to version 1.29.41"
+sed -i -e "s/version=0.51.0/version=0.52.0/g" python/python-django-allauth/spkgbuild
+changelog "python/python-django-allauth/spkgbuild" "Upgraded from version 0.51.0 to version 0.52.0"
+sed -i -e "s/version=5.1.0/version=5.2.0/g" python/python-jaraco.test/spkgbuild
+changelog "python/python-jaraco.test/spkgbuild" "Upgraded from version 5.1.0 to version 5.2.0"
+sed -i -e "s/version=4.9.0/version=4.10.0/g" python/python-pwntools/spkgbuild
+changelog "python/python-pwntools/spkgbuild" "Upgraded from version 4.9.0 to version 4.10.0"
+sed -i -e "s/version=1.10.2/version=1.10.4/g" python/python-pydantic/spkgbuild
+changelog "python/python-pydantic/spkgbuild" "Upgraded from version 1.10.2 to version 1.10.4"
+sed -i -e "s/version=12.6.0/version=13.0.0/g" python/python-rich/spkgbuild
+changelog "python/python-rich/spkgbuild" "Upgraded from version 12.6.0 to version 13.0.0"
+sed -i -e "s/version=0.12.1/version=0.12.2/g" python/python-seaborn/spkgbuild
+changelog "python/python-seaborn/spkgbuild" "Upgraded from version 0.12.1 to version 0.12.2"
+sed -i -e "s/version=2.4.0/version=2.4.1/g" python/python-spyder-kernels/spkgbuild
+changelog "python/python-spyder-kernels/spkgbuild" "Upgraded from version 2.4.0 to version 2.4.1"
+sed -i -e "s/version=3.11.45/version=3.11.47/g" server/blacklist-hosts/spkgbuild
+changelog "server/blacklist-hosts/spkgbuild" "Upgraded from version 3.11.45 to version 3.11.47"
