df27859bb956c47647d9f6773370cd9cfdfc7372
Voncloft <voncloft@gmail.com>
2023-01-02 14:53:29 -0500

    Minor Updates

---
+++ logs/old-test-updates/2023/Jan/applied_updates-01-02-23.sh
@@ -0,0 +1,34 @@
+sed -i -e "s/version=1.16.1/version=1.17.0/g" core/conky/spkgbuild
+changelog "core/conky/spkgbuild" "Upgraded from version 1.16.1 to version 1.17.0"
+sed -i -e "s/version=6.1.1/version=6.1.2/g" core/linux-api-headers/spkgbuild
+changelog "core/linux-api-headers/spkgbuild" "Upgraded from version 6.1.1 to version 6.1.2"
+sed -i -e "s/version=6.1.1/version=6.1.2/g" core/linux-desktop/spkgbuild
+changelog "core/linux-desktop/spkgbuild" "Upgraded from version 6.1.1 to version 6.1.2"
+sed -i -e "s/version=6.1.1/version=6.1.2/g" core/linux-router/spkgbuild
+changelog "core/linux-router/spkgbuild" "Upgraded from version 6.1.1 to version 6.1.2"
+sed -i -e "s/version=6.3/version=6.4/g" core/ncurses/spkgbuild
+changelog "core/ncurses/spkgbuild" "Upgraded from version 6.3 to version 6.4"
+sed -i -e "s/version=1.10.1/version=1.10.2/g" libs/libpcap/spkgbuild
+changelog "libs/libpcap/spkgbuild" "Upgraded from version 1.10.1 to version 1.10.2"
+sed -i -e "s/version=22.12.0/version=23.01.0/g" libs/poppler/spkgbuild
+changelog "libs/poppler/spkgbuild" "Upgraded from version 22.12.0 to version 23.01.0"
+sed -i -e "s/version=2.14.10/version=2.14.12/g" networking/pidgin/spkgbuild
+changelog "networking/pidgin/spkgbuild" "Upgraded from version 2.14.10 to version 2.14.12"
+sed -i -e "s/version=4.99.1/version=4.99.2/g" networking/tcpdump/spkgbuild
+changelog "networking/tcpdump/spkgbuild" "Upgraded from version 4.99.1 to version 4.99.2"
+sed -i -e "s/version=5.2.0/version=6.0.0/g" python/python-importlib-metadata/spkgbuild
+changelog "python/python-importlib-metadata/spkgbuild" "Upgraded from version 5.2.0 to version 6.0.0"
+sed -i -e "s/version=5.2.0/version=5.3.0/g" python/python-jaraco.test/spkgbuild
+changelog "python/python-jaraco.test/spkgbuild" "Upgraded from version 5.2.0 to version 5.3.0"
+sed -i -e "s/version=0.18.1/version=0.18.2/g" python/python-libtmux/spkgbuild
+changelog "python/python-libtmux/spkgbuild" "Upgraded from version 0.18.1 to version 0.18.2"
+sed -i -e "s/version=1.1.5/version=2.0.0/g" python/python-pytest-relaxed/spkgbuild
+changelog "python/python-pytest-relaxed/spkgbuild" "Upgraded from version 1.1.5 to version 2.0.0"
+sed -i -e "s/version=2.2.0/version=2.2.1/g" python/python-watchdog/spkgbuild
+changelog "python/python-watchdog/spkgbuild" "Upgraded from version 2.2.0 to version 2.2.1"
+sed -i -e "s/version=3.0.4/version=3.0.5/g" python/python-xlsxwriter/spkgbuild
+changelog "python/python-xlsxwriter/spkgbuild" "Upgraded from version 3.0.4 to version 3.0.5"
+sed -i -e "s/version=6.2.1/version=7.0.0/g" python/python-zope-schema/spkgbuild
+changelog "python/python-zope-schema/spkgbuild" "Upgraded from version 6.2.1 to version 7.0.0"
+sed -i -e "s/version=3.11.47/version=3.11.48/g" server/blacklist-hosts/spkgbuild
+changelog "server/blacklist-hosts/spkgbuild" "Upgraded from version 3.11.47 to version 3.11.48"
