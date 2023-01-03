bb9bd642d95c3f289ac80983b948579091583ca6
Voncloft <voncloft@gmail.com>
2023-01-03 09:08:32 -0500

    Minor Updates

---
+++ logs/old-test-updates/2023/Jan/applied_updates-01-03-23.sh
@@ -0,0 +1,58 @@
+sed -i -e "s/version=4.21.1/version=4.22/g" core/i3/spkgbuild
+changelog "core/i3/spkgbuild" "Upgraded from version 4.21.1 to version 4.22"
+sed -i -e "s/version=3.5.15/version=3.5.16/g" extra/flexget/spkgbuild
+changelog "extra/flexget/spkgbuild" "Upgraded from version 3.5.15 to version 3.5.16"
+sed -i -e "s/version=1.2.0/version=1.2.1/g" lxqt/lxqt-panel/spkgbuild
+changelog "lxqt/lxqt-panel/spkgbuild" "Upgraded from version 1.2.0 to version 1.2.1"
+sed -i -e "s/version=1.2.0/version=1.2.1/g" libs/libfm-qt/spkgbuild
+changelog "libs/libfm-qt/spkgbuild" "Upgraded from version 1.2.0 to version 1.2.1"
+sed -i -e "s/version=1.4.1/version=1.5.0/g" libs/libxkbcommon/spkgbuild
+changelog "libs/libxkbcommon/spkgbuild" "Upgraded from version 1.4.1 to version 1.5.0"
+sed -i -e "s/version=2022.11.11/version=2023.01.02/g" networking/yt-dlp/spkgbuild
+changelog "networking/yt-dlp/spkgbuild" "Upgraded from version 2022.11.11 to version 2023.01.02"
+sed -i -e "s/version=7.0.1/version=7.0.2/g" python/python-coverage/spkgbuild
+changelog "python/python-coverage/spkgbuild" "Upgraded from version 7.0.1 to version 7.0.2"
+sed -i -e "s/version=5.1.1/version=5.1.2/g" python/python-jupyter_core/spkgbuild
+changelog "python/python-jupyter_core/spkgbuild" "Upgraded from version 5.1.1 to version 5.1.2"
+sed -i -e "s/version=2022.08/version=2022.12/g" python/python-litedram/spkgbuild
+changelog "python/python-litedram/spkgbuild" "Upgraded from version 2022.08 to version 2022.12"
+sed -i -e "s/version=2022.08/version=2022.12/g" python/python-liteeth/spkgbuild
+changelog "python/python-liteeth/spkgbuild" "Upgraded from version 2022.08 to version 2022.12"
+sed -i -e "s/version=2022.08/version=2022.12/g" python/python-liteiclink/spkgbuild
+changelog "python/python-liteiclink/spkgbuild" "Upgraded from version 2022.08 to version 2022.12"
+sed -i -e "s/version=2022.08/version=2022.12/g" python/python-litejesd204b/spkgbuild
+changelog "python/python-litejesd204b/spkgbuild" "Upgraded from version 2022.08 to version 2022.12"
+sed -i -e "s/version=2022.08/version=2022.12/g" python/python-litepcie/spkgbuild
+changelog "python/python-litepcie/spkgbuild" "Upgraded from version 2022.08 to version 2022.12"
+sed -i -e "s/version=2022.08/version=2022.12/g" python/python-litesata/spkgbuild
+changelog "python/python-litesata/spkgbuild" "Upgraded from version 2022.08 to version 2022.12"
+sed -i -e "s/version=2022.08/version=2022.12/g" python/python-litescope/spkgbuild
+changelog "python/python-litescope/spkgbuild" "Upgraded from version 2022.08 to version 2022.12"
+sed -i -e "s/version=2022.08/version=2022.12/g" python/python-litesdcard/spkgbuild
+changelog "python/python-litesdcard/spkgbuild" "Upgraded from version 2022.08 to version 2022.12"
+sed -i -e "s/version=2022.08/version=2022.12/g" python/python-liteusb/spkgbuild
+changelog "python/python-liteusb/spkgbuild" "Upgraded from version 2022.08 to version 2022.12"
+sed -i -e "s/version=2022.08/version=2022.12/g" python/python-litex/spkgbuild
+changelog "python/python-litex/spkgbuild" "Upgraded from version 2022.08 to version 2022.12"
+sed -i -e "s/version=2022.08/version=2022.12/g" python/python-litex-boards/spkgbuild
+changelog "python/python-litex-boards/spkgbuild" "Upgraded from version 2022.08 to version 2022.12"
+sed -i -e "s/version=2022.08/version=2022.12/g" python/python-pythondata-cpu-lm32/spkgbuild
+changelog "python/python-pythondata-cpu-lm32/spkgbuild" "Upgraded from version 2022.08 to version 2022.12"
+sed -i -e "s/version=2022.08/version=2022.12/g" python/python-pythondata-cpu-mor1kx/spkgbuild
+changelog "python/python-pythondata-cpu-mor1kx/spkgbuild" "Upgraded from version 2022.08 to version 2022.12"
+sed -i -e "s/version=2022.08/version=2022.12/g" python/python-pythondata-cpu-picorv32/spkgbuild
+changelog "python/python-pythondata-cpu-picorv32/spkgbuild" "Upgraded from version 2022.08 to version 2022.12"
+sed -i -e "s/version=2022.08/version=2022.12/g" python/python-pythondata-cpu-vexriscv/spkgbuild
+changelog "python/python-pythondata-cpu-vexriscv/spkgbuild" "Upgraded from version 2022.08 to version 2022.12"
+sed -i -e "s/version=2022.08/version=2022.12/g" python/python-pythondata-software-compiler_rt/spkgbuild
+changelog "python/python-pythondata-software-compiler_rt/spkgbuild" "Upgraded from version 2022.08 to version 2022.12"
+sed -i -e "s/version=5.1.0/version=5.2.0/g" python/python-tempora/spkgbuild
+changelog "python/python-tempora/spkgbuild" "Upgraded from version 5.1.0 to version 5.2.0"
+sed -i -e "s/version=7.0.0/version=7.0.1/g" python/python-zope-schema/spkgbuild
+changelog "python/python-zope-schema/spkgbuild" "Upgraded from version 7.0.0 to version 7.0.1"
+sed -i -e "s/version=1.2.0/version=1.2.1/g" qt/pcmanfm-qt/spkgbuild
+changelog "qt/pcmanfm-qt/spkgbuild" "Upgraded from version 1.2.0 to version 1.2.1"
+sed -i -e "s/version=2.4.1/version=2.4.2/g" ruby-gems/ruby-bundler/spkgbuild
+changelog "ruby-gems/ruby-bundler/spkgbuild" "Upgraded from version 2.4.1 to version 2.4.2"
+sed -i -e "s/version=1.10.1/version=1.10.2/g" xfce/xfce4-screenshooter/spkgbuild
+changelog "xfce/xfce4-screenshooter/spkgbuild" "Upgraded from version 1.10.1 to version 1.10.2"
