3949a3e428e757866323532baeda4b3664fd3a98
Voncloft <voncloft@gmail.com>
2023-01-05 05:33:14 -0500

    Minor Updates

---
+++ logs/old-test-updates/2023/Jan/applied_updates-01-05-23.sh
@@ -0,0 +1,46 @@
+sed -i -e "s/version=6.1.2/version=6.1.3/g" core/linux-api-headers/spkgbuild
+changelog "core/linux-api-headers/spkgbuild" "Upgraded from version 6.1.2 to version 6.1.3"
+sed -i -e "s/version=6.1.2/version=6.1.3/g" core/linux-desktop/spkgbuild
+changelog "core/linux-desktop/spkgbuild" "Upgraded from version 6.1.2 to version 6.1.3"
+sed -i -e "s/version=6.1.2/version=6.1.3/g" core/linux-router/spkgbuild
+changelog "core/linux-router/spkgbuild" "Upgraded from version 6.1.2 to version 6.1.3"
+sed -i -e "s/version=6.3.1/version=6.3.2/g" core/mercurial/spkgbuild
+changelog "core/mercurial/spkgbuild" "Upgraded from version 6.3.1 to version 6.3.2"
+sed -i -e "s/version=4.5.0/version=4.5.1/g" core/qalculate/spkgbuild
+changelog "core/qalculate/spkgbuild" "Upgraded from version 4.5.0 to version 4.5.1"
+sed -i -e "s/version=4.5.0/version=4.5.1/g" libs/libqalculate/spkgbuild
+changelog "libs/libqalculate/spkgbuild" "Upgraded from version 4.5.0 to version 4.5.1"
+sed -i -e "s/version=109.0b7/version=109.0b8/g" networking/firefox/spkgbuild
+changelog "networking/firefox/spkgbuild" "Upgraded from version 109.0b7 to version 109.0b8"
+sed -i -e "s/version=109.0b2/version=109.0b3/g" networking/thunderbird/spkgbuild
+changelog "networking/thunderbird/spkgbuild" "Upgraded from version 109.0b2 to version 109.0b3"
+sed -i -e "s/version=1.26.42/version=1.26.43/g" python/python-boto3/spkgbuild
+changelog "python/python-boto3/spkgbuild" "Upgraded from version 1.26.42 to version 1.26.43"
+sed -i -e "s/version=1.29.42/version=1.29.43/g" python/python-botocore/spkgbuild
+changelog "python/python-botocore/spkgbuild" "Upgraded from version 1.29.42 to version 1.29.43"
+sed -i -e "s/version=0.72.5/version=0.72.6/g" python/python-cfn-lint/spkgbuild
+changelog "python/python-cfn-lint/spkgbuild" "Upgraded from version 0.72.5 to version 0.72.6"
+sed -i -e "s/version=7.0.2/version=7.0.3/g" python/python-coverage/spkgbuild
+changelog "python/python-coverage/spkgbuild" "Upgraded from version 7.0.2 to version 7.0.3"
+sed -i -e "s/version=2.70.0/version=2.71.0/g" python/python-google-api-python-client/spkgbuild
+changelog "python/python-google-api-python-client/spkgbuild" "Upgraded from version 2.70.0 to version 2.71.0"
+sed -i -e "s/version=1.1.2/version=1.2.0/g" python/python-kikit/spkgbuild
+changelog "python/python-kikit/spkgbuild" "Upgraded from version 1.1.2 to version 1.2.0"
+sed -i -e "s/version=1.8.0/version=1.9.0/g" python/python-lazy-object-proxy/spkgbuild
+changelog "python/python-lazy-object-proxy/spkgbuild" "Upgraded from version 1.8.0 to version 1.9.0"
+sed -i -e "s/version=8.2.0/version=8.2.1/g" python/python-neutronclient/spkgbuild
+changelog "python/python-neutronclient/spkgbuild" "Upgraded from version 8.2.0 to version 8.2.1"
+sed -i -e "s/version=3.8.3/version=3.8.4/g" python/python-orjson/spkgbuild
+changelog "python/python-orjson/spkgbuild" "Upgraded from version 3.8.3 to version 3.8.4"
+sed -i -e "s/version=1.3.13/version=1.3.14/g" python/python-sqlalchemy-continuum/spkgbuild
+changelog "python/python-sqlalchemy-continuum/spkgbuild" "Upgraded from version 1.3.13 to version 1.3.14"
+sed -i -e "s/version=5.15.7/version=5.15.8/g" qt/qt5/spkgbuild
+changelog "qt/qt5/spkgbuild" "Upgraded from version 5.15.7 to version 5.15.8"
+sed -i -e "s/version=5.15.7/version=5.15.8/g" qt/qt5-multimedia/spkgbuild
+changelog "qt/qt5-multimedia/spkgbuild" "Upgraded from version 5.15.7 to version 5.15.8"
+sed -i -e "s/version=5.15.7/version=5.15.8/g" qt/qt5-speech/spkgbuild
+changelog "qt/qt5-speech/spkgbuild" "Upgraded from version 5.15.7 to version 5.15.8"
+sed -i -e "s/version=5.15.7/version=5.15.8/g" qt/qt5-x11extras/spkgbuild
+changelog "qt/qt5-x11extras/spkgbuild" "Upgraded from version 5.15.7 to version 5.15.8"
+sed -i -e "s/version=5.15.7/version=5.15.8/g" qt/qtsql/spkgbuild
+changelog "qt/qtsql/spkgbuild" "Upgraded from version 5.15.7 to version 5.15.8"
