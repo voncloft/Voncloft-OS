f3a2b52179d589a408f42b029506e23271962e55
Voncloft <voncloft@gmail.com>
2023-01-06 17:20:30 -0500

    Minor Updates

---
+++ logs/old-test-updates/2023/Jan/applied_updates-01-06-23.sh
@@ -0,0 +1,34 @@
+sed -i -e "s/version=3.86/version=3.87/g" core/nss/spkgbuild
+changelog "core/nss/spkgbuild" "Upgraded from version 3.86 to version 3.87"
+sed -i -e "s/version=3.7.12/version=3.7.13/g" core/recode/spkgbuild
+changelog "core/recode/spkgbuild" "Upgraded from version 3.7.12 to version 3.7.13"
+sed -i -e "s/version=109.0b8/version=109.0b9/g" networking/firefox/spkgbuild
+changelog "networking/firefox/spkgbuild" "Upgraded from version 109.0b8 to version 109.0b9"
+sed -i -e "s/version=2.12.13/version=2.12.14/g" python/python-astroid/spkgbuild
+changelog "python/python-astroid/spkgbuild" "Upgraded from version 2.12.13 to version 2.12.14"
+sed -i -e "s/version=1.55.0/version=1.56.0/g" python/python-aws-sam-translator/spkgbuild
+changelog "python/python-aws-sam-translator/spkgbuild" "Upgraded from version 1.55.0 to version 1.56.0"
+sed -i -e "s/version=1.26.44/version=1.26.45/g" python/python-boto3/spkgbuild
+changelog "python/python-boto3/spkgbuild" "Upgraded from version 1.26.44 to version 1.26.45"
+sed -i -e "s/version=1.29.44/version=1.29.45/g" python/python-botocore/spkgbuild
+changelog "python/python-botocore/spkgbuild" "Upgraded from version 1.29.44 to version 1.29.45"
+sed -i -e "s/version=1.2.65/version=1.2.67/g" python/python-cloudscraper/spkgbuild
+changelog "python/python-cloudscraper/spkgbuild" "Upgraded from version 1.2.65 to version 1.2.67"
+sed -i -e "s/version=6.61.0/version=6.61.1/g" python/python-hypothesis/spkgbuild
+changelog "python/python-hypothesis/spkgbuild" "Upgraded from version 6.61.0 to version 6.61.1"
+sed -i -e "s/version=3.0.2/version=3.2.0/g" python/python-internetarchive/spkgbuild
+changelog "python/python-internetarchive/spkgbuild" "Upgraded from version 3.0.2 to version 3.2.0"
+sed -i -e "s/version=1.6.0/version=1.6.1/g" python/python-reedsolo/spkgbuild
+changelog "python/python-reedsolo/spkgbuild" "Upgraded from version 1.6.0 to version 1.6.1"
+sed -i -e "s/version=13.0.0/version=13.0.1/g" python/python-rich/spkgbuild
+changelog "python/python-rich/spkgbuild" "Upgraded from version 13.0.0 to version 13.0.1"
+sed -i -e "s/version=5.6.0/version=5.7.0/g" python/python-ujson/spkgbuild
+changelog "python/python-ujson/spkgbuild" "Upgraded from version 5.6.0 to version 5.7.0"
+sed -i -e "s/version=2.2.0/version=2.3.0/g" python/python-yaspin/spkgbuild
+changelog "python/python-yaspin/spkgbuild" "Upgraded from version 2.2.0 to version 2.3.0"
+sed -i -e "s/version=5.15.11/version=5.15.12/g" qt/qtwebengine/spkgbuild
+changelog "qt/qtwebengine/spkgbuild" "Upgraded from version 5.15.11 to version 5.15.12"
+sed -i -e "s/version=2.4.2/version=2.4.3/g" ruby-gems/ruby-bundler/spkgbuild
+changelog "ruby-gems/ruby-bundler/spkgbuild" "Upgraded from version 2.4.2 to version 2.4.3"
+sed -i -e "s/version=2023.1.0/version=2023.1.1/g" server/homeassistant/spkgbuild
+changelog "server/homeassistant/spkgbuild" "Upgraded from version 2023.1.0 to version 2023.1.1"
