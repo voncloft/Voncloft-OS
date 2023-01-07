447c6998ec9eaacac25d5d3c427bdb9220c6c019
Voncloft <voncloft@gmail.com>
2023-01-07 17:57:14 -0500

    Minor Updates

---
+++ logs/old-test-updates/2023/Jan/applied_updates-01-07-23.sh
@@ -0,0 +1,38 @@
+sed -i -e "s/version=5.6.0/version=5.6.1/g" cinnamon/cinnamon-control-center/spkgbuild
+changelog "cinnamon/cinnamon-control-center/spkgbuild" "Upgraded from version 5.6.0 to version 5.6.1"
+sed -i -e "s/version=5.6.0/version=5.6.1/g" cinnamon/cinnamon-desktop/spkgbuild
+changelog "cinnamon/cinnamon-desktop/spkgbuild" "Upgraded from version 5.6.0 to version 5.6.1"
+sed -i -e "s/version=5.6.2/version=5.6.3/g" cinnamon/cinnamon-screensaver/spkgbuild
+changelog "cinnamon/cinnamon-screensaver/spkgbuild" "Upgraded from version 5.6.2 to version 5.6.3"
+sed -i -e "s/version=5.6.0/version=5.6.1/g" cinnamon/cinnamon-settings-daemon/spkgbuild
+changelog "cinnamon/cinnamon-settings-daemon/spkgbuild" "Upgraded from version 5.6.0 to version 5.6.1"
+sed -i -e "s/version=5.6.2/version=5.6.3/g" cinnamon/muffin/spkgbuild
+changelog "cinnamon/muffin/spkgbuild" "Upgraded from version 5.6.2 to version 5.6.3"
+sed -i -e "s/version=5.6.1/version=5.6.2/g" cinnamon/nemo/spkgbuild
+changelog "cinnamon/nemo/spkgbuild" "Upgraded from version 5.6.1 to version 5.6.2"
+sed -i -e "s/version=6.1.3/version=6.1.4/g" core/linux-api-headers/spkgbuild
+changelog "core/linux-api-headers/spkgbuild" "Upgraded from version 6.1.3 to version 6.1.4"
+sed -i -e "s/version=6.1.3/version=6.1.4/g" core/linux-desktop/spkgbuild
+changelog "core/linux-desktop/spkgbuild" "Upgraded from version 6.1.3 to version 6.1.4"
+sed -i -e "s/version=6.1.3/version=6.1.4/g" core/linux-router/spkgbuild
+changelog "core/linux-router/spkgbuild" "Upgraded from version 6.1.3 to version 6.1.4"
+sed -i -e "s/version=2023.01.02/version=2023.01.06/g" networking/yt-dlp/spkgbuild
+changelog "networking/yt-dlp/spkgbuild" "Upgraded from version 2023.01.02 to version 2023.01.06"
+sed -i -e "s/version=2.12.14/version=2.13.0/g" python/python-astroid/spkgbuild
+changelog "python/python-astroid/spkgbuild" "Upgraded from version 2.12.14 to version 2.13.0"
+sed -i -e "s/version=0.88.0/version=0.89.0/g" python/python-fastapi/spkgbuild
+changelog "python/python-fastapi/spkgbuild" "Upgraded from version 0.88.0 to version 0.89.0"
+sed -i -e "s/version=6.61.1/version=6.61.2/g" python/python-hypothesis/spkgbuild
+changelog "python/python-hypothesis/spkgbuild" "Upgraded from version 6.61.1 to version 6.61.2"
+sed -i -e "s/version=1.1.1/version=2.0.0/g" python/python-iniconfig/spkgbuild
+changelog "python/python-iniconfig/spkgbuild" "Upgraded from version 1.1.1 to version 2.0.0"
+sed -i -e "s/version=0.18.2/version=0.19.1/g" python/python-libtmux/spkgbuild
+changelog "python/python-libtmux/spkgbuild" "Upgraded from version 0.18.2 to version 0.19.1"
+sed -i -e "s/version=12.3.0/version=12.3.1/g" python/python-pdoc/spkgbuild
+changelog "python/python-pdoc/spkgbuild" "Upgraded from version 12.3.0 to version 12.3.1"
+sed -i -e "s/version=3.12.1/version=3.12.2/g" ruby-gems/ruby-rspec-expectations/spkgbuild
+changelog "ruby-gems/ruby-rspec-expectations/spkgbuild" "Upgraded from version 3.12.1 to version 3.12.2"
+sed -i -e "s/version=3.12.1/version=3.12.2/g" ruby-gems/ruby-rspec-mocks/spkgbuild
+changelog "ruby-gems/ruby-rspec-mocks/spkgbuild" "Upgraded from version 3.12.1 to version 3.12.2"
+sed -i -e "s/version=3.11.48/version=3.11.49/g" server/blacklist-hosts/spkgbuild
+changelog "server/blacklist-hosts/spkgbuild" "Upgraded from version 3.11.48 to version 3.11.49"
