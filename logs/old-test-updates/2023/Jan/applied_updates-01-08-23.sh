260eb22f74ace1d85f51de416a9376f5d7a1891b
Voncloft <voncloft@gmail.com>
2023-01-08 20:11:00 -0500

    Minor Updates

---
+++ logs/old-test-updates/2023/Jan/applied_updates-01-08-23.sh
@@ -0,0 +1,16 @@
+sed -i -e "s/version=3.7.1/version=3.8/g" core/weechat/spkgbuild
+changelog "core/weechat/spkgbuild" "Upgraded from version 3.7.1 to version 3.8"
+sed -i -e "s/version=2.36.4/version=2.37/g" extra/discordchatexporter/spkgbuild
+changelog "extra/discordchatexporter/spkgbuild" "Upgraded from version 2.36.4 to version 2.37"
+sed -i -e "s/version=2.13.0/version=2.13.2/g" python/python-astroid/spkgbuild
+changelog "python/python-astroid/spkgbuild" "Upgraded from version 2.13.0 to version 2.13.2"
+sed -i -e "s/version=7.0.3/version=7.0.4/g" python/python-coverage/spkgbuild
+changelog "python/python-coverage/spkgbuild" "Upgraded from version 7.0.3 to version 7.0.4"
+sed -i -e "s/version=6.61.2/version=6.62.0/g" python/python-hypothesis/spkgbuild
+changelog "python/python-hypothesis/spkgbuild" "Upgraded from version 6.61.2 to version 6.62.0"
+sed -i -e "s/version=22.0/version=23.0/g" python/python-packaging/spkgbuild
+changelog "python/python-packaging/spkgbuild" "Upgraded from version 22.0 to version 23.0"
+sed -i -e "s/version=6.2.2/version=6.2.3/g" python/python-pydocstyle/spkgbuild
+changelog "python/python-pydocstyle/spkgbuild" "Upgraded from version 6.2.2 to version 6.2.3"
+sed -i -e "s/version=3.11.49/version=3.11.50/g" server/blacklist-hosts/spkgbuild
+changelog "server/blacklist-hosts/spkgbuild" "Upgraded from version 3.11.49 to version 3.11.50"
