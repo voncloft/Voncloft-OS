648b96fe9f992dfdf867daa46aed262370238207
Voncloft <voncloft@gmail.com>
2023-01-09 22:02:34 -0500

    woops for sddm

--- REPOS/displaym/sddm/post-install.sh
+++ REPOS/displaym/sddm/post-install.sh
@@ -1,1 +1,13 @@
+sed -e '/ServerPath/ s|usr|opt/xorg|' \
+    -i.orig /etc/sddm.conf
+
+sed -e 's/-nolisten tcp//'\
+    -i /etc/sddm.conf
+
+sed -e 's/\"none\"/\"on\"/' \
+    -i /etc/sddm.conf
+
+scratch install -y -c sddm-boot
+
+cp -v /etc/inittab{,-orig}
 sed -i '/initdefault/ s/3/5/' /etc/inittab
