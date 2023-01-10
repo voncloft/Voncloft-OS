c45ec4748a6371127245a217e4a69815980b3a4c
Voncloft <voncloft@gmail.com>
2023-01-10 17:36:24 -0500

    Minor Updates

--- REPOS/networking/openssh/post-install.sh
+++ REPOS/networking/openssh/post-install.sh
@@ -5,3 +5,4 @@ chmod 644 /etc/pam.d/sshd &&
 echo "UsePAM yes" >> /etc/ssh/sshd_config
 scratch install -y openssh-boot
 rm rf /etc/ssh/sshd_config.spkgnew
+/etc/init.d/sshd start
---
+++ logs/old-test-updates/2023/Jan/applied_updates-01-10-23.sh
@@ -0,0 +1,74 @@
+sed -i -e "s/version=1.10.12/version=1.10.13/g" compilers/apache-ant/spkgbuild
+changelog "compilers/apache-ant/spkgbuild" "Upgraded from version 1.10.12 to version 1.10.13"
+sed -i -e "s/version=2.11.1/version=2.11.2/g" core/man-db/spkgbuild
+changelog "core/man-db/spkgbuild" "Upgraded from version 2.11.1 to version 2.11.2"
+sed -i -e "s/version=1.7/version=1.8/g" core/scrot/spkgbuild
+changelog "core/scrot/spkgbuild" "Upgraded from version 1.7 to version 1.8"
+sed -i -e "s/version=2.37/version=2.37.1/g" extra/discordchatexporter/spkgbuild
+changelog "extra/discordchatexporter/spkgbuild" "Upgraded from version 2.37 to version 2.37.1"
+sed -i -e "s/version=4.13/version=4.900/g" extra/gnucash/spkgbuild
+changelog "extra/gnucash/spkgbuild" "Upgraded from version 4.13 to version 4.900"
+sed -i -e "s/version=0.13.0/version=0.13.1/g" libs/hidapi/spkgbuild
+changelog "libs/hidapi/spkgbuild" "Upgraded from version 0.13.0 to version 0.13.1"
+sed -i -e "s/version=1.10.4/version=1.10.6/g" libs/libnma/spkgbuild
+changelog "libs/libnma/spkgbuild" "Upgraded from version 1.10.4 to version 1.10.6"
+sed -i -e "s/version=0.2.11/version=0.2.12/g" libs/libspectre/spkgbuild
+changelog "libs/libspectre/spkgbuild" "Upgraded from version 0.2.11 to version 0.2.12"
+sed -i -e "s/version=4.18.0/version=4.18.1/g" libs/libxfce4util/spkgbuild
+changelog "libs/libxfce4util/spkgbuild" "Upgraded from version 4.18.0 to version 4.18.1"
+sed -i -e "s/version=109.0b3/version=109.0b4/g" networking/thunderbird/spkgbuild
+changelog "networking/thunderbird/spkgbuild" "Upgraded from version 109.0b3 to version 109.0b4"
+sed -i -e "s/version=1.26.45/version=1.26.47/g" python/python-boto3/spkgbuild
+changelog "python/python-boto3/spkgbuild" "Upgraded from version 1.26.45 to version 1.26.47"
+sed -i -e "s/version=1.29.45/version=1.29.47/g" python/python-botocore/spkgbuild
+changelog "python/python-botocore/spkgbuild" "Upgraded from version 1.29.45 to version 1.29.47"
+sed -i -e "s/version=5.2.0/version=5.2.1/g" python/python-cachetools/spkgbuild
+changelog "python/python-cachetools/spkgbuild" "Upgraded from version 5.2.0 to version 5.2.1"
+sed -i -e "s/version=0.72.6/version=0.72.7/g" python/python-cfn-lint/spkgbuild
+changelog "python/python-cfn-lint/spkgbuild" "Upgraded from version 0.72.6 to version 0.72.7"
+sed -i -e "s/version=1.2.67/version=1.2.68/g" python/python-cloudscraper/spkgbuild
+changelog "python/python-cloudscraper/spkgbuild" "Upgraded from version 1.2.67 to version 1.2.68"
+sed -i -e "s/version=8.5.3/version=8.6.0/g" python/python-elasticsearch/spkgbuild
+changelog "python/python-elasticsearch/spkgbuild" "Upgraded from version 8.5.3 to version 8.6.0"
+sed -i -e "s/version=15.3.4/version=16.3.0/g" python/python-faker/spkgbuild
+changelog "python/python-faker/spkgbuild" "Upgraded from version 15.3.4 to version 16.3.0"
+sed -i -e "s/version=0.89.0/version=0.89.1/g" python/python-fastapi/spkgbuild
+changelog "python/python-fastapi/spkgbuild" "Upgraded from version 0.89.0 to version 0.89.1"
+sed -i -e "s/version=2.71.0/version=2.72.0/g" python/python-google-api-python-client/spkgbuild
+changelog "python/python-google-api-python-client/spkgbuild" "Upgraded from version 2.71.0 to version 2.72.0"
+sed -i -e "s/version=2.15.0/version=2.16.0/g" python/python-google-auth/spkgbuild
+changelog "python/python-google-auth/spkgbuild" "Upgraded from version 2.15.0 to version 2.16.0"
+sed -i -e "s/version=6.20.0/version=6.20.1/g" python/python-ipykernel/spkgbuild
+changelog "python/python-ipykernel/spkgbuild" "Upgraded from version 6.20.0 to version 6.20.1"
+sed -i -e "s/version=5.1.2/version=5.1.3/g" python/python-jupyter_core/spkgbuild
+changelog "python/python-jupyter_core/spkgbuild" "Upgraded from version 5.1.2 to version 5.1.3"
+sed -i -e "s/version=5.0.0/version=5.0.1/g" python/python-mock/spkgbuild
+changelog "python/python-mock/spkgbuild" "Upgraded from version 5.0.0 to version 5.0.1"
+sed -i -e "s/version=3.22.0/version=3.23.0/g" python/python-openmdao/spkgbuild
+changelog "python/python-openmdao/spkgbuild" "Upgraded from version 3.22.0 to version 3.23.0"
+sed -i -e "s/version=3.8.4/version=3.8.5/g" python/python-orjson/spkgbuild
+changelog "python/python-orjson/spkgbuild" "Upgraded from version 3.8.4 to version 3.8.5"
+sed -i -e "s/version=4.9.3/version=5.0/g" python/python-persistent/spkgbuild
+changelog "python/python-persistent/spkgbuild" "Upgraded from version 4.9.3 to version 5.0"
+sed -i -e "s/version=8.13.3/version=8.13.4/g" python/python-phonenumbers/spkgbuild
+changelog "python/python-phonenumbers/spkgbuild" "Upgraded from version 8.13.3 to version 8.13.4"
+sed -i -e "s/version=1.3.1/version=1.3.2/g" python/python-poetry/spkgbuild
+changelog "python/python-poetry/spkgbuild" "Upgraded from version 1.3.1 to version 1.3.2"
+sed -i -e "s/version=1.0.2/version=1.1.0/g" python/python-pook/spkgbuild
+changelog "python/python-pook/spkgbuild" "Upgraded from version 1.0.2 to version 1.1.0"
+sed -i -e "s/version=0.0.10/version=0.0.11/g" python/python-tox-current-env/spkgbuild
+changelog "python/python-tox-current-env/spkgbuild" "Upgraded from version 0.0.10 to version 0.0.11"
+sed -i -e "s/version=2.13.3/version=3.0.0/g" python/python-typeguard/spkgbuild
+changelog "python/python-typeguard/spkgbuild" "Upgraded from version 2.13.3 to version 3.0.0"
+sed -i -e "s/version=1.1.2/version=1.2.0/g" python/python-xcffib/spkgbuild
+changelog "python/python-xcffib/spkgbuild" "Upgraded from version 1.1.2 to version 1.2.0"
+sed -i -e "s/version=2023.1.1/version=2023.1.3/g" server/homeassistant/spkgbuild
+changelog "server/homeassistant/spkgbuild" "Upgraded from version 2023.1.1 to version 2023.1.3"
+sed -i -e "s/version=1.30.0.6486/version=1.30.1.6562/g" server/plex-media-server/spkgbuild
+changelog "server/plex-media-server/spkgbuild" "Upgraded from version 1.30.0.6486 to version 1.30.1.6562"
+sed -i -e "s/version=4.18.1/version=4.18.2/g" xfce/thunar/spkgbuild
+changelog "xfce/thunar/spkgbuild" "Upgraded from version 4.18.1 to version 4.18.2"
+sed -i -e "s/version=4.18.0/version=4.19.0/g" xfce/xfce4-panel/spkgbuild
+changelog "xfce/xfce4-panel/spkgbuild" "Upgraded from version 4.18.0 to version 4.19.0"
+sed -i -e "s/version=4.18.0/version=4.19.0/g" xfce/xfdesktop/spkgbuild
+changelog "xfce/xfdesktop/spkgbuild" "Upgraded from version 4.18.0 to version 4.19.0"
