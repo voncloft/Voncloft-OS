0b0710276074d74cd0378ec2bb006b04ec10fd7d
Voncloft <voncloft@gmail.com>
2023-01-11 18:14:32 -0500

    Minor Updates

---
+++ logs/old-test-updates/2023/Jan/applied_updates-01-11-23.sh
@@ -0,0 +1,20 @@
+sed -i -e "s/version=5.6.5/version=5.6.6/g" cinnamon/cinnamon/spkgbuild
+changelog "cinnamon/cinnamon/spkgbuild" "Upgraded from version 5.6.5 to version 5.6.6"
+sed -i -e "s/version=1.66.0/version=1.66.1/g" compilers/rust/spkgbuild
+changelog "compilers/rust/spkgbuild" "Upgraded from version 1.66.0 to version 1.66.1"
+sed -i -e "s/version=5.4.0/version=5.4.1/g" core/xz/spkgbuild
+changelog "core/xz/spkgbuild" "Upgraded from version 5.4.0 to version 5.4.1"
+sed -i -e "s/version=1.26.47/version=1.26.48/g" python/python-boto3/spkgbuild
+changelog "python/python-boto3/spkgbuild" "Upgraded from version 1.26.47 to version 1.26.48"
+sed -i -e "s/version=1.29.47/version=1.29.48/g" python/python-botocore/spkgbuild
+changelog "python/python-botocore/spkgbuild" "Upgraded from version 1.29.47 to version 1.29.48"
+sed -i -e "s/version=4.17.1/version=4.18.0/g" python/python-braintree/spkgbuild
+changelog "python/python-braintree/spkgbuild" "Upgraded from version 4.17.1 to version 4.18.0"
+sed -i -e "s/version=7.0.4/version=7.0.5/g" python/python-coverage/spkgbuild
+changelog "python/python-coverage/spkgbuild" "Upgraded from version 7.0.4 to version 7.0.5"
+sed -i -e "s/version=5.11.0/version=5.11.1/g" python/python-pbr/spkgbuild
+changelog "python/python-pbr/spkgbuild" "Upgraded from version 5.11.0 to version 5.11.1"
+sed -i -e "s/version=65.6.3/version=65.7.0/g" python/python-setuptools/spkgbuild
+changelog "python/python-setuptools/spkgbuild" "Upgraded from version 65.6.3 to version 65.7.0"
+sed -i -e "s/version=3.17.1/version=3.18.0/g" python/python-trimesh/spkgbuild
+changelog "python/python-trimesh/spkgbuild" "Upgraded from version 3.17.1 to version 3.18.0"
