e1fb25666618f2d1fa3a40a28b6c5f7118de4580
Voncloft <voncloft@gmail.com>
2023-01-14 15:41:48 -0500

    Minor Updates

---
+++ logs/old-test-updates/2023/Jan/applied_updates-01-14-23.sh
@@ -0,0 +1,226 @@
+sed -i -e "s/version=2.39/version=2.40/g" core/binutils/spkgbuild
+changelog "core/binutils/spkgbuild" "Upgraded from version 2.39 to version 2.40"
+sed -i -e "s/version=3.12.0/version=3.13.0/g" core/fuse2/spkgbuild
+changelog "core/fuse2/spkgbuild" "Upgraded from version 3.12.0 to version 3.13.0"
+sed -i -e "s/version=3.12.0/version=3.13.0/g" core/fuse3/spkgbuild
+changelog "core/fuse3/spkgbuild" "Upgraded from version 3.12.0 to version 3.13.0"
+sed -i -e "s/version=6.1.5/version=6.1.6/g" core/linux-api-headers/spkgbuild
+changelog "core/linux-api-headers/spkgbuild" "Upgraded from version 6.1.5 to version 6.1.6"
+sed -i -e "s/version=6.1.5/version=6.1.6/g" core/linux-desktop/spkgbuild
+changelog "core/linux-desktop/spkgbuild" "Upgraded from version 6.1.5 to version 6.1.6"
+sed -i -e "s/version=6.1.5/version=6.1.6/g" core/linux-router/spkgbuild
+changelog "core/linux-router/spkgbuild" "Upgraded from version 6.1.5 to version 6.1.6"
+sed -i -e "s/version=4.8.28/version=4.8.29/g" core/mc/spkgbuild
+changelog "core/mc/spkgbuild" "Upgraded from version 4.8.28 to version 4.8.29"
+sed -i -e "s/version=5.101.0/version=5.102.0/g" extra/extra-cmake-modules/spkgbuild
+changelog "extra/extra-cmake-modules/spkgbuild" "Upgraded from version 5.101.0 to version 5.102.0"
+sed -i -e "s/version=5.101.0/version=5.102.0/g" kf5/attica/spkgbuild
+changelog "kf5/attica/spkgbuild" "Upgraded from version 5.101.0 to version 5.102.0"
+sed -i -e "s/version=5.101.0/version=5.102.0/g" kf5/baloo/spkgbuild
+changelog "kf5/baloo/spkgbuild" "Upgraded from version 5.101.0 to version 5.102.0"
+sed -i -e "s/version=5.101.0/version=5.102.0/g" kf5/breeze-icons/spkgbuild
+changelog "kf5/breeze-icons/spkgbuild" "Upgraded from version 5.101.0 to version 5.102.0"
+sed -i -e "s/version=5.101.0/version=5.102.0/g" kf5/frameworkintegration/spkgbuild
+changelog "kf5/frameworkintegration/spkgbuild" "Upgraded from version 5.101.0 to version 5.102.0"
+sed -i -e "s/version=5.101.0/version=5.102.0/g" kf5/kactivities/spkgbuild
+changelog "kf5/kactivities/spkgbuild" "Upgraded from version 5.101.0 to version 5.102.0"
+sed -i -e "s/version=5.101.0/version=5.102.0/g" kf5/kactivities-stats/spkgbuild
+changelog "kf5/kactivities-stats/spkgbuild" "Upgraded from version 5.101.0 to version 5.102.0"
+sed -i -e "s/version=5.101.0/version=5.102.0/g" kf5/kapidox/spkgbuild
+changelog "kf5/kapidox/spkgbuild" "Upgraded from version 5.101.0 to version 5.102.0"
+sed -i -e "s/version=5.101.0/version=5.102.0/g" kf5/karchive/spkgbuild
+changelog "kf5/karchive/spkgbuild" "Upgraded from version 5.101.0 to version 5.102.0"
+sed -i -e "s/version=5.101.0/version=5.102.0/g" kf5/kauth/spkgbuild
+changelog "kf5/kauth/spkgbuild" "Upgraded from version 5.101.0 to version 5.102.0"
+sed -i -e "s/version=5.101.0/version=5.102.0/g" kf5/kbookmarks/spkgbuild
+changelog "kf5/kbookmarks/spkgbuild" "Upgraded from version 5.101.0 to version 5.102.0"
+sed -i -e "s/version=5.101.0/version=5.102.0/g" kf5/kcalendarcore/spkgbuild
+changelog "kf5/kcalendarcore/spkgbuild" "Upgraded from version 5.101.0 to version 5.102.0"
+sed -i -e "s/version=5.101.0/version=5.102.0/g" kf5/kcmutils/spkgbuild
+changelog "kf5/kcmutils/spkgbuild" "Upgraded from version 5.101.0 to version 5.102.0"
+sed -i -e "s/version=5.101.0/version=5.102.0/g" kf5/kcodecs/spkgbuild
+changelog "kf5/kcodecs/spkgbuild" "Upgraded from version 5.101.0 to version 5.102.0"
+sed -i -e "s/version=5.101.0/version=5.102.0/g" kf5/kcompletion/spkgbuild
+changelog "kf5/kcompletion/spkgbuild" "Upgraded from version 5.101.0 to version 5.102.0"
+sed -i -e "s/version=5.101.0/version=5.102.0/g" kf5/kconfig/spkgbuild
+changelog "kf5/kconfig/spkgbuild" "Upgraded from version 5.101.0 to version 5.102.0"
+sed -i -e "s/version=5.101.0/version=5.102.0/g" kf5/kconfigwidgets/spkgbuild
+changelog "kf5/kconfigwidgets/spkgbuild" "Upgraded from version 5.101.0 to version 5.102.0"
+sed -i -e "s/version=5.101.0/version=5.102.0/g" kf5/kcontacts/spkgbuild
+changelog "kf5/kcontacts/spkgbuild" "Upgraded from version 5.101.0 to version 5.102.0"
+sed -i -e "s/version=5.101.0/version=5.102.0/g" kf5/kcoreaddons/spkgbuild
+changelog "kf5/kcoreaddons/spkgbuild" "Upgraded from version 5.101.0 to version 5.102.0"
+sed -i -e "s/version=5.101.0/version=5.102.0/g" kf5/kcrash/spkgbuild
+changelog "kf5/kcrash/spkgbuild" "Upgraded from version 5.101.0 to version 5.102.0"
+sed -i -e "s/version=5.101.0/version=5.102.0/g" kf5/kdav/spkgbuild
+changelog "kf5/kdav/spkgbuild" "Upgraded from version 5.101.0 to version 5.102.0"
+sed -i -e "s/version=5.101.0/version=5.102.0/g" kf5/kdbusaddons/spkgbuild
+changelog "kf5/kdbusaddons/spkgbuild" "Upgraded from version 5.101.0 to version 5.102.0"
+sed -i -e "s/version=5.101.0/version=5.102.0/g" kf5/kdeclarative/spkgbuild
+changelog "kf5/kdeclarative/spkgbuild" "Upgraded from version 5.101.0 to version 5.102.0"
+sed -i -e "s/version=5.101.0/version=5.102.0/g" kf5/kded/spkgbuild
+changelog "kf5/kded/spkgbuild" "Upgraded from version 5.101.0 to version 5.102.0"
+sed -i -e "s/version=5.101.0/version=5.102.0/g" kf5/kdelibs4support/spkgbuild
+changelog "kf5/kdelibs4support/spkgbuild" "Upgraded from version 5.101.0 to version 5.102.0"
+sed -i -e "s/version=5.101.0/version=5.102.0/g" kf5/kdesignerplugin/spkgbuild
+changelog "kf5/kdesignerplugin/spkgbuild" "Upgraded from version 5.101.0 to version 5.102.0"
+sed -i -e "s/version=5.101.0/version=5.102.0/g" kf5/kdesu/spkgbuild
+changelog "kf5/kdesu/spkgbuild" "Upgraded from version 5.101.0 to version 5.102.0"
+sed -i -e "s/version=5.101.0/version=5.102.0/g" kf5/kdewebkit/spkgbuild
+changelog "kf5/kdewebkit/spkgbuild" "Upgraded from version 5.101.0 to version 5.102.0"
+sed -i -e "s/version=5.101.0/version=5.102.0/g" kf5/kdnssd/spkgbuild
+changelog "kf5/kdnssd/spkgbuild" "Upgraded from version 5.101.0 to version 5.102.0"
+sed -i -e "s/version=5.101.0/version=5.102.0/g" kf5/kdoctools/spkgbuild
+changelog "kf5/kdoctools/spkgbuild" "Upgraded from version 5.101.0 to version 5.102.0"
+sed -i -e "s/version=5.101.0/version=5.102.0/g" kf5/kemoticons/spkgbuild
+changelog "kf5/kemoticons/spkgbuild" "Upgraded from version 5.101.0 to version 5.102.0"
+sed -i -e "s/version=5.101.0/version=5.102.0/g" kf5/kf5-depends/spkgbuild
+changelog "kf5/kf5-depends/spkgbuild" "Upgraded from version 5.101.0 to version 5.102.0"
+sed -i -e "s/version=5.101.0/version=5.102.0/g" kf5/kf5-programs/spkgbuild
+changelog "kf5/kf5-programs/spkgbuild" "Upgraded from version 5.101.0 to version 5.102.0"
+sed -i -e "s/version=5.101.0/version=5.102.0/g" kf5/kfilemetadata/spkgbuild
+changelog "kf5/kfilemetadata/spkgbuild" "Upgraded from version 5.101.0 to version 5.102.0"
+sed -i -e "s/version=5.101.0/version=5.102.0/g" kf5/kglobalaccel/spkgbuild
+changelog "kf5/kglobalaccel/spkgbuild" "Upgraded from version 5.101.0 to version 5.102.0"
+sed -i -e "s/version=5.101.0/version=5.102.0/g" kf5/kguiaddons/spkgbuild
+changelog "kf5/kguiaddons/spkgbuild" "Upgraded from version 5.101.0 to version 5.102.0"
+sed -i -e "s/version=5.101.0/version=5.102.0/g" kf5/kholidays/spkgbuild
+changelog "kf5/kholidays/spkgbuild" "Upgraded from version 5.101.0 to version 5.102.0"
+sed -i -e "s/version=5.101.0/version=5.102.0/g" kf5/khtml/spkgbuild
+changelog "kf5/khtml/spkgbuild" "Upgraded from version 5.101.0 to version 5.102.0"
+sed -i -e "s/version=5.101.0/version=5.102.0/g" kf5/ki18n/spkgbuild
+changelog "kf5/ki18n/spkgbuild" "Upgraded from version 5.101.0 to version 5.102.0"
+sed -i -e "s/version=5.101.0/version=5.102.0/g" kf5/kiconthemes/spkgbuild
+changelog "kf5/kiconthemes/spkgbuild" "Upgraded from version 5.101.0 to version 5.102.0"
+sed -i -e "s/version=5.101.0/version=5.102.0/g" kf5/kidletime/spkgbuild
+changelog "kf5/kidletime/spkgbuild" "Upgraded from version 5.101.0 to version 5.102.0"
+sed -i -e "s/version=5.101.0/version=5.102.0/g" kf5/kimageformats/spkgbuild
+changelog "kf5/kimageformats/spkgbuild" "Upgraded from version 5.101.0 to version 5.102.0"
+sed -i -e "s/version=5.101.0/version=5.102.0/g" kf5/kinit/spkgbuild
+changelog "kf5/kinit/spkgbuild" "Upgraded from version 5.101.0 to version 5.102.0"
+sed -i -e "s/version=5.101.0/version=5.102.0/g" kf5/kio/spkgbuild
+changelog "kf5/kio/spkgbuild" "Upgraded from version 5.101.0 to version 5.102.0"
+sed -i -e "s/version=5.101.0/version=5.102.0/g" kf5/kirigami2/spkgbuild
+changelog "kf5/kirigami2/spkgbuild" "Upgraded from version 5.101.0 to version 5.102.0"
+sed -i -e "s/version=5.101.0/version=5.102.0/g" kf5/kitemmodels/spkgbuild
+changelog "kf5/kitemmodels/spkgbuild" "Upgraded from version 5.101.0 to version 5.102.0"
+sed -i -e "s/version=5.101.0/version=5.102.0/g" kf5/kitemviews/spkgbuild
+changelog "kf5/kitemviews/spkgbuild" "Upgraded from version 5.101.0 to version 5.102.0"
+sed -i -e "s/version=5.101.0/version=5.102.0/g" kf5/kjobwidgets/spkgbuild
+changelog "kf5/kjobwidgets/spkgbuild" "Upgraded from version 5.101.0 to version 5.102.0"
+sed -i -e "s/version=5.101.0/version=5.102.0/g" kf5/kjs/spkgbuild
+changelog "kf5/kjs/spkgbuild" "Upgraded from version 5.101.0 to version 5.102.0"
+sed -i -e "s/version=5.101.0/version=5.102.0/g" kf5/kjsembed/spkgbuild
+changelog "kf5/kjsembed/spkgbuild" "Upgraded from version 5.101.0 to version 5.102.0"
+sed -i -e "s/version=5.101.0/version=5.102.0/g" kf5/kmediaplayer/spkgbuild
+changelog "kf5/kmediaplayer/spkgbuild" "Upgraded from version 5.101.0 to version 5.102.0"
+sed -i -e "s/version=5.101.0/version=5.102.0/g" kf5/knewstuff/spkgbuild
+changelog "kf5/knewstuff/spkgbuild" "Upgraded from version 5.101.0 to version 5.102.0"
+sed -i -e "s/version=5.101.0/version=5.102.0/g" kf5/knotifications/spkgbuild
+changelog "kf5/knotifications/spkgbuild" "Upgraded from version 5.101.0 to version 5.102.0"
+sed -i -e "s/version=5.101.0/version=5.102.0/g" kf5/knotifyconfig/spkgbuild
+changelog "kf5/knotifyconfig/spkgbuild" "Upgraded from version 5.101.0 to version 5.102.0"
+sed -i -e "s/version=5.101.0/version=5.102.0/g" kf5/kpackage/spkgbuild
+changelog "kf5/kpackage/spkgbuild" "Upgraded from version 5.101.0 to version 5.102.0"
+sed -i -e "s/version=5.101.0/version=5.102.0/g" kf5/kparts/spkgbuild
+changelog "kf5/kparts/spkgbuild" "Upgraded from version 5.101.0 to version 5.102.0"
+sed -i -e "s/version=5.101.0/version=5.102.0/g" kf5/kpeople/spkgbuild
+changelog "kf5/kpeople/spkgbuild" "Upgraded from version 5.101.0 to version 5.102.0"
+sed -i -e "s/version=5.101.0/version=5.102.0/g" kf5/kplotting/spkgbuild
+changelog "kf5/kplotting/spkgbuild" "Upgraded from version 5.101.0 to version 5.102.0"
+sed -i -e "s/version=5.101.0/version=5.102.0/g" kf5/kpty/spkgbuild
+changelog "kf5/kpty/spkgbuild" "Upgraded from version 5.101.0 to version 5.102.0"
+sed -i -e "s/version=5.101.0/version=5.102.0/g" kf5/kquickcharts/spkgbuild
+changelog "kf5/kquickcharts/spkgbuild" "Upgraded from version 5.101.0 to version 5.102.0"
+sed -i -e "s/version=5.101.0/version=5.102.0/g" kf5/kross/spkgbuild
+changelog "kf5/kross/spkgbuild" "Upgraded from version 5.101.0 to version 5.102.0"
+sed -i -e "s/version=5.101.0/version=5.102.0/g" kf5/krunner/spkgbuild
+changelog "kf5/krunner/spkgbuild" "Upgraded from version 5.101.0 to version 5.102.0"
+sed -i -e "s/version=5.101.0/version=5.102.0/g" kf5/kservice/spkgbuild
+changelog "kf5/kservice/spkgbuild" "Upgraded from version 5.101.0 to version 5.102.0"
+sed -i -e "s/version=5.101.0/version=5.102.0/g" kf5/ktexteditor/spkgbuild
+changelog "kf5/ktexteditor/spkgbuild" "Upgraded from version 5.101.0 to version 5.102.0"
+sed -i -e "s/version=5.101.0/version=5.102.0/g" kf5/ktextwidgets/spkgbuild
+changelog "kf5/ktextwidgets/spkgbuild" "Upgraded from version 5.101.0 to version 5.102.0"
+sed -i -e "s/version=5.101.0/version=5.102.0/g" kf5/kunitconversion/spkgbuild
+changelog "kf5/kunitconversion/spkgbuild" "Upgraded from version 5.101.0 to version 5.102.0"
+sed -i -e "s/version=5.101.0/version=5.102.0/g" kf5/kwallet/spkgbuild
+changelog "kf5/kwallet/spkgbuild" "Upgraded from version 5.101.0 to version 5.102.0"
+sed -i -e "s/version=5.101.0/version=5.102.0/g" kf5/kwayland/spkgbuild
+changelog "kf5/kwayland/spkgbuild" "Upgraded from version 5.101.0 to version 5.102.0"
+sed -i -e "s/version=5.101.0/version=5.102.0/g" kf5/kwidgetsaddons/spkgbuild
+changelog "kf5/kwidgetsaddons/spkgbuild" "Upgraded from version 5.101.0 to version 5.102.0"
+sed -i -e "s/version=5.101.0/version=5.102.0/g" kf5/kwindowsystem/spkgbuild
+changelog "kf5/kwindowsystem/spkgbuild" "Upgraded from version 5.101.0 to version 5.102.0"
+sed -i -e "s/version=5.101.0/version=5.102.0/g" kf5/kxmlgui/spkgbuild
+changelog "kf5/kxmlgui/spkgbuild" "Upgraded from version 5.101.0 to version 5.102.0"
+sed -i -e "s/version=5.101.0/version=5.102.0/g" kf5/kxmlrpcclient/spkgbuild
+changelog "kf5/kxmlrpcclient/spkgbuild" "Upgraded from version 5.101.0 to version 5.102.0"
+sed -i -e "s/version=5.101.0/version=5.102.0/g" kf5/oxygen-icons5/spkgbuild
+changelog "kf5/oxygen-icons5/spkgbuild" "Upgraded from version 5.101.0 to version 5.102.0"
+sed -i -e "s/version=5.101.0/version=5.102.0/g" kf5/plasma-framework/spkgbuild
+changelog "kf5/plasma-framework/spkgbuild" "Upgraded from version 5.101.0 to version 5.102.0"
+sed -i -e "s/version=5.101.0/version=5.102.0/g" kf5/prison/spkgbuild
+changelog "kf5/prison/spkgbuild" "Upgraded from version 5.101.0 to version 5.102.0"
+sed -i -e "s/version=5.101.0/version=5.102.0/g" kf5/purpose/spkgbuild
+changelog "kf5/purpose/spkgbuild" "Upgraded from version 5.101.0 to version 5.102.0"
+sed -i -e "s/version=5.101.0/version=5.102.0/g" kf5/qqc2-desktop-style/spkgbuild
+changelog "kf5/qqc2-desktop-style/spkgbuild" "Upgraded from version 5.101.0 to version 5.102.0"
+sed -i -e "s/version=5.101.0/version=5.102.0/g" kf5/solid/spkgbuild
+changelog "kf5/solid/spkgbuild" "Upgraded from version 5.101.0 to version 5.102.0"
+sed -i -e "s/version=5.101.0/version=5.102.0/g" kf5/sonnet/spkgbuild
+changelog "kf5/sonnet/spkgbuild" "Upgraded from version 5.101.0 to version 5.102.0"
+sed -i -e "s/version=5.101.0/version=5.102.0/g" kf5/syndication/spkgbuild
+changelog "kf5/syndication/spkgbuild" "Upgraded from version 5.101.0 to version 5.102.0"
+sed -i -e "s/version=5.101.0/version=5.102.0/g" kf5/syntax-highlighting/spkgbuild
+changelog "kf5/syntax-highlighting/spkgbuild" "Upgraded from version 5.101.0 to version 5.102.0"
+sed -i -e "s/version=5.101.0/version=5.102.0/g" kf5/threadweaver/spkgbuild
+changelog "kf5/threadweaver/spkgbuild" "Upgraded from version 5.101.0 to version 5.102.0"
+sed -i -e "s/version=1.21.3/version=1.21.90/g" media/gst-libav/spkgbuild
+changelog "media/gst-libav/spkgbuild" "Upgraded from version 1.21.3 to version 1.21.90"
+sed -i -e "s/version=1.21.3/version=1.21.90/g" media/gst-plugins-bad/spkgbuild
+changelog "media/gst-plugins-bad/spkgbuild" "Upgraded from version 1.21.3 to version 1.21.90"
+sed -i -e "s/version=1.21.3/version=1.21.90/g" media/gst-plugins-base/spkgbuild
+changelog "media/gst-plugins-base/spkgbuild" "Upgraded from version 1.21.3 to version 1.21.90"
+sed -i -e "s/version=1.21.3/version=1.21.90/g" media/gst-plugins-good/spkgbuild
+changelog "media/gst-plugins-good/spkgbuild" "Upgraded from version 1.21.3 to version 1.21.90"
+sed -i -e "s/version=1.21.3/version=1.21.90/g" media/gst-plugins-ugly/spkgbuild
+changelog "media/gst-plugins-ugly/spkgbuild" "Upgraded from version 1.21.3 to version 1.21.90"
+sed -i -e "s/version=1.21.3/version=1.21.90/g" media/gstreamer/spkgbuild
+changelog "media/gstreamer/spkgbuild" "Upgraded from version 1.21.3 to version 1.21.90"
+sed -i -e "s/version=1.10.2/version=1.10.3/g" libs/libpcap/spkgbuild
+changelog "libs/libpcap/spkgbuild" "Upgraded from version 1.10.2 to version 1.10.3"
+sed -i -e "s/version=1.2.4/version=1.3.0/g" libs/libwebp/spkgbuild
+changelog "libs/libwebp/spkgbuild" "Upgraded from version 1.2.4 to version 1.3.0"
+sed -i -e "s/version=4.99.2/version=4.99.3/g" networking/tcpdump/spkgbuild
+changelog "networking/tcpdump/spkgbuild" "Upgraded from version 4.99.2 to version 4.99.3"
+sed -i -e "s/version=0.0.23/version=0.0.24/g" nonfree/discord/spkgbuild
+changelog "nonfree/discord/spkgbuild" "Upgraded from version 0.0.23 to version 0.0.24"
+sed -i -e "s/version=1.26.48/version=1.26.50/g" python/python-boto3/spkgbuild
+changelog "python/python-boto3/spkgbuild" "Upgraded from version 1.26.48 to version 1.26.50"
+sed -i -e "s/version=1.29.48/version=1.29.50/g" python/python-botocore/spkgbuild
+changelog "python/python-botocore/spkgbuild" "Upgraded from version 1.29.48 to version 1.29.50"
+sed -i -e "s/version=2022.12.1/version=2023.1.0/g" python/python-distributed/spkgbuild
+changelog "python/python-distributed/spkgbuild" "Upgraded from version 2022.12.1 to version 2023.1.0"
+sed -i -e "s/version=16.3.0/version=16.4.0/g" python/python-faker/spkgbuild
+changelog "python/python-faker/spkgbuild" "Upgraded from version 16.3.0 to version 16.4.0"
+sed -i -e "s/version=6.62.0/version=6.62.1/g" python/python-hypothesis/spkgbuild
+changelog "python/python-hypothesis/spkgbuild" "Upgraded from version 6.62.0 to version 6.62.1"
+sed -i -e "s/version=2.4.1/version=2.4.2/g" python/python-pytest-regressions/spkgbuild
+changelog "python/python-pytest-regressions/spkgbuild" "Upgraded from version 2.4.1 to version 2.4.2"
+sed -i -e "s/version=13.0.1/version=13.1.0/g" python/python-rich/spkgbuild
+changelog "python/python-rich/spkgbuild" "Upgraded from version 13.0.1 to version 13.1.0"
+sed -i -e "s/version=3.0.6/version=3.0.7/g" python/python-xlsxwriter/spkgbuild
+changelog "python/python-xlsxwriter/spkgbuild" "Upgraded from version 3.0.6 to version 3.0.7"
+sed -i -e "s/version=5.101.0/version=5.102.0/g" qt/bluez-qt/spkgbuild
+changelog "qt/bluez-qt/spkgbuild" "Upgraded from version 5.101.0 to version 5.102.0"
+sed -i -e "s/version=5.101.0/version=5.102.0/g" qt/modemmanager-qt/spkgbuild
+changelog "qt/modemmanager-qt/spkgbuild" "Upgraded from version 5.101.0 to version 5.102.0"
+sed -i -e "s/version=5.101.0/version=5.102.0/g" qt/networkmanager-qt/spkgbuild
+changelog "qt/networkmanager-qt/spkgbuild" "Upgraded from version 5.101.0 to version 5.102.0"
+sed -i -e "s/version=3.11.51/version=3.11.52/g" server/blacklist-hosts/spkgbuild
+changelog "server/blacklist-hosts/spkgbuild" "Upgraded from version 3.11.51 to version 3.11.52"
+sed -i -e "s/version=1.10.2/version=1.10.3/g" xfce/xfce4-screenshooter/spkgbuild
+changelog "xfce/xfce4-screenshooter/spkgbuild" "Upgraded from version 1.10.2 to version 1.10.3"
+sed -i -e "s/version=6.1.0/version=6.1.1/g" xfce/xfsprogs/spkgbuild
+changelog "xfce/xfsprogs/spkgbuild" "Upgraded from version 6.1.0 to version 6.1.1"
