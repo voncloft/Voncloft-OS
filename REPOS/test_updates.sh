4a5eba6c34c80f1baea515de749733b15b551912
Voncloft <voncloft@gmail.com>
2023-01-13 23:37:43 -0500

    added patch for coreutils to have progress bar

---
+++ REPOS/test_updates.sh
@@ -0,0 +1,52 @@
+sed -i -e "s/version=15.0.6/version=15.0.7/g" compilers/clang/spkgbuild
+changelog "compilers/clang/spkgbuild" "Upgraded from version 15.0.6 to version 15.0.7"
+sed -i -e "s/version=15.0.6/version=15.0.7/g" compilers/lld/spkgbuild
+changelog "compilers/lld/spkgbuild" "Upgraded from version 15.0.6 to version 15.0.7"
+sed -i -e "s/version=15.0.6/version=15.0.7/g" compilers/llvm/spkgbuild
+changelog "compilers/llvm/spkgbuild" "Upgraded from version 15.0.6 to version 15.0.7"
+sed -i -e "s/version=6.1.4/version=6.1.5/g" core/linux-api-headers/spkgbuild
+changelog "core/linux-api-headers/spkgbuild" "Upgraded from version 6.1.4 to version 6.1.5"
+sed -i -e "s/version=6.1.4/version=6.1.5/g" core/linux-desktop/spkgbuild
+changelog "core/linux-desktop/spkgbuild" "Upgraded from version 6.1.4 to version 6.1.5"
+sed -i -e "s/version=6.1.4/version=6.1.5/g" core/linux-router/spkgbuild
+changelog "core/linux-router/spkgbuild" "Upgraded from version 6.1.4 to version 6.1.5"
+sed -i -e "s/version=0.3.63/version=0.3.64/g" core/pipewire/spkgbuild
+changelog "core/pipewire/spkgbuild" "Upgraded from version 0.3.63 to version 0.3.64"
+sed -i -e "s/version=15.0.6/version=15.0.7/g" multilib/llvm-32/spkgbuild
+changelog "multilib/llvm-32/spkgbuild" "Upgraded from version 15.0.6 to version 15.0.7"
+sed -i -e "s/version=0.40.0/version=0.41.0/g" libs/libfilezilla/spkgbuild
+changelog "libs/libfilezilla/spkgbuild" "Upgraded from version 0.40.0 to version 0.41.0"
+sed -i -e "s/version=1.41.7/version=1.41.8/g" networking/networkmanager/spkgbuild
+changelog "networking/networkmanager/spkgbuild" "Upgraded from version 1.41.7 to version 1.41.8"
+sed -i -e "s/version=0.4.7.12/version=0.4.7.13/g" networking/tor/spkgbuild
+changelog "networking/tor/spkgbuild" "Upgraded from version 0.4.7.12 to version 0.4.7.13"
+sed -i -e "s/version=0.0.22/version=0.0.23/g" nonfree/discord/spkgbuild
+changelog "nonfree/discord/spkgbuild" "Upgraded from version 0.0.22 to version 0.0.23"
+sed -i -e "s/version=7.4.3/version=7.4.4/g" nonfree/libreoffice/spkgbuild
+changelog "nonfree/libreoffice/spkgbuild" "Upgraded from version 7.4.3 to version 7.4.4"
+sed -i -e "s/version=1.56.0/version=1.57.0/g" python/python-aws-sam-translator/spkgbuild
+changelog "python/python-aws-sam-translator/spkgbuild" "Upgraded from version 1.56.0 to version 1.57.0"
+sed -i -e "s/version=2.5.2/version=2.6.0/g" python/python-cairosvg/spkgbuild
+changelog "python/python-cairosvg/spkgbuild" "Upgraded from version 2.5.2 to version 2.6.0"
+sed -i -e "s/version=0.72.7/version=0.72.8/g" python/python-cfn-lint/spkgbuild
+changelog "python/python-cfn-lint/spkgbuild" "Upgraded from version 0.72.7 to version 0.72.8"
+sed -i -e "s/version=2.0.1/version=2.0.2/g" python/python-flask-caching/spkgbuild
+changelog "python/python-flask-caching/spkgbuild" "Upgraded from version 2.0.1 to version 2.0.2"
+sed -i -e "s/version=2.5.12/version=2.5.13/g" python/python-identify/spkgbuild
+changelog "python/python-identify/spkgbuild" "Upgraded from version 2.5.12 to version 2.5.13"
+sed -i -e "s/version=4.27.0/version=4.28.0/g" python/python-mt-940/spkgbuild
+changelog "python/python-mt-940/spkgbuild" "Upgraded from version 4.27.0 to version 4.28.0"
+sed -i -e "s/version=1.1.0/version=1.1.1/g" python/python-pook/spkgbuild
+changelog "python/python-pook/spkgbuild" "Upgraded from version 1.1.0 to version 1.1.1"
+sed -i -e "s/version=2.28.1/version=2.28.2/g" python/python-requests/spkgbuild
+changelog "python/python-requests/spkgbuild" "Upgraded from version 2.28.1 to version 2.28.2"
+sed -i -e "s/version=7.4.2/version=7.4.3/g" python/python-traitsui/spkgbuild
+changelog "python/python-traitsui/spkgbuild" "Upgraded from version 7.4.2 to version 7.4.3"
+sed -i -e "s/version=1.14.1/version=1.15.0/g" python/python-wrapt/spkgbuild
+changelog "python/python-wrapt/spkgbuild" "Upgraded from version 1.14.1 to version 1.15.0"
+sed -i -e "s/version=3.11.50/version=3.11.51/g" server/blacklist-hosts/spkgbuild
+changelog "server/blacklist-hosts/spkgbuild" "Upgraded from version 3.11.50 to version 3.11.51"
+sed -i -e "s/version=2023.1.3/version=2023.1.4/g" server/homeassistant/spkgbuild
+changelog "server/homeassistant/spkgbuild" "Upgraded from version 2023.1.3 to version 2023.1.4"
+sed -i -e "s/version=1.8.8/version=1.8.9/g" server/iptables/spkgbuild
+changelog "server/iptables/spkgbuild" "Upgraded from version 1.8.8 to version 1.8.9"
