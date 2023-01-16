0cbd1ec7f711f94eeb21bd84b70771b29ecab643
Voncloft <voncloft@gmail.com>
2023-01-15 20:03:01 -0500

    Minor Updates

---
+++ logs/old-test-updates/2023/Jan/applied_updates-01-15-23.sh
@@ -0,0 +1,8 @@
+sed -i -e "s/version=1.31.1/version=1.31.2/g" media/mpg123/spkgbuild
+changelog "media/mpg123/spkgbuild" "Upgraded from version 1.31.1 to version 1.31.2"
+sed -i -e "s/version=3.40.0.0/version=3.40.1.0/g" python/python-apsw/spkgbuild
+changelog "python/python-apsw/spkgbuild" "Upgraded from version 3.40.0.0 to version 3.40.1.0"
+sed -i -e "s/version=1.0.1/version=1.1.0/g" python/python-flask-wtf/spkgbuild
+changelog "python/python-flask-wtf/spkgbuild" "Upgraded from version 1.0.1 to version 1.1.0"
+sed -i -e "s/version=0.19.1/version=0.20.0/g" python/python-libtmux/spkgbuild
+changelog "python/python-libtmux/spkgbuild" "Upgraded from version 0.19.1 to version 0.20.0"
