aef3f46e42337796a109941ea436e516280a2fdc
Voncloft <voncloft@gmail.com>
2023-01-17 08:07:36 -0500

    Minor Updates

---
+++ logs/old-test-updates/2023/Jan/applied_updates-01-17-23.sh
@@ -0,0 +1,20 @@
+sed -i -e "s/version=5.6.6/version=5.6.7/g" cinnamon/cinnamon/spkgbuild
+changelog "cinnamon/cinnamon/spkgbuild" "Upgraded from version 5.6.6 to version 5.6.7"
+sed -i -e "s/version=3.8/version=3.9/g" core/diffutils/spkgbuild
+changelog "core/diffutils/spkgbuild" "Upgraded from version 3.8 to version 3.9"
+sed -i -e "s/version=7.0.7/version=7.0.8/g" core/redis/spkgbuild
+changelog "core/redis/spkgbuild" "Upgraded from version 7.0.7 to version 7.0.8"
+sed -i -e "s/version=2.39.3/version=2.39.4/g" core/webkitgtk/spkgbuild
+changelog "core/webkitgtk/spkgbuild" "Upgraded from version 2.39.3 to version 2.39.4"
+sed -i -e "s/version=109.0b9/version=110.0b1/g" networking/firefox/spkgbuild
+changelog "networking/firefox/spkgbuild" "Upgraded from version 109.0b9 to version 110.0b1"
+sed -i -e "s/version=6.20.1/version=6.20.2/g" python/python-ipykernel/spkgbuild
+changelog "python/python-ipykernel/spkgbuild" "Upgraded from version 6.20.1 to version 6.20.2"
+sed -i -e "s/version=0.5.1/version=0.5.2/g" python/python-openapi-spec-validator/spkgbuild
+changelog "python/python-openapi-spec-validator/spkgbuild" "Upgraded from version 0.5.1 to version 0.5.2"
+sed -i -e "s/version=65.7.0/version=66.0.0/g" python/python-setuptools/spkgbuild
+changelog "python/python-setuptools/spkgbuild" "Upgraded from version 65.7.0 to version 66.0.0"
+sed -i -e "s/version=3.18.0/version=3.18.1/g" python/python-trimesh/spkgbuild
+changelog "python/python-trimesh/spkgbuild" "Upgraded from version 3.18.0 to version 3.18.1"
+sed -i -e "s/version=5.8/version=6.0/g" python/python-zope-security/spkgbuild
+changelog "python/python-zope-security/spkgbuild" "Upgraded from version 5.8 to version 6.0"
