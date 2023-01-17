76ce4852df57e085779ef5c7627a11208a5d55cf
Voncloft <voncloft@gmail.com>
2023-01-17 18:44:45 -0500

    Minor Updates

--- logs/old-test-updates/2023/Jan/applied_updates-01-17-23.sh
+++ logs/old-test-updates/2023/Jan/applied_updates-01-17-23.sh
@@ -18,3 +18,47 @@ sed -i -e "s/version=3.18.0/version=3.18.1/g" python/python-trimesh/spkgbuild
 changelog "python/python-trimesh/spkgbuild" "Upgraded from version 3.18.0 to version 3.18.1"
 sed -i -e "s/version=5.8/version=6.0/g" python/python-zope-security/spkgbuild
 changelog "python/python-zope-security/spkgbuild" "Upgraded from version 5.8 to version 6.0"
+sed -i -e "s/version=1.12.0/version=1.13.0/g" core/gtest/spkgbuild
+changelog "core/gtest/spkgbuild" "Upgraded from version 1.12.0 to version 1.13.0"
+sed -i -e "s/version=0.109.0/version=0.110.0/g" core/hugo/spkgbuild
+changelog "core/hugo/spkgbuild" "Upgraded from version 0.109.0 to version 0.110.0"
+sed -i -e "s/version=4.96.5/version=4.97.0/g" core/lsof/spkgbuild
+changelog "core/lsof/spkgbuild" "Upgraded from version 4.96.5 to version 4.97.0"
+sed -i -e "s/version=4.0.4/version=4.0.5/g" libs/libimagequant/spkgbuild
+changelog "libs/libimagequant/spkgbuild" "Upgraded from version 4.0.4 to version 4.0.5"
+sed -i -e "s/version=2.39.0/version=2.39.1/g" networking/git/spkgbuild
+changelog "networking/git/spkgbuild" "Upgraded from version 2.39.0 to version 2.39.1"
+sed -i -e "s/version=7.0.7.0.4/version=7.0.7.0.6/g" nonfree/virtualbox/spkgbuild
+changelog "nonfree/virtualbox/spkgbuild" "Upgraded from version 7.0.7.0.4 to version 7.0.7.0.6"
+sed -i -e "s/version=7.0.4/version=7.0.6/g" nonfree/virtualbox-guest-modules/spkgbuild
+changelog "nonfree/virtualbox-guest-modules/spkgbuild" "Upgraded from version 7.0.4 to version 7.0.6"
+sed -i -e "s/version=7.0.4/version=7.0.6/g" nonfree/virtualbox-guest-utils/spkgbuild
+changelog "nonfree/virtualbox-guest-utils/spkgbuild" "Upgraded from version 7.0.4 to version 7.0.6"
+sed -i -e "s/version=1.26.50/version=1.26.51/g" python/python-boto3/spkgbuild
+changelog "python/python-boto3/spkgbuild" "Upgraded from version 1.26.50 to version 1.26.51"
+sed -i -e "s/version=1.29.50/version=1.29.51/g" python/python-botocore/spkgbuild
+changelog "python/python-botocore/spkgbuild" "Upgraded from version 1.29.50 to version 1.29.51"
+sed -i -e "s/version=0.72.8/version=0.72.9/g" python/python-cfn-lint/spkgbuild
+changelog "python/python-cfn-lint/spkgbuild" "Upgraded from version 0.72.8 to version 0.72.9"
+sed -i -e "s/version=16.4.0/version=16.6.0/g" python/python-faker/spkgbuild
+changelog "python/python-faker/spkgbuild" "Upgraded from version 16.4.0 to version 16.6.0"
+sed -i -e "s/version=1.1.0/version=1.1.1/g" python/python-flask-wtf/spkgbuild
+changelog "python/python-flask-wtf/spkgbuild" "Upgraded from version 1.1.0 to version 1.1.1"
+sed -i -e "s/version=2.72.0/version=2.73.0/g" python/python-google-api-python-client/spkgbuild
+changelog "python/python-google-api-python-client/spkgbuild" "Upgraded from version 2.72.0 to version 2.73.0"
+sed -i -e "s/version=8.0.31/version=8.0.32/g" python/python-mysql-connector/spkgbuild
+changelog "python/python-mysql-connector/spkgbuild" "Upgraded from version 8.0.31 to version 8.0.32"
+sed -i -e "s/version=6.2.3/version=6.3.0/g" python/python-pydocstyle/spkgbuild
+changelog "python/python-pydocstyle/spkgbuild" "Upgraded from version 6.2.3 to version 6.3.0"
+sed -i -e "s/version=1.6.0/version=1.7.0/g" python/python-rope/spkgbuild
+changelog "python/python-rope/spkgbuild" "Upgraded from version 1.6.0 to version 1.7.0"
+sed -i -e "s/version=2.4.3/version=2.4.4/g" ruby-gems/ruby-bundler/spkgbuild
+changelog "ruby-gems/ruby-bundler/spkgbuild" "Upgraded from version 2.4.3 to version 2.4.4"
+sed -i -e "s/version=3.12.2/version=3.12.3/g" ruby-gems/ruby-rspec-mocks/spkgbuild
+changelog "ruby-gems/ruby-rspec-mocks/spkgbuild" "Upgraded from version 3.12.2 to version 3.12.3"
+sed -i -e "s/version=2023.1.4/version=2023.1.5/g" server/homeassistant/spkgbuild
+changelog "server/homeassistant/spkgbuild" "Upgraded from version 2023.1.4 to version 2023.1.5"
+sed -i -e "s/version=2.4.54/version=2.4.55/g" server/httpd/spkgbuild
+changelog "server/httpd/spkgbuild" "Upgraded from version 2.4.54 to version 2.4.55"
+sed -i -e "s/version=0.8.2/version=0.8.3/g" xfce/xfce4-datetime-plugin/spkgbuild
+changelog "xfce/xfce4-datetime-plugin/spkgbuild" "Upgraded from version 0.8.2 to version 0.8.3"
