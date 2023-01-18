f989f4a7cf6c655a26c5ac717285dec8e2493d4f
Voncloft <voncloft@gmail.com>
2023-01-18 10:32:35 -0500

    Minor Updates

---
+++ logs/old-test-updates/2023/Jan/applied_updates-01-18-23.sh
@@ -0,0 +1,6 @@
+sed -i -e "s/version=7.1/version=7.2/g" core/nano/spkgbuild
+changelog "core/nano/spkgbuild" "Upgraded from version 7.1 to version 7.2"
+sed -i -e "s/version=6.1.6/version=6.1.7/g" core/linux-desktop/spkgbuild
+changelog "core/linux-desktop/spkgbuild" "Upgraded from version 6.1.6 to version 6.1.7"
+sed -i -e "s/version=6.1.6/version=6.1.7/g" core/linux-router/spkgbuild
+changelog "core/linux-router/spkgbuild" "Upgraded from version 6.1.6 to version 6.1.7"
