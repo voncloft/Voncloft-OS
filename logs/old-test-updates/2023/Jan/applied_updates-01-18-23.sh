e7361ffca9c231887b51e4372d4be21e5508ddf0
Voncloft <voncloft@gmail.com>
2023-01-18 20:33:28 -0500

    Minor Updates

--- logs/old-test-updates/2023/Jan/applied_updates-01-18-23.sh
+++ logs/old-test-updates/2023/Jan/applied_updates-01-18-23.sh
@@ -4,3 +4,29 @@ sed -i -e "s/version=6.1.6/version=6.1.7/g" core/linux-desktop/spkgbuild
 changelog "core/linux-desktop/spkgbuild" "Upgraded from version 6.1.6 to version 6.1.7"
 sed -i -e "s/version=6.1.6/version=6.1.7/g" core/linux-router/spkgbuild
 changelog "core/linux-router/spkgbuild" "Upgraded from version 6.1.6 to version 6.1.7"
+sed -i -e "s/version=5.6.2/version=5.6.3/g" cinnamon/nemo/spkgbuild
+changelog "cinnamon/nemo/spkgbuild" "Upgraded from version 5.6.2 to version 5.6.3"
+sed -i -e "s/version=6.1.6/version=6.1.7/g" core/linux-api-headers/spkgbuild
+changelog "core/linux-api-headers/spkgbuild" "Upgraded from version 6.1.6 to version 6.1.7"
+sed -i -e "s/version=3.5.17/version=3.5.18/g" extra/flexget/spkgbuild
+changelog "extra/flexget/spkgbuild" "Upgraded from version 3.5.17 to version 3.5.18"
+sed -i -e "s/version=110.0b1/version=110.0b2/g" networking/firefox/spkgbuild
+changelog "networking/firefox/spkgbuild" "Upgraded from version 110.0b1 to version 110.0b2"
+sed -i -e "s/version=1.9.0/version=1.10/g" plasma/plasma-wayland-protocols/spkgbuild
+changelog "plasma/plasma-wayland-protocols/spkgbuild" "Upgraded from version 1.9.0 to version 1.10"
+sed -i -e "s/version=0.33.2/version=0.33.3/g" python/python-eventlet/spkgbuild
+changelog "python/python-eventlet/spkgbuild" "Upgraded from version 0.33.2 to version 0.33.3"
+sed -i -e "s/version=4.0.1/version=4.0.2/g" python/python-flask-migrate/spkgbuild
+changelog "python/python-flask-migrate/spkgbuild" "Upgraded from version 4.0.1 to version 4.0.2"
+sed -i -e "s/version=4.2.0/version=4.2.1/g" python/python-jaraco.context/spkgbuild
+changelog "python/python-jaraco.context/spkgbuild" "Upgraded from version 4.2.0 to version 4.2.1"
+sed -i -e "s/version=1.0.4/version=1.0.5/g" python/python-msgpack/spkgbuild
+changelog "python/python-msgpack/spkgbuild" "Upgraded from version 1.0.4 to version 1.0.5"
+sed -i -e "s/version=1.6.1/version=1.7.0/g" python/python-reedsolo/spkgbuild
+changelog "python/python-reedsolo/spkgbuild" "Upgraded from version 1.6.1 to version 1.7.0"
+sed -i -e "s/version=2.4.1/version=2.4.2/g" python/python-spyder-kernels/spkgbuild
+changelog "python/python-spyder-kernels/spkgbuild" "Upgraded from version 2.4.1 to version 2.4.2"
+sed -i -e "s/version=4.6.1/version=5.0.0/g" python/python-zope-proxy/spkgbuild
+changelog "python/python-zope-proxy/spkgbuild" "Upgraded from version 4.6.1 to version 5.0.0"
+sed -i -e "s/version=6.0/version=6.1/g" python/python-zope-security/spkgbuild
+changelog "python/python-zope-security/spkgbuild" "Upgraded from version 6.0 to version 6.1"
