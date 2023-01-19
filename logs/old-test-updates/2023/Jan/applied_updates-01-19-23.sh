e684998c2dd0c58bf6588a72d86052662fa7e959
Voncloft <voncloft@gmail.com>
2023-01-19 14:05:52 -0500

    Minor Updates

---
+++ logs/old-test-updates/2023/Jan/applied_updates-01-19-23.sh
@@ -0,0 +1,2 @@
+sed -i -e "s/version=1.10/version=1.10.0/g" plasma/plasma-wayland-protocols/spkgbuild
+changelog "plasma/plasma-wayland-protocols/spkgbuild" "Upgraded from version 1.10 to version 1.10.0"
