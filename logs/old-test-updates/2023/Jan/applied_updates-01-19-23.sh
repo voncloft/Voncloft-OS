0ec6ecf13691967bbbdcc94d05db66781152d165
Voncloft <voncloft@gmail.com>
2023-01-20 17:10:58 -0500

    Minor Updates

--- logs/old-test-updates/2023/Jan/applied_updates-01-19-23.sh
+++ logs/old-test-updates/2023/Jan/applied_updates-01-19-23.sh
@@ -1,2 +1,4 @@
 sed -i -e "s/version=1.10/version=1.10.0/g" plasma/plasma-wayland-protocols/spkgbuild
 changelog "plasma/plasma-wayland-protocols/spkgbuild" "Upgraded from version 1.10 to version 1.10.0"
+sed -i -e "s/version=1.9.0/version=1.10.0/g" plasma/plasma-wayland-protocols/spkgbuild
+changelog "plasma/plasma-wayland-protocols/spkgbuild" "Upgraded from version 1.9.0 to version 1.10.0"
---
+++ logs/old-test-updates/2023/Jan/applied_updates-01-20-23.sh
@@ -0,0 +1,52 @@
+sed -i -e "s/version=3.25.1/version=3.25.2/g" compilers/cmake/spkgbuild
+changelog "compilers/cmake/spkgbuild" "Upgraded from version 3.25.1 to version 3.25.2"
+sed -i -e "s/version=2.39.4/version=2.39.5/g" core/webkitgtk/spkgbuild
+changelog "core/webkitgtk/spkgbuild" "Upgraded from version 2.39.4 to version 2.39.5"
+sed -i -e "s/version=2.2.1/version=2.2.2/g" extra/ncdu/spkgbuild
+changelog "extra/ncdu/spkgbuild" "Upgraded from version 2.2.1 to version 2.2.2"
+sed -i -e "s/version=110.0b2/version=110.0b3/g" networking/firefox/spkgbuild
+changelog "networking/firefox/spkgbuild" "Upgraded from version 110.0b2 to version 110.0b3"
+sed -i -e "s/version=1.41.8/version=1.43.0/g" networking/networkmanager/spkgbuild
+changelog "networking/networkmanager/spkgbuild" "Upgraded from version 1.41.8 to version 1.43.0"
+sed -i -e "s/version=109.0b4/version=110.0b1/g" networking/thunderbird/spkgbuild
+changelog "networking/thunderbird/spkgbuild" "Upgraded from version 109.0b4 to version 110.0b1"
+sed -i -e "s/version=4.0.2/version=4.0.3/g" networking/wireshark/spkgbuild
+changelog "networking/wireshark/spkgbuild" "Upgraded from version 4.0.2 to version 4.0.3"
+sed -i -e "s/version=525.78.01/version=525.85.05/g" nonfree/nvidia/spkgbuild
+changelog "nonfree/nvidia/spkgbuild" "Upgraded from version 525.78.01 to version 525.85.05"
+sed -i -e "s/version=525.78.01/version=525.85.05/g" nonfree/nvidia-32/spkgbuild
+changelog "nonfree/nvidia-32/spkgbuild" "Upgraded from version 525.78.01 to version 525.85.05"
+sed -i -e "s/version=525.78.01/version=525.85.05/g" nonfree/nvidia-modules/spkgbuild
+changelog "nonfree/nvidia-modules/spkgbuild" "Upgraded from version 525.78.01 to version 525.85.05"
+sed -i -e "s/version=2.19.2/version=3.0/g" nonfree/pandoc-bin/spkgbuild
+changelog "nonfree/pandoc-bin/spkgbuild" "Upgraded from version 2.19.2 to version 3.0"
+sed -i -e "s/version=2.13.2/version=2.13.3/g" python/python-astroid/spkgbuild
+changelog "python/python-astroid/spkgbuild" "Upgraded from version 2.13.2 to version 2.13.3"
+sed -i -e "s/version=1.26.51/version=1.26.53/g" python/python-boto3/spkgbuild
+changelog "python/python-boto3/spkgbuild" "Upgraded from version 1.26.51 to version 1.26.53"
+sed -i -e "s/version=1.29.51/version=1.29.53/g" python/python-botocore/spkgbuild
+changelog "python/python-botocore/spkgbuild" "Upgraded from version 1.29.51 to version 1.29.53"
+sed -i -e "s/version=1.6.0/version=1.6.1/g" python/python-evdev/spkgbuild
+changelog "python/python-evdev/spkgbuild" "Upgraded from version 1.6.0 to version 1.6.1"
+sed -i -e "s/version=2022.11.0/version=2023.1.0/g" python/python-fsspec/spkgbuild
+changelog "python/python-fsspec/spkgbuild" "Upgraded from version 2022.11.0 to version 2023.1.0"
+sed -i -e "s/version=6.62.1/version=6.63.0/g" python/python-hypothesis/spkgbuild
+changelog "python/python-hypothesis/spkgbuild" "Upgraded from version 6.62.1 to version 6.63.0"
+sed -i -e "s/version=12.3.0/version=12.3.1/g" python/python-oslo-db/spkgbuild
+changelog "python/python-oslo-db/spkgbuild" "Upgraded from version 12.3.0 to version 12.3.1"
+sed -i -e "s/version=0.21.2/version=0.21.3/g" python/python-pytest-httpx/spkgbuild
+changelog "python/python-pytest-httpx/spkgbuild" "Upgraded from version 0.21.2 to version 0.21.3"
+sed -i -e "s/version=7.9.0/version=7.10.0/g" python/python-qiniu/spkgbuild
+changelog "python/python-qiniu/spkgbuild" "Upgraded from version 7.9.0 to version 7.10.0"
+sed -i -e "s/version=13.1.0/version=13.2.0/g" python/python-rich/spkgbuild
+changelog "python/python-rich/spkgbuild" "Upgraded from version 13.1.0 to version 13.2.0"
+sed -i -e "s/version=0.16.4/version=0.16.6/g" python/python-scikit-build/spkgbuild
+changelog "python/python-scikit-build/spkgbuild" "Upgraded from version 0.16.4 to version 0.16.6"
+sed -i -e "s/version=66.0.0/version=66.1.1/g" python/python-setuptools/spkgbuild
+changelog "python/python-setuptools/spkgbuild" "Upgraded from version 66.0.0 to version 66.1.1"
+sed -i -e "s/version=5.2.0/version=5.2.1/g" python/python-tempora/spkgbuild
+changelog "python/python-tempora/spkgbuild" "Upgraded from version 5.2.0 to version 5.2.1"
+sed -i -e "s/version=3.11.52/version=3.11.53/g" server/blacklist-hosts/spkgbuild
+changelog "server/blacklist-hosts/spkgbuild" "Upgraded from version 3.11.52 to version 3.11.53"
+sed -i -e "s/version=2023.1.5/version=2023.1.6/g" server/homeassistant/spkgbuild
+changelog "server/homeassistant/spkgbuild" "Upgraded from version 2023.1.5 to version 2023.1.6"
