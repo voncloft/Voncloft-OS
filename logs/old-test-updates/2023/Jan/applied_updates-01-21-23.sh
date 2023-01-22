acf73f4d7f7f0a6e02d6edb70538bf261eb1c507
Voncloft <voncloft@gmail.com>
2023-01-21 22:47:22 -0500

    Minor Updates

---
+++ logs/old-test-updates/2023/Jan/applied_updates-01-21-23.sh
@@ -0,0 +1,22 @@
+sed -i -e "s/version=1.8/version=1.8.1/g" core/scrot/spkgbuild
+changelog "core/scrot/spkgbuild" "Upgraded from version 1.8 to version 1.8.1"
+sed -i -e "s/version=0.32.4/version=0.32.5/g" libs/neon/spkgbuild
+changelog "libs/neon/spkgbuild" "Upgraded from version 0.32.4 to version 0.32.5"
+sed -i -e "s/version=1.26.53/version=1.26.54/g" python/python-boto3/spkgbuild
+changelog "python/python-boto3/spkgbuild" "Upgraded from version 1.26.53 to version 1.26.54"
+sed -i -e "s/version=1.29.53/version=1.29.54/g" python/python-botocore/spkgbuild
+changelog "python/python-botocore/spkgbuild" "Upgraded from version 1.29.53 to version 1.29.54"
+sed -i -e "s/version=0.21.0/version=0.21.1/g" python/python-dotenv/spkgbuild
+changelog "python/python-dotenv/spkgbuild" "Upgraded from version 0.21.0 to version 0.21.1"
+sed -i -e "s/version=1.3.0/version=1.3.1/g" python/python-email-validator/spkgbuild
+changelog "python/python-email-validator/spkgbuild" "Upgraded from version 1.3.0 to version 1.3.1"
+sed -i -e "s/version=4.2.1/version=4.3.0/g" python/python-jaraco.context/spkgbuild
+changelog "python/python-jaraco.context/spkgbuild" "Upgraded from version 4.2.1 to version 4.3.0"
+sed -i -e "s/version=0.25.0/version=0.26.0/g" python/python-mongoengine/spkgbuild
+changelog "python/python-mongoengine/spkgbuild" "Upgraded from version 0.25.0 to version 0.26.0"
+sed -i -e "s/version=2.12.0/version=3.0.0/g" python/python-paramiko/spkgbuild
+changelog "python/python-paramiko/spkgbuild" "Upgraded from version 2.12.0 to version 3.0.0"
+sed -i -e "s/version=4.18.2/version=4.18.3/g" xfce/thunar/spkgbuild
+changelog "xfce/thunar/spkgbuild" "Upgraded from version 4.18.2 to version 4.18.3"
+sed -i -e "s/version=0.6.5/version=0.7.1/g" xfce/xfce4-notifyd/spkgbuild
+changelog "xfce/xfce4-notifyd/spkgbuild" "Upgraded from version 0.6.5 to version 0.7.1"
