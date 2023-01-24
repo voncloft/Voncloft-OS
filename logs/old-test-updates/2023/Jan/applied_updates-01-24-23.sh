2a9f791bd692926effcd103a6b74cf788b7b5654
Voncloft <voncloft@gmail.com>
2023-01-24 16:42:47 -0500

    Minor Updates

---
+++ logs/old-test-updates/2023/Jan/applied_updates-01-24-23.sh
@@ -0,0 +1,50 @@
+sed -i -e "s/version=3.3.0/version=3.3.1/g" core/icewm/spkgbuild
+changelog "core/icewm/spkgbuild" "Upgraded from version 3.3.0 to version 3.3.1"
+sed -i -e "s/version=6.1.7/version=6.1.8/g" core/linux-api-headers/spkgbuild
+changelog "core/linux-api-headers/spkgbuild" "Upgraded from version 6.1.7 to version 6.1.8"
+sed -i -e "s/version=6.1.7/version=6.1.8/g" core/linux-desktop/spkgbuild
+changelog "core/linux-desktop/spkgbuild" "Upgraded from version 6.1.7 to version 6.1.8"
+sed -i -e "s/version=6.1.7/version=6.1.8/g" core/linux-router/spkgbuild
+changelog "core/linux-router/spkgbuild" "Upgraded from version 6.1.7 to version 6.1.8"
+sed -i -e "s/version=5.1.2/version=5.2.1/g" core/streamlink/spkgbuild
+changelog "core/streamlink/spkgbuild" "Upgraded from version 5.1.2 to version 5.2.1"
+sed -i -e "s/version=3.5.19/version=3.5.20/g" extra/flexget/spkgbuild
+changelog "extra/flexget/spkgbuild" "Upgraded from version 3.5.19 to version 3.5.20"
+sed -i -e "s/version=1.22/version=1.23/g" extra/iotop/spkgbuild
+changelog "extra/iotop/spkgbuild" "Upgraded from version 1.22 to version 1.23"
+sed -i -e "s/version=1.21.90/version=1.22.0/g" media/gst-libav/spkgbuild
+changelog "media/gst-libav/spkgbuild" "Upgraded from version 1.21.90 to version 1.22.0"
+sed -i -e "s/version=1.21.90/version=1.22.0/g" media/gst-plugins-bad/spkgbuild
+changelog "media/gst-plugins-bad/spkgbuild" "Upgraded from version 1.21.90 to version 1.22.0"
+sed -i -e "s/version=1.21.90/version=1.22.0/g" media/gst-plugins-base/spkgbuild
+changelog "media/gst-plugins-base/spkgbuild" "Upgraded from version 1.21.90 to version 1.22.0"
+sed -i -e "s/version=1.21.90/version=1.22.0/g" media/gst-plugins-good/spkgbuild
+changelog "media/gst-plugins-good/spkgbuild" "Upgraded from version 1.21.90 to version 1.22.0"
+sed -i -e "s/version=1.21.90/version=1.22.0/g" media/gst-plugins-ugly/spkgbuild
+changelog "media/gst-plugins-ugly/spkgbuild" "Upgraded from version 1.21.90 to version 1.22.0"
+sed -i -e "s/version=1.21.90/version=1.22.0/g" media/gstreamer/spkgbuild
+changelog "media/gstreamer/spkgbuild" "Upgraded from version 1.21.90 to version 1.22.0"
+sed -i -e "s/version=110.0b1/version=110.0b2/g" networking/thunderbird/spkgbuild
+changelog "networking/thunderbird/spkgbuild" "Upgraded from version 110.0b1 to version 110.0b2"
+sed -i -e "s/version=1.57.0/version=1.58.1/g" python/python-aws-sam-translator/spkgbuild
+changelog "python/python-aws-sam-translator/spkgbuild" "Upgraded from version 1.57.0 to version 1.58.1"
+sed -i -e "s/version=1.26.54/version=1.26.56/g" python/python-boto3/spkgbuild
+changelog "python/python-boto3/spkgbuild" "Upgraded from version 1.26.54 to version 1.26.56"
+sed -i -e "s/version=1.29.54/version=1.29.56/g" python/python-botocore/spkgbuild
+changelog "python/python-botocore/spkgbuild" "Upgraded from version 1.29.54 to version 1.29.56"
+sed -i -e "s/version=16.6.0/version=16.6.1/g" python/python-faker/spkgbuild
+changelog "python/python-faker/spkgbuild" "Upgraded from version 16.6.0 to version 16.6.1"
+sed -i -e "s/version=5.0.2/version=5.1.0/g" python/python-flask-security-too/spkgbuild
+changelog "python/python-flask-security-too/spkgbuild" "Upgraded from version 5.0.2 to version 5.1.0"
+sed -i -e "s/version=2.73.0/version=2.74.0/g" python/python-google-api-python-client/spkgbuild
+changelog "python/python-google-api-python-client/spkgbuild" "Upgraded from version 2.73.0 to version 2.74.0"
+sed -i -e "s/version=6.64.0/version=6.65.0/g" python/python-hypothesis/spkgbuild
+changelog "python/python-hypothesis/spkgbuild" "Upgraded from version 6.64.0 to version 6.65.0"
+sed -i -e "s/version=5.1.3/version=5.1.5/g" python/python-jupyter_core/spkgbuild
+changelog "python/python-jupyter_core/spkgbuild" "Upgraded from version 5.1.3 to version 5.1.5"
+sed -i -e "s/version=2.21.0/version=3.0.0/g" python/python-pre-commit/spkgbuild
+changelog "python/python-pre-commit/spkgbuild" "Upgraded from version 2.21.0 to version 3.0.0"
+sed -i -e "s/version=0.15.0/version=0.16.0/g" python/python-prometheus_client/spkgbuild
+changelog "python/python-prometheus_client/spkgbuild" "Upgraded from version 0.15.0 to version 0.16.0"
+sed -i -e "s/version=1.2.0/version=1.2.1/g" python/python-scikit-learn/spkgbuild
+changelog "python/python-scikit-learn/spkgbuild" "Upgraded from version 1.2.0 to version 1.2.1"
