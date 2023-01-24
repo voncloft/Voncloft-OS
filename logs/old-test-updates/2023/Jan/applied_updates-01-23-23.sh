f98ff50170942f1518d75a642a83ca1f41f054a5
Voncloft <voncloft@gmail.com>
2023-01-23 20:26:34 -0500

    Minor Updates

---
+++ logs/old-test-updates/2023/Jan/applied_updates-01-23-23.sh
@@ -0,0 +1,34 @@
+sed -i -e "s/version=1.9.3/version=1.9.4/g" compilers/pkgconf/spkgbuild
+changelog "compilers/pkgconf/spkgbuild" "Upgraded from version 1.9.3 to version 1.9.4"
+sed -i -e "s/version=4.97.0/version=4.98.0/g" core/lsof/spkgbuild
+changelog "core/lsof/spkgbuild" "Upgraded from version 4.97.0 to version 4.98.0"
+sed -i -e "s/version=7.0.1/version=7.0.2/g" core/texinfo/spkgbuild
+changelog "core/texinfo/spkgbuild" "Upgraded from version 7.0.1 to version 7.0.2"
+sed -i -e "s/version=3.5.18/version=3.5.19/g" extra/flexget/spkgbuild
+changelog "extra/flexget/spkgbuild" "Upgraded from version 3.5.18 to version 3.5.19"
+sed -i -e "s/version=0.30.2/version=0.30.3/g" libs/cmark/spkgbuild
+changelog "libs/cmark/spkgbuild" "Upgraded from version 0.30.2 to version 0.30.3"
+sed -i -e "s/version=4.0.5/version=4.1.0/g" libs/libimagequant/spkgbuild
+changelog "libs/libimagequant/spkgbuild" "Upgraded from version 4.0.5 to version 4.1.0"
+sed -i -e "s/version=2.5.0/version=2.6.0/g" libs/libwacom/spkgbuild
+changelog "libs/libwacom/spkgbuild" "Upgraded from version 2.5.0 to version 2.6.0"
+sed -i -e "s/version=110.0b3/version=110.0b4/g" networking/firefox/spkgbuild
+changelog "networking/firefox/spkgbuild" "Upgraded from version 110.0b3 to version 110.0b4"
+sed -i -e "s/version=5.2.1/version=5.3.0/g" python/python-cachetools/spkgbuild
+changelog "python/python-cachetools/spkgbuild" "Upgraded from version 5.2.1 to version 5.3.0"
+sed -i -e "s/version=6.63.0/version=6.64.0/g" python/python-hypothesis/spkgbuild
+changelog "python/python-hypothesis/spkgbuild" "Upgraded from version 6.63.0 to version 6.64.0"
+sed -i -e "s/version=2.5.13/version=2.5.15/g" python/python-identify/spkgbuild
+changelog "python/python-identify/spkgbuild" "Upgraded from version 2.5.13 to version 2.5.15"
+sed -i -e "s/version=2.4.6/version=2.4.7/g" python/python-markdown2/spkgbuild
+changelog "python/python-markdown2/spkgbuild" "Upgraded from version 2.4.6 to version 2.4.7"
+sed -i -e "s/version=2.4.4/version=2.4.5/g" ruby-gems/ruby-bundler/spkgbuild
+changelog "ruby-gems/ruby-bundler/spkgbuild" "Upgraded from version 2.4.4 to version 2.4.5"
+sed -i -e "s/version=3.11.53/version=3.11.54/g" server/blacklist-hosts/spkgbuild
+changelog "server/blacklist-hosts/spkgbuild" "Upgraded from version 3.11.53 to version 3.11.54"
+sed -i -e "s/version=2023.1.6/version=2023.1.7/g" server/homeassistant/spkgbuild
+changelog "server/homeassistant/spkgbuild" "Upgraded from version 2023.1.6 to version 2023.1.7"
+sed -i -e "s/version=3.7.3/version=3.7.4/g" server/postfix/spkgbuild
+changelog "server/postfix/spkgbuild" "Upgraded from version 3.7.3 to version 3.7.4"
+sed -i -e "s/version=0.7.1/version=0.7.2/g" xfce/xfce4-notifyd/spkgbuild
+changelog "xfce/xfce4-notifyd/spkgbuild" "Upgraded from version 0.7.1 to version 0.7.2"
