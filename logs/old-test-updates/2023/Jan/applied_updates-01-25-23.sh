2b9ca5d87c27a8671c2ad1a1e372cd95b714012f
Voncloft <voncloft@gmail.com>
2023-01-25 16:26:24 -0500

    Minor Updates

---
+++ logs/old-test-updates/2023/Jan/applied_updates-01-25-23.sh
@@ -0,0 +1,30 @@
+sed -i -e "s/version=6.1.2/version=6.1.3/g" core/btrfs-progs/spkgbuild
+changelog "core/btrfs-progs/spkgbuild" "Upgraded from version 6.1.2 to version 6.1.3"
+sed -i -e "s/version=3.5.20/version=3.5.21/g" extra/flexget/spkgbuild
+changelog "extra/flexget/spkgbuild" "Upgraded from version 3.5.20 to version 3.5.21"
+sed -i -e "s/version=3.0.8/version=3.0.9/g" libs/guile/spkgbuild
+changelog "libs/guile/spkgbuild" "Upgraded from version 3.0.8 to version 3.0.9"
+sed -i -e "s/version=9.19.8/version=9.19.9/g" networking/bind/spkgbuild
+changelog "networking/bind/spkgbuild" "Upgraded from version 9.19.8 to version 9.19.9"
+sed -i -e "s/version=110.0b4/version=110.0b5/g" networking/firefox/spkgbuild
+changelog "networking/firefox/spkgbuild" "Upgraded from version 110.0b4 to version 110.0b5"
+sed -i -e "s/version=2.5.8/version=2.6.0/g" networking/openvpn/spkgbuild
+changelog "networking/openvpn/spkgbuild" "Upgraded from version 2.5.8 to version 2.6.0"
+sed -i -e "s/version=3.0/version=3.0.1/g" nonfree/pandoc-bin/spkgbuild
+changelog "nonfree/pandoc-bin/spkgbuild" "Upgraded from version 3.0 to version 3.0.1"
+sed -i -e "s/version=0.11.2/version=0.12.0/g" python/python-bitcoinlib/spkgbuild
+changelog "python/python-bitcoinlib/spkgbuild" "Upgraded from version 0.11.2 to version 0.12.0"
+sed -i -e "s/version=4.18.0/version=4.18.1/g" python/python-braintree/spkgbuild
+changelog "python/python-braintree/spkgbuild" "Upgraded from version 4.18.0 to version 4.18.1"
+sed -i -e "s/version=2.14.1/version=2.14.2/g" python/python-connexion/spkgbuild
+changelog "python/python-connexion/spkgbuild" "Upgraded from version 2.14.1 to version 2.14.2"
+sed -i -e "s/version=7.0.5/version=7.1.0/g" python/python-coverage/spkgbuild
+changelog "python/python-coverage/spkgbuild" "Upgraded from version 7.0.5 to version 7.1.0"
+sed -i -e "s/version=3.23.0/version=3.24.0/g" python/python-openmdao/spkgbuild
+changelog "python/python-openmdao/spkgbuild" "Upgraded from version 3.23.0 to version 3.24.0"
+sed -i -e "s/version=3.11.54/version=3.11.55/g" server/blacklist-hosts/spkgbuild
+changelog "server/blacklist-hosts/spkgbuild" "Upgraded from version 3.11.54 to version 3.11.55"
+sed -i -e "s/version=2023.1.7/version=2023.2.0/g" server/homeassistant/spkgbuild
+changelog "server/homeassistant/spkgbuild" "Upgraded from version 2023.1.7 to version 2023.2.0"
+sed -i -e "s/version=1.30.1.6562/version=1.30.2.6563/g" server/plex-media-server/spkgbuild
+changelog "server/plex-media-server/spkgbuild" "Upgraded from version 1.30.1.6562 to version 1.30.2.6563"
