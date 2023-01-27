516a831d46ea4f2a727a2cc35a54e3a900ec13ae
Voncloft <voncloft@gmail.com>
2023-01-27 17:24:07 -0500

    Minor Updates

--- logs/old-test-updates/2023/Jan/applied_updates-01-27-23.sh
+++ logs/old-test-updates/2023/Jan/applied_updates-01-27-23.sh
@@ -20,3 +20,21 @@ sed -i -e "s/version=3.0.0/version=3.0.1/g" python/python-pre-commit/spkgbuild
 changelog "python/python-pre-commit/spkgbuild" "Upgraded from version 3.0.0 to version 3.0.1"
 sed -i -e "s/version=4.17.4/version=4.17.5/g" server/samba/spkgbuild
 changelog "server/samba/spkgbuild" "Upgraded from version 4.17.4 to version 4.17.5"
+sed -i -e "s/version=2.37.1/version=2.37.2/g" extra/discordchatexporter/spkgbuild
+changelog "extra/discordchatexporter/spkgbuild" "Upgraded from version 2.37.1 to version 2.37.2"
+sed -i -e "s/version=2.14.1/version=2.14.2/g" fonts/fontconfig/spkgbuild
+changelog "fonts/fontconfig/spkgbuild" "Upgraded from version 2.14.1 to version 2.14.2"
+sed -i -e "s/version=110.0b5/version=110.0b6/g" networking/firefox/spkgbuild
+changelog "networking/firefox/spkgbuild" "Upgraded from version 110.0b5 to version 110.0b6"
+sed -i -e "s/version=0.72.9/version=0.72.10/g" python/python-cfn-lint/spkgbuild
+changelog "python/python-cfn-lint/spkgbuild" "Upgraded from version 0.72.9 to version 0.72.10"
+sed -i -e "s/version=1.17/version=1.18/g" python/python-funcy/spkgbuild
+changelog "python/python-funcy/spkgbuild" "Upgraded from version 1.17 to version 1.18"
+sed -i -e "s/version=6.65.1/version=6.65.2/g" python/python-hypothesis/spkgbuild
+changelog "python/python-hypothesis/spkgbuild" "Upgraded from version 6.65.1 to version 6.65.2"
+sed -i -e "s/version=3.24.0/version=3.25.0/g" python/python-openmdao/spkgbuild
+changelog "python/python-openmdao/spkgbuild" "Upgraded from version 3.24.0 to version 3.25.0"
+sed -i -e "s/version=1.2.2/version=1.3.0/g" python/python-pycapnp/spkgbuild
+changelog "python/python-pycapnp/spkgbuild" "Upgraded from version 1.2.2 to version 1.3.0"
+sed -i -e "s/version=13.2.0/version=13.3.0/g" python/python-rich/spkgbuild
+changelog "python/python-rich/spkgbuild" "Upgraded from version 13.2.0 to version 13.3.0"
