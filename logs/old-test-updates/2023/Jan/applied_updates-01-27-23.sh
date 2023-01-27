b69a8616f353a17dd932c3901c19235232ba5a21
Voncloft <voncloft@gmail.com>
2023-01-27 05:38:59 -0500

    Minor Updates

---
+++ logs/old-test-updates/2023/Jan/applied_updates-01-27-23.sh
@@ -0,0 +1,22 @@
+sed -i -e "s/version=1.66.1/version=1.67.0/g" compilers/rust/spkgbuild
+changelog "compilers/rust/spkgbuild" "Upgraded from version 1.66.1 to version 1.67.0"
+sed -i -e "s/version=0.3.64/version=0.3.65/g" core/pipewire/spkgbuild
+changelog "core/pipewire/spkgbuild" "Upgraded from version 0.3.64 to version 0.3.65"
+sed -i -e "s/version=1.43.0/version=1.43.1/g" networking/networkmanager/spkgbuild
+changelog "networking/networkmanager/spkgbuild" "Upgraded from version 1.43.0 to version 1.43.1"
+sed -i -e "s/version=7.4.4/version=7.4.5/g" nonfree/libreoffice/spkgbuild
+changelog "nonfree/libreoffice/spkgbuild" "Upgraded from version 7.4.4 to version 7.4.5"
+sed -i -e "s/version=1.26.56/version=1.26.58/g" python/python-boto3/spkgbuild
+changelog "python/python-boto3/spkgbuild" "Upgraded from version 1.26.56 to version 1.26.58"
+sed -i -e "s/version=1.29.56/version=1.29.58/g" python/python-botocore/spkgbuild
+changelog "python/python-botocore/spkgbuild" "Upgraded from version 1.29.56 to version 1.29.58"
+sed -i -e "s/version=8.6.0/version=8.6.1/g" python/python-elasticsearch/spkgbuild
+changelog "python/python-elasticsearch/spkgbuild" "Upgraded from version 8.6.0 to version 8.6.1"
+sed -i -e "s/version=6.65.0/version=6.65.1/g" python/python-hypothesis/spkgbuild
+changelog "python/python-hypothesis/spkgbuild" "Upgraded from version 6.65.0 to version 6.65.1"
+sed -i -e "s/version=25.3.0/version=26.1.0/g" python/python-kubernetes/spkgbuild
+changelog "python/python-kubernetes/spkgbuild" "Upgraded from version 25.3.0 to version 26.1.0"
+sed -i -e "s/version=3.0.0/version=3.0.1/g" python/python-pre-commit/spkgbuild
+changelog "python/python-pre-commit/spkgbuild" "Upgraded from version 3.0.0 to version 3.0.1"
+sed -i -e "s/version=4.17.4/version=4.17.5/g" server/samba/spkgbuild
+changelog "server/samba/spkgbuild" "Upgraded from version 4.17.4 to version 4.17.5"
