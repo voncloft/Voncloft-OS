99539b7d5c8c542f9db41fa5e6a504bc8359a93f
Voncloft <voncloft@gmail.com>
2023-01-28 20:54:46 -0500

    Minor Updates

---
+++ logs/old-test-updates/2023/Jan/applied_updates-01-28-23.sh
@@ -0,0 +1,28 @@
+sed -i -e "s/version=0.2.8/version=2.9.4/g" core/mps-youtube/spkgbuild
+changelog "core/mps-youtube/spkgbuild" "Upgraded from version 0.2.8 to version 2.9.4"
+sed -i -e "s/version=0.35.0/version=0.35.1/g" core/mpv/spkgbuild
+changelog "core/mpv/spkgbuild" "Upgraded from version 0.35.0 to version 0.35.1"
+sed -i -e "s/version=1.26.58/version=1.26.59/g" python/python-boto3/spkgbuild
+changelog "python/python-boto3/spkgbuild" "Upgraded from version 1.26.58 to version 1.26.59"
+sed -i -e "s/version=1.29.58/version=1.29.59/g" python/python-botocore/spkgbuild
+changelog "python/python-botocore/spkgbuild" "Upgraded from version 1.29.58 to version 1.29.59"
+sed -i -e "s/version=2023.1.0/version=2023.1.1/g" python/python-distributed/spkgbuild
+changelog "python/python-distributed/spkgbuild" "Upgraded from version 2023.1.0 to version 2023.1.1"
+sed -i -e "s/version=2.5.15/version=2.5.16/g" python/python-identify/spkgbuild
+changelog "python/python-identify/spkgbuild" "Upgraded from version 2.5.15 to version 2.5.16"
+sed -i -e "s/version=1.4.0/version=1.5.0/g" python/python-poetry-core/spkgbuild
+changelog "python/python-poetry-core/spkgbuild" "Upgraded from version 1.4.0 to version 1.5.0"
+sed -i -e "s/version=3.16.0/version=3.17.0/g" python/python-pycryptodome/spkgbuild
+changelog "python/python-pycryptodome/spkgbuild" "Upgraded from version 3.16.0 to version 3.17.0"
+sed -i -e "s/version=5.3.4/version=5.3.5/g" python/python-pynamodb/spkgbuild
+changelog "python/python-pynamodb/spkgbuild" "Upgraded from version 5.3.4 to version 5.3.5"
+sed -i -e "s/version=2.0.0/version=2.1.0/g" python/python-pytest-enabler/spkgbuild
+changelog "python/python-pytest-enabler/spkgbuild" "Upgraded from version 2.0.0 to version 2.1.0"
+sed -i -e "s/version=13.3.0/version=13.3.1/g" python/python-rich/spkgbuild
+changelog "python/python-rich/spkgbuild" "Upgraded from version 13.3.0 to version 13.3.1"
+sed -i -e "s/version=66.1.1/version=67.0.0/g" python/python-setuptools/spkgbuild
+changelog "python/python-setuptools/spkgbuild" "Upgraded from version 66.1.1 to version 67.0.0"
+sed -i -e "s/version=3.11.0/version=3.12.0/g" python/python-zipp/spkgbuild
+changelog "python/python-zipp/spkgbuild" "Upgraded from version 3.11.0 to version 3.12.0"
+sed -i -e "s/version=3.11.55/version=3.12.1/g" server/blacklist-hosts/spkgbuild
+changelog "server/blacklist-hosts/spkgbuild" "Upgraded from version 3.11.55 to version 3.12.1"
