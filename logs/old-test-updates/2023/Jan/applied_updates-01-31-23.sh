2cc20b7607b60adcc99e4b74f0cf83ea289298dd
Voncloft <voncloft@gmail.com>
2023-01-31 07:20:00 -0500

    Minor Updates

---
+++ logs/old-test-updates/2023/Jan/applied_updates-01-31-23.sh
@@ -0,0 +1,24 @@
+sed -i -e "s/version=2.39.5/version=2.39.6/g" core/webkitgtk/spkgbuild
+changelog "core/webkitgtk/spkgbuild" "Upgraded from version 2.39.5 to version 2.39.6"
+sed -i -e "s/version=1.18.1/version=1.19.0/g" libs/c-ares/spkgbuild
+changelog "libs/c-ares/spkgbuild" "Upgraded from version 1.18.1 to version 1.19.0"
+sed -i -e "s/version=110.0b6/version=110.0b7/g" networking/firefox/spkgbuild
+changelog "networking/firefox/spkgbuild" "Upgraded from version 110.0b6 to version 110.0b7"
+sed -i -e "s/version=4.0.2/version=4.0.3/g" python/python-flask-migrate/spkgbuild
+changelog "python/python-flask-migrate/spkgbuild" "Upgraded from version 4.0.2 to version 4.0.3"
+sed -i -e "s/version=2.5.16/version=2.5.17/g" python/python-identify/spkgbuild
+changelog "python/python-identify/spkgbuild" "Upgraded from version 2.5.16 to version 2.5.17"
+sed -i -e "s/version=6.20.2/version=6.21.0/g" python/python-ipykernel/spkgbuild
+changelog "python/python-ipykernel/spkgbuild" "Upgraded from version 6.20.2 to version 6.21.0"
+sed -i -e "s/version=5.1.5/version=5.2.0/g" python/python-jupyter_core/spkgbuild
+changelog "python/python-jupyter_core/spkgbuild" "Upgraded from version 5.1.5 to version 5.2.0"
+sed -i -e "s/version=0.20.0/version=0.21.0/g" python/python-libtmux/spkgbuild
+changelog "python/python-libtmux/spkgbuild" "Upgraded from version 0.20.0 to version 0.21.0"
+sed -i -e "s/version=8.13.4/version=8.13.5/g" python/python-phonenumbers/spkgbuild
+changelog "python/python-phonenumbers/spkgbuild" "Upgraded from version 8.13.4 to version 8.13.5"
+sed -i -e "s/version=3.0.1/version=3.0.2/g" python/python-pre-commit/spkgbuild
+changelog "python/python-pre-commit/spkgbuild" "Upgraded from version 3.0.1 to version 3.0.2"
+sed -i -e "s/version=5.3.5/version=5.4.0/g" python/python-pynamodb/spkgbuild
+changelog "python/python-pynamodb/spkgbuild" "Upgraded from version 5.3.5 to version 5.4.0"
+sed -i -e "s/version=0.6/version=0.7/g" python/python-whatever/spkgbuild
+changelog "python/python-whatever/spkgbuild" "Upgraded from version 0.6 to version 0.7"
