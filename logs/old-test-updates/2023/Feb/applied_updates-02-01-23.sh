2030d5f48e4ebf0d2fe2618d1984895015303452
Voncloft <voncloft@gmail.com>
2023-02-01 05:25:38 -0500

    Minor Updates

---
+++ logs/old-test-updates/2023/Feb/applied_updates-02-01-23.sh
@@ -0,0 +1,36 @@
+sed -i -e "s/version=3.7.13/version=3.7.14/g" core/recode/spkgbuild
+changelog "core/recode/spkgbuild" "Upgraded from version 3.7.13 to version 3.7.14"
+sed -i -e "s/version=2.39.6/version=2.39.7/g" core/webkitgtk/spkgbuild
+changelog "core/webkitgtk/spkgbuild" "Upgraded from version 2.39.6 to version 2.39.7"
+sed -i -e "s/version=1.7.0/version=1.7.1/g" libs/apr/spkgbuild
+changelog "libs/apr/spkgbuild" "Upgraded from version 1.7.0 to version 1.7.1"
+sed -i -e "s/version=1.6.1/version=1.6.2/g" libs/apr-util/spkgbuild
+changelog "libs/apr-util/spkgbuild" "Upgraded from version 1.6.1 to version 1.6.2"
+sed -i -e "s/version=2.1.4/version=2.1.5/g" libs/libjpeg-turbo/spkgbuild
+changelog "libs/libjpeg-turbo/spkgbuild" "Upgraded from version 2.1.4 to version 2.1.5"
+sed -i -e "s/version=110.0b2/version=110.0b3/g" networking/thunderbird/spkgbuild
+changelog "networking/thunderbird/spkgbuild" "Upgraded from version 110.0b2 to version 110.0b3"
+sed -i -e "s/version=2.13.3/version=2.14.0/g" python/python-astroid/spkgbuild
+changelog "python/python-astroid/spkgbuild" "Upgraded from version 2.13.3 to version 2.14.0"
+sed -i -e "s/version=1.26.59/version=1.26.60/g" python/python-boto3/spkgbuild
+changelog "python/python-boto3/spkgbuild" "Upgraded from version 1.26.59 to version 1.26.60"
+sed -i -e "s/version=1.29.59/version=1.29.60/g" python/python-botocore/spkgbuild
+changelog "python/python-botocore/spkgbuild" "Upgraded from version 1.29.59 to version 1.29.60"
+sed -i -e "s/version=3.0.2/version=3.0.3/g" python/python-flask-sqlalchemy/spkgbuild
+changelog "python/python-flask-sqlalchemy/spkgbuild" "Upgraded from version 3.0.2 to version 3.0.3"
+sed -i -e "s/version=3.12.0/version=3.13.0/g" python/python-gitlab/spkgbuild
+changelog "python/python-gitlab/spkgbuild" "Upgraded from version 3.12.0 to version 3.13.0"
+sed -i -e "s/version=2.74.0/version=2.75.0/g" python/python-google-api-python-client/spkgbuild
+changelog "python/python-google-api-python-client/spkgbuild" "Upgraded from version 2.74.0 to version 2.75.0"
+sed -i -e "s/version=3.2.0/version=3.3.0/g" python/python-internetarchive/spkgbuild
+changelog "python/python-internetarchive/spkgbuild" "Upgraded from version 3.2.0 to version 3.3.0"
+sed -i -e "s/version=0.103.0/version=1.0.0/g" python/python-openstacksdk/spkgbuild
+changelog "python/python-openstacksdk/spkgbuild" "Upgraded from version 0.103.0 to version 1.0.0"
+sed -i -e "s/version=22.3.1/version=23.0/g" python/python-pip/spkgbuild
+changelog "python/python-pip/spkgbuild" "Upgraded from version 22.3.1 to version 23.0"
+sed -i -e "s/version=3.18.1/version=3.18.2/g" python/python-trimesh/spkgbuild
+changelog "python/python-trimesh/spkgbuild" "Upgraded from version 3.18.1 to version 3.18.2"
+sed -i -e "s/version=2.4.5/version=2.4.6/g" ruby-gems/ruby-bundler/spkgbuild
+changelog "ruby-gems/ruby-bundler/spkgbuild" "Upgraded from version 2.4.5 to version 2.4.6"
+sed -i -e "s/version=3.12.1/version=3.12.2/g" server/blacklist-hosts/spkgbuild
+changelog "server/blacklist-hosts/spkgbuild" "Upgraded from version 3.12.1 to version 3.12.2"
