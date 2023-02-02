df044209f5f903c07abba10215ff03882aca8ef0
Voncloft <voncloft@gmail.com>
2023-02-02 07:34:47 -0500

    Minor Updates

---
+++ logs/old-test-updates/2023/Feb/applied_updates-02-02-23.sh
@@ -0,0 +1,36 @@
+sed -i -e "s/version=6.1.8/version=6.1.9/g" core/linux-api-headers/spkgbuild
+changelog "core/linux-api-headers/spkgbuild" "Upgraded from version 6.1.8 to version 6.1.9"
+sed -i -e "s/version=6.1.8/version=6.1.9/g" core/linux-desktop/spkgbuild
+changelog "core/linux-desktop/spkgbuild" "Upgraded from version 6.1.8 to version 6.1.9"
+sed -i -e "s/version=6.1.8/version=6.1.9/g" core/linux-router/spkgbuild
+changelog "core/linux-router/spkgbuild" "Upgraded from version 6.1.8 to version 6.1.9"
+sed -i -e "s/version=0.4.11/version=0.4.12/g" core/poppler-data/spkgbuild
+changelog "core/poppler-data/spkgbuild" "Upgraded from version 0.4.11 to version 0.4.12"
+sed -i -e "s/version=32.0/version=33.0/g" media/mythtv/spkgbuild
+changelog "media/mythtv/spkgbuild" "Upgraded from version 32.0 to version 33.0"
+sed -i -e "s/version=1.7.1/version=1.7.2/g" libs/apr/spkgbuild
+changelog "libs/apr/spkgbuild" "Upgraded from version 1.7.1 to version 1.7.2"
+sed -i -e "s/version=1.6.2/version=1.6.3/g" libs/apr-util/spkgbuild
+changelog "libs/apr-util/spkgbuild" "Upgraded from version 1.6.2 to version 1.6.3"
+sed -i -e "s/version=2.1.5/version=2.1.90/g" libs/libjpeg-turbo/spkgbuild
+changelog "libs/libjpeg-turbo/spkgbuild" "Upgraded from version 2.1.5 to version 2.1.90"
+sed -i -e "s/version=23.01.0/version=23.02.0/g" libs/poppler/spkgbuild
+changelog "libs/poppler/spkgbuild" "Upgraded from version 23.01.0 to version 23.02.0"
+sed -i -e "s/version=110.0b7/version=110.0b8/g" networking/firefox/spkgbuild
+changelog "networking/firefox/spkgbuild" "Upgraded from version 110.0b7 to version 110.0b8"
+sed -i -e "s/version=2.14.0/version=2.14.1/g" python/python-astroid/spkgbuild
+changelog "python/python-astroid/spkgbuild" "Upgraded from version 2.14.0 to version 2.14.1"
+sed -i -e "s/version=1.26.60/version=1.26.62/g" python/python-boto3/spkgbuild
+changelog "python/python-boto3/spkgbuild" "Upgraded from version 1.26.60 to version 1.26.62"
+sed -i -e "s/version=1.29.60/version=1.29.62/g" python/python-botocore/spkgbuild
+changelog "python/python-botocore/spkgbuild" "Upgraded from version 1.29.60 to version 1.29.62"
+sed -i -e "s/version=0.18.7/version=0.18.8/g" python/python-django-modeltranslation/spkgbuild
+changelog "python/python-django-modeltranslation/spkgbuild" "Upgraded from version 0.18.7 to version 0.18.8"
+sed -i -e "s/version=3.0.2/version=4.0.0/g" python/python-elementpath/spkgbuild
+changelog "python/python-elementpath/spkgbuild" "Upgraded from version 3.0.2 to version 4.0.0"
+sed -i -e "s/version=0.5.2/version=0.5.3/g" python/python-openapi-spec-validator/spkgbuild
+changelog "python/python-openapi-spec-validator/spkgbuild" "Upgraded from version 0.5.2 to version 0.5.3"
+sed -i -e "s/version=7.3.1/version=7.4/g" python/python-qrcode/spkgbuild
+changelog "python/python-qrcode/spkgbuild" "Upgraded from version 7.3.1 to version 7.4"
+sed -i -e "s/version=67.0.0/version=67.1.0/g" python/python-setuptools/spkgbuild
+changelog "python/python-setuptools/spkgbuild" "Upgraded from version 67.0.0 to version 67.1.0"
