4bd21833950b01b841ed19c2d176a7fffa1f04f6
Voncloft <voncloft@gmail.com>
2023-02-04 20:28:24 -0500

    Minor Updates

---
+++ logs/old-test-updates/2023/Feb/applied_updates-02-04-23.sh
@@ -0,0 +1,46 @@
+sed -i -e "s/version=3.25.2/version=3.26.0-rc1/g" compilers/cmake/spkgbuild
+changelog "compilers/cmake/spkgbuild" "Upgraded from version 3.25.2 to version 3.26.0-rc1"
+sed -i -e "s/version=3.13.0/version=3.13.1/g" core/fuse2/spkgbuild
+changelog "core/fuse2/spkgbuild" "Upgraded from version 3.13.0 to version 3.13.1"
+sed -i -e "s/version=3.13.0/version=3.13.1/g" core/fuse3/spkgbuild
+changelog "core/fuse3/spkgbuild" "Upgraded from version 3.13.0 to version 3.13.1"
+sed -i -e "s/version=1.22.2/version=1.23.0/g" media/openal/spkgbuild
+changelog "media/openal/spkgbuild" "Upgraded from version 1.22.2 to version 1.23.0"
+sed -i -e "s/version=2.66/version=2.67/g" libs/libcap/spkgbuild
+changelog "libs/libcap/spkgbuild" "Upgraded from version 2.66 to version 2.67"
+sed -i -e "s/version=1.14.0/version=1.14.1/g" libs/libwpe/spkgbuild
+changelog "libs/libwpe/spkgbuild" "Upgraded from version 1.14.0 to version 1.14.1"
+sed -i -e "s/version=110.0b8/version=110.0b9/g" networking/firefox/spkgbuild
+changelog "networking/firefox/spkgbuild" "Upgraded from version 110.0b8 to version 110.0b9"
+sed -i -e "s/version=1.12.90/version=1.13.0/g" networking/tigervnc/spkgbuild
+changelog "networking/tigervnc/spkgbuild" "Upgraded from version 1.12.90 to version 1.13.0"
+sed -i -e "s/version=1.11.0/version=1.12.1/g" python/python-beaker/spkgbuild
+changelog "python/python-beaker/spkgbuild" "Upgraded from version 1.11.0 to version 1.12.1"
+sed -i -e "s/version=1.26.62/version=1.26.64/g" python/python-boto3/spkgbuild
+changelog "python/python-boto3/spkgbuild" "Upgraded from version 1.26.62 to version 1.26.64"
+sed -i -e "s/version=1.29.62/version=1.29.64/g" python/python-botocore/spkgbuild
+changelog "python/python-botocore/spkgbuild" "Upgraded from version 1.29.62 to version 1.29.64"
+sed -i -e "s/version=4.0.3/version=4.0.4/g" python/python-flask-migrate/spkgbuild
+changelog "python/python-flask-migrate/spkgbuild" "Upgraded from version 4.0.3 to version 4.0.4"
+sed -i -e "s/version=2.75.0/version=2.76.0/g" python/python-google-api-python-client/spkgbuild
+changelog "python/python-google-api-python-client/spkgbuild" "Upgraded from version 2.75.0 to version 2.76.0"
+sed -i -e "s/version=6.66.0/version=6.66.2/g" python/python-hypothesis/spkgbuild
+changelog "python/python-hypothesis/spkgbuild" "Upgraded from version 6.66.0 to version 6.66.2"
+sed -i -e "s/version=2022.12.19/version=2023.2.4/g" python/python-pipenv/spkgbuild
+changelog "python/python-pipenv/spkgbuild" "Upgraded from version 2022.12.19 to version 2023.2.4"
+sed -i -e "s/version=3.0.3/version=3.0.4/g" python/python-pre-commit/spkgbuild
+changelog "python/python-pre-commit/spkgbuild" "Upgraded from version 3.0.3 to version 3.0.4"
+sed -i -e "s/version=7.3.2209181503/version=7.3.2302031449/g" python/python-pycontracts/spkgbuild
+changelog "python/python-pycontracts/spkgbuild" "Upgraded from version 7.3.2209181503 to version 7.3.2302031449"
+sed -i -e "s/version=0.5.1/version=0.6.0/g" python/python-pytest-socket/spkgbuild
+changelog "python/python-pytest-socket/spkgbuild" "Upgraded from version 0.5.1 to version 0.6.0"
+sed -i -e "s/version=7.4/version=7.4.1/g" python/python-qrcode/spkgbuild
+changelog "python/python-qrcode/spkgbuild" "Upgraded from version 7.4 to version 7.4.1"
+sed -i -e "s/version=3.0.7/version=3.0.8/g" python/python-xlsxwriter/spkgbuild
+changelog "python/python-xlsxwriter/spkgbuild" "Upgraded from version 3.0.7 to version 3.0.8"
+sed -i -e "s/version=3.12.0/version=3.12.1/g" ruby-gems/ruby-rspec-core/spkgbuild
+changelog "ruby-gems/ruby-rspec-core/spkgbuild" "Upgraded from version 3.12.0 to version 3.12.1"
+sed -i -e "s/version=2023.2.0/version=2023.2.1/g" server/homeassistant/spkgbuild
+changelog "server/homeassistant/spkgbuild" "Upgraded from version 2023.2.0 to version 2023.2.1"
+sed -i -e "s/version=2.37/version=2.38/g" xorg/xkeyboard-config/spkgbuild
+changelog "xorg/xkeyboard-config/spkgbuild" "Upgraded from version 2.37 to version 2.38"
