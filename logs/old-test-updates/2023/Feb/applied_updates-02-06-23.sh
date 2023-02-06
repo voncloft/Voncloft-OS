e3354930cf2d5dbf5a4963d40d0cd7cf00df734b
Voncloft <voncloft@gmail.com>
2023-02-06 10:03:50 -0500

    Minor Updates

---
+++ logs/old-test-updates/2023/Feb/applied_updates-02-06-23.sh
@@ -0,0 +1,18 @@
+sed -i -e "s/version=1.9.1/version=1.10/g" core/cabextract/spkgbuild
+changelog "core/cabextract/spkgbuild" "Upgraded from version 1.9.1 to version 1.10"
+sed -i -e "s/version=1.3.5/version=2.0.0/g" core/vsv/spkgbuild
+changelog "core/vsv/spkgbuild" "Upgraded from version 1.3.5 to version 2.0.0"
+sed -i -e "s/version=4.900/version=4.901/g" extra/gnucash/spkgbuild
+changelog "extra/gnucash/spkgbuild" "Upgraded from version 4.900 to version 4.901"
+sed -i -e "s/version=0.5.1/version=0.6.0/g" python/python-aioconsole/spkgbuild
+changelog "python/python-aioconsole/spkgbuild" "Upgraded from version 0.5.1 to version 0.6.0"
+sed -i -e "s/version=6.66.2/version=6.67.0/g" python/python-hypothesis/spkgbuild
+changelog "python/python-hypothesis/spkgbuild" "Upgraded from version 6.66.2 to version 6.67.0"
+sed -i -e "s/version=3.18.1/version=3.18.3/g" python/python-simplejson/spkgbuild
+changelog "python/python-simplejson/spkgbuild" "Upgraded from version 3.18.1 to version 3.18.3"
+sed -i -e "s/version=3.12.0/version=3.12.1/g" python/python-zipp/spkgbuild
+changelog "python/python-zipp/spkgbuild" "Upgraded from version 3.12.0 to version 3.12.1"
+sed -i -e "s/version=2.88/version=2.89/g" server/dnsmasq/spkgbuild
+changelog "server/dnsmasq/spkgbuild" "Upgraded from version 2.88 to version 2.89"
+sed -i -e "s/version=2023.2.1/version=2023.2.2/g" server/homeassistant/spkgbuild
+changelog "server/homeassistant/spkgbuild" "Upgraded from version 2023.2.1 to version 2023.2.2"
