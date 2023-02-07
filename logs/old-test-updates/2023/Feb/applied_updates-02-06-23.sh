cb1b76f900a044376892cdbd4be339382adab89a
Voncloft <voncloft@gmail.com>
2023-02-06 21:57:00 -0500

    Minor Updates

--- logs/old-test-updates/2023/Feb/applied_updates-02-06-23.sh
+++ logs/old-test-updates/2023/Feb/applied_updates-02-06-23.sh
@@ -16,3 +16,33 @@ sed -i -e "s/version=2.88/version=2.89/g" server/dnsmasq/spkgbuild
 changelog "server/dnsmasq/spkgbuild" "Upgraded from version 2.88 to version 2.89"
 sed -i -e "s/version=2023.2.1/version=2023.2.2/g" server/homeassistant/spkgbuild
 changelog "server/homeassistant/spkgbuild" "Upgraded from version 2023.2.1 to version 2023.2.2"
+sed -i -e "s/version=6.1.9/version=6.1.10/g" core/linux-api-headers/spkgbuild
+changelog "core/linux-api-headers/spkgbuild" "Upgraded from version 6.1.9 to version 6.1.10"
+sed -i -e "s/version=6.1.9/version=6.1.10/g" core/linux-desktop/spkgbuild
+changelog "core/linux-desktop/spkgbuild" "Upgraded from version 6.1.9 to version 6.1.10"
+sed -i -e "s/version=6.1.9/version=6.1.10/g" core/linux-router/spkgbuild
+changelog "core/linux-router/spkgbuild" "Upgraded from version 6.1.9 to version 6.1.10"
+sed -i -e "s/version=33.0/version=33.1/g" media/mythtv/spkgbuild
+changelog "media/mythtv/spkgbuild" "Upgraded from version 33.0 to version 33.1"
+sed -i -e "s/version=6.67.0/version=6.67.1/g" python/python-hypothesis/spkgbuild
+changelog "python/python-hypothesis/spkgbuild" "Upgraded from version 6.67.0 to version 6.67.1"
+sed -i -e "s/version=0.23.0/version=0.24.0/g" python/python-meilisearch/spkgbuild
+changelog "python/python-meilisearch/spkgbuild" "Upgraded from version 0.23.0 to version 0.24.0"
+sed -i -e "s/version=0.5.4/version=0.5.5/g" python/python-openapi-spec-validator/spkgbuild
+changelog "python/python-openapi-spec-validator/spkgbuild" "Upgraded from version 0.5.4 to version 0.5.5"
+sed -i -e "s/version=7.3.2302031449/version=7.3.2302061555/g" python/python-pycontracts/spkgbuild
+changelog "python/python-pycontracts/spkgbuild" "Upgraded from version 7.3.2302031449 to version 7.3.2302061555"
+sed -i -e "s/version=7.4.1/version=7.4.2/g" python/python-qrcode/spkgbuild
+changelog "python/python-qrcode/spkgbuild" "Upgraded from version 7.4.1 to version 7.4.2"
+sed -i -e "s/version=0.23.1/version=0.24.0/g" python/python-starlette/spkgbuild
+changelog "python/python-starlette/spkgbuild" "Upgraded from version 0.23.1 to version 0.24.0"
+sed -i -e "s/version=2.0.0/version=2.0.1/g" python/python-sure/spkgbuild
+changelog "python/python-sure/spkgbuild" "Upgraded from version 2.0.0 to version 2.0.1"
+sed -i -e "s/version=20.17.1/version=20.18.0/g" python/python-virtualenv/spkgbuild
+changelog "python/python-virtualenv/spkgbuild" "Upgraded from version 20.17.1 to version 20.18.0"
+sed -i -e "s/version=2.1.1/version=2.2.0/g" python/python-xmlschema/spkgbuild
+changelog "python/python-xmlschema/spkgbuild" "Upgraded from version 2.1.1 to version 2.2.0"
+sed -i -e "s/version=3.12.2/version=3.12.3/g" server/blacklist-hosts/spkgbuild
+changelog "server/blacklist-hosts/spkgbuild" "Upgraded from version 3.12.2 to version 3.12.3"
+sed -i -e "s/version=0.3.0/version=0.4.0/g" xfce/thunar-media-tags-plugin/spkgbuild
+changelog "xfce/thunar-media-tags-plugin/spkgbuild" "Upgraded from version 0.3.0 to version 0.4.0"
