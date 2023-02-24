96df2e64cb9978f5725ef5773d22475746c5b4c5
Voncloft <voncloft@gmail.com>
2023-02-24 15:46:17 -0500

    Minor Updates

---
+++ logs/old-test-updates/2023/Feb/applied_updates-02-24-23.sh
@@ -0,0 +1,70 @@
+sed -i -e "s/version=3.26.0-rc3/version=3.26.0-rc4/g" compilers/cmake/spkgbuild
+changelog "compilers/cmake/spkgbuild" "Upgraded from version 3.26.0-rc3 to version 3.26.0-rc4"
+sed -i -e "s/version=1.0.0/version=1.0.1/g" compilers/meson/spkgbuild
+changelog "compilers/meson/spkgbuild" "Upgraded from version 1.0.0 to version 1.0.1"
+sed -i -e "s/version=1.10/version=1.11/g" core/cabextract/spkgbuild
+changelog "core/cabextract/spkgbuild" "Upgraded from version 1.10 to version 1.11"
+sed -i -e "s/version=1.18.0/version=1.18.1/g" core/conky/spkgbuild
+changelog "core/conky/spkgbuild" "Upgraded from version 1.18.0 to version 1.18.1"
+sed -i -e "s/version=3.14.1/version=3.16.0/g" core/fortune-mod/spkgbuild
+changelog "core/fortune-mod/spkgbuild" "Upgraded from version 3.14.1 to version 3.16.0"
+sed -i -e "s/version=1.22.1/version=1.24.1/g" core/v4l-utils/spkgbuild
+changelog "core/v4l-utils/spkgbuild" "Upgraded from version 1.22.1 to version 1.24.1"
+sed -i -e "s/version=2.2.12/version=2.2.13/g" extra/bluefish/spkgbuild
+changelog "extra/bluefish/spkgbuild" "Upgraded from version 2.2.12 to version 2.2.13"
+sed -i -e "s/version=3.5.26/version=3.5.27/g" extra/flexget/spkgbuild
+changelog "extra/flexget/spkgbuild" "Upgraded from version 3.5.26 to version 3.5.27"
+sed -i -e "s/version=2.10.32/version=2.10.34/g" extra/gimp/spkgbuild
+changelog "extra/gimp/spkgbuild" "Upgraded from version 2.10.32 to version 2.10.34"
+sed -i -e "s/version=1.3.3/version=1.4.0/g" fonts/font-util/spkgbuild
+changelog "fonts/font-util/spkgbuild" "Upgraded from version 1.3.3 to version 1.4.0"
+sed -i -e "s/version=0.41.0/version=0.41.1/g" libs/libfilezilla/spkgbuild
+changelog "libs/libfilezilla/spkgbuild" "Upgraded from version 0.41.0 to version 0.41.1"
+sed -i -e "s/version=111.0b3/version=111.0b5/g" networking/firefox/spkgbuild
+changelog "networking/firefox/spkgbuild" "Upgraded from version 111.0b3 to version 111.0b5"
+sed -i -e "s/version=1.26.75/version=1.26.78/g" python/python-boto3/spkgbuild
+changelog "python/python-boto3/spkgbuild" "Upgraded from version 1.26.75 to version 1.26.78"
+sed -i -e "s/version=1.29.75/version=1.29.78/g" python/python-botocore/spkgbuild
+changelog "python/python-botocore/spkgbuild" "Upgraded from version 1.29.75 to version 1.29.78"
+sed -i -e "s/version=7.1.0/version=7.2.0/g" python/python-coverage/spkgbuild
+changelog "python/python-coverage/spkgbuild" "Upgraded from version 7.1.0 to version 7.2.0"
+sed -i -e "s/version=2023.2.0/version=2023.2.1/g" python/python-distributed/spkgbuild
+changelog "python/python-distributed/spkgbuild" "Upgraded from version 2023.2.0 to version 2023.2.1"
+sed -i -e "s/version=0.21.1/version=1.0.0/g" python/python-dotenv/spkgbuild
+changelog "python/python-dotenv/spkgbuild" "Upgraded from version 0.21.1 to version 1.0.0"
+sed -i -e "s/version=17.0.0/version=17.1.0/g" python/python-faker/spkgbuild
+changelog "python/python-faker/spkgbuild" "Upgraded from version 17.0.0 to version 17.1.0"
+sed -i -e "s/version=3.1.0/version=3.2.0/g" python/python-heatclient/spkgbuild
+changelog "python/python-heatclient/spkgbuild" "Upgraded from version 3.1.0 to version 3.2.0"
+sed -i -e "s/version=5.3.0/version=5.4.0/g" python/python-jaraco.test/spkgbuild
+changelog "python/python-jaraco.test/spkgbuild" "Upgraded from version 5.3.0 to version 5.4.0"
+sed -i -e "s/version=5.0.1/version=5.1.0/g" python/python-keystoneclient/spkgbuild
+changelog "python/python-keystoneclient/spkgbuild" "Upgraded from version 5.0.1 to version 5.1.0"
+sed -i -e "s/version=2.1.0/version=2.2.0/g" python/python-markdown-it-py/spkgbuild
+changelog "python/python-markdown-it-py/spkgbuild" "Upgraded from version 2.1.0 to version 2.2.0"
+sed -i -e "s/version=0.24.0/version=0.25.0/g" python/python-meilisearch/spkgbuild
+changelog "python/python-meilisearch/spkgbuild" "Upgraded from version 0.24.0 to version 0.25.0"
+sed -i -e "s/version=3.3.0/version=3.4.0/g" python/python-octaviaclient/spkgbuild
+changelog "python/python-octaviaclient/spkgbuild" "Upgraded from version 3.3.0 to version 3.4.0"
+sed -i -e "s/version=0.28.0/version=0.28.1/g" python/python-owslib/spkgbuild
+changelog "python/python-owslib/spkgbuild" "Upgraded from version 0.28.0 to version 0.28.1"
+sed -i -e "s/version=3.0.4/version=3.1.0/g" python/python-pre-commit/spkgbuild
+changelog "python/python-pre-commit/spkgbuild" "Upgraded from version 3.0.4 to version 3.1.0"
+sed -i -e "s/version=7.3.2302212119/version=7.3.2302241022/g" python/python-pycontracts/spkgbuild
+changelog "python/python-pycontracts/spkgbuild" "Upgraded from version 7.3.2302212119 to version 7.3.2302241022"
+sed -i -e "s/version=3.20.0/version=3.20.1/g" python/python-trimesh/spkgbuild
+changelog "python/python-trimesh/spkgbuild" "Upgraded from version 3.20.0 to version 3.20.1"
+sed -i -e "s/version=2.2.1/version=2.3.0/g" python/python-watchdog/spkgbuild
+changelog "python/python-watchdog/spkgbuild" "Upgraded from version 2.2.1 to version 2.3.0"
+sed -i -e "s/version=2.0/version=3.0/g" python/python-zc.lockfile/spkgbuild
+changelog "python/python-zc.lockfile/spkgbuild" "Upgraded from version 2.0 to version 3.0"
+sed -i -e "s/version=3.12.7/version=3.12.8/g" server/blacklist-hosts/spkgbuild
+changelog "server/blacklist-hosts/spkgbuild" "Upgraded from version 3.12.7 to version 3.12.8"
+sed -i -e "s/version=2023.2.5/version=2023.3.0/g" server/homeassistant/spkgbuild
+changelog "server/homeassistant/spkgbuild" "Upgraded from version 2023.2.5 to version 2023.3.0"
+sed -i -e "s/version=4.18.3/version=4.18.4/g" xfce/thunar/spkgbuild
+changelog "xfce/thunar/spkgbuild" "Upgraded from version 4.18.3 to version 4.18.4"
+sed -i -e "s/version=1.1.2/version=1.1.3/g" xorg/xbitmaps/spkgbuild
+changelog "xorg/xbitmaps/spkgbuild" "Upgraded from version 1.1.2 to version 1.1.3"
+sed -i -e "s/version=1.0.6/version=1.0.7/g" xorg/xcursor-themes/spkgbuild
+changelog "xorg/xcursor-themes/spkgbuild" "Upgraded from version 1.0.6 to version 1.0.7"
