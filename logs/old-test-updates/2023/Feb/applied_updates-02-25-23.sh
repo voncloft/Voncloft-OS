1f814fa21c028ee0cf589911e9006620800384b6
Voncloft <voncloft@gmail.com>
2023-02-25 17:17:38 -0500

    Minor Updates

---
+++ logs/old-test-updates/2023/Feb/applied_updates-02-25-23.sh
@@ -0,0 +1,24 @@
+sed -i -e "s/version=6.2/version=6.2.1/g" core/linux-api-headers/spkgbuild
+changelog "core/linux-api-headers/spkgbuild" "Upgraded from version 6.2 to version 6.2.1"
+sed -i -e "s/version=6.2/version=6.2.1/g" core/linux-desktop/spkgbuild
+changelog "core/linux-desktop/spkgbuild" "Upgraded from version 6.2 to version 6.2.1"
+sed -i -e "s/version=6.2/version=6.2.1/g" core/linux-router/spkgbuild
+changelog "core/linux-router/spkgbuild" "Upgraded from version 6.2 to version 6.2.1"
+sed -i -e "s/version=5.3.0/version=5.3.1/g" core/streamlink/spkgbuild
+changelog "core/streamlink/spkgbuild" "Upgraded from version 5.3.0 to version 5.3.1"
+sed -i -e "s/version=2.38/version=2.39/g" extra/discordchatexporter/spkgbuild
+changelog "extra/discordchatexporter/spkgbuild" "Upgraded from version 2.38 to version 2.39"
+sed -i -e "s/version=0.1.100/version=0.1.102/g" libs/babl/spkgbuild
+changelog "libs/babl/spkgbuild" "Upgraded from version 0.1.100 to version 0.1.102"
+sed -i -e "s/version=1.26.78/version=1.26.79/g" python/python-boto3/spkgbuild
+changelog "python/python-boto3/spkgbuild" "Upgraded from version 1.26.78 to version 1.26.79"
+sed -i -e "s/version=1.29.78/version=1.29.79/g" python/python-botocore/spkgbuild
+changelog "python/python-botocore/spkgbuild" "Upgraded from version 1.29.78 to version 1.29.79"
+sed -i -e "s/version=1.2.68/version=1.2.69/g" python/python-cloudscraper/spkgbuild
+changelog "python/python-cloudscraper/spkgbuild" "Upgraded from version 1.2.68 to version 1.2.69"
+sed -i -e "s/version=17.1.0/version=17.3.0/g" python/python-faker/spkgbuild
+changelog "python/python-faker/spkgbuild" "Upgraded from version 17.1.0 to version 17.3.0"
+sed -i -e "s/version=7.6.1/version=7.7.0/g" python/python-praw/spkgbuild
+changelog "python/python-praw/spkgbuild" "Upgraded from version 7.6.1 to version 7.7.0"
+sed -i -e "s/version=3.14.0/version=3.15.0/g" python/python-zipp/spkgbuild
+changelog "python/python-zipp/spkgbuild" "Upgraded from version 3.14.0 to version 3.15.0"
