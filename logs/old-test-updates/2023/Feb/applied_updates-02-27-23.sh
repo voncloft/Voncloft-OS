4c66041b82e7ffa656058bc573ce06a68eb470c2
Voncloft <voncloft@gmail.com>
2023-02-27 08:41:39 -0500

    Minor Updates

---
+++ logs/old-test-updates/2023/Feb/applied_updates-02-27-23.sh
@@ -0,0 +1,8 @@
+sed -i -e "s/version=0.17.0/version=0.17.1/g" libs/libass/spkgbuild
+changelog "libs/libass/spkgbuild" "Upgraded from version 0.17.0 to version 0.17.1"
+sed -i -e "s/version=7.2.0/version=7.2.1/g" python/python-coverage/spkgbuild
+changelog "python/python-coverage/spkgbuild" "Upgraded from version 7.2.0 to version 7.2.1"
+sed -i -e "s/version=1.2.0/version=1.3.0/g" python/python-kikit/spkgbuild
+changelog "python/python-kikit/spkgbuild" "Upgraded from version 1.2.0 to version 1.3.0"
+sed -i -e "s/version=0.8/version=0.8.1/g" python/python-sphinxcontrib-mermaid/spkgbuild
+changelog "python/python-sphinxcontrib-mermaid/spkgbuild" "Upgraded from version 0.8 to version 0.8.1"
