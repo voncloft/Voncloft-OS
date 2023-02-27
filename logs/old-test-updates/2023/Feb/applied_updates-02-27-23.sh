432bdba2d27cae29ba2a0f6f3e6f26a8d5d9237d
Voncloft <voncloft@gmail.com>
2023-02-27 15:43:59 -0500

    added radicale to repo

--- logs/old-test-updates/2023/Feb/applied_updates-02-27-23.sh
+++ logs/old-test-updates/2023/Feb/applied_updates-02-27-23.sh
@@ -10,3 +10,15 @@ sed -i -e "s/version=0.7.0/version=0.8.0/g" core/bubblewrap/spkgbuild
 changelog "core/bubblewrap/spkgbuild" "Upgraded from version 0.7.0 to version 0.8.0"
 sed -i -e "s/version=4.4/version=4.4.1/g" core/make/spkgbuild
 changelog "core/make/spkgbuild" "Upgraded from version 4.4 to version 4.4.1"
+sed -i -e "s/version=1.0.3/version=1.0.4/g" fonts/font-adobe-100dpi/spkgbuild
+changelog "fonts/font-adobe-100dpi/spkgbuild" "Upgraded from version 1.0.3 to version 1.0.4"
+sed -i -e "s/version=0.9.75/version=0.9.76/g" libs/libmicrohttpd/spkgbuild
+changelog "libs/libmicrohttpd/spkgbuild" "Upgraded from version 0.9.75 to version 0.9.76"
+sed -i -e "s/version=111.0b5/version=111.0b6/g" networking/firefox/spkgbuild
+changelog "networking/firefox/spkgbuild" "Upgraded from version 111.0b5 to version 111.0b6"
+sed -i -e "s/version=1.59.0/version=1.60.0/g" python/python-aws-sam-translator/spkgbuild
+changelog "python/python-aws-sam-translator/spkgbuild" "Upgraded from version 1.59.0 to version 1.60.0"
+sed -i -e "s/version=3.15.4/version=3.16.0/g" python/python-peewee/spkgbuild
+changelog "python/python-peewee/spkgbuild" "Upgraded from version 3.15.4 to version 3.16.0"
+sed -i -e "s/version=0.0.5/version=0.0.6/g" python/python-python-multipart/spkgbuild
+changelog "python/python-python-multipart/spkgbuild" "Upgraded from version 0.0.5 to version 0.0.6"
