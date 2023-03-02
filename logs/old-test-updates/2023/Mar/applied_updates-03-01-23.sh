cbed51e6c242c0f7acc32444eda0ac71f89b1a98
Voncloft <voncloft@gmail.com>
2023-03-01 20:05:49 -0500

    fixing evnet viewer for calendar of plasma

---
+++ logs/old-test-updates/2023/Mar/applied_updates-03-01-23.sh
@@ -0,0 +1,26 @@
+sed -i -e "s/version=3.26.0-rc4/version=3.26.0-rc5/g" compilers/cmake/spkgbuild
+changelog "compilers/cmake/spkgbuild" "Upgraded from version 3.26.0-rc4 to version 3.26.0-rc5"
+sed -i -e "s/version=3.5.28/version=3.5.29/g" extra/flexget/spkgbuild
+changelog "extra/flexget/spkgbuild" "Upgraded from version 3.5.28 to version 3.5.29"
+sed -i -e "s/version=1.0.0.75/version=1.0.0.76/g" multilib/steam/spkgbuild
+changelog "multilib/steam/spkgbuild" "Upgraded from version 1.0.0.75 to version 1.0.0.76"
+sed -i -e "s/version=111.0b6/version=111.0b7/g" networking/firefox/spkgbuild
+changelog "networking/firefox/spkgbuild" "Upgraded from version 111.0b6 to version 111.0b7"
+sed -i -e "s/version=3.40.1.0/version=3.41.0.0/g" python/python-apsw/spkgbuild
+changelog "python/python-apsw/spkgbuild" "Upgraded from version 3.40.1.0 to version 3.41.0.0"
+sed -i -e "s/version=1.60.0/version=1.60.1/g" python/python-aws-sam-translator/spkgbuild
+changelog "python/python-aws-sam-translator/spkgbuild" "Upgraded from version 1.60.0 to version 1.60.1"
+sed -i -e "s/version=1.26.81/version=1.26.82/g" python/python-boto3/spkgbuild
+changelog "python/python-boto3/spkgbuild" "Upgraded from version 1.26.81 to version 1.26.82"
+sed -i -e "s/version=1.29.81/version=1.29.82/g" python/python-botocore/spkgbuild
+changelog "python/python-botocore/spkgbuild" "Upgraded from version 1.29.81 to version 1.29.82"
+sed -i -e "s/version=17.3.0/version=17.5.0/g" python/python-faker/spkgbuild
+changelog "python/python-faker/spkgbuild" "Upgraded from version 17.3.0 to version 17.5.0"
+sed -i -e "s/version=5.1.0/version=5.1.1/g" python/python-flask-security-too/spkgbuild
+changelog "python/python-flask-security-too/spkgbuild" "Upgraded from version 5.1.0 to version 5.1.1"
+sed -i -e "s/version=2.79.0/version=2.80.0/g" python/python-google-api-python-client/spkgbuild
+changelog "python/python-google-api-python-client/spkgbuild" "Upgraded from version 2.79.0 to version 2.80.0"
+sed -i -e "s/version=0.18.1/version=0.19.0/g" python/python-myst-parser/spkgbuild
+changelog "python/python-myst-parser/spkgbuild" "Upgraded from version 0.18.1 to version 0.19.0"
+sed -i -e "s/version=3.12.8/version=3.12.9/g" server/blacklist-hosts/spkgbuild
+changelog "server/blacklist-hosts/spkgbuild" "Upgraded from version 3.12.8 to version 3.12.9"
