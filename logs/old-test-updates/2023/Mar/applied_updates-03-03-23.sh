7133a2873e220cc397640dde0fb9e8f6e36488ac
Voncloft <voncloft@gmail.com>
2023-03-03 15:43:12 -0500

    Minor Updates

---
+++ logs/old-test-updates/2023/Mar/applied_updates-03-03-23.sh
@@ -0,0 +1,34 @@
+sed -i -e "s/version=33.0.3/version=34.0.0/g" core/android-tools/spkgbuild
+changelog "core/android-tools/spkgbuild" "Upgraded from version 33.0.3 to version 34.0.0"
+sed -i -e "s/version=6.2/version=6.2.1/g" core/btrfs-progs/spkgbuild
+changelog "core/btrfs-progs/spkgbuild" "Upgraded from version 6.2 to version 6.2.1"
+sed -i -e "s/version=6.2.1/version=6.2.2/g" core/linux-api-headers/spkgbuild
+changelog "core/linux-api-headers/spkgbuild" "Upgraded from version 6.2.1 to version 6.2.2"
+sed -i -e "s/version=6.2.1/version=6.2.2/g" core/linux-desktop/spkgbuild
+changelog "core/linux-desktop/spkgbuild" "Upgraded from version 6.2.1 to version 6.2.2"
+sed -i -e "s/version=6.2.1/version=6.2.2/g" core/linux-router/spkgbuild
+changelog "core/linux-router/spkgbuild" "Upgraded from version 6.2.1 to version 6.2.2"
+sed -i -e "s/version=111.0b7/version=111.0b8/g" networking/firefox/spkgbuild
+changelog "networking/firefox/spkgbuild" "Upgraded from version 111.0b7 to version 111.0b8"
+sed -i -e "s/version=4.0.3/version=4.0.4/g" networking/wireshark/spkgbuild
+changelog "networking/wireshark/spkgbuild" "Upgraded from version 4.0.3 to version 4.0.4"
+sed -i -e "s/version=6.0.2/version=6.1.0/g" python/python-apispec/spkgbuild
+changelog "python/python-apispec/spkgbuild" "Upgraded from version 6.0.2 to version 6.1.0"
+sed -i -e "s/version=1.26.83/version=1.26.84/g" python/python-boto3/spkgbuild
+changelog "python/python-boto3/spkgbuild" "Upgraded from version 1.26.83 to version 1.26.84"
+sed -i -e "s/version=1.29.83/version=1.29.84/g" python/python-botocore/spkgbuild
+changelog "python/python-botocore/spkgbuild" "Upgraded from version 1.29.83 to version 1.29.84"
+sed -i -e "s/version=2.14.2/version=3.0.0/g" python/python-connexion/spkgbuild
+changelog "python/python-connexion/spkgbuild" "Upgraded from version 2.14.2 to version 3.0.0"
+sed -i -e "s/version=17.5.0/version=17.6.0/g" python/python-faker/spkgbuild
+changelog "python/python-faker/spkgbuild" "Upgraded from version 17.5.0 to version 17.6.0"
+sed -i -e "s/version=0.26.0/version=0.27.0/g" python/python-mongoengine/spkgbuild
+changelog "python/python-mongoengine/spkgbuild" "Upgraded from version 0.26.0 to version 0.27.0"
+sed -i -e "s/version=8.13.6/version=8.13.7/g" python/python-phonenumbers/spkgbuild
+changelog "python/python-phonenumbers/spkgbuild" "Upgraded from version 8.13.6 to version 8.13.7"
+sed -i -e "s/version=0.7.5/version=0.8.0/g" python/python-vistir/spkgbuild
+changelog "python/python-vistir/spkgbuild" "Upgraded from version 0.7.5 to version 0.8.0"
+sed -i -e "s/version=3.12.9/version=3.12.10/g" server/blacklist-hosts/spkgbuild
+changelog "server/blacklist-hosts/spkgbuild" "Upgraded from version 3.12.9 to version 3.12.10"
+sed -i -e "s/version=2023.3.0/version=2023.3.1/g" server/homeassistant/spkgbuild
+changelog "server/homeassistant/spkgbuild" "Upgraded from version 2023.3.0 to version 2023.3.1"
