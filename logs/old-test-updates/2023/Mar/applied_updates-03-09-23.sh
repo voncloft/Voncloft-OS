f18161026fe1dae4a4c96f6c99138b8bf334570d
Voncloft <voncloft@gmail.com>
2023-03-09 08:44:52 -0500

    Minor Updates

---
+++ logs/old-test-updates/2023/Mar/applied_updates-03-09-23.sh
@@ -0,0 +1,40 @@
+sed -i -e "s/version=1.9.0/version=1.9.1/g" core/dunst/spkgbuild
+changelog "core/dunst/spkgbuild" "Upgraded from version 1.9.0 to version 1.9.1"
+sed -i -e "s/version=2.39.90/version=2.39.91/g" core/webkitgtk/spkgbuild
+changelog "core/webkitgtk/spkgbuild" "Upgraded from version 2.39.90 to version 2.39.91"
+sed -i -e "s/version=3.5.29/version=3.5.30/g" extra/flexget/spkgbuild
+changelog "extra/flexget/spkgbuild" "Upgraded from version 3.5.29 to version 3.5.30"
+sed -i -e "s/version=22.0/version=22.1/g" extra/protobuf/spkgbuild
+changelog "extra/protobuf/spkgbuild" "Upgraded from version 22.0 to version 22.1"
+sed -i -e "s/version=0.7.1/version=0.7.2/g" kf5/kirigami-addons/spkgbuild
+changelog "kf5/kirigami-addons/spkgbuild" "Upgraded from version 0.7.1 to version 0.7.2"
+sed -i -e "s/version=1.43.2/version=1.43.3/g" networking/networkmanager/spkgbuild
+changelog "networking/networkmanager/spkgbuild" "Upgraded from version 1.43.2 to version 1.43.3"
+sed -i -e "s/version=2.6.0/version=2.6.1/g" networking/openvpn/spkgbuild
+changelog "networking/openvpn/spkgbuild" "Upgraded from version 2.6.0 to version 2.6.1"
+sed -i -e "s/version=1.26.86/version=1.26.87/g" python/python-boto3/spkgbuild
+changelog "python/python-boto3/spkgbuild" "Upgraded from version 1.26.86 to version 1.26.87"
+sed -i -e "s/version=1.29.86/version=1.29.87/g" python/python-botocore/spkgbuild
+changelog "python/python-botocore/spkgbuild" "Upgraded from version 1.29.86 to version 1.29.87"
+sed -i -e "s/version=2.5.18/version=2.5.19/g" python/python-identify/spkgbuild
+changelog "python/python-identify/spkgbuild" "Upgraded from version 2.5.18 to version 2.5.19"
+sed -i -e "s/version=1.10.5/version=1.10.6/g" python/python-pydantic/spkgbuild
+changelog "python/python-pydantic/spkgbuild" "Upgraded from version 1.10.5 to version 1.10.6"
+sed -i -e "s/version=3.0.9/version=3.1.0/g" python/python-pyparsing/spkgbuild
+changelog "python/python-pyparsing/spkgbuild" "Upgraded from version 3.0.9 to version 3.1.0"
+sed -i -e "s/version=0.9.0/version=1.0.0/g" python/python-resolvelib/spkgbuild
+changelog "python/python-resolvelib/spkgbuild" "Upgraded from version 0.9.0 to version 1.0.0"
+sed -i -e "s/version=1.2.1/version=1.2.2/g" python/python-scikit-learn/spkgbuild
+changelog "python/python-scikit-learn/spkgbuild" "Upgraded from version 1.2.1 to version 1.2.2"
+sed -i -e "s/version=67.5.1/version=67.6.0/g" python/python-setuptools/spkgbuild
+changelog "python/python-setuptools/spkgbuild" "Upgraded from version 67.5.1 to version 67.6.0"
+sed -i -e "s/version=3.20.1/version=3.20.2/g" python/python-trimesh/spkgbuild
+changelog "python/python-trimesh/spkgbuild" "Upgraded from version 3.20.1 to version 3.20.2"
+sed -i -e "s/version=2.4.7/version=2.4.8/g" ruby-gems/ruby-bundler/spkgbuild
+changelog "ruby-gems/ruby-bundler/spkgbuild" "Upgraded from version 2.4.7 to version 2.4.8"
+sed -i -e "s/version=3.12.11/version=3.12.12/g" server/blacklist-hosts/spkgbuild
+changelog "server/blacklist-hosts/spkgbuild" "Upgraded from version 3.12.11 to version 3.12.12"
+sed -i -e "s/version=2023.3.1/version=2023.3.2/g" server/homeassistant/spkgbuild
+changelog "server/homeassistant/spkgbuild" "Upgraded from version 2023.3.1 to version 2023.3.2"
+sed -i -e "s/version=4.17.5/version=4.18.0/g" server/samba/spkgbuild
+changelog "server/samba/spkgbuild" "Upgraded from version 4.17.5 to version 4.18.0"
