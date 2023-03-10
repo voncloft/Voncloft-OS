c7e5d94c807b58d4e3575bbfc24b15327d6db19f
Voncloft <voncloft@gmail.com>
2023-03-10 14:49:27 -0500

    Minor Updates

---
+++ logs/old-test-updates/2023/Mar/applied_updates-03-10-23.sh
@@ -0,0 +1,34 @@
+sed -i -e "s/version=3.26.0-rc5/version=3.26.0-rc6/g" compilers/cmake/spkgbuild
+changelog "compilers/cmake/spkgbuild" "Upgraded from version 3.26.0-rc5 to version 3.26.0-rc6"
+sed -i -e "s/version=1.67.1/version=1.68.0/g" compilers/rust/spkgbuild
+changelog "compilers/rust/spkgbuild" "Upgraded from version 1.67.1 to version 1.68.0"
+sed -i -e "s/version=0.3.66/version=0.3.67/g" core/pipewire/spkgbuild
+changelog "core/pipewire/spkgbuild" "Upgraded from version 0.3.66 to version 0.3.67"
+sed -i -e "s/version=1.1.8rc3/version=1.1.9rc1/g" extra/solaar/spkgbuild
+changelog "extra/solaar/spkgbuild" "Upgraded from version 1.1.8rc3 to version 1.1.9rc1"
+sed -i -e "s/version=3.1.5/version=3.1.6/g" libs/openexr/spkgbuild
+changelog "libs/openexr/spkgbuild" "Upgraded from version 3.1.5 to version 3.1.6"
+sed -i -e "s/version=1.1.9/version=1.1.10/g" libs/snappy/spkgbuild
+changelog "libs/snappy/spkgbuild" "Upgraded from version 1.1.9 to version 1.1.10"
+sed -i -e "s/version=1.2.4/version=1.2.5/g" networking/libnftnl/spkgbuild
+changelog "networking/libnftnl/spkgbuild" "Upgraded from version 1.2.4 to version 1.2.5"
+sed -i -e "s/version=111.0b3/version=111.0b4/g" networking/thunderbird/spkgbuild
+changelog "networking/thunderbird/spkgbuild" "Upgraded from version 111.0b3 to version 111.0b4"
+sed -i -e "s/version=1.26.87/version=1.26.88/g" python/python-boto3/spkgbuild
+changelog "python/python-boto3/spkgbuild" "Upgraded from version 1.26.87 to version 1.26.88"
+sed -i -e "s/version=1.29.87/version=1.29.88/g" python/python-botocore/spkgbuild
+changelog "python/python-botocore/spkgbuild" "Upgraded from version 1.29.87 to version 1.29.88"
+sed -i -e "s/version=0.13.11/version=0.13.13/g" python/python-ipdb/spkgbuild
+changelog "python/python-ipdb/spkgbuild" "Upgraded from version 0.13.11 to version 0.13.13"
+sed -i -e "s/version=6.1.0/version=6.2.0/g" python/python-openstackclient/spkgbuild
+changelog "python/python-openstackclient/spkgbuild" "Upgraded from version 6.1.0 to version 6.2.0"
+sed -i -e "s/version=1.0.0/version=1.0.1/g" python/python-resolvelib/spkgbuild
+changelog "python/python-resolvelib/spkgbuild" "Upgraded from version 1.0.0 to version 1.0.1"
+sed -i -e "s/version=4.12.1/version=4.13.0/g" python/python-tweepy/spkgbuild
+changelog "python/python-tweepy/spkgbuild" "Upgraded from version 4.12.1 to version 4.13.0"
+sed -i -e "s/version=5.15.12/version=5.15.13/g" qt/qtwebengine/spkgbuild
+changelog "qt/qtwebengine/spkgbuild" "Upgraded from version 5.15.12 to version 5.15.13"
+sed -i -e "s/version=2023.3.2/version=2023.3.3/g" server/homeassistant/spkgbuild
+changelog "server/homeassistant/spkgbuild" "Upgraded from version 2023.3.2 to version 2023.3.3"
+sed -i -e "s/version=1.31.1.6733/version=1.31.1.6782/g" server/plex-media-server/spkgbuild
+changelog "server/plex-media-server/spkgbuild" "Upgraded from version 1.31.1.6733 to version 1.31.1.6782"
