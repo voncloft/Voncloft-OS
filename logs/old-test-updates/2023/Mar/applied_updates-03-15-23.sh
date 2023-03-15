2dd8230b494dc5066a51b152dab026a80fb50243
Voncloft <voncloft@gmail.com>
2023-03-15 16:09:54 -0400

    Minor Updates

---
+++ logs/old-test-updates/2023/Mar/applied_updates-03-15-23.sh
@@ -0,0 +1,28 @@
+sed -i -e "s/version=3.5.31/version=3.5.32/g" extra/flexget/spkgbuild
+changelog "extra/flexget/spkgbuild" "Upgraded from version 3.5.31 to version 3.5.32"
+sed -i -e "s/version=4.902/version=4.903/g" extra/gnucash/spkgbuild
+changelog "extra/gnucash/spkgbuild" "Upgraded from version 4.902 to version 4.903"
+sed -i -e "s/version=1.26.0/version=1.26.1/g" mate/mate-desktop/spkgbuild
+changelog "mate/mate-desktop/spkgbuild" "Upgraded from version 1.26.0 to version 1.26.1"
+sed -i -e "s/version=9.19.10/version=9.19.11/g" networking/bind/spkgbuild
+changelog "networking/bind/spkgbuild" "Upgraded from version 9.19.10 to version 9.19.11"
+sed -i -e "s/version=112.0b1/version=112.0b2/g" networking/firefox/spkgbuild
+changelog "networking/firefox/spkgbuild" "Upgraded from version 112.0b1 to version 112.0b2"
+sed -i -e "s/version=111.0b4/version=112.0b1/g" networking/thunderbird/spkgbuild
+changelog "networking/thunderbird/spkgbuild" "Upgraded from version 111.0b4 to version 112.0b1"
+sed -i -e "s/version=1.61.0/version=1.62.0/g" python/python-aws-sam-translator/spkgbuild
+changelog "python/python-aws-sam-translator/spkgbuild" "Upgraded from version 1.61.0 to version 1.62.0"
+sed -i -e "s/version=1.26.90/version=1.26.92/g" python/python-boto3/spkgbuild
+changelog "python/python-boto3/spkgbuild" "Upgraded from version 1.26.90 to version 1.26.92"
+sed -i -e "s/version=1.29.90/version=1.29.92/g" python/python-botocore/spkgbuild
+changelog "python/python-botocore/spkgbuild" "Upgraded from version 1.29.90 to version 1.29.92"
+sed -i -e "s/version=1.6.5/version=2.0.0/g" python/python-canonicaljson/spkgbuild
+changelog "python/python-canonicaljson/spkgbuild" "Upgraded from version 1.6.5 to version 2.0.0"
+sed -i -e "s/version=6.68.2/version=6.68.3/g" python/python-hypothesis/spkgbuild
+changelog "python/python-hypothesis/spkgbuild" "Upgraded from version 6.68.2 to version 6.68.3"
+sed -i -e "s/version=3.18.3/version=3.18.4/g" python/python-simplejson/spkgbuild
+changelog "python/python-simplejson/spkgbuild" "Upgraded from version 3.18.3 to version 3.18.4"
+sed -i -e "s/version=3.12.13/version=3.12.14/g" server/blacklist-hosts/spkgbuild
+changelog "server/blacklist-hosts/spkgbuild" "Upgraded from version 3.12.13 to version 3.12.14"
+sed -i -e "s/version=1.31.1.6782/version=1.31.2.6810/g" server/plex-media-server/spkgbuild
+changelog "server/plex-media-server/spkgbuild" "Upgraded from version 1.31.1.6782 to version 1.31.2.6810"
