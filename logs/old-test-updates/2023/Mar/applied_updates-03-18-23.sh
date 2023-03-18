c12bdc69e1469ba4b6e061c9c8b637ac0e25342a
Voncloft <voncloft@gmail.com>
2023-03-18 17:55:21 -0400

    Minor Updates

---
+++ logs/old-test-updates/2023/Mar/applied_updates-03-18-23.sh
@@ -0,0 +1,28 @@
+sed -i -e "s/version=15.0.7/version=16.0.0/g" compilers/clang/spkgbuild
+changelog "compilers/clang/spkgbuild" "Upgraded from version 15.0.7 to version 16.0.0"
+sed -i -e "s/version=15.0.7/version=16.0.0/g" compilers/lld/spkgbuild
+changelog "compilers/lld/spkgbuild" "Upgraded from version 15.0.7 to version 16.0.0"
+sed -i -e "s/version=15.0.7/version=16.0.0/g" compilers/llvm/spkgbuild
+changelog "compilers/llvm/spkgbuild" "Upgraded from version 15.0.7 to version 16.0.0"
+sed -i -e "s/version=15.0.7/version=16.0.0/g" compilers/llvm-bak/spkgbuild
+changelog "compilers/llvm-bak/spkgbuild" "Upgraded from version 15.0.7 to version 16.0.0"
+sed -i -e "s/version=5.4.1/version=5.4.2/g" core/xz/spkgbuild
+changelog "core/xz/spkgbuild" "Upgraded from version 5.4.1 to version 5.4.2"
+sed -i -e "s/version=3.5.32/version=3.5.33/g" extra/flexget/spkgbuild
+changelog "extra/flexget/spkgbuild" "Upgraded from version 3.5.32 to version 3.5.33"
+sed -i -e "s/version=15.0.7/version=16.0.0/g" multilib/llvm-32/spkgbuild
+changelog "multilib/llvm-32/spkgbuild" "Upgraded from version 15.0.7 to version 16.0.0"
+sed -i -e "s/version=6.0.0/version=6.1.0/g" python/python-importlib-metadata/spkgbuild
+changelog "python/python-importlib-metadata/spkgbuild" "Upgraded from version 6.0.0 to version 6.1.0"
+sed -i -e "s/version=2023.2.18/version=2023.3.18/g" python/python-pipenv/spkgbuild
+changelog "python/python-pipenv/spkgbuild" "Upgraded from version 2023.2.18 to version 2023.3.18"
+sed -i -e "s/version=1.58.0/version=1.58.1/g" python/python-pygithub/spkgbuild
+changelog "python/python-pygithub/spkgbuild" "Upgraded from version 1.58.0 to version 1.58.1"
+sed -i -e "s/version=6.3.0/version=6.3.1/g" python/python-rdflib/spkgbuild
+changelog "python/python-rdflib/spkgbuild" "Upgraded from version 6.3.0 to version 6.3.1"
+sed -i -e "s/version=2.2.3/version=2.2.4/g" python/python-requirementslib/spkgbuild
+changelog "python/python-requirementslib/spkgbuild" "Upgraded from version 2.2.3 to version 2.2.4"
+sed -i -e "s/version=6.1.4/version=6.1.5/g" python/python-softlayer/spkgbuild
+changelog "python/python-softlayer/spkgbuild" "Upgraded from version 6.1.4 to version 6.1.5"
+sed -i -e "s/version=1.9.0/version=1.10.0/g" xfce/xfce4-notes-plugin/spkgbuild
+changelog "xfce/xfce4-notes-plugin/spkgbuild" "Upgraded from version 1.9.0 to version 1.10.0"
