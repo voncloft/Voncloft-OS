3b9db8958296df06f1a66981f7460e47913e7f0b
Voncloft <voncloft@gmail.com>
2023-03-20 09:16:00 -0400

    Minor Updates

---
+++ logs/old-test-updates/2023/Mar/applied_updates-03-20-23.sh
@@ -0,0 +1,10 @@
+sed -i -e "s/version=1.0.4/version=1.0.5/g" core/wmderland/spkgbuild
+changelog "core/wmderland/spkgbuild" "Upgraded from version 1.0.4 to version 1.0.5"
+sed -i -e "s/version=0.94.1/version=0.95.0/g" python/python-fastapi/spkgbuild
+changelog "python/python-fastapi/spkgbuild" "Upgraded from version 0.94.1 to version 0.95.0"
+sed -i -e "s/version=2023.3.18/version=2023.3.20/g" python/python-pipenv/spkgbuild
+changelog "python/python-pipenv/spkgbuild" "Upgraded from version 2023.3.18 to version 2023.3.20"
+sed -i -e "s/version=1.4.0/version=1.4.1/g" python/python-poetry/spkgbuild
+changelog "python/python-poetry/spkgbuild" "Upgraded from version 1.4.0 to version 1.4.1"
+sed -i -e "s/version=0.20.3/version=0.21.0/g" python/python-pytest-asyncio/spkgbuild
+changelog "python/python-pytest-asyncio/spkgbuild" "Upgraded from version 0.20.3 to version 0.21.0"
