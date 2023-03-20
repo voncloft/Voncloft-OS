94d4984317e30e69970d32f1c82f32bd12cf35c2
Voncloft <voncloft@gmail.com>
2023-03-20 15:52:07 -0400

    Minor Updates

--- logs/old-test-updates/2023/Mar/applied_updates-03-20-23.sh
+++ logs/old-test-updates/2023/Mar/applied_updates-03-20-23.sh
@@ -8,3 +8,33 @@ sed -i -e "s/version=1.4.0/version=1.4.1/g" python/python-poetry/spkgbuild
 changelog "python/python-poetry/spkgbuild" "Upgraded from version 1.4.0 to version 1.4.1"
 sed -i -e "s/version=0.20.3/version=0.21.0/g" python/python-pytest-asyncio/spkgbuild
 changelog "python/python-pytest-asyncio/spkgbuild" "Upgraded from version 0.20.3 to version 0.21.0"
+sed -i -e "s/version=9.1/version=9.2/g" core/coreutils/spkgbuild
+changelog "core/coreutils/spkgbuild" "Upgraded from version 9.1 to version 9.2"
+sed -i -e "s/version=7.88.1/version=8.0.1/g" core/curl/spkgbuild
+changelog "core/curl/spkgbuild" "Upgraded from version 7.88.1 to version 8.0.1"
+sed -i -e "s/version=7.0.9/version=7.0.10/g" core/redis/spkgbuild
+changelog "core/redis/spkgbuild" "Upgraded from version 7.0.9 to version 7.0.10"
+sed -i -e "s/version=4.903/version=4.904/g" extra/gnucash/spkgbuild
+changelog "extra/gnucash/spkgbuild" "Upgraded from version 4.903 to version 4.904"
+sed -i -e "s/version=1.31.2/version=1.31.3/g" media/mpg123/spkgbuild
+changelog "media/mpg123/spkgbuild" "Upgraded from version 1.31.2 to version 1.31.3"
+sed -i -e "s/version=112.0b3/version=112.0b4/g" networking/firefox/spkgbuild
+changelog "networking/firefox/spkgbuild" "Upgraded from version 112.0b3 to version 112.0b4"
+sed -i -e "s/version=1.26.94/version=1.26.95/g" python/python-boto3/spkgbuild
+changelog "python/python-boto3/spkgbuild" "Upgraded from version 1.26.94 to version 1.26.95"
+sed -i -e "s/version=1.29.94/version=1.29.95/g" python/python-botocore/spkgbuild
+changelog "python/python-botocore/spkgbuild" "Upgraded from version 1.29.94 to version 1.29.95"
+sed -i -e "s/version=2.6.0/version=2.7.0/g" python/python-cairosvg/spkgbuild
+changelog "python/python-cairosvg/spkgbuild" "Upgraded from version 2.6.0 to version 2.7.0"
+sed -i -e "s/version=0.53.0/version=0.53.1/g" python/python-django-allauth/spkgbuild
+changelog "python/python-django-allauth/spkgbuild" "Upgraded from version 0.53.0 to version 0.53.1"
+sed -i -e "s/version=17.6.0/version=18.2.0/g" python/python-faker/spkgbuild
+changelog "python/python-faker/spkgbuild" "Upgraded from version 17.6.0 to version 18.2.0"
+sed -i -e "s/version=6.21.3/version=6.22.0/g" python/python-ipykernel/spkgbuild
+changelog "python/python-ipykernel/spkgbuild" "Upgraded from version 6.21.3 to version 6.22.0"
+sed -i -e "s/version=2.11.0/version=2.12.0/g" python/python-tensorflow-estimator/spkgbuild
+changelog "python/python-tensorflow-estimator/spkgbuild" "Upgraded from version 2.11.0 to version 2.12.0"
+sed -i -e "s/version=2.3.1/version=3.0.0/g" python/python-watchdog/spkgbuild
+changelog "python/python-watchdog/spkgbuild" "Upgraded from version 2.3.1 to version 3.0.0"
+sed -i -e "s/version=0.47.3/version=0.47.4/g" python/python-zeroconf/spkgbuild
+changelog "python/python-zeroconf/spkgbuild" "Upgraded from version 0.47.3 to version 0.47.4"
