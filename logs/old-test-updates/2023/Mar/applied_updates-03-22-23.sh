83c58c66b2c27e73e58ad5a26b8bd0b7de703b62
Voncloft <voncloft@gmail.com>
2023-03-22 16:50:42 -0400

    Minor Updates

--- logs/old-test-updates/2023/Mar/applied_updates-03-22-23.sh
+++ logs/old-test-updates/2023/Mar/applied_updates-03-22-23.sh
@@ -30,3 +30,35 @@ sed -i -e "s/version=2.4.8/version=2.4.9/g" ruby-gems/ruby-bundler/spkgbuild
 changelog "ruby-gems/ruby-bundler/spkgbuild" "Upgraded from version 2.4.8 to version 2.4.9"
 sed -i -e "s/version=1.6.2/version=1.6.3/g" xfce/xfce4-clipman-plugin/spkgbuild
 changelog "xfce/xfce4-clipman-plugin/spkgbuild" "Upgraded from version 1.6.2 to version 1.6.3"
+sed -i -e "s/version=6.2.7/version=6.2.8/g" core/linux-api-headers/spkgbuild
+changelog "core/linux-api-headers/spkgbuild" "Upgraded from version 6.2.7 to version 6.2.8"
+sed -i -e "s/version=6.2.7/version=6.2.8/g" core/linux-desktop/spkgbuild
+changelog "core/linux-desktop/spkgbuild" "Upgraded from version 6.2.7 to version 6.2.8"
+sed -i -e "s/version=6.2.7/version=6.2.8/g" core/linux-router/spkgbuild
+changelog "core/linux-router/spkgbuild" "Upgraded from version 6.2.7 to version 6.2.8"
+sed -i -e "s/version=2.10.1/version=2.10.2/g" core/mps-youtube/spkgbuild
+changelog "core/mps-youtube/spkgbuild" "Upgraded from version 2.10.1 to version 2.10.2"
+sed -i -e "s/version=4.0.42/version=4.0.43/g" core/mtools/spkgbuild
+changelog "core/mtools/spkgbuild" "Upgraded from version 4.0.42 to version 4.0.43"
+sed -i -e "s/version=1.14.1/version=1.14.2/g" core/wpebackend-fdo/spkgbuild
+changelog "core/wpebackend-fdo/spkgbuild" "Upgraded from version 1.14.1 to version 1.14.2"
+sed -i -e "s/version=8.4.0/version=9.0.0/g" media/mesa-demos/spkgbuild
+changelog "media/mesa-demos/spkgbuild" "Upgraded from version 8.4.0 to version 9.0.0"
+sed -i -e "s/version=1.43.3/version=1.43.4/g" networking/networkmanager/spkgbuild
+changelog "networking/networkmanager/spkgbuild" "Upgraded from version 1.43.3 to version 1.43.4"
+sed -i -e "s/version=112.0b4/version=112.0b5/g" nonfree/firefox/spkgbuild
+changelog "nonfree/firefox/spkgbuild" "Upgraded from version 112.0b4 to version 112.0b5"
+sed -i -e "s/version=1.26.96/version=1.26.97/g" python/python-boto3/spkgbuild
+changelog "python/python-boto3/spkgbuild" "Upgraded from version 1.26.96 to version 1.26.97"
+sed -i -e "s/version=1.29.96/version=1.29.97/g" python/python-botocore/spkgbuild
+changelog "python/python-botocore/spkgbuild" "Upgraded from version 1.29.96 to version 1.29.97"
+sed -i -e "s/version=18.2.0/version=18.3.0/g" python/python-faker/spkgbuild
+changelog "python/python-faker/spkgbuild" "Upgraded from version 18.2.0 to version 18.3.0"
+sed -i -e "s/version=3.10.0/version=3.10.1/g" python/python-filelock/spkgbuild
+changelog "python/python-filelock/spkgbuild" "Upgraded from version 3.10.0 to version 3.10.1"
+sed -i -e "s/version=0.21.0/version=0.22.0/g" python/python-httplib2/spkgbuild
+changelog "python/python-httplib2/spkgbuild" "Upgraded from version 0.21.0 to version 0.22.0"
+sed -i -e "s/version=2.2/version=2.3/g" python/python-uhashring/spkgbuild
+changelog "python/python-uhashring/spkgbuild" "Upgraded from version 2.2 to version 2.3"
+sed -i -e "s/version=3.12.14/version=3.12.15/g" server/blacklist-hosts/spkgbuild
+changelog "server/blacklist-hosts/spkgbuild" "Upgraded from version 3.12.14 to version 3.12.15"
