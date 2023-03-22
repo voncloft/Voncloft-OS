ca5367fd91e6c333740340b7c62a0723fb14a8b2
Voncloft <voncloft@gmail.com>
2023-03-22 08:44:47 -0400

    Minor Updates

---
+++ logs/old-test-updates/2023/Mar/applied_updates-03-22-23.sh
@@ -0,0 +1,32 @@
+sed -i -e "s/version=2.28/version=2.29/g" core/links/spkgbuild
+changelog "core/links/spkgbuild" "Upgraded from version 2.28 to version 2.29"
+sed -i -e "s/version=2.9.4/version=2.10.1/g" core/mps-youtube/spkgbuild
+changelog "core/mps-youtube/spkgbuild" "Upgraded from version 2.9.4 to version 2.10.1"
+sed -i -e "s/version=4.5.1/version=4.5.2/g" core/scons/spkgbuild
+changelog "core/scons/spkgbuild" "Upgraded from version 4.5.1 to version 4.5.2"
+sed -i -e "s/version=2.17.0/version=2.18.0/g" libs/libva/spkgbuild
+changelog "libs/libva/spkgbuild" "Upgraded from version 2.17.0 to version 2.18.0"
+sed -i -e "s/version=112.0b1/version=112.0b2/g" networking/thunderbird/spkgbuild
+changelog "networking/thunderbird/spkgbuild" "Upgraded from version 112.0b1 to version 112.0b2"
+sed -i -e "s/version=1.26.95/version=1.26.96/g" python/python-boto3/spkgbuild
+changelog "python/python-boto3/spkgbuild" "Upgraded from version 1.26.95 to version 1.26.96"
+sed -i -e "s/version=1.29.95/version=1.29.96/g" python/python-botocore/spkgbuild
+changelog "python/python-botocore/spkgbuild" "Upgraded from version 1.29.95 to version 1.29.96"
+sed -i -e "s/version=4.0.1/version=4.1.0/g" python/python-elementpath/spkgbuild
+changelog "python/python-elementpath/spkgbuild" "Upgraded from version 4.0.1 to version 4.1.0"
+sed -i -e "s/version=2.81.0/version=2.82.0/g" python/python-google-api-python-client/spkgbuild
+changelog "python/python-google-api-python-client/spkgbuild" "Upgraded from version 2.81.0 to version 2.82.0"
+sed -i -e "s/version=8.0.4/version=8.0.6/g" python/python-ipywidgets/spkgbuild
+changelog "python/python-ipywidgets/spkgbuild" "Upgraded from version 8.0.4 to version 8.0.6"
+sed -i -e "s/version=3.8.7/version=3.8.8/g" python/python-orjson/spkgbuild
+changelog "python/python-orjson/spkgbuild" "Upgraded from version 3.8.7 to version 3.8.8"
+sed -i -e "s/version=5.1.0/version=5.2.0/g" python/python-oslo-log/spkgbuild
+changelog "python/python-oslo-log/spkgbuild" "Upgraded from version 5.1.0 to version 5.2.0"
+sed -i -e "s/version=13.0.0/version=13.0.1/g" python/python-pdoc/spkgbuild
+changelog "python/python-pdoc/spkgbuild" "Upgraded from version 13.0.0 to version 13.0.1"
+sed -i -e "s/version=3.20.2/version=3.21.0/g" python/python-trimesh/spkgbuild
+changelog "python/python-trimesh/spkgbuild" "Upgraded from version 3.20.2 to version 3.21.0"
+sed -i -e "s/version=2.4.8/version=2.4.9/g" ruby-gems/ruby-bundler/spkgbuild
+changelog "ruby-gems/ruby-bundler/spkgbuild" "Upgraded from version 2.4.8 to version 2.4.9"
+sed -i -e "s/version=1.6.2/version=1.6.3/g" xfce/xfce4-clipman-plugin/spkgbuild
+changelog "xfce/xfce4-clipman-plugin/spkgbuild" "Upgraded from version 1.6.2 to version 1.6.3"
