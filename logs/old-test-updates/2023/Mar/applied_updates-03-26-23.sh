c2c253d620050e66cb73acacc458683ae82fb0b7
Voncloft <voncloft@gmail.com>
2023-03-26 12:36:30 -0400

    Minor Updates

---
+++ logs/old-test-updates/2023/Mar/applied_updates-03-26-23.sh
@@ -0,0 +1,74 @@
+sed -i -e "s/version=3.26.0-rc6/version=3.26.1/g" compilers/cmake/spkgbuild
+changelog "compilers/cmake/spkgbuild" "Upgraded from version 3.26.0-rc6 to version 3.26.1"
+sed -i -e "s/version=1.68.0/version=1.68.1/g" compilers/rust/spkgbuild
+changelog "compilers/rust/spkgbuild" "Upgraded from version 1.68.0 to version 1.68.1"
+sed -i -e "s/version=6.2.1/version=6.2.2/g" core/btrfs-progs/spkgbuild
+changelog "core/btrfs-progs/spkgbuild" "Upgraded from version 6.2.1 to version 6.2.2"
+sed -i -e "s/version=3.9/version=3.10/g" core/grep/spkgbuild
+changelog "core/grep/spkgbuild" "Upgraded from version 3.9 to version 3.10"
+sed -i -e "s/version=6.3.3/version=6.4/g" core/mercurial/spkgbuild
+changelog "core/mercurial/spkgbuild" "Upgraded from version 6.3.3 to version 6.4"
+sed -i -e "s/version=3.5.33/version=3.5.34/g" extra/flexget/spkgbuild
+changelog "extra/flexget/spkgbuild" "Upgraded from version 3.5.33 to version 3.5.34"
+sed -i -e "s/version=2.7/version=2.8/g" libs/libbytesize/spkgbuild
+changelog "libs/libbytesize/spkgbuild" "Upgraded from version 2.7 to version 2.8"
+sed -i -e "s/version=2.6.1/version=2.6.2/g" networking/openvpn/spkgbuild
+changelog "networking/openvpn/spkgbuild" "Upgraded from version 2.6.1 to version 2.6.2"
+sed -i -e "s/version=112.0b5/version=112.0b6/g" nonfree/firefox/spkgbuild
+changelog "nonfree/firefox/spkgbuild" "Upgraded from version 112.0b5 to version 112.0b6"
+sed -i -e "s/version=530.30.02/version=530.41.03/g" nonfree/nvidia/spkgbuild
+changelog "nonfree/nvidia/spkgbuild" "Upgraded from version 530.30.02 to version 530.41.03"
+sed -i -e "s/version=530.30.02/version=530.41.03/g" nonfree/nvidia-32/spkgbuild
+changelog "nonfree/nvidia-32/spkgbuild" "Upgraded from version 530.30.02 to version 530.41.03"
+sed -i -e "s/version=530.30.02/version=530.41.03/g" nonfree/nvidia-modules/spkgbuild
+changelog "nonfree/nvidia-modules/spkgbuild" "Upgraded from version 530.30.02 to version 530.41.03"
+sed -i -e "s/version=112.0b2/version=112.0b3/g" nonfree/thunderbird/spkgbuild
+changelog "nonfree/thunderbird/spkgbuild" "Upgraded from version 112.0b2 to version 112.0b3"
+sed -i -e "s/version=1.26.97/version=1.26.99/g" python/python-boto3/spkgbuild
+changelog "python/python-boto3/spkgbuild" "Upgraded from version 1.26.97 to version 1.26.99"
+sed -i -e "s/version=1.29.97/version=1.29.99/g" python/python-botocore/spkgbuild
+changelog "python/python-botocore/spkgbuild" "Upgraded from version 1.29.97 to version 1.29.99"
+sed -i -e "s/version=2023.3.1/version=2023.3.2/g" python/python-distributed/spkgbuild
+changelog "python/python-distributed/spkgbuild" "Upgraded from version 2023.3.1 to version 2023.3.2"
+sed -i -e "s/version=0.4.1/version=0.5.0/g" python/python-edalize/spkgbuild
+changelog "python/python-edalize/spkgbuild" "Upgraded from version 0.4.1 to version 0.5.0"
+sed -i -e "s/version=6.1.1/version=7.0.1/g" python/python-envisage/spkgbuild
+changelog "python/python-envisage/spkgbuild" "Upgraded from version 6.1.1 to version 7.0.1"
+sed -i -e "s/version=18.3.0/version=18.3.1/g" python/python-faker/spkgbuild
+changelog "python/python-faker/spkgbuild" "Upgraded from version 18.3.0 to version 18.3.1"
+sed -i -e "s/version=3.10.1/version=3.10.4/g" python/python-filelock/spkgbuild
+changelog "python/python-filelock/spkgbuild" "Upgraded from version 3.10.1 to version 3.10.4"
+sed -i -e "s/version=2.16.2/version=2.16.3/g" python/python-google-auth/spkgbuild
+changelog "python/python-google-auth/spkgbuild" "Upgraded from version 2.16.2 to version 2.16.3"
+sed -i -e "s/version=2.5.21/version=2.5.22/g" python/python-identify/spkgbuild
+changelog "python/python-identify/spkgbuild" "Upgraded from version 2.5.21 to version 2.5.22"
+sed -i -e "s/version=2.5.0/version=2.6.0/g" python/python-perf/spkgbuild
+changelog "python/python-perf/spkgbuild" "Upgraded from version 2.5.0 to version 2.6.0"
+sed -i -e "s/version=3.2.0/version=3.2.1/g" python/python-pre-commit/spkgbuild
+changelog "python/python-pre-commit/spkgbuild" "Upgraded from version 3.2.0 to version 3.2.1"
+sed -i -e "s/version=1.10.6/version=1.10.7/g" python/python-pydantic/spkgbuild
+changelog "python/python-pydantic/spkgbuild" "Upgraded from version 1.10.6 to version 1.10.7"
+sed -i -e "s/version=3.4.1/version=3.5.0/g" python/python-pyproj/spkgbuild
+changelog "python/python-pyproj/spkgbuild" "Upgraded from version 3.4.1 to version 3.5.0"
+sed -i -e "s/version=2022.12.07/version=2023.03.23/g" python/python-sphinx-furo/spkgbuild
+changelog "python/python-sphinx-furo/spkgbuild" "Upgraded from version 2022.12.07 to version 2023.03.23"
+sed -i -e "s/version=0.9.2/version=0.10.0/g" python/python-symengine/spkgbuild
+changelog "python/python-symengine/spkgbuild" "Upgraded from version 0.9.2 to version 0.10.0"
+sed -i -e "s/version=20.1/version=20.2/g" python/python-telegram-bot/spkgbuild
+changelog "python/python-telegram-bot/spkgbuild" "Upgraded from version 20.1 to version 20.2"
+sed -i -e "s/version=2.11.0/version=2.11.1/g" python/python-tensorflow-serving-api/spkgbuild
+changelog "python/python-tensorflow-serving-api/spkgbuild" "Upgraded from version 2.11.0 to version 2.11.1"
+sed -i -e "s/version=3.21.0/version=3.21.1/g" python/python-trimesh/spkgbuild
+changelog "python/python-trimesh/spkgbuild" "Upgraded from version 3.21.0 to version 3.21.1"
+sed -i -e "s/version=3.0.1/version=3.0.2/g" python/python-typeguard/spkgbuild
+changelog "python/python-typeguard/spkgbuild" "Upgraded from version 3.0.1 to version 3.0.2"
+sed -i -e "s/version=0.20.0/version=0.20.9/g" python/python-validators/spkgbuild
+changelog "python/python-validators/spkgbuild" "Upgraded from version 0.20.0 to version 0.20.9"
+sed -i -e "s/version=5.1.1/version=6.0.1/g" python/python-zope-i18nmessageid/spkgbuild
+changelog "python/python-zope-i18nmessageid/spkgbuild" "Upgraded from version 5.1.1 to version 6.0.1"
+sed -i -e "s/version=3.12.15/version=3.12.16/g" server/blacklist-hosts/spkgbuild
+changelog "server/blacklist-hosts/spkgbuild" "Upgraded from version 3.12.15 to version 3.12.16"
+sed -i -e "s/version=2023.3.5/version=2023.3.6/g" server/homeassistant/spkgbuild
+changelog "server/homeassistant/spkgbuild" "Upgraded from version 2023.3.5 to version 2023.3.6"
+sed -i -e "s/version=6.1.1/version=6.2.0/g" xfce/xfsprogs/spkgbuild
+changelog "xfce/xfsprogs/spkgbuild" "Upgraded from version 6.1.1 to version 6.2.0"
