8797b6f11abda88d6993149d03baf977b1ec9187
Voncloft <voncloft@gmail.com>
2023-03-27 10:00:28 -0400

    Minor Updates

---
+++ logs/old-test-updates/2023/Mar/applied_updates-03-27-23.sh
@@ -0,0 +1,20 @@
+sed -i -e "s/version=3.14.0/version=3.14.1/g" core/fuse2/spkgbuild
+changelog "core/fuse2/spkgbuild" "Upgraded from version 3.14.0 to version 3.14.1"
+sed -i -e "s/version=3.14.0/version=3.14.1/g" core/fuse3/spkgbuild
+changelog "core/fuse3/spkgbuild" "Upgraded from version 3.14.0 to version 3.14.1"
+sed -i -e "s/version=0.4.42/version=0.4.44/g" core/gegl/spkgbuild
+changelog "core/gegl/spkgbuild" "Upgraded from version 0.4.42 to version 0.4.44"
+sed -i -e "s/version=7.0.2/version=7.0.3/g" core/texinfo/spkgbuild
+changelog "core/texinfo/spkgbuild" "Upgraded from version 7.0.2 to version 7.0.3"
+sed -i -e "s/version=3.41.0.0/version=3.41.2.0/g" python/python-apsw/spkgbuild
+changelog "python/python-apsw/spkgbuild" "Upgraded from version 3.41.0.0 to version 3.41.2.0"
+sed -i -e "s/version=2.15.0/version=2.15.1/g" python/python-astroid/spkgbuild
+changelog "python/python-astroid/spkgbuild" "Upgraded from version 2.15.0 to version 2.15.1"
+sed -i -e "s/version=3.10.4/version=3.10.6/g" python/python-filelock/spkgbuild
+changelog "python/python-filelock/spkgbuild" "Upgraded from version 3.10.4 to version 3.10.6"
+sed -i -e "s/version=6.3.1/version=6.3.2/g" python/python-rdflib/spkgbuild
+changelog "python/python-rdflib/spkgbuild" "Upgraded from version 6.3.1 to version 6.3.2"
+sed -i -e "s/version=4.0.1/version=5.0.0/g" python/python-sybil/spkgbuild
+changelog "python/python-sybil/spkgbuild" "Upgraded from version 4.0.1 to version 5.0.0"
+sed -i -e "s/version=3.21.1/version=3.21.2/g" python/python-trimesh/spkgbuild
+changelog "python/python-trimesh/spkgbuild" "Upgraded from version 3.21.1 to version 3.21.2"
