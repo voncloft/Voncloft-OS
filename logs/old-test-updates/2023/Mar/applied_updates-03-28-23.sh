d1c3764efc6e474a6b067e94ae42b5104709c643
Voncloft <voncloft@gmail.com>
2023-03-28 09:59:55 -0400

    Minor Updates

---
+++ logs/old-test-updates/2023/Mar/applied_updates-03-28-23.sh
@@ -0,0 +1,32 @@
+sed -i -e "s/version=4.904/version=5.0/g" extra/gnucash/spkgbuild
+changelog "extra/gnucash/spkgbuild" "Upgraded from version 4.904 to version 5.0"
+sed -i -e "s/version=6.3.0/version=6.4.0/g" fonts/awesome-font/spkgbuild
+changelog "fonts/awesome-font/spkgbuild" "Upgraded from version 6.3.0 to version 6.4.0"
+sed -i -e "s/version=112.0b6/version=112.0b7/g" nonfree/firefox/spkgbuild
+changelog "nonfree/firefox/spkgbuild" "Upgraded from version 112.0b6 to version 112.0b7"
+sed -i -e "s/version=1.26.99/version=1.26.100/g" python/python-boto3/spkgbuild
+changelog "python/python-boto3/spkgbuild" "Upgraded from version 1.26.99 to version 1.26.100"
+sed -i -e "s/version=1.29.99/version=1.29.100/g" python/python-botocore/spkgbuild
+changelog "python/python-botocore/spkgbuild" "Upgraded from version 1.29.99 to version 1.29.100"
+sed -i -e "s/version=3.10.6/version=3.10.7/g" python/python-filelock/spkgbuild
+changelog "python/python-filelock/spkgbuild" "Upgraded from version 3.10.6 to version 3.10.7"
+sed -i -e "s/version=6.70.0/version=6.70.1/g" python/python-hypothesis/spkgbuild
+changelog "python/python-hypothesis/spkgbuild" "Upgraded from version 6.70.0 to version 6.70.1"
+sed -i -e "s/version=4.28.0/version=4.29.0/g" python/python-mt-940/spkgbuild
+changelog "python/python-mt-940/spkgbuild" "Upgraded from version 4.28.0 to version 4.29.0"
+sed -i -e "s/version=0.8.1/version=0.9.0/g" python/python-parameterized/spkgbuild
+changelog "python/python-parameterized/spkgbuild" "Upgraded from version 0.8.1 to version 0.9.0"
+sed -i -e "s/version=8.13.7/version=8.13.8/g" python/python-phonenumbers/spkgbuild
+changelog "python/python-phonenumbers/spkgbuild" "Upgraded from version 8.13.7 to version 8.13.8"
+sed -i -e "s/version=13.3.2/version=13.3.3/g" python/python-rich/spkgbuild
+changelog "python/python-rich/spkgbuild" "Upgraded from version 13.3.2 to version 13.3.3"
+sed -i -e "s/version=2023.03.23/version=2023.03.27/g" python/python-sphinx-furo/spkgbuild
+changelog "python/python-sphinx-furo/spkgbuild" "Upgraded from version 2023.03.23 to version 2023.03.27"
+sed -i -e "s/version=0.20.9/version=0.21.0/g" python/python-validators/spkgbuild
+changelog "python/python-validators/spkgbuild" "Upgraded from version 0.20.9 to version 0.21.0"
+sed -i -e "s/version=4.8/version=5.0/g" python/python-zope-annotation/spkgbuild
+changelog "python/python-zope-annotation/spkgbuild" "Upgraded from version 4.8 to version 5.0"
+sed -i -e "s/version=2.4.9/version=2.4.10/g" ruby-gems/ruby-bundler/spkgbuild
+changelog "ruby-gems/ruby-bundler/spkgbuild" "Upgraded from version 2.4.9 to version 2.4.10"
+sed -i -e "s/version=10.6.9/version=11.0/g" server/mariadb/spkgbuild
+changelog "server/mariadb/spkgbuild" "Upgraded from version 10.6.9 to version 11.0"
