703efb96c3edd07ac9d33766242881255b63dd1a
Voncloft <voncloft@gmail.com>
2023-04-05 15:44:59 -0400

    Minor Updates

---
+++ logs/old-test-updates/2023/Apr/applied_updates-04-05-23.sh
@@ -0,0 +1,32 @@
+sed -i -e "s/version=16.0.0/version=16.0.1/g" compilers/clang/spkgbuild
+changelog "compilers/clang/spkgbuild" "Upgraded from version 16.0.0 to version 16.0.1"
+sed -i -e "s/version=3.26.2/version=3.26.3/g" compilers/cmake/spkgbuild
+changelog "compilers/cmake/spkgbuild" "Upgraded from version 3.26.2 to version 3.26.3"
+sed -i -e "s/version=16.0.0/version=16.0.1/g" compilers/lld/spkgbuild
+changelog "compilers/lld/spkgbuild" "Upgraded from version 16.0.0 to version 16.0.1"
+sed -i -e "s/version=16.0.0/version=16.0.1/g" compilers/llvm/spkgbuild
+changelog "compilers/llvm/spkgbuild" "Upgraded from version 16.0.0 to version 16.0.1"
+sed -i -e "s/version=16.0.0/version=16.0.1/g" compilers/llvm-bak/spkgbuild
+changelog "compilers/llvm-bak/spkgbuild" "Upgraded from version 16.0.0 to version 16.0.1"
+sed -i -e "s/version=1.5.4/version=1.5.5/g" core/zstd/spkgbuild
+changelog "core/zstd/spkgbuild" "Upgraded from version 1.5.4 to version 1.5.5"
+sed -i -e "s/version=3.5.35/version=3.5.36/g" extra/flexget/spkgbuild
+changelog "extra/flexget/spkgbuild" "Upgraded from version 3.5.35 to version 3.5.36"
+sed -i -e "s/version=16.0.0/version=16.0.1/g" multilib/llvm-32/spkgbuild
+changelog "multilib/llvm-32/spkgbuild" "Upgraded from version 16.0.0 to version 16.0.1"
+sed -i -e "s/version=2.26.4/version=2.26.5/g" libs/sdl2/spkgbuild
+changelog "libs/sdl2/spkgbuild" "Upgraded from version 2.26.4 to version 2.26.5"
+sed -i -e "s/version=1.43.4/version=1.43.5/g" networking/networkmanager/spkgbuild
+changelog "networking/networkmanager/spkgbuild" "Upgraded from version 1.43.4 to version 1.43.5"
+sed -i -e "s/version=1.26.105/version=1.26.106/g" python/python-boto3/spkgbuild
+changelog "python/python-boto3/spkgbuild" "Upgraded from version 1.26.105 to version 1.26.106"
+sed -i -e "s/version=1.29.105/version=1.29.106/g" python/python-botocore/spkgbuild
+changelog "python/python-botocore/spkgbuild" "Upgraded from version 1.29.105 to version 1.29.106"
+sed -i -e "s/version=2023.3.2/version=2023.3.2.1/g" python/python-distributed/spkgbuild
+changelog "python/python-distributed/spkgbuild" "Upgraded from version 2023.3.2 to version 2023.3.2.1"
+sed -i -e "s/version=2.83.0/version=2.84.0/g" python/python-google-api-python-client/spkgbuild
+changelog "python/python-google-api-python-client/spkgbuild" "Upgraded from version 2.83.0 to version 2.84.0"
+sed -i -e "s/version=2.17.1/version=2.17.2/g" python/python-google-auth/spkgbuild
+changelog "python/python-google-auth/spkgbuild" "Upgraded from version 2.17.1 to version 2.17.2"
+sed -i -e "s/version=4.2.0/version=4.3.0/g" python/python-swiftclient/spkgbuild
+changelog "python/python-swiftclient/spkgbuild" "Upgraded from version 4.2.0 to version 4.3.0"
