41845155fd6bce672c5525b5be5143686f2d5f5d
Voncloft <voncloft@gmail.com>
2023-04-07 00:07:05 -0400

    Minor Updates

---
+++ logs/old-test-updates/2023/Apr/applied_updates-04-07-23.sh
@@ -0,0 +1,52 @@
+sed -i -e "s/version=6.2.9/version=6.2.10/g" core/linux-api-headers/spkgbuild
+changelog "core/linux-api-headers/spkgbuild" "Upgraded from version 6.2.9 to version 6.2.10"
+sed -i -e "s/version=6.2.9/version=6.2.10/g" core/linux-desktop/spkgbuild
+changelog "core/linux-desktop/spkgbuild" "Upgraded from version 6.2.9 to version 6.2.10"
+sed -i -e "s/version=6.2.9/version=6.2.10/g" core/linux-router/spkgbuild
+changelog "core/linux-router/spkgbuild" "Upgraded from version 6.2.9 to version 6.2.10"
+sed -i -e "s/version=0.3.67/version=0.3.68/g" core/pipewire/spkgbuild
+changelog "core/pipewire/spkgbuild" "Upgraded from version 0.3.67 to version 0.3.68"
+sed -i -e "s/version=1.46/version=1.47/g" libs/libgpg-error/spkgbuild
+changelog "libs/libgpg-error/spkgbuild" "Upgraded from version 1.46 to version 1.47"
+sed -i -e "s/version=112.0b6/version=112.0b7/g" nonfree/thunderbird/spkgbuild
+changelog "nonfree/thunderbird/spkgbuild" "Upgraded from version 112.0b6 to version 112.0b7"
+sed -i -e "s/version=1.4.15/version=1.4.16/g" python/python-aiodiscover/spkgbuild
+changelog "python/python-aiodiscover/spkgbuild" "Upgraded from version 1.4.15 to version 1.4.16"
+sed -i -e "s/version=1.63.0/version=1.64.0/g" python/python-aws-sam-translator/spkgbuild
+changelog "python/python-aws-sam-translator/spkgbuild" "Upgraded from version 1.63.0 to version 1.64.0"
+sed -i -e "s/version=1.26.106/version=1.26.107/g" python/python-boto3/spkgbuild
+changelog "python/python-boto3/spkgbuild" "Upgraded from version 1.26.106 to version 1.26.107"
+sed -i -e "s/version=1.29.106/version=1.29.107/g" python/python-botocore/spkgbuild
+changelog "python/python-botocore/spkgbuild" "Upgraded from version 1.29.106 to version 1.29.107"
+sed -i -e "s/version=7.2.2/version=7.2.3/g" python/python-coverage/spkgbuild
+changelog "python/python-coverage/spkgbuild" "Upgraded from version 7.2.2 to version 7.2.3"
+sed -i -e "s/version=18.3.4/version=18.4.0/g" python/python-faker/spkgbuild
+changelog "python/python-faker/spkgbuild" "Upgraded from version 18.3.4 to version 18.4.0"
+sed -i -e "s/version=3.10.7/version=3.11.0/g" python/python-filelock/spkgbuild
+changelog "python/python-filelock/spkgbuild" "Upgraded from version 3.10.7 to version 3.11.0"
+sed -i -e "s/version=3.3.0/version=3.4.0/g" python/python-internetarchive/spkgbuild
+changelog "python/python-internetarchive/spkgbuild" "Upgraded from version 3.3.0 to version 3.4.0"
+sed -i -e "s/version=1.4.0/version=1.4.1/g" python/python-lmdb/spkgbuild
+changelog "python/python-lmdb/spkgbuild" "Upgraded from version 1.4.0 to version 1.4.1"
+sed -i -e "s/version=7.4.4/version=8.0.0/g" python/python-pyface/spkgbuild
+changelog "python/python-pyface/spkgbuild" "Upgraded from version 7.4.4 to version 8.0.0"
+sed -i -e "s/version=2.0.5/version=2.0.30/g" python/python-reedsolo/spkgbuild
+changelog "python/python-reedsolo/spkgbuild" "Upgraded from version 2.0.5 to version 2.0.30"
+sed -i -e "s/version=3.18.4/version=3.19.1/g" python/python-simplejson/spkgbuild
+changelog "python/python-simplejson/spkgbuild" "Upgraded from version 3.18.4 to version 3.19.1"
+sed -i -e "s/version=22.3.0/version=23.1.0/g" python/python-structlog/spkgbuild
+changelog "python/python-structlog/spkgbuild" "Upgraded from version 22.3.0 to version 23.1.0"
+sed -i -e "s/version=11.0/version=11.0.1/g" python/python-websockets/spkgbuild
+changelog "python/python-websockets/spkgbuild" "Upgraded from version 11.0 to version 11.0.1"
+sed -i -e "s/version=5.15.8/version=5.15.9/g" qt/qt5/spkgbuild
+changelog "qt/qt5/spkgbuild" "Upgraded from version 5.15.8 to version 5.15.9"
+sed -i -e "s/version=5.15.8/version=5.15.9/g" qt/qt5-multimedia/spkgbuild
+changelog "qt/qt5-multimedia/spkgbuild" "Upgraded from version 5.15.8 to version 5.15.9"
+sed -i -e "s/version=5.15.8/version=5.15.9/g" qt/qt5-speech/spkgbuild
+changelog "qt/qt5-speech/spkgbuild" "Upgraded from version 5.15.8 to version 5.15.9"
+sed -i -e "s/version=5.15.8/version=5.15.9/g" qt/qt5-x11extras/spkgbuild
+changelog "qt/qt5-x11extras/spkgbuild" "Upgraded from version 5.15.8 to version 5.15.9"
+sed -i -e "s/version=5.15.8/version=5.15.9/g" qt/qtsql/spkgbuild
+changelog "qt/qtsql/spkgbuild" "Upgraded from version 5.15.8 to version 5.15.9"
+sed -i -e "s/version=2.4.56/version=2.4.57/g" server/httpd/spkgbuild
+changelog "server/httpd/spkgbuild" "Upgraded from version 2.4.56 to version 2.4.57"
