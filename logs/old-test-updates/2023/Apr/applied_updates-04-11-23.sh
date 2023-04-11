f5bc93292db8635b6d402df0d69568d4f4f5f547
Voncloft <voncloft@gmail.com>
2023-04-11 08:09:23 -0400

    Minor Updates

---
+++ logs/old-test-updates/2023/Apr/applied_updates-04-11-23.sh
@@ -0,0 +1,26 @@
+sed -i -e "s/version=3.3.2/version=3.3.3/g" core/icewm/spkgbuild
+changelog "core/icewm/spkgbuild" "Upgraded from version 3.3.2 to version 3.3.3"
+sed -i -e "s/version=6.24.07/version=6.24.08/g" core/tcsh/spkgbuild
+changelog "core/tcsh/spkgbuild" "Upgraded from version 6.24.07 to version 6.24.08"
+sed -i -e "s/version=3.5.36/version=3.6.0/g" extra/flexget/spkgbuild
+changelog "extra/flexget/spkgbuild" "Upgraded from version 3.5.36 to version 3.6.0"
+sed -i -e "s/version=1.10.3/version=1.10.4/g" libs/libpcap/spkgbuild
+changelog "libs/libpcap/spkgbuild" "Upgraded from version 1.10.3 to version 1.10.4"
+sed -i -e "s/version=4.99.3/version=4.99.4/g" networking/tcpdump/spkgbuild
+changelog "networking/tcpdump/spkgbuild" "Upgraded from version 4.99.3 to version 4.99.4"
+sed -i -e "s/version=1.26.109/version=1.26.110/g" python/python-boto3/spkgbuild
+changelog "python/python-boto3/spkgbuild" "Upgraded from version 1.26.109 to version 1.26.110"
+sed -i -e "s/version=1.29.109/version=1.29.110/g" python/python-botocore/spkgbuild
+changelog "python/python-botocore/spkgbuild" "Upgraded from version 1.29.109 to version 1.29.110"
+sed -i -e "s/version=6.2.1/version=6.3.0/g" python/python-importlib-metadata/spkgbuild
+changelog "python/python-importlib-metadata/spkgbuild" "Upgraded from version 6.2.1 to version 6.3.0"
+sed -i -e "s/version=7.3.2302281257/version=7.3.2304092211/g" python/python-pycontracts/spkgbuild
+changelog "python/python-pycontracts/spkgbuild" "Upgraded from version 7.3.2302281257 to version 7.3.2304092211"
+sed -i -e "s/version=0.9.4/version=0.9.7/g" python/python-pytest-sugar/spkgbuild
+changelog "python/python-pytest-sugar/spkgbuild" "Upgraded from version 0.9.4 to version 0.9.7"
+sed -i -e "s/version=5.2.1/version=5.2.2/g" python/python-tempora/spkgbuild
+changelog "python/python-tempora/spkgbuild" "Upgraded from version 5.2.1 to version 5.2.2"
+sed -i -e "s/version=6.2.0/version=6.3.0/g" python/python-tornado/spkgbuild
+changelog "python/python-tornado/spkgbuild" "Upgraded from version 6.2.0 to version 6.3.0"
+sed -i -e "s/version=1.31.3.6868/version=1.32.0.6918/g" server/plex-media-server/spkgbuild
+changelog "server/plex-media-server/spkgbuild" "Upgraded from version 1.31.3.6868 to version 1.32.0.6918"
