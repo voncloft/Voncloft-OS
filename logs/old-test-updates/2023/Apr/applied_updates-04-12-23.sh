938ec2818106d1a578bb518e811ec9386a70481d
Voncloft <voncloft@gmail.com>
2023-04-12 07:23:17 -0400

    Minor Updates

---
+++ logs/old-test-updates/2023/Apr/applied_updates-04-12-23.sh
@@ -0,0 +1,44 @@
+sed -i -e "s/version=1.68.2/version=⁨1.68.2⁩/g" compilers/rust/spkgbuild
+changelog "compilers/rust/spkgbuild" "Upgraded from version 1.68.2 to version ⁨1.68.2⁩"
+sed -i -e "s/version=1.19.0/version=1.19.1/g" core/conky/spkgbuild
+changelog "core/conky/spkgbuild" "Upgraded from version 1.19.0 to version 1.19.1"
+sed -i -e "s/version=3.5/version=3.6/g" core/parted/spkgbuild
+changelog "core/parted/spkgbuild" "Upgraded from version 3.5 to version 3.6"
+sed -i -e "s/version=3.6.0/version=3.6.1/g" extra/flexget/spkgbuild
+changelog "extra/flexget/spkgbuild" "Upgraded from version 3.6.0 to version 3.6.1"
+sed -i -e "s/version=1.22.1/version=1.22.2/g" media/gst-libav/spkgbuild
+changelog "media/gst-libav/spkgbuild" "Upgraded from version 1.22.1 to version 1.22.2"
+sed -i -e "s/version=1.22.1/version=1.22.2/g" media/gst-plugins-bad/spkgbuild
+changelog "media/gst-plugins-bad/spkgbuild" "Upgraded from version 1.22.1 to version 1.22.2"
+sed -i -e "s/version=1.22.1/version=1.22.2/g" media/gst-plugins-base/spkgbuild
+changelog "media/gst-plugins-base/spkgbuild" "Upgraded from version 1.22.1 to version 1.22.2"
+sed -i -e "s/version=1.22.1/version=1.22.2/g" media/gst-plugins-good/spkgbuild
+changelog "media/gst-plugins-good/spkgbuild" "Upgraded from version 1.22.1 to version 1.22.2"
+sed -i -e "s/version=1.22.1/version=1.22.2/g" media/gst-plugins-ugly/spkgbuild
+changelog "media/gst-plugins-ugly/spkgbuild" "Upgraded from version 1.22.1 to version 1.22.2"
+sed -i -e "s/version=1.22.1/version=1.22.2/g" media/gstreamer/spkgbuild
+changelog "media/gstreamer/spkgbuild" "Upgraded from version 1.22.1 to version 1.22.2"
+sed -i -e "s/version=112.0b9/version=113.0b1/g" nonfree/firefox/spkgbuild
+changelog "nonfree/firefox/spkgbuild" "Upgraded from version 112.0b9 to version 113.0b1"
+sed -i -e "s/version=4.1.0/version=4.1.1/g" python/python-elementpath/spkgbuild
+changelog "python/python-elementpath/spkgbuild" "Upgraded from version 4.1.0 to version 4.1.1"
+sed -i -e "s/version=3.13.0/version=3.14.0/g" python/python-gitlab/spkgbuild
+changelog "python/python-gitlab/spkgbuild" "Upgraded from version 3.13.0 to version 3.14.0"
+sed -i -e "s/version=2.84.0/version=2.85.0/g" python/python-google-api-python-client/spkgbuild
+changelog "python/python-google-api-python-client/spkgbuild" "Upgraded from version 2.84.0 to version 2.85.0"
+sed -i -e "s/version=8.13.8/version=8.13.9/g" python/python-phonenumbers/spkgbuild
+changelog "python/python-phonenumbers/spkgbuild" "Upgraded from version 8.13.8 to version 8.13.9"
+sed -i -e "s/version=4.0.35/version=4.0.38/g" python/python-pyodbc/spkgbuild
+changelog "python/python-pyodbc/spkgbuild" "Upgraded from version 4.0.35 to version 4.0.38"
+sed -i -e "s/version=0.23.0/version=0.24.1/g" python/python-qasync/spkgbuild
+changelog "python/python-qasync/spkgbuild" "Upgraded from version 0.23.0 to version 0.24.1"
+sed -i -e "s/version=2.0.31/version=2.1.0/g" python/python-reedsolo/spkgbuild
+changelog "python/python-reedsolo/spkgbuild" "Upgraded from version 2.0.31 to version 2.1.0"
+sed -i -e "s/version=0.16.7/version=0.17.0/g" python/python-scikit-build/spkgbuild
+changelog "python/python-scikit-build/spkgbuild" "Upgraded from version 0.16.7 to version 0.17.0"
+sed -i -e "s/version=3.21.4/version=3.21.5/g" python/python-trimesh/spkgbuild
+changelog "python/python-trimesh/spkgbuild" "Upgraded from version 3.21.4 to version 3.21.5"
+sed -i -e "s/version=2.4.10/version=2.4.11/g" ruby-gems/ruby-bundler/spkgbuild
+changelog "ruby-gems/ruby-bundler/spkgbuild" "Upgraded from version 2.4.10 to version 2.4.11"
+sed -i -e "s/version=1.23.4/version=1.24.0/g" server/nginx/spkgbuild
+changelog "server/nginx/spkgbuild" "Upgraded from version 1.23.4 to version 1.24.0"
