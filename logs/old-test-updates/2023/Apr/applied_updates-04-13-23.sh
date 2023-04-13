826d28dc0754aede798f5828682a533263056ddb
Voncloft <voncloft@gmail.com>
2023-04-13 15:44:30 -0400

    Minor Updates

---
+++ logs/old-test-updates/2023/Apr/applied_updates-04-13-23.sh
@@ -0,0 +1,76 @@
+sed -i -e "s/version=6.2.10/version=6.2.11/g" core/linux-api-headers/spkgbuild
+changelog "core/linux-api-headers/spkgbuild" "Upgraded from version 6.2.10 to version 6.2.11"
+sed -i -e "s/version=6.2.10/version=6.2.11/g" core/linux-desktop/spkgbuild
+changelog "core/linux-desktop/spkgbuild" "Upgraded from version 6.2.10 to version 6.2.11"
+sed -i -e "s/version=6.2.10/version=6.2.11/g" core/linux-router/spkgbuild
+changelog "core/linux-router/spkgbuild" "Upgraded from version 6.2.10 to version 6.2.11"
+sed -i -e "s/version=6.4/version=6.4.1/g" core/mercurial/spkgbuild
+changelog "core/mercurial/spkgbuild" "Upgraded from version 6.4 to version 6.4.1"
+sed -i -e "s/version=4.7/version=4.8/g" core/nnn/spkgbuild
+changelog "core/nnn/spkgbuild" "Upgraded from version 4.7 to version 4.8"
+sed -i -e "s/version=0.3.68/version=0.3.69/g" core/pipewire/spkgbuild
+changelog "core/pipewire/spkgbuild" "Upgraded from version 0.3.68 to version 0.3.69"
+sed -i -e "s/version=5.3.1/version=5.4.0/g" core/streamlink/spkgbuild
+changelog "core/streamlink/spkgbuild" "Upgraded from version 5.3.1 to version 5.4.0"
+sed -i -e "s/version=6.24.08/version=6.24.09/g" core/tcsh/spkgbuild
+changelog "core/tcsh/spkgbuild" "Upgraded from version 6.24.08 to version 6.24.09"
+sed -i -e "s/version=3.6.1/version=3.6.2/g" extra/flexget/spkgbuild
+changelog "extra/flexget/spkgbuild" "Upgraded from version 3.6.1 to version 3.6.2"
+sed -i -e "s/version=22.2/version=22.3/g" extra/protobuf/spkgbuild
+changelog "extra/protobuf/spkgbuild" "Upgraded from version 22.2 to version 22.3"
+sed -i -e "s/version=23.03.80/version=23.03.90/g" kde-apps/kalendar/spkgbuild
+changelog "kde-apps/kalendar/spkgbuild" "Upgraded from version 23.03.80 to version 23.03.90"
+sed -i -e "s/version=0.7.2/version=0.8.0/g" kf5/kirigami-addons/spkgbuild
+changelog "kf5/kirigami-addons/spkgbuild" "Upgraded from version 0.7.2 to version 0.8.0"
+sed -i -e "s/version=1.23.0/version=1.23.1/g" media/openal/spkgbuild
+changelog "media/openal/spkgbuild" "Upgraded from version 1.23.0 to version 1.23.1"
+sed -i -e "s/version=0.41.1/version=0.42.0/g" libs/libfilezilla/spkgbuild
+changelog "libs/libfilezilla/spkgbuild" "Upgraded from version 0.41.1 to version 0.42.0"
+sed -i -e "s/version=2.6.2/version=2.6.3/g" networking/openvpn/spkgbuild
+changelog "networking/openvpn/spkgbuild" "Upgraded from version 2.6.2 to version 2.6.3"
+sed -i -e "s/version=4.0.4/version=4.0.5/g" networking/wireshark/spkgbuild
+changelog "networking/wireshark/spkgbuild" "Upgraded from version 4.0.4 to version 4.0.5"
+sed -i -e "s/version=113.0b1/version=113.0b2/g" nonfree/firefox/spkgbuild
+changelog "nonfree/firefox/spkgbuild" "Upgraded from version 113.0b1 to version 113.0b2"
+sed -i -e "s/version=112.0b7/version=113.0b1/g" nonfree/thunderbird/spkgbuild
+changelog "nonfree/thunderbird/spkgbuild" "Upgraded from version 112.0b7 to version 113.0b1"
+sed -i -e "s/version=1.26.110/version=1.26.113/g" python/python-boto3/spkgbuild
+changelog "python/python-boto3/spkgbuild" "Upgraded from version 1.26.110 to version 1.26.113"
+sed -i -e "s/version=1.29.110/version=1.29.113/g" python/python-botocore/spkgbuild
+changelog "python/python-botocore/spkgbuild" "Upgraded from version 1.29.110 to version 1.29.113"
+sed -i -e "s/version=0.95.0/version=0.95.1/g" python/python-fastapi/spkgbuild
+changelog "python/python-fastapi/spkgbuild" "Upgraded from version 0.95.0 to version 0.95.1"
+sed -i -e "s/version=2.17.2/version=2.17.3/g" python/python-google-auth/spkgbuild
+changelog "python/python-google-auth/spkgbuild" "Upgraded from version 2.17.2 to version 2.17.3"
+sed -i -e "s/version=1.18.2/version=1.19.0/g" python/python-hcloud/spkgbuild
+changelog "python/python-hcloud/spkgbuild" "Upgraded from version 1.18.2 to version 1.19.0"
+sed -i -e "s/version=4.29.0/version=4.30.0/g" python/python-mt-940/spkgbuild
+changelog "python/python-mt-940/spkgbuild" "Upgraded from version 4.29.0 to version 4.30.0"
+sed -i -e "s/version=0.29.0/version=0.29.1/g" python/python-owslib/spkgbuild
+changelog "python/python-owslib/spkgbuild" "Upgraded from version 0.29.0 to version 0.29.1"
+sed -i -e "s/version=23.0/version=23.1/g" python/python-packaging/spkgbuild
+changelog "python/python-packaging/spkgbuild" "Upgraded from version 23.0 to version 23.1"
+sed -i -e "s/version=0.4.11/version=0.4.12/g" python/python-pipreqs/spkgbuild
+changelog "python/python-pipreqs/spkgbuild" "Upgraded from version 0.4.11 to version 0.4.12"
+sed -i -e "s/version=0.21.3/version=0.22.0/g" python/python-pytest-httpx/spkgbuild
+changelog "python/python-pytest-httpx/spkgbuild" "Upgraded from version 0.21.3 to version 0.22.0"
+sed -i -e "s/version=13.3.3/version=13.3.4/g" python/python-rich/spkgbuild
+changelog "python/python-rich/spkgbuild" "Upgraded from version 13.3.3 to version 13.3.4"
+sed -i -e "s/version=0.17.0/version=0.17.1/g" python/python-scikit-build/spkgbuild
+changelog "python/python-scikit-build/spkgbuild" "Upgraded from version 0.17.0 to version 0.17.1"
+sed -i -e "s/version=0.40.0/version=0.41.0/g" python/python-sqlalchemy-utils/spkgbuild
+changelog "python/python-sqlalchemy-utils/spkgbuild" "Upgraded from version 0.40.0 to version 0.41.0"
+sed -i -e "s/version=0.21.0/version=0.21.1/g" python/python-validators/spkgbuild
+changelog "python/python-validators/spkgbuild" "Upgraded from version 0.21.0 to version 0.21.1"
+sed -i -e "s/version=3.0.9/version=3.1.0/g" python/python-xlsxwriter/spkgbuild
+changelog "python/python-xlsxwriter/spkgbuild" "Upgraded from version 3.0.9 to version 3.1.0"
+sed -i -e "s/version=2.4.11/version=2.4.12/g" ruby-gems/ruby-bundler/spkgbuild
+changelog "ruby-gems/ruby-bundler/spkgbuild" "Upgraded from version 2.4.11 to version 2.4.12"
+sed -i -e "s/version=0.9.32/version=0.9.34/g" ruby-gems/ruby-yard/spkgbuild
+changelog "ruby-gems/ruby-yard/spkgbuild" "Upgraded from version 0.9.32 to version 0.9.34"
+sed -i -e "s/version=3.12.18/version=3.12.19/g" server/blacklist-hosts/spkgbuild
+changelog "server/blacklist-hosts/spkgbuild" "Upgraded from version 3.12.18 to version 3.12.19"
+sed -i -e "s/version=2023.4.2/version=2023.4.4/g" server/homeassistant/spkgbuild
+changelog "server/homeassistant/spkgbuild" "Upgraded from version 2023.4.2 to version 2023.4.4"
+sed -i -e "s/version=8.2.4/version=8.2.5/g" server/php/spkgbuild
+changelog "server/php/spkgbuild" "Upgraded from version 8.2.4 to version 8.2.5"
