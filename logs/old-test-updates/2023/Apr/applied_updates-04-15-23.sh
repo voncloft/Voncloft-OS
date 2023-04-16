cab74afb9a1fade88d805f900523919b7b96a02b
Voncloft <voncloft@gmail.com>
2023-04-15 20:07:21 -0400

    Minor Updates

---
+++ logs/old-test-updates/2023/Apr/applied_updates-04-15-23.sh
@@ -0,0 +1,90 @@
+sed -i -e "s/version=6.24.09/version=6.24.10/g" core/tcsh/spkgbuild
+changelog "core/tcsh/spkgbuild" "Upgraded from version 6.24.09 to version 6.24.10"
+sed -i -e "s/version=2.41.1/version=2.41.2/g" core/webkitgtk/spkgbuild
+changelog "core/webkitgtk/spkgbuild" "Upgraded from version 2.41.1 to version 2.41.2"
+sed -i -e "s/version=3.6.2/version=3.6.3/g" extra/flexget/spkgbuild
+changelog "extra/flexget/spkgbuild" "Upgraded from version 3.6.2 to version 3.6.3"
+sed -i -e "s/version=1.2.0/version=1.3.0/g" lxde/lximage-qt/spkgbuild
+changelog "lxde/lximage-qt/spkgbuild" "Upgraded from version 1.2.0 to version 1.3.0"
+sed -i -e "s/version=1.2.0/version=1.3.0/g" lxqt/lxqt-about/spkgbuild
+changelog "lxqt/lxqt-about/spkgbuild" "Upgraded from version 1.2.0 to version 1.3.0"
+sed -i -e "s/version=1.2.0/version=1.3.0/g" lxqt/lxqt-admin/spkgbuild
+changelog "lxqt/lxqt-admin/spkgbuild" "Upgraded from version 1.2.0 to version 1.3.0"
+sed -i -e "s/version=0.7.0/version=0.8.0/g" lxqt/lxqt-archiver/spkgbuild
+changelog "lxqt/lxqt-archiver/spkgbuild" "Upgraded from version 0.7.0 to version 0.8.0"
+sed -i -e "s/version=0.12.0/version=0.13.0/g" lxqt/lxqt-build-tools/spkgbuild
+changelog "lxqt/lxqt-build-tools/spkgbuild" "Upgraded from version 0.12.0 to version 0.13.0"
+sed -i -e "s/version=1.2.0/version=1.3.0/g" lxqt/lxqt-config/spkgbuild
+changelog "lxqt/lxqt-config/spkgbuild" "Upgraded from version 1.2.0 to version 1.3.0"
+sed -i -e "s/version=1.2.0/version=1.3.0/g" lxqt/lxqt-globalkeys/spkgbuild
+changelog "lxqt/lxqt-globalkeys/spkgbuild" "Upgraded from version 1.2.0 to version 1.3.0"
+sed -i -e "s/version=1.2.0/version=1.3.0/g" lxqt/lxqt-notificationd/spkgbuild
+changelog "lxqt/lxqt-notificationd/spkgbuild" "Upgraded from version 1.2.0 to version 1.3.0"
+sed -i -e "s/version=1.2.0/version=1.3.0/g" lxqt/lxqt-openssh-askpass/spkgbuild
+changelog "lxqt/lxqt-openssh-askpass/spkgbuild" "Upgraded from version 1.2.0 to version 1.3.0"
+sed -i -e "s/version=1.2.1/version=1.3.0/g" lxqt/lxqt-panel/spkgbuild
+changelog "lxqt/lxqt-panel/spkgbuild" "Upgraded from version 1.2.1 to version 1.3.0"
+sed -i -e "s/version=1.2.0/version=1.3.0/g" lxqt/lxqt-policykit/spkgbuild
+changelog "lxqt/lxqt-policykit/spkgbuild" "Upgraded from version 1.2.0 to version 1.3.0"
+sed -i -e "s/version=1.2.0/version=1.3.0/g" lxqt/lxqt-powermanagement/spkgbuild
+changelog "lxqt/lxqt-powermanagement/spkgbuild" "Upgraded from version 1.2.0 to version 1.3.0"
+sed -i -e "s/version=1.2.0/version=1.3.0/g" lxqt/lxqt-qtplugin/spkgbuild
+changelog "lxqt/lxqt-qtplugin/spkgbuild" "Upgraded from version 1.2.0 to version 1.3.0"
+sed -i -e "s/version=1.2.0/version=1.3.0/g" lxqt/lxqt-runner/spkgbuild
+changelog "lxqt/lxqt-runner/spkgbuild" "Upgraded from version 1.2.0 to version 1.3.0"
+sed -i -e "s/version=1.2.0/version=1.3.0/g" lxqt/lxqt-session/spkgbuild
+changelog "lxqt/lxqt-session/spkgbuild" "Upgraded from version 1.2.0 to version 1.3.0"
+sed -i -e "s/version=1.2.0/version=1.3.0/g" lxqt/lxqt-sudo/spkgbuild
+changelog "lxqt/lxqt-sudo/spkgbuild" "Upgraded from version 1.2.0 to version 1.3.0"
+sed -i -e "s/version=1.2.0/version=1.3.0/g" lxqt/lxqt-themes/spkgbuild
+changelog "lxqt/lxqt-themes/spkgbuild" "Upgraded from version 1.2.0 to version 1.3.0"
+sed -i -e "s/version=1.2.1/version=1.3.0/g" libs/libfm-qt/spkgbuild
+changelog "libs/libfm-qt/spkgbuild" "Upgraded from version 1.2.1 to version 1.3.0"
+sed -i -e "s/version=1.2.0/version=1.3.0/g" libs/liblxqt/spkgbuild
+changelog "libs/liblxqt/spkgbuild" "Upgraded from version 1.2.0 to version 1.3.0"
+sed -i -e "s/version=3.10.0/version=3.11.0/g" libs/libqtxdg/spkgbuild
+changelog "libs/libqtxdg/spkgbuild" "Upgraded from version 3.10.0 to version 3.11.0"
+sed -i -e "s/version=113.0b2/version=113.0b3/g" nonfree/firefox/spkgbuild
+changelog "nonfree/firefox/spkgbuild" "Upgraded from version 113.0b2 to version 113.0b3"
+sed -i -e "s/version=2.2/version=4.1/g" python/python-argparse-manpage/spkgbuild
+changelog "python/python-argparse-manpage/spkgbuild" "Upgraded from version 2.2 to version 4.1"
+sed -i -e "s/version=1.26.113/version=1.26.114/g" python/python-boto3/spkgbuild
+changelog "python/python-boto3/spkgbuild" "Upgraded from version 1.26.113 to version 1.26.114"
+sed -i -e "s/version=1.29.113/version=1.29.114/g" python/python-botocore/spkgbuild
+changelog "python/python-botocore/spkgbuild" "Upgraded from version 1.29.113 to version 1.29.114"
+sed -i -e "s/version=1.5.0/version=1.5.1/g" python/python-cairocffi/spkgbuild
+changelog "python/python-cairocffi/spkgbuild" "Upgraded from version 1.5.0 to version 1.5.1"
+sed -i -e "s/version=2023.3.2.1/version=2023.4.0/g" python/python-distributed/spkgbuild
+changelog "python/python-distributed/spkgbuild" "Upgraded from version 2023.3.2.1 to version 2023.4.0"
+sed -i -e "s/version=11.36.0/version=11.36.1/g" python/python-dropbox/spkgbuild
+changelog "python/python-dropbox/spkgbuild" "Upgraded from version 11.36.0 to version 11.36.1"
+sed -i -e "s/version=6.3.0/version=6.4.1/g" python/python-importlib-metadata/spkgbuild
+changelog "python/python-importlib-metadata/spkgbuild" "Upgraded from version 6.3.0 to version 6.4.1"
+sed -i -e "s/version=3.25.0/version=3.26.0/g" python/python-openmdao/spkgbuild
+changelog "python/python-openmdao/spkgbuild" "Upgraded from version 3.25.0 to version 3.26.0"
+sed -i -e "s/version=23.0.1/version=23.1/g" python/python-pip/spkgbuild
+changelog "python/python-pip/spkgbuild" "Upgraded from version 23.0.1 to version 23.1"
+sed -i -e "s/version=0.4.12/version=0.4.13/g" python/python-pipreqs/spkgbuild
+changelog "python/python-pipreqs/spkgbuild" "Upgraded from version 0.4.12 to version 0.4.13"
+sed -i -e "s/version=0.0.14/version=0.0.15/g" python/python-pyadi-iio/spkgbuild
+changelog "python/python-pyadi-iio/spkgbuild" "Upgraded from version 0.0.14 to version 0.0.15"
+sed -i -e "s/version=4.0.38/version=4.0.39/g" python/python-pyodbc/spkgbuild
+changelog "python/python-pyodbc/spkgbuild" "Upgraded from version 4.0.38 to version 4.0.39"
+sed -i -e "s/version=0.13.2/version=0.13.3/g" python/python-pyqtgraph/spkgbuild
+changelog "python/python-pyqtgraph/spkgbuild" "Upgraded from version 0.13.2 to version 0.13.3"
+sed -i -e "s/version=1.2.0/version=1.3.0/g" python/python-xcffib/spkgbuild
+changelog "python/python-xcffib/spkgbuild" "Upgraded from version 1.2.0 to version 1.3.0"
+sed -i -e "s/version=2.2.2/version=2.2.3/g" python/python-xmlschema/spkgbuild
+changelog "python/python-xmlschema/spkgbuild" "Upgraded from version 2.2.2 to version 2.2.3"
+sed -i -e "s/version=5.1.0/version=6.0/g" python/python-zope-component/spkgbuild
+changelog "python/python-zope-component/spkgbuild" "Upgraded from version 5.1.0 to version 6.0"
+sed -i -e "s/version=1.2.0/version=1.3.0/g" qt/pavucontrol-qt/spkgbuild
+changelog "qt/pavucontrol-qt/spkgbuild" "Upgraded from version 1.2.0 to version 1.3.0"
+sed -i -e "s/version=1.2.1/version=1.3.0/g" qt/pcmanfm-qt/spkgbuild
+changelog "qt/pcmanfm-qt/spkgbuild" "Upgraded from version 1.2.1 to version 1.3.0"
+sed -i -e "s/version=1.2.0/version=1.3.0/g" qt/qterminal/spkgbuild
+changelog "qt/qterminal/spkgbuild" "Upgraded from version 1.2.0 to version 1.3.0"
+sed -i -e "s/version=1.2.0/version=1.3.0/g" qt/qtermwidget/spkgbuild
+changelog "qt/qtermwidget/spkgbuild" "Upgraded from version 1.2.0 to version 1.3.0"
+sed -i -e "s/version=4.19.0/version=4.19.1/g" xfce/xfdesktop/spkgbuild
+changelog "xfce/xfdesktop/spkgbuild" "Upgraded from version 4.19.0 to version 4.19.1"
