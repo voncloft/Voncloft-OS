52a44286bd51868953c2b5bd01a145528a4c1a26
Voncloft <voncloft@gmail.com>
2023-04-17 10:18:56 -0400

    Minor Updates

---
+++ logs/old-test-updates/2023/Apr/applied_updates-04-17-23.sh
@@ -0,0 +1,10 @@
+sed -i -e "s/version=1.7.3/version=1.7.4/g" libs/apr/spkgbuild
+changelog "libs/apr/spkgbuild" "Upgraded from version 1.7.3 to version 1.7.4"
+sed -i -e "s/version=4.0.1/version=4.0.2/g" python/python-bitstring/spkgbuild
+changelog "python/python-bitstring/spkgbuild" "Upgraded from version 4.0.1 to version 4.0.2"
+sed -i -e "s/version=1.3.1/version=2.0.0/g" python/python-email-validator/spkgbuild
+changelog "python/python-email-validator/spkgbuild" "Upgraded from version 1.3.1 to version 2.0.0"
+sed -i -e "s/version=6.71.0/version=6.72.0/g" python/python-hypothesis/spkgbuild
+changelog "python/python-hypothesis/spkgbuild" "Upgraded from version 6.71.0 to version 6.72.0"
+sed -i -e "s/version=5.0.1/version=5.0.2/g" python/python-mock/spkgbuild
+changelog "python/python-mock/spkgbuild" "Upgraded from version 5.0.1 to version 5.0.2"
