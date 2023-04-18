f2295b0c051ddd4e358ff69234ae9040657d96db
Voncloft <voncloft@gmail.com>
2023-04-18 07:53:10 -0400

    Minor Updates

---
+++ logs/old-test-updates/2023/Apr/applied_updates-04-18-23.sh
@@ -0,0 +1,34 @@
+sed -i -e "s/version=4.6.0/version=4.6.1/g" core/qalculate/spkgbuild
+changelog "core/qalculate/spkgbuild" "Upgraded from version 4.6.0 to version 4.6.1"
+sed -i -e "s/version=7.0.10/version=7.0.11/g" core/redis/spkgbuild
+changelog "core/redis/spkgbuild" "Upgraded from version 7.0.10 to version 7.0.11"
+sed -i -e "s/version=4.6.0/version=4.6.1/g" libs/libqalculate/spkgbuild
+changelog "libs/libqalculate/spkgbuild" "Upgraded from version 4.6.0 to version 4.6.1"
+sed -i -e "s/version=4.18.3/version=4.19.0/g" libs/libxfce4ui/spkgbuild
+changelog "libs/libxfce4ui/spkgbuild" "Upgraded from version 4.18.3 to version 4.19.0"
+sed -i -e "s/version=4.18.1/version=4.19.0/g" libs/libxfce4util/spkgbuild
+changelog "libs/libxfce4util/spkgbuild" "Upgraded from version 4.18.1 to version 4.19.0"
+sed -i -e "s/version=113.0b3/version=113.0b4/g" nonfree/firefox/spkgbuild
+changelog "nonfree/firefox/spkgbuild" "Upgraded from version 113.0b3 to version 113.0b4"
+sed -i -e "s/version=0.18.0/version=0.19.0/g" python/python-aiosqlite/spkgbuild
+changelog "python/python-aiosqlite/spkgbuild" "Upgraded from version 0.18.0 to version 0.19.0"
+sed -i -e "s/version=2.15.2/version=2.15.3/g" python/python-astroid/spkgbuild
+changelog "python/python-astroid/spkgbuild" "Upgraded from version 2.15.2 to version 2.15.3"
+sed -i -e "s/version=1.26.114/version=1.26.115/g" python/python-boto3/spkgbuild
+changelog "python/python-boto3/spkgbuild" "Upgraded from version 1.26.114 to version 1.26.115"
+sed -i -e "s/version=1.29.114/version=1.29.115/g" python/python-botocore/spkgbuild
+changelog "python/python-botocore/spkgbuild" "Upgraded from version 1.29.114 to version 1.29.115"
+sed -i -e "s/version=5.9.4/version=5.9.5/g" python/python-psutil/spkgbuild
+changelog "python/python-psutil/spkgbuild" "Upgraded from version 5.9.4 to version 5.9.5"
+sed -i -e "s/version=2.4/version=2.4.1/g" python/python-soupsieve/spkgbuild
+changelog "python/python-soupsieve/spkgbuild" "Upgraded from version 2.4 to version 2.4.1"
+sed -i -e "s/version=0.20.0/version=0.21.0/g" python/python-zstandard/spkgbuild
+changelog "python/python-zstandard/spkgbuild" "Upgraded from version 0.20.0 to version 0.21.0"
+sed -i -e "s/version=3.12.19/version=3.12.20/g" server/blacklist-hosts/spkgbuild
+changelog "server/blacklist-hosts/spkgbuild" "Upgraded from version 3.12.19 to version 3.12.20"
+sed -i -e "s/version=3.7.4/version=3.8.0/g" server/postfix/spkgbuild
+changelog "server/postfix/spkgbuild" "Upgraded from version 3.7.4 to version 3.8.0"
+sed -i -e "s/version=4.18.0/version=4.19.0/g" xfce/tumbler/spkgbuild
+changelog "xfce/tumbler/spkgbuild" "Upgraded from version 4.18.0 to version 4.19.0"
+sed -i -e "s/version=4.18.0/version=4.19.0/g" xfce/xfconf/spkgbuild
+changelog "xfce/xfconf/spkgbuild" "Upgraded from version 4.18.0 to version 4.19.0"
