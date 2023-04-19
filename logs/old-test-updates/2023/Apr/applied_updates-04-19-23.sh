e7988c9e527dd7ee46e8026bbb96e707246bf970
Voncloft <voncloft@gmail.com>
2023-04-19 07:18:12 -0400

    Minor Updates

---
+++ logs/old-test-updates/2023/Apr/applied_updates-04-19-23.sh
@@ -0,0 +1,52 @@
+sed -i -e "s/version=9.2/version=9.3/g" core/coreutils/spkgbuild
+changelog "core/coreutils/spkgbuild" "Upgraded from version 9.2 to version 9.3"
+sed -i -e "s/version=6.4.1/version=6.4.2/g" core/mercurial/spkgbuild
+changelog "core/mercurial/spkgbuild" "Upgraded from version 6.4.1 to version 6.4.2"
+sed -i -e "s/version=113.0b1/version=113.0b2/g" nonfree/thunderbird/spkgbuild
+changelog "nonfree/thunderbird/spkgbuild" "Upgraded from version 113.0b1 to version 113.0b2"
+sed -i -e "s/version=7.0.7.0.6/version=7.0.8.156879/g" nonfree/virtualbox/spkgbuild
+changelog "nonfree/virtualbox/spkgbuild" "Upgraded from version 7.0.7.0.6 to version 7.0.8.156879"
+sed -i -e "s/version=7.0.6/version=7.0.8/g" nonfree/virtualbox-guest-modules/spkgbuild
+changelog "nonfree/virtualbox-guest-modules/spkgbuild" "Upgraded from version 7.0.6 to version 7.0.8"
+sed -i -e "s/version=7.0.6/version=7.0.8/g" nonfree/virtualbox-guest-utils/spkgbuild
+changelog "nonfree/virtualbox-guest-utils/spkgbuild" "Upgraded from version 7.0.6 to version 7.0.8"
+sed -i -e "s/version=3.11.0/version=3.12.0/g" python/python-filelock/spkgbuild
+changelog "python/python-filelock/spkgbuild" "Upgraded from version 3.11.0 to version 3.12.0"
+sed -i -e "s/version=2.85.0/version=2.86.0/g" python/python-google-api-python-client/spkgbuild
+changelog "python/python-google-api-python-client/spkgbuild" "Upgraded from version 2.85.0 to version 2.86.0"
+sed -i -e "s/version=6.4.1/version=6.5.0/g" python/python-importlib-metadata/spkgbuild
+changelog "python/python-importlib-metadata/spkgbuild" "Upgraded from version 6.4.1 to version 6.5.0"
+sed -i -e "s/version=8.0.32/version=8.0.33/g" python/python-mysql-connector/spkgbuild
+changelog "python/python-mysql-connector/spkgbuild" "Upgraded from version 8.0.32 to version 8.0.33"
+sed -i -e "s/version=12.3.1/version=13.0.0/g" python/python-oslo-db/spkgbuild
+changelog "python/python-oslo-db/spkgbuild" "Upgraded from version 12.3.1 to version 13.0.0"
+sed -i -e "s/version=1.7.0/version=1.8.1/g" python/python-parsel/spkgbuild
+changelog "python/python-parsel/spkgbuild" "Upgraded from version 1.7.0 to version 1.8.1"
+sed -i -e "s/version=3.16.0/version=3.16.1/g" python/python-peewee/spkgbuild
+changelog "python/python-peewee/spkgbuild" "Upgraded from version 3.16.0 to version 3.16.1"
+sed -i -e "s/version=2.3.0/version=2.4.0/g" python/python-periphery/spkgbuild
+changelog "python/python-periphery/spkgbuild" "Upgraded from version 2.3.0 to version 2.4.0"
+sed -i -e "s/version=8.13.9/version=8.13.10/g" python/python-phonenumbers/spkgbuild
+changelog "python/python-phonenumbers/spkgbuild" "Upgraded from version 8.13.9 to version 8.13.10"
+sed -i -e "s/version=0.6.9/version=0.6.10/g" python/python-podcastparser/spkgbuild
+changelog "python/python-podcastparser/spkgbuild" "Upgraded from version 0.6.9 to version 0.6.10"
+sed -i -e "s/version=0.4.3/version=0.4.4/g" python/python-sqlparse/spkgbuild
+changelog "python/python-sqlparse/spkgbuild" "Upgraded from version 0.4.3 to version 0.4.4"
+sed -i -e "s/version=11.0.1/version=11.0.2/g" python/python-websockets/spkgbuild
+changelog "python/python-websockets/spkgbuild" "Upgraded from version 11.0.1 to version 11.0.2"
+sed -i -e "s/version=3.12.20/version=3.12.21/g" server/blacklist-hosts/spkgbuild
+changelog "server/blacklist-hosts/spkgbuild" "Upgraded from version 3.12.20 to version 3.12.21"
+sed -i -e "s/version=2023.4.4/version=2023.4.5/g" server/homeassistant/spkgbuild
+changelog "server/homeassistant/spkgbuild" "Upgraded from version 2023.4.4 to version 2023.4.5"
+sed -i -e "s/version=1.32.0.6918/version=1.32.0.6950/g" server/plex-media-server/spkgbuild
+changelog "server/plex-media-server/spkgbuild" "Upgraded from version 1.32.0.6918 to version 1.32.0.6950"
+sed -i -e "s/version=4.18.1/version=4.19.0/g" xfce/garcon/spkgbuild
+changelog "xfce/garcon/spkgbuild" "Upgraded from version 4.18.1 to version 4.19.0"
+sed -i -e "s/version=4.18.1/version=4.19.0/g" xfce/xfce4-power-manager/spkgbuild
+changelog "xfce/xfce4-power-manager/spkgbuild" "Upgraded from version 4.18.1 to version 4.19.0"
+sed -i -e "s/version=4.18.2/version=4.19.0/g" xfce/xfce4-session/spkgbuild
+changelog "xfce/xfce4-session/spkgbuild" "Upgraded from version 4.18.2 to version 4.19.0"
+sed -i -e "s/version=4.18.2/version=4.19.0/g" xfce/xfce4-settings/spkgbuild
+changelog "xfce/xfce4-settings/spkgbuild" "Upgraded from version 4.18.2 to version 4.19.0"
+sed -i -e "s/version=1.7.1/version=1.7.2/g" xfce/xfce4-timer-plugin/spkgbuild
+changelog "xfce/xfce4-timer-plugin/spkgbuild" "Upgraded from version 1.7.1 to version 1.7.2"
