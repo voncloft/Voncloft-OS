5ff682cc471b6a38a3c3c6c29b947774b73ad383
Voncloft <voncloft@gmail.com>
2023-04-20 12:58:00 -0400

    Minor Updates

---
+++ logs/old-test-updates/2023/Apr/applied_updates-04-20-23.sh
@@ -0,0 +1,34 @@
+sed -i -e "s/version=16.0.1/version=16.0.2/g" compilers/clang/spkgbuild
+changelog "compilers/clang/spkgbuild" "Upgraded from version 16.0.1 to version 16.0.2"
+sed -i -e "s/version=16.0.1/version=16.0.2/g" compilers/lld/spkgbuild
+changelog "compilers/lld/spkgbuild" "Upgraded from version 16.0.1 to version 16.0.2"
+sed -i -e "s/version=16.0.1/version=16.0.2/g" compilers/llvm/spkgbuild
+changelog "compilers/llvm/spkgbuild" "Upgraded from version 16.0.1 to version 16.0.2"
+sed -i -e "s/version=16.0.1/version=16.0.2/g" compilers/llvm-bak/spkgbuild
+changelog "compilers/llvm-bak/spkgbuild" "Upgraded from version 16.0.1 to version 16.0.2"
+sed -i -e "s/version=16.0.1/version=16.0.2/g" multilib/llvm-32/spkgbuild
+changelog "multilib/llvm-32/spkgbuild" "Upgraded from version 16.0.1 to version 16.0.2"
+sed -i -e "s/version=9.19.11/version=9.19.12/g" networking/bind/spkgbuild
+changelog "networking/bind/spkgbuild" "Upgraded from version 9.19.11 to version 9.19.12"
+sed -i -e "s/version=1.43.5/version=1.43.6/g" networking/networkmanager/spkgbuild
+changelog "networking/networkmanager/spkgbuild" "Upgraded from version 1.43.5 to version 1.43.6"
+sed -i -e "s/version=113.0b4/version=113.0b5/g" nonfree/firefox/spkgbuild
+changelog "nonfree/firefox/spkgbuild" "Upgraded from version 113.0b4 to version 113.0b5"
+sed -i -e "s/version=4.4.0/version=4.4.1/g" python/python-engineio/spkgbuild
+changelog "python/python-engineio/spkgbuild" "Upgraded from version 4.4.0 to version 4.4.1"
+sed -i -e "s/version=5.0.0/version=6.0.0/g" python/python-hacking/spkgbuild
+changelog "python/python-hacking/spkgbuild" "Upgraded from version 5.0.0 to version 6.0.0"
+sed -i -e "s/version=2.2.4/version=2.2.5/g" python/python-requirementslib/spkgbuild
+changelog "python/python-requirementslib/spkgbuild" "Upgraded from version 2.2.4 to version 2.2.5"
+sed -i -e "s/version=3.12.1/version=3.12.2/g" ruby-gems/ruby-rspec-core/spkgbuild
+changelog "ruby-gems/ruby-rspec-core/spkgbuild" "Upgraded from version 3.12.1 to version 3.12.2"
+sed -i -e "s/version=3.12.21/version=3.12.23/g" server/blacklist-hosts/spkgbuild
+changelog "server/blacklist-hosts/spkgbuild" "Upgraded from version 3.12.21 to version 3.12.23"
+sed -i -e "s/version=4.18.1/version=4.18.2/g" server/samba/spkgbuild
+changelog "server/samba/spkgbuild" "Upgraded from version 4.18.1 to version 4.18.2"
+sed -i -e "s/version=4.18.0/version=4.19.0/g" xfce/xfce4-appfinder/spkgbuild
+changelog "xfce/xfce4-appfinder/spkgbuild" "Upgraded from version 4.18.0 to version 4.19.0"
+sed -i -e "s/version=1.1.4/version=1.1.5/g" xfce/xfce4-battery-plugin/spkgbuild
+changelog "xfce/xfce4-battery-plugin/spkgbuild" "Upgraded from version 1.1.4 to version 1.1.5"
+sed -i -e "s/version=1.1.2/version=1.1.3/g" xfce/xfce4-fsguard-plugin/spkgbuild
+changelog "xfce/xfce4-fsguard-plugin/spkgbuild" "Upgraded from version 1.1.2 to version 1.1.3"
