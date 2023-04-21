05be7bddcbc79f4cca11780998dab48ab18cc456
Voncloft <voncloft@gmail.com>
2023-04-21 18:51:05 -0400

    Minor Updates

---
+++ logs/old-test-updates/2023/Apr/applied_updates-04-21-23.sh
@@ -0,0 +1,32 @@
+sed -i -e "s/version=2.41.2/version=2.41.3/g" core/webkitgtk/spkgbuild
+changelog "core/webkitgtk/spkgbuild" "Upgraded from version 2.41.2 to version 2.41.3"
+sed -i -e "s/version=0.1.102/version=0.1.104/g" libs/babl/spkgbuild
+changelog "libs/babl/spkgbuild" "Upgraded from version 0.1.102 to version 0.1.104"
+sed -i -e "s/version=4.19.0/version=4.19.1/g" libs/libxfce4ui/spkgbuild
+changelog "libs/libxfce4ui/spkgbuild" "Upgraded from version 4.19.0 to version 4.19.1"
+sed -i -e "s/version=113.0b5/version=113.0b6/g" nonfree/firefox/spkgbuild
+changelog "nonfree/firefox/spkgbuild" "Upgraded from version 113.0b5 to version 113.0b6"
+sed -i -e "s/version=1.0.2/version=1.2.0/g" plasma/ktextaddons/spkgbuild
+changelog "plasma/ktextaddons/spkgbuild" "Upgraded from version 1.0.2 to version 1.2.0"
+sed -i -e "s/version=1.26.116/version=1.26.117/g" python/python-boto3/spkgbuild
+changelog "python/python-boto3/spkgbuild" "Upgraded from version 1.26.116 to version 1.26.117"
+sed -i -e "s/version=1.29.116/version=1.29.117/g" python/python-botocore/spkgbuild
+changelog "python/python-botocore/spkgbuild" "Upgraded from version 1.29.116 to version 1.29.117"
+sed -i -e "s/version=1.0.6/version=1.1.0/g" python/python-itemloaders/spkgbuild
+changelog "python/python-itemloaders/spkgbuild" "Upgraded from version 1.0.6 to version 1.1.0"
+sed -i -e "s/version=2.4.0/version=2.4.1/g" python/python-periphery/spkgbuild
+changelog "python/python-periphery/spkgbuild" "Upgraded from version 2.4.0 to version 2.4.1"
+sed -i -e "s/version=67.7.0/version=67.7.1/g" python/python-setuptools/spkgbuild
+changelog "python/python-setuptools/spkgbuild" "Upgraded from version 67.7.0 to version 67.7.1"
+sed -i -e "s/version=1.8.2/version=1.9.1/g" python/python-yarl/spkgbuild
+changelog "python/python-yarl/spkgbuild" "Upgraded from version 1.8.2 to version 1.9.1"
+sed -i -e "s/version=5.15.2/version=5.15.9/g" qt/qt5-declarative/spkgbuild
+changelog "qt/qt5-declarative/spkgbuild" "Upgraded from version 5.15.2 to version 5.15.9"
+sed -i -e "s/version=1.32.0.6950/version=1.32.0.6973/g" server/plex-media-server/spkgbuild
+changelog "server/plex-media-server/spkgbuild" "Upgraded from version 1.32.0.6950 to version 1.32.0.6973"
+sed -i -e "s/version=1.1.5/version=1.1.6/g" xfce/xfce4-mount-plugin/spkgbuild
+changelog "xfce/xfce4-mount-plugin/spkgbuild" "Upgraded from version 1.1.5 to version 1.1.6"
+sed -i -e "s/version=1.1.2/version=1.1.3/g" xfce/xfce4-time-out-plugin/spkgbuild
+changelog "xfce/xfce4-time-out-plugin/spkgbuild" "Upgraded from version 1.1.2 to version 1.1.3"
+sed -i -e "s/version=0.11.0/version=0.11.1/g" xfce/xfce4-weather-plugin/spkgbuild
+changelog "xfce/xfce4-weather-plugin/spkgbuild" "Upgraded from version 0.11.0 to version 0.11.1"
