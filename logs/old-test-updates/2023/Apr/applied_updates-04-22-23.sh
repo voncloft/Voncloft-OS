f7f6f3fb7f32af5fa39fe81dbca2a6a5f568cb66
Voncloft <voncloft@gmail.com>
2023-04-22 16:54:36 -0400

    Minor Updates

---
+++ logs/old-test-updates/2023/Apr/applied_updates-04-22-23.sh
@@ -0,0 +1,24 @@
+sed -i -e "s/version=1.1.20/version=1.1.21/g" libs/libmtp/spkgbuild
+changelog "libs/libmtp/spkgbuild" "Upgraded from version 1.1.20 to version 1.1.21"
+sed -i -e "s/version=1.26.117/version=1.26.118/g" python/python-boto3/spkgbuild
+changelog "python/python-boto3/spkgbuild" "Upgraded from version 1.26.117 to version 1.26.118"
+sed -i -e "s/version=1.29.117/version=1.29.118/g" python/python-botocore/spkgbuild
+changelog "python/python-botocore/spkgbuild" "Upgraded from version 1.29.117 to version 1.29.118"
+sed -i -e "s/version=3.1.0/version=4.0.0/g" python/python-fints/spkgbuild
+changelog "python/python-fints/spkgbuild" "Upgraded from version 3.1.0 to version 4.0.0"
+sed -i -e "s/version=6.5.0/version=6.6.0/g" python/python-importlib-metadata/spkgbuild
+changelog "python/python-importlib-metadata/spkgbuild" "Upgraded from version 6.5.0 to version 6.6.0"
+sed -i -e "s/version=3.16.1/version=3.16.2/g" python/python-peewee/spkgbuild
+changelog "python/python-peewee/spkgbuild" "Upgraded from version 3.16.1 to version 3.16.2"
+sed -i -e "s/version=23.1/version=23.1.1/g" python/python-pip/spkgbuild
+changelog "python/python-pip/spkgbuild" "Upgraded from version 23.1 to version 23.1.1"
+sed -i -e "s/version=0.24.0/version=0.24.1/g" python/python-pyudev/spkgbuild
+changelog "python/python-pyudev/spkgbuild" "Upgraded from version 0.24.0 to version 0.24.1"
+sed -i -e "s/version=2022.01.02/version=2023.04.21/g" python/python-sphinx-inline-tabs/spkgbuild
+changelog "python/python-sphinx-inline-tabs/spkgbuild" "Upgraded from version 2022.01.02 to version 2023.04.21"
+sed -i -e "s/version=6.3.0/version=6.3.1/g" python/python-tornado/spkgbuild
+changelog "python/python-tornado/spkgbuild" "Upgraded from version 6.3.0 to version 6.3.1"
+sed -i -e "s/version=2023.4.5/version=2023.4.6/g" server/homeassistant/spkgbuild
+changelog "server/homeassistant/spkgbuild" "Upgraded from version 2023.4.5 to version 2023.4.6"
+sed -i -e "s/version=0.8.4/version=0.8.5/g" xfce/xfce4-dict/spkgbuild
+changelog "xfce/xfce4-dict/spkgbuild" "Upgraded from version 0.8.4 to version 0.8.5"
