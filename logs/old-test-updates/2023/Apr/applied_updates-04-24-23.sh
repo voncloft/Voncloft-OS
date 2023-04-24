49ed3bf62cda6c6e62d1a9e6a3fa713b50e66a78
Voncloft <voncloft@gmail.com>
2023-04-24 05:34:57 -0400

    Minor Updates

---
+++ logs/old-test-updates/2023/Apr/applied_updates-04-24-23.sh
@@ -0,0 +1,4 @@
+sed -i -e "s/version=2022.11.21/version=2023.04.22/g" python/python-nox/spkgbuild
+changelog "python/python-nox/spkgbuild" "Upgraded from version 2022.11.21 to version 2023.04.22"
+sed -i -e "s/version=0.56.0/version=0.57.0/g" python/python-zeroconf/spkgbuild
+changelog "python/python-zeroconf/spkgbuild" "Upgraded from version 0.56.0 to version 0.57.0"
