6b4dd586a04cda5f82bcd7e92ed09acae821795b
Voncloft <voncloft@gmail.com>
2023-04-26 08:01:43 -0400

    Minor Updates

---
+++ logs/old-test-updates/2023/Apr/applied_updates-04-26-23.sh
@@ -0,0 +1,48 @@
+sed -i -e "s/version=6.2.12/version=6.3/g" core/linux-api-headers/spkgbuild
+changelog "core/linux-api-headers/spkgbuild" "Upgraded from version 6.2.12 to version 6.3"
+sed -i -e "s/version=6.2.12/version=6.3/g" core/linux-desktop/spkgbuild
+changelog "core/linux-desktop/spkgbuild" "Upgraded from version 6.2.12 to version 6.3"
+sed -i -e "s/version=6.2.12/version=6.3/g" core/linux-router/spkgbuild
+changelog "core/linux-router/spkgbuild" "Upgraded from version 6.2.12 to version 6.3"
+sed -i -e "s/version=3.6.3/version=3.6.4/g" extra/flexget/spkgbuild
+changelog "extra/flexget/spkgbuild" "Upgraded from version 3.6.3 to version 3.6.4"
+sed -i -e "s/version=0.42.0/version=0.42.2/g" libs/libfilezilla/spkgbuild
+changelog "libs/libfilezilla/spkgbuild" "Upgraded from version 0.42.0 to version 0.42.2"
+sed -i -e "s/version=2.40.0/version=2.40.1/g" networking/git/spkgbuild
+changelog "networking/git/spkgbuild" "Upgraded from version 2.40.0 to version 2.40.1"
+sed -i -e "s/version=113.0b6/version=113.0b7/g" nonfree/firefox/spkgbuild
+changelog "nonfree/firefox/spkgbuild" "Upgraded from version 113.0b6 to version 113.0b7"
+sed -i -e "s/version=113.0b3/version=113.0b4/g" nonfree/thunderbird/spkgbuild
+changelog "nonfree/thunderbird/spkgbuild" "Upgraded from version 113.0b3 to version 113.0b4"
+sed -i -e "s/version=1.2.0/version=1.2.1/g" plasma/ktextaddons/spkgbuild
+changelog "plasma/ktextaddons/spkgbuild" "Upgraded from version 1.2.0 to version 1.2.1"
+sed -i -e "s/version=2.15.3/version=3.0.0/g" python/python-astroid/spkgbuild
+changelog "python/python-astroid/spkgbuild" "Upgraded from version 2.15.3 to version 3.0.0"
+sed -i -e "s/version=1.26.118/version=1.26.120/g" python/python-boto3/spkgbuild
+changelog "python/python-boto3/spkgbuild" "Upgraded from version 1.26.118 to version 1.26.120"
+sed -i -e "s/version=1.29.118/version=1.29.120/g" python/python-botocore/spkgbuild
+changelog "python/python-botocore/spkgbuild" "Upgraded from version 1.29.118 to version 1.29.120"
+sed -i -e "s/version=18.4.0/version=18.5.1/g" python/python-faker/spkgbuild
+changelog "python/python-faker/spkgbuild" "Upgraded from version 18.4.0 to version 18.5.1"
+sed -i -e "s/version=2.2.3/version=2.3.0/g" python/python-flask/spkgbuild
+changelog "python/python-flask/spkgbuild" "Upgraded from version 2.2.3 to version 2.3.0"
+sed -i -e "s/version=3.5.1/version=3.6.0/g" python/python-hunter/spkgbuild
+changelog "python/python-hunter/spkgbuild" "Upgraded from version 3.5.1 to version 3.6.0"
+sed -i -e "s/version=6.72.1/version=6.72.4/g" python/python-hypothesis/spkgbuild
+changelog "python/python-hypothesis/spkgbuild" "Upgraded from version 6.72.1 to version 6.72.4"
+sed -i -e "s/version=2.0.3/version=2.1.0/g" python/python-pallets-sphinx-themes/spkgbuild
+changelog "python/python-pallets-sphinx-themes/spkgbuild" "Upgraded from version 2.0.3 to version 2.1.0"
+sed -i -e "s/version=13.1.0/version=13.1.1/g" python/python-pdoc/spkgbuild
+changelog "python/python-pdoc/spkgbuild" "Upgraded from version 13.1.0 to version 13.1.1"
+sed -i -e "s/version=67.7.1/version=67.7.2/g" python/python-setuptools/spkgbuild
+changelog "python/python-setuptools/spkgbuild" "Upgraded from version 67.7.1 to version 67.7.2"
+sed -i -e "s/version=4.13.0/version=4.14.0/g" python/python-tweepy/spkgbuild
+changelog "python/python-tweepy/spkgbuild" "Upgraded from version 4.13.0 to version 4.14.0"
+sed -i -e "s/version=2.2.3/version=2.3.0/g" python/python-werkzeug/spkgbuild
+changelog "python/python-werkzeug/spkgbuild" "Upgraded from version 2.2.3 to version 2.3.0"
+sed -i -e "s/version=1.9.1/version=1.9.2/g" python/python-yarl/spkgbuild
+changelog "python/python-yarl/spkgbuild" "Upgraded from version 1.9.1 to version 1.9.2"
+sed -i -e "s/version=0.57.0/version=0.58.0/g" python/python-zeroconf/spkgbuild
+changelog "python/python-zeroconf/spkgbuild" "Upgraded from version 0.57.0 to version 0.58.0"
+sed -i -e "s/version=3.12.23/version=3.12.24/g" server/blacklist-hosts/spkgbuild
+changelog "server/blacklist-hosts/spkgbuild" "Upgraded from version 3.12.23 to version 3.12.24"
