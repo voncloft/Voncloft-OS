2115fa68839f022e67e06ab1f8ea0a2bbdb4cacc
Voncloft <voncloft@gmail.com>
2023-04-27 19:01:57 -0400

    Minor Updates

---
+++ logs/old-test-updates/2023/Apr/applied_updates-04-27-23.sh
@@ -0,0 +1,38 @@
+sed -i -e "s/version=6.2.2/version=6.3/g" core/btrfs-progs/spkgbuild
+changelog "core/btrfs-progs/spkgbuild" "Upgraded from version 6.2.2 to version 6.3"
+sed -i -e "s/version=3.0.10/version=3.0.11/g" core/dkms/spkgbuild
+changelog "core/dkms/spkgbuild" "Upgraded from version 3.0.10 to version 3.0.11"
+sed -i -e "s/version=6.2.0/version=6.3.0/g" core/iproute2/spkgbuild
+changelog "core/iproute2/spkgbuild" "Upgraded from version 6.2.0 to version 6.3.0"
+sed -i -e "s/version=0.10.1/version=0.10.2/g" libs/rinutils/spkgbuild
+changelog "libs/rinutils/spkgbuild" "Upgraded from version 0.10.1 to version 0.10.2"
+sed -i -e "s/version=113.0b4/version=113.0b5/g" nonfree/thunderbird/spkgbuild
+changelog "nonfree/thunderbird/spkgbuild" "Upgraded from version 113.0b4 to version 113.0b5"
+sed -i -e "s/version=1.26.120/version=1.26.121/g" python/python-boto3/spkgbuild
+changelog "python/python-boto3/spkgbuild" "Upgraded from version 1.26.120 to version 1.26.121"
+sed -i -e "s/version=1.29.120/version=1.29.121/g" python/python-botocore/spkgbuild
+changelog "python/python-botocore/spkgbuild" "Upgraded from version 1.29.120 to version 1.29.121"
+sed -i -e "s/version=18.5.1/version=18.6.0/g" python/python-faker/spkgbuild
+changelog "python/python-faker/spkgbuild" "Upgraded from version 18.5.1 to version 18.6.0"
+sed -i -e "s/version=4.7.0/version=4.8.0/g" python/python-flask-httpauth/spkgbuild
+changelog "python/python-flask-httpauth/spkgbuild" "Upgraded from version 4.7.0 to version 4.8.0"
+sed -i -e "s/version=6.0.0/version=6.0.1/g" python/python-hacking/spkgbuild
+changelog "python/python-hacking/spkgbuild" "Upgraded from version 6.0.0 to version 6.0.1"
+sed -i -e "s/version=1.0.1/version=1.1.0/g" python/python-openstacksdk/spkgbuild
+changelog "python/python-openstacksdk/spkgbuild" "Upgraded from version 1.0.1 to version 1.1.0"
+sed -i -e "s/version=8.13.10/version=8.13.11/g" python/python-phonenumbers/spkgbuild
+changelog "python/python-phonenumbers/spkgbuild" "Upgraded from version 8.13.10 to version 8.13.11"
+sed -i -e "s/version=13.3.4/version=13.3.5/g" python/python-rich/spkgbuild
+changelog "python/python-rich/spkgbuild" "Upgraded from version 13.3.4 to version 13.3.5"
+sed -i -e "s/version=1.8.1/version=1.8.2/g" python/python-send2trash/spkgbuild
+changelog "python/python-send2trash/spkgbuild" "Upgraded from version 1.8.1 to version 1.8.2"
+sed -i -e "s/version=0.41.0/version=0.41.1/g" python/python-sqlalchemy-utils/spkgbuild
+changelog "python/python-sqlalchemy-utils/spkgbuild" "Upgraded from version 0.41.0 to version 0.41.1"
+sed -i -e "s/version=2.3.0/version=2.3.1/g" python/python-werkzeug/spkgbuild
+changelog "python/python-werkzeug/spkgbuild" "Upgraded from version 2.3.0 to version 2.3.1"
+sed -i -e "s/version=0.8.0/version=0.9.0/g" qt/qcoro/spkgbuild
+changelog "qt/qcoro/spkgbuild" "Upgraded from version 0.8.0 to version 0.9.0"
+sed -i -e "s/version=3.12.24/version=3.12.26/g" server/blacklist-hosts/spkgbuild
+changelog "server/blacklist-hosts/spkgbuild" "Upgraded from version 3.12.24 to version 3.12.26"
+sed -i -e "s/version=2023.4.6/version=2023.5.0/g" server/homeassistant/spkgbuild
+changelog "server/homeassistant/spkgbuild" "Upgraded from version 2023.4.6 to version 2023.5.0"
