f878299925c43e2232cd7f5fcad97e03c7c0276d
Voncloft <voncloft@gmail.com>
2023-04-28 20:01:56 -0400

    Minor Updates

---
+++ logs/old-test-updates/2023/Apr/applied_updates-04-28-23.sh
@@ -0,0 +1,34 @@
+sed -i -e "s/version=2.4.0/version=2.4.1/g" core/gnupg/spkgbuild
+changelog "core/gnupg/spkgbuild" "Upgraded from version 2.4.0 to version 2.4.1"
+sed -i -e "s/version=3.20.0/version=3.21.0/g" core/valgrind/spkgbuild
+changelog "core/valgrind/spkgbuild" "Upgraded from version 3.20.0 to version 3.21.0"
+sed -i -e "s/version=0.0.26/version=0.0.27/g" nonfree/discord/spkgbuild
+changelog "nonfree/discord/spkgbuild" "Upgraded from version 0.0.26 to version 0.0.27"
+sed -i -e "s/version=113.0b8/version=113.0b9/g" nonfree/firefox/spkgbuild
+changelog "nonfree/firefox/spkgbuild" "Upgraded from version 113.0b8 to version 113.0b9"
+sed -i -e "s/version=1.65.0/version=1.66.0/g" python/python-aws-sam-translator/spkgbuild
+changelog "python/python-aws-sam-translator/spkgbuild" "Upgraded from version 1.65.0 to version 1.66.0"
+sed -i -e "s/version=0.12.0/version=0.12.1/g" python/python-bitcoinlib/spkgbuild
+changelog "python/python-bitcoinlib/spkgbuild" "Upgraded from version 0.12.0 to version 0.12.1"
+sed -i -e "s/version=1.26.121/version=1.26.122/g" python/python-boto3/spkgbuild
+changelog "python/python-boto3/spkgbuild" "Upgraded from version 1.26.121 to version 1.26.122"
+sed -i -e "s/version=1.29.121/version=1.29.122/g" python/python-botocore/spkgbuild
+changelog "python/python-botocore/spkgbuild" "Upgraded from version 1.29.121 to version 1.29.122"
+sed -i -e "s/version=7.2.3/version=7.2.4/g" python/python-coverage/spkgbuild
+changelog "python/python-coverage/spkgbuild" "Upgraded from version 7.2.3 to version 7.2.4"
+sed -i -e "s/version=2023.4.0/version=2023.4.1/g" python/python-distributed/spkgbuild
+changelog "python/python-distributed/spkgbuild" "Upgraded from version 2023.4.0 to version 2023.4.1"
+sed -i -e "s/version=4.1.1/version=4.1.2/g" python/python-elementpath/spkgbuild
+changelog "python/python-elementpath/spkgbuild" "Upgraded from version 4.1.1 to version 4.1.2"
+sed -i -e "s/version=6.74.0/version=6.74.1/g" python/python-hypothesis/spkgbuild
+changelog "python/python-hypothesis/spkgbuild" "Upgraded from version 6.74.0 to version 6.74.1"
+sed -i -e "s/version=3.8.10/version=3.8.11/g" python/python-orjson/spkgbuild
+changelog "python/python-orjson/spkgbuild" "Upgraded from version 3.8.10 to version 3.8.11"
+sed -i -e "s/version=20.22.0/version=20.23.0/g" python/python-virtualenv/spkgbuild
+changelog "python/python-virtualenv/spkgbuild" "Upgraded from version 20.22.0 to version 20.23.0"
+sed -i -e "s/version=2.3.1/version=2.3.2/g" python/python-werkzeug/spkgbuild
+changelog "python/python-werkzeug/spkgbuild" "Upgraded from version 2.3.1 to version 2.3.2"
+sed -i -e "s/version=1.3.3/version=1.3.4/g" xorg/xdpyinfo/spkgbuild
+changelog "xorg/xdpyinfo/spkgbuild" "Upgraded from version 1.3.3 to version 1.3.4"
+sed -i -e "s/version=1.6.3/version=1.6.4/g" xorg/xinput/spkgbuild
+changelog "xorg/xinput/spkgbuild" "Upgraded from version 1.6.3 to version 1.6.4"
