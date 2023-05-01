4672d5223cde0a097e2fee7fda4776b12ec26a64
Voncloft <voncloft@gmail.com>
2023-05-01 10:42:51 -0400

    Minor Updates

---
+++ logs/old-test-updates/2023/May/applied_updates-05-01-23.sh
@@ -0,0 +1,32 @@
+sed -i -e "s/version=4.3/version=4.3.1/g" core/audacious/spkgbuild
+changelog "core/audacious/spkgbuild" "Upgraded from version 4.3 to version 4.3.1"
+sed -i -e "s/version=4.3/version=4.3.1/g" core/audacious-plugins/spkgbuild
+changelog "core/audacious-plugins/spkgbuild" "Upgraded from version 4.3 to version 4.3.1"
+sed -i -e "s/version=5.4.4/version=5.4.5/g" core/lua/spkgbuild
+changelog "core/lua/spkgbuild" "Upgraded from version 5.4.4 to version 5.4.5"
+sed -i -e "s/version=5.0/version=5.1/g" extra/gnucash/spkgbuild
+changelog "extra/gnucash/spkgbuild" "Upgraded from version 5.0 to version 5.1"
+sed -i -e "s/version=1.26.122/version=1.26.123/g" python/python-boto3/spkgbuild
+changelog "python/python-boto3/spkgbuild" "Upgraded from version 1.26.122 to version 1.26.123"
+sed -i -e "s/version=1.29.122/version=1.29.123/g" python/python-botocore/spkgbuild
+changelog "python/python-botocore/spkgbuild" "Upgraded from version 1.29.122 to version 1.29.123"
+sed -i -e "s/version=7.2.4/version=7.2.5/g" python/python-coverage/spkgbuild
+changelog "python/python-coverage/spkgbuild" "Upgraded from version 7.2.4 to version 7.2.5"
+sed -i -e "s/version=6.74.1/version=6.75.1/g" python/python-hypothesis/spkgbuild
+changelog "python/python-hypothesis/spkgbuild" "Upgraded from version 6.74.1 to version 6.75.1"
+sed -i -e "s/version=2023.4.20/version=2023.4.29/g" python/python-pipenv/spkgbuild
+changelog "python/python-pipenv/spkgbuild" "Upgraded from version 2023.4.20 to version 2023.4.29"
+sed -i -e "s/version=2.2.5/version=2.3.0/g" python/python-requirementslib/spkgbuild
+changelog "python/python-requirementslib/spkgbuild" "Upgraded from version 2.2.5 to version 2.3.0"
+sed -i -e "s/version=0.17.2/version=0.17.3/g" python/python-scikit-build/spkgbuild
+changelog "python/python-scikit-build/spkgbuild" "Upgraded from version 0.17.2 to version 0.17.3"
+sed -i -e "s/version=6.2/version=6.3/g" python/python-svg.path/spkgbuild
+changelog "python/python-svg.path/spkgbuild" "Upgraded from version 6.2 to version 6.3"
+sed -i -e "s/version=2.0.0/version=2.0.1/g" python/python-urllib3/spkgbuild
+changelog "python/python-urllib3/spkgbuild" "Upgraded from version 2.0.0 to version 2.0.1"
+sed -i -e "s/version=4.18.4/version=4.18.6/g" xfce/thunar/spkgbuild
+changelog "xfce/thunar/spkgbuild" "Upgraded from version 4.18.4 to version 4.18.6"
+sed -i -e "s/version=0.5.2/version=0.5.3/g" xfce/xfce4-mpc-plugin/spkgbuild
+changelog "xfce/xfce4-mpc-plugin/spkgbuild" "Upgraded from version 0.5.2 to version 0.5.3"
+sed -i -e "s/version=1.4.0/version=1.4.1/g" xfce/xfce4-netload-plugin/spkgbuild
+changelog "xfce/xfce4-netload-plugin/spkgbuild" "Upgraded from version 1.4.0 to version 1.4.1"
