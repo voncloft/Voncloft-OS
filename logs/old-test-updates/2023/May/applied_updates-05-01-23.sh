9d60962ef76acbda21b11538c961249d3c5af17e
Voncloft <voncloft@gmail.com>
2023-05-01 19:55:05 -0400

    Minor Updates

--- logs/old-test-updates/2023/May/applied_updates-05-01-23.sh
+++ logs/old-test-updates/2023/May/applied_updates-05-01-23.sh
@@ -30,3 +30,37 @@ sed -i -e "s/version=0.5.2/version=0.5.3/g" xfce/xfce4-mpc-plugin/spkgbuild
 changelog "xfce/xfce4-mpc-plugin/spkgbuild" "Upgraded from version 0.5.2 to version 0.5.3"
 sed -i -e "s/version=1.4.0/version=1.4.1/g" xfce/xfce4-netload-plugin/spkgbuild
 changelog "xfce/xfce4-netload-plugin/spkgbuild" "Upgraded from version 1.4.0 to version 1.4.1"
+sed -i -e "s/version=2.13/version=2.14/g" core/cpio/spkgbuild
+changelog "core/cpio/spkgbuild" "Upgraded from version 2.13 to version 2.14"
+sed -i -e "s/version=6.3/version=6.3.1/g" core/linux-api-headers/spkgbuild
+changelog "core/linux-api-headers/spkgbuild" "Upgraded from version 6.3 to version 6.3.1"
+sed -i -e "s/version=6.3/version=6.3.1/g" core/linux-desktop/spkgbuild
+changelog "core/linux-desktop/spkgbuild" "Upgraded from version 6.3 to version 6.3.1"
+sed -i -e "s/version=6.3/version=6.3.1/g" core/linux-router/spkgbuild
+changelog "core/linux-router/spkgbuild" "Upgraded from version 6.3 to version 6.3.1"
+sed -i -e "s/version=3.9.0/version=3.10.0/g" core/pciutils/spkgbuild
+changelog "core/pciutils/spkgbuild" "Upgraded from version 3.9.0 to version 3.10.0"
+sed -i -e "s/version=4.1.1/version=4.2.0/g" libs/libimagequant/spkgbuild
+changelog "libs/libimagequant/spkgbuild" "Upgraded from version 4.1.1 to version 4.2.0"
+sed -i -e "s/version=5.3.0/version=5.4.0/g" python/python-auditwheel/spkgbuild
+changelog "python/python-auditwheel/spkgbuild" "Upgraded from version 5.3.0 to version 5.4.0"
+sed -i -e "s/version=2.3.1/version=2.3.2/g" python/python-flask/spkgbuild
+changelog "python/python-flask/spkgbuild" "Upgraded from version 2.3.1 to version 2.3.2"
+sed -i -e "s/version=2.0.0/version=2.0.1/g" python/python-linkify-it-py/spkgbuild
+changelog "python/python-linkify-it-py/spkgbuild" "Upgraded from version 2.0.0 to version 2.0.1"
+sed -i -e "s/version=7.3.2304092211/version=7.3.2305011243/g" python/python-pycontracts/spkgbuild
+changelog "python/python-pycontracts/spkgbuild" "Upgraded from version 7.3.2304092211 to version 7.3.2305011243"
+sed -i -e "s/version=0.10.1/version=1.0.0/g" python/python-requests-toolbelt/spkgbuild
+changelog "python/python-requests-toolbelt/spkgbuild" "Upgraded from version 0.10.1 to version 1.0.0"
+sed -i -e "s/version=0.9.0/version=1.0.0/g" python/python-trustme/spkgbuild
+changelog "python/python-trustme/spkgbuild" "Upgraded from version 0.9.0 to version 1.0.0"
+sed -i -e "s/version=0.7.0/version=0.8.0/g" python/python-typer/spkgbuild
+changelog "python/python-typer/spkgbuild" "Upgraded from version 0.7.0 to version 0.8.0"
+sed -i -e "s/version=1.0.1/version=1.0.2/g" python/python-uc-micro-py/spkgbuild
+changelog "python/python-uc-micro-py/spkgbuild" "Upgraded from version 1.0.1 to version 1.0.2"
+sed -i -e "s/version=2.3.2/version=2.3.3/g" python/python-werkzeug/spkgbuild
+changelog "python/python-werkzeug/spkgbuild" "Upgraded from version 2.3.2 to version 2.3.3"
+sed -i -e "s/version=0.58.2/version=0.60.0/g" python/python-zeroconf/spkgbuild
+changelog "python/python-zeroconf/spkgbuild" "Upgraded from version 0.58.2 to version 0.60.0"
+sed -i -e "s/version=3.12.26/version=3.12.27/g" server/blacklist-hosts/spkgbuild
+changelog "server/blacklist-hosts/spkgbuild" "Upgraded from version 3.12.26 to version 3.12.27"
