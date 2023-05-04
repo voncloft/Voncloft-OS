c3d8323191f591df2371d21629a8820d697bbd8a
Voncloft <voncloft@gmail.com>
2023-05-04 09:05:04 -0400

    Minor Updates

---
+++ logs/old-test-updates/2023/May/applied_updates-05-04-23.sh
@@ -0,0 +1,38 @@
+sed -i -e "s/version=16.0.2/version=16.0.3/g" compilers/clang/spkgbuild
+changelog "compilers/clang/spkgbuild" "Upgraded from version 16.0.2 to version 16.0.3"
+sed -i -e "s/version=16.0.2/version=16.0.3/g" compilers/lld/spkgbuild
+changelog "compilers/lld/spkgbuild" "Upgraded from version 16.0.2 to version 16.0.3"
+sed -i -e "s/version=16.0.2/version=16.0.3/g" compilers/llvm/spkgbuild
+changelog "compilers/llvm/spkgbuild" "Upgraded from version 16.0.2 to version 16.0.3"
+sed -i -e "s/version=16.0.2/version=16.0.3/g" compilers/llvm-bak/spkgbuild
+changelog "compilers/llvm-bak/spkgbuild" "Upgraded from version 16.0.2 to version 16.0.3"
+sed -i -e "s/version=1.9.4/version=1.9.5/g" compilers/pkgconf/spkgbuild
+changelog "compilers/pkgconf/spkgbuild" "Upgraded from version 1.9.4 to version 1.9.5"
+sed -i -e "s/version=3.3.3/version=3.3.4/g" core/icewm/spkgbuild
+changelog "core/icewm/spkgbuild" "Upgraded from version 3.3.3 to version 3.3.4"
+sed -i -e "s/version=3.06/version=3.07/g" core/sysvinit/spkgbuild
+changelog "core/sysvinit/spkgbuild" "Upgraded from version 3.06 to version 3.07"
+sed -i -e "s/version=3.6.5/version=3.6.6/g" extra/flexget/spkgbuild
+changelog "extra/flexget/spkgbuild" "Upgraded from version 3.6.5 to version 3.6.6"
+sed -i -e "s/version=16.0.2/version=16.0.3/g" multilib/llvm-32/spkgbuild
+changelog "multilib/llvm-32/spkgbuild" "Upgraded from version 16.0.2 to version 16.0.3"
+sed -i -e "s/version=23.04.0/version=23.05.0/g" libs/poppler/spkgbuild
+changelog "libs/poppler/spkgbuild" "Upgraded from version 23.04.0 to version 23.05.0"
+sed -i -e "s/version=1.43.6/version=1.43.7/g" networking/networkmanager/spkgbuild
+changelog "networking/networkmanager/spkgbuild" "Upgraded from version 1.43.6 to version 1.43.7"
+sed -i -e "s/version=5.5.16/version=5.5.17/g" networking/whois/spkgbuild
+changelog "networking/whois/spkgbuild" "Upgraded from version 5.5.16 to version 5.5.17"
+sed -i -e "s/version=1.26.124/version=1.26.125/g" python/python-boto3/spkgbuild
+changelog "python/python-boto3/spkgbuild" "Upgraded from version 1.26.124 to version 1.26.125"
+sed -i -e "s/version=1.29.124/version=1.29.125/g" python/python-botocore/spkgbuild
+changelog "python/python-botocore/spkgbuild" "Upgraded from version 1.29.124 to version 1.29.125"
+sed -i -e "s/version=5.3.3/version=5.3.4/g" python/python-flask-socketio/spkgbuild
+changelog "python/python-flask-socketio/spkgbuild" "Upgraded from version 5.3.3 to version 5.3.4"
+sed -i -e "s/version=2.5.23/version=2.5.24/g" python/python-identify/spkgbuild
+changelog "python/python-identify/spkgbuild" "Upgraded from version 2.5.23 to version 2.5.24"
+sed -i -e "s/version=2.29.0/version=2.30.0/g" python/python-requests/spkgbuild
+changelog "python/python-requests/spkgbuild" "Upgraded from version 2.29.0 to version 2.30.0"
+sed -i -e "s/version=1.1.1/version=1.2.1/g" python/python-requests-hawk/spkgbuild
+changelog "python/python-requests-hawk/spkgbuild" "Upgraded from version 1.1.1 to version 1.2.1"
+sed -i -e "s/version=3.12.27/version=3.12.28/g" server/blacklist-hosts/spkgbuild
+changelog "server/blacklist-hosts/spkgbuild" "Upgraded from version 3.12.27 to version 3.12.28"
