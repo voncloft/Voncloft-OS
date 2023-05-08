ec954b3282b85c51b31a5255bfd0b71d5885f0dc
Voncloft <voncloft@gmail.com>
2023-05-08 08:58:48 -0400

    Minor Updates

---
+++ logs/old-test-updates/2023/May/applied_updates-05-08-23.sh
@@ -0,0 +1,18 @@
+sed -i -e "s/version=7.14.0/version=7.16.0/g" core/mlt/spkgbuild
+changelog "core/mlt/spkgbuild" "Upgraded from version 7.14.0 to version 7.16.0"
+sed -i -e "s/version=4.19.0/version=4.19.1/g" libs/libxfce4util/spkgbuild
+changelog "libs/libxfce4util/spkgbuild" "Upgraded from version 4.19.0 to version 4.19.1"
+sed -i -e "s/version=0.9.21.1/version=0.9.22/g" networking/xrdp/spkgbuild
+changelog "networking/xrdp/spkgbuild" "Upgraded from version 0.9.21.1 to version 0.9.22"
+sed -i -e "s/version=1.3.0/version=1.3.1/g" plasma/ktextaddons/spkgbuild
+changelog "plasma/ktextaddons/spkgbuild" "Upgraded from version 1.3.0 to version 1.3.1"
+sed -i -e "s/version=2023.4.0/version=2023.5.0/g" python/python-fsspec/spkgbuild
+changelog "python/python-fsspec/spkgbuild" "Upgraded from version 2023.4.0 to version 2023.5.0"
+sed -i -e "s/version=3.8.11/version=3.8.12/g" python/python-orjson/spkgbuild
+changelog "python/python-orjson/spkgbuild" "Upgraded from version 3.8.11 to version 3.8.12"
+sed -i -e "s/version=1.12.0/version=1.12.1/g" python/python-pygit2/spkgbuild
+changelog "python/python-pygit2/spkgbuild" "Upgraded from version 1.12.0 to version 1.12.1"
+sed -i -e "s/version=20.2/version=20.3/g" python/python-telegram-bot/spkgbuild
+changelog "python/python-telegram-bot/spkgbuild" "Upgraded from version 20.2 to version 20.3"
+sed -i -e "s/version=11.0.2/version=11.0.3/g" python/python-websockets/spkgbuild
+changelog "python/python-websockets/spkgbuild" "Upgraded from version 11.0.2 to version 11.0.3"
