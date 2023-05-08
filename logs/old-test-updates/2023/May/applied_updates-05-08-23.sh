89ac118078c6e515c7ab2287a3cf2796d23a32ea
Voncloft <voncloft@gmail.com>
2023-05-08 16:46:24 -0400

    fixed steam for controllers

--- logs/old-test-updates/2023/May/applied_updates-05-08-23.sh
+++ logs/old-test-updates/2023/May/applied_updates-05-08-23.sh
@@ -16,3 +16,59 @@ sed -i -e "s/version=20.2/version=20.3/g" python/python-telegram-bot/spkgbuild
 changelog "python/python-telegram-bot/spkgbuild" "Upgraded from version 20.2 to version 20.3"
 sed -i -e "s/version=11.0.2/version=11.0.3/g" python/python-websockets/spkgbuild
 changelog "python/python-websockets/spkgbuild" "Upgraded from version 11.0.2 to version 11.0.3"
+sed -i -e "s/version=5.5.0/version=5.5.1/g" core/streamlink/spkgbuild
+changelog "core/streamlink/spkgbuild" "Upgraded from version 5.5.0 to version 5.5.1"
+sed -i -e "s/version=3.6.7/version=3.6.8/g" extra/flexget/spkgbuild
+changelog "extra/flexget/spkgbuild" "Upgraded from version 3.6.7 to version 3.6.8"
+sed -i -e "s/version=22.4/version=23.0/g" extra/protobuf/spkgbuild
+changelog "extra/protobuf/spkgbuild" "Upgraded from version 22.4 to version 23.0"
+sed -i -e "s/version=1.14.1/version=1.15.1/g" libs/libwpe/spkgbuild
+changelog "libs/libwpe/spkgbuild" "Upgraded from version 1.14.1 to version 1.15.1"
+sed -i -e "s/version=1.26.129/version=1.26.130/g" python/python-boto3/spkgbuild
+changelog "python/python-boto3/spkgbuild" "Upgraded from version 1.26.129 to version 1.26.130"
+sed -i -e "s/version=1.29.129/version=1.29.130/g" python/python-botocore/spkgbuild
+changelog "python/python-botocore/spkgbuild" "Upgraded from version 1.29.129 to version 1.29.130"
+sed -i -e "s/version=6.1.0/version=6.1.1/g" python/python-docker/spkgbuild
+changelog "python/python-docker/spkgbuild" "Upgraded from version 6.1.0 to version 6.1.1"
+sed -i -e "s/version=18.6.2/version=18.7.0/g" python/python-faker/spkgbuild
+changelog "python/python-faker/spkgbuild" "Upgraded from version 18.6.2 to version 18.7.0"
+sed -i -e "s/version=6.22.0/version=6.23.0/g" python/python-ipykernel/spkgbuild
+changelog "python/python-ipykernel/spkgbuild" "Upgraded from version 6.22.0 to version 6.23.0"
+sed -i -e "s/version=2022.12/version=2023.04/g" python/python-litedram/spkgbuild
+changelog "python/python-litedram/spkgbuild" "Upgraded from version 2022.12 to version 2023.04"
+sed -i -e "s/version=2022.12/version=2023.04/g" python/python-liteeth/spkgbuild
+changelog "python/python-liteeth/spkgbuild" "Upgraded from version 2022.12 to version 2023.04"
+sed -i -e "s/version=2022.12/version=2023.04/g" python/python-liteiclink/spkgbuild
+changelog "python/python-liteiclink/spkgbuild" "Upgraded from version 2022.12 to version 2023.04"
+sed -i -e "s/version=2022.12/version=2023.04/g" python/python-litejesd204b/spkgbuild
+changelog "python/python-litejesd204b/spkgbuild" "Upgraded from version 2022.12 to version 2023.04"
+sed -i -e "s/version=2022.12/version=2023.04/g" python/python-litepcie/spkgbuild
+changelog "python/python-litepcie/spkgbuild" "Upgraded from version 2022.12 to version 2023.04"
+sed -i -e "s/version=2022.12/version=2023.04/g" python/python-litesata/spkgbuild
+changelog "python/python-litesata/spkgbuild" "Upgraded from version 2022.12 to version 2023.04"
+sed -i -e "s/version=2022.12/version=2023.04/g" python/python-litescope/spkgbuild
+changelog "python/python-litescope/spkgbuild" "Upgraded from version 2022.12 to version 2023.04"
+sed -i -e "s/version=2022.12/version=2023.04/g" python/python-litesdcard/spkgbuild
+changelog "python/python-litesdcard/spkgbuild" "Upgraded from version 2022.12 to version 2023.04"
+sed -i -e "s/version=2022.12/version=2023.04/g" python/python-liteusb/spkgbuild
+changelog "python/python-liteusb/spkgbuild" "Upgraded from version 2022.12 to version 2023.04"
+sed -i -e "s/version=2022.12/version=2023.04/g" python/python-litex/spkgbuild
+changelog "python/python-litex/spkgbuild" "Upgraded from version 2022.12 to version 2023.04"
+sed -i -e "s/version=2022.12/version=2023.04/g" python/python-litex-boards/spkgbuild
+changelog "python/python-litex-boards/spkgbuild" "Upgraded from version 2022.12 to version 2023.04"
+sed -i -e "s/version=2022.12/version=2023.04/g" python/python-pythondata-cpu-lm32/spkgbuild
+changelog "python/python-pythondata-cpu-lm32/spkgbuild" "Upgraded from version 2022.12 to version 2023.04"
+sed -i -e "s/version=2022.12/version=2023.04/g" python/python-pythondata-cpu-mor1kx/spkgbuild
+changelog "python/python-pythondata-cpu-mor1kx/spkgbuild" "Upgraded from version 2022.12 to version 2023.04"
+sed -i -e "s/version=2022.12/version=2023.04/g" python/python-pythondata-cpu-picorv32/spkgbuild
+changelog "python/python-pythondata-cpu-picorv32/spkgbuild" "Upgraded from version 2022.12 to version 2023.04"
+sed -i -e "s/version=2022.12/version=2023.04/g" python/python-pythondata-cpu-vexriscv/spkgbuild
+changelog "python/python-pythondata-cpu-vexriscv/spkgbuild" "Upgraded from version 2022.12 to version 2023.04"
+sed -i -e "s/version=2022.12/version=2023.04/g" python/python-pythondata-software-compiler_rt/spkgbuild
+changelog "python/python-pythondata-software-compiler_rt/spkgbuild" "Upgraded from version 2022.12 to version 2023.04"
+sed -i -e "s/version=3.21.5/version=3.21.6/g" python/python-trimesh/spkgbuild
+changelog "python/python-trimesh/spkgbuild" "Upgraded from version 3.21.5 to version 3.21.6"
+sed -i -e "s/version=3.12.29/version=3.12.30/g" server/blacklist-hosts/spkgbuild
+changelog "server/blacklist-hosts/spkgbuild" "Upgraded from version 3.12.29 to version 3.12.30"
+sed -i -e "s/version=1.32.0.6973/version=1.32.1.6999/g" server/plex-media-server/spkgbuild
+changelog "server/plex-media-server/spkgbuild" "Upgraded from version 1.32.0.6973 to version 1.32.1.6999"
