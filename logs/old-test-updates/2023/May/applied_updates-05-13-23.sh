ca1b62adfbf408588e792dc7bfdd883b08c44fa1
Voncloft <voncloft@gmail.com>
2023-05-13 15:08:39 -0400

    Minor Updates

---
+++ logs/old-test-updates/2023/May/applied_updates-05-13-23.sh
@@ -0,0 +1,4 @@
+sed -i -e "s/version=3.10/version=3.11/g" core/grep/spkgbuild
+changelog "core/grep/spkgbuild" "Upgraded from version 3.10 to version 3.11"
+sed -i -e "s/version=5.105.0/version=5.106.0/g" extra/extra-cmake-modules/spkgbuild
+changelog "extra/extra-cmake-modules/spkgbuild" "Upgraded from version 5.105.0 to version 5.106.0"
