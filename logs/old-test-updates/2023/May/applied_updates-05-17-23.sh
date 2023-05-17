8460062fd421ab258031f446559b727172f7991c
Voncloft <voncloft@gmail.com>
2023-05-17 19:06:49 -0400

    Minor Updates

---
+++ logs/old-test-updates/2023/May/applied_updates-05-17-23.sh
@@ -0,0 +1,50 @@
+sed -i -e "s/version=16.0.3/version=16.0.4/g" compilers/clang/spkgbuild
+changelog "compilers/clang/spkgbuild" "Upgraded from version 16.0.3 to version 16.0.4"
+sed -i -e "s/version=16.0.3/version=16.0.4/g" compilers/lld/spkgbuild
+changelog "compilers/lld/spkgbuild" "Upgraded from version 16.0.3 to version 16.0.4"
+sed -i -e "s/version=16.0.3/version=16.0.4/g" compilers/llvm/spkgbuild
+changelog "compilers/llvm/spkgbuild" "Upgraded from version 16.0.3 to version 16.0.4"
+sed -i -e "s/version=16.0.3/version=16.0.4/g" compilers/llvm-bak/spkgbuild
+changelog "compilers/llvm-bak/spkgbuild" "Upgraded from version 16.0.3 to version 16.0.4"
+sed -i -e "s/version=1.19.1/version=1.19.2/g" core/conky/spkgbuild
+changelog "core/conky/spkgbuild" "Upgraded from version 1.19.1 to version 1.19.2"
+sed -i -e "s/version=8.0.1/version=8.1.0/g" core/curl/spkgbuild
+changelog "core/curl/spkgbuild" "Upgraded from version 8.0.1 to version 8.1.0"
+sed -i -e "s/version=6.3.2/version=6.3.3/g" core/linux-api-headers/spkgbuild
+changelog "core/linux-api-headers/spkgbuild" "Upgraded from version 6.3.2 to version 6.3.3"
+sed -i -e "s/version=6.3.2/version=6.3.3/g" core/linux-desktop/spkgbuild
+changelog "core/linux-desktop/spkgbuild" "Upgraded from version 6.3.2 to version 6.3.3"
+sed -i -e "s/version=6.3.2/version=6.3.3/g" core/linux-router/spkgbuild
+changelog "core/linux-router/spkgbuild" "Upgraded from version 6.3.2 to version 6.3.3"
+sed -i -e "s/version=0.3.70/version=0.3.71/g" core/pipewire/spkgbuild
+changelog "core/pipewire/spkgbuild" "Upgraded from version 0.3.70 to version 0.3.71"
+sed -i -e "s/version=2.41.3/version=2.41.4/g" core/webkitgtk/spkgbuild
+changelog "core/webkitgtk/spkgbuild" "Upgraded from version 2.41.3 to version 2.41.4"
+sed -i -e "s/version=23.0/version=23.1/g" extra/protobuf/spkgbuild
+changelog "extra/protobuf/spkgbuild" "Upgraded from version 23.0 to version 23.1"
+sed -i -e "s/version=16.0.3/version=16.0.4/g" multilib/llvm-32/spkgbuild
+changelog "multilib/llvm-32/spkgbuild" "Upgraded from version 16.0.3 to version 16.0.4"
+sed -i -e "s/version=2.6.0/version=2.7.0/g" libs/libwacom/spkgbuild
+changelog "libs/libwacom/spkgbuild" "Upgraded from version 2.6.0 to version 2.7.0"
+sed -i -e "s/version=9.19.12/version=9.19.13/g" networking/bind/spkgbuild
+changelog "networking/bind/spkgbuild" "Upgraded from version 9.19.12 to version 9.19.13"
+sed -i -e "s/version=114.0b4/version=114.0b5/g" nonfree/firefox/spkgbuild
+changelog "nonfree/firefox/spkgbuild" "Upgraded from version 114.0b4 to version 114.0b5"
+sed -i -e "s/version=3.41.2.0/version=3.42.0.0/g" python/python-apsw/spkgbuild
+changelog "python/python-apsw/spkgbuild" "Upgraded from version 3.41.2.0 to version 3.42.0.0"
+sed -i -e "s/version=4.2/version=4.3/g" python/python-argparse-manpage/spkgbuild
+changelog "python/python-argparse-manpage/spkgbuild" "Upgraded from version 4.2 to version 4.3"
+sed -i -e "s/version=1.66.0/version=1.67.0/g" python/python-aws-sam-translator/spkgbuild
+changelog "python/python-aws-sam-translator/spkgbuild" "Upgraded from version 1.66.0 to version 1.67.0"
+sed -i -e "s/version=1.26.134/version=1.26.135/g" python/python-boto3/spkgbuild
+changelog "python/python-boto3/spkgbuild" "Upgraded from version 1.26.134 to version 1.26.135"
+sed -i -e "s/version=1.29.134/version=1.29.135/g" python/python-botocore/spkgbuild
+changelog "python/python-botocore/spkgbuild" "Upgraded from version 1.29.134 to version 1.29.135"
+sed -i -e "s/version=0.5.0/version=0.5.1/g" python/python-edalize/spkgbuild
+changelog "python/python-edalize/spkgbuild" "Upgraded from version 0.5.0 to version 0.5.1"
+sed -i -e "s/version=3.2.0/version=3.3.0/g" python/python-heatclient/spkgbuild
+changelog "python/python-heatclient/spkgbuild" "Upgraded from version 3.2.0 to version 3.3.0"
+sed -i -e "s/version=0.17.0/version=0.17.1/g" python/python-httpcore/spkgbuild
+changelog "python/python-httpcore/spkgbuild" "Upgraded from version 0.17.0 to version 0.17.1"
+sed -i -e "s/version=5.1.2/version=5.2.0/g" python/python-keystoneauth1/spkgbuild
+changelog "python/python-keystoneauth1/spkgbuild" "Upgraded from version 5.1.2 to version 5.2.0"
