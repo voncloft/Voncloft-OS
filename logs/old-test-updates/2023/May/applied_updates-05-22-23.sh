bc8a31829bac4f8581cfc5f54c2f51da11e308cc
Voncloft <voncloft@gmail.com>
2023-05-22 08:09:15 -0400

    Minor Updates

---
+++ logs/old-test-updates/2023/May/applied_updates-05-22-23.sh
@@ -0,0 +1,72 @@
+sed -i -e "s/version=4.8/version=4.8.1/g" compilers/ccache/spkgbuild
+changelog "compilers/ccache/spkgbuild" "Upgraded from version 4.8 to version 4.8.1"
+sed -i -e "s/version=3.26.3/version=3.26.4/g" compilers/cmake/spkgbuild
+changelog "compilers/cmake/spkgbuild" "Upgraded from version 3.26.3 to version 3.26.4"
+sed -i -e "s/version=1.36.0/version=1.36.1/g" core/busybox/spkgbuild
+changelog "core/busybox/spkgbuild" "Upgraded from version 1.36.0 to version 1.36.1"
+sed -i -e "s/version=3.9/version=3.10/g" core/diffutils/spkgbuild
+changelog "core/diffutils/spkgbuild" "Upgraded from version 3.9 to version 3.10"
+sed -i -e "s/version=3.2.1/version=3.3.0/g" core/double-conversion/spkgbuild
+changelog "core/double-conversion/spkgbuild" "Upgraded from version 3.2.1 to version 3.3.0"
+sed -i -e "s/version=3.3.4/version=3.3.5/g" core/icewm/spkgbuild
+changelog "core/icewm/spkgbuild" "Upgraded from version 3.3.4 to version 3.3.5"
+sed -i -e "s/version=1.22.2/version=1.22.3/g" media/gst-libav/spkgbuild
+changelog "media/gst-libav/spkgbuild" "Upgraded from version 1.22.2 to version 1.22.3"
+sed -i -e "s/version=1.22.2/version=1.22.3/g" media/gst-plugins-bad/spkgbuild
+changelog "media/gst-plugins-bad/spkgbuild" "Upgraded from version 1.22.2 to version 1.22.3"
+sed -i -e "s/version=1.22.2/version=1.22.3/g" media/gst-plugins-base/spkgbuild
+changelog "media/gst-plugins-base/spkgbuild" "Upgraded from version 1.22.2 to version 1.22.3"
+sed -i -e "s/version=1.22.2/version=1.22.3/g" media/gst-plugins-good/spkgbuild
+changelog "media/gst-plugins-good/spkgbuild" "Upgraded from version 1.22.2 to version 1.22.3"
+sed -i -e "s/version=1.22.2/version=1.22.3/g" media/gst-plugins-ugly/spkgbuild
+changelog "media/gst-plugins-ugly/spkgbuild" "Upgraded from version 1.22.2 to version 1.22.3"
+sed -i -e "s/version=1.22.2/version=1.22.3/g" media/gstreamer/spkgbuild
+changelog "media/gstreamer/spkgbuild" "Upgraded from version 1.22.2 to version 1.22.3"
+sed -i -e "s/version=7.93/version=7.94/g" networking/nmap/spkgbuild
+changelog "networking/nmap/spkgbuild" "Upgraded from version 7.93 to version 7.94"
+sed -i -e "s/version=114.0b5/version=114.0b6/g" nonfree/firefox/spkgbuild
+changelog "nonfree/firefox/spkgbuild" "Upgraded from version 114.0b5 to version 114.0b6"
+sed -i -e "s/version=114.0b2/version=114.0b3/g" nonfree/thunderbird/spkgbuild
+changelog "nonfree/thunderbird/spkgbuild" "Upgraded from version 114.0b2 to version 114.0b3"
+sed -i -e "s/version=1.3.1/version=1.3.2/g" plasma/ktextaddons/spkgbuild
+changelog "plasma/ktextaddons/spkgbuild" "Upgraded from version 1.3.1 to version 1.3.2"
+sed -i -e "s/version=3.6.2/version=3.7.0/g" python/python-anyio/spkgbuild
+changelog "python/python-anyio/spkgbuild" "Upgraded from version 3.6.2 to version 3.7.0"
+sed -i -e "s/version=22.9.0/version=23.5.0/g" python/python-awesomeversion/spkgbuild
+changelog "python/python-awesomeversion/spkgbuild" "Upgraded from version 22.9.0 to version 23.5.0"
+sed -i -e "s/version=1.26.135/version=1.26.137/g" python/python-boto3/spkgbuild
+changelog "python/python-boto3/spkgbuild" "Upgraded from version 1.26.135 to version 1.26.137"
+sed -i -e "s/version=1.29.135/version=1.29.137/g" python/python-botocore/spkgbuild
+changelog "python/python-botocore/spkgbuild" "Upgraded from version 1.29.135 to version 1.29.137"
+sed -i -e "s/version=2.11.1/version=2.11.6/g" python/python-cloudflare/spkgbuild
+changelog "python/python-cloudflare/spkgbuild" "Upgraded from version 2.11.1 to version 2.11.6"
+sed -i -e "s/version=0.9.7/version=0.9.7.1/g" python/python-flasgger/spkgbuild
+changelog "python/python-flasgger/spkgbuild" "Upgraded from version 0.9.7 to version 0.9.7.1"
+sed -i -e "s/version=2.18.0/version=2.18.1/g" python/python-google-auth/spkgbuild
+changelog "python/python-google-auth/spkgbuild" "Upgraded from version 2.18.0 to version 2.18.1"
+sed -i -e "s/version=0.29.1/version=0.29.2/g" python/python-owslib/spkgbuild
+changelog "python/python-owslib/spkgbuild" "Upgraded from version 0.29.1 to version 0.29.2"
+sed -i -e "s/version=2023.4.29/version=2023.5.19/g" python/python-pipenv/spkgbuild
+changelog "python/python-pipenv/spkgbuild" "Upgraded from version 2023.4.29 to version 2023.5.19"
+sed -i -e "s/version=1.4.2/version=1.5.0/g" python/python-poetry/spkgbuild
+changelog "python/python-poetry/spkgbuild" "Upgraded from version 1.4.2 to version 1.5.0"
+sed -i -e "s/version=3.3.1/version=3.3.2/g" python/python-pre-commit/spkgbuild
+changelog "python/python-pre-commit/spkgbuild" "Upgraded from version 3.3.1 to version 3.3.2"
+sed -i -e "s/version=4.10.0/version=4.11.0/g" python/python-pwntools/spkgbuild
+changelog "python/python-pwntools/spkgbuild" "Upgraded from version 4.10.0 to version 4.11.0"
+sed -i -e "s/version=3.17.0/version=3.18.0/g" python/python-pycryptodome/spkgbuild
+changelog "python/python-pycryptodome/spkgbuild" "Upgraded from version 3.17.0 to version 3.18.0"
+sed -i -e "s/version=3.3.0/version=3.3.1/g" python/python-pytest-xdist/spkgbuild
+changelog "python/python-pytest-xdist/spkgbuild" "Upgraded from version 3.3.0 to version 3.3.1"
+sed -i -e "s/version=67.7.2/version=67.8.0/g" python/python-setuptools/spkgbuild
+changelog "python/python-setuptools/spkgbuild" "Upgraded from version 67.7.2 to version 67.8.0"
+sed -i -e "s/version=2023.03.27/version=2023.05.20/g" python/python-sphinx-furo/spkgbuild
+changelog "python/python-sphinx-furo/spkgbuild" "Upgraded from version 2023.03.27 to version 2023.05.20"
+sed -i -e "s/version=5.0.1/version=5.0.2/g" python/python-sybil/spkgbuild
+changelog "python/python-sybil/spkgbuild" "Upgraded from version 5.0.1 to version 5.0.2"
+sed -i -e "s/version=3.21.6/version=3.21.7/g" python/python-trimesh/spkgbuild
+changelog "python/python-trimesh/spkgbuild" "Upgraded from version 3.21.6 to version 3.21.7"
+sed -i -e "s/version=2.2.3/version=2.3.0/g" python/python-xmlschema/spkgbuild
+changelog "python/python-xmlschema/spkgbuild" "Upgraded from version 2.2.3 to version 2.3.0"
+sed -i -e "s/version=3.12.33/version=3.12.35/g" server/blacklist-hosts/spkgbuild
+changelog "server/blacklist-hosts/spkgbuild" "Upgraded from version 3.12.33 to version 3.12.35"
