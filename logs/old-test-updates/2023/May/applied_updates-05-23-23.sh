c3068e6d8484c855845bfa42ec9e4faa5d7dcc3b
Voncloft <voncloft@gmail.com>
2023-05-23 08:26:58 -0400

    Minor Updates

---
+++ logs/old-test-updates/2023/May/applied_updates-05-23-23.sh
@@ -0,0 +1,32 @@
+sed -i -e "s/version=0.16.0/version=0.19.0/g" core/alacritty/spkgbuild
+changelog "core/alacritty/spkgbuild" "Upgraded from version 0.16.0 to version 0.19.0"
+sed -i -e "s/version=2.3.4/version=2.4.0/g" core/enchant/spkgbuild
+changelog "core/enchant/spkgbuild" "Upgraded from version 2.3.4 to version 2.4.0"
+sed -i -e "s/version=1.19.0/version=1.19.1/g" libs/c-ares/spkgbuild
+changelog "libs/c-ares/spkgbuild" "Upgraded from version 1.19.0 to version 1.19.1"
+sed -i -e "s/version=2.0.8/version=2.0.9/g" libs/libtorrent/spkgbuild
+changelog "libs/libtorrent/spkgbuild" "Upgraded from version 2.0.8 to version 2.0.9"
+sed -i -e "s/version=2.0.8/version=2.0.9/g" libs/libtorrent-rasterbar/spkgbuild
+changelog "libs/libtorrent-rasterbar/spkgbuild" "Upgraded from version 2.0.8 to version 2.0.9"
+sed -i -e "s/version=114.0b6/version=114.0b7/g" nonfree/firefox/spkgbuild
+changelog "nonfree/firefox/spkgbuild" "Upgraded from version 114.0b6 to version 114.0b7"
+sed -i -e "s/version=1.26.137/version=1.26.138/g" python/python-boto3/spkgbuild
+changelog "python/python-boto3/spkgbuild" "Upgraded from version 1.26.137 to version 1.26.138"
+sed -i -e "s/version=1.29.137/version=1.29.138/g" python/python-botocore/spkgbuild
+changelog "python/python-botocore/spkgbuild" "Upgraded from version 1.29.137 to version 1.29.138"
+sed -i -e "s/version=4.19.0/version=4.20.0/g" python/python-braintree/spkgbuild
+changelog "python/python-braintree/spkgbuild" "Upgraded from version 4.19.0 to version 4.20.0"
+sed -i -e "s/version=2.7.0/version=2.8.0/g" python/python-dominate/spkgbuild
+changelog "python/python-dominate/spkgbuild" "Upgraded from version 2.7.0 to version 2.8.0"
+sed -i -e "s/version=1.1.0/version=1.2.0/g" python/python-openstacksdk/spkgbuild
+changelog "python/python-openstacksdk/spkgbuild" "Upgraded from version 1.1.0 to version 1.2.0"
+sed -i -e "s/version=2.30.0/version=2.31.0/g" python/python-requests/spkgbuild
+changelog "python/python-requests/spkgbuild" "Upgraded from version 2.30.0 to version 2.31.0"
+sed -i -e "s/version=7.4.3/version=8.0.0/g" python/python-traitsui/spkgbuild
+changelog "python/python-traitsui/spkgbuild" "Upgraded from version 7.4.3 to version 8.0.0"
+sed -i -e "s/version=3.1.0/version=3.1.1/g" python/python-xlsxwriter/spkgbuild
+changelog "python/python-xlsxwriter/spkgbuild" "Upgraded from version 3.1.0 to version 3.1.1"
+sed -i -e "s/version=1.32.1.6999/version=1.32.2.7100/g" server/plex-media-server/spkgbuild
+changelog "server/plex-media-server/spkgbuild" "Upgraded from version 1.32.1.6999 to version 1.32.2.7100"
+sed -i -e "s/version=6.2.0/version=6.3.0/g" xfce/xfsprogs/spkgbuild
+changelog "xfce/xfsprogs/spkgbuild" "Upgraded from version 6.2.0 to version 6.3.0"
