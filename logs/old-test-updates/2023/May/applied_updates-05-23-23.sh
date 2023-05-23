89c26df1a5803d04776cfdfe030ef7c79f157916
Voncloft <voncloft@gmail.com>
2023-05-23 17:09:42 -0400

    Minor Updates

--- logs/old-test-updates/2023/May/applied_updates-05-23-23.sh
+++ logs/old-test-updates/2023/May/applied_updates-05-23-23.sh
@@ -30,3 +30,25 @@ sed -i -e "s/version=1.32.1.6999/version=1.32.2.7100/g" server/plex-media-server
 changelog "server/plex-media-server/spkgbuild" "Upgraded from version 1.32.1.6999 to version 1.32.2.7100"
 sed -i -e "s/version=6.2.0/version=6.3.0/g" xfce/xfsprogs/spkgbuild
 changelog "xfce/xfsprogs/spkgbuild" "Upgraded from version 6.2.0 to version 6.3.0"
+sed -i -e "s/version=8.1.0/version=8.1.1/g" core/curl/spkgbuild
+changelog "core/curl/spkgbuild" "Upgraded from version 8.1.0 to version 8.1.1"
+sed -i -e "s/version=0.111.3/version=0.112.1/g" core/hugo/spkgbuild
+changelog "core/hugo/spkgbuild" "Upgraded from version 0.111.3 to version 0.112.1"
+sed -i -e "s/version=0.13.1/version=0.14.0/g" libs/hidapi/spkgbuild
+changelog "libs/hidapi/spkgbuild" "Upgraded from version 0.13.1 to version 0.14.0"
+sed -i -e "s/version=0.9.22/version=0.9.22.1/g" networking/xrdp/spkgbuild
+changelog "networking/xrdp/spkgbuild" "Upgraded from version 0.9.22 to version 0.9.22.1"
+sed -i -e "s/version=114.0b3/version=114.0b4/g" nonfree/thunderbird/spkgbuild
+changelog "nonfree/thunderbird/spkgbuild" "Upgraded from version 114.0b3 to version 114.0b4"
+sed -i -e "s/version=6.14.9/version=6.15.0/g" python/python-cx-freeze/spkgbuild
+changelog "python/python-cx-freeze/spkgbuild" "Upgraded from version 6.14.9 to version 6.15.0"
+sed -i -e "s/version=0.17.1/version=0.17.2/g" python/python-httpcore/spkgbuild
+changelog "python/python-httpcore/spkgbuild" "Upgraded from version 0.17.1 to version 0.17.2"
+sed -i -e "s/version=0.4.9/version=0.4.10/g" python/python-libcst/spkgbuild
+changelog "python/python-libcst/spkgbuild" "Upgraded from version 0.4.9 to version 0.4.10"
+sed -i -e "s/version=2.0.0/version=2.0.1/g" python/python-pytest-relaxed/spkgbuild
+changelog "python/python-pytest-relaxed/spkgbuild" "Upgraded from version 2.0.0 to version 2.0.1"
+sed -i -e "s/version=1.2.0/version=1.2.1/g" python/python-sphinx_rtd_theme/spkgbuild
+changelog "python/python-sphinx_rtd_theme/spkgbuild" "Upgraded from version 1.2.0 to version 1.2.1"
+sed -i -e "s/version=1.24.0/version=1.25.0/g" server/nginx/spkgbuild
+changelog "server/nginx/spkgbuild" "Upgraded from version 1.24.0 to version 1.25.0"
