b009aa6162c1d663a0062417aa670349c9411970
Voncloft <voncloft@gmail.com>
2023-05-25 05:24:54 -0400

    Minor Updates

---
+++ logs/old-test-updates/2023/May/applied_updates-05-25-23.sh
@@ -0,0 +1,28 @@
+sed -i -e "s/version=2.4.0/version=2.5.0/g" core/enchant/spkgbuild
+changelog "core/enchant/spkgbuild" "Upgraded from version 2.4.0 to version 2.5.0"
+sed -i -e "s/version=0.112.1/version=0.112.3/g" core/hugo/spkgbuild
+changelog "core/hugo/spkgbuild" "Upgraded from version 0.112.1 to version 0.112.3"
+sed -i -e "s/version=6.3.3/version=6.3.4/g" core/linux-api-headers/spkgbuild
+changelog "core/linux-api-headers/spkgbuild" "Upgraded from version 6.3.3 to version 6.3.4"
+sed -i -e "s/version=6.3.3/version=6.3.4/g" core/linux-desktop/spkgbuild
+changelog "core/linux-desktop/spkgbuild" "Upgraded from version 6.3.3 to version 6.3.4"
+sed -i -e "s/version=6.3.3/version=6.3.4/g" core/linux-router/spkgbuild
+changelog "core/linux-router/spkgbuild" "Upgraded from version 6.3.3 to version 6.3.4"
+sed -i -e "s/version=114.0b7/version=114.0b8/g" nonfree/firefox/spkgbuild
+changelog "nonfree/firefox/spkgbuild" "Upgraded from version 114.0b7 to version 114.0b8"
+sed -i -e "s/version=1.26.138/version=1.26.139/g" python/python-boto3/spkgbuild
+changelog "python/python-boto3/spkgbuild" "Upgraded from version 1.26.138 to version 1.26.139"
+sed -i -e "s/version=1.29.138/version=1.29.139/g" python/python-botocore/spkgbuild
+changelog "python/python-botocore/spkgbuild" "Upgraded from version 1.29.138 to version 1.29.139"
+sed -i -e "s/version=7.2.5/version=7.2.6/g" python/python-coverage/spkgbuild
+changelog "python/python-coverage/spkgbuild" "Upgraded from version 7.2.5 to version 7.2.6"
+sed -i -e "s/version=2.86.0/version=2.87.0/g" python/python-google-api-python-client/spkgbuild
+changelog "python/python-google-api-python-client/spkgbuild" "Upgraded from version 2.86.0 to version 2.87.0"
+sed -i -e "s/version=3.8.12/version=3.8.13/g" python/python-orjson/spkgbuild
+changelog "python/python-orjson/spkgbuild" "Upgraded from version 3.8.12 to version 3.8.13"
+sed -i -e "s/version=4.0.0/version=4.1.0/g" python/python-pytest-cov/spkgbuild
+changelog "python/python-pytest-cov/spkgbuild" "Upgraded from version 4.0.0 to version 4.1.0"
+sed -i -e "s/version=4.2.1/version=4.3.0/g" python/python-vcrpy/spkgbuild
+changelog "python/python-vcrpy/spkgbuild" "Upgraded from version 4.2.1 to version 4.3.0"
+sed -i -e "s/version=2023.5.3/version=2023.5.4/g" server/homeassistant/spkgbuild
+changelog "server/homeassistant/spkgbuild" "Upgraded from version 2023.5.3 to version 2023.5.4"
