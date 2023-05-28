3d9de44450027a2b03dc3ebdc1cbdb786b590a66
Voncloft <voncloft@gmail.com>
2023-05-28 11:33:13 -0400

    Minor Updates

---
+++ logs/old-test-updates/2023/May/applied_updates-05-28-23.sh
@@ -0,0 +1,60 @@
+sed -i -e "s/version=13.1/version=13.2/g" compilers/gdb/spkgbuild
+changelog "compilers/gdb/spkgbuild" "Upgraded from version 13.1 to version 13.2"
+sed -i -e "s/version=1.1.0/version=1.1.1/g" compilers/meson/spkgbuild
+changelog "compilers/meson/spkgbuild" "Upgraded from version 1.1.0 to version 1.1.1"
+sed -i -e "s/version=8.2.2/version=8.2.4/g" core/gc/spkgbuild
+changelog "core/gc/spkgbuild" "Upgraded from version 8.2.2 to version 8.2.4"
+sed -i -e "s/version=3.7.0/version=3.7.2/g" extra/flexget/spkgbuild
+changelog "extra/flexget/spkgbuild" "Upgraded from version 3.7.0 to version 3.7.2"
+sed -i -e "s/version=23.1/version=23.2/g" extra/protobuf/spkgbuild
+changelog "extra/protobuf/spkgbuild" "Upgraded from version 23.1 to version 23.2"
+sed -i -e "s/version=0.42.2/version=0.43.0/g" libs/libfilezilla/spkgbuild
+changelog "libs/libfilezilla/spkgbuild" "Upgraded from version 0.42.2 to version 0.43.0"
+sed -i -e "s/version=3.7.2/version=3.8.0/g" libs/libressl/spkgbuild
+changelog "libs/libressl/spkgbuild" "Upgraded from version 3.7.2 to version 3.8.0"
+sed -i -e "s/version=4.0.5/version=4.0.6/g" networking/wireshark/spkgbuild
+changelog "networking/wireshark/spkgbuild" "Upgraded from version 4.0.5 to version 4.0.6"
+sed -i -e "s/version=114.0b8/version=114.0b9/g" nonfree/firefox/spkgbuild
+changelog "nonfree/firefox/spkgbuild" "Upgraded from version 114.0b8 to version 114.0b9"
+sed -i -e "s/version=114.0b4/version=114.0b5/g" nonfree/thunderbird/spkgbuild
+changelog "nonfree/thunderbird/spkgbuild" "Upgraded from version 114.0b4 to version 114.0b5"
+sed -i -e "s/version=1.67.0/version=1.68.0/g" python/python-aws-sam-translator/spkgbuild
+changelog "python/python-aws-sam-translator/spkgbuild" "Upgraded from version 1.67.0 to version 1.68.0"
+sed -i -e "s/version=1.26.139/version=1.26.142/g" python/python-boto3/spkgbuild
+changelog "python/python-boto3/spkgbuild" "Upgraded from version 1.26.139 to version 1.26.142"
+sed -i -e "s/version=1.29.139/version=1.29.142/g" python/python-botocore/spkgbuild
+changelog "python/python-botocore/spkgbuild" "Upgraded from version 1.29.139 to version 1.29.142"
+sed -i -e "s/version=1.2.13/version=1.2.14/g" python/python-deprecated/spkgbuild
+changelog "python/python-deprecated/spkgbuild" "Upgraded from version 1.2.13 to version 1.2.14"
+sed -i -e "s/version=2023.5.0/version=2023.5.1/g" python/python-distributed/spkgbuild
+changelog "python/python-distributed/spkgbuild" "Upgraded from version 2023.5.0 to version 2023.5.1"
+sed -i -e "s/version=8.7.1/version=8.8.0/g" python/python-elasticsearch/spkgbuild
+changelog "python/python-elasticsearch/spkgbuild" "Upgraded from version 8.7.1 to version 8.8.0"
+sed -i -e "s/version=2.18.1/version=2.19.0/g" python/python-google-auth/spkgbuild
+changelog "python/python-google-auth/spkgbuild" "Upgraded from version 2.18.1 to version 2.19.0"
+sed -i -e "s/version=6.75.3/version=6.75.6/g" python/python-hypothesis/spkgbuild
+changelog "python/python-hypothesis/spkgbuild" "Upgraded from version 6.75.3 to version 6.75.6"
+sed -i -e "s/version=0.4.10/version=1.0.0/g" python/python-libcst/spkgbuild
+changelog "python/python-libcst/spkgbuild" "Upgraded from version 0.4.10 to version 1.0.0"
+sed -i -e "s/version=0.21.1/version=0.22.0/g" python/python-libtmux/spkgbuild
+changelog "python/python-libtmux/spkgbuild" "Upgraded from version 0.21.1 to version 0.22.0"
+sed -i -e "s/version=1.1.8/version=1.2.0/g" python/python-lru-dict/spkgbuild
+changelog "python/python-lru-dict/spkgbuild" "Upgraded from version 1.1.8 to version 1.2.0"
+sed -i -e "s/version=1.25.0/version=2.0.0/g" python/python-numpy/spkgbuild
+changelog "python/python-numpy/spkgbuild" "Upgraded from version 1.25.0 to version 2.0.0"
+sed -i -e "s/version=3.8.13/version=3.8.14/g" python/python-orjson/spkgbuild
+changelog "python/python-orjson/spkgbuild" "Upgraded from version 3.8.13 to version 3.8.14"
+sed -i -e "s/version=3.1.0/version=3.2.0/g" python/python-paramiko/spkgbuild
+changelog "python/python-paramiko/spkgbuild" "Upgraded from version 3.1.0 to version 3.2.0"
+sed -i -e "s/version=0.16.0/version=0.17.0/g" python/python-prometheus_client/spkgbuild
+changelog "python/python-prometheus_client/spkgbuild" "Upgraded from version 0.16.0 to version 0.17.0"
+sed -i -e "s/version=0.8.0/version=0.9.0/g" python/python-typing_inspect/spkgbuild
+changelog "python/python-typing_inspect/spkgbuild" "Upgraded from version 0.8.0 to version 0.9.0"
+sed -i -e "s/version=4.3.0/version=4.3.1/g" python/python-vcrpy/spkgbuild
+changelog "python/python-vcrpy/spkgbuild" "Upgraded from version 4.3.0 to version 4.3.1"
+sed -i -e "s/version=0.62.0/version=0.63.0/g" python/python-zeroconf/spkgbuild
+changelog "python/python-zeroconf/spkgbuild" "Upgraded from version 0.62.0 to version 0.63.0"
+sed -i -e "s/version=4.3/version=5.0/g" python/python-zope-location/spkgbuild
+changelog "python/python-zope-location/spkgbuild" "Upgraded from version 4.3 to version 5.0"
+sed -i -e "s/version=3.12.35/version=3.12.36/g" server/blacklist-hosts/spkgbuild
+changelog "server/blacklist-hosts/spkgbuild" "Upgraded from version 3.12.35 to version 3.12.36"
