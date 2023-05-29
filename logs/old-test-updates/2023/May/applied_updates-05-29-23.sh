041b55a203cde2278c53f9ad5a4e9d4c5727c8cd
Voncloft <voncloft@gmail.com>
2023-05-29 14:06:41 -0400

    Minor Updates

---
+++ logs/old-test-updates/2023/May/applied_updates-05-29-23.sh
@@ -0,0 +1,12 @@
+sed -i -e "s/version=0.112.3/version=0.112.4/g" core/hugo/spkgbuild
+changelog "core/hugo/spkgbuild" "Upgraded from version 0.112.3 to version 0.112.4"
+sed -i -e "s/version=0.9.76/version=0.9.77/g" libs/libmicrohttpd/spkgbuild
+changelog "libs/libmicrohttpd/spkgbuild" "Upgraded from version 0.9.76 to version 0.9.77"
+sed -i -e "s/version=5.3.0/version=5.3.1/g" python/python-cachetools/spkgbuild
+changelog "python/python-cachetools/spkgbuild" "Upgraded from version 5.3.0 to version 5.3.1"
+sed -i -e "s/version=0.22.0/version=0.22.1/g" python/python-libtmux/spkgbuild
+changelog "python/python-libtmux/spkgbuild" "Upgraded from version 0.22.0 to version 0.22.1"
+sed -i -e "s/version=0.8.1/version=0.9.2/g" python/python-sphinxcontrib-mermaid/spkgbuild
+changelog "python/python-sphinxcontrib-mermaid/spkgbuild" "Upgraded from version 0.8.1 to version 0.9.2"
+sed -i -e "s/version=3.1.1/version=3.1.2/g" python/python-xlsxwriter/spkgbuild
+changelog "python/python-xlsxwriter/spkgbuild" "Upgraded from version 3.1.1 to version 3.1.2"
