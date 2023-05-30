27e508c76d4e827f74caa410368504eef0137393
Voncloft <voncloft@gmail.com>
2023-05-30 06:49:45 -0400

    Minor Updates

---
+++ logs/old-test-updates/2023/May/applied_updates-05-30-23.sh
@@ -0,0 +1,16 @@
+sed -i -e "s/version=6.3/version=6.3.1/g" core/btrfs-progs/spkgbuild
+changelog "core/btrfs-progs/spkgbuild" "Upgraded from version 6.3 to version 6.3.1"
+sed -i -e "s/version=0.24.3/version=0.24.5/g" core/cbindgen/spkgbuild
+changelog "core/cbindgen/spkgbuild" "Upgraded from version 0.24.3 to version 0.24.5"
+sed -i -e "s/version=0.112.4/version=0.112.5/g" core/hugo/spkgbuild
+changelog "core/hugo/spkgbuild" "Upgraded from version 0.112.4 to version 0.112.5"
+sed -i -e "s/version=4.19.1/version=4.19.2/g" libs/libxfce4ui/spkgbuild
+changelog "libs/libxfce4ui/spkgbuild" "Upgraded from version 4.19.1 to version 4.19.2"
+sed -i -e "s/version=1.5.0/version=1.5.1/g" python/python-poetry/spkgbuild
+changelog "python/python-poetry/spkgbuild" "Upgraded from version 1.5.0 to version 1.5.1"
+sed -i -e "s/version=1.6.0/version=1.6.1/g" python/python-poetry-core/spkgbuild
+changelog "python/python-poetry-core/spkgbuild" "Upgraded from version 1.6.0 to version 1.6.1"
+sed -i -e "s/version=4.5.2/version=4.5.3/g" qt/qbittorrent/spkgbuild
+changelog "qt/qbittorrent/spkgbuild" "Upgraded from version 4.5.2 to version 4.5.3"
+sed -i -e "s/version=4.19.0/version=4.19.1/g" xfce/xfce4-session/spkgbuild
+changelog "xfce/xfce4-session/spkgbuild" "Upgraded from version 4.19.0 to version 4.19.1"
