c697134d173fc0cb0d3ef18da3f99700dbdc91ef
Voncloft <voncloft@gmail.com>
2023-05-30 16:47:03 -0400

    adding cec to kernel for desktop kernel

--- logs/old-test-updates/2023/May/applied_updates-05-30-23.sh
+++ logs/old-test-updates/2023/May/applied_updates-05-30-23.sh
@@ -14,3 +14,35 @@ sed -i -e "s/version=4.5.2/version=4.5.3/g" qt/qbittorrent/spkgbuild
 changelog "qt/qbittorrent/spkgbuild" "Upgraded from version 4.5.2 to version 4.5.3"
 sed -i -e "s/version=4.19.0/version=4.19.1/g" xfce/xfce4-session/spkgbuild
 changelog "xfce/xfce4-session/spkgbuild" "Upgraded from version 4.19.0 to version 4.19.1"
+sed -i -e "s/version=8.1.1/version=8.1.2/g" core/curl/spkgbuild
+changelog "core/curl/spkgbuild" "Upgraded from version 8.1.1 to version 8.1.2"
+sed -i -e "s/version=2.4.1/version=2.4.2/g" core/gnupg/spkgbuild
+changelog "core/gnupg/spkgbuild" "Upgraded from version 2.4.1 to version 2.4.2"
+sed -i -e "s/version=6.3.4/version=6.3.5/g" core/linux-api-headers/spkgbuild
+changelog "core/linux-api-headers/spkgbuild" "Upgraded from version 6.3.4 to version 6.3.5"
+sed -i -e "s/version=6.3.4/version=6.3.5/g" core/linux-desktop/spkgbuild
+changelog "core/linux-desktop/spkgbuild" "Upgraded from version 6.3.4 to version 6.3.5"
+sed -i -e "s/version=6.3.4/version=6.3.5/g" core/linux-router/spkgbuild
+changelog "core/linux-router/spkgbuild" "Upgraded from version 6.3.4 to version 6.3.5"
+sed -i -e "s/version=1.10.0/version=1.11.0/g" networking/libssh2/spkgbuild
+changelog "networking/libssh2/spkgbuild" "Upgraded from version 1.10.0 to version 1.11.0"
+sed -i -e "s/version=3.1.0/version=3.1.1/g" networking/openssl/spkgbuild
+changelog "networking/openssl/spkgbuild" "Upgraded from version 3.1.0 to version 3.1.1"
+sed -i -e "s/version=530.41.03/version=535.43.02/g" nonfree/nvidia/spkgbuild
+changelog "nonfree/nvidia/spkgbuild" "Upgraded from version 530.41.03 to version 535.43.02"
+sed -i -e "s/version=530.41.03/version=535.43.02/g" nonfree/nvidia-32/spkgbuild
+changelog "nonfree/nvidia-32/spkgbuild" "Upgraded from version 530.41.03 to version 535.43.02"
+sed -i -e "s/version=530.41.03/version=535.43.02/g" nonfree/nvidia-modules/spkgbuild
+changelog "nonfree/nvidia-modules/spkgbuild" "Upgraded from version 530.41.03 to version 535.43.02"
+sed -i -e "s/version=1.26.142/version=1.26.143/g" python/python-boto3/spkgbuild
+changelog "python/python-boto3/spkgbuild" "Upgraded from version 1.26.142 to version 1.26.143"
+sed -i -e "s/version=1.29.142/version=1.29.143/g" python/python-botocore/spkgbuild
+changelog "python/python-botocore/spkgbuild" "Upgraded from version 1.29.142 to version 1.29.143"
+sed -i -e "s/version=7.2.6/version=7.2.7/g" python/python-coverage/spkgbuild
+changelog "python/python-coverage/spkgbuild" "Upgraded from version 7.2.6 to version 7.2.7"
+sed -i -e "s/version=2.87.0/version=2.88.0/g" python/python-google-api-python-client/spkgbuild
+changelog "python/python-google-api-python-client/spkgbuild" "Upgraded from version 2.87.0 to version 2.88.0"
+sed -i -e "s/version=8.13.11/version=8.13.13/g" python/python-phonenumbers/spkgbuild
+changelog "python/python-phonenumbers/spkgbuild" "Upgraded from version 8.13.11 to version 8.13.13"
+sed -i -e "s/version=4.19.0/version=4.19.1/g" xfce/xfce4-power-manager/spkgbuild
+changelog "xfce/xfce4-power-manager/spkgbuild" "Upgraded from version 4.19.0 to version 4.19.1"
