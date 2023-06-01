130bb1db4432102c54e5e12609d5a3f9c2c79532
Voncloft <voncloft@gmail.com>
2023-06-01 09:19:20 -0400

    Minor Updates

---
+++ logs/old-test-updates/2023/Jun/applied_updates-06-01-23.sh
@@ -0,0 +1,20 @@
+sed -i -e "s/version=0.24.5/version=0.24.6/g" core/cbindgen/spkgbuild
+changelog "core/cbindgen/spkgbuild" "Upgraded from version 0.24.5 to version 0.24.6"
+sed -i -e "s/version=12.3/version=13.0/g" core/yad/spkgbuild
+changelog "core/yad/spkgbuild" "Upgraded from version 12.3 to version 13.0"
+sed -i -e "s/version=3.7.2/version=3.7.3/g" extra/flexget/spkgbuild
+changelog "extra/flexget/spkgbuild" "Upgraded from version 3.7.2 to version 3.7.3"
+sed -i -e "s/version=4.4.33/version=4.4.34/g" libs/libxcrypt/spkgbuild
+changelog "libs/libxcrypt/spkgbuild" "Upgraded from version 4.4.33 to version 4.4.34"
+sed -i -e "s/version=1.43.8/version=1.43.9/g" networking/networkmanager/spkgbuild
+changelog "networking/networkmanager/spkgbuild" "Upgraded from version 1.43.8 to version 1.43.9"
+sed -i -e "s/version=6.75.6/version=6.75.9/g" python/python-hypothesis/spkgbuild
+changelog "python/python-hypothesis/spkgbuild" "Upgraded from version 6.75.6 to version 6.75.9"
+sed -i -e "s/version=13.3.5/version=13.4.1/g" python/python-rich/spkgbuild
+changelog "python/python-rich/spkgbuild" "Upgraded from version 13.3.5 to version 13.4.1"
+sed -i -e "s/version=0.17.5/version=0.17.6/g" python/python-scikit-build/spkgbuild
+changelog "python/python-scikit-build/spkgbuild" "Upgraded from version 0.17.5 to version 0.17.6"
+sed -i -e "s/version=2023.5.4/version=2023.6.0/g" server/homeassistant/spkgbuild
+changelog "server/homeassistant/spkgbuild" "Upgraded from version 2023.5.4 to version 2023.6.0"
+sed -i -e "s/version=4.18.2/version=4.18.3/g" server/samba/spkgbuild
+changelog "server/samba/spkgbuild" "Upgraded from version 4.18.2 to version 4.18.3"
