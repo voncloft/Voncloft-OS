9b8582a73d11c476502ed8b5de3ac04e2e3297b2
Voncloft <voncloft@gmail.com>
2023-06-05 10:18:37 -0400

    Minor Updates

---
+++ logs/old-test-updates/2023/Jun/applied_updates-06-05-23.sh
@@ -0,0 +1,96 @@
+sed -i -e "s/version=5.6.1/version=5.8.0/g" cinnamon/cinnamon-control-center/spkgbuild
+changelog "cinnamon/cinnamon-control-center/spkgbuild" "Upgraded from version 5.6.1 to version 5.8.0"
+sed -i -e "s/version=5.6.2/version=5.8.0/g" cinnamon/cinnamon-desktop/spkgbuild
+changelog "cinnamon/cinnamon-desktop/spkgbuild" "Upgraded from version 5.6.2 to version 5.8.0"
+sed -i -e "s/version=5.6.0/version=5.8.0/g" cinnamon/cinnamon-menus/spkgbuild
+changelog "cinnamon/cinnamon-menus/spkgbuild" "Upgraded from version 5.6.0 to version 5.8.0"
+sed -i -e "s/version=5.6.3/version=5.8.0/g" cinnamon/cinnamon-screensaver/spkgbuild
+changelog "cinnamon/cinnamon-screensaver/spkgbuild" "Upgraded from version 5.6.3 to version 5.8.0"
+sed -i -e "s/version=5.6.0/version=5.8.0/g" cinnamon/cinnamon-session/spkgbuild
+changelog "cinnamon/cinnamon-session/spkgbuild" "Upgraded from version 5.6.0 to version 5.8.0"
+sed -i -e "s/version=5.6.2/version=5.8.0/g" cinnamon/cinnamon-settings-daemon/spkgbuild
+changelog "cinnamon/cinnamon-settings-daemon/spkgbuild" "Upgraded from version 5.6.2 to version 5.8.0"
+sed -i -e "s/version=5.6.1/version=5.8.0/g" cinnamon/cinnamon-translations/spkgbuild
+changelog "cinnamon/cinnamon-translations/spkgbuild" "Upgraded from version 5.6.1 to version 5.8.0"
+sed -i -e "s/version=5.6.1/version=5.8.0/g" cinnamon/cjs/spkgbuild
+changelog "cinnamon/cjs/spkgbuild" "Upgraded from version 5.6.1 to version 5.8.0"
+sed -i -e "s/version=5.6.4/version=5.8.0/g" cinnamon/muffin/spkgbuild
+changelog "cinnamon/muffin/spkgbuild" "Upgraded from version 5.6.4 to version 5.8.0"
+sed -i -e "s/version=5.6.5/version=5.8.0/g" cinnamon/nemo/spkgbuild
+changelog "cinnamon/nemo/spkgbuild" "Upgraded from version 5.6.5 to version 5.8.0"
+sed -i -e "s/version=16.0.4/version=16.0.5/g" compilers/clang/spkgbuild
+changelog "compilers/clang/spkgbuild" "Upgraded from version 16.0.4 to version 16.0.5"
+sed -i -e "s/version=16.0.4/version=16.0.5/g" compilers/lld/spkgbuild
+changelog "compilers/lld/spkgbuild" "Upgraded from version 16.0.4 to version 16.0.5"
+sed -i -e "s/version=16.0.4/version=16.0.5/g" compilers/llvm/spkgbuild
+changelog "compilers/llvm/spkgbuild" "Upgraded from version 16.0.4 to version 16.0.5"
+sed -i -e "s/version=16.0.4/version=16.0.5/g" compilers/llvm-bak/spkgbuild
+changelog "compilers/llvm-bak/spkgbuild" "Upgraded from version 16.0.4 to version 16.0.5"
+sed -i -e "s/version=1.69.0/version=1.70.0/g" compilers/rust/spkgbuild
+changelog "compilers/rust/spkgbuild" "Upgraded from version 1.69.0 to version 1.70.0"
+sed -i -e "s/version=2.4.2/version=2.4.3/g" core/cups/spkgbuild
+changelog "core/cups/spkgbuild" "Upgraded from version 2.4.2 to version 2.4.3"
+sed -i -e "s/version=0.112.5/version=0.112.7/g" core/hugo/spkgbuild
+changelog "core/hugo/spkgbuild" "Upgraded from version 0.112.5 to version 0.112.7"
+sed -i -e "s/version=3.89.1/version=3.90/g" core/nss/spkgbuild
+changelog "core/nss/spkgbuild" "Upgraded from version 3.89.1 to version 3.90"
+sed -i -e "s/version=2.2.13/version=2.2.14/g" extra/bluefish/spkgbuild
+changelog "extra/bluefish/spkgbuild" "Upgraded from version 2.2.13 to version 2.2.14"
+sed -i -e "s/version=3.7.3/version=3.7.4/g" extra/flexget/spkgbuild
+changelog "extra/flexget/spkgbuild" "Upgraded from version 3.7.3 to version 3.7.4"
+sed -i -e "s/version=16.0.4/version=16.0.5/g" multilib/llvm-32/spkgbuild
+changelog "multilib/llvm-32/spkgbuild" "Upgraded from version 16.0.4 to version 16.0.5"
+sed -i -e "s/version=1.1.4/version=1.1.5/g" libs/libdmx/spkgbuild
+changelog "libs/libdmx/spkgbuild" "Upgraded from version 1.1.4 to version 1.1.5"
+sed -i -e "s/version=3.9/version=3.9.1/g" libs/nettle/spkgbuild
+changelog "libs/nettle/spkgbuild" "Upgraded from version 3.9 to version 3.9.1"
+sed -i -e "s/version=1.3.9/version=1.3.10/g" libs/serf/spkgbuild
+changelog "libs/serf/spkgbuild" "Upgraded from version 1.3.9 to version 1.3.10"
+sed -i -e "s/version=2.4.3/version=2.6.0/g" libs/xapp/spkgbuild
+changelog "libs/xapp/spkgbuild" "Upgraded from version 2.4.3 to version 2.6.0"
+sed -i -e "s/version=2.40.1/version=2.41.0/g" networking/git/spkgbuild
+changelog "networking/git/spkgbuild" "Upgraded from version 2.40.1 to version 2.41.0"
+sed -i -e "s/version=114.0b5/version=114.0b6/g" nonfree/thunderbird/spkgbuild
+changelog "nonfree/thunderbird/spkgbuild" "Upgraded from version 114.0b5 to version 114.0b6"
+sed -i -e "s/version=0.12.1/version=0.12.2/g" python/python-bitcoinlib/spkgbuild
+changelog "python/python-bitcoinlib/spkgbuild" "Upgraded from version 0.12.1 to version 0.12.2"
+sed -i -e "s/version=1.26.143/version=1.26.146/g" python/python-boto3/spkgbuild
+changelog "python/python-boto3/spkgbuild" "Upgraded from version 1.26.143 to version 1.26.146"
+sed -i -e "s/version=1.29.143/version=1.29.146/g" python/python-botocore/spkgbuild
+changelog "python/python-botocore/spkgbuild" "Upgraded from version 1.29.143 to version 1.29.146"
+sed -i -e "s/version=0.12.12/version=0.13.0/g" python/python-cachecontrol/spkgbuild
+changelog "python/python-cachecontrol/spkgbuild" "Upgraded from version 0.12.12 to version 0.13.0"
+sed -i -e "s/version=0.18.9/version=0.18.10/g" python/python-django-modeltranslation/spkgbuild
+changelog "python/python-django-modeltranslation/spkgbuild" "Upgraded from version 0.18.9 to version 0.18.10"
+sed -i -e "s/version=6.1.2/version=6.1.3/g" python/python-docker/spkgbuild
+changelog "python/python-docker/spkgbuild" "Upgraded from version 6.1.2 to version 6.1.3"
+sed -i -e "s/version=18.9.0/version=18.10.1/g" python/python-faker/spkgbuild
+changelog "python/python-faker/spkgbuild" "Upgraded from version 18.9.0 to version 18.10.1"
+sed -i -e "s/version=0.95.2/version=0.96.0/g" python/python-fastapi/spkgbuild
+changelog "python/python-fastapi/spkgbuild" "Upgraded from version 0.95.2 to version 0.96.0"
+sed -i -e "s/version=2.19.0/version=2.19.1/g" python/python-google-auth/spkgbuild
+changelog "python/python-google-auth/spkgbuild" "Upgraded from version 2.19.0 to version 2.19.1"
+sed -i -e "s/version=6.75.9/version=6.76.0/g" python/python-hypothesis/spkgbuild
+changelog "python/python-hypothesis/spkgbuild" "Upgraded from version 6.75.9 to version 6.76.0"
+sed -i -e "s/version=1.14.7/version=1.14.8/g" python/python-jsbeautifier/spkgbuild
+changelog "python/python-jsbeautifier/spkgbuild" "Upgraded from version 1.14.7 to version 1.14.8"
+sed -i -e "s/version=2.2.0/version=3.0.0/g" python/python-markdown-it-py/spkgbuild
+changelog "python/python-markdown-it-py/spkgbuild" "Upgraded from version 2.2.0 to version 3.0.0"
+sed -i -e "s/version=0.26.0/version=0.27.0/g" python/python-meilisearch/spkgbuild
+changelog "python/python-meilisearch/spkgbuild" "Upgraded from version 0.26.0 to version 0.27.0"
+sed -i -e "s/version=3.8.14/version=3.9.0/g" python/python-orjson/spkgbuild
+changelog "python/python-orjson/spkgbuild" "Upgraded from version 3.8.14 to version 3.9.0"
+sed -i -e "s/version=2023.5.19/version=2023.6.2/g" python/python-pipenv/spkgbuild
+changelog "python/python-pipenv/spkgbuild" "Upgraded from version 2023.5.19 to version 2023.6.2"
+sed -i -e "s/version=2.4.0/version=2.4.1/g" python/python-xapp/spkgbuild
+changelog "python/python-xapp/spkgbuild" "Upgraded from version 2.4.0 to version 2.4.1"
+sed -i -e "s/version=1.3.0/version=1.4.0/g" python/python-xcffib/spkgbuild
+changelog "python/python-xcffib/spkgbuild" "Upgraded from version 1.3.0 to version 1.4.0"
+sed -i -e "s/version=3.12.36/version=3.12.37/g" server/blacklist-hosts/spkgbuild
+changelog "server/blacklist-hosts/spkgbuild" "Upgraded from version 3.12.36 to version 3.12.37"
+sed -i -e "s/version=1.2.1/version=1.2.2/g" xorg/xrdb/spkgbuild
+changelog "xorg/xrdb/spkgbuild" "Upgraded from version 1.2.1 to version 1.2.2"
+sed -i -e "s/version=1.4.0/version=1.5.0/g" xorg/xtrans/spkgbuild
+changelog "xorg/xtrans/spkgbuild" "Upgraded from version 1.4.0 to version 1.5.0"
+sed -i -e "s/version=1.0.8/version=1.0.9/g" xorg/xwd/spkgbuild
+changelog "xorg/xwd/spkgbuild" "Upgraded from version 1.0.8 to version 1.0.9"
