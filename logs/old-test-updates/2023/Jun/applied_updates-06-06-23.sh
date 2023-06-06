3a1c40847a1f15d1c83d9c6bcf7eb3fade6323d1
Voncloft <voncloft@gmail.com>
2023-06-06 18:15:28 -0400

    Minor Updates

--- logs/old-test-updates/2023/Jun/applied_updates-06-06-23.sh
+++ logs/old-test-updates/2023/Jun/applied_updates-06-06-23.sh
@@ -24,3 +24,23 @@ sed -i -e "s/version=0.63.0/version=0.64.1/g" python/python-zeroconf/spkgbuild
 changelog "python/python-zeroconf/spkgbuild" "Upgraded from version 0.63.0 to version 0.64.1"
 sed -i -e "s/version=2022.2/version=2023.1/g" xorg/xorgproto/spkgbuild
 changelog "xorg/xorgproto/spkgbuild" "Upgraded from version 2022.2 to version 2023.1"
+sed -i -e "s/version=2.4.3/version=2.4.4/g" core/cups/spkgbuild
+changelog "core/cups/spkgbuild" "Upgraded from version 2.4.3 to version 2.4.4"
+sed -i -e "s/version=1.15.4/version=1.15.6/g" core/dbus/spkgbuild
+changelog "core/dbus/spkgbuild" "Upgraded from version 1.15.4 to version 1.15.6"
+sed -i -e "s/version=4.4.34/version=4.4.35/g" libs/libxcrypt/spkgbuild
+changelog "libs/libxcrypt/spkgbuild" "Upgraded from version 4.4.34 to version 4.4.35"
+sed -i -e "s/version=114.0b9/version=115.0b1/g" nonfree/firefox/spkgbuild
+changelog "nonfree/firefox/spkgbuild" "Upgraded from version 114.0b9 to version 115.0b1"
+sed -i -e "s/version=6.15.0/version=6.15.1/g" python/python-cx-freeze/spkgbuild
+changelog "python/python-cx-freeze/spkgbuild" "Upgraded from version 6.15.0 to version 6.15.1"
+sed -i -e "s/version=26.1.0/version=27.2.0/g" python/python-kubernetes/spkgbuild
+changelog "python/python-kubernetes/spkgbuild" "Upgraded from version 26.1.0 to version 27.2.0"
+sed -i -e "s/version=4.0.4/version=4.0.5/g" python/python-pykeepass/spkgbuild
+changelog "python/python-pykeepass/spkgbuild" "Upgraded from version 4.0.4 to version 4.0.5"
+sed -i -e "s/version=3.21.7/version=3.22.0/g" python/python-trimesh/spkgbuild
+changelog "python/python-trimesh/spkgbuild" "Upgraded from version 3.21.7 to version 3.22.0"
+sed -i -e "s/version=1.32.2.7100/version=1.32.3.7162/g" server/plex-media-server/spkgbuild
+changelog "server/plex-media-server/spkgbuild" "Upgraded from version 1.32.2.7100 to version 1.32.3.7162"
+sed -i -e "s/version=3.8.0/version=3.8.1/g" server/postfix/spkgbuild
+changelog "server/postfix/spkgbuild" "Upgraded from version 3.8.0 to version 3.8.1"
