27de16b17c5a1cb4534893ac0740e7641cee75e8
Voncloft <voncloft@gmail.com>
2023-06-06 05:31:03 -0400

    Minor Updates

---
+++ logs/old-test-updates/2023/Jun/applied_updates-06-06-23.sh
@@ -0,0 +1,26 @@
+sed -i -e "s/version=0.112.7/version=0.113.0/g" core/hugo/spkgbuild
+changelog "core/hugo/spkgbuild" "Upgraded from version 0.112.7 to version 0.113.0"
+sed -i -e "s/version=3.3.5/version=3.4.0/g" core/icewm/spkgbuild
+changelog "core/icewm/spkgbuild" "Upgraded from version 3.3.5 to version 3.4.0"
+sed -i -e "s/version=6.3.5/version=6.3.6/g" core/linux-api-headers/spkgbuild
+changelog "core/linux-api-headers/spkgbuild" "Upgraded from version 6.3.5 to version 6.3.6"
+sed -i -e "s/version=6.3.5/version=6.3.6/g" core/linux-desktop/spkgbuild
+changelog "core/linux-desktop/spkgbuild" "Upgraded from version 6.3.5 to version 6.3.6"
+sed -i -e "s/version=6.3.5/version=6.3.6/g" core/linux-router/spkgbuild
+changelog "core/linux-router/spkgbuild" "Upgraded from version 6.3.5 to version 6.3.6"
+sed -i -e "s/version=3.1.7/version=3.1.8/g" libs/openexr/spkgbuild
+changelog "libs/openexr/spkgbuild" "Upgraded from version 3.1.7 to version 3.1.8"
+sed -i -e "s/version=23.05.0/version=23.06.0/g" libs/poppler/spkgbuild
+changelog "libs/poppler/spkgbuild" "Upgraded from version 23.05.0 to version 23.06.0"
+sed -i -e "s/version=1.26.146/version=1.26.147/g" python/python-boto3/spkgbuild
+changelog "python/python-boto3/spkgbuild" "Upgraded from version 1.26.146 to version 1.26.147"
+sed -i -e "s/version=1.29.146/version=1.29.147/g" python/python-botocore/spkgbuild
+changelog "python/python-botocore/spkgbuild" "Upgraded from version 1.29.146 to version 1.29.147"
+sed -i -e "s/version=0.3.5/version=0.4.0/g" python/python-mdit_py_plugins/spkgbuild
+changelog "python/python-mdit_py_plugins/spkgbuild" "Upgraded from version 0.3.5 to version 0.4.0"
+sed -i -e "s/version=0.27.0/version=0.28.0/g" python/python-meilisearch/spkgbuild
+changelog "python/python-meilisearch/spkgbuild" "Upgraded from version 0.27.0 to version 0.28.0"
+sed -i -e "s/version=0.63.0/version=0.64.1/g" python/python-zeroconf/spkgbuild
+changelog "python/python-zeroconf/spkgbuild" "Upgraded from version 0.63.0 to version 0.64.1"
+sed -i -e "s/version=2022.2/version=2023.1/g" xorg/xorgproto/spkgbuild
+changelog "xorg/xorgproto/spkgbuild" "Upgraded from version 2022.2 to version 2023.1"
