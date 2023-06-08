a6a249cb6dacc5115b962c68cdfa85f9879de3aa
Voncloft <voncloft@gmail.com>
2023-06-08 08:08:28 -0400

    Minor Updates

---
+++ logs/old-test-updates/2023/Jun/applied_updates-06-08-23.sh
@@ -0,0 +1,20 @@
+sed -i -e "s/version=5.6.8/version=5.8.0/g" cinnamon/cinnamon/spkgbuild
+changelog "cinnamon/cinnamon/spkgbuild" "Upgraded from version 5.6.8 to version 5.8.0"
+sed -i -e "s/version=115.0b1/version=115.0b2/g" nonfree/firefox/spkgbuild
+changelog "nonfree/firefox/spkgbuild" "Upgraded from version 115.0b1 to version 115.0b2"
+sed -i -e "s/version=3.1.2/version=3.1.3/g" nonfree/pandoc-bin/spkgbuild
+changelog "nonfree/pandoc-bin/spkgbuild" "Upgraded from version 3.1.2 to version 3.1.3"
+sed -i -e "s/version=1.26.147/version=1.26.148/g" python/python-boto3/spkgbuild
+changelog "python/python-boto3/spkgbuild" "Upgraded from version 1.26.147 to version 1.26.148"
+sed -i -e "s/version=1.29.147/version=1.29.148/g" python/python-botocore/spkgbuild
+changelog "python/python-botocore/spkgbuild" "Upgraded from version 1.29.147 to version 1.29.148"
+sed -i -e "s/version=1.0.0/version=1.0.1/g" python/python-libcst/spkgbuild
+changelog "python/python-libcst/spkgbuild" "Upgraded from version 1.0.0 to version 1.0.1"
+sed -i -e "s/version=1.2.1/version=1.2.2/g" python/python-sphinx_rtd_theme/spkgbuild
+changelog "python/python-sphinx_rtd_theme/spkgbuild" "Upgraded from version 1.2.1 to version 1.2.2"
+sed -i -e "s/version=0.27.0/version=0.28.0/g" python/python-starlette/spkgbuild
+changelog "python/python-starlette/spkgbuild" "Upgraded from version 0.27.0 to version 0.28.0"
+sed -i -e "s/version=2.0.2/version=2.0.3/g" python/python-urllib3/spkgbuild
+changelog "python/python-urllib3/spkgbuild" "Upgraded from version 2.0.2 to version 2.0.3"
+sed -i -e "s/version=3.12.37/version=3.12.38/g" server/blacklist-hosts/spkgbuild
+changelog "server/blacklist-hosts/spkgbuild" "Upgraded from version 3.12.37 to version 3.12.38"
