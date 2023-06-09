a016aa27bc00b2868da7ae7a6283b2ec32c7c13f
Voncloft <voncloft@gmail.com>
2023-06-09 19:15:52 -0400

    Minor Updates

---
+++ logs/old-test-updates/2023/Jun/applied_updates-06-09-23.sh
@@ -0,0 +1,34 @@
+sed -i -e "s/version=3.14.1/version=3.15.0/g" core/fuse2/spkgbuild
+changelog "core/fuse2/spkgbuild" "Upgraded from version 3.14.1 to version 3.15.0"
+sed -i -e "s/version=3.14.1/version=3.15.0/g" core/fuse3/spkgbuild
+changelog "core/fuse3/spkgbuild" "Upgraded from version 3.14.1 to version 3.15.0"
+sed -i -e "s/version=6.3.6/version=6.3.7/g" core/linux-api-headers/spkgbuild
+changelog "core/linux-api-headers/spkgbuild" "Upgraded from version 6.3.6 to version 6.3.7"
+sed -i -e "s/version=6.3.6/version=6.3.7/g" core/linux-desktop/spkgbuild
+changelog "core/linux-desktop/spkgbuild" "Upgraded from version 6.3.6 to version 6.3.7"
+sed -i -e "s/version=6.3.6/version=6.3.7/g" core/linux-router/spkgbuild
+changelog "core/linux-router/spkgbuild" "Upgraded from version 6.3.6 to version 6.3.7"
+sed -i -e "s/version=2.39.1/version=2.40/g" extra/discordchatexporter/spkgbuild
+changelog "extra/discordchatexporter/spkgbuild" "Upgraded from version 2.39.1 to version 2.40"
+sed -i -e "s/version=3.7.4/version=3.7.5/g" extra/flexget/spkgbuild
+changelog "extra/flexget/spkgbuild" "Upgraded from version 3.7.4 to version 3.7.5"
+sed -i -e "s/version=115.0b2/version=115.0b3/g" nonfree/firefox/spkgbuild
+changelog "nonfree/firefox/spkgbuild" "Upgraded from version 115.0b2 to version 115.0b3"
+sed -i -e "s/version=1.26.149/version=1.26.150/g" python/python-boto3/spkgbuild
+changelog "python/python-boto3/spkgbuild" "Upgraded from version 1.26.149 to version 1.26.150"
+sed -i -e "s/version=1.29.149/version=1.29.150/g" python/python-botocore/spkgbuild
+changelog "python/python-botocore/spkgbuild" "Upgraded from version 1.29.149 to version 1.29.150"
+sed -i -e "s/version=2023.5.1/version=2023.6.0/g" python/python-distributed/spkgbuild
+changelog "python/python-distributed/spkgbuild" "Upgraded from version 2023.5.1 to version 2023.6.0"
+sed -i -e "s/version=2023.5.0/version=2023.6.0/g" python/python-fsspec/spkgbuild
+changelog "python/python-fsspec/spkgbuild" "Upgraded from version 2023.5.0 to version 2023.6.0"
+sed -i -e "s/version=3.14.0/version=3.15.0/g" python/python-gitlab/spkgbuild
+changelog "python/python-gitlab/spkgbuild" "Upgraded from version 3.14.0 to version 3.15.0"
+sed -i -e "s/version=6.76.0/version=6.77.0/g" python/python-hypothesis/spkgbuild
+changelog "python/python-hypothesis/spkgbuild" "Upgraded from version 6.76.0 to version 6.77.0"
+sed -i -e "s/version=3.9.0/version=3.9.1/g" python/python-orjson/spkgbuild
+changelog "python/python-orjson/spkgbuild" "Upgraded from version 3.9.0 to version 3.9.1"
+sed -i -e "s/version=3.5.0/version=3.6.0/g" python/python-pyproj/spkgbuild
+changelog "python/python-pyproj/spkgbuild" "Upgraded from version 3.5.0 to version 3.6.0"
+sed -i -e "s/version=2.3.5/version=2.3.6/g" python/python-werkzeug/spkgbuild
+changelog "python/python-werkzeug/spkgbuild" "Upgraded from version 2.3.5 to version 2.3.6"
