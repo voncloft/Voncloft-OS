79f5057501e7cb1ca2725ff69d2f6a3ff1e02fb3
Voncloft <voncloft@gmail.com>
2023-06-12 08:49:40 -0400

    Minor Updates

---
+++ logs/old-test-updates/2023/Jun/applied_updates-06-12-23.sh
@@ -0,0 +1,12 @@
+sed -i -e "s/version=0.96.0/version=0.96.1/g" python/python-fastapi/spkgbuild
+changelog "python/python-fastapi/spkgbuild" "Upgraded from version 0.96.0 to version 0.96.1"
+sed -i -e "s/version=6.77.0/version=6.78.0/g" python/python-hypothesis/spkgbuild
+changelog "python/python-hypothesis/spkgbuild" "Upgraded from version 6.77.0 to version 6.78.0"
+sed -i -e "s/version=2.6.0/version=2.6.1/g" python/python-perf/spkgbuild
+changelog "python/python-perf/spkgbuild" "Upgraded from version 2.6.0 to version 2.6.1"
+sed -i -e "s/version=2023.6.2/version=2023.6.11/g" python/python-pipenv/spkgbuild
+changelog "python/python-pipenv/spkgbuild" "Upgraded from version 2023.6.2 to version 2023.6.11"
+sed -i -e "s/version=5.7.0/version=5.8.0/g" python/python-ujson/spkgbuild
+changelog "python/python-ujson/spkgbuild" "Upgraded from version 5.7.0 to version 5.8.0"
+sed -i -e "s/version=3.12.38/version=3.12.39/g" server/blacklist-hosts/spkgbuild
+changelog "server/blacklist-hosts/spkgbuild" "Upgraded from version 3.12.38 to version 3.12.39"
