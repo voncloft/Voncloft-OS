cc2f40f14140b5d80c9d022c5dd628b09e61a130
Voncloft <voncloft@gmail.com>
2023-06-13 06:53:08 -0400

    Minor Updates

---
+++ logs/old-test-updates/2023/Jun/applied_updates-06-13-23.sh
@@ -0,0 +1,32 @@
+sed -i -e "s/version=3.26.4/version=3.27.0-rc2/g" compilers/cmake/spkgbuild
+changelog "compilers/cmake/spkgbuild" "Upgraded from version 3.26.4 to version 3.27.0-rc2"
+sed -i -e "s/version=3.18.0/version=3.20.0/g" core/fortune-mod/spkgbuild
+changelog "core/fortune-mod/spkgbuild" "Upgraded from version 3.18.0 to version 3.20.0"
+sed -i -e "s/version=115.0b3/version=115.0b4/g" nonfree/firefox/spkgbuild
+changelog "nonfree/firefox/spkgbuild" "Upgraded from version 115.0b3 to version 115.0b4"
+sed -i -e "s/version=0.1.1/version=0.2.0/g" python/python-aiomysql/spkgbuild
+changelog "python/python-aiomysql/spkgbuild" "Upgraded from version 0.1.1 to version 0.2.0"
+sed -i -e "s/version=1.26.151/version=1.26.152/g" python/python-boto3/spkgbuild
+changelog "python/python-boto3/spkgbuild" "Upgraded from version 1.26.151 to version 1.26.152"
+sed -i -e "s/version=1.29.151/version=1.29.152/g" python/python-botocore/spkgbuild
+changelog "python/python-botocore/spkgbuild" "Upgraded from version 1.29.151 to version 1.29.152"
+sed -i -e "s/version=1.5.1/version=1.6.0/g" python/python-cairocffi/spkgbuild
+changelog "python/python-cairocffi/spkgbuild" "Upgraded from version 1.5.1 to version 1.6.0"
+sed -i -e "s/version=0.96.1/version=0.97.0/g" python/python-fastapi/spkgbuild
+changelog "python/python-fastapi/spkgbuild" "Upgraded from version 0.96.1 to version 0.97.0"
+sed -i -e "s/version=4.39.4/version=4.40.0/g" python/python-fonttools/spkgbuild
+changelog "python/python-fonttools/spkgbuild" "Upgraded from version 4.39.4 to version 4.40.0"
+sed -i -e "s/version=6.78.0/version=6.78.1/g" python/python-hypothesis/spkgbuild
+changelog "python/python-hypothesis/spkgbuild" "Upgraded from version 6.78.0 to version 6.78.1"
+sed -i -e "s/version=0.5.6/version=0.5.7/g" python/python-openapi-spec-validator/spkgbuild
+changelog "python/python-openapi-spec-validator/spkgbuild" "Upgraded from version 0.5.6 to version 0.5.7"
+sed -i -e "s/version=2023.6.11/version=2023.6.12/g" python/python-pipenv/spkgbuild
+changelog "python/python-pipenv/spkgbuild" "Upgraded from version 2023.6.11 to version 2023.6.12"
+sed -i -e "s/version=13.4.1/version=13.4.2/g" python/python-rich/spkgbuild
+changelog "python/python-rich/spkgbuild" "Upgraded from version 13.4.1 to version 13.4.2"
+sed -i -e "s/version=2.4.13/version=2.4.14/g" ruby-gems/ruby-bundler/spkgbuild
+changelog "ruby-gems/ruby-bundler/spkgbuild" "Upgraded from version 2.4.13 to version 2.4.14"
+sed -i -e "s/version=3.12.39/version=3.13.0/g" server/blacklist-hosts/spkgbuild
+changelog "server/blacklist-hosts/spkgbuild" "Upgraded from version 3.12.39 to version 3.13.0"
+sed -i -e "s/version=2.38/version=2.39/g" xorg/xkeyboard-config/spkgbuild
+changelog "xorg/xkeyboard-config/spkgbuild" "Upgraded from version 2.38 to version 2.39"
