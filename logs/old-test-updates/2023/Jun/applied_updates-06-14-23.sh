b986dd6b7672fbe1b7c7e75ff7bea7247dbc07d1
Voncloft <voncloft@gmail.com>
2023-06-14 08:24:19 -0400

    Minor Updates

---
+++ logs/old-test-updates/2023/Jun/applied_updates-06-14-23.sh
@@ -0,0 +1,38 @@
+sed -i -e "s/version=4.8.1/version=4.8.2/g" compilers/ccache/spkgbuild
+changelog "compilers/ccache/spkgbuild" "Upgraded from version 4.8.1 to version 4.8.2"
+sed -i -e "s/version=2.4.4/version=2.4.5/g" core/cups/spkgbuild
+changelog "core/cups/spkgbuild" "Upgraded from version 2.4.4 to version 2.4.5"
+sed -i -e "s/version=2.41.4/version=2.41.5/g" core/webkitgtk/spkgbuild
+changelog "core/webkitgtk/spkgbuild" "Upgraded from version 2.41.4 to version 2.41.5"
+sed -i -e "s/version=2.6.4/version=2.6.5/g" networking/openvpn/spkgbuild
+changelog "networking/openvpn/spkgbuild" "Upgraded from version 2.6.4 to version 2.6.5"
+sed -i -e "s/version=1.26.152/version=1.26.153/g" python/python-boto3/spkgbuild
+changelog "python/python-boto3/spkgbuild" "Upgraded from version 1.26.152 to version 1.26.153"
+sed -i -e "s/version=1.29.152/version=1.29.153/g" python/python-botocore/spkgbuild
+changelog "python/python-botocore/spkgbuild" "Upgraded from version 1.29.152 to version 1.29.153"
+sed -i -e "s/version=11.36.1/version=11.36.2/g" python/python-dropbox/spkgbuild
+changelog "python/python-dropbox/spkgbuild" "Upgraded from version 11.36.1 to version 11.36.2"
+sed -i -e "s/version=3.12.1/version=3.12.2/g" python/python-filelock/spkgbuild
+changelog "python/python-filelock/spkgbuild" "Upgraded from version 3.12.1 to version 3.12.2"
+sed -i -e "s/version=2.88.0/version=2.89.0/g" python/python-google-api-python-client/spkgbuild
+changelog "python/python-google-api-python-client/spkgbuild" "Upgraded from version 2.88.0 to version 2.89.0"
+sed -i -e "s/version=2.19.1/version=2.20.0/g" python/python-google-auth/spkgbuild
+changelog "python/python-google-auth/spkgbuild" "Upgraded from version 2.19.1 to version 2.20.0"
+sed -i -e "s/version=6.78.1/version=6.78.2/g" python/python-hypothesis/spkgbuild
+changelog "python/python-hypothesis/spkgbuild" "Upgraded from version 6.78.1 to version 6.78.2"
+sed -i -e "s/version=6.23.1/version=6.23.2/g" python/python-ipykernel/spkgbuild
+changelog "python/python-ipykernel/spkgbuild" "Upgraded from version 6.23.1 to version 6.23.2"
+sed -i -e "s/version=1.6.6/version=1.6.8/g" python/python-milc/spkgbuild
+changelog "python/python-milc/spkgbuild" "Upgraded from version 1.6.6 to version 1.6.8"
+sed -i -e "s/version=1.0.0/version=2.0.0/g" python/python-myst-parser/spkgbuild
+changelog "python/python-myst-parser/spkgbuild" "Upgraded from version 1.0.0 to version 2.0.0"
+sed -i -e "s/version=8.13.13/version=8.13.14/g" python/python-phonenumbers/spkgbuild
+changelog "python/python-phonenumbers/spkgbuild" "Upgraded from version 8.13.13 to version 8.13.14"
+sed -i -e "s/version=0.64.1/version=0.66.0/g" python/python-zeroconf/spkgbuild
+changelog "python/python-zeroconf/spkgbuild" "Upgraded from version 0.64.1 to version 0.66.0"
+sed -i -e "s/version=3.13.0/version=3.13.1/g" server/blacklist-hosts/spkgbuild
+changelog "server/blacklist-hosts/spkgbuild" "Upgraded from version 3.13.0 to version 3.13.1"
+sed -i -e "s/version=1.25.0/version=1.25.1/g" server/nginx/spkgbuild
+changelog "server/nginx/spkgbuild" "Upgraded from version 1.25.0 to version 1.25.1"
+sed -i -e "s/version=1.32.3.7162/version=1.32.3.7192/g" server/plex-media-server/spkgbuild
+changelog "server/plex-media-server/spkgbuild" "Upgraded from version 1.32.3.7162 to version 1.32.3.7192"
