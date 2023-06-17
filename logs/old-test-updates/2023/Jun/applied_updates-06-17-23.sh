2268dc04c3d1afb720993634941978c98e2faf2a
Voncloft <voncloft@gmail.com>
2023-06-17 19:31:45 -0400

    Minor Updates

---
+++ logs/old-test-updates/2023/Jun/applied_updates-06-17-23.sh
@@ -0,0 +1,18 @@
+sed -i -e "s/version=0.21.1/version=0.22/g" core/gettext/spkgbuild
+changelog "core/gettext/spkgbuild" "Upgraded from version 0.21.1 to version 0.22"
+sed -i -e "s/version=1.17.8/version=1.17.12/g" libs/cairo/spkgbuild
+changelog "libs/cairo/spkgbuild" "Upgraded from version 1.17.8 to version 1.17.12"
+sed -i -e "s/version=1.26.154/version=1.26.155/g" python/python-boto3/spkgbuild
+changelog "python/python-boto3/spkgbuild" "Upgraded from version 1.26.154 to version 1.26.155"
+sed -i -e "s/version=1.29.154/version=1.29.155/g" python/python-botocore/spkgbuild
+changelog "python/python-botocore/spkgbuild" "Upgraded from version 1.29.154 to version 1.29.155"
+sed -i -e "s/version=4.1.2/version=4.1.3/g" python/python-elementpath/spkgbuild
+changelog "python/python-elementpath/spkgbuild" "Upgraded from version 4.1.2 to version 4.1.3"
+sed -i -e "s/version=1.32/version=1.33/g" python/python-jsonpatch/spkgbuild
+changelog "python/python-jsonpatch/spkgbuild" "Upgraded from version 1.32 to version 1.33"
+sed -i -e "s/version=3.2.2/version=3.3.0/g" python/python-pyenchant/spkgbuild
+changelog "python/python-pyenchant/spkgbuild" "Upgraded from version 3.2.2 to version 3.3.0"
+sed -i -e "s/version=0.66.0/version=0.68.0/g" python/python-zeroconf/spkgbuild
+changelog "python/python-zeroconf/spkgbuild" "Upgraded from version 0.66.0 to version 0.68.0"
+sed -i -e "s/version=3.13.4/version=3.13.5/g" server/blacklist-hosts/spkgbuild
+changelog "server/blacklist-hosts/spkgbuild" "Upgraded from version 3.13.4 to version 3.13.5"
