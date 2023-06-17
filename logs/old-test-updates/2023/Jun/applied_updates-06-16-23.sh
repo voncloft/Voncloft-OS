8a8c34413ca571cd3eeb33a7b315700ccd6fac40
Voncloft <voncloft@gmail.com>
2023-06-16 20:23:40 -0400

    Minor Updates

---
+++ logs/old-test-updates/2023/Jun/applied_updates-06-16-23.sh
@@ -0,0 +1,94 @@
+sed -i -e "s/version=5.8.1/version=5.8.2/g" cinnamon/cinnamon/spkgbuild
+changelog "cinnamon/cinnamon/spkgbuild" "Upgraded from version 5.8.1 to version 5.8.2"
+sed -i -e "s/version=5.8.0/version=5.8.1/g" cinnamon/cinnamon-session/spkgbuild
+changelog "cinnamon/cinnamon-session/spkgbuild" "Upgraded from version 5.8.0 to version 5.8.1"
+sed -i -e "s/version=5.8.0/version=5.8.1/g" cinnamon/cinnamon-settings-daemon/spkgbuild
+changelog "cinnamon/cinnamon-settings-daemon/spkgbuild" "Upgraded from version 5.8.0 to version 5.8.1"
+sed -i -e "s/version=5.8.1/version=5.8.2/g" cinnamon/nemo/spkgbuild
+changelog "cinnamon/nemo/spkgbuild" "Upgraded from version 5.8.1 to version 5.8.2"
+sed -i -e "s/version=16.0.5/version=16.0.6/g" compilers/clang/spkgbuild
+changelog "compilers/clang/spkgbuild" "Upgraded from version 16.0.5 to version 16.0.6"
+sed -i -e "s/version=16.0.5/version=16.0.6/g" compilers/lld/spkgbuild
+changelog "compilers/lld/spkgbuild" "Upgraded from version 16.0.5 to version 16.0.6"
+sed -i -e "s/version=16.0.5/version=16.0.6/g" compilers/llvm/spkgbuild
+changelog "compilers/llvm/spkgbuild" "Upgraded from version 16.0.5 to version 16.0.6"
+sed -i -e "s/version=16.0.5/version=16.0.6/g" compilers/llvm-bak/spkgbuild
+changelog "compilers/llvm-bak/spkgbuild" "Upgraded from version 16.0.5 to version 16.0.6"
+sed -i -e "s/version=16.0.5/version=16.0.6/g" compilers/llvm-old-2023-06-07/spkgbuild
+changelog "compilers/llvm-old-2023-06-07/spkgbuild" "Upgraded from version 16.0.5 to version 16.0.6"
+sed -i -e "s/version=6.3.1/version=6.3.2/g" core/btrfs-progs/spkgbuild
+changelog "core/btrfs-progs/spkgbuild" "Upgraded from version 6.3.1 to version 6.3.2"
+sed -i -e "s/version=6.3.7/version=6.3.8/g" core/linux-api-headers/spkgbuild
+changelog "core/linux-api-headers/spkgbuild" "Upgraded from version 6.3.7 to version 6.3.8"
+sed -i -e "s/version=6.3.7/version=6.3.8/g" core/linux-desktop/spkgbuild
+changelog "core/linux-desktop/spkgbuild" "Upgraded from version 6.3.7 to version 6.3.8"
+sed -i -e "s/version=6.3.7/version=6.3.8/g" core/linux-router/spkgbuild
+changelog "core/linux-router/spkgbuild" "Upgraded from version 6.3.7 to version 6.3.8"
+sed -i -e "s/version=2.10.2/version=2.10.4/g" core/mps-youtube/spkgbuild
+changelog "core/mps-youtube/spkgbuild" "Upgraded from version 2.10.2 to version 2.10.4"
+sed -i -e "s/version=1.8.1/version=1.10/g" core/scrot/spkgbuild
+changelog "core/scrot/spkgbuild" "Upgraded from version 1.8.1 to version 1.10"
+sed -i -e "s/version=3.7.5/version=3.7.7/g" extra/flexget/spkgbuild
+changelog "extra/flexget/spkgbuild" "Upgraded from version 3.7.5 to version 3.7.7"
+sed -i -e "s/version=23.2/version=23.3/g" extra/protobuf/spkgbuild
+changelog "extra/protobuf/spkgbuild" "Upgraded from version 23.2 to version 23.3"
+sed -i -e "s/version=16.0.5/version=16.0.6/g" multilib/llvm-32/spkgbuild
+changelog "multilib/llvm-32/spkgbuild" "Upgraded from version 16.0.5 to version 16.0.6"
+sed -i -e "s/version=2.26.5/version=2.27.1/g" libs/sdl2/spkgbuild
+changelog "libs/sdl2/spkgbuild" "Upgraded from version 2.26.5 to version 2.27.1"
+sed -i -e "s/version=1.43.9/version=1.43.10/g" networking/networkmanager/spkgbuild
+changelog "networking/networkmanager/spkgbuild" "Upgraded from version 1.43.9 to version 1.43.10"
+sed -i -e "s/version=115.0b4/version=115.0b6/g" nonfree/firefox/spkgbuild
+changelog "nonfree/firefox/spkgbuild" "Upgraded from version 115.0b4 to version 115.0b6"
+sed -i -e "s/version=535.43.02/version=535.54.03/g" nonfree/nvidia/spkgbuild
+changelog "nonfree/nvidia/spkgbuild" "Upgraded from version 535.43.02 to version 535.54.03"
+sed -i -e "s/version=535.43.02/version=535.54.03/g" nonfree/nvidia-32/spkgbuild
+changelog "nonfree/nvidia-32/spkgbuild" "Upgraded from version 535.43.02 to version 535.54.03"
+sed -i -e "s/version=535.43.02/version=535.54.03/g" nonfree/nvidia-modules/spkgbuild
+changelog "nonfree/nvidia-modules/spkgbuild" "Upgraded from version 535.43.02 to version 535.54.03"
+sed -i -e "s/version=115.0b1/version=115.0b3/g" nonfree/thunderbird/spkgbuild
+changelog "nonfree/thunderbird/spkgbuild" "Upgraded from version 115.0b1 to version 115.0b3"
+sed -i -e "s/version=1.68.0/version=1.69.0/g" python/python-aws-sam-translator/spkgbuild
+changelog "python/python-aws-sam-translator/spkgbuild" "Upgraded from version 1.68.0 to version 1.69.0"
+sed -i -e "s/version=1.26.153/version=1.26.154/g" python/python-boto3/spkgbuild
+changelog "python/python-boto3/spkgbuild" "Upgraded from version 1.26.153 to version 1.26.154"
+sed -i -e "s/version=1.29.153/version=1.29.154/g" python/python-botocore/spkgbuild
+changelog "python/python-botocore/spkgbuild" "Upgraded from version 1.29.153 to version 1.29.154"
+sed -i -e "s/version=0.2.0/version=0.3.0/g" python/python-click-repl/spkgbuild
+changelog "python/python-click-repl/spkgbuild" "Upgraded from version 0.2.0 to version 0.3.0"
+sed -i -e "s/version=6.78.2/version=6.78.3/g" python/python-hypothesis/spkgbuild
+changelog "python/python-hypothesis/spkgbuild" "Upgraded from version 6.78.2 to version 6.78.3"
+sed -i -e "s/version=5.3.0/version=5.3.1/g" python/python-jupyter_core/spkgbuild
+changelog "python/python-jupyter_core/spkgbuild" "Upgraded from version 5.3.0 to version 5.3.1"
+sed -i -e "s/version=5.3.0/version=5.3.1/g" python/python-kombu/spkgbuild
+changelog "python/python-kombu/spkgbuild" "Upgraded from version 5.3.0 to version 5.3.1"
+sed -i -e "s/version=3.5.0/version=3.6.0/g" python/python-objgraph/spkgbuild
+changelog "python/python-objgraph/spkgbuild" "Upgraded from version 3.5.0 to version 3.6.0"
+sed -i -e "s/version=0.5.7/version=0.6.0/g" python/python-openapi-spec-validator/spkgbuild
+changelog "python/python-openapi-spec-validator/spkgbuild" "Upgraded from version 0.5.7 to version 0.6.0"
+sed -i -e "s/version=3.3.2/version=3.3.3/g" python/python-pre-commit/spkgbuild
+changelog "python/python-pre-commit/spkgbuild" "Upgraded from version 3.3.2 to version 3.3.3"
+sed -i -e "s/version=3.10.0/version=3.11.1/g" python/python-pytest-mock/spkgbuild
+changelog "python/python-pytest-mock/spkgbuild" "Upgraded from version 3.10.0 to version 3.11.1"
+sed -i -e "s/version=1.2.2/version=1.3.0/g" python/python-scikit-learn/spkgbuild
+changelog "python/python-scikit-learn/spkgbuild" "Upgraded from version 1.2.2 to version 1.3.0"
+sed -i -e "s/version=3.0.0/version=3.0.1/g" python/python-semver/spkgbuild
+changelog "python/python-semver/spkgbuild" "Upgraded from version 3.0.0 to version 3.0.1"
+sed -i -e "s/version=21.1.0/version=23.1.0/g" python/python-service-identity/spkgbuild
+changelog "python/python-service-identity/spkgbuild" "Upgraded from version 21.1.0 to version 23.1.0"
+sed -i -e "s/version=21.1.0/version=23.1.0/g" python/python-service_identity/spkgbuild
+changelog "python/python-service_identity/spkgbuild" "Upgraded from version 21.1.0 to version 23.1.0"
+sed -i -e "s/version=2.4.3/version=3.0.0/g" python/python-spyder-kernels/spkgbuild
+changelog "python/python-spyder-kernels/spkgbuild" "Upgraded from version 2.4.3 to version 3.0.0"
+sed -i -e "s/version=3.22.0/version=3.22.1/g" python/python-trimesh/spkgbuild
+changelog "python/python-trimesh/spkgbuild" "Upgraded from version 3.22.0 to version 3.22.1"
+sed -i -e "s/version=20.23.0/version=20.23.1/g" python/python-virtualenv/spkgbuild
+changelog "python/python-virtualenv/spkgbuild" "Upgraded from version 20.23.0 to version 20.23.1"
+sed -i -e "s/version=2.3.0/version=2.3.1/g" python/python-xmlschema/spkgbuild
+changelog "python/python-xmlschema/spkgbuild" "Upgraded from version 2.3.0 to version 2.3.1"
+sed -i -e "s/version=3.13.1/version=3.13.4/g" server/blacklist-hosts/spkgbuild
+changelog "server/blacklist-hosts/spkgbuild" "Upgraded from version 3.13.1 to version 3.13.4"
+sed -i -e "s/version=2023.6.1/version=2023.6.2/g" server/homeassistant/spkgbuild
+changelog "server/homeassistant/spkgbuild" "Upgraded from version 2023.6.1 to version 2023.6.2"
+sed -i -e "s/version=2023.1/version=2023.2/g" xorg/xorgproto/spkgbuild
+changelog "xorg/xorgproto/spkgbuild" "Upgraded from version 2023.1 to version 2023.2"
