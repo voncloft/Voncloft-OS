6941f3d7b194c7971f18e50996ec985d4274e707
Voncloft <voncloft@gmail.com>
2023-06-23 14:39:43 -0400

    Minor Updates

---
+++ logs/old-test-updates/2023/Jun/applied_updates-06-23-23.sh
@@ -0,0 +1,56 @@
+sed -i -e "s/version=3.27.0-rc2/version=3.27.0-rc3/g" compilers/cmake/spkgbuild
+changelog "compilers/cmake/spkgbuild" "Upgraded from version 3.27.0-rc2 to version 3.27.0-rc3"
+sed -i -e "s/version=2.4.5/version=2.4.6/g" core/cups/spkgbuild
+changelog "core/cups/spkgbuild" "Upgraded from version 2.4.5 to version 2.4.6"
+sed -i -e "s/version=6.3.8/version=6.3.9/g" core/linux-api-headers/spkgbuild
+changelog "core/linux-api-headers/spkgbuild" "Upgraded from version 6.3.8 to version 6.3.9"
+sed -i -e "s/version=6.3.8/version=6.3.9/g" core/linux-desktop/spkgbuild
+changelog "core/linux-desktop/spkgbuild" "Upgraded from version 6.3.8 to version 6.3.9"
+sed -i -e "s/version=6.3.8/version=6.3.9/g" core/linux-router/spkgbuild
+changelog "core/linux-router/spkgbuild" "Upgraded from version 6.3.8 to version 6.3.9"
+sed -i -e "s/version=6.4.4/version=6.4.5/g" core/mercurial/spkgbuild
+changelog "core/mercurial/spkgbuild" "Upgraded from version 6.4.4 to version 6.4.5"
+sed -i -e "s/version=12.7.2/version=12.7.4/g" core/sysstat/spkgbuild
+changelog "core/sysstat/spkgbuild" "Upgraded from version 12.7.2 to version 12.7.4"
+sed -i -e "s/version=3.7.7/version=3.7.8/g" extra/flexget/spkgbuild
+changelog "extra/flexget/spkgbuild" "Upgraded from version 3.7.7 to version 3.7.8"
+sed -i -e "s/version=1.6.39/version=1.6.40/g" libs/libpng/spkgbuild
+changelog "libs/libpng/spkgbuild" "Upgraded from version 1.6.39 to version 1.6.40"
+sed -i -e "s/version=9.19.13/version=9.19.14/g" networking/bind/spkgbuild
+changelog "networking/bind/spkgbuild" "Upgraded from version 9.19.13 to version 9.19.14"
+sed -i -e "s/version=2023.03.04/version=2023.06.22/g" networking/yt-dlp/spkgbuild
+changelog "networking/yt-dlp/spkgbuild" "Upgraded from version 2023.03.04 to version 2023.06.22"
+sed -i -e "s/version=115.0b7/version=115.0b8/g" nonfree/firefox/spkgbuild
+changelog "nonfree/firefox/spkgbuild" "Upgraded from version 115.0b7 to version 115.0b8"
+sed -i -e "s/version=115.0b3/version=115.0b4/g" nonfree/thunderbird/spkgbuild
+changelog "nonfree/thunderbird/spkgbuild" "Upgraded from version 115.0b3 to version 115.0b4"
+sed -i -e "s/version=1.69.0/version=1.70.0/g" python/python-aws-sam-translator/spkgbuild
+changelog "python/python-aws-sam-translator/spkgbuild" "Upgraded from version 1.69.0 to version 1.70.0"
+sed -i -e "s/version=1.26.156/version=1.26.158/g" python/python-boto3/spkgbuild
+changelog "python/python-boto3/spkgbuild" "Upgraded from version 1.26.156 to version 1.26.158"
+sed -i -e "s/version=1.29.156/version=1.29.158/g" python/python-botocore/spkgbuild
+changelog "python/python-botocore/spkgbuild" "Upgraded from version 1.29.156 to version 1.29.158"
+sed -i -e "s/version=18.11.0/version=18.11.1/g" python/python-faker/spkgbuild
+changelog "python/python-faker/spkgbuild" "Upgraded from version 18.11.0 to version 18.11.1"
+sed -i -e "s/version=0.97.0/version=0.98.0/g" python/python-fastapi/spkgbuild
+changelog "python/python-fastapi/spkgbuild" "Upgraded from version 0.97.0 to version 0.98.0"
+sed -i -e "s/version=3.0.4/version=3.0.5/g" python/python-flask-sqlalchemy/spkgbuild
+changelog "python/python-flask-sqlalchemy/spkgbuild" "Upgraded from version 3.0.4 to version 3.0.5"
+sed -i -e "s/version=1.21.0/version=1.22.0/g" python/python-hcloud/spkgbuild
+changelog "python/python-hcloud/spkgbuild" "Upgraded from version 1.21.0 to version 1.22.0"
+sed -i -e "s/version=6.79.1/version=6.79.2/g" python/python-hypothesis/spkgbuild
+changelog "python/python-hypothesis/spkgbuild" "Upgraded from version 6.79.1 to version 6.79.2"
+sed -i -e "s/version=1.2.0/version=1.3.0/g" python/python-openstacksdk/spkgbuild
+changelog "python/python-openstacksdk/spkgbuild" "Upgraded from version 1.2.0 to version 1.3.0"
+sed -i -e "s/version=13.0.0/version=13.0.1/g" python/python-oslo-db/spkgbuild
+changelog "python/python-oslo-db/spkgbuild" "Upgraded from version 13.0.0 to version 13.0.1"
+sed -i -e "s/version=3.4.3/version=4.0.0/g" python/python-osprofiler/spkgbuild
+changelog "python/python-osprofiler/spkgbuild" "Upgraded from version 3.4.3 to version 4.0.0"
+sed -i -e "s/version=1.1.0/version=1.2.0/g" python/python-pluggy/spkgbuild
+changelog "python/python-pluggy/spkgbuild" "Upgraded from version 1.1.0 to version 1.2.0"
+sed -i -e "s/version=0.22.11.04.0/version=23.06.21.0/g" python/python-prance/spkgbuild
+changelog "python/python-prance/spkgbuild" "Upgraded from version 0.22.11.04.0 to version 23.06.21.0"
+sed -i -e "s/version=1.58.2/version=1.59.0/g" python/python-pygithub/spkgbuild
+changelog "python/python-pygithub/spkgbuild" "Upgraded from version 1.58.2 to version 1.59.0"
+sed -i -e "s/version=1.7.0/version=2.0.0/g" python/python-tblib/spkgbuild
+changelog "python/python-tblib/spkgbuild" "Upgraded from version 1.7.0 to version 2.0.0"
