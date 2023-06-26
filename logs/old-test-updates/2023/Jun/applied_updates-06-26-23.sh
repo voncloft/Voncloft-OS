6a2dc3f1e469eafbce8f56b9e465b43553021d2d
Voncloft <voncloft@gmail.com>
2023-06-26 10:03:01 -0400

    Minor Updates

---
+++ logs/old-test-updates/2023/Jun/applied_updates-06-26-23.sh
@@ -0,0 +1,10 @@
+sed -i -e "s/version=3.8/version=4.0.0/g" core/weechat/spkgbuild
+changelog "core/weechat/spkgbuild" "Upgraded from version 3.8 to version 4.0.0"
+sed -i -e "s/version=5.1/version=5.2/g" extra/gnucash/spkgbuild
+changelog "extra/gnucash/spkgbuild" "Upgraded from version 5.1 to version 5.2"
+sed -i -e "s/version=3.1.3/version=3.1.4/g" nonfree/pandoc-bin/spkgbuild
+changelog "nonfree/pandoc-bin/spkgbuild" "Upgraded from version 3.1.3 to version 3.1.4"
+sed -i -e "s/version=7.3.2305131934/version=7.3.2306251027/g" python/python-pycontracts/spkgbuild
+changelog "python/python-pycontracts/spkgbuild" "Upgraded from version 7.3.2305131934 to version 7.3.2306251027"
+sed -i -e "s/version=1.12.1/version=1.12.2/g" python/python-pygit2/spkgbuild
+changelog "python/python-pygit2/spkgbuild" "Upgraded from version 1.12.1 to version 1.12.2"
