590fd851dd1e169e5edd54ad0db00d7b9adfa82e
Voncloft <voncloft@gmail.com>
2023-06-27 20:54:39 -0400

    Minor Updates

---
+++ logs/old-test-updates/2023/Jun/applied_updates-06-27-23.sh
@@ -0,0 +1,54 @@
+sed -i -e "s/version=5.8.2/version=5.8.3/g" cinnamon/cinnamon/spkgbuild
+changelog "cinnamon/cinnamon/spkgbuild" "Upgraded from version 5.8.2 to version 5.8.3"
+sed -i -e "s/version=5.8.2/version=5.8.3/g" cinnamon/nemo/spkgbuild
+changelog "cinnamon/nemo/spkgbuild" "Upgraded from version 5.8.2 to version 5.8.3"
+sed -i -e "s/version=0.4.44/version=0.4.46/g" core/gegl/spkgbuild
+changelog "core/gegl/spkgbuild" "Upgraded from version 0.4.44 to version 0.4.46"
+sed -i -e "s/version=3.11.2/version=3.12.0/g" core/geos/spkgbuild
+changelog "core/geos/spkgbuild" "Upgraded from version 3.11.2 to version 3.12.0"
+sed -i -e "s/version=6.3.0/version=6.4.0/g" core/iproute2/spkgbuild
+changelog "core/iproute2/spkgbuild" "Upgraded from version 6.3.0 to version 6.4.0"
+sed -i -e "s/version=6.3.9/version=6.4/g" core/linux-api-headers/spkgbuild
+changelog "core/linux-api-headers/spkgbuild" "Upgraded from version 6.3.9 to version 6.4"
+sed -i -e "s/version=6.3.9/version=6.4/g" core/linux-desktop/spkgbuild
+changelog "core/linux-desktop/spkgbuild" "Upgraded from version 6.3.9 to version 6.4"
+sed -i -e "s/version=6.3.9/version=6.4/g" core/linux-router/spkgbuild
+changelog "core/linux-router/spkgbuild" "Upgraded from version 6.3.9 to version 6.4"
+sed -i -e "s/version=0.3.71/version=0.3.72/g" core/pipewire/spkgbuild
+changelog "core/pipewire/spkgbuild" "Upgraded from version 0.3.71 to version 0.3.72"
+sed -i -e "s/version=2.39/version=2.39.1/g" core/util-linux/spkgbuild
+changelog "core/util-linux/spkgbuild" "Upgraded from version 2.39 to version 2.39.1"
+sed -i -e "s/version=5.2/version=5.3/g" extra/gnucash/spkgbuild
+changelog "extra/gnucash/spkgbuild" "Upgraded from version 5.2 to version 5.3"
+sed -i -e "s/version=3.1.8/version=3.1.9/g" libs/openexr/spkgbuild
+changelog "libs/openexr/spkgbuild" "Upgraded from version 3.1.8 to version 3.1.9"
+sed -i -e "s/version=115.0b4/version=115.0b5/g" nonfree/thunderbird/spkgbuild
+changelog "nonfree/thunderbird/spkgbuild" "Upgraded from version 115.0b4 to version 115.0b5"
+sed -i -e "s/version=1.26.160/version=1.26.161/g" python/python-boto3/spkgbuild
+changelog "python/python-boto3/spkgbuild" "Upgraded from version 1.26.160 to version 1.26.161"
+sed -i -e "s/version=1.29.160/version=1.29.161/g" python/python-botocore/spkgbuild
+changelog "python/python-botocore/spkgbuild" "Upgraded from version 1.29.160 to version 1.29.161"
+sed -i -e "s/version=6.15.1/version=6.15.2/g" python/python-cx-freeze/spkgbuild
+changelog "python/python-cx-freeze/spkgbuild" "Upgraded from version 6.15.1 to version 6.15.2"
+sed -i -e "s/version=2023.6.0/version=2023.6.1/g" python/python-distributed/spkgbuild
+changelog "python/python-distributed/spkgbuild" "Upgraded from version 2023.6.0 to version 2023.6.1"
+sed -i -e "s/version=4.1.3/version=4.1.4/g" python/python-elementpath/spkgbuild
+changelog "python/python-elementpath/spkgbuild" "Upgraded from version 4.1.3 to version 4.1.4"
+sed -i -e "s/version=18.11.1/version=18.11.2/g" python/python-faker/spkgbuild
+changelog "python/python-faker/spkgbuild" "Upgraded from version 18.11.1 to version 18.11.2"
+sed -i -e "s/version=2.20.0/version=2.22.0/g" python/python-google-auth/spkgbuild
+changelog "python/python-google-auth/spkgbuild" "Upgraded from version 2.20.0 to version 2.22.0"
+sed -i -e "s/version=6.79.2/version=6.79.4/g" python/python-hypothesis/spkgbuild
+changelog "python/python-hypothesis/spkgbuild" "Upgraded from version 6.79.2 to version 6.79.4"
+sed -i -e "s/version=3.26.0/version=3.27.0/g" python/python-openmdao/spkgbuild
+changelog "python/python-openmdao/spkgbuild" "Upgraded from version 3.26.0 to version 3.27.0"
+sed -i -e "s/version=2023.6.18/version=2023.6.26/g" python/python-pipenv/spkgbuild
+changelog "python/python-pipenv/spkgbuild" "Upgraded from version 2023.6.18 to version 2023.6.26"
+sed -i -e "s/version=2.1.1/version=2.2.0/g" python/python-pytest-enabler/spkgbuild
+changelog "python/python-pytest-enabler/spkgbuild" "Upgraded from version 2.1.1 to version 2.2.0"
+sed -i -e "s/version=5.3.0/version=5.5.0/g" python/python-tempora/spkgbuild
+changelog "python/python-tempora/spkgbuild" "Upgraded from version 5.3.0 to version 5.5.0"
+sed -i -e "s/version=4.3.1/version=5.0.0/g" python/python-vcrpy/spkgbuild
+changelog "python/python-vcrpy/spkgbuild" "Upgraded from version 4.3.1 to version 5.0.0"
+sed -i -e "s/version=3.12.0/version=3.12.1/g" ruby-gems/ruby-rspec-support/spkgbuild
+changelog "ruby-gems/ruby-rspec-support/spkgbuild" "Upgraded from version 3.12.0 to version 3.12.1"
