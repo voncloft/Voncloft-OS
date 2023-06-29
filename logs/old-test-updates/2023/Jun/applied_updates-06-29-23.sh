2112fc50bbdfa509b9765bb27117b3c829dc712f
Voncloft <voncloft@gmail.com>
2023-06-29 08:17:19 -0400

    Minor Updates

---
+++ logs/old-test-updates/2023/Jun/applied_updates-06-29-23.sh
@@ -0,0 +1,24 @@
+sed -i -e "s/version=1.1.1/version=1.2.0/g" compilers/meson/spkgbuild
+changelog "compilers/meson/spkgbuild" "Upgraded from version 1.1.1 to version 1.2.0"
+sed -i -e "s/version=2.9.4/version=2.10.0/g" core/udisks2/spkgbuild
+changelog "core/udisks2/spkgbuild" "Upgraded from version 2.9.4 to version 2.10.0"
+sed -i -e "s/version=0.43.0/version=0.44.0/g" libs/libfilezilla/spkgbuild
+changelog "libs/libfilezilla/spkgbuild" "Upgraded from version 0.43.0 to version 0.44.0"
+sed -i -e "s/version=2.5.0/version=2.5.1/g" python/python-aiobotocore/spkgbuild
+changelog "python/python-aiobotocore/spkgbuild" "Upgraded from version 2.5.0 to version 2.5.1"
+sed -i -e "s/version=1.26.161/version=1.26.162/g" python/python-boto3/spkgbuild
+changelog "python/python-boto3/spkgbuild" "Upgraded from version 1.26.161 to version 1.26.162"
+sed -i -e "s/version=1.29.161/version=1.29.162/g" python/python-botocore/spkgbuild
+changelog "python/python-botocore/spkgbuild" "Upgraded from version 1.29.161 to version 1.29.162"
+sed -i -e "s/version=6.79.4/version=6.80.0/g" python/python-hypothesis/spkgbuild
+changelog "python/python-hypothesis/spkgbuild" "Upgraded from version 6.79.4 to version 6.80.0"
+sed -i -e "s/version=1.2.0/version=1.3.0/g" python/python-joblib/spkgbuild
+changelog "python/python-joblib/spkgbuild" "Upgraded from version 1.2.0 to version 1.3.0"
+sed -i -e "s/version=2.3.0/version=2.4.0/g" python/python-maxminddb/spkgbuild
+changelog "python/python-maxminddb/spkgbuild" "Upgraded from version 2.3.0 to version 2.4.0"
+sed -i -e "s/version=2.12.0/version=2.13.0/g" python/python-tensorflow-estimator/spkgbuild
+changelog "python/python-tensorflow-estimator/spkgbuild" "Upgraded from version 2.12.0 to version 2.13.0"
+sed -i -e "s/version=3.13.7/version=3.13.8/g" server/blacklist-hosts/spkgbuild
+changelog "server/blacklist-hosts/spkgbuild" "Upgraded from version 3.13.7 to version 3.13.8"
+sed -i -e "s/version=2023.6.3/version=2023.7.0/g" server/homeassistant/spkgbuild
+changelog "server/homeassistant/spkgbuild" "Upgraded from version 2023.6.3 to version 2023.7.0"
