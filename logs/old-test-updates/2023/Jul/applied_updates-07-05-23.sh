a2e5d8f1ef4f94b178d864e9031a543a94541b81
Voncloft <voncloft@gmail.com>
2023-07-05 09:03:23 -0400

    Minor Updates

---
+++ logs/old-test-updates/2023/Jul/applied_updates-07-05-23.sh
@@ -0,0 +1,38 @@
+sed -i -e "s/version=2.4.2/version=2.4.3/g" core/gnupg/spkgbuild
+changelog "core/gnupg/spkgbuild" "Upgraded from version 2.4.2 to version 2.4.3"
+sed -i -e "s/version=0.115.0/version=0.115.1/g" core/hugo/spkgbuild
+changelog "core/hugo/spkgbuild" "Upgraded from version 0.115.0 to version 0.115.1"
+sed -i -e "s/version=4.6.1/version=4.7.0/g" core/qalculate/spkgbuild
+changelog "core/qalculate/spkgbuild" "Upgraded from version 4.6.1 to version 4.7.0"
+sed -i -e "s/version=2.41.5/version=2.41.6/g" core/webkitgtk/spkgbuild
+changelog "core/webkitgtk/spkgbuild" "Upgraded from version 2.41.5 to version 2.41.6"
+sed -i -e "s/version=2.1.91/version=3.0.0/g" libs/libjpeg-turbo/spkgbuild
+changelog "libs/libjpeg-turbo/spkgbuild" "Upgraded from version 2.1.91 to version 3.0.0"
+sed -i -e "s/version=4.6.1/version=4.7.0/g" libs/libqalculate/spkgbuild
+changelog "libs/libqalculate/spkgbuild" "Upgraded from version 4.6.1 to version 4.7.0"
+sed -i -e "s/version=2.18.0/version=2.19.0/g" libs/libva/spkgbuild
+changelog "libs/libva/spkgbuild" "Upgraded from version 2.18.0 to version 2.19.0"
+sed -i -e "s/version=23.06.0/version=23.07.0/g" libs/poppler/spkgbuild
+changelog "libs/poppler/spkgbuild" "Upgraded from version 23.06.0 to version 23.07.0"
+sed -i -e "s/version=1.26.165/version=1.27.0/g" python/python-boto3/spkgbuild
+changelog "python/python-boto3/spkgbuild" "Upgraded from version 1.26.165 to version 1.27.0"
+sed -i -e "s/version=1.29.165/version=1.30.0/g" python/python-botocore/spkgbuild
+changelog "python/python-botocore/spkgbuild" "Upgraded from version 1.29.165 to version 1.30.0"
+sed -i -e "s/version=1.23.1/version=1.24.0/g" python/python-hcloud/spkgbuild
+changelog "python/python-hcloud/spkgbuild" "Upgraded from version 1.23.1 to version 1.24.0"
+sed -i -e "s/version=6.23.3/version=6.24.0/g" python/python-ipykernel/spkgbuild
+changelog "python/python-ipykernel/spkgbuild" "Upgraded from version 6.23.3 to version 6.24.0"
+sed -i -e "s/version=8.0.6/version=8.0.7/g" python/python-ipywidgets/spkgbuild
+changelog "python/python-ipywidgets/spkgbuild" "Upgraded from version 8.0.6 to version 8.0.7"
+sed -i -e "s/version=4.0/version=5.0/g" python/python-libarchive-c/spkgbuild
+changelog "python/python-libarchive-c/spkgbuild" "Upgraded from version 4.0 to version 5.0"
+sed -i -e "s/version=0.28.0/version=0.28.1/g" python/python-meilisearch/spkgbuild
+changelog "python/python-meilisearch/spkgbuild" "Upgraded from version 0.28.0 to version 0.28.1"
+sed -i -e "s/version=0.6.1/version=0.6.2/g" python/python-parse-type/spkgbuild
+changelog "python/python-parse-type/spkgbuild" "Upgraded from version 0.6.1 to version 0.6.2"
+sed -i -e "s/version=2023.7.1/version=2023.7.3/g" python/python-pipenv/spkgbuild
+changelog "python/python-pipenv/spkgbuild" "Upgraded from version 2023.7.1 to version 2023.7.3"
+sed -i -e "s/version=2.0/version=2.0.1/g" python/python-pydantic/spkgbuild
+changelog "python/python-pydantic/spkgbuild" "Upgraded from version 2.0 to version 2.0.1"
+sed -i -e "s/version=1.5.4/version=1.5.5/g" python/python-typed-ast/spkgbuild
+changelog "python/python-typed-ast/spkgbuild" "Upgraded from version 1.5.4 to version 1.5.5"
