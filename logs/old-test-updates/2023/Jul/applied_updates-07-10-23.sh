b6bb44fa62695ac1ff5e4d48b732aab4c1bdc61b
Voncloft <voncloft@gmail.com>
2023-07-10 18:53:16 -0400

    Minor Updates

---
+++ logs/old-test-updates/2023/Jul/applied_updates-07-10-23.sh
@@ -0,0 +1,24 @@
+sed -i -e "s/version=7.0.11/version=7.0.12/g" core/redis/spkgbuild
+changelog "core/redis/spkgbuild" "Upgraded from version 7.0.11 to version 7.0.12"
+sed -i -e "s/version=116.0b2/version=116.0b3/g" nonfree/firefox/spkgbuild
+changelog "nonfree/firefox/spkgbuild" "Upgraded from version 116.0b2 to version 116.0b3"
+sed -i -e "s/version=6.80.1/version=6.81.0/g" python/python-hypothesis/spkgbuild
+changelog "python/python-hypothesis/spkgbuild" "Upgraded from version 6.80.1 to version 6.81.0"
+sed -i -e "s/version=1.3.0/version=1.3.1/g" python/python-joblib/spkgbuild
+changelog "python/python-joblib/spkgbuild" "Upgraded from version 1.3.0 to version 1.3.1"
+sed -i -e "s/version=8.13.15/version=8.13.16/g" python/python-phonenumbers/spkgbuild
+changelog "python/python-phonenumbers/spkgbuild" "Upgraded from version 8.13.15 to version 8.13.16"
+sed -i -e "s/version=2023.7.4/version=2023.7.9/g" python/python-pipenv/spkgbuild
+changelog "python/python-pipenv/spkgbuild" "Upgraded from version 2023.7.4 to version 2023.7.9"
+sed -i -e "s/version=0.13.8/version=0.13.10/g" python/python-pybuilder/spkgbuild
+changelog "python/python-pybuilder/spkgbuild" "Upgraded from version 0.13.8 to version 0.13.10"
+sed -i -e "s/version=3.12.0/version=3.13.0/g" python/python-pytest-randomly/spkgbuild
+changelog "python/python-pytest-randomly/spkgbuild" "Upgraded from version 3.12.0 to version 3.13.0"
+sed -i -e "s/version=1.0.0/version=1.1.0/g" python/python-trustme/spkgbuild
+changelog "python/python-trustme/spkgbuild" "Upgraded from version 1.0.0 to version 1.1.0"
+sed -i -e "s/version=8.2.0/version=8.3.0/g" python/python-webargs/spkgbuild
+changelog "python/python-webargs/spkgbuild" "Upgraded from version 8.2.0 to version 8.3.0"
+sed -i -e "s/version=2.4.15/version=2.4.16/g" ruby-gems/ruby-bundler/spkgbuild
+changelog "ruby-gems/ruby-bundler/spkgbuild" "Upgraded from version 2.4.15 to version 2.4.16"
+sed -i -e "s/version=3.13.10/version=3.13.11/g" server/blacklist-hosts/spkgbuild
+changelog "server/blacklist-hosts/spkgbuild" "Upgraded from version 3.13.10 to version 3.13.11"
