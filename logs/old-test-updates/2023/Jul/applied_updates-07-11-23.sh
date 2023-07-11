3ee6a31330adaf950612f994812317784f708e1a
Voncloft <voncloft@gmail.com>
2023-07-11 18:44:11 -0400

    Minor Updates

---
+++ logs/old-test-updates/2023/Jul/applied_updates-07-11-23.sh
@@ -0,0 +1,36 @@
+sed -i -e "s/version=1.31/version=1.32/g" core/fakeroot/spkgbuild
+changelog "core/fakeroot/spkgbuild" "Upgraded from version 1.31 to version 1.32"
+sed -i -e "s/version=6.4.2/version=6.4.3/g" core/linux-api-headers/spkgbuild
+changelog "core/linux-api-headers/spkgbuild" "Upgraded from version 6.4.2 to version 6.4.3"
+sed -i -e "s/version=6.4.2/version=6.4.3/g" core/linux-desktop/spkgbuild
+changelog "core/linux-desktop/spkgbuild" "Upgraded from version 6.4.2 to version 6.4.3"
+sed -i -e "s/version=6.4.2/version=6.4.3/g" core/linux-router/spkgbuild
+changelog "core/linux-router/spkgbuild" "Upgraded from version 6.4.2 to version 6.4.3"
+sed -i -e "s/version=0.9.29/version=0.9.31/g" core/lmdb/spkgbuild
+changelog "core/lmdb/spkgbuild" "Upgraded from version 0.9.29 to version 0.9.31"
+sed -i -e "s/version=3.7.8/version=3.7.9/g" extra/flexget/spkgbuild
+changelog "extra/flexget/spkgbuild" "Upgraded from version 3.7.8 to version 3.7.9"
+sed -i -e "s/version=1.2.5/version=1.2.6/g" networking/libnftnl/spkgbuild
+changelog "networking/libnftnl/spkgbuild" "Upgraded from version 1.2.5 to version 1.2.6"
+sed -i -e "s/version=116.0b1/version=116.0b2/g" nonfree/thunderbird/spkgbuild
+changelog "nonfree/thunderbird/spkgbuild" "Upgraded from version 116.0b1 to version 116.0b2"
+sed -i -e "s/version=1.28.1/version=1.28.2/g" python/python-boto3/spkgbuild
+changelog "python/python-boto3/spkgbuild" "Upgraded from version 1.28.1 to version 1.28.2"
+sed -i -e "s/version=1.31.1/version=1.31.2/g" python/python-botocore/spkgbuild
+changelog "python/python-botocore/spkgbuild" "Upgraded from version 1.31.1 to version 1.31.2"
+sed -i -e "s/version=4.20.0/version=4.21.0/g" python/python-braintree/spkgbuild
+changelog "python/python-braintree/spkgbuild" "Upgraded from version 4.20.0 to version 4.21.0"
+sed -i -e "s/version=22.10.2/version=23.7.0/g" python/python-gevent/spkgbuild
+changelog "python/python-gevent/spkgbuild" "Upgraded from version 22.10.2 to version 23.7.0"
+sed -i -e "s/version=2.92.0/version=2.93.0/g" python/python-google-api-python-client/spkgbuild
+changelog "python/python-google-api-python-client/spkgbuild" "Upgraded from version 2.92.0 to version 2.93.0"
+sed -i -e "s/version=6.81.0/version=6.81.1/g" python/python-hypothesis/spkgbuild
+changelog "python/python-hypothesis/spkgbuild" "Upgraded from version 6.81.0 to version 6.81.1"
+sed -i -e "s/version=5.0.2/version=5.1.0/g" python/python-mock/spkgbuild
+changelog "python/python-mock/spkgbuild" "Upgraded from version 5.0.2 to version 5.1.0"
+sed -i -e "s/version=2023.7.9/version=2023.7.11/g" python/python-pipenv/spkgbuild
+changelog "python/python-pipenv/spkgbuild" "Upgraded from version 2023.7.9 to version 2023.7.11"
+sed -i -e "s/version=0.17.0/version=0.17.1/g" python/python-prometheus_client/spkgbuild
+changelog "python/python-prometheus_client/spkgbuild" "Upgraded from version 0.17.0 to version 0.17.1"
+sed -i -e "s/version=5.0/version=5.0.1/g" python/python-zope-exceptions/spkgbuild
+changelog "python/python-zope-exceptions/spkgbuild" "Upgraded from version 5.0 to version 5.0.1"
