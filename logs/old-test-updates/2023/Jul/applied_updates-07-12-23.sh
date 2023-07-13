2dcdb4f66bf1f1cb44f69637b6facab5a5a8c478
Voncloft <voncloft@gmail.com>
2023-07-12 20:53:31 -0400

    Minor Updates

---
+++ logs/old-test-updates/2023/Jul/applied_updates-07-12-23.sh
@@ -0,0 +1,24 @@
+sed -i -e "s/version=0.3.73/version=0.3.74/g" core/pipewire/spkgbuild
+changelog "core/pipewire/spkgbuild" "Upgraded from version 0.3.73 to version 0.3.74"
+sed -i -e "s/version=4.0.1/version=4.0.2/g" core/weechat/spkgbuild
+changelog "core/weechat/spkgbuild" "Upgraded from version 4.0.1 to version 4.0.2"
+sed -i -e "s/version=22.1.8/version=23.1.2/g" libs/xwayland/spkgbuild
+changelog "libs/xwayland/spkgbuild" "Upgraded from version 22.1.8 to version 23.1.2"
+sed -i -e "s/version=1.43.10/version=1.43.11/g" networking/networkmanager/spkgbuild
+changelog "networking/networkmanager/spkgbuild" "Upgraded from version 1.43.10 to version 1.43.11"
+sed -i -e "s/version=4.0.6/version=4.0.7/g" networking/wireshark/spkgbuild
+changelog "networking/wireshark/spkgbuild" "Upgraded from version 4.0.6 to version 4.0.7"
+sed -i -e "s/version=116.0b3/version=116.0b4/g" nonfree/firefox/spkgbuild
+changelog "nonfree/firefox/spkgbuild" "Upgraded from version 116.0b3 to version 116.0b4"
+sed -i -e "s/version=18.13.0/version=19.1.0/g" python/python-faker/spkgbuild
+changelog "python/python-faker/spkgbuild" "Upgraded from version 18.13.0 to version 19.1.0"
+sed -i -e "s/version=4.40.0/version=4.41.0/g" python/python-fonttools/spkgbuild
+changelog "python/python-fonttools/spkgbuild" "Upgraded from version 4.40.0 to version 4.41.0"
+sed -i -e "s/version=4.18.0/version=4.18.2/g" python/python-jsonschema/spkgbuild
+changelog "python/python-jsonschema/spkgbuild" "Upgraded from version 4.18.0 to version 4.18.2"
+sed -i -e "s/version=7.7.0/version=7.7.1/g" python/python-praw/spkgbuild
+changelog "python/python-praw/spkgbuild" "Upgraded from version 7.7.0 to version 7.7.1"
+sed -i -e "s/version=0.21.0/version=0.21.1/g" python/python-pytest-asyncio/spkgbuild
+changelog "python/python-pytest-asyncio/spkgbuild" "Upgraded from version 0.21.0 to version 0.21.1"
+sed -i -e "s/version=3.12.5/version=3.12.6/g" ruby-gems/ruby-rspec-mocks/spkgbuild
+changelog "ruby-gems/ruby-rspec-mocks/spkgbuild" "Upgraded from version 3.12.5 to version 3.12.6"
