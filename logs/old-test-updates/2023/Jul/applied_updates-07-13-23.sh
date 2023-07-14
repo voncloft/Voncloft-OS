669d7b838b5423c7628c03ba614139e6e136595e
Voncloft <voncloft@gmail.com>
2023-07-13 23:28:29 -0400

    Minor Updates

---
+++ logs/old-test-updates/2023/Jul/applied_updates-07-13-23.sh
@@ -0,0 +1,24 @@
+sed -i -e "s/version=1.70.0/version=1.71.0/g" compilers/rust/spkgbuild
+changelog "compilers/rust/spkgbuild" "Upgraded from version 1.70.0 to version 1.71.0"
+sed -i -e "s/version=1.32/version=1.32.1/g" core/fakeroot/spkgbuild
+changelog "core/fakeroot/spkgbuild" "Upgraded from version 1.32 to version 1.32.1"
+sed -i -e "s/version=0.115.2/version=0.115.3/g" core/hugo/spkgbuild
+changelog "core/hugo/spkgbuild" "Upgraded from version 0.115.2 to version 0.115.3"
+sed -i -e "s/version=116.0b2/version=116.0b3/g" nonfree/thunderbird/spkgbuild
+changelog "nonfree/thunderbird/spkgbuild" "Upgraded from version 116.0b2 to version 116.0b3"
+sed -i -e "s/version=8.1.4/version=8.1.5/g" python/python-click/spkgbuild
+changelog "python/python-click/spkgbuild" "Upgraded from version 8.1.4 to version 8.1.5"
+sed -i -e "s/version=1.9.0/version=1.9.1/g" python/python-jsonmerge/spkgbuild
+changelog "python/python-jsonmerge/spkgbuild" "Upgraded from version 1.9.0 to version 1.9.1"
+sed -i -e "s/version=4.18.2/version=4.18.3/g" python/python-jsonschema/spkgbuild
+changelog "python/python-jsonschema/spkgbuild" "Upgraded from version 4.18.2 to version 4.18.3"
+sed -i -e "s/version=4.0.0/version=4.1.0/g" python/python-osprofiler/spkgbuild
+changelog "python/python-osprofiler/spkgbuild" "Upgraded from version 4.0.0 to version 4.1.0"
+sed -i -e "s/version=0.28.0/version=0.30.0/g" python/python-starlette/spkgbuild
+changelog "python/python-starlette/spkgbuild" "Upgraded from version 0.28.0 to version 0.30.0"
+sed -i -e "s/version=3.22.3/version=3.22.4/g" python/python-trimesh/spkgbuild
+changelog "python/python-trimesh/spkgbuild" "Upgraded from version 3.22.3 to version 3.22.4"
+sed -i -e "s/version=3.16.0/version=3.16.1/g" python/python-zipp/spkgbuild
+changelog "python/python-zipp/spkgbuild" "Upgraded from version 3.16.0 to version 3.16.1"
+sed -i -e "s/version=2023.7.1/version=2023.7.2/g" server/homeassistant/spkgbuild
+changelog "server/homeassistant/spkgbuild" "Upgraded from version 2023.7.1 to version 2023.7.2"
