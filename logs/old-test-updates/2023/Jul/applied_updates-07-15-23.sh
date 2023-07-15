5f05447bb6622fb6349fe6d56e60225d084d32af
Voncloft <voncloft@gmail.com>
2023-07-15 19:27:49 -0400

    Minor Updates

---
+++ logs/old-test-updates/2023/Jul/applied_updates-07-15-23.sh
@@ -0,0 +1,34 @@
+sed -i -e "s/version=3.27.0-rc4/version=3.27.0-rc5/g" compilers/cmake/spkgbuild
+changelog "compilers/cmake/spkgbuild" "Upgraded from version 3.27.0-rc4 to version 3.27.0-rc5"
+sed -i -e "s/version=1.0.7/version=1.0.8/g" networking/nftables/spkgbuild
+changelog "networking/nftables/spkgbuild" "Upgraded from version 1.0.7 to version 1.0.8"
+sed -i -e "s/version=116.0b4/version=116.0b5/g" nonfree/firefox/spkgbuild
+changelog "nonfree/firefox/spkgbuild" "Upgraded from version 116.0b4 to version 116.0b5"
+sed -i -e "s/version=1.28.2/version=1.28.3/g" python/python-boto3/spkgbuild
+changelog "python/python-boto3/spkgbuild" "Upgraded from version 1.28.2 to version 1.28.3"
+sed -i -e "s/version=1.31.2/version=1.31.3/g" python/python-botocore/spkgbuild
+changelog "python/python-botocore/spkgbuild" "Upgraded from version 1.31.2 to version 1.31.3"
+sed -i -e "s/version=6.15.3/version=6.15.4/g" python/python-cx-freeze/spkgbuild
+changelog "python/python-cx-freeze/spkgbuild" "Upgraded from version 6.15.3 to version 6.15.4"
+sed -i -e "s/version=1.24.0/version=1.25.0/g" python/python-hcloud/spkgbuild
+changelog "python/python-hcloud/spkgbuild" "Upgraded from version 1.24.0 to version 1.25.0"
+sed -i -e "s/version=6.81.1/version=6.81.2/g" python/python-hypothesis/spkgbuild
+changelog "python/python-hypothesis/spkgbuild" "Upgraded from version 6.81.1 to version 6.81.2"
+sed -i -e "s/version=1.4.2/version=1.5.1/g" python/python-pecan/spkgbuild
+changelog "python/python-pecan/spkgbuild" "Upgraded from version 1.4.2 to version 1.5.1"
+sed -i -e "s/version=23.1.2/version=23.2/g" python/python-pip/spkgbuild
+changelog "python/python-pip/spkgbuild" "Upgraded from version 23.1.2 to version 23.2"
+sed -i -e "s/version=2.0.2/version=2.0.3/g" python/python-pydantic/spkgbuild
+changelog "python/python-pydantic/spkgbuild" "Upgraded from version 2.0.2 to version 2.0.3"
+sed -i -e "s/version=2.2.0/version=2.3.1/g" python/python-pytest-enabler/spkgbuild
+changelog "python/python-pytest-enabler/spkgbuild" "Upgraded from version 2.2.0 to version 2.3.1"
+sed -i -e "s/version=5.0.2/version=5.0.3/g" python/python-sybil/spkgbuild
+changelog "python/python-sybil/spkgbuild" "Upgraded from version 5.0.2 to version 5.0.3"
+sed -i -e "s/version=20.23.1/version=20.24.0/g" python/python-virtualenv/spkgbuild
+changelog "python/python-virtualenv/spkgbuild" "Upgraded from version 20.23.1 to version 20.24.0"
+sed -i -e "s/version=3.16.1/version=3.16.2/g" python/python-zipp/spkgbuild
+changelog "python/python-zipp/spkgbuild" "Upgraded from version 3.16.1 to version 3.16.2"
+sed -i -e "s/version=2.4.16/version=2.4.17/g" ruby-gems/ruby-bundler/spkgbuild
+changelog "ruby-gems/ruby-bundler/spkgbuild" "Upgraded from version 2.4.16 to version 2.4.17"
+sed -i -e "s/version=3.13.11/version=3.13.13/g" server/blacklist-hosts/spkgbuild
+changelog "server/blacklist-hosts/spkgbuild" "Upgraded from version 3.13.11 to version 3.13.13"
