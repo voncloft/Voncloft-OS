212ea0eee23a9861eb299ad2c0aaf8a59e84b739
Voncloft <voncloft@gmail.com>
2023-07-20 16:54:27 -0400

    Minor Updates

---
+++ logs/old-test-updates/2023/Jul/applied_updates-07-20-23.sh
@@ -0,0 +1,36 @@
+sed -i -e "s/version=0.115.3/version=0.115.4/g" core/hugo/spkgbuild
+changelog "core/hugo/spkgbuild" "Upgraded from version 0.115.3 to version 0.115.4"
+sed -i -e "s/version=5.5.1/version=6.0.0/g" core/streamlink/spkgbuild
+changelog "core/streamlink/spkgbuild" "Upgraded from version 5.5.1 to version 6.0.0"
+sed -i -e "s/version=1.22.4/version=1.22.5/g" media/gst-libav/spkgbuild
+changelog "media/gst-libav/spkgbuild" "Upgraded from version 1.22.4 to version 1.22.5"
+sed -i -e "s/version=1.22.4/version=1.22.5/g" media/gst-plugins-bad/spkgbuild
+changelog "media/gst-plugins-bad/spkgbuild" "Upgraded from version 1.22.4 to version 1.22.5"
+sed -i -e "s/version=1.22.4/version=1.22.5/g" media/gst-plugins-base/spkgbuild
+changelog "media/gst-plugins-base/spkgbuild" "Upgraded from version 1.22.4 to version 1.22.5"
+sed -i -e "s/version=1.22.4/version=1.22.5/g" media/gst-plugins-good/spkgbuild
+changelog "media/gst-plugins-good/spkgbuild" "Upgraded from version 1.22.4 to version 1.22.5"
+sed -i -e "s/version=1.22.4/version=1.22.5/g" media/gst-plugins-ugly/spkgbuild
+changelog "media/gst-plugins-ugly/spkgbuild" "Upgraded from version 1.22.4 to version 1.22.5"
+sed -i -e "s/version=1.22.4/version=1.22.5/g" media/gstreamer/spkgbuild
+changelog "media/gstreamer/spkgbuild" "Upgraded from version 1.22.4 to version 1.22.5"
+sed -i -e "s/version=7.5.4/version=7.5.5/g" nonfree/libreoffice/spkgbuild
+changelog "nonfree/libreoffice/spkgbuild" "Upgraded from version 7.5.4 to version 7.5.5"
+sed -i -e "s/version=116.0b4/version=116.0b5/g" nonfree/thunderbird/spkgbuild
+changelog "nonfree/thunderbird/spkgbuild" "Upgraded from version 116.0b4 to version 116.0b5"
+sed -i -e "s/version=1.28.5/version=1.28.7/g" python/python-boto3/spkgbuild
+changelog "python/python-boto3/spkgbuild" "Upgraded from version 1.28.5 to version 1.28.7"
+sed -i -e "s/version=1.31.5/version=1.31.7/g" python/python-botocore/spkgbuild
+changelog "python/python-botocore/spkgbuild" "Upgraded from version 1.31.5 to version 1.31.7"
+sed -i -e "s/version=19.1.0/version=19.2.0/g" python/python-faker/spkgbuild
+changelog "python/python-faker/spkgbuild" "Upgraded from version 19.1.0 to version 19.2.0"
+sed -i -e "s/version=6.81.2/version=6.82.0/g" python/python-hypothesis/spkgbuild
+changelog "python/python-hypothesis/spkgbuild" "Upgraded from version 6.81.2 to version 6.82.0"
+sed -i -e "s/version=1.14.8/version=1.14.9/g" python/python-jsbeautifier/spkgbuild
+changelog "python/python-jsbeautifier/spkgbuild" "Upgraded from version 1.14.8 to version 1.14.9"
+sed -i -e "s/version=1.1.6/version=1.1.7/g" python/python-lark-parser/spkgbuild
+changelog "python/python-lark-parser/spkgbuild" "Upgraded from version 1.1.6 to version 1.1.7"
+sed -i -e "s/version=20.24.0/version=20.24.1/g" python/python-virtualenv/spkgbuild
+changelog "python/python-virtualenv/spkgbuild" "Upgraded from version 20.24.0 to version 20.24.1"
+sed -i -e "s/version=1.32.4.7195/version=1.32.5.7328/g" server/plex-media-server/spkgbuild
+changelog "server/plex-media-server/spkgbuild" "Upgraded from version 1.32.4.7195 to version 1.32.5.7328"
