109590c3e5274c20d766d8d17bdb5b583ccc1b08
Voncloft <voncloft@gmail.com>
2023-07-22 23:35:44 -0400

    Minor Updates

---
+++ logs/old-test-updates/2023/Jul/applied_updates-07-22-23.sh
@@ -0,0 +1,34 @@
+sed -i -e "s/version=3.4.0/version=3.4.1/g" core/icewm/spkgbuild
+changelog "core/icewm/spkgbuild" "Upgraded from version 3.4.0 to version 3.4.1"
+sed -i -e "s/version=0.3.74/version=0.3.75/g" core/pipewire/spkgbuild
+changelog "core/pipewire/spkgbuild" "Upgraded from version 0.3.74 to version 0.3.75"
+sed -i -e "s/version=2.40/version=2.40.1/g" extra/discordchatexporter/spkgbuild
+changelog "extra/discordchatexporter/spkgbuild" "Upgraded from version 2.40 to version 2.40.1"
+sed -i -e "s/version=5.5.17/version=5.5.18/g" networking/whois/spkgbuild
+changelog "networking/whois/spkgbuild" "Upgraded from version 5.5.17 to version 5.5.18"
+sed -i -e "s/version=116.0b7/version=116.0b8/g" nonfree/firefox/spkgbuild
+changelog "nonfree/firefox/spkgbuild" "Upgraded from version 116.0b7 to version 116.0b8"
+sed -i -e "s/version=3.1.5/version=3.1.6/g" nonfree/pandoc-bin/spkgbuild
+changelog "nonfree/pandoc-bin/spkgbuild" "Upgraded from version 3.1.5 to version 3.1.6"
+sed -i -e "s/version=0.6.1/version=0.6.2/g" python/python-aioconsole/spkgbuild
+changelog "python/python-aioconsole/spkgbuild" "Upgraded from version 0.6.1 to version 0.6.2"
+sed -i -e "s/version=1.28.7/version=1.28.9/g" python/python-boto3/spkgbuild
+changelog "python/python-boto3/spkgbuild" "Upgraded from version 1.28.7 to version 1.28.9"
+sed -i -e "s/version=1.31.7/version=1.31.9/g" python/python-botocore/spkgbuild
+changelog "python/python-botocore/spkgbuild" "Upgraded from version 1.31.7 to version 1.31.9"
+sed -i -e "s/version=2023.7.0/version=2023.7.1/g" python/python-distributed/spkgbuild
+changelog "python/python-distributed/spkgbuild" "Upgraded from version 2023.7.0 to version 2023.7.1"
+sed -i -e "s/version=4.41.0/version=4.41.1/g" python/python-fonttools/spkgbuild
+changelog "python/python-fonttools/spkgbuild" "Upgraded from version 4.41.0 to version 4.41.1"
+sed -i -e "s/version=2.5.25/version=2.5.26/g" python/python-identify/spkgbuild
+changelog "python/python-identify/spkgbuild" "Upgraded from version 2.5.25 to version 2.5.26"
+sed -i -e "s/version=8.13.16/version=8.13.17/g" python/python-phonenumbers/spkgbuild
+changelog "python/python-phonenumbers/spkgbuild" "Upgraded from version 8.13.16 to version 8.13.17"
+sed -i -e "s/version=23.2/version=23.2.1/g" python/python-pip/spkgbuild
+changelog "python/python-pip/spkgbuild" "Upgraded from version 23.2 to version 23.2.1"
+sed -i -e "s/version=0.40.0/version=0.41.0/g" python/python-wheel/spkgbuild
+changelog "python/python-wheel/spkgbuild" "Upgraded from version 0.40.0 to version 0.41.0"
+sed -i -e "s/version=3.13.13/version=3.13.14/g" server/blacklist-hosts/spkgbuild
+changelog "server/blacklist-hosts/spkgbuild" "Upgraded from version 3.13.13 to version 3.13.14"
+sed -i -e "s/version=2023.7.2/version=2023.7.3/g" server/homeassistant/spkgbuild
+changelog "server/homeassistant/spkgbuild" "Upgraded from version 2023.7.2 to version 2023.7.3"
