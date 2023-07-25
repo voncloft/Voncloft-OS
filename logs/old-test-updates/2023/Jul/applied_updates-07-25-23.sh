ef890d7be7e3cb5fdbe3a4316044dab5dbd83831
Voncloft <voncloft@gmail.com>
2023-07-25 10:09:55 -0400

    Minor Updates

---
+++ logs/old-test-updates/2023/Jul/applied_updates-07-25-23.sh
@@ -0,0 +1,26 @@
+sed -i -e "s/version=6.4.4/version=6.4.5/g" core/linux-api-headers/spkgbuild
+changelog "core/linux-api-headers/spkgbuild" "Upgraded from version 6.4.4 to version 6.4.5"
+sed -i -e "s/version=6.4.4/version=6.4.5/g" core/linux-desktop/spkgbuild
+changelog "core/linux-desktop/spkgbuild" "Upgraded from version 6.4.4 to version 6.4.5"
+sed -i -e "s/version=6.4.4/version=6.4.5/g" core/linux-router/spkgbuild
+changelog "core/linux-router/spkgbuild" "Upgraded from version 6.4.4 to version 6.4.5"
+sed -i -e "s/version=0.35.1/version=0.36.0/g" core/mpv/spkgbuild
+changelog "core/mpv/spkgbuild" "Upgraded from version 0.35.1 to version 0.36.0"
+sed -i -e "s/version=0.9.0/version=0.10.0/g" kf5/kirigami-addons/spkgbuild
+changelog "kf5/kirigami-addons/spkgbuild" "Upgraded from version 0.9.0 to version 0.10.0"
+sed -i -e "s/version=1.6.0/version=1.6.1/g" python/python-cairocffi/spkgbuild
+changelog "python/python-cairocffi/spkgbuild" "Upgraded from version 1.6.0 to version 1.6.1"
+sed -i -e "s/version=4.2.0/version=5.0.0/g" python/python-keyrings-alt/spkgbuild
+changelog "python/python-keyrings-alt/spkgbuild" "Upgraded from version 4.2.0 to version 5.0.0"
+sed -i -e "s/version=2023.7.11/version=2023.7.23/g" python/python-pipenv/spkgbuild
+changelog "python/python-pipenv/spkgbuild" "Upgraded from version 2023.7.11 to version 2023.7.23"
+sed -i -e "s/version=0.30.0/version=0.31.0/g" python/python-starlette/spkgbuild
+changelog "python/python-starlette/spkgbuild" "Upgraded from version 0.30.0 to version 0.31.0"
+sed -i -e "s/version=20.24.1/version=20.24.2/g" python/python-virtualenv/spkgbuild
+changelog "python/python-virtualenv/spkgbuild" "Upgraded from version 20.24.1 to version 20.24.2"
+sed -i -e "s/version=0.71.0/version=0.71.4/g" python/python-zeroconf/spkgbuild
+changelog "python/python-zeroconf/spkgbuild" "Upgraded from version 0.71.0 to version 0.71.4"
+sed -i -e "s/version=3.13.14/version=3.13.15/g" server/blacklist-hosts/spkgbuild
+changelog "server/blacklist-hosts/spkgbuild" "Upgraded from version 3.13.14 to version 3.13.15"
+sed -i -e "s/version=1.2.7/version=1.2.8/g" xfce/xfce4-cpugraph-plugin/spkgbuild
+changelog "xfce/xfce4-cpugraph-plugin/spkgbuild" "Upgraded from version 1.2.7 to version 1.2.8"
