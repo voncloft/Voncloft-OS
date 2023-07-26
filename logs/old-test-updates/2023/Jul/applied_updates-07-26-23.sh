92828ae2221dc6dde4e2f7e46e8a6b67e3a20ec2
Voncloft <voncloft@gmail.com>
2023-07-26 09:28:56 -0400

    Minor Updates

---
+++ logs/old-test-updates/2023/Jul/applied_updates-07-26-23.sh
@@ -0,0 +1,28 @@
+sed -i -e "s/version=6.4.5/version=6.4.6/g" core/linux-api-headers/spkgbuild
+changelog "core/linux-api-headers/spkgbuild" "Upgraded from version 6.4.5 to version 6.4.6"
+sed -i -e "s/version=6.4.5/version=6.4.6/g" core/linux-desktop/spkgbuild
+changelog "core/linux-desktop/spkgbuild" "Upgraded from version 6.4.5 to version 6.4.6"
+sed -i -e "s/version=6.4.5/version=6.4.6/g" core/linux-router/spkgbuild
+changelog "core/linux-router/spkgbuild" "Upgraded from version 6.4.5 to version 6.4.6"
+sed -i -e "s/version=3.7.10/version=3.7.11/g" extra/flexget/spkgbuild
+changelog "extra/flexget/spkgbuild" "Upgraded from version 3.7.10 to version 3.7.11"
+sed -i -e "s/version=116.0b5/version=116.0b6/g" nonfree/thunderbird/spkgbuild
+changelog "nonfree/thunderbird/spkgbuild" "Upgraded from version 116.0b5 to version 116.0b6"
+sed -i -e "s/version=1.28.9/version=1.28.10/g" python/python-boto3/spkgbuild
+changelog "python/python-boto3/spkgbuild" "Upgraded from version 1.28.9 to version 1.28.10"
+sed -i -e "s/version=1.31.9/version=1.31.10/g" python/python-botocore/spkgbuild
+changelog "python/python-botocore/spkgbuild" "Upgraded from version 1.31.9 to version 1.31.10"
+sed -i -e "s/version=2.94.0/version=2.95.0/g" python/python-google-api-python-client/spkgbuild
+changelog "python/python-google-api-python-client/spkgbuild" "Upgraded from version 2.94.0 to version 2.95.0"
+sed -i -e "s/version=6.24.0/version=6.25.0/g" python/python-ipykernel/spkgbuild
+changelog "python/python-ipykernel/spkgbuild" "Upgraded from version 6.24.0 to version 6.25.0"
+sed -i -e "s/version=2.4.9/version=2.4.10/g" python/python-markdown2/spkgbuild
+changelog "python/python-markdown2/spkgbuild" "Upgraded from version 2.4.9 to version 2.4.10"
+sed -i -e "s/version=2.0.3/version=2.1.0/g" python/python-pydantic/spkgbuild
+changelog "python/python-pydantic/spkgbuild" "Upgraded from version 2.0.3 to version 2.1.0"
+sed -i -e "s/version=0.23.1/version=0.23.2/g" python/python-responses/spkgbuild
+changelog "python/python-responses/spkgbuild" "Upgraded from version 0.23.1 to version 0.23.2"
+sed -i -e "s/version=3.0.1/version=3.1.0/g" ruby-gems/ruby-optimist/spkgbuild
+changelog "ruby-gems/ruby-optimist/spkgbuild" "Upgraded from version 3.0.1 to version 3.1.0"
+sed -i -e "s/version=1.32.5.7328/version=1.32.5.7349/g" server/plex-media-server/spkgbuild
+changelog "server/plex-media-server/spkgbuild" "Upgraded from version 1.32.5.7328 to version 1.32.5.7349"
