2bc66d759c6df72c36e789fca2c488af52105af2
Voncloft <voncloft@gmail.com>
2023-07-31 13:14:06 -0400

    Minor Updates

---
+++ logs/old-test-updates/2023/Jul/applied_updates-07-31-23.sh
@@ -0,0 +1,36 @@
+sed -i -e "s/version=2.40/version=2.41/g" core/binutils/spkgbuild
+changelog "core/binutils/spkgbuild" "Upgraded from version 2.40 to version 2.41"
+sed -i -e "s/version=28.2/version=29.1/g" core/emacs/spkgbuild
+changelog "core/emacs/spkgbuild" "Upgraded from version 28.2 to version 29.1"
+sed -i -e "s/version=6.2.1/version=6.3.0/g" core/gmp/spkgbuild
+changelog "core/gmp/spkgbuild" "Upgraded from version 6.2.1 to version 6.3.0"
+sed -i -e "s/version=2.40.2/version=2.40.3/g" extra/discordchatexporter/spkgbuild
+changelog "extra/discordchatexporter/spkgbuild" "Upgraded from version 2.40.2 to version 2.40.3"
+sed -i -e "s/version=3.7.11/version=3.8.0/g" extra/flexget/spkgbuild
+changelog "extra/flexget/spkgbuild" "Upgraded from version 3.7.11 to version 3.8.0"
+sed -i -e "s/version=3.7.0/version=3.7.1/g" libs/libarchive/spkgbuild
+changelog "libs/libarchive/spkgbuild" "Upgraded from version 3.7.0 to version 3.7.1"
+sed -i -e "s/version=1.71.0/version=1.72.0/g" python/python-aws-sam-translator/spkgbuild
+changelog "python/python-aws-sam-translator/spkgbuild" "Upgraded from version 1.71.0 to version 1.72.0"
+sed -i -e "s/version=1.28.14/version=1.28.15/g" python/python-boto3/spkgbuild
+changelog "python/python-boto3/spkgbuild" "Upgraded from version 1.28.14 to version 1.28.15"
+sed -i -e "s/version=1.31.14/version=1.31.15/g" python/python-botocore/spkgbuild
+changelog "python/python-botocore/spkgbuild" "Upgraded from version 1.31.14 to version 1.31.15"
+sed -i -e "s/version=4.0.0/version=4.1.0/g" python/python-django-classy-tags/spkgbuild
+changelog "python/python-django-classy-tags/spkgbuild" "Upgraded from version 4.0.0 to version 4.1.0"
+sed -i -e "s/version=3.4.0/version=3.5.0/g" python/python-mypy-protobuf/spkgbuild
+changelog "python/python-mypy-protobuf/spkgbuild" "Upgraded from version 3.4.0 to version 3.5.0"
+sed -i -e "s/version=3.3.0/version=3.3.1/g" python/python-paramiko/spkgbuild
+changelog "python/python-paramiko/spkgbuild" "Upgraded from version 3.3.0 to version 3.3.1"
+sed -i -e "s/version=2.10.0/version=2.11.0/g" python/python-pycodestyle/spkgbuild
+changelog "python/python-pycodestyle/spkgbuild" "Upgraded from version 2.10.0 to version 2.11.0"
+sed -i -e "s/version=3.1.0/version=3.1.1/g" python/python-pyparsing/spkgbuild
+changelog "python/python-pyparsing/spkgbuild" "Upgraded from version 3.1.0 to version 3.1.1"
+sed -i -e "s/version=2.10.0/version=2.10.1/g" python/python-pytest-checkdocs/spkgbuild
+changelog "python/python-pytest-checkdocs/spkgbuild" "Upgraded from version 2.10.0 to version 2.10.1"
+sed -i -e "s/version=13.4.2/version=13.5.0/g" python/python-rich/spkgbuild
+changelog "python/python-rich/spkgbuild" "Upgraded from version 13.4.2 to version 13.5.0"
+sed -i -e "s/version=4.0.1/version=4.1.0/g" python/python-typeguard/spkgbuild
+changelog "python/python-typeguard/spkgbuild" "Upgraded from version 4.0.1 to version 4.1.0"
+sed -i -e "s/version=3.2.0/version=3.3.0/g" python/python-xxhash/spkgbuild
+changelog "python/python-xxhash/spkgbuild" "Upgraded from version 3.2.0 to version 3.3.0"
