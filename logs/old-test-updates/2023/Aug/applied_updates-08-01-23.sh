5410782e34f795a397a7f3e25910ba7c7e93d507
Voncloft <voncloft@gmail.com>
2023-08-01 07:32:28 -0400

    adding anki

---
+++ logs/old-test-updates/2023/Aug/applied_updates-08-01-23.sh
@@ -0,0 +1,16 @@
+sed -i -e "s/version=0.115.4/version=0.116.0/g" core/hugo/spkgbuild
+changelog "core/hugo/spkgbuild" "Upgraded from version 0.115.4 to version 0.116.0"
+sed -i -e "s/version=1.6/version=1.7/g" core/jq/spkgbuild
+changelog "core/jq/spkgbuild" "Upgraded from version 1.6 to version 1.7"
+sed -i -e "s/version=1.5.3/version=1.6.0/g" python/python-logbook/spkgbuild
+changelog "python/python-logbook/spkgbuild" "Upgraded from version 1.5.3 to version 1.6.0"
+sed -i -e "s/version=0.28.1/version=0.28.2/g" python/python-meilisearch/spkgbuild
+changelog "python/python-meilisearch/spkgbuild" "Upgraded from version 0.28.1 to version 0.28.2"
+sed -i -e "s/version=7.10.0/version=7.11.0/g" python/python-qiniu/spkgbuild
+changelog "python/python-qiniu/spkgbuild" "Upgraded from version 7.10.0 to version 7.11.0"
+sed -i -e "s/version=13.5.0/version=13.5.1/g" python/python-rich/spkgbuild
+changelog "python/python-rich/spkgbuild" "Upgraded from version 13.5.0 to version 13.5.1"
+sed -i -e "s/version=5.0.0/version=5.1.0/g" python/python-vcrpy/spkgbuild
+changelog "python/python-vcrpy/spkgbuild" "Upgraded from version 5.0.0 to version 5.1.0"
+sed -i -e "s/version=1.6.3/version=1.6.4/g" xfce/xfce4-clipman-plugin/spkgbuild
+changelog "xfce/xfce4-clipman-plugin/spkgbuild" "Upgraded from version 1.6.3 to version 1.6.4"
