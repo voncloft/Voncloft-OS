f347b84cd4cab55a04d99ba7782887e1b5b963af
Voncloft <voncloft@gmail.com>
2023-08-03 10:49:25 -0400

    Minor Updates

---
+++ logs/old-test-updates/2023/Aug/applied_updates-08-03-23.sh
@@ -0,0 +1,34 @@
+sed -i -e "s/version=1.13.0/version=1.14.0/g" core/gtest/spkgbuild
+changelog "core/gtest/spkgbuild" "Upgraded from version 1.13.0 to version 1.14.0"
+sed -i -e "s/version=6.0.0/version=6.0.1/g" core/streamlink/spkgbuild
+changelog "core/streamlink/spkgbuild" "Upgraded from version 6.0.0 to version 6.0.1"
+sed -i -e "s/version=5.4.3/version=5.4.4/g" core/xz/spkgbuild
+changelog "core/xz/spkgbuild" "Upgraded from version 5.4.3 to version 5.4.4"
+sed -i -e "s/version=23.07.0/version=23.08.0/g" libs/poppler/spkgbuild
+changelog "libs/poppler/spkgbuild" "Upgraded from version 23.07.0 to version 23.08.0"
+sed -i -e "s/version=2.28.1/version=2.28.2/g" libs/sdl2/spkgbuild
+changelog "libs/sdl2/spkgbuild" "Upgraded from version 2.28.1 to version 2.28.2"
+sed -i -e "s/version=23.1.99.901/version=23.1.99.902/g" libs/xwayland/spkgbuild
+changelog "libs/xwayland/spkgbuild" "Upgraded from version 23.1.99.901 to version 23.1.99.902"
+sed -i -e "s/version=117.0b1/version=117.0b2/g" nonfree/firefox/spkgbuild
+changelog "nonfree/firefox/spkgbuild" "Upgraded from version 117.0b1 to version 117.0b2"
+sed -i -e "s/version=1.3.2/version=1.4.0/g" plasma/ktextaddons/spkgbuild
+changelog "plasma/ktextaddons/spkgbuild" "Upgraded from version 1.3.2 to version 1.4.0"
+sed -i -e "s/version=1.28.16/version=1.28.17/g" python/python-boto3/spkgbuild
+changelog "python/python-boto3/spkgbuild" "Upgraded from version 1.28.16 to version 1.28.17"
+sed -i -e "s/version=4.41.1/version=4.42.0/g" python/python-fonttools/spkgbuild
+changelog "python/python-fonttools/spkgbuild" "Upgraded from version 4.41.1 to version 4.42.0"
+sed -i -e "s/version=4.18.4/version=4.18.6/g" python/python-jsonschema/spkgbuild
+changelog "python/python-jsonschema/spkgbuild" "Upgraded from version 4.18.4 to version 4.18.6"
+sed -i -e "s/version=0.22.0/version=0.23.1/g" python/python-pytest-httpx/spkgbuild
+changelog "python/python-pytest-httpx/spkgbuild" "Upgraded from version 0.22.0 to version 0.23.1"
+sed -i -e "s/version=6.3.2/version=7.0.0/g" python/python-rdflib/spkgbuild
+changelog "python/python-rdflib/spkgbuild" "Upgraded from version 6.3.2 to version 7.0.0"
+sed -i -e "s/version=3.22.5/version=3.23.0/g" python/python-trimesh/spkgbuild
+changelog "python/python-trimesh/spkgbuild" "Upgraded from version 3.22.5 to version 3.23.0"
+sed -i -e "s/version=0.71.4/version=0.72.0/g" python/python-zeroconf/spkgbuild
+changelog "python/python-zeroconf/spkgbuild" "Upgraded from version 0.71.4 to version 0.72.0"
+sed -i -e "s/version=2.4.17/version=2.4.18/g" ruby-gems/ruby-bundler/spkgbuild
+changelog "ruby-gems/ruby-bundler/spkgbuild" "Upgraded from version 2.4.17 to version 2.4.18"
+sed -i -e "s/version=3.13.16/version=3.13.17/g" server/blacklist-hosts/spkgbuild
+changelog "server/blacklist-hosts/spkgbuild" "Upgraded from version 3.13.16 to version 3.13.17"
