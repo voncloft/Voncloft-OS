8a30264b79b52d2aa36c5a32ced67614b494edf8
Voncloft <voncloft@gmail.com>
2023-08-06 14:07:55 -0400

    Minor Updates

---
+++ logs/old-test-updates/2023/Aug/applied_updates-08-06-23.sh
@@ -0,0 +1,62 @@
+sed -i -e "s/version=1.9.5/version=2.0.0/g" compilers/pkgconf/spkgbuild
+changelog "compilers/pkgconf/spkgbuild" "Upgraded from version 1.9.5 to version 2.0.0"
+sed -i -e "s/version=1.71.0/version=1.71.1/g" compilers/rust/spkgbuild
+changelog "compilers/rust/spkgbuild" "Upgraded from version 1.71.0 to version 1.71.1"
+sed -i -e "s/version=3.7.9/version=3.7.10/g" core/gnutls/spkgbuild
+changelog "core/gnutls/spkgbuild" "Upgraded from version 3.7.9 to version 3.7.10"
+sed -i -e "s/version=6.4.7/version=6.4.8/g" core/linux-api-headers/spkgbuild
+changelog "core/linux-api-headers/spkgbuild" "Upgraded from version 6.4.7 to version 6.4.8"
+sed -i -e "s/version=6.4.7/version=6.4.8/g" core/linux-desktop/spkgbuild
+changelog "core/linux-desktop/spkgbuild" "Upgraded from version 6.4.7 to version 6.4.8"
+sed -i -e "s/version=6.4.7/version=6.4.8/g" core/linux-router/spkgbuild
+changelog "core/linux-router/spkgbuild" "Upgraded from version 6.4.7 to version 6.4.8"
+sed -i -e "s/version=6.05/version=6.05.01/g" core/man-pages/spkgbuild
+changelog "core/man-pages/spkgbuild" "Upgraded from version 6.05 to version 6.05.01"
+sed -i -e "s/version=0.3.76/version=0.3.77/g" core/pipewire/spkgbuild
+changelog "core/pipewire/spkgbuild" "Upgraded from version 0.3.76 to version 0.3.77"
+sed -i -e "s/version=3.8.0/version=3.8.2/g" extra/flexget/spkgbuild
+changelog "extra/flexget/spkgbuild" "Upgraded from version 3.8.0 to version 3.8.2"
+sed -i -e "s/version=2.2.2/version=2.3/g" extra/ncdu/spkgbuild
+changelog "extra/ncdu/spkgbuild" "Upgraded from version 2.2.2 to version 2.3"
+sed -i -e "s/version=6.4.0/version=6.4.2/g" fonts/awesome-font/spkgbuild
+changelog "fonts/awesome-font/spkgbuild" "Upgraded from version 6.4.0 to version 6.4.2"
+sed -i -e "s/version=3.1.9/version=3.1.10/g" libs/openexr/spkgbuild
+changelog "libs/openexr/spkgbuild" "Upgraded from version 3.1.9 to version 3.1.10"
+sed -i -e "s/version=117.0b2/version=117.0b3/g" nonfree/firefox/spkgbuild
+changelog "nonfree/firefox/spkgbuild" "Upgraded from version 117.0b2 to version 117.0b3"
+sed -i -e "s/version=116.0b7/version=117.0b1/g" nonfree/thunderbird/spkgbuild
+changelog "nonfree/thunderbird/spkgbuild" "Upgraded from version 116.0b7 to version 117.0b1"
+sed -i -e "s/version=1.28.17/version=1.28.20/g" python/python-boto3/spkgbuild
+changelog "python/python-boto3/spkgbuild" "Upgraded from version 1.28.17 to version 1.28.20"
+sed -i -e "s/version=1.31.17/version=1.31.20/g" python/python-botocore/spkgbuild
+changelog "python/python-botocore/spkgbuild" "Upgraded from version 1.31.17 to version 1.31.20"
+sed -i -e "s/version=2.7.0/version=2.7.1/g" python/python-cairosvg/spkgbuild
+changelog "python/python-cairosvg/spkgbuild" "Upgraded from version 2.7.0 to version 2.7.1"
+sed -i -e "s/version=1.2.2/version=1.2.4/g" python/python-click-default-group/spkgbuild
+changelog "python/python-click-default-group/spkgbuild" "Upgraded from version 1.2.2 to version 1.2.4"
+sed -i -e "s/version=2023.7.1/version=2023.8.0/g" python/python-distributed/spkgbuild
+changelog "python/python-distributed/spkgbuild" "Upgraded from version 2023.7.1 to version 2023.8.0"
+sed -i -e "s/version=0.100.1/version=0.101.0/g" python/python-fastapi/spkgbuild
+changelog "python/python-fastapi/spkgbuild" "Upgraded from version 0.100.1 to version 0.101.0"
+sed -i -e "s/version=0.4.2/version=0.4.3/g" python/python-jsonschema-objects/spkgbuild
+changelog "python/python-jsonschema-objects/spkgbuild" "Upgraded from version 0.4.2 to version 0.4.3"
+sed -i -e "s/version=1.3.1/version=1.4.0/g" python/python-openstacksdk/spkgbuild
+changelog "python/python-openstacksdk/spkgbuild" "Upgraded from version 1.3.1 to version 1.4.0"
+sed -i -e "s/version=8.13.17/version=8.13.18/g" python/python-phonenumbers/spkgbuild
+changelog "python/python-phonenumbers/spkgbuild" "Upgraded from version 8.13.17 to version 8.13.18"
+sed -i -e "s/version=0.0.15/version=0.0.16/g" python/python-pyadi-iio/spkgbuild
+changelog "python/python-pyadi-iio/spkgbuild" "Upgraded from version 0.0.15 to version 0.0.16"
+sed -i -e "s/version=1.59.0/version=1.59.1/g" python/python-pygithub/spkgbuild
+changelog "python/python-pygithub/spkgbuild" "Upgraded from version 1.59.0 to version 1.59.1"
+sed -i -e "s/version=1.2.2/version=1.3.0/g" python/python-sphinx_rtd_theme/spkgbuild
+changelog "python/python-sphinx_rtd_theme/spkgbuild" "Upgraded from version 1.2.2 to version 1.3.0"
+sed -i -e "s/version=2.1.1/version=2.1.2/g" python/python-w3lib/spkgbuild
+changelog "python/python-w3lib/spkgbuild" "Upgraded from version 2.1.1 to version 2.1.2"
+sed -i -e "s/version=0.41.0/version=0.41.1/g" python/python-wheel/spkgbuild
+changelog "python/python-wheel/spkgbuild" "Upgraded from version 0.41.0 to version 0.41.1"
+sed -i -e "s/version=0.72.0/version=0.74.0/g" python/python-zeroconf/spkgbuild
+changelog "python/python-zeroconf/spkgbuild" "Upgraded from version 0.72.0 to version 0.74.0"
+sed -i -e "s/version=3.13.17/version=3.13.18/g" server/blacklist-hosts/spkgbuild
+changelog "server/blacklist-hosts/spkgbuild" "Upgraded from version 3.13.17 to version 3.13.18"
+sed -i -e "s/version=2023.8.0/version=2023.8.1/g" server/homeassistant/spkgbuild
+changelog "server/homeassistant/spkgbuild" "Upgraded from version 2023.8.0 to version 2023.8.1"
