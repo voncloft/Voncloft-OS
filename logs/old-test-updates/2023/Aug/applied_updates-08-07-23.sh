eb10aab0e85fe6b69f3f7723e61bf6d84ba0e7a6
Voncloft <voncloft@gmail.com>
2023-08-07 05:31:12 -0400

    Minor Updates

---
+++ logs/old-test-updates/2023/Aug/applied_updates-08-07-23.sh
@@ -0,0 +1,10 @@
+sed -i -e "s/version=1.4.0/version=1.4.1/g" plasma/ktextaddons/spkgbuild
+changelog "plasma/ktextaddons/spkgbuild" "Upgraded from version 1.4.0 to version 1.4.1"
+sed -i -e "s/version=6.82.0/version=6.82.1/g" python/python-hypothesis/spkgbuild
+changelog "python/python-hypothesis/spkgbuild" "Upgraded from version 6.82.0 to version 6.82.1"
+sed -i -e "s/version=3.9.2/version=3.9.3/g" python/python-orjson/spkgbuild
+changelog "python/python-orjson/spkgbuild" "Upgraded from version 3.9.2 to version 3.9.3"
+sed -i -e "s/version=2.3.0/version=2.4.0/g" python/python-yaspin/spkgbuild
+changelog "python/python-yaspin/spkgbuild" "Upgraded from version 2.3.0 to version 2.4.0"
+sed -i -e "s/version=3.13.18/version=3.13.19/g" server/blacklist-hosts/spkgbuild
+changelog "server/blacklist-hosts/spkgbuild" "Upgraded from version 3.13.18 to version 3.13.19"
