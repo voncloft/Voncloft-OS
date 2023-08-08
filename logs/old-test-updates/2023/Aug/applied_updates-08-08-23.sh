ebe32a4b68ab6e4060a8c12fe5a6c23517eb6192
Voncloft <voncloft@gmail.com>
2023-08-08 08:15:58 -0400

    Minor Updates

---
+++ logs/old-test-updates/2023/Aug/applied_updates-08-08-23.sh
@@ -0,0 +1,30 @@
+sed -i -e "s/version=0.116.1/version=0.117.0/g" core/hugo/spkgbuild
+changelog "core/hugo/spkgbuild" "Upgraded from version 0.116.1 to version 0.117.0"
+sed -i -e "s/version=6.5/version=6.5.1/g" core/mercurial/spkgbuild
+changelog "core/mercurial/spkgbuild" "Upgraded from version 6.5 to version 6.5.1"
+sed -i -e "s/version=117.0b3/version=117.0b4/g" nonfree/firefox/spkgbuild
+changelog "nonfree/firefox/spkgbuild" "Upgraded from version 117.0b3 to version 117.0b4"
+sed -i -e "s/version=3.1.6/version=3.1.6.1/g" nonfree/pandoc-bin/spkgbuild
+changelog "nonfree/pandoc-bin/spkgbuild" "Upgraded from version 3.1.6 to version 3.1.6.1"
+sed -i -e "s/version=2.5.2/version=2.5.3/g" python/python-aiobotocore/spkgbuild
+changelog "python/python-aiobotocore/spkgbuild" "Upgraded from version 2.5.2 to version 2.5.3"
+sed -i -e "s/version=19.2.0/version=19.3.0/g" python/python-faker/spkgbuild
+changelog "python/python-faker/spkgbuild" "Upgraded from version 19.2.0 to version 19.3.0"
+sed -i -e "s/version=6.82.1/version=6.82.2/g" python/python-hypothesis/spkgbuild
+changelog "python/python-hypothesis/spkgbuild" "Upgraded from version 6.82.1 to version 6.82.2"
+sed -i -e "s/version=6.25.0/version=6.25.1/g" python/python-ipykernel/spkgbuild
+changelog "python/python-ipykernel/spkgbuild" "Upgraded from version 6.25.0 to version 6.25.1"
+sed -i -e "s/version=4.18.6/version=4.19.0/g" python/python-jsonschema/spkgbuild
+changelog "python/python-jsonschema/spkgbuild" "Upgraded from version 4.18.6 to version 4.19.0"
+sed -i -e "s/version=0.4.3/version=0.4.4/g" python/python-jsonschema-objects/spkgbuild
+changelog "python/python-jsonschema-objects/spkgbuild" "Upgraded from version 0.4.3 to version 0.4.4"
+sed -i -e "s/version=6.4.1/version=6.4.2/g" python/python-traits/spkgbuild
+changelog "python/python-traits/spkgbuild" "Upgraded from version 6.4.1 to version 6.4.2"
+sed -i -e "s/version=3.23.0/version=3.23.1/g" python/python-trimesh/spkgbuild
+changelog "python/python-trimesh/spkgbuild" "Upgraded from version 3.23.0 to version 3.23.1"
+sed -i -e "s/version=0.21.1/version=0.21.2/g" python/python-validators/spkgbuild
+changelog "python/python-validators/spkgbuild" "Upgraded from version 0.21.1 to version 0.21.2"
+sed -i -e "s/version=2.4.0/version=2.5.0/g" python/python-yaspin/spkgbuild
+changelog "python/python-yaspin/spkgbuild" "Upgraded from version 2.4.0 to version 2.5.0"
+sed -i -e "s/version=0.1.4/version=0.1.5/g" xorg/xdg-dbus-proxy/spkgbuild
+changelog "xorg/xdg-dbus-proxy/spkgbuild" "Upgraded from version 0.1.4 to version 0.1.5"
