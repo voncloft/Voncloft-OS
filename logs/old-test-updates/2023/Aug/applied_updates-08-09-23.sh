9443ea8ba79f96925564af0edb1ca44745374f40
Voncloft <voncloft@gmail.com>
2023-08-09 08:13:26 -0400

    Minor Updates

---
+++ logs/old-test-updates/2023/Aug/applied_updates-08-09-23.sh
@@ -0,0 +1,40 @@
+sed -i -e "s/version=1.2.0/version=1.2.1/g" compilers/meson/spkgbuild
+changelog "compilers/meson/spkgbuild" "Upgraded from version 1.2.0 to version 1.2.1"
+sed -i -e "s/version=1.2.5/version=1.2.6/g" core/consolekit2/spkgbuild
+changelog "core/consolekit2/spkgbuild" "Upgraded from version 1.2.5 to version 1.2.6"
+sed -i -e "s/version=3.15.1/version=3.16.1/g" core/fuse2/spkgbuild
+changelog "core/fuse2/spkgbuild" "Upgraded from version 3.15.1 to version 3.16.1"
+sed -i -e "s/version=3.15.1/version=3.16.1/g" core/fuse3/spkgbuild
+changelog "core/fuse3/spkgbuild" "Upgraded from version 3.15.1 to version 3.16.1"
+sed -i -e "s/version=6.4.8/version=6.4.9/g" core/linux-api-headers/spkgbuild
+changelog "core/linux-api-headers/spkgbuild" "Upgraded from version 6.4.8 to version 6.4.9"
+sed -i -e "s/version=6.4.8/version=6.4.9/g" core/linux-desktop/spkgbuild
+changelog "core/linux-desktop/spkgbuild" "Upgraded from version 6.4.8 to version 6.4.9"
+sed -i -e "s/version=6.4.8/version=6.4.9/g" core/linux-router/spkgbuild
+changelog "core/linux-router/spkgbuild" "Upgraded from version 6.4.8 to version 6.4.9"
+sed -i -e "s/version=535.86.05/version=535.98/g" nonfree/nvidia/spkgbuild
+changelog "nonfree/nvidia/spkgbuild" "Upgraded from version 535.86.05 to version 535.98"
+sed -i -e "s/version=535.86.05/version=535.98/g" nonfree/nvidia-32/spkgbuild
+changelog "nonfree/nvidia-32/spkgbuild" "Upgraded from version 535.86.05 to version 535.98"
+sed -i -e "s/version=535.86.05/version=535.98/g" nonfree/nvidia-modules/spkgbuild
+changelog "nonfree/nvidia-modules/spkgbuild" "Upgraded from version 535.86.05 to version 535.98"
+sed -i -e "s/version=117.0b1/version=117.0b2/g" nonfree/thunderbird/spkgbuild
+changelog "nonfree/thunderbird/spkgbuild" "Upgraded from version 117.0b1 to version 117.0b2"
+sed -i -e "s/version=2.5.3/version=2.5.4/g" python/python-aiobotocore/spkgbuild
+changelog "python/python-aiobotocore/spkgbuild" "Upgraded from version 2.5.3 to version 2.5.4"
+sed -i -e "s/version=1.28.20/version=1.28.22/g" python/python-boto3/spkgbuild
+changelog "python/python-boto3/spkgbuild" "Upgraded from version 1.28.20 to version 1.28.22"
+sed -i -e "s/version=1.31.20/version=1.31.22/g" python/python-botocore/spkgbuild
+changelog "python/python-botocore/spkgbuild" "Upgraded from version 1.31.20 to version 1.31.22"
+sed -i -e "s/version=2.95.0/version=2.96.0/g" python/python-google-api-python-client/spkgbuild
+changelog "python/python-google-api-python-client/spkgbuild" "Upgraded from version 2.95.0 to version 2.96.0"
+sed -i -e "s/version=1.26.0/version=1.27.1/g" python/python-hcloud/spkgbuild
+changelog "python/python-hcloud/spkgbuild" "Upgraded from version 1.26.0 to version 1.27.1"
+sed -i -e "s/version=6.82.2/version=6.82.3/g" python/python-hypothesis/spkgbuild
+changelog "python/python-hypothesis/spkgbuild" "Upgraded from version 6.82.2 to version 6.82.3"
+sed -i -e "s/version=3.9.3/version=3.9.4/g" python/python-orjson/spkgbuild
+changelog "python/python-orjson/spkgbuild" "Upgraded from version 3.9.3 to version 3.9.4"
+sed -i -e "s/version=4.65.0/version=4.65.1/g" python/python-tqdm/spkgbuild
+changelog "python/python-tqdm/spkgbuild" "Upgraded from version 4.65.0 to version 4.65.1"
+sed -i -e "s/version=3.13.19/version=3.13.20/g" server/blacklist-hosts/spkgbuild
+changelog "server/blacklist-hosts/spkgbuild" "Upgraded from version 3.13.19 to version 3.13.20"
