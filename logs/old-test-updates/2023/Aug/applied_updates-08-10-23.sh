b3092340bee23bb422624e52e546dc51ee6a3c17
Voncloft <voncloft@gmail.com>
2023-08-10 07:38:34 -0400

    Minor Updates

---
+++ logs/old-test-updates/2023/Aug/applied_updates-08-10-23.sh
@@ -0,0 +1,32 @@
+sed -i -e "s/version=1.19.2/version=1.19.3/g" core/conky/spkgbuild
+changelog "core/conky/spkgbuild" "Upgraded from version 1.19.2 to version 1.19.3"
+sed -i -e "s/version=4.0.2/version=4.0.3/g" core/weechat/spkgbuild
+changelog "core/weechat/spkgbuild" "Upgraded from version 4.0.2 to version 4.0.3"
+sed -i -e "s/version=3.8.2/version=3.8.3/g" extra/flexget/spkgbuild
+changelog "extra/flexget/spkgbuild" "Upgraded from version 3.8.2 to version 3.8.3"
+sed -i -e "s/version=23.4/version=24.0/g" extra/protobuf/spkgbuild
+changelog "extra/protobuf/spkgbuild" "Upgraded from version 23.4 to version 24.0"
+sed -i -e "s/version=1.45.0/version=1.45.1/g" networking/networkmanager/spkgbuild
+changelog "networking/networkmanager/spkgbuild" "Upgraded from version 1.45.0 to version 1.45.1"
+sed -i -e "s/version=117.0b4/version=117.0b5/g" nonfree/firefox/spkgbuild
+changelog "nonfree/firefox/spkgbuild" "Upgraded from version 117.0b4 to version 117.0b5"
+sed -i -e "s/version=23.1.0/version=23.2.0/g" python/python-aiofiles/spkgbuild
+changelog "python/python-aiofiles/spkgbuild" "Upgraded from version 23.1.0 to version 23.2.0"
+sed -i -e "s/version=1.28.22/version=1.28.23/g" python/python-boto3/spkgbuild
+changelog "python/python-boto3/spkgbuild" "Upgraded from version 1.28.22 to version 1.28.23"
+sed -i -e "s/version=1.31.22/version=1.31.23/g" python/python-botocore/spkgbuild
+changelog "python/python-botocore/spkgbuild" "Upgraded from version 1.31.22 to version 1.31.23"
+sed -i -e "s/version=1.3.4/version=1.3.5/g" python/python-cerberus/spkgbuild
+changelog "python/python-cerberus/spkgbuild" "Upgraded from version 1.3.4 to version 1.3.5"
+sed -i -e "s/version=1.3.1/version=1.3.2/g" python/python-cvxopt/spkgbuild
+changelog "python/python-cvxopt/spkgbuild" "Upgraded from version 1.3.1 to version 1.3.2"
+sed -i -e "s/version=1.27.1/version=1.27.2/g" python/python-hcloud/spkgbuild
+changelog "python/python-hcloud/spkgbuild" "Upgraded from version 1.27.1 to version 1.27.2"
+sed -i -e "s/version=1.3.1/version=1.3.2/g" python/python-joblib/spkgbuild
+changelog "python/python-joblib/spkgbuild" "Upgraded from version 1.3.1 to version 1.3.2"
+sed -i -e "s/version=0.2.1/version=0.3.0/g" python/python-protego/spkgbuild
+changelog "python/python-protego/spkgbuild" "Upgraded from version 0.2.1 to version 0.3.0"
+sed -i -e "s/version=3.10.1/version=3.10.2/g" python/python-sqlobject/spkgbuild
+changelog "python/python-sqlobject/spkgbuild" "Upgraded from version 3.10.1 to version 3.10.2"
+sed -i -e "s/version=4.65.1/version=4.66.0/g" python/python-tqdm/spkgbuild
+changelog "python/python-tqdm/spkgbuild" "Upgraded from version 4.65.1 to version 4.66.0"
