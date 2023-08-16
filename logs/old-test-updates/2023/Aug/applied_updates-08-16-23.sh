3ce704b18268b5c53d2e3e4065489049a27d4be6
Voncloft <voncloft@gmail.com>
2023-08-16 10:23:45 -0400

    Minor Updates

---
+++ logs/old-test-updates/2023/Aug/applied_updates-08-16-23.sh
@@ -0,0 +1,26 @@
+sed -i -e "s/version=1.19.3/version=1.19.4/g" core/conky/spkgbuild
+changelog "core/conky/spkgbuild" "Upgraded from version 1.19.3 to version 1.19.4"
+sed -i -e "s/version=7.0.12/version=7.2.0/g" core/redis/spkgbuild
+changelog "core/redis/spkgbuild" "Upgraded from version 7.0.12 to version 7.2.0"
+sed -i -e "s/version=3.8.6/version=3.8.7/g" extra/flexget/spkgbuild
+changelog "extra/flexget/spkgbuild" "Upgraded from version 3.8.6 to version 3.8.7"
+sed -i -e "s/version=2.6.5/version=2.6.6/g" networking/openvpn/spkgbuild
+changelog "networking/openvpn/spkgbuild" "Upgraded from version 2.6.5 to version 2.6.6"
+sed -i -e "s/version=117.0b3/version=117.0b4/g" nonfree/thunderbird/spkgbuild
+changelog "nonfree/thunderbird/spkgbuild" "Upgraded from version 117.0b3 to version 117.0b4"
+sed -i -e "s/version=23.5.0/version=23.8.0/g" python/python-awesomeversion/spkgbuild
+changelog "python/python-awesomeversion/spkgbuild" "Upgraded from version 23.5.0 to version 23.8.0"
+sed -i -e "s/version=1.28.25/version=1.28.26/g" python/python-boto3/spkgbuild
+changelog "python/python-boto3/spkgbuild" "Upgraded from version 1.28.25 to version 1.28.26"
+sed -i -e "s/version=1.31.25/version=1.31.26/g" python/python-botocore/spkgbuild
+changelog "python/python-botocore/spkgbuild" "Upgraded from version 1.31.25 to version 1.31.26"
+sed -i -e "s/version=2.96.0/version=2.97.0/g" python/python-google-api-python-client/spkgbuild
+changelog "python/python-google-api-python-client/spkgbuild" "Upgraded from version 2.96.0 to version 2.97.0"
+sed -i -e "s/version=3.13.0/version=3.15.0/g" python/python-pytest-randomly/spkgbuild
+changelog "python/python-pytest-randomly/spkgbuild" "Upgraded from version 3.13.0 to version 3.15.0"
+sed -i -e "s/version=68.0.0/version=68.1.0/g" python/python-setuptools/spkgbuild
+changelog "python/python-setuptools/spkgbuild" "Upgraded from version 68.0.0 to version 68.1.0"
+sed -i -e "s/version=0.78.0/version=0.79.0/g" python/python-zeroconf/spkgbuild
+changelog "python/python-zeroconf/spkgbuild" "Upgraded from version 0.78.0 to version 0.79.0"
+sed -i -e "s/version=1.25.1/version=1.25.2/g" server/nginx/spkgbuild
+changelog "server/nginx/spkgbuild" "Upgraded from version 1.25.1 to version 1.25.2"
