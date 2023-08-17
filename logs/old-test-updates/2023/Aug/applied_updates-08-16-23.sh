01e9766e34aaabaf0cc301bafbacde229567da37
Voncloft <voncloft@gmail.com>
2023-08-16 21:03:54 -0400

    Minor Updates

--- logs/old-test-updates/2023/Aug/applied_updates-08-16-23.sh
+++ logs/old-test-updates/2023/Aug/applied_updates-08-16-23.sh
@@ -24,3 +24,39 @@ sed -i -e "s/version=0.78.0/version=0.79.0/g" python/python-zeroconf/spkgbuild
 changelog "python/python-zeroconf/spkgbuild" "Upgraded from version 0.78.0 to version 0.79.0"
 sed -i -e "s/version=1.25.1/version=1.25.2/g" server/nginx/spkgbuild
 changelog "server/nginx/spkgbuild" "Upgraded from version 1.25.1 to version 1.25.2"
+sed -i -e "s/version=6.4.10/version=6.4.11/g" core/linux-api-headers/spkgbuild
+changelog "core/linux-api-headers/spkgbuild" "Upgraded from version 6.4.10 to version 6.4.11"
+sed -i -e "s/version=6.4.10/version=6.4.11/g" core/linux-desktop/spkgbuild
+changelog "core/linux-desktop/spkgbuild" "Upgraded from version 6.4.10 to version 6.4.11"
+sed -i -e "s/version=6.4.10/version=6.4.11/g" core/linux-router/spkgbuild
+changelog "core/linux-router/spkgbuild" "Upgraded from version 6.4.10 to version 6.4.11"
+sed -i -e "s/version=4.13/version=4.14.0/g" core/shadow/spkgbuild
+changelog "core/shadow/spkgbuild" "Upgraded from version 4.13 to version 4.14.0"
+sed -i -e "s/version=6.0.1/version=6.1.0/g" core/streamlink/spkgbuild
+changelog "core/streamlink/spkgbuild" "Upgraded from version 6.0.1 to version 6.1.0"
+sed -i -e "s/version=0.10.0/version=0.11.0/g" kf5/kirigami-addons/spkgbuild
+changelog "kf5/kirigami-addons/spkgbuild" "Upgraded from version 0.10.0 to version 0.11.0"
+sed -i -e "s/version=23.1.99.902/version=23.2.0/g" libs/xwayland/spkgbuild
+changelog "libs/xwayland/spkgbuild" "Upgraded from version 23.1.99.902 to version 23.2.0"
+sed -i -e "s/version=9.19.15/version=9.19.16/g" networking/bind/spkgbuild
+changelog "networking/bind/spkgbuild" "Upgraded from version 9.19.15 to version 9.19.16"
+sed -i -e "s/version=117.0b7/version=117.0b8/g" nonfree/firefox/spkgbuild
+changelog "nonfree/firefox/spkgbuild" "Upgraded from version 117.0b7 to version 117.0b8"
+sed -i -e "s/version=1.28.26/version=1.28.27/g" python/python-boto3/spkgbuild
+changelog "python/python-boto3/spkgbuild" "Upgraded from version 1.28.26 to version 1.28.27"
+sed -i -e "s/version=1.31.26/version=1.31.27/g" python/python-botocore/spkgbuild
+changelog "python/python-botocore/spkgbuild" "Upgraded from version 1.31.26 to version 1.31.27"
+sed -i -e "s/version=3.9.4/version=3.9.5/g" python/python-orjson/spkgbuild
+changelog "python/python-orjson/spkgbuild" "Upgraded from version 3.9.4 to version 3.9.5"
+sed -i -e "s/version=0.6.1/version=0.6.2/g" python/python-s3transfer/spkgbuild
+changelog "python/python-s3transfer/spkgbuild" "Upgraded from version 0.6.1 to version 0.6.2"
+sed -i -e "s/version=1.5.1/version=1.5.2/g" python/python-shellingham/spkgbuild
+changelog "python/python-shellingham/spkgbuild" "Upgraded from version 1.5.1 to version 1.5.2"
+sed -i -e "s/version=4.1.0/version=4.1.1/g" python/python-typeguard/spkgbuild
+changelog "python/python-typeguard/spkgbuild" "Upgraded from version 4.1.0 to version 4.1.1"
+sed -i -e "s/version=0.79.0/version=0.80.0/g" python/python-zeroconf/spkgbuild
+changelog "python/python-zeroconf/spkgbuild" "Upgraded from version 0.79.0 to version 0.80.0"
+sed -i -e "s/version=8.2.8/version=8.2.9/g" server/php/spkgbuild
+changelog "server/php/spkgbuild" "Upgraded from version 8.2.8 to version 8.2.9"
+sed -i -e "s/version=4.18.5/version=4.18.6/g" server/samba/spkgbuild
+changelog "server/samba/spkgbuild" "Upgraded from version 4.18.5 to version 4.18.6"
