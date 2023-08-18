71de12d286bf818e59597ada01218bbf9a27496e
Voncloft <voncloft@gmail.com>
2023-08-18 19:13:32 -0400

    Minor Updates

---
+++ logs/old-test-updates/2023/Aug/applied_updates-08-18-23.sh
@@ -0,0 +1,32 @@
+sed -i -e "s/version=2.5.4/version=3.0.0/g" core/qutebrowser/spkgbuild
+changelog "core/qutebrowser/spkgbuild" "Upgraded from version 2.5.4 to version 3.0.0"
+sed -i -e "s/version=3.07/version=3.08/g" core/sysvinit/spkgbuild
+changelog "core/sysvinit/spkgbuild" "Upgraded from version 3.07 to version 3.08"
+sed -i -e "s/version=1.2.13/version=1.3/g" core/zlib/spkgbuild
+changelog "core/zlib/spkgbuild" "Upgraded from version 1.2.13 to version 1.3"
+sed -i -e "s/version=3.9/version=3.9.1/g" extra/flexget/spkgbuild
+changelog "extra/flexget/spkgbuild" "Upgraded from version 3.9 to version 3.9.1"
+sed -i -e "s/version=117.0b8/version=117.0b9/g" nonfree/firefox/spkgbuild
+changelog "nonfree/firefox/spkgbuild" "Upgraded from version 117.0b8 to version 117.0b9"
+sed -i -e "s/version=1.3.7/version=1.3.8/g" python/python-basemap/spkgbuild
+changelog "python/python-basemap/spkgbuild" "Upgraded from version 1.3.7 to version 1.3.8"
+sed -i -e "s/version=1.28.28/version=1.28.29/g" python/python-boto3/spkgbuild
+changelog "python/python-boto3/spkgbuild" "Upgraded from version 1.28.28 to version 1.28.29"
+sed -i -e "s/version=1.31.28/version=1.31.29/g" python/python-botocore/spkgbuild
+changelog "python/python-botocore/spkgbuild" "Upgraded from version 1.31.28 to version 1.31.29"
+sed -i -e "s/version=6.82.4/version=6.82.5/g" python/python-hypothesis/spkgbuild
+changelog "python/python-hypothesis/spkgbuild" "Upgraded from version 6.82.4 to version 6.82.5"
+sed -i -e "s/version=2.2.0/version=2.2.1/g" python/python-pydantic/spkgbuild
+changelog "python/python-pydantic/spkgbuild" "Upgraded from version 2.2.0 to version 2.2.1"
+sed -i -e "s/version=68.1.0/version=68.1.2/g" python/python-setuptools/spkgbuild
+changelog "python/python-setuptools/spkgbuild" "Upgraded from version 68.1.0 to version 68.1.2"
+sed -i -e "s/version=2023.07.26/version=2023.08.17/g" python/python-sphinx-furo/spkgbuild
+changelog "python/python-sphinx-furo/spkgbuild" "Upgraded from version 2023.07.26 to version 2023.08.17"
+sed -i -e "s/version=22.10.0/version=23.8.0/g" python/python-twisted/spkgbuild
+changelog "python/python-twisted/spkgbuild" "Upgraded from version 22.10.0 to version 23.8.0"
+sed -i -e "s/version=4.1.1/version=4.1.2/g" python/python-typeguard/spkgbuild
+changelog "python/python-typeguard/spkgbuild" "Upgraded from version 4.1.1 to version 4.1.2"
+sed -i -e "s/version=2.4.18/version=2.4.19/g" ruby-gems/ruby-bundler/spkgbuild
+changelog "ruby-gems/ruby-bundler/spkgbuild" "Upgraded from version 2.4.18 to version 2.4.19"
+sed -i -e "s/version=2023.8.2/version=2023.8.3/g" server/homeassistant/spkgbuild
+changelog "server/homeassistant/spkgbuild" "Upgraded from version 2023.8.2 to version 2023.8.3"
