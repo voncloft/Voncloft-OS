6e46f5176630f403a8c81dc59992cf1a5efd5b9c
Voncloft <voncloft@gmail.com>
2023-08-20 18:12:57 -0400

    Minor Updates

---
+++ logs/old-test-updates/2023/Aug/applied_updates-08-20-23.sh
@@ -0,0 +1,40 @@
+sed -i -e "s/version=1.10.13/version=1.10.14/g" compilers/apache-ant/spkgbuild
+changelog "compilers/apache-ant/spkgbuild" "Upgraded from version 1.10.13 to version 1.10.14"
+sed -i -e "s/version=1.12/version=1.13/g" core/gzip/spkgbuild
+changelog "core/gzip/spkgbuild" "Upgraded from version 1.12 to version 1.13"
+sed -i -e "s/version=2.41.90/version=2.41.91/g" core/webkitgtk/spkgbuild
+changelog "core/webkitgtk/spkgbuild" "Upgraded from version 2.41.90 to version 2.41.91"
+sed -i -e "s/version=2.1.65/version=2.1.66/g" extra/anki/spkgbuild
+changelog "extra/anki/spkgbuild" "Upgraded from version 2.1.65 to version 2.1.66"
+sed -i -e "s/version=3.9.1/version=3.9.3/g" extra/flexget/spkgbuild
+changelog "extra/flexget/spkgbuild" "Upgraded from version 3.9.1 to version 3.9.3"
+sed -i -e "s/version=24.0/version=24.1/g" extra/protobuf/spkgbuild
+changelog "extra/protobuf/spkgbuild" "Upgraded from version 24.0 to version 24.1"
+sed -i -e "s/version=117.0b4/version=117.0b5/g" nonfree/thunderbird/spkgbuild
+changelog "nonfree/thunderbird/spkgbuild" "Upgraded from version 117.0b4 to version 117.0b5"
+sed -i -e "s/version=1.28.29/version=1.28.30/g" python/python-boto3/spkgbuild
+changelog "python/python-boto3/spkgbuild" "Upgraded from version 1.28.29 to version 1.28.30"
+sed -i -e "s/version=1.31.29/version=1.31.30/g" python/python-botocore/spkgbuild
+changelog "python/python-botocore/spkgbuild" "Upgraded from version 1.31.29 to version 1.31.30"
+sed -i -e "s/version=2.11.6/version=2.11.7/g" python/python-cloudflare/spkgbuild
+changelog "python/python-cloudflare/spkgbuild" "Upgraded from version 2.11.6 to version 2.11.7"
+sed -i -e "s/version=2023.8.0/version=2023.8.1/g" python/python-distributed/spkgbuild
+changelog "python/python-distributed/spkgbuild" "Upgraded from version 2023.8.0 to version 2023.8.1"
+sed -i -e "s/version=4.42.0/version=4.42.1/g" python/python-fonttools/spkgbuild
+changelog "python/python-fonttools/spkgbuild" "Upgraded from version 4.42.0 to version 4.42.1"
+sed -i -e "s/version=0.22.1/version=0.23.0/g" python/python-libtmux/spkgbuild
+changelog "python/python-libtmux/spkgbuild" "Upgraded from version 0.22.1 to version 0.23.0"
+sed -i -e "s/version=2023.7.23/version=2023.8.20/g" python/python-pipenv/spkgbuild
+changelog "python/python-pipenv/spkgbuild" "Upgraded from version 2023.7.23 to version 2023.8.20"
+sed -i -e "s/version=1.5.1/version=1.6.0/g" python/python-poetry/spkgbuild
+changelog "python/python-poetry/spkgbuild" "Upgraded from version 1.5.1 to version 1.6.0"
+sed -i -e "s/version=1.6.1/version=1.7.0/g" python/python-poetry-core/spkgbuild
+changelog "python/python-poetry-core/spkgbuild" "Upgraded from version 1.6.1 to version 1.7.0"
+sed -i -e "s/version=40.0/version=41.0/g" python/python-readme-renderer/spkgbuild
+changelog "python/python-readme-renderer/spkgbuild" "Upgraded from version 40.0 to version 41.0"
+sed -i -e "s/version=2023.08.17/version=2023.08.19/g" python/python-sphinx-furo/spkgbuild
+changelog "python/python-sphinx-furo/spkgbuild" "Upgraded from version 2023.08.17 to version 2023.08.19"
+sed -i -e "s/version=1.4.0/version=1.5.0/g" python/python-xcffib/spkgbuild
+changelog "python/python-xcffib/spkgbuild" "Upgraded from version 1.4.0 to version 1.5.0"
+sed -i -e "s/version=2.5.0/version=3.0.0/g" python/python-yaspin/spkgbuild
+changelog "python/python-yaspin/spkgbuild" "Upgraded from version 2.5.0 to version 3.0.0"
