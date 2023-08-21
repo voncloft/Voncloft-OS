ab6660f899a5899946ff03e7b6d0f4b43806761a
Voncloft <voncloft@gmail.com>
2023-08-21 16:04:50 -0400

    fixed packettracer

---
+++ logs/old-test-updates/2023/Aug/applied_updates-08-21-23.sh
@@ -0,0 +1,30 @@
+sed -i -e "s/version=1.0.9/version=1.1.0/g" core/brotli/spkgbuild
+changelog "core/brotli/spkgbuild" "Upgraded from version 1.0.9 to version 1.1.0"
+sed -i -e "s/version=4.7.0/version=4.8.0/g" core/qalculate/spkgbuild
+changelog "core/qalculate/spkgbuild" "Upgraded from version 4.7.0 to version 4.8.0"
+sed -i -e "s/version=3.9.3/version=3.9.4/g" extra/flexget/spkgbuild
+changelog "extra/flexget/spkgbuild" "Upgraded from version 3.9.3 to version 3.9.4"
+sed -i -e "s/version=1.0.9/version=1.1.0/g" multilib/brotli-32/spkgbuild
+changelog "multilib/brotli-32/spkgbuild" "Upgraded from version 1.0.9 to version 1.1.0"
+sed -i -e "s/version=1.21.0/version=1.22.0/g" libs/gpgme/spkgbuild
+changelog "libs/gpgme/spkgbuild" "Upgraded from version 1.21.0 to version 1.22.0"
+sed -i -e "s/version=4.7.0/version=4.8.0/g" libs/libqalculate/spkgbuild
+changelog "libs/libqalculate/spkgbuild" "Upgraded from version 4.7.0 to version 4.8.0"
+sed -i -e "s/version=2.41.0/version=2.42.0/g" networking/git/spkgbuild
+changelog "networking/git/spkgbuild" "Upgraded from version 2.41.0 to version 2.42.0"
+sed -i -e "s/version=7.5.5/version=7.6.0/g" nonfree/libreoffice/spkgbuild
+changelog "nonfree/libreoffice/spkgbuild" "Upgraded from version 7.5.5 to version 7.6.0"
+sed -i -e "s/version=4.5.1/version=4.6.0/g" python/python-engineio/spkgbuild
+changelog "python/python-engineio/spkgbuild" "Upgraded from version 4.5.1 to version 4.6.0"
+sed -i -e "s/version=2.3.2/version=2.3.3/g" python/python-flask/spkgbuild
+changelog "python/python-flask/spkgbuild" "Upgraded from version 2.3.2 to version 2.3.3"
+sed -i -e "s/version=6.82.5/version=6.82.6/g" python/python-hypothesis/spkgbuild
+changelog "python/python-hypothesis/spkgbuild" "Upgraded from version 6.82.5 to version 6.82.6"
+sed -i -e "s/version=2.5.26/version=2.5.27/g" python/python-identify/spkgbuild
+changelog "python/python-identify/spkgbuild" "Upgraded from version 2.5.26 to version 2.5.27"
+sed -i -e "s/version=1.6.0/version=1.6.1/g" python/python-poetry/spkgbuild
+changelog "python/python-poetry/spkgbuild" "Upgraded from version 1.6.0 to version 1.6.1"
+sed -i -e "s/version=7.11.0/version=7.11.1/g" python/python-qiniu/spkgbuild
+changelog "python/python-qiniu/spkgbuild" "Upgraded from version 7.11.0 to version 7.11.1"
+sed -i -e "s/version=1.3.0/version=2.0.0/g" python/python-sphinx_rtd_theme/spkgbuild
+changelog "python/python-sphinx_rtd_theme/spkgbuild" "Upgraded from version 1.3.0 to version 2.0.0"
