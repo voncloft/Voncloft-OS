5456040b069c653936f5c52d33f47205e4acd827
Voncloft <voncloft@gmail.com>
2023-08-23 15:44:07 -0400

    Minor Updates

---
+++ logs/old-test-updates/2023/Aug/applied_updates-08-23-23.sh
@@ -0,0 +1,58 @@
+sed -i -e "s/version=1.15.6/version=1.15.8/g" core/dbus/spkgbuild
+changelog "core/dbus/spkgbuild" "Upgraded from version 1.15.6 to version 1.15.8"
+sed -i -e "s/version=2.8/version=2.9/g" core/fop/spkgbuild
+changelog "core/fop/spkgbuild" "Upgraded from version 2.8 to version 2.9"
+sed -i -e "s/version=6.4.11/version=6.4.12/g" core/linux-api-headers/spkgbuild
+changelog "core/linux-api-headers/spkgbuild" "Upgraded from version 6.4.11 to version 6.4.12"
+sed -i -e "s/version=6.4.11/version=6.4.12/g" core/linux-desktop/spkgbuild
+changelog "core/linux-desktop/spkgbuild" "Upgraded from version 6.4.11 to version 6.4.12"
+sed -i -e "s/version=6.4.11/version=6.4.12/g" core/linux-router/spkgbuild
+changelog "core/linux-router/spkgbuild" "Upgraded from version 6.4.11 to version 6.4.12"
+sed -i -e "s/version=0.3.77/version=0.3.78/g" core/pipewire/spkgbuild
+changelog "core/pipewire/spkgbuild" "Upgraded from version 0.3.77 to version 0.3.78"
+sed -i -e "s/version=4.0.3/version=4.0.4/g" core/weechat/spkgbuild
+changelog "core/weechat/spkgbuild" "Upgraded from version 4.0.3 to version 4.0.4"
+sed -i -e "s/version=3.22.6.0/version=4.23.8.0/g" extra/inotify-tools/spkgbuild
+changelog "extra/inotify-tools/spkgbuild" "Upgraded from version 3.22.6.0 to version 4.23.8.0"
+sed -i -e "s/version=2.4.115/version=2.4.116/g" libs/libdrm/spkgbuild
+changelog "libs/libdrm/spkgbuild" "Upgraded from version 2.4.115 to version 2.4.116"
+sed -i -e "s/version=1.10.0/version=1.10.1/g" libs/libzip/spkgbuild
+changelog "libs/libzip/spkgbuild" "Upgraded from version 1.10.0 to version 1.10.1"
+sed -i -e "s/version=0.4.7.14/version=0.4.8.4/g" networking/tor/spkgbuild
+changelog "networking/tor/spkgbuild" "Upgraded from version 0.4.7.14 to version 0.4.8.4"
+sed -i -e "s/version=535.98/version=535.104.05/g" nonfree/nvidia/spkgbuild
+changelog "nonfree/nvidia/spkgbuild" "Upgraded from version 535.98 to version 535.104.05"
+sed -i -e "s/version=535.98/version=535.104.05/g" nonfree/nvidia-32/spkgbuild
+changelog "nonfree/nvidia-32/spkgbuild" "Upgraded from version 535.98 to version 535.104.05"
+sed -i -e "s/version=535.98/version=535.104.05/g" nonfree/nvidia-modules/spkgbuild
+changelog "nonfree/nvidia-modules/spkgbuild" "Upgraded from version 535.98 to version 535.104.05"
+sed -i -e "s/version=3.1.6.1/version=3.1.6.2/g" nonfree/pandoc-bin/spkgbuild
+changelog "nonfree/pandoc-bin/spkgbuild" "Upgraded from version 3.1.6.1 to version 3.1.6.2"
+sed -i -e "s/version=117.0b5/version=117.0b6/g" nonfree/thunderbird/spkgbuild
+changelog "nonfree/thunderbird/spkgbuild" "Upgraded from version 117.0b5 to version 117.0b6"
+sed -i -e "s/version=4.1.0/version=4.1.1/g" python/python-bitstring/spkgbuild
+changelog "python/python-bitstring/spkgbuild" "Upgraded from version 4.1.0 to version 4.1.1"
+sed -i -e "s/version=1.28.30/version=1.28.32/g" python/python-boto3/spkgbuild
+changelog "python/python-boto3/spkgbuild" "Upgraded from version 1.28.30 to version 1.28.32"
+sed -i -e "s/version=1.31.30/version=1.31.32/g" python/python-botocore/spkgbuild
+changelog "python/python-botocore/spkgbuild" "Upgraded from version 1.31.30 to version 1.31.32"
+sed -i -e "s/version=0.54.0/version=0.55.0/g" python/python-django-allauth/spkgbuild
+changelog "python/python-django-allauth/spkgbuild" "Upgraded from version 0.54.0 to version 0.55.0"
+sed -i -e "s/version=4.6.0/version=4.6.1/g" python/python-engineio/spkgbuild
+changelog "python/python-engineio/spkgbuild" "Upgraded from version 4.6.0 to version 4.6.1"
+sed -i -e "s/version=8.13.18/version=8.13.19/g" python/python-phonenumbers/spkgbuild
+changelog "python/python-phonenumbers/spkgbuild" "Upgraded from version 8.13.18 to version 8.13.19"
+sed -i -e "s/version=2023.8.20/version=2023.8.23/g" python/python-pipenv/spkgbuild
+changelog "python/python-pipenv/spkgbuild" "Upgraded from version 2023.8.20 to version 2023.8.23"
+sed -i -e "s/version=2.2.1/version=2.3.0/g" python/python-pydantic/spkgbuild
+changelog "python/python-pydantic/spkgbuild" "Upgraded from version 2.2.1 to version 2.3.0"
+sed -i -e "s/version=4.0.5/version=4.0.6/g" python/python-pykeepass/spkgbuild
+changelog "python/python-pykeepass/spkgbuild" "Upgraded from version 4.0.5 to version 4.0.6"
+sed -i -e "s/version=0.41.1/version=0.41.2/g" python/python-wheel/spkgbuild
+changelog "python/python-wheel/spkgbuild" "Upgraded from version 0.41.1 to version 0.41.2"
+sed -i -e "s/version=0.80.0/version=0.82.1/g" python/python-zeroconf/spkgbuild
+changelog "python/python-zeroconf/spkgbuild" "Upgraded from version 0.80.0 to version 0.82.1"
+sed -i -e "s/version=3.13.22/version=3.13.24/g" server/blacklist-hosts/spkgbuild
+changelog "server/blacklist-hosts/spkgbuild" "Upgraded from version 3.13.22 to version 3.13.24"
+sed -i -e "s/version=1.0.4/version=1.1.0/g" xfce/xfce4-terminal/spkgbuild
+changelog "xfce/xfce4-terminal/spkgbuild" "Upgraded from version 1.0.4 to version 1.1.0"
