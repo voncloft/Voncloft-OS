8d8109ce5d2600195c1993488ea0484fe40073d3
Voncloft <voncloft@gmail.com>
2023-08-28 10:17:17 -0400

    Minor Updates

---
+++ logs/old-test-updates/2023/Aug/applied_updates-08-28-23.sh
@@ -0,0 +1,42 @@
+sed -i -e "s/version=0.24.6/version=0.25.0/g" core/cbindgen/spkgbuild
+changelog "core/cbindgen/spkgbuild" "Upgraded from version 0.24.6 to version 0.25.0"
+sed -i -e "s/version=4.8/version=4.9/g" core/nnn/spkgbuild
+changelog "core/nnn/spkgbuild" "Upgraded from version 4.8 to version 4.9"
+sed -i -e "s/version=3.9.4/version=3.9.5/g" extra/flexget/spkgbuild
+changelog "extra/flexget/spkgbuild" "Upgraded from version 3.9.4 to version 3.9.5"
+sed -i -e "s/version=24.1/version=24.2/g" extra/protobuf/spkgbuild
+changelog "extra/protobuf/spkgbuild" "Upgraded from version 24.1 to version 24.2"
+sed -i -e "s/version=4.0.8/version=4.1.0/g" networking/wireshark/spkgbuild
+changelog "networking/wireshark/spkgbuild" "Upgraded from version 4.0.8 to version 4.1.0"
+sed -i -e "s/version=3.42.0.1/version=3.43.0.0/g" python/python-apsw/spkgbuild
+changelog "python/python-apsw/spkgbuild" "Upgraded from version 3.42.0.1 to version 3.43.0.0"
+sed -i -e "s/version=1.28.33/version=1.28.35/g" python/python-boto3/spkgbuild
+changelog "python/python-boto3/spkgbuild" "Upgraded from version 1.28.33 to version 1.28.35"
+sed -i -e "s/version=1.31.33/version=1.31.35/g" python/python-botocore/spkgbuild
+changelog "python/python-botocore/spkgbuild" "Upgraded from version 1.31.33 to version 1.31.35"
+sed -i -e "s/version=0.101.1/version=0.103.0/g" python/python-fastapi/spkgbuild
+changelog "python/python-fastapi/spkgbuild" "Upgraded from version 0.101.1 to version 0.103.0"
+sed -i -e "s/version=2.3.0/version=2.4.0/g" python/python-geopy/spkgbuild
+changelog "python/python-geopy/spkgbuild" "Upgraded from version 2.3.0 to version 2.4.0"
+sed -i -e "s/version=2.3.6/version=2.4.0/g" python/python-gphoto2/spkgbuild
+changelog "python/python-gphoto2/spkgbuild" "Upgraded from version 2.3.6 to version 2.4.0"
+sed -i -e "s/version=2023.8.23/version=2023.8.26/g" python/python-pipenv/spkgbuild
+changelog "python/python-pipenv/spkgbuild" "Upgraded from version 2023.8.23 to version 2023.8.26"
+sed -i -e "s/version=1.2.0/version=1.3.0/g" python/python-pluggy/spkgbuild
+changelog "python/python-pluggy/spkgbuild" "Upgraded from version 1.2.0 to version 1.3.0"
+sed -i -e "s/version=4.0.39/version=5.0.0/g" python/python-pyodbc/spkgbuild
+changelog "python/python-pyodbc/spkgbuild" "Upgraded from version 4.0.39 to version 5.0.0"
+sed -i -e "s/version=2.5.0/version=2.6.1/g" python/python-sphinxcontrib-bibtex/spkgbuild
+changelog "python/python-sphinxcontrib-bibtex/spkgbuild" "Upgraded from version 2.5.0 to version 2.6.1"
+sed -i -e "s/version=0.31.0/version=0.31.1/g" python/python-starlette/spkgbuild
+changelog "python/python-starlette/spkgbuild" "Upgraded from version 0.31.0 to version 0.31.1"
+sed -i -e "s/version=3.23.3/version=3.23.5/g" python/python-trimesh/spkgbuild
+changelog "python/python-trimesh/spkgbuild" "Upgraded from version 3.23.3 to version 3.23.5"
+sed -i -e "s/version=4.1.2/version=4.1.3/g" python/python-typeguard/spkgbuild
+changelog "python/python-typeguard/spkgbuild" "Upgraded from version 4.1.2 to version 4.1.3"
+sed -i -e "s/version=0.82.1/version=0.85.0/g" python/python-zeroconf/spkgbuild
+changelog "python/python-zeroconf/spkgbuild" "Upgraded from version 0.82.1 to version 0.85.0"
+sed -i -e "s/version=6.0/version=6.1/g" python/python-zope-testrunner/spkgbuild
+changelog "python/python-zope-testrunner/spkgbuild" "Upgraded from version 6.0 to version 6.1"
+sed -i -e "s/version=3.13.24/version=3.13.25/g" server/blacklist-hosts/spkgbuild
+changelog "server/blacklist-hosts/spkgbuild" "Upgraded from version 3.13.24 to version 3.13.25"
