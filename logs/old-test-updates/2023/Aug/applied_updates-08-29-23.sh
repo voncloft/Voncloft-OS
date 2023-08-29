41108a2c20b5d84dde8bbf5a601a04dafe070d8b
Voncloft <voncloft@gmail.com>
2023-08-29 17:57:09 -0400

    Minor Updates

---
+++ logs/old-test-updates/2023/Aug/applied_updates-08-29-23.sh
@@ -0,0 +1,60 @@
+sed -i -e "s/version=9.3/version=9.4/g" core/coreutils/spkgbuild
+changelog "core/coreutils/spkgbuild" "Upgraded from version 9.3 to version 9.4"
+sed -i -e "s/version=6.4.12/version=6.5/g" core/linux-api-headers/spkgbuild
+changelog "core/linux-api-headers/spkgbuild" "Upgraded from version 6.4.12 to version 6.5"
+sed -i -e "s/version=6.4.12/version=6.5/g" core/linux-desktop/spkgbuild
+changelog "core/linux-desktop/spkgbuild" "Upgraded from version 6.4.12 to version 6.5"
+sed -i -e "s/version=6.4.12/version=6.5/g" core/linux-router/spkgbuild
+changelog "core/linux-router/spkgbuild" "Upgraded from version 6.4.12 to version 6.5"
+sed -i -e "s/version=2.10.4/version=2.10.5/g" core/mps-youtube/spkgbuild
+changelog "core/mps-youtube/spkgbuild" "Upgraded from version 2.10.4 to version 2.10.5"
+sed -i -e "s/version=0.3.78/version=0.3.79/g" core/pipewire/spkgbuild
+changelog "core/pipewire/spkgbuild" "Upgraded from version 0.3.78 to version 0.3.79"
+sed -i -e "s/version=3.9.5/version=3.9.6/g" extra/flexget/spkgbuild
+changelog "extra/flexget/spkgbuild" "Upgraded from version 3.9.5 to version 3.9.6"
+sed -i -e "s/version=10.1.0/version=10.1.1/g" libs/fmt/spkgbuild
+changelog "libs/fmt/spkgbuild" "Upgraded from version 10.1.0 to version 10.1.1"
+sed -i -e "s/version=1.10.1/version=1.11.2/g" libs/zita-resampler/spkgbuild
+changelog "libs/zita-resampler/spkgbuild" "Upgraded from version 1.10.1 to version 1.11.2"
+sed -i -e "s/version=117.0b9/version=118.0b1/g" nonfree/firefox/spkgbuild
+changelog "nonfree/firefox/spkgbuild" "Upgraded from version 117.0b9 to version 118.0b1"
+sed -i -e "s/version=1.28.35/version=1.28.36/g" python/python-boto3/spkgbuild
+changelog "python/python-boto3/spkgbuild" "Upgraded from version 1.28.35 to version 1.28.36"
+sed -i -e "s/version=1.31.35/version=1.31.36/g" python/python-botocore/spkgbuild
+changelog "python/python-botocore/spkgbuild" "Upgraded from version 1.31.35 to version 1.31.36"
+sed -i -e "s/version=4.0.1/version=4.1.0/g" python/python-chameleon/spkgbuild
+changelog "python/python-chameleon/spkgbuild" "Upgraded from version 4.0.1 to version 4.1.0"
+sed -i -e "s/version=0.7.0/version=0.8.0/g" python/python-databases/spkgbuild
+changelog "python/python-databases/spkgbuild" "Upgraded from version 0.7.0 to version 0.8.0"
+sed -i -e "s/version=3.12.2/version=3.12.3/g" python/python-filelock/spkgbuild
+changelog "python/python-filelock/spkgbuild" "Upgraded from version 3.12.2 to version 3.12.3"
+sed -i -e "s/version=1.8.2/version=1.8.3/g" python/python-gssapi/spkgbuild
+changelog "python/python-gssapi/spkgbuild" "Upgraded from version 1.8.2 to version 1.8.3"
+sed -i -e "s/version=6.82.6/version=6.82.7/g" python/python-hypothesis/spkgbuild
+changelog "python/python-hypothesis/spkgbuild" "Upgraded from version 6.82.6 to version 6.82.7"
+sed -i -e "s/version=5.2.1/version=5.3.0/g" python/python-keystoneauth1/spkgbuild
+changelog "python/python-keystoneauth1/spkgbuild" "Upgraded from version 5.2.1 to version 5.3.0"
+sed -i -e "s/version=5.1.1/version=5.2.0/g" python/python-oslo-concurrency/spkgbuild
+changelog "python/python-oslo-concurrency/spkgbuild" "Upgraded from version 5.1.1 to version 5.2.0"
+sed -i -e "s/version=9.1.1/version=9.2.0/g" python/python-oslo-config/spkgbuild
+changelog "python/python-oslo-config/spkgbuild" "Upgraded from version 9.1.1 to version 9.2.0"
+sed -i -e "s/version=5.1.1/version=5.2.0/g" python/python-oslo-context/spkgbuild
+changelog "python/python-oslo-context/spkgbuild" "Upgraded from version 5.1.1 to version 5.2.0"
+sed -i -e "s/version=13.1.0/version=14.0.0/g" python/python-oslo-db/spkgbuild
+changelog "python/python-oslo-db/spkgbuild" "Upgraded from version 13.1.0 to version 14.0.0"
+sed -i -e "s/version=6.0.0/version=6.1.0/g" python/python-oslo-i18n/spkgbuild
+changelog "python/python-oslo-i18n/spkgbuild" "Upgraded from version 6.0.0 to version 6.1.0"
+sed -i -e "s/version=5.2.0/version=5.3.0/g" python/python-oslo-log/spkgbuild
+changelog "python/python-oslo-log/spkgbuild" "Upgraded from version 5.2.0 to version 5.3.0"
+sed -i -e "s/version=5.1.1/version=5.2.0/g" python/python-oslo-serialization/spkgbuild
+changelog "python/python-oslo-serialization/spkgbuild" "Upgraded from version 5.1.1 to version 5.2.0"
+sed -i -e "s/version=6.2.0/version=6.2.1/g" python/python-oslo-utils/spkgbuild
+changelog "python/python-oslo-utils/spkgbuild" "Upgraded from version 6.2.0 to version 6.2.1"
+sed -i -e "s/version=2023.8.26/version=2023.8.28/g" python/python-pipenv/spkgbuild
+changelog "python/python-pipenv/spkgbuild" "Upgraded from version 2023.8.26 to version 2023.8.28"
+sed -i -e "s/version=2.3.1/version=2.4.0/g" python/python-qtpy/spkgbuild
+changelog "python/python-qtpy/spkgbuild" "Upgraded from version 2.3.1 to version 2.4.0"
+sed -i -e "s/version=0.85.0/version=0.88.0/g" python/python-zeroconf/spkgbuild
+changelog "python/python-zeroconf/spkgbuild" "Upgraded from version 0.85.0 to version 0.88.0"
+sed -i -e "s/version=3.13.25/version=3.13.26/g" server/blacklist-hosts/spkgbuild
+changelog "server/blacklist-hosts/spkgbuild" "Upgraded from version 3.13.25 to version 3.13.26"
