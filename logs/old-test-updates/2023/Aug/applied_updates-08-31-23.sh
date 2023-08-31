199f5dba11a9d426262c81737e484e3e9881db3c
Voncloft <voncloft@gmail.com>
2023-08-31 11:31:26 -0400

    Minor Updates

---
+++ logs/old-test-updates/2023/Aug/applied_updates-08-31-23.sh
@@ -0,0 +1,32 @@
+sed -i -e "s/version=4.8.2/version=4.8.3/g" compilers/ccache/spkgbuild
+changelog "compilers/ccache/spkgbuild" "Upgraded from version 4.8.2 to version 4.8.3"
+sed -i -e "s/version=3.1.11/version=3.2.0/g" libs/openexr/spkgbuild
+changelog "libs/openexr/spkgbuild" "Upgraded from version 3.1.11 to version 3.2.0"
+sed -i -e "s/version=0.4.8.4/version=0.4.8.5/g" networking/tor/spkgbuild
+changelog "networking/tor/spkgbuild" "Upgraded from version 0.4.8.4 to version 0.4.8.5"
+sed -i -e "s/version=118.0b1/version=118.0b2/g" nonfree/firefox/spkgbuild
+changelog "nonfree/firefox/spkgbuild" "Upgraded from version 118.0b1 to version 118.0b2"
+sed -i -e "s/version=117.0b6/version=118.0b1/g" nonfree/thunderbird/spkgbuild
+changelog "nonfree/thunderbird/spkgbuild" "Upgraded from version 117.0b6 to version 118.0b1"
+sed -i -e "s/version=1.4.0/version=1.5.0/g" python/python-aiopg/spkgbuild
+changelog "python/python-aiopg/spkgbuild" "Upgraded from version 1.4.0 to version 1.5.0"
+sed -i -e "s/version=1.28.36/version=1.28.37/g" python/python-boto3/spkgbuild
+changelog "python/python-boto3/spkgbuild" "Upgraded from version 1.28.36 to version 1.28.37"
+sed -i -e "s/version=1.31.36/version=1.31.37/g" python/python-botocore/spkgbuild
+changelog "python/python-botocore/spkgbuild" "Upgraded from version 1.31.36 to version 1.31.37"
+sed -i -e "s/version=4.21.0/version=4.22.0/g" python/python-braintree/spkgbuild
+changelog "python/python-braintree/spkgbuild" "Upgraded from version 4.21.0 to version 4.22.0"
+sed -i -e "s/version=0.55.0/version=0.55.2/g" python/python-django-allauth/spkgbuild
+changelog "python/python-django-allauth/spkgbuild" "Upgraded from version 0.55.0 to version 0.55.2"
+sed -i -e "s/version=3.4.0/version=3.5.0/g" python/python-octaviaclient/spkgbuild
+changelog "python/python-octaviaclient/spkgbuild" "Upgraded from version 3.4.0 to version 3.5.0"
+sed -i -e "s/version=3.27.0/version=3.28.0/g" python/python-openmdao/spkgbuild
+changelog "python/python-openmdao/spkgbuild" "Upgraded from version 3.27.0 to version 3.28.0"
+sed -i -e "s/version=2.4.2/version=2.4.3/g" python/python-pytest-regressions/spkgbuild
+changelog "python/python-pytest-regressions/spkgbuild" "Upgraded from version 2.4.2 to version 2.4.3"
+sed -i -e "s/version=20.24.3/version=20.24.4/g" python/python-virtualenv/spkgbuild
+changelog "python/python-virtualenv/spkgbuild" "Upgraded from version 20.24.3 to version 20.24.4"
+sed -i -e "s/version=2023.8.4/version=2023.9.0/g" server/homeassistant/spkgbuild
+changelog "server/homeassistant/spkgbuild" "Upgraded from version 2023.8.4 to version 2023.9.0"
+sed -i -e "s/version=3.92/version=3.93/g" core/nss/spkgbuild
+changelog "core/nss/spkgbuild" "Upgraded from version 3.92 to version 3.93"
