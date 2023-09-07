378058a5aa4a639f6e87f296413f5e23d5b57822
Voncloft <voncloft@gmail.com>
2023-09-07 09:03:43 -0400

    Minor Updates

---
+++ logs/old-test-updates/2023/Sep/applied_updates-09-07-23.sh
@@ -0,0 +1,28 @@
+sed -i -e "s/version=6.4.0/version=6.5.0/g" core/iproute2/spkgbuild
+changelog "core/iproute2/spkgbuild" "Upgraded from version 6.4.0 to version 6.5.0"
+sed -i -e "s/version=6.5.1/version=6.5.2/g" core/mercurial/spkgbuild
+changelog "core/mercurial/spkgbuild" "Upgraded from version 6.5.1 to version 6.5.2"
+sed -i -e "s/version=7.2.0/version=7.2.1/g" core/redis/spkgbuild
+changelog "core/redis/spkgbuild" "Upgraded from version 7.2.0 to version 7.2.1"
+sed -i -e "s/version=23.08.0/version=23.09.0/g" libs/poppler/spkgbuild
+changelog "libs/poppler/spkgbuild" "Upgraded from version 23.08.0 to version 23.09.0"
+sed -i -e "s/version=1.45.1/version=1.45.2/g" networking/networkmanager/spkgbuild
+changelog "networking/networkmanager/spkgbuild" "Upgraded from version 1.45.1 to version 1.45.2"
+sed -i -e "s/version=118.0b4/version=118.0b5/g" nonfree/firefox/spkgbuild
+changelog "nonfree/firefox/spkgbuild" "Upgraded from version 118.0b4 to version 118.0b5"
+sed -i -e "s/version=1.4.1/version=1.5.0/g" plasma/ktextaddons/spkgbuild
+changelog "plasma/ktextaddons/spkgbuild" "Upgraded from version 1.4.1 to version 1.5.0"
+sed -i -e "s/version=1.28.40/version=1.28.41/g" python/python-boto3/spkgbuild
+changelog "python/python-boto3/spkgbuild" "Upgraded from version 1.28.40 to version 1.28.41"
+sed -i -e "s/version=1.31.40/version=1.31.41/g" python/python-botocore/spkgbuild
+changelog "python/python-botocore/spkgbuild" "Upgraded from version 1.31.40 to version 1.31.41"
+sed -i -e "s/version=7.3.0/version=7.3.1/g" python/python-coverage/spkgbuild
+changelog "python/python-coverage/spkgbuild" "Upgraded from version 7.3.0 to version 7.3.1"
+sed -i -e "s/version=2.97.0/version=2.98.0/g" python/python-google-api-python-client/spkgbuild
+changelog "python/python-google-api-python-client/spkgbuild" "Upgraded from version 2.97.0 to version 2.98.0"
+sed -i -e "s/version=6.84.1/version=6.84.2/g" python/python-hypothesis/spkgbuild
+changelog "python/python-hypothesis/spkgbuild" "Upgraded from version 6.84.1 to version 6.84.2"
+sed -i -e "s/version=1.0.4/version=1.0.5/g" python/python-pytest-aiohttp/spkgbuild
+changelog "python/python-pytest-aiohttp/spkgbuild" "Upgraded from version 1.0.4 to version 1.0.5"
+sed -i -e "s/version=0.97.0/version=0.98.0/g" python/python-zeroconf/spkgbuild
+changelog "python/python-zeroconf/spkgbuild" "Upgraded from version 0.97.0 to version 0.98.0"
