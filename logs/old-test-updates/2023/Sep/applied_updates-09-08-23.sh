d835c3233bed10ace828d35cfeaf313611f105fc
Voncloft <voncloft@gmail.com>
2023-09-08 15:09:41 -0400

    Minor Updates

---
+++ logs/old-test-updates/2023/Sep/applied_updates-09-08-23.sh
@@ -0,0 +1,12 @@
+sed -i -e "s/version=6.5.1/version=6.5.2/g" core/linux-api-headers/spkgbuild
+changelog "core/linux-api-headers/spkgbuild" "Upgraded from version 6.5.1 to version 6.5.2"
+sed -i -e "s/version=6.5.1/version=6.5.2/g" core/linux-desktop/spkgbuild
+changelog "core/linux-desktop/spkgbuild" "Upgraded from version 6.5.1 to version 6.5.2"
+sed -i -e "s/version=6.5.1/version=6.5.2/g" core/linux-router/spkgbuild
+changelog "core/linux-router/spkgbuild" "Upgraded from version 6.5.1 to version 6.5.2"
+sed -i -e "s/version=2.10.0/version=2.10.1/g" core/udisks2/spkgbuild
+changelog "core/udisks2/spkgbuild" "Upgraded from version 2.10.0 to version 2.10.1"
+sed -i -e "s/version=2.41.91/version=2.41.92/g" core/webkitgtk/spkgbuild
+changelog "core/webkitgtk/spkgbuild" "Upgraded from version 2.41.91 to version 2.41.92"
+sed -i -e "s/version=24.2/version=24.3/g" extra/protobuf/spkgbuild
+changelog "extra/protobuf/spkgbuild" "Upgraded from version 24.2 to version 24.3"
