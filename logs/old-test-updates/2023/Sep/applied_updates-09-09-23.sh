7a2fdec06149c4e47c43414ba96753352dc3da1f
Voncloft <voncloft@gmail.com>
2023-09-09 15:28:03 -0400

    Minor Updates

---
+++ logs/old-test-updates/2023/Sep/applied_updates-09-09-23.sh
@@ -0,0 +1,26 @@
+sed -i -e "s/version=1.4.0/version=1.4.1/g" fonts/font-util/spkgbuild
+changelog "fonts/font-util/spkgbuild" "Upgraded from version 1.4.0 to version 1.4.1"
+sed -i -e "s/version=118.0b5/version=118.0b6/g" nonfree/firefox/spkgbuild
+changelog "nonfree/firefox/spkgbuild" "Upgraded from version 118.0b5 to version 118.0b6"
+sed -i -e "s/version=3.1.7/version=3.1.8/g" nonfree/pandoc-bin/spkgbuild
+changelog "nonfree/pandoc-bin/spkgbuild" "Upgraded from version 3.1.7 to version 3.1.8"
+sed -i -e "s/version=118.0b1/version=118.0b2/g" nonfree/thunderbird/spkgbuild
+changelog "nonfree/thunderbird/spkgbuild" "Upgraded from version 118.0b1 to version 118.0b2"
+sed -i -e "s/version=4.1.1/version=4.1.2/g" python/python-bitstring/spkgbuild
+changelog "python/python-bitstring/spkgbuild" "Upgraded from version 4.1.1 to version 4.1.2"
+sed -i -e "s/version=1.28.41/version=1.28.44/g" python/python-boto3/spkgbuild
+changelog "python/python-boto3/spkgbuild" "Upgraded from version 1.28.41 to version 1.28.44"
+sed -i -e "s/version=1.31.41/version=1.31.44/g" python/python-botocore/spkgbuild
+changelog "python/python-botocore/spkgbuild" "Upgraded from version 1.31.41 to version 1.31.44"
+sed -i -e "s/version=6.15.6/version=6.15.7/g" python/python-cx-freeze/spkgbuild
+changelog "python/python-cx-freeze/spkgbuild" "Upgraded from version 6.15.6 to version 6.15.7"
+sed -i -e "s/version=2023.9.0/version=2023.9.1/g" python/python-distributed/spkgbuild
+changelog "python/python-distributed/spkgbuild" "Upgraded from version 2023.9.0 to version 2023.9.1"
+sed -i -e "s/version=0.55.2/version=0.56.1/g" python/python-django-allauth/spkgbuild
+changelog "python/python-django-allauth/spkgbuild" "Upgraded from version 0.55.2 to version 0.56.1"
+sed -i -e "s/version=0.18.11/version=0.18.12/g" python/python-django-modeltranslation/spkgbuild
+changelog "python/python-django-modeltranslation/spkgbuild" "Upgraded from version 0.18.11 to version 0.18.12"
+sed -i -e "s/version=19.3.1/version=19.6.0/g" python/python-faker/spkgbuild
+changelog "python/python-faker/spkgbuild" "Upgraded from version 19.3.1 to version 19.6.0"
+sed -i -e "s/version=0.17.3/version=0.18.0/g" python/python-httpcore/spkgbuild
+changelog "python/python-httpcore/spkgbuild" "Upgraded from version 0.17.3 to version 0.18.0"
