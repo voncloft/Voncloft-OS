6702202fdc6aefe3d55b4da002745f8a7191077a
Voncloft <voncloft@gmail.com>
2023-09-12 09:57:39 -0400

    Minor Updates

---
+++ logs/old-test-updates/2023/Sep/applied_updates-09-12-23.sh
@@ -0,0 +1,28 @@
+sed -i -e "s/version=3.4.1/version=3.4.2/g" core/icewm/spkgbuild
+changelog "core/icewm/spkgbuild" "Upgraded from version 3.4.1 to version 3.4.2"
+sed -i -e "s/version=3.9.7/version=3.9.8/g" extra/flexget/spkgbuild
+changelog "extra/flexget/spkgbuild" "Upgraded from version 3.9.7 to version 3.9.8"
+sed -i -e "s/version=4.2.0/version=4.2.1/g" libs/libimagequant/spkgbuild
+changelog "libs/libimagequant/spkgbuild" "Upgraded from version 4.2.0 to version 4.2.1"
+sed -i -e "s/version=118.0b6/version=118.0b7/g" nonfree/firefox/spkgbuild
+changelog "nonfree/firefox/spkgbuild" "Upgraded from version 118.0b6 to version 118.0b7"
+sed -i -e "s/version=19.6.0/version=19.6.1/g" python/python-faker/spkgbuild
+changelog "python/python-faker/spkgbuild" "Upgraded from version 19.6.0 to version 19.6.1"
+sed -i -e "s/version=3.0.5/version=3.1.0/g" python/python-flask-sqlalchemy/spkgbuild
+changelog "python/python-flask-sqlalchemy/spkgbuild" "Upgraded from version 3.0.5 to version 3.1.0"
+sed -i -e "s/version=2.22.0/version=2.23.0/g" python/python-google-auth/spkgbuild
+changelog "python/python-google-auth/spkgbuild" "Upgraded from version 2.22.0 to version 2.23.0"
+sed -i -e "s/version=6.84.2/version=6.84.3/g" python/python-hypothesis/spkgbuild
+changelog "python/python-hypothesis/spkgbuild" "Upgraded from version 6.84.2 to version 6.84.3"
+sed -i -e "s/version=14.0.0/version=14.1.0/g" python/python-pdoc/spkgbuild
+changelog "python/python-pdoc/spkgbuild" "Upgraded from version 14.0.0 to version 14.1.0"
+sed -i -e "s/version=0.24.0/version=0.25.0/g" python/python-pytest-httpx/spkgbuild
+changelog "python/python-pytest-httpx/spkgbuild" "Upgraded from version 0.24.0 to version 0.25.0"
+sed -i -e "s/version=68.2.0/version=68.2.1/g" python/python-setuptools/spkgbuild
+changelog "python/python-setuptools/spkgbuild" "Upgraded from version 68.2.0 to version 68.2.1"
+sed -i -e "s/version=4.1.3/version=4.1.5/g" python/python-typeguard/spkgbuild
+changelog "python/python-typeguard/spkgbuild" "Upgraded from version 4.1.3 to version 4.1.5"
+sed -i -e "s/version=0.104.0/version=0.108.0/g" python/python-zeroconf/spkgbuild
+changelog "python/python-zeroconf/spkgbuild" "Upgraded from version 0.104.0 to version 0.108.0"
+sed -i -e "s/version=3.14.3/version=3.14.4/g" server/blacklist-hosts/spkgbuild
+changelog "server/blacklist-hosts/spkgbuild" "Upgraded from version 3.14.3 to version 3.14.4"
