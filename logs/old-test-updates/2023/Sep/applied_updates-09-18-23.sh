79350ed4d8ed929ed90db049e2a628a6c7e9168e
Voncloft <voncloft@gmail.com>
2023-09-18 07:48:52 -0400

    Minor Updates

---
+++ logs/old-test-updates/2023/Sep/applied_updates-09-18-23.sh
@@ -0,0 +1,42 @@
+sed -i -e "s/version=3.27.4/version=3.27.5/g" compilers/cmake/spkgbuild
+changelog "compilers/cmake/spkgbuild" "Upgraded from version 3.27.4 to version 3.27.5"
+sed -i -e "s/version=2.41.92/version=2.42.0/g" core/webkitgtk/spkgbuild
+changelog "core/webkitgtk/spkgbuild" "Upgraded from version 2.41.92 to version 2.42.0"
+sed -i -e "s/version=2.40.4/version=2.41/g" extra/discordchatexporter/spkgbuild
+changelog "extra/discordchatexporter/spkgbuild" "Upgraded from version 2.40.4 to version 2.41"
+sed -i -e "s/version=1.1.9rc1/version=1.1.10rc3/g" extra/solaar/spkgbuild
+changelog "extra/solaar/spkgbuild" "Upgraded from version 1.1.9rc1 to version 1.1.10rc3"
+sed -i -e "s/version=1.26.1/version=1.26.2/g" mate/mate-desktop/spkgbuild
+changelog "mate/mate-desktop/spkgbuild" "Upgraded from version 1.26.1 to version 1.26.2"
+sed -i -e "s/version=118.0b8/version=118.0b9/g" nonfree/firefox/spkgbuild
+changelog "nonfree/firefox/spkgbuild" "Upgraded from version 118.0b8 to version 118.0b9"
+sed -i -e "s/version=1.28.47/version=1.28.49/g" python/python-boto3/spkgbuild
+changelog "python/python-boto3/spkgbuild" "Upgraded from version 1.28.47 to version 1.28.49"
+sed -i -e "s/version=1.31.47/version=1.31.49/g" python/python-botocore/spkgbuild
+changelog "python/python-botocore/spkgbuild" "Upgraded from version 1.31.47 to version 1.31.49"
+sed -i -e "s/version=2.0.7/version=2.0.8/g" python/python-cymem/spkgbuild
+changelog "python/python-cymem/spkgbuild" "Upgraded from version 2.0.7 to version 2.0.8"
+sed -i -e "s/version=2023.9.1/version=2023.9.2/g" python/python-distributed/spkgbuild
+changelog "python/python-distributed/spkgbuild" "Upgraded from version 2023.9.1 to version 2023.9.2"
+sed -i -e "s/version=2023.9.0/version=2023.9.1/g" python/python-fsspec/spkgbuild
+changelog "python/python-fsspec/spkgbuild" "Upgraded from version 2023.9.0 to version 2023.9.1"
+sed -i -e "s/version=6.84.3/version=6.86.1/g" python/python-hypothesis/spkgbuild
+changelog "python/python-hypothesis/spkgbuild" "Upgraded from version 6.84.3 to version 6.86.1"
+sed -i -e "s/version=2.5.28/version=2.5.29/g" python/python-identify/spkgbuild
+changelog "python/python-identify/spkgbuild" "Upgraded from version 2.5.28 to version 2.5.29"
+sed -i -e "s/version=0.4.5/version=0.4.6/g" python/python-jsonschema-objects/spkgbuild
+changelog "python/python-jsonschema-objects/spkgbuild" "Upgraded from version 0.4.5 to version 0.4.6"
+sed -i -e "s/version=1.3.0/version=1.4.0/g" python/python-kikit/spkgbuild
+changelog "python/python-kikit/spkgbuild" "Upgraded from version 1.3.0 to version 1.4.0"
+sed -i -e "s/version=6.2.0/version=6.3.0/g" python/python-openstackclient/spkgbuild
+changelog "python/python-openstackclient/spkgbuild" "Upgraded from version 6.2.0 to version 6.3.0"
+sed -i -e "s/version=3.18.0/version=3.19.0/g" python/python-pycryptodome/spkgbuild
+changelog "python/python-pycryptodome/spkgbuild" "Upgraded from version 3.18.0 to version 3.19.0"
+sed -i -e "s/version=13.5.2/version=13.5.3/g" python/python-rich/spkgbuild
+changelog "python/python-rich/spkgbuild" "Upgraded from version 13.5.2 to version 13.5.3"
+sed -i -e "s/version=7.1.0/version=7.2.0/g" python/python-testfixtures/spkgbuild
+changelog "python/python-testfixtures/spkgbuild" "Upgraded from version 7.1.0 to version 7.2.0"
+sed -i -e "s/version=3.16.2/version=3.17.0/g" python/python-zipp/spkgbuild
+changelog "python/python-zipp/spkgbuild" "Upgraded from version 3.16.2 to version 3.17.0"
+sed -i -e "s/version=3.14.4/version=3.14.5/g" server/blacklist-hosts/spkgbuild
+changelog "server/blacklist-hosts/spkgbuild" "Upgraded from version 3.14.4 to version 3.14.5"
