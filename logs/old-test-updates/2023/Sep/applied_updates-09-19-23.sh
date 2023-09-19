651a83391cf9f194201d734cca8ac3402a49ef7b
Voncloft <voncloft@gmail.com>
2023-09-19 09:05:25 -0400

    Minor Updates

---
+++ logs/old-test-updates/2023/Sep/applied_updates-09-19-23.sh
@@ -0,0 +1,20 @@
+sed -i -e "s/version=4.8.0/version=4.8.1/g" core/qalculate/spkgbuild
+changelog "core/qalculate/spkgbuild" "Upgraded from version 4.8.0 to version 4.8.1"
+sed -i -e "s/version=0.59.1/version=0.59.2/g" core/xbps/spkgbuild
+changelog "core/xbps/spkgbuild" "Upgraded from version 0.59.1 to version 0.59.2"
+sed -i -e "s/version=3.9.9/version=3.9.10/g" extra/flexget/spkgbuild
+changelog "extra/flexget/spkgbuild" "Upgraded from version 3.9.9 to version 3.9.10"
+sed -i -e "s/version=4.8.0/version=4.8.1/g" libs/libqalculate/spkgbuild
+changelog "libs/libqalculate/spkgbuild" "Upgraded from version 4.8.0 to version 4.8.1"
+sed -i -e "s/version=0.4.8.5/version=0.4.8.6/g" networking/tor/spkgbuild
+changelog "networking/tor/spkgbuild" "Upgraded from version 0.4.8.5 to version 0.4.8.6"
+sed -i -e "s/version=6.86.1/version=6.86.2/g" python/python-hypothesis/spkgbuild
+changelog "python/python-hypothesis/spkgbuild" "Upgraded from version 6.86.1 to version 6.86.2"
+sed -i -e "s/version=4.11.0/version=4.12.0/g" python/python-pwntools/spkgbuild
+changelog "python/python-pwntools/spkgbuild" "Upgraded from version 4.11.0 to version 4.12.0"
+sed -i -e "s/version=0.25.0/version=0.26.0/g" python/python-pytest-httpx/spkgbuild
+changelog "python/python-pytest-httpx/spkgbuild" "Upgraded from version 0.25.0 to version 0.26.0"
+sed -i -e "s/version=3.1.3/version=3.1.4/g" python/python-xlsxwriter/spkgbuild
+changelog "python/python-xlsxwriter/spkgbuild" "Upgraded from version 3.1.3 to version 3.1.4"
+sed -i -e "s/version=3.14.5/version=3.14.6/g" server/blacklist-hosts/spkgbuild
+changelog "server/blacklist-hosts/spkgbuild" "Upgraded from version 3.14.5 to version 3.14.6"
