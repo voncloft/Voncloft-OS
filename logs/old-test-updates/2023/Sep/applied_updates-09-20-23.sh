c9140ef79ab5b25a17dd13673b0ebe5e4af26a61
Voncloft <voncloft@gmail.com>
2023-09-20 06:16:21 -0400

    Minor Updates

---
+++ logs/old-test-updates/2023/Sep/applied_updates-09-20-23.sh
@@ -0,0 +1,40 @@
+sed -i -e "s/version=16.0.6/version=17.0.1/g" compilers/clang/spkgbuild
+changelog "compilers/clang/spkgbuild" "Upgraded from version 16.0.6 to version 17.0.1"
+sed -i -e "s/version=16.0.6/version=17.0.1/g" compilers/lld/spkgbuild
+changelog "compilers/lld/spkgbuild" "Upgraded from version 16.0.6 to version 17.0.1"
+sed -i -e "s/version=16.0.6/version=17.0.1/g" compilers/llvm/spkgbuild
+changelog "compilers/llvm/spkgbuild" "Upgraded from version 16.0.6 to version 17.0.1"
+sed -i -e "s/version=16.0.6/version=17.0.1/g" compilers/llvm-bak/spkgbuild
+changelog "compilers/llvm-bak/spkgbuild" "Upgraded from version 16.0.6 to version 17.0.1"
+sed -i -e "s/version=16.0.6/version=17.0.1/g" compilers/llvm-old-2023-06-07/spkgbuild
+changelog "compilers/llvm-old-2023-06-07/spkgbuild" "Upgraded from version 16.0.6 to version 17.0.1"
+sed -i -e "s/version=1.72.0/version=1.72.1/g" compilers/rust/spkgbuild
+changelog "compilers/rust/spkgbuild" "Upgraded from version 1.72.0 to version 1.72.1"
+sed -i -e "s/version=0.22/version=0.22.1/g" core/gettext/spkgbuild
+changelog "core/gettext/spkgbuild" "Upgraded from version 0.22 to version 0.22.1"
+sed -i -e "s/version=6.5.3/version=6.5.4/g" core/linux-api-headers/spkgbuild
+changelog "core/linux-api-headers/spkgbuild" "Upgraded from version 6.5.3 to version 6.5.4"
+sed -i -e "s/version=6.5.3/version=6.5.4/g" core/linux-desktop/spkgbuild
+changelog "core/linux-desktop/spkgbuild" "Upgraded from version 6.5.3 to version 6.5.4"
+sed -i -e "s/version=6.5.3/version=6.5.4/g" core/linux-router/spkgbuild
+changelog "core/linux-router/spkgbuild" "Upgraded from version 6.5.3 to version 6.5.4"
+sed -i -e "s/version=16.0.6/version=17.0.1/g" multilib/llvm-32/spkgbuild
+changelog "multilib/llvm-32/spkgbuild" "Upgraded from version 16.0.6 to version 17.0.1"
+sed -i -e "s/version=3.1.2/version=3.1.3/g" networking/openssl/spkgbuild
+changelog "networking/openssl/spkgbuild" "Upgraded from version 3.1.2 to version 3.1.3"
+sed -i -e "s/version=1.28.49/version=1.28.50/g" python/python-boto3/spkgbuild
+changelog "python/python-boto3/spkgbuild" "Upgraded from version 1.28.49 to version 1.28.50"
+sed -i -e "s/version=1.31.49/version=1.31.50/g" python/python-botocore/spkgbuild
+changelog "python/python-botocore/spkgbuild" "Upgraded from version 1.31.49 to version 1.31.50"
+sed -i -e "s/version=2.11.7/version=2.12.2/g" python/python-cloudflare/spkgbuild
+changelog "python/python-cloudflare/spkgbuild" "Upgraded from version 2.11.7 to version 2.12.2"
+sed -i -e "s/version=0.18/version=0.19/g" python/python-fasteners/spkgbuild
+changelog "python/python-fasteners/spkgbuild" "Upgraded from version 0.18 to version 0.19"
+sed -i -e "s/version=2.99.0/version=2.100.0/g" python/python-google-api-python-client/spkgbuild
+changelog "python/python-google-api-python-client/spkgbuild" "Upgraded from version 2.99.0 to version 2.100.0"
+sed -i -e "s/version=0.8.0/version=0.9.0/g" python/python-netaddr/spkgbuild
+changelog "python/python-netaddr/spkgbuild" "Upgraded from version 0.8.0 to version 0.9.0"
+sed -i -e "s/version=3.6.0/version=3.6.1/g" python/python-pyproj/spkgbuild
+changelog "python/python-pyproj/spkgbuild" "Upgraded from version 3.6.0 to version 3.6.1"
+sed -i -e "s/version=0.8.2/version=0.9.0/g" xfce/xfce4-notifyd/spkgbuild
+changelog "xfce/xfce4-notifyd/spkgbuild" "Upgraded from version 0.8.2 to version 0.9.0"
