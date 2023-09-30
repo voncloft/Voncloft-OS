87d6673300520be06cbcf872146c0657c18555a4
Voncloft <voncloft@gmail.com>
2023-09-30 11:05:18 -0400

    Minor Updates

---
+++ logs/old-test-updates/2023/Sep/applied_updates-09-30-23.sh
@@ -0,0 +1,36 @@
+sed -i -e "s/version=1.2.1/version=1.2.2/g" compilers/meson/spkgbuild
+changelog "compilers/meson/spkgbuild" "Upgraded from version 1.2.1 to version 1.2.2"
+sed -i -e "s/version=5.69/version=5.70/g" core/bluez/spkgbuild
+changelog "core/bluez/spkgbuild" "Upgraded from version 5.69 to version 5.70"
+sed -i -e "s/version=2.41/version=2.41.1/g" extra/discordchatexporter/spkgbuild
+changelog "extra/discordchatexporter/spkgbuild" "Upgraded from version 2.41 to version 2.41.1"
+sed -i -e "s/version=1.32.1/version=1.32.2/g" media/mpg123/spkgbuild
+changelog "media/mpg123/spkgbuild" "Upgraded from version 1.32.1 to version 1.32.2"
+sed -i -e "s/version=119.0b2/version=119.0b3/g" nonfree/firefox/spkgbuild
+changelog "nonfree/firefox/spkgbuild" "Upgraded from version 119.0b2 to version 119.0b3"
+sed -i -e "s/version=1.75.0/version=1.76.0/g" python/python-aws-sam-translator/spkgbuild
+changelog "python/python-aws-sam-translator/spkgbuild" "Upgraded from version 1.75.0 to version 1.76.0"
+sed -i -e "s/version=1.28.56/version=1.28.57/g" python/python-boto3/spkgbuild
+changelog "python/python-boto3/spkgbuild" "Upgraded from version 1.28.56 to version 1.28.57"
+sed -i -e "s/version=1.31.56/version=1.31.57/g" python/python-botocore/spkgbuild
+changelog "python/python-botocore/spkgbuild" "Upgraded from version 1.31.56 to version 1.31.57"
+sed -i -e "s/version=0.11.0/version=0.12.0/g" python/python-cycler/spkgbuild
+changelog "python/python-cycler/spkgbuild" "Upgraded from version 0.11.0 to version 0.12.0"
+sed -i -e "s/version=0.103.1/version=0.103.2/g" python/python-fastapi/spkgbuild
+changelog "python/python-fastapi/spkgbuild" "Upgraded from version 0.103.1 to version 0.103.2"
+sed -i -e "s/version=4.42.1/version=4.43.0/g" python/python-fonttools/spkgbuild
+changelog "python/python-fonttools/spkgbuild" "Upgraded from version 4.42.1 to version 4.43.0"
+sed -i -e "s/version=2.23.1/version=2.23.2/g" python/python-google-auth/spkgbuild
+changelog "python/python-google-auth/spkgbuild" "Upgraded from version 2.23.1 to version 2.23.2"
+sed -i -e "s/version=1.0.6/version=1.0.7/g" python/python-msgpack/spkgbuild
+changelog "python/python-msgpack/spkgbuild" "Upgraded from version 1.0.6 to version 1.0.7"
+sed -i -e "s/version=8.13.21/version=8.13.22/g" python/python-phonenumbers/spkgbuild
+changelog "python/python-phonenumbers/spkgbuild" "Upgraded from version 8.13.21 to version 8.13.22"
+sed -i -e "s/version=1.59.1/version=2.1.0/g" python/python-pygithub/spkgbuild
+changelog "python/python-pygithub/spkgbuild" "Upgraded from version 1.59.1 to version 2.1.0"
+sed -i -e "s/version=1.9.0/version=1.10.0/g" python/python-rope/spkgbuild
+changelog "python/python-rope/spkgbuild" "Upgraded from version 1.9.0 to version 1.10.0"
+sed -i -e "s/version=8.2.10/version=8.2.11/g" server/php/spkgbuild
+changelog "server/php/spkgbuild" "Upgraded from version 8.2.10 to version 8.2.11"
+sed -i -e "s/version=1.6.4/version=1.6.5/g" xfce/xfce4-clipman-plugin/spkgbuild
+changelog "xfce/xfce4-clipman-plugin/spkgbuild" "Upgraded from version 1.6.4 to version 1.6.5"
