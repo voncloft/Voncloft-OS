247abb69ce9d14cd915d05548b1a10748d79cb64
Voncloft <voncloft@gmail.com>
2023-10-02 07:29:32 -0400

    Minor Updates

---
+++ logs/old-test-updates/2023/Oct/applied_updates-10-02-23.sh
@@ -0,0 +1,36 @@
+sed -i -e "s/version=1.13/version=1.14/g" core/pugixml/spkgbuild
+changelog "core/pugixml/spkgbuild" "Upgraded from version 1.13 to version 1.14"
+sed -i -e "s/version=1.23/version=1.24/g" extra/iotop/spkgbuild
+changelog "extra/iotop/spkgbuild" "Upgraded from version 1.23 to version 1.24"
+sed -i -e "s/version=1.13.0/version=1.13.1/g" libs/libvpx/spkgbuild
+changelog "libs/libvpx/spkgbuild" "Upgraded from version 1.13.0 to version 1.13.1"
+sed -i -e "s/version=6.15.8/version=6.15.9/g" python/python-cx-freeze/spkgbuild
+changelog "python/python-cx-freeze/spkgbuild" "Upgraded from version 6.15.8 to version 6.15.9"
+sed -i -e "s/version=2023.9.2/version=2023.9.3/g" python/python-distributed/spkgbuild
+changelog "python/python-distributed/spkgbuild" "Upgraded from version 2023.9.2 to version 2023.9.3"
+sed -i -e "s/version=2.3.3/version=3.0.0/g" python/python-flask/spkgbuild
+changelog "python/python-flask/spkgbuild" "Upgraded from version 2.3.3 to version 3.0.0"
+sed -i -e "s/version=1.1.1/version=1.2.0/g" python/python-flask-wtf/spkgbuild
+changelog "python/python-flask-wtf/spkgbuild" "Upgraded from version 1.1.1 to version 1.2.0"
+sed -i -e "s/version=6.87.0/version=6.87.1/g" python/python-hypothesis/spkgbuild
+changelog "python/python-hypothesis/spkgbuild" "Upgraded from version 6.87.0 to version 6.87.1"
+sed -i -e "s/version=2.5.29/version=2.5.30/g" python/python-identify/spkgbuild
+changelog "python/python-identify/spkgbuild" "Upgraded from version 2.5.29 to version 2.5.30"
+sed -i -e "s/version=3.0.1/version=3.0.2/g" python/python-mistune/spkgbuild
+changelog "python/python-mistune/spkgbuild" "Upgraded from version 3.0.1 to version 3.0.2"
+sed -i -e "s/version=23.1/version=23.2/g" python/python-packaging/spkgbuild
+changelog "python/python-packaging/spkgbuild" "Upgraded from version 23.1 to version 23.2"
+sed -i -e "s/version=7.3.2308311245/version=7.3.2309301657/g" python/python-pycontracts/spkgbuild
+changelog "python/python-pycontracts/spkgbuild" "Upgraded from version 7.3.2308311245 to version 7.3.2309301657"
+sed -i -e "s/version=2.1.0/version=2.1.1/g" python/python-pygithub/spkgbuild
+changelog "python/python-pygithub/spkgbuild" "Upgraded from version 2.1.0 to version 2.1.1"
+sed -i -e "s/version=13.5.3/version=13.6.0/g" python/python-rich/spkgbuild
+changelog "python/python-rich/spkgbuild" "Upgraded from version 13.5.3 to version 13.6.0"
+sed -i -e "s/version=2.3.7/version=3.0.0/g" python/python-werkzeug/spkgbuild
+changelog "python/python-werkzeug/spkgbuild" "Upgraded from version 2.3.7 to version 3.0.0"
+sed -i -e "s/version=3.1.5/version=3.1.6/g" python/python-xlsxwriter/spkgbuild
+changelog "python/python-xlsxwriter/spkgbuild" "Upgraded from version 3.1.5 to version 3.1.6"
+sed -i -e "s/version=0.115.0/version=0.115.1/g" python/python-zeroconf/spkgbuild
+changelog "python/python-zeroconf/spkgbuild" "Upgraded from version 0.115.0 to version 0.115.1"
+sed -i -e "s/version=3.14.9/version=3.14.10/g" server/blacklist-hosts/spkgbuild
+changelog "server/blacklist-hosts/spkgbuild" "Upgraded from version 3.14.9 to version 3.14.10"
