2d9ba4e0d988128499f184071d0f3fd00cc9d645
Voncloft <voncloft@gmail.com>
2023-10-10 11:45:20 -0400

    Minor Updates

---
+++ logs/old-test-updates/2023/Oct/applied_updates-10-10-23.sh
@@ -0,0 +1,34 @@
+sed -i -e "s/version=4.3.4/version=4.3.5/g" core/zeromq/spkgbuild
+changelog "core/zeromq/spkgbuild" "Upgraded from version 4.3.4 to version 4.3.5"
+sed -i -e "s/version=3.9.11/version=3.9.12/g" extra/flexget/spkgbuild
+changelog "extra/flexget/spkgbuild" "Upgraded from version 3.9.11 to version 3.9.12"
+sed -i -e "s/version=3.0.18/version=3.0.19/g" media/vlc/spkgbuild
+changelog "media/vlc/spkgbuild" "Upgraded from version 3.0.18 to version 3.0.19"
+sed -i -e "s/version=1.20.0/version=1.20.1/g" libs/c-ares/spkgbuild
+changelog "libs/c-ares/spkgbuild" "Upgraded from version 1.20.0 to version 1.20.1"
+sed -i -e "s/version=1.5.0/version=1.6.0/g" libs/libxkbcommon/spkgbuild
+changelog "libs/libxkbcommon/spkgbuild" "Upgraded from version 1.5.0 to version 1.6.0"
+sed -i -e "s/version=5.5.18/version=5.5.19/g" networking/whois/spkgbuild
+changelog "networking/whois/spkgbuild" "Upgraded from version 5.5.18 to version 5.5.19"
+sed -i -e "s/version=119.0b6/version=119.0b7/g" nonfree/firefox/spkgbuild
+changelog "nonfree/firefox/spkgbuild" "Upgraded from version 119.0b6 to version 119.0b7"
+sed -i -e "s/version=19.6.2/version=19.8.0/g" python/python-faker/spkgbuild
+changelog "python/python-faker/spkgbuild" "Upgraded from version 19.6.2 to version 19.8.0"
+sed -i -e "s/version=2.0.2/version=2.1.0/g" python/python-flask-caching/spkgbuild
+changelog "python/python-flask-caching/spkgbuild" "Upgraded from version 2.0.2 to version 2.1.0"
+sed -i -e "s/version=0.6.0/version=0.7.0/g" python/python-openapi-spec-validator/spkgbuild
+changelog "python/python-openapi-spec-validator/spkgbuild" "Upgraded from version 0.6.0 to version 0.7.0"
+sed -i -e "s/version=0.9.2/version=0.9.3/g" python/python-pytest-repeat/spkgbuild
+changelog "python/python-pytest-repeat/spkgbuild" "Upgraded from version 0.9.2 to version 0.9.3"
+sed -i -e "s/version=2.1.0/version=2.2.0/g" python/python-pytest-timeout/spkgbuild
+changelog "python/python-pytest-timeout/spkgbuild" "Upgraded from version 2.1.0 to version 2.2.0"
+sed -i -e "s/version=3.0.1/version=3.0.2/g" python/python-semver/spkgbuild
+changelog "python/python-semver/spkgbuild" "Upgraded from version 3.0.1 to version 3.0.2"
+sed -i -e "s/version=23.1.0/version=23.2.0/g" python/python-structlog/spkgbuild
+changelog "python/python-structlog/spkgbuild" "Upgraded from version 23.1.0 to version 23.2.0"
+sed -i -e "s/version=6.4.2/version=6.4.3/g" python/python-traits/spkgbuild
+changelog "python/python-traits/spkgbuild" "Upgraded from version 6.4.2 to version 6.4.3"
+sed -i -e "s/version=3.1.6/version=3.1.7/g" python/python-xlsxwriter/spkgbuild
+changelog "python/python-xlsxwriter/spkgbuild" "Upgraded from version 3.1.6 to version 3.1.7"
+sed -i -e "s/version=3.14.12/version=3.14.13/g" server/blacklist-hosts/spkgbuild
+changelog "server/blacklist-hosts/spkgbuild" "Upgraded from version 3.14.12 to version 3.14.13"
