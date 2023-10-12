6988ad96b43333c07b500f899c27266fdab30117
Voncloft <voncloft@gmail.com>
2023-10-12 10:16:38 -0400

    Minor Updates

---
+++ logs/old-test-updates/2023/Oct/applied_updates-10-12-23.sh
@@ -0,0 +1,46 @@
+sed -i -e "s/version=1.19.4/version=1.19.5/g" core/conky/spkgbuild
+changelog "core/conky/spkgbuild" "Upgraded from version 1.19.4 to version 1.19.5"
+sed -i -e "s/version=8.3.0/version=8.4.0/g" core/curl/spkgbuild
+changelog "core/curl/spkgbuild" "Upgraded from version 8.3.0 to version 8.4.0"
+sed -i -e "s/version=3.16.1/version=3.16.2/g" core/fuse2/spkgbuild
+changelog "core/fuse2/spkgbuild" "Upgraded from version 3.16.1 to version 3.16.2"
+sed -i -e "s/version=3.16.1/version=3.16.2/g" core/fuse3/spkgbuild
+changelog "core/fuse3/spkgbuild" "Upgraded from version 3.16.1 to version 3.16.2"
+sed -i -e "s/version=6.5.6/version=6.5.7/g" core/linux-api-headers/spkgbuild
+changelog "core/linux-api-headers/spkgbuild" "Upgraded from version 6.5.6 to version 6.5.7"
+sed -i -e "s/version=6.5.6/version=6.5.7/g" core/linux-desktop/spkgbuild
+changelog "core/linux-desktop/spkgbuild" "Upgraded from version 6.5.6 to version 6.5.7"
+sed -i -e "s/version=6.5.6/version=6.5.7/g" core/linux-router/spkgbuild
+changelog "core/linux-router/spkgbuild" "Upgraded from version 6.5.6 to version 6.5.7"
+sed -i -e "s/version=3.2.2.1/version=3.2.3/g" core/wxgtk3/spkgbuild
+changelog "core/wxgtk3/spkgbuild" "Upgraded from version 3.2.2.1 to version 3.2.3"
+sed -i -e "s/version=2.2.1/version=2.3.0/g" libs/libcue/spkgbuild
+changelog "libs/libcue/spkgbuild" "Upgraded from version 2.2.1 to version 2.3.0"
+sed -i -e "s/version=0.44.0/version=0.45.0/g" libs/libfilezilla/spkgbuild
+changelog "libs/libfilezilla/spkgbuild" "Upgraded from version 0.44.0 to version 0.45.0"
+sed -i -e "s/version=119.0b7/version=119.0b8/g" nonfree/firefox/spkgbuild
+changelog "nonfree/firefox/spkgbuild" "Upgraded from version 119.0b7 to version 119.0b8"
+sed -i -e "s/version=119.0b3/version=119.0b4/g" nonfree/thunderbird/spkgbuild
+changelog "nonfree/thunderbird/spkgbuild" "Upgraded from version 119.0b3 to version 119.0b4"
+sed -i -e "s/version=4.22.0/version=4.23.0/g" python/python-braintree/spkgbuild
+changelog "python/python-braintree/spkgbuild" "Upgraded from version 4.22.0 to version 4.23.0"
+sed -i -e "s/version=19.8.0/version=19.10.0/g" python/python-faker/spkgbuild
+changelog "python/python-faker/spkgbuild" "Upgraded from version 19.8.0 to version 19.10.0"
+sed -i -e "s/version=2.102.0/version=2.103.0/g" python/python-google-api-python-client/spkgbuild
+changelog "python/python-google-api-python-client/spkgbuild" "Upgraded from version 2.102.0 to version 2.103.0"
+sed -i -e "s/version=2.23.2/version=2.23.3/g" python/python-google-auth/spkgbuild
+changelog "python/python-google-auth/spkgbuild" "Upgraded from version 2.23.2 to version 2.23.3"
+sed -i -e "s/version=5.3.2/version=5.4.0/g" python/python-jupyter_core/spkgbuild
+changelog "python/python-jupyter_core/spkgbuild" "Upgraded from version 5.3.2 to version 5.4.0"
+sed -i -e "s/version=3.1/version=3.2/g" python/python-networkx/spkgbuild
+changelog "python/python-networkx/spkgbuild" "Upgraded from version 3.1 to version 3.2"
+sed -i -e "s/version=3.9.7/version=3.9.8/g" python/python-orjson/spkgbuild
+changelog "python/python-orjson/spkgbuild" "Upgraded from version 3.9.7 to version 3.9.8"
+sed -i -e "s/version=3.0.1/version=3.1.0/g" python/python-wtforms/spkgbuild
+changelog "python/python-wtforms/spkgbuild" "Upgraded from version 3.0.1 to version 3.1.0"
+sed -i -e "s/version=1.8.9/version=1.8.10/g" server/iptables/spkgbuild
+changelog "server/iptables/spkgbuild" "Upgraded from version 1.8.9 to version 1.8.10"
+sed -i -e "s/version=4.19.0/version=4.19.1/g" server/samba/spkgbuild
+changelog "server/samba/spkgbuild" "Upgraded from version 4.19.0 to version 4.19.1"
+sed -i -e "s/version=4.18.7/version=4.19.0/g" xfce/thunar/spkgbuild
+changelog "xfce/thunar/spkgbuild" "Upgraded from version 4.18.7 to version 4.19.0"
