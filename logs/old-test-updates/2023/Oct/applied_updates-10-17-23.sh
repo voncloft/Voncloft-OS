4d36baa1a3ae4f44a2b658677c1d7c50e78869bd
Voncloft <voncloft@gmail.com>
2023-10-17 11:51:19 -0400

    Minor Updates

---
+++ logs/old-test-updates/2023/Oct/applied_updates-10-17-23.sh
@@ -0,0 +1,88 @@
+sed -i -e "s/version=1.19.5/version=1.19.6/g" core/conky/spkgbuild
+changelog "core/conky/spkgbuild" "Upgraded from version 1.19.5 to version 1.19.6"
+sed -i -e "s/version=1.6.1/version=1.7.0/g" core/cronie/spkgbuild
+changelog "core/cronie/spkgbuild" "Upgraded from version 1.6.1 to version 1.7.0"
+sed -i -e "s/version=0.3.81/version=0.3.82/g" core/pipewire/spkgbuild
+changelog "core/pipewire/spkgbuild" "Upgraded from version 0.3.81 to version 0.3.82"
+sed -i -e "s/version=4.0.5/version=4.1.0/g" core/weechat/spkgbuild
+changelog "core/weechat/spkgbuild" "Upgraded from version 4.0.5 to version 4.1.0"
+sed -i -e "s/version=3.9.12/version=3.9.14/g" extra/flexget/spkgbuild
+changelog "extra/flexget/spkgbuild" "Upgraded from version 3.9.12 to version 3.9.14"
+sed -i -e "s/version=1.24/version=1.25/g" extra/iotop/spkgbuild
+changelog "extra/iotop/spkgbuild" "Upgraded from version 1.24 to version 1.25"
+sed -i -e "s/version=3.0.16/version=3.0.17/g" libs/libical/spkgbuild
+changelog "libs/libical/spkgbuild" "Upgraded from version 3.0.16 to version 3.0.17"
+sed -i -e "s/version=4.2.1/version=4.2.2/g" libs/libimagequant/spkgbuild
+changelog "libs/libimagequant/spkgbuild" "Upgraded from version 4.2.1 to version 4.2.2"
+sed -i -e "s/version=3.0.0/version=3.0.1/g" libs/libjpeg-turbo/spkgbuild
+changelog "libs/libjpeg-turbo/spkgbuild" "Upgraded from version 3.0.0 to version 3.0.1"
+sed -i -e "s/version=2.0.0/version=2.0.1/g" libs/libnsl/spkgbuild
+changelog "libs/libnsl/spkgbuild" "Upgraded from version 2.0.0 to version 2.0.1"
+sed -i -e "s/version=4.19.2/version=4.19.3/g" libs/libxfce4ui/spkgbuild
+changelog "libs/libxfce4ui/spkgbuild" "Upgraded from version 4.19.2 to version 4.19.3"
+sed -i -e "s/version=6.9.8/version=6.9.9/g" libs/oniguruma/spkgbuild
+changelog "libs/oniguruma/spkgbuild" "Upgraded from version 6.9.8 to version 6.9.9"
+sed -i -e "s/version=2023.10.07/version=2023.10.13/g" networking/yt-dlp/spkgbuild
+changelog "networking/yt-dlp/spkgbuild" "Upgraded from version 2023.10.07 to version 2023.10.13"
+sed -i -e "s/version=119.0b8/version=119.0b9/g" nonfree/firefox/spkgbuild
+changelog "nonfree/firefox/spkgbuild" "Upgraded from version 119.0b8 to version 119.0b9"
+sed -i -e "s/version=119.0b4/version=119.0b5/g" nonfree/thunderbird/spkgbuild
+changelog "nonfree/thunderbird/spkgbuild" "Upgraded from version 119.0b4 to version 119.0b5"
+sed -i -e "s/version=3.0.0/version=3.0.1/g" python/python-astroid/spkgbuild
+changelog "python/python-astroid/spkgbuild" "Upgraded from version 3.0.0 to version 3.0.1"
+sed -i -e "s/version=1.77.0/version=1.78.0/g" python/python-aws-sam-translator/spkgbuild
+changelog "python/python-aws-sam-translator/spkgbuild" "Upgraded from version 1.77.0 to version 1.78.0"
+sed -i -e "s/version=1.28.62/version=1.28.63/g" python/python-boto3/spkgbuild
+changelog "python/python-boto3/spkgbuild" "Upgraded from version 1.28.62 to version 1.28.63"
+sed -i -e "s/version=1.31.62/version=1.31.63/g" python/python-botocore/spkgbuild
+changelog "python/python-botocore/spkgbuild" "Upgraded from version 1.31.62 to version 1.31.63"
+sed -i -e "s/version=2023.9.3/version=2023.10.0/g" python/python-distributed/spkgbuild
+changelog "python/python-distributed/spkgbuild" "Upgraded from version 2023.9.3 to version 2023.10.0"
+sed -i -e "s/version=8.10.0/version=8.10.1/g" python/python-elasticsearch/spkgbuild
+changelog "python/python-elasticsearch/spkgbuild" "Upgraded from version 8.10.0 to version 8.10.1"
+sed -i -e "s/version=4.7.1/version=4.8.0/g" python/python-engineio/spkgbuild
+changelog "python/python-engineio/spkgbuild" "Upgraded from version 4.7.1 to version 4.8.0"
+sed -i -e "s/version=5.3.0/version=5.3.1/g" python/python-flask-security-too/spkgbuild
+changelog "python/python-flask-security-too/spkgbuild" "Upgraded from version 5.3.0 to version 5.3.1"
+sed -i -e "s/version=1.29.1/version=1.30.0/g" python/python-hcloud/spkgbuild
+changelog "python/python-hcloud/spkgbuild" "Upgraded from version 1.29.1 to version 1.30.0"
+sed -i -e "s/version=6.87.3/version=6.88.1/g" python/python-hypothesis/spkgbuild
+changelog "python/python-hypothesis/spkgbuild" "Upgraded from version 6.87.3 to version 6.88.1"
+sed -i -e "s/version=0.7.0/version=0.7.1/g" python/python-openapi-spec-validator/spkgbuild
+changelog "python/python-openapi-spec-validator/spkgbuild" "Upgraded from version 0.7.0 to version 0.7.1"
+sed -i -e "s/version=3.9.8/version=3.9.9/g" python/python-orjson/spkgbuild
+changelog "python/python-orjson/spkgbuild" "Upgraded from version 3.9.8 to version 3.9.9"
+sed -i -e "s/version=3.16.3/version=3.17.0/g" python/python-peewee/spkgbuild
+changelog "python/python-peewee/spkgbuild" "Upgraded from version 3.16.3 to version 3.17.0"
+sed -i -e "s/version=23.2.1/version=23.3/g" python/python-pip/spkgbuild
+changelog "python/python-pip/spkgbuild" "Upgraded from version 23.2.1 to version 23.3"
+sed -i -e "s/version=3.4.0/version=3.5.0/g" python/python-pre-commit/spkgbuild
+changelog "python/python-pre-commit/spkgbuild" "Upgraded from version 3.4.0 to version 3.5.0"
+sed -i -e "s/version=5.9.5/version=5.9.6/g" python/python-psutil/spkgbuild
+changelog "python/python-psutil/spkgbuild" "Upgraded from version 5.9.5 to version 5.9.6"
+sed -i -e "s/version=2.11.0/version=2.11.1/g" python/python-pycodestyle/spkgbuild
+changelog "python/python-pycodestyle/spkgbuild" "Upgraded from version 2.11.0 to version 2.11.1"
+sed -i -e "s/version=5.0.0/version=5.0.1/g" python/python-pyodbc/spkgbuild
+changelog "python/python-pyodbc/spkgbuild" "Upgraded from version 5.0.0 to version 5.0.1"
+sed -i -e "s/version=5.9.0/version=5.10.0/g" python/python-socketio/spkgbuild
+changelog "python/python-socketio/spkgbuild" "Upgraded from version 5.9.0 to version 5.10.0"
+sed -i -e "s/version=6.1.9/version=6.1.10/g" python/python-softlayer/spkgbuild
+changelog "python/python-softlayer/spkgbuild" "Upgraded from version 6.1.9 to version 6.1.10"
+sed -i -e "s/version=0.17.0/version=0.18.0/g" python/python-uvloop/spkgbuild
+changelog "python/python-uvloop/spkgbuild" "Upgraded from version 0.17.0 to version 0.18.0"
+sed -i -e "s/version=3.1.7/version=3.1.8/g" python/python-xlsxwriter/spkgbuild
+changelog "python/python-xlsxwriter/spkgbuild" "Upgraded from version 3.1.7 to version 3.1.8"
+sed -i -e "s/version=0.115.2/version=0.118.0/g" python/python-zeroconf/spkgbuild
+changelog "python/python-zeroconf/spkgbuild" "Upgraded from version 0.115.2 to version 0.118.0"
+sed -i -e "s/version=3.14.14/version=3.14.15/g" server/blacklist-hosts/spkgbuild
+changelog "server/blacklist-hosts/spkgbuild" "Upgraded from version 3.14.14 to version 3.14.15"
+sed -i -e "s/version=2023.10.2/version=2023.10.3/g" server/homeassistant/spkgbuild
+changelog "server/homeassistant/spkgbuild" "Upgraded from version 2023.10.2 to version 2023.10.3"
+sed -i -e "s/version=4.19.1/version=4.19.2/g" server/samba/spkgbuild
+changelog "server/samba/spkgbuild" "Upgraded from version 4.19.1 to version 4.19.2"
+sed -i -e "s/version=4.18.0/version=4.19.0/g" xfce/exo/spkgbuild
+changelog "xfce/exo/spkgbuild" "Upgraded from version 4.18.0 to version 4.19.0"
+sed -i -e "s/version=1.1.0/version=1.1.1/g" xfce/xfce4-terminal/spkgbuild
+changelog "xfce/xfce4-terminal/spkgbuild" "Upgraded from version 1.1.0 to version 1.1.1"
+sed -i -e "s/version=6.4.0/version=6.5.0/g" xfce/xfsprogs/spkgbuild
+changelog "xfce/xfsprogs/spkgbuild" "Upgraded from version 6.4.0 to version 6.5.0"
