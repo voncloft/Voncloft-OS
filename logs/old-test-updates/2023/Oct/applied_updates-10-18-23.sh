f165f62cc649558de5aaeccf87361ee4159e623d
Voncloft <voncloft@gmail.com>
2023-10-18 11:14:14 -0400

    Minor Updates

---
+++ logs/old-test-updates/2023/Oct/applied_updates-10-18-23.sh
@@ -0,0 +1,50 @@
+sed -i -e "s/version=17.0.2/version=17.0.3/g" compilers/clang/spkgbuild
+changelog "compilers/clang/spkgbuild" "Upgraded from version 17.0.2 to version 17.0.3"
+sed -i -e "s/version=17.0.2/version=17.0.3/g" compilers/lld/spkgbuild
+changelog "compilers/lld/spkgbuild" "Upgraded from version 17.0.2 to version 17.0.3"
+sed -i -e "s/version=17.0.2/version=17.0.3/g" compilers/llvm/spkgbuild
+changelog "compilers/llvm/spkgbuild" "Upgraded from version 17.0.2 to version 17.0.3"
+sed -i -e "s/version=17.0.2/version=17.0.3/g" compilers/llvm-bak/spkgbuild
+changelog "compilers/llvm-bak/spkgbuild" "Upgraded from version 17.0.2 to version 17.0.3"
+sed -i -e "s/version=17.0.2/version=17.0.3/g" compilers/llvm-old-2023-06-07/spkgbuild
+changelog "compilers/llvm-old-2023-06-07/spkgbuild" "Upgraded from version 17.0.2 to version 17.0.3"
+sed -i -e "s/version=3.9.14/version=3.9.15/g" extra/flexget/spkgbuild
+changelog "extra/flexget/spkgbuild" "Upgraded from version 3.9.14 to version 3.9.15"
+sed -i -e "s/version=17.0.2/version=17.0.3/g" multilib/llvm-32/spkgbuild
+changelog "multilib/llvm-32/spkgbuild" "Upgraded from version 17.0.2 to version 17.0.3"
+sed -i -e "s/version=535.113.01/version=545.23.06/g" nonfree/nvidia/spkgbuild
+changelog "nonfree/nvidia/spkgbuild" "Upgraded from version 535.113.01 to version 545.23.06"
+sed -i -e "s/version=535.113.01/version=545.23.06/g" nonfree/nvidia-32/spkgbuild
+changelog "nonfree/nvidia-32/spkgbuild" "Upgraded from version 535.113.01 to version 545.23.06"
+sed -i -e "s/version=535.113.01/version=545.23.06/g" nonfree/nvidia-modules/spkgbuild
+changelog "nonfree/nvidia-modules/spkgbuild" "Upgraded from version 535.113.01 to version 545.23.06"
+sed -i -e "s/version=119.0b5/version=119.0b6/g" nonfree/thunderbird/spkgbuild
+changelog "nonfree/thunderbird/spkgbuild" "Upgraded from version 119.0b5 to version 119.0b6"
+sed -i -e "s/version=7.0.10.158379/version=7.0.12.159484/g" nonfree/virtualbox/spkgbuild
+changelog "nonfree/virtualbox/spkgbuild" "Upgraded from version 7.0.10.158379 to version 7.0.12.159484"
+sed -i -e "s/version=7.0.10/version=7.0.12/g" nonfree/virtualbox-guest-modules/spkgbuild
+changelog "nonfree/virtualbox-guest-modules/spkgbuild" "Upgraded from version 7.0.10 to version 7.0.12"
+sed -i -e "s/version=7.0.10/version=7.0.12/g" nonfree/virtualbox-guest-utils/spkgbuild
+changelog "nonfree/virtualbox-guest-utils/spkgbuild" "Upgraded from version 7.0.10 to version 7.0.12"
+sed -i -e "s/version=2.6.0/version=2.7.0/g" python/python-aiobotocore/spkgbuild
+changelog "python/python-aiobotocore/spkgbuild" "Upgraded from version 2.6.0 to version 2.7.0"
+sed -i -e "s/version=1.28.63/version=1.28.64/g" python/python-boto3/spkgbuild
+changelog "python/python-boto3/spkgbuild" "Upgraded from version 1.28.63 to version 1.28.64"
+sed -i -e "s/version=1.31.63/version=1.31.64/g" python/python-botocore/spkgbuild
+changelog "python/python-botocore/spkgbuild" "Upgraded from version 1.31.63 to version 1.31.64"
+sed -i -e "s/version=3.15.0/version=4.0.0/g" python/python-gitlab/spkgbuild
+changelog "python/python-gitlab/spkgbuild" "Upgraded from version 3.15.0 to version 4.0.0"
+sed -i -e "s/version=2.103.0/version=2.104.0/g" python/python-google-api-python-client/spkgbuild
+changelog "python/python-google-api-python-client/spkgbuild" "Upgraded from version 2.103.0 to version 2.104.0"
+sed -i -e "s/version=8.13.22/version=8.13.23/g" python/python-phonenumbers/spkgbuild
+changelog "python/python-phonenumbers/spkgbuild" "Upgraded from version 8.13.22 to version 8.13.23"
+sed -i -e "s/version=1.11/version=1.12/g" python/python-pypandoc/spkgbuild
+changelog "python/python-pypandoc/spkgbuild" "Upgraded from version 1.11 to version 1.12"
+sed -i -e "s/version=1.0.1/version=1.1.0/g" python/python-rtree/spkgbuild
+changelog "python/python-rtree/spkgbuild" "Upgraded from version 1.0.1 to version 1.1.0"
+sed -i -e "s/version=2.4.20/version=2.4.21/g" ruby-gems/ruby-bundler/spkgbuild
+changelog "ruby-gems/ruby-bundler/spkgbuild" "Upgraded from version 2.4.20 to version 2.4.21"
+sed -i -e "s/version=3.14.15/version=3.14.16/g" server/blacklist-hosts/spkgbuild
+changelog "server/blacklist-hosts/spkgbuild" "Upgraded from version 3.14.15 to version 3.14.16"
+sed -i -e "s/version=4.19.0/version=4.19.1/g" xfce/xfconf/spkgbuild
+changelog "xfce/xfconf/spkgbuild" "Upgraded from version 4.19.0 to version 4.19.1"
