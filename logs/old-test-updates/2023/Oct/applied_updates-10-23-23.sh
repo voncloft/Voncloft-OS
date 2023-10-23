5a36f8caa45c7d410c354e49a23a857ddb399246
Voncloft <voncloft@gmail.com>
2023-10-23 19:41:58 -0400

    Minor Updates

---
+++ logs/old-test-updates/2023/Oct/applied_updates-10-23-23.sh
@@ -0,0 +1,34 @@
+sed -i -e "s/version=0.29.4/version=0.30.1/g" python/python-argh/spkgbuild
+changelog "python/python-argh/spkgbuild" "Upgraded from version 0.29.4 to version 0.30.1"
+sed -i -e "s/version=2.10.68/version=2.10.69/g" python/python-construct/spkgbuild
+changelog "python/python-construct/spkgbuild" "Upgraded from version 2.10.68 to version 2.10.69"
+sed -i -e "s/version=2.0.0/version=2.1.0/g" python/python-email-validator/spkgbuild
+changelog "python/python-email-validator/spkgbuild" "Upgraded from version 2.0.0 to version 2.1.0"
+sed -i -e "s/version=1.30.0/version=1.31.0/g" python/python-hcloud/spkgbuild
+changelog "python/python-hcloud/spkgbuild" "Upgraded from version 1.30.0 to version 1.31.0"
+sed -i -e "s/version=2.4.0/version=2.6.0/g" python/python-jaraco.envs/spkgbuild
+changelog "python/python-jaraco.envs/spkgbuild" "Upgraded from version 2.4.0 to version 2.6.0"
+sed -i -e "s/version=1.1.7/version=1.1.8/g" python/python-lark-parser/spkgbuild
+changelog "python/python-lark-parser/spkgbuild" "Upgraded from version 1.1.7 to version 1.1.8"
+sed -i -e "s/version=3.5.0/version=3.5.1/g" python/python-octaviaclient/spkgbuild
+changelog "python/python-octaviaclient/spkgbuild" "Upgraded from version 3.5.0 to version 3.5.1"
+sed -i -e "s/version=1.2.0/version=1.2.1/g" python/python-pipx/spkgbuild
+changelog "python/python-pipx/spkgbuild" "Upgraded from version 1.2.0 to version 1.2.1"
+sed -i -e "s/version=2.0.4/version=2.1.0/g" python/python-pyhamcrest/spkgbuild
+changelog "python/python-pyhamcrest/spkgbuild" "Upgraded from version 2.0.4 to version 2.1.0"
+sed -i -e "s/version=3.0.1/version=3.0.2/g" python/python-pylint/spkgbuild
+changelog "python/python-pylint/spkgbuild" "Upgraded from version 3.0.1 to version 3.0.2"
+sed -i -e "s/version=1.3.1/version=1.3.2/g" python/python-scikit-learn/spkgbuild
+changelog "python/python-scikit-learn/spkgbuild" "Upgraded from version 1.3.1 to version 1.3.2"
+sed -i -e "s/version=2.0.0/version=3.0.0/g" python/python-tblib/spkgbuild
+changelog "python/python-tblib/spkgbuild" "Upgraded from version 2.0.0 to version 3.0.0"
+sed -i -e "s/version=0.18.0/version=0.19.0/g" python/python-uvloop/spkgbuild
+changelog "python/python-uvloop/spkgbuild" "Upgraded from version 0.18.0 to version 0.19.0"
+sed -i -e "s/version=20.24.5/version=20.24.6/g" python/python-virtualenv/spkgbuild
+changelog "python/python-virtualenv/spkgbuild" "Upgraded from version 20.24.5 to version 20.24.6"
+sed -i -e "s/version=3.14.17/version=3.14.19/g" server/blacklist-hosts/spkgbuild
+changelog "server/blacklist-hosts/spkgbuild" "Upgraded from version 3.14.17 to version 3.14.19"
+sed -i -e "s/version=2023.10.4/version=2023.10.5/g" server/homeassistant/spkgbuild
+changelog "server/homeassistant/spkgbuild" "Upgraded from version 2023.10.4 to version 2023.10.5"
+sed -i -e "s/version=0.9.2/version=0.9.3/g" xfce/xfce4-notifyd/spkgbuild
+changelog "xfce/xfce4-notifyd/spkgbuild" "Upgraded from version 0.9.2 to version 0.9.3"
