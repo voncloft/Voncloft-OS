85861009d659c64373f55015209d93c1776f0a5f
Voncloft <voncloft@gmail.com>
2023-10-31 17:46:00 -0400

    Minor Updates

---
+++ logs/old-test-updates/2023/Oct/applied_updates-10-31-23.sh
@@ -0,0 +1,50 @@
+sed -i -e "s/version=17.0.3/version=17.0.4/g" compilers/clang/spkgbuild
+changelog "compilers/clang/spkgbuild" "Upgraded from version 17.0.3 to version 17.0.4"
+sed -i -e "s/version=17.0.3/version=17.0.4/g" compilers/lld/spkgbuild
+changelog "compilers/lld/spkgbuild" "Upgraded from version 17.0.3 to version 17.0.4"
+sed -i -e "s/version=17.0.3/version=17.0.4/g" compilers/llvm/spkgbuild
+changelog "compilers/llvm/spkgbuild" "Upgraded from version 17.0.3 to version 17.0.4"
+sed -i -e "s/version=17.0.3/version=17.0.4/g" compilers/llvm-bak/spkgbuild
+changelog "compilers/llvm-bak/spkgbuild" "Upgraded from version 17.0.3 to version 17.0.4"
+sed -i -e "s/version=17.0.3/version=17.0.4/g" compilers/llvm-old-2023-06-07/spkgbuild
+changelog "compilers/llvm-old-2023-06-07/spkgbuild" "Upgraded from version 17.0.3 to version 17.0.4"
+sed -i -e "s/version=0.120.1/version=0.120.2/g" core/hugo/spkgbuild
+changelog "core/hugo/spkgbuild" "Upgraded from version 0.120.1 to version 0.120.2"
+sed -i -e "s/version=0.25.1/version=0.25.2/g" core/p11-kit/spkgbuild
+changelog "core/p11-kit/spkgbuild" "Upgraded from version 0.25.1 to version 0.25.2"
+sed -i -e "s/version=3.21.0/version=3.22.0/g" core/valgrind/spkgbuild
+changelog "core/valgrind/spkgbuild" "Upgraded from version 3.21.0 to version 3.22.0"
+sed -i -e "s/version=17.0.3/version=17.0.4/g" multilib/llvm-32/spkgbuild
+changelog "multilib/llvm-32/spkgbuild" "Upgraded from version 17.0.3 to version 17.0.4"
+sed -i -e "s/version=0.25.1/version=0.25.2/g" multilib/p11-kit-32/spkgbuild
+changelog "multilib/p11-kit-32/spkgbuild" "Upgraded from version 0.25.1 to version 0.25.2"
+sed -i -e "s/version=545.23.06/version=545.29.02/g" nonfree/nvidia/spkgbuild
+changelog "nonfree/nvidia/spkgbuild" "Upgraded from version 545.23.06 to version 545.29.02"
+sed -i -e "s/version=545.23.06/version=545.29.02/g" nonfree/nvidia-32/spkgbuild
+changelog "nonfree/nvidia-32/spkgbuild" "Upgraded from version 545.23.06 to version 545.29.02"
+sed -i -e "s/version=545.23.06/version=545.29.02/g" nonfree/nvidia-modules/spkgbuild
+changelog "nonfree/nvidia-modules/spkgbuild" "Upgraded from version 545.23.06 to version 545.29.02"
+sed -i -e "s/version=120.0b1/version=120.0b2/g" nonfree/thunderbird/spkgbuild
+changelog "nonfree/thunderbird/spkgbuild" "Upgraded from version 120.0b1 to version 120.0b2"
+sed -i -e "s/version=0.30.2/version=0.30.3/g" python/python-argh/spkgbuild
+changelog "python/python-argh/spkgbuild" "Upgraded from version 0.30.2 to version 0.30.3"
+sed -i -e "s/version=1.28.73/version=1.28.74/g" python/python-boto3/spkgbuild
+changelog "python/python-boto3/spkgbuild" "Upgraded from version 1.28.73 to version 1.28.74"
+sed -i -e "s/version=1.31.73/version=1.31.74/g" python/python-botocore/spkgbuild
+changelog "python/python-botocore/spkgbuild" "Upgraded from version 1.31.73 to version 1.31.74"
+sed -i -e "s/version=2.3.0/version=2.3.1/g" python/python-ciso8601/spkgbuild
+changelog "python/python-ciso8601/spkgbuild" "Upgraded from version 2.3.0 to version 2.3.1"
+sed -i -e "s/version=19.12.0/version=19.12.1/g" python/python-faker/spkgbuild
+changelog "python/python-faker/spkgbuild" "Upgraded from version 19.12.0 to version 19.12.1"
+sed -i -e "s/version=2.105.0/version=2.106.0/g" python/python-google-api-python-client/spkgbuild
+changelog "python/python-google-api-python-client/spkgbuild" "Upgraded from version 2.105.0 to version 2.106.0"
+sed -i -e "s/version=8.13.23/version=8.13.24/g" python/python-phonenumbers/spkgbuild
+changelog "python/python-phonenumbers/spkgbuild" "Upgraded from version 8.13.23 to version 8.13.24"
+sed -i -e "s/version=1.7.0/version=1.8.1/g" python/python-poetry-core/spkgbuild
+changelog "python/python-poetry-core/spkgbuild" "Upgraded from version 1.7.0 to version 1.8.1"
+sed -i -e "s/version=0.21.1/version=0.22.0/g" python/python-pytest-asyncio/spkgbuild
+changelog "python/python-pytest-asyncio/spkgbuild" "Upgraded from version 0.21.1 to version 0.22.0"
+sed -i -e "s/version=2.13.1/version=2.14.0/g" python/python-tensorflow-serving-api/spkgbuild
+changelog "python/python-tensorflow-serving-api/spkgbuild" "Upgraded from version 2.13.1 to version 2.14.0"
+sed -i -e "s/version=4.0.1/version=4.0.2/g" python/python-trimesh/spkgbuild
+changelog "python/python-trimesh/spkgbuild" "Upgraded from version 4.0.1 to version 4.0.2"
