3c26271b1b1bb1c7270e74cd89c6a94e7c26233d
Voncloft <voncloft@gmail.com>
2023-11-03 15:17:54 -0400

    Minor Updates

---
+++ logs/old-test-updates/2023/Nov/applied_updates-11-03-23.sh
@@ -0,0 +1,30 @@
+sed -i -e "s/version=6.5.3/version=6.6/g" core/btrfs-progs/spkgbuild
+changelog "core/btrfs-progs/spkgbuild" "Upgraded from version 6.5.3 to version 6.6"
+sed -i -e "s/version=5.2.2/version=5.3.0/g" core/gawk/spkgbuild
+changelog "core/gawk/spkgbuild" "Upgraded from version 5.2.2 to version 5.3.0"
+sed -i -e "s/version=0.120.2/version=0.120.3/g" core/hugo/spkgbuild
+changelog "core/hugo/spkgbuild" "Upgraded from version 0.120.2 to version 0.120.3"
+sed -i -e "s/version=3.4.3/version=3.4.4/g" core/icewm/spkgbuild
+changelog "core/icewm/spkgbuild" "Upgraded from version 3.4.3 to version 3.4.4"
+sed -i -e "s/version=5.4.4/version=5.4.5/g" core/xz/spkgbuild
+changelog "core/xz/spkgbuild" "Upgraded from version 5.4.4 to version 5.4.5"
+sed -i -e "s/version=24.4/version=25.0/g" extra/protobuf/spkgbuild
+changelog "extra/protobuf/spkgbuild" "Upgraded from version 24.4 to version 25.0"
+sed -i -e "s/version=3.8.1/version=3.8.2/g" libs/libressl/spkgbuild
+changelog "libs/libressl/spkgbuild" "Upgraded from version 3.8.1 to version 3.8.2"
+sed -i -e "s/version=23.10.0/version=23.11.0/g" libs/poppler/spkgbuild
+changelog "libs/poppler/spkgbuild" "Upgraded from version 23.10.0 to version 23.11.0"
+sed -i -e "s/version=2.28.4/version=2.28.5/g" libs/sdl2/spkgbuild
+changelog "libs/sdl2/spkgbuild" "Upgraded from version 2.28.4 to version 2.28.5"
+sed -i -e "s/version=2.42.0/version=2.42.1/g" networking/git/spkgbuild
+changelog "networking/git/spkgbuild" "Upgraded from version 2.42.0 to version 2.42.1"
+sed -i -e "s/version=1.45.5/version=1.45.6/g" networking/networkmanager/spkgbuild
+changelog "networking/networkmanager/spkgbuild" "Upgraded from version 1.45.5 to version 1.45.6"
+sed -i -e "s/version=5.6/version=6.0/g" networking/ntopng/spkgbuild
+changelog "networking/ntopng/spkgbuild" "Upgraded from version 5.6 to version 6.0"
+sed -i -e "s/version=0.4.8.7/version=0.4.8.8/g" networking/tor/spkgbuild
+changelog "networking/tor/spkgbuild" "Upgraded from version 0.4.8.7 to version 0.4.8.8"
+sed -i -e "s/version=120.0b4/version=120.0b6/g" nonfree/firefox/spkgbuild
+changelog "nonfree/firefox/spkgbuild" "Upgraded from version 120.0b4 to version 120.0b6"
+sed -i -e "s/version=120.0b2/version=120.0b3/g" nonfree/thunderbird/spkgbuild
+changelog "nonfree/thunderbird/spkgbuild" "Upgraded from version 120.0b2 to version 120.0b3"
