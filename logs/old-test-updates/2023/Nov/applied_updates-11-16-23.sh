3a6e30a3d924f0d71887ff571ec9fbfa1ac18d73
Voncloft <voncloft@gmail.com>
2023-11-16 10:07:52 -0500

    Minor Updates

---
+++ logs/old-test-updates/2023/Nov/applied_updates-11-16-23.sh
@@ -0,0 +1,30 @@
+sed -i -e "s/version=6.6.1/version=6.6.2/g" core/btrfs-progs/spkgbuild
+changelog "core/btrfs-progs/spkgbuild" "Upgraded from version 6.6.1 to version 6.6.2"
+sed -i -e "s/version=0.25.2/version=0.25.3/g" core/p11-kit/spkgbuild
+changelog "core/p11-kit/spkgbuild" "Upgraded from version 0.25.2 to version 0.25.3"
+sed -i -e "s/version=0.25.2/version=0.25.3/g" multilib/p11-kit-32/spkgbuild
+changelog "multilib/p11-kit-32/spkgbuild" "Upgraded from version 0.25.2 to version 0.25.3"
+sed -i -e "s/version=9.19.17/version=9.19.18/g" networking/bind/spkgbuild
+changelog "networking/bind/spkgbuild" "Upgraded from version 9.19.17 to version 9.19.18"
+sed -i -e "s/version=2023.10.13/version=2023.11.14/g" networking/yt-dlp/spkgbuild
+changelog "networking/yt-dlp/spkgbuild" "Upgraded from version 2023.10.13 to version 2023.11.14"
+sed -i -e "s/version=120.0b6/version=120.0b7/g" nonfree/thunderbird/spkgbuild
+changelog "nonfree/thunderbird/spkgbuild" "Upgraded from version 120.0b6 to version 120.0b7"
+sed -i -e "s/version=1.28.85/version=1.29.0/g" python/python-boto3/spkgbuild
+changelog "python/python-boto3/spkgbuild" "Upgraded from version 1.28.85 to version 1.29.0"
+sed -i -e "s/version=1.31.85/version=1.32.0/g" python/python-botocore/spkgbuild
+changelog "python/python-botocore/spkgbuild" "Upgraded from version 1.31.85 to version 1.32.0"
+sed -i -e "s/version=4.44.2/version=4.44.3/g" python/python-fonttools/spkgbuild
+changelog "python/python-fonttools/spkgbuild" "Upgraded from version 4.44.2 to version 4.44.3"
+sed -i -e "s/version=2023.10.24/version=2023.11.14/g" python/python-pipenv/spkgbuild
+changelog "python/python-pipenv/spkgbuild" "Upgraded from version 2023.10.24 to version 2023.11.14"
+sed -i -e "s/version=2.5.0/version=2.5.1/g" python/python-pydantic/spkgbuild
+changelog "python/python-pydantic/spkgbuild" "Upgraded from version 2.5.0 to version 2.5.1"
+sed -i -e "s/version=0.24.0/version=0.24.1/g" python/python-responses/spkgbuild
+changelog "python/python-responses/spkgbuild" "Upgraded from version 0.24.0 to version 0.24.1"
+sed -i -e "s/version=13.6.0/version=13.7.0/g" python/python-rich/spkgbuild
+changelog "python/python-rich/spkgbuild" "Upgraded from version 13.6.0 to version 13.7.0"
+sed -i -e "s/version=0.126.0/version=0.127.0/g" python/python-zeroconf/spkgbuild
+changelog "python/python-zeroconf/spkgbuild" "Upgraded from version 0.126.0 to version 0.127.0"
+sed -i -e "s/version=4.19.0/version=4.19.1/g" xfce/xfce4-settings/spkgbuild
+changelog "xfce/xfce4-settings/spkgbuild" "Upgraded from version 4.19.0 to version 4.19.1"
