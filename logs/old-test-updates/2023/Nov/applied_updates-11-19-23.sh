2334ee863f5170a7e0cc3b281a77824406356e9c
Voncloft <voncloft@gmail.com>
2023-11-19 14:07:04 -0500

    Minor Updates

---
+++ logs/old-test-updates/2023/Nov/applied_updates-11-19-23.sh
@@ -0,0 +1,56 @@
+sed -i -e "s/version=5.8.0/version=6.0.0/g" cinnamon/cjs/spkgbuild
+changelog "cinnamon/cjs/spkgbuild" "Upgraded from version 5.8.0 to version 6.0.0"
+sed -i -e "s/version=3.28.0-rc4/version=3.28.0-rc5/g" compilers/cmake/spkgbuild
+changelog "compilers/cmake/spkgbuild" "Upgraded from version 3.28.0-rc4 to version 3.28.0-rc5"
+sed -i -e "s/version=1.73.0/version=1.74.0/g" compilers/rust/spkgbuild
+changelog "compilers/rust/spkgbuild" "Upgraded from version 1.73.0 to version 1.74.0"
+sed -i -e "s/version=1.5.28/version=1.5.29/g" core/ibus/spkgbuild
+changelog "core/ibus/spkgbuild" "Upgraded from version 1.5.28 to version 1.5.29"
+sed -i -e "s/version=0.3.84/version=0.3.85/g" core/pipewire/spkgbuild
+changelog "core/pipewire/spkgbuild" "Upgraded from version 0.3.84 to version 0.3.85"
+sed -i -e "s/version=2.42.2/version=2.43.1/g" core/webkitgtk/spkgbuild
+changelog "core/webkitgtk/spkgbuild" "Upgraded from version 2.42.2 to version 2.43.1"
+sed -i -e "s/version=2.42.3/version=2.42.4/g" extra/discordchatexporter/spkgbuild
+changelog "extra/discordchatexporter/spkgbuild" "Upgraded from version 2.42.3 to version 2.42.4"
+sed -i -e "s/version=25.0/version=25.1/g" extra/protobuf/spkgbuild
+changelog "extra/protobuf/spkgbuild" "Upgraded from version 25.0 to version 25.1"
+sed -i -e "s/version=1.6.4/version=1.6.5/g" libs/libksba/spkgbuild
+changelog "libs/libksba/spkgbuild" "Upgraded from version 1.6.4 to version 1.6.5"
+sed -i -e "s/version=1.45.6/version=1.45.7/g" networking/networkmanager/spkgbuild
+changelog "networking/networkmanager/spkgbuild" "Upgraded from version 1.45.6 to version 1.45.7"
+sed -i -e "s/version=2.6.7/version=2.6.8/g" networking/openvpn/spkgbuild
+changelog "networking/openvpn/spkgbuild" "Upgraded from version 2.6.7 to version 2.6.8"
+sed -i -e "s/version=2023.11.14/version=2023.11.16/g" networking/yt-dlp/spkgbuild
+changelog "networking/yt-dlp/spkgbuild" "Upgraded from version 2023.11.14 to version 2023.11.16"
+sed -i -e "s/version=1.79.0/version=1.80.0/g" python/python-aws-sam-translator/spkgbuild
+changelog "python/python-aws-sam-translator/spkgbuild" "Upgraded from version 1.79.0 to version 1.80.0"
+sed -i -e "s/version=1.29.0/version=1.29.3/g" python/python-boto3/spkgbuild
+changelog "python/python-boto3/spkgbuild" "Upgraded from version 1.29.0 to version 1.29.3"
+sed -i -e "s/version=1.32.0/version=1.32.3/g" python/python-botocore/spkgbuild
+changelog "python/python-botocore/spkgbuild" "Upgraded from version 1.32.0 to version 1.32.3"
+sed -i -e "s/version=2.8.0/version=2.9.0/g" python/python-dominate/spkgbuild
+changelog "python/python-dominate/spkgbuild" "Upgraded from version 2.8.0 to version 2.9.0"
+sed -i -e "s/version=1.31.0/version=1.32.0/g" python/python-hcloud/spkgbuild
+changelog "python/python-hcloud/spkgbuild" "Upgraded from version 1.31.0 to version 1.32.0"
+sed -i -e "s/version=6.88.4/version=6.89.0/g" python/python-hypothesis/spkgbuild
+changelog "python/python-hypothesis/spkgbuild" "Upgraded from version 6.88.4 to version 6.89.0"
+sed -i -e "s/version=2.5.31/version=2.5.32/g" python/python-identify/spkgbuild
+changelog "python/python-identify/spkgbuild" "Upgraded from version 2.5.31 to version 2.5.32"
+sed -i -e "s/version=4.19.2/version=4.20.0/g" python/python-jsonschema/spkgbuild
+changelog "python/python-jsonschema/spkgbuild" "Upgraded from version 4.19.2 to version 4.20.0"
+sed -i -e "s/version=5.3.3/version=5.3.4/g" python/python-kombu/spkgbuild
+changelog "python/python-kombu/spkgbuild" "Upgraded from version 5.3.3 to version 5.3.4"
+sed -i -e "s/version=3.4.3/version=3.4.4/g" python/python-ldap/spkgbuild
+changelog "python/python-ldap/spkgbuild" "Upgraded from version 3.4.3 to version 3.4.4"
+sed -i -e "s/version=11.0.0/version=11.1.0/g" python/python-neutronclient/spkgbuild
+changelog "python/python-neutronclient/spkgbuild" "Upgraded from version 11.0.0 to version 11.1.0"
+sed -i -e "s/version=2023.11.14/version=2023.11.15/g" python/python-pipenv/spkgbuild
+changelog "python/python-pipenv/spkgbuild" "Upgraded from version 2023.11.14 to version 2023.11.15"
+sed -i -e "s/version=1.7.0/version=1.7.1/g" python/python-poetry/spkgbuild
+changelog "python/python-poetry/spkgbuild" "Upgraded from version 1.7.0 to version 1.7.1"
+sed -i -e "s/version=3.0.0/version=3.0.1/g" python/python-pygal/spkgbuild
+changelog "python/python-pygal/spkgbuild" "Upgraded from version 3.0.0 to version 3.0.1"
+sed -i -e "s/version=0.26.1/version=0.27.0/g" python/python-qasync/spkgbuild
+changelog "python/python-qasync/spkgbuild" "Upgraded from version 0.26.1 to version 0.27.0"
+sed -i -e "s/version=5.5.0/version=5.5.1/g" python/python-qtconsole/spkgbuild
+changelog "python/python-qtconsole/spkgbuild" "Upgraded from version 5.5.0 to version 5.5.1"
