e0f3ddd59df0932855fb49fcb52ef1fb6c763599
Voncloft <voncloft@gmail.com>
2023-11-20 09:46:05 -0500

    Minor Updates

---
+++ logs/old-test-updates/2023/Nov/applied_updates-11-20-23.sh
@@ -0,0 +1,36 @@
+sed -i -e "s/version=5.8.2/version=6.0.0/g" cinnamon/cinnamon-control-center/spkgbuild
+changelog "cinnamon/cinnamon-control-center/spkgbuild" "Upgraded from version 5.8.2 to version 6.0.0"
+sed -i -e "s/version=5.8.0/version=6.0.0/g" cinnamon/cinnamon-desktop/spkgbuild
+changelog "cinnamon/cinnamon-desktop/spkgbuild" "Upgraded from version 5.8.0 to version 6.0.0"
+sed -i -e "s/version=5.8.0/version=6.0.0/g" cinnamon/cinnamon-menus/spkgbuild
+changelog "cinnamon/cinnamon-menus/spkgbuild" "Upgraded from version 5.8.0 to version 6.0.0"
+sed -i -e "s/version=5.8.1/version=6.0.0/g" cinnamon/cinnamon-screensaver/spkgbuild
+changelog "cinnamon/cinnamon-screensaver/spkgbuild" "Upgraded from version 5.8.1 to version 6.0.0"
+sed -i -e "s/version=5.8.1/version=6.0.0/g" cinnamon/cinnamon-session/spkgbuild
+changelog "cinnamon/cinnamon-session/spkgbuild" "Upgraded from version 5.8.1 to version 6.0.0"
+sed -i -e "s/version=5.8.1/version=6.0.0/g" cinnamon/cinnamon-settings-daemon/spkgbuild
+changelog "cinnamon/cinnamon-settings-daemon/spkgbuild" "Upgraded from version 5.8.1 to version 6.0.0"
+sed -i -e "s/version=5.8.2/version=6.0.0/g" cinnamon/cinnamon-translations/spkgbuild
+changelog "cinnamon/cinnamon-translations/spkgbuild" "Upgraded from version 5.8.2 to version 6.0.0"
+sed -i -e "s/version=5.8.1/version=6.0.0/g" cinnamon/muffin/spkgbuild
+changelog "cinnamon/muffin/spkgbuild" "Upgraded from version 5.8.1 to version 6.0.0"
+sed -i -e "s/version=5.8.5/version=6.0.0/g" cinnamon/nemo/spkgbuild
+changelog "cinnamon/nemo/spkgbuild" "Upgraded from version 5.8.5 to version 6.0.0"
+sed -i -e "s/version=3.9.19/version=3.10.0/g" extra/flexget/spkgbuild
+changelog "extra/flexget/spkgbuild" "Upgraded from version 3.9.19 to version 3.10.0"
+sed -i -e "s/version=1.22.0/version=1.22.1/g" libs/c-ares/spkgbuild
+changelog "libs/c-ares/spkgbuild" "Upgraded from version 1.22.0 to version 1.22.1"
+sed -i -e "s/version=2.6.1/version=2.8.0/g" libs/xapp/spkgbuild
+changelog "libs/xapp/spkgbuild" "Upgraded from version 2.6.1 to version 2.8.0"
+sed -i -e "s/version=1.0.1/version=2.0.0/g" python/python-funcparserlib/spkgbuild
+changelog "python/python-funcparserlib/spkgbuild" "Upgraded from version 1.0.1 to version 2.0.0"
+sed -i -e "s/version=6.89.0/version=6.89.1/g" python/python-hypothesis/spkgbuild
+changelog "python/python-hypothesis/spkgbuild" "Upgraded from version 6.89.0 to version 6.89.1"
+sed -i -e "s/version=0.23.2/version=0.24.0/g" python/python-libtmux/spkgbuild
+changelog "python/python-libtmux/spkgbuild" "Upgraded from version 0.23.2 to version 0.24.0"
+sed -i -e "s/version=0.27.0/version=0.27.1/g" python/python-qasync/spkgbuild
+changelog "python/python-qasync/spkgbuild" "Upgraded from version 0.27.0 to version 0.27.1"
+sed -i -e "s/version=0.10.1/version=1.0.0/g" python/python-xattr/spkgbuild
+changelog "python/python-xattr/spkgbuild" "Upgraded from version 0.10.1 to version 1.0.0"
+sed -i -e "s/version=0.8.5/version=0.8.6/g" xfce/xfce4-dict/spkgbuild
+changelog "xfce/xfce4-dict/spkgbuild" "Upgraded from version 0.8.5 to version 0.8.6"
