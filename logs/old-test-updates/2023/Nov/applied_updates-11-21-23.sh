67235cbd04f6aa3ce9b8df7efacf9f0e9fb67101
Voncloft <voncloft@gmail.com>
2023-11-21 09:21:21 -0500

    Minor Updates

---
+++ logs/old-test-updates/2023/Nov/applied_updates-11-21-23.sh
@@ -0,0 +1,36 @@
+sed -i -e "s/version=0.22.3/version=0.22.4/g" core/gettext/spkgbuild
+changelog "core/gettext/spkgbuild" "Upgraded from version 0.22.3 to version 0.22.4"
+sed -i -e "s/version=6.6.1/version=6.6.2/g" core/linux-api-headers/spkgbuild
+changelog "core/linux-api-headers/spkgbuild" "Upgraded from version 6.6.1 to version 6.6.2"
+sed -i -e "s/version=6.6.1/version=6.6.2/g" core/linux-desktop/spkgbuild
+changelog "core/linux-desktop/spkgbuild" "Upgraded from version 6.6.1 to version 6.6.2"
+sed -i -e "s/version=6.6.1/version=6.6.2/g" core/linux-router/spkgbuild
+changelog "core/linux-router/spkgbuild" "Upgraded from version 6.6.1 to version 6.6.2"
+sed -i -e "s/version=4.5.2/version=4.6.0/g" core/scons/spkgbuild
+changelog "core/scons/spkgbuild" "Upgraded from version 4.5.2 to version 4.6.0"
+sed -i -e "s/version=2.4.117/version=2.4.118/g" libs/libdrm/spkgbuild
+changelog "libs/libdrm/spkgbuild" "Upgraded from version 2.4.117 to version 2.4.118"
+sed -i -e "s/version=2.42.1/version=2.43.0/g" networking/git/spkgbuild
+changelog "networking/git/spkgbuild" "Upgraded from version 2.42.1 to version 2.43.0"
+sed -i -e "s/version=0.7.5/version=0.7.6/g" python/python-aioresponses/spkgbuild
+changelog "python/python-aioresponses/spkgbuild" "Upgraded from version 0.7.5 to version 0.7.6"
+sed -i -e "s/version=3.0.1/version=3.0.2/g" python/python-connexion/spkgbuild
+changelog "python/python-connexion/spkgbuild" "Upgraded from version 3.0.1 to version 3.0.2"
+sed -i -e "s/version=20.0.3/version=20.1.0/g" python/python-faker/spkgbuild
+changelog "python/python-faker/spkgbuild" "Upgraded from version 20.0.3 to version 20.1.0"
+sed -i -e "s/version=4.44.3/version=4.45.0/g" python/python-fonttools/spkgbuild
+changelog "python/python-fonttools/spkgbuild" "Upgraded from version 4.44.3 to version 4.45.0"
+sed -i -e "s/version=6.89.1/version=6.90.0/g" python/python-hypothesis/spkgbuild
+changelog "python/python-hypothesis/spkgbuild" "Upgraded from version 6.89.1 to version 6.90.0"
+sed -i -e "s/version=5.3.0/version=5.4.0/g" python/python-keystoneauth1/spkgbuild
+changelog "python/python-keystoneauth1/spkgbuild" "Upgraded from version 5.3.0 to version 5.4.0"
+sed -i -e "s/version=4.2.0/version=4.3.0/g" python/python-magnumclient/spkgbuild
+changelog "python/python-magnumclient/spkgbuild" "Upgraded from version 4.2.0 to version 4.3.0"
+sed -i -e "s/version=3.5.1/version=3.6.0/g" python/python-octaviaclient/spkgbuild
+changelog "python/python-octaviaclient/spkgbuild" "Upgraded from version 3.5.1 to version 3.6.0"
+sed -i -e "s/version=7.12.0/version=7.12.1/g" python/python-qiniu/spkgbuild
+changelog "python/python-qiniu/spkgbuild" "Upgraded from version 7.12.0 to version 7.12.1"
+sed -i -e "s/version=68.2.2/version=69.0.0/g" python/python-setuptools/spkgbuild
+changelog "python/python-setuptools/spkgbuild" "Upgraded from version 68.2.2 to version 69.0.0"
+sed -i -e "s/version=3.14.26/version=3.14.27/g" server/blacklist-hosts/spkgbuild
+changelog "server/blacklist-hosts/spkgbuild" "Upgraded from version 3.14.26 to version 3.14.27"
