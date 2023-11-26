0d5b27161094e72910b21fc10f05451cfe852a61
Voncloft <voncloft@gmail.com>
2023-11-26 11:29:16 -0500

    Minor Updates

---
+++ logs/old-test-updates/2023/Nov/applied_updates-11-26-23.sh
@@ -0,0 +1,94 @@
+sed -i -e "s/version=2.0.3/version=2.1.0/g" compilers/pkgconf/spkgbuild
+changelog "compilers/pkgconf/spkgbuild" "Upgraded from version 2.0.3 to version 2.1.0"
+sed -i -e "s/version=6.5/version=6.6/g" core/ethtool/spkgbuild
+changelog "core/ethtool/spkgbuild" "Upgraded from version 6.5 to version 6.6"
+sed -i -e "s/version=6.5.3/version=6.6/g" core/mercurial/spkgbuild
+changelog "core/mercurial/spkgbuild" "Upgraded from version 6.5.3 to version 6.6"
+sed -i -e "s/version=0.36.0/version=0.37.0/g" core/mpv/spkgbuild
+changelog "core/mpv/spkgbuild" "Upgraded from version 0.36.0 to version 0.37.0"
+sed -i -e "s/version=6.3.1/version=6.4.1/g" core/streamlink/spkgbuild
+changelog "core/streamlink/spkgbuild" "Upgraded from version 6.3.1 to version 6.4.1"
+sed -i -e "s/version=1.24.1/version=1.26.0/g" core/v4l-utils/spkgbuild
+changelog "core/v4l-utils/spkgbuild" "Upgraded from version 1.24.1 to version 1.26.0"
+sed -i -e "s/version=2.42.4/version=2.42.5/g" extra/discordchatexporter/spkgbuild
+changelog "extra/discordchatexporter/spkgbuild" "Upgraded from version 2.42.4 to version 2.42.5"
+sed -i -e "s/version=3.10.0/version=3.10.1/g" extra/flexget/spkgbuild
+changelog "extra/flexget/spkgbuild" "Upgraded from version 3.10.0 to version 3.10.1"
+sed -i -e "s/version=3.11.0/version=3.12.0/g" libs/blas/spkgbuild
+changelog "libs/blas/spkgbuild" "Upgraded from version 3.11.0 to version 3.12.0"
+sed -i -e "s/version=3.11.0/version=3.12.0/g" libs/cblas/spkgbuild
+changelog "libs/cblas/spkgbuild" "Upgraded from version 3.11.0 to version 3.12.0"
+sed -i -e "s/version=3.11.0/version=3.12.0/g" libs/lapack/spkgbuild
+changelog "libs/lapack/spkgbuild" "Upgraded from version 3.11.0 to version 3.12.0"
+sed -i -e "s/version=3.1.4/version=3.2.0/g" networking/openssl/spkgbuild
+changelog "networking/openssl/spkgbuild" "Upgraded from version 3.1.4 to version 3.2.0"
+sed -i -e "s/version=120.0b9/version=121.0b3/g" nonfree/firefox/spkgbuild
+changelog "nonfree/firefox/spkgbuild" "Upgraded from version 120.0b9 to version 121.0b3"
+sed -i -e "s/version=7.6.2/version=7.6.3/g" nonfree/libreoffice/spkgbuild
+changelog "nonfree/libreoffice/spkgbuild" "Upgraded from version 7.6.2 to version 7.6.3"
+sed -i -e "s/version=545.29.02/version=545.29.06/g" nonfree/nvidia/spkgbuild
+changelog "nonfree/nvidia/spkgbuild" "Upgraded from version 545.29.02 to version 545.29.06"
+sed -i -e "s/version=545.29.02/version=545.29.06/g" nonfree/nvidia-32/spkgbuild
+changelog "nonfree/nvidia-32/spkgbuild" "Upgraded from version 545.29.02 to version 545.29.06"
+sed -i -e "s/version=545.29.02/version=545.29.06/g" nonfree/nvidia-modules/spkgbuild
+changelog "nonfree/nvidia-modules/spkgbuild" "Upgraded from version 545.29.02 to version 545.29.06"
+sed -i -e "s/version=120.0b7/version=121.0b1/g" nonfree/thunderbird/spkgbuild
+changelog "nonfree/thunderbird/spkgbuild" "Upgraded from version 120.0b7 to version 121.0b1"
+sed -i -e "s/version=3.1.1/version=3.2.0/g" python/python-aiogram/spkgbuild
+changelog "python/python-aiogram/spkgbuild" "Upgraded from version 3.1.1 to version 3.2.0"
+sed -i -e "s/version=4.0.0/version=4.1.0/g" python/python-anyio/spkgbuild
+changelog "python/python-anyio/spkgbuild" "Upgraded from version 4.0.0 to version 4.1.0"
+sed -i -e "s/version=1.80.0/version=1.81.0/g" python/python-aws-sam-translator/spkgbuild
+changelog "python/python-aws-sam-translator/spkgbuild" "Upgraded from version 1.80.0 to version 1.81.0"
+sed -i -e "s/version=4.1.2/version=4.1.3/g" python/python-bitstring/spkgbuild
+changelog "python/python-bitstring/spkgbuild" "Upgraded from version 4.1.2 to version 4.1.3"
+sed -i -e "s/version=1.29.3/version=1.29.6/g" python/python-boto3/spkgbuild
+changelog "python/python-boto3/spkgbuild" "Upgraded from version 1.29.3 to version 1.29.6"
+sed -i -e "s/version=1.32.3/version=1.32.6/g" python/python-botocore/spkgbuild
+changelog "python/python-botocore/spkgbuild" "Upgraded from version 1.32.3 to version 1.32.6"
+sed -i -e "s/version=2.12.4/version=2.13.1/g" python/python-cloudflare/spkgbuild
+changelog "python/python-cloudflare/spkgbuild" "Upgraded from version 2.12.4 to version 2.13.1"
+sed -i -e "s/version=6.1.3/version=7.0.0/g" python/python-docker/spkgbuild
+changelog "python/python-docker/spkgbuild" "Upgraded from version 6.1.3 to version 7.0.0"
+sed -i -e "s/version=4.45.0/version=4.45.1/g" python/python-fonttools/spkgbuild
+changelog "python/python-fonttools/spkgbuild" "Upgraded from version 4.45.0 to version 4.45.1"
+sed -i -e "s/version=2.4.0/version=2.4.1/g" python/python-geopy/spkgbuild
+changelog "python/python-geopy/spkgbuild" "Upgraded from version 2.4.0 to version 2.4.1"
+sed -i -e "s/version=6.26.0/version=6.27.0/g" python/python-ipykernel/spkgbuild
+changelog "python/python-ipykernel/spkgbuild" "Upgraded from version 6.26.0 to version 6.27.0"
+sed -i -e "s/version=0.24.0/version=0.24.1/g" python/python-libtmux/spkgbuild
+changelog "python/python-libtmux/spkgbuild" "Upgraded from version 0.24.0 to version 0.24.1"
+sed -i -e "s/version=3.0.1/version=3.1/g" python/python-pastedeploy/spkgbuild
+changelog "python/python-pastedeploy/spkgbuild" "Upgraded from version 3.0.1 to version 3.1"
+sed -i -e "s/version=8.13.25/version=8.13.26/g" python/python-phonenumbers/spkgbuild
+changelog "python/python-phonenumbers/spkgbuild" "Upgraded from version 8.13.25 to version 8.13.26"
+sed -i -e "s/version=0.18.0/version=0.19.0/g" python/python-prometheus_client/spkgbuild
+changelog "python/python-prometheus_client/spkgbuild" "Upgraded from version 0.18.0 to version 0.19.0"
+sed -i -e "s/version=7.3.2309301657/version=7.3.2311251353/g" python/python-pycontracts/spkgbuild
+changelog "python/python-pycontracts/spkgbuild" "Upgraded from version 7.3.2309301657 to version 7.3.2311251353"
+sed -i -e "s/version=2.5.1/version=2.5.2/g" python/python-pydantic/spkgbuild
+changelog "python/python-pydantic/spkgbuild" "Upgraded from version 2.5.1 to version 2.5.2"
+sed -i -e "s/version=3.0.1/version=3.0.4/g" python/python-pygal/spkgbuild
+changelog "python/python-pygal/spkgbuild" "Upgraded from version 3.0.1 to version 3.0.4"
+sed -i -e "s/version=1.13.2/version=1.13.3/g" python/python-pygit2/spkgbuild
+changelog "python/python-pygit2/spkgbuild" "Upgraded from version 1.13.2 to version 1.13.3"
+sed -i -e "s/version=3.4.0/version=3.5.0/g" python/python-pytest-xdist/spkgbuild
+changelog "python/python-pytest-xdist/spkgbuild" "Upgraded from version 3.4.0 to version 3.5.0"
+sed -i -e "s/version=69.0.0/version=69.0.2/g" python/python-setuptools/spkgbuild
+changelog "python/python-setuptools/spkgbuild" "Upgraded from version 69.0.0 to version 69.0.2"
+sed -i -e "s/version=5.0.3/version=6.0.2/g" python/python-sybil/spkgbuild
+changelog "python/python-sybil/spkgbuild" "Upgraded from version 5.0.3 to version 6.0.2"
+sed -i -e "s/version=2.14.0/version=2.14.1/g" python/python-tensorflow-serving-api/spkgbuild
+changelog "python/python-tensorflow-serving-api/spkgbuild" "Upgraded from version 2.14.0 to version 2.14.1"
+sed -i -e "s/version=4.0.4/version=4.0.5/g" python/python-trimesh/spkgbuild
+changelog "python/python-trimesh/spkgbuild" "Upgraded from version 4.0.4 to version 4.0.5"
+sed -i -e "s/version=20.24.6/version=20.24.7/g" python/python-virtualenv/spkgbuild
+changelog "python/python-virtualenv/spkgbuild" "Upgraded from version 20.24.6 to version 20.24.7"
+sed -i -e "s/version=1.9.2/version=1.9.3/g" python/python-yarl/spkgbuild
+changelog "python/python-yarl/spkgbuild" "Upgraded from version 1.9.2 to version 1.9.3"
+sed -i -e "s/version=3.14.27/version=3.14.29/g" server/blacklist-hosts/spkgbuild
+changelog "server/blacklist-hosts/spkgbuild" "Upgraded from version 3.14.27 to version 3.14.29"
+sed -i -e "s/version=2023.11.2/version=2023.11.3/g" server/homeassistant/spkgbuild
+changelog "server/homeassistant/spkgbuild" "Upgraded from version 2023.11.2 to version 2023.11.3"
+sed -i -e "s/version=8.2.12/version=8.3.0/g" server/php/spkgbuild
+changelog "server/php/spkgbuild" "Upgraded from version 8.2.12 to version 8.3.0"
