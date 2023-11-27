1573674435a439689ebb5988a8e0a8a5eeab8e53
Voncloft <voncloft@gmail.com>
2023-11-27 06:29:51 -0500

    Minor Updates

---
+++ logs/old-test-updates/2023/Nov/applied_updates-11-27-23.sh
@@ -0,0 +1,16 @@
+sed -i -e "s/version=0.3.85/version=1.0.0/g" core/pipewire/spkgbuild
+changelog "core/pipewire/spkgbuild" "Upgraded from version 0.3.85 to version 1.0.0"
+sed -i -e "s/version=4.8.1/version=4.9.0/g" core/qalculate/spkgbuild
+changelog "core/qalculate/spkgbuild" "Upgraded from version 4.8.1 to version 4.9.0"
+sed -i -e "s/version=4.8.1/version=4.9.0/g" libs/libqalculate/spkgbuild
+changelog "libs/libqalculate/spkgbuild" "Upgraded from version 4.8.1 to version 4.9.0"
+sed -i -e "s/version=2.13.1/version=2.14.2/g" python/python-cloudflare/spkgbuild
+changelog "python/python-cloudflare/spkgbuild" "Upgraded from version 2.13.1 to version 2.14.2"
+sed -i -e "s/version=0.24.1/version=0.25.0/g" python/python-libtmux/spkgbuild
+changelog "python/python-libtmux/spkgbuild" "Upgraded from version 0.24.1 to version 0.25.0"
+sed -i -e "s/version=7.3.2311251353/version=7.3.2311261330/g" python/python-pycontracts/spkgbuild
+changelog "python/python-pycontracts/spkgbuild" "Upgraded from version 7.3.2311251353 to version 7.3.2311261330"
+sed -i -e "s/version=0.41.3/version=0.42.0/g" python/python-wheel/spkgbuild
+changelog "python/python-wheel/spkgbuild" "Upgraded from version 0.41.3 to version 0.42.0"
+sed -i -e "s/version=3.14.29/version=3.14.34/g" server/blacklist-hosts/spkgbuild
+changelog "server/blacklist-hosts/spkgbuild" "Upgraded from version 3.14.29 to version 3.14.34"
