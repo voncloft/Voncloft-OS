7d0ab87f0c31045a554ae11f4dbb8ca49d289439
Voncloft <voncloft@gmail.com>
2023-11-28 06:21:28 -0500

    Minor Updates

---
+++ logs/old-test-updates/2023/Nov/applied_updates-11-28-23.sh
@@ -0,0 +1,24 @@
+sed -i -e "s/version=3.10.1/version=3.10.2/g" extra/flexget/spkgbuild
+changelog "extra/flexget/spkgbuild" "Upgraded from version 3.10.1 to version 3.10.2"
+sed -i -e "s/version=2.8.0/version=2.9.0/g" libs/libwacom/spkgbuild
+changelog "libs/libwacom/spkgbuild" "Upgraded from version 2.8.0 to version 2.9.0"
+sed -i -e "s/version=121.0b3/version=121.0b4/g" nonfree/firefox/spkgbuild
+changelog "nonfree/firefox/spkgbuild" "Upgraded from version 121.0b3 to version 121.0b4"
+sed -i -e "s/version=4.0.1/version=4.1.0/g" python/python-bcrypt/spkgbuild
+changelog "python/python-bcrypt/spkgbuild" "Upgraded from version 4.0.1 to version 4.1.0"
+sed -i -e "s/version=1.29.6/version=1.29.7/g" python/python-boto3/spkgbuild
+changelog "python/python-boto3/spkgbuild" "Upgraded from version 1.29.6 to version 1.29.7"
+sed -i -e "s/version=1.32.6/version=1.32.7/g" python/python-botocore/spkgbuild
+changelog "python/python-botocore/spkgbuild" "Upgraded from version 1.32.6 to version 1.32.7"
+sed -i -e "s/version=6.90.0/version=6.91.0/g" python/python-hypothesis/spkgbuild
+changelog "python/python-hypothesis/spkgbuild" "Upgraded from version 6.90.0 to version 6.91.0"
+sed -i -e "s/version=6.27.0/version=6.27.1/g" python/python-ipykernel/spkgbuild
+changelog "python/python-ipykernel/spkgbuild" "Upgraded from version 6.27.0 to version 6.27.1"
+sed -i -e "s/version=3.9.1/version=3.9.2/g" python/python-pytables/spkgbuild
+changelog "python/python-pytables/spkgbuild" "Upgraded from version 3.9.1 to version 3.9.2"
+sed -i -e "s/version=0.7.0/version=0.8.0/g" python/python-s3transfer/spkgbuild
+changelog "python/python-s3transfer/spkgbuild" "Upgraded from version 0.7.0 to version 0.8.0"
+sed -i -e "s/version=20.6/version=20.7/g" python/python-telegram-bot/spkgbuild
+changelog "python/python-telegram-bot/spkgbuild" "Upgraded from version 20.6 to version 20.7"
+sed -i -e "s/version=4.19.2/version=4.19.3/g" server/samba/spkgbuild
+changelog "server/samba/spkgbuild" "Upgraded from version 4.19.2 to version 4.19.3"
