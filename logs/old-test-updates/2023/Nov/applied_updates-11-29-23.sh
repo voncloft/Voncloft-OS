8a07370abbca1905bed3e97d64a71dcb461a04a0
Voncloft <voncloft@gmail.com>
2023-11-29 10:12:44 -0500

    Minor Updates

---
+++ logs/old-test-updates/2023/Nov/applied_updates-11-29-23.sh
@@ -0,0 +1,40 @@
+sed -i -e "s/version=6.0.0/version=6.0.1/g" cinnamon/cinnamon-session/spkgbuild
+changelog "cinnamon/cinnamon-session/spkgbuild" "Upgraded from version 6.0.0 to version 6.0.1"
+sed -i -e "s/version=17.0.5/version=17.0.6/g" compilers/clang/spkgbuild
+changelog "compilers/clang/spkgbuild" "Upgraded from version 17.0.5 to version 17.0.6"
+sed -i -e "s/version=17.0.5/version=17.0.6/g" compilers/lld/spkgbuild
+changelog "compilers/lld/spkgbuild" "Upgraded from version 17.0.5 to version 17.0.6"
+sed -i -e "s/version=17.0.5/version=17.0.6/g" compilers/llvm/spkgbuild
+changelog "compilers/llvm/spkgbuild" "Upgraded from version 17.0.5 to version 17.0.6"
+sed -i -e "s/version=17.0.5/version=17.0.6/g" compilers/llvm-bak/spkgbuild
+changelog "compilers/llvm-bak/spkgbuild" "Upgraded from version 17.0.5 to version 17.0.6"
+sed -i -e "s/version=17.0.5/version=17.0.6/g" compilers/llvm-old-2023-06-07/spkgbuild
+changelog "compilers/llvm-old-2023-06-07/spkgbuild" "Upgraded from version 17.0.5 to version 17.0.6"
+sed -i -e "s/version=6.6.2/version=6.6.3/g" core/linux-api-headers/spkgbuild
+changelog "core/linux-api-headers/spkgbuild" "Upgraded from version 6.6.2 to version 6.6.3"
+sed -i -e "s/version=6.6.2/version=6.6.3/g" core/linux-desktop/spkgbuild
+changelog "core/linux-desktop/spkgbuild" "Upgraded from version 6.6.2 to version 6.6.3"
+sed -i -e "s/version=6.6.2/version=6.6.3/g" core/linux-router/spkgbuild
+changelog "core/linux-router/spkgbuild" "Upgraded from version 6.6.2 to version 6.6.3"
+sed -i -e "s/version=6.4.1/version=6.4.2/g" core/streamlink/spkgbuild
+changelog "core/streamlink/spkgbuild" "Upgraded from version 6.4.1 to version 6.4.2"
+sed -i -e "s/version=6.4.2/version=6.5.0/g" fonts/awesome-font/spkgbuild
+changelog "fonts/awesome-font/spkgbuild" "Upgraded from version 6.4.2 to version 6.5.0"
+sed -i -e "s/version=17.0.5/version=17.0.6/g" multilib/llvm-32/spkgbuild
+changelog "multilib/llvm-32/spkgbuild" "Upgraded from version 17.0.5 to version 17.0.6"
+sed -i -e "s/version=1.23.1/version=1.23.2/g" libs/gpgme/spkgbuild
+changelog "libs/gpgme/spkgbuild" "Upgraded from version 1.23.1 to version 1.23.2"
+sed -i -e "s/version=121.0b1/version=121.0b2/g" nonfree/thunderbird/spkgbuild
+changelog "nonfree/thunderbird/spkgbuild" "Upgraded from version 121.0b1 to version 121.0b2"
+sed -i -e "s/version=4.1.0/version=4.1.1/g" python/python-bcrypt/spkgbuild
+changelog "python/python-bcrypt/spkgbuild" "Upgraded from version 4.1.0 to version 4.1.1"
+sed -i -e "s/version=1.29.7/version=1.33.2/g" python/python-boto3/spkgbuild
+changelog "python/python-boto3/spkgbuild" "Upgraded from version 1.29.7 to version 1.33.2"
+sed -i -e "s/version=1.32.7/version=1.33.2/g" python/python-botocore/spkgbuild
+changelog "python/python-botocore/spkgbuild" "Upgraded from version 1.32.7 to version 1.33.2"
+sed -i -e "s/version=4.1.1/version=4.2.0/g" python/python-gitlab/spkgbuild
+changelog "python/python-gitlab/spkgbuild" "Upgraded from version 4.1.1 to version 4.2.0"
+sed -i -e "s/version=3.2.1/version=3.2.3/g" python/python-qdarkstyle/spkgbuild
+changelog "python/python-qdarkstyle/spkgbuild" "Upgraded from version 3.2.1 to version 3.2.3"
+sed -i -e "s/version=0.8.0/version=0.8.1/g" python/python-s3transfer/spkgbuild
+changelog "python/python-s3transfer/spkgbuild" "Upgraded from version 0.8.0 to version 0.8.1"
