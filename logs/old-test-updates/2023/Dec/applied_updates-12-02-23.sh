cf996f1fdefa5f231c01bcbf833580d815b32f5e
Voncloft <voncloft@gmail.com>
2023-12-02 21:19:14 -0500

    Minor Updates

---
+++ logs/old-test-updates/2023/Dec/applied_updates-12-02-23.sh
@@ -0,0 +1,74 @@
+sed -i -e "s/version=5.8.4/version=6.0.0/g" cinnamon/cinnamon/spkgbuild
+changelog "cinnamon/cinnamon/spkgbuild" "Upgraded from version 5.8.4 to version 6.0.0"
+sed -i -e "s/version=2.6.2/version=2.6.3/g" core/enchant/spkgbuild
+changelog "core/enchant/spkgbuild" "Upgraded from version 2.6.2 to version 2.6.3"
+sed -i -e "s/version=7.20.0/version=7.22.0/g" core/mlt/spkgbuild
+changelog "core/mlt/spkgbuild" "Upgraded from version 7.20.0 to version 7.22.0"
+sed -i -e "s/version=23.10.1/version=23.12/g" extra/anki/spkgbuild
+changelog "extra/anki/spkgbuild" "Upgraded from version 23.10.1 to version 23.12"
+sed -i -e "s/version=2.42.5/version=2.42.6/g" extra/discordchatexporter/spkgbuild
+changelog "extra/discordchatexporter/spkgbuild" "Upgraded from version 2.42.5 to version 2.42.6"
+sed -i -e "s/version=6.5.0/version=6.5.1/g" fonts/awesome-font/spkgbuild
+changelog "fonts/awesome-font/spkgbuild" "Upgraded from version 6.5.0 to version 6.5.1"
+sed -i -e "s/version=1.22.1/version=1.23.0/g" libs/c-ares/spkgbuild
+changelog "libs/c-ares/spkgbuild" "Upgraded from version 1.22.1 to version 1.23.0"
+sed -i -e "s/version=2.5.4/version=2.5.5/g" libs/libseccomp/spkgbuild
+changelog "libs/libseccomp/spkgbuild" "Upgraded from version 2.5.4 to version 2.5.5"
+sed -i -e "s/version=1.29.0/version=1.30.0/g" libs/pkcs11-helper/spkgbuild
+changelog "libs/pkcs11-helper/spkgbuild" "Upgraded from version 1.29.0 to version 1.30.0"
+sed -i -e "s/version=23.11.0/version=23.12.0/g" libs/poppler/spkgbuild
+changelog "libs/poppler/spkgbuild" "Upgraded from version 23.11.0 to version 23.12.0"
+sed -i -e "s/version=1.45.7/version=1.45.8/g" networking/networkmanager/spkgbuild
+changelog "networking/networkmanager/spkgbuild" "Upgraded from version 1.45.7 to version 1.45.8"
+sed -i -e "s/version=121.0b4/version=121.0b6/g" nonfree/firefox/spkgbuild
+changelog "nonfree/firefox/spkgbuild" "Upgraded from version 121.0b4 to version 121.0b6"
+sed -i -e "s/version=121.0b2/version=121.0b3/g" nonfree/thunderbird/spkgbuild
+changelog "nonfree/thunderbird/spkgbuild" "Upgraded from version 121.0b2 to version 121.0b3"
+sed -i -e "s/version=2.7.0/version=2.8.0/g" python/python-aiobotocore/spkgbuild
+changelog "python/python-aiobotocore/spkgbuild" "Upgraded from version 2.7.0 to version 2.8.0"
+sed -i -e "s/version=3.44.0.0/version=3.44.2.0/g" python/python-apsw/spkgbuild
+changelog "python/python-apsw/spkgbuild" "Upgraded from version 3.44.0.0 to version 3.44.2.0"
+sed -i -e "s/version=4.1.3/version=4.1.4/g" python/python-bitstring/spkgbuild
+changelog "python/python-bitstring/spkgbuild" "Upgraded from version 4.1.3 to version 4.1.4"
+sed -i -e "s/version=1.33.2/version=1.33.6/g" python/python-boto3/spkgbuild
+changelog "python/python-boto3/spkgbuild" "Upgraded from version 1.33.2 to version 1.33.6"
+sed -i -e "s/version=1.33.2/version=1.33.6/g" python/python-botocore/spkgbuild
+changelog "python/python-botocore/spkgbuild" "Upgraded from version 1.33.2 to version 1.33.6"
+sed -i -e "s/version=6.7.0/version=6.8.0/g" python/python-colorlog/spkgbuild
+changelog "python/python-colorlog/spkgbuild" "Upgraded from version 6.7.0 to version 6.8.0"
+sed -i -e "s/version=3.0.2/version=3.0.3/g" python/python-connexion/spkgbuild
+changelog "python/python-connexion/spkgbuild" "Upgraded from version 3.0.2 to version 3.0.3"
+sed -i -e "s/version=2.10.69/version=2.10.70/g" python/python-construct/spkgbuild
+changelog "python/python-construct/spkgbuild" "Upgraded from version 2.10.69 to version 2.10.70"
+sed -i -e "s/version=6.15.10/version=6.15.11/g" python/python-cx-freeze/spkgbuild
+changelog "python/python-cx-freeze/spkgbuild" "Upgraded from version 6.15.10 to version 6.15.11"
+sed -i -e "s/version=2023.11.0/version=2023.12.0/g" python/python-distributed/spkgbuild
+changelog "python/python-distributed/spkgbuild" "Upgraded from version 2023.11.0 to version 2023.12.0"
+sed -i -e "s/version=4.45.1/version=4.46.0/g" python/python-fonttools/spkgbuild
+changelog "python/python-fonttools/spkgbuild" "Upgraded from version 4.45.1 to version 4.46.0"
+sed -i -e "s/version=2.108.0/version=2.109.0/g" python/python-google-api-python-client/spkgbuild
+changelog "python/python-google-api-python-client/spkgbuild" "Upgraded from version 2.108.0 to version 2.109.0"
+sed -i -e "s/version=2.23.4/version=2.24.0/g" python/python-google-auth/spkgbuild
+changelog "python/python-google-auth/spkgbuild" "Upgraded from version 2.23.4 to version 2.24.0"
+sed -i -e "s/version=6.8.0/version=6.9.0/g" python/python-importlib-metadata/spkgbuild
+changelog "python/python-importlib-metadata/spkgbuild" "Upgraded from version 6.8.0 to version 6.9.0"
+sed -i -e "s/version=0.5.3/version=0.5.4/g" python/python-patsy/spkgbuild
+changelog "python/python-patsy/spkgbuild" "Upgraded from version 0.5.3 to version 0.5.4"
+sed -i -e "s/version=1.2.1/version=1.3.1/g" python/python-pipx/spkgbuild
+changelog "python/python-pipx/spkgbuild" "Upgraded from version 1.2.1 to version 1.3.1"
+sed -i -e "s/version=5.5.0/version=5.5.1/g" python/python-pynamodb/spkgbuild
+changelog "python/python-pynamodb/spkgbuild" "Upgraded from version 5.5.0 to version 5.5.1"
+sed -i -e "s/version=0.8.1/version=0.8.2/g" python/python-s3transfer/spkgbuild
+changelog "python/python-s3transfer/spkgbuild" "Upgraded from version 0.8.1 to version 0.8.2"
+sed -i -e "s/version=0.32.0/version=0.33.0/g" python/python-starlette/spkgbuild
+changelog "python/python-starlette/spkgbuild" "Upgraded from version 0.32.0 to version 0.33.0"
+sed -i -e "s/version=20.24.7/version=20.25.0/g" python/python-virtualenv/spkgbuild
+changelog "python/python-virtualenv/spkgbuild" "Upgraded from version 20.24.7 to version 20.25.0"
+sed -i -e "s/version=3.14.34/version=3.14.35/g" server/blacklist-hosts/spkgbuild
+changelog "server/blacklist-hosts/spkgbuild" "Upgraded from version 3.14.34 to version 3.14.35"
+sed -i -e "s/version=2023.11.3/version=2023.12.0/g" server/homeassistant/spkgbuild
+changelog "server/homeassistant/spkgbuild" "Upgraded from version 2023.11.3 to version 2023.12.0"
+sed -i -e "s/version=4.18.0/version=4.18.1/g" xfce/parole/spkgbuild
+changelog "xfce/parole/spkgbuild" "Upgraded from version 4.18.0 to version 4.18.1"
+sed -i -e "s/version=4.19.1/version=4.19.2/g" xfce/xfce4-power-manager/spkgbuild
+changelog "xfce/xfce4-power-manager/spkgbuild" "Upgraded from version 4.19.1 to version 4.19.2"
