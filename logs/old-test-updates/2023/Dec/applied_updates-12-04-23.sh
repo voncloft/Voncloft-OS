072c25a89d14cee2f73b864322e08ce9430e8265
Voncloft <voncloft@gmail.com>
2023-12-04 03:45:32 -0500

    Minor Updates

---
+++ logs/old-test-updates/2023/Dec/applied_updates-12-04-23.sh
@@ -0,0 +1,32 @@
+sed -i -e "s/version=6.0.0/version=6.0.1/g" cinnamon/cinnamon-translations/spkgbuild
+changelog "cinnamon/cinnamon-translations/spkgbuild" "Upgraded from version 6.0.0 to version 6.0.1"
+sed -i -e "s/version=13.2/version=14.1/g" compilers/gdb/spkgbuild
+changelog "compilers/gdb/spkgbuild" "Upgraded from version 13.2 to version 14.1"
+sed -i -e "s/version=2.15/version=2.16/g" core/lcms2/spkgbuild
+changelog "core/lcms2/spkgbuild" "Upgraded from version 2.15 to version 2.16"
+sed -i -e "s/version=6.6.3/version=6.6.4/g" core/linux-api-headers/spkgbuild
+changelog "core/linux-api-headers/spkgbuild" "Upgraded from version 6.6.3 to version 6.6.4"
+sed -i -e "s/version=6.6.3/version=6.6.4/g" core/linux-desktop/spkgbuild
+changelog "core/linux-desktop/spkgbuild" "Upgraded from version 6.6.3 to version 6.6.4"
+sed -i -e "s/version=6.6.3/version=6.6.4/g" core/linux-router/spkgbuild
+changelog "core/linux-router/spkgbuild" "Upgraded from version 6.6.3 to version 6.6.4"
+sed -i -e "s/version=4.1.1/version=4.1.2/g" core/weechat/spkgbuild
+changelog "core/weechat/spkgbuild" "Upgraded from version 4.1.1 to version 4.1.2"
+sed -i -e "s/version=2.8.0/version=2.8.1/g" libs/xapp/spkgbuild
+changelog "libs/xapp/spkgbuild" "Upgraded from version 2.8.0 to version 2.8.1"
+sed -i -e "s/version=1.2.1/version=1.3.0/g" python/python-freezegun/spkgbuild
+changelog "python/python-freezegun/spkgbuild" "Upgraded from version 1.2.1 to version 1.3.0"
+sed -i -e "s/version=2023.10.0/version=2023.12.0/g" python/python-fsspec/spkgbuild
+changelog "python/python-fsspec/spkgbuild" "Upgraded from version 2023.10.0 to version 2023.12.0"
+sed -i -e "s/version=6.9.0/version=7.0.0/g" python/python-importlib-metadata/spkgbuild
+changelog "python/python-importlib-metadata/spkgbuild" "Upgraded from version 6.9.0 to version 7.0.0"
+sed -i -e "s/version=2.4.10/version=2.4.11/g" python/python-markdown2/spkgbuild
+changelog "python/python-markdown2/spkgbuild" "Upgraded from version 2.4.10 to version 2.4.11"
+sed -i -e "s/version=1.3.1/version=1.3.2/g" python/python-pipx/spkgbuild
+changelog "python/python-pipx/spkgbuild" "Upgraded from version 1.3.1 to version 1.3.2"
+sed -i -e "s/version=0.23.0/version=0.23.1/g" python/python-pytest-asyncio/spkgbuild
+changelog "python/python-pytest-asyncio/spkgbuild" "Upgraded from version 0.23.0 to version 0.23.1"
+sed -i -e "s/version=0.127.0/version=0.128.0/g" python/python-zeroconf/spkgbuild
+changelog "python/python-zeroconf/spkgbuild" "Upgraded from version 0.127.0 to version 0.128.0"
+sed -i -e "s/version=1.2.8/version=1.2.10/g" xfce/xfce4-cpugraph-plugin/spkgbuild
+changelog "xfce/xfce4-cpugraph-plugin/spkgbuild" "Upgraded from version 1.2.8 to version 1.2.10"
