bddf8ba0c70755c9eecb1f155c7fbcd5ad44a5e2
Voncloft <voncloft@gmail.com>
2023-12-06 06:35:58 -0500

    Minor Updates

---
+++ logs/old-test-updates/2023/Dec/applied_updates-12-06-23.sh
@@ -0,0 +1,38 @@
+sed -i -e "s/version=0.120.4/version=0.121.0/g" core/hugo/spkgbuild
+changelog "core/hugo/spkgbuild" "Upgraded from version 0.120.4 to version 0.121.0"
+sed -i -e "s/version=2.39.2/version=2.39.3/g" core/util-linux/spkgbuild
+changelog "core/util-linux/spkgbuild" "Upgraded from version 2.39.2 to version 2.39.3"
+sed -i -e "s/version=2.43.1/version=2.43.2/g" core/webkitgtk/spkgbuild
+changelog "core/webkitgtk/spkgbuild" "Upgraded from version 2.43.1 to version 2.43.2"
+sed -i -e "s/version=3.10.2/version=3.10.3/g" extra/flexget/spkgbuild
+changelog "extra/flexget/spkgbuild" "Upgraded from version 3.10.2 to version 3.10.3"
+sed -i -e "s/version=121.0b6/version=121.0b7/g" nonfree/firefox/spkgbuild
+changelog "nonfree/firefox/spkgbuild" "Upgraded from version 121.0b6 to version 121.0b7"
+sed -i -e "s/version=121.0b3/version=121.0b4/g" nonfree/thunderbird/spkgbuild
+changelog "nonfree/thunderbird/spkgbuild" "Upgraded from version 121.0b3 to version 121.0b4"
+sed -i -e "s/version=1.33.6/version=1.33.7/g" python/python-boto3/spkgbuild
+changelog "python/python-boto3/spkgbuild" "Upgraded from version 1.33.6 to version 1.33.7"
+sed -i -e "s/version=1.33.6/version=1.33.7/g" python/python-botocore/spkgbuild
+changelog "python/python-botocore/spkgbuild" "Upgraded from version 1.33.6 to version 1.33.7"
+sed -i -e "s/version=4.2.0/version=4.3.0/g" python/python-chameleon/spkgbuild
+changelog "python/python-chameleon/spkgbuild" "Upgraded from version 4.2.0 to version 4.3.0"
+sed -i -e "s/version=1.3.0/version=1.3.1/g" python/python-freezegun/spkgbuild
+changelog "python/python-freezegun/spkgbuild" "Upgraded from version 1.3.0 to version 1.3.1"
+sed -i -e "s/version=2023.12.0/version=2023.12.1/g" python/python-fsspec/spkgbuild
+changelog "python/python-fsspec/spkgbuild" "Upgraded from version 2023.12.0 to version 2023.12.1"
+sed -i -e "s/version=1.0.6/version=1.0.7/g" python/python-fuse/spkgbuild
+changelog "python/python-fuse/spkgbuild" "Upgraded from version 1.0.6 to version 1.0.7"
+sed -i -e "s/version=2.24.0/version=2.25.0/g" python/python-google-auth/spkgbuild
+changelog "python/python-google-auth/spkgbuild" "Upgraded from version 2.24.0 to version 2.25.0"
+sed -i -e "s/version=16.7.1/version=16.9.0/g" python/python-path/spkgbuild
+changelog "python/python-path/spkgbuild" "Upgraded from version 16.7.1 to version 16.9.0"
+sed -i -e "s/version=1.3.2/version=1.3.3/g" python/python-pipx/spkgbuild
+changelog "python/python-pipx/spkgbuild" "Upgraded from version 1.3.2 to version 1.3.3"
+sed -i -e "s/version=0.4.3/version=0.5.0/g" python/python-pynvim/spkgbuild
+changelog "python/python-pynvim/spkgbuild" "Upgraded from version 0.4.3 to version 0.5.0"
+sed -i -e "s/version=0.23.1/version=0.23.2/g" python/python-pytest-asyncio/spkgbuild
+changelog "python/python-pytest-asyncio/spkgbuild" "Upgraded from version 0.23.1 to version 0.23.2"
+sed -i -e "s/version=6.0.0/version=6.0.1/g" python/python-pytest-runner/spkgbuild
+changelog "python/python-pytest-runner/spkgbuild" "Upgraded from version 6.0.0 to version 6.0.1"
+sed -i -e "s/version=3.14.35/version=3.14.36/g" server/blacklist-hosts/spkgbuild
+changelog "server/blacklist-hosts/spkgbuild" "Upgraded from version 3.14.35 to version 3.14.36"
