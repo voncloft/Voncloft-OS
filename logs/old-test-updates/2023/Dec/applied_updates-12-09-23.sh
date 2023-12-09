feac3b947881ef6324d015e3168a4342f85f56b6
Voncloft <voncloft@gmail.com>
2023-12-09 17:49:01 -0500

    Minor Updates

---
+++ logs/old-test-updates/2023/Dec/applied_updates-12-09-23.sh
@@ -0,0 +1,6 @@
+sed -i -e "s/version=0.6.2/version=0.7.0/g" python/python-aioconsole/spkgbuild
+changelog "python/python-aioconsole/spkgbuild" "Upgraded from version 0.6.2 to version 0.7.0"
+sed -i -e "s/version=0.23.2/version=0.23.3/g" python/python-pytest-asyncio/spkgbuild
+changelog "python/python-pytest-asyncio/spkgbuild" "Upgraded from version 0.23.2 to version 0.23.3"
+sed -i -e "s/version=2023.12.0/version=2023.12.1/g" server/homeassistant/spkgbuild
+changelog "server/homeassistant/spkgbuild" "Upgraded from version 2023.12.0 to version 2023.12.1"
