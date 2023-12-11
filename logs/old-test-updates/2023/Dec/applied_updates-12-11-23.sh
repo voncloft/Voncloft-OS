dcd19f8dc60b649eea68b4d938850be7ddb5f24b
Voncloft <voncloft@gmail.com>
2023-12-11 04:33:22 -0500

    Minor Updates

---
+++ logs/old-test-updates/2023/Dec/applied_updates-12-11-23.sh
@@ -0,0 +1,6 @@
+sed -i -e "s/version=6.0.10/version=6.0.11/g" python/python-feedparser/spkgbuild
+changelog "python/python-feedparser/spkgbuild" "Upgraded from version 6.0.10 to version 6.0.11"
+sed -i -e "s/version=6.91.1/version=6.92.0/g" python/python-hypothesis/spkgbuild
+changelog "python/python-hypothesis/spkgbuild" "Upgraded from version 6.91.1 to version 6.92.0"
+sed -i -e "s/version=3.5.0/version=3.6.0/g" python/python-pre-commit/spkgbuild
+changelog "python/python-pre-commit/spkgbuild" "Upgraded from version 3.5.0 to version 3.6.0"
