ee3a7b570ba2c8869e46c37a593947c92cccbf5b
Voncloft <voncloft@gmail.com>
2023-12-12 03:58:42 -0500

    Minor Updates

---
+++ logs/old-test-updates/2023/Dec/applied_updates-12-12-23.sh
@@ -0,0 +1,22 @@
+sed -i -e "s/version=3.2.2/version=3.3.0.rc1/g" compilers/ruby/spkgbuild
+changelog "compilers/ruby/spkgbuild" "Upgraded from version 3.2.2 to version 3.3.0.rc1"
+sed -i -e "s/version=2.6.3/version=2.6.4/g" core/enchant/spkgbuild
+changelog "core/enchant/spkgbuild" "Upgraded from version 2.6.3 to version 2.6.4"
+sed -i -e "s/version=6.6.5/version=6.6.6/g" core/linux-api-headers/spkgbuild
+changelog "core/linux-api-headers/spkgbuild" "Upgraded from version 6.6.5 to version 6.6.6"
+sed -i -e "s/version=6.6.5/version=6.6.6/g" core/linux-desktop/spkgbuild
+changelog "core/linux-desktop/spkgbuild" "Upgraded from version 6.6.5 to version 6.6.6"
+sed -i -e "s/version=6.6.5/version=6.6.6/g" core/linux-router/spkgbuild
+changelog "core/linux-router/spkgbuild" "Upgraded from version 6.6.5 to version 6.6.6"
+sed -i -e "s/version=2.2.0/version=2.2.1/g" core/zxing-cpp/spkgbuild
+changelog "core/zxing-cpp/spkgbuild" "Upgraded from version 2.2.0 to version 2.2.1"
+sed -i -e "s/version=3.10.4/version=3.10.5/g" extra/flexget/spkgbuild
+changelog "extra/flexget/spkgbuild" "Upgraded from version 3.10.4 to version 3.10.5"
+sed -i -e "s/version=0.5.1/version=0.5.4/g" python/python-edalize/spkgbuild
+changelog "python/python-edalize/spkgbuild" "Upgraded from version 0.5.1 to version 0.5.4"
+sed -i -e "s/version=3.0.2/version=3.0.3/g" python/python-pylint/spkgbuild
+changelog "python/python-pylint/spkgbuild" "Upgraded from version 3.0.2 to version 3.0.3"
+sed -i -e "s/version=5.8.0/version=5.9.0/g" python/python-ujson/spkgbuild
+changelog "python/python-ujson/spkgbuild" "Upgraded from version 5.8.0 to version 5.9.0"
+sed -i -e "s/version=0.128.0/version=0.128.4/g" python/python-zeroconf/spkgbuild
+changelog "python/python-zeroconf/spkgbuild" "Upgraded from version 0.128.0 to version 0.128.4"
