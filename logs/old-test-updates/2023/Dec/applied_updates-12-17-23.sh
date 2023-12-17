d55133f7cad574b7e5e0758d85695b791caa71a3
Voncloft <voncloft@gmail.com>
2023-12-17 11:11:52 -0500

    Minor Updates

---
+++ logs/old-test-updates/2023/Dec/applied_updates-12-17-23.sh
@@ -0,0 +1,34 @@
+sed -i -e "s/version=4.99.0/version=4.99.2/g" core/lsof/spkgbuild
+changelog "core/lsof/spkgbuild" "Upgraded from version 4.99.0 to version 4.99.2"
+sed -i -e "s/version=3.95/version=3.96/g" core/nss/spkgbuild
+changelog "core/nss/spkgbuild" "Upgraded from version 3.95 to version 3.96"
+sed -i -e "s/version=6.4.2/version=6.5.0/g" core/streamlink/spkgbuild
+changelog "core/streamlink/spkgbuild" "Upgraded from version 6.4.2 to version 6.5.0"
+sed -i -e "s/version=7.8.0/version=7.8.2/g" libs/libatomic_ops/spkgbuild
+changelog "libs/libatomic_ops/spkgbuild" "Upgraded from version 7.8.0 to version 7.8.2"
+sed -i -e "s/version=3.1.10/version=3.1.11/g" nonfree/pandoc-bin/spkgbuild
+changelog "nonfree/pandoc-bin/spkgbuild" "Upgraded from version 3.1.10 to version 3.1.11"
+sed -i -e "s/version=4.1.0/version=4.2.0/g" python/python-anyio/spkgbuild
+changelog "python/python-anyio/spkgbuild" "Upgraded from version 4.1.0 to version 4.2.0"
+sed -i -e "s/version=4.1.1/version=4.1.2/g" python/python-bcrypt/spkgbuild
+changelog "python/python-bcrypt/spkgbuild" "Upgraded from version 4.1.1 to version 4.1.2"
+sed -i -e "s/version=1.34.0/version=1.34.2/g" python/python-boto3/spkgbuild
+changelog "python/python-boto3/spkgbuild" "Upgraded from version 1.34.0 to version 1.34.2"
+sed -i -e "s/version=1.34.0/version=1.34.2/g" python/python-botocore/spkgbuild
+changelog "python/python-botocore/spkgbuild" "Upgraded from version 1.34.0 to version 1.34.2"
+sed -i -e "s/version=2023.12.0/version=2023.12.1/g" python/python-distributed/spkgbuild
+changelog "python/python-distributed/spkgbuild" "Upgraded from version 2023.12.0 to version 2023.12.1"
+sed -i -e "s/version=6.92.0/version=6.92.1/g" python/python-hypothesis/spkgbuild
+changelog "python/python-hypothesis/spkgbuild" "Upgraded from version 6.92.0 to version 6.92.1"
+sed -i -e "s/version=1.9.0/version=1.10.0/g" python/python-lazy-object-proxy/spkgbuild
+changelog "python/python-lazy-object-proxy/spkgbuild" "Upgraded from version 1.9.0 to version 1.10.0"
+sed -i -e "s/version=0.33.0/version=0.34.0/g" python/python-starlette/spkgbuild
+changelog "python/python-starlette/spkgbuild" "Upgraded from version 0.33.0 to version 0.34.0"
+sed -i -e "s/version=0.129.0/version=0.130.0/g" python/python-zeroconf/spkgbuild
+changelog "python/python-zeroconf/spkgbuild" "Upgraded from version 0.129.0 to version 0.130.0"
+sed -i -e "s/version=2.4.22/version=2.5.1/g" ruby-gems/ruby-bundler/spkgbuild
+changelog "ruby-gems/ruby-bundler/spkgbuild" "Upgraded from version 2.4.22 to version 2.5.1"
+sed -i -e "s/version=3.14.37/version=3.14.38/g" server/blacklist-hosts/spkgbuild
+changelog "server/blacklist-hosts/spkgbuild" "Upgraded from version 3.14.37 to version 3.14.38"
+sed -i -e "s/version=2023.12.2/version=2023.12.3/g" server/homeassistant/spkgbuild
+changelog "server/homeassistant/spkgbuild" "Upgraded from version 2023.12.2 to version 2023.12.3"
