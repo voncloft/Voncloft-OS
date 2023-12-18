4670e353741b818bb1220b5180e49c991179fb57
Voncloft <voncloft@gmail.com>
2023-12-18 04:54:49 -0500

    Minor Updates

---
+++ logs/old-test-updates/2023/Dec/applied_updates-12-18-23.sh
@@ -0,0 +1,16 @@
+sed -i -e "s/version=4.99.2/version=4.99.3/g" core/lsof/spkgbuild
+changelog "core/lsof/spkgbuild" "Upgraded from version 4.99.2 to version 4.99.3"
+sed -i -e "s/version=12.7.4/version=12.7.5/g" core/sysstat/spkgbuild
+changelog "core/sysstat/spkgbuild" "Upgraded from version 12.7.4 to version 12.7.5"
+sed -i -e "s/version=1.23.0/version=1.24.0/g" libs/c-ares/spkgbuild
+changelog "libs/c-ares/spkgbuild" "Upgraded from version 1.23.0 to version 1.24.0"
+sed -i -e "s/version=1.5.1/version=1.6.0/g" python/python-aiodiscover/spkgbuild
+changelog "python/python-aiodiscover/spkgbuild" "Upgraded from version 1.5.1 to version 1.6.0"
+sed -i -e "s/version=23.3.1/version=23.3.2/g" python/python-pip/spkgbuild
+changelog "python/python-pip/spkgbuild" "Upgraded from version 23.3.1 to version 23.3.2"
+sed -i -e "s/version=1.1.1/version=1.2.0/g" python/python-pook/spkgbuild
+changelog "python/python-pook/spkgbuild" "Upgraded from version 1.1.1 to version 1.2.0"
+sed -i -e "s/version=5.9.6/version=5.9.7/g" python/python-psutil/spkgbuild
+changelog "python/python-psutil/spkgbuild" "Upgraded from version 5.9.6 to version 5.9.7"
+sed -i -e "s/version=3.14.38/version=3.14.40/g" server/blacklist-hosts/spkgbuild
+changelog "server/blacklist-hosts/spkgbuild" "Upgraded from version 3.14.38 to version 3.14.40"
