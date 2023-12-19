dee5e8bc647d588b18182ef4d4edae4e8b21f747
Voncloft <voncloft@gmail.com>
2023-12-19 06:26:05 -0500

    Minor Updates

---
+++ logs/old-test-updates/2023/Dec/applied_updates-12-19-23.sh
@@ -0,0 +1,30 @@
+sed -i -e "s/version=3.96/version=3.96.1/g" core/nss/spkgbuild
+changelog "core/nss/spkgbuild" "Upgraded from version 3.96 to version 3.96.1"
+sed -i -e "s/version=5.4/version=5.5/g" extra/gnucash/spkgbuild
+changelog "extra/gnucash/spkgbuild" "Upgraded from version 5.4 to version 5.5"
+sed -i -e "s/version=1.22.7/version=1.22.8/g" media/gst-libav/spkgbuild
+changelog "media/gst-libav/spkgbuild" "Upgraded from version 1.22.7 to version 1.22.8"
+sed -i -e "s/version=1.22.7/version=1.22.8/g" media/gst-plugins-bad/spkgbuild
+changelog "media/gst-plugins-bad/spkgbuild" "Upgraded from version 1.22.7 to version 1.22.8"
+sed -i -e "s/version=1.22.7/version=1.22.8/g" media/gst-plugins-base/spkgbuild
+changelog "media/gst-plugins-base/spkgbuild" "Upgraded from version 1.22.7 to version 1.22.8"
+sed -i -e "s/version=1.22.7/version=1.22.8/g" media/gst-plugins-good/spkgbuild
+changelog "media/gst-plugins-good/spkgbuild" "Upgraded from version 1.22.7 to version 1.22.8"
+sed -i -e "s/version=1.22.7/version=1.22.8/g" media/gst-plugins-ugly/spkgbuild
+changelog "media/gst-plugins-ugly/spkgbuild" "Upgraded from version 1.22.7 to version 1.22.8"
+sed -i -e "s/version=1.22.7/version=1.22.8/g" media/gstreamer/spkgbuild
+changelog "media/gstreamer/spkgbuild" "Upgraded from version 1.22.7 to version 1.22.8"
+sed -i -e "s/version=9.5/version=9.6/g" networking/openssh/spkgbuild
+changelog "networking/openssh/spkgbuild" "Upgraded from version 9.5 to version 9.6"
+sed -i -e "s/version=4.4.0/version=4.4.1/g" python/python-chameleon/spkgbuild
+changelog "python/python-chameleon/spkgbuild" "Upgraded from version 4.4.0 to version 4.4.1"
+sed -i -e "s/version=6.15.11/version=6.15.12/g" python/python-cx-freeze/spkgbuild
+changelog "python/python-cx-freeze/spkgbuild" "Upgraded from version 6.15.11 to version 6.15.12"
+sed -i -e "s/version=4.46.0/version=4.47.0/g" python/python-fonttools/spkgbuild
+changelog "python/python-fonttools/spkgbuild" "Upgraded from version 4.46.0 to version 4.47.0"
+sed -i -e "s/version=5.5.0/version=5.5.1/g" python/python-jupyter_core/spkgbuild
+changelog "python/python-jupyter_core/spkgbuild" "Upgraded from version 5.5.0 to version 5.5.1"
+sed -i -e "s/version=2.4.11/version=2.4.12/g" python/python-markdown2/spkgbuild
+changelog "python/python-markdown2/spkgbuild" "Upgraded from version 2.4.11 to version 2.4.12"
+sed -i -e "s/version=3.3.1/version=3.4.0/g" python/python-paramiko/spkgbuild
+changelog "python/python-paramiko/spkgbuild" "Upgraded from version 3.3.1 to version 3.4.0"
