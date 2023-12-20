615b5859c91bc20dca95bf2d06f0df989af3faa1
Voncloft <voncloft@gmail.com>
2023-12-20 04:01:45 -0500

    Minor Updates

---
+++ logs/old-test-updates/2023/Dec/applied_updates-12-20-23.sh
@@ -0,0 +1,32 @@
+sed -i -e "s/version=6.0.1/version=6.0.2/g" cinnamon/cinnamon/spkgbuild
+changelog "cinnamon/cinnamon/spkgbuild" "Upgraded from version 6.0.1 to version 6.0.2"
+sed -i -e "s/version=6.0.0/version=6.0.1/g" cinnamon/cinnamon-screensaver/spkgbuild
+changelog "cinnamon/cinnamon-screensaver/spkgbuild" "Upgraded from version 6.0.0 to version 6.0.1"
+sed -i -e "s/version=6.0.0/version=6.0.1/g" cinnamon/nemo/spkgbuild
+changelog "cinnamon/nemo/spkgbuild" "Upgraded from version 6.0.0 to version 6.0.1"
+sed -i -e "s/version=121.0b9/version=122.0b1/g" nonfree/firefox/spkgbuild
+changelog "nonfree/firefox/spkgbuild" "Upgraded from version 121.0b9 to version 122.0b1"
+sed -i -e "s/version=1.34.2/version=1.34.4/g" python/python-boto3/spkgbuild
+changelog "python/python-boto3/spkgbuild" "Upgraded from version 1.34.2 to version 1.34.4"
+sed -i -e "s/version=1.34.2/version=1.34.4/g" python/python-botocore/spkgbuild
+changelog "python/python-botocore/spkgbuild" "Upgraded from version 1.34.2 to version 1.34.4"
+sed -i -e "s/version=4.24.0/version=4.25.0/g" python/python-braintree/spkgbuild
+changelog "python/python-braintree/spkgbuild" "Upgraded from version 4.24.0 to version 4.25.0"
+sed -i -e "s/version=4.4.1/version=4.4.2/g" python/python-chameleon/spkgbuild
+changelog "python/python-chameleon/spkgbuild" "Upgraded from version 4.4.1 to version 4.4.2"
+sed -i -e "s/version=1.8.0/version=1.9.0/g" python/python-distro/spkgbuild
+changelog "python/python-distro/spkgbuild" "Upgraded from version 1.8.0 to version 1.9.0"
+sed -i -e "s/version=1.3.1/version=1.4.0/g" python/python-freezegun/spkgbuild
+changelog "python/python-freezegun/spkgbuild" "Upgraded from version 1.3.1 to version 1.4.0"
+sed -i -e "s/version=1.32.0/version=1.33.0/g" python/python-hcloud/spkgbuild
+changelog "python/python-hcloud/spkgbuild" "Upgraded from version 1.32.0 to version 1.33.0"
+sed -i -e "s/version=28.1.0/version=29.0.0/g" python/python-kubernetes/spkgbuild
+changelog "python/python-kubernetes/spkgbuild" "Upgraded from version 28.1.0 to version 29.0.0"
+sed -i -e "s/version=4.9.3/version=4.9.4/g" python/python-lxml/spkgbuild
+changelog "python/python-lxml/spkgbuild" "Upgraded from version 4.9.3 to version 4.9.4"
+sed -i -e "s/version=4.0.5/version=4.0.6/g" python/python-trimesh/spkgbuild
+changelog "python/python-trimesh/spkgbuild" "Upgraded from version 4.0.5 to version 4.0.6"
+sed -i -e "s/version=2.5.0/version=2.5.1/g" python/python-xmlschema/spkgbuild
+changelog "python/python-xmlschema/spkgbuild" "Upgraded from version 2.5.0 to version 2.5.1"
+sed -i -e "s/version=0.130.0/version=0.131.0/g" python/python-zeroconf/spkgbuild
+changelog "python/python-zeroconf/spkgbuild" "Upgraded from version 0.130.0 to version 0.131.0"
