5bd010f5b531df4312d0401cce6e0cebfe598b66
Voncloft <voncloft@gmail.com>
2023-12-21 04:55:58 -0500

    Minor Updates

---
+++ logs/old-test-updates/2023/Dec/applied_updates-12-21-23.sh
@@ -0,0 +1,28 @@
+sed -i -e "s/version=0.60.8/version=0.60.8.1/g" core/aspell/spkgbuild
+changelog "core/aspell/spkgbuild" "Upgraded from version 0.60.8 to version 0.60.8.1"
+sed -i -e "s/version=2.06/version=2.12/g" core/grub/spkgbuild
+changelog "core/grub/spkgbuild" "Upgraded from version 2.06 to version 2.12"
+sed -i -e "s/version=6.6.7/version=6.6.8/g" core/linux-api-headers/spkgbuild
+changelog "core/linux-api-headers/spkgbuild" "Upgraded from version 6.6.7 to version 6.6.8"
+sed -i -e "s/version=6.6.7/version=6.6.8/g" core/linux-desktop/spkgbuild
+changelog "core/linux-desktop/spkgbuild" "Upgraded from version 6.6.7 to version 6.6.8"
+sed -i -e "s/version=6.6.7/version=6.6.8/g" core/linux-router/spkgbuild
+changelog "core/linux-router/spkgbuild" "Upgraded from version 6.6.7 to version 6.6.8"
+sed -i -e "s/version=2.20/version=2.21/g" libs/libmpdclient/spkgbuild
+changelog "libs/libmpdclient/spkgbuild" "Upgraded from version 2.20 to version 2.21"
+sed -i -e "s/version=9.19.18/version=9.19.19/g" networking/bind/spkgbuild
+changelog "networking/bind/spkgbuild" "Upgraded from version 9.19.18 to version 9.19.19"
+sed -i -e "s/version=122.0b1/version=122.0b2/g" nonfree/firefox/spkgbuild
+changelog "nonfree/firefox/spkgbuild" "Upgraded from version 122.0b1 to version 122.0b2"
+sed -i -e "s/version=7.3.3/version=7.3.4/g" python/python-coverage/spkgbuild
+changelog "python/python-coverage/spkgbuild" "Upgraded from version 7.3.3 to version 7.3.4"
+sed -i -e "s/version=0.33.3/version=0.34.1/g" python/python-eventlet/spkgbuild
+changelog "python/python-eventlet/spkgbuild" "Upgraded from version 0.33.3 to version 0.34.1"
+sed -i -e "s/version=8.13.26/version=8.13.27/g" python/python-phonenumbers/spkgbuild
+changelog "python/python-phonenumbers/spkgbuild" "Upgraded from version 8.13.26 to version 8.13.27"
+sed -i -e "s/version=1.3.2/version=1.4.0/g" python/python-scikit-learn/spkgbuild
+changelog "python/python-scikit-learn/spkgbuild" "Upgraded from version 1.3.2 to version 1.4.0"
+sed -i -e "s/version=6.1.10/version=6.1.11/g" python/python-softlayer/spkgbuild
+changelog "python/python-softlayer/spkgbuild" "Upgraded from version 6.1.10 to version 6.1.11"
+sed -i -e "s/version=4.0.6/version=4.0.7/g" python/python-trimesh/spkgbuild
+changelog "python/python-trimesh/spkgbuild" "Upgraded from version 4.0.6 to version 4.0.7"
