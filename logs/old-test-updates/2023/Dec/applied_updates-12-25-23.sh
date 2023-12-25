5b1bd4e2a9f1c534bd3e5bd0ee1bc1e3ce5b7692
Voncloft <voncloft@gmail.com>
2023-12-25 07:52:38 -0500

    Minor Updates

---
+++ logs/old-test-updates/2023/Dec/applied_updates-12-25-23.sh
@@ -0,0 +1,62 @@
+sed -i -e "s/version=2.71/version=2.72/g" compilers/autoconf/spkgbuild
+changelog "compilers/autoconf/spkgbuild" "Upgraded from version 2.71 to version 2.72"
+sed -i -e "s/version=5.19/version=6.7/g" core/iw/spkgbuild
+changelog "core/iw/spkgbuild" "Upgraded from version 5.19 to version 6.7"
+sed -i -e "s/version=0.34/version=0.35/g" core/mpc/spkgbuild
+changelog "core/mpc/spkgbuild" "Upgraded from version 0.34 to version 0.35"
+sed -i -e "s/version=2.43.2/version=2.43.3/g" core/webkitgtk/spkgbuild
+changelog "core/webkitgtk/spkgbuild" "Upgraded from version 2.43.2 to version 2.43.3"
+sed -i -e "s/version=23.10.1/version=23.12rc1/g" extra/anki/spkgbuild
+changelog "extra/anki/spkgbuild" "Upgraded from version 23.10.1 to version 23.12rc1"
+sed -i -e "s/version=3.10.6/version=3.10.7/g" extra/flexget/spkgbuild
+changelog "extra/flexget/spkgbuild" "Upgraded from version 3.10.6 to version 3.10.7"
+sed -i -e "s/version=2.14.2/version=2.15.0/g" fonts/fontconfig/spkgbuild
+changelog "fonts/fontconfig/spkgbuild" "Upgraded from version 2.14.2 to version 2.15.0"
+sed -i -e "s/version=2.0.2/version=2.0.3/g" libs/fdk-aac/spkgbuild
+changelog "libs/fdk-aac/spkgbuild" "Upgraded from version 2.0.2 to version 2.0.3"
+sed -i -e "s/version=2.4.118/version=2.4.119/g" libs/libdrm/spkgbuild
+changelog "libs/libdrm/spkgbuild" "Upgraded from version 2.4.118 to version 2.4.119"
+sed -i -e "s/version=2.21/version=2.22/g" libs/libmpdclient/spkgbuild
+changelog "libs/libmpdclient/spkgbuild" "Upgraded from version 2.21 to version 2.22"
+sed -i -e "s/version=3.6.5/version=3.6.6/g" libs/libsass/spkgbuild
+changelog "libs/libsass/spkgbuild" "Upgraded from version 3.6.5 to version 3.6.6"
+sed -i -e "s/version=122.0b2/version=122.0b3/g" nonfree/firefox/spkgbuild
+changelog "nonfree/firefox/spkgbuild" "Upgraded from version 122.0b2 to version 122.0b3"
+sed -i -e "s/version=121.0b6/version=122.0b1/g" nonfree/thunderbird/spkgbuild
+changelog "nonfree/thunderbird/spkgbuild" "Upgraded from version 121.0b6 to version 122.0b1"
+sed -i -e "s/version=6.3.0/version=6.3.1/g" python/python-apispec/spkgbuild
+changelog "python/python-apispec/spkgbuild" "Upgraded from version 6.3.0 to version 6.3.1"
+sed -i -e "s/version=1.34.4/version=1.34.7/g" python/python-boto3/spkgbuild
+changelog "python/python-boto3/spkgbuild" "Upgraded from version 1.34.4 to version 1.34.7"
+sed -i -e "s/version=1.34.4/version=1.34.7/g" python/python-botocore/spkgbuild
+changelog "python/python-botocore/spkgbuild" "Upgraded from version 1.34.4 to version 1.34.7"
+sed -i -e "s/version=0.34.1/version=0.34.2/g" python/python-eventlet/spkgbuild
+changelog "python/python-eventlet/spkgbuild" "Upgraded from version 0.34.1 to version 0.34.2"
+sed -i -e "s/version=7.0.0/version=7.0.1/g" python/python-importlib-metadata/spkgbuild
+changelog "python/python-importlib-metadata/spkgbuild" "Upgraded from version 7.0.0 to version 7.0.1"
+sed -i -e "s/version=23.5.0/version=23.12.0/g" python/python-klein/spkgbuild
+changelog "python/python-klein/spkgbuild" "Upgraded from version 23.5.0 to version 23.12.0"
+sed -i -e "s/version=2.2.0/version=2.3.0/g" python/python-pandas/spkgbuild
+changelog "python/python-pandas/spkgbuild" "Upgraded from version 2.2.0 to version 2.3.0"
+sed -i -e "s/version=14.2.0/version=14.3.0/g" python/python-pdoc/spkgbuild
+changelog "python/python-pdoc/spkgbuild" "Upgraded from version 14.2.0 to version 14.3.0"
+sed -i -e "s/version=1.2.0/version=1.2.1/g" python/python-pook/spkgbuild
+changelog "python/python-pook/spkgbuild" "Upgraded from version 1.2.0 to version 1.2.1"
+sed -i -e "s/version=2.5.2/version=2.5.3/g" python/python-pydantic/spkgbuild
+changelog "python/python-pydantic/spkgbuild" "Upgraded from version 2.5.2 to version 2.5.3"
+sed -i -e "s/version=2.3.1/version=3.0.0/g" python/python-pytest-enabler/spkgbuild
+changelog "python/python-pytest-enabler/spkgbuild" "Upgraded from version 2.3.1 to version 3.0.0"
+sed -i -e "s/version=0.27.0/version=0.28.0/g" python/python-pytest-httpx/spkgbuild
+changelog "python/python-pytest-httpx/spkgbuild" "Upgraded from version 0.27.0 to version 0.28.0"
+sed -i -e "s/version=0.9.0/version=0.10.0/g" python/python-s3transfer/spkgbuild
+changelog "python/python-s3transfer/spkgbuild" "Upgraded from version 0.9.0 to version 0.10.0"
+sed -i -e "s/version=4.0.7/version=4.0.8/g" python/python-trimesh/spkgbuild
+changelog "python/python-trimesh/spkgbuild" "Upgraded from version 4.0.7 to version 4.0.8"
+sed -i -e "s/version=6.2/version=6.2.1/g" python/python-zope-testrunner/spkgbuild
+changelog "python/python-zope-testrunner/spkgbuild" "Upgraded from version 6.2 to version 6.2.1"
+sed -i -e "s/version=2.5.1/version=2.5.3/g" ruby-gems/ruby-bundler/spkgbuild
+changelog "ruby-gems/ruby-bundler/spkgbuild" "Upgraded from version 2.5.1 to version 2.5.3"
+sed -i -e "s/version=8.3.0/version=8.3.1/g" server/php/spkgbuild
+changelog "server/php/spkgbuild" "Upgraded from version 8.3.0 to version 8.3.1"
+sed -i -e "s/version=3.8.3/version=3.8.4/g" server/postfix/spkgbuild
+changelog "server/postfix/spkgbuild" "Upgraded from version 3.8.3 to version 3.8.4"
