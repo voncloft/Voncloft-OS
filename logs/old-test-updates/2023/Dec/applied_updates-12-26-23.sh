0dc8d707cae44dced3d918a46a285cc4a8e98acf
Voncloft <voncloft@gmail.com>
2023-12-26 10:39:27 -0500

    Minor Updates

---
+++ logs/old-test-updates/2023/Dec/applied_updates-12-26-23.sh
@@ -0,0 +1,46 @@
+sed -i -e "s/version=3.10.7/version=3.11.0/g" extra/flexget/spkgbuild
+changelog "extra/flexget/spkgbuild" "Upgraded from version 3.10.7 to version 3.11.0"
+sed -i -e "s/version=2.9.0/version=2.9.1/g" python/python-dominate/spkgbuild
+changelog "python/python-dominate/spkgbuild" "Upgraded from version 2.9.0 to version 2.9.1"
+sed -i -e "s/version=0.105.0/version=0.106.0/g" python/python-fastapi/spkgbuild
+changelog "python/python-fastapi/spkgbuild" "Upgraded from version 0.105.0 to version 0.106.0"
+sed -i -e "s/version=0.9.0/version=1.0.0/g" python/python-feedgen/spkgbuild
+changelog "python/python-feedgen/spkgbuild" "Upgraded from version 0.9.0 to version 1.0.0"
+sed -i -e "s/version=2023.08/version=2023.12/g" python/python-litedram/spkgbuild
+changelog "python/python-litedram/spkgbuild" "Upgraded from version 2023.08 to version 2023.12"
+sed -i -e "s/version=2023.08/version=2023.12/g" python/python-liteeth/spkgbuild
+changelog "python/python-liteeth/spkgbuild" "Upgraded from version 2023.08 to version 2023.12"
+sed -i -e "s/version=2023.08/version=2023.12/g" python/python-liteiclink/spkgbuild
+changelog "python/python-liteiclink/spkgbuild" "Upgraded from version 2023.08 to version 2023.12"
+sed -i -e "s/version=2023.08/version=2023.12/g" python/python-litejesd204b/spkgbuild
+changelog "python/python-litejesd204b/spkgbuild" "Upgraded from version 2023.08 to version 2023.12"
+sed -i -e "s/version=2023.08/version=2023.12/g" python/python-litepcie/spkgbuild
+changelog "python/python-litepcie/spkgbuild" "Upgraded from version 2023.08 to version 2023.12"
+sed -i -e "s/version=2023.08/version=2023.12/g" python/python-litesata/spkgbuild
+changelog "python/python-litesata/spkgbuild" "Upgraded from version 2023.08 to version 2023.12"
+sed -i -e "s/version=2023.08/version=2023.12/g" python/python-litescope/spkgbuild
+changelog "python/python-litescope/spkgbuild" "Upgraded from version 2023.08 to version 2023.12"
+sed -i -e "s/version=2023.08/version=2023.12/g" python/python-litesdcard/spkgbuild
+changelog "python/python-litesdcard/spkgbuild" "Upgraded from version 2023.08 to version 2023.12"
+sed -i -e "s/version=2023.08/version=2023.12/g" python/python-liteusb/spkgbuild
+changelog "python/python-liteusb/spkgbuild" "Upgraded from version 2023.08 to version 2023.12"
+sed -i -e "s/version=2023.08/version=2023.12/g" python/python-litex/spkgbuild
+changelog "python/python-litex/spkgbuild" "Upgraded from version 2023.08 to version 2023.12"
+sed -i -e "s/version=2023.08/version=2023.12/g" python/python-litex-boards/spkgbuild
+changelog "python/python-litex-boards/spkgbuild" "Upgraded from version 2023.08 to version 2023.12"
+sed -i -e "s/version=3.12.0/version=3.12.2/g" python/python-mocket/spkgbuild
+changelog "python/python-mocket/spkgbuild" "Upgraded from version 3.12.0 to version 3.12.2"
+sed -i -e "s/version=0.5.4/version=0.5.5/g" python/python-patsy/spkgbuild
+changelog "python/python-patsy/spkgbuild" "Upgraded from version 0.5.4 to version 0.5.5"
+sed -i -e "s/version=1.2.1/version=1.3.0/g" python/python-pook/spkgbuild
+changelog "python/python-pook/spkgbuild" "Upgraded from version 1.2.1 to version 1.3.0"
+sed -i -e "s/version=2023.08/version=2023.12/g" python/python-pythondata-cpu-lm32/spkgbuild
+changelog "python/python-pythondata-cpu-lm32/spkgbuild" "Upgraded from version 2023.08 to version 2023.12"
+sed -i -e "s/version=2023.08/version=2023.12/g" python/python-pythondata-cpu-mor1kx/spkgbuild
+changelog "python/python-pythondata-cpu-mor1kx/spkgbuild" "Upgraded from version 2023.08 to version 2023.12"
+sed -i -e "s/version=2023.08/version=2023.12/g" python/python-pythondata-cpu-picorv32/spkgbuild
+changelog "python/python-pythondata-cpu-picorv32/spkgbuild" "Upgraded from version 2023.08 to version 2023.12"
+sed -i -e "s/version=2023.08/version=2023.12/g" python/python-pythondata-cpu-vexriscv/spkgbuild
+changelog "python/python-pythondata-cpu-vexriscv/spkgbuild" "Upgraded from version 2023.08 to version 2023.12"
+sed -i -e "s/version=2023.08/version=2023.12/g" python/python-pythondata-software-compiler_rt/spkgbuild
+changelog "python/python-pythondata-software-compiler_rt/spkgbuild" "Upgraded from version 2023.08 to version 2023.12"
