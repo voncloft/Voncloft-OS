4127bcc7b9038a3a0d8925324d2f4a341ce96de7
Voncloft <voncloft@gmail.com>
2023-12-27 06:14:21 -0500

    Minor Updates

---
+++ logs/old-test-updates/2023/Dec/applied_updates-12-27-23.sh
@@ -0,0 +1,24 @@
+sed -i -e "s/version=1.3.0/version=1.3.1/g" compilers/meson/spkgbuild
+changelog "compilers/meson/spkgbuild" "Upgraded from version 1.3.0 to version 1.3.1"
+sed -i -e "s/version=23.6.0/version=23.12.0/g" core/smplayer/spkgbuild
+changelog "core/smplayer/spkgbuild" "Upgraded from version 23.6.0 to version 23.12.0"
+sed -i -e "s/version=3.11.0/version=3.11.1/g" extra/flexget/spkgbuild
+changelog "extra/flexget/spkgbuild" "Upgraded from version 3.11.0 to version 3.11.1"
+sed -i -e "s/version=0.30.4/version=0.30.5/g" python/python-argh/spkgbuild
+changelog "python/python-argh/spkgbuild" "Upgraded from version 0.30.4 to version 0.30.5"
+sed -i -e "s/version=1.3.8/version=1.3.9/g" python/python-basemap/spkgbuild
+changelog "python/python-basemap/spkgbuild" "Upgraded from version 1.3.8 to version 1.3.9"
+sed -i -e "s/version=1.34.7/version=1.34.8/g" python/python-boto3/spkgbuild
+changelog "python/python-boto3/spkgbuild" "Upgraded from version 1.34.7 to version 1.34.8"
+sed -i -e "s/version=1.34.7/version=1.34.8/g" python/python-botocore/spkgbuild
+changelog "python/python-botocore/spkgbuild" "Upgraded from version 1.34.7 to version 1.34.8"
+sed -i -e "s/version=0.106.0/version=0.108.0/g" python/python-fastapi/spkgbuild
+changelog "python/python-fastapi/spkgbuild" "Upgraded from version 0.106.0 to version 0.108.0"
+sed -i -e "s/version=6.27.1/version=6.28.0/g" python/python-ipykernel/spkgbuild
+changelog "python/python-ipykernel/spkgbuild" "Upgraded from version 6.27.1 to version 6.28.0"
+sed -i -e "s/version=5.5.1/version=5.6.0/g" python/python-jupyter_core/spkgbuild
+changelog "python/python-jupyter_core/spkgbuild" "Upgraded from version 5.5.1 to version 5.6.0"
+sed -i -e "s/version=7.12.1/version=7.13.0/g" python/python-qiniu/spkgbuild
+changelog "python/python-qiniu/spkgbuild" "Upgraded from version 7.12.1 to version 7.13.0"
+sed -i -e "s/version=3.14.40/version=3.14.41/g" server/blacklist-hosts/spkgbuild
+changelog "server/blacklist-hosts/spkgbuild" "Upgraded from version 3.14.40 to version 3.14.41"
