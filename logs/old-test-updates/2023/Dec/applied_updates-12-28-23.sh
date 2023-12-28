fdfb7c3c1fc541754493fdfa43ed78701cce8656
Voncloft <voncloft@gmail.com>
2023-12-28 08:59:03 -0500

    Minor Updates

---
+++ logs/old-test-updates/2023/Dec/applied_updates-12-28-23.sh
@@ -0,0 +1,16 @@
+sed -i -e "s/version=3.11.1/version=3.11.2/g" extra/flexget/spkgbuild
+changelog "extra/flexget/spkgbuild" "Upgraded from version 3.11.1 to version 3.11.2"
+sed -i -e "s/version=1.34.8/version=1.34.9/g" python/python-boto3/spkgbuild
+changelog "python/python-boto3/spkgbuild" "Upgraded from version 1.34.8 to version 1.34.9"
+sed -i -e "s/version=1.34.8/version=1.34.9/g" python/python-botocore/spkgbuild
+changelog "python/python-botocore/spkgbuild" "Upgraded from version 1.34.8 to version 1.34.9"
+sed -i -e "s/version=2.14.3/version=2.15.0/g" python/python-cloudflare/spkgbuild
+changelog "python/python-cloudflare/spkgbuild" "Upgraded from version 2.14.3 to version 2.15.0"
+sed -i -e "s/version=6.92.1/version=6.92.2/g" python/python-hypothesis/spkgbuild
+changelog "python/python-hypothesis/spkgbuild" "Upgraded from version 6.92.1 to version 6.92.2"
+sed -i -e "s/version=3.5.0/version=3.6.0/g" python/python-internetarchive/spkgbuild
+changelog "python/python-internetarchive/spkgbuild" "Upgraded from version 3.5.0 to version 3.6.0"
+sed -i -e "s/version=0.5.1/version=0.5.2/g" python/python-jsonschema-objects/spkgbuild
+changelog "python/python-jsonschema-objects/spkgbuild" "Upgraded from version 0.5.1 to version 0.5.2"
+sed -i -e "s/version=2023.12.3/version=2023.12.4/g" server/homeassistant/spkgbuild
+changelog "server/homeassistant/spkgbuild" "Upgraded from version 2023.12.3 to version 2023.12.4"
