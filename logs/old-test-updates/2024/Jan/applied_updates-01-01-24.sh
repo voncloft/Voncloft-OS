cdc6faadcf1213057a3e8d8c7050940d69090049
Voncloft <voncloft@gmail.com>
2024-01-01 16:28:02 -0500

    Minor Updates

---
+++ logs/old-test-updates/2024/Jan/applied_updates-01-01-24.sh
@@ -0,0 +1,12 @@
+sed -i -e "s/version=6.6.8/version=6.6.9/g" core/linux-api-headers/spkgbuild
+changelog "core/linux-api-headers/spkgbuild" "Upgraded from version 6.6.8 to version 6.6.9"
+sed -i -e "s/version=6.6.8/version=6.6.9/g" core/linux-desktop/spkgbuild
+changelog "core/linux-desktop/spkgbuild" "Upgraded from version 6.6.8 to version 6.6.9"
+sed -i -e "s/version=6.6.8/version=6.6.9/g" core/linux-router/spkgbuild
+changelog "core/linux-router/spkgbuild" "Upgraded from version 6.6.8 to version 6.6.9"
+sed -i -e "s/version=3.11.3/version=3.11.4/g" extra/flexget/spkgbuild
+changelog "extra/flexget/spkgbuild" "Upgraded from version 3.11.3 to version 3.11.4"
+sed -i -e "s/version=10.1.1/version=10.2.0/g" libs/fmt/spkgbuild
+changelog "libs/fmt/spkgbuild" "Upgraded from version 10.1.1 to version 10.2.0"
+sed -i -e "s/version=0.13.0/version=0.13.1/g" python/python-seaborn/spkgbuild
+changelog "python/python-seaborn/spkgbuild" "Upgraded from version 0.13.0 to version 0.13.1"
