a5b3d6c548d34a029c42e1e34d6b9e179f518a8e
Voncloft <voncloft@gmail.com>
2024-01-03 18:21:38 -0500

    Minor Updates

---
+++ logs/old-test-updates/2024/Jan/applied_updates-01-03-24.sh
@@ -0,0 +1,24 @@
+sed -i -e "s/version=3.11.4/version=3.11.5/g" extra/flexget/spkgbuild
+changelog "extra/flexget/spkgbuild" "Upgraded from version 3.11.4 to version 3.11.5"
+sed -i -e "s/version=23.12.0/version=24.01.0/g" libs/poppler/spkgbuild
+changelog "libs/poppler/spkgbuild" "Upgraded from version 23.12.0 to version 24.01.0"
+sed -i -e "s/version=122.0b4/version=122.0b5/g" nonfree/firefox/spkgbuild
+changelog "nonfree/firefox/spkgbuild" "Upgraded from version 122.0b4 to version 122.0b5"
+sed -i -e "s/version=122.0b1/version=122.0b2/g" nonfree/thunderbird/spkgbuild
+changelog "nonfree/thunderbird/spkgbuild" "Upgraded from version 122.0b1 to version 122.0b2"
+sed -i -e "s/version=1.7.0/version=1.7.1/g" python/python-ddt/spkgbuild
+changelog "python/python-ddt/spkgbuild" "Upgraded from version 1.7.0 to version 1.7.1"
+sed -i -e "s/version=2.25.2/version=2.26.0/g" python/python-google-auth/spkgbuild
+changelog "python/python-google-auth/spkgbuild" "Upgraded from version 2.25.2 to version 2.26.0"
+sed -i -e "s/version=1.33.0/version=1.33.2/g" python/python-hcloud/spkgbuild
+changelog "python/python-hcloud/spkgbuild" "Upgraded from version 1.33.0 to version 1.33.2"
+sed -i -e "s/version=5.6.0/version=5.7.0/g" python/python-jupyter_core/spkgbuild
+changelog "python/python-jupyter_core/spkgbuild" "Upgraded from version 5.6.0 to version 5.7.0"
+sed -i -e "s/version=0.10.0/version=0.10.1/g" python/python-netaddr/spkgbuild
+changelog "python/python-netaddr/spkgbuild" "Upgraded from version 0.10.0 to version 0.10.1"
+sed -i -e "s/version=0.0.30/version=0.0.31/g" python/python-pip-api/spkgbuild
+changelog "python/python-pip-api/spkgbuild" "Upgraded from version 0.0.30 to version 0.0.31"
+sed -i -e "s/version=1.4.0/version=1.4.1/g" python/python-pipx/spkgbuild
+changelog "python/python-pipx/spkgbuild" "Upgraded from version 1.4.0 to version 1.4.1"
+sed -i -e "s/version=3.2.1/version=3.2.2/g" python/python-wheezy-template/spkgbuild
+changelog "python/python-wheezy-template/spkgbuild" "Upgraded from version 3.2.1 to version 3.2.2"
