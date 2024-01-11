8259c9ecd00b26874d41f7e94f16bc9876efbfd2
Voncloft <voncloft@gmail.com>
2024-01-11 13:06:59 -0500

    Minor Updates

---
+++ logs/old-test-updates/2024/Jan/applied_updates-01-11-24.sh
@@ -0,0 +1,34 @@
+sed -i -e "s/version=25.1/version=25.2/g" extra/protobuf/spkgbuild
+changelog "extra/protobuf/spkgbuild" "Upgraded from version 25.1 to version 25.2"
+sed -i -e "s/version=1.32.3/version=1.32.4/g" media/mpg123/spkgbuild
+changelog "media/mpg123/spkgbuild" "Upgraded from version 1.32.3 to version 1.32.4"
+sed -i -e "s/version=1.9.0/version=1.9.1/g" libs/libmaxminddb/spkgbuild
+changelog "libs/libmaxminddb/spkgbuild" "Upgraded from version 1.9.0 to version 1.9.1"
+sed -i -e "s/version=122.0b7/version=122.0b8/g" nonfree/firefox/spkgbuild
+changelog "nonfree/firefox/spkgbuild" "Upgraded from version 122.0b7 to version 122.0b8"
+sed -i -e "s/version=6.3.1/version=6.4.0/g" python/python-apispec/spkgbuild
+changelog "python/python-apispec/spkgbuild" "Upgraded from version 6.3.1 to version 6.4.0"
+sed -i -e "s/version=1.34.15/version=1.34.16/g" python/python-boto3/spkgbuild
+changelog "python/python-boto3/spkgbuild" "Upgraded from version 1.34.15 to version 1.34.16"
+sed -i -e "s/version=1.34.15/version=1.34.16/g" python/python-botocore/spkgbuild
+changelog "python/python-botocore/spkgbuild" "Upgraded from version 1.34.15 to version 1.34.16"
+sed -i -e "s/version=4.25.0/version=4.26.0/g" python/python-braintree/spkgbuild
+changelog "python/python-braintree/spkgbuild" "Upgraded from version 4.25.0 to version 4.26.0"
+sed -i -e "s/version=1.2.1/version=1.2.2/g" python/python-cfn-lint/spkgbuild
+changelog "python/python-cfn-lint/spkgbuild" "Upgraded from version 1.2.1 to version 1.2.2"
+sed -i -e "s/version=3.3.0/version=3.4.0/g" python/python-heatclient/spkgbuild
+changelog "python/python-heatclient/spkgbuild" "Upgraded from version 3.3.0 to version 3.4.0"
+sed -i -e "s/version=6.92.6/version=6.92.7/g" python/python-hypothesis/spkgbuild
+changelog "python/python-hypothesis/spkgbuild" "Upgraded from version 6.92.6 to version 6.92.7"
+sed -i -e "s/version=1.1.8/version=1.1.9/g" python/python-lark-parser/spkgbuild
+changelog "python/python-lark-parser/spkgbuild" "Upgraded from version 1.1.8 to version 1.1.9"
+sed -i -e "s/version=2.5.1/version=2.5.2/g" python/python-maxminddb/spkgbuild
+changelog "python/python-maxminddb/spkgbuild" "Upgraded from version 2.5.1 to version 2.5.2"
+sed -i -e "s/version=5.2.0/version=5.3.0/g" python/python-oslo-concurrency/spkgbuild
+changelog "python/python-oslo-concurrency/spkgbuild" "Upgraded from version 5.2.0 to version 5.3.0"
+sed -i -e "s/version=9.2.0/version=9.3.0/g" python/python-oslo-config/spkgbuild
+changelog "python/python-oslo-config/spkgbuild" "Upgraded from version 9.2.0 to version 9.3.0"
+sed -i -e "s/version=5.2.0/version=5.3.0/g" python/python-oslo-serialization/spkgbuild
+changelog "python/python-oslo-serialization/spkgbuild" "Upgraded from version 5.2.0 to version 5.3.0"
+sed -i -e "s/version=2.6.1/version=2.6.2/g" python/python-sphinxcontrib-bibtex/spkgbuild
+changelog "python/python-sphinxcontrib-bibtex/spkgbuild" "Upgraded from version 2.6.1 to version 2.6.2"
