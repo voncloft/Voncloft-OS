f2a3e6b42ca7ecee9fbaf66a11d4b483d1274c70
Voncloft <voncloft@gmail.com>
2024-01-17 08:47:33 -0500

    fixed oxygen-icons

---
+++ logs/old-test-updates/2024/Jan/applied_updates-01-17-24.sh
@@ -0,0 +1,30 @@
+sed -i -e "s/version=4.14.2/version=4.14.3/g" core/shadow/spkgbuild
+changelog "core/shadow/spkgbuild" "Upgraded from version 4.14.2 to version 4.14.3"
+sed -i -e "s/version=6.5.0/version=6.5.1/g" core/streamlink/spkgbuild
+changelog "core/streamlink/spkgbuild" "Upgraded from version 6.5.0 to version 6.5.1"
+sed -i -e "s/version=3.11.8/version=3.11.9/g" extra/flexget/spkgbuild
+changelog "extra/flexget/spkgbuild" "Upgraded from version 3.11.8 to version 3.11.9"
+sed -i -e "s/version=23.2.3/version=23.2.4/g" libs/xwayland/spkgbuild
+changelog "libs/xwayland/spkgbuild" "Upgraded from version 23.2.3 to version 23.2.4"
+sed -i -e "s/version=7.0.12.159484/version=7.0.14.161095/g" nonfree/virtualbox/spkgbuild
+changelog "nonfree/virtualbox/spkgbuild" "Upgraded from version 7.0.12.159484 to version 7.0.14.161095"
+sed -i -e "s/version=7.0.12/version=7.0.14/g" nonfree/virtualbox-guest-modules/spkgbuild
+changelog "nonfree/virtualbox-guest-modules/spkgbuild" "Upgraded from version 7.0.12 to version 7.0.14"
+sed -i -e "s/version=7.0.12/version=7.0.14/g" nonfree/virtualbox-guest-utils/spkgbuild
+changelog "nonfree/virtualbox-guest-utils/spkgbuild" "Upgraded from version 7.0.12 to version 7.0.14"
+sed -i -e "s/version=3.44.2.0/version=3.45.0.0/g" python/python-apsw/spkgbuild
+changelog "python/python-apsw/spkgbuild" "Upgraded from version 3.44.2.0 to version 3.45.0.0"
+sed -i -e "s/version=2.113.0/version=2.114.0/g" python/python-google-api-python-client/spkgbuild
+changelog "python/python-google-api-python-client/spkgbuild" "Upgraded from version 2.113.0 to version 2.114.0"
+sed -i -e "s/version=6.93.1/version=6.94.0/g" python/python-hypothesis/spkgbuild
+changelog "python/python-hypothesis/spkgbuild" "Upgraded from version 6.93.1 to version 6.94.0"
+sed -i -e "s/version=6.28.0/version=6.29.0/g" python/python-ipykernel/spkgbuild
+changelog "python/python-ipykernel/spkgbuild" "Upgraded from version 6.28.0 to version 6.29.0"
+sed -i -e "s/version=4.20.0/version=4.21.0/g" python/python-jsonschema/spkgbuild
+changelog "python/python-jsonschema/spkgbuild" "Upgraded from version 4.20.0 to version 4.21.0"
+sed -i -e "s/version=8.2.0/version=8.3.0/g" python/python-mysql-connector/spkgbuild
+changelog "python/python-mysql-connector/spkgbuild" "Upgraded from version 8.2.0 to version 8.3.0"
+sed -i -e "s/version=1.4.2/version=1.4.3/g" python/python-pipx/spkgbuild
+changelog "python/python-pipx/spkgbuild" "Upgraded from version 1.4.2 to version 1.4.3"
+sed -i -e "s/version=21.1.10/version=21.1.11/g" xorg/xorg-server/spkgbuild
+changelog "xorg/xorg-server/spkgbuild" "Upgraded from version 21.1.10 to version 21.1.11"
