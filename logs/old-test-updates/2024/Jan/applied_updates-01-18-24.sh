5988ba4b3d6b1782967213b816262c5ac9439706
Voncloft <voncloft@gmail.com>
2024-01-18 05:29:18 -0500

    Minor Updates

---
+++ logs/old-test-updates/2024/Jan/applied_updates-01-18-24.sh
@@ -0,0 +1,24 @@
+sed -i -e "s/version=1.5.3/version=1.6.0/g" core/linux-pam/spkgbuild
+changelog "core/linux-pam/spkgbuild" "Upgraded from version 1.5.3 to version 1.6.0"
+sed -i -e "s/version=2.0.16/version=2.0.17/g" core/numactl/spkgbuild
+changelog "core/numactl/spkgbuild" "Upgraded from version 2.0.16 to version 2.0.17"
+sed -i -e "s/version=0.5.2/version=1.0.0/g" python/python-apispec-webframeworks/spkgbuild
+changelog "python/python-apispec-webframeworks/spkgbuild" "Upgraded from version 0.5.2 to version 1.0.0"
+sed -i -e "s/version=1.34.19/version=1.34.21/g" python/python-boto3/spkgbuild
+changelog "python/python-boto3/spkgbuild" "Upgraded from version 1.34.19 to version 1.34.21"
+sed -i -e "s/version=1.34.19/version=1.34.21/g" python/python-botocore/spkgbuild
+changelog "python/python-botocore/spkgbuild" "Upgraded from version 1.34.19 to version 1.34.21"
+sed -i -e "s/version=6.94.0/version=6.96.0/g" python/python-hypothesis/spkgbuild
+changelog "python/python-hypothesis/spkgbuild" "Upgraded from version 6.94.0 to version 6.96.0"
+sed -i -e "s/version=0.4.0/version=0.4.1/g" python/python-nmigen/spkgbuild
+changelog "python/python-nmigen/spkgbuild" "Upgraded from version 0.4.0 to version 0.4.1"
+sed -i -e "s/version=6.3.0/version=7.0.0/g" python/python-oslo-utils/spkgbuild
+changelog "python/python-oslo-utils/spkgbuild" "Upgraded from version 6.3.0 to version 7.0.0"
+sed -i -e "s/version=2.0.1/version=2.0.2/g" python/python-param/spkgbuild
+changelog "python/python-param/spkgbuild" "Upgraded from version 2.0.1 to version 2.0.2"
+sed -i -e "s/version=8.13.27/version=8.13.28/g" python/python-phonenumbers/spkgbuild
+changelog "python/python-phonenumbers/spkgbuild" "Upgraded from version 8.13.27 to version 8.13.28"
+sed -i -e "s/version=2023.1/version=2024.1/g" python/python-pudb/spkgbuild
+changelog "python/python-pudb/spkgbuild" "Upgraded from version 2023.1 to version 2024.1"
+sed -i -e "s/version=7.3.2311261330/version=7.3.2401171258/g" python/python-pycontracts/spkgbuild
+changelog "python/python-pycontracts/spkgbuild" "Upgraded from version 7.3.2311261330 to version 7.3.2401171258"
