6fb3253356ff57968f64849256d0f3a8611f4a9e
Voncloft <voncloft@gmail.com>
2024-01-25 09:20:18 -0500

    Minor Updates

---
+++ logs/old-test-updates/2024/Jan/applied_updates-01-25-24.sh
@@ -0,0 +1,54 @@
+sed -i -e "s/version=6.0.2/version=6.0.3/g" cinnamon/cinnamon-screensaver/spkgbuild
+changelog "cinnamon/cinnamon-screensaver/spkgbuild" "Upgraded from version 6.0.2 to version 6.0.3"
+sed -i -e "s/version=6.0.1/version=6.0.2/g" cinnamon/cinnamon-session/spkgbuild
+changelog "cinnamon/cinnamon-session/spkgbuild" "Upgraded from version 6.0.1 to version 6.0.2"
+sed -i -e "s/version=2.3.1/version=2.3.2/g" core/acl/spkgbuild
+changelog "core/acl/spkgbuild" "Upgraded from version 2.3.1 to version 2.3.2"
+sed -i -e "s/version=29.1/version=29.2/g" core/emacs/spkgbuild
+changelog "core/emacs/spkgbuild" "Upgraded from version 29.1 to version 29.2"
+sed -i -e "s/version=2.39.3/version=2.40/g" core/util-linux/spkgbuild
+changelog "core/util-linux/spkgbuild" "Upgraded from version 2.39.3 to version 2.40"
+sed -i -e "s/version=3.11.10/version=3.11.11/g" extra/flexget/spkgbuild
+changelog "extra/flexget/spkgbuild" "Upgraded from version 3.11.10 to version 3.11.11"
+sed -i -e "s/version=0.9.9.8/version=1.0.0/g" libs/glm/spkgbuild
+changelog "libs/glm/spkgbuild" "Upgraded from version 0.9.9.8 to version 1.0.0"
+sed -i -e "s/version=4.2.2/version=4.3.0/g" libs/libimagequant/spkgbuild
+changelog "libs/libimagequant/spkgbuild" "Upgraded from version 4.2.2 to version 4.3.0"
+sed -i -e "s/version=122.0b9/version=123.0b2/g" nonfree/firefox/spkgbuild
+changelog "nonfree/firefox/spkgbuild" "Upgraded from version 122.0b9 to version 123.0b2"
+sed -i -e "s/version=545.29.06/version=550.40.07/g" nonfree/nvidia/spkgbuild
+changelog "nonfree/nvidia/spkgbuild" "Upgraded from version 545.29.06 to version 550.40.07"
+sed -i -e "s/version=545.29.06/version=550.40.07/g" nonfree/nvidia-32/spkgbuild
+changelog "nonfree/nvidia-32/spkgbuild" "Upgraded from version 545.29.06 to version 550.40.07"
+sed -i -e "s/version=545.29.06/version=550.40.07/g" nonfree/nvidia-modules/spkgbuild
+changelog "nonfree/nvidia-modules/spkgbuild" "Upgraded from version 545.29.06 to version 550.40.07"
+sed -i -e "s/version=122.0b4/version=123.0b1/g" nonfree/thunderbird/spkgbuild
+changelog "nonfree/thunderbird/spkgbuild" "Upgraded from version 122.0b4 to version 123.0b1"
+sed -i -e "s/version=0.10.3/version=0.10.4/g" python/python-atom/spkgbuild
+changelog "python/python-atom/spkgbuild" "Upgraded from version 0.10.3 to version 0.10.4"
+sed -i -e "s/version=1.34.23/version=1.34.27/g" python/python-boto3/spkgbuild
+changelog "python/python-boto3/spkgbuild" "Upgraded from version 1.34.23 to version 1.34.27"
+sed -i -e "s/version=1.34.23/version=1.34.27/g" python/python-botocore/spkgbuild
+changelog "python/python-botocore/spkgbuild" "Upgraded from version 1.34.23 to version 1.34.27"
+sed -i -e "s/version=1.2.2/version=1.2.3/g" python/python-cfn-lint/spkgbuild
+changelog "python/python-cfn-lint/spkgbuild" "Upgraded from version 1.2.2 to version 1.2.3"
+sed -i -e "s/version=2.16.0/version=2.17.0/g" python/python-cloudflare/spkgbuild
+changelog "python/python-cloudflare/spkgbuild" "Upgraded from version 2.16.0 to version 2.17.0"
+sed -i -e "s/version=6.15.12/version=6.15.13/g" python/python-cx-freeze/spkgbuild
+changelog "python/python-cx-freeze/spkgbuild" "Upgraded from version 6.15.12 to version 6.15.13"
+sed -i -e "s/version=1.0.0/version=1.0.1/g" python/python-dotenv/spkgbuild
+changelog "python/python-dotenv/spkgbuild" "Upgraded from version 1.0.0 to version 1.0.1"
+sed -i -e "s/version=22.5.0/version=22.5.1/g" python/python-faker/spkgbuild
+changelog "python/python-faker/spkgbuild" "Upgraded from version 22.5.0 to version 22.5.1"
+sed -i -e "s/version=2.114.0/version=2.115.0/g" python/python-google-api-python-client/spkgbuild
+changelog "python/python-google-api-python-client/spkgbuild" "Upgraded from version 2.114.0 to version 2.115.0"
+sed -i -e "s/version=2.26.2/version=2.27.0/g" python/python-google-auth/spkgbuild
+changelog "python/python-google-auth/spkgbuild" "Upgraded from version 2.26.2 to version 2.27.0"
+sed -i -e "s/version=6.96.2/version=6.96.4/g" python/python-hypothesis/spkgbuild
+changelog "python/python-hypothesis/spkgbuild" "Upgraded from version 6.96.2 to version 6.96.4"
+sed -i -e "s/version=1.3.0/version=1.4.0/g" python/python-pluggy/spkgbuild
+changelog "python/python-pluggy/spkgbuild" "Upgraded from version 1.3.0 to version 1.4.0"
+sed -i -e "s/version=0.35.1/version=0.36.1/g" python/python-starlette/spkgbuild
+changelog "python/python-starlette/spkgbuild" "Upgraded from version 0.35.1 to version 0.36.1"
+sed -i -e "s/version=5.1.0/version=6.0.0/g" python/python-vcrpy/spkgbuild
+changelog "python/python-vcrpy/spkgbuild" "Upgraded from version 5.1.0 to version 6.0.0"
