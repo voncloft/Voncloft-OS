f27711368e45d08528adf957586a2521a3bc3c89
Voncloft <voncloft@gmail.com>
2024-02-06 06:48:04 -0500

    Minor Updates

---
+++ logs/old-test-updates/2024/Feb/applied_updates-02-06-24.sh
@@ -0,0 +1,60 @@
+sed -i -e "s/version=4.9/version=4.9.1/g" compilers/ccache/spkgbuild
+changelog "compilers/ccache/spkgbuild" "Upgraded from version 4.9 to version 4.9.1"
+sed -i -e "s/version=3.28.2/version=3.28.3/g" compilers/cmake/spkgbuild
+changelog "compilers/cmake/spkgbuild" "Upgraded from version 3.28.2 to version 3.28.3"
+sed -i -e "s/version=0.21.0/version=0.22.0/g" core/alacritty/spkgbuild
+changelog "core/alacritty/spkgbuild" "Upgraded from version 0.21.0 to version 0.22.0"
+sed -i -e "s/version=0.17/version=0.18/g" libs/libpciaccess/spkgbuild
+changelog "libs/libpciaccess/spkgbuild" "Upgraded from version 0.17 to version 0.18"
+sed -i -e "s/version=4.19.3/version=4.19.4/g" libs/libxfce4ui/spkgbuild
+changelog "libs/libxfce4ui/spkgbuild" "Upgraded from version 4.19.3 to version 4.19.4"
+sed -i -e "s/version=4.19.2/version=4.19.3/g" libs/libxfce4util/spkgbuild
+changelog "libs/libxfce4util/spkgbuild" "Upgraded from version 4.19.2 to version 4.19.3"
+sed -i -e "s/version=1.1.2/version=1.1.3/g" libs/libxkbfile/spkgbuild
+changelog "libs/libxkbfile/spkgbuild" "Upgraded from version 1.1.2 to version 1.1.3"
+sed -i -e "s/version=123.0b6/version=123.0b7/g" nonfree/firefox/spkgbuild
+changelog "nonfree/firefox/spkgbuild" "Upgraded from version 123.0b6 to version 123.0b7"
+sed -i -e "s/version=1.34.34/version=1.34.35/g" python/python-boto3/spkgbuild
+changelog "python/python-boto3/spkgbuild" "Upgraded from version 1.34.34 to version 1.34.35"
+sed -i -e "s/version=1.34.34/version=1.34.35/g" python/python-botocore/spkgbuild
+changelog "python/python-botocore/spkgbuild" "Upgraded from version 1.34.34 to version 1.34.35"
+sed -i -e "s/version=4.8.2/version=4.9.0/g" python/python-engineio/spkgbuild
+changelog "python/python-engineio/spkgbuild" "Upgraded from version 4.8.2 to version 4.9.0"
+sed -i -e "s/version=22.6.0/version=22.7.0/g" python/python-faker/spkgbuild
+changelog "python/python-faker/spkgbuild" "Upgraded from version 22.6.0 to version 22.7.0"
+sed -i -e "s/version=0.109.1/version=0.109.2/g" python/python-fastapi/spkgbuild
+changelog "python/python-fastapi/spkgbuild" "Upgraded from version 0.109.1 to version 0.109.2"
+sed -i -e "s/version=2023.12.2/version=2024.2.0/g" python/python-fsspec/spkgbuild
+changelog "python/python-fsspec/spkgbuild" "Upgraded from version 2023.12.2 to version 2024.2.0"
+sed -i -e "s/version=6.97.5/version=6.98.1/g" python/python-hypothesis/spkgbuild
+changelog "python/python-hypothesis/spkgbuild" "Upgraded from version 6.97.5 to version 6.98.1"
+sed -i -e "s/version=3.17.0/version=3.17.1/g" python/python-peewee/spkgbuild
+changelog "python/python-peewee/spkgbuild" "Upgraded from version 3.17.0 to version 3.17.1"
+sed -i -e "s/version=2023.12.0/version=2023.12.1/g" python/python-pipenv/spkgbuild
+changelog "python/python-pipenv/spkgbuild" "Upgraded from version 2023.12.0 to version 2023.12.1"
+sed -i -e "s/version=2.6.0/version=2.6.1/g" python/python-pydantic/spkgbuild
+changelog "python/python-pydantic/spkgbuild" "Upgraded from version 2.6.0 to version 2.6.1"
+sed -i -e "s/version=5.0.1/version=5.1.0/g" python/python-pyodbc/spkgbuild
+changelog "python/python-pyodbc/spkgbuild" "Upgraded from version 5.0.1 to version 5.1.0"
+sed -i -e "s/version=5.11.0/version=5.11.1/g" python/python-socketio/spkgbuild
+changelog "python/python-socketio/spkgbuild" "Upgraded from version 5.11.0 to version 5.11.1"
+sed -i -e "s/version=0.36.3/version=0.37.0/g" python/python-starlette/spkgbuild
+changelog "python/python-starlette/spkgbuild" "Upgraded from version 0.36.3 to version 0.37.0"
+sed -i -e "s/version=2.1.2/version=3.0.0/g" python/python-waitress/spkgbuild
+changelog "python/python-waitress/spkgbuild" "Upgraded from version 2.1.2 to version 3.0.0"
+sed -i -e "s/version=4.19.0/version=4.19.1/g" xfce/garcon/spkgbuild
+changelog "xfce/garcon/spkgbuild" "Upgraded from version 4.19.0 to version 4.19.1"
+sed -i -e "s/version=0.6.1/version=0.6.2/g" xfce/mousepad/spkgbuild
+changelog "xfce/mousepad/spkgbuild" "Upgraded from version 0.6.1 to version 0.6.2"
+sed -i -e "s/version=0.13.1/version=0.13.2/g" xfce/ristretto/spkgbuild
+changelog "xfce/ristretto/spkgbuild" "Upgraded from version 0.13.1 to version 0.13.2"
+sed -i -e "s/version=4.19.0/version=4.19.1/g" xfce/tumbler/spkgbuild
+changelog "xfce/tumbler/spkgbuild" "Upgraded from version 4.19.0 to version 4.19.1"
+sed -i -e "s/version=1.10.4/version=1.10.5/g" xfce/xfce4-screenshooter/spkgbuild
+changelog "xfce/xfce4-screenshooter/spkgbuild" "Upgraded from version 1.10.4 to version 1.10.5"
+sed -i -e "s/version=1.1.1/version=1.1.2/g" xfce/xfce4-terminal/spkgbuild
+changelog "xfce/xfce4-terminal/spkgbuild" "Upgraded from version 1.1.1 to version 1.1.2"
+sed -i -e "s/version=6.5.0/version=6.6.0/g" xfce/xfsprogs/spkgbuild
+changelog "xfce/xfsprogs/spkgbuild" "Upgraded from version 6.5.0 to version 6.6.0"
+sed -i -e "s/version=2.40/version=2.41/g" xorg/xkeyboard-config/spkgbuild
+changelog "xorg/xkeyboard-config/spkgbuild" "Upgraded from version 2.40 to version 2.41"
