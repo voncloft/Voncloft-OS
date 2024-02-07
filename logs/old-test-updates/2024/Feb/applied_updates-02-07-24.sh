58b643e0e9d41a0168ef786f405feaa87e214629
Voncloft <voncloft@gmail.com>
2024-02-07 06:37:02 -0500

    Minor Updates

---
+++ logs/old-test-updates/2024/Feb/applied_updates-02-07-24.sh
@@ -0,0 +1,42 @@
+sed -i -e "s/version=6.7.3/version=6.7.4/g" core/linux-api-headers/spkgbuild
+changelog "core/linux-api-headers/spkgbuild" "Upgraded from version 6.7.3 to version 6.7.4"
+sed -i -e "s/version=6.7.3/version=6.7.4/g" core/linux-desktop/spkgbuild
+changelog "core/linux-desktop/spkgbuild" "Upgraded from version 6.7.3 to version 6.7.4"
+sed -i -e "s/version=6.7.3/version=6.7.4/g" core/linux-router/spkgbuild
+changelog "core/linux-router/spkgbuild" "Upgraded from version 6.7.3 to version 6.7.4"
+sed -i -e "s/version=3.11.14/version=3.11.15/g" extra/flexget/spkgbuild
+changelog "extra/flexget/spkgbuild" "Upgraded from version 3.11.14 to version 3.11.15"
+sed -i -e "s/version=1.22.9/version=1.23.1/g" media/gst-libav/spkgbuild
+changelog "media/gst-libav/spkgbuild" "Upgraded from version 1.22.9 to version 1.23.1"
+sed -i -e "s/version=1.22.9/version=1.23.1/g" media/gst-plugins-bad/spkgbuild
+changelog "media/gst-plugins-bad/spkgbuild" "Upgraded from version 1.22.9 to version 1.23.1"
+sed -i -e "s/version=1.22.9/version=1.23.1/g" media/gst-plugins-base/spkgbuild
+changelog "media/gst-plugins-base/spkgbuild" "Upgraded from version 1.22.9 to version 1.23.1"
+sed -i -e "s/version=1.22.9/version=1.23.1/g" media/gst-plugins-good/spkgbuild
+changelog "media/gst-plugins-good/spkgbuild" "Upgraded from version 1.22.9 to version 1.23.1"
+sed -i -e "s/version=1.22.9/version=1.23.1/g" media/gst-plugins-ugly/spkgbuild
+changelog "media/gst-plugins-ugly/spkgbuild" "Upgraded from version 1.22.9 to version 1.23.1"
+sed -i -e "s/version=1.22.9/version=1.23.1/g" media/gstreamer/spkgbuild
+changelog "media/gstreamer/spkgbuild" "Upgraded from version 1.22.9 to version 1.23.1"
+sed -i -e "s/version=2.5.0/version=2.6.0/g" libs/expat/spkgbuild
+changelog "libs/expat/spkgbuild" "Upgraded from version 2.5.0 to version 2.6.0"
+sed -i -e "s/version=0.45.0/version=0.46.0/g" libs/libfilezilla/spkgbuild
+changelog "libs/libfilezilla/spkgbuild" "Upgraded from version 0.45.0 to version 0.46.0"
+sed -i -e "s/version=123.0b2/version=123.0b3/g" nonfree/thunderbird/spkgbuild
+changelog "nonfree/thunderbird/spkgbuild" "Upgraded from version 123.0b2 to version 123.0b3"
+sed -i -e "s/version=23.11.0/version=24.2.0/g" python/python-awesomeversion/spkgbuild
+changelog "python/python-awesomeversion/spkgbuild" "Upgraded from version 23.11.0 to version 24.2.0"
+sed -i -e "s/version=4.47.2/version=4.48.1/g" python/python-fonttools/spkgbuild
+changelog "python/python-fonttools/spkgbuild" "Upgraded from version 4.47.2 to version 4.48.1"
+sed -i -e "s/version=6.98.1/version=6.98.2/g" python/python-hypothesis/spkgbuild
+changelog "python/python-hypothesis/spkgbuild" "Upgraded from version 6.98.1 to version 6.98.2"
+sed -i -e "s/version=6.29.0/version=6.29.1/g" python/python-ipykernel/spkgbuild
+changelog "python/python-ipykernel/spkgbuild" "Upgraded from version 6.29.0 to version 6.29.1"
+sed -i -e "s/version=0.25.0/version=0.26.0/g" python/python-libtmux/spkgbuild
+changelog "python/python-libtmux/spkgbuild" "Upgraded from version 0.25.0 to version 0.26.0"
+sed -i -e "s/version=3.12.3/version=3.12.4/g" python/python-mocket/spkgbuild
+changelog "python/python-mocket/spkgbuild" "Upgraded from version 3.12.3 to version 3.12.4"
+sed -i -e "s/version=0.23.4/version=0.23.5/g" python/python-pytest-asyncio/spkgbuild
+changelog "python/python-pytest-asyncio/spkgbuild" "Upgraded from version 0.23.4 to version 0.23.5"
+sed -i -e "s/version=2.5.5/version=2.5.6/g" ruby-gems/ruby-bundler/spkgbuild
+changelog "ruby-gems/ruby-bundler/spkgbuild" "Upgraded from version 2.5.5 to version 2.5.6"
