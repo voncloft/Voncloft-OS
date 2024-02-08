c038a3b57b4af0691841b4ccd900297480ebb7e1
Voncloft <voncloft@gmail.com>
2024-02-08 09:04:13 -0500

    Minor Updates

---
+++ logs/old-test-updates/2024/Feb/applied_updates-02-08-24.sh
@@ -0,0 +1,28 @@
+sed -i -e "s/version=2.0.17/version=2.0.18/g" core/numactl/spkgbuild
+changelog "core/numactl/spkgbuild" "Upgraded from version 2.0.17 to version 2.0.18"
+sed -i -e "s/version=2.9.0/version=2.10.0/g" libs/libwacom/spkgbuild
+changelog "libs/libwacom/spkgbuild" "Upgraded from version 2.9.0 to version 2.10.0"
+sed -i -e "s/version=123.0b7/version=123.0b8/g" nonfree/firefox/spkgbuild
+changelog "nonfree/firefox/spkgbuild" "Upgraded from version 123.0b7 to version 123.0b8"
+sed -i -e "s/version=1.34.35/version=1.34.37/g" python/python-boto3/spkgbuild
+changelog "python/python-boto3/spkgbuild" "Upgraded from version 1.34.35 to version 1.34.37"
+sed -i -e "s/version=1.34.35/version=1.34.37/g" python/python-botocore/spkgbuild
+changelog "python/python-botocore/spkgbuild" "Upgraded from version 1.34.35 to version 1.34.37"
+sed -i -e "s/version=5.1/version=5.2/g" python/python-btrees/spkgbuild
+changelog "python/python-btrees/spkgbuild" "Upgraded from version 5.1 to version 5.2"
+sed -i -e "s/version=1.2.3/version=1.2.4/g" python/python-cfn-lint/spkgbuild
+changelog "python/python-cfn-lint/spkgbuild" "Upgraded from version 1.2.3 to version 1.2.4"
+sed -i -e "s/version=0.60.1/version=0.61.0/g" python/python-django-allauth/spkgbuild
+changelog "python/python-django-allauth/spkgbuild" "Upgraded from version 0.60.1 to version 0.61.0"
+sed -i -e "s/version=22.7.0/version=23.1.0/g" python/python-faker/spkgbuild
+changelog "python/python-faker/spkgbuild" "Upgraded from version 22.7.0 to version 23.1.0"
+sed -i -e "s/version=6.29.1/version=6.29.2/g" python/python-ipykernel/spkgbuild
+changelog "python/python-ipykernel/spkgbuild" "Upgraded from version 6.29.1 to version 6.29.2"
+sed -i -e "s/version=0.26.0/version=0.27.0/g" python/python-libtmux/spkgbuild
+changelog "python/python-libtmux/spkgbuild" "Upgraded from version 0.26.0 to version 0.27.0"
+sed -i -e "s/version=3.0.0/version=4.0.0/g" python/python-watchdog/spkgbuild
+changelog "python/python-watchdog/spkgbuild" "Upgraded from version 3.0.0 to version 4.0.0"
+sed -i -e "s/version=6.2.1/version=6.3/g" python/python-zope-testrunner/spkgbuild
+changelog "python/python-zope-testrunner/spkgbuild" "Upgraded from version 6.2.1 to version 6.3"
+sed -i -e "s/version=3.14.49/version=3.14.50/g" server/blacklist-hosts/spkgbuild
+changelog "server/blacklist-hosts/spkgbuild" "Upgraded from version 3.14.49 to version 3.14.50"
