7aecbc09d07087fdce14b10edcd394c1d486f09d
Voncloft <voncloft@gmail.com>
2024-02-13 10:05:34 -0500

    Minor Updates

---
+++ logs/old-test-updates/2024/Feb/applied_updates-02-13-24.sh
@@ -0,0 +1,26 @@
+sed -i -e "s/version=0.4.46/version=0.4.48/g" core/gegl/spkgbuild
+changelog "core/gegl/spkgbuild" "Upgraded from version 0.4.46 to version 0.4.48"
+sed -i -e "s/version=6.05.01/version=6.06/g" core/man-pages/spkgbuild
+changelog "core/man-pages/spkgbuild" "Upgraded from version 6.05.01 to version 6.06"
+sed -i -e "s/version=6.6.2/version=6.6.3/g" core/mercurial/spkgbuild
+changelog "core/mercurial/spkgbuild" "Upgraded from version 6.6.2 to version 6.6.3"
+sed -i -e "s/version=4.14.3/version=4.14.4/g" core/shadow/spkgbuild
+changelog "core/shadow/spkgbuild" "Upgraded from version 4.14.3 to version 4.14.4"
+sed -i -e "s/version=3.2.1/version=3.2.2/g" libs/openexr/spkgbuild
+changelog "libs/openexr/spkgbuild" "Upgraded from version 3.2.1 to version 3.2.2"
+sed -i -e "s/version=2.6.8/version=2.6.9/g" networking/openvpn/spkgbuild
+changelog "networking/openvpn/spkgbuild" "Upgraded from version 2.6.8 to version 2.6.9"
+sed -i -e "s/version=1.25.1/version=1.26.0/g" python/python-cairo/spkgbuild
+changelog "python/python-cairo/spkgbuild" "Upgraded from version 1.25.1 to version 1.26.0"
+sed -i -e "s/version=6.98.3/version=6.98.4/g" python/python-hypothesis/spkgbuild
+changelog "python/python-hypothesis/spkgbuild" "Upgraded from version 6.98.3 to version 6.98.4"
+sed -i -e "s/version=1.4.0/version=1.4.1/g" python/python-pook/spkgbuild
+changelog "python/python-pook/spkgbuild" "Upgraded from version 1.4.0 to version 1.4.1"
+sed -i -e "s/version=69.0.3/version=69.1.0/g" python/python-setuptools/spkgbuild
+changelog "python/python-setuptools/spkgbuild" "Upgraded from version 69.0.3 to version 69.1.0"
+sed -i -e "s/version=5.0/version=5.0.1/g" python/python-zope-configuration/spkgbuild
+changelog "python/python-zope-configuration/spkgbuild" "Upgraded from version 5.0 to version 5.0.1"
+sed -i -e "s/version=6.3/version=6.3.1/g" python/python-zope-testrunner/spkgbuild
+changelog "python/python-zope-testrunner/spkgbuild" "Upgraded from version 6.3 to version 6.3.1"
+sed -i -e "s/version=3.14.51/version=3.14.52/g" server/blacklist-hosts/spkgbuild
+changelog "server/blacklist-hosts/spkgbuild" "Upgraded from version 3.14.51 to version 3.14.52"
