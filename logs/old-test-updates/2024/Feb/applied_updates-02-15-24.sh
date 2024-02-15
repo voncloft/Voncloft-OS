987702a6be5593b6b0a6749b6063f65b9faa885c
Voncloft <voncloft@gmail.com>
2024-02-15 06:35:29 -0500

    Minor Updates

---
+++ logs/old-test-updates/2024/Feb/applied_updates-02-15-24.sh
@@ -0,0 +1,50 @@
+sed -i -e "s/version=3.28.3/version=3.29.0-rc1/g" compilers/cmake/spkgbuild
+changelog "compilers/cmake/spkgbuild" "Upgraded from version 3.28.3 to version 3.29.0-rc1"
+sed -i -e "s/version=1.3.1/version=1.3.2/g" compilers/meson/spkgbuild
+changelog "compilers/meson/spkgbuild" "Upgraded from version 1.3.1 to version 1.3.2"
+sed -i -e "s/version=6.7/version=6.7.1/g" core/btrfs-progs/spkgbuild
+changelog "core/btrfs-progs/spkgbuild" "Upgraded from version 6.7 to version 6.7.1"
+sed -i -e "s/version=11.1/version=11.2/g" core/picom/spkgbuild
+changelog "core/picom/spkgbuild" "Upgraded from version 11.1 to version 11.2"
+sed -i -e "s/version=4.14.4/version=4.14.5/g" core/shadow/spkgbuild
+changelog "core/shadow/spkgbuild" "Upgraded from version 4.14.4 to version 4.14.5"
+sed -i -e "s/version=3.3/version=3.4/g" core/tmux/spkgbuild
+changelog "core/tmux/spkgbuild" "Upgraded from version 3.3 to version 3.4"
+sed -i -e "s/version=3.11.17/version=3.11.18/g" extra/flexget/spkgbuild
+changelog "extra/flexget/spkgbuild" "Upgraded from version 3.11.17 to version 3.11.18"
+sed -i -e "s/version=1.1.11rc1/version=1.1.11rc3/g" extra/solaar/spkgbuild
+changelog "extra/solaar/spkgbuild" "Upgraded from version 1.1.11rc1 to version 1.1.11rc3"
+sed -i -e "s/version=9.19.19/version=9.19.21/g" networking/bind/spkgbuild
+changelog "networking/bind/spkgbuild" "Upgraded from version 9.19.19 to version 9.19.21"
+sed -i -e "s/version=2.43.1/version=2.43.2/g" networking/git/spkgbuild
+changelog "networking/git/spkgbuild" "Upgraded from version 2.43.1 to version 2.43.2"
+sed -i -e "s/version=123.0b4/version=123.0b5/g" nonfree/thunderbird/spkgbuild
+changelog "nonfree/thunderbird/spkgbuild" "Upgraded from version 123.0b4 to version 123.0b5"
+sed -i -e "s/version=1.34.39/version=1.34.42/g" python/python-boto3/spkgbuild
+changelog "python/python-boto3/spkgbuild" "Upgraded from version 1.34.39 to version 1.34.42"
+sed -i -e "s/version=1.34.39/version=1.34.42/g" python/python-botocore/spkgbuild
+changelog "python/python-botocore/spkgbuild" "Upgraded from version 1.34.39 to version 1.34.42"
+sed -i -e "s/version=2.18.1/version=2.18.2/g" python/python-cloudflare/spkgbuild
+changelog "python/python-cloudflare/spkgbuild" "Upgraded from version 2.18.1 to version 2.18.2"
+sed -i -e "s/version=3.0.5/version=3.0.6/g" python/python-connexion/spkgbuild
+changelog "python/python-connexion/spkgbuild" "Upgraded from version 3.0.5 to version 3.0.6"
+sed -i -e "s/version=23.9.1/version=24.2.1/g" python/python-gevent/spkgbuild
+changelog "python/python-gevent/spkgbuild" "Upgraded from version 23.9.1 to version 24.2.1"
+sed -i -e "s/version=2.117.0/version=2.118.0/g" python/python-google-api-python-client/spkgbuild
+changelog "python/python-google-api-python-client/spkgbuild" "Upgraded from version 2.117.0 to version 2.118.0"
+sed -i -e "s/version=1.0.2/version=1.0.3/g" python/python-httpcore/spkgbuild
+changelog "python/python-httpcore/spkgbuild" "Upgraded from version 1.0.2 to version 1.0.3"
+sed -i -e "s/version=4.1.2/version=4.1.3/g" python/python-mongomock/spkgbuild
+changelog "python/python-mongomock/spkgbuild" "Upgraded from version 4.1.2 to version 4.1.3"
+sed -i -e "s/version=3.9.13/version=3.9.14/g" python/python-orjson/spkgbuild
+changelog "python/python-orjson/spkgbuild" "Upgraded from version 3.9.13 to version 3.9.14"
+sed -i -e "s/version=0.19.0/version=0.20.0/g" python/python-prometheus_client/spkgbuild
+changelog "python/python-prometheus_client/spkgbuild" "Upgraded from version 0.19.0 to version 0.20.0"
+sed -i -e "s/version=0.24.1/version=0.25.0/g" python/python-responses/spkgbuild
+changelog "python/python-responses/spkgbuild" "Upgraded from version 0.24.1 to version 0.25.0"
+sed -i -e "s/version=1.4.0/version=1.4.1/g" python/python-scikit-learn/spkgbuild
+changelog "python/python-scikit-learn/spkgbuild" "Upgraded from version 1.4.0 to version 1.4.1"
+sed -i -e "s/version=2.89/version=2.90/g" server/dnsmasq/spkgbuild
+changelog "server/dnsmasq/spkgbuild" "Upgraded from version 2.89 to version 2.90"
+sed -i -e "s/version=1.25.3/version=1.25.4/g" server/nginx/spkgbuild
+changelog "server/nginx/spkgbuild" "Upgraded from version 1.25.3 to version 1.25.4"
