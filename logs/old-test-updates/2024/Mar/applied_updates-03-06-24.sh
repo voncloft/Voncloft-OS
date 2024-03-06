8c6becdc5167e656a90419cef12abc3379563ecc
Voncloft <voncloft@gmail.com>
2024-03-06 14:41:46 -0500

    Minor Updates

---
+++ logs/old-test-updates/2024/Mar/applied_updates-03-06-24.sh
@@ -0,0 +1,4 @@
+sed -i -e "s/version=3.29.0-rc2/version=3.29.0-rc3/g" compilers/cmake/spkgbuild
+changelog "compilers/cmake/spkgbuild" "Upgraded from version 3.29.0-rc2 to version 3.29.0-rc3"
+sed -i -e "s/version=23.6/version=23.7/g" core/psmisc/spkgbuild
+changelog "core/psmisc/spkgbuild" "Upgraded from version 23.6 to version 23.7"
