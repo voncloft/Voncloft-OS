6f95628bb2a9c4c80d783c72064f95f5f7a18a96
Voncloft <voncloft@gmail.com>
2024-03-07 03:26:15 -0500

    added plex update

---
+++ logs/old-test-updates/2024/Mar/applied_updates-03-07-24.sh
@@ -0,0 +1,8 @@
+sed -i -e "s/version=1.33/version=1.34/g" core/fakeroot/spkgbuild
+changelog "core/fakeroot/spkgbuild" "Upgraded from version 1.33 to version 1.34"
+sed -i -e "s/version=6.7.8/version=6.7.9/g" core/linux-api-headers/spkgbuild
+changelog "core/linux-api-headers/spkgbuild" "Upgraded from version 6.7.8 to version 6.7.9"
+sed -i -e "s/version=6.7.8/version=6.7.9/g" core/linux-desktop/spkgbuild
+changelog "core/linux-desktop/spkgbuild" "Upgraded from version 6.7.8 to version 6.7.9"
+sed -i -e "s/version=6.7.8/version=6.7.9/g" core/linux-router/spkgbuild
+changelog "core/linux-router/spkgbuild" "Upgraded from version 6.7.8 to version 6.7.9"
