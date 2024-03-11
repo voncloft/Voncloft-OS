b9e7c4b43de7a3c879dcc84e0c389aa288e5162c
Voncloft <voncloft@gmail.com>
2024-03-11 01:21:37 -0400

    Minor Updates

---
+++ logs/old-test-updates/2024/Mar/applied_updates-03-11-24.sh
@@ -0,0 +1,36 @@
+sed -i -e "s/version=17.0.6/version=18.1.1/g" compilers/clang/spkgbuild
+changelog "compilers/clang/spkgbuild" "Upgraded from version 17.0.6 to version 18.1.1"
+sed -i -e "s/version=17.0.6/version=18.1.1/g" compilers/lld/spkgbuild
+changelog "compilers/lld/spkgbuild" "Upgraded from version 17.0.6 to version 18.1.1"
+sed -i -e "s/version=17.0.6/version=18.1.1/g" compilers/llvm/spkgbuild
+changelog "compilers/llvm/spkgbuild" "Upgraded from version 17.0.6 to version 18.1.1"
+sed -i -e "s/version=17.0.6/version=18.1.1/g" compilers/llvm-bak/spkgbuild
+changelog "compilers/llvm-bak/spkgbuild" "Upgraded from version 17.0.6 to version 18.1.1"
+sed -i -e "s/version=17.0.6/version=18.1.1/g" compilers/llvm-old-2023-06-07/spkgbuild
+changelog "compilers/llvm-old-2023-06-07/spkgbuild" "Upgraded from version 17.0.6 to version 18.1.1"
+sed -i -e "s/version=5.72/version=5.73/g" core/bluez/spkgbuild
+changelog "core/bluez/spkgbuild" "Upgraded from version 5.72 to version 5.73"
+sed -i -e "s/version=3.0.12/version=3.0.13/g" core/dkms/spkgbuild
+changelog "core/dkms/spkgbuild" "Upgraded from version 3.0.12 to version 3.0.13"
+sed -i -e "s/version=2.4.4/version=2.4.5/g" core/gnupg/spkgbuild
+changelog "core/gnupg/spkgbuild" "Upgraded from version 2.4.4 to version 2.4.5"
+sed -i -e "s/version=0.123.7/version=0.123.8/g" core/hugo/spkgbuild
+changelog "core/hugo/spkgbuild" "Upgraded from version 0.123.7 to version 0.123.8"
+sed -i -e "s/version=3.4.5/version=3.4.6/g" core/icewm/spkgbuild
+changelog "core/icewm/spkgbuild" "Upgraded from version 3.4.5 to version 3.4.6"
+sed -i -e "s/version=6.7.9/version=6.8/g" core/linux-api-headers/spkgbuild
+changelog "core/linux-api-headers/spkgbuild" "Upgraded from version 6.7.9 to version 6.8"
+sed -i -e "s/version=6.7.9/version=6.8/g" core/linux-desktop/spkgbuild
+changelog "core/linux-desktop/spkgbuild" "Upgraded from version 6.7.9 to version 6.8"
+sed -i -e "s/version=6.7.9/version=6.8/g" core/linux-router/spkgbuild
+changelog "core/linux-router/spkgbuild" "Upgraded from version 6.7.9 to version 6.8"
+sed -i -e "s/version=4.14.6/version=4.15.0/g" core/shadow/spkgbuild
+changelog "core/shadow/spkgbuild" "Upgraded from version 4.14.6 to version 4.15.0"
+sed -i -e "s/version=6.6.2/version=6.7.0/g" core/streamlink/spkgbuild
+changelog "core/streamlink/spkgbuild" "Upgraded from version 6.6.2 to version 6.7.0"
+sed -i -e "s/version=1.21.4/version=1.24.5/g" core/wget/spkgbuild
+changelog "core/wget/spkgbuild" "Upgraded from version 1.21.4 to version 1.24.5"
+sed -i -e "s/version=5.6.0/version=5.6.1/g" core/xz/spkgbuild
+changelog "core/xz/spkgbuild" "Upgraded from version 5.6.0 to version 5.6.1"
+sed -i -e "s/version=3.11.19/version=3.11.22/g" extra/flexget/spkgbuild
+changelog "extra/flexget/spkgbuild" "Upgraded from version 3.11.19 to version 3.11.22"
