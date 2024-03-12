350c68575e84a3d57cfcd66f5ac1aef749dc5fc0
Voncloft <voncloft@gmail.com>
2024-03-12 06:16:16 -0400

    Minor Updates

---
+++ logs/old-test-updates/2024/Mar/applied_updates-03-12-24.sh
@@ -0,0 +1,6 @@
+sed -i -e "s/version=252.9/version=252.23/g" cinnamon/elogind/spkgbuild
+changelog "cinnamon/elogind/spkgbuild" "Upgraded from version 252.9 to version 252.23"
+sed -i -e "s/version=6.7.0/version=6.8.0/g" core/iproute2/spkgbuild
+changelog "core/iproute2/spkgbuild" "Upgraded from version 6.7.0 to version 6.8.0"
+sed -i -e "s/version=4.9.0/version=5.0.0/g" core/qalculate/spkgbuild
+changelog "core/qalculate/spkgbuild" "Upgraded from version 4.9.0 to version 5.0.0"
