08810f5d92e09d35a71e5b6377243ca808a8c1ca
Voncloft <voncloft@gmail.com>
2024-03-14 10:50:26 -0400

    Minor Updates

---
+++ logs/old-test-updates/2024/Mar/applied_updates-03-14-24.sh
@@ -0,0 +1,6 @@
+sed -i -e "s/version=1.0.3/version=1.0.4/g" core/pipewire/spkgbuild
+changelog "core/pipewire/spkgbuild" "Upgraded from version 1.0.3 to version 1.0.4"
+sed -i -e "s/version=3.11.22/version=3.11.23/g" extra/flexget/spkgbuild
+changelog "extra/flexget/spkgbuild" "Upgraded from version 3.11.22 to version 3.11.23"
+sed -i -e "s/version=25.3/version=26.0/g" extra/protobuf/spkgbuild
+changelog "extra/protobuf/spkgbuild" "Upgraded from version 25.3 to version 26.0"
