0cc54e1d00c62ed7336822f3c54afeb8d3974dde
Voncloft <voncloft@gmail.com>
2024-03-17 11:27:20 -0400

    Minor Updates

---
+++ logs/old-test-updates/2024/Mar/applied_updates-03-17-24.sh
@@ -0,0 +1,24 @@
+sed -i -e "s/version=24.0.2/version=24.0.3/g" hardware/mesa/spkgbuild
+changelog "hardware/mesa/spkgbuild" "Upgraded from version 24.0.2 to version 24.0.3"
+sed -i -e "s/version=3.29.0-rc3/version=3.29.0-rc4/g" compilers/cmake/spkgbuild
+changelog "compilers/cmake/spkgbuild" "Upgraded from version 3.29.0-rc3 to version 3.29.0-rc4"
+sed -i -e "s/version=0.123.8/version=0.124.0/g" core/hugo/spkgbuild
+changelog "core/hugo/spkgbuild" "Upgraded from version 0.123.8 to version 0.124.0"
+sed -i -e "s/version=2.14.1/version=2.15/g" core/i3lock/spkgbuild
+changelog "core/i3lock/spkgbuild" "Upgraded from version 2.14.1 to version 2.15"
+sed -i -e "s/version=6.8/version=6.8.1/g" core/linux-api-headers/spkgbuild
+changelog "core/linux-api-headers/spkgbuild" "Upgraded from version 6.8 to version 6.8.1"
+sed -i -e "s/version=6.8/version=6.8.1/g" core/linux-desktop/spkgbuild
+changelog "core/linux-desktop/spkgbuild" "Upgraded from version 6.8 to version 6.8.1"
+sed -i -e "s/version=6.8/version=6.8.1/g" core/linux-router/spkgbuild
+changelog "core/linux-router/spkgbuild" "Upgraded from version 6.8 to version 6.8.1"
+sed -i -e "s/version=6.6.3/version=6.7/g" core/mercurial/spkgbuild
+changelog "core/mercurial/spkgbuild" "Upgraded from version 6.6.3 to version 6.7"
+sed -i -e "s/version=3.98/version=3.99/g" core/nss/spkgbuild
+changelog "core/nss/spkgbuild" "Upgraded from version 3.98 to version 3.99"
+sed -i -e "s/version=6.24.10/version=6.24.11/g" core/tcsh/spkgbuild
+changelog "core/tcsh/spkgbuild" "Upgraded from version 6.24.10 to version 6.24.11"
+sed -i -e "s/version=2.43.4/version=2.44.0/g" core/webkitgtk/spkgbuild
+changelog "core/webkitgtk/spkgbuild" "Upgraded from version 2.43.4 to version 2.44.0"
+sed -i -e "s/version=3.11.23/version=3.11.24/g" extra/flexget/spkgbuild
+changelog "extra/flexget/spkgbuild" "Upgraded from version 3.11.23 to version 3.11.24"
