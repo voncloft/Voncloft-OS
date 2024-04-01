b7a8cd3d268cc3dcc420c740ce83bd65b876688c
Voncloft <voncloft@gmail.com>
2024-04-01 06:57:18 -0400

    Minor Updates

---
+++ logs/old-test-updates/2024/Apr/applied_updates-04-01-24.sh
@@ -0,0 +1,36 @@
+sed -i -e "s/version=24.0.3/version=24.0.4/g" hardware/mesa/spkgbuild
+changelog "hardware/mesa/spkgbuild" "Upgraded from version 24.0.3 to version 24.0.4"
+sed -i -e "s/version=2.1.1/version=2.2.0/g" compilers/pkgconf/spkgbuild
+changelog "compilers/pkgconf/spkgbuild" "Upgraded from version 2.1.1 to version 2.2.0"
+sed -i -e "s/version=1.77.0/version=1.77.1/g" compilers/rust/spkgbuild
+changelog "compilers/rust/spkgbuild" "Upgraded from version 1.77.0 to version 1.77.1"
+sed -i -e "s/version=2.3.5/version=2.4/g" core/blueman/spkgbuild
+changelog "core/blueman/spkgbuild" "Upgraded from version 2.3.5 to version 2.4"
+sed -i -e "s/version=6.7.1/version=6.8/g" core/btrfs-progs/spkgbuild
+changelog "core/btrfs-progs/spkgbuild" "Upgraded from version 6.7.1 to version 6.8"
+sed -i -e "s/version=0.8.0/version=0.9.0/g" core/bubblewrap/spkgbuild
+changelog "core/bubblewrap/spkgbuild" "Upgraded from version 0.8.0 to version 0.9.0"
+sed -i -e "s/version=9.4/version=9.5/g" core/coreutils/spkgbuild
+changelog "core/coreutils/spkgbuild" "Upgraded from version 9.4 to version 9.5"
+sed -i -e "s/version=8.6.0/version=8.7.1/g" core/curl/spkgbuild
+changelog "core/curl/spkgbuild" "Upgraded from version 8.6.0 to version 8.7.1"
+sed -i -e "s/version=3.4.6/version=3.4.7/g" core/icewm/spkgbuild
+changelog "core/icewm/spkgbuild" "Upgraded from version 3.4.6 to version 3.4.7"
+sed -i -e "s/version=6.8.1/version=6.8.2/g" core/linux-api-headers/spkgbuild
+changelog "core/linux-api-headers/spkgbuild" "Upgraded from version 6.8.1 to version 6.8.2"
+sed -i -e "s/version=6.8.1/version=6.8.2/g" core/linux-desktop/spkgbuild
+changelog "core/linux-desktop/spkgbuild" "Upgraded from version 6.8.1 to version 6.8.2"
+sed -i -e "s/version=6.8.1/version=6.8.2/g" core/linux-router/spkgbuild
+changelog "core/linux-router/spkgbuild" "Upgraded from version 6.8.1 to version 6.8.2"
+sed -i -e "s/version=6.7.1/version=6.7.2/g" core/mercurial/spkgbuild
+changelog "core/mercurial/spkgbuild" "Upgraded from version 6.7.1 to version 6.7.2"
+sed -i -e "s/version=3.08/version=3.09/g" core/sysvinit/spkgbuild
+changelog "core/sysvinit/spkgbuild" "Upgraded from version 3.08 to version 3.09"
+sed -i -e "s/version=1.5.5/version=1.5.6/g" core/zstd/spkgbuild
+changelog "core/zstd/spkgbuild" "Upgraded from version 1.5.5 to version 1.5.6"
+sed -i -e "s/version=23.12.1/version=24.04/g" extra/anki/spkgbuild
+changelog "extra/anki/spkgbuild" "Upgraded from version 23.12.1 to version 24.04"
+sed -i -e "s/version=3.11.26/version=3.11.27/g" extra/flexget/spkgbuild
+changelog "extra/flexget/spkgbuild" "Upgraded from version 3.11.26 to version 3.11.27"
+sed -i -e "s/version=26.0/version=26.1/g" extra/protobuf/spkgbuild
+changelog "extra/protobuf/spkgbuild" "Upgraded from version 26.0 to version 26.1"
