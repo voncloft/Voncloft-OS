085d77e60366f5141a8ade74654ac02d9d88d05e
Voncloft <voncloft@gmail.com>
2024-04-03 09:45:51 -0400

    Minor Updates

---
+++ logs/old-test-updates/2024/Apr/applied_updates-04-03-24.sh
@@ -0,0 +1,4 @@
+sed -i -e "s/version=5.5/version=5.6/g" extra/gnucash/spkgbuild
+changelog "extra/gnucash/spkgbuild" "Upgraded from version 5.5 to version 5.6"
+sed -i -e "s/version=6.5.1/version=6.5.2/g" fonts/awesome-font/spkgbuild
+changelog "fonts/awesome-font/spkgbuild" "Upgraded from version 6.5.1 to version 6.5.2"
