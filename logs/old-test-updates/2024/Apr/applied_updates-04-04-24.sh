d7ae38208d3de4c70878e142c82ca33ec564fcba
Voncloft <voncloft@gmail.com>
2024-04-04 08:45:36 -0400

    Minor Updates

---
+++ logs/old-test-updates/2024/Apr/applied_updates-04-04-24.sh
@@ -0,0 +1,6 @@
+sed -i -e "s/version=6.8.2/version=6.8.3/g" core/linux-api-headers/spkgbuild
+changelog "core/linux-api-headers/spkgbuild" "Upgraded from version 6.8.2 to version 6.8.3"
+sed -i -e "s/version=6.8.2/version=6.8.3/g" core/linux-desktop/spkgbuild
+changelog "core/linux-desktop/spkgbuild" "Upgraded from version 6.8.2 to version 6.8.3"
+sed -i -e "s/version=6.8.2/version=6.8.3/g" core/linux-router/spkgbuild
+changelog "core/linux-router/spkgbuild" "Upgraded from version 6.8.2 to version 6.8.3"
