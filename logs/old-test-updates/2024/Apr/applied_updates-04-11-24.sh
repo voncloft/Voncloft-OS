0b39bc5c5921d6ff634bdf002bcdfe982f611594
Voncloft <voncloft@gmail.com>
2024-04-11 13:22:47 -0400

    Minor Updates

---
+++ logs/old-test-updates/2024/Apr/applied_updates-04-11-24.sh
@@ -0,0 +1,42 @@
+sed -i -e "s/version=18.1.2/version=18.1.3/g" compilers/clang/spkgbuild
+changelog "compilers/clang/spkgbuild" "Upgraded from version 18.1.2 to version 18.1.3"
+sed -i -e "s/version=3.29.0-rc4/version=3.29.1/g" compilers/cmake/spkgbuild
+changelog "compilers/cmake/spkgbuild" "Upgraded from version 3.29.0-rc4 to version 3.29.1"
+sed -i -e "s/version=18.1.2/version=18.1.3/g" compilers/lld/spkgbuild
+changelog "compilers/lld/spkgbuild" "Upgraded from version 18.1.2 to version 18.1.3"
+sed -i -e "s/version=18.1.2/version=18.1.3/g" compilers/llvm/spkgbuild
+changelog "compilers/llvm/spkgbuild" "Upgraded from version 18.1.2 to version 18.1.3"
+sed -i -e "s/version=18.1.2/version=18.1.3/g" compilers/llvm-bak/spkgbuild
+changelog "compilers/llvm-bak/spkgbuild" "Upgraded from version 18.1.2 to version 18.1.3"
+sed -i -e "s/version=18.1.2/version=18.1.3/g" compilers/llvm-old-2023-06-07/spkgbuild
+changelog "compilers/llvm-old-2023-06-07/spkgbuild" "Upgraded from version 18.1.2 to version 18.1.3"
+sed -i -e "s/version=1.77.1/version=1.77.2/g" compilers/rust/spkgbuild
+changelog "compilers/rust/spkgbuild" "Upgraded from version 1.77.1 to version 1.77.2"
+sed -i -e "s/version=2.4/version=2.4.1/g" core/blueman/spkgbuild
+changelog "core/blueman/spkgbuild" "Upgraded from version 2.4 to version 2.4.1"
+sed -i -e "s/version=1.7.1/version=1.7.2/g" core/cronie/spkgbuild
+changelog "core/cronie/spkgbuild" "Upgraded from version 1.7.1 to version 1.7.2"
+sed -i -e "s/version=2.6.8/version=2.6.9/g" core/enchant/spkgbuild
+changelog "core/enchant/spkgbuild" "Upgraded from version 2.6.8 to version 2.6.9"
+sed -i -e "s/version=6.8.3/version=6.8.5/g" core/linux-api-headers/spkgbuild
+changelog "core/linux-api-headers/spkgbuild" "Upgraded from version 6.8.3 to version 6.8.5"
+sed -i -e "s/version=6.8.3/version=6.8.5/g" core/linux-desktop/spkgbuild
+changelog "core/linux-desktop/spkgbuild" "Upgraded from version 6.8.3 to version 6.8.5"
+sed -i -e "s/version=1.6.0/version=1.6.1/g" core/linux-pam/spkgbuild
+changelog "core/linux-pam/spkgbuild" "Upgraded from version 1.6.0 to version 1.6.1"
+sed -i -e "s/version=6.8.3/version=6.8.5/g" core/linux-router/spkgbuild
+changelog "core/linux-router/spkgbuild" "Upgraded from version 6.8.3 to version 6.8.5"
+sed -i -e "s/version=2.12.0/version=2.12.1/g" core/man-db/spkgbuild
+changelog "core/man-db/spkgbuild" "Upgraded from version 2.12.0 to version 2.12.1"
+sed -i -e "s/version=1.1.6/version=1.1.7/g" core/mtdev/spkgbuild
+changelog "core/mtdev/spkgbuild" "Upgraded from version 1.1.6 to version 1.1.7"
+sed -i -e "s/version=3.11.1/version=3.12.0/g" core/pciutils/spkgbuild
+changelog "core/pciutils/spkgbuild" "Upgraded from version 3.11.1 to version 3.12.0"
+sed -i -e "s/version=3.2.7/version=3.3.0/g" core/rsync/spkgbuild
+changelog "core/rsync/spkgbuild" "Upgraded from version 3.2.7 to version 3.3.0"
+sed -i -e "s/version=6.24.11/version=6.24.12/g" core/tcsh/spkgbuild
+changelog "core/tcsh/spkgbuild" "Upgraded from version 6.24.11 to version 6.24.12"
+sed -i -e "s/version=2.44.0/version=2.44.1/g" core/webkitgtk/spkgbuild
+changelog "core/webkitgtk/spkgbuild" "Upgraded from version 2.44.0 to version 2.44.1"
+sed -i -e "s/version=4.2.1/version=4.2.2/g" core/weechat/spkgbuild
+changelog "core/weechat/spkgbuild" "Upgraded from version 4.2.1 to version 4.2.2"
