6d6cc807cdd262981ac77bb5fe974c59dd59ebfe
Voncloft <voncloft@gmail.com>
2024-04-17 07:17:42 -0400

    Minor Updates

---
+++ logs/old-test-updates/2024/Apr/applied_updates-04-17-24.sh
@@ -0,0 +1,10 @@
+sed -i -e "s/version=1.2.1/version=1.2.2/g" hardware/xf86-input-wacom/spkgbuild
+changelog "hardware/xf86-input-wacom/spkgbuild" "Upgraded from version 1.2.1 to version 1.2.2"
+sed -i -e "s/version=5.74/version=5.75/g" core/bluez/spkgbuild
+changelog "core/bluez/spkgbuild" "Upgraded from version 5.74 to version 5.75"
+sed -i -e "s/version=1.10.0/version=1.11.0/g" core/dunst/spkgbuild
+changelog "core/dunst/spkgbuild" "Upgraded from version 1.10.0 to version 1.11.0"
+sed -i -e "s/version=0.124.1/version=0.125.0/g" core/hugo/spkgbuild
+changelog "core/hugo/spkgbuild" "Upgraded from version 0.124.1 to version 0.125.0"
+sed -i -e "s/version=1.0.4/version=1.0.5/g" core/pipewire/spkgbuild
+changelog "core/pipewire/spkgbuild" "Upgraded from version 1.0.4 to version 1.0.5"
