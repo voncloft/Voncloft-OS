81836919627296a153fe1a98d614d2b2a2963839
Voncloft <voncloft@gmail.com>
2024-04-18 07:29:54 -0400

    Minor Updates

---
+++ logs/old-test-updates/2024/Apr/applied_updates-04-18-24.sh
@@ -0,0 +1,18 @@
+sed -i -e "s/version=18.1.3/version=18.1.4/g" compilers/clang/spkgbuild
+changelog "compilers/clang/spkgbuild" "Upgraded from version 18.1.3 to version 18.1.4"
+sed -i -e "s/version=18.1.3/version=18.1.4/g" compilers/lld/spkgbuild
+changelog "compilers/lld/spkgbuild" "Upgraded from version 18.1.3 to version 18.1.4"
+sed -i -e "s/version=18.1.3/version=18.1.4/g" compilers/llvm/spkgbuild
+changelog "compilers/llvm/spkgbuild" "Upgraded from version 18.1.3 to version 18.1.4"
+sed -i -e "s/version=18.1.3/version=18.1.4/g" compilers/llvm-bak/spkgbuild
+changelog "compilers/llvm-bak/spkgbuild" "Upgraded from version 18.1.3 to version 18.1.4"
+sed -i -e "s/version=18.1.3/version=18.1.4/g" compilers/llvm-old-2023-06-07/spkgbuild
+changelog "compilers/llvm-old-2023-06-07/spkgbuild" "Upgraded from version 18.1.3 to version 18.1.4"
+sed -i -e "s/version=1.19.8/version=1.20.0/g" core/conky/spkgbuild
+changelog "core/conky/spkgbuild" "Upgraded from version 1.19.8 to version 1.20.0"
+sed -i -e "s/version=6.8.6/version=6.8.7/g" core/linux-api-headers/spkgbuild
+changelog "core/linux-api-headers/spkgbuild" "Upgraded from version 6.8.6 to version 6.8.7"
+sed -i -e "s/version=6.8.6/version=6.8.7/g" core/linux-desktop/spkgbuild
+changelog "core/linux-desktop/spkgbuild" "Upgraded from version 6.8.6 to version 6.8.7"
+sed -i -e "s/version=6.8.6/version=6.8.7/g" core/linux-router/spkgbuild
+changelog "core/linux-router/spkgbuild" "Upgraded from version 6.8.6 to version 6.8.7"
