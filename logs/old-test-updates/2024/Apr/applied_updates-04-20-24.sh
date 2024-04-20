74c0e17c9534b8425c9e87b0da83296969687e17
Voncloft <voncloft@gmail.com>
2024-04-20 12:05:21 -0400

    fifixed php to include https for next install

---
+++ logs/old-test-updates/2024/Apr/applied_updates-04-20-24.sh
@@ -0,0 +1,10 @@
+sed -i -e "s/version=1.20.0/version=1.20.1/g" core/conky/spkgbuild
+changelog "core/conky/spkgbuild" "Upgraded from version 1.20.0 to version 1.20.1"
+sed -i -e "s/version=0.125.0/version=0.125.1/g" core/hugo/spkgbuild
+changelog "core/hugo/spkgbuild" "Upgraded from version 0.125.0 to version 0.125.1"
+sed -i -e "s/version=0.37.0/version=0.38.0/g" core/mpv/spkgbuild
+changelog "core/mpv/spkgbuild" "Upgraded from version 0.37.0 to version 0.38.0"
+sed -i -e "s/version=3.11.27/version=3.11.28/g" extra/flexget/spkgbuild
+changelog "extra/flexget/spkgbuild" "Upgraded from version 3.11.27 to version 3.11.28"
+sed -i -e "s/version=1.1.11rc4/version=1.1.12rc1/g" extra/solaar/spkgbuild
+changelog "extra/solaar/spkgbuild" "Upgraded from version 1.1.11rc4 to version 1.1.12rc1"
