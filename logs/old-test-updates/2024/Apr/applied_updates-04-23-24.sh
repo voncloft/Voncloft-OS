fde7984f3420471067a3a388f3ee0914aa5b0d33
Voncloft <voncloft@gmail.com>
2024-04-23 08:34:41 -0400

    Minor Updates

---
+++ logs/old-test-updates/2024/Apr/applied_updates-04-23-24.sh
@@ -0,0 +1,10 @@
+sed -i -e "s/version=2.6.9/version=2.7.0/g" core/enchant/spkgbuild
+changelog "core/enchant/spkgbuild" "Upgraded from version 2.6.9 to version 2.7.0"
+sed -i -e "s/version=0.125.1/version=0.125.3/g" core/hugo/spkgbuild
+changelog "core/hugo/spkgbuild" "Upgraded from version 0.125.1 to version 0.125.3"
+sed -i -e "s/version=3.11.28/version=3.11.29/g" extra/flexget/spkgbuild
+changelog "extra/flexget/spkgbuild" "Upgraded from version 3.11.28 to version 3.11.29"
+sed -i -e "s/version=2.3/version=2.4/g" extra/ncdu/spkgbuild
+changelog "extra/ncdu/spkgbuild" "Upgraded from version 2.3 to version 2.4"
+sed -i -e "s/version=1.1.12rc1/version=1.1.12rc2/g" extra/solaar/spkgbuild
+changelog "extra/solaar/spkgbuild" "Upgraded from version 1.1.12rc1 to version 1.1.12rc2"
