b02d27717ceecc4d35f9094706ab34d72918bf41
Voncloft <voncloft@gmail.com>
2024-05-11 17:31:36 -0400

    Minor Updates

---
+++ REPOS/xorg/xdg-desktop-portal/post-upgrade.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+rm -rvf /usr/lib/systemd
---
+++ logs/old-test-updates/2024/May/applied_updates-05-11-24.sh
@@ -0,0 +1,24 @@
+sed -i -e "s/version=24.0.6/version=24.0.7/g" hardware/mesa/spkgbuild
+changelog "hardware/mesa/spkgbuild" "Upgraded from version 24.0.6 to version 24.0.7"
+sed -i -e "s/version=3.29.2/version=3.29.3/g" compilers/cmake/spkgbuild
+changelog "compilers/cmake/spkgbuild" "Upgraded from version 3.29.2 to version 3.29.3"
+sed -i -e "s/version=1.12.0/version=1.12.1/g" compilers/ninja/spkgbuild
+changelog "compilers/ninja/spkgbuild" "Upgraded from version 1.12.0 to version 1.12.1"
+sed -i -e "s/version=1.20.2/version=1.21.0/g" core/conky/spkgbuild
+changelog "core/conky/spkgbuild" "Upgraded from version 1.20.2 to version 1.21.0"
+sed -i -e "s/version=0.125.6/version=0.125.7/g" core/hugo/spkgbuild
+changelog "core/hugo/spkgbuild" "Upgraded from version 0.125.6 to version 0.125.7"
+sed -i -e "s/version=3.99/version=3.100/g" core/nss/spkgbuild
+changelog "core/nss/spkgbuild" "Upgraded from version 3.99 to version 3.100"
+sed -i -e "s/version=1.0.5/version=1.0.6/g" core/pipewire/spkgbuild
+changelog "core/pipewire/spkgbuild" "Upgraded from version 1.0.5 to version 1.0.6"
+sed -i -e "s/version=2.44.1/version=2.45.1/g" core/webkitgtk/spkgbuild
+changelog "core/webkitgtk/spkgbuild" "Upgraded from version 2.44.1 to version 2.45.1"
+sed -i -e "s/version=6.1.0/version=6.2.0/g" extra/extra-cmake-modules/spkgbuild
+changelog "extra/extra-cmake-modules/spkgbuild" "Upgraded from version 6.1.0 to version 6.2.0"
+sed -i -e "s/version=3.11.31/version=3.11.33/g" extra/flexget/spkgbuild
+changelog "extra/flexget/spkgbuild" "Upgraded from version 3.11.31 to version 3.11.33"
+sed -i -e "s/version=1.1.12rc2/version=1.1.13rc1/g" extra/solaar/spkgbuild
+changelog "extra/solaar/spkgbuild" "Upgraded from version 1.1.12rc2 to version 1.1.13rc1"
+sed -i -e "s/version=6.1.0/version=6.2.0/g" kde-apps/khtml/spkgbuild
+changelog "kde-apps/khtml/spkgbuild" "Upgraded from version 6.1.0 to version 6.2.0"
