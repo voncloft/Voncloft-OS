867d10a7784e3092fc5a7e84814dce374b960560
Voncloft <voncloft@gmail.com>
2024-05-20 12:37:34 -0400

    Minor Updates

---
+++ logs/old-test-updates/2024/May/applied_updates-05-20-24.sh
@@ -0,0 +1,46 @@
+sed -i -e "s/version=18.1.5/version=18.1.6/g" compilers/clang/spkgbuild
+changelog "compilers/clang/spkgbuild" "Upgraded from version 18.1.5 to version 18.1.6"
+sed -i -e "s/version=18.1.5/version=18.1.6/g" compilers/lld/spkgbuild
+changelog "compilers/lld/spkgbuild" "Upgraded from version 18.1.5 to version 18.1.6"
+sed -i -e "s/version=18.1.5/version=18.1.6/g" compilers/llvm/spkgbuild
+changelog "compilers/llvm/spkgbuild" "Upgraded from version 18.1.5 to version 18.1.6"
+sed -i -e "s/version=18.1.5/version=18.1.6/g" compilers/llvm-bak/spkgbuild
+changelog "compilers/llvm-bak/spkgbuild" "Upgraded from version 18.1.5 to version 18.1.6"
+sed -i -e "s/version=18.1.5/version=18.1.6/g" compilers/llvm-old-2023-06-07/spkgbuild
+changelog "compilers/llvm-old-2023-06-07/spkgbuild" "Upgraded from version 18.1.5 to version 18.1.6"
+sed -i -e "s/version=5.75/version=5.76/g" core/bluez/spkgbuild
+changelog "core/bluez/spkgbuild" "Upgraded from version 5.75 to version 5.76"
+sed -i -e "s/version=2.10.0/version=2.11.0/g" core/cmus/spkgbuild
+changelog "core/cmus/spkgbuild" "Upgraded from version 2.10.0 to version 2.11.0"
+sed -i -e "s/version=1.21.0/version=1.21.1/g" core/conky/spkgbuild
+changelog "core/conky/spkgbuild" "Upgraded from version 1.21.0 to version 1.21.1"
+sed -i -e "s/version=10.0.0/version=10.03.1/g" core/ghostscript/spkgbuild
+changelog "core/ghostscript/spkgbuild" "Upgraded from version 10.0.0 to version 10.03.1"
+sed -i -e "s/version=0.125.7/version=0.126.1/g" core/hugo/spkgbuild
+changelog "core/hugo/spkgbuild" "Upgraded from version 0.125.7 to version 0.126.1"
+sed -i -e "s/version=6.8.0/version=6.9.0/g" core/iproute2/spkgbuild
+changelog "core/iproute2/spkgbuild" "Upgraded from version 6.8.0 to version 6.9.0"
+sed -i -e "s/version=6.7/version=6.9/g" core/iw/spkgbuild
+changelog "core/iw/spkgbuild" "Upgraded from version 6.7 to version 6.9"
+sed -i -e "s/version=6.8.9/version=6.9.1/g" core/linux-api-headers/spkgbuild
+changelog "core/linux-api-headers/spkgbuild" "Upgraded from version 6.8.9 to version 6.9.1"
+sed -i -e "s/version=6.8.9/version=6.9.1/g" core/linux-desktop/spkgbuild
+changelog "core/linux-desktop/spkgbuild" "Upgraded from version 6.8.9 to version 6.9.1"
+sed -i -e "s/version=6.8.9/version=6.9.1/g" core/linux-router/spkgbuild
+changelog "core/linux-router/spkgbuild" "Upgraded from version 6.8.9 to version 6.9.1"
+sed -i -e "s/version=6.7/version=6.8/g" core/man-pages/spkgbuild
+changelog "core/man-pages/spkgbuild" "Upgraded from version 6.7 to version 6.8"
+sed -i -e "s/version=1.0.6/version=1.1.81/g" core/pipewire/spkgbuild
+changelog "core/pipewire/spkgbuild" "Upgraded from version 1.0.6 to version 1.1.81"
+sed -i -e "s/version=7.2.4/version=7.2.5/g" core/redis/spkgbuild
+changelog "core/redis/spkgbuild" "Upgraded from version 7.2.4 to version 7.2.5"
+sed -i -e "s/version=23.12.0/version=24.5.0/g" core/smplayer/spkgbuild
+changelog "core/smplayer/spkgbuild" "Upgraded from version 23.12.0 to version 24.5.0"
+sed -i -e "s/version=6.7.3/version=6.7.4/g" core/streamlink/spkgbuild
+changelog "core/streamlink/spkgbuild" "Upgraded from version 6.7.3 to version 6.7.4"
+sed -i -e "s/version=2.45.1/version=2.45.2/g" core/webkitgtk/spkgbuild
+changelog "core/webkitgtk/spkgbuild" "Upgraded from version 2.45.1 to version 2.45.2"
+sed -i -e "s/version=3.2.4.1/version=3.2.5/g" core/wxgtk3/spkgbuild
+changelog "core/wxgtk3/spkgbuild" "Upgraded from version 3.2.4.1 to version 3.2.5"
+sed -i -e "s/version=2.43/version=2.43.1/g" extra/discordchatexporter/spkgbuild
+changelog "extra/discordchatexporter/spkgbuild" "Upgraded from version 2.43 to version 2.43.1"
