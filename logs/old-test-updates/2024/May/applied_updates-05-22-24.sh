7858e02cfee90aade2d51caa5dad01da6b20d892
Voncloft <voncloft@gmail.com>
2024-05-22 07:17:48 -0400

    Minor Updates

--- logs/old-test-updates/2024/May/applied_updates-05-22-24.sh
+++ logs/old-test-updates/2024/May/applied_updates-05-22-24.sh
@@ -1,2 +1,4 @@
 sed -i -e "s/version=3.4.7/version=3.5.0/g" core/icewm/spkgbuild
 changelog "core/icewm/spkgbuild" "Upgraded from version 3.4.7 to version 3.5.0"
+sed -i -e "s/version=3.14.55/version=3.14.73/g" server/blacklist-hosts/spkgbuild
+changelog "server/blacklist-hosts/spkgbuild" "Upgraded from version 3.14.55 to version 3.14.73"
