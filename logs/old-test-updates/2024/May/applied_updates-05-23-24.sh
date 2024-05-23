d7f3de078b51650486dba9bec9b2870db810ebf3
Voncloft <voncloft@gmail.com>
2024-05-23 08:38:37 -0400

    Minor Updates

---
+++ logs/old-test-updates/2024/May/applied_updates-05-23-24.sh
@@ -0,0 +1,10 @@
+sed -i -e "s/version=24.0.7/version=24.1.0/g" hardware/mesa/spkgbuild
+changelog "hardware/mesa/spkgbuild" "Upgraded from version 24.0.7 to version 24.1.0"
+sed -i -e "s/version=8.7.1/version=8.8.0/g" core/curl/spkgbuild
+changelog "core/curl/spkgbuild" "Upgraded from version 8.7.1 to version 8.8.0"
+sed -i -e "s/version=0.17/version=0.18/g" core/hicolor-icon-theme/spkgbuild
+changelog "core/hicolor-icon-theme/spkgbuild" "Upgraded from version 0.17 to version 0.18"
+sed -i -e "s/version=5.0/version=6.0/g" core/pavucontrol/spkgbuild
+changelog "core/pavucontrol/spkgbuild" "Upgraded from version 5.0 to version 6.0"
+sed -i -e "s/version=3.11.33/version=3.11.34/g" extra/flexget/spkgbuild
+changelog "extra/flexget/spkgbuild" "Upgraded from version 3.11.33 to version 3.11.34"
