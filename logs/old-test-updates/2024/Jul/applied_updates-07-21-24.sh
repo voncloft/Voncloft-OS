8f12e0dc6ad9a66fb24abb5806bec448dbd9d49f
Voncloft <voncloft@gmail.com>
2024-07-21 08:40:34 -0400

    Minor Updates

---
+++ logs/old-test-updates/2024/Jul/applied_updates-07-21-24.sh
@@ -0,0 +1,20 @@
+sed -i -e "s/version=6.2.4/version=6.2.7/g" cinnamon/cinnamon/spkgbuild
+changelog "cinnamon/cinnamon/spkgbuild" "Upgraded from version 6.2.4 to version 6.2.7"
+sed -i -e "s/version=6.2.0/version=6.2.1/g" cinnamon/cinnamon-session/spkgbuild
+changelog "cinnamon/cinnamon-session/spkgbuild" "Upgraded from version 6.2.0 to version 6.2.1"
+sed -i -e "s/version=6.2.3/version=6.2.5/g" cinnamon/nemo/spkgbuild
+changelog "cinnamon/nemo/spkgbuild" "Upgraded from version 6.2.3 to version 6.2.5"
+sed -i -e "s/version=24.1.3/version=24.1.4/g" hardware/mesa/spkgbuild
+changelog "hardware/mesa/spkgbuild" "Upgraded from version 24.1.3 to version 24.1.4"
+sed -i -e "s/version=3.30.0-rc4/version=3.30.1/g" compilers/cmake/spkgbuild
+changelog "compilers/cmake/spkgbuild" "Upgraded from version 3.30.0-rc4 to version 3.30.1"
+sed -i -e "s/version=0.128.2/version=0.129.0/g" core/hugo/spkgbuild
+changelog "core/hugo/spkgbuild" "Upgraded from version 0.128.2 to version 0.129.0"
+sed -i -e "s/version=6.9.0/version=6.10.0/g" core/iproute2/spkgbuild
+changelog "core/iproute2/spkgbuild" "Upgraded from version 6.9.0 to version 6.10.0"
+sed -i -e "s/version=1.26.1/version=1.28.0/g" core/v4l-utils/spkgbuild
+changelog "core/v4l-utils/spkgbuild" "Upgraded from version 1.26.1 to version 1.28.0"
+sed -i -e "s/version=4.3.4/version=4.3.5/g" core/weechat/spkgbuild
+changelog "core/weechat/spkgbuild" "Upgraded from version 4.3.4 to version 4.3.5"
+sed -i -e "s/version=3.11.40/version=3.11.41/g" extra/flexget/spkgbuild
+changelog "extra/flexget/spkgbuild" "Upgraded from version 3.11.40 to version 3.11.41"
