bd8e8c38ecbe96255f7ecf2590066a1d54a92c18
Voncloft <voncloft@gmail.com>
2024-07-22 10:02:12 -0400

    Minor Updates

---
+++ logs/old-test-updates/2024/Jul/applied_updates-07-22-24.sh
@@ -0,0 +1,4 @@
+sed -i -e "s/version=6.2.1/version=6.2.2/g" cinnamon/cinnamon-translations/spkgbuild
+changelog "cinnamon/cinnamon-translations/spkgbuild" "Upgraded from version 6.2.1 to version 6.2.2"
+sed -i -e "s/version=6.2.5/version=6.2.6/g" cinnamon/nemo/spkgbuild
+changelog "cinnamon/nemo/spkgbuild" "Upgraded from version 6.2.5 to version 6.2.6"
