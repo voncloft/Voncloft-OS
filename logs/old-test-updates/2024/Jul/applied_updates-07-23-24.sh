ced62a63deaae0dfa9b17b9debba75cf725f3532
Voncloft <voncloft@gmail.com>
2024-07-23 08:34:23 -0400

    Minor Updates

---
+++ logs/old-test-updates/2024/Jul/applied_updates-07-23-24.sh
@@ -0,0 +1,4 @@
+sed -i -e "s/version=4.10.1/version=4.10.2/g" compilers/ccache/spkgbuild
+changelog "compilers/ccache/spkgbuild" "Upgraded from version 4.10.1 to version 4.10.2"
+sed -i -e "s/version=1.9.4/version=1.10.0/g" core/lz4/spkgbuild
+changelog "core/lz4/spkgbuild" "Upgraded from version 1.9.4 to version 1.10.0"
