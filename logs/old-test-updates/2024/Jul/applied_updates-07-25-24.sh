4422f95068428d37dbed97801f45af79ed426c0d
Voncloft <voncloft@gmail.com>
2024-07-25 07:22:58 -0400

    Minor Updates

---
+++ logs/old-test-updates/2024/Jul/applied_updates-07-25-24.sh
@@ -0,0 +1,18 @@
+sed -i -e "s/version=1.21.4/version=1.21.5/g" core/conky/spkgbuild
+changelog "core/conky/spkgbuild" "Upgraded from version 1.21.4 to version 1.21.5"
+sed -i -e "s/version=8.8.0/version=8.9.0/g" core/curl/spkgbuild
+changelog "core/curl/spkgbuild" "Upgraded from version 8.8.0 to version 8.9.0"
+sed -i -e "s/version=6.10/version=6.10.1/g" core/linux-api-headers/spkgbuild
+changelog "core/linux-api-headers/spkgbuild" "Upgraded from version 6.10 to version 6.10.1"
+sed -i -e "s/version=6.10/version=6.10.1/g" core/linux-desktop/spkgbuild
+changelog "core/linux-desktop/spkgbuild" "Upgraded from version 6.10 to version 6.10.1"
+sed -i -e "s/version=6.10/version=6.10.1/g" core/linux-router/spkgbuild
+changelog "core/linux-router/spkgbuild" "Upgraded from version 6.10 to version 6.10.1"
+sed -i -e "s/version=3.102/version=3.102.1/g" core/nss/spkgbuild
+changelog "core/nss/spkgbuild" "Upgraded from version 3.102 to version 3.102.1"
+sed -i -e "s/version=256.2/version=256.4/g" core/systemd/spkgbuild
+changelog "core/systemd/spkgbuild" "Upgraded from version 256.2 to version 256.4"
+sed -i -e "s/version=1.28.0/version=1.28.1/g" core/v4l-utils/spkgbuild
+changelog "core/v4l-utils/spkgbuild" "Upgraded from version 1.28.0 to version 1.28.1"
+sed -i -e "s/version=2.4/version=2.5/g" extra/ncdu/spkgbuild
+changelog "extra/ncdu/spkgbuild" "Upgraded from version 2.4 to version 2.5"
