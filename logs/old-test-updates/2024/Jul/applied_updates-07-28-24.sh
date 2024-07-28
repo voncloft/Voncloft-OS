e21385b2e00d0099fc05db782e42f5b915564d6e
Voncloft <voncloft@gmail.com>
2024-07-28 09:05:20 -0400

    Minor Updates

---
+++ logs/old-test-updates/2024/Jul/applied_updates-07-28-24.sh
@@ -0,0 +1,14 @@
+sed -i -e "s/version=1.5.0/version=1.5.1/g" compilers/meson/spkgbuild
+changelog "compilers/meson/spkgbuild" "Upgraded from version 1.5.0 to version 1.5.1"
+sed -i -e "s/version=1.79.0/version=1.80.0/g" compilers/rust/spkgbuild
+changelog "compilers/rust/spkgbuild" "Upgraded from version 1.79.0 to version 1.80.0"
+sed -i -e "s/version=2.4.2/version=2.4.3/g" core/blueman/spkgbuild
+changelog "core/blueman/spkgbuild" "Upgraded from version 2.4.2 to version 2.4.3"
+sed -i -e "s/version=2.10.0/version=2.10.1/g" core/cracklib/spkgbuild
+changelog "core/cracklib/spkgbuild" "Upgraded from version 2.10.0 to version 2.10.1"
+sed -i -e "s/version=6.10.1/version=6.10.2/g" core/linux-api-headers/spkgbuild
+changelog "core/linux-api-headers/spkgbuild" "Upgraded from version 6.10.1 to version 6.10.2"
+sed -i -e "s/version=6.10.1/version=6.10.2/g" core/linux-desktop/spkgbuild
+changelog "core/linux-desktop/spkgbuild" "Upgraded from version 6.10.1 to version 6.10.2"
+sed -i -e "s/version=6.10.1/version=6.10.2/g" core/linux-router/spkgbuild
+changelog "core/linux-router/spkgbuild" "Upgraded from version 6.10.1 to version 6.10.2"
