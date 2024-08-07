90900e4c7663318dfb13a5effd69586c3905ee1a
Voncloft <voncloft@gmail.com>
2024-08-07 10:32:09 -0400

    Minor Updates

---
+++ logs/old-test-updates/2024/Aug/applied_updates-08-07-24.sh
@@ -0,0 +1,50 @@
+sed -i -e "s/version=6.2.7/version=6.2.8/g" cinnamon/cinnamon/spkgbuild
+changelog "cinnamon/cinnamon/spkgbuild" "Upgraded from version 6.2.7 to version 6.2.8"
+sed -i -e "s/version=6.2.6/version=6.2.7/g" cinnamon/nemo/spkgbuild
+changelog "cinnamon/nemo/spkgbuild" "Upgraded from version 6.2.6 to version 6.2.7"
+sed -i -e "s/version=24.1.4/version=24.1.5/g" hardware/mesa/spkgbuild
+changelog "hardware/mesa/spkgbuild" "Upgraded from version 24.1.4 to version 24.1.5"
+sed -i -e "s/version=3.30.1/version=3.30.2/g" compilers/cmake/spkgbuild
+changelog "compilers/cmake/spkgbuild" "Upgraded from version 3.30.1 to version 3.30.2"
+sed -i -e "s/version=5.2.21/version=5.2.32/g" core/bash/spkgbuild
+changelog "core/bash/spkgbuild" "Upgraded from version 5.2.21 to version 5.2.32"
+sed -i -e "s/version=2.42/version=2.43/g" core/binutils/spkgbuild
+changelog "core/binutils/spkgbuild" "Upgraded from version 2.42 to version 2.43"
+sed -i -e "s/version=6.9.2/version=6.10/g" core/btrfs-progs/spkgbuild
+changelog "core/btrfs-progs/spkgbuild" "Upgraded from version 6.9.2 to version 6.10"
+sed -i -e "s/version=1.21.5/version=1.21.6/g" core/conky/spkgbuild
+changelog "core/conky/spkgbuild" "Upgraded from version 1.21.5 to version 1.21.6"
+sed -i -e "s/version=2.10.1/version=2.10.2/g" core/cracklib/spkgbuild
+changelog "core/cracklib/spkgbuild" "Upgraded from version 2.10.1 to version 2.10.2"
+sed -i -e "s/version=8.9.0/version=8.9.1/g" core/curl/spkgbuild
+changelog "core/curl/spkgbuild" "Upgraded from version 8.9.0 to version 8.9.1"
+sed -i -e "s/version=1.15.0/version=1.15.2/g" core/gtest/spkgbuild
+changelog "core/gtest/spkgbuild" "Upgraded from version 1.15.0 to version 1.15.2"
+sed -i -e "s/version=0.129.0/version=0.131.0/g" core/hugo/spkgbuild
+changelog "core/hugo/spkgbuild" "Upgraded from version 0.129.0 to version 0.131.0"
+sed -i -e "s/version=2.29/version=2.30/g" core/links/spkgbuild
+changelog "core/links/spkgbuild" "Upgraded from version 2.29 to version 2.30"
+sed -i -e "s/version=6.10.2/version=6.10.3/g" core/linux-api-headers/spkgbuild
+changelog "core/linux-api-headers/spkgbuild" "Upgraded from version 6.10.2 to version 6.10.3"
+sed -i -e "s/version=6.10.2/version=6.10.3/g" core/linux-desktop/spkgbuild
+changelog "core/linux-desktop/spkgbuild" "Upgraded from version 6.10.2 to version 6.10.3"
+sed -i -e "s/version=6.10.2/version=6.10.3/g" core/linux-router/spkgbuild
+changelog "core/linux-router/spkgbuild" "Upgraded from version 6.10.2 to version 6.10.3"
+sed -i -e "s/version=6.8/version=6.8.1/g" core/mercurial/spkgbuild
+changelog "core/mercurial/spkgbuild" "Upgraded from version 6.8 to version 6.8.1"
+sed -i -e "s/version=3.102.1/version=3.103/g" core/nss/spkgbuild
+changelog "core/nss/spkgbuild" "Upgraded from version 3.102.1 to version 3.103"
+sed -i -e "s/version=1.2.1/version=1.2.2/g" core/pipewire/spkgbuild
+changelog "core/pipewire/spkgbuild" "Upgraded from version 1.2.1 to version 1.2.2"
+sed -i -e "s/version=8.2/version=8.2.13/g" core/readline/spkgbuild
+changelog "core/readline/spkgbuild" "Upgraded from version 8.2 to version 8.2.13"
+sed -i -e "s/version=7.2.5/version=7.4.0/g" core/redis/spkgbuild
+changelog "core/redis/spkgbuild" "Upgraded from version 7.2.5 to version 7.4.0"
+sed -i -e "s/version=3.09/version=3.10/g" core/sysvinit/spkgbuild
+changelog "core/sysvinit/spkgbuild" "Upgraded from version 3.09 to version 3.10"
+sed -i -e "s/version=2.45.5/version=2.45.6/g" core/webkitgtk/spkgbuild
+changelog "core/webkitgtk/spkgbuild" "Upgraded from version 2.45.5 to version 2.45.6"
+sed -i -e "s/version=3.11.41/version=3.11.42/g" extra/flexget/spkgbuild
+changelog "extra/flexget/spkgbuild" "Upgraded from version 3.11.41 to version 3.11.42"
+sed -i -e "s/version=27.2/version=27.3/g" extra/protobuf/spkgbuild
+changelog "extra/protobuf/spkgbuild" "Upgraded from version 27.2 to version 27.3"
