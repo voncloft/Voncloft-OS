8b3bed141d7cb3c13191f46b595cda67a160a7b0
Voncloft <voncloft@gmail.com>
2024-08-20 12:23:57 -0400

    Minor Updates

---
+++ logs/old-test-updates/2024/Aug/applied_updates-08-20-24.sh
@@ -0,0 +1,48 @@
+sed -i -e "s/version=6.2.8/version=6.2.9/g" cinnamon/cinnamon/spkgbuild
+changelog "cinnamon/cinnamon/spkgbuild" "Upgraded from version 6.2.8 to version 6.2.9"
+sed -i -e "s/version=6.2.7/version=6.2.8/g" cinnamon/nemo/spkgbuild
+changelog "cinnamon/nemo/spkgbuild" "Upgraded from version 6.2.7 to version 6.2.8"
+sed -i -e "s/version=24.1.5/version=24.2.0/g" hardware/mesa/spkgbuild
+changelog "hardware/mesa/spkgbuild" "Upgraded from version 24.1.5 to version 24.2.0"
+sed -i -e "s/version=2.2.0/version=2.3.0/g" compilers/pkgconf/spkgbuild
+changelog "compilers/pkgconf/spkgbuild" "Upgraded from version 2.2.0 to version 2.3.0"
+sed -i -e "s/version=1.80.0/version=1.80.1/g" compilers/rust/spkgbuild
+changelog "compilers/rust/spkgbuild" "Upgraded from version 1.80.0 to version 1.80.1"
+sed -i -e "s/version=2.43/version=2.43.1/g" core/binutils/spkgbuild
+changelog "core/binutils/spkgbuild" "Upgraded from version 2.43 to version 2.43.1"
+sed -i -e "s/version=6.10/version=6.10.1/g" core/btrfs-progs/spkgbuild
+changelog "core/btrfs-progs/spkgbuild" "Upgraded from version 6.10 to version 6.10.1"
+sed -i -e "s/version=0.9.0/version=0.10.0/g" core/bubblewrap/spkgbuild
+changelog "core/bubblewrap/spkgbuild" "Upgraded from version 0.9.0 to version 0.10.0"
+sed -i -e "s/version=0.26.0/version=0.27.0/g" core/cbindgen/spkgbuild
+changelog "core/cbindgen/spkgbuild" "Upgraded from version 0.26.0 to version 0.27.0"
+sed -i -e "s/version=2.8.1/version=2.8.2/g" core/enchant/spkgbuild
+changelog "core/enchant/spkgbuild" "Upgraded from version 2.8.1 to version 2.8.2"
+sed -i -e "s/version=6.9/version=6.10/g" core/ethtool/spkgbuild
+changelog "core/ethtool/spkgbuild" "Upgraded from version 6.9 to version 6.10"
+sed -i -e "s/version=1.35.1/version=1.36/g" core/fakeroot/spkgbuild
+changelog "core/fakeroot/spkgbuild" "Upgraded from version 1.35.1 to version 1.36"
+sed -i -e "s/version=0.131.0/version=0.133.0/g" core/hugo/spkgbuild
+changelog "core/hugo/spkgbuild" "Upgraded from version 0.131.0 to version 0.133.0"
+sed -i -e "s/version=2.14/version=2.15/g" core/i3status/spkgbuild
+changelog "core/i3status/spkgbuild" "Upgraded from version 2.14 to version 2.15"
+sed -i -e "s/version=6.10.3/version=6.10.6/g" core/linux-api-headers/spkgbuild
+changelog "core/linux-api-headers/spkgbuild" "Upgraded from version 6.10.3 to version 6.10.6"
+sed -i -e "s/version=6.10.3/version=6.10.6/g" core/linux-desktop/spkgbuild
+changelog "core/linux-desktop/spkgbuild" "Upgraded from version 6.10.3 to version 6.10.6"
+sed -i -e "s/version=6.10.3/version=6.10.6/g" core/linux-router/spkgbuild
+changelog "core/linux-router/spkgbuild" "Upgraded from version 6.10.3 to version 6.10.6"
+sed -i -e "s/version=6.8.3/version=6.9.0/g" core/streamlink/spkgbuild
+changelog "core/streamlink/spkgbuild" "Upgraded from version 6.8.3 to version 6.9.0"
+sed -i -e "s/version=256.4/version=256.5/g" core/systemd/spkgbuild
+changelog "core/systemd/spkgbuild" "Upgraded from version 256.4 to version 256.5"
+sed -i -e "s/version=2.45.6/version=2.45.90/g" core/webkitgtk/spkgbuild
+changelog "core/webkitgtk/spkgbuild" "Upgraded from version 2.45.6 to version 2.45.90"
+sed -i -e "s/version=4.3.5/version=4.4.1/g" core/weechat/spkgbuild
+changelog "core/weechat/spkgbuild" "Upgraded from version 4.3.5 to version 4.4.1"
+sed -i -e "s/version=6.4.0/version=6.5.0/g" extra/extra-cmake-modules/spkgbuild
+changelog "extra/extra-cmake-modules/spkgbuild" "Upgraded from version 6.4.0 to version 6.5.0"
+sed -i -e "s/version=3.11.42/version=3.11.43/g" extra/flexget/spkgbuild
+changelog "extra/flexget/spkgbuild" "Upgraded from version 3.11.42 to version 3.11.43"
+sed -i -e "s/version=6.4.0/version=6.5.0/g" kde-apps/khtml/spkgbuild
+changelog "kde-apps/khtml/spkgbuild" "Upgraded from version 6.4.0 to version 6.5.0"
