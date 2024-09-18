2d3f1129dcdc8609beee487cf11e662cfe320150
Voncloft <voncloft@gmail.com>
2024-09-18 10:37:43 -0400

    Minor Updates

---
+++ logs/old-test-updates/2024/Sep/applied_updates-09-18-24.sh
@@ -0,0 +1,14 @@
+sed -i -e "s/version=18.1.8/version=19.1.0/g" compilers/clang/spkgbuild
+changelog "compilers/clang/spkgbuild" "Upgraded from version 18.1.8 to version 19.1.0"
+sed -i -e "s/version=18.1.8/version=19.1.0/g" compilers/lld/spkgbuild
+changelog "compilers/lld/spkgbuild" "Upgraded from version 18.1.8 to version 19.1.0"
+sed -i -e "s/version=18.1.8/version=19.1.0/g" compilers/llvm/spkgbuild
+changelog "compilers/llvm/spkgbuild" "Upgraded from version 18.1.8 to version 19.1.0"
+sed -i -e "s/version=18.1.8/version=19.1.0/g" compilers/llvm-bak/spkgbuild
+changelog "compilers/llvm-bak/spkgbuild" "Upgraded from version 18.1.8 to version 19.1.0"
+sed -i -e "s/version=18.1.8/version=19.1.0/g" compilers/llvm-old-2023-06-07/spkgbuild
+changelog "compilers/llvm-old-2023-06-07/spkgbuild" "Upgraded from version 18.1.8 to version 19.1.0"
+sed -i -e "s/version=6.10.1/version=6.11/g" core/btrfs-progs/spkgbuild
+changelog "core/btrfs-progs/spkgbuild" "Upgraded from version 6.10.1 to version 6.11"
+sed -i -e "s/version=2.45.92/version=2.46.0/g" core/webkitgtk/spkgbuild
+changelog "core/webkitgtk/spkgbuild" "Upgraded from version 2.45.92 to version 2.46.0"
