c45ad48553cf551370ee5510f78be6232bd23508
Voncloft <voncloft@gmail.com>
2024-09-23 10:51:21 -0400

    Minor Updates

---
+++ logs/old-test-updates/2024/Sep/applied_updates-09-23-24.sh
@@ -0,0 +1,16 @@
+sed -i -e "s/version=24.2.2/version=24.2.3/g" hardware/mesa/spkgbuild
+changelog "hardware/mesa/spkgbuild" "Upgraded from version 24.2.2 to version 24.2.3"
+sed -i -e "s/version=1.5.1/version=1.5.2/g" compilers/meson/spkgbuild
+changelog "compilers/meson/spkgbuild" "Upgraded from version 1.5.1 to version 1.5.2"
+sed -i -e "s/version=8.10.0/version=8.10.1/g" core/curl/spkgbuild
+changelog "core/curl/spkgbuild" "Upgraded from version 8.10.0 to version 8.10.1"
+sed -i -e "s/version=5.3.0/version=5.3.1/g" core/gawk/spkgbuild
+changelog "core/gawk/spkgbuild" "Upgraded from version 5.3.0 to version 5.3.1"
+sed -i -e "s/version=0.134.2/version=0.134.3/g" core/hugo/spkgbuild
+changelog "core/hugo/spkgbuild" "Upgraded from version 0.134.2 to version 0.134.3"
+sed -i -e "s/version=1.2.3/version=1.2.4/g" core/pipewire/spkgbuild
+changelog "core/pipewire/spkgbuild" "Upgraded from version 1.2.3 to version 1.2.4"
+sed -i -e "s/version=2.2.15/version=2.2.16/g" extra/bluefish/spkgbuild
+changelog "extra/bluefish/spkgbuild" "Upgraded from version 2.2.15 to version 2.2.16"
+sed -i -e "s/version=28.1/version=28.2/g" extra/protobuf/spkgbuild
+changelog "extra/protobuf/spkgbuild" "Upgraded from version 28.1 to version 28.2"
