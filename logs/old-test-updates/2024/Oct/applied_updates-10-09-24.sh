6a61e4ab394d4b71f24fa063634eb30447833979
Voncloft <voncloft@gmail.com>
2024-10-09 15:21:25 -0400

    added firefox-bin

---
+++ logs/old-test-updates/2024/Oct/applied_updates-10-09-24.sh
@@ -0,0 +1,84 @@
+sed -i -e "s/version=6.2.0/version=6.2.1/g" cinnamon/cinnamon-screensaver/spkgbuild
+changelog "cinnamon/cinnamon-screensaver/spkgbuild" "Upgraded from version 6.2.0 to version 6.2.1"
+sed -i -e "s/version=24.2.3/version=24.2.4/g" hardware/mesa/spkgbuild
+changelog "hardware/mesa/spkgbuild" "Upgraded from version 24.2.3 to version 24.2.4"
+sed -i -e "s/version=19.1.0/version=19.1.1/g" compilers/clang/spkgbuild
+changelog "compilers/clang/spkgbuild" "Upgraded from version 19.1.0 to version 19.1.1"
+sed -i -e "s/version=3.30.3/version=3.30.5/g" compilers/cmake/spkgbuild
+changelog "compilers/cmake/spkgbuild" "Upgraded from version 3.30.3 to version 3.30.5"
+sed -i -e "s/version=15.1/version=15.2/g" compilers/gdb/spkgbuild
+changelog "compilers/gdb/spkgbuild" "Upgraded from version 15.1 to version 15.2"
+sed -i -e "s/version=19.1.0/version=19.1.1/g" compilers/lld/spkgbuild
+changelog "compilers/lld/spkgbuild" "Upgraded from version 19.1.0 to version 19.1.1"
+sed -i -e "s/version=19.1.0/version=19.1.1/g" compilers/llvm/spkgbuild
+changelog "compilers/llvm/spkgbuild" "Upgraded from version 19.1.0 to version 19.1.1"
+sed -i -e "s/version=19.1.0/version=19.1.1/g" compilers/llvm-bak/spkgbuild
+changelog "compilers/llvm-bak/spkgbuild" "Upgraded from version 19.1.0 to version 19.1.1"
+sed -i -e "s/version=19.1.0/version=19.1.1/g" compilers/llvm-old-2023-06-07/spkgbuild
+changelog "compilers/llvm-old-2023-06-07/spkgbuild" "Upgraded from version 19.1.0 to version 19.1.1"
+sed -i -e "s/version=1.5.2/version=1.6.0/g" compilers/meson/spkgbuild
+changelog "compilers/meson/spkgbuild" "Upgraded from version 1.5.2 to version 1.6.0"
+sed -i -e "s/version=4.4/version=4.4.1/g" core/audacious/spkgbuild
+changelog "core/audacious/spkgbuild" "Upgraded from version 4.4 to version 4.4.1"
+sed -i -e "s/version=4.4/version=4.4.1/g" core/audacious-plugins/spkgbuild
+changelog "core/audacious-plugins/spkgbuild" "Upgraded from version 4.4 to version 4.4.1"
+sed -i -e "s/version=5.2.32/version=5.2.37/g" core/bash/spkgbuild
+changelog "core/bash/spkgbuild" "Upgraded from version 5.2.32 to version 5.2.37"
+sed -i -e "s/version=1.36.1/version=1.37.0/g" core/busybox/spkgbuild
+changelog "core/busybox/spkgbuild" "Upgraded from version 1.36.1 to version 1.37.0"
+sed -i -e "s/version=7.0/version=7.1/g" core/cifs-utils/spkgbuild
+changelog "core/cifs-utils/spkgbuild" "Upgraded from version 7.0 to version 7.1"
+sed -i -e "s/version=2.4.10/version=2.4.11/g" core/cups/spkgbuild
+changelog "core/cups/spkgbuild" "Upgraded from version 2.4.10 to version 2.4.11"
+sed -i -e "s/version=1.15.8/version=1.15.10/g" core/dbus/spkgbuild
+changelog "core/dbus/spkgbuild" "Upgraded from version 1.15.8 to version 1.15.10"
+sed -i -e "s/version=3.0.13/version=3.1.0/g" core/dkms/spkgbuild
+changelog "core/dkms/spkgbuild" "Upgraded from version 3.0.13 to version 3.1.0"
+sed -i -e "s/version=6.10/version=6.11/g" core/ethtool/spkgbuild
+changelog "core/ethtool/spkgbuild" "Upgraded from version 6.10 to version 6.11"
+sed -i -e "s/version=2.9/version=2.10/g" core/fop/spkgbuild
+changelog "core/fop/spkgbuild" "Upgraded from version 2.9 to version 2.10"
+sed -i -e "s/version=1.0.15/version=1.0.16/g" core/fribidi/spkgbuild
+changelog "core/fribidi/spkgbuild" "Upgraded from version 1.0.15 to version 1.0.16"
+sed -i -e "s/version=0.134.3/version=0.135.0/g" core/hugo/spkgbuild
+changelog "core/hugo/spkgbuild" "Upgraded from version 0.134.3 to version 0.135.0"
+sed -i -e "s/version=6.11/version=6.11.2/g" core/linux-api-headers/spkgbuild
+changelog "core/linux-api-headers/spkgbuild" "Upgraded from version 6.11 to version 6.11.2"
+sed -i -e "s/version=6.11/version=6.11.2/g" core/linux-desktop/spkgbuild
+changelog "core/linux-desktop/spkgbuild" "Upgraded from version 6.11 to version 6.11.2"
+sed -i -e "s/version=6.11/version=6.11.2/g" core/linux-router/spkgbuild
+changelog "core/linux-router/spkgbuild" "Upgraded from version 6.11 to version 6.11.2"
+sed -i -e "s/version=5.34.0/version=5.34.1/g" core/monit/spkgbuild
+changelog "core/monit/spkgbuild" "Upgraded from version 5.34.0 to version 5.34.1"
+sed -i -e "s/version=0.38.0/version=0.39.0/g" core/mpv/spkgbuild
+changelog "core/mpv/spkgbuild" "Upgraded from version 0.38.0 to version 0.39.0"
+sed -i -e "s/version=4.0.44/version=4.0.45/g" core/mtools/spkgbuild
+changelog "core/mtools/spkgbuild" "Upgraded from version 4.0.44 to version 4.0.45"
+sed -i -e "s/version=3.104/version=3.105/g" core/nss/spkgbuild
+changelog "core/nss/spkgbuild" "Upgraded from version 3.104 to version 3.105"
+sed -i -e "s/version=11.2/version=12.1/g" core/picom/spkgbuild
+changelog "core/picom/spkgbuild" "Upgraded from version 11.2 to version 12.1"
+sed -i -e "s/version=1.2.4/version=1.2.5/g" core/pipewire/spkgbuild
+changelog "core/pipewire/spkgbuild" "Upgraded from version 1.2.4 to version 1.2.5"
+sed -i -e "s/version=5.2.0/version=5.3.0/g" core/qalculate/spkgbuild
+changelog "core/qalculate/spkgbuild" "Upgraded from version 5.2.0 to version 5.3.0"
+sed -i -e "s/version=7.4.0/version=7.4.1/g" core/redis/spkgbuild
+changelog "core/redis/spkgbuild" "Upgraded from version 7.4.0 to version 7.4.1"
+sed -i -e "s/version=6.10.0/version=6.11.0/g" core/streamlink/spkgbuild
+changelog "core/streamlink/spkgbuild" "Upgraded from version 6.10.0 to version 6.11.0"
+sed -i -e "s/version=256.6/version=256.7/g" core/systemd/spkgbuild
+changelog "core/systemd/spkgbuild" "Upgraded from version 256.6 to version 256.7"
+sed -i -e "s/version=3.4/version=3.5/g" core/tmux/spkgbuild
+changelog "core/tmux/spkgbuild" "Upgraded from version 3.4 to version 3.5"
+sed -i -e "s/version=2.10.1/version=2.10.90/g" core/udisks2/spkgbuild
+changelog "core/udisks2/spkgbuild" "Upgraded from version 2.10.1 to version 2.10.90"
+sed -i -e "s/version=2.46.0/version=2.46.1/g" core/webkitgtk/spkgbuild
+changelog "core/webkitgtk/spkgbuild" "Upgraded from version 2.46.0 to version 2.46.1"
+sed -i -e "s/version=5.6.2/version=5.6.3/g" core/xz/spkgbuild
+changelog "core/xz/spkgbuild" "Upgraded from version 5.6.2 to version 5.6.3"
+sed -i -e "s/version=3.11.46/version=3.11.47/g" extra/flexget/spkgbuild
+changelog "extra/flexget/spkgbuild" "Upgraded from version 3.11.46 to version 3.11.47"
+sed -i -e "s/version=5.8/version=5.9/g" extra/gnucash/spkgbuild
+changelog "extra/gnucash/spkgbuild" "Upgraded from version 5.8 to version 5.9"
+sed -i -e "s/version=2.5/version=2.6/g" extra/ncdu/spkgbuild
+changelog "extra/ncdu/spkgbuild" "Upgraded from version 2.5 to version 2.6"
