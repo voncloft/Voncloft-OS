11615c87f28292cc78b8c7e7df88fde0de8f1f4b
Voncloft <voncloft@gmail.com>
2024-10-16 10:27:03 -0400

    Minor Updates

---
+++ logs/old-test-updates/2024/Oct/applied_updates-10-16-24.sh
@@ -0,0 +1,20 @@
+sed -i -e "s/version=2.10.6/version=2.11.0/g" hardware/xf86-input-evdev/spkgbuild
+changelog "hardware/xf86-input-evdev/spkgbuild" "Upgraded from version 2.10.6 to version 2.11.0"
+sed -i -e "s/version=1.4.0/version=1.5.0/g" hardware/xf86-input-libinput/spkgbuild
+changelog "hardware/xf86-input-libinput/spkgbuild" "Upgraded from version 1.4.0 to version 1.5.0"
+sed -i -e "s/version=19.1.1/version=19.1.2/g" compilers/clang/spkgbuild
+changelog "compilers/clang/spkgbuild" "Upgraded from version 19.1.1 to version 19.1.2"
+sed -i -e "s/version=19.1.1/version=19.1.2/g" compilers/lld/spkgbuild
+changelog "compilers/lld/spkgbuild" "Upgraded from version 19.1.1 to version 19.1.2"
+sed -i -e "s/version=19.1.1/version=19.1.2/g" compilers/llvm/spkgbuild
+changelog "compilers/llvm/spkgbuild" "Upgraded from version 19.1.1 to version 19.1.2"
+sed -i -e "s/version=19.1.1/version=19.1.2/g" compilers/llvm-bak/spkgbuild
+changelog "compilers/llvm-bak/spkgbuild" "Upgraded from version 19.1.1 to version 19.1.2"
+sed -i -e "s/version=19.1.1/version=19.1.2/g" compilers/llvm-old-2023-06-07/spkgbuild
+changelog "compilers/llvm-old-2023-06-07/spkgbuild" "Upgraded from version 19.1.1 to version 19.1.2"
+sed -i -e "s/version=0.135.0/version=0.136.0/g" core/hugo/spkgbuild
+changelog "core/hugo/spkgbuild" "Upgraded from version 0.135.0 to version 0.136.0"
+sed -i -e "s/version=12.2/version=12.3/g" core/picom/spkgbuild
+changelog "core/picom/spkgbuild" "Upgraded from version 12.2 to version 12.3"
+sed -i -e "s/version=3.2.1/version=3.3.1/g" core/qutebrowser/spkgbuild
+changelog "core/qutebrowser/spkgbuild" "Upgraded from version 3.2.1 to version 3.3.1"
