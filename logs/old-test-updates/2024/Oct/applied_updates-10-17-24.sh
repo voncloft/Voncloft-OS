5f457ab480bf61aa8c5e7bfea3d23eb2935c0b19
Voncloft <voncloft@gmail.com>
2024-10-17 07:28:45 -0400

    Minor Updates

---
+++ logs/old-test-updates/2024/Oct/applied_updates-10-17-24.sh
@@ -0,0 +1,18 @@
+sed -i -e "s/version=24.2.4/version=24.2.5/g" hardware/mesa/spkgbuild
+changelog "hardware/mesa/spkgbuild" "Upgraded from version 24.2.4 to version 24.2.5"
+sed -i -e "s/version=2023.02.20/version=2024.10.16/g" compilers/autoconf-archive/spkgbuild
+changelog "compilers/autoconf-archive/spkgbuild" "Upgraded from version 2023.02.20 to version 2024.10.16"
+sed -i -e "s/version=0.136.0/version=0.136.1/g" core/hugo/spkgbuild
+changelog "core/hugo/spkgbuild" "Upgraded from version 0.136.0 to version 0.136.1"
+sed -i -e "s/version=1.2/version=1.3/g" libs/libunistring/spkgbuild
+changelog "libs/libunistring/spkgbuild" "Upgraded from version 1.2 to version 1.3"
+sed -i -e "s/version=132.0b7/version=132.0b8/g" nonfree/firefox/spkgbuild
+changelog "nonfree/firefox/spkgbuild" "Upgraded from version 132.0b7 to version 132.0b8"
+sed -i -e "s/version=1.35.41/version=1.35.42/g" python/python-boto3/spkgbuild
+changelog "python/python-boto3/spkgbuild" "Upgraded from version 1.35.41 to version 1.35.42"
+sed -i -e "s/version=1.35.41/version=1.35.42/g" python/python-botocore/spkgbuild
+changelog "python/python-botocore/spkgbuild" "Upgraded from version 1.35.41 to version 1.35.42"
+sed -i -e "s/version=30.4.0/version=30.6.0/g" python/python-faker/spkgbuild
+changelog "python/python-faker/spkgbuild" "Upgraded from version 30.4.0 to version 30.6.0"
+sed -i -e "s/version=1.15.3/version=1.15.4/g" python/python-yarl/spkgbuild
+changelog "python/python-yarl/spkgbuild" "Upgraded from version 1.15.3 to version 1.15.4"
