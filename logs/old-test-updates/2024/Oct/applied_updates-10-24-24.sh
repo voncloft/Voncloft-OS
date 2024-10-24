80a861e479a5548df7801f5a93e01d5542f175a4
Voncloft <voncloft@gmail.com>
2024-10-24 08:58:32 -0400

    Minor Updates

---
+++ logs/old-test-updates/2024/Oct/applied_updates-10-24-24.sh
@@ -0,0 +1,18 @@
+sed -i -e "s/version=0.24.0/version=0.24.1/g" core/alacritty/spkgbuild
+changelog "core/alacritty/spkgbuild" "Upgraded from version 0.24.0 to version 0.24.1"
+sed -i -e "s/version=1.2.5/version=1.2.6/g" core/pipewire/spkgbuild
+changelog "core/pipewire/spkgbuild" "Upgraded from version 1.2.5 to version 1.2.6"
+sed -i -e "s/version=28.2/version=28.3/g" extra/protobuf/spkgbuild
+changelog "extra/protobuf/spkgbuild" "Upgraded from version 28.2 to version 28.3"
+sed -i -e "s/version=560.35.03/version=565.57.01/g" nonfree/nvidia/spkgbuild
+changelog "nonfree/nvidia/spkgbuild" "Upgraded from version 560.35.03 to version 565.57.01"
+sed -i -e "s/version=560.35.03/version=565.57.01/g" nonfree/nvidia-32/spkgbuild
+changelog "nonfree/nvidia-32/spkgbuild" "Upgraded from version 560.35.03 to version 565.57.01"
+sed -i -e "s/version=560.35.03/version=565.57.01/g" nonfree/nvidia-modules/spkgbuild
+changelog "nonfree/nvidia-modules/spkgbuild" "Upgraded from version 560.35.03 to version 565.57.01"
+sed -i -e "s/version=1.35.45/version=1.35.46/g" python/python-boto3/spkgbuild
+changelog "python/python-boto3/spkgbuild" "Upgraded from version 1.35.45 to version 1.35.46"
+sed -i -e "s/version=1.35.45/version=1.35.47/g" python/python-botocore/spkgbuild
+changelog "python/python-botocore/spkgbuild" "Upgraded from version 1.35.45 to version 1.35.47"
+sed -i -e "s/version=6.115.3/version=6.115.4/g" python/python-hypothesis/spkgbuild
+changelog "python/python-hypothesis/spkgbuild" "Upgraded from version 6.115.3 to version 6.115.4"
