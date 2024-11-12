2170964e37bdafba71107b9c768f14b28bdd507a
Voncloft <voncloft@gmail.com>
2024-11-12 09:46:24 -0500

    Minor Updates

---
+++ logs/old-test-updates/2024/Nov/applied_updates-11-12-24.sh
@@ -0,0 +1,26 @@
+sed -i -e "s/version=12.3/version=12.4/g" core/picom/spkgbuild
+changelog "core/picom/spkgbuild" "Upgraded from version 12.3 to version 12.4"
+sed -i -e "s/version=1.24.5/version=1.25.0/g" core/wget/spkgbuild
+changelog "core/wget/spkgbuild" "Upgraded from version 1.24.5 to version 1.25.0"
+sed -i -e "s/version=2.71/version=2.72/g" libs/libcap/spkgbuild
+changelog "libs/libcap/spkgbuild" "Upgraded from version 2.71 to version 2.72"
+sed -i -e "s/version=3.3.1/version=3.3.2/g" libs/openexr/spkgbuild
+changelog "libs/openexr/spkgbuild" "Upgraded from version 3.3.1 to version 3.3.2"
+sed -i -e "s/version=133.0b6/version=133.0b7/g" nonfree/firefox/spkgbuild
+changelog "nonfree/firefox/spkgbuild" "Upgraded from version 133.0b6 to version 133.0b7"
+sed -i -e "s/version=1.35.57/version=1.35.58/g" python/python-boto3/spkgbuild
+changelog "python/python-boto3/spkgbuild" "Upgraded from version 1.35.57 to version 1.35.58"
+sed -i -e "s/version=1.35.57/version=1.35.58/g" python/python-botocore/spkgbuild
+changelog "python/python-botocore/spkgbuild" "Upgraded from version 1.35.57 to version 1.35.58"
+sed -i -e "s/version=2024.11.0/version=2024.11.1/g" python/python-distributed/spkgbuild
+changelog "python/python-distributed/spkgbuild" "Upgraded from version 2024.11.0 to version 2024.11.1"
+sed -i -e "s/version=24.10.3/version=24.11.1/g" python/python-gevent/spkgbuild
+changelog "python/python-gevent/spkgbuild" "Upgraded from version 24.10.3 to version 24.11.1"
+sed -i -e "s/version=5.0.1/version=5.0.2/g" python/python-internetarchive/spkgbuild
+changelog "python/python-internetarchive/spkgbuild" "Upgraded from version 5.0.1 to version 5.0.2"
+sed -i -e "s/version=0.9.25/version=0.9.28/g" python/python-json5/spkgbuild
+changelog "python/python-json5/spkgbuild" "Upgraded from version 0.9.25 to version 0.9.28"
+sed -i -e "s/version=0.4.0/version=0.4.1/g" python/python-pyvcd/spkgbuild
+changelog "python/python-pyvcd/spkgbuild" "Upgraded from version 0.4.0 to version 0.4.1"
+sed -i -e "s/version=75.3.0/version=75.4.0/g" python/python-setuptools/spkgbuild
+changelog "python/python-setuptools/spkgbuild" "Upgraded from version 75.3.0 to version 75.4.0"
