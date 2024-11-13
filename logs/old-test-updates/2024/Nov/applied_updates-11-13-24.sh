29b29a7ce081d90e3936c397ee94149c102b37ea
Voncloft <voncloft@gmail.com>
2024-11-13 05:31:06 -0500

    Minor Updates

---
+++ logs/old-test-updates/2024/Nov/applied_updates-11-13-24.sh
@@ -0,0 +1,36 @@
+sed -i -e "s/version=1.2.12/version=1.2.13/g" media/alsa-utils/spkgbuild
+changelog "media/alsa-utils/spkgbuild" "Upgraded from version 1.2.12 to version 1.2.13"
+sed -i -e "s/version=1.2.12/version=1.2.13/g" libs/alsa-lib/spkgbuild
+changelog "libs/alsa-lib/spkgbuild" "Upgraded from version 1.2.12 to version 1.2.13"
+sed -i -e "s/version=5.2.0/version=5.3.1/g" python/python-amqp/spkgbuild
+changelog "python/python-amqp/spkgbuild" "Upgraded from version 5.2.0 to version 5.3.1"
+sed -i -e "s/version=1.18.4/version=1.19.0/g" python/python-cfn-lint/spkgbuild
+changelog "python/python-cfn-lint/spkgbuild" "Upgraded from version 1.18.4 to version 1.19.0"
+sed -i -e "s/version=7.2.4/version=7.2.5/g" python/python-cx-freeze/spkgbuild
+changelog "python/python-cx-freeze/spkgbuild" "Upgraded from version 7.2.4 to version 7.2.5"
+sed -i -e "s/version=0.19.10/version=0.19.11/g" python/python-django-modeltranslation/spkgbuild
+changelog "python/python-django-modeltranslation/spkgbuild" "Upgraded from version 0.19.10 to version 0.19.11"
+sed -i -e "s/version=0.115.4/version=0.115.5/g" python/python-fastapi/spkgbuild
+changelog "python/python-fastapi/spkgbuild" "Upgraded from version 0.115.4 to version 0.115.5"
+sed -i -e "s/version=2.152.0/version=2.153.0/g" python/python-google-api-python-client/spkgbuild
+changelog "python/python-google-api-python-client/spkgbuild" "Upgraded from version 2.152.0 to version 2.153.0"
+sed -i -e "s/version=6.118.7/version=6.118.8/g" python/python-hypothesis/spkgbuild
+changelog "python/python-hypothesis/spkgbuild" "Upgraded from version 6.118.7 to version 6.118.8"
+sed -i -e "s/version=5.0.2/version=5.0.3/g" python/python-internetarchive/spkgbuild
+changelog "python/python-internetarchive/spkgbuild" "Upgraded from version 5.0.2 to version 5.0.3"
+sed -i -e "s/version=3.4.2/version=4.0.0/g" python/python-jsonpickle/spkgbuild
+changelog "python/python-jsonpickle/spkgbuild" "Upgraded from version 3.4.2 to version 4.0.0"
+sed -i -e "s/version=0.5.5/version=0.5.6/g" python/python-jsonschema-objects/spkgbuild
+changelog "python/python-jsonschema-objects/spkgbuild" "Upgraded from version 0.5.5 to version 0.5.6"
+sed -i -e "s/version=1.12.4/version=1.13.0/g" python/python-manuel/spkgbuild
+changelog "python/python-manuel/spkgbuild" "Upgraded from version 1.12.4 to version 1.13.0"
+sed -i -e "s/version=0.5.6/version=1.0.1/g" python/python-patsy/spkgbuild
+changelog "python/python-patsy/spkgbuild" "Upgraded from version 0.5.6 to version 1.0.1"
+sed -i -e "s/version=3.17.7/version=3.17.8/g" python/python-peewee/spkgbuild
+changelog "python/python-peewee/spkgbuild" "Upgraded from version 3.17.7 to version 3.17.8"
+sed -i -e "s/version=8.0.1/version=9.0.0/g" python/python-sybil/spkgbuild
+changelog "python/python-sybil/spkgbuild" "Upgraded from version 8.0.1 to version 9.0.0"
+sed -i -e "s/version=3.1.0/version=3.2.0/g" ruby-gems/ruby-optimist/spkgbuild
+changelog "ruby-gems/ruby-optimist/spkgbuild" "Upgraded from version 3.1.0 to version 3.2.0"
+sed -i -e "s/version=3.14.130/version=3.14.131/g" server/blacklist-hosts/spkgbuild
+changelog "server/blacklist-hosts/spkgbuild" "Upgraded from version 3.14.130 to version 3.14.131"
