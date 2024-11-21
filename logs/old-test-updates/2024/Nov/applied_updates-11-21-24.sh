14fa8bc8f636825bd6880070f15705c5e9642ffc
Voncloft <voncloft@gmail.com>
2024-11-21 04:13:44 -0500

    Minor Updates

---
+++ logs/old-test-updates/2024/Nov/applied_updates-11-21-24.sh
@@ -0,0 +1,58 @@
+sed -i -e "s/version=19.1.3/version=19.1.4/g" compilers/clang/spkgbuild
+changelog "compilers/clang/spkgbuild" "Upgraded from version 19.1.3 to version 19.1.4"
+sed -i -e "s/version=19.1.3/version=19.1.4/g" compilers/lld/spkgbuild
+changelog "compilers/lld/spkgbuild" "Upgraded from version 19.1.3 to version 19.1.4"
+sed -i -e "s/version=19.1.3/version=19.1.4/g" compilers/llvm/spkgbuild
+changelog "compilers/llvm/spkgbuild" "Upgraded from version 19.1.3 to version 19.1.4"
+sed -i -e "s/version=19.1.3/version=19.1.4/g" compilers/llvm-bak/spkgbuild
+changelog "compilers/llvm-bak/spkgbuild" "Upgraded from version 19.1.3 to version 19.1.4"
+sed -i -e "s/version=19.1.3/version=19.1.4/g" compilers/llvm-old-2023-06-07/spkgbuild
+changelog "compilers/llvm-old-2023-06-07/spkgbuild" "Upgraded from version 19.1.3 to version 19.1.4"
+sed -i -e "s/version=6.11.0/version=6.12.0/g" core/iproute2/spkgbuild
+changelog "core/iproute2/spkgbuild" "Upgraded from version 6.11.0 to version 6.12.0"
+sed -i -e "s/version=6.8.2/version=6.9/g" core/mercurial/spkgbuild
+changelog "core/mercurial/spkgbuild" "Upgraded from version 6.8.2 to version 6.9"
+sed -i -e "s/version=2.6/version=2.7/g" extra/ncdu/spkgbuild
+changelog "extra/ncdu/spkgbuild" "Upgraded from version 2.6 to version 2.7"
+sed -i -e "s/version=6.6.0/version=6.7.1/g" fonts/awesome-font/spkgbuild
+changelog "fonts/awesome-font/spkgbuild" "Upgraded from version 6.6.0 to version 6.7.1"
+sed -i -e "s/version=19.1.3/version=19.1.4/g" multilib/llvm-32/spkgbuild
+changelog "multilib/llvm-32/spkgbuild" "Upgraded from version 19.1.3 to version 19.1.4"
+sed -i -e "s/version=1.1.21/version=1.1.22/g" libs/libmtp/spkgbuild
+changelog "libs/libmtp/spkgbuild" "Upgraded from version 1.1.21 to version 1.1.22"
+sed -i -e "s/version=2.5.3/version=2.5.4/g" libs/libtool/spkgbuild
+changelog "libs/libtool/spkgbuild" "Upgraded from version 2.5.3 to version 2.5.4"
+sed -i -e "s/version=4.4.1/version=4.4.2/g" networking/wireshark/spkgbuild
+changelog "networking/wireshark/spkgbuild" "Upgraded from version 4.4.1 to version 4.4.2"
+sed -i -e "s/version=133.0b4/version=133.0b5/g" nonfree/thunderbird/spkgbuild
+changelog "nonfree/thunderbird/spkgbuild" "Upgraded from version 133.0b4 to version 133.0b5"
+sed -i -e "s/version=4.2.0/version=4.2.1/g" python/python-bcrypt/spkgbuild
+changelog "python/python-bcrypt/spkgbuild" "Upgraded from version 4.2.0 to version 4.2.1"
+sed -i -e "s/version=1.35.64/version=1.35.65/g" python/python-boto3/spkgbuild
+changelog "python/python-boto3/spkgbuild" "Upgraded from version 1.35.64 to version 1.35.65"
+sed -i -e "s/version=1.35.64/version=1.35.65/g" python/python-botocore/spkgbuild
+changelog "python/python-botocore/spkgbuild" "Upgraded from version 1.35.64 to version 1.35.65"
+sed -i -e "s/version=1.19.0/version=1.20.0/g" python/python-cfn-lint/spkgbuild
+changelog "python/python-cfn-lint/spkgbuild" "Upgraded from version 1.19.0 to version 1.20.0"
+sed -i -e "s/version=4.7.0/version=4.8.0/g" python/python-cliff/spkgbuild
+changelog "python/python-cliff/spkgbuild" "Upgraded from version 4.7.0 to version 4.8.0"
+sed -i -e "s/version=2.153.0/version=2.154.0/g" python/python-google-api-python-client/spkgbuild
+changelog "python/python-google-api-python-client/spkgbuild" "Upgraded from version 2.153.0 to version 2.154.0"
+sed -i -e "s/version=3.1.0/version=3.2.0/g" python/python-osc-lib/spkgbuild
+changelog "python/python-osc-lib/spkgbuild" "Upgraded from version 3.1.0 to version 3.2.0"
+sed -i -e "s/version=9.6.0/version=9.7.0/g" python/python-oslo-config/spkgbuild
+changelog "python/python-oslo-config/spkgbuild" "Upgraded from version 9.6.0 to version 9.7.0"
+sed -i -e "s/version=5.6.0/version=5.7.0/g" python/python-oslo-context/spkgbuild
+changelog "python/python-oslo-context/spkgbuild" "Upgraded from version 5.6.0 to version 5.7.0"
+sed -i -e "s/version=6.4.0/version=6.5.0/g" python/python-oslo-i18n/spkgbuild
+changelog "python/python-oslo-i18n/spkgbuild" "Upgraded from version 6.4.0 to version 6.5.0"
+sed -i -e "s/version=6.1.2/version=6.2.0/g" python/python-oslo-log/spkgbuild
+changelog "python/python-oslo-log/spkgbuild" "Upgraded from version 6.1.2 to version 6.2.0"
+sed -i -e "s/version=5.5.0/version=5.6.0/g" python/python-oslo-serialization/spkgbuild
+changelog "python/python-oslo-serialization/spkgbuild" "Upgraded from version 5.5.0 to version 5.6.0"
+sed -i -e "s/version=75.5.0/version=75.6.0/g" python/python-setuptools/spkgbuild
+changelog "python/python-setuptools/spkgbuild" "Upgraded from version 75.5.0 to version 75.6.0"
+sed -i -e "s/version=0.13.0/version=0.13.1/g" python/python-typer/spkgbuild
+changelog "python/python-typer/spkgbuild" "Upgraded from version 0.13.0 to version 0.13.1"
+sed -i -e "s/version=3.14.132/version=3.14.133/g" server/blacklist-hosts/spkgbuild
+changelog "server/blacklist-hosts/spkgbuild" "Upgraded from version 3.14.132 to version 3.14.133"
