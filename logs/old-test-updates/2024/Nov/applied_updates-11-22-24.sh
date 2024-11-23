0170416ee8ca0b1fc4962b55c09251832312324e
Voncloft <voncloft@gmail.com>
2024-11-22 22:12:23 -0500

    Minor Updates

---
+++ logs/old-test-updates/2024/Nov/applied_updates-11-22-24.sh
@@ -0,0 +1,46 @@
+sed -i -e "s/version=24.2.7/version=24.3.0/g" hardware/mesa/spkgbuild
+changelog "hardware/mesa/spkgbuild" "Upgraded from version 24.2.7 to version 24.3.0"
+sed -i -e "s/version=6.12/version=6.12.1/g" core/linux-api-headers/spkgbuild
+changelog "core/linux-api-headers/spkgbuild" "Upgraded from version 6.12 to version 6.12.1"
+sed -i -e "s/version=6.12/version=6.12.1/g" core/linux-desktop/spkgbuild
+changelog "core/linux-desktop/spkgbuild" "Upgraded from version 6.12 to version 6.12.1"
+sed -i -e "s/version=6.12/version=6.12.1/g" core/linux-router/spkgbuild
+changelog "core/linux-router/spkgbuild" "Upgraded from version 6.12 to version 6.12.1"
+sed -i -e "s/version=4.0.45/version=4.0.46/g" core/mtools/spkgbuild
+changelog "core/mtools/spkgbuild" "Upgraded from version 4.0.45 to version 4.0.46"
+sed -i -e "s/version=3.106/version=3.107/g" core/nss/spkgbuild
+changelog "core/nss/spkgbuild" "Upgraded from version 3.106 to version 3.107"
+sed -i -e "s/version=1.2.1/version=1.2.2/g" networking/iptraf-ng/spkgbuild
+changelog "networking/iptraf-ng/spkgbuild" "Upgraded from version 1.2.1 to version 1.2.2"
+sed -i -e "s/version=1.92.0/version=1.94.0/g" python/python-aws-sam-translator/spkgbuild
+changelog "python/python-aws-sam-translator/spkgbuild" "Upgraded from version 1.92.0 to version 1.94.0"
+sed -i -e "s/version=1.35.65/version=1.35.68/g" python/python-boto3/spkgbuild
+changelog "python/python-boto3/spkgbuild" "Upgraded from version 1.35.65 to version 1.35.68"
+sed -i -e "s/version=1.35.65/version=1.35.68/g" python/python-botocore/spkgbuild
+changelog "python/python-botocore/spkgbuild" "Upgraded from version 1.35.65 to version 1.35.68"
+sed -i -e "s/version=2024.1.14/version=2024.11.20/g" python/python-cmarkgfm/spkgbuild
+changelog "python/python-cmarkgfm/spkgbuild" "Upgraded from version 2024.1.14 to version 2024.11.20"
+sed -i -e "s/version=6.119.3/version=6.119.4/g" python/python-hypothesis/spkgbuild
+changelog "python/python-hypothesis/spkgbuild" "Upgraded from version 6.119.3 to version 6.119.4"
+sed -i -e "s/version=5.8.0/version=5.9.0/g" python/python-keystoneauth1/spkgbuild
+changelog "python/python-keystoneauth1/spkgbuild" "Upgraded from version 5.8.0 to version 5.9.0"
+sed -i -e "s/version=16.0.0/version=17.0.0/g" python/python-oslo-db/spkgbuild
+changelog "python/python-oslo-db/spkgbuild" "Upgraded from version 16.0.0 to version 17.0.0"
+sed -i -e "s/version=2.1.1/version=2.1.2/g" python/python-pook/spkgbuild
+changelog "python/python-pook/spkgbuild" "Upgraded from version 2.1.1 to version 2.1.2"
+sed -i -e "s/version=2.10.0/version=2.10.1/g" python/python-pydantic/spkgbuild
+changelog "python/python-pydantic/spkgbuild" "Upgraded from version 2.10.0 to version 2.10.1"
+sed -i -e "s/version=0.10.3/version=0.10.4/g" python/python-s3transfer/spkgbuild
+changelog "python/python-s3transfer/spkgbuild" "Upgraded from version 0.10.3 to version 0.10.4"
+sed -i -e "s/version=6.4.1/version=6.4.2/g" python/python-tornado/spkgbuild
+changelog "python/python-tornado/spkgbuild" "Upgraded from version 6.4.1 to version 6.4.2"
+sed -i -e "s/version=24.10.0/version=24.11.0/g" python/python-twisted/spkgbuild
+changelog "python/python-twisted/spkgbuild" "Upgraded from version 24.10.0 to version 24.11.0"
+sed -i -e "s/version=1.17.2/version=1.18.0/g" python/python-yarl/spkgbuild
+changelog "python/python-yarl/spkgbuild" "Upgraded from version 1.17.2 to version 1.18.0"
+sed -i -e "s/version=0.136.0/version=0.136.2/g" python/python-zeroconf/spkgbuild
+changelog "python/python-zeroconf/spkgbuild" "Upgraded from version 0.136.0 to version 0.136.2"
+sed -i -e "s/version=2024.11.2/version=2024.11.3/g" server/homeassistant/spkgbuild
+changelog "server/homeassistant/spkgbuild" "Upgraded from version 2024.11.2 to version 2024.11.3"
+sed -i -e "s/version=8.3.13/version=8.4.1/g" server/php/spkgbuild
+changelog "server/php/spkgbuild" "Upgraded from version 8.3.13 to version 8.4.1"
