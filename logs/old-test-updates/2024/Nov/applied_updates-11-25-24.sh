a8c721733c4dd5ca413ec79e1e825752245b7fc9
Voncloft <voncloft@gmail.com>
2024-11-25 07:18:28 -0500

    Minor Updates

---
+++ logs/old-test-updates/2024/Nov/applied_updates-11-25-24.sh
@@ -0,0 +1,22 @@
+sed -i -e "s/version=0.139.0/version=0.139.2/g" core/hugo/spkgbuild
+changelog "core/hugo/spkgbuild" "Upgraded from version 0.139.0 to version 0.139.2"
+sed -i -e "s/version=2.1.1/version=2.1.2/g" lxqt/lxqt-panel/spkgbuild
+changelog "lxqt/lxqt-panel/spkgbuild" "Upgraded from version 2.1.1 to version 2.1.2"
+sed -i -e "s/version=0.33.0/version=0.34.0/g" libs/neon/spkgbuild
+changelog "libs/neon/spkgbuild" "Upgraded from version 0.33.0 to version 0.34.0"
+sed -i -e "s/version=7.6.7/version=7.6.8/g" python/python-coverage/spkgbuild
+changelog "python/python-coverage/spkgbuild" "Upgraded from version 7.6.7 to version 7.6.8"
+sed -i -e "s/version=7.2.5/version=7.2.6/g" python/python-cx-freeze/spkgbuild
+changelog "python/python-cx-freeze/spkgbuild" "Upgraded from version 7.2.5 to version 7.2.6"
+sed -i -e "s/version=2.2.0/version=2.3.0/g" python/python-numpy/spkgbuild
+changelog "python/python-numpy/spkgbuild" "Upgraded from version 2.2.0 to version 2.3.0"
+sed -i -e "s/version=3.10.11/version=3.10.12/g" python/python-orjson/spkgbuild
+changelog "python/python-orjson/spkgbuild" "Upgraded from version 3.10.11 to version 3.10.12"
+sed -i -e "s/version=4.67.0/version=4.67.1/g" python/python-tqdm/spkgbuild
+changelog "python/python-tqdm/spkgbuild" "Upgraded from version 4.67.0 to version 4.67.1"
+sed -i -e "s/version=0.45.0/version=0.45.1/g" python/python-wheel/spkgbuild
+changelog "python/python-wheel/spkgbuild" "Upgraded from version 0.45.0 to version 0.45.1"
+sed -i -e "s/version=3.14.133/version=3.14.135/g" server/blacklist-hosts/spkgbuild
+changelog "server/blacklist-hosts/spkgbuild" "Upgraded from version 3.14.133 to version 3.14.135"
+sed -i -e "s/version=3.3.0/version=3.3.1/g" server/radicale/spkgbuild
+changelog "server/radicale/spkgbuild" "Upgraded from version 3.3.0 to version 3.3.1"
