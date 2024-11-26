399e21090d79d6155d7c66f6b8b6612274ab1d7c
Voncloft <voncloft@gmail.com>
2024-11-26 10:15:21 -0500

    Minor Updates

---
+++ logs/old-test-updates/2024/Nov/applied_updates-11-26-24.sh
@@ -0,0 +1,20 @@
+sed -i -e "s/version=6.2.0/version=6.4.0/g" cinnamon/cinnamon-menus/spkgbuild
+changelog "cinnamon/cinnamon-menus/spkgbuild" "Upgraded from version 6.2.0 to version 6.4.0"
+sed -i -e "s/version=6.2.0/version=6.4.0/g" cinnamon/cjs/spkgbuild
+changelog "cinnamon/cjs/spkgbuild" "Upgraded from version 6.2.0 to version 6.4.0"
+sed -i -e "s/version=5.3.0/version=5.4.0/g" core/qalculate/spkgbuild
+changelog "core/qalculate/spkgbuild" "Upgraded from version 5.3.0 to version 5.4.0"
+sed -i -e "s/version=5.3.0/version=5.4.0/g" libs/libqalculate/spkgbuild
+changelog "libs/libqalculate/spkgbuild" "Upgraded from version 5.3.0 to version 5.4.0"
+sed -i -e "s/version=2.8.5/version=2.8.6/g" libs/xapp/spkgbuild
+changelog "libs/xapp/spkgbuild" "Upgraded from version 2.8.5 to version 2.8.6"
+sed -i -e "s/version=2.47.0/version=2.47.1/g" networking/git/spkgbuild
+changelog "networking/git/spkgbuild" "Upgraded from version 2.47.0 to version 2.47.1"
+sed -i -e "s/version=3.47.0.0/version=3.47.1.0/g" python/python-apsw/spkgbuild
+changelog "python/python-apsw/spkgbuild" "Upgraded from version 3.47.0.0 to version 3.47.1.0"
+sed -i -e "s/version=1.35.68/version=1.35.69/g" python/python-boto3/spkgbuild
+changelog "python/python-boto3/spkgbuild" "Upgraded from version 1.35.68 to version 1.35.69"
+sed -i -e "s/version=1.35.68/version=1.35.69/g" python/python-botocore/spkgbuild
+changelog "python/python-botocore/spkgbuild" "Upgraded from version 1.35.68 to version 1.35.69"
+sed -i -e "s/version=4.21.1/version=4.21.2/g" server/samba/spkgbuild
+changelog "server/samba/spkgbuild" "Upgraded from version 4.21.1 to version 4.21.2"
