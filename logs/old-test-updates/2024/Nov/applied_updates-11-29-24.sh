2372fb1dadfc8e8bca42361b1966dc2a0e51159f
Voncloft <voncloft@gmail.com>
2024-11-29 06:40:49 -0500

    Minor Updates

---
+++ logs/old-test-updates/2024/Nov/applied_updates-11-29-24.sh
@@ -0,0 +1,48 @@
+sed -i -e "s/version=6.2.9/version=6.4.0/g" cinnamon/cinnamon/spkgbuild
+changelog "cinnamon/cinnamon/spkgbuild" "Upgraded from version 6.2.9 to version 6.4.0"
+sed -i -e "s/version=6.2.1/version=6.4.0/g" cinnamon/cinnamon-session/spkgbuild
+changelog "cinnamon/cinnamon-session/spkgbuild" "Upgraded from version 6.2.1 to version 6.4.0"
+sed -i -e "s/version=6.2.0/version=6.4.0/g" cinnamon/cinnamon-settings-daemon/spkgbuild
+changelog "cinnamon/cinnamon-settings-daemon/spkgbuild" "Upgraded from version 6.2.0 to version 6.4.0"
+sed -i -e "s/version=6.2.8/version=6.4.0/g" cinnamon/nemo/spkgbuild
+changelog "cinnamon/nemo/spkgbuild" "Upgraded from version 6.2.8 to version 6.4.0"
+sed -i -e "s/version=1.82.0/version=1.83.0/g" compilers/rust/spkgbuild
+changelog "compilers/rust/spkgbuild" "Upgraded from version 1.82.0 to version 1.83.0"
+sed -i -e "s/version=5.45/version=5.46/g" core/file/spkgbuild
+changelog "core/file/spkgbuild" "Upgraded from version 5.45 to version 5.46"
+sed -i -e "s/version=6.24.13/version=6.24.14/g" core/tcsh/spkgbuild
+changelog "core/tcsh/spkgbuild" "Upgraded from version 6.24.13 to version 6.24.14"
+sed -i -e "s/version=28.3/version=29.0/g" extra/protobuf/spkgbuild
+changelog "extra/protobuf/spkgbuild" "Upgraded from version 28.3 to version 29.0"
+sed -i -e "s/version=1.24.0/version=1.24.1/g" media/openal/spkgbuild
+changelog "media/openal/spkgbuild" "Upgraded from version 1.24.0 to version 1.24.1"
+sed -i -e "s/version=5.4.0/version=5.4.0.1/g" libs/libqalculate/spkgbuild
+changelog "libs/libqalculate/spkgbuild" "Upgraded from version 5.4.0 to version 5.4.0.1"
+sed -i -e "s/version=134.0b1/version=134.0b2/g" nonfree/firefox/spkgbuild
+changelog "nonfree/firefox/spkgbuild" "Upgraded from version 134.0b1 to version 134.0b2"
+sed -i -e "s/version=1.35.70/version=1.35.71/g" python/python-boto3/spkgbuild
+changelog "python/python-boto3/spkgbuild" "Upgraded from version 1.35.70 to version 1.35.71"
+sed -i -e "s/version=1.35.70/version=1.35.71/g" python/python-botocore/spkgbuild
+changelog "python/python-botocore/spkgbuild" "Upgraded from version 1.35.70 to version 1.35.71"
+sed -i -e "s/version=1.20.0/version=1.20.1/g" python/python-cfn-lint/spkgbuild
+changelog "python/python-cfn-lint/spkgbuild" "Upgraded from version 1.20.0 to version 1.20.1"
+sed -i -e "s/version=33.0.0/version=33.1.0/g" python/python-faker/spkgbuild
+changelog "python/python-faker/spkgbuild" "Upgraded from version 33.0.0 to version 33.1.0"
+sed -i -e "s/version=5.0.0/version=5.1.0/g" python/python-gitlab/spkgbuild
+changelog "python/python-gitlab/spkgbuild" "Upgraded from version 5.0.0 to version 5.1.0"
+sed -i -e "s/version=6.119.4/version=6.121.0/g" python/python-hypothesis/spkgbuild
+changelog "python/python-hypothesis/spkgbuild" "Upgraded from version 6.119.4 to version 6.121.0"
+sed -i -e "s/version=0.31.6/version=0.32.0/g" python/python-meilisearch/spkgbuild
+changelog "python/python-meilisearch/spkgbuild" "Upgraded from version 0.31.6 to version 0.32.0"
+sed -i -e "s/version=1.1.0/version=1.2.0/g" python/python-multipart/spkgbuild
+changelog "python/python-multipart/spkgbuild" "Upgraded from version 1.1.0 to version 1.2.0"
+sed -i -e "s/version=2.10.0/version=2.10.1/g" python/python-pyjwt/spkgbuild
+changelog "python/python-pyjwt/spkgbuild" "Upgraded from version 2.10.0 to version 2.10.1"
+sed -i -e "s/version=0.34.0/version=0.35.0/g" python/python-pytest-httpx/spkgbuild
+changelog "python/python-pytest-httpx/spkgbuild" "Upgraded from version 0.34.0 to version 0.35.0"
+sed -i -e "s/version=0.0.17/version=0.0.18/g" python/python-python-multipart/spkgbuild
+changelog "python/python-python-multipart/spkgbuild" "Upgraded from version 0.0.17 to version 0.0.18"
+sed -i -e "s/version=4.5.2/version=4.5.3/g" python/python-trimesh/spkgbuild
+changelog "python/python-trimesh/spkgbuild" "Upgraded from version 4.5.2 to version 4.5.3"
+sed -i -e "s/version=2024.11.3/version=2024.12.0/g" server/homeassistant/spkgbuild
+changelog "server/homeassistant/spkgbuild" "Upgraded from version 2024.11.3 to version 2024.12.0"
