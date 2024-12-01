5c5bd537972056b00188b8cc0d2e1d752c456df4
Voncloft <voncloft@gmail.com>
2024-12-01 12:29:01 -0500

    Minor Updates

---
+++ logs/old-test-updates/2024/Dec/applied_updates-12-01-24.sh
@@ -0,0 +1,34 @@
+sed -i -e "s/version=6.11/version=6.12/g" core/btrfs-progs/spkgbuild
+changelog "core/btrfs-progs/spkgbuild" "Upgraded from version 6.11 to version 6.12"
+sed -i -e "s/version=1.21.8/version=1.21.9/g" core/conky/spkgbuild
+changelog "core/conky/spkgbuild" "Upgraded from version 1.21.8 to version 1.21.9"
+sed -i -e "s/version=1.11.0/version=1.12.0/g" core/dunst/spkgbuild
+changelog "core/dunst/spkgbuild" "Upgraded from version 1.11.0 to version 1.12.0"
+sed -i -e "s/version=0.139.2/version=0.139.3/g" core/hugo/spkgbuild
+changelog "core/hugo/spkgbuild" "Upgraded from version 0.139.2 to version 0.139.3"
+sed -i -e "s/version=256.8/version=256.9/g" core/systemd/spkgbuild
+changelog "core/systemd/spkgbuild" "Upgraded from version 256.8 to version 256.9"
+sed -i -e "s/version=4.4.3/version=4.4.4/g" core/weechat/spkgbuild
+changelog "core/weechat/spkgbuild" "Upgraded from version 4.4.3 to version 4.4.4"
+sed -i -e "s/version=24.06.3/version=24.11/g" extra/anki/spkgbuild
+changelog "extra/anki/spkgbuild" "Upgraded from version 24.06.3 to version 24.11"
+sed -i -e "s/version=1.5.0/version=1.6.0/g" kf/kirigami-addons/spkgbuild
+changelog "kf/kirigami-addons/spkgbuild" "Upgraded from version 1.5.0 to version 1.6.0"
+sed -i -e "s/version=134.0b2/version=134.0b3/g" nonfree/firefox/spkgbuild
+changelog "nonfree/firefox/spkgbuild" "Upgraded from version 134.0b2 to version 134.0b3"
+sed -i -e "s/version=7.2.6/version=7.2.7/g" python/python-cx-freeze/spkgbuild
+changelog "python/python-cx-freeze/spkgbuild" "Upgraded from version 7.2.6 to version 7.2.7"
+sed -i -e "s/version=6.121.0/version=6.122.0/g" python/python-hypothesis/spkgbuild
+changelog "python/python-hypothesis/spkgbuild" "Upgraded from version 6.121.0 to version 6.122.0"
+sed -i -e "s/version=0.9.0/version=0.10.0/g" python/python-itemadapter/spkgbuild
+changelog "python/python-itemadapter/spkgbuild" "Upgraded from version 0.9.0 to version 0.10.0"
+sed -i -e "s/version=1.2.0/version=1.2.1/g" python/python-multipart/spkgbuild
+changelog "python/python-multipart/spkgbuild" "Upgraded from version 1.2.0 to version 1.2.1"
+sed -i -e "s/version=1.8.0/version=1.8.1/g" python/python-pytest-plugins/spkgbuild
+changelog "python/python-pytest-plugins/spkgbuild" "Upgraded from version 1.8.0 to version 1.8.1"
+sed -i -e "s/version=0.13.1/version=0.14.0/g" python/python-typer/spkgbuild
+changelog "python/python-typer/spkgbuild" "Upgraded from version 0.13.1 to version 0.14.0"
+sed -i -e "s/version=1.18.0/version=1.18.2/g" python/python-yarl/spkgbuild
+changelog "python/python-yarl/spkgbuild" "Upgraded from version 1.18.0 to version 1.18.2"
+sed -i -e "s/version=3.14.135/version=3.14.136/g" server/blacklist-hosts/spkgbuild
+changelog "server/blacklist-hosts/spkgbuild" "Upgraded from version 3.14.135 to version 3.14.136"
