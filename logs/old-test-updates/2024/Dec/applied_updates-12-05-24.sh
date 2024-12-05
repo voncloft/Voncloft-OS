012858e2e4d3c71673abf244aec82f2f3b596404
Voncloft <voncloft@gmail.com>
2024-12-05 11:46:20 -0500

    Minor Updates

---
+++ logs/old-test-updates/2024/Dec/applied_updates-12-05-24.sh
@@ -0,0 +1,40 @@
+sed -i -e "s/version=3.12.0/version=3.12.1/g" extra/flexget/spkgbuild
+changelog "extra/flexget/spkgbuild" "Upgraded from version 3.12.0 to version 3.12.1"
+sed -i -e "s/version=29.0/version=29.1/g" extra/protobuf/spkgbuild
+changelog "extra/protobuf/spkgbuild" "Upgraded from version 29.0 to version 29.1"
+sed -i -e "s/version=1.24.9/version=1.24.10/g" media/gst-libav/spkgbuild
+changelog "media/gst-libav/spkgbuild" "Upgraded from version 1.24.9 to version 1.24.10"
+sed -i -e "s/version=1.24.9/version=1.24.10/g" media/gst-plugins-bad/spkgbuild
+changelog "media/gst-plugins-bad/spkgbuild" "Upgraded from version 1.24.9 to version 1.24.10"
+sed -i -e "s/version=1.24.9/version=1.24.10/g" media/gst-plugins-base/spkgbuild
+changelog "media/gst-plugins-base/spkgbuild" "Upgraded from version 1.24.9 to version 1.24.10"
+sed -i -e "s/version=1.24.9/version=1.24.10/g" media/gst-plugins-good/spkgbuild
+changelog "media/gst-plugins-good/spkgbuild" "Upgraded from version 1.24.9 to version 1.24.10"
+sed -i -e "s/version=1.24.9/version=1.24.10/g" media/gst-plugins-ugly/spkgbuild
+changelog "media/gst-plugins-ugly/spkgbuild" "Upgraded from version 1.24.9 to version 1.24.10"
+sed -i -e "s/version=1.24.9/version=1.24.10/g" media/gstreamer/spkgbuild
+changelog "media/gstreamer/spkgbuild" "Upgraded from version 1.24.9 to version 1.24.10"
+sed -i -e "s/version=2.4.123/version=2.4.124/g" libs/libdrm/spkgbuild
+changelog "libs/libdrm/spkgbuild" "Upgraded from version 2.4.123 to version 2.4.124"
+sed -i -e "s/version=2024.11.18/version=2024.12.03/g" networking/yt-dlp/spkgbuild
+changelog "networking/yt-dlp/spkgbuild" "Upgraded from version 2024.11.18 to version 2024.12.03"
+sed -i -e "s/version=134.0b4/version=134.0b5/g" nonfree/firefox/spkgbuild
+changelog "nonfree/firefox/spkgbuild" "Upgraded from version 134.0b4 to version 134.0b5"
+sed -i -e "s/version=1.35.74/version=1.35.75/g" python/python-boto3/spkgbuild
+changelog "python/python-boto3/spkgbuild" "Upgraded from version 1.35.74 to version 1.35.75"
+sed -i -e "s/version=1.35.74/version=1.35.75/g" python/python-botocore/spkgbuild
+changelog "python/python-botocore/spkgbuild" "Upgraded from version 1.35.74 to version 1.35.75"
+sed -i -e "s/version=2024.11.2/version=2024.12.0/g" python/python-distributed/spkgbuild
+changelog "python/python-distributed/spkgbuild" "Upgraded from version 2024.11.2 to version 2024.12.0"
+sed -i -e "s/version=0.115.5/version=0.115.6/g" python/python-fastapi/spkgbuild
+changelog "python/python-fastapi/spkgbuild" "Upgraded from version 0.115.5 to version 0.115.6"
+sed -i -e "s/version=0.32.0/version=0.33.0/g" python/python-meilisearch/spkgbuild
+changelog "python/python-meilisearch/spkgbuild" "Upgraded from version 0.32.0 to version 0.33.0"
+sed -i -e "s/version=7.4.0/version=8.0.0/g" python/python-oslo-utils/spkgbuild
+changelog "python/python-oslo-utils/spkgbuild" "Upgraded from version 7.4.0 to version 8.0.0"
+sed -i -e "s/version=7.3.2410301219/version=7.3.2412041710/g" python/python-pycontracts/spkgbuild
+changelog "python/python-pycontracts/spkgbuild" "Upgraded from version 7.3.2410301219 to version 7.3.2412041710"
+sed -i -e "s/version=3.0.1/version=3.0.2/g" python/python-spyder-kernels/spkgbuild
+changelog "python/python-spyder-kernels/spkgbuild" "Upgraded from version 3.0.1 to version 3.0.2"
+sed -i -e "s/version=0.15.0/version=0.15.1/g" python/python-typer/spkgbuild
+changelog "python/python-typer/spkgbuild" "Upgraded from version 0.15.0 to version 0.15.1"
