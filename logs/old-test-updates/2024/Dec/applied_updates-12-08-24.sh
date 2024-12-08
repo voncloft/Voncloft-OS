bcac3e74605f16589c71ef4ad4ce76e8c6eb2901
Voncloft <voncloft@gmail.com>
2024-12-08 11:25:10 -0500

    Minor Updates

---
+++ logs/old-test-updates/2024/Dec/applied_updates-12-08-24.sh
@@ -0,0 +1,68 @@
+sed -i -e "s/version=6.4.1/version=6.4.2/g" cinnamon/cinnamon/spkgbuild
+changelog "cinnamon/cinnamon/spkgbuild" "Upgraded from version 6.4.1 to version 6.4.2"
+sed -i -e "s/version=6.4.0/version=6.4.1/g" cinnamon/cinnamon-control-center/spkgbuild
+changelog "cinnamon/cinnamon-control-center/spkgbuild" "Upgraded from version 6.4.0 to version 6.4.1"
+sed -i -e "s/version=6.4.0/version=6.4.1/g" cinnamon/cinnamon-translations/spkgbuild
+changelog "cinnamon/cinnamon-translations/spkgbuild" "Upgraded from version 6.4.0 to version 6.4.1"
+sed -i -e "s/version=6.4.2/version=6.4.3/g" cinnamon/nemo/spkgbuild
+changelog "cinnamon/nemo/spkgbuild" "Upgraded from version 6.4.2 to version 6.4.3"
+sed -i -e "s/version=24.3.0/version=24.3.1/g" hardware/mesa/spkgbuild
+changelog "hardware/mesa/spkgbuild" "Upgraded from version 24.3.0 to version 24.3.1"
+sed -i -e "s/version=3.31.1/version=3.31.2/g" compilers/cmake/spkgbuild
+changelog "compilers/cmake/spkgbuild" "Upgraded from version 3.31.1 to version 3.31.2"
+sed -i -e "s/version=1.15.12/version=1.15.90/g" core/dbus/spkgbuild
+changelog "core/dbus/spkgbuild" "Upgraded from version 1.15.12 to version 1.15.90"
+sed -i -e "s/version=2.5.1/version=2.5.2/g" core/gnupg/spkgbuild
+changelog "core/gnupg/spkgbuild" "Upgraded from version 2.5.1 to version 2.5.2"
+sed -i -e "s/version=6.12.1/version=6.12.3/g" core/linux-api-headers/spkgbuild
+changelog "core/linux-api-headers/spkgbuild" "Upgraded from version 6.12.1 to version 6.12.3"
+sed -i -e "s/version=6.12.1/version=6.12.3/g" core/linux-desktop/spkgbuild
+changelog "core/linux-desktop/spkgbuild" "Upgraded from version 6.12.1 to version 6.12.3"
+sed -i -e "s/version=6.12.1/version=6.12.3/g" core/linux-router/spkgbuild
+changelog "core/linux-router/spkgbuild" "Upgraded from version 6.12.1 to version 6.12.3"
+sed -i -e "s/version=2.13.0/version=2.14.0/g" libs/libwacom/spkgbuild
+changelog "libs/libwacom/spkgbuild" "Upgraded from version 2.13.0 to version 2.14.0"
+sed -i -e "s/version=2.30.9/version=2.30.10/g" libs/sdl2/spkgbuild
+changelog "libs/sdl2/spkgbuild" "Upgraded from version 2.30.9 to version 2.30.10"
+sed -i -e "s/version=2.8.6/version=2.8.7/g" libs/xapp/spkgbuild
+changelog "libs/xapp/spkgbuild" "Upgraded from version 2.8.6 to version 2.8.7"
+sed -i -e "s/version=2024.12.03/version=2024.12.06/g" networking/yt-dlp/spkgbuild
+changelog "networking/yt-dlp/spkgbuild" "Upgraded from version 2024.12.03 to version 2024.12.06"
+sed -i -e "s/version=134.0b5/version=134.0b7/g" nonfree/firefox/spkgbuild
+changelog "nonfree/firefox/spkgbuild" "Upgraded from version 134.0b5 to version 134.0b7"
+sed -i -e "s/version=565.57.01/version=565.77/g" nonfree/nvidia/spkgbuild
+changelog "nonfree/nvidia/spkgbuild" "Upgraded from version 565.57.01 to version 565.77"
+sed -i -e "s/version=565.57.01/version=565.77/g" nonfree/nvidia-32/spkgbuild
+changelog "nonfree/nvidia-32/spkgbuild" "Upgraded from version 565.57.01 to version 565.77"
+sed -i -e "s/version=565.57.01/version=565.77/g" nonfree/nvidia-modules/spkgbuild
+changelog "nonfree/nvidia-modules/spkgbuild" "Upgraded from version 565.57.01 to version 565.77"
+sed -i -e "s/version=134.0b2/version=134.0b3/g" nonfree/thunderbird/spkgbuild
+changelog "nonfree/thunderbird/spkgbuild" "Upgraded from version 134.0b2 to version 134.0b3"
+sed -i -e "s/version=1.14.0/version=1.15.0/g" plasma/plasma-wayland-protocols/spkgbuild
+changelog "plasma/plasma-wayland-protocols/spkgbuild" "Upgraded from version 1.14.0 to version 1.15.0"
+sed -i -e "s/version=4.6.2/version=4.7.0/g" python/python-anyio/spkgbuild
+changelog "python/python-anyio/spkgbuild" "Upgraded from version 4.6.2 to version 4.7.0"
+sed -i -e "s/version=1.35.75/version=1.35.76/g" python/python-boto3/spkgbuild
+changelog "python/python-boto3/spkgbuild" "Upgraded from version 1.35.75 to version 1.35.76"
+sed -i -e "s/version=1.35.75/version=1.35.76/g" python/python-botocore/spkgbuild
+changelog "python/python-botocore/spkgbuild" "Upgraded from version 1.35.75 to version 1.35.76"
+sed -i -e "s/version=7.6.8/version=7.6.9/g" python/python-coverage/spkgbuild
+changelog "python/python-coverage/spkgbuild" "Upgraded from version 7.6.8 to version 7.6.9"
+sed -i -e "s/version=4.55.1/version=4.55.2/g" python/python-fonttools/spkgbuild
+changelog "python/python-fonttools/spkgbuild" "Upgraded from version 4.55.1 to version 4.55.2"
+sed -i -e "s/version=1.8.4/version=1.8.5/g" python/python-poetry/spkgbuild
+changelog "python/python-poetry/spkgbuild" "Upgraded from version 1.8.4 to version 1.8.5"
+sed -i -e "s/version=0.13.1/version=0.14.0/g" python/python-pytest-subtests/spkgbuild
+changelog "python/python-pytest-subtests/spkgbuild" "Upgraded from version 0.13.1 to version 0.14.0"
+sed -i -e "s/version=21.8/version=21.9/g" python/python-telegram-bot/spkgbuild
+changelog "python/python-telegram-bot/spkgbuild" "Upgraded from version 21.8 to version 21.9"
+sed -i -e "s/version=5.0.1/version=6.0/g" python/python-zope-configuration/spkgbuild
+changelog "python/python-zope-configuration/spkgbuild" "Upgraded from version 5.0.1 to version 6.0"
+sed -i -e "s/version=3.14.137/version=3.14.139/g" server/blacklist-hosts/spkgbuild
+changelog "server/blacklist-hosts/spkgbuild" "Upgraded from version 3.14.137 to version 3.14.139"
+sed -i -e "s/version=2024.12.0/version=2024.12.1/g" server/homeassistant/spkgbuild
+changelog "server/homeassistant/spkgbuild" "Upgraded from version 2024.12.0 to version 2024.12.1"
+sed -i -e "s/version=3.9.0/version=3.9.1/g" server/postfix/spkgbuild
+changelog "server/postfix/spkgbuild" "Upgraded from version 3.9.0 to version 3.9.1"
+sed -i -e "s/version=1.1.5/version=1.1.6/g" xorg/xkbevd/spkgbuild
+changelog "xorg/xkbevd/spkgbuild" "Upgraded from version 1.1.5 to version 1.1.6"
