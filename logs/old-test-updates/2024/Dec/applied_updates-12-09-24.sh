18ffc1172cd534ed9461aeacf00841fc7bc54fc0
Voncloft <voncloft@gmail.com>
2024-12-09 04:33:38 -0500

    Minor Updates

---
+++ logs/old-test-updates/2024/Dec/applied_updates-12-09-24.sh
@@ -0,0 +1,8 @@
+sed -i -e "s/version=3.12.1/version=3.12.2/g" extra/flexget/spkgbuild
+changelog "extra/flexget/spkgbuild" "Upgraded from version 3.12.1 to version 3.12.2"
+sed -i -e "s/version=3.5/version=3.6/g" nonfree/pandoc-bin/spkgbuild
+changelog "nonfree/pandoc-bin/spkgbuild" "Upgraded from version 3.5 to version 3.6"
+sed -i -e "s/version=3.47.1.0/version=3.47.2.0/g" python/python-apsw/spkgbuild
+changelog "python/python-apsw/spkgbuild" "Upgraded from version 3.47.1.0 to version 3.47.2.0"
+sed -i -e "s/version=3.3.5/version=3.3.6/g" python/python-astroid/spkgbuild
+changelog "python/python-astroid/spkgbuild" "Upgraded from version 3.3.5 to version 3.3.6"
