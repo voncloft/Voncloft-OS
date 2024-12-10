7136cd4e41de634e3a82eaf6c908f5d789ca001b
Voncloft <voncloft@gmail.com>
2024-12-10 05:16:48 -0500

    Minor Updates

---
+++ logs/old-test-updates/2024/Dec/applied_updates-12-10-24.sh
@@ -0,0 +1,22 @@
+sed -i -e "s/version=0.139.3/version=0.139.4/g" core/hugo/spkgbuild
+changelog "core/hugo/spkgbuild" "Upgraded from version 0.139.3 to version 0.139.4"
+sed -i -e "s/version=6.12.3/version=6.12.4/g" core/linux-api-headers/spkgbuild
+changelog "core/linux-api-headers/spkgbuild" "Upgraded from version 6.12.3 to version 6.12.4"
+sed -i -e "s/version=6.12.3/version=6.12.4/g" core/linux-desktop/spkgbuild
+changelog "core/linux-desktop/spkgbuild" "Upgraded from version 6.12.3 to version 6.12.4"
+sed -i -e "s/version=6.12.3/version=6.12.4/g" core/linux-router/spkgbuild
+changelog "core/linux-router/spkgbuild" "Upgraded from version 6.12.3 to version 6.12.4"
+sed -i -e "s/version=2.5.2/version=2.5.3/g" core/openjpeg2/spkgbuild
+changelog "core/openjpeg2/spkgbuild" "Upgraded from version 2.5.2 to version 2.5.3"
+sed -i -e "s/version=3.12.2/version=3.12.3/g" extra/flexget/spkgbuild
+changelog "extra/flexget/spkgbuild" "Upgraded from version 3.12.2 to version 3.12.3"
+sed -i -e "s/version=1.14.4/version=1.14.5/g" networking/subversion/spkgbuild
+changelog "networking/subversion/spkgbuild" "Upgraded from version 1.14.4 to version 1.14.5"
+sed -i -e "s/version=134.0b7/version=134.0b8/g" nonfree/firefox/spkgbuild
+changelog "nonfree/firefox/spkgbuild" "Upgraded from version 134.0b7 to version 134.0b8"
+sed -i -e "s/version=1.20.2/version=1.21.0/g" python/python-cfn-lint/spkgbuild
+changelog "python/python-cfn-lint/spkgbuild" "Upgraded from version 1.20.2 to version 1.21.0"
+sed -i -e "s/version=2.3.1/version=2.3.2/g" python/python-ciso8601/spkgbuild
+changelog "python/python-ciso8601/spkgbuild" "Upgraded from version 2.3.1 to version 2.3.2"
+sed -i -e "s/version=6.122.1/version=6.122.3/g" python/python-hypothesis/spkgbuild
+changelog "python/python-hypothesis/spkgbuild" "Upgraded from version 6.122.1 to version 6.122.3"
