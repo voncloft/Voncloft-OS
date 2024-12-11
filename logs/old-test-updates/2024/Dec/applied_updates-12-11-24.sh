bcda12c70d3edd593bdbeb95e4ce4214639f459e
Voncloft <voncloft@gmail.com>
2024-12-11 03:58:20 -0500

    Minor Updates

---
+++ logs/old-test-updates/2024/Dec/applied_updates-12-11-24.sh
@@ -0,0 +1,16 @@
+sed -i -e "s/version=5.34.2/version=5.34.3/g" core/monit/spkgbuild
+changelog "core/monit/spkgbuild" "Upgraded from version 5.34.2 to version 5.34.3"
+sed -i -e "s/version=1.35.76/version=1.35.77/g" python/python-boto3/spkgbuild
+changelog "python/python-boto3/spkgbuild" "Upgraded from version 1.35.76 to version 1.35.77"
+sed -i -e "s/version=1.35.76/version=1.35.77/g" python/python-botocore/spkgbuild
+changelog "python/python-botocore/spkgbuild" "Upgraded from version 1.35.76 to version 1.35.77"
+sed -i -e "s/version=1.21.0/version=1.22.0/g" python/python-cfn-lint/spkgbuild
+changelog "python/python-cfn-lint/spkgbuild" "Upgraded from version 1.21.0 to version 1.22.0"
+sed -i -e "s/version=5.0.4/version=5.0.5/g" python/python-internetarchive/spkgbuild
+changelog "python/python-internetarchive/spkgbuild" "Upgraded from version 5.0.4 to version 5.0.5"
+sed -i -e "s/version=0.14.0/version=0.14.1/g" python/python-pytest-subtests/spkgbuild
+changelog "python/python-pytest-subtests/spkgbuild" "Upgraded from version 0.14.0 to version 0.14.1"
+sed -i -e "s/version=0.5.2/version=0.5.3/g" python/python-sqlparse/spkgbuild
+changelog "python/python-sqlparse/spkgbuild" "Upgraded from version 0.5.2 to version 0.5.3"
+sed -i -e "s/version=2024.12.1/version=2024.12.2/g" server/homeassistant/spkgbuild
+changelog "server/homeassistant/spkgbuild" "Upgraded from version 2024.12.1 to version 2024.12.2"
