1b55ab41e0c6a27c954dc721c3abd86eefffe839
Voncloft <voncloft@gmail.com>
2024-12-12 07:08:58 -0500

    Minor Updates

---
+++ logs/old-test-updates/2024/Dec/applied_updates-12-12-24.sh
@@ -0,0 +1,30 @@
+sed -i -e "s/version=6.4.1/version=6.4.2/g" cinnamon/cinnamon-settings-daemon/spkgbuild
+changelog "cinnamon/cinnamon-settings-daemon/spkgbuild" "Upgraded from version 6.4.1 to version 6.4.2"
+sed -i -e "s/version=8.11.0/version=8.11.1/g" core/curl/spkgbuild
+changelog "core/curl/spkgbuild" "Upgraded from version 8.11.0 to version 8.11.1"
+sed -i -e "s/version=1.15.90/version=1.15.92/g" core/dbus/spkgbuild
+changelog "core/dbus/spkgbuild" "Upgraded from version 1.15.90 to version 1.15.92"
+sed -i -e "s/version=1.8.0.1/version=1.8.0.2/g" core/socat/spkgbuild
+changelog "core/socat/spkgbuild" "Upgraded from version 1.8.0.1 to version 1.8.0.2"
+sed -i -e "s/version=3.12.3/version=3.12.4/g" extra/flexget/spkgbuild
+changelog "extra/flexget/spkgbuild" "Upgraded from version 3.12.3 to version 3.12.4"
+sed -i -e "s/version=5.0.3/version=6.0.0/g" libs/libnfs/spkgbuild
+changelog "libs/libnfs/spkgbuild" "Upgraded from version 5.0.3 to version 6.0.0"
+sed -i -e "s/version=9.21.2/version=9.21.3/g" networking/bind/spkgbuild
+changelog "networking/bind/spkgbuild" "Upgraded from version 9.21.2 to version 9.21.3"
+sed -i -e "s/version=134.0b8/version=134.0b9/g" nonfree/firefox/spkgbuild
+changelog "nonfree/firefox/spkgbuild" "Upgraded from version 134.0b8 to version 134.0b9"
+sed -i -e "s/version=134.0b3/version=134.0b4/g" nonfree/thunderbird/spkgbuild
+changelog "nonfree/thunderbird/spkgbuild" "Upgraded from version 134.0b3 to version 134.0b4"
+sed -i -e "s/version=1.35.77/version=1.35.78/g" python/python-boto3/spkgbuild
+changelog "python/python-boto3/spkgbuild" "Upgraded from version 1.35.77 to version 1.35.78"
+sed -i -e "s/version=1.35.77/version=1.35.78/g" python/python-botocore/spkgbuild
+changelog "python/python-botocore/spkgbuild" "Upgraded from version 1.35.77 to version 1.35.78"
+sed -i -e "s/version=4.55.2/version=4.55.3/g" python/python-fonttools/spkgbuild
+changelog "python/python-fonttools/spkgbuild" "Upgraded from version 4.55.2 to version 4.55.3"
+sed -i -e "s/version=2.36.1/version=2.37.0/g" python/python-google-auth/spkgbuild
+changelog "python/python-google-auth/spkgbuild" "Upgraded from version 2.36.1 to version 2.37.0"
+sed -i -e "s/version=17.0.0/version=17.1.0/g" python/python-oslo-db/spkgbuild
+changelog "python/python-oslo-db/spkgbuild" "Upgraded from version 17.0.0 to version 17.1.0"
+sed -i -e "s/version=1.41.2.9200/version=1.41.3.9292/g" server/plex-media-server/spkgbuild
+changelog "server/plex-media-server/spkgbuild" "Upgraded from version 1.41.2.9200 to version 1.41.3.9292"
