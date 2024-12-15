980c9a51660396935a3d83e49a7f47d8fda8040e
Voncloft <voncloft@gmail.com>
2024-12-15 11:49:36 -0500

    Minor Updates

---
+++ logs/old-test-updates/2024/Dec/applied_updates-12-15-24.sh
@@ -0,0 +1,212 @@
+sed -i -e "s/version=0.139.4/version=0.139.5/g" core/hugo/spkgbuild
+changelog "core/hugo/spkgbuild" "Upgraded from version 0.139.4 to version 0.139.5"
+sed -i -e "s/version=6.12.4/version=6.12.5/g" core/linux-api-headers/spkgbuild
+changelog "core/linux-api-headers/spkgbuild" "Upgraded from version 6.12.4 to version 6.12.5"
+sed -i -e "s/version=6.12.4/version=6.12.5/g" core/linux-desktop/spkgbuild
+changelog "core/linux-desktop/spkgbuild" "Upgraded from version 6.12.4 to version 6.12.5"
+sed -i -e "s/version=6.12.4/version=6.12.5/g" core/linux-router/spkgbuild
+changelog "core/linux-router/spkgbuild" "Upgraded from version 6.12.4 to version 6.12.5"
+sed -i -e "s/version=6.8.0/version=6.9.0/g" extra/extra-cmake-modules/spkgbuild
+changelog "extra/extra-cmake-modules/spkgbuild" "Upgraded from version 6.8.0 to version 6.9.0"
+sed -i -e "s/version=3.13.0/version=3.13.2/g" extra/flexget/spkgbuild
+changelog "extra/flexget/spkgbuild" "Upgraded from version 3.13.0 to version 3.13.2"
+sed -i -e "s/version=6.8.0/version=6.9.0/g" kde-apps/khtml/spkgbuild
+changelog "kde-apps/khtml/spkgbuild" "Upgraded from version 6.8.0 to version 6.9.0"
+sed -i -e "s/version=6.8.0/version=6.9.0/g" kf/attica/spkgbuild
+changelog "kf/attica/spkgbuild" "Upgraded from version 6.8.0 to version 6.9.0"
+sed -i -e "s/version=6.8.0/version=6.9.0/g" kf/baloo/spkgbuild
+changelog "kf/baloo/spkgbuild" "Upgraded from version 6.8.0 to version 6.9.0"
+sed -i -e "s/version=6.8.0/version=6.9.0/g" kf/breeze-icons/spkgbuild
+changelog "kf/breeze-icons/spkgbuild" "Upgraded from version 6.8.0 to version 6.9.0"
+sed -i -e "s/version=6.8.0/version=6.9.0/g" kf/frameworkintegration/spkgbuild
+changelog "kf/frameworkintegration/spkgbuild" "Upgraded from version 6.8.0 to version 6.9.0"
+sed -i -e "s/version=6.8.0/version=6.9.0/g" kf/kactivities/spkgbuild
+changelog "kf/kactivities/spkgbuild" "Upgraded from version 6.8.0 to version 6.9.0"
+sed -i -e "s/version=6.8.0/version=6.9.0/g" kf/kactivities-stats/spkgbuild
+changelog "kf/kactivities-stats/spkgbuild" "Upgraded from version 6.8.0 to version 6.9.0"
+sed -i -e "s/version=6.8.0/version=6.9.0/g" kf/kapidox/spkgbuild
+changelog "kf/kapidox/spkgbuild" "Upgraded from version 6.8.0 to version 6.9.0"
+sed -i -e "s/version=6.8.0/version=6.9.0/g" kf/karchive/spkgbuild
+changelog "kf/karchive/spkgbuild" "Upgraded from version 6.8.0 to version 6.9.0"
+sed -i -e "s/version=6.8.0/version=6.9.0/g" kf/kauth/spkgbuild
+changelog "kf/kauth/spkgbuild" "Upgraded from version 6.8.0 to version 6.9.0"
+sed -i -e "s/version=6.8.0/version=6.9.0/g" kf/kbookmarks/spkgbuild
+changelog "kf/kbookmarks/spkgbuild" "Upgraded from version 6.8.0 to version 6.9.0"
+sed -i -e "s/version=6.8.0/version=6.9.0/g" kf/kcalendarcore/spkgbuild
+changelog "kf/kcalendarcore/spkgbuild" "Upgraded from version 6.8.0 to version 6.9.0"
+sed -i -e "s/version=6.8.0/version=6.9.0/g" kf/kcmutils/spkgbuild
+changelog "kf/kcmutils/spkgbuild" "Upgraded from version 6.8.0 to version 6.9.0"
+sed -i -e "s/version=6.8.0/version=6.9.0/g" kf/kcodecs/spkgbuild
+changelog "kf/kcodecs/spkgbuild" "Upgraded from version 6.8.0 to version 6.9.0"
+sed -i -e "s/version=6.8.0/version=6.9.0/g" kf/kcompletion/spkgbuild
+changelog "kf/kcompletion/spkgbuild" "Upgraded from version 6.8.0 to version 6.9.0"
+sed -i -e "s/version=6.8.0/version=6.9.0/g" kf/kconfig/spkgbuild
+changelog "kf/kconfig/spkgbuild" "Upgraded from version 6.8.0 to version 6.9.0"
+sed -i -e "s/version=6.8.0/version=6.9.0/g" kf/kconfigwidgets/spkgbuild
+changelog "kf/kconfigwidgets/spkgbuild" "Upgraded from version 6.8.0 to version 6.9.0"
+sed -i -e "s/version=6.8.0/version=6.9.0/g" kf/kcontacts/spkgbuild
+changelog "kf/kcontacts/spkgbuild" "Upgraded from version 6.8.0 to version 6.9.0"
+sed -i -e "s/version=6.8.0/version=6.9.0/g" kf/kcoreaddons/spkgbuild
+changelog "kf/kcoreaddons/spkgbuild" "Upgraded from version 6.8.0 to version 6.9.0"
+sed -i -e "s/version=6.8.0/version=6.9.0/g" kf/kcrash/spkgbuild
+changelog "kf/kcrash/spkgbuild" "Upgraded from version 6.8.0 to version 6.9.0"
+sed -i -e "s/version=6.8.0/version=6.9.0/g" kf/kdav/spkgbuild
+changelog "kf/kdav/spkgbuild" "Upgraded from version 6.8.0 to version 6.9.0"
+sed -i -e "s/version=6.8.0/version=6.9.0/g" kf/kdbusaddons/spkgbuild
+changelog "kf/kdbusaddons/spkgbuild" "Upgraded from version 6.8.0 to version 6.9.0"
+sed -i -e "s/version=6.8.0/version=6.9.0/g" kf/kdeclarative/spkgbuild
+changelog "kf/kdeclarative/spkgbuild" "Upgraded from version 6.8.0 to version 6.9.0"
+sed -i -e "s/version=6.8.0/version=6.9.0/g" kf/kded/spkgbuild
+changelog "kf/kded/spkgbuild" "Upgraded from version 6.8.0 to version 6.9.0"
+sed -i -e "s/version=6.8.0/version=6.9.0/g" kf/kdelibs4support/spkgbuild
+changelog "kf/kdelibs4support/spkgbuild" "Upgraded from version 6.8.0 to version 6.9.0"
+sed -i -e "s/version=6.8.0/version=6.9.0/g" kf/kdesignerplugin/spkgbuild
+changelog "kf/kdesignerplugin/spkgbuild" "Upgraded from version 6.8.0 to version 6.9.0"
+sed -i -e "s/version=6.8.0/version=6.9.0/g" kf/kdesu/spkgbuild
+changelog "kf/kdesu/spkgbuild" "Upgraded from version 6.8.0 to version 6.9.0"
+sed -i -e "s/version=6.8.0/version=6.9.0/g" kf/kdewebkit/spkgbuild
+changelog "kf/kdewebkit/spkgbuild" "Upgraded from version 6.8.0 to version 6.9.0"
+sed -i -e "s/version=6.8.0/version=6.9.0/g" kf/kdnssd/spkgbuild
+changelog "kf/kdnssd/spkgbuild" "Upgraded from version 6.8.0 to version 6.9.0"
+sed -i -e "s/version=6.8.0/version=6.9.0/g" kf/kdoctools/spkgbuild
+changelog "kf/kdoctools/spkgbuild" "Upgraded from version 6.8.0 to version 6.9.0"
+sed -i -e "s/version=6.8.0/version=6.9.0/g" kf/kemoticons/spkgbuild
+changelog "kf/kemoticons/spkgbuild" "Upgraded from version 6.8.0 to version 6.9.0"
+sed -i -e "s/version=6.8.0/version=6.9.0/g" kf/kf-depends/spkgbuild
+changelog "kf/kf-depends/spkgbuild" "Upgraded from version 6.8.0 to version 6.9.0"
+sed -i -e "s/version=6.8.0/version=6.9.0/g" kf/kf-programs/spkgbuild
+changelog "kf/kf-programs/spkgbuild" "Upgraded from version 6.8.0 to version 6.9.0"
+sed -i -e "s/version=6.8.0/version=6.9.0/g" kf/kfilemetadata/spkgbuild
+changelog "kf/kfilemetadata/spkgbuild" "Upgraded from version 6.8.0 to version 6.9.0"
+sed -i -e "s/version=6.8.0/version=6.9.0/g" kf/kglobalaccel/spkgbuild
+changelog "kf/kglobalaccel/spkgbuild" "Upgraded from version 6.8.0 to version 6.9.0"
+sed -i -e "s/version=6.8.0/version=6.9.0/g" kf/kguiaddons/spkgbuild
+changelog "kf/kguiaddons/spkgbuild" "Upgraded from version 6.8.0 to version 6.9.0"
+sed -i -e "s/version=6.8.0/version=6.9.0/g" kf/kholidays/spkgbuild
+changelog "kf/kholidays/spkgbuild" "Upgraded from version 6.8.0 to version 6.9.0"
+sed -i -e "s/version=6.8.0/version=6.9.0/g" kf/ki18n/spkgbuild
+changelog "kf/ki18n/spkgbuild" "Upgraded from version 6.8.0 to version 6.9.0"
+sed -i -e "s/version=6.8.0/version=6.9.0/g" kf/kiconthemes/spkgbuild
+changelog "kf/kiconthemes/spkgbuild" "Upgraded from version 6.8.0 to version 6.9.0"
+sed -i -e "s/version=6.8.0/version=6.9.0/g" kf/kidletime/spkgbuild
+changelog "kf/kidletime/spkgbuild" "Upgraded from version 6.8.0 to version 6.9.0"
+sed -i -e "s/version=6.8.0/version=6.9.0/g" kf/kimageformats/spkgbuild
+changelog "kf/kimageformats/spkgbuild" "Upgraded from version 6.8.0 to version 6.9.0"
+sed -i -e "s/version=6.8.0/version=6.9.0/g" kf/kinit/spkgbuild
+changelog "kf/kinit/spkgbuild" "Upgraded from version 6.8.0 to version 6.9.0"
+sed -i -e "s/version=6.8.0/version=6.9.0/g" kf/kio/spkgbuild
+changelog "kf/kio/spkgbuild" "Upgraded from version 6.8.0 to version 6.9.0"
+sed -i -e "s/version=6.8.0/version=6.9.0/g" kf/kirigami2/spkgbuild
+changelog "kf/kirigami2/spkgbuild" "Upgraded from version 6.8.0 to version 6.9.0"
+sed -i -e "s/version=6.8.0/version=6.9.0/g" kf/kitemmodels/spkgbuild
+changelog "kf/kitemmodels/spkgbuild" "Upgraded from version 6.8.0 to version 6.9.0"
+sed -i -e "s/version=6.8.0/version=6.9.0/g" kf/kitemviews/spkgbuild
+changelog "kf/kitemviews/spkgbuild" "Upgraded from version 6.8.0 to version 6.9.0"
+sed -i -e "s/version=6.8.0/version=6.9.0/g" kf/kjobwidgets/spkgbuild
+changelog "kf/kjobwidgets/spkgbuild" "Upgraded from version 6.8.0 to version 6.9.0"
+sed -i -e "s/version=6.8.0/version=6.9.0/g" kf/kjs/spkgbuild
+changelog "kf/kjs/spkgbuild" "Upgraded from version 6.8.0 to version 6.9.0"
+sed -i -e "s/version=6.8.0/version=6.9.0/g" kf/kjsembed/spkgbuild
+changelog "kf/kjsembed/spkgbuild" "Upgraded from version 6.8.0 to version 6.9.0"
+sed -i -e "s/version=6.8.0/version=6.9.0/g" kf/kmediaplayer/spkgbuild
+changelog "kf/kmediaplayer/spkgbuild" "Upgraded from version 6.8.0 to version 6.9.0"
+sed -i -e "s/version=6.8.0/version=6.9.0/g" kf/knewstuff/spkgbuild
+changelog "kf/knewstuff/spkgbuild" "Upgraded from version 6.8.0 to version 6.9.0"
+sed -i -e "s/version=6.8.0/version=6.9.0/g" kf/knotifications/spkgbuild
+changelog "kf/knotifications/spkgbuild" "Upgraded from version 6.8.0 to version 6.9.0"
+sed -i -e "s/version=6.8.0/version=6.9.0/g" kf/knotifyconfig/spkgbuild
+changelog "kf/knotifyconfig/spkgbuild" "Upgraded from version 6.8.0 to version 6.9.0"
+sed -i -e "s/version=6.8.0/version=6.9.0/g" kf/kpackage/spkgbuild
+changelog "kf/kpackage/spkgbuild" "Upgraded from version 6.8.0 to version 6.9.0"
+sed -i -e "s/version=6.8.0/version=6.9.0/g" kf/kparts/spkgbuild
+changelog "kf/kparts/spkgbuild" "Upgraded from version 6.8.0 to version 6.9.0"
+sed -i -e "s/version=6.8.0/version=6.9.0/g" kf/kpeople/spkgbuild
+changelog "kf/kpeople/spkgbuild" "Upgraded from version 6.8.0 to version 6.9.0"
+sed -i -e "s/version=6.8.0/version=6.9.0/g" kf/kplotting/spkgbuild
+changelog "kf/kplotting/spkgbuild" "Upgraded from version 6.8.0 to version 6.9.0"
+sed -i -e "s/version=6.8.0/version=6.9.0/g" kf/kpty/spkgbuild
+changelog "kf/kpty/spkgbuild" "Upgraded from version 6.8.0 to version 6.9.0"
+sed -i -e "s/version=6.8.0/version=6.9.0/g" kf/kquickcharts/spkgbuild
+changelog "kf/kquickcharts/spkgbuild" "Upgraded from version 6.8.0 to version 6.9.0"
+sed -i -e "s/version=6.8.0/version=6.9.0/g" kf/kross/spkgbuild
+changelog "kf/kross/spkgbuild" "Upgraded from version 6.8.0 to version 6.9.0"
+sed -i -e "s/version=6.8.0/version=6.9.0/g" kf/krunner/spkgbuild
+changelog "kf/krunner/spkgbuild" "Upgraded from version 6.8.0 to version 6.9.0"
+sed -i -e "s/version=6.8.0/version=6.9.0/g" kf/kservice/spkgbuild
+changelog "kf/kservice/spkgbuild" "Upgraded from version 6.8.0 to version 6.9.0"
+sed -i -e "s/version=6.8.0/version=6.9.0/g" kf/ktexteditor/spkgbuild
+changelog "kf/ktexteditor/spkgbuild" "Upgraded from version 6.8.0 to version 6.9.0"
+sed -i -e "s/version=6.8.0/version=6.9.0/g" kf/ktextwidgets/spkgbuild
+changelog "kf/ktextwidgets/spkgbuild" "Upgraded from version 6.8.0 to version 6.9.0"
+sed -i -e "s/version=6.8.0/version=6.9.0/g" kf/kunitconversion/spkgbuild
+changelog "kf/kunitconversion/spkgbuild" "Upgraded from version 6.8.0 to version 6.9.0"
+sed -i -e "s/version=6.8.0/version=6.9.0/g" kf/kwallet/spkgbuild
+changelog "kf/kwallet/spkgbuild" "Upgraded from version 6.8.0 to version 6.9.0"
+sed -i -e "s/version=6.8.0/version=6.9.0/g" kf/kwayland/spkgbuild
+changelog "kf/kwayland/spkgbuild" "Upgraded from version 6.8.0 to version 6.9.0"
+sed -i -e "s/version=6.8.0/version=6.9.0/g" kf/kwidgetsaddons/spkgbuild
+changelog "kf/kwidgetsaddons/spkgbuild" "Upgraded from version 6.8.0 to version 6.9.0"
+sed -i -e "s/version=6.8.0/version=6.9.0/g" kf/kwindowsystem/spkgbuild
+changelog "kf/kwindowsystem/spkgbuild" "Upgraded from version 6.8.0 to version 6.9.0"
+sed -i -e "s/version=6.8.0/version=6.9.0/g" kf/kxmlgui/spkgbuild
+changelog "kf/kxmlgui/spkgbuild" "Upgraded from version 6.8.0 to version 6.9.0"
+sed -i -e "s/version=6.8.0/version=6.9.0/g" kf/kxmlrpcclient/spkgbuild
+changelog "kf/kxmlrpcclient/spkgbuild" "Upgraded from version 6.8.0 to version 6.9.0"
+sed -i -e "s/version=6.8.0/version=6.9.0/g" kf/oxygen-icons5/spkgbuild
+changelog "kf/oxygen-icons5/spkgbuild" "Upgraded from version 6.8.0 to version 6.9.0"
+sed -i -e "s/version=6.8.0/version=6.9.0/g" kf/plasma-framework/spkgbuild
+changelog "kf/plasma-framework/spkgbuild" "Upgraded from version 6.8.0 to version 6.9.0"
+sed -i -e "s/version=6.8.0/version=6.9.0/g" kf/prison/spkgbuild
+changelog "kf/prison/spkgbuild" "Upgraded from version 6.8.0 to version 6.9.0"
+sed -i -e "s/version=6.8.0/version=6.9.0/g" kf/purpose/spkgbuild
+changelog "kf/purpose/spkgbuild" "Upgraded from version 6.8.0 to version 6.9.0"
+sed -i -e "s/version=6.8.0/version=6.9.0/g" kf/qqc2-desktop-style/spkgbuild
+changelog "kf/qqc2-desktop-style/spkgbuild" "Upgraded from version 6.8.0 to version 6.9.0"
+sed -i -e "s/version=6.8.0/version=6.9.0/g" kf/solid/spkgbuild
+changelog "kf/solid/spkgbuild" "Upgraded from version 6.8.0 to version 6.9.0"
+sed -i -e "s/version=6.8.0/version=6.9.0/g" kf/sonnet/spkgbuild
+changelog "kf/sonnet/spkgbuild" "Upgraded from version 6.8.0 to version 6.9.0"
+sed -i -e "s/version=6.8.0/version=6.9.0/g" kf/syndication/spkgbuild
+changelog "kf/syndication/spkgbuild" "Upgraded from version 6.8.0 to version 6.9.0"
+sed -i -e "s/version=6.8.0/version=6.9.0/g" kf/syntax-highlighting/spkgbuild
+changelog "kf/syntax-highlighting/spkgbuild" "Upgraded from version 6.8.0 to version 6.9.0"
+sed -i -e "s/version=6.8.0/version=6.9.0/g" kf/threadweaver/spkgbuild
+changelog "kf/threadweaver/spkgbuild" "Upgraded from version 6.8.0 to version 6.9.0"
+sed -i -e "s/version=1.32.9/version=1.32.10/g" media/mpg123/spkgbuild
+changelog "media/mpg123/spkgbuild" "Upgraded from version 1.32.9 to version 1.32.10"
+sed -i -e "s/version=1.34.3/version=1.34.4/g" libs/c-ares/spkgbuild
+changelog "libs/c-ares/spkgbuild" "Upgraded from version 1.34.3 to version 1.34.4"
+sed -i -e "s/version=6.0.0/version=6.0.2/g" libs/libnfs/spkgbuild
+changelog "libs/libnfs/spkgbuild" "Upgraded from version 6.0.0 to version 6.0.2"
+sed -i -e "s/version=0.1.2/version=0.1.3/g" libs/libxcvt/spkgbuild
+changelog "libs/libxcvt/spkgbuild" "Upgraded from version 0.1.2 to version 0.1.3"
+sed -i -e "s/version=1.3.2/version=1.3.3/g" libs/libxshmfence/spkgbuild
+changelog "libs/libxshmfence/spkgbuild" "Upgraded from version 1.3.2 to version 1.3.3"
+sed -i -e "s/version=2.3.4/version=2.3.5/g" libs/muparser/spkgbuild
+changelog "libs/muparser/spkgbuild" "Upgraded from version 2.3.4 to version 2.3.5"
+sed -i -e "s/version=2024.12.06/version=2024.12.13/g" networking/yt-dlp/spkgbuild
+changelog "networking/yt-dlp/spkgbuild" "Upgraded from version 2024.12.06 to version 2024.12.13"
+sed -i -e "s/version=134.0b9/version=134.0b10/g" nonfree/firefox/spkgbuild
+changelog "nonfree/firefox/spkgbuild" "Upgraded from version 134.0b9 to version 134.0b10"
+sed -i -e "s/version=1.35.79/version=1.35.81/g" python/python-boto3/spkgbuild
+changelog "python/python-boto3/spkgbuild" "Upgraded from version 1.35.79 to version 1.35.81"
+sed -i -e "s/version=1.35.80/version=1.35.81/g" python/python-botocore/spkgbuild
+changelog "python/python-botocore/spkgbuild" "Upgraded from version 1.35.80 to version 1.35.81"
+sed -i -e "s/version=1.1.0/version=2.0.0/g" python/python-janus/spkgbuild
+changelog "python/python-janus/spkgbuild" "Upgraded from version 1.1.0 to version 2.0.0"
+sed -i -e "s/version=4.0.0/version=4.0.1/g" python/python-jsonpickle/spkgbuild
+changelog "python/python-jsonpickle/spkgbuild" "Upgraded from version 4.0.0 to version 4.0.1"
+sed -i -e "s/version=3.35.0/version=3.36.0/g" python/python-openmdao/spkgbuild
+changelog "python/python-openmdao/spkgbuild" "Upgraded from version 3.35.0 to version 3.36.0"
+sed -i -e "s/version=8.13.51/version=8.13.52/g" python/python-phonenumbers/spkgbuild
+changelog "python/python-phonenumbers/spkgbuild" "Upgraded from version 8.13.51 to version 8.13.52"
+sed -i -e "s/version=0.24.0/version=0.25.0/g" python/python-pytest-asyncio/spkgbuild
+changelog "python/python-pytest-asyncio/spkgbuild" "Upgraded from version 0.24.0 to version 0.25.0"
+sed -i -e "s/version=0.41.3/version=0.42.0/g" python/python-starlette/spkgbuild
+changelog "python/python-starlette/spkgbuild" "Upgraded from version 0.41.3 to version 0.42.0"
+sed -i -e "s/version=6.6/version=6.6.1/g" python/python-zope-testrunner/spkgbuild
+changelog "python/python-zope-testrunner/spkgbuild" "Upgraded from version 6.6 to version 6.6.1"
+sed -i -e "s/version=1.37.0/version=1.37.1/g" ruby-gems/ruby-tins/spkgbuild
+changelog "ruby-gems/ruby-tins/spkgbuild" "Upgraded from version 1.37.0 to version 1.37.1"
+sed -i -e "s/version=2024.12.2/version=2024.12.3/g" server/homeassistant/spkgbuild
+changelog "server/homeassistant/spkgbuild" "Upgraded from version 2024.12.2 to version 2024.12.3"
