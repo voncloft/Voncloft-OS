795956f42c5ed5462bc5fdc0897e3f7258cc027a
Voncloft <voncloft@gmail.com>
2024-12-16 03:58:51 -0500

    Minor Updates

---
+++ logs/old-test-updates/2024/Dec/applied_updates-12-16-24.sh
@@ -0,0 +1,36 @@
+sed -i -e "s/version=3.3.1/version=3.4.0/g" core/qutebrowser/spkgbuild
+changelog "core/qutebrowser/spkgbuild" "Upgraded from version 3.3.1 to version 3.4.0"
+sed -i -e "s/version=4.4.4/version=4.5.0/g" core/weechat/spkgbuild
+changelog "core/weechat/spkgbuild" "Upgraded from version 4.4.4 to version 4.5.0"
+sed -i -e "s/version=4.19.7/version=4.20.0/g" libs/libxfce4ui/spkgbuild
+changelog "libs/libxfce4ui/spkgbuild" "Upgraded from version 4.19.7 to version 4.20.0"
+sed -i -e "s/version=4.19.5/version=4.20.0/g" libs/libxfce4util/spkgbuild
+changelog "libs/libxfce4util/spkgbuild" "Upgraded from version 4.19.5 to version 4.20.0"
+sed -i -e "s/version=3.3.1/version=3.3.2/g" server/radicale/spkgbuild
+changelog "server/radicale/spkgbuild" "Upgraded from version 3.3.1 to version 3.3.2"
+sed -i -e "s/version=4.19.2/version=4.20.0/g" xfce/exo/spkgbuild
+changelog "xfce/exo/spkgbuild" "Upgraded from version 4.19.2 to version 4.20.0"
+sed -i -e "s/version=4.19.3/version=4.20.0/g" xfce/garcon/spkgbuild
+changelog "xfce/garcon/spkgbuild" "Upgraded from version 4.19.3 to version 4.20.0"
+sed -i -e "s/version=4.19.5/version=4.20.0/g" xfce/thunar/spkgbuild
+changelog "xfce/thunar/spkgbuild" "Upgraded from version 4.19.5 to version 4.20.0"
+sed -i -e "s/version=4.19.1/version=4.20.0/g" xfce/thunar-volman/spkgbuild
+changelog "xfce/thunar-volman/spkgbuild" "Upgraded from version 4.19.1 to version 4.20.0"
+sed -i -e "s/version=4.19.3/version=4.20.0/g" xfce/tumbler/spkgbuild
+changelog "xfce/tumbler/spkgbuild" "Upgraded from version 4.19.3 to version 4.20.0"
+sed -i -e "s/version=4.19.4/version=4.20.0/g" xfce/xfce4-appfinder/spkgbuild
+changelog "xfce/xfce4-appfinder/spkgbuild" "Upgraded from version 4.19.4 to version 4.20.0"
+sed -i -e "s/version=4.19.7/version=4.20.0/g" xfce/xfce4-panel/spkgbuild
+changelog "xfce/xfce4-panel/spkgbuild" "Upgraded from version 4.19.7 to version 4.20.0"
+sed -i -e "s/version=4.19.5/version=4.20.0/g" xfce/xfce4-power-manager/spkgbuild
+changelog "xfce/xfce4-power-manager/spkgbuild" "Upgraded from version 4.19.5 to version 4.20.0"
+sed -i -e "s/version=4.19.4/version=4.20.0/g" xfce/xfce4-session/spkgbuild
+changelog "xfce/xfce4-session/spkgbuild" "Upgraded from version 4.19.4 to version 4.20.0"
+sed -i -e "s/version=4.19.4/version=4.20.0/g" xfce/xfce4-settings/spkgbuild
+changelog "xfce/xfce4-settings/spkgbuild" "Upgraded from version 4.19.4 to version 4.20.0"
+sed -i -e "s/version=4.19.5/version=4.20.0/g" xfce/xfconf/spkgbuild
+changelog "xfce/xfconf/spkgbuild" "Upgraded from version 4.19.5 to version 4.20.0"
+sed -i -e "s/version=4.19.7/version=4.20.0/g" xfce/xfdesktop/spkgbuild
+changelog "xfce/xfdesktop/spkgbuild" "Upgraded from version 4.19.7 to version 4.20.0"
+sed -i -e "s/version=4.19.1/version=4.20.0/g" xfce/xfwm4/spkgbuild
+changelog "xfce/xfwm4/spkgbuild" "Upgraded from version 4.19.1 to version 4.20.0"
