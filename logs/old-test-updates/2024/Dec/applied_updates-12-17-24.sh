b235bb2bc0831bc938fd2b7802dbf2048b28ace1
Voncloft <voncloft@gmail.com>
2024-12-17 07:43:19 -0500

    Minor Updates

---
+++ logs/old-test-updates/2024/Dec/applied_updates-12-17-24.sh
@@ -0,0 +1,20 @@
+sed -i -e "s/version=1.15.92/version=1.16.0/g" core/dbus/spkgbuild
+changelog "core/dbus/spkgbuild" "Upgraded from version 1.15.92 to version 1.16.0"
+sed -i -e "s/version=5.9/version=5.10/g" extra/gnucash/spkgbuild
+changelog "extra/gnucash/spkgbuild" "Upgraded from version 5.9 to version 5.10"
+sed -i -e "s/version=8.16.0/version=8.17.0/g" python/python-elasticsearch/spkgbuild
+changelog "python/python-elasticsearch/spkgbuild" "Upgraded from version 8.16.0 to version 8.17.0"
+sed -i -e "s/version=4.1.0/version=4.2.0/g" python/python-openstacksdk/spkgbuild
+changelog "python/python-openstacksdk/spkgbuild" "Upgraded from version 4.1.0 to version 4.2.0"
+sed -i -e "s/version=2.1.1/version=2.2.0/g" python/python-param/spkgbuild
+changelog "python/python-param/spkgbuild" "Upgraded from version 2.1.1 to version 2.2.0"
+sed -i -e "s/version=1.9.1/version=1.10.0/g" python/python-parsel/spkgbuild
+changelog "python/python-parsel/spkgbuild" "Upgraded from version 1.9.1 to version 1.10.0"
+sed -i -e "s/version=2.1.2/version=2.1.3/g" python/python-pook/spkgbuild
+changelog "python/python-pook/spkgbuild" "Upgraded from version 2.1.2 to version 2.1.3"
+sed -i -e "s/version=0.0.19/version=0.0.20/g" python/python-python-multipart/spkgbuild
+changelog "python/python-python-multipart/spkgbuild" "Upgraded from version 0.0.19 to version 0.0.20"
+sed -i -e "s/version=2.5.23/version=2.6.0/g" ruby-gems/ruby-bundler/spkgbuild
+changelog "ruby-gems/ruby-bundler/spkgbuild" "Upgraded from version 2.5.23 to version 2.6.0"
+sed -i -e "s/version=3.14.140/version=3.14.142/g" server/blacklist-hosts/spkgbuild
+changelog "server/blacklist-hosts/spkgbuild" "Upgraded from version 3.14.140 to version 3.14.142"
