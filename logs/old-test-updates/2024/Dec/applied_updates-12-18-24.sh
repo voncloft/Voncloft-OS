32e6cd4c5109890f5ccfc6835338c56df97c5fa4
Voncloft <voncloft@gmail.com>
2024-12-18 07:11:17 -0500

    Minor Updates

---
+++ logs/old-test-updates/2024/Dec/applied_updates-12-18-24.sh
@@ -0,0 +1,46 @@
+sed -i -e "s/version=19.1.5/version=19.1.6/g" compilers/clang/spkgbuild
+changelog "compilers/clang/spkgbuild" "Upgraded from version 19.1.5 to version 19.1.6"
+sed -i -e "s/version=19.1.5/version=19.1.6/g" compilers/lld/spkgbuild
+changelog "compilers/lld/spkgbuild" "Upgraded from version 19.1.5 to version 19.1.6"
+sed -i -e "s/version=19.1.5/version=19.1.6/g" compilers/llvm/spkgbuild
+changelog "compilers/llvm/spkgbuild" "Upgraded from version 19.1.5 to version 19.1.6"
+sed -i -e "s/version=19.1.5/version=19.1.6/g" compilers/llvm-bak/spkgbuild
+changelog "compilers/llvm-bak/spkgbuild" "Upgraded from version 19.1.5 to version 19.1.6"
+sed -i -e "s/version=19.1.5/version=19.1.6/g" compilers/llvm-old-2023-06-07/spkgbuild
+changelog "compilers/llvm-old-2023-06-07/spkgbuild" "Upgraded from version 19.1.5 to version 19.1.6"
+sed -i -e "s/version=1.6.0/version=1.6.1/g" compilers/meson/spkgbuild
+changelog "compilers/meson/spkgbuild" "Upgraded from version 1.6.0 to version 1.6.1"
+sed -i -e "s/version=1.12.0/version=1.12.1/g" core/dunst/spkgbuild
+changelog "core/dunst/spkgbuild" "Upgraded from version 1.12.0 to version 1.12.1"
+sed -i -e "s/version=0.139.5/version=0.140.0/g" core/hugo/spkgbuild
+changelog "core/hugo/spkgbuild" "Upgraded from version 0.139.5 to version 0.140.0"
+sed -i -e "s/version=24.11/version=release/g" extra/anki/spkgbuild
+changelog "extra/anki/spkgbuild" "Upgraded from version 24.11 to version release"
+sed -i -e "s/version=6.7.1/version=6.7.2/g" fonts/awesome-font/spkgbuild
+changelog "fonts/awesome-font/spkgbuild" "Upgraded from version 6.7.1 to version 6.7.2"
+sed -i -e "s/version=19.1.5/version=19.1.6/g" multilib/llvm-32/spkgbuild
+changelog "multilib/llvm-32/spkgbuild" "Upgraded from version 19.1.5 to version 19.1.6"
+sed -i -e "s/version=2.15.2/version=2.16.0/g" python/python-aiobotocore/spkgbuild
+changelog "python/python-aiobotocore/spkgbuild" "Upgraded from version 2.15.2 to version 2.16.0"
+sed -i -e "s/version=1.35.81/version=1.35.83/g" python/python-boto3/spkgbuild
+changelog "python/python-boto3/spkgbuild" "Upgraded from version 1.35.81 to version 1.35.83"
+sed -i -e "s/version=1.35.81/version=1.35.83/g" python/python-botocore/spkgbuild
+changelog "python/python-botocore/spkgbuild" "Upgraded from version 1.35.81 to version 1.35.83"
+sed -i -e "s/version=1.22.1/version=1.22.2/g" python/python-cfn-lint/spkgbuild
+changelog "python/python-cfn-lint/spkgbuild" "Upgraded from version 1.22.1 to version 1.22.2"
+sed -i -e "s/version=2024.12.0/version=2024.12.1/g" python/python-distributed/spkgbuild
+changelog "python/python-distributed/spkgbuild" "Upgraded from version 2024.12.0 to version 2024.12.1"
+sed -i -e "s/version=4.10.1/version=4.11.0/g" python/python-engineio/spkgbuild
+changelog "python/python-engineio/spkgbuild" "Upgraded from version 4.10.1 to version 4.11.0"
+sed -i -e "s/version=5.1.0/version=5.2.0/g" python/python-gitlab/spkgbuild
+changelog "python/python-gitlab/spkgbuild" "Upgraded from version 5.1.0 to version 5.2.0"
+sed -i -e "s/version=2.5.0/version=2.6.0/g" python/python-pytest-regressions/spkgbuild
+changelog "python/python-pytest-regressions/spkgbuild" "Upgraded from version 2.5.0 to version 2.6.0"
+sed -i -e "s/version=2.6.0/version=2.6.1/g" ruby-gems/ruby-bundler/spkgbuild
+changelog "ruby-gems/ruby-bundler/spkgbuild" "Upgraded from version 2.6.0 to version 2.6.1"
+sed -i -e "s/version=3.14.142/version=3.15.0/g" server/blacklist-hosts/spkgbuild
+changelog "server/blacklist-hosts/spkgbuild" "Upgraded from version 3.14.142 to version 3.15.0"
+sed -i -e "s/version=2024.12.3/version=2024.12.4/g" server/homeassistant/spkgbuild
+changelog "server/homeassistant/spkgbuild" "Upgraded from version 2024.12.3 to version 2024.12.4"
+sed -i -e "s/version=21.1.14/version=21.1.15/g" xorg/xorg-server/spkgbuild
+changelog "xorg/xorg-server/spkgbuild" "Upgraded from version 21.1.14 to version 21.1.15"
