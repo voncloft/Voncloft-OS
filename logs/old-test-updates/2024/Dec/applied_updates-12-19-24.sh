51c5e92aead406f52b49eed2b5b7b5f2e0ef23b2
Voncloft <voncloft@gmail.com>
2024-12-19 08:33:31 -0500

    Minor Updates

---
+++ logs/old-test-updates/2024/Dec/applied_updates-12-19-24.sh
@@ -0,0 +1,24 @@
+sed -i -e "s/version=3.22.0/version=3.24.0/g" core/fortune-mod/spkgbuild
+changelog "core/fortune-mod/spkgbuild" "Upgraded from version 3.22.0 to version 3.24.0"
+sed -i -e "s/version=29.1/version=29.2/g" extra/protobuf/spkgbuild
+changelog "extra/protobuf/spkgbuild" "Upgraded from version 29.1 to version 29.2"
+sed -i -e "s/version=1.35.83/version=1.35.84/g" python/python-boto3/spkgbuild
+changelog "python/python-boto3/spkgbuild" "Upgraded from version 1.35.83 to version 1.35.84"
+sed -i -e "s/version=1.35.83/version=1.35.84/g" python/python-botocore/spkgbuild
+changelog "python/python-botocore/spkgbuild" "Upgraded from version 1.35.83 to version 1.35.84"
+sed -i -e "s/version=4.11.0/version=4.11.1/g" python/python-engineio/spkgbuild
+changelog "python/python-engineio/spkgbuild" "Upgraded from version 4.11.0 to version 4.11.1"
+sed -i -e "s/version=5.4.1/version=5.5.0/g" python/python-flask-socketio/spkgbuild
+changelog "python/python-flask-socketio/spkgbuild" "Upgraded from version 5.4.1 to version 5.5.0"
+sed -i -e "s/version=2.155.0/version=2.156.0/g" python/python-google-api-python-client/spkgbuild
+changelog "python/python-google-api-python-client/spkgbuild" "Upgraded from version 2.155.0 to version 2.156.0"
+sed -i -e "s/version=2.10.3/version=2.10.4/g" python/python-pydantic/spkgbuild
+changelog "python/python-pydantic/spkgbuild" "Upgraded from version 2.10.3 to version 2.10.4"
+sed -i -e "s/version=5.11.4/version=5.12.0/g" python/python-socketio/spkgbuild
+changelog "python/python-socketio/spkgbuild" "Upgraded from version 5.11.4 to version 5.12.0"
+sed -i -e "s/version=3.11.0/version=3.12.0/g" python/python-sqlobject/spkgbuild
+changelog "python/python-sqlobject/spkgbuild" "Upgraded from version 3.11.0 to version 3.12.0"
+sed -i -e "s/version=3.15.0/version=3.15.1/g" server/blacklist-hosts/spkgbuild
+changelog "server/blacklist-hosts/spkgbuild" "Upgraded from version 3.15.0 to version 3.15.1"
+sed -i -e "s/version=1.41.3.9292/version=1.41.3.9314/g" server/plex-media-server/spkgbuild
+changelog "server/plex-media-server/spkgbuild" "Upgraded from version 1.41.3.9292 to version 1.41.3.9314"
