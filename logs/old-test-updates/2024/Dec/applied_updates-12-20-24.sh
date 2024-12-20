8458a3a726080563b39aa1d95f1fc4b047332320
Voncloft <voncloft@gmail.com>
2024-12-20 06:56:27 -0500

    Minor Updates

---
+++ logs/old-test-updates/2024/Dec/applied_updates-12-20-24.sh
@@ -0,0 +1,32 @@
+sed -i -e "s/version=6.4.2/version=6.4.3/g" cinnamon/cinnamon/spkgbuild
+changelog "cinnamon/cinnamon/spkgbuild" "Upgraded from version 6.4.2 to version 6.4.3"
+sed -i -e "s/version=3.1.3/version=3.1.4/g" core/dkms/spkgbuild
+changelog "core/dkms/spkgbuild" "Upgraded from version 3.1.3 to version 3.1.4"
+sed -i -e "s/version=6.12.5/version=6.12.6/g" core/linux-api-headers/spkgbuild
+changelog "core/linux-api-headers/spkgbuild" "Upgraded from version 6.12.5 to version 6.12.6"
+sed -i -e "s/version=6.12.5/version=6.12.6/g" core/linux-desktop/spkgbuild
+changelog "core/linux-desktop/spkgbuild" "Upgraded from version 6.12.5 to version 6.12.6"
+sed -i -e "s/version=6.12.5/version=6.12.6/g" core/linux-router/spkgbuild
+changelog "core/linux-router/spkgbuild" "Upgraded from version 6.12.5 to version 6.12.6"
+sed -i -e "s/version=4.0.4/version=4.0.5/g" core/procps-ng/spkgbuild
+changelog "core/procps-ng/spkgbuild" "Upgraded from version 4.0.4 to version 4.0.5"
+sed -i -e "s/version=3.13.2/version=3.13.3/g" extra/flexget/spkgbuild
+changelog "extra/flexget/spkgbuild" "Upgraded from version 3.13.2 to version 3.13.3"
+sed -i -e "s/version=4.1.2/version=4.1.3/g" media/projectm/spkgbuild
+changelog "media/projectm/spkgbuild" "Upgraded from version 4.1.2 to version 4.1.3"
+sed -i -e "s/version=24.8.3/version=24.8.4/g" nonfree/libreoffice/spkgbuild
+changelog "nonfree/libreoffice/spkgbuild" "Upgraded from version 24.8.3 to version 24.8.4"
+sed -i -e "s/version=1.35.84/version=1.35.85/g" python/python-boto3/spkgbuild
+changelog "python/python-boto3/spkgbuild" "Upgraded from version 1.35.84 to version 1.35.85"
+sed -i -e "s/version=1.35.84/version=1.35.85/g" python/python-botocore/spkgbuild
+changelog "python/python-botocore/spkgbuild" "Upgraded from version 1.35.84 to version 1.35.85"
+sed -i -e "s/version=8.1.7/version=8.1.8/g" python/python-click/spkgbuild
+changelog "python/python-click/spkgbuild" "Upgraded from version 8.1.7 to version 8.1.8"
+sed -i -e "s/version=2024.10.0/version=2024.12.0/g" python/python-fsspec/spkgbuild
+changelog "python/python-fsspec/spkgbuild" "Upgraded from version 2024.10.0 to version 2024.12.0"
+sed -i -e "s/version=6.122.3/version=6.122.4/g" python/python-hypothesis/spkgbuild
+changelog "python/python-hypothesis/spkgbuild" "Upgraded from version 6.122.3 to version 6.122.4"
+sed -i -e "s/version=6.1.0/version=6.1.1/g" python/python-psutil/spkgbuild
+changelog "python/python-psutil/spkgbuild" "Upgraded from version 6.1.0 to version 6.1.1"
+sed -i -e "s/version=8.4.1/version=8.4.2/g" server/php/spkgbuild
+changelog "server/php/spkgbuild" "Upgraded from version 8.4.1 to version 8.4.2"
