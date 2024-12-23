660d6c54218be7a594f8d338f3c711ad9bc9731f
Voncloft <voncloft@gmail.com>
2024-12-23 07:21:57 -0500

    Minor Updates

---
+++ logs/old-test-updates/2024/Dec/applied_updates-12-23-24.sh
@@ -0,0 +1,38 @@
+sed -i -e "s/version=24.3.1/version=24.3.2/g" hardware/mesa/spkgbuild
+changelog "hardware/mesa/spkgbuild" "Upgraded from version 24.3.1 to version 24.3.2"
+sed -i -e "s/version=3.31.2/version=3.31.3/g" compilers/cmake/spkgbuild
+changelog "compilers/cmake/spkgbuild" "Upgraded from version 3.31.2 to version 3.31.3"
+sed -i -e "s/version=1.36/version=1.36.2/g" core/fakeroot/spkgbuild
+changelog "core/fakeroot/spkgbuild" "Upgraded from version 1.36 to version 1.36.2"
+sed -i -e "s/version=8.2/version=8.3/g" core/nano/spkgbuild
+changelog "core/nano/spkgbuild" "Upgraded from version 8.2 to version 8.3"
+sed -i -e "s/version=256.9/version=257.1/g" core/systemd/spkgbuild
+changelog "core/systemd/spkgbuild" "Upgraded from version 256.9 to version 257.1"
+sed -i -e "s/version=3.11/version=3.12/g" core/sysvinit/spkgbuild
+changelog "core/sysvinit/spkgbuild" "Upgraded from version 3.11 to version 3.12"
+sed -i -e "s/version=3.13.3/version=3.13.4/g" extra/flexget/spkgbuild
+changelog "extra/flexget/spkgbuild" "Upgraded from version 3.13.3 to version 3.13.4"
+sed -i -e "s/version=1.4.0/version=1.5.0/g" libs/libwebp/spkgbuild
+changelog "libs/libwebp/spkgbuild" "Upgraded from version 1.4.0 to version 1.5.0"
+sed -i -e "s/version=3.15.0/version=3.16.0/g" python/python-aiogram/spkgbuild
+changelog "python/python-aiogram/spkgbuild" "Upgraded from version 3.15.0 to version 3.16.0"
+sed -i -e "s/version=3.3.6/version=3.3.7/g" python/python-astroid/spkgbuild
+changelog "python/python-astroid/spkgbuild" "Upgraded from version 3.3.6 to version 3.3.7"
+sed -i -e "s/version=1.35.85/version=1.35.86/g" python/python-boto3/spkgbuild
+changelog "python/python-boto3/spkgbuild" "Upgraded from version 1.35.85 to version 1.35.86"
+sed -i -e "s/version=1.35.85/version=1.35.86/g" python/python-botocore/spkgbuild
+changelog "python/python-botocore/spkgbuild" "Upgraded from version 1.35.85 to version 1.35.86"
+sed -i -e "s/version=4.6.0/version=4.7.0/g" python/python-elementpath/spkgbuild
+changelog "python/python-elementpath/spkgbuild" "Upgraded from version 4.6.0 to version 4.7.0"
+sed -i -e "s/version=6.122.4/version=6.122.6/g" python/python-hypothesis/spkgbuild
+changelog "python/python-hypothesis/spkgbuild" "Upgraded from version 6.122.4 to version 6.122.6"
+sed -i -e "s/version=0.39.0/version=0.40.0/g" python/python-libtmux/spkgbuild
+changelog "python/python-libtmux/spkgbuild" "Upgraded from version 0.39.0 to version 0.40.0"
+sed -i -e "s/version=2.2.3/version=2.3.0/g" python/python-urllib3/spkgbuild
+changelog "python/python-urllib3/spkgbuild" "Upgraded from version 2.2.3 to version 2.3.0"
+sed -i -e "s/version=3.15.1/version=3.15.2/g" server/blacklist-hosts/spkgbuild
+changelog "server/blacklist-hosts/spkgbuild" "Upgraded from version 3.15.1 to version 3.15.2"
+sed -i -e "s/version=2024.12.4/version=2024.12.5/g" server/homeassistant/spkgbuild
+changelog "server/homeassistant/spkgbuild" "Upgraded from version 2024.12.4 to version 2024.12.5"
+sed -i -e "s/version=1.19.0/version=1.19.1/g" xorg/xdg-desktop-portal/spkgbuild
+changelog "xorg/xdg-desktop-portal/spkgbuild" "Upgraded from version 1.19.0 to version 1.19.1"
