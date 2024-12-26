400710ca07ed21667adef1bcb953419caea68f25
Voncloft <voncloft@gmail.com>
2024-12-26 08:44:20 -0500

    Minor Updates

---
+++ logs/old-test-updates/2024/Dec/applied_updates-12-26-24.sh
@@ -0,0 +1,96 @@
+sed -i -e "s/version=3.3.6/version=3.4.1/g" compilers/ruby/spkgbuild
+changelog "compilers/ruby/spkgbuild" "Upgraded from version 3.3.6 to version 3.4.1"
+sed -i -e "s/version=2.10.2/version=2.10.3/g" core/cracklib/spkgbuild
+changelog "core/cracklib/spkgbuild" "Upgraded from version 2.10.2 to version 2.10.3"
+sed -i -e "s/version=0.140.0/version=0.140.1/g" core/hugo/spkgbuild
+changelog "core/hugo/spkgbuild" "Upgraded from version 0.140.0 to version 0.140.1"
+sed -i -e "s/version=7.1.1/version=7.2/g" core/texinfo/spkgbuild
+changelog "core/texinfo/spkgbuild" "Upgraded from version 7.1.1 to version 7.2"
+sed -i -e "s/version=4.5.0/version=4.5.1/g" core/weechat/spkgbuild
+changelog "core/weechat/spkgbuild" "Upgraded from version 4.5.0 to version 4.5.1"
+sed -i -e "s/version=3.13.4/version=3.13.5/g" extra/flexget/spkgbuild
+changelog "extra/flexget/spkgbuild" "Upgraded from version 3.13.4 to version 3.13.5"
+sed -i -e "s/version=1.1.13rc1/version=1.1.14rc4/g" extra/solaar/spkgbuild
+changelog "extra/solaar/spkgbuild" "Upgraded from version 1.1.13rc1 to version 1.1.14rc4"
+sed -i -e "s/version=2.1.0/version=2.1.1/g" lxqt/lxqt-session/spkgbuild
+changelog "lxqt/lxqt-session/spkgbuild" "Upgraded from version 2.1.0 to version 2.1.1"
+sed -i -e "s/version=2.10.2/version=2.10.3/g" libs/cracklib-dict/spkgbuild
+changelog "libs/cracklib-dict/spkgbuild" "Upgraded from version 2.10.2 to version 2.10.3"
+sed -i -e "s/version=11.0.2/version=11.1.0/g" libs/fmt/spkgbuild
+changelog "libs/fmt/spkgbuild" "Upgraded from version 11.0.2 to version 11.1.0"
+sed -i -e "s/version=3.0.18/version=3.0.19/g" libs/libical/spkgbuild
+changelog "libs/libical/spkgbuild" "Upgraded from version 3.0.18 to version 3.0.19"
+sed -i -e "s/version=0.10.1/version=0.10.2/g" networking/xrdp/spkgbuild
+changelog "networking/xrdp/spkgbuild" "Upgraded from version 0.10.1 to version 0.10.2"
+sed -i -e "s/version=2024.12.13/version=2024.12.23/g" networking/yt-dlp/spkgbuild
+changelog "networking/yt-dlp/spkgbuild" "Upgraded from version 2024.12.13 to version 2024.12.23"
+sed -i -e "s/version=3.6/version=3.6.1/g" nonfree/pandoc-bin/spkgbuild
+changelog "nonfree/pandoc-bin/spkgbuild" "Upgraded from version 3.6 to version 3.6.1"
+sed -i -e "s/version=3.3.7/version=3.3.8/g" python/python-astroid/spkgbuild
+changelog "python/python-astroid/spkgbuild" "Upgraded from version 3.3.7 to version 3.3.8"
+sed -i -e "s/version=1.35.86/version=1.35.87/g" python/python-boto3/spkgbuild
+changelog "python/python-boto3/spkgbuild" "Upgraded from version 1.35.86 to version 1.35.87"
+sed -i -e "s/version=1.35.86/version=1.35.87/g" python/python-botocore/spkgbuild
+changelog "python/python-botocore/spkgbuild" "Upgraded from version 1.35.86 to version 1.35.87"
+sed -i -e "s/version=1.3.5/version=1.3.6/g" python/python-cerberus/spkgbuild
+changelog "python/python-cerberus/spkgbuild" "Upgraded from version 1.3.5 to version 1.3.6"
+sed -i -e "s/version=6.122.6/version=6.123.1/g" python/python-hypothesis/spkgbuild
+changelog "python/python-hypothesis/spkgbuild" "Upgraded from version 6.122.6 to version 6.123.1"
+sed -i -e "s/version=1.4.7/version=1.4.8/g" python/python-kiwisolver/spkgbuild
+changelog "python/python-kiwisolver/spkgbuild" "Upgraded from version 1.4.7 to version 1.4.8"
+sed -i -e "s/version=0.40.0/version=0.40.1/g" python/python-libtmux/spkgbuild
+changelog "python/python-libtmux/spkgbuild" "Upgraded from version 0.40.0 to version 0.40.1"
+sed -i -e "s/version=3.13.2/version=3.13.3/g" python/python-mocket/spkgbuild
+changelog "python/python-mocket/spkgbuild" "Upgraded from version 3.13.2 to version 3.13.3"
+sed -i -e "s/version=3.3.2/version=3.3.3/g" python/python-pylint/spkgbuild
+changelog "python/python-pylint/spkgbuild" "Upgraded from version 3.3.2 to version 3.3.3"
+sed -i -e "s/version=2.3.1.1/version=2.3.1.2/g" python/python-sphobjinv/spkgbuild
+changelog "python/python-sphobjinv/spkgbuild" "Upgraded from version 2.3.1.1 to version 2.3.1.2"
+sed -i -e "s/version=0.42.0/version=0.43.0/g" python/python-starlette/spkgbuild
+changelog "python/python-starlette/spkgbuild" "Upgraded from version 0.42.0 to version 0.43.0"
+sed -i -e "s/version=2.6.1/version=2.6.2/g" ruby-gems/ruby-bundler/spkgbuild
+changelog "ruby-gems/ruby-bundler/spkgbuild" "Upgraded from version 2.6.1 to version 2.6.2"
+sed -i -e "s/version=3.15.2/version=3.15.3/g" server/blacklist-hosts/spkgbuild
+changelog "server/blacklist-hosts/spkgbuild" "Upgraded from version 3.15.2 to version 3.15.3"
+sed -i -e "s/version=2024.12.5/version=2025.1.0/g" server/homeassistant/spkgbuild
+changelog "server/homeassistant/spkgbuild" "Upgraded from version 2024.12.5 to version 2025.1.0"
+sed -i -e "s/version=4.18.1/version=4.18.2/g" xfce/parole/spkgbuild
+changelog "xfce/parole/spkgbuild" "Upgraded from version 4.18.1 to version 4.18.2"
+sed -i -e "s/version=0.13.2/version=0.13.3/g" xfce/ristretto/spkgbuild
+changelog "xfce/ristretto/spkgbuild" "Upgraded from version 0.13.2 to version 0.13.3"
+sed -i -e "s/version=1.1.5/version=1.1.6/g" xfce/xfce4-battery-plugin/spkgbuild
+changelog "xfce/xfce4-battery-plugin/spkgbuild" "Upgraded from version 1.1.5 to version 1.1.6"
+sed -i -e "s/version=1.6.6/version=1.6.7/g" xfce/xfce4-clipman-plugin/spkgbuild
+changelog "xfce/xfce4-clipman-plugin/spkgbuild" "Upgraded from version 1.6.6 to version 1.6.7"
+sed -i -e "s/version=1.2.8/version=1.2.9/g" xfce/xfce4-cpufreq-plugin/spkgbuild
+changelog "xfce/xfce4-cpufreq-plugin/spkgbuild" "Upgraded from version 1.2.8 to version 1.2.9"
+sed -i -e "s/version=1.2.10/version=1.2.11/g" xfce/xfce4-cpugraph-plugin/spkgbuild
+changelog "xfce/xfce4-cpugraph-plugin/spkgbuild" "Upgraded from version 1.2.10 to version 1.2.11"
+sed -i -e "s/version=2.7.0/version=2.7.1/g" xfce/xfce4-diskperf-plugin/spkgbuild
+changelog "xfce/xfce4-diskperf-plugin/spkgbuild" "Upgraded from version 2.7.0 to version 2.7.1"
+sed -i -e "s/version=4.6.0/version=4.6.2/g" xfce/xfce4-eyes-plugin/spkgbuild
+changelog "xfce/xfce4-eyes-plugin/spkgbuild" "Upgraded from version 4.6.0 to version 4.6.2"
+sed -i -e "s/version=1.1.3/version=1.1.4/g" xfce/xfce4-fsguard-plugin/spkgbuild
+changelog "xfce/xfce4-fsguard-plugin/spkgbuild" "Upgraded from version 1.1.3 to version 1.1.4"
+sed -i -e "s/version=1.3.1/version=1.3.2/g" xfce/xfce4-mailwatch-plugin/spkgbuild
+changelog "xfce/xfce4-mailwatch-plugin/spkgbuild" "Upgraded from version 1.3.1 to version 1.3.2"
+sed -i -e "s/version=1.1.6/version=1.1.7/g" xfce/xfce4-mount-plugin/spkgbuild
+changelog "xfce/xfce4-mount-plugin/spkgbuild" "Upgraded from version 1.1.6 to version 1.1.7"
+sed -i -e "s/version=0.5.3/version=0.5.5/g" xfce/xfce4-mpc-plugin/spkgbuild
+changelog "xfce/xfce4-mpc-plugin/spkgbuild" "Upgraded from version 0.5.3 to version 0.5.5"
+sed -i -e "s/version=1.4.1/version=1.4.2/g" xfce/xfce4-netload-plugin/spkgbuild
+changelog "xfce/xfce4-netload-plugin/spkgbuild" "Upgraded from version 1.4.1 to version 1.4.2"
+sed -i -e "s/version=1.8.3/version=1.8.4/g" xfce/xfce4-places-plugin/spkgbuild
+changelog "xfce/xfce4-places-plugin/spkgbuild" "Upgraded from version 1.8.3 to version 1.8.4"
+sed -i -e "s/version=1.4.4/version=1.4.5/g" xfce/xfce4-sensors-plugin/spkgbuild
+changelog "xfce/xfce4-sensors-plugin/spkgbuild" "Upgraded from version 1.4.4 to version 1.4.5"
+sed -i -e "s/version=1.3.2/version=1.3.3/g" xfce/xfce4-systemload-plugin/spkgbuild
+changelog "xfce/xfce4-systemload-plugin/spkgbuild" "Upgraded from version 1.3.2 to version 1.3.3"
+sed -i -e "s/version=1.1.3/version=1.1.4/g" xfce/xfce4-terminal/spkgbuild
+changelog "xfce/xfce4-terminal/spkgbuild" "Upgraded from version 1.1.3 to version 1.1.4"
+sed -i -e "s/version=1.1.3/version=1.1.4/g" xfce/xfce4-time-out-plugin/spkgbuild
+changelog "xfce/xfce4-time-out-plugin/spkgbuild" "Upgraded from version 1.1.3 to version 1.1.4"
+sed -i -e "s/version=1.7.2/version=1.7.3/g" xfce/xfce4-timer-plugin/spkgbuild
+changelog "xfce/xfce4-timer-plugin/spkgbuild" "Upgraded from version 1.7.2 to version 1.7.3"
+sed -i -e "s/version=0.6.3/version=0.6.4/g" xfce/xfce4-wavelan-plugin/spkgbuild
+changelog "xfce/xfce4-wavelan-plugin/spkgbuild" "Upgraded from version 0.6.3 to version 0.6.4"
