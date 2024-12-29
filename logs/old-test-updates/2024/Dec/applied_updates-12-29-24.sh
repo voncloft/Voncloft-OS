d7b2493d13fbd124359aee4749a30df67259bcb7
Voncloft <voncloft@gmail.com>
2024-12-29 11:58:49 -0500

    Minor Updates

---
+++ logs/old-test-updates/2024/Dec/applied_updates-12-29-24.sh
@@ -0,0 +1,44 @@
+sed -i -e "s/version=6.4.2/version=6.4.3/g" cinnamon/cinnamon-settings-daemon/spkgbuild
+changelog "cinnamon/cinnamon-settings-daemon/spkgbuild" "Upgraded from version 6.4.2 to version 6.4.3"
+sed -i -e "s/version=0.4.50/version=0.4.52/g" core/gegl/spkgbuild
+changelog "core/gegl/spkgbuild" "Upgraded from version 0.4.50 to version 0.4.52"
+sed -i -e "s/version=6.12.6/version=6.12.7/g" core/linux-api-headers/spkgbuild
+changelog "core/linux-api-headers/spkgbuild" "Upgraded from version 6.12.6 to version 6.12.7"
+sed -i -e "s/version=6.12.6/version=6.12.7/g" core/linux-desktop/spkgbuild
+changelog "core/linux-desktop/spkgbuild" "Upgraded from version 6.12.6 to version 6.12.7"
+sed -i -e "s/version=6.12.6/version=6.12.7/g" core/linux-router/spkgbuild
+changelog "core/linux-router/spkgbuild" "Upgraded from version 6.12.6 to version 6.12.7"
+sed -i -e "s/version=4.16.0/version=4.17.0/g" core/shadow/spkgbuild
+changelog "core/shadow/spkgbuild" "Upgraded from version 4.16.0 to version 4.17.0"
+sed -i -e "s/version=7.0.0/version=7.1.1/g" core/streamlink/spkgbuild
+changelog "core/streamlink/spkgbuild" "Upgraded from version 7.0.0 to version 7.1.1"
+sed -i -e "s/version=11.1.0/version=11.1.1/g" libs/fmt/spkgbuild
+changelog "libs/fmt/spkgbuild" "Upgraded from version 11.1.0 to version 11.1.1"
+sed -i -e "s/version=2.16.0/version=2.16.1/g" python/python-aiobotocore/spkgbuild
+changelog "python/python-aiobotocore/spkgbuild" "Upgraded from version 2.16.0 to version 2.16.1"
+sed -i -e "s/version=1.35.87/version=1.35.90/g" python/python-boto3/spkgbuild
+changelog "python/python-boto3/spkgbuild" "Upgraded from version 1.35.87 to version 1.35.90"
+sed -i -e "s/version=1.35.87/version=1.35.90/g" python/python-botocore/spkgbuild
+changelog "python/python-botocore/spkgbuild" "Upgraded from version 1.35.87 to version 1.35.90"
+sed -i -e "s/version=7.6.9/version=7.6.10/g" python/python-coverage/spkgbuild
+changelog "python/python-coverage/spkgbuild" "Upgraded from version 7.6.9 to version 7.6.10"
+sed -i -e "s/version=65.3.0/version=65.3.1/g" python/python-django-allauth/spkgbuild
+changelog "python/python-django-allauth/spkgbuild" "Upgraded from version 65.3.0 to version 65.3.1"
+sed -i -e "s/version=0.19.11/version=0.19.12/g" python/python-django-modeltranslation/spkgbuild
+changelog "python/python-django-modeltranslation/spkgbuild" "Upgraded from version 0.19.11 to version 0.19.12"
+sed -i -e "s/version=0.12.1/version=0.12.2/g" python/python-flexmock/spkgbuild
+changelog "python/python-flexmock/spkgbuild" "Upgraded from version 0.12.1 to version 0.12.2"
+sed -i -e "s/version=5.2.0/version=5.3.0/g" python/python-gitlab/spkgbuild
+changelog "python/python-gitlab/spkgbuild" "Upgraded from version 5.2.0 to version 5.3.0"
+sed -i -e "s/version=6.123.1/version=6.123.2/g" python/python-hypothesis/spkgbuild
+changelog "python/python-hypothesis/spkgbuild" "Upgraded from version 6.123.1 to version 6.123.2"
+sed -i -e "s/version=3.1.0/version=3.2.0/g" python/python-libusb1/spkgbuild
+changelog "python/python-libusb1/spkgbuild" "Upgraded from version 3.1.0 to version 3.2.0"
+sed -i -e "s/version=17.0.0/version=17.1.0/g" python/python-path/spkgbuild
+changelog "python/python-path/spkgbuild" "Upgraded from version 17.0.0 to version 17.1.0"
+sed -i -e "s/version=0.43.0/version=0.44.0/g" python/python-starlette/spkgbuild
+changelog "python/python-starlette/spkgbuild" "Upgraded from version 0.43.0 to version 0.44.0"
+sed -i -e "s/version=3.15.3/version=3.15.5/g" server/blacklist-hosts/spkgbuild
+changelog "server/blacklist-hosts/spkgbuild" "Upgraded from version 3.15.3 to version 3.15.5"
+sed -i -e "s/version=3.3.2/version=3.3.3/g" server/radicale/spkgbuild
+changelog "server/radicale/spkgbuild" "Upgraded from version 3.3.2 to version 3.3.3"
