eb6557a7e1063359e8047192474deeeaef3f43f1
Voncloft <voncloft@gmail.com>
2024-12-31 09:39:01 -0500

    Minor Updates

---
+++ logs/old-test-updates/2024/Dec/applied_updates-12-31-24.sh
@@ -0,0 +1,30 @@
+sed -i -e "s/version=0.140.1/version=0.140.2/g" core/hugo/spkgbuild
+changelog "core/hugo/spkgbuild" "Upgraded from version 0.140.1 to version 0.140.2"
+sed -i -e "s/version=4.4.36/version=4.4.37/g" libs/libxcrypt/spkgbuild
+changelog "libs/libxcrypt/spkgbuild" "Upgraded from version 4.4.36 to version 4.4.37"
+sed -i -e "s/version=3.10/version=3.10.1/g" libs/nettle/spkgbuild
+changelog "libs/nettle/spkgbuild" "Upgraded from version 3.10 to version 3.10.1"
+sed -i -e "s/version=1.22.2/version=1.22.3/g" python/python-cfn-lint/spkgbuild
+changelog "python/python-cfn-lint/spkgbuild" "Upgraded from version 1.22.2 to version 1.22.3"
+sed -i -e "s/version=4.11.1/version=4.11.2/g" python/python-engineio/spkgbuild
+changelog "python/python-engineio/spkgbuild" "Upgraded from version 4.11.1 to version 4.11.2"
+sed -i -e "s/version=2.6.3/version=2.6.4/g" python/python-identify/spkgbuild
+changelog "python/python-identify/spkgbuild" "Upgraded from version 2.6.3 to version 2.6.4"
+sed -i -e "s/version=3.0.2/version=3.1.0/g" python/python-mistune/spkgbuild
+changelog "python/python-mistune/spkgbuild" "Upgraded from version 3.0.2 to version 3.1.0"
+sed -i -e "s/version=3.10.12/version=3.10.13/g" python/python-orjson/spkgbuild
+changelog "python/python-orjson/spkgbuild" "Upgraded from version 3.10.12 to version 3.10.13"
+sed -i -e "s/version=5.12.0/version=5.12.1/g" python/python-socketio/spkgbuild
+changelog "python/python-socketio/spkgbuild" "Upgraded from version 5.12.0 to version 5.12.1"
+sed -i -e "s/version=0.44.0/version=0.45.0/g" python/python-starlette/spkgbuild
+changelog "python/python-starlette/spkgbuild" "Upgraded from version 0.44.0 to version 0.45.0"
+sed -i -e "s/version=4.20.0/version=4.20.1/g" xfce/thunar/spkgbuild
+changelog "xfce/thunar/spkgbuild" "Upgraded from version 4.20.0 to version 4.20.1"
+sed -i -e "s/version=0.5.2/version=0.5.3/g" xfce/thunar-archive-plugin/spkgbuild
+changelog "xfce/thunar-archive-plugin/spkgbuild" "Upgraded from version 0.5.2 to version 0.5.3"
+sed -i -e "s/version=0.4.0/version=0.5.0/g" xfce/thunar-media-tags-plugin/spkgbuild
+changelog "xfce/thunar-media-tags-plugin/spkgbuild" "Upgraded from version 0.4.0 to version 0.5.0"
+sed -i -e "s/version=1.11.0/version=1.11.1/g" xfce/xfce4-notes-plugin/spkgbuild
+changelog "xfce/xfce4-notes-plugin/spkgbuild" "Upgraded from version 1.11.0 to version 1.11.1"
+sed -i -e "s/version=0.8.3/version=0.8.5/g" xfce/xfce4-xkb-plugin/spkgbuild
+changelog "xfce/xfce4-xkb-plugin/spkgbuild" "Upgraded from version 0.8.3 to version 0.8.5"
