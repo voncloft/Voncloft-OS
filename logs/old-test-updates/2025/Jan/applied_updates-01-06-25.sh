670ea597f09ffa108811b16751be0e1fd4fcfabd
Voncloft <voncloft@gmail.com>
2025-01-06 07:03:40 -0500

    Minor Updates

---
+++ logs/old-test-updates/2025/Jan/applied_updates-01-06-25.sh
@@ -0,0 +1,18 @@
+sed -i -e "s/version=1.9.2/version=1.10.0/g" hardware/xf86-input-synaptics/spkgbuild
+changelog "hardware/xf86-input-synaptics/spkgbuild" "Upgraded from version 1.9.2 to version 1.10.0"
+sed -i -e "s/version=4.7.0/version=4.8.0/g" python/python-anyio/spkgbuild
+changelog "python/python-anyio/spkgbuild" "Upgraded from version 4.7.0 to version 4.8.0"
+sed -i -e "s/version=1.8.5/version=2.0.0/g" python/python-poetry/spkgbuild
+changelog "python/python-poetry/spkgbuild" "Upgraded from version 1.8.5 to version 2.0.0"
+sed -i -e "s/version=3.0.3/version=3.0.4/g" python/python-pydot/spkgbuild
+changelog "python/python-pydot/spkgbuild" "Upgraded from version 3.0.3 to version 3.0.4"
+sed -i -e "s/version=3.10.1/version=3.10.2/g" python/python-pytables/spkgbuild
+changelog "python/python-pytables/spkgbuild" "Upgraded from version 3.10.1 to version 3.10.2"
+sed -i -e "s/version=75.6.0/version=75.7.0/g" python/python-setuptools/spkgbuild
+changelog "python/python-setuptools/spkgbuild" "Upgraded from version 75.6.0 to version 75.7.0"
+sed -i -e "s/version=1.1.0/version=1.1.1/g" python/python-xattr/spkgbuild
+changelog "python/python-xattr/spkgbuild" "Upgraded from version 1.1.0 to version 1.1.1"
+sed -i -e "s/version=3.15.6/version=3.15.7/g" server/blacklist-hosts/spkgbuild
+changelog "server/blacklist-hosts/spkgbuild" "Upgraded from version 3.15.6 to version 3.15.7"
+sed -i -e "s/version=1.4.2/version=1.4.3/g" xorg/xinit/spkgbuild
+changelog "xorg/xinit/spkgbuild" "Upgraded from version 1.4.2 to version 1.4.3"
