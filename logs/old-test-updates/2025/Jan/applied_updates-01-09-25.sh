ec8957bd5b368568bd5559fa406209a01f0b59d3
Voncloft <voncloft@gmail.com>
2025-01-09 10:33:04 -0500

    Minor Updates

---
+++ logs/old-test-updates/2025/Jan/applied_updates-01-09-25.sh
@@ -0,0 +1,40 @@
+sed -i -e "s/version=7.1.1/version=7.1.2/g" core/streamlink/spkgbuild
+changelog "core/streamlink/spkgbuild" "Upgraded from version 7.1.1 to version 7.1.2"
+sed -i -e "s/version=257.1/version=257.2/g" core/systemd/spkgbuild
+changelog "core/systemd/spkgbuild" "Upgraded from version 257.1 to version 257.2"
+sed -i -e "s/version=3.13.8/version=3.13.9/g" extra/flexget/spkgbuild
+changelog "extra/flexget/spkgbuild" "Upgraded from version 3.13.8 to version 3.13.9"
+sed -i -e "s/version=3.12.0/version=3.12.1/g" libs/blas/spkgbuild
+changelog "libs/blas/spkgbuild" "Upgraded from version 3.12.0 to version 3.12.1"
+sed -i -e "s/version=3.12.0/version=3.12.1/g" libs/cblas/spkgbuild
+changelog "libs/cblas/spkgbuild" "Upgraded from version 3.12.0 to version 3.12.1"
+sed -i -e "s/version=3.12.0/version=3.12.1/g" libs/lapack/spkgbuild
+changelog "libs/lapack/spkgbuild" "Upgraded from version 3.12.0 to version 3.12.1"
+sed -i -e "s/version=1.11.0/version=1.12.1/g" libs/libmaxminddb/spkgbuild
+changelog "libs/libmaxminddb/spkgbuild" "Upgraded from version 1.11.0 to version 1.12.1"
+sed -i -e "s/version=1.6.44/version=1.6.45/g" libs/libpng/spkgbuild
+changelog "libs/libpng/spkgbuild" "Upgraded from version 1.6.44 to version 1.6.45"
+sed -i -e "s/version=135.0b1/version=135.0b2/g" nonfree/firefox/spkgbuild
+changelog "nonfree/firefox/spkgbuild" "Upgraded from version 135.0b1 to version 135.0b2"
+sed -i -e "s/version=1.35.94/version=1.35.95/g" python/python-boto3/spkgbuild
+changelog "python/python-boto3/spkgbuild" "Upgraded from version 1.35.94 to version 1.35.95"
+sed -i -e "s/version=1.35.94/version=1.35.95/g" python/python-botocore/spkgbuild
+changelog "python/python-botocore/spkgbuild" "Upgraded from version 1.35.94 to version 1.35.95"
+sed -i -e "s/version=3.1.0/version=3.2.0/g" python/python-connexion/spkgbuild
+changelog "python/python-connexion/spkgbuild" "Upgraded from version 3.1.0 to version 3.2.0"
+sed -i -e "s/version=6.123.7/version=6.123.9/g" python/python-hypothesis/spkgbuild
+changelog "python/python-hypothesis/spkgbuild" "Upgraded from version 6.123.7 to version 6.123.9"
+sed -i -e "s/version=5.0.5/version=5.1.0/g" python/python-internetarchive/spkgbuild
+changelog "python/python-internetarchive/spkgbuild" "Upgraded from version 5.0.5 to version 5.1.0"
+sed -i -e "s/version=3.10.13/version=3.10.14/g" python/python-orjson/spkgbuild
+changelog "python/python-orjson/spkgbuild" "Upgraded from version 3.10.13 to version 3.10.14"
+sed -i -e "s/version=1.16.0/version=1.17.0/g" python/python-pygit2/spkgbuild
+changelog "python/python-pygit2/spkgbuild" "Upgraded from version 1.16.0 to version 1.17.0"
+sed -i -e "s/version=1.14/version=1.15/g" python/python-pypandoc/spkgbuild
+changelog "python/python-pypandoc/spkgbuild" "Upgraded from version 1.14 to version 1.15"
+sed -i -e "s/version=0.25.1/version=0.25.2/g" python/python-pytest-asyncio/spkgbuild
+changelog "python/python-pytest-asyncio/spkgbuild" "Upgraded from version 0.25.1 to version 0.25.2"
+sed -i -e "s/version=75.7.0/version=75.8.0/g" python/python-setuptools/spkgbuild
+changelog "python/python-setuptools/spkgbuild" "Upgraded from version 75.7.0 to version 75.8.0"
+sed -i -e "s/version=0.137.2/version=0.138.0/g" python/python-zeroconf/spkgbuild
+changelog "python/python-zeroconf/spkgbuild" "Upgraded from version 0.137.2 to version 0.138.0"
