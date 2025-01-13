5b4265d622f5f2fc7da0c0409f1bede230252d41
Voncloft <voncloft@gmail.com>
2025-01-13 08:36:17 -0500

    Minor Updates

---
+++ logs/old-test-updates/2025/Jan/applied_updates-01-13-25.sh
@@ -0,0 +1,12 @@
+sed -i -e "s/version=3.13.12/version=3.13.13/g" extra/flexget/spkgbuild
+changelog "extra/flexget/spkgbuild" "Upgraded from version 3.13.12 to version 3.13.13"
+sed -i -e "s/version=11.1.1/version=11.1.2/g" libs/fmt/spkgbuild
+changelog "libs/fmt/spkgbuild" "Upgraded from version 11.1.1 to version 11.1.2"
+sed -i -e "s/version=8.1.8/version=8.2.0/g" python/python-click/spkgbuild
+changelog "python/python-click/spkgbuild" "Upgraded from version 8.1.8 to version 8.2.0"
+sed -i -e "s/version=2.157.0/version=2.158.0/g" python/python-google-api-python-client/spkgbuild
+changelog "python/python-google-api-python-client/spkgbuild" "Upgraded from version 2.157.0 to version 2.158.0"
+sed -i -e "s/version=2.0.0/version=2.0.1/g" python/python-poetry/spkgbuild
+changelog "python/python-poetry/spkgbuild" "Upgraded from version 2.0.0 to version 2.0.1"
+sed -i -e "s/version=1.6.2/version=1.7.0/g" python/python-xcffib/spkgbuild
+changelog "python/python-xcffib/spkgbuild" "Upgraded from version 1.6.2 to version 1.7.0"
