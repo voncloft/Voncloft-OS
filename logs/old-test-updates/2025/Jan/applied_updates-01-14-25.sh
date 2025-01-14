fa1c7e7b90b4648dadcf6d00c0170260acc82036
Voncloft <voncloft@gmail.com>
2025-01-14 05:38:33 -0500

    Minor Updates

---
+++ logs/old-test-updates/2025/Jan/applied_updates-01-14-25.sh
@@ -0,0 +1,28 @@
+sed -i -e "s/version=255.5/version=255.17/g" cinnamon/elogind/spkgbuild
+changelog "cinnamon/elogind/spkgbuild" "Upgraded from version 255.5 to version 255.17"
+sed -i -e "s/version=5.4.0/version=5.5.0/g" core/qalculate/spkgbuild
+changelog "core/qalculate/spkgbuild" "Upgraded from version 5.4.0 to version 5.5.0"
+sed -i -e "s/version=2.40.3/version=2.40.4/g" core/util-linux/spkgbuild
+changelog "core/util-linux/spkgbuild" "Upgraded from version 2.40.3 to version 2.40.4"
+sed -i -e "s/version=3.13.13/version=3.13.14/g" extra/flexget/spkgbuild
+changelog "extra/flexget/spkgbuild" "Upgraded from version 3.13.13 to version 3.13.14"
+sed -i -e "s/version=1.26/version=1.27/g" extra/iotop/spkgbuild
+changelog "extra/iotop/spkgbuild" "Upgraded from version 1.26 to version 1.27"
+sed -i -e "s/version=5.4.0.1/version=5.5.0/g" libs/libqalculate/spkgbuild
+changelog "libs/libqalculate/spkgbuild" "Upgraded from version 5.4.0.1 to version 5.5.0"
+sed -i -e "s/version=2024.12.23/version=2025.01.12/g" networking/yt-dlp/spkgbuild
+changelog "networking/yt-dlp/spkgbuild" "Upgraded from version 2024.12.23 to version 2025.01.12"
+sed -i -e "s/version=135.0b3/version=135.0b4/g" nonfree/firefox/spkgbuild
+changelog "nonfree/firefox/spkgbuild" "Upgraded from version 135.0b3 to version 135.0b4"
+sed -i -e "s/version=3.6.1/version=3.6.2/g" nonfree/pandoc-bin/spkgbuild
+changelog "nonfree/pandoc-bin/spkgbuild" "Upgraded from version 3.6.1 to version 3.6.2"
+sed -i -e "s/version=134.0b4/version=135.0b1/g" nonfree/thunderbird/spkgbuild
+changelog "nonfree/thunderbird/spkgbuild" "Upgraded from version 134.0b4 to version 135.0b1"
+sed -i -e "s/version=1.35.97/version=1.35.98/g" python/python-boto3/spkgbuild
+changelog "python/python-boto3/spkgbuild" "Upgraded from version 1.35.97 to version 1.35.98"
+sed -i -e "s/version=1.35.97/version=1.35.98/g" python/python-botocore/spkgbuild
+changelog "python/python-botocore/spkgbuild" "Upgraded from version 1.35.97 to version 1.35.98"
+sed -i -e "s/version=6.123.15/version=6.123.17/g" python/python-hypothesis/spkgbuild
+changelog "python/python-hypothesis/spkgbuild" "Upgraded from version 6.123.15 to version 6.123.17"
+sed -i -e "s/version=1.7.0/version=1.7.1/g" python/python-xcffib/spkgbuild
+changelog "python/python-xcffib/spkgbuild" "Upgraded from version 1.7.0 to version 1.7.1"
