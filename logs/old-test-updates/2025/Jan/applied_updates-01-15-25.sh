57ceb77e23d123498f70e4d0f7736d61fc2717a2
Voncloft <voncloft@gmail.com>
2025-01-15 09:17:11 -0500

    Minor Updates

---
+++ logs/old-test-updates/2025/Jan/applied_updates-01-15-25.sh
@@ -0,0 +1,52 @@
+sed -i -e "s/version=19.1.6/version=19.1.7/g" compilers/clang/spkgbuild
+changelog "compilers/clang/spkgbuild" "Upgraded from version 19.1.6 to version 19.1.7"
+sed -i -e "s/version=19.1.6/version=19.1.7/g" compilers/lld/spkgbuild
+changelog "compilers/lld/spkgbuild" "Upgraded from version 19.1.6 to version 19.1.7"
+sed -i -e "s/version=19.1.6/version=19.1.7/g" compilers/llvm/spkgbuild
+changelog "compilers/llvm/spkgbuild" "Upgraded from version 19.1.6 to version 19.1.7"
+sed -i -e "s/version=19.1.6/version=19.1.7/g" compilers/llvm-bak/spkgbuild
+changelog "compilers/llvm-bak/spkgbuild" "Upgraded from version 19.1.6 to version 19.1.7"
+sed -i -e "s/version=19.1.6/version=19.1.7/g" compilers/llvm-old-2023-06-07/spkgbuild
+changelog "compilers/llvm-old-2023-06-07/spkgbuild" "Upgraded from version 19.1.6 to version 19.1.7"
+sed -i -e "s/version=3.3.0/version=3.4.0/g" core/rsync/spkgbuild
+changelog "core/rsync/spkgbuild" "Upgraded from version 3.3.0 to version 3.4.0"
+sed -i -e "s/version=3.13.14/version=3.13.15/g" extra/flexget/spkgbuild
+changelog "extra/flexget/spkgbuild" "Upgraded from version 3.13.14 to version 3.13.15"
+sed -i -e "s/version=3.36.0/version=3.36.2/g" gnome/gnome-color-manager/spkgbuild
+changelog "gnome/gnome-color-manager/spkgbuild" "Upgraded from version 3.36.0 to version 3.36.2"
+sed -i -e "s/version=1.24.11/version=1.25.1/g" media/gst-libav/spkgbuild
+changelog "media/gst-libav/spkgbuild" "Upgraded from version 1.24.11 to version 1.25.1"
+sed -i -e "s/version=1.24.11/version=1.25.1/g" media/gst-plugins-bad/spkgbuild
+changelog "media/gst-plugins-bad/spkgbuild" "Upgraded from version 1.24.11 to version 1.25.1"
+sed -i -e "s/version=1.24.11/version=1.25.1/g" media/gst-plugins-base/spkgbuild
+changelog "media/gst-plugins-base/spkgbuild" "Upgraded from version 1.24.11 to version 1.25.1"
+sed -i -e "s/version=1.24.11/version=1.25.1/g" media/gst-plugins-good/spkgbuild
+changelog "media/gst-plugins-good/spkgbuild" "Upgraded from version 1.24.11 to version 1.25.1"
+sed -i -e "s/version=1.24.11/version=1.25.1/g" media/gst-plugins-ugly/spkgbuild
+changelog "media/gst-plugins-ugly/spkgbuild" "Upgraded from version 1.24.11 to version 1.25.1"
+sed -i -e "s/version=1.24.11/version=1.25.1/g" media/gstreamer/spkgbuild
+changelog "media/gstreamer/spkgbuild" "Upgraded from version 1.24.11 to version 1.25.1"
+sed -i -e "s/version=19.1.6/version=19.1.7/g" multilib/llvm-32/spkgbuild
+changelog "multilib/llvm-32/spkgbuild" "Upgraded from version 19.1.6 to version 19.1.7"
+sed -i -e "s/version=4.4.37/version=4.4.38/g" libs/libxcrypt/spkgbuild
+changelog "libs/libxcrypt/spkgbuild" "Upgraded from version 4.4.37 to version 4.4.38"
+sed -i -e "s/version=2.48.0/version=2.48.1/g" networking/git/spkgbuild
+changelog "networking/git/spkgbuild" "Upgraded from version 2.48.0 to version 2.48.1"
+sed -i -e "s/version=1.14.1/version=1.14.90/g" networking/tigervnc/spkgbuild
+changelog "networking/tigervnc/spkgbuild" "Upgraded from version 1.14.1 to version 1.14.90"
+sed -i -e "s/version=3.47.2.0/version=3.48.0.0/g" python/python-apsw/spkgbuild
+changelog "python/python-apsw/spkgbuild" "Upgraded from version 3.47.2.0 to version 3.48.0.0"
+sed -i -e "s/version=1.35.98/version=1.35.99/g" python/python-boto3/spkgbuild
+changelog "python/python-boto3/spkgbuild" "Upgraded from version 1.35.98 to version 1.35.99"
+sed -i -e "s/version=1.35.98/version=1.35.99/g" python/python-botocore/spkgbuild
+changelog "python/python-botocore/spkgbuild" "Upgraded from version 1.35.98 to version 1.35.99"
+sed -i -e "s/version=1.22.4/version=1.22.5/g" python/python-cfn-lint/spkgbuild
+changelog "python/python-cfn-lint/spkgbuild" "Upgraded from version 1.22.4 to version 1.22.5"
+sed -i -e "s/version=2.158.0/version=2.159.0/g" python/python-google-api-python-client/spkgbuild
+changelog "python/python-google-api-python-client/spkgbuild" "Upgraded from version 2.158.0 to version 2.159.0"
+sed -i -e "s/version=31.0.0/version=32.0.0/g" python/python-kubernetes/spkgbuild
+changelog "python/python-kubernetes/spkgbuild" "Upgraded from version 31.0.0 to version 32.0.0"
+sed -i -e "s/version=0.25.5/version=0.25.6/g" python/python-responses/spkgbuild
+changelog "python/python-responses/spkgbuild" "Upgraded from version 0.25.5 to version 0.25.6"
+sed -i -e "s/version=1.17.1/version=1.17.2/g" python/python-wrapt/spkgbuild
+changelog "python/python-wrapt/spkgbuild" "Upgraded from version 1.17.1 to version 1.17.2"
