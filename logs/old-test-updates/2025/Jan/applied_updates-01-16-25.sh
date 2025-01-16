270bd8d78ced6541bc9cee947bcb554a2b9b42e9
Voncloft <voncloft@gmail.com>
2025-01-16 10:42:33 -0500

    Minor Updates

---
+++ logs/old-test-updates/2025/Jan/applied_updates-01-16-25.sh
@@ -0,0 +1,14 @@
+sed -i -e "s/version=0.27.0/version=0.28.0/g" core/cbindgen/spkgbuild
+changelog "core/cbindgen/spkgbuild" "Upgraded from version 0.27.0 to version 0.28.0"
+sed -i -e "s/version=2.6.12/version=2.6.13/g" networking/openvpn/spkgbuild
+changelog "networking/openvpn/spkgbuild" "Upgraded from version 2.6.12 to version 2.6.13"
+sed -i -e "s/version=135.0b4/version=135.0b5/g" nonfree/firefox/spkgbuild
+changelog "nonfree/firefox/spkgbuild" "Upgraded from version 135.0b4 to version 135.0b5"
+sed -i -e "s/version=4.14.0/version=4.15.0/g" python/python-pwntools/spkgbuild
+changelog "python/python-pwntools/spkgbuild" "Upgraded from version 4.14.0 to version 4.15.0"
+sed -i -e "s/version=0.13.13/version=0.13.14/g" python/python-pybuilder/spkgbuild
+changelog "python/python-pybuilder/spkgbuild" "Upgraded from version 0.13.13 to version 0.13.14"
+sed -i -e "s/version=4.14.0/version=4.15.0/g" python/python-tweepy/spkgbuild
+changelog "python/python-tweepy/spkgbuild" "Upgraded from version 4.14.0 to version 4.15.0"
+sed -i -e "s/version=5.0/version=5.1/g" python/python-zope-deprecation/spkgbuild
+changelog "python/python-zope-deprecation/spkgbuild" "Upgraded from version 5.0 to version 5.1"
