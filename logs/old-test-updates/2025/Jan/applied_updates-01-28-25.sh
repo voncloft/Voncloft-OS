e04bca4a7e19a898745eeae9b7bb0f20311877b5
Voncloft <voncloft@gmail.com>
2025-01-28 10:06:09 -0500

    Minor Updates

---
+++ logs/old-test-updates/2025/Jan/applied_updates-01-28-25.sh
@@ -0,0 +1,14 @@
+sed -i -e "s/version=1.36.6/version=1.36.7/g" python/python-boto3/spkgbuild
+changelog "python/python-boto3/spkgbuild" "Upgraded from version 1.36.6 to version 1.36.7"
+sed -i -e "s/version=1.36.6/version=1.36.7/g" python/python-botocore/spkgbuild
+changelog "python/python-botocore/spkgbuild" "Upgraded from version 1.36.6 to version 1.36.7"
+sed -i -e "s/version=1.2.17/version=1.2.18/g" python/python-deprecated/spkgbuild
+changelog "python/python-deprecated/spkgbuild" "Upgraded from version 1.2.17 to version 1.2.18"
+sed -i -e "s/version=4.2.0/version=4.3.0/g" python/python-openstacksdk/spkgbuild
+changelog "python/python-openstacksdk/spkgbuild" "Upgraded from version 4.2.0 to version 4.3.0"
+sed -i -e "s/version=6.1.0/version=6.1.1.0/g" python/python-pbr/spkgbuild
+changelog "python/python-pbr/spkgbuild" "Upgraded from version 6.1.0 to version 6.1.1.0"
+sed -i -e "s/version=2.2.1/version=2.3.1/g" python/python-w3lib/spkgbuild
+changelog "python/python-w3lib/spkgbuild" "Upgraded from version 2.2.1 to version 2.3.1"
+sed -i -e "s/version=3.15.13/version=3.15.14/g" server/blacklist-hosts/spkgbuild
+changelog "server/blacklist-hosts/spkgbuild" "Upgraded from version 3.15.13 to version 3.15.14"
