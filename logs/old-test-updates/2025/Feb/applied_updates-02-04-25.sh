222367eab10cebedaca7e27ec95244bd5ee280a1
Voncloft <voncloft@gmail.com>
2025-02-04 09:35:23 -0500

    Minor Updates

---
+++ logs/old-test-updates/2025/Feb/applied_updates-02-04-25.sh
@@ -0,0 +1,18 @@
+sed -i -e "s/version=2.4.3/version=2.4.4/g" core/blueman/spkgbuild
+changelog "core/blueman/spkgbuild" "Upgraded from version 2.4.3 to version 2.4.4"
+sed -i -e "s/version=3.10/version=3.11/g" core/diffutils/spkgbuild
+changelog "core/diffutils/spkgbuild" "Upgraded from version 3.10 to version 3.11"
+sed -i -e "s/version=0.20.0/version=0.21.0/g" python/python-aiosqlite/spkgbuild
+changelog "python/python-aiosqlite/spkgbuild" "Upgraded from version 0.20.0 to version 0.21.0"
+sed -i -e "s/version=3.3.8/version=4.0.0/g" python/python-astroid/spkgbuild
+changelog "python/python-astroid/spkgbuild" "Upgraded from version 3.3.8 to version 4.0.0"
+sed -i -e "s/version=1.36.11/version=1.36.12/g" python/python-boto3/spkgbuild
+changelog "python/python-boto3/spkgbuild" "Upgraded from version 1.36.11 to version 1.36.12"
+sed -i -e "s/version=1.36.11/version=1.36.12/g" python/python-botocore/spkgbuild
+changelog "python/python-botocore/spkgbuild" "Upgraded from version 1.36.11 to version 1.36.12"
+sed -i -e "s/version=3.3.1/version=3.3.3/g" python/python-factory-boy/spkgbuild
+changelog "python/python-factory-boy/spkgbuild" "Upgraded from version 3.3.1 to version 3.3.3"
+sed -i -e "s/version=6.124.9/version=6.125.1/g" python/python-hypothesis/spkgbuild
+changelog "python/python-hypothesis/spkgbuild" "Upgraded from version 6.124.9 to version 6.125.1"
+sed -i -e "s/version=4.20.1/version=4.20.2/g" xfce/xfce4-panel/spkgbuild
+changelog "xfce/xfce4-panel/spkgbuild" "Upgraded from version 4.20.1 to version 4.20.2"
