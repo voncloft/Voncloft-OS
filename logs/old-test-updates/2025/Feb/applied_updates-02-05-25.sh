3292793ae5b41db0bdf2f62db6815f81288aefb7
Voncloft <voncloft@gmail.com>
2025-02-05 08:42:17 -0500

    Minor Updates

---
+++ logs/old-test-updates/2025/Feb/applied_updates-02-05-25.sh
@@ -0,0 +1,16 @@
+sed -i -e "s/version=0.143.0/version=0.143.1/g" core/hugo/spkgbuild
+changelog "core/hugo/spkgbuild" "Upgraded from version 0.143.0 to version 0.143.1"
+sed -i -e "s/version=3.14.0/version=3.14.1/g" extra/flexget/spkgbuild
+changelog "extra/flexget/spkgbuild" "Upgraded from version 3.14.0 to version 3.14.1"
+sed -i -e "s/version=25.01.0/version=25.02.0/g" libs/poppler/spkgbuild
+changelog "libs/poppler/spkgbuild" "Upgraded from version 25.01.0 to version 25.02.0"
+sed -i -e "s/version=135.0b9/version=136.0b1/g" nonfree/firefox/spkgbuild
+changelog "nonfree/firefox/spkgbuild" "Upgraded from version 135.0b9 to version 136.0b1"
+sed -i -e "s/version=1.36.12/version=1.36.13/g" python/python-boto3/spkgbuild
+changelog "python/python-boto3/spkgbuild" "Upgraded from version 1.36.12 to version 1.36.13"
+sed -i -e "s/version=1.36.12/version=1.36.13/g" python/python-botocore/spkgbuild
+changelog "python/python-botocore/spkgbuild" "Upgraded from version 1.36.12 to version 1.36.13"
+sed -i -e "s/version=3.5.0/version=3.5.1/g" python/python-paramiko/spkgbuild
+changelog "python/python-paramiko/spkgbuild" "Upgraded from version 3.5.0 to version 3.5.1"
+sed -i -e "s/version=4.20.2/version=4.20.3/g" xfce/xfce4-panel/spkgbuild
+changelog "xfce/xfce4-panel/spkgbuild" "Upgraded from version 4.20.2 to version 4.20.3"
