30e0c58629e60c822f73fb9500e0d5566698f607
Voncloft <voncloft@gmail.com>
2025-02-06 11:09:21 -0500

    Minor Updates

---
+++ logs/old-test-updates/2025/Feb/applied_updates-02-06-25.sh
@@ -0,0 +1,36 @@
+sed -i -e "s/version=8.11.1/version=8.12.0/g" core/curl/spkgbuild
+changelog "core/curl/spkgbuild" "Upgraded from version 8.11.1 to version 8.12.0"
+sed -i -e "s/version=3.107/version=3.108/g" core/nss/spkgbuild
+changelog "core/nss/spkgbuild" "Upgraded from version 3.107 to version 3.108"
+sed -i -e "s/version=10.44/version=10.45/g" core/pcre2/spkgbuild
+changelog "core/pcre2/spkgbuild" "Upgraded from version 10.44 to version 10.45"
+sed -i -e "s/version=6.24.14/version=6.24.15/g" core/tcsh/spkgbuild
+changelog "core/tcsh/spkgbuild" "Upgraded from version 6.24.14 to version 6.24.15"
+sed -i -e "s/version=2.47.3/version=2.47.4/g" core/webkitgtk/spkgbuild
+changelog "core/webkitgtk/spkgbuild" "Upgraded from version 2.47.3 to version 2.47.4"
+sed -i -e "s/version=3.14.1/version=3.14.2/g" extra/flexget/spkgbuild
+changelog "extra/flexget/spkgbuild" "Upgraded from version 3.14.1 to version 3.14.2"
+sed -i -e "s/version=24.1.4/version=24.1.5/g" libs/xwayland/spkgbuild
+changelog "libs/xwayland/spkgbuild" "Upgraded from version 24.1.4 to version 24.1.5"
+sed -i -e "s/version=0.4.8.13/version=0.4.8.14/g" networking/tor/spkgbuild
+changelog "networking/tor/spkgbuild" "Upgraded from version 0.4.8.13 to version 0.4.8.14"
+sed -i -e "s/version=136.0b1/version=136.0b2/g" nonfree/firefox/spkgbuild
+changelog "nonfree/firefox/spkgbuild" "Upgraded from version 136.0b1 to version 136.0b2"
+sed -i -e "s/version=1.26.19/version=1.26.20/g" nonfree/veracrypt/spkgbuild
+changelog "nonfree/veracrypt/spkgbuild" "Upgraded from version 1.26.19 to version 1.26.20"
+sed -i -e "s/version=2.2.0/version=2.2.2/g" python/python-aiodiscover/spkgbuild
+changelog "python/python-aiodiscover/spkgbuild" "Upgraded from version 2.2.0 to version 2.2.2"
+sed -i -e "s/version=1.36.13/version=1.36.14/g" python/python-boto3/spkgbuild
+changelog "python/python-boto3/spkgbuild" "Upgraded from version 1.36.13 to version 1.36.14"
+sed -i -e "s/version=1.36.13/version=1.36.14/g" python/python-botocore/spkgbuild
+changelog "python/python-botocore/spkgbuild" "Upgraded from version 1.36.13 to version 1.36.14"
+sed -i -e "s/version=1.23.1/version=1.24.0/g" python/python-cfn-lint/spkgbuild
+changelog "python/python-cfn-lint/spkgbuild" "Upgraded from version 1.23.1 to version 1.24.0"
+sed -i -e "s/version=3.17.8/version=3.17.9/g" python/python-peewee/spkgbuild
+changelog "python/python-peewee/spkgbuild" "Upgraded from version 3.17.8 to version 3.17.9"
+sed -i -e "s/version=3.0.2/version=3.0.3/g" python/python-spyder-kernels/spkgbuild
+changelog "python/python-spyder-kernels/spkgbuild" "Upgraded from version 3.0.2 to version 3.0.3"
+sed -i -e "s/version=3.15.15/version=3.15.16/g" server/blacklist-hosts/spkgbuild
+changelog "server/blacklist-hosts/spkgbuild" "Upgraded from version 3.15.15 to version 3.15.16"
+sed -i -e "s/version=1.27.3/version=1.27.4/g" server/nginx/spkgbuild
+changelog "server/nginx/spkgbuild" "Upgraded from version 1.27.3 to version 1.27.4"
