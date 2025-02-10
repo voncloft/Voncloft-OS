73a2c2b4f389309386e81e5b7d325d1554c33675
Voncloft <voncloft@gmail.com>
2025-02-10 13:10:50 -0500

    Minor Updates

---
+++ logs/old-test-updates/2025/Feb/applied_updates-02-10-25.sh
@@ -0,0 +1,34 @@
+sed -i -e "s/version=0.4.52/version=0.4.54/g" core/gegl/spkgbuild
+changelog "core/gegl/spkgbuild" "Upgraded from version 0.4.52 to version 0.4.54"
+sed -i -e "s/version=2.16/version=2.17/g" core/lcms2/spkgbuild
+changelog "core/lcms2/spkgbuild" "Upgraded from version 2.16 to version 2.17"
+sed -i -e "s/version=6.13.1/version=6.13.2/g" core/linux-api-headers/spkgbuild
+changelog "core/linux-api-headers/spkgbuild" "Upgraded from version 6.13.1 to version 6.13.2"
+sed -i -e "s/version=6.13.1/version=6.13.2/g" core/linux-desktop/spkgbuild
+changelog "core/linux-desktop/spkgbuild" "Upgraded from version 6.13.1 to version 6.13.2"
+sed -i -e "s/version=6.13.1/version=6.13.2/g" core/linux-router/spkgbuild
+changelog "core/linux-router/spkgbuild" "Upgraded from version 6.13.1 to version 6.13.2"
+sed -i -e "s/version=3.13/version=3.14/g" core/sysvinit/spkgbuild
+changelog "core/sysvinit/spkgbuild" "Upgraded from version 3.13 to version 3.14"
+sed -i -e "s/version=3.14.4/version=3.15.1/g" extra/flexget/spkgbuild
+changelog "extra/flexget/spkgbuild" "Upgraded from version 3.14.4 to version 3.15.1"
+sed -i -e "s/version=2.30.12/version=2.32.0/g" libs/sdl2/spkgbuild
+changelog "libs/sdl2/spkgbuild" "Upgraded from version 2.30.12 to version 2.32.0"
+sed -i -e "s/version=1.14/version=1.15/g" networking/make-ca/spkgbuild
+changelog "networking/make-ca/spkgbuild" "Upgraded from version 1.14 to version 1.15"
+sed -i -e "s/version=2.2.2/version=2.3.0/g" python/python-aiodiscover/spkgbuild
+changelog "python/python-aiodiscover/spkgbuild" "Upgraded from version 2.2.2 to version 2.3.0"
+sed -i -e "s/version=7.6.10/version=7.6.11/g" python/python-coverage/spkgbuild
+changelog "python/python-coverage/spkgbuild" "Upgraded from version 7.6.10 to version 7.6.11"
+sed -i -e "s/version=1.8.0/version=1.9.0/g" python/python-evdev/spkgbuild
+changelog "python/python-evdev/spkgbuild" "Upgraded from version 1.8.0 to version 1.9.0"
+sed -i -e "s/version=2.6.6/version=2.6.7/g" python/python-identify/spkgbuild
+changelog "python/python-identify/spkgbuild" "Upgraded from version 2.6.6 to version 2.6.7"
+sed -i -e "s/version=5.3.0/version=5.3.1/g" python/python-lxml/spkgbuild
+changelog "python/python-lxml/spkgbuild" "Upgraded from version 5.3.0 to version 5.3.1"
+sed -i -e "s/version=2024.10.09/version=2025.02.09/g" python/python-nox/spkgbuild
+changelog "python/python-nox/spkgbuild" "Upgraded from version 2024.10.09 to version 2025.02.09"
+sed -i -e "s/version=25.0/version=25.0.1/g" python/python-pip/spkgbuild
+changelog "python/python-pip/spkgbuild" "Upgraded from version 25.0 to version 25.0.1"
+sed -i -e "s/version=4.20.0/version=4.20.1/g" xfce/xfce4-settings/spkgbuild
+changelog "xfce/xfce4-settings/spkgbuild" "Upgraded from version 4.20.0 to version 4.20.1"
