ecb03556d82578f93972fb92ff73aa85edd5f62d
Voncloft <voncloft@gmail.com>
2025-02-11 05:35:06 -0500

    Minor Updates

---
+++ logs/old-test-updates/2025/Feb/applied_updates-02-11-25.sh
@@ -0,0 +1,32 @@
+sed -i -e "s/version=3.15.1/version=3.15.2/g" extra/flexget/spkgbuild
+changelog "extra/flexget/spkgbuild" "Upgraded from version 3.15.1 to version 3.15.2"
+sed -i -e "s/version=1.25.1/version=1.25.50/g" media/gst-libav/spkgbuild
+changelog "media/gst-libav/spkgbuild" "Upgraded from version 1.25.1 to version 1.25.50"
+sed -i -e "s/version=1.25.1/version=1.25.50/g" media/gst-plugins-bad/spkgbuild
+changelog "media/gst-plugins-bad/spkgbuild" "Upgraded from version 1.25.1 to version 1.25.50"
+sed -i -e "s/version=1.25.1/version=1.25.50/g" media/gst-plugins-base/spkgbuild
+changelog "media/gst-plugins-base/spkgbuild" "Upgraded from version 1.25.1 to version 1.25.50"
+sed -i -e "s/version=1.25.1/version=1.25.50/g" media/gst-plugins-good/spkgbuild
+changelog "media/gst-plugins-good/spkgbuild" "Upgraded from version 1.25.1 to version 1.25.50"
+sed -i -e "s/version=1.25.1/version=1.25.50/g" media/gst-plugins-ugly/spkgbuild
+changelog "media/gst-plugins-ugly/spkgbuild" "Upgraded from version 1.25.1 to version 1.25.50"
+sed -i -e "s/version=1.25.1/version=1.25.50/g" media/gstreamer/spkgbuild
+changelog "media/gstreamer/spkgbuild" "Upgraded from version 1.25.1 to version 1.25.50"
+sed -i -e "s/version=136.0b3/version=136.0b4/g" nonfree/firefox/spkgbuild
+changelog "nonfree/firefox/spkgbuild" "Upgraded from version 136.0b3 to version 136.0b4"
+sed -i -e "s/version=3.6.2/version=3.6.3/g" nonfree/pandoc-bin/spkgbuild
+changelog "nonfree/pandoc-bin/spkgbuild" "Upgraded from version 3.6.2 to version 3.6.3"
+sed -i -e "s/version=2.3.0/version=2.4.0/g" python/python-aiodiscover/spkgbuild
+changelog "python/python-aiodiscover/spkgbuild" "Upgraded from version 2.3.0 to version 2.4.0"
+sed -i -e "s/version=35.2.0/version=36.0.0/g" python/python-faker/spkgbuild
+changelog "python/python-faker/spkgbuild" "Upgraded from version 35.2.0 to version 36.0.0"
+sed -i -e "s/version=1.15.2/version=1.15.3/g" python/python-jsbeautifier/spkgbuild
+changelog "python/python-jsbeautifier/spkgbuild" "Upgraded from version 1.15.2 to version 1.15.3"
+sed -i -e "s/version=4.9.0/version=4.10.0/g" python/python-pytest-django/spkgbuild
+changelog "python/python-pytest-django/spkgbuild" "Upgraded from version 4.9.0 to version 4.10.0"
+sed -i -e "s/version=20.29.1/version=20.29.2/g" python/python-virtualenv/spkgbuild
+changelog "python/python-virtualenv/spkgbuild" "Upgraded from version 20.29.1 to version 20.29.2"
+sed -i -e "s/version=4.20.1/version=4.20.2/g" xfce/thunar/spkgbuild
+changelog "xfce/thunar/spkgbuild" "Upgraded from version 4.20.1 to version 4.20.2"
+sed -i -e "s/version=2.43/version=2.44/g" xorg/xkeyboard-config/spkgbuild
+changelog "xorg/xkeyboard-config/spkgbuild" "Upgraded from version 2.43 to version 2.44"
