be581c8c768b6dd5cb02ae597162f216651e6fd9
Voncloft <voncloft@gmail.com>
2025-02-17 06:57:02 -0500

    Minor Updates

---
+++ logs/old-test-updates/2025/Feb/applied_updates-02-17-25.sh
@@ -0,0 +1,12 @@
+sed -i -e "s/version=3.15.7/version=3.15.8/g" extra/flexget/spkgbuild
+changelog "extra/flexget/spkgbuild" "Upgraded from version 3.15.7 to version 3.15.8"
+sed -i -e "s/version=3.17.0/version=3.18.0/g" python/python-aiogram/spkgbuild
+changelog "python/python-aiogram/spkgbuild" "Upgraded from version 3.17.0 to version 3.18.0"
+sed -i -e "s/version=2.1.0/version=2.1.1/g" python/python-poetry-core/spkgbuild
+changelog "python/python-poetry-core/spkgbuild" "Upgraded from version 2.1.0 to version 2.1.1"
+sed -i -e "s/version=4.4.1/version=4.4.2/g" python/python-typeguard/spkgbuild
+changelog "python/python-typeguard/spkgbuild" "Upgraded from version 4.4.1 to version 4.4.2"
+sed -i -e "s/version=14.2/version=15.0/g" python/python-websockets/spkgbuild
+changelog "python/python-websockets/spkgbuild" "Upgraded from version 14.2 to version 15.0"
+sed -i -e "s/version=0.144.3/version=0.145.0/g" python/python-zeroconf/spkgbuild
+changelog "python/python-zeroconf/spkgbuild" "Upgraded from version 0.144.3 to version 0.145.0"
