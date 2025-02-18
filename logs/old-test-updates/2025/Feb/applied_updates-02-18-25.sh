e2a1aded23c7e8fc107c90fe920031f7d77edb0a
Voncloft <voncloft@gmail.com>
2025-02-18 08:55:15 -0500

    Minor Updates

---
+++ logs/old-test-updates/2025/Feb/applied_updates-02-18-25.sh
@@ -0,0 +1,42 @@
+sed -i -e "s/version=0.24.1/version=0.25.0/g" core/alacritty/spkgbuild
+changelog "core/alacritty/spkgbuild" "Upgraded from version 0.24.1 to version 0.25.0"
+sed -i -e "s/version=0.143.1/version=0.144.0/g" core/hugo/spkgbuild
+changelog "core/hugo/spkgbuild" "Upgraded from version 0.143.1 to version 0.144.0"
+sed -i -e "s/version=6.13.2/version=6.13.3/g" core/linux-api-headers/spkgbuild
+changelog "core/linux-api-headers/spkgbuild" "Upgraded from version 6.13.2 to version 6.13.3"
+sed -i -e "s/version=6.13.2/version=6.13.3/g" core/linux-desktop/spkgbuild
+changelog "core/linux-desktop/spkgbuild" "Upgraded from version 6.13.2 to version 6.13.3"
+sed -i -e "s/version=6.13.2/version=6.13.3/g" core/linux-router/spkgbuild
+changelog "core/linux-router/spkgbuild" "Upgraded from version 6.13.2 to version 6.13.3"
+sed -i -e "s/version=3.15.8/version=3.15.9/g" extra/flexget/spkgbuild
+changelog "extra/flexget/spkgbuild" "Upgraded from version 3.15.8 to version 3.15.9"
+sed -i -e "s/version=136.0b6/version=136.0b7/g" nonfree/firefox/spkgbuild
+changelog "nonfree/firefox/spkgbuild" "Upgraded from version 136.0b6 to version 136.0b7"
+sed -i -e "s/version=1.36.21/version=1.36.22/g" python/python-boto3/spkgbuild
+changelog "python/python-boto3/spkgbuild" "Upgraded from version 1.36.21 to version 1.36.22"
+sed -i -e "s/version=1.36.21/version=1.36.22/g" python/python-botocore/spkgbuild
+changelog "python/python-botocore/spkgbuild" "Upgraded from version 1.36.21 to version 1.36.22"
+sed -i -e "s/version=0.8/version=0.9/g" python/python-collada/spkgbuild
+changelog "python/python-collada/spkgbuild" "Upgraded from version 0.8 to version 0.9"
+sed -i -e "s/version=4.0.1/version=4.0.2/g" python/python-jsonpickle/spkgbuild
+changelog "python/python-jsonpickle/spkgbuild" "Upgraded from version 4.0.1 to version 4.0.2"
+sed -i -e "s/version=0.43.0/version=0.44.2/g" python/python-libtmux/spkgbuild
+changelog "python/python-libtmux/spkgbuild" "Upgraded from version 0.43.0 to version 0.44.2"
+sed -i -e "s/version=2.1.0/version=2.1.1/g" python/python-poetry/spkgbuild
+changelog "python/python-poetry/spkgbuild" "Upgraded from version 2.1.0 to version 2.1.1"
+sed -i -e "s/version=0.13.0/version=0.14.0/g" python/python-symengine/spkgbuild
+changelog "python/python-symengine/spkgbuild" "Upgraded from version 0.13.0 to version 0.14.0"
+sed -i -e "s/version=2.6.3/version=2.6.4/g" ruby-gems/ruby-bundler/spkgbuild
+changelog "ruby-gems/ruby-bundler/spkgbuild" "Upgraded from version 2.6.3 to version 2.6.4"
+sed -i -e "s/version=3.15.17/version=3.15.18/g" server/blacklist-hosts/spkgbuild
+changelog "server/blacklist-hosts/spkgbuild" "Upgraded from version 3.15.17 to version 3.15.18"
+sed -i -e "s/version=3.9.1/version=3.9.2/g" server/postfix/spkgbuild
+changelog "server/postfix/spkgbuild" "Upgraded from version 3.9.1 to version 3.9.2"
+sed -i -e "s/version=4.21.3/version=4.21.4/g" server/samba/spkgbuild
+changelog "server/samba/spkgbuild" "Upgraded from version 4.21.3 to version 4.21.4"
+sed -i -e "s/version=0.9.6/version=0.9.7/g" xfce/xfce4-notifyd/spkgbuild
+changelog "xfce/xfce4-notifyd/spkgbuild" "Upgraded from version 0.9.6 to version 0.9.7"
+sed -i -e "s/version=4.20.0/version=4.20.1/g" xfce/xfdesktop/spkgbuild
+changelog "xfce/xfdesktop/spkgbuild" "Upgraded from version 4.20.0 to version 4.20.1"
+sed -i -e "s/version=6.12.0/version=6.13.0/g" xfce/xfsprogs/spkgbuild
+changelog "xfce/xfsprogs/spkgbuild" "Upgraded from version 6.12.0 to version 6.13.0"
