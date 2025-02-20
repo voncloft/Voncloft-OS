19a0576fbf0817d6f469f0d6d02cbeddf9bae44e
Voncloft <voncloft@gmail.com>
2025-02-20 10:25:21 -0500

    Minor Updates

---
+++ logs/old-test-updates/2025/Feb/applied_updates-02-20-25.sh
@@ -0,0 +1,30 @@
+sed -i -e "s/version=24.3.4/version=25.0.0/g" hardware/mesa/spkgbuild
+changelog "hardware/mesa/spkgbuild" "Upgraded from version 24.3.4 to version 25.0.0"
+sed -i -e "s/version=3.31.5/version=4.0.0-rc1-sunos-sparc/g" compilers/cmake/spkgbuild
+changelog "compilers/cmake/spkgbuild" "Upgraded from version 3.31.5 to version 4.0.0-rc1-sunos-sparc"
+sed -i -e "s/version=0.144.1/version=0.144.2/g" core/hugo/spkgbuild
+changelog "core/hugo/spkgbuild" "Upgraded from version 0.144.1 to version 0.144.2"
+sed -i -e "s/version=1.6.46/version=1.6.47/g" libs/libpng/spkgbuild
+changelog "libs/libpng/spkgbuild" "Upgraded from version 1.6.46 to version 1.6.47"
+sed -i -e "s/version=9.21.4/version=9.21.5/g" networking/bind/spkgbuild
+changelog "networking/bind/spkgbuild" "Upgraded from version 9.21.4 to version 9.21.5"
+sed -i -e "s/version=2025.01.26/version=2025.02.19/g" networking/yt-dlp/spkgbuild
+changelog "networking/yt-dlp/spkgbuild" "Upgraded from version 2025.01.26 to version 2025.02.19"
+sed -i -e "s/version=136.0b7/version=136.0b8/g" nonfree/firefox/spkgbuild
+changelog "nonfree/firefox/spkgbuild" "Upgraded from version 136.0b7 to version 136.0b8"
+sed -i -e "s/version=1.36.23/version=1.36.24/g" python/python-boto3/spkgbuild
+changelog "python/python-boto3/spkgbuild" "Upgraded from version 1.36.23 to version 1.36.24"
+sed -i -e "s/version=1.36.23/version=1.36.24/g" python/python-botocore/spkgbuild
+changelog "python/python-botocore/spkgbuild" "Upgraded from version 1.36.23 to version 1.36.24"
+sed -i -e "s/version=3.10.1/version=3.11.0/g" python/python-flit/spkgbuild
+changelog "python/python-flit/spkgbuild" "Upgraded from version 3.10.1 to version 3.11.0"
+sed -i -e "s/version=32.0.0/version=32.0.1/g" python/python-kubernetes/spkgbuild
+changelog "python/python-kubernetes/spkgbuild" "Upgraded from version 32.0.0 to version 32.0.1"
+sed -i -e "s/version=3.1.1/version=3.1.2/g" python/python-mistune/spkgbuild
+changelog "python/python-mistune/spkgbuild" "Upgraded from version 3.1.1 to version 3.1.2"
+sed -i -e "s/version=0.0.18/version=0.0.19/g" python/python-pyadi-iio/spkgbuild
+changelog "python/python-pyadi-iio/spkgbuild" "Upgraded from version 0.0.18 to version 0.0.19"
+sed -i -e "s/version=0.31/version=0.32/g" python/python-pyelftools/spkgbuild
+changelog "python/python-pyelftools/spkgbuild" "Upgraded from version 0.31 to version 0.32"
+sed -i -e "s/version=1.19.4/version=1.20.0/g" xorg/xdg-desktop-portal/spkgbuild
+changelog "xorg/xdg-desktop-portal/spkgbuild" "Upgraded from version 1.19.4 to version 1.20.0"
