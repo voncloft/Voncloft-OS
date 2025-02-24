ac8aab211dfd3ac54016e6bc58b6c9d67fbe2ffe
Voncloft <voncloft@gmail.com>
2025-02-24 14:48:11 -0500

    Minor Updates

---
+++ logs/old-test-updates/2025/Feb/applied_updates-02-24-25.sh
@@ -0,0 +1,10 @@
+sed -i -e "s/version=29.4/version=30.1/g" core/emacs/spkgbuild
+changelog "core/emacs/spkgbuild" "Upgraded from version 29.4 to version 30.1"
+sed -i -e "s/version=3.6.0/version=3.7.0/g" core/icewm/spkgbuild
+changelog "core/icewm/spkgbuild" "Upgraded from version 3.6.0 to version 3.7.0"
+sed -i -e "s/version=3.15.12/version=3.15.13/g" extra/flexget/spkgbuild
+changelog "extra/flexget/spkgbuild" "Upgraded from version 3.15.12 to version 3.15.13"
+sed -i -e "s/version=0.44.2/version=0.45.0/g" python/python-libtmux/spkgbuild
+changelog "python/python-libtmux/spkgbuild" "Upgraded from version 0.44.2 to version 0.45.0"
+sed -i -e "s/version=3.15.19/version=3.15.20/g" server/blacklist-hosts/spkgbuild
+changelog "server/blacklist-hosts/spkgbuild" "Upgraded from version 3.15.19 to version 3.15.20"
