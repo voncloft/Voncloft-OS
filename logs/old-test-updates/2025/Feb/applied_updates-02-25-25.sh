6bf2a3d2f5fb45ff82c022c38529386f10dd6872
Voncloft <voncloft@gmail.com>
2025-02-25 09:18:52 -0500

    Minor Updates

---
+++ logs/old-test-updates/2025/Feb/applied_updates-02-25-25.sh
@@ -0,0 +1,34 @@
+sed -i -e "s/version=6.4.7/version=6.4.8/g" cinnamon/cinnamon/spkgbuild
+changelog "cinnamon/cinnamon/spkgbuild" "Upgraded from version 6.4.7 to version 6.4.8"
+sed -i -e "s/version=6.4.4/version=6.4.5/g" cinnamon/nemo/spkgbuild
+changelog "cinnamon/nemo/spkgbuild" "Upgraded from version 6.4.4 to version 6.4.5"
+sed -i -e "s/version=6.11/version=6.12/g" core/man-pages/spkgbuild
+changelog "core/man-pages/spkgbuild" "Upgraded from version 6.11 to version 6.12"
+sed -i -e "s/version=5.5.0/version=5.5.1/g" core/qalculate/spkgbuild
+changelog "core/qalculate/spkgbuild" "Upgraded from version 5.5.0 to version 5.5.1"
+sed -i -e "s/version=4.17.2/version=4.17.3/g" core/shadow/spkgbuild
+changelog "core/shadow/spkgbuild" "Upgraded from version 4.17.2 to version 4.17.3"
+sed -i -e "s/version=3.15.13/version=3.15.14/g" extra/flexget/spkgbuild
+changelog "extra/flexget/spkgbuild" "Upgraded from version 3.15.13 to version 3.15.14"
+sed -i -e "s/version=1.25.50/version=1.25.90/g" media/gst-libav/spkgbuild
+changelog "media/gst-libav/spkgbuild" "Upgraded from version 1.25.50 to version 1.25.90"
+sed -i -e "s/version=1.25.50/version=1.25.90/g" media/gst-plugins-bad/spkgbuild
+changelog "media/gst-plugins-bad/spkgbuild" "Upgraded from version 1.25.50 to version 1.25.90"
+sed -i -e "s/version=1.25.50/version=1.25.90/g" media/gst-plugins-base/spkgbuild
+changelog "media/gst-plugins-base/spkgbuild" "Upgraded from version 1.25.50 to version 1.25.90"
+sed -i -e "s/version=1.25.50/version=1.25.90/g" media/gst-plugins-good/spkgbuild
+changelog "media/gst-plugins-good/spkgbuild" "Upgraded from version 1.25.50 to version 1.25.90"
+sed -i -e "s/version=1.25.50/version=1.25.90/g" media/gst-plugins-ugly/spkgbuild
+changelog "media/gst-plugins-ugly/spkgbuild" "Upgraded from version 1.25.50 to version 1.25.90"
+sed -i -e "s/version=1.25.50/version=1.25.90/g" media/gstreamer/spkgbuild
+changelog "media/gstreamer/spkgbuild" "Upgraded from version 1.25.50 to version 1.25.90"
+sed -i -e "s/version=34.0/version=35.0/g" media/mythtv/spkgbuild
+changelog "media/mythtv/spkgbuild" "Upgraded from version 34.0 to version 35.0"
+sed -i -e "s/version=5.5.0/version=5.5.1/g" libs/libqalculate/spkgbuild
+changelog "libs/libqalculate/spkgbuild" "Upgraded from version 5.5.0 to version 5.5.1"
+sed -i -e "s/version=6.126.0/version=6.127.2/g" python/python-hypothesis/spkgbuild
+changelog "python/python-hypothesis/spkgbuild" "Upgraded from version 6.126.0 to version 6.127.2"
+sed -i -e "s/version=1.7.0/version=1.7.1/g" python/python-kikit/spkgbuild
+changelog "python/python-kikit/spkgbuild" "Upgraded from version 1.7.0 to version 1.7.1"
+sed -i -e "s/version=3.10.0/version=3.10.1/g" server/postfix/spkgbuild
+changelog "server/postfix/spkgbuild" "Upgraded from version 3.10.0 to version 3.10.1"
