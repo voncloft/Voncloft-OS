85cb415735ef0a6e5dc07a52ec6c169c0b034a72
Voncloft <voncloft@gmail.com>
2025-03-10 11:17:43 -0400

    Minor Updates

---
+++ logs/old-test-updates/2025/Mar/applied_updates-03-10-25.sh
@@ -0,0 +1,16 @@
+sed -i -e "s/version=4.10.2/version=4.11/g" compilers/ccache/spkgbuild
+changelog "compilers/ccache/spkgbuild" "Upgraded from version 4.10.2 to version 4.11"
+sed -i -e "s/version=2.3.7/version=2.3.8/g" libs/libidn2/spkgbuild
+changelog "libs/libidn2/spkgbuild" "Upgraded from version 2.3.7 to version 2.3.8"
+sed -i -e "s/version=6.127.9/version=6.128.1/g" python/python-hypothesis/spkgbuild
+changelog "python/python-hypothesis/spkgbuild" "Upgraded from version 6.127.9 to version 6.128.1"
+sed -i -e "s/version=3.3.4/version=3.3.5/g" python/python-pylint/spkgbuild
+changelog "python/python-pylint/spkgbuild" "Upgraded from version 3.3.4 to version 3.3.5"
+sed -i -e "s/version=75.8.2/version=76.0.0/g" python/python-setuptools/spkgbuild
+changelog "python/python-setuptools/spkgbuild" "Upgraded from version 75.8.2 to version 76.0.0"
+sed -i -e "s/version=3.15.22/version=3.15.23/g" server/blacklist-hosts/spkgbuild
+changelog "server/blacklist-hosts/spkgbuild" "Upgraded from version 3.15.22 to version 3.15.23"
+sed -i -e "s/version=1.1.3/version=1.1.4/g" xorg/xauth/spkgbuild
+changelog "xorg/xauth/spkgbuild" "Upgraded from version 1.1.3 to version 1.1.4"
+sed -i -e "s/version=1.5.2/version=1.6.0/g" xorg/xtrans/spkgbuild
+changelog "xorg/xtrans/spkgbuild" "Upgraded from version 1.5.2 to version 1.6.0"
