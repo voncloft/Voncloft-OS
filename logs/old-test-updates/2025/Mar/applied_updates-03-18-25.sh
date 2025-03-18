4e64c449b9b49efcc66278b9a7cb3ba5ac99e9d5
Voncloft <voncloft@gmail.com>
2025-03-18 09:58:58 -0400

    Minor Updates

---
+++ logs/old-test-updates/2025/Mar/applied_updates-03-18-25.sh
@@ -0,0 +1,44 @@
+sed -i -e "s/version=4.11/version=4.11.1/g" compilers/ccache/spkgbuild
+changelog "compilers/ccache/spkgbuild" "Upgraded from version 4.11 to version 4.11.1"
+sed -i -e "s/version=5.79/version=5.80/g" core/bluez/spkgbuild
+changelog "core/bluez/spkgbuild" "Upgraded from version 5.79 to version 5.80"
+sed -i -e "s/version=1.22.0/version=1.22.1/g" core/conky/spkgbuild
+changelog "core/conky/spkgbuild" "Upgraded from version 1.22.0 to version 1.22.1"
+sed -i -e "s/version=1.37/version=1.37.1/g" core/fakeroot/spkgbuild
+changelog "core/fakeroot/spkgbuild" "Upgraded from version 1.37 to version 1.37.1"
+sed -i -e "s/version=0.4.54/version=0.4.56/g" core/gegl/spkgbuild
+changelog "core/gegl/spkgbuild" "Upgraded from version 0.4.54 to version 0.4.56"
+sed -i -e "s/version=3.7.14/version=3.7.15/g" core/recode/spkgbuild
+changelog "core/recode/spkgbuild" "Upgraded from version 3.7.14 to version 3.7.15"
+sed -i -e "s/version=3.15.26/version=3.15.27/g" extra/flexget/spkgbuild
+changelog "extra/flexget/spkgbuild" "Upgraded from version 3.15.26 to version 3.15.27"
+sed -i -e "s/version=0.1.110/version=0.1.112/g" libs/babl/spkgbuild
+changelog "libs/babl/spkgbuild" "Upgraded from version 0.1.110 to version 0.1.112"
+sed -i -e "s/version=4.20.0/version=4.21.0/g" libs/libxfce4ui/spkgbuild
+changelog "libs/libxfce4ui/spkgbuild" "Upgraded from version 4.20.0 to version 4.21.0"
+sed -i -e "s/version=137.0b6/version=137.0b7/g" nonfree/firefox/spkgbuild
+changelog "nonfree/firefox/spkgbuild" "Upgraded from version 137.0b6 to version 137.0b7"
+sed -i -e "s/version=3.6.3/version=3.6.4/g" nonfree/pandoc-bin/spkgbuild
+changelog "nonfree/pandoc-bin/spkgbuild" "Upgraded from version 3.6.3 to version 3.6.4"
+sed -i -e "s/version=4.8.0/version=4.9.0/g" python/python-anyio/spkgbuild
+changelog "python/python-anyio/spkgbuild" "Upgraded from version 4.8.0 to version 4.9.0"
+sed -i -e "s/version=6.2.0/version=6.3.0/g" python/python-auditwheel/spkgbuild
+changelog "python/python-auditwheel/spkgbuild" "Upgraded from version 6.2.0 to version 6.3.0"
+sed -i -e "s/version=1.37.13/version=1.37.14/g" python/python-boto3/spkgbuild
+changelog "python/python-boto3/spkgbuild" "Upgraded from version 1.37.13 to version 1.37.14"
+sed -i -e "s/version=1.37.13/version=1.37.14/g" python/python-botocore/spkgbuild
+changelog "python/python-botocore/spkgbuild" "Upgraded from version 1.37.13 to version 1.37.14"
+sed -i -e "s/version=7.6.12/version=7.7.0/g" python/python-coverage/spkgbuild
+changelog "python/python-coverage/spkgbuild" "Upgraded from version 7.6.12 to version 7.7.0"
+sed -i -e "s/version=6.129.2/version=6.129.3/g" python/python-hypothesis/spkgbuild
+changelog "python/python-hypothesis/spkgbuild" "Upgraded from version 6.129.2 to version 6.129.3"
+sed -i -e "s/version=0.46.0/version=0.46.1/g" python/python-libtmux/spkgbuild
+changelog "python/python-libtmux/spkgbuild" "Upgraded from version 0.46.0 to version 0.46.1"
+sed -i -e "s/version=6.1.0/version=6.2.0/g" python/python-multidict/spkgbuild
+changelog "python/python-multidict/spkgbuild" "Upgraded from version 6.1.0 to version 6.2.0"
+sed -i -e "s/version=3.21.0/version=3.22.0/g" python/python-pycryptodome/spkgbuild
+changelog "python/python-pycryptodome/spkgbuild" "Upgraded from version 3.21.0 to version 3.22.0"
+sed -i -e "s/version=3.15.24/version=3.15.25/g" server/blacklist-hosts/spkgbuild
+changelog "server/blacklist-hosts/spkgbuild" "Upgraded from version 3.15.24 to version 3.15.25"
+sed -i -e "s/version=3.4.1/version=3.5.0/g" server/radicale/spkgbuild
+changelog "server/radicale/spkgbuild" "Upgraded from version 3.4.1 to version 3.5.0"
