b6c4a62104c9905363ee7f4ca982729d9261ce6b
Voncloft <voncloft@gmail.com>
2025-03-19 10:51:43 -0400

    Minor Updates

---
+++ logs/old-test-updates/2025/Mar/applied_updates-03-19-25.sh
@@ -0,0 +1,24 @@
+sed -i -e "s/version=1.85.0/version=1.85.1/g" compilers/rust/spkgbuild
+changelog "compilers/rust/spkgbuild" "Upgraded from version 1.85.0 to version 1.85.1"
+sed -i -e "s/version=3.2.6/version=3.2.7.1/g" core/wxgtk3/spkgbuild
+changelog "core/wxgtk3/spkgbuild" "Upgraded from version 3.2.6 to version 3.2.7.1"
+sed -i -e "s/version=2.44.1/version=2.44.2/g" extra/discordchatexporter/spkgbuild
+changelog "extra/discordchatexporter/spkgbuild" "Upgraded from version 2.44.1 to version 2.44.2"
+sed -i -e "s/version=1.37.14/version=1.37.15/g" python/python-boto3/spkgbuild
+changelog "python/python-boto3/spkgbuild" "Upgraded from version 1.37.14 to version 1.37.15"
+sed -i -e "s/version=1.37.14/version=1.37.15/g" python/python-botocore/spkgbuild
+changelog "python/python-botocore/spkgbuild" "Upgraded from version 1.37.14 to version 1.37.15"
+sed -i -e "s/version=1.30.0/version=1.31.0/g" python/python-cfn-lint/spkgbuild
+changelog "python/python-cfn-lint/spkgbuild" "Upgraded from version 1.30.0 to version 1.31.0"
+sed -i -e "s/version=37.0.0/version=37.0.1/g" python/python-faker/spkgbuild
+changelog "python/python-faker/spkgbuild" "Upgraded from version 37.0.0 to version 37.0.1"
+sed -i -e "s/version=5.6.0/version=5.6.1/g" python/python-flask-security-too/spkgbuild
+changelog "python/python-flask-security-too/spkgbuild" "Upgraded from version 5.6.0 to version 5.6.1"
+sed -i -e "s/version=6.129.3/version=6.129.4/g" python/python-hypothesis/spkgbuild
+changelog "python/python-hypothesis/spkgbuild" "Upgraded from version 6.129.3 to version 6.129.4"
+sed -i -e "s/version=1.5.1/version=1.6.1/g" python/python-pecan/spkgbuild
+changelog "python/python-pecan/spkgbuild" "Upgraded from version 1.5.1 to version 1.6.1"
+sed -i -e "s/version=76.0.0/version=76.1.0/g" python/python-setuptools/spkgbuild
+changelog "python/python-setuptools/spkgbuild" "Upgraded from version 76.0.0 to version 76.1.0"
+sed -i -e "s/version=3.4.3/version=3.4.4/g" python/python-xmlschema/spkgbuild
+changelog "python/python-xmlschema/spkgbuild" "Upgraded from version 3.4.3 to version 3.4.4"
