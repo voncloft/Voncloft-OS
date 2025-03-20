cde2bca4e8af04ef29d47ec5bad149f599d2af91
Voncloft <voncloft@gmail.com>
2025-03-20 08:55:01 -0400

    Minor Updates

---
+++ logs/old-test-updates/2025/Mar/applied_updates-03-20-25.sh
@@ -0,0 +1,40 @@
+sed -i -e "s/version=20.1.0/version=20.1.1/g" compilers/clang/spkgbuild
+changelog "compilers/clang/spkgbuild" "Upgraded from version 20.1.0 to version 20.1.1"
+sed -i -e "s/version=20.1.0/version=20.1.1/g" compilers/lld/spkgbuild
+changelog "compilers/lld/spkgbuild" "Upgraded from version 20.1.0 to version 20.1.1"
+sed -i -e "s/version=20.1.0/version=20.1.1/g" compilers/llvm/spkgbuild
+changelog "compilers/llvm/spkgbuild" "Upgraded from version 20.1.0 to version 20.1.1"
+sed -i -e "s/version=20.1.0/version=20.1.1/g" compilers/llvm-bak/spkgbuild
+changelog "compilers/llvm-bak/spkgbuild" "Upgraded from version 20.1.0 to version 20.1.1"
+sed -i -e "s/version=20.1.0/version=20.1.1/g" compilers/llvm-old-2023-06-07/spkgbuild
+changelog "compilers/llvm-old-2023-06-07/spkgbuild" "Upgraded from version 20.1.0 to version 20.1.1"
+sed -i -e "s/version=6.9.3/version=6.9.4/g" core/mercurial/spkgbuild
+changelog "core/mercurial/spkgbuild" "Upgraded from version 6.9.3 to version 6.9.4"
+sed -i -e "s/version=4.17.3/version=4.17.4/g" core/shadow/spkgbuild
+changelog "core/shadow/spkgbuild" "Upgraded from version 4.17.3 to version 4.17.4"
+sed -i -e "s/version=3.15.27/version=3.15.28/g" extra/flexget/spkgbuild
+changelog "extra/flexget/spkgbuild" "Upgraded from version 3.15.27 to version 3.15.28"
+sed -i -e "s/version=20.1.0/version=20.1.1/g" multilib/llvm-32/spkgbuild
+changelog "multilib/llvm-32/spkgbuild" "Upgraded from version 20.1.0 to version 20.1.1"
+sed -i -e "s/version=1.0.27/version=1.0.28/g" libs/libusb/spkgbuild
+changelog "libs/libusb/spkgbuild" "Upgraded from version 1.0.27 to version 1.0.28"
+sed -i -e "s/version=9.21.5/version=9.21.6/g" networking/bind/spkgbuild
+changelog "networking/bind/spkgbuild" "Upgraded from version 9.21.5 to version 9.21.6"
+sed -i -e "s/version=137.0b7/version=137.0b8/g" nonfree/firefox/spkgbuild
+changelog "nonfree/firefox/spkgbuild" "Upgraded from version 137.0b7 to version 137.0b8"
+sed -i -e "s/version=570.124.04/version=570.133.07/g" nonfree/nvidia/spkgbuild
+changelog "nonfree/nvidia/spkgbuild" "Upgraded from version 570.124.04 to version 570.133.07"
+sed -i -e "s/version=570.124.04/version=570.133.07/g" nonfree/nvidia-32/spkgbuild
+changelog "nonfree/nvidia-32/spkgbuild" "Upgraded from version 570.124.04 to version 570.133.07"
+sed -i -e "s/version=570.124.04/version=570.133.07/g" nonfree/nvidia-modules/spkgbuild
+changelog "nonfree/nvidia-modules/spkgbuild" "Upgraded from version 570.124.04 to version 570.133.07"
+sed -i -e "s/version=137.0b1/version=137.0b2/g" nonfree/thunderbird/spkgbuild
+changelog "nonfree/thunderbird/spkgbuild" "Upgraded from version 137.0b1 to version 137.0b2"
+sed -i -e "s/version=3.18.0/version=3.19.0/g" python/python-aiogram/spkgbuild
+changelog "python/python-aiogram/spkgbuild" "Upgraded from version 3.18.0 to version 3.19.0"
+sed -i -e "s/version=76.1.0/version=77.0.1/g" python/python-setuptools/spkgbuild
+changelog "python/python-setuptools/spkgbuild" "Upgraded from version 76.1.0 to version 77.0.1"
+sed -i -e "s/version=2.18.0/version=2.18.1/g" python/python-tensorflow-serving-api/spkgbuild
+changelog "python/python-tensorflow-serving-api/spkgbuild" "Upgraded from version 2.18.0 to version 2.18.1"
+sed -i -e "s/version=4.6.4/version=4.6.5/g" python/python-trimesh/spkgbuild
+changelog "python/python-trimesh/spkgbuild" "Upgraded from version 4.6.4 to version 4.6.5"
