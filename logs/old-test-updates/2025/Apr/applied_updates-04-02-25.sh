00f5ac663b71736843cea4754ae7ff72fbc2f9c6
Voncloft <voncloft@gmail.com>
2025-04-02 05:44:08 -0400

    Minor Updates

---
+++ logs/old-test-updates/2025/Apr/applied_updates-04-02-25.sh
@@ -0,0 +1,20 @@
+sed -i -e "s/version=5.80/version=5.81/g" core/bluez/spkgbuild
+changelog "core/bluez/spkgbuild" "Upgraded from version 5.80 to version 5.81"
+sed -i -e "s/version=2.2.16/version=2.2.17/g" extra/bluefish/spkgbuild
+changelog "extra/bluefish/spkgbuild" "Upgraded from version 2.2.16 to version 2.2.17"
+sed -i -e "s/version=2025.03.27/version=2025.03.31/g" networking/yt-dlp/spkgbuild
+changelog "networking/yt-dlp/spkgbuild" "Upgraded from version 2025.03.27 to version 2025.03.31"
+sed -i -e "s/version=137.0b10/version=138.0b1/g" nonfree/firefox/spkgbuild
+changelog "nonfree/firefox/spkgbuild" "Upgraded from version 137.0b10 to version 138.0b1"
+sed -i -e "s/version=1.37.23/version=1.37.24/g" python/python-boto3/spkgbuild
+changelog "python/python-boto3/spkgbuild" "Upgraded from version 1.37.23 to version 1.37.24"
+sed -i -e "s/version=1.7.1/version=1.7.2/g" python/python-kikit/spkgbuild
+changelog "python/python-kikit/spkgbuild" "Upgraded from version 1.7.1 to version 1.7.2"
+sed -i -e "s/version=6.0.0/version=6.1.0/g" python/python-pytest-cov/spkgbuild
+changelog "python/python-pytest-cov/spkgbuild" "Upgraded from version 6.0.0 to version 6.1.0"
+sed -i -e "s/version=4.10.0/version=4.11.0/g" python/python-pytest-django/spkgbuild
+changelog "python/python-pytest-django/spkgbuild" "Upgraded from version 4.10.0 to version 4.11.0"
+sed -i -e "s/version=0.146.1/version=0.146.2/g" python/python-zeroconf/spkgbuild
+changelog "python/python-zeroconf/spkgbuild" "Upgraded from version 0.146.1 to version 0.146.2"
+sed -i -e "s/version=3.15.28/version=3.15.29/g" server/blacklist-hosts/spkgbuild
+changelog "server/blacklist-hosts/spkgbuild" "Upgraded from version 3.15.28 to version 3.15.29"
