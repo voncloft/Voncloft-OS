ea395106b87e345402749e3f91e444684612e31f
Voncloft <voncloft@gmail.com>
2025-04-09 08:59:29 -0400

    Minor Updates

---
+++ logs/old-test-updates/2025/Apr/applied_updates-04-09-25.sh
@@ -0,0 +1,26 @@
+sed -i -e "s/version=2.4.11/version=2.4.12/g" core/cups/spkgbuild
+changelog "core/cups/spkgbuild" "Upgraded from version 2.4.11 to version 2.4.12"
+sed -i -e "s/version=6.11/version=6.14/g" core/ethtool/spkgbuild
+changelog "core/ethtool/spkgbuild" "Upgraded from version 6.11 to version 6.14"
+sed -i -e "s/version=1.5.31/version=1.5.32/g" core/ibus/spkgbuild
+changelog "core/ibus/spkgbuild" "Upgraded from version 1.5.31 to version 1.5.32"
+sed -i -e "s/version=1.34.4/version=1.34.5/g" libs/c-ares/spkgbuild
+changelog "libs/c-ares/spkgbuild" "Upgraded from version 1.34.4 to version 1.34.5"
+sed -i -e "s/version=1.15/version=1.16/g" networking/make-ca/spkgbuild
+changelog "networking/make-ca/spkgbuild" "Upgraded from version 1.15 to version 1.16"
+sed -i -e "s/version=3.4.1/version=3.5.0/g" networking/openssl/spkgbuild
+changelog "networking/openssl/spkgbuild" "Upgraded from version 3.4.1 to version 3.5.0"
+sed -i -e "s/version=1.37.28/version=1.37.29/g" python/python-boto3/spkgbuild
+changelog "python/python-boto3/spkgbuild" "Upgraded from version 1.37.28 to version 1.37.29"
+sed -i -e "s/version=1.37.28/version=1.37.29/g" python/python-botocore/spkgbuild
+changelog "python/python-botocore/spkgbuild" "Upgraded from version 1.37.28 to version 1.37.29"
+sed -i -e "s/version=1.32.4/version=1.33.0/g" python/python-cfn-lint/spkgbuild
+changelog "python/python-cfn-lint/spkgbuild" "Upgraded from version 1.32.4 to version 1.33.0"
+sed -i -e "s/version=2.0.1/version=2.0.2/g" python/python-cpplint/spkgbuild
+changelog "python/python-cpplint/spkgbuild" "Upgraded from version 2.0.1 to version 2.0.2"
+sed -i -e "s/version=6.130.10/version=6.130.11/g" python/python-hypothesis/spkgbuild
+changelog "python/python-hypothesis/spkgbuild" "Upgraded from version 6.130.10 to version 6.130.11"
+sed -i -e "s/version=23.06.21.0/version=25.04.08.0/g" python/python-prance/spkgbuild
+changelog "python/python-prance/spkgbuild" "Upgraded from version 23.06.21.0 to version 25.04.08.0"
+sed -i -e "s/version=2.11.2/version=2.11.3/g" python/python-pydantic/spkgbuild
+changelog "python/python-pydantic/spkgbuild" "Upgraded from version 2.11.2 to version 2.11.3"
