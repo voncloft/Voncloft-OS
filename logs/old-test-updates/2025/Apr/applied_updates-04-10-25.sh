61e7ae3e8947e12b3d842cad0fd021b7eea37fed
Voncloft <voncloft@gmail.com>
2025-04-10 12:57:33 -0400

    Minor Updates

---
+++ logs/old-test-updates/2025/Apr/applied_updates-04-10-25.sh
@@ -0,0 +1,24 @@
+sed -i -e "s/version=9.6/version=9.7/g" core/coreutils/spkgbuild
+changelog "core/coreutils/spkgbuild" "Upgraded from version 9.6 to version 9.7"
+sed -i -e "s/version=3.11/version=3.12/g" core/diffutils/spkgbuild
+changelog "core/diffutils/spkgbuild" "Upgraded from version 3.11 to version 3.12"
+sed -i -e "s/version=4.6.0/version=4.6.1/g" core/weechat/spkgbuild
+changelog "core/weechat/spkgbuild" "Upgraded from version 4.6.0 to version 4.6.1"
+sed -i -e "s/version=1.15.0/version=1.15.1/g" libs/libvpx/spkgbuild
+changelog "libs/libvpx/spkgbuild" "Upgraded from version 1.15.0 to version 1.15.1"
+sed -i -e "s/version=9.9/version=10.0/g" networking/openssh/spkgbuild
+changelog "networking/openssh/spkgbuild" "Upgraded from version 9.9 to version 10.0"
+sed -i -e "s/version=138.0b4/version=138.0b5/g" nonfree/firefox/spkgbuild
+changelog "nonfree/firefox/spkgbuild" "Upgraded from version 138.0b4 to version 138.0b5"
+sed -i -e "s/version=1.37.29/version=1.37.30/g" python/python-boto3/spkgbuild
+changelog "python/python-boto3/spkgbuild" "Upgraded from version 1.37.29 to version 1.37.30"
+sed -i -e "s/version=1.37.29/version=1.37.30/g" python/python-botocore/spkgbuild
+changelog "python/python-botocore/spkgbuild" "Upgraded from version 1.37.29 to version 1.37.30"
+sed -i -e "s/version=6.130.11/version=6.130.13/g" python/python-hypothesis/spkgbuild
+changelog "python/python-hypothesis/spkgbuild" "Upgraded from version 6.130.11 to version 6.130.13"
+sed -i -e "s/version=6.3.2/version=6.4.1/g" python/python-multidict/spkgbuild
+changelog "python/python-multidict/spkgbuild" "Upgraded from version 6.3.2 to version 6.4.1"
+sed -i -e "s/version=2.3/version=2.4/g" python/python-uhashring/spkgbuild
+changelog "python/python-uhashring/spkgbuild" "Upgraded from version 2.3 to version 2.4"
+sed -i -e "s/version=0.46.0/version=0.46.1/g" python/python-wheel/spkgbuild
+changelog "python/python-wheel/spkgbuild" "Upgraded from version 0.46.0 to version 0.46.1"
