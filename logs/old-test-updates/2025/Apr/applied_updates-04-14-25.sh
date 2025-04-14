6755f4851f88f1a8e104f17b6ae2527dbbe0e975
Voncloft <voncloft@gmail.com>
2025-04-14 07:29:28 -0400

    Minor Updates

---
+++ logs/old-test-updates/2025/Apr/applied_updates-04-14-25.sh
@@ -0,0 +1,10 @@
+sed -i -e "s/version=3.2/version=3.2.1/g" core/gperf/spkgbuild
+changelog "core/gperf/spkgbuild" "Upgraded from version 3.2 to version 3.2.1"
+sed -i -e "s/version=3.4.0/version=3.5.0/g" core/qutebrowser/spkgbuild
+changelog "core/qutebrowser/spkgbuild" "Upgraded from version 3.4.0 to version 3.5.0"
+sed -i -e "s/version=2.75/version=2.76/g" libs/libcap/spkgbuild
+changelog "libs/libcap/spkgbuild" "Upgraded from version 2.75 to version 2.76"
+sed -i -e "s/version=0.46.1/version=0.46.2/g" python/python-starlette/spkgbuild
+changelog "python/python-starlette/spkgbuild" "Upgraded from version 0.46.1 to version 0.46.2"
+sed -i -e "s/version=2.6.7/version=2.6.8/g" ruby-gems/ruby-bundler/spkgbuild
+changelog "ruby-gems/ruby-bundler/spkgbuild" "Upgraded from version 2.6.7 to version 2.6.8"
