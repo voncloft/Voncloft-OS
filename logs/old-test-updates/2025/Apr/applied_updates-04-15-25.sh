e803cc799da67d4537b538be819351cd9ac4040b
Voncloft <voncloft@gmail.com>
2025-04-15 08:42:05 -0400

    Minor Updates

---
+++ logs/old-test-updates/2025/Apr/applied_updates-04-15-25.sh
@@ -0,0 +1,36 @@
+sed -i -e "s/version=3.4.2/version=3.4.3/g" compilers/ruby/spkgbuild
+changelog "compilers/ruby/spkgbuild" "Upgraded from version 3.4.2 to version 3.4.3"
+sed -i -e "s/version=0.146.3/version=0.146.4/g" core/hugo/spkgbuild
+changelog "core/hugo/spkgbuild" "Upgraded from version 0.146.3 to version 0.146.4"
+sed -i -e "s/version=1.4.1/version=1.4.2/g" core/pipewire/spkgbuild
+changelog "core/pipewire/spkgbuild" "Upgraded from version 1.4.1 to version 1.4.2"
+sed -i -e "s/version=3.15.31/version=3.15.32/g" extra/flexget/spkgbuild
+changelog "extra/flexget/spkgbuild" "Upgraded from version 3.15.31 to version 3.15.32"
+sed -i -e "s/version=1.2.13/version=1.2.14/g" media/alsa-utils/spkgbuild
+changelog "media/alsa-utils/spkgbuild" "Upgraded from version 1.2.13 to version 1.2.14"
+sed -i -e "s/version=1.2.13/version=1.2.14/g" libs/alsa-lib/spkgbuild
+changelog "libs/alsa-lib/spkgbuild" "Upgraded from version 1.2.13 to version 1.2.14"
+sed -i -e "s/version=0.34.0/version=0.34.1/g" libs/neon/spkgbuild
+changelog "libs/neon/spkgbuild" "Upgraded from version 0.34.0 to version 0.34.1"
+sed -i -e "s/version=1.2.8/version=1.2.9/g" networking/libnftnl/spkgbuild
+changelog "networking/libnftnl/spkgbuild" "Upgraded from version 1.2.8 to version 1.2.9"
+sed -i -e "s/version=1.1.1/version=1.1.2/g" networking/nftables/spkgbuild
+changelog "networking/nftables/spkgbuild" "Upgraded from version 1.1.1 to version 1.1.2"
+sed -i -e "s/version=138.0b6/version=138.0b7/g" nonfree/firefox/spkgbuild
+changelog "nonfree/firefox/spkgbuild" "Upgraded from version 138.0b6 to version 138.0b7"
+sed -i -e "s/version=3.19.0/version=3.20.0/g" python/python-aiogram/spkgbuild
+changelog "python/python-aiogram/spkgbuild" "Upgraded from version 3.19.0 to version 3.20.0"
+sed -i -e "s/version=1.37.33/version=1.37.34/g" python/python-boto3/spkgbuild
+changelog "python/python-boto3/spkgbuild" "Upgraded from version 1.37.33 to version 1.37.34"
+sed -i -e "s/version=1.37.33/version=1.37.34/g" python/python-botocore/spkgbuild
+changelog "python/python-botocore/spkgbuild" "Upgraded from version 1.37.33 to version 1.37.34"
+sed -i -e "s/version=2.166.0/version=2.167.0/g" python/python-google-api-python-client/spkgbuild
+changelog "python/python-google-api-python-client/spkgbuild" "Upgraded from version 2.166.0 to version 2.167.0"
+sed -i -e "s/version=2.38.0/version=2.39.0/g" python/python-google-auth/spkgbuild
+changelog "python/python-google-auth/spkgbuild" "Upgraded from version 2.38.0 to version 2.39.0"
+sed -i -e "s/version=4.0.0/version=4.0.1/g" python/python-xmlschema/spkgbuild
+changelog "python/python-xmlschema/spkgbuild" "Upgraded from version 4.0.0 to version 4.0.1"
+sed -i -e "s/version=0.146.3/version=0.146.4/g" python/python-zeroconf/spkgbuild
+changelog "python/python-zeroconf/spkgbuild" "Upgraded from version 0.146.3 to version 0.146.4"
+sed -i -e "s/version=6.13.0/version=6.14.0/g" xfce/xfsprogs/spkgbuild
+changelog "xfce/xfsprogs/spkgbuild" "Upgraded from version 6.13.0 to version 6.14.0"
