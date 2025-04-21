bb262d00b7684289e99d0ad52d6bcf9f38ed68d7
Voncloft <voncloft@gmail.com>
2025-04-21 08:13:03 -0400

    Minor Updates

---
+++ logs/old-test-updates/2025/Apr/applied_updates-04-21-25.sh
@@ -0,0 +1,20 @@
+sed -i -e "s/version=16.2/version=16.3/g" compilers/gdb/spkgbuild
+changelog "compilers/gdb/spkgbuild" "Upgraded from version 16.2 to version 16.3"
+sed -i -e "s/version=3.2.1/version=3.3/g" core/gperf/spkgbuild
+changelog "core/gperf/spkgbuild" "Upgraded from version 3.2.1 to version 3.3"
+sed -i -e "s/version=0.146.5/version=0.146.6/g" core/hugo/spkgbuild
+changelog "core/hugo/spkgbuild" "Upgraded from version 0.146.5 to version 0.146.6"
+sed -i -e "s/version=6.14.2/version=6.14.3/g" core/linux-api-headers/spkgbuild
+changelog "core/linux-api-headers/spkgbuild" "Upgraded from version 6.14.2 to version 6.14.3"
+sed -i -e "s/version=6.14.2/version=6.14.3/g" core/linux-desktop/spkgbuild
+changelog "core/linux-desktop/spkgbuild" "Upgraded from version 6.14.2 to version 6.14.3"
+sed -i -e "s/version=6.14.2/version=6.14.3/g" core/linux-router/spkgbuild
+changelog "core/linux-router/spkgbuild" "Upgraded from version 6.14.2 to version 6.14.3"
+sed -i -e "s/version=0.19.13/version=0.19.14/g" python/python-django-modeltranslation/spkgbuild
+changelog "python/python-django-modeltranslation/spkgbuild" "Upgraded from version 0.19.13 to version 0.19.14"
+sed -i -e "s/version=78.1.1/version=79.0.0/g" python/python-setuptools/spkgbuild
+changelog "python/python-setuptools/spkgbuild" "Upgraded from version 78.1.1 to version 79.0.0"
+sed -i -e "s/version=2.6/version=2.7/g" python/python-soupsieve/spkgbuild
+changelog "python/python-soupsieve/spkgbuild" "Upgraded from version 2.6 to version 2.7"
+sed -i -e "s/version=0.14.0/version=0.14.1/g" python/python-symengine/spkgbuild
+changelog "python/python-symengine/spkgbuild" "Upgraded from version 0.14.0 to version 0.14.1"
