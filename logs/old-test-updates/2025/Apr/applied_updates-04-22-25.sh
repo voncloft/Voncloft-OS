9f176d8bf06fed64bb3a72f632cfd52f8be86193
Voncloft <voncloft@gmail.com>
2025-04-22 12:44:39 -0400

    Minor Updates

---
+++ logs/old-test-updates/2025/Apr/applied_updates-04-22-25.sh
@@ -0,0 +1,14 @@
+sed -i -e "s/version=3.7.3/version=3.7.4/g" core/icewm/spkgbuild
+changelog "core/icewm/spkgbuild" "Upgraded from version 3.7.3 to version 3.7.4"
+sed -i -e "s/version=1.37.37/version=1.37.38/g" python/python-boto3/spkgbuild
+changelog "python/python-boto3/spkgbuild" "Upgraded from version 1.37.37 to version 1.37.38"
+sed -i -e "s/version=1.37.37/version=1.37.38/g" python/python-botocore/spkgbuild
+changelog "python/python-botocore/spkgbuild" "Upgraded from version 1.37.37 to version 1.37.38"
+sed -i -e "s/version=0.13.2/version=0.13.3/g" python/python-bottle/spkgbuild
+changelog "python/python-bottle/spkgbuild" "Upgraded from version 0.13.2 to version 0.13.3"
+sed -i -e "s/version=1.34.0/version=1.34.1/g" python/python-cfn-lint/spkgbuild
+changelog "python/python-cfn-lint/spkgbuild" "Upgraded from version 1.34.0 to version 1.34.1"
+sed -i -e "s/version=15.0.2/version=15.0.3/g" python/python-pdoc/spkgbuild
+changelog "python/python-pdoc/spkgbuild" "Upgraded from version 15.0.2 to version 15.0.3"
+sed -i -e "s/version=1.4.3/version=1.4.4/g" python/python-qt.py/spkgbuild
+changelog "python/python-qt.py/spkgbuild" "Upgraded from version 1.4.3 to version 1.4.4"
