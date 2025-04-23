30d50a156c175e5e4da481c66645890ad90c3ecc
Voncloft <voncloft@gmail.com>
2025-04-23 07:14:38 -0400

    Minor Updates

---
+++ logs/old-test-updates/2025/Apr/applied_updates-04-23-25.sh
@@ -0,0 +1,12 @@
+sed -i -e "s/version=0.146.6/version=0.146.7/g" core/hugo/spkgbuild
+changelog "core/hugo/spkgbuild" "Upgraded from version 0.146.6 to version 0.146.7"
+sed -i -e "s/version=1.1.2/version=1.1.3/g" networking/nftables/spkgbuild
+changelog "networking/nftables/spkgbuild" "Upgraded from version 1.1.2 to version 1.1.3"
+sed -i -e "s/version=2025.3.0/version=2025.4.0/g" python/python-distributed/spkgbuild
+changelog "python/python-distributed/spkgbuild" "Upgraded from version 2025.3.0 to version 2025.4.0"
+sed -i -e "s/version=5.3.2/version=5.4.0/g" python/python-lxml/spkgbuild
+changelog "python/python-lxml/spkgbuild" "Upgraded from version 5.3.2 to version 5.4.0"
+sed -i -e "s/version=3.2.2/version=3.2.3/g" python/python-uncertainties/spkgbuild
+changelog "python/python-uncertainties/spkgbuild" "Upgraded from version 3.2.2 to version 3.2.3"
+sed -i -e "s/version=3.10.1/version=3.10.2/g" server/postfix/spkgbuild
+changelog "server/postfix/spkgbuild" "Upgraded from version 3.10.1 to version 3.10.2"
