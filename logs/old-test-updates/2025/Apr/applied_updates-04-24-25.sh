05d34d4a3334f2482513b2f32441fdf73f831bb1
Voncloft <voncloft@gmail.com>
2025-04-24 09:28:01 -0400

    Minor Updates

---
+++ logs/old-test-updates/2025/Apr/applied_updates-04-24-25.sh
@@ -0,0 +1,22 @@
+sed -i -e "s/version=7.4.2/version=7.4.3/g" core/redis/spkgbuild
+changelog "core/redis/spkgbuild" "Upgraded from version 7.4.2 to version 7.4.3"
+sed -i -e "s/version=3.15.34/version=3.15.37/g" extra/flexget/spkgbuild
+changelog "extra/flexget/spkgbuild" "Upgraded from version 3.15.34 to version 3.15.37"
+sed -i -e "s/version=138.0b3/version=138.0b4/g" nonfree/thunderbird/spkgbuild
+changelog "nonfree/thunderbird/spkgbuild" "Upgraded from version 138.0b3 to version 138.0b4"
+sed -i -e "s/version=1.37.38/version=1.38.1/g" python/python-boto3/spkgbuild
+changelog "python/python-boto3/spkgbuild" "Upgraded from version 1.37.38 to version 1.38.1"
+sed -i -e "s/version=1.37.38/version=1.38.1/g" python/python-botocore/spkgbuild
+changelog "python/python-botocore/spkgbuild" "Upgraded from version 1.37.38 to version 1.38.1"
+sed -i -e "s/version=8.1.0/version=8.2.0/g" python/python-cx-freeze/spkgbuild
+changelog "python/python-cx-freeze/spkgbuild" "Upgraded from version 8.1.0 to version 8.2.0"
+sed -i -e "s/version=6.131.6/version=6.131.7/g" python/python-hypothesis/spkgbuild
+changelog "python/python-hypothesis/spkgbuild" "Upgraded from version 6.131.6 to version 6.131.7"
+sed -i -e "s/version=0.11.5/version=0.12.0/g" python/python-s3transfer/spkgbuild
+changelog "python/python-s3transfer/spkgbuild" "Upgraded from version 0.11.5 to version 0.12.0"
+sed -i -e "s/version=3.15.33/version=3.15.34/g" server/blacklist-hosts/spkgbuild
+changelog "server/blacklist-hosts/spkgbuild" "Upgraded from version 3.15.33 to version 3.15.34"
+sed -i -e "s/version=1.27.5/version=1.28.0/g" server/nginx/spkgbuild
+changelog "server/nginx/spkgbuild" "Upgraded from version 1.27.5 to version 1.28.0"
+sed -i -e "s/version=3.5.1/version=3.5.2/g" server/radicale/spkgbuild
+changelog "server/radicale/spkgbuild" "Upgraded from version 3.5.1 to version 3.5.2"
