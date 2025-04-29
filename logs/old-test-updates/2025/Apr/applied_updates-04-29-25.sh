57a6eb9a8c26fdbe3cec86edfe19affd1cc015b7
Voncloft <voncloft@gmail.com>
2025-04-29 08:25:55 -0400

    Minor Updates

---
+++ logs/old-test-updates/2025/Apr/applied_updates-04-29-25.sh
@@ -0,0 +1,10 @@
+sed -i -e "s/version=2.8/version=2.8.1/g" extra/ncdu/spkgbuild
+changelog "extra/ncdu/spkgbuild" "Upgraded from version 2.8 to version 2.8.1"
+sed -i -e "s/version=1.38.3/version=1.38.4/g" python/python-boto3/spkgbuild
+changelog "python/python-boto3/spkgbuild" "Upgraded from version 1.38.3 to version 1.38.4"
+sed -i -e "s/version=1.38.3/version=1.38.4/g" python/python-botocore/spkgbuild
+changelog "python/python-botocore/spkgbuild" "Upgraded from version 1.38.3 to version 1.38.4"
+sed -i -e "s/version=9.0.0/version=9.0.1/g" python/python-elasticsearch/spkgbuild
+changelog "python/python-elasticsearch/spkgbuild" "Upgraded from version 9.0.0 to version 9.0.1"
+sed -i -e "s/version=4.8.0/version=5.0.0/g" python/python-elementpath/spkgbuild
+changelog "python/python-elementpath/spkgbuild" "Upgraded from version 4.8.0 to version 5.0.0"
