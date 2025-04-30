1ed88183e2d6d18ab3b3808bcc3d81520000a7e9
Voncloft <voncloft@gmail.com>
2025-04-30 07:44:09 -0400

    Minor Updates

---
+++ logs/old-test-updates/2025/Apr/applied_updates-04-30-25.sh
@@ -0,0 +1,26 @@
+sed -i -e "s/version=6.4.9/version=6.4.10/g" cinnamon/cinnamon/spkgbuild
+changelog "cinnamon/cinnamon/spkgbuild" "Upgraded from version 6.4.9 to version 6.4.10"
+sed -i -e "s/version=5.35.0/version=5.35.1/g" core/monit/spkgbuild
+changelog "core/monit/spkgbuild" "Upgraded from version 5.35.0 to version 5.35.1"
+sed -i -e "s/version=1.7.3/version=1.8/g" libs/libconfig/spkgbuild
+changelog "libs/libconfig/spkgbuild" "Upgraded from version 1.7.3 to version 1.8"
+sed -i -e "s/version=138.0b9/version=139.0b1/g" nonfree/firefox/spkgbuild
+changelog "nonfree/firefox/spkgbuild" "Upgraded from version 138.0b9 to version 139.0b1"
+sed -i -e "s/version=1.38.4/version=1.38.5/g" python/python-boto3/spkgbuild
+changelog "python/python-boto3/spkgbuild" "Upgraded from version 1.38.4 to version 1.38.5"
+sed -i -e "s/version=1.38.4/version=1.38.5/g" python/python-botocore/spkgbuild
+changelog "python/python-botocore/spkgbuild" "Upgraded from version 1.38.4 to version 1.38.5"
+sed -i -e "s/version=0.7.7/version=0.8.0/g" python/python-fxa/spkgbuild
+changelog "python/python-fxa/spkgbuild" "Upgraded from version 0.7.7 to version 0.8.0"
+sed -i -e "s/version=2.168.0/version=2.169.0/g" python/python-google-api-python-client/spkgbuild
+changelog "python/python-google-api-python-client/spkgbuild" "Upgraded from version 2.168.0 to version 2.169.0"
+sed -i -e "s/version=5.3.1/version=5.4.0/g" python/python-internetarchive/spkgbuild
+changelog "python/python-internetarchive/spkgbuild" "Upgraded from version 5.3.1 to version 5.4.0"
+sed -i -e "s/version=3.10.16/version=3.10.17/g" python/python-orjson/spkgbuild
+changelog "python/python-orjson/spkgbuild" "Upgraded from version 3.10.16 to version 3.10.17"
+sed -i -e "s/version=3.17.9/version=3.18.0/g" python/python-peewee/spkgbuild
+changelog "python/python-peewee/spkgbuild" "Upgraded from version 3.17.9 to version 3.18.0"
+sed -i -e "s/version=0.15.2/version=0.15.3/g" python/python-typer/spkgbuild
+changelog "python/python-typer/spkgbuild" "Upgraded from version 0.15.2 to version 0.15.3"
+sed -i -e "s/version=2.1.2/version=2.2.0/g" server/deluge/spkgbuild
+changelog "server/deluge/spkgbuild" "Upgraded from version 2.1.2 to version 2.2.0"
