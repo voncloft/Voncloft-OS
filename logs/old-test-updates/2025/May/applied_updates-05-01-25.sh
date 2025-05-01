aadf1df8edfe457c5c179f61435f968f660e9e7e
Voncloft <voncloft@gmail.com>
2025-05-01 12:48:48 -0400

    Minor Updates

---
+++ logs/old-test-updates/2025/May/applied_updates-05-01-25.sh
@@ -0,0 +1,42 @@
+sed -i -e "s/version=25.0.4/version=25.0.5/g" hardware/mesa/spkgbuild
+changelog "hardware/mesa/spkgbuild" "Upgraded from version 25.0.4 to version 25.0.5"
+sed -i -e "s/version=20.1.3/version=20.1.4/g" compilers/clang/spkgbuild
+changelog "compilers/clang/spkgbuild" "Upgraded from version 20.1.3 to version 20.1.4"
+sed -i -e "s/version=20.1.3/version=20.1.4/g" compilers/lld/spkgbuild
+changelog "compilers/lld/spkgbuild" "Upgraded from version 20.1.3 to version 20.1.4"
+sed -i -e "s/version=20.1.3/version=20.1.4/g" compilers/llvm/spkgbuild
+changelog "compilers/llvm/spkgbuild" "Upgraded from version 20.1.3 to version 20.1.4"
+sed -i -e "s/version=20.1.3/version=20.1.4/g" compilers/llvm-bak/spkgbuild
+changelog "compilers/llvm-bak/spkgbuild" "Upgraded from version 20.1.3 to version 20.1.4"
+sed -i -e "s/version=20.1.3/version=20.1.4/g" compilers/llvm-old-2023-06-07/spkgbuild
+changelog "compilers/llvm-old-2023-06-07/spkgbuild" "Upgraded from version 20.1.3 to version 20.1.4"
+sed -i -e "s/version=1.16.0/version=1.17.0/g" core/gtest/spkgbuild
+changelog "core/gtest/spkgbuild" "Upgraded from version 1.16.0 to version 1.17.0"
+sed -i -e "s/version=0.44.2/version=0.46.0/g" core/pixman/spkgbuild
+changelog "core/pixman/spkgbuild" "Upgraded from version 0.44.2 to version 0.46.0"
+sed -i -e "s/version=7.4/version=7.5/g" core/smartmontools/spkgbuild
+changelog "core/smartmontools/spkgbuild" "Upgraded from version 7.4 to version 7.5"
+sed -i -e "s/version=20.1.3/version=20.1.4/g" multilib/llvm-32/spkgbuild
+changelog "multilib/llvm-32/spkgbuild" "Upgraded from version 20.1.3 to version 20.1.4"
+sed -i -e "s/version=1.6.47/version=1.6.48/g" libs/libpng/spkgbuild
+changelog "libs/libpng/spkgbuild" "Upgraded from version 1.6.47 to version 1.6.48"
+sed -i -e "s/version=4.0.0/version=4.1.0/g" libs/libressl/spkgbuild
+changelog "libs/libressl/spkgbuild" "Upgraded from version 4.0.0 to version 4.1.0"
+sed -i -e "s/version=25.2.2/version=25.2.3/g" nonfree/libreoffice/spkgbuild
+changelog "nonfree/libreoffice/spkgbuild" "Upgraded from version 25.2.2 to version 25.2.3"
+sed -i -e "s/version=0.14.2/version=0.14.3/g" python/python-cachecontrol/spkgbuild
+changelog "python/python-cachecontrol/spkgbuild" "Upgraded from version 0.14.2 to version 0.14.3"
+sed -i -e "s/version=2.5.0/version=2.5.1/g" python/python-hcloud/spkgbuild
+changelog "python/python-hcloud/spkgbuild" "Upgraded from version 2.5.0 to version 2.5.1"
+sed -i -e "s/version=3.10.17/version=3.10.18/g" python/python-orjson/spkgbuild
+changelog "python/python-orjson/spkgbuild" "Upgraded from version 3.10.17 to version 3.10.18"
+sed -i -e "s/version=3.18.0/version=3.18.1/g" python/python-peewee/spkgbuild
+changelog "python/python-peewee/spkgbuild" "Upgraded from version 3.18.0 to version 3.18.1"
+sed -i -e "s/version=2.11.3/version=2.11.4/g" python/python-pydantic/spkgbuild
+changelog "python/python-pydantic/spkgbuild" "Upgraded from version 2.11.3 to version 2.11.4"
+sed -i -e "s/version=80.0.0/version=80.1.0/g" python/python-setuptools/spkgbuild
+changelog "python/python-setuptools/spkgbuild" "Upgraded from version 80.0.0 to version 80.1.0"
+sed -i -e "s/version=6.4.2/version=6.5.0/g" python/python-tornado/spkgbuild
+changelog "python/python-tornado/spkgbuild" "Upgraded from version 6.4.2 to version 6.5.0"
+sed -i -e "s/version=2025.4.4/version=2025.5.0/g" server/homeassistant/spkgbuild
+changelog "server/homeassistant/spkgbuild" "Upgraded from version 2025.4.4 to version 2025.5.0"
