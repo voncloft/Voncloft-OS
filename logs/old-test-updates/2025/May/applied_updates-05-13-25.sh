9e38b3296b8e52dc91aa9f79161cba94f08347eb
Voncloft <voncloft@gmail.com>
2025-05-13 09:35:24 -0400

    Minor Updates

---
+++ logs/old-test-updates/2025/May/applied_updates-05-13-25.sh
@@ -0,0 +1,26 @@
+sed -i -e "s/version=3.2.0/version=3.2.1/g" core/dkms/spkgbuild
+changelog "core/dkms/spkgbuild" "Upgraded from version 3.2.0 to version 3.2.1"
+sed -i -e "s/version=0.147.2/version=0.147.3/g" core/hugo/spkgbuild
+changelog "core/hugo/spkgbuild" "Upgraded from version 0.147.2 to version 0.147.3"
+sed -i -e "s/version=3.15.40/version=3.15.41/g" extra/flexget/spkgbuild
+changelog "extra/flexget/spkgbuild" "Upgraded from version 3.15.40 to version 3.15.41"
+sed -i -e "s/version=139.0b6/version=139.0b7/g" nonfree/firefox/spkgbuild
+changelog "nonfree/firefox/spkgbuild" "Upgraded from version 139.0b6 to version 139.0b7"
+sed -i -e "s/version=6.8.1/version=6.8.2/g" python/python-apispec/spkgbuild
+changelog "python/python-apispec/spkgbuild" "Upgraded from version 6.8.1 to version 6.8.2"
+sed -i -e "s/version=1.38.13/version=1.38.14/g" python/python-boto3/spkgbuild
+changelog "python/python-boto3/spkgbuild" "Upgraded from version 1.38.13 to version 1.38.14"
+sed -i -e "s/version=1.38.13/version=1.38.14/g" python/python-botocore/spkgbuild
+changelog "python/python-botocore/spkgbuild" "Upgraded from version 1.38.13 to version 1.38.14"
+sed -i -e "s/version=2.7.1/version=2.8.0/g" python/python-cairosvg/spkgbuild
+changelog "python/python-cairosvg/spkgbuild" "Upgraded from version 2.7.1 to version 2.8.0"
+sed -i -e "s/version=8.2.0/version=8.3.0/g" python/python-cx-freeze/spkgbuild
+changelog "python/python-cx-freeze/spkgbuild" "Upgraded from version 8.2.0 to version 8.3.0"
+sed -i -e "s/version=25.4.2/version=25.5.1/g" python/python-gevent/spkgbuild
+changelog "python/python-gevent/spkgbuild" "Upgraded from version 25.4.2 to version 25.5.1"
+sed -i -e "s/version=80.4.0/version=80.5.0/g" python/python-setuptools/spkgbuild
+changelog "python/python-setuptools/spkgbuild" "Upgraded from version 80.4.0 to version 80.5.0"
+sed -i -e "s/version=1.6.1/version=1.6.2/g" ruby-gems/ruby-diff-lcs/spkgbuild
+changelog "ruby-gems/ruby-diff-lcs/spkgbuild" "Upgraded from version 1.6.1 to version 1.6.2"
+sed -i -e "s/version=0.8.8/version=0.8.9/g" xfce/xfce4-dict/spkgbuild
+changelog "xfce/xfce4-dict/spkgbuild" "Upgraded from version 0.8.8 to version 0.8.9"
