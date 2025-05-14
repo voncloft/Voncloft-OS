b0e4bc2b05aad6e0313932af1afd3229062555ab
Voncloft <voncloft@gmail.com>
2025-05-14 05:34:16 -0400

    Minor Updates

---
+++ logs/old-test-updates/2025/May/applied_updates-05-14-25.sh
@@ -0,0 +1,28 @@
+sed -i -e "s/version=1.37.1.1/version=1.37.1.2/g" core/fakeroot/spkgbuild
+changelog "core/fakeroot/spkgbuild" "Upgraded from version 1.37.1.1 to version 1.37.1.2"
+sed -i -e "s/version=0.4.58/version=0.4.62/g" core/gegl/spkgbuild
+changelog "core/gegl/spkgbuild" "Upgraded from version 0.4.58 to version 0.4.62"
+sed -i -e "s/version=8.0.0/version=8.0.1/g" core/redis/spkgbuild
+changelog "core/redis/spkgbuild" "Upgraded from version 8.0.0 to version 8.0.1"
+sed -i -e "s/version=3.15.41/version=3.15.42/g" extra/flexget/spkgbuild
+changelog "extra/flexget/spkgbuild" "Upgraded from version 3.15.41 to version 3.15.42"
+sed -i -e "s/version=0.1.112/version=0.1.114/g" libs/babl/spkgbuild
+changelog "libs/babl/spkgbuild" "Upgraded from version 0.1.112 to version 0.1.114"
+sed -i -e "s/version=7.96/version=7.97/g" networking/nmap/spkgbuild
+changelog "networking/nmap/spkgbuild" "Upgraded from version 7.96 to version 7.97"
+sed -i -e "s/version=5.3.0/version=5.3.1/g" python/python-apptools/spkgbuild
+changelog "python/python-apptools/spkgbuild" "Upgraded from version 5.3.0 to version 5.3.1"
+sed -i -e "s/version=1.38.14/version=1.38.15/g" python/python-boto3/spkgbuild
+changelog "python/python-boto3/spkgbuild" "Upgraded from version 1.38.14 to version 1.38.15"
+sed -i -e "s/version=1.38.14/version=1.38.15/g" python/python-botocore/spkgbuild
+changelog "python/python-botocore/spkgbuild" "Upgraded from version 1.38.14 to version 1.38.15"
+sed -i -e "s/version=1.4.2/version=1.5.0/g" python/python-bottleneck/spkgbuild
+changelog "python/python-bottleneck/spkgbuild" "Upgraded from version 1.4.2 to version 1.5.0"
+sed -i -e "s/version=3.1.0/version=3.1.1/g" python/python-flask/spkgbuild
+changelog "python/python-flask/spkgbuild" "Upgraded from version 3.1.0 to version 3.1.1"
+sed -i -e "s/version=6.131.15/version=6.131.16/g" python/python-hypothesis/spkgbuild
+changelog "python/python-hypothesis/spkgbuild" "Upgraded from version 6.131.15 to version 6.131.16"
+sed -i -e "s/version=1.4.4/version=1.4.6/g" python/python-qt.py/spkgbuild
+changelog "python/python-qt.py/spkgbuild" "Upgraded from version 1.4.4 to version 1.4.6"
+sed -i -e "s/version=2.6.8/version=2.6.9/g" ruby-gems/ruby-bundler/spkgbuild
+changelog "ruby-gems/ruby-bundler/spkgbuild" "Upgraded from version 2.6.8 to version 2.6.9"
