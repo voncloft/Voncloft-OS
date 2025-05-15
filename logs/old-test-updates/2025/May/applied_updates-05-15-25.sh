4ca60216fdfea05f34a38e1998fbe0ea1d5cfa03
Voncloft <voncloft@gmail.com>
2025-05-15 10:42:46 -0400

    Minor Updates

---
+++ logs/old-test-updates/2025/May/applied_updates-05-15-25.sh
@@ -0,0 +1,34 @@
+sed -i -e "s/version=20.1.4/version=20.1.5/g" compilers/clang/spkgbuild
+changelog "compilers/clang/spkgbuild" "Upgraded from version 20.1.4 to version 20.1.5"
+sed -i -e "s/version=20.1.4/version=20.1.5/g" compilers/lld/spkgbuild
+changelog "compilers/lld/spkgbuild" "Upgraded from version 20.1.4 to version 20.1.5"
+sed -i -e "s/version=20.1.4/version=20.1.5/g" compilers/llvm/spkgbuild
+changelog "compilers/llvm/spkgbuild" "Upgraded from version 20.1.4 to version 20.1.5"
+sed -i -e "s/version=20.1.4/version=20.1.5/g" compilers/llvm-bak/spkgbuild
+changelog "compilers/llvm-bak/spkgbuild" "Upgraded from version 20.1.4 to version 20.1.5"
+sed -i -e "s/version=20.1.4/version=20.1.5/g" compilers/llvm-old-2023-06-07/spkgbuild
+changelog "compilers/llvm-old-2023-06-07/spkgbuild" "Upgraded from version 20.1.4 to version 20.1.5"
+sed -i -e "s/version=3.4.3/version=3.4.4/g" compilers/ruby/spkgbuild
+changelog "compilers/ruby/spkgbuild" "Upgraded from version 3.4.3 to version 3.4.4"
+sed -i -e "s/version=30.2/version=31.0/g" extra/protobuf/spkgbuild
+changelog "extra/protobuf/spkgbuild" "Upgraded from version 30.2 to version 31.0"
+sed -i -e "s/version=20.1.4/version=20.1.5/g" multilib/llvm-32/spkgbuild
+changelog "multilib/llvm-32/spkgbuild" "Upgraded from version 20.1.4 to version 20.1.5"
+sed -i -e "s/version=139.0b7/version=139.0b8/g" nonfree/firefox/spkgbuild
+changelog "nonfree/firefox/spkgbuild" "Upgraded from version 139.0b7 to version 139.0b8"
+sed -i -e "s/version=1.38.15/version=1.38.16/g" python/python-botocore/spkgbuild
+changelog "python/python-botocore/spkgbuild" "Upgraded from version 1.38.15 to version 1.38.16"
+sed -i -e "s/version=2.8.0/version=2.8.1/g" python/python-cairosvg/spkgbuild
+changelog "python/python-cairosvg/spkgbuild" "Upgraded from version 2.8.0 to version 2.8.1"
+sed -i -e "s/version=2025.4.1/version=2025.5.0/g" python/python-distributed/spkgbuild
+changelog "python/python-distributed/spkgbuild" "Upgraded from version 2025.4.1 to version 2025.5.0"
+sed -i -e "s/version=37.1.0/version=37.3.0/g" python/python-faker/spkgbuild
+changelog "python/python-faker/spkgbuild" "Upgraded from version 37.1.0 to version 37.3.0"
+sed -i -e "s/version=80.5.0/version=80.6.0/g" python/python-setuptools/spkgbuild
+changelog "python/python-setuptools/spkgbuild" "Upgraded from version 80.5.0 to version 80.6.0"
+sed -i -e "s/version=4.6.8/version=4.6.9/g" python/python-trimesh/spkgbuild
+changelog "python/python-trimesh/spkgbuild" "Upgraded from version 4.6.8 to version 4.6.9"
+sed -i -e "s/version=0.15.3/version=0.15.4/g" python/python-typer/spkgbuild
+changelog "python/python-typer/spkgbuild" "Upgraded from version 0.15.3 to version 0.15.4"
+sed -i -e "s/version=1.8.0/version=1.9.0/g" python/python-xcffib/spkgbuild
+changelog "python/python-xcffib/spkgbuild" "Upgraded from version 1.8.0 to version 1.9.0"
