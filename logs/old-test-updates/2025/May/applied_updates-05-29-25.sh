9706e9aa8e859160dc41dfe410d24a572fd0539e
Voncloft <voncloft@gmail.com>
2025-05-29 10:31:29 -0400

    Minor Updates

---
+++ logs/old-test-updates/2025/May/applied_updates-05-29-25.sh
@@ -0,0 +1,42 @@
+sed -i -e "s/version=1.17/version=1.18/g" compilers/automake/spkgbuild
+changelog "compilers/automake/spkgbuild" "Upgraded from version 1.17 to version 1.18"
+sed -i -e "s/version=20.1.5/version=20.1.6/g" compilers/clang/spkgbuild
+changelog "compilers/clang/spkgbuild" "Upgraded from version 20.1.5 to version 20.1.6"
+sed -i -e "s/version=20.1.5/version=20.1.6/g" compilers/lld/spkgbuild
+changelog "compilers/lld/spkgbuild" "Upgraded from version 20.1.5 to version 20.1.6"
+sed -i -e "s/version=20.1.5/version=20.1.6/g" compilers/llvm/spkgbuild
+changelog "compilers/llvm/spkgbuild" "Upgraded from version 20.1.5 to version 20.1.6"
+sed -i -e "s/version=20.1.5/version=20.1.6/g" compilers/llvm-bak/spkgbuild
+changelog "compilers/llvm-bak/spkgbuild" "Upgraded from version 20.1.5 to version 20.1.6"
+sed -i -e "s/version=20.1.5/version=20.1.6/g" compilers/llvm-old-2023-06-07/spkgbuild
+changelog "compilers/llvm-old-2023-06-07/spkgbuild" "Upgraded from version 20.1.5 to version 20.1.6"
+sed -i -e "s/version=8.13.0/version=8.14.0/g" core/curl/spkgbuild
+changelog "core/curl/spkgbuild" "Upgraded from version 8.13.0 to version 8.14.0"
+sed -i -e "s/version=3.16.2/version=3.16.3/g" extra/flexget/spkgbuild
+changelog "extra/flexget/spkgbuild" "Upgraded from version 3.16.2 to version 3.16.3"
+sed -i -e "s/version=31.0/version=31.1/g" extra/protobuf/spkgbuild
+changelog "extra/protobuf/spkgbuild" "Upgraded from version 31.0 to version 31.1"
+sed -i -e "s/version=20.1.5/version=20.1.6/g" multilib/llvm-32/spkgbuild
+changelog "multilib/llvm-32/spkgbuild" "Upgraded from version 20.1.5 to version 20.1.6"
+sed -i -e "s/version=140.0b1/version=140.0b2/g" nonfree/firefox/spkgbuild
+changelog "nonfree/firefox/spkgbuild" "Upgraded from version 140.0b1 to version 140.0b2"
+sed -i -e "s/version=1.38.23/version=1.38.25/g" python/python-boto3/spkgbuild
+changelog "python/python-boto3/spkgbuild" "Upgraded from version 1.38.23 to version 1.38.25"
+sed -i -e "s/version=1.38.23/version=1.38.25/g" python/python-botocore/spkgbuild
+changelog "python/python-botocore/spkgbuild" "Upgraded from version 1.38.23 to version 1.38.25"
+sed -i -e "s/version=4.58.0/version=4.58.1/g" python/python-fonttools/spkgbuild
+changelog "python/python-fonttools/spkgbuild" "Upgraded from version 4.58.0 to version 4.58.1"
+sed -i -e "s/version=3.4.0/version=3.5.0/g" python/python-jose/spkgbuild
+changelog "python/python-jose/spkgbuild" "Upgraded from version 3.4.0 to version 3.5.0"
+sed -i -e "s/version=32.0.1/version=33.1.0/g" python/python-kubernetes/spkgbuild
+changelog "python/python-kubernetes/spkgbuild" "Upgraded from version 32.0.1 to version 33.1.0"
+sed -i -e "s/version=4.2.0/version=4.3.0/g" python/python-osprofiler/spkgbuild
+changelog "python/python-osprofiler/spkgbuild" "Upgraded from version 4.2.0 to version 4.3.0"
+sed -i -e "s/version=9.0.5/version=9.0.6/g" python/python-phonenumbers/spkgbuild
+changelog "python/python-phonenumbers/spkgbuild" "Upgraded from version 9.0.5 to version 9.0.6"
+sed -i -e "s/version=7.3/version=7.4/g" python/python-zope-testrunner/spkgbuild
+changelog "python/python-zope-testrunner/spkgbuild" "Upgraded from version 7.3 to version 7.4"
+sed -i -e "s/version=3.15.40/version=3.15.41/g" server/blacklist-hosts/spkgbuild
+changelog "server/blacklist-hosts/spkgbuild" "Upgraded from version 3.15.40 to version 3.15.41"
+sed -i -e "s/version=1.41.7.9799/version=1.41.7.9823/g" server/plex-media-server/spkgbuild
+changelog "server/plex-media-server/spkgbuild" "Upgraded from version 1.41.7.9799 to version 1.41.7.9823"
