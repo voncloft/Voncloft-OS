ec05e1ad9dc034592ead0d852d6e7267141271c1
Voncloft <voncloft@gmail.com>
2025-06-01 12:47:41 -0400

    Minor Updates

---
+++ logs/old-test-updates/2025/Jun/applied_updates-06-01-25.sh
@@ -0,0 +1,62 @@
+sed -i -e "s/version=2.8.5/version=2.8.6/g" core/enchant/spkgbuild
+changelog "core/enchant/spkgbuild" "Upgraded from version 2.8.5 to version 2.8.6"
+sed -i -e "s/version=0.147.6/version=0.147.7/g" core/hugo/spkgbuild
+changelog "core/hugo/spkgbuild" "Upgraded from version 0.147.6 to version 0.147.7"
+sed -i -e "s/version=1.4.3/version=1.4.4/g" core/pipewire/spkgbuild
+changelog "core/pipewire/spkgbuild" "Upgraded from version 1.4.3 to version 1.4.4"
+sed -i -e "s/version=257.5/version=257.6/g" core/systemd/spkgbuild
+changelog "core/systemd/spkgbuild" "Upgraded from version 257.5 to version 257.6"
+sed -i -e "s/version=2.49.1/version=2.49.2/g" core/webkitgtk/spkgbuild
+changelog "core/webkitgtk/spkgbuild" "Upgraded from version 2.49.1 to version 2.49.2"
+sed -i -e "s/version=1.26.1/version=1.26.2/g" media/gst-libav/spkgbuild
+changelog "media/gst-libav/spkgbuild" "Upgraded from version 1.26.1 to version 1.26.2"
+sed -i -e "s/version=1.26.1/version=1.26.2/g" media/gst-plugins-bad/spkgbuild
+changelog "media/gst-plugins-bad/spkgbuild" "Upgraded from version 1.26.1 to version 1.26.2"
+sed -i -e "s/version=1.26.1/version=1.26.2/g" media/gst-plugins-base/spkgbuild
+changelog "media/gst-plugins-base/spkgbuild" "Upgraded from version 1.26.1 to version 1.26.2"
+sed -i -e "s/version=1.26.1/version=1.26.2/g" media/gst-plugins-good/spkgbuild
+changelog "media/gst-plugins-good/spkgbuild" "Upgraded from version 1.26.1 to version 1.26.2"
+sed -i -e "s/version=1.26.1/version=1.26.2/g" media/gst-plugins-ugly/spkgbuild
+changelog "media/gst-plugins-ugly/spkgbuild" "Upgraded from version 1.26.1 to version 1.26.2"
+sed -i -e "s/version=1.26.1/version=1.26.2/g" media/gstreamer/spkgbuild
+changelog "media/gstreamer/spkgbuild" "Upgraded from version 1.26.1 to version 1.26.2"
+sed -i -e "s/version=1.8/version=1.8.1/g" libs/libconfig/spkgbuild
+changelog "libs/libconfig/spkgbuild" "Upgraded from version 1.8 to version 1.8.1"
+sed -i -e "s/version=140.0b2/version=140.0b3/g" nonfree/firefox/spkgbuild
+changelog "nonfree/firefox/spkgbuild" "Upgraded from version 140.0b2 to version 140.0b3"
+sed -i -e "s/version=575.51.02/version=575.57.08/g" nonfree/nvidia/spkgbuild
+changelog "nonfree/nvidia/spkgbuild" "Upgraded from version 575.51.02 to version 575.57.08"
+sed -i -e "s/version=575.51.02/version=575.57.08/g" nonfree/nvidia-32/spkgbuild
+changelog "nonfree/nvidia-32/spkgbuild" "Upgraded from version 575.51.02 to version 575.57.08"
+sed -i -e "s/version=575.51.02/version=575.57.08/g" nonfree/nvidia-modules/spkgbuild
+changelog "nonfree/nvidia-modules/spkgbuild" "Upgraded from version 575.51.02 to version 575.57.08"
+sed -i -e "s/version=3.7.0.1/version=3.7.0.2/g" nonfree/pandoc-bin/spkgbuild
+changelog "nonfree/pandoc-bin/spkgbuild" "Upgraded from version 3.7.0.1 to version 3.7.0.2"
+sed -i -e "s/version=1.26.20/version=1.26.24/g" nonfree/veracrypt/spkgbuild
+changelog "nonfree/veracrypt/spkgbuild" "Upgraded from version 1.26.20 to version 1.26.24"
+sed -i -e "s/version=3.49.2.0/version=3.50.0.0/g" python/python-apsw/spkgbuild
+changelog "python/python-apsw/spkgbuild" "Upgraded from version 3.49.2.0 to version 3.50.0.0"
+sed -i -e "s/version=24.6.0/version=25.5.0/g" python/python-awesomeversion/spkgbuild
+changelog "python/python-awesomeversion/spkgbuild" "Upgraded from version 24.6.0 to version 25.5.0"
+sed -i -e "s/version=1.38.25/version=1.38.27/g" python/python-boto3/spkgbuild
+changelog "python/python-boto3/spkgbuild" "Upgraded from version 1.38.25 to version 1.38.27"
+sed -i -e "s/version=1.38.25/version=1.38.27/g" python/python-botocore/spkgbuild
+changelog "python/python-botocore/spkgbuild" "Upgraded from version 1.38.25 to version 1.38.27"
+sed -i -e "s/version=4.35.0/version=4.36.0/g" python/python-braintree/spkgbuild
+changelog "python/python-braintree/spkgbuild" "Upgraded from version 4.35.0 to version 4.36.0"
+sed -i -e "s/version=6.131.30/version=6.131.33/g" python/python-hypothesis/spkgbuild
+changelog "python/python-hypothesis/spkgbuild" "Upgraded from version 6.131.30 to version 6.131.33"
+sed -i -e "s/version=2025.0.2/version=2025.0.3/g" python/python-pipenv/spkgbuild
+changelog "python/python-pipenv/spkgbuild" "Upgraded from version 2025.0.2 to version 2025.0.3"
+sed -i -e "s/version=1.6.1/version=1.7.0/g" python/python-pytest-datadir/spkgbuild
+changelog "python/python-pytest-datadir/spkgbuild" "Upgraded from version 1.6.1 to version 1.7.0"
+sed -i -e "s/version=2.7.0/version=2.8.0/g" python/python-pytest-regressions/spkgbuild
+changelog "python/python-pytest-regressions/spkgbuild" "Upgraded from version 2.7.0 to version 2.8.0"
+sed -i -e "s/version=1.0.6/version=1.0.8/g" python/python-rencode/spkgbuild
+changelog "python/python-rencode/spkgbuild" "Upgraded from version 1.0.6 to version 1.0.8"
+sed -i -e "s/version=0.46.2/version=0.47.0/g" python/python-starlette/spkgbuild
+changelog "python/python-starlette/spkgbuild" "Upgraded from version 0.46.2 to version 0.47.0"
+sed -i -e "s/version=2.0.1/version=2.1.0/g" python/python-warlock/spkgbuild
+changelog "python/python-warlock/spkgbuild" "Upgraded from version 2.0.1 to version 2.1.0"
+sed -i -e "s/version=13.2.1/version=13.3.0/g" ruby-gems/ruby-rake/spkgbuild
+changelog "ruby-gems/ruby-rake/spkgbuild" "Upgraded from version 13.2.1 to version 13.3.0"
