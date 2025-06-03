350d522c53f2279394abe3f6f9c173acc9e16c57
Voncloft <voncloft@gmail.com>
2025-06-03 11:00:20 -0400

    Minor Updates

---
+++ logs/old-test-updates/2025/Jun/applied_updates-06-03-25.sh
@@ -0,0 +1,44 @@
+sed -i -e "s/version=5.82/version=5.83/g" core/bluez/spkgbuild
+changelog "core/bluez/spkgbuild" "Upgraded from version 5.82 to version 5.83"
+sed -i -e "s/version=2.5.6/version=2.5.7/g" core/gnupg/spkgbuild
+changelog "core/gnupg/spkgbuild" "Upgraded from version 2.5.6 to version 2.5.7"
+sed -i -e "s/version=1.7.1/version=1.8.0/g" core/jq/spkgbuild
+changelog "core/jq/spkgbuild" "Upgraded from version 1.7.1 to version 1.8.0"
+sed -i -e "s/version=3.8.0/version=3.8.1/g" libs/libarchive/spkgbuild
+changelog "libs/libarchive/spkgbuild" "Upgraded from version 3.8.0 to version 3.8.1"
+sed -i -e "s/version=1.0.28/version=1.0.29/g" libs/libusb/spkgbuild
+changelog "libs/libusb/spkgbuild" "Upgraded from version 1.0.28 to version 1.0.29"
+sed -i -e "s/version=140.0b3/version=140.0b4/g" nonfree/firefox/spkgbuild
+changelog "nonfree/firefox/spkgbuild" "Upgraded from version 140.0b3 to version 140.0b4"
+sed -i -e "s/version=139.0b4/version=139.0.1/g" nonfree/thunderbird/spkgbuild
+changelog "nonfree/thunderbird/spkgbuild" "Upgraded from version 139.0b4 to version 139.0.1"
+sed -i -e "s/version=1.38.27/version=1.38.28/g" python/python-boto3/spkgbuild
+changelog "python/python-boto3/spkgbuild" "Upgraded from version 1.38.27 to version 1.38.28"
+sed -i -e "s/version=1.38.27/version=1.38.28/g" python/python-botocore/spkgbuild
+changelog "python/python-botocore/spkgbuild" "Upgraded from version 1.38.27 to version 1.38.28"
+sed -i -e "s/version=1.35.3/version=1.35.4/g" python/python-cfn-lint/spkgbuild
+changelog "python/python-cfn-lint/spkgbuild" "Upgraded from version 1.35.3 to version 1.35.4"
+sed -i -e "s/version=65.8.1/version=65.9.0/g" python/python-django-allauth/spkgbuild
+changelog "python/python-django-allauth/spkgbuild" "Upgraded from version 65.8.1 to version 65.9.0"
+sed -i -e "s/version=2.5.1/version=2.6.0/g" python/python-gphoto2/spkgbuild
+changelog "python/python-gphoto2/spkgbuild" "Upgraded from version 2.5.1 to version 2.6.0"
+sed -i -e "s/version=6.131.33/version=6.133.0/g" python/python-hypothesis/spkgbuild
+changelog "python/python-hypothesis/spkgbuild" "Upgraded from version 6.131.33 to version 6.133.0"
+sed -i -e "s/version=5.5.3/version=5.5.4/g" python/python-kombu/spkgbuild
+changelog "python/python-kombu/spkgbuild" "Upgraded from version 5.5.3 to version 5.5.4"
+sed -i -e "s/version=0.33.0/version=0.34.0/g" python/python-owslib/spkgbuild
+changelog "python/python-owslib/spkgbuild" "Upgraded from version 0.33.0 to version 0.34.0"
+sed -i -e "s/version=0.22.0/version=0.22.1/g" python/python-prometheus_client/spkgbuild
+changelog "python/python-prometheus_client/spkgbuild" "Upgraded from version 0.22.0 to version 0.22.1"
+sed -i -e "s/version=7.3.2505161209/version=7.3.2506011618/g" python/python-pycontracts/spkgbuild
+changelog "python/python-pycontracts/spkgbuild" "Upgraded from version 7.3.2505161209 to version 7.3.2506011618"
+sed -i -e "s/version=6.0.2/version=6.1.0/g" python/python-pynamodb/spkgbuild
+changelog "python/python-pynamodb/spkgbuild" "Upgraded from version 6.0.2 to version 6.1.0"
+sed -i -e "s/version=1.7.0/version=1.7.1/g" python/python-pytest-datadir/spkgbuild
+changelog "python/python-pytest-datadir/spkgbuild" "Upgraded from version 1.7.0 to version 1.7.1"
+sed -i -e "s/version=25.3.0/version=25.4.0/g" python/python-structlog/spkgbuild
+changelog "python/python-structlog/spkgbuild" "Upgraded from version 25.3.0 to version 25.4.0"
+sed -i -e "s/version=4.1.1/version=4.2.0/g" python/python-uritemplate/spkgbuild
+changelog "python/python-uritemplate/spkgbuild" "Upgraded from version 4.1.1 to version 4.2.0"
+sed -i -e "s/version=3.15.41/version=3.15.42/g" server/blacklist-hosts/spkgbuild
+changelog "server/blacklist-hosts/spkgbuild" "Upgraded from version 3.15.41 to version 3.15.42"
