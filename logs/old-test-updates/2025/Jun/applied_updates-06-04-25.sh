a1bd4a3047fbf0979346a7cf0e6e0ea158dbb9cc
Voncloft <voncloft@gmail.com>
2025-06-04 07:19:08 -0400

    Minor Updates

---
+++ logs/old-test-updates/2025/Jun/applied_updates-06-04-25.sh
@@ -0,0 +1,16 @@
+sed -i -e "s/version=2.32.6/version=2.32.8/g" libs/sdl2/spkgbuild
+changelog "libs/sdl2/spkgbuild" "Upgraded from version 2.32.6 to version 2.32.8"
+sed -i -e "s/version=1.38.28/version=1.38.29/g" python/python-boto3/spkgbuild
+changelog "python/python-boto3/spkgbuild" "Upgraded from version 1.38.28 to version 1.38.29"
+sed -i -e "s/version=1.38.28/version=1.38.29/g" python/python-botocore/spkgbuild
+changelog "python/python-botocore/spkgbuild" "Upgraded from version 1.38.28 to version 1.38.29"
+sed -i -e "s/version=2.170.0/version=2.171.0/g" python/python-google-api-python-client/spkgbuild
+changelog "python/python-google-api-python-client/spkgbuild" "Upgraded from version 2.170.0 to version 2.171.0"
+sed -i -e "s/version=2.6.0/version=2.6.1/g" python/python-gphoto2/spkgbuild
+changelog "python/python-gphoto2/spkgbuild" "Upgraded from version 2.6.0 to version 2.6.1"
+sed -i -e "s/version=6.133.0/version=6.133.2/g" python/python-hypothesis/spkgbuild
+changelog "python/python-hypothesis/spkgbuild" "Upgraded from version 6.133.0 to version 6.133.2"
+sed -i -e "s/version=4.5.0/version=4.6.0/g" python/python-openstacksdk/spkgbuild
+changelog "python/python-openstacksdk/spkgbuild" "Upgraded from version 4.5.0 to version 4.6.0"
+sed -i -e "s/version=24.9.1/version=25.5.0/g" python/python-treq/spkgbuild
+changelog "python/python-treq/spkgbuild" "Upgraded from version 24.9.1 to version 25.5.0"
