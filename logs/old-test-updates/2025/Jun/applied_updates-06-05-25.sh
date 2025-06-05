5ecb685206383a9e33bc09048dcd7103425f587a
Voncloft <voncloft@gmail.com>
2025-06-05 07:37:29 -0400

    Minor Updates

---
+++ logs/old-test-updates/2025/Jun/applied_updates-06-05-25.sh
@@ -0,0 +1,34 @@
+sed -i -e "s/version=25.1.1/version=25.1.2/g" hardware/mesa/spkgbuild
+changelog "hardware/mesa/spkgbuild" "Upgraded from version 25.1.1 to version 25.1.2"
+sed -i -e "s/version=8.14.0/version=8.14.1/g" core/curl/spkgbuild
+changelog "core/curl/spkgbuild" "Upgraded from version 8.14.0 to version 8.14.1"
+sed -i -e "s/version=6.15/version=6.15.1/g" core/linux-api-headers/spkgbuild
+changelog "core/linux-api-headers/spkgbuild" "Upgraded from version 6.15 to version 6.15.1"
+sed -i -e "s/version=6.15/version=6.15.1/g" core/linux-desktop/spkgbuild
+changelog "core/linux-desktop/spkgbuild" "Upgraded from version 6.15 to version 6.15.1"
+sed -i -e "s/version=6.15/version=6.15.1/g" core/linux-router/spkgbuild
+changelog "core/linux-router/spkgbuild" "Upgraded from version 6.15 to version 6.15.1"
+sed -i -e "s/version=5.4.7/version=5.4.8/g" core/lua/spkgbuild
+changelog "core/lua/spkgbuild" "Upgraded from version 5.4.7 to version 5.4.8"
+sed -i -e "s/version=1.4.4/version=1.4.5/g" core/pipewire/spkgbuild
+changelog "core/pipewire/spkgbuild" "Upgraded from version 1.4.4 to version 1.4.5"
+sed -i -e "s/version=25.05.0/version=25.06.0/g" libs/poppler/spkgbuild
+changelog "libs/poppler/spkgbuild" "Upgraded from version 25.05.0 to version 25.06.0"
+sed -i -e "s/version=1.16/version=1.16.1/g" networking/make-ca/spkgbuild
+changelog "networking/make-ca/spkgbuild" "Upgraded from version 1.16 to version 1.16.1"
+sed -i -e "s/version=1.97.0/version=1.98.0/g" python/python-aws-sam-translator/spkgbuild
+changelog "python/python-aws-sam-translator/spkgbuild" "Upgraded from version 1.97.0 to version 1.98.0"
+sed -i -e "s/version=1.38.29/version=1.38.30/g" python/python-boto3/spkgbuild
+changelog "python/python-boto3/spkgbuild" "Upgraded from version 1.38.29 to version 1.38.30"
+sed -i -e "s/version=1.38.29/version=1.38.30/g" python/python-botocore/spkgbuild
+changelog "python/python-botocore/spkgbuild" "Upgraded from version 1.38.29 to version 1.38.30"
+sed -i -e "s/version=4.12.1/version=4.12.2/g" python/python-engineio/spkgbuild
+changelog "python/python-engineio/spkgbuild" "Upgraded from version 4.12.1 to version 4.12.2"
+sed -i -e "s/version=5.6.0/version=6.0.0/g" python/python-gitlab/spkgbuild
+changelog "python/python-gitlab/spkgbuild" "Upgraded from version 5.6.0 to version 6.0.0"
+sed -i -e "s/version=2.40.2/version=2.40.3/g" python/python-google-auth/spkgbuild
+changelog "python/python-google-auth/spkgbuild" "Upgraded from version 2.40.2 to version 2.40.3"
+sed -i -e "s/version=6.133.2/version=6.135.0/g" python/python-hypothesis/spkgbuild
+changelog "python/python-hypothesis/spkgbuild" "Upgraded from version 6.133.2 to version 6.135.0"
+sed -i -e "s/version=15.0.3/version=15.0.4/g" python/python-pdoc/spkgbuild
+changelog "python/python-pdoc/spkgbuild" "Upgraded from version 15.0.3 to version 15.0.4"
