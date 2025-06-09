072fb232d7f015039ec5c00f1bcfc1442bdef001
Voncloft <voncloft@gmail.com>
2025-06-09 10:15:53 -0400

    Minor Updates

---
+++ logs/old-test-updates/2025/Jun/applied_updates-06-09-25.sh
@@ -0,0 +1,12 @@
+sed -i -e "s/version=7.3.0/version=7.4.0/g" core/streamlink/spkgbuild
+changelog "core/streamlink/spkgbuild" "Upgraded from version 7.3.0 to version 7.4.0"
+sed -i -e "s/version=2.4.124/version=2.4.125/g" libs/libdrm/spkgbuild
+changelog "libs/libdrm/spkgbuild" "Upgraded from version 2.4.124 to version 2.4.125"
+sed -i -e "s/version=6.135.1/version=6.135.2/g" python/python-hypothesis/spkgbuild
+changelog "python/python-hypothesis/spkgbuild" "Upgraded from version 6.135.1 to version 6.135.2"
+sed -i -e "s/version=0.7.2/version=0.8.0/g" python/python-openapi-spec-validator/spkgbuild
+changelog "python/python-openapi-spec-validator/spkgbuild" "Upgraded from version 0.7.2 to version 0.8.0"
+sed -i -e "s/version=3.22.0/version=3.23.0/g" python/python-zipp/spkgbuild
+changelog "python/python-zipp/spkgbuild" "Upgraded from version 3.22.0 to version 3.23.0"
+sed -i -e "s/version=2.44/version=2.45/g" xorg/xkeyboard-config/spkgbuild
+changelog "xorg/xkeyboard-config/spkgbuild" "Upgraded from version 2.44 to version 2.45"
