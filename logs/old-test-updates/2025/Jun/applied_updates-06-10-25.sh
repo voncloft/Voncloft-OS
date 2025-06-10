362eac5565ef66fe0e44277063fee5f2d74c48ba
Voncloft <voncloft@gmail.com>
2025-06-10 06:59:32 -0400

    Minor Updates

---
+++ logs/old-test-updates/2025/Jun/applied_updates-06-10-25.sh
@@ -0,0 +1,20 @@
+sed -i -e "s/version=1.9.8/version=1.9.16/g" core/sudo/spkgbuild
+changelog "core/sudo/spkgbuild" "Upgraded from version 1.9.8 to version 1.9.16"
+sed -i -e "s/version=2.45/version=2.46/g" extra/discordchatexporter/spkgbuild
+changelog "extra/discordchatexporter/spkgbuild" "Upgraded from version 2.45 to version 2.46"
+sed -i -e "s/version=3.16.3/version=3.16.4/g" extra/flexget/spkgbuild
+changelog "extra/flexget/spkgbuild" "Upgraded from version 3.16.3 to version 3.16.4"
+sed -i -e "s/version=3.3.3/version=3.3.4/g" libs/openexr/spkgbuild
+changelog "libs/openexr/spkgbuild" "Upgraded from version 3.3.3 to version 3.3.4"
+sed -i -e "s/version=140.0b6/version=140.0b7/g" nonfree/firefox/spkgbuild
+changelog "nonfree/firefox/spkgbuild" "Upgraded from version 140.0b6 to version 140.0b7"
+sed -i -e "s/version=1.38.32/version=1.38.33/g" python/python-boto3/spkgbuild
+changelog "python/python-boto3/spkgbuild" "Upgraded from version 1.38.32 to version 1.38.33"
+sed -i -e "s/version=1.38.32/version=1.38.33/g" python/python-botocore/spkgbuild
+changelog "python/python-botocore/spkgbuild" "Upgraded from version 1.38.32 to version 1.38.33"
+sed -i -e "s/version=6.135.2/version=6.135.4/g" python/python-hypothesis/spkgbuild
+changelog "python/python-hypothesis/spkgbuild" "Upgraded from version 6.135.2 to version 6.135.4"
+sed -i -e "s/version=9.0.6/version=9.0.7/g" python/python-phonenumbers/spkgbuild
+changelog "python/python-phonenumbers/spkgbuild" "Upgraded from version 9.0.6 to version 9.0.7"
+sed -i -e "s/version=2.32.3/version=2.32.4/g" python/python-requests/spkgbuild
+changelog "python/python-requests/spkgbuild" "Upgraded from version 2.32.3 to version 2.32.4"
