e7711bc0f724b116a236563a91feb6830a3eddd4
Voncloft <voncloft@gmail.com>
2025-06-11 10:15:12 -0400

    Minor Updates

---
+++ logs/old-test-updates/2025/Jun/applied_updates-06-11-25.sh
@@ -0,0 +1,32 @@
+sed -i -e "s/version=1.8.1/version=1.8.2/g" compilers/meson/spkgbuild
+changelog "compilers/meson/spkgbuild" "Upgraded from version 1.8.1 to version 1.8.2"
+sed -i -e "s/version=6.15.1/version=6.15.2/g" core/linux-api-headers/spkgbuild
+changelog "core/linux-api-headers/spkgbuild" "Upgraded from version 6.15.1 to version 6.15.2"
+sed -i -e "s/version=6.15.1/version=6.15.2/g" core/linux-desktop/spkgbuild
+changelog "core/linux-desktop/spkgbuild" "Upgraded from version 6.15.1 to version 6.15.2"
+sed -i -e "s/version=6.15.1/version=6.15.2/g" core/linux-router/spkgbuild
+changelog "core/linux-router/spkgbuild" "Upgraded from version 6.15.1 to version 6.15.2"
+sed -i -e "s/version=0.46.0/version=0.46.2/g" core/pixman/spkgbuild
+changelog "core/pixman/spkgbuild" "Upgraded from version 0.46.0 to version 0.46.2"
+sed -i -e "s/version=3.16.4/version=3.16.5/g" extra/flexget/spkgbuild
+changelog "extra/flexget/spkgbuild" "Upgraded from version 3.16.4 to version 3.16.5"
+sed -i -e "s/version=2025.05.22/version=2025.06.09/g" networking/yt-dlp/spkgbuild
+changelog "networking/yt-dlp/spkgbuild" "Upgraded from version 2025.05.22 to version 2025.06.09"
+sed -i -e "s/version=139.0.1/version=140.0b1/g" nonfree/thunderbird/spkgbuild
+changelog "nonfree/thunderbird/spkgbuild" "Upgraded from version 139.0.1 to version 140.0b1"
+sed -i -e "s/version=1.38.33/version=1.38.34/g" python/python-boto3/spkgbuild
+changelog "python/python-boto3/spkgbuild" "Upgraded from version 1.38.33 to version 1.38.34"
+sed -i -e "s/version=1.38.33/version=1.38.34/g" python/python-botocore/spkgbuild
+changelog "python/python-botocore/spkgbuild" "Upgraded from version 1.38.33 to version 1.38.34"
+sed -i -e "s/version=1.2.69/version=3.0.0/g" python/python-cloudscraper/spkgbuild
+changelog "python/python-cloudscraper/spkgbuild" "Upgraded from version 1.2.69 to version 3.0.0"
+sed -i -e "s/version=2.171.0/version=2.172.0/g" python/python-google-api-python-client/spkgbuild
+changelog "python/python-google-api-python-client/spkgbuild" "Upgraded from version 2.171.0 to version 2.172.0"
+sed -i -e "s/version=2.5.2/version=2.5.3/g" python/python-hcloud/spkgbuild
+changelog "python/python-hcloud/spkgbuild" "Upgraded from version 2.5.2 to version 2.5.3"
+sed -i -e "s/version=1.8.0/version=1.8.1/g" python/python-libcst/spkgbuild
+changelog "python/python-libcst/spkgbuild" "Upgraded from version 1.8.0 to version 1.8.1"
+sed -i -e "s/version=3.13.7/version=3.13.8/g" python/python-mocket/spkgbuild
+changelog "python/python-mocket/spkgbuild" "Upgraded from version 3.13.7 to version 3.13.8"
+sed -i -e "s/version=1.20.0/version=1.21.1/g" python/python-yarl/spkgbuild
+changelog "python/python-yarl/spkgbuild" "Upgraded from version 1.20.0 to version 1.21.1"
