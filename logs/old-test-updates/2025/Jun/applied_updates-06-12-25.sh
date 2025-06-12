d0a2ea0c9d7dc80be48489715180f213b98ee85d
Voncloft <voncloft@gmail.com>
2025-06-12 05:39:22 -0400

    Minor Updates

---
+++ logs/old-test-updates/2025/Jun/applied_updates-06-12-25.sh
@@ -0,0 +1,16 @@
+sed -i -e "s/version=7.3/version=7.4/g" core/cifs-utils/spkgbuild
+changelog "core/cifs-utils/spkgbuild" "Upgraded from version 7.3 to version 7.4"
+sed -i -e "s/version=140.0b7/version=140.0b8/g" nonfree/firefox/spkgbuild
+changelog "nonfree/firefox/spkgbuild" "Upgraded from version 140.0b7 to version 140.0b8"
+sed -i -e "s/version=140.0b1/version=140.0b2/g" nonfree/thunderbird/spkgbuild
+changelog "nonfree/thunderbird/spkgbuild" "Upgraded from version 140.0b1 to version 140.0b2"
+sed -i -e "s/version=1.38.34/version=1.38.35/g" python/python-boto3/spkgbuild
+changelog "python/python-boto3/spkgbuild" "Upgraded from version 1.38.34 to version 1.38.35"
+sed -i -e "s/version=1.38.34/version=1.38.35/g" python/python-botocore/spkgbuild
+changelog "python/python-botocore/spkgbuild" "Upgraded from version 1.38.34 to version 1.38.35"
+sed -i -e "s/version=37.3.0/version=37.4.0/g" python/python-faker/spkgbuild
+changelog "python/python-faker/spkgbuild" "Upgraded from version 37.3.0 to version 37.4.0"
+sed -i -e "s/version=6.135.4/version=6.135.6/g" python/python-hypothesis/spkgbuild
+changelog "python/python-hypothesis/spkgbuild" "Upgraded from version 6.135.4 to version 6.135.6"
+sed -i -e "s/version=2.2.0/version=2.2.1/g" python/python-param/spkgbuild
+changelog "python/python-param/spkgbuild" "Upgraded from version 2.2.0 to version 2.2.1"
