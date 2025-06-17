e3e319c292947546e2bea83d87f8a3a230c8c2c0
Voncloft <voncloft@gmail.com>
2025-06-17 08:44:06 -0400

    Minor Updates

---
+++ logs/old-test-updates/2025/Jun/applied_updates-06-17-25.sh
@@ -0,0 +1,20 @@
+sed -i -e "s/version=2.8.6/version=2.8.7/g" core/enchant/spkgbuild
+changelog "core/enchant/spkgbuild" "Upgraded from version 2.8.6 to version 2.8.7"
+sed -i -e "s/version=show/version=test/g" extra/anki/spkgbuild
+changelog "extra/anki/spkgbuild" "Upgraded from version show to version test"
+sed -i -e "s/version=3.16.5/version=3.16.6/g" extra/flexget/spkgbuild
+changelog "extra/flexget/spkgbuild" "Upgraded from version 3.16.5 to version 3.16.6"
+sed -i -e "s/version=1.3.5/version=1.3.6/g" libs/libogg/spkgbuild
+changelog "libs/libogg/spkgbuild" "Upgraded from version 1.3.5 to version 1.3.6"
+sed -i -e "s/version=2.16.0/version=2.16.1/g" libs/libwacom/spkgbuild
+changelog "libs/libwacom/spkgbuild" "Upgraded from version 2.16.0 to version 2.16.1"
+sed -i -e "s/version=2.49.0/version=2.50.0/g" networking/git/spkgbuild
+changelog "networking/git/spkgbuild" "Upgraded from version 2.49.0 to version 2.50.0"
+sed -i -e "s/version=1.38.36/version=1.38.37/g" python/python-boto3/spkgbuild
+changelog "python/python-boto3/spkgbuild" "Upgraded from version 1.38.36 to version 1.38.37"
+sed -i -e "s/version=1.38.36/version=1.38.37/g" python/python-botocore/spkgbuild
+changelog "python/python-botocore/spkgbuild" "Upgraded from version 1.38.36 to version 1.38.37"
+sed -i -e "s/version=6.0.0/version=6.1.0/g" python/python-cachetools/spkgbuild
+changelog "python/python-cachetools/spkgbuild" "Upgraded from version 6.0.0 to version 6.1.0"
+sed -i -e "s/version=3.15.45/version=3.15.46/g" server/blacklist-hosts/spkgbuild
+changelog "server/blacklist-hosts/spkgbuild" "Upgraded from version 3.15.45 to version 3.15.46"
