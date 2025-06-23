f6f9f81819179b5a1bdc2b07a2e68440815d1f31
Voncloft <voncloft@gmail.com>
2025-06-23 13:26:58 -0400

    Minor Updates

---
+++ logs/old-test-updates/2025/Jun/applied_updates-06-23-25.sh
@@ -0,0 +1,68 @@
+sed -i -e "s/version=25.1.3/version=25.1.4/g" hardware/mesa/spkgbuild
+changelog "hardware/mesa/spkgbuild" "Upgraded from version 25.1.3 to version 25.1.4"
+sed -i -e "s/version=2.8.8/version=2.8.10/g" core/enchant/spkgbuild
+changelog "core/enchant/spkgbuild" "Upgraded from version 2.8.8 to version 2.8.10"
+sed -i -e "s/version=2.5.7/version=2.5.8/g" core/gnupg/spkgbuild
+changelog "core/gnupg/spkgbuild" "Upgraded from version 2.5.7 to version 2.5.8"
+sed -i -e "s/version=6.15.2/version=6.15.3/g" core/linux-api-headers/spkgbuild
+changelog "core/linux-api-headers/spkgbuild" "Upgraded from version 6.15.2 to version 6.15.3"
+sed -i -e "s/version=6.15.2/version=6.15.3/g" core/linux-desktop/spkgbuild
+changelog "core/linux-desktop/spkgbuild" "Upgraded from version 6.15.2 to version 6.15.3"
+sed -i -e "s/version=6.15.2/version=6.15.3/g" core/linux-router/spkgbuild
+changelog "core/linux-router/spkgbuild" "Upgraded from version 6.15.2 to version 6.15.3"
+sed -i -e "s/version=3.112/version=3.113/g" core/nss/spkgbuild
+changelog "core/nss/spkgbuild" "Upgraded from version 3.112 to version 3.113"
+sed -i -e "s/version=3.13.0/version=3.14.0/g" core/pciutils/spkgbuild
+changelog "core/pciutils/spkgbuild" "Upgraded from version 3.13.0 to version 3.14.0"
+sed -i -e "s/version=1.9.16/version=1.9.17/g" core/sudo/spkgbuild
+changelog "core/sudo/spkgbuild" "Upgraded from version 1.9.16 to version 1.9.17"
+sed -i -e "s/version=3.16.6/version=3.16.7/g" extra/flexget/spkgbuild
+changelog "extra/flexget/spkgbuild" "Upgraded from version 3.16.6 to version 3.16.7"
+sed -i -e "s/version=2.2.0/version=2.2.1/g" lxqt/lxqt-build-tools/spkgbuild
+changelog "lxqt/lxqt-build-tools/spkgbuild" "Upgraded from version 2.2.0 to version 2.2.1"
+sed -i -e "s/version=7.0.0/version=7.1.0/g" media/libcec/spkgbuild
+changelog "media/libcec/spkgbuild" "Upgraded from version 7.0.0 to version 7.1.0"
+sed -i -e "s/version=3.3.0/version=3.3.1/g" libs/libblockdev/spkgbuild
+changelog "libs/libblockdev/spkgbuild" "Upgraded from version 3.3.0 to version 3.3.1"
+sed -i -e "s/version=24.1.7/version=24.1.8/g" libs/xwayland/spkgbuild
+changelog "libs/xwayland/spkgbuild" "Upgraded from version 24.1.7 to version 24.1.8"
+sed -i -e "s/version=9.21.8/version=9.21.9/g" networking/bind/spkgbuild
+changelog "networking/bind/spkgbuild" "Upgraded from version 9.21.8 to version 9.21.9"
+sed -i -e "s/version=1.98.0/version=1.99.0/g" python/python-aws-sam-translator/spkgbuild
+changelog "python/python-aws-sam-translator/spkgbuild" "Upgraded from version 1.98.0 to version 1.99.0"
+sed -i -e "s/version=1.38.37/version=1.38.41/g" python/python-boto3/spkgbuild
+changelog "python/python-boto3/spkgbuild" "Upgraded from version 1.38.37 to version 1.38.41"
+sed -i -e "s/version=1.38.38/version=1.38.41/g" python/python-botocore/spkgbuild
+changelog "python/python-botocore/spkgbuild" "Upgraded from version 1.38.38 to version 1.38.41"
+sed -i -e "s/version=1.36.0/version=1.36.1/g" python/python-cfn-lint/spkgbuild
+changelog "python/python-cfn-lint/spkgbuild" "Upgraded from version 1.36.0 to version 1.36.1"
+sed -i -e "s/version=2.172.0/version=2.173.0/g" python/python-google-api-python-client/spkgbuild
+changelog "python/python-google-api-python-client/spkgbuild" "Upgraded from version 2.172.0 to version 2.173.0"
+sed -i -e "s/version=6.135.10/version=6.135.14/g" python/python-hypothesis/spkgbuild
+changelog "python/python-hypothesis/spkgbuild" "Upgraded from version 6.135.10 to version 6.135.14"
+sed -i -e "s/version=0.35/version=0.36.0/g" python/python-meilisearch/spkgbuild
+changelog "python/python-meilisearch/spkgbuild" "Upgraded from version 0.35 to version 0.36.0"
+sed -i -e "s/version=2.13.0/version=2.14.0/g" python/python-pycodestyle/spkgbuild
+changelog "python/python-pycodestyle/spkgbuild" "Upgraded from version 2.13.0 to version 2.14.0"
+sed -i -e "s/version=4.0.0/version=4.0.1/g" python/python-pydot/spkgbuild
+changelog "python/python-pydot/spkgbuild" "Upgraded from version 4.0.0 to version 4.0.1"
+sed -i -e "s/version=2.0.1/version=2.0.2/g" python/python-smartypants/spkgbuild
+changelog "python/python-smartypants/spkgbuild" "Upgraded from version 2.0.1 to version 2.0.2"
+sed -i -e "s/version=0.47.0/version=0.47.1/g" python/python-starlette/spkgbuild
+changelog "python/python-starlette/spkgbuild" "Upgraded from version 0.47.0 to version 0.47.1"
+sed -i -e "s/version=5.8.0/version=5.8.1/g" python/python-tempora/spkgbuild
+changelog "python/python-tempora/spkgbuild" "Upgraded from version 5.8.0 to version 5.8.1"
+sed -i -e "s/version=4.15.0/version=4.16.0/g" python/python-tweepy/spkgbuild
+changelog "python/python-tweepy/spkgbuild" "Upgraded from version 4.15.0 to version 4.16.0"
+sed -i -e "s/version=4.4.3/version=4.4.4/g" python/python-typeguard/spkgbuild
+changelog "python/python-typeguard/spkgbuild" "Upgraded from version 4.4.3 to version 4.4.4"
+sed -i -e "s/version=2.4.0/version=2.5.0/g" python/python-urllib3/spkgbuild
+changelog "python/python-urllib3/spkgbuild" "Upgraded from version 2.4.0 to version 2.5.0"
+sed -i -e "s/version=3.2.3/version=3.2.5/g" python/python-xlsxwriter/spkgbuild
+changelog "python/python-xlsxwriter/spkgbuild" "Upgraded from version 3.2.3 to version 3.2.5"
+sed -i -e "s/version=3.15.46/version=3.15.48/g" server/blacklist-hosts/spkgbuild
+changelog "server/blacklist-hosts/spkgbuild" "Upgraded from version 3.15.46 to version 3.15.48"
+sed -i -e "s/version=0.7.2/version=0.8.0/g" xfce/xfburn/spkgbuild
+changelog "xfce/xfburn/spkgbuild" "Upgraded from version 0.7.2 to version 0.8.0"
+sed -i -e "s/version=21.1.17/version=21.1.18/g" xorg/xorg-server/spkgbuild
+changelog "xorg/xorg-server/spkgbuild" "Upgraded from version 21.1.17 to version 21.1.18"
