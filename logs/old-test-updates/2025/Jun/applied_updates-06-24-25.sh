8ccf13b64871ec4e3d1d7acbc8cbdfc3d5442636
Voncloft <voncloft@gmail.com>
2025-06-24 05:41:22 -0400

    Minor Updates

---
+++ logs/old-test-updates/2025/Jun/applied_updates-06-24-25.sh
@@ -0,0 +1,30 @@
+sed -i -e "s/version=1.12.1/version=1.13.0/g" compilers/ninja/spkgbuild
+changelog "compilers/ninja/spkgbuild" "Upgraded from version 1.12.1 to version 1.13.0"
+sed -i -e "s/version=2.4.3/version=2.5.0/g" compilers/pkgconf/spkgbuild
+changelog "compilers/pkgconf/spkgbuild" "Upgraded from version 2.4.3 to version 2.5.0"
+sed -i -e "s/version=6.14/version=6.15/g" core/btrfs-progs/spkgbuild
+changelog "core/btrfs-progs/spkgbuild" "Upgraded from version 6.14 to version 6.15"
+sed -i -e "s/version=1.4.7/version=1.4.8/g" core/colord/spkgbuild
+changelog "core/colord/spkgbuild" "Upgraded from version 1.4.7 to version 1.4.8"
+sed -i -e "s/version=6.14/version=6.15/g" core/ethtool/spkgbuild
+changelog "core/ethtool/spkgbuild" "Upgraded from version 6.14 to version 6.15"
+sed -i -e "s/version=0.147.8/version=0.147.9/g" core/hugo/spkgbuild
+changelog "core/hugo/spkgbuild" "Upgraded from version 0.147.8 to version 0.147.9"
+sed -i -e "s/version=3.7.5/version=3.8.0/g" core/icewm/spkgbuild
+changelog "core/icewm/spkgbuild" "Upgraded from version 3.7.5 to version 3.8.0"
+sed -i -e "s/version=3.16.7/version=3.16.8/g" extra/flexget/spkgbuild
+changelog "extra/flexget/spkgbuild" "Upgraded from version 3.16.7 to version 3.16.8"
+sed -i -e "s/version=2.2.1/version=2.2.2/g" lxqt/lxqt-panel/spkgbuild
+changelog "lxqt/lxqt-panel/spkgbuild" "Upgraded from version 2.2.1 to version 2.2.2"
+sed -i -e "s/version=7.1.0/version=7.1.1/g" media/libcec/spkgbuild
+changelog "media/libcec/spkgbuild" "Upgraded from version 7.1.0 to version 7.1.1"
+sed -i -e "s/version=140.0b9/version=140.0esr/g" nonfree/firefox/spkgbuild
+changelog "nonfree/firefox/spkgbuild" "Upgraded from version 140.0b9 to version 140.0esr"
+sed -i -e "s/version=1.38.41/version=1.38.42/g" python/python-boto3/spkgbuild
+changelog "python/python-boto3/spkgbuild" "Upgraded from version 1.38.41 to version 1.38.42"
+sed -i -e "s/version=1.38.41/version=1.38.42/g" python/python-botocore/spkgbuild
+changelog "python/python-botocore/spkgbuild" "Upgraded from version 1.38.41 to version 1.38.42"
+sed -i -e "s/version=5.0.1/version=5.0.2/g" python/python-elementpath/spkgbuild
+changelog "python/python-elementpath/spkgbuild" "Upgraded from version 5.0.1 to version 5.0.2"
+sed -i -e "s/version=2025.6.1/version=2025.6.2/g" server/homeassistant/spkgbuild
+changelog "server/homeassistant/spkgbuild" "Upgraded from version 2025.6.1 to version 2025.6.2"
