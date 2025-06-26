ae42d4f80ce53007b4e10546600d697da8269c40
Voncloft <voncloft@gmail.com>
2025-06-26 10:26:34 -0400

    Minor Updates

---
+++ logs/old-test-updates/2025/Jun/applied_updates-06-26-25.sh
@@ -0,0 +1,26 @@
+sed -i -e "s/version=2.5.0/version=2.5.1/g" compilers/pkgconf/spkgbuild
+changelog "compilers/pkgconf/spkgbuild" "Upgraded from version 2.5.0 to version 2.5.1"
+sed -i -e "s/version=4.17.4/version=4.18.0/g" core/shadow/spkgbuild
+changelog "core/shadow/spkgbuild" "Upgraded from version 4.17.4 to version 4.18.0"
+sed -i -e "s/version=257.6/version=257.7/g" core/systemd/spkgbuild
+changelog "core/systemd/spkgbuild" "Upgraded from version 257.6 to version 257.7"
+sed -i -e "s/version=141.0b1/version=141.0b2/g" nonfree/firefox/spkgbuild
+changelog "nonfree/firefox/spkgbuild" "Upgraded from version 141.0b1 to version 141.0b2"
+sed -i -e "s/version=1.38.43/version=1.38.44/g" python/python-boto3/spkgbuild
+changelog "python/python-boto3/spkgbuild" "Upgraded from version 1.38.43 to version 1.38.44"
+sed -i -e "s/version=1.38.43/version=1.38.44/g" python/python-botocore/spkgbuild
+changelog "python/python-botocore/spkgbuild" "Upgraded from version 1.38.43 to version 1.38.44"
+sed -i -e "s/version=2.173.0/version=2.174.0/g" python/python-google-api-python-client/spkgbuild
+changelog "python/python-google-api-python-client/spkgbuild" "Upgraded from version 2.173.0 to version 2.174.0"
+sed -i -e "s/version=6.135.14/version=6.135.15/g" python/python-hypothesis/spkgbuild
+changelog "python/python-hypothesis/spkgbuild" "Upgraded from version 6.135.14 to version 6.135.15"
+sed -i -e "s/version=4.0.3/version=4.1.0/g" python/python-mpi4py/spkgbuild
+changelog "python/python-mpi4py/spkgbuild" "Upgraded from version 4.0.3 to version 4.1.0"
+sed -i -e "s/version=6.5.0/version=6.5.1/g" python/python-multidict/spkgbuild
+changelog "python/python-multidict/spkgbuild" "Upgraded from version 6.5.0 to version 6.5.1"
+sed -i -e "s/version=9.0.7/version=9.0.8/g" python/python-phonenumbers/spkgbuild
+changelog "python/python-phonenumbers/spkgbuild" "Upgraded from version 9.0.7 to version 9.0.8"
+sed -i -e "s/version=3.13.4/version=3.13.5/g" ruby-gems/ruby-rspec-core/spkgbuild
+changelog "ruby-gems/ruby-rspec-core/spkgbuild" "Upgraded from version 3.13.4 to version 3.13.5"
+sed -i -e "s/version=2025.6.3/version=2025.7.0/g" server/homeassistant/spkgbuild
+changelog "server/homeassistant/spkgbuild" "Upgraded from version 2025.6.3 to version 2025.7.0"
