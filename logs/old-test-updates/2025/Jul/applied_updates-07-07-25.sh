d9455e2c13d84dd2b3ae9f842b304cfe92952fda
Voncloft <voncloft@gmail.com>
2025-07-07 08:14:02 -0400

    Minor Updates

---
+++ logs/old-test-updates/2025/Jul/applied_updates-07-07-25.sh
@@ -0,0 +1,28 @@
+sed -i -e "s/version=1.22.1/version=1.22.2/g" core/conky/spkgbuild
+changelog "core/conky/spkgbuild" "Upgraded from version 1.22.1 to version 1.22.2"
+sed -i -e "s/version=2.0/version=2.1/g" core/geany/spkgbuild
+changelog "core/geany/spkgbuild" "Upgraded from version 2.0 to version 2.1"
+sed -i -e "s/version=6.15.4/version=6.15.5/g" core/linux-api-headers/spkgbuild
+changelog "core/linux-api-headers/spkgbuild" "Upgraded from version 6.15.4 to version 6.15.5"
+sed -i -e "s/version=6.15.4/version=6.15.5/g" core/linux-desktop/spkgbuild
+changelog "core/linux-desktop/spkgbuild" "Upgraded from version 6.15.4 to version 6.15.5"
+sed -i -e "s/version=6.15.4/version=6.15.5/g" core/linux-router/spkgbuild
+changelog "core/linux-router/spkgbuild" "Upgraded from version 6.15.4 to version 6.15.5"
+sed -i -e "s/version=8.0.2/version=8.0.3/g" core/redis/spkgbuild
+changelog "core/redis/spkgbuild" "Upgraded from version 8.0.2 to version 8.0.3"
+sed -i -e "s/version=1.8.1/version=1.9.0/g" kf/kirigami-addons/spkgbuild
+changelog "kf/kirigami-addons/spkgbuild" "Upgraded from version 1.8.1 to version 1.9.0"
+sed -i -e "s/version=3.20.0/version=3.21.0/g" python/python-aiogram/spkgbuild
+changelog "python/python-aiogram/spkgbuild" "Upgraded from version 3.20.0 to version 3.21.0"
+sed -i -e "s/version=6.4.0/version=6.4.1/g" python/python-auditwheel/spkgbuild
+changelog "python/python-auditwheel/spkgbuild" "Upgraded from version 6.4.0 to version 6.4.1"
+sed -i -e "s/version=6.135.24/version=6.135.26/g" python/python-hypothesis/spkgbuild
+changelog "python/python-hypothesis/spkgbuild" "Upgraded from version 6.135.24 to version 6.135.26"
+sed -i -e "s/version=2.1.3/version=2.1.4/g" python/python-pook/spkgbuild
+changelog "python/python-pook/spkgbuild" "Upgraded from version 2.1.3 to version 2.1.4"
+sed -i -e "s/version=6.4/version=7.0/g" python/python-svg.path/spkgbuild
+changelog "python/python-svg.path/spkgbuild" "Upgraded from version 6.4 to version 7.0"
+sed -i -e "s/version=3.15.52/version=3.15.53/g" server/blacklist-hosts/spkgbuild
+changelog "server/blacklist-hosts/spkgbuild" "Upgraded from version 3.15.52 to version 3.15.53"
+sed -i -e "s/version=2025.7.0/version=2025.7.1/g" server/homeassistant/spkgbuild
+changelog "server/homeassistant/spkgbuild" "Upgraded from version 2025.7.0 to version 2025.7.1"
