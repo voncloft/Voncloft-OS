97ff368759095249baaf852acb71bfa4bc4638a1
Voncloft <voncloft@gmail.com>
2025-09-16 12:20:40 -0400

    Minor Updates

---
+++ logs/old-test-updates/2025/Sep/applied_updates-09-16-25.sh
@@ -0,0 +1,32 @@
+sed -i -e "s/version=6.4.1/version=6.4.2/g" cinnamon/cinnamon-session/spkgbuild
+changelog "cinnamon/cinnamon-session/spkgbuild" "Upgraded from version 6.4.1 to version 6.4.2"
+sed -i -e "s/version=4.11.3/version=4.12/g" compilers/ccache/spkgbuild
+changelog "compilers/ccache/spkgbuild" "Upgraded from version 4.11.3 to version 4.12"
+sed -i -e "s/version=3.18.7/version=3.18.8/g" extra/flexget/spkgbuild
+changelog "extra/flexget/spkgbuild" "Upgraded from version 3.18.7 to version 3.18.8"
+sed -i -e "s/version=1.40.30/version=1.40.31/g" python/python-boto3/spkgbuild
+changelog "python/python-boto3/spkgbuild" "Upgraded from version 1.40.30 to version 1.40.31"
+sed -i -e "s/version=1.40.30/version=1.40.31/g" python/python-botocore/spkgbuild
+changelog "python/python-botocore/spkgbuild" "Upgraded from version 1.40.30 to version 1.40.31"
+sed -i -e "s/version=0.19.16/version=0.19.17/g" python/python-django-modeltranslation/spkgbuild
+changelog "python/python-django-modeltranslation/spkgbuild" "Upgraded from version 0.19.16 to version 0.19.17"
+sed -i -e "s/version=37.6.0/version=37.7.0/g" python/python-faker/spkgbuild
+changelog "python/python-faker/spkgbuild" "Upgraded from version 37.6.0 to version 37.7.0"
+sed -i -e "s/version=6.138.15/version=6.138.16/g" python/python-hypothesis/spkgbuild
+changelog "python/python-hypothesis/spkgbuild" "Upgraded from version 6.138.15 to version 6.138.16"
+sed -i -e "s/version=2.1.4/version=2.2.0/g" python/python-poetry/spkgbuild
+changelog "python/python-poetry/spkgbuild" "Upgraded from version 2.1.4 to version 2.2.0"
+sed -i -e "s/version=2.1.3/version=2.2.0/g" python/python-poetry-core/spkgbuild
+changelog "python/python-poetry-core/spkgbuild" "Upgraded from version 2.1.3 to version 2.2.0"
+sed -i -e "s/version=3.2.7/version=3.2.8/g" python/python-xlsxwriter/spkgbuild
+changelog "python/python-xlsxwriter/spkgbuild" "Upgraded from version 3.2.7 to version 3.2.8"
+sed -i -e "s/version=4.3.1/version=4.3.2/g" python/python-zeep/spkgbuild
+changelog "python/python-zeep/spkgbuild" "Upgraded from version 4.3.1 to version 4.3.2"
+sed -i -e "s/version=7.3/version=8.0/g" python/python-zope-security/spkgbuild
+changelog "python/python-zope-security/spkgbuild" "Upgraded from version 7.3 to version 8.0"
+sed -i -e "s/version=0.24.0/version=0.25.0/g" python/python-zstandard/spkgbuild
+changelog "python/python-zstandard/spkgbuild" "Upgraded from version 0.24.0 to version 0.25.0"
+sed -i -e "s/version=3.16.17/version=3.16.19/g" server/blacklist-hosts/spkgbuild
+changelog "server/blacklist-hosts/spkgbuild" "Upgraded from version 3.16.17 to version 3.16.19"
+sed -i -e "s/version=3.5.5/version=3.5.6/g" server/radicale/spkgbuild
+changelog "server/radicale/spkgbuild" "Upgraded from version 3.5.5 to version 3.5.6"
