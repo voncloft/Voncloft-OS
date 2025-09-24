a8b714e46d6ac1ae82359dc985b03350956bc781
Voncloft <voncloft@gmail.com>
2025-09-24 12:04:09 -0400

    Minor Updates

---
+++ logs/old-test-updates/2025/Sep/applied_updates-09-24-25.sh
@@ -0,0 +1,42 @@
+sed -i -e "s/version=6.4.12/version=6.4.13/g" cinnamon/cinnamon/spkgbuild
+changelog "cinnamon/cinnamon/spkgbuild" "Upgraded from version 6.4.12 to version 6.4.13"
+sed -i -e "s/version=1.9.0/version=1.9.1/g" compilers/meson/spkgbuild
+changelog "compilers/meson/spkgbuild" "Upgraded from version 1.9.0 to version 1.9.1"
+sed -i -e "s/version=9.7/version=9.8/g" core/coreutils/spkgbuild
+changelog "core/coreutils/spkgbuild" "Upgraded from version 9.7 to version 9.8"
+sed -i -e "s/version=0.25.9/version=0.25.10/g" core/p11-kit/spkgbuild
+changelog "core/p11-kit/spkgbuild" "Upgraded from version 0.25.9 to version 0.25.10"
+sed -i -e "s/version=3.18.9/version=3.18.10/g" extra/flexget/spkgbuild
+changelog "extra/flexget/spkgbuild" "Upgraded from version 3.18.9 to version 3.18.10"
+sed -i -e "s/version=0.25.9/version=0.25.10/g" multilib/p11-kit-32/spkgbuild
+changelog "multilib/p11-kit-32/spkgbuild" "Upgraded from version 0.25.9 to version 0.25.10"
+sed -i -e "s/version=2.6.14/version=2.6.15/g" networking/openvpn/spkgbuild
+changelog "networking/openvpn/spkgbuild" "Upgraded from version 2.6.14 to version 2.6.15"
+sed -i -e "s/version=2025.09.05/version=2025.09.23/g" networking/yt-dlp/spkgbuild
+changelog "networking/yt-dlp/spkgbuild" "Upgraded from version 2025.09.05 to version 2025.09.23"
+sed -i -e "s/version=144.0b3/version=144.0b4/g" nonfree/firefox/spkgbuild
+changelog "nonfree/firefox/spkgbuild" "Upgraded from version 144.0b3 to version 144.0b4"
+sed -i -e "s/version=4.10.0/version=4.11.0/g" python/python-anyio/spkgbuild
+changelog "python/python-anyio/spkgbuild" "Upgraded from version 4.10.0 to version 4.11.0"
+sed -i -e "s/version=6.8.3/version=6.8.4/g" python/python-apispec/spkgbuild
+changelog "python/python-apispec/spkgbuild" "Upgraded from version 6.8.3 to version 6.8.4"
+sed -i -e "s/version=1.40.35/version=1.40.37/g" python/python-boto3/spkgbuild
+changelog "python/python-boto3/spkgbuild" "Upgraded from version 1.40.35 to version 1.40.37"
+sed -i -e "s/version=1.40.35/version=1.40.37/g" python/python-botocore/spkgbuild
+changelog "python/python-botocore/spkgbuild" "Upgraded from version 1.40.35 to version 1.40.37"
+sed -i -e "s/version=7.10.6/version=7.10.7/g" python/python-coverage/spkgbuild
+changelog "python/python-coverage/spkgbuild" "Upgraded from version 7.10.6 to version 7.10.7"
+sed -i -e "s/version=0.6.1/version=0.6.2/g" python/python-edalize/spkgbuild
+changelog "python/python-edalize/spkgbuild" "Upgraded from version 0.6.1 to version 0.6.2"
+sed -i -e "s/version=6.139.2/version=6.140.2/g" python/python-hypothesis/spkgbuild
+changelog "python/python-hypothesis/spkgbuild" "Upgraded from version 6.139.2 to version 6.140.2"
+sed -i -e "s/version=1.2.2/version=1.3.0/g" python/python-lark-parser/spkgbuild
+changelog "python/python-lark-parser/spkgbuild" "Upgraded from version 1.2.2 to version 1.3.0"
+sed -i -e "s/version=2025.1/version=2025.1.1/g" python/python-pudb/spkgbuild
+changelog "python/python-pudb/spkgbuild" "Upgraded from version 2025.1 to version 2025.1.1"
+sed -i -e "s/version=0.19.1/version=0.19.2/g" python/python-typer/spkgbuild
+changelog "python/python-typer/spkgbuild" "Upgraded from version 0.19.1 to version 0.19.2"
+sed -i -e "s/version=8.0/version=8.1/g" python/python-zope-security/spkgbuild
+changelog "python/python-zope-security/spkgbuild" "Upgraded from version 8.0 to version 8.1"
+sed -i -e "s/version=1.42.1.10060/version=1.42.2.10156/g" server/plex-media-server/spkgbuild
+changelog "server/plex-media-server/spkgbuild" "Upgraded from version 1.42.1.10060 to version 1.42.2.10156"
