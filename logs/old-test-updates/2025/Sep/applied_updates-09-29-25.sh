16734b237b8e3856dc2da31cd6d2467bed7e3cd9
Voncloft <voncloft@gmail.com>
2025-09-29 08:35:38 -0400

    Minor Updates

---
+++ logs/old-test-updates/2025/Sep/applied_updates-09-29-25.sh
@@ -0,0 +1,6 @@
+sed -i -e "s/version=1.3/version=1.4/g" libs/libunistring/spkgbuild
+changelog "libs/libunistring/spkgbuild" "Upgraded from version 1.3 to version 1.4"
+sed -i -e "s/version=4.12.2/version=4.12.3/g" python/python-engineio/spkgbuild
+changelog "python/python-engineio/spkgbuild" "Upgraded from version 4.12.2 to version 4.12.3"
+sed -i -e "s/version=6.3.0/version=6.4.0/g" python/python-gitlab/spkgbuild
+changelog "python/python-gitlab/spkgbuild" "Upgraded from version 6.3.0 to version 6.4.0"
