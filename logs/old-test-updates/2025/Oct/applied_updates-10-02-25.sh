b5484bbc1141f62d75b5869012f89ed7a48f9831
Voncloft <voncloft@gmail.com>
2025-10-02 10:22:10 -0400

    Minor Updates

---
+++ logs/old-test-updates/2025/Oct/applied_updates-10-02-25.sh
@@ -0,0 +1,62 @@
+sed -i -e "s/version=25.2.3/version=25.2.4/g" hardware/mesa/spkgbuild
+changelog "hardware/mesa/spkgbuild" "Upgraded from version 25.2.3 to version 25.2.4"
+sed -i -e "s/version=4.12/version=4.12.1/g" compilers/ccache/spkgbuild
+changelog "compilers/ccache/spkgbuild" "Upgraded from version 4.12 to version 4.12.1"
+sed -i -e "s/version=4.1.1-sunos-x86_64/version=4.1.2-sunos-x86_64/g" compilers/cmake/spkgbuild
+changelog "compilers/cmake/spkgbuild" "Upgraded from version 4.1.1-sunos-x86_64 to version 4.1.2-sunos-x86_64"
+sed -i -e "s/version=6.16.1/version=6.17/g" core/btrfs-progs/spkgbuild
+changelog "core/btrfs-progs/spkgbuild" "Upgraded from version 6.16.1 to version 6.17"
+sed -i -e "s/version=6.16.0/version=6.17.0/g" core/iproute2/spkgbuild
+changelog "core/iproute2/spkgbuild" "Upgraded from version 6.16.0 to version 6.17.0"
+sed -i -e "s/version=6.16.9/version=6.17/g" core/linux-api-headers/spkgbuild
+changelog "core/linux-api-headers/spkgbuild" "Upgraded from version 6.16.9 to version 6.17"
+sed -i -e "s/version=6.16.9/version=6.17/g" core/linux-desktop/spkgbuild
+changelog "core/linux-desktop/spkgbuild" "Upgraded from version 6.16.9 to version 6.17"
+sed -i -e "s/version=6.16.9/version=6.17/g" core/linux-router/spkgbuild
+changelog "core/linux-router/spkgbuild" "Upgraded from version 6.16.9 to version 6.17"
+sed -i -e "s/version=3.18.11/version=3.18.12/g" extra/flexget/spkgbuild
+changelog "extra/flexget/spkgbuild" "Upgraded from version 3.18.11 to version 3.18.12"
+sed -i -e "s/version=5.12/version=5.13/g" extra/gnucash/spkgbuild
+changelog "extra/gnucash/spkgbuild" "Upgraded from version 5.12 to version 5.13"
+sed -i -e "s/version=1.0.0.84/version=1.0.0.85/g" multilib/steam/spkgbuild
+changelog "multilib/steam/spkgbuild" "Upgraded from version 1.0.0.84 to version 1.0.0.85"
+sed -i -e "s/version=2.4.125/version=2.4.126/g" libs/libdrm/spkgbuild
+changelog "libs/libdrm/spkgbuild" "Upgraded from version 2.4.125 to version 2.4.126"
+sed -i -e "s/version=4.1.0/version=4.1.1/g" libs/libressl/spkgbuild
+changelog "libs/libressl/spkgbuild" "Upgraded from version 4.1.0 to version 4.1.1"
+sed -i -e "s/version=3.5.3/version=3.6.0/g" networking/openssl/spkgbuild
+changelog "networking/openssl/spkgbuild" "Upgraded from version 3.5.3 to version 3.6.0"
+sed -i -e "s/version=144.0b6/version=144.0b8/g" nonfree/firefox/spkgbuild
+changelog "nonfree/firefox/spkgbuild" "Upgraded from version 144.0b6 to version 144.0b8"
+sed -i -e "s/version=580.82.09/version=580.95.05/g" nonfree/nvidia/spkgbuild
+changelog "nonfree/nvidia/spkgbuild" "Upgraded from version 580.82.09 to version 580.95.05"
+sed -i -e "s/version=580.82.09/version=580.95.05/g" nonfree/nvidia-32/spkgbuild
+changelog "nonfree/nvidia-32/spkgbuild" "Upgraded from version 580.82.09 to version 580.95.05"
+sed -i -e "s/version=580.82.09/version=580.95.05/g" nonfree/nvidia-modules/spkgbuild
+changelog "nonfree/nvidia-modules/spkgbuild" "Upgraded from version 580.82.09 to version 580.95.05"
+sed -i -e "s/version=3.8/version=3.8.1/g" nonfree/pandoc-bin/spkgbuild
+changelog "nonfree/pandoc-bin/spkgbuild" "Upgraded from version 3.8 to version 3.8.1"
+sed -i -e "s/version=144.0b2/version=144.0b3/g" nonfree/thunderbird/spkgbuild
+changelog "nonfree/thunderbird/spkgbuild" "Upgraded from version 144.0b2 to version 144.0b3"
+sed -i -e "s/version=1.40.40/version=1.40.42/g" python/python-boto3/spkgbuild
+changelog "python/python-boto3/spkgbuild" "Upgraded from version 1.40.40 to version 1.40.42"
+sed -i -e "s/version=1.40.40/version=1.40.43/g" python/python-botocore/spkgbuild
+changelog "python/python-botocore/spkgbuild" "Upgraded from version 1.40.40 to version 1.40.43"
+sed -i -e "s/version=1.39.1/version=1.40.0/g" python/python-cfn-lint/spkgbuild
+changelog "python/python-cfn-lint/spkgbuild" "Upgraded from version 1.39.1 to version 1.40.0"
+sed -i -e "s/version=0.117.1/version=0.118.0/g" python/python-fastapi/spkgbuild
+changelog "python/python-fastapi/spkgbuild" "Upgraded from version 0.117.1 to version 0.118.0"
+sed -i -e "s/version=4.60.0/version=4.60.1/g" python/python-fonttools/spkgbuild
+changelog "python/python-fonttools/spkgbuild" "Upgraded from version 4.60.0 to version 4.60.1"
+sed -i -e "s/version=2.40.3/version=2.41.1/g" python/python-google-auth/spkgbuild
+changelog "python/python-google-auth/spkgbuild" "Upgraded from version 2.40.3 to version 2.41.1"
+sed -i -e "s/version=3.9.1/version=3.9.2/g" python/python-nltk/spkgbuild
+changelog "python/python-nltk/spkgbuild" "Upgraded from version 3.9.1 to version 3.9.2"
+sed -i -e "s/version=1.7.1/version=1.8.0/g" python/python-pipx/spkgbuild
+changelog "python/python-pipx/spkgbuild" "Upgraded from version 1.7.1 to version 1.8.0"
+sed -i -e "s/version=3.2.5/version=3.3.0/g" python/python-pyparsing/spkgbuild
+changelog "python/python-pyparsing/spkgbuild" "Upgraded from version 3.2.5 to version 3.3.0"
+sed -i -e "s/version=5.13.0/version=5.14.0/g" python/python-socketio/spkgbuild
+changelog "python/python-socketio/spkgbuild" "Upgraded from version 5.13.0 to version 5.14.0"
+sed -i -e "s/version=1.5.0/version=1.5.1/g" python/python-sqlalchemy-continuum/spkgbuild
+changelog "python/python-sqlalchemy-continuum/spkgbuild" "Upgraded from version 1.5.0 to version 1.5.1"
