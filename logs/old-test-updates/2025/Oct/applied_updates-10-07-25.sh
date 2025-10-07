a972c074c1a9529b30cea68d3192b0eadd681766
Voncloft <voncloft@gmail.com>
2025-10-07 11:34:58 -0400

    Minor Updates

---
+++ logs/old-test-updates/2025/Oct/applied_updates-10-07-25.sh
@@ -0,0 +1,38 @@
+sed -i -e "s/version=1.1.0/version=1.2.0/g" core/brotli/spkgbuild
+changelog "core/brotli/spkgbuild" "Upgraded from version 1.1.0 to version 1.2.0"
+sed -i -e "s/version=6.17/version=6.17.1/g" core/linux-api-headers/spkgbuild
+changelog "core/linux-api-headers/spkgbuild" "Upgraded from version 6.17 to version 6.17.1"
+sed -i -e "s/version=6.17/version=6.17.1/g" core/linux-desktop/spkgbuild
+changelog "core/linux-desktop/spkgbuild" "Upgraded from version 6.17 to version 6.17.1"
+sed -i -e "s/version=6.17/version=6.17.1/g" core/linux-router/spkgbuild
+changelog "core/linux-router/spkgbuild" "Upgraded from version 6.17 to version 6.17.1"
+sed -i -e "s/version=1.30.1/version=1.32.0/g" core/v4l-utils/spkgbuild
+changelog "core/v4l-utils/spkgbuild" "Upgraded from version 1.30.1 to version 1.32.0"
+sed -i -e "s/version=3.18.13/version=3.18.14/g" extra/flexget/spkgbuild
+changelog "extra/flexget/spkgbuild" "Upgraded from version 3.18.13 to version 3.18.14"
+sed -i -e "s/version=1.33.2/version=1.33.3/g" media/mpg123/spkgbuild
+changelog "media/mpg123/spkgbuild" "Upgraded from version 1.33.2 to version 1.33.3"
+sed -i -e "s/version=1.1.0/version=1.2.0/g" multilib/brotli-32/spkgbuild
+changelog "multilib/brotli-32/spkgbuild" "Upgraded from version 1.1.0 to version 1.2.0"
+sed -i -e "s/version=1.4/version=1.4.1/g" libs/libunistring/spkgbuild
+changelog "libs/libunistring/spkgbuild" "Upgraded from version 1.4 to version 1.4.1"
+sed -i -e "s/version=10.0/version=10.1/g" networking/openssh/spkgbuild
+changelog "networking/openssh/spkgbuild" "Upgraded from version 10.0 to version 10.1"
+sed -i -e "s/version=0.4.8.18/version=0.4.8.19/g" networking/tor/spkgbuild
+changelog "networking/tor/spkgbuild" "Upgraded from version 0.4.8.18 to version 0.4.8.19"
+sed -i -e "s/version=3.8.1/version=3.8.2/g" nonfree/pandoc-bin/spkgbuild
+changelog "nonfree/pandoc-bin/spkgbuild" "Upgraded from version 3.8.1 to version 3.8.2"
+sed -i -e "s/version=65.11.2/version=65.12.0/g" python/python-django-allauth/spkgbuild
+changelog "python/python-django-allauth/spkgbuild" "Upgraded from version 65.11.2 to version 65.12.0"
+sed -i -e "s/version=6.140.2/version=6.140.3/g" python/python-hypothesis/spkgbuild
+changelog "python/python-hypothesis/spkgbuild" "Upgraded from version 6.140.2 to version 6.140.3"
+sed -i -e "s/version=6.6.4/version=6.7.0/g" python/python-multidict/spkgbuild
+changelog "python/python-multidict/spkgbuild" "Upgraded from version 6.6.4 to version 6.7.0"
+sed -i -e "s/version=3.40.0/version=3.41.0/g" python/python-openmdao/spkgbuild
+changelog "python/python-openmdao/spkgbuild" "Upgraded from version 3.40.0 to version 3.41.0"
+sed -i -e "s/version=3.3.8/version=3.3.9/g" python/python-pylint/spkgbuild
+changelog "python/python-pylint/spkgbuild" "Upgraded from version 3.3.8 to version 3.3.9"
+sed -i -e "s/version=1.21.1/version=1.22.0/g" python/python-yarl/spkgbuild
+changelog "python/python-yarl/spkgbuild" "Upgraded from version 1.21.1 to version 1.22.0"
+sed -i -e "s/version=0.147.2/version=1.0.0/g" python/python-zeroconf/spkgbuild
+changelog "python/python-zeroconf/spkgbuild" "Upgraded from version 0.147.2 to version 1.0.0"
