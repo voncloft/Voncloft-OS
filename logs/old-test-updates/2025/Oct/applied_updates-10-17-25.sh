e0c28a0666b66b27e3125d3c70234cbf9f8b6383
Voncloft <voncloft@gmail.com>
2025-10-17 11:21:53 -0400

    Minor Updates

---
+++ logs/old-test-updates/2025/Oct/applied_updates-10-17-25.sh
@@ -0,0 +1,34 @@
+sed -i -e "s/version=0.151.1/version=0.151.2/g" core/hugo/spkgbuild
+changelog "core/hugo/spkgbuild" "Upgraded from version 0.151.1 to version 0.151.2"
+sed -i -e "s/version=1.4.9/version=1.5.81/g" core/pipewire/spkgbuild
+changelog "core/pipewire/spkgbuild" "Upgraded from version 1.4.9 to version 1.5.81"
+sed -i -e "s/version=32.1/version=33.0/g" extra/protobuf/spkgbuild
+changelog "extra/protobuf/spkgbuild" "Upgraded from version 32.1 to version 33.0"
+sed -i -e "s/version=3.8.1/version=3.8.2/g" libs/libarchive/spkgbuild
+changelog "libs/libarchive/spkgbuild" "Upgraded from version 3.8.1 to version 3.8.2"
+sed -i -e "s/version=3.4.1/version=3.4.2/g" libs/openexr/spkgbuild
+changelog "libs/openexr/spkgbuild" "Upgraded from version 3.4.1 to version 3.4.2"
+sed -i -e "s/version=144.0b4/version=145.0b1/g" nonfree/thunderbird/spkgbuild
+changelog "nonfree/thunderbird/spkgbuild" "Upgraded from version 144.0b4 to version 145.0b1"
+sed -i -e "s/version=1.40.53/version=1.40.54/g" python/python-boto3/spkgbuild
+changelog "python/python-boto3/spkgbuild" "Upgraded from version 1.40.53 to version 1.40.54"
+sed -i -e "s/version=1.40.53/version=1.40.54/g" python/python-botocore/spkgbuild
+changelog "python/python-botocore/spkgbuild" "Upgraded from version 1.40.53 to version 1.40.54"
+sed -i -e "s/version=6.9.0/version=6.10.1/g" python/python-colorlog/spkgbuild
+changelog "python/python-colorlog/spkgbuild" "Upgraded from version 6.9.0 to version 6.10.1"
+sed -i -e "s/version=65.12.0/version=65.12.1/g" python/python-django-allauth/spkgbuild
+changelog "python/python-django-allauth/spkgbuild" "Upgraded from version 65.12.0 to version 65.12.1"
+sed -i -e "s/version=6.141.1/version=6.142.0/g" python/python-hypothesis/spkgbuild
+changelog "python/python-hypothesis/spkgbuild" "Upgraded from version 6.141.1 to version 6.142.0"
+sed -i -e "s/version=5.6.1/version=5.7.0/g" python/python-internetarchive/spkgbuild
+changelog "python/python-internetarchive/spkgbuild" "Upgraded from version 5.6.1 to version 5.7.0"
+sed -i -e "s/version=5.8.1/version=5.9.1/g" python/python-jupyter_core/spkgbuild
+changelog "python/python-jupyter_core/spkgbuild" "Upgraded from version 5.8.1 to version 5.9.1"
+sed -i -e "s/version=2.8.2/version=3.0.0/g" python/python-maxminddb/spkgbuild
+changelog "python/python-maxminddb/spkgbuild" "Upgraded from version 2.8.2 to version 3.0.0"
+sed -i -e "s/version=2025.1.1/version=2025.1.2/g" python/python-pudb/spkgbuild
+changelog "python/python-pudb/spkgbuild" "Upgraded from version 2025.1.1 to version 2025.1.2"
+sed -i -e "s/version=0.21.0/version=0.22.0/g" python/python-uvloop/spkgbuild
+changelog "python/python-uvloop/spkgbuild" "Upgraded from version 0.21.0 to version 0.22.0"
+sed -i -e "s/version=4.21.2/version=4.21.3/g" xfce/thunar/spkgbuild
+changelog "xfce/thunar/spkgbuild" "Upgraded from version 4.21.2 to version 4.21.3"
