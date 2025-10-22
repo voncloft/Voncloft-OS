3f126e5bc094217a633395183bd43b907f5e1fee
Voncloft <voncloft@gmail.com>
2025-10-22 13:01:07 -0400

    Minor Updates

---
+++ logs/old-test-updates/2025/Oct/applied_updates-10-22-25.sh
@@ -0,0 +1,26 @@
+sed -i -e "s/version=21.1.3/version=21.1.4/g" compilers/clang/spkgbuild
+changelog "compilers/clang/spkgbuild" "Upgraded from version 21.1.3 to version 21.1.4"
+sed -i -e "s/version=21.1.3/version=21.1.4/g" compilers/lld/spkgbuild
+changelog "compilers/lld/spkgbuild" "Upgraded from version 21.1.3 to version 21.1.4"
+sed -i -e "s/version=21.1.3/version=21.1.4/g" compilers/llvm/spkgbuild
+changelog "compilers/llvm/spkgbuild" "Upgraded from version 21.1.3 to version 21.1.4"
+sed -i -e "s/version=21.1.3/version=21.1.4/g" compilers/llvm-bak/spkgbuild
+changelog "compilers/llvm-bak/spkgbuild" "Upgraded from version 21.1.3 to version 21.1.4"
+sed -i -e "s/version=21.1.3/version=21.1.4/g" compilers/llvm-old-2023-06-07/spkgbuild
+changelog "compilers/llvm-old-2023-06-07/spkgbuild" "Upgraded from version 21.1.3 to version 21.1.4"
+sed -i -e "s/version=0.151.2/version=0.152.0/g" core/hugo/spkgbuild
+changelog "core/hugo/spkgbuild" "Upgraded from version 0.151.2 to version 0.152.0"
+sed -i -e "s/version=10.46/version=10.47/g" core/pcre2/spkgbuild
+changelog "core/pcre2/spkgbuild" "Upgraded from version 10.46 to version 10.47"
+sed -i -e "s/version=21.1.3/version=21.1.4/g" multilib/llvm-32/spkgbuild
+changelog "multilib/llvm-32/spkgbuild" "Upgraded from version 21.1.3 to version 21.1.4"
+sed -i -e "s/version=7.2.7.2.2/version=7.2.7.2.4/g" nonfree/virtualbox/spkgbuild
+changelog "nonfree/virtualbox/spkgbuild" "Upgraded from version 7.2.7.2.2 to version 7.2.7.2.4"
+sed -i -e "s/version=7.2.2/version=7.2.4/g" nonfree/virtualbox-guest-modules/spkgbuild
+changelog "nonfree/virtualbox-guest-modules/spkgbuild" "Upgraded from version 7.2.2 to version 7.2.4"
+sed -i -e "s/version=7.2.2/version=7.2.4/g" nonfree/virtualbox-guest-utils/spkgbuild
+changelog "nonfree/virtualbox-guest-utils/spkgbuild" "Upgraded from version 7.2.2 to version 7.2.4"
+sed -i -e "s/version=2025.1.2/version=2025.1.3/g" python/python-pudb/spkgbuild
+changelog "python/python-pudb/spkgbuild" "Upgraded from version 2025.1.2 to version 2025.1.3"
+sed -i -e "s/version=3.13.1/version=3.13.2/g" ruby-gems/ruby-rspec/spkgbuild
+changelog "ruby-gems/ruby-rspec/spkgbuild" "Upgraded from version 3.13.1 to version 3.13.2"
