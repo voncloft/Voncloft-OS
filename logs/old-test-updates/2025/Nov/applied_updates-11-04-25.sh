0f51ee21c69d58d3e8adefba54267feb24b9fdc1
Voncloft <voncloft@gmail.com>
2025-11-04 10:30:30 -0500

    Minor Updates

---
+++ logs/old-test-updates/2025/Nov/applied_updates-11-04-25.sh
@@ -0,0 +1,60 @@
+sed -i -e "s/version=4.2.0-rc1-sunos-x86_64/version=4.2.0-rc2-sunos-x86_64/g" compilers/cmake/spkgbuild
+changelog "compilers/cmake/spkgbuild" "Upgraded from version 4.2.0-rc1-sunos-x86_64 to version 4.2.0-rc2-sunos-x86_64"
+sed -i -e "s/version=3.2.2/version=3.3.0/g" core/dkms/spkgbuild
+changelog "core/dkms/spkgbuild" "Upgraded from version 3.2.2 to version 3.3.0"
+sed -i -e "s/version=2.15/version=2.16/g" core/i3lock/spkgbuild
+changelog "core/i3lock/spkgbuild" "Upgraded from version 2.15 to version 2.16"
+sed -i -e "s/version=6.17.6/version=6.17.7/g" core/linux-api-headers/spkgbuild
+changelog "core/linux-api-headers/spkgbuild" "Upgraded from version 6.17.6 to version 6.17.7"
+sed -i -e "s/version=6.17.6/version=6.17.7/g" core/linux-desktop/spkgbuild
+changelog "core/linux-desktop/spkgbuild" "Upgraded from version 6.17.6 to version 6.17.7"
+sed -i -e "s/version=6.17.6/version=6.17.7/g" core/linux-router/spkgbuild
+changelog "core/linux-router/spkgbuild" "Upgraded from version 6.17.6 to version 6.17.7"
+sed -i -e "s/version=3.6.0/version=3.6.1/g" core/qutebrowser/spkgbuild
+changelog "core/qutebrowser/spkgbuild" "Upgraded from version 3.6.0 to version 3.6.1"
+sed -i -e "s/version=8.2.2/version=8.2.3/g" core/redis/spkgbuild
+changelog "core/redis/spkgbuild" "Upgraded from version 8.2.2 to version 8.2.3"
+sed -i -e "s/version=4.1.4/version=4.1.5/g" media/projectm/spkgbuild
+changelog "media/projectm/spkgbuild" "Upgraded from version 4.1.4 to version 4.1.5"
+sed -i -e "s/version=2.4.127/version=2.4.128/g" libs/libdrm/spkgbuild
+changelog "libs/libdrm/spkgbuild" "Upgraded from version 2.4.127 to version 2.4.128"
+sed -i -e "s/version=145.0b8/version=145.0b9/g" nonfree/firefox/spkgbuild
+changelog "nonfree/firefox/spkgbuild" "Upgraded from version 145.0b8 to version 145.0b9"
+sed -i -e "s/version=6.4.2/version=6.5.0/g" python/python-auditwheel/spkgbuild
+changelog "python/python-auditwheel/spkgbuild" "Upgraded from version 6.4.2 to version 6.5.0"
+sed -i -e "s/version=1.40.63/version=1.40.64/g" python/python-boto3/spkgbuild
+changelog "python/python-boto3/spkgbuild" "Upgraded from version 1.40.63 to version 1.40.64"
+sed -i -e "s/version=1.40.63/version=1.40.64/g" python/python-botocore/spkgbuild
+changelog "python/python-botocore/spkgbuild" "Upgraded from version 1.40.63 to version 1.40.64"
+sed -i -e "s/version=2.0.11/version=2.0.12/g" python/python-cymem/spkgbuild
+changelog "python/python-cymem/spkgbuild" "Upgraded from version 2.0.11 to version 2.0.12"
+sed -i -e "s/version=65.12.1/version=65.13.0/g" python/python-django-allauth/spkgbuild
+changelog "python/python-django-allauth/spkgbuild" "Upgraded from version 65.12.1 to version 65.13.0"
+sed -i -e "s/version=0.6.2/version=0.6.3/g" python/python-edalize/spkgbuild
+changelog "python/python-edalize/spkgbuild" "Upgraded from version 0.6.2 to version 0.6.3"
+sed -i -e "s/version=0.120.2/version=0.121.0/g" python/python-fastapi/spkgbuild
+changelog "python/python-fastapi/spkgbuild" "Upgraded from version 0.120.2 to version 0.121.0"
+sed -i -e "s/version=2.185.0/version=2.186.0/g" python/python-google-api-python-client/spkgbuild
+changelog "python/python-google-api-python-client/spkgbuild" "Upgraded from version 2.185.0 to version 2.186.0"
+sed -i -e "s/version=6.142.4/version=6.145.0/g" python/python-hypothesis/spkgbuild
+changelog "python/python-hypothesis/spkgbuild" "Upgraded from version 6.142.4 to version 6.145.0"
+sed -i -e "s/version=8.1.7/version=8.1.8/g" python/python-ipywidgets/spkgbuild
+changelog "python/python-ipywidgets/spkgbuild" "Upgraded from version 8.1.7 to version 8.1.8"
+sed -i -e "s/version=0.46.2/version=0.47.0/g" python/python-libtmux/spkgbuild
+changelog "python/python-libtmux/spkgbuild" "Upgraded from version 0.46.2 to version 0.47.0"
+sed -i -e "s/version=7.0.2/version=7.0.3/g" python/python-pbr/spkgbuild
+changelog "python/python-pbr/spkgbuild" "Upgraded from version 7.0.2 to version 7.0.3"
+sed -i -e "s/version=3.18.2/version=3.18.3/g" python/python-peewee/spkgbuild
+changelog "python/python-peewee/spkgbuild" "Upgraded from version 3.18.2 to version 3.18.3"
+sed -i -e "s/version=7.1.2/version=7.1.3/g" python/python-psutil/spkgbuild
+changelog "python/python-psutil/spkgbuild" "Upgraded from version 7.1.2 to version 7.1.3"
+sed -i -e "s/version=0.49.1/version=0.50.0/g" python/python-starlette/spkgbuild
+changelog "python/python-starlette/spkgbuild" "Upgraded from version 0.49.1 to version 0.50.0"
+sed -i -e "s/version=3.2.0/version=3.2.1/g" python/python-tblib/spkgbuild
+changelog "python/python-tblib/spkgbuild" "Upgraded from version 3.2.0 to version 3.2.1"
+sed -i -e "s/version=8.7.0/version=8.7.1/g" python/python-webargs/spkgbuild
+changelog "python/python-webargs/spkgbuild" "Upgraded from version 8.7.0 to version 8.7.1"
+sed -i -e "s/version=3.13.6/version=3.13.7/g" ruby-gems/ruby-rspec-mocks/spkgbuild
+changelog "ruby-gems/ruby-rspec-mocks/spkgbuild" "Upgraded from version 3.13.6 to version 3.13.7"
+sed -i -e "s/version=3.16.27/version=3.16.29/g" server/blacklist-hosts/spkgbuild
+changelog "server/blacklist-hosts/spkgbuild" "Upgraded from version 3.16.27 to version 3.16.29"
