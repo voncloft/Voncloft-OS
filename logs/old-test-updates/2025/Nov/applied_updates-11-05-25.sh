8dba5e9763e2e5d61a615e1aced8752dac127e4b
Voncloft <voncloft@gmail.com>
2025-11-05 10:52:06 -0500

    Minor Updates

---
+++ logs/old-test-updates/2025/Nov/applied_updates-11-05-25.sh
@@ -0,0 +1,42 @@
+sed -i -e "s/version=21.1.4/version=21.1.5/g" compilers/clang/spkgbuild
+changelog "compilers/clang/spkgbuild" "Upgraded from version 21.1.4 to version 21.1.5"
+sed -i -e "s/version=21.1.4/version=21.1.5/g" compilers/lld/spkgbuild
+changelog "compilers/lld/spkgbuild" "Upgraded from version 21.1.4 to version 21.1.5"
+sed -i -e "s/version=21.1.4/version=21.1.5/g" compilers/llvm/spkgbuild
+changelog "compilers/llvm/spkgbuild" "Upgraded from version 21.1.4 to version 21.1.5"
+sed -i -e "s/version=21.1.4/version=21.1.5/g" compilers/llvm-bak/spkgbuild
+changelog "compilers/llvm-bak/spkgbuild" "Upgraded from version 21.1.4 to version 21.1.5"
+sed -i -e "s/version=21.1.4/version=21.1.5/g" compilers/llvm-old-2023-06-07/spkgbuild
+changelog "compilers/llvm-old-2023-06-07/spkgbuild" "Upgraded from version 21.1.4 to version 21.1.5"
+sed -i -e "s/version=21.1.4/version=21.1.5/g" multilib/llvm-32/spkgbuild
+changelog "multilib/llvm-32/spkgbuild" "Upgraded from version 21.1.4 to version 21.1.5"
+sed -i -e "s/version=4.4.38/version=4.5.0/g" libs/libxcrypt/spkgbuild
+changelog "libs/libxcrypt/spkgbuild" "Upgraded from version 4.4.38 to version 4.5.0"
+sed -i -e "s/version=4.21.2/version=4.21.3/g" libs/libxfce4ui/spkgbuild
+changelog "libs/libxfce4ui/spkgbuild" "Upgraded from version 4.21.2 to version 4.21.3"
+sed -i -e "s/version=25.10.0/version=25.11.0/g" libs/poppler/spkgbuild
+changelog "libs/poppler/spkgbuild" "Upgraded from version 25.10.0 to version 25.11.0"
+sed -i -e "s/version=580.95.05/version=580.105.08/g" nonfree/nvidia/spkgbuild
+changelog "nonfree/nvidia/spkgbuild" "Upgraded from version 580.95.05 to version 580.105.08"
+sed -i -e "s/version=580.95.05/version=580.105.08/g" nonfree/nvidia-32/spkgbuild
+changelog "nonfree/nvidia-32/spkgbuild" "Upgraded from version 580.95.05 to version 580.105.08"
+sed -i -e "s/version=580.95.05/version=580.105.08/g" nonfree/nvidia-modules/spkgbuild
+changelog "nonfree/nvidia-modules/spkgbuild" "Upgraded from version 580.95.05 to version 580.105.08"
+sed -i -e "s/version=145.0b3/version=145.0b4/g" nonfree/thunderbird/spkgbuild
+changelog "nonfree/thunderbird/spkgbuild" "Upgraded from version 145.0b3 to version 145.0b4"
+sed -i -e "s/version=1.40.64/version=1.40.65/g" python/python-boto3/spkgbuild
+changelog "python/python-boto3/spkgbuild" "Upgraded from version 1.40.64 to version 1.40.65"
+sed -i -e "s/version=1.40.64/version=1.40.66/g" python/python-botocore/spkgbuild
+changelog "python/python-botocore/spkgbuild" "Upgraded from version 1.40.64 to version 1.40.66"
+sed -i -e "s/version=1.40.3/version=1.40.4/g" python/python-cfn-lint/spkgbuild
+changelog "python/python-cfn-lint/spkgbuild" "Upgraded from version 1.40.3 to version 1.40.4"
+sed -i -e "s/version=6.145.0/version=6.145.1/g" python/python-hypothesis/spkgbuild
+changelog "python/python-hypothesis/spkgbuild" "Upgraded from version 6.145.0 to version 6.145.1"
+sed -i -e "s/version=1.8.5/version=1.8.6/g" python/python-libcst/spkgbuild
+changelog "python/python-libcst/spkgbuild" "Upgraded from version 1.8.5 to version 1.8.6"
+sed -i -e "s/version=3.5/version=3.6/g" python/python-networkx/spkgbuild
+changelog "python/python-networkx/spkgbuild" "Upgraded from version 3.5 to version 3.6"
+sed -i -e "s/version=5.2.0/version=5.2.1/g" python/python-pytest-benchmark/spkgbuild
+changelog "python/python-pytest-benchmark/spkgbuild" "Upgraded from version 5.2.0 to version 5.2.1"
+sed -i -e "s/version=16.0.0/version=17.0.0/g" python/python-unicodedata2/spkgbuild
+changelog "python/python-unicodedata2/spkgbuild" "Upgraded from version 16.0.0 to version 17.0.0"
