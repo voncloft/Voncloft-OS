3279c2ae0d2d3241fcfbc2f0e0593de534b02d8b
Voncloft <voncloft@gmail.com>
2025-11-06 10:26:53 -0500

    Minor Updates

---
+++ logs/old-test-updates/2025/Nov/applied_updates-11-06-25.sh
@@ -0,0 +1,56 @@
+sed -i -e "s/version=8.16.0/version=8.17.0/g" core/curl/spkgbuild
+changelog "core/curl/spkgbuild" "Upgraded from version 8.16.0 to version 8.17.0"
+sed -i -e "s/version=4.37/version=4.38/g" extra/nspr/spkgbuild
+changelog "extra/nspr/spkgbuild" "Upgraded from version 4.37 to version 4.38"
+sed -i -e "s/version=2.2.0/version=2.3.0/g" lxde/lximage-qt/spkgbuild
+changelog "lxde/lximage-qt/spkgbuild" "Upgraded from version 2.2.0 to version 2.3.0"
+sed -i -e "s/version=2.2.0/version=2.3.0/g" lxqt/lxqt-about/spkgbuild
+changelog "lxqt/lxqt-about/spkgbuild" "Upgraded from version 2.2.0 to version 2.3.0"
+sed -i -e "s/version=2.2.0/version=2.3.0/g" lxqt/lxqt-admin/spkgbuild
+changelog "lxqt/lxqt-admin/spkgbuild" "Upgraded from version 2.2.0 to version 2.3.0"
+sed -i -e "s/version=1.2.0/version=1.3.0/g" lxqt/lxqt-archiver/spkgbuild
+changelog "lxqt/lxqt-archiver/spkgbuild" "Upgraded from version 1.2.0 to version 1.3.0"
+sed -i -e "s/version=2.2.1/version=2.3.0/g" lxqt/lxqt-build-tools/spkgbuild
+changelog "lxqt/lxqt-build-tools/spkgbuild" "Upgraded from version 2.2.1 to version 2.3.0"
+sed -i -e "s/version=2.2.0/version=2.3.0/g" lxqt/lxqt-config/spkgbuild
+changelog "lxqt/lxqt-config/spkgbuild" "Upgraded from version 2.2.0 to version 2.3.0"
+sed -i -e "s/version=2.2.0/version=2.3.0/g" lxqt/lxqt-globalkeys/spkgbuild
+changelog "lxqt/lxqt-globalkeys/spkgbuild" "Upgraded from version 2.2.0 to version 2.3.0"
+sed -i -e "s/version=2.2.0/version=2.3.0/g" lxqt/lxqt-notificationd/spkgbuild
+changelog "lxqt/lxqt-notificationd/spkgbuild" "Upgraded from version 2.2.0 to version 2.3.0"
+sed -i -e "s/version=2.2.0/version=2.3.0/g" lxqt/lxqt-openssh-askpass/spkgbuild
+changelog "lxqt/lxqt-openssh-askpass/spkgbuild" "Upgraded from version 2.2.0 to version 2.3.0"
+sed -i -e "s/version=2.2.2/version=2.3.0/g" lxqt/lxqt-panel/spkgbuild
+changelog "lxqt/lxqt-panel/spkgbuild" "Upgraded from version 2.2.2 to version 2.3.0"
+sed -i -e "s/version=2.2.0/version=2.3.0/g" lxqt/lxqt-policykit/spkgbuild
+changelog "lxqt/lxqt-policykit/spkgbuild" "Upgraded from version 2.2.0 to version 2.3.0"
+sed -i -e "s/version=2.2.1/version=2.3.0/g" lxqt/lxqt-powermanagement/spkgbuild
+changelog "lxqt/lxqt-powermanagement/spkgbuild" "Upgraded from version 2.2.1 to version 2.3.0"
+sed -i -e "s/version=2.2.0/version=2.3.0/g" lxqt/lxqt-qtplugin/spkgbuild
+changelog "lxqt/lxqt-qtplugin/spkgbuild" "Upgraded from version 2.2.0 to version 2.3.0"
+sed -i -e "s/version=2.2.0/version=2.3.0/g" lxqt/lxqt-runner/spkgbuild
+changelog "lxqt/lxqt-runner/spkgbuild" "Upgraded from version 2.2.0 to version 2.3.0"
+sed -i -e "s/version=2.2.0/version=2.3.0/g" lxqt/lxqt-session/spkgbuild
+changelog "lxqt/lxqt-session/spkgbuild" "Upgraded from version 2.2.0 to version 2.3.0"
+sed -i -e "s/version=2.2.0/version=2.3.0/g" lxqt/lxqt-sudo/spkgbuild
+changelog "lxqt/lxqt-sudo/spkgbuild" "Upgraded from version 2.2.0 to version 2.3.0"
+sed -i -e "s/version=2.2.0/version=2.3.0/g" lxqt/lxqt-themes/spkgbuild
+changelog "lxqt/lxqt-themes/spkgbuild" "Upgraded from version 2.2.0 to version 2.3.0"
+sed -i -e "s/version=2.2.0/version=2.3.0/g" libs/libfm-qt/spkgbuild
+changelog "libs/libfm-qt/spkgbuild" "Upgraded from version 2.2.0 to version 2.3.0"
+sed -i -e "s/version=2.2.0/version=2.3.0/g" libs/liblxqt/spkgbuild
+changelog "libs/liblxqt/spkgbuild" "Upgraded from version 2.2.0 to version 2.3.0"
+sed -i -e "s/version=4.2.0/version=4.3.0/g" libs/libqtxdg/spkgbuild
+changelog "libs/libqtxdg/spkgbuild" "Upgraded from version 4.2.0 to version 4.3.0"
+sed -i -e "s/version=3.4.2/version=3.4.3/g" libs/openexr/spkgbuild
+changelog "libs/openexr/spkgbuild" "Upgraded from version 3.4.2 to version 3.4.3"
+sed -i -e "s/version=3.50.4.0/version=3.51.0.0/g" python/python-apsw/spkgbuild
+changelog "python/python-apsw/spkgbuild" "Upgraded from version 3.50.4.0 to version 3.51.0.0"
+sed -i -e "s/version=1.40.65/version=1.40.66/g" python/python-boto3/spkgbuild
+changelog "python/python-boto3/spkgbuild" "Upgraded from version 1.40.65 to version 1.40.66"
+sed -i -e "s/version=6.145.1/version=6.146.0/g" python/python-hypothesis/spkgbuild
+changelog "python/python-hypothesis/spkgbuild" "Upgraded from version 6.145.1 to version 6.146.0"
+sed -i -e "s/version=2.12.3/version=2.12.4/g" python/python-pydantic/spkgbuild
+changelog "python/python-pydantic/spkgbuild" "Upgraded from version 2.12.3 to version 2.12.4"
+sed -i -e "s/version=3.16.29/version=3.16.30/g" server/blacklist-hosts/spkgbuild
+changelog "server/blacklist-hosts/spkgbuild" "Upgraded from version 3.16.29 to version 3.16.30"
