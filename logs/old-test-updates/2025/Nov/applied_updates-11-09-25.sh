0cc45291d184bfc08f49b28129eadc6095bf603a
Voncloft <voncloft@gmail.com>
2025-11-09 11:33:52 -0500

    Minor Updates

---
+++ logs/old-test-updates/2025/Nov/applied_updates-11-09-25.sh
@@ -0,0 +1,48 @@
+sed -i -e "s/version=6.17/version=6.17.1/g" core/btrfs-progs/spkgbuild
+changelog "core/btrfs-progs/spkgbuild" "Upgraded from version 6.17 to version 6.17.1"
+sed -i -e "s/version=1.5.81/version=1.5.83/g" core/pipewire/spkgbuild
+changelog "core/pipewire/spkgbuild" "Upgraded from version 1.5.81 to version 1.5.83"
+sed -i -e "s/version=258.1/version=258.2/g" core/systemd/spkgbuild
+changelog "core/systemd/spkgbuild" "Upgraded from version 258.1 to version 258.2"
+sed -i -e "s/version=2.10.91/version=2.11.0/g" core/udisks2/spkgbuild
+changelog "core/udisks2/spkgbuild" "Upgraded from version 2.10.91 to version 2.11.0"
+sed -i -e "s/version=4.5.0/version=4.5.1/g" libs/libxcrypt/spkgbuild
+changelog "libs/libxcrypt/spkgbuild" "Upgraded from version 4.5.0 to version 4.5.1"
+sed -i -e "s/version=1.15.0/version=1.15.90/g" networking/tigervnc/spkgbuild
+changelog "networking/tigervnc/spkgbuild" "Upgraded from version 1.15.0 to version 1.15.90"
+sed -i -e "s/version=1.40.66/version=1.40.69/g" python/python-boto3/spkgbuild
+changelog "python/python-boto3/spkgbuild" "Upgraded from version 1.40.66 to version 1.40.69"
+sed -i -e "s/version=1.40.66/version=1.40.69/g" python/python-botocore/spkgbuild
+changelog "python/python-botocore/spkgbuild" "Upgraded from version 1.40.66 to version 1.40.69"
+sed -i -e "s/version=1.3.7/version=1.3.8/g" python/python-cerberus/spkgbuild
+changelog "python/python-cerberus/spkgbuild" "Upgraded from version 1.3.7 to version 1.3.8"
+sed -i -e "s/version=7.11.0/version=7.11.1/g" python/python-coverage/spkgbuild
+changelog "python/python-coverage/spkgbuild" "Upgraded from version 7.11.0 to version 7.11.1"
+sed -i -e "s/version=2025.10.0/version=2025.11.0/g" python/python-distributed/spkgbuild
+changelog "python/python-distributed/spkgbuild" "Upgraded from version 2025.10.0 to version 2025.11.0"
+sed -i -e "s/version=2.186.0/version=2.187.0/g" python/python-google-api-python-client/spkgbuild
+changelog "python/python-google-api-python-client/spkgbuild" "Upgraded from version 2.186.0 to version 2.187.0"
+sed -i -e "s/version=2.42.1/version=2.43.0/g" python/python-google-auth/spkgbuild
+changelog "python/python-google-auth/spkgbuild" "Upgraded from version 2.42.1 to version 2.43.0"
+sed -i -e "s/version=7.0.0/version=8.0.0/g" python/python-hacking/spkgbuild
+changelog "python/python-hacking/spkgbuild" "Upgraded from version 7.0.0 to version 8.0.0"
+sed -i -e "s/version=2.9.0/version=2.10.0/g" python/python-hcloud/spkgbuild
+changelog "python/python-hcloud/spkgbuild" "Upgraded from version 2.9.0 to version 2.10.0"
+sed -i -e "s/version=6.146.0/version=6.147.0/g" python/python-hypothesis/spkgbuild
+changelog "python/python-hypothesis/spkgbuild" "Upgraded from version 6.146.0 to version 6.147.0"
+sed -i -e "s/version=1.8.2/version=1.9.1/g" python/python-logbook/spkgbuild
+changelog "python/python-logbook/spkgbuild" "Upgraded from version 1.8.2 to version 1.9.1"
+sed -i -e "s/version=9.0.17/version=9.0.18/g" python/python-phonenumbers/spkgbuild
+changelog "python/python-phonenumbers/spkgbuild" "Upgraded from version 9.0.17 to version 9.0.18"
+sed -i -e "s/version=1.15/version=1.16/g" python/python-pypandoc/spkgbuild
+changelog "python/python-pypandoc/spkgbuild" "Upgraded from version 1.15 to version 1.16"
+sed -i -e "s/version=5.2.1/version=5.2.2/g" python/python-pytest-benchmark/spkgbuild
+changelog "python/python-pytest-benchmark/spkgbuild" "Upgraded from version 5.2.1 to version 5.2.2"
+sed -i -e "s/version=2.0.0/version=2.0.1/g" python/python-wrapt/spkgbuild
+changelog "python/python-wrapt/spkgbuild" "Upgraded from version 2.0.0 to version 2.0.1"
+sed -i -e "s/version=6.0/version=6.1/g" python/python-zope-event/spkgbuild
+changelog "python/python-zope-event/spkgbuild" "Upgraded from version 6.0 to version 6.1"
+sed -i -e "s/version=3.16.30/version=3.16.31/g" server/blacklist-hosts/spkgbuild
+changelog "server/blacklist-hosts/spkgbuild" "Upgraded from version 3.16.30 to version 3.16.31"
+sed -i -e "s/version=2025.11.0/version=2025.11.1/g" server/homeassistant/spkgbuild
+changelog "server/homeassistant/spkgbuild" "Upgraded from version 2025.11.0 to version 2025.11.1"
