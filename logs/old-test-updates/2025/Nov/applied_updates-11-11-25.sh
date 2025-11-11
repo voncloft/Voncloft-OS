b8fee61c4287d9e036a4b8a0b8cac060e80d91bc
Voncloft <voncloft@gmail.com>
2025-11-11 04:08:55 -0500

    Minor Updates

---
+++ logs/old-test-updates/2025/Nov/applied_updates-11-11-25.sh
@@ -0,0 +1,34 @@
+sed -i -e "s/version=9.8/version=9.9/g" core/coreutils/spkgbuild
+changelog "core/coreutils/spkgbuild" "Upgraded from version 9.8 to version 9.9"
+sed -i -e "s/version=4.5.1/version=4.5.2/g" libs/libxcrypt/spkgbuild
+changelog "libs/libxcrypt/spkgbuild" "Upgraded from version 4.5.1 to version 4.5.2"
+sed -i -e "s/version=1.30.0/version=1.31.0/g" libs/pkcs11-helper/spkgbuild
+changelog "libs/pkcs11-helper/spkgbuild" "Upgraded from version 1.30.0 to version 1.31.0"
+sed -i -e "s/version=0.4.8.19/version=0.4.8.20/g" networking/tor/spkgbuild
+changelog "networking/tor/spkgbuild" "Upgraded from version 0.4.8.19 to version 0.4.8.20"
+sed -i -e "s/version=4.0.1/version=4.0.2/g" python/python-astroid/spkgbuild
+changelog "python/python-astroid/spkgbuild" "Upgraded from version 4.0.1 to version 4.0.2"
+sed -i -e "s/version=1.40.69/version=1.40.70/g" python/python-botocore/spkgbuild
+changelog "python/python-botocore/spkgbuild" "Upgraded from version 1.40.69 to version 1.40.70"
+sed -i -e "s/version=7.11.1/version=7.11.3/g" python/python-coverage/spkgbuild
+changelog "python/python-coverage/spkgbuild" "Upgraded from version 7.11.1 to version 7.11.3"
+sed -i -e "s/version=0.121.0/version=0.121.1/g" python/python-fastapi/spkgbuild
+changelog "python/python-fastapi/spkgbuild" "Upgraded from version 0.121.0 to version 0.121.1"
+sed -i -e "s/version=2.10.0/version=2.11.0/g" python/python-hcloud/spkgbuild
+changelog "python/python-hcloud/spkgbuild" "Upgraded from version 2.10.0 to version 2.11.0"
+sed -i -e "s/version=4.3.0/version=4.4.0/g" python/python-pre-commit/spkgbuild
+changelog "python/python-pre-commit/spkgbuild" "Upgraded from version 4.3.0 to version 4.4.0"
+sed -i -e "s/version=1.2.0/version=1.3.0/g" python/python-pytest-asyncio/spkgbuild
+changelog "python/python-pytest-asyncio/spkgbuild" "Upgraded from version 1.2.0 to version 1.3.0"
+sed -i -e "s/version=5.2.2/version=5.2.3/g" python/python-pytest-benchmark/spkgbuild
+changelog "python/python-pytest-benchmark/spkgbuild" "Upgraded from version 5.2.2 to version 5.2.3"
+sed -i -e "s/version=2.7.2/version=2.8.0/g" python/python-testtools/spkgbuild
+changelog "python/python-testtools/spkgbuild" "Upgraded from version 2.7.2 to version 2.8.0"
+sed -i -e "s/version=1.45.0/version=1.46.0/g" ruby-gems/ruby-tins/spkgbuild
+changelog "ruby-gems/ruby-tins/spkgbuild" "Upgraded from version 1.45.0 to version 1.46.0"
+sed -i -e "s/version=3.5.7/version=3.5.8/g" server/radicale/spkgbuild
+changelog "server/radicale/spkgbuild" "Upgraded from version 3.5.7 to version 3.5.8"
+sed -i -e "s/version=4.23.2/version=4.23.3/g" server/samba/spkgbuild
+changelog "server/samba/spkgbuild" "Upgraded from version 4.23.2 to version 4.23.3"
+sed -i -e "s/version=1.11.2/version=1.11.3/g" xfce/xfce4-screenshooter/spkgbuild
+changelog "xfce/xfce4-screenshooter/spkgbuild" "Upgraded from version 1.11.2 to version 1.11.3"
