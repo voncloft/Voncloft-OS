7bc8cd91ddd075e119f062a552ddc90bf4e3759a
Voncloft <voncloft@gmail.com>
2025-11-16 12:24:35 -0500

    Minor Updates

---
+++ logs/old-test-updates/2025/Nov/applied_updates-11-16-25.sh
@@ -0,0 +1,10 @@
+sed -i -e "s/version=25.2.6/version=25.2.7/g" hardware/mesa/spkgbuild
+changelog "hardware/mesa/spkgbuild" "Upgraded from version 25.2.6 to version 25.2.7"
+sed -i -e "s/version=4.2.0-rc2-sunos-x86_64/version=4.2.0-rc3-sunos-x86_64/g" compilers/cmake/spkgbuild
+changelog "compilers/cmake/spkgbuild" "Upgraded from version 4.2.0-rc2-sunos-x86_64 to version 4.2.0-rc3-sunos-x86_64"
+sed -i -e "s/version=2.45/version=2.45.1/g" core/binutils/spkgbuild
+changelog "core/binutils/spkgbuild" "Upgraded from version 2.45 to version 2.45.1"
+sed -i -e "s/version=1.5.32/version=1.5.33/g" core/ibus/spkgbuild
+changelog "core/ibus/spkgbuild" "Upgraded from version 1.5.32 to version 1.5.33"
+sed -i -e "s/version=8.6/version=8.7/g" core/nano/spkgbuild
+changelog "core/nano/spkgbuild" "Upgraded from version 8.6 to version 8.7"
