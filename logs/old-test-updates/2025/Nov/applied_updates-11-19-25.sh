62f5125ee2998f01a37f92d7eb6a4eaf1e136ff0
Voncloft <voncloft@gmail.com>
2025-11-19 09:23:55 -0500

    Minor Updates

---
+++ logs/old-test-updates/2025/Nov/applied_updates-11-19-25.sh
@@ -0,0 +1,44 @@
+sed -i -e "s/version=21.1.5/version=21.1.6/g" compilers/clang/spkgbuild
+changelog "compilers/clang/spkgbuild" "Upgraded from version 21.1.5 to version 21.1.6"
+sed -i -e "s/version=4.2.0-rc3-sunos-x86_64/version=4.2.0-rc4-sunos-x86_64/g" compilers/cmake/spkgbuild
+changelog "compilers/cmake/spkgbuild" "Upgraded from version 4.2.0-rc3-sunos-x86_64 to version 4.2.0-rc4-sunos-x86_64"
+sed -i -e "s/version=21.1.5/version=21.1.6/g" compilers/lld/spkgbuild
+changelog "compilers/lld/spkgbuild" "Upgraded from version 21.1.5 to version 21.1.6"
+sed -i -e "s/version=21.1.5/version=21.1.6/g" compilers/llvm/spkgbuild
+changelog "compilers/llvm/spkgbuild" "Upgraded from version 21.1.5 to version 21.1.6"
+sed -i -e "s/version=21.1.5/version=21.1.6/g" compilers/llvm-bak/spkgbuild
+changelog "compilers/llvm-bak/spkgbuild" "Upgraded from version 21.1.5 to version 21.1.6"
+sed -i -e "s/version=21.1.5/version=21.1.6/g" compilers/llvm-old-2023-06-07/spkgbuild
+changelog "compilers/llvm-old-2023-06-07/spkgbuild" "Upgraded from version 21.1.5 to version 21.1.6"
+sed -i -e "s/version=3.117/version=3.118.1/g" core/nss/spkgbuild
+changelog "core/nss/spkgbuild" "Upgraded from version 3.117 to version 3.118.1"
+sed -i -e "s/version=8.2.3/version=8.4.0/g" core/redis/spkgbuild
+changelog "core/redis/spkgbuild" "Upgraded from version 8.2.3 to version 8.4.0"
+sed -i -e "s/version=2.51.1/version=2.51.2/g" core/webkitgtk/spkgbuild
+changelog "core/webkitgtk/spkgbuild" "Upgraded from version 2.51.1 to version 2.51.2"
+sed -i -e "s/version=3.8.2/version=3.8.3/g" libs/libarchive/spkgbuild
+changelog "libs/libarchive/spkgbuild" "Upgraded from version 3.8.2 to version 3.8.3"
+sed -i -e "s/version=1.40.74/version=1.40.76/g" python/python-boto3/spkgbuild
+changelog "python/python-boto3/spkgbuild" "Upgraded from version 1.40.74 to version 1.40.76"
+sed -i -e "s/version=1.40.74/version=1.40.76/g" python/python-botocore/spkgbuild
+changelog "python/python-botocore/spkgbuild" "Upgraded from version 1.40.74 to version 1.40.76"
+sed -i -e "s/version=1.40.4/version=1.41.0/g" python/python-cfn-lint/spkgbuild
+changelog "python/python-cfn-lint/spkgbuild" "Upgraded from version 1.40.4 to version 1.41.0"
+sed -i -e "s/version=7.11.3/version=7.12.0/g" python/python-coverage/spkgbuild
+changelog "python/python-coverage/spkgbuild" "Upgraded from version 7.11.3 to version 7.12.0"
+sed -i -e "s/version=6.148.1/version=6.148.2/g" python/python-hypothesis/spkgbuild
+changelog "python/python-hypothesis/spkgbuild" "Upgraded from version 6.148.1 to version 6.148.2"
+sed -i -e "s/version=3.6.0/version=3.7.0/g" python/python-mypy-protobuf/spkgbuild
+changelog "python/python-mypy-protobuf/spkgbuild" "Upgraded from version 3.6.0 to version 3.7.0"
+sed -i -e "s/version=6.4/version=6.5/g" python/python-persistent/spkgbuild
+changelog "python/python-persistent/spkgbuild" "Upgraded from version 6.4 to version 6.5"
+sed -i -e "s/version=0.6.10/version=0.6.11/g" python/python-podcastparser/spkgbuild
+changelog "python/python-podcastparser/spkgbuild" "Upgraded from version 0.6.10 to version 0.6.11"
+sed -i -e "s/version=8.1/version=8.2/g" python/python-zope-hookable/spkgbuild
+changelog "python/python-zope-hookable/spkgbuild" "Upgraded from version 8.1 to version 8.2"
+sed -i -e "s/version=8.1/version=8.2/g" python/python-zope-i18nmessageid/spkgbuild
+changelog "python/python-zope-i18nmessageid/spkgbuild" "Upgraded from version 8.1 to version 8.2"
+sed -i -e "s/version=7.0/version=7.1/g" python/python-zope-proxy/spkgbuild
+changelog "python/python-zope-proxy/spkgbuild" "Upgraded from version 7.0 to version 7.1"
+sed -i -e "s/version=3.16.34/version=3.16.35/g" server/blacklist-hosts/spkgbuild
+changelog "server/blacklist-hosts/spkgbuild" "Upgraded from version 3.16.34 to version 3.16.35"
