f49312b449a6fb777811a0c1a2f65cf77530228e
Voncloft <voncloft@gmail.com>
2025-11-24 04:32:13 -0500

    Minor Updates

---
+++ logs/old-test-updates/2025/Nov/applied_updates-11-24-25.sh
@@ -0,0 +1,10 @@
+sed -i -e "s/version=4.7.1/version=4.7.2/g" core/weechat/spkgbuild
+changelog "core/weechat/spkgbuild" "Upgraded from version 4.7.1 to version 4.7.2"
+sed -i -e "s/version=3.18.24/version=3.18.25/g" extra/flexget/spkgbuild
+changelog "extra/flexget/spkgbuild" "Upgraded from version 3.18.24 to version 3.18.25"
+sed -i -e "s/version=5.7.0/version=5.7.1/g" python/python-flask-security-too/spkgbuild
+changelog "python/python-flask-security-too/spkgbuild" "Upgraded from version 5.7.0 to version 5.7.1"
+sed -i -e "s/version=4.4.0/version=4.5.0/g" python/python-pre-commit/spkgbuild
+changelog "python/python-pre-commit/spkgbuild" "Upgraded from version 4.4.0 to version 4.5.0"
+sed -i -e "s/version=3.13.0/version=3.13.1/g" python/python-sqlobject/spkgbuild
+changelog "python/python-sqlobject/spkgbuild" "Upgraded from version 3.13.0 to version 3.13.1"
