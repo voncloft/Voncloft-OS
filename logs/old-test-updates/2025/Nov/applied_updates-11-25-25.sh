3f392bc98d32d79e806bf130d08cb1e085480a87
Voncloft <voncloft@gmail.com>
2025-11-25 06:52:40 -0500

    Minor Updates

---
+++ logs/old-test-updates/2025/Nov/applied_updates-11-25-25.sh
@@ -0,0 +1,24 @@
+sed -i -e "s/version=1.9.1/version=1.10.0/g" compilers/meson/spkgbuild
+changelog "compilers/meson/spkgbuild" "Upgraded from version 1.9.1 to version 1.10.0"
+sed -i -e "s/version=6.17.8/version=6.17.9/g" core/linux-api-headers/spkgbuild
+changelog "core/linux-api-headers/spkgbuild" "Upgraded from version 6.17.8 to version 6.17.9"
+sed -i -e "s/version=6.17.8/version=6.17.9/g" core/linux-desktop/spkgbuild
+changelog "core/linux-desktop/spkgbuild" "Upgraded from version 6.17.8 to version 6.17.9"
+sed -i -e "s/version=6.17.8/version=6.17.9/g" core/linux-router/spkgbuild
+changelog "core/linux-router/spkgbuild" "Upgraded from version 6.17.8 to version 6.17.9"
+sed -i -e "s/version=3.18.25/version=3.18.26/g" extra/flexget/spkgbuild
+changelog "extra/flexget/spkgbuild" "Upgraded from version 3.18.25 to version 3.18.26"
+sed -i -e "s/version=146.0b6/version=146.0b7/g" nonfree/firefox/spkgbuild
+changelog "nonfree/firefox/spkgbuild" "Upgraded from version 146.0b6 to version 146.0b7"
+sed -i -e "s/version=1.41.2/version=1.41.3/g" python/python-boto3/spkgbuild
+changelog "python/python-boto3/spkgbuild" "Upgraded from version 1.41.2 to version 1.41.3"
+sed -i -e "s/version=1.41.2/version=1.41.3/g" python/python-botocore/spkgbuild
+changelog "python/python-botocore/spkgbuild" "Upgraded from version 1.41.2 to version 1.41.3"
+sed -i -e "s/version=8.4.1/version=8.5.0/g" python/python-cx-freeze/spkgbuild
+changelog "python/python-cx-freeze/spkgbuild" "Upgraded from version 8.4.1 to version 8.5.0"
+sed -i -e "s/version=0.121.3/version=0.122.0/g" python/python-fastapi/spkgbuild
+changelog "python/python-fastapi/spkgbuild" "Upgraded from version 0.121.3 to version 0.122.0"
+sed -i -e "s/version=1.1.0/version=1.2.2/g" python/python-sphinxcontrib-mermaid/spkgbuild
+changelog "python/python-sphinxcontrib-mermaid/spkgbuild" "Upgraded from version 1.1.0 to version 1.2.2"
+sed -i -e "s/version=4.9.0/version=4.10.0/g" python/python-trimesh/spkgbuild
+changelog "python/python-trimesh/spkgbuild" "Upgraded from version 4.9.0 to version 4.10.0"
