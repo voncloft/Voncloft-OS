83ad39541123c6af3f525a43ecb86035282fc1aa
Voncloft <voncloft@gmail.com>
2025-11-26 04:35:04 -0500

    Minor Updates

---
+++ logs/old-test-updates/2025/Nov/applied_updates-11-26-25.sh
@@ -0,0 +1,34 @@
+sed -i -e "s/version=6.4.2/version=6.6.0/g" cinnamon/cinnamon-control-center/spkgbuild
+changelog "cinnamon/cinnamon-control-center/spkgbuild" "Upgraded from version 6.4.2 to version 6.6.0"
+sed -i -e "s/version=6.4.2/version=6.6.0/g" cinnamon/cinnamon-desktop/spkgbuild
+changelog "cinnamon/cinnamon-desktop/spkgbuild" "Upgraded from version 6.4.2 to version 6.6.0"
+sed -i -e "s/version=6.4.0/version=6.6.0/g" cinnamon/cinnamon-menus/spkgbuild
+changelog "cinnamon/cinnamon-menus/spkgbuild" "Upgraded from version 6.4.0 to version 6.6.0"
+sed -i -e "s/version=6.4.1/version=6.6.0/g" cinnamon/cinnamon-screensaver/spkgbuild
+changelog "cinnamon/cinnamon-screensaver/spkgbuild" "Upgraded from version 6.4.1 to version 6.6.0"
+sed -i -e "s/version=6.4.2/version=6.6.0/g" cinnamon/cinnamon-session/spkgbuild
+changelog "cinnamon/cinnamon-session/spkgbuild" "Upgraded from version 6.4.2 to version 6.6.0"
+sed -i -e "s/version=6.4.3/version=6.6.0/g" cinnamon/cinnamon-settings-daemon/spkgbuild
+changelog "cinnamon/cinnamon-settings-daemon/spkgbuild" "Upgraded from version 6.4.3 to version 6.6.0"
+sed -i -e "s/version=6.4.2/version=6.6.0/g" cinnamon/cinnamon-translations/spkgbuild
+changelog "cinnamon/cinnamon-translations/spkgbuild" "Upgraded from version 6.4.2 to version 6.6.0"
+sed -i -e "s/version=6.4.5/version=6.6.0/g" cinnamon/nemo/spkgbuild
+changelog "cinnamon/nemo/spkgbuild" "Upgraded from version 6.4.5 to version 6.6.0"
+sed -i -e "s/version=3.18.26/version=3.18.27/g" extra/flexget/spkgbuild
+changelog "extra/flexget/spkgbuild" "Upgraded from version 3.18.26 to version 3.18.27"
+sed -i -e "s/version=2.3.0/version=2.3.1/g" lxqt/lxqt-panel/spkgbuild
+changelog "lxqt/lxqt-panel/spkgbuild" "Upgraded from version 2.3.0 to version 2.3.1"
+sed -i -e "s/version=0.35.0/version=0.36.0/g" libs/neon/spkgbuild
+changelog "libs/neon/spkgbuild" "Upgraded from version 0.35.0 to version 0.36.0"
+sed -i -e "s/version=1.41.3/version=1.41.4/g" python/python-boto3/spkgbuild
+changelog "python/python-boto3/spkgbuild" "Upgraded from version 1.41.3 to version 1.41.4"
+sed -i -e "s/version=1.41.3/version=1.41.4/g" python/python-botocore/spkgbuild
+changelog "python/python-botocore/spkgbuild" "Upgraded from version 1.41.3 to version 1.41.4"
+sed -i -e "s/version=2.2.1/version=2.3.0/g" python/python-gmpy2/spkgbuild
+changelog "python/python-gmpy2/spkgbuild" "Upgraded from version 2.2.1 to version 2.3.0"
+sed -i -e "s/version=5.6.0/version=5.6.1/g" python/python-kombu/spkgbuild
+changelog "python/python-kombu/spkgbuild" "Upgraded from version 5.6.0 to version 5.6.1"
+sed -i -e "s/version=2.3.0/version=2.3.1/g" python/python-param/spkgbuild
+changelog "python/python-param/spkgbuild" "Upgraded from version 2.3.0 to version 2.3.1"
+sed -i -e "s/version=21.1.20/version=21.1.21/g" xorg/xorg-server/spkgbuild
+changelog "xorg/xorg-server/spkgbuild" "Upgraded from version 21.1.20 to version 21.1.21"
