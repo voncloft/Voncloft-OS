6054c6bbc9205a4114f1116eeb8b90ebd2058240
Voncloft <voncloft@gmail.com>
2025-12-01 12:07:03 -0500

    Minor Updates

---
+++ logs/old-test-updates/2025/Dec/applied_updates-12-01-25.sh
@@ -0,0 +1,78 @@
+sed -i -e "s/version=4.12.1/version=4.12.2/g" compilers/ccache/spkgbuild
+changelog "compilers/ccache/spkgbuild" "Upgraded from version 4.12.1 to version 4.12.2"
+sed -i -e "s/version=2.4.14/version=2.4.15/g" core/cups/spkgbuild
+changelog "core/cups/spkgbuild" "Upgraded from version 2.4.14 to version 2.4.15"
+sed -i -e "s/version=1.83/version=1.84/g" core/os-prober/spkgbuild
+changelog "core/os-prober/spkgbuild" "Upgraded from version 1.83 to version 1.84"
+sed -i -e "s/version=1.5.83/version=1.5.84/g" core/pipewire/spkgbuild
+changelog "core/pipewire/spkgbuild" "Upgraded from version 1.5.83 to version 1.5.84"
+sed -i -e "s/version=3.6.1/version=3.6.3/g" core/qutebrowser/spkgbuild
+changelog "core/qutebrowser/spkgbuild" "Upgraded from version 3.6.1 to version 3.6.3"
+sed -i -e "s/version=3.5/version=3.6/g" core/tmux/spkgbuild
+changelog "core/tmux/spkgbuild" "Upgraded from version 3.5 to version 3.6"
+sed -i -e "s/version=4.7.2/version=4.8.0/g" core/weechat/spkgbuild
+changelog "core/weechat/spkgbuild" "Upgraded from version 4.7.2 to version 4.8.0"
+sed -i -e "s/version=14.1/version=14.2/g" core/yad/spkgbuild
+changelog "core/yad/spkgbuild" "Upgraded from version 14.1 to version 14.2"
+sed -i -e "s/version=4.1.5/version=4.1.6/g" media/projectm/spkgbuild
+changelog "media/projectm/spkgbuild" "Upgraded from version 4.1.5 to version 4.1.6"
+sed -i -e "s/version=7.8.4/version=7.10.0/g" libs/libatomic_ops/spkgbuild
+changelog "libs/libatomic_ops/spkgbuild" "Upgraded from version 7.8.4 to version 7.10.0"
+sed -i -e "s/version=2.3.0/version=2.3.1/g" libs/libfm-qt/spkgbuild
+changelog "libs/libfm-qt/spkgbuild" "Upgraded from version 2.3.0 to version 2.3.1"
+sed -i -e "s/version=2.6.16/version=2.6.17/g" networking/openvpn/spkgbuild
+changelog "networking/openvpn/spkgbuild" "Upgraded from version 2.6.16 to version 2.6.17"
+sed -i -e "s/version=146.0b7/version=146.0b9/g" nonfree/firefox/spkgbuild
+changelog "nonfree/firefox/spkgbuild" "Upgraded from version 146.0b7 to version 146.0b9"
+sed -i -e "s/version=2.25.2/version=2.26.0/g" python/python-aiobotocore/spkgbuild
+changelog "python/python-aiobotocore/spkgbuild" "Upgraded from version 2.25.2 to version 2.26.0"
+sed -i -e "s/version=4.11.0/version=4.12.0/g" python/python-anyio/spkgbuild
+changelog "python/python-anyio/spkgbuild" "Upgraded from version 4.11.0 to version 4.12.0"
+sed -i -e "s/version=3.51.0.0/version=3.51.1.0/g" python/python-apsw/spkgbuild
+changelog "python/python-apsw/spkgbuild" "Upgraded from version 3.51.0.0 to version 3.51.1.0"
+sed -i -e "s/version=1.41.4/version=1.41.5/g" python/python-boto3/spkgbuild
+changelog "python/python-boto3/spkgbuild" "Upgraded from version 1.41.4 to version 1.41.5"
+sed -i -e "s/version=1.41.4/version=1.41.5/g" python/python-botocore/spkgbuild
+changelog "python/python-botocore/spkgbuild" "Upgraded from version 1.41.4 to version 1.41.5"
+sed -i -e "s/version=0.40.3/version=0.40.4/g" python/python-eventlet/spkgbuild
+changelog "python/python-eventlet/spkgbuild" "Upgraded from version 0.40.3 to version 0.40.4"
+sed -i -e "s/version=0.122.0/version=0.123.0/g" python/python-fastapi/spkgbuild
+changelog "python/python-fastapi/spkgbuild" "Upgraded from version 0.122.0 to version 0.123.0"
+sed -i -e "s/version=4.2.4/version=5.0.0/g" python/python-fints/spkgbuild
+changelog "python/python-fints/spkgbuild" "Upgraded from version 4.2.4 to version 5.0.0"
+sed -i -e "s/version=4.60.1/version=4.61.0/g" python/python-fonttools/spkgbuild
+changelog "python/python-fonttools/spkgbuild" "Upgraded from version 4.60.1 to version 4.61.0"
+sed -i -e "s/version=0.7.9/version=0.7.10/g" python/python-genshi/spkgbuild
+changelog "python/python-genshi/spkgbuild" "Upgraded from version 0.7.9 to version 0.7.10"
+sed -i -e "s/version=6.148.2/version=6.148.3/g" python/python-hypothesis/spkgbuild
+changelog "python/python-hypothesis/spkgbuild" "Upgraded from version 6.148.2 to version 6.148.3"
+sed -i -e "s/version=24.7.2/version=24.11.0/g" python/python-incremental/spkgbuild
+changelog "python/python-incremental/spkgbuild" "Upgraded from version 24.7.2 to version 24.11.0"
+sed -i -e "s/version=0.47.0/version=0.49.0/g" python/python-libtmux/spkgbuild
+changelog "python/python-libtmux/spkgbuild" "Upgraded from version 0.47.0 to version 0.49.0"
+sed -i -e "s/version=1.9.1/version=1.9.2/g" python/python-logbook/spkgbuild
+changelog "python/python-logbook/spkgbuild" "Upgraded from version 1.9.1 to version 1.9.2"
+sed -i -e "s/version=2.4.0/version=2.5.0/g" python/python-numpy/spkgbuild
+changelog "python/python-numpy/spkgbuild" "Upgraded from version 2.4.0 to version 2.5.0"
+sed -i -e "s/version=2.12.4/version=2.12.5/g" python/python-pydantic/spkgbuild
+changelog "python/python-pydantic/spkgbuild" "Upgraded from version 2.12.4 to version 2.12.5"
+sed -i -e "s/version=4.0.3/version=4.0.4/g" python/python-pylint/spkgbuild
+changelog "python/python-pylint/spkgbuild" "Upgraded from version 4.0.3 to version 4.0.4"
+sed -i -e "s/version=7.4.0/version=7.5.0/g" python/python-rdflib/spkgbuild
+changelog "python/python-rdflib/spkgbuild" "Upgraded from version 7.4.0 to version 7.5.0"
+sed -i -e "s/version=1.7.2/version=1.8.0/g" python/python-scikit-learn/spkgbuild
+changelog "python/python-scikit-learn/spkgbuild" "Upgraded from version 1.7.2 to version 1.8.0"
+sed -i -e "s/version=1.2.2/version=2.0.0/g" python/python-sphinxcontrib-mermaid/spkgbuild
+changelog "python/python-sphinxcontrib-mermaid/spkgbuild" "Upgraded from version 1.2.2 to version 2.0.0"
+sed -i -e "s/version=0.5.3/version=0.5.4/g" python/python-sqlparse/spkgbuild
+changelog "python/python-sqlparse/spkgbuild" "Upgraded from version 0.5.3 to version 0.5.4"
+sed -i -e "s/version=3.1.3/version=3.1.4/g" python/python-werkzeug/spkgbuild
+changelog "python/python-werkzeug/spkgbuild" "Upgraded from version 3.1.3 to version 3.1.4"
+sed -i -e "s/version=3.16.36/version=3.16.38/g" server/blacklist-hosts/spkgbuild
+changelog "server/blacklist-hosts/spkgbuild" "Upgraded from version 3.16.36 to version 3.16.38"
+sed -i -e "s/version=2025.11.3/version=2025.12.0/g" server/homeassistant/spkgbuild
+changelog "server/homeassistant/spkgbuild" "Upgraded from version 2025.11.3 to version 2025.12.0"
+sed -i -e "s/version=3.10.5/version=3.10.6/g" server/postfix/spkgbuild
+changelog "server/postfix/spkgbuild" "Upgraded from version 3.10.5 to version 3.10.6"
+sed -i -e "s/version=3.5.8/version=3.5.9/g" server/radicale/spkgbuild
+changelog "server/radicale/spkgbuild" "Upgraded from version 3.5.8 to version 3.5.9"
