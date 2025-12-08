912506984c04825e1c272a1c66f9e46513f52882
Voncloft <voncloft@gmail.com>
2025-12-08 09:56:22 -0500

    Minor Updates

---
+++ logs/old-test-updates/2025/Dec/applied_updates-12-08-25.sh
@@ -0,0 +1,48 @@
+sed -i -e "s/version=3.118.1/version=3.119/g" core/nss/spkgbuild
+changelog "core/nss/spkgbuild" "Upgraded from version 3.118.1 to version 3.119"
+sed -i -e "s/version=12.7.8/version=12.7.9/g" core/sysstat/spkgbuild
+changelog "core/sysstat/spkgbuild" "Upgraded from version 12.7.8 to version 12.7.9"
+sed -i -e "s/version=33.1/version=33.2/g" extra/protobuf/spkgbuild
+changelog "extra/protobuf/spkgbuild" "Upgraded from version 33.1 to version 33.2"
+sed -i -e "s/version=1.6.52/version=1.6.53/g" libs/libpng/spkgbuild
+changelog "libs/libpng/spkgbuild" "Upgraded from version 1.6.52 to version 1.6.53"
+sed -i -e "s/version=1.1.5/version=1.1.6/g" networking/nftables/spkgbuild
+changelog "networking/nftables/spkgbuild" "Upgraded from version 1.1.5 to version 1.1.6"
+sed -i -e "s/version=3.22.0/version=3.23.0/g" python/python-aiogram/spkgbuild
+changelog "python/python-aiogram/spkgbuild" "Upgraded from version 3.22.0 to version 3.23.0"
+sed -i -e "s/version=1.42.3/version=1.42.4/g" python/python-boto3/spkgbuild
+changelog "python/python-boto3/spkgbuild" "Upgraded from version 1.42.3 to version 1.42.4"
+sed -i -e "s/version=1.42.3/version=1.42.4/g" python/python-botocore/spkgbuild
+changelog "python/python-botocore/spkgbuild" "Upgraded from version 1.42.3 to version 1.42.4"
+sed -i -e "s/version=0.123.8/version=0.124.0/g" python/python-fastapi/spkgbuild
+changelog "python/python-fastapi/spkgbuild" "Upgraded from version 0.123.8 to version 0.124.0"
+sed -i -e "s/version=6.148.6/version=6.148.7/g" python/python-hypothesis/spkgbuild
+changelog "python/python-hypothesis/spkgbuild" "Upgraded from version 6.148.6 to version 6.148.7"
+sed -i -e "s/version=0.50.0/version=0.51.0/g" python/python-libtmux/spkgbuild
+changelog "python/python-libtmux/spkgbuild" "Upgraded from version 0.50.0 to version 0.51.0"
+sed -i -e "s/version=3.11.4/version=3.11.5/g" python/python-orjson/spkgbuild
+changelog "python/python-orjson/spkgbuild" "Upgraded from version 3.11.4 to version 3.11.5"
+sed -i -e "s/version=9.0.19/version=9.0.20/g" python/python-phonenumbers/spkgbuild
+changelog "python/python-phonenumbers/spkgbuild" "Upgraded from version 9.0.19 to version 9.0.20"
+sed -i -e "s/version=2025.0.4/version=2025.1.1/g" python/python-pipenv/spkgbuild
+changelog "python/python-pipenv/spkgbuild" "Upgraded from version 2025.0.4 to version 2025.1.1"
+sed -i -e "s/version=2025.1.3/version=2025.1.5/g" python/python-pudb/spkgbuild
+changelog "python/python-pudb/spkgbuild" "Upgraded from version 2025.1.3 to version 2025.1.5"
+sed -i -e "s/version=4.0.3/version=4.0.6/g" python/python-pypubsub/spkgbuild
+changelog "python/python-pypubsub/spkgbuild" "Upgraded from version 4.0.3 to version 4.0.6"
+sed -i -e "s/version=4.10.0/version=4.10.1/g" python/python-trimesh/spkgbuild
+changelog "python/python-trimesh/spkgbuild" "Upgraded from version 4.10.0 to version 4.10.1"
+sed -i -e "s/version=2.5.0/version=2.6.0/g" python/python-urllib3/spkgbuild
+changelog "python/python-urllib3/spkgbuild" "Upgraded from version 2.5.0 to version 2.6.0"
+sed -i -e "s/version=3.3.0/version=3.4.0/g" python/python-yaspin/spkgbuild
+changelog "python/python-yaspin/spkgbuild" "Upgraded from version 3.3.0 to version 3.4.0"
+sed -i -e "s/version=1.47.0/version=1.48.0/g" ruby-gems/ruby-tins/spkgbuild
+changelog "ruby-gems/ruby-tins/spkgbuild" "Upgraded from version 1.47.0 to version 1.48.0"
+sed -i -e "s/version=0.9.37/version=0.9.38/g" ruby-gems/ruby-yard/spkgbuild
+changelog "ruby-gems/ruby-yard/spkgbuild" "Upgraded from version 0.9.37 to version 0.9.38"
+sed -i -e "s/version=3.16.39/version=3.16.40/g" server/blacklist-hosts/spkgbuild
+changelog "server/blacklist-hosts/spkgbuild" "Upgraded from version 3.16.39 to version 3.16.40"
+sed -i -e "s/version=2025.12.0/version=2025.12.1/g" server/homeassistant/spkgbuild
+changelog "server/homeassistant/spkgbuild" "Upgraded from version 2025.12.0 to version 2025.12.1"
+sed -i -e "s/version=3.10.6/version=3.10.7/g" server/postfix/spkgbuild
+changelog "server/postfix/spkgbuild" "Upgraded from version 3.10.6 to version 3.10.7"
