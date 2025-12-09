f2b16287b23ddfae6c9e616c4813c25e6fe094bb
Voncloft <voncloft@gmail.com>
2025-12-09 09:10:00 -0500

    Minor Updates

---
+++ logs/old-test-updates/2025/Dec/applied_updates-12-09-25.sh
@@ -0,0 +1,34 @@
+sed -i -e "s/version=3.3.1/version=3.4.0/g" core/double-conversion/spkgbuild
+changelog "core/double-conversion/spkgbuild" "Upgraded from version 3.3.1 to version 3.4.0"
+sed -i -e "s/version=1.8.0.3/version=1.8.1.0/g" core/socat/spkgbuild
+changelog "core/socat/spkgbuild" "Upgraded from version 1.8.0.3 to version 1.8.1.0"
+sed -i -e "s/version=4.4.0/version=4.4.1/g" core/swig/spkgbuild
+changelog "core/swig/spkgbuild" "Upgraded from version 4.4.0 to version 4.4.1"
+sed -i -e "s/version=3.18.29/version=3.18.30/g" extra/flexget/spkgbuild
+changelog "extra/flexget/spkgbuild" "Upgraded from version 3.18.29 to version 3.18.30"
+sed -i -e "s/version=1.2.14/version=1.2.15/g" media/alsa-utils/spkgbuild
+changelog "media/alsa-utils/spkgbuild" "Upgraded from version 1.2.14 to version 1.2.15"
+sed -i -e "s/version=1.2.14/version=1.2.15/g" libs/alsa-lib/spkgbuild
+changelog "libs/alsa-lib/spkgbuild" "Upgraded from version 1.2.14 to version 1.2.15"
+sed -i -e "s/version=0.1.116/version=0.1.118/g" libs/babl/spkgbuild
+changelog "libs/babl/spkgbuild" "Upgraded from version 0.1.116 to version 0.1.118"
+sed -i -e "s/version=1.34.5/version=1.34.6/g" libs/c-ares/spkgbuild
+changelog "libs/c-ares/spkgbuild" "Upgraded from version 1.34.5 to version 1.34.6"
+sed -i -e "s/version=2.4.129/version=2.4.130/g" libs/libdrm/spkgbuild
+changelog "libs/libdrm/spkgbuild" "Upgraded from version 2.4.129 to version 2.4.130"
+sed -i -e "s/version=2025.11.12/version=2025.12.08/g" networking/yt-dlp/spkgbuild
+changelog "networking/yt-dlp/spkgbuild" "Upgraded from version 2025.11.12 to version 2025.12.08"
+sed -i -e "s/version=1.42.4/version=1.42.5/g" python/python-botocore/spkgbuild
+changelog "python/python-botocore/spkgbuild" "Upgraded from version 1.42.4 to version 1.42.5"
+sed -i -e "s/version=7.12.1/version=7.13.0/g" python/python-coverage/spkgbuild
+changelog "python/python-coverage/spkgbuild" "Upgraded from version 7.12.1 to version 7.13.0"
+sed -i -e "s/version=0.51.0/version=0.52.1/g" python/python-libtmux/spkgbuild
+changelog "python/python-libtmux/spkgbuild" "Upgraded from version 0.51.0 to version 0.52.1"
+sed -i -e "s/version=3.6/version=3.6.1/g" python/python-networkx/spkgbuild
+changelog "python/python-networkx/spkgbuild" "Upgraded from version 3.6 to version 3.6.1"
+sed -i -e "s/version=4.0.6/version=4.0.7/g" python/python-pypubsub/spkgbuild
+changelog "python/python-pypubsub/spkgbuild" "Upgraded from version 4.0.6 to version 4.0.7"
+sed -i -e "s/version=2.6.0/version=2.6.1/g" python/python-urllib3/spkgbuild
+changelog "python/python-urllib3/spkgbuild" "Upgraded from version 2.6.0 to version 2.6.1"
+sed -i -e "s/version=8.0.0/version=8.1.0/g" python/python-vcrpy/spkgbuild
+changelog "python/python-vcrpy/spkgbuild" "Upgraded from version 8.0.0 to version 8.1.0"
