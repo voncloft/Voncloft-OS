c75543e23dbecc2619081b924380de329b300041
Voncloft <voncloft@gmail.com>
2025-12-23 12:09:40 -0500

    Minor Updates

---
+++ logs/old-test-updates/2025/Dec/applied_updates-12-23-25.sh
@@ -0,0 +1,18 @@
+sed -i -e "s/version=0.153.1/version=0.153.2/g" core/hugo/spkgbuild
+changelog "core/hugo/spkgbuild" "Upgraded from version 0.153.1 to version 0.153.2"
+sed -i -e "s/version=5.4.8/version=5.5.0/g" core/lua/spkgbuild
+changelog "core/lua/spkgbuild" "Upgraded from version 5.4.8 to version 5.5.0"
+sed -i -e "s/version=3.18.32/version=3.18.33/g" extra/flexget/spkgbuild
+changelog "extra/flexget/spkgbuild" "Upgraded from version 3.18.32 to version 3.18.33"
+sed -i -e "s/version=1.24.3/version=1.25.0/g" media/openal/spkgbuild
+changelog "media/openal/spkgbuild" "Upgraded from version 1.24.3 to version 1.25.0"
+sed -i -e "s/version=2.4.130/version=2.4.131/g" libs/libdrm/spkgbuild
+changelog "libs/libdrm/spkgbuild" "Upgraded from version 2.4.130 to version 2.4.131"
+sed -i -e "s/version=1.22.0/version=1.23.0/g" python/python-yarl/spkgbuild
+changelog "python/python-yarl/spkgbuild" "Upgraded from version 1.22.0 to version 1.23.0"
+sed -i -e "s/version=3.5.9/version=3.5.10/g" server/radicale/spkgbuild
+changelog "server/radicale/spkgbuild" "Upgraded from version 3.5.9 to version 3.5.10"
+sed -i -e "s/version=4.23.3/version=4.23.4/g" server/samba/spkgbuild
+changelog "server/samba/spkgbuild" "Upgraded from version 4.23.3 to version 4.23.4"
+sed -i -e "s/version=2024.1/version=2025.1/g" xorg/xorgproto/spkgbuild
+changelog "xorg/xorgproto/spkgbuild" "Upgraded from version 2024.1 to version 2025.1"
