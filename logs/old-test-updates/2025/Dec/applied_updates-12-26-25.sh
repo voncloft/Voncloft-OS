855a5b31cd08db2dc2c3811a518c0b915c0655fb
Voncloft <voncloft@gmail.com>
2025-12-26 09:10:06 -0500

    Minor Updates

---
+++ logs/old-test-updates/2025/Dec/applied_updates-12-26-25.sh
@@ -0,0 +1,42 @@
+sed -i -e "s/version=6.6.2/version=6.6.3/g" cinnamon/cinnamon/spkgbuild
+changelog "cinnamon/cinnamon/spkgbuild" "Upgraded from version 6.6.2 to version 6.6.3"
+sed -i -e "s/version=3.4.8/version=4.0.0/g" compilers/ruby/spkgbuild
+changelog "compilers/ruby/spkgbuild" "Upgraded from version 3.4.8 to version 4.0.0"
+sed -i -e "s/version=147.0b6/version=147.0b7/g" nonfree/firefox/spkgbuild
+changelog "nonfree/firefox/spkgbuild" "Upgraded from version 147.0b6 to version 147.0b7"
+sed -i -e "s/version=0.22.0/version=0.22.1/g" python/python-aiosqlite/spkgbuild
+changelog "python/python-aiosqlite/spkgbuild" "Upgraded from version 0.22.0 to version 0.22.1"
+sed -i -e "s/version=1.42.14/version=1.42.16/g" python/python-boto3/spkgbuild
+changelog "python/python-boto3/spkgbuild" "Upgraded from version 1.42.14 to version 1.42.16"
+sed -i -e "s/version=1.42.14/version=1.42.16/g" python/python-botocore/spkgbuild
+changelog "python/python-botocore/spkgbuild" "Upgraded from version 1.42.14 to version 1.42.16"
+sed -i -e "s/version=9.2.0/version=9.2.1/g" python/python-elasticsearch/spkgbuild
+changelog "python/python-elasticsearch/spkgbuild" "Upgraded from version 9.2.0 to version 9.2.1"
+sed -i -e "s/version=4.12.3/version=4.13.0/g" python/python-engineio/spkgbuild
+changelog "python/python-engineio/spkgbuild" "Upgraded from version 4.12.3 to version 4.13.0"
+sed -i -e "s/version=5.5.1/version=5.6.0/g" python/python-flask-socketio/spkgbuild
+changelog "python/python-flask-socketio/spkgbuild" "Upgraded from version 5.5.1 to version 5.6.0"
+sed -i -e "s/version=6.148.7/version=6.148.8/g" python/python-hypothesis/spkgbuild
+changelog "python/python-hypothesis/spkgbuild" "Upgraded from version 6.148.7 to version 6.148.8"
+sed -i -e "s/version=3.2.0/version=3.2.1/g" python/python-intervaltree/spkgbuild
+changelog "python/python-intervaltree/spkgbuild" "Upgraded from version 3.2.0 to version 3.2.1"
+sed -i -e "s/version=6.0.1/version=6.0.2/g" python/python-jaraco.context/spkgbuild
+changelog "python/python-jaraco.context/spkgbuild" "Upgraded from version 6.0.1 to version 6.0.2"
+sed -i -e "s/version=5.5.1/version=5.6.0/g" python/python-jaraco.test/spkgbuild
+changelog "python/python-jaraco.test/spkgbuild" "Upgraded from version 5.5.1 to version 5.6.0"
+sed -i -e "s/version=3.1.4/version=3.2.0/g" python/python-mistune/spkgbuild
+changelog "python/python-mistune/spkgbuild" "Upgraded from version 3.1.4 to version 3.2.0"
+sed -i -e "s/version=5.8.0/version=5.9.0/g" python/python-oslo-serialization/spkgbuild
+changelog "python/python-oslo-serialization/spkgbuild" "Upgraded from version 5.8.0 to version 5.9.0"
+sed -i -e "s/version=7.1.3/version=7.2.0/g" python/python-psutil/spkgbuild
+changelog "python/python-psutil/spkgbuild" "Upgraded from version 7.1.3 to version 7.2.0"
+sed -i -e "s/version=3.3.0/version=3.3.1/g" python/python-pyparsing/spkgbuild
+changelog "python/python-pyparsing/spkgbuild" "Upgraded from version 3.3.0 to version 3.3.1"
+sed -i -e "s/version=5.15.1/version=5.16.0/g" python/python-socketio/spkgbuild
+changelog "python/python-socketio/spkgbuild" "Upgraded from version 5.15.1 to version 5.16.0"
+sed -i -e "s/version=0.20.1/version=0.21.0/g" python/python-typer/spkgbuild
+changelog "python/python-typer/spkgbuild" "Upgraded from version 0.20.1 to version 0.21.0"
+sed -i -e "s/version=4.0.2/version=4.0.3/g" ruby-gems/ruby-bundler/spkgbuild
+changelog "ruby-gems/ruby-bundler/spkgbuild" "Upgraded from version 4.0.2 to version 4.0.3"
+sed -i -e "s/version=3.16.43/version=3.16.44/g" server/blacklist-hosts/spkgbuild
+changelog "server/blacklist-hosts/spkgbuild" "Upgraded from version 3.16.43 to version 3.16.44"
