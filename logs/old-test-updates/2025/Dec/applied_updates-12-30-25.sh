df668c2ee6c72ffc888af3ff54686dc6397cd19c
Voncloft <voncloft@gmail.com>
2025-12-30 09:34:05 -0500

    Minor Updates

---
+++ logs/old-test-updates/2025/Dec/applied_updates-12-30-25.sh
@@ -0,0 +1,40 @@
+sed -i -e "s/version=0.153.3/version=0.153.4/g" core/hugo/spkgbuild
+changelog "core/hugo/spkgbuild" "Upgraded from version 0.153.3 to version 0.153.4"
+sed -i -e "s/version=3.18.33/version=3.18.34/g" extra/flexget/spkgbuild
+changelog "extra/flexget/spkgbuild" "Upgraded from version 3.18.33 to version 3.18.34"
+sed -i -e "s/version=1.1.18/version=1.1.19rc1/g" extra/solaar/spkgbuild
+changelog "extra/solaar/spkgbuild" "Upgraded from version 1.1.18 to version 1.1.19rc1"
+sed -i -e "s/version=147.0b7/version=147.0b8/g" nonfree/firefox/spkgbuild
+changelog "nonfree/firefox/spkgbuild" "Upgraded from version 147.0b7 to version 147.0b8"
+sed -i -e "s/version=6.5.0/version=6.5.1/g" python/python-auditwheel/spkgbuild
+changelog "python/python-auditwheel/spkgbuild" "Upgraded from version 6.5.0 to version 6.5.1"
+sed -i -e "s/version=1.42.17/version=1.42.18/g" python/python-boto3/spkgbuild
+changelog "python/python-boto3/spkgbuild" "Upgraded from version 1.42.17 to version 1.42.18"
+sed -i -e "s/version=1.42.17/version=1.42.18/g" python/python-botocore/spkgbuild
+changelog "python/python-botocore/spkgbuild" "Upgraded from version 1.42.17 to version 1.42.18"
+sed -i -e "s/version=7.13.0/version=7.13.1/g" python/python-coverage/spkgbuild
+changelog "python/python-coverage/spkgbuild" "Upgraded from version 7.13.0 to version 7.13.1"
+sed -i -e "s/version=8.5.1/version=8.5.3/g" python/python-cx-freeze/spkgbuild
+changelog "python/python-cx-freeze/spkgbuild" "Upgraded from version 8.5.1 to version 8.5.3"
+sed -i -e "s/version=5.0.4/version=5.1.0/g" python/python-elementpath/spkgbuild
+changelog "python/python-elementpath/spkgbuild" "Upgraded from version 5.0.4 to version 5.1.0"
+sed -i -e "s/version=39.0.0/version=40.1.0/g" python/python-faker/spkgbuild
+changelog "python/python-faker/spkgbuild" "Upgraded from version 39.0.0 to version 40.1.0"
+sed -i -e "s/version=7.0.0/version=7.1.0/g" python/python-gitlab/spkgbuild
+changelog "python/python-gitlab/spkgbuild" "Upgraded from version 7.0.0 to version 7.1.0"
+sed -i -e "s/version=7.2.0/version=7.2.1/g" python/python-psutil/spkgbuild
+changelog "python/python-psutil/spkgbuild" "Upgraded from version 7.2.0 to version 7.2.1"
+sed -i -e "s/version=1.19.0/version=1.19.1/g" python/python-pygit2/spkgbuild
+changelog "python/python-pygit2/spkgbuild" "Upgraded from version 1.19.0 to version 1.19.1"
+sed -i -e "s/version=1.17.2/version=1.17.3/g" ruby-gems/ruby-ffi/spkgbuild
+changelog "ruby-gems/ruby-ffi/spkgbuild" "Upgraded from version 1.17.2 to version 1.17.3"
+sed -i -e "s/version=2025.12.4/version=2026.1.0/g" server/homeassistant/spkgbuild
+changelog "server/homeassistant/spkgbuild" "Upgraded from version 2025.12.4 to version 2026.1.0"
+sed -i -e "s/version=4.21.0/version=4.21.1/g" xfce/tumbler/spkgbuild
+changelog "xfce/tumbler/spkgbuild" "Upgraded from version 4.21.0 to version 4.21.1"
+sed -i -e "s/version=4.21.0/version=4.21.1/g" xfce/xfce4-power-manager/spkgbuild
+changelog "xfce/xfce4-power-manager/spkgbuild" "Upgraded from version 4.21.0 to version 4.21.1"
+sed -i -e "s/version=4.21.0/version=4.21.1/g" xfce/xfce4-session/spkgbuild
+changelog "xfce/xfce4-session/spkgbuild" "Upgraded from version 4.21.0 to version 4.21.1"
+sed -i -e "s/version=4.21.0/version=4.21.1/g" xfce/xfce4-settings/spkgbuild
+changelog "xfce/xfce4-settings/spkgbuild" "Upgraded from version 4.21.0 to version 4.21.1"
