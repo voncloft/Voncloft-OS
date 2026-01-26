7f69403c99f6c38690f3a57a9b2176fffe86d9b2
Voncloft <voncloft@gmail.com>
2026-01-26 12:06:11 -0500

    Minor Updates

---
+++ logs/old-test-updates/2026/Jan/applied_updates-01-26-26.sh
@@ -0,0 +1,22 @@
+sed -i -e "s/version=6.6.5/version=6.6.6/g" cinnamon/cinnamon/spkgbuild
+changelog "cinnamon/cinnamon/spkgbuild" "Upgraded from version 6.6.5 to version 6.6.6"
+sed -i -e "s/version=6.6.2/version=6.6.3/g" cinnamon/cinnamon-settings-daemon/spkgbuild
+changelog "cinnamon/cinnamon-settings-daemon/spkgbuild" "Upgraded from version 6.6.2 to version 6.6.3"
+sed -i -e "s/version=6.6.2/version=6.6.3/g" cinnamon/muffin/spkgbuild
+changelog "cinnamon/muffin/spkgbuild" "Upgraded from version 6.6.2 to version 6.6.3"
+sed -i -e "s/version=6.2.4/version=6.2.5/g" python/python-cachetools/spkgbuild
+changelog "python/python-cachetools/spkgbuild" "Upgraded from version 6.2.4 to version 6.2.5"
+sed -i -e "s/version=7.13.1/version=7.13.2/g" python/python-coverage/spkgbuild
+changelog "python/python-coverage/spkgbuild" "Upgraded from version 7.13.1 to version 7.13.2"
+sed -i -e "s/version=6.150.3/version=6.151.0/g" python/python-hypothesis/spkgbuild
+changelog "python/python-hypothesis/spkgbuild" "Upgraded from version 6.150.3 to version 6.151.0"
+sed -i -e "s/version=0.0.21/version=0.0.22/g" python/python-python-multipart/spkgbuild
+changelog "python/python-python-multipart/spkgbuild" "Upgraded from version 0.0.21 to version 0.0.22"
+sed -i -e "s/version=14.3.0/version=14.3.1/g" python/python-rich/spkgbuild
+changelog "python/python-rich/spkgbuild" "Upgraded from version 14.3.0 to version 14.3.1"
+sed -i -e "s/version=3.16.53/version=3.16.54/g" server/blacklist-hosts/spkgbuild
+changelog "server/blacklist-hosts/spkgbuild" "Upgraded from version 3.16.53 to version 3.16.54"
+sed -i -e "s/version=1.1.4/version=1.1.5/g" xfce/xfd/spkgbuild
+changelog "xfce/xfd/spkgbuild" "Upgraded from version 1.1.4 to version 1.1.5"
+sed -i -e "s/version=1.0.6/version=1.0.7/g" xorg/xkill/spkgbuild
+changelog "xorg/xkill/spkgbuild" "Upgraded from version 1.0.6 to version 1.0.7"
