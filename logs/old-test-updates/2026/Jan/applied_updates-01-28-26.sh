f3aa4fb9ba76ec9f23472f0601a441e1a39cf2a9
Voncloft <voncloft@gmail.com>
2026-01-28 12:24:01 -0500

    Minor Updates

---
+++ logs/old-test-updates/2026/Jan/applied_updates-01-28-26.sh
@@ -0,0 +1,48 @@
+sed -i -e "s/version=4.2.2-sunos-x86_64/version=4.2.3-sunos-x86_64/g" compilers/cmake/spkgbuild
+changelog "compilers/cmake/spkgbuild" "Upgraded from version 4.2.2-sunos-x86_64 to version 4.2.3-sunos-x86_64"
+sed -i -e "s/version=0.60.8.1/version=0.60.8.2/g" core/aspell/spkgbuild
+changelog "core/aspell/spkgbuild" "Upgraded from version 0.60.8.1 to version 0.60.8.2"
+sed -i -e "s/version=2.5.16/version=2.5.17/g" core/gnupg/spkgbuild
+changelog "core/gnupg/spkgbuild" "Upgraded from version 2.5.16 to version 2.5.17"
+sed -i -e "s/version=3.18.39/version=3.18.41/g" extra/flexget/spkgbuild
+changelog "extra/flexget/spkgbuild" "Upgraded from version 3.18.39 to version 3.18.41"
+sed -i -e "s/version=1.27.90/version=1.28.0/g" media/gst-libav/spkgbuild
+changelog "media/gst-libav/spkgbuild" "Upgraded from version 1.27.90 to version 1.28.0"
+sed -i -e "s/version=1.27.90/version=1.28.0/g" media/gst-plugins-bad/spkgbuild
+changelog "media/gst-plugins-bad/spkgbuild" "Upgraded from version 1.27.90 to version 1.28.0"
+sed -i -e "s/version=1.27.90/version=1.28.0/g" media/gst-plugins-base/spkgbuild
+changelog "media/gst-plugins-base/spkgbuild" "Upgraded from version 1.27.90 to version 1.28.0"
+sed -i -e "s/version=1.27.90/version=1.28.0/g" media/gst-plugins-good/spkgbuild
+changelog "media/gst-plugins-good/spkgbuild" "Upgraded from version 1.27.90 to version 1.28.0"
+sed -i -e "s/version=1.27.90/version=1.28.0/g" media/gst-plugins-ugly/spkgbuild
+changelog "media/gst-plugins-ugly/spkgbuild" "Upgraded from version 1.27.90 to version 1.28.0"
+sed -i -e "s/version=1.27.90/version=1.28.0/g" media/gstreamer/spkgbuild
+changelog "media/gstreamer/spkgbuild" "Upgraded from version 1.27.90 to version 1.28.0"
+sed -i -e "s/version=1.15.2/version=1.16.0/g" libs/libvpx/spkgbuild
+changelog "libs/libvpx/spkgbuild" "Upgraded from version 1.15.2 to version 1.16.0"
+sed -i -e "s/version=1.1.3/version=1.2.0/g" libs/libxkbfile/spkgbuild
+changelog "libs/libxkbfile/spkgbuild" "Upgraded from version 1.1.3 to version 1.2.0"
+sed -i -e "s/version=3.6.0/version=3.6.1/g" networking/openssl/spkgbuild
+changelog "networking/openssl/spkgbuild" "Upgraded from version 3.6.0 to version 3.6.1"
+sed -i -e "s/version=0.10.4.1/version=0.10.5/g" networking/xrdp/spkgbuild
+changelog "networking/xrdp/spkgbuild" "Upgraded from version 0.10.4.1 to version 0.10.5"
+sed -i -e "s/version=148.0b6/version=148.0b7/g" nonfree/firefox/spkgbuild
+changelog "nonfree/firefox/spkgbuild" "Upgraded from version 148.0b6 to version 148.0b7"
+sed -i -e "s/version=1.42.34/version=1.42.35/g" python/python-boto3/spkgbuild
+changelog "python/python-boto3/spkgbuild" "Upgraded from version 1.42.34 to version 1.42.35"
+sed -i -e "s/version=1.42.34/version=1.42.35/g" python/python-botocore/spkgbuild
+changelog "python/python-botocore/spkgbuild" "Upgraded from version 1.42.34 to version 1.42.35"
+sed -i -e "s/version=6.2.5/version=6.2.6/g" python/python-cachetools/spkgbuild
+changelog "python/python-cachetools/spkgbuild" "Upgraded from version 6.2.5 to version 6.2.6"
+sed -i -e "s/version=1.10.1/version=1.11.1/g" python/python-gssapi/spkgbuild
+changelog "python/python-gssapi/spkgbuild" "Upgraded from version 1.10.1 to version 1.11.1"
+sed -i -e "s/version=6.151.0/version=6.151.2/g" python/python-hypothesis/spkgbuild
+changelog "python/python-hypothesis/spkgbuild" "Upgraded from version 6.151.0 to version 6.151.2"
+sed -i -e "s/version=6.7.0/version=6.7.1/g" python/python-multidict/spkgbuild
+changelog "python/python-multidict/spkgbuild" "Upgraded from version 6.7.0 to version 6.7.1"
+sed -i -e "s/version=3.0.0/version=3.1.0/g" python/python-pandas/spkgbuild
+changelog "python/python-pandas/spkgbuild" "Upgraded from version 3.0.0 to version 3.1.0"
+sed -i -e "s/version=80.10.1/version=80.10.2/g" python/python-setuptools/spkgbuild
+changelog "python/python-setuptools/spkgbuild" "Upgraded from version 80.10.1 to version 80.10.2"
+sed -i -e "s/version=2.8.2/version=2.8.3/g" python/python-testtools/spkgbuild
+changelog "python/python-testtools/spkgbuild" "Upgraded from version 2.8.2 to version 2.8.3"
