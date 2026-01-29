272ff15a4df834b56332df6065b78713a19c4294
Voncloft <voncloft@gmail.com>
2026-01-29 10:27:50 -0500

    Minor Updates

---
+++ logs/old-test-updates/2026/Jan/applied_updates-01-29-26.sh
@@ -0,0 +1,34 @@
+sed -i -e "s/version=1.7.0/version=1.8.0/g" core/gparted/spkgbuild
+changelog "core/gparted/spkgbuild" "Upgraded from version 1.7.0 to version 1.8.0"
+sed -i -e "s/version=0.154.5/version=0.155.0/g" core/hugo/spkgbuild
+changelog "core/hugo/spkgbuild" "Upgraded from version 0.154.5 to version 0.155.0"
+sed -i -e "s/version=0.9.31/version=0.9.34/g" core/lmdb/spkgbuild
+changelog "core/lmdb/spkgbuild" "Upgraded from version 0.9.31 to version 0.9.34"
+sed -i -e "s/version=0.1.120/version=0.1.122/g" libs/babl/spkgbuild
+changelog "libs/babl/spkgbuild" "Upgraded from version 0.1.120 to version 0.1.122"
+sed -i -e "s/version=0.4.8.21/version=0.4.8.22/g" networking/tor/spkgbuild
+changelog "networking/tor/spkgbuild" "Upgraded from version 0.4.8.21 to version 0.4.8.22"
+sed -i -e "s/version=148.0b7/version=148.0b8/g" nonfree/firefox/spkgbuild
+changelog "nonfree/firefox/spkgbuild" "Upgraded from version 148.0b7 to version 148.0b8"
+sed -i -e "s/version=148.0b2/version=148.0b3/g" nonfree/thunderbird/spkgbuild
+changelog "nonfree/thunderbird/spkgbuild" "Upgraded from version 148.0b2 to version 148.0b3"
+sed -i -e "s/version=7.2.7.2.4/version=7.2.7.2.6/g" nonfree/virtualbox/spkgbuild
+changelog "nonfree/virtualbox/spkgbuild" "Upgraded from version 7.2.7.2.4 to version 7.2.7.2.6"
+sed -i -e "s/version=7.2.4/version=7.2.6/g" nonfree/virtualbox-guest-modules/spkgbuild
+changelog "nonfree/virtualbox-guest-modules/spkgbuild" "Upgraded from version 7.2.4 to version 7.2.6"
+sed -i -e "s/version=7.2.4/version=7.2.6/g" nonfree/virtualbox-guest-utils/spkgbuild
+changelog "nonfree/virtualbox-guest-utils/spkgbuild" "Upgraded from version 7.2.4 to version 7.2.6"
+sed -i -e "s/version=1.42.35/version=1.42.36/g" python/python-boto3/spkgbuild
+changelog "python/python-boto3/spkgbuild" "Upgraded from version 1.42.35 to version 1.42.36"
+sed -i -e "s/version=1.42.35/version=1.42.36/g" python/python-botocore/spkgbuild
+changelog "python/python-botocore/spkgbuild" "Upgraded from version 1.42.35 to version 1.42.36"
+sed -i -e "s/version=7.1.0/version=8.0.0/g" python/python-gitlab/spkgbuild
+changelog "python/python-gitlab/spkgbuild" "Upgraded from version 7.1.0 to version 8.0.0"
+sed -i -e "s/version=6.151.2/version=6.151.3/g" python/python-hypothesis/spkgbuild
+changelog "python/python-hypothesis/spkgbuild" "Upgraded from version 6.151.2 to version 6.151.3"
+sed -i -e "s/version=7.2.1/version=7.2.2/g" python/python-psutil/spkgbuild
+changelog "python/python-psutil/spkgbuild" "Upgraded from version 7.2.1 to version 7.2.2"
+sed -i -e "s/version=2026.1.3/version=2026.2.0/g" server/homeassistant/spkgbuild
+changelog "server/homeassistant/spkgbuild" "Upgraded from version 2026.1.3 to version 2026.2.0"
+sed -i -e "s/version=1.42.2.10156/version=1.43.0.10467/g" server/plex-media-server/spkgbuild
+changelog "server/plex-media-server/spkgbuild" "Upgraded from version 1.42.2.10156 to version 1.43.0.10467"
