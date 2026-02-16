0aad00039cf64f18457b270cfaf47f637b06b288
Voncloft <voncloft@gmail.com>
2026-02-16 08:34:47 -0500

    Minor Updates

---
+++ logs/old-test-updates/2026/Feb/applied_updates-02-16-26.sh
@@ -0,0 +1,6 @@
+sed -i -e "s/version=1.1.22/version=1.1.23/g" libs/libmtp/spkgbuild
+changelog "libs/libmtp/spkgbuild" "Upgraded from version 1.1.22 to version 1.1.23"
+sed -i -e "s/version=4.0.1/version=4.1.0/g" python/python-flatdict/spkgbuild
+changelog "python/python-flatdict/spkgbuild" "Upgraded from version 4.0.1 to version 4.1.0"
+sed -i -e "s/version=4.4.4/version=4.5.0/g" python/python-typeguard/spkgbuild
+changelog "python/python-typeguard/spkgbuild" "Upgraded from version 4.4.4 to version 4.5.0"
