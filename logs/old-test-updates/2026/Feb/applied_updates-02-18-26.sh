8bdd5a87a9d86a54f20fdeda226b4a03fb557231
Voncloft <voncloft@gmail.com>
2026-02-18 12:50:10 -0500

    Minor Updates

---
+++ logs/old-test-updates/2026/Feb/applied_updates-02-18-26.sh
@@ -0,0 +1,46 @@
+sed -i -e "s/version=6.19/version=6.19.2/g" core/linux-api-headers/spkgbuild
+changelog "core/linux-api-headers/spkgbuild" "Upgraded from version 6.19 to version 6.19.2"
+sed -i -e "s/version=6.19/version=6.19.2/g" core/linux-desktop/spkgbuild
+changelog "core/linux-desktop/spkgbuild" "Upgraded from version 6.19 to version 6.19.2"
+sed -i -e "s/version=6.19/version=6.19.2/g" core/linux-router/spkgbuild
+changelog "core/linux-router/spkgbuild" "Upgraded from version 6.19 to version 6.19.2"
+sed -i -e "s/version=3.120/version=3.120.1/g" core/nss/spkgbuild
+changelog "core/nss/spkgbuild" "Upgraded from version 3.120 to version 3.120.1"
+sed -i -e "s/version=1.3.1/version=1.3.2/g" core/zlib/spkgbuild
+changelog "core/zlib/spkgbuild" "Upgraded from version 1.3.1 to version 1.3.2"
+sed -i -e "s/version=2.46/version=2.46.1/g" extra/discordchatexporter/spkgbuild
+changelog "extra/discordchatexporter/spkgbuild" "Upgraded from version 2.46 to version 2.46.1"
+sed -i -e "s/version=3.18.47/version=3.18.48/g" extra/flexget/spkgbuild
+changelog "extra/flexget/spkgbuild" "Upgraded from version 3.18.47 to version 3.18.48"
+sed -i -e "s/version=5.6.5/version=5.6.6/g" networking/whois/spkgbuild
+changelog "networking/whois/spkgbuild" "Upgraded from version 5.6.5 to version 5.6.6"
+sed -i -e "s/version=148.0b5/version=148.0b6/g" nonfree/thunderbird/spkgbuild
+changelog "nonfree/thunderbird/spkgbuild" "Upgraded from version 148.0b5 to version 148.0b6"
+sed -i -e "s/version=1.42.49/version=1.42.50/g" python/python-boto3/spkgbuild
+changelog "python/python-boto3/spkgbuild" "Upgraded from version 1.42.49 to version 1.42.50"
+sed -i -e "s/version=1.42.49/version=1.42.50/g" python/python-botocore/spkgbuild
+changelog "python/python-botocore/spkgbuild" "Upgraded from version 1.42.49 to version 1.42.50"
+sed -i -e "s/version=4.13.1/version=4.13.2/g" python/python-cliff/spkgbuild
+changelog "python/python-cliff/spkgbuild" "Upgraded from version 4.13.1 to version 4.13.2"
+sed -i -e "s/version=3.24.0/version=3.24.2/g" python/python-filelock/spkgbuild
+changelog "python/python-filelock/spkgbuild" "Upgraded from version 3.24.0 to version 3.24.2"
+sed -i -e "s/version=6.151.6/version=6.151.9/g" python/python-hypothesis/spkgbuild
+changelog "python/python-hypothesis/spkgbuild" "Upgraded from version 6.151.6 to version 6.151.9"
+sed -i -e "s/version=8.3.0/version=9.0.0/g" python/python-openstackclient/spkgbuild
+changelog "python/python-openstackclient/spkgbuild" "Upgraded from version 8.3.0 to version 9.0.0"
+sed -i -e "s/version=4.9.0/version=4.10.0/g" python/python-openstacksdk/spkgbuild
+changelog "python/python-openstacksdk/spkgbuild" "Upgraded from version 4.9.0 to version 4.10.0"
+sed -i -e "s/version=4.3.0/version=4.4.0/g" python/python-osc-lib/spkgbuild
+changelog "python/python-osc-lib/spkgbuild" "Upgraded from version 4.3.0 to version 4.4.0"
+sed -i -e "s/version=10.2.0/version=10.3.0/g" python/python-oslo-config/spkgbuild
+changelog "python/python-oslo-config/spkgbuild" "Upgraded from version 10.2.0 to version 10.3.0"
+sed -i -e "s/version=6.2.0/version=6.3.0/g" python/python-oslo-context/spkgbuild
+changelog "python/python-oslo-context/spkgbuild" "Upgraded from version 6.2.0 to version 6.3.0"
+sed -i -e "s/version=6.7.1/version=6.7.2/g" python/python-oslo-i18n/spkgbuild
+changelog "python/python-oslo-i18n/spkgbuild" "Upgraded from version 6.7.1 to version 6.7.2"
+sed -i -e "s/version=5.9.0/version=5.9.1/g" python/python-oslo-serialization/spkgbuild
+changelog "python/python-oslo-serialization/spkgbuild" "Upgraded from version 5.9.0 to version 5.9.1"
+sed -i -e "s/version=0.23.1/version=0.24.0/g" python/python-typer/spkgbuild
+changelog "python/python-typer/spkgbuild" "Upgraded from version 0.23.1 to version 0.24.0"
+sed -i -e "s/version=6.1/version=6.1.1/g" python/python-zope-deferredimport/spkgbuild
+changelog "python/python-zope-deferredimport/spkgbuild" "Upgraded from version 6.1 to version 6.1.1"
