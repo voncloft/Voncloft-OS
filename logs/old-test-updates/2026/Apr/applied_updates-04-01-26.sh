d3028ee888b8032dd255e02b353eb988ffbfacf3
Voncloft <voncloft@gmail.com>
2026-04-01 14:22:54 -0400

    Minor Updates

---
+++ logs/old-test-updates/2026/Apr/applied_updates-04-01-26.sh
@@ -0,0 +1,14 @@
+sed -i -e "s/version=5.8.2/version=5.8.3/g" core/xz/spkgbuild
+changelog "core/xz/spkgbuild" "Upgraded from version 5.8.2 to version 5.8.3"
+sed -i -e "s/version=2.7.0/version=2.7.1/g" networking/openvpn/spkgbuild
+changelog "networking/openvpn/spkgbuild" "Upgraded from version 2.7.0 to version 2.7.1"
+sed -i -e "s/version=1.42.78/version=1.42.80/g" python/python-boto3/spkgbuild
+changelog "python/python-boto3/spkgbuild" "Upgraded from version 1.42.78 to version 1.42.80"
+sed -i -e "s/version=1.42.78/version=1.42.80/g" python/python-botocore/spkgbuild
+changelog "python/python-botocore/spkgbuild" "Upgraded from version 1.42.78 to version 1.42.80"
+sed -i -e "s/version=1.47.1/version=1.48.0/g" python/python-cfn-lint/spkgbuild
+changelog "python/python-cfn-lint/spkgbuild" "Upgraded from version 1.47.1 to version 1.48.0"
+sed -i -e "s/version=3.11.7/version=3.11.8/g" python/python-orjson/spkgbuild
+changelog "python/python-orjson/spkgbuild" "Upgraded from version 3.11.7 to version 3.11.8"
+sed -i -e "s/version=1.11.0/version=1.11.1/g" python/python-pipx/spkgbuild
+changelog "python/python-pipx/spkgbuild" "Upgraded from version 1.11.0 to version 1.11.1"
