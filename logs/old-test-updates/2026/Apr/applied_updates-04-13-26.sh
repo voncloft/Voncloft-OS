1f6426ffd56c5930114853a4c19727592268e7df
Voncloft <voncloft@gmail.com>
2026-04-13 18:37:27 -0400

    Minor Updates

---
+++ logs/old-test-updates/2026/Apr/applied_updates-04-13-26.sh
@@ -0,0 +1,44 @@
+sed -i -e "s/version=128.1/version=140.0/g" cinnamon/cjs/spkgbuild
+changelog "cinnamon/cjs/spkgbuild" "Upgraded from version 128.1 to version 140.0"
+sed -i -e "s/version=4.13.2/version=4.13.3/g" compilers/ccache/spkgbuild
+changelog "compilers/ccache/spkgbuild" "Upgraded from version 4.13.2 to version 4.13.3"
+sed -i -e "s/version=6.1.2/version=6.1.3/g" core/giflib/spkgbuild
+changelog "core/giflib/spkgbuild" "Upgraded from version 6.1.2 to version 6.1.3"
+sed -i -e "s/version=2.52.1/version=2.52.2/g" core/webkitgtk/spkgbuild
+changelog "core/webkitgtk/spkgbuild" "Upgraded from version 2.52.1 to version 2.52.2"
+sed -i -e "s/version=3.19.10/version=3.19.11/g" extra/flexget/spkgbuild
+changelog "extra/flexget/spkgbuild" "Upgraded from version 3.19.10 to version 3.19.11"
+sed -i -e "s/version=3.8.6/version=3.8.7/g" libs/libarchive/spkgbuild
+changelog "libs/libarchive/spkgbuild" "Upgraded from version 3.8.6 to version 3.8.7"
+sed -i -e "s/version=1.0.3/version=1.0.4/g" libs/libmicrohttpd/spkgbuild
+changelog "libs/libmicrohttpd/spkgbuild" "Upgraded from version 1.0.3 to version 1.0.4"
+sed -i -e "s/version=150.0b8/version=150.0b9/g" nonfree/firefox/spkgbuild
+changelog "nonfree/firefox/spkgbuild" "Upgraded from version 150.0b8 to version 150.0b9"
+sed -i -e "s/version=1.42.88/version=1.42.89/g" python/python-botocore/spkgbuild
+changelog "python/python-botocore/spkgbuild" "Upgraded from version 1.42.88 to version 1.42.89"
+sed -i -e "s/version=8.6.3/version=8.6.4/g" python/python-cx-freeze/spkgbuild
+changelog "python/python-cx-freeze/spkgbuild" "Upgraded from version 8.6.3 to version 8.6.4"
+sed -i -e "s/version=65.15.0/version=65.16.0/g" python/python-django-allauth/spkgbuild
+changelog "python/python-django-allauth/spkgbuild" "Upgraded from version 65.15.0 to version 65.16.0"
+sed -i -e "s/version=6.151.12/version=6.151.13/g" python/python-hypothesis/spkgbuild
+changelog "python/python-hypothesis/spkgbuild" "Upgraded from version 6.151.12 to version 6.151.13"
+sed -i -e "s/version=1.7.2/version=1.8.0/g" python/python-kikit/spkgbuild
+changelog "python/python-kikit/spkgbuild" "Upgraded from version 1.7.2 to version 1.8.0"
+sed -i -e "s/version=6.0.3/version=6.0.4/g" python/python-lxml/spkgbuild
+changelog "python/python-lxml/spkgbuild" "Upgraded from version 6.0.3 to version 6.0.4"
+sed -i -e "s/version=4.10.0/version=4.11.0/g" python/python-openstacksdk/spkgbuild
+changelog "python/python-openstacksdk/spkgbuild" "Upgraded from version 4.10.0 to version 4.11.0"
+sed -i -e "s/version=7.4.0/version=7.4.1/g" python/python-oslo-concurrency/spkgbuild
+changelog "python/python-oslo-concurrency/spkgbuild" "Upgraded from version 7.4.0 to version 7.4.1"
+sed -i -e "s/version=9.0.27/version=9.0.28/g" python/python-phonenumbers/spkgbuild
+changelog "python/python-phonenumbers/spkgbuild" "Upgraded from version 9.0.27 to version 9.0.28"
+sed -i -e "s/version=2.3.3/version=2.3.4/g" python/python-poetry/spkgbuild
+changelog "python/python-poetry/spkgbuild" "Upgraded from version 2.3.3 to version 2.3.4"
+sed -i -e "s/version=14.3.4/version=15.0.0/g" python/python-rich/spkgbuild
+changelog "python/python-rich/spkgbuild" "Upgraded from version 14.3.4 to version 15.0.0"
+sed -i -e "s/version=0.9.39/version=0.9.40/g" ruby-gems/ruby-yard/spkgbuild
+changelog "ruby-gems/ruby-yard/spkgbuild" "Upgraded from version 0.9.39 to version 0.9.40"
+sed -i -e "s/version=3.16.74/version=3.16.75/g" server/blacklist-hosts/spkgbuild
+changelog "server/blacklist-hosts/spkgbuild" "Upgraded from version 3.16.74 to version 3.16.75"
+sed -i -e "s/version=3.7.0/version=3.7.1/g" server/radicale/spkgbuild
+changelog "server/radicale/spkgbuild" "Upgraded from version 3.7.0 to version 3.7.1"
