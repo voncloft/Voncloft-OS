c2fb97a0c3560ce9dec40bf65189c14320dd86cd
Voncloft <voncloft@gmail.com>
2026-06-10 07:10:51 -0400

    Minor Updates

---
+++ logs/old-test-updates/2026/Jun/applied_updates-06-10-26.sh
@@ -0,0 +1,50 @@
+sed -i -e "s/version=26.1.1/version=26.1.2/g" hardware/mesa/spkgbuild
+changelog "hardware/mesa/spkgbuild" "Upgraded from version 26.1.1 to version 26.1.2"
+sed -i -e "s/version=2.46.0/version=2.46.1/g" core/binutils/spkgbuild
+changelog "core/binutils/spkgbuild" "Upgraded from version 2.46.0 to version 2.46.1"
+sed -i -e "s/version=0.29.3/version=0.29.4/g" core/cbindgen/spkgbuild
+changelog "core/cbindgen/spkgbuild" "Upgraded from version 0.29.3 to version 0.29.4"
+sed -i -e "s/version=5.47/version=5.48/g" core/file/spkgbuild
+changelog "core/file/spkgbuild" "Upgraded from version 5.47 to version 5.48"
+sed -i -e "s/version=0.162.1/version=0.163.0/g" core/hugo/spkgbuild
+changelog "core/hugo/spkgbuild" "Upgraded from version 0.162.1 to version 0.163.0"
+sed -i -e "s/version=3.4.3/version=3.4.4/g" core/rsync/spkgbuild
+changelog "core/rsync/spkgbuild" "Upgraded from version 3.4.3 to version 3.4.4"
+sed -i -e "s/version=2.0.12/version=2.0.13/g" libs/libtorrent/spkgbuild
+changelog "libs/libtorrent/spkgbuild" "Upgraded from version 2.0.12 to version 2.0.13"
+sed -i -e "s/version=2.0.12/version=2.0.13/g" libs/libtorrent-rasterbar/spkgbuild
+changelog "libs/libtorrent-rasterbar/spkgbuild" "Upgraded from version 2.0.12 to version 2.0.13"
+sed -i -e "s/version=4.21.7/version=4.21.8/g" libs/libxfce4ui/spkgbuild
+changelog "libs/libxfce4ui/spkgbuild" "Upgraded from version 4.21.7 to version 4.21.8"
+sed -i -e "s/version=4.0.0/version=4.0.1/g" networking/openssl/spkgbuild
+changelog "networking/openssl/spkgbuild" "Upgraded from version 4.0.0 to version 4.0.1"
+sed -i -e "s/version=152.0b8/version=152.0b9/g" nonfree/firefox/spkgbuild
+changelog "nonfree/firefox/spkgbuild" "Upgraded from version 152.0b8 to version 152.0b9"
+sed -i -e "s/version=1.43.24/version=1.43.26/g" python/python-boto3/spkgbuild
+changelog "python/python-boto3/spkgbuild" "Upgraded from version 1.43.24 to version 1.43.26"
+sed -i -e "s/version=1.43.24/version=1.43.26/g" python/python-botocore/spkgbuild
+changelog "python/python-botocore/spkgbuild" "Upgraded from version 1.43.24 to version 1.43.26"
+sed -i -e "s/version=2.6.3/version=2.6.4/g" python/python-gphoto2/spkgbuild
+changelog "python/python-gphoto2/spkgbuild" "Upgraded from version 2.6.3 to version 2.6.4"
+sed -i -e "s/version=4.14.0/version=4.15.0/g" python/python-openstacksdk/spkgbuild
+changelog "python/python-openstacksdk/spkgbuild" "Upgraded from version 4.14.0 to version 4.15.0"
+sed -i -e "s/version=10.1.0/version=10.1.1/g" python/python-oslo-utils/spkgbuild
+changelog "python/python-oslo-utils/spkgbuild" "Upgraded from version 10.1.0 to version 10.1.1"
+sed -i -e "s/version=2026.6.1/version=2026.6.2/g" python/python-pipenv/spkgbuild
+changelog "python/python-pipenv/spkgbuild" "Upgraded from version 2026.6.1 to version 2026.6.2"
+sed -i -e "s/version=7.8.1/version=8.0.0/g" python/python-praw/spkgbuild
+changelog "python/python-praw/spkgbuild" "Upgraded from version 7.8.1 to version 8.0.0"
+sed -i -e "s/version=3.1.0/version=3.2.0/g" python/python-prawcore/spkgbuild
+changelog "python/python-prawcore/spkgbuild" "Upgraded from version 3.1.0 to version 3.2.0"
+sed -i -e "s/version=1.1.0/version=1.1.1/g" python/python-pytest-aiohttp/spkgbuild
+changelog "python/python-pytest-aiohttp/spkgbuild" "Upgraded from version 1.1.0 to version 1.1.1"
+sed -i -e "s/version=6.5.6/version=6.5.7/g" python/python-tornado/spkgbuild
+changelog "python/python-tornado/spkgbuild" "Upgraded from version 6.5.6 to version 6.5.7"
+sed -i -e "s/version=4.68.1/version=4.68.2/g" python/python-tqdm/spkgbuild
+changelog "python/python-tqdm/spkgbuild" "Upgraded from version 4.68.1 to version 4.68.2"
+sed -i -e "s/version=2.4.67/version=2.4.68/g" server/httpd/spkgbuild
+changelog "server/httpd/spkgbuild" "Upgraded from version 2.4.67 to version 2.4.68"
+sed -i -e "s/version=4.21.1/version=4.21.2/g" xfce/xfce4-panel/spkgbuild
+changelog "xfce/xfce4-panel/spkgbuild" "Upgraded from version 4.21.1 to version 4.21.2"
+sed -i -e "s/version=1.21.2/version=1.22.0/g" xorg/xdg-desktop-portal/spkgbuild
+changelog "xorg/xdg-desktop-portal/spkgbuild" "Upgraded from version 1.21.2 to version 1.22.0"
