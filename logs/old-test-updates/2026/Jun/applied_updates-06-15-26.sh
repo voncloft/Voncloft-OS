ba5ed145656603c1d4b564e56a765cebdfd36d6d
Voncloft <voncloft@gmail.com>
2026-06-15 08:48:57 -0400

    Minor Updates

---
+++ logs/old-test-updates/2026/Jun/applied_updates-06-15-26.sh
@@ -0,0 +1,30 @@
+sed -i -e "s/version=3.0.1.2/version=3.0.1.3/g" extra/discount/spkgbuild
+changelog "extra/discount/spkgbuild" "Upgraded from version 3.0.1.2 to version 3.0.1.3"
+sed -i -e "s/version=1.2.16/version=1.2.16.1/g" libs/alsa-lib/spkgbuild
+changelog "libs/alsa-lib/spkgbuild" "Upgraded from version 1.2.16 to version 1.2.16.1"
+sed -i -e "s/version=4.0.2/version=4.0.3/g" libs/libical/spkgbuild
+changelog "libs/libical/spkgbuild" "Upgraded from version 4.0.2 to version 4.0.3"
+sed -i -e "s/version=3.3.1/version=3.3.2/g" python/python-aiodiscover/spkgbuild
+changelog "python/python-aiodiscover/spkgbuild" "Upgraded from version 3.3.1 to version 3.3.2"
+sed -i -e "s/version=3.28.2/version=3.29.0/g" python/python-aiogram/spkgbuild
+changelog "python/python-aiogram/spkgbuild" "Upgraded from version 3.28.2 to version 3.29.0"
+sed -i -e "s/version=5.1.1/version=5.1.2/g" python/python-elementpath/spkgbuild
+changelog "python/python-elementpath/spkgbuild" "Upgraded from version 5.1.1 to version 5.1.2"
+sed -i -e "s/version=0.136.3/version=0.137.0/g" python/python-fastapi/spkgbuild
+changelog "python/python-fastapi/spkgbuild" "Upgraded from version 0.136.3 to version 0.137.0"
+sed -i -e "s/version=3.29.3/version=3.29.4/g" python/python-filelock/spkgbuild
+changelog "python/python-filelock/spkgbuild" "Upgraded from version 3.29.3 to version 3.29.4"
+sed -i -e "s/version=4.0.7/version=4.0.8/g" python/python-peewee/spkgbuild
+changelog "python/python-peewee/spkgbuild" "Upgraded from version 4.0.7 to version 4.0.8"
+sed -i -e "s/version=3.2.1/version=4.0.0/g" python/python-prawcore/spkgbuild
+changelog "python/python-prawcore/spkgbuild" "Upgraded from version 3.2.1 to version 4.0.0"
+sed -i -e "s/version=1.19.2/version=1.19.3/g" python/python-pygit2/spkgbuild
+changelog "python/python-pygit2/spkgbuild" "Upgraded from version 1.19.2 to version 1.19.3"
+sed -i -e "s/version=4.0.5/version=4.0.6/g" python/python-pylint/spkgbuild
+changelog "python/python-pylint/spkgbuild" "Upgraded from version 4.0.5 to version 4.0.6"
+sed -i -e "s/version=21.4.3/version=21.5.0/g" python/python-virtualenv/spkgbuild
+changelog "python/python-virtualenv/spkgbuild" "Upgraded from version 21.4.3 to version 21.5.0"
+sed -i -e "s/version=2026.6.2/version=2026.6.3/g" server/homeassistant/spkgbuild
+changelog "server/homeassistant/spkgbuild" "Upgraded from version 2026.6.2 to version 2026.6.3"
+sed -i -e "s/version=3.7.4/version=3.7.5/g" server/radicale/spkgbuild
+changelog "server/radicale/spkgbuild" "Upgraded from version 3.7.4 to version 3.7.5"
