ca05cc25cbbed6886d81acef966371b833a993f9
Voncloft <voncloft@gmail.com>
2026-06-29 19:58:26 -0400

    Minor Updates

---
+++ logs/old-test-updates/2026/Jun/applied_updates-06-29-26.sh
@@ -0,0 +1,68 @@
+sed -i -e "s/version=2.9.91/version=2.9.95/g" compilers/pkgconf/spkgbuild
+changelog "compilers/pkgconf/spkgbuild" "Upgraded from version 2.9.91 to version 2.9.95"
+sed -i -e "s/version=2.3.2/version=2.4.0/g" core/acl/spkgbuild
+changelog "core/acl/spkgbuild" "Upgraded from version 2.3.2 to version 2.4.0"
+sed -i -e "s/version=2.5.2/version=2.6.0/g" core/attr/spkgbuild
+changelog "core/attr/spkgbuild" "Upgraded from version 2.5.2 to version 2.6.0"
+sed -i -e "s/version=7.2.2/version=7.2.3/g" core/mercurial/spkgbuild
+changelog "core/mercurial/spkgbuild" "Upgraded from version 7.2.2 to version 7.2.3"
+sed -i -e "s/version=1.8.1.2/version=1.8.1.3/g" core/socat/spkgbuild
+changelog "core/socat/spkgbuild" "Upgraded from version 1.8.1.2 to version 1.8.1.3"
+sed -i -e "s/version=260.2/version=261.1/g" core/systemd/spkgbuild
+changelog "core/systemd/spkgbuild" "Upgraded from version 260.2 to version 261.1"
+sed -i -e "s/version=3.6/version=3.7/g" core/tmux/spkgbuild
+changelog "core/tmux/spkgbuild" "Upgraded from version 3.6 to version 3.7"
+sed -i -e "s/version=2.47.2/version=2.47.3/g" extra/discordchatexporter/spkgbuild
+changelog "extra/discordchatexporter/spkgbuild" "Upgraded from version 2.47.2 to version 2.47.3"
+sed -i -e "s/version=3.19.25/version=3.19.26/g" extra/flexget/spkgbuild
+changelog "extra/flexget/spkgbuild" "Upgraded from version 3.19.25 to version 3.19.26"
+sed -i -e "s/version=5.15/version=5.16/g" extra/gnucash/spkgbuild
+changelog "extra/gnucash/spkgbuild" "Upgraded from version 5.15 to version 5.16"
+sed -i -e "s/version=7.2.0/version=7.3.0/g" fonts/awesome-font/spkgbuild
+changelog "fonts/awesome-font/spkgbuild" "Upgraded from version 7.2.0 to version 7.3.0"
+sed -i -e "s/version=1.29.1/version=1.29.2/g" media/gst-libav/spkgbuild
+changelog "media/gst-libav/spkgbuild" "Upgraded from version 1.29.1 to version 1.29.2"
+sed -i -e "s/version=1.29.1/version=1.29.2/g" media/gst-plugins-bad/spkgbuild
+changelog "media/gst-plugins-bad/spkgbuild" "Upgraded from version 1.29.1 to version 1.29.2"
+sed -i -e "s/version=1.29.1/version=1.29.2/g" media/gst-plugins-base/spkgbuild
+changelog "media/gst-plugins-base/spkgbuild" "Upgraded from version 1.29.1 to version 1.29.2"
+sed -i -e "s/version=1.29.1/version=1.29.2/g" media/gst-plugins-good/spkgbuild
+changelog "media/gst-plugins-good/spkgbuild" "Upgraded from version 1.29.1 to version 1.29.2"
+sed -i -e "s/version=1.29.1/version=1.29.2/g" media/gst-plugins-ugly/spkgbuild
+changelog "media/gst-plugins-ugly/spkgbuild" "Upgraded from version 1.29.1 to version 1.29.2"
+sed -i -e "s/version=1.29.1/version=1.29.2/g" media/gstreamer/spkgbuild
+changelog "media/gstreamer/spkgbuild" "Upgraded from version 1.29.1 to version 1.29.2"
+sed -i -e "s/version=1.0.0.86/version=1.0.0.87/g" multilib/steam/spkgbuild
+changelog "multilib/steam/spkgbuild" "Upgraded from version 1.0.0.86 to version 1.0.0.87"
+sed -i -e "s/version=2.54.0/version=2.55.0/g" networking/git/spkgbuild
+changelog "networking/git/spkgbuild" "Upgraded from version 2.54.0 to version 2.55.0"
+sed -i -e "s/version=0.4.9.10/version=0.4.9.11/g" networking/tor/spkgbuild
+changelog "networking/tor/spkgbuild" "Upgraded from version 0.4.9.10 to version 0.4.9.11"
+sed -i -e "s/version=153.0b4/version=153.0b6/g" nonfree/firefox/spkgbuild
+changelog "nonfree/firefox/spkgbuild" "Upgraded from version 153.0b4 to version 153.0b6"
+sed -i -e "s/version=1.52.0/version=1.52.1/g" python/python-cfn-lint/spkgbuild
+changelog "python/python-cfn-lint/spkgbuild" "Upgraded from version 1.52.0 to version 1.52.1"
+sed -i -e "s/version=5.1.2/version=5.1.3/g" python/python-elementpath/spkgbuild
+changelog "python/python-elementpath/spkgbuild" "Upgraded from version 5.1.2 to version 5.1.3"
+sed -i -e "s/version=0.138.1/version=0.138.2/g" python/python-fastapi/spkgbuild
+changelog "python/python-fastapi/spkgbuild" "Upgraded from version 0.138.1 to version 0.138.2"
+sed -i -e "s/version=0.31.2/version=0.32.0/g" python/python-httplib2/spkgbuild
+changelog "python/python-httplib2/spkgbuild" "Upgraded from version 0.31.2 to version 0.32.0"
+sed -i -e "s/version=0.58.1/version=0.60.0/g" python/python-libtmux/spkgbuild
+changelog "python/python-libtmux/spkgbuild" "Upgraded from version 0.58.1 to version 0.60.0"
+sed -i -e "s/version=4.1.0/version=4.1.1/g" python/python-peewee/spkgbuild
+changelog "python/python-peewee/spkgbuild" "Upgraded from version 4.1.0 to version 4.1.1"
+sed -i -e "s/version=7.44.1/version=7.47.0/g" python/python-pycurl/spkgbuild
+changelog "python/python-pycurl/spkgbuild" "Upgraded from version 7.44.1 to version 7.47.0"
+sed -i -e "s/version=7.17.0/version=7.18.0/g" python/python-qiniu/spkgbuild
+changelog "python/python-qiniu/spkgbuild" "Upgraded from version 7.17.0 to version 7.18.0"
+sed -i -e "s/version=0.26.7/version=0.26.8/g" python/python-typer/spkgbuild
+changelog "python/python-typer/spkgbuild" "Upgraded from version 0.26.7 to version 0.26.8"
+sed -i -e "s/version=3.7.1/version=3.8.0/g" python/python-xxhash/spkgbuild
+changelog "python/python-xxhash/spkgbuild" "Upgraded from version 3.7.1 to version 3.8.0"
+sed -i -e "s/version=3.16.91/version=3.16.92/g" server/blacklist-hosts/spkgbuild
+changelog "server/blacklist-hosts/spkgbuild" "Upgraded from version 3.16.91 to version 3.16.92"
+sed -i -e "s/version=1.1.1/version=1.1.2/g" xfce/xfontsel/spkgbuild
+changelog "xfce/xfontsel/spkgbuild" "Upgraded from version 1.1.1 to version 1.1.2"
+sed -i -e "s/version=1.1.1/version=1.2.0/g" xorg/xclock/spkgbuild
+changelog "xorg/xclock/spkgbuild" "Upgraded from version 1.1.1 to version 1.2.0"
