3cc5cd168ba6f02066f3e6881c7ebdde6d198715
Voncloft <voncloft@gmail.com>
2026-09-10 20:07:47 -0400

    Minor Updates

---
+++ logs/old-test-updates/2026/Sep/applied_updates-09-10-26.sh
@@ -0,0 +1,54 @@
+sed -i -e "s/version=1.18.1/version=1.19/g" compilers/automake/spkgbuild
+changelog "compilers/automake/spkgbuild" "Upgraded from version 1.18.1 to version 1.19"
+sed -i -e "s/version=3.18.2/version=3.18.3/g" core/fuse2/spkgbuild
+changelog "core/fuse2/spkgbuild" "Upgraded from version 3.18.2 to version 3.18.3"
+sed -i -e "s/version=3.18.2/version=3.18.3/g" core/fuse3/spkgbuild
+changelog "core/fuse3/spkgbuild" "Upgraded from version 3.18.2 to version 3.18.3"
+sed -i -e "s/version=0.165.0/version=0.166.0/g" core/hugo/spkgbuild
+changelog "core/hugo/spkgbuild" "Upgraded from version 0.165.0 to version 0.166.0"
+sed -i -e "s/version=3.128/version=3.129/g" core/nss/spkgbuild
+changelog "core/nss/spkgbuild" "Upgraded from version 3.128 to version 3.129"
+sed -i -e "s/version=5.8.3/version=5.8.4/g" core/xz/spkgbuild
+changelog "core/xz/spkgbuild" "Upgraded from version 5.8.3 to version 5.8.4"
+sed -i -e "s/version=26.0.99.901/version=26.0.99.902/g" libs/xwayland/spkgbuild
+changelog "libs/xwayland/spkgbuild" "Upgraded from version 26.0.99.901 to version 26.0.99.902"
+sed -i -e "s/version=156.0b4/version=156.0b5/g" nonfree/firefox/spkgbuild
+changelog "nonfree/firefox/spkgbuild" "Upgraded from version 156.0b4 to version 156.0b5"
+sed -i -e "s/version=610.57.04/version=615.71.09/g" nonfree/nvidia/spkgbuild
+changelog "nonfree/nvidia/spkgbuild" "Upgraded from version 610.57.04 to version 615.71.09"
+sed -i -e "s/version=610.57.04/version=615.71.09/g" nonfree/nvidia-32/spkgbuild
+changelog "nonfree/nvidia-32/spkgbuild" "Upgraded from version 610.57.04 to version 615.71.09"
+sed -i -e "s/version=610.57.04/version=615.71.09/g" nonfree/nvidia-modules/spkgbuild
+changelog "nonfree/nvidia-modules/spkgbuild" "Upgraded from version 610.57.04 to version 615.71.09"
+sed -i -e "s/version=156.0b2/version=156.0b3/g" nonfree/thunderbird/spkgbuild
+changelog "nonfree/thunderbird/spkgbuild" "Upgraded from version 156.0b2 to version 156.0b3"
+sed -i -e "s/version=1.43.90/version=1.43.92/g" python/python-boto3/spkgbuild
+changelog "python/python-boto3/spkgbuild" "Upgraded from version 1.43.90 to version 1.43.92"
+sed -i -e "s/version=1.43.90/version=1.43.92/g" python/python-botocore/spkgbuild
+changelog "python/python-botocore/spkgbuild" "Upgraded from version 1.43.90 to version 1.43.92"
+sed -i -e "s/version=1.56.1/version=1.56.3/g" python/python-cfn-lint/spkgbuild
+changelog "python/python-cfn-lint/spkgbuild" "Upgraded from version 1.56.1 to version 1.56.3"
+sed -i -e "s/version=3.32.5/version=3.32.6/g" python/python-filelock/spkgbuild
+changelog "python/python-filelock/spkgbuild" "Upgraded from version 3.32.5 to version 3.32.6"
+sed -i -e "s/version=4.64.0/version=4.65.0/g" python/python-fonttools/spkgbuild
+changelog "python/python-fonttools/spkgbuild" "Upgraded from version 4.64.0 to version 4.65.0"
+sed -i -e "s/version=6.7.1/version=6.8.0/g" python/python-multidict/spkgbuild
+changelog "python/python-multidict/spkgbuild" "Upgraded from version 6.7.1 to version 6.8.0"
+sed -i -e "s/version=18.13.0/version=18.13.1/g" python/python-novaclient/spkgbuild
+changelog "python/python-novaclient/spkgbuild" "Upgraded from version 18.13.0 to version 18.13.1"
+sed -i -e "s/version=8.3.1/version=8.3.2/g" python/python-oslo-log/spkgbuild
+changelog "python/python-oslo-log/spkgbuild" "Upgraded from version 8.3.1 to version 8.3.2"
+sed -i -e "s/version=9.0.38/version=9.0.39/g" python/python-phonenumbers/spkgbuild
+changelog "python/python-phonenumbers/spkgbuild" "Upgraded from version 9.0.38 to version 9.0.39"
+sed -i -e "s/version=1.9.0/version=1.9.1/g" python/python-scikit-learn/spkgbuild
+changelog "python/python-scikit-learn/spkgbuild" "Upgraded from version 1.9.0 to version 1.9.1"
+sed -i -e "s/version=21.7.8/version=21.7.9/g" python/python-virtualenv/spkgbuild
+changelog "python/python-virtualenv/spkgbuild" "Upgraded from version 21.7.8 to version 21.7.9"
+sed -i -e "s/version=4.0.20/version=4.1.0/g" ruby-gems/ruby-bundler/spkgbuild
+changelog "ruby-gems/ruby-bundler/spkgbuild" "Upgraded from version 4.0.20 to version 4.1.0"
+sed -i -e "s/version=3.16.112/version=3.16.113/g" server/blacklist-hosts/spkgbuild
+changelog "server/blacklist-hosts/spkgbuild" "Upgraded from version 3.16.112 to version 3.16.113"
+sed -i -e "s/version=4.24.6/version=4.24.7/g" server/samba/spkgbuild
+changelog "server/samba/spkgbuild" "Upgraded from version 4.24.6 to version 4.24.7"
+sed -i -e "s/version=26.0.99.901/version=26.0.99.902/g" xorg/xorg-server/spkgbuild
+changelog "xorg/xorg-server/spkgbuild" "Upgraded from version 26.0.99.901 to version 26.0.99.902"
