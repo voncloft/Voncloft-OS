bd53c4d2477a00b3110b8412228d9fde05741256
Voncloft <voncloft@gmail.com>
2026-09-14 22:33:30 -0400

    w2i8fk7j

---
+++ logs/old-test-updates/2026/Sep/applied_updates-09-14-26.sh
@@ -0,0 +1,52 @@
+sed -i -e "s/version=9.11/version=9.12/g" core/coreutils/spkgbuild
+changelog "core/coreutils/spkgbuild" "Upgraded from version 9.11 to version 9.12"
+sed -i -e "s/version=261.2/version=261.3/g" core/systemd/spkgbuild
+changelog "core/systemd/spkgbuild" "Upgraded from version 261.2 to version 261.3"
+sed -i -e "s/version=3.20.11/version=3.20.13/g" extra/flexget/spkgbuild
+changelog "extra/flexget/spkgbuild" "Upgraded from version 3.20.11 to version 3.20.13"
+sed -i -e "s/version=1.12.3/version=1.12.4/g" libs/libgcrypt/spkgbuild
+changelog "libs/libgcrypt/spkgbuild" "Upgraded from version 1.12.3 to version 1.12.4"
+sed -i -e "s/version=1.10.7/version=1.11.0/g" libs/libpcap/spkgbuild
+changelog "libs/libpcap/spkgbuild" "Upgraded from version 1.10.7 to version 1.11.0"
+sed -i -e "s/version=1.2.2/version=1.3.0/g" libs/snappy/spkgbuild
+changelog "libs/snappy/spkgbuild" "Upgraded from version 1.2.2 to version 1.3.0"
+sed -i -e "s/version=156.0b5/version=157.0b1/g" nonfree/firefox/spkgbuild
+changelog "nonfree/firefox/spkgbuild" "Upgraded from version 156.0b5 to version 157.0b1"
+sed -i -e "s/version=156.0b3/version=157.0b1/g" nonfree/thunderbird/spkgbuild
+changelog "nonfree/thunderbird/spkgbuild" "Upgraded from version 156.0b3 to version 157.0b1"
+sed -i -e "s/version=1.43.92/version=1.43.93/g" python/python-boto3/spkgbuild
+changelog "python/python-boto3/spkgbuild" "Upgraded from version 1.43.92 to version 1.43.93"
+sed -i -e "s/version=1.43.92/version=1.43.93/g" python/python-botocore/spkgbuild
+changelog "python/python-botocore/spkgbuild" "Upgraded from version 1.43.92 to version 1.43.93"
+sed -i -e "s/version=7.16.0/version=7.16.1/g" python/python-coverage/spkgbuild
+changelog "python/python-coverage/spkgbuild" "Upgraded from version 7.16.0 to version 7.16.1"
+sed -i -e "s/version=65.19.2/version=65.19.3/g" python/python-django-allauth/spkgbuild
+changelog "python/python-django-allauth/spkgbuild" "Upgraded from version 65.19.2 to version 65.19.3"
+sed -i -e "s/version=40.38.0/version=40.39.0/g" python/python-faker/spkgbuild
+changelog "python/python-faker/spkgbuild" "Upgraded from version 40.38.0 to version 40.39.0"
+sed -i -e "s/version=2.23.0/version=2.24.0/g" python/python-hcloud/spkgbuild
+changelog "python/python-hcloud/spkgbuild" "Upgraded from version 2.23.0 to version 2.24.0"
+sed -i -e "s/version=3.1.1/version=3.2.0/g" python/python-maxminddb/spkgbuild
+changelog "python/python-maxminddb/spkgbuild" "Upgraded from version 3.1.1 to version 3.2.0"
+sed -i -e "s/version=3.45.0/version=3.45.1/g" python/python-openmdao/spkgbuild
+changelog "python/python-openmdao/spkgbuild" "Upgraded from version 3.45.0 to version 3.45.1"
+sed -i -e "s/version=0.13.21/version=0.13.23/g" python/python-pybuilder/spkgbuild
+changelog "python/python-pybuilder/spkgbuild" "Upgraded from version 0.13.21 to version 0.13.23"
+sed -i -e "s/version=1.20.0/version=1.20.1/g" python/python-pygit2/spkgbuild
+changelog "python/python-pygit2/spkgbuild" "Upgraded from version 1.20.0 to version 1.20.1"
+sed -i -e "s/version=2.13.0/version=2.14.0/g" python/python-pyjwt/spkgbuild
+changelog "python/python-pyjwt/spkgbuild" "Upgraded from version 2.13.0 to version 2.14.0"
+sed -i -e "s/version=1.0.8/version=1.0.9/g" python/python-rencode/spkgbuild
+changelog "python/python-rencode/spkgbuild" "Upgraded from version 1.0.8 to version 1.0.9"
+sed -i -e "s/version=3.0.4/version=3.1.0/g" python/python-semver/spkgbuild
+changelog "python/python-semver/spkgbuild" "Upgraded from version 3.0.4 to version 3.1.0"
+sed -i -e "s/version=6.5.8/version=6.5.9/g" python/python-tornado/spkgbuild
+changelog "python/python-tornado/spkgbuild" "Upgraded from version 6.5.8 to version 6.5.9"
+sed -i -e "s/version=4.70.0/version=4.70.1/g" python/python-tqdm/spkgbuild
+changelog "python/python-tqdm/spkgbuild" "Upgraded from version 4.70.0 to version 4.70.1"
+sed -i -e "s/version=3.16.113/version=3.16.114/g" server/blacklist-hosts/spkgbuild
+changelog "server/blacklist-hosts/spkgbuild" "Upgraded from version 3.16.113 to version 3.16.114"
+sed -i -e "s/version=2026.9.1/version=2026.9.2/g" server/homeassistant/spkgbuild
+changelog "server/homeassistant/spkgbuild" "Upgraded from version 2026.9.1 to version 2026.9.2"
+sed -i -e "s/version=1.43.3.10896/version=1.43.4.10903/g" server/plex-media-server/spkgbuild
+changelog "server/plex-media-server/spkgbuild" "Upgraded from version 1.43.3.10896 to version 1.43.4.10903"
